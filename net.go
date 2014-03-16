ed1df53a6d58439fabeaf109ffb2f129aae784e7
Monica Sarbu <monica@packetbeat.com>
2014-03-16 19:11:09 +0100

    - keep a topology map in elasticsearch with all public IPs that each agent has - at startup, each agent adds its IPs to the topology map (in elasticsearch under packetbeat-topology index) - determine src_server and dst_server for each transaction - ignore duplicated transactions

---
+++ net.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "net"
+)
+
+
+func LocalAddrs() ([]string, error) {
+    var localAddrs = []string{}
+    addrs, err := net.InterfaceAddrs()
+    if err != nil {
+        return nil, err
+    }
+    for _, addr := range addrs {
+        // a bit wtf'ish.. Don't know how to do this otherwise
+        ip, _, err := net.ParseCIDR(addr.String())
+        if err == nil && ip != nil {
+            localAddrs = append(localAddrs, ip.String())
+        }
+    }
+    return localAddrs, nil
+}
