89ea7d367464a10f18f91aa57a61928749237a6a
Pradeep Chhetri <pradeep@Pradeeps-MacBook-Air.local>
2014-05-28 16:00:25 +0530

    Improved the way of getting local addresses

--- net.go
+++ net.go
@@ -11,23 +11,9 @@ func LocalAddrs() ([]string, error) {
         return nil, err
     }
     for _, addr := range addrs {
-        // a bit wtf'ish.. Don't know how to do this otherwise
-        ip, _, err := net.ParseCIDR(addr.String())
-        if ip.IsLoopback() {
-            continue
-        }
-        if err == nil && ip != nil {
-            localAddrs = append(localAddrs, ip.String())
+        if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+            localAddrs = append(localAddrs, ipnet.IP.String())
         }
     }
     return localAddrs, nil
-}
-
-func IsLoopback(ip_str string) (bool, error) {
-
-    ip := net.ParseIP(ip_str)
-    if ip == nil {
-        return false, MsgError("Wrong IP format %s", ip_str)
-    }
-    return ip.IsLoopback(), nil
 }
