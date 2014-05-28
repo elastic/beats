09f686b40dc68107f93fb45e7a21fd1d4bf8d752
Pradeep Chhetri <pradeep@indix.com>
2014-05-28 16:10:57 +0530

    Adding custom IsLookback function back.

--- net.go
+++ net.go
@@ -17,3 +17,11 @@ func LocalAddrs() ([]string, error) {
     }
     return localAddrs, nil
 }
+
+func IsLoopback(ip_str string) (bool, error) {
+    ip := net.ParseIP(ip_str)
+    if ip == nil {
+        return false, MsgError("Wrong IP format %s", ip_str)
+    }
+    return ip.IsLoopback(), nil
+}
