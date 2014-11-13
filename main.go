183b228296b6e1f442fa4f777ade7173965b9710
Tudor Golubenco <tudor@packetbeat.com>
2014-11-13 23:21:01 +0100

    Started the switch to gopacket (#76).
    For now we have an "as is" replacement of gopcap with gopacket, just
    slightly more configurable. Only the sniffer type 'pcap' is currently
    implemented, but 'pfring' and 'afpacket' are to follow.
    
    Didn't yet make use of the layer decoding features of gopacket, this is
    also to follow.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -13,8 +14,9 @@ import (
 	"strings"
 	"time"
 
+	"code.google.com/p/gopacket"
+	"code.google.com/p/gopacket/layers"
 	"github.com/BurntSushi/toml"
-	"github.com/akrennmair/gopcap"
 	"github.com/nranchev/go-libGeoIP"
 )
 
@@ -51,10 +53,6 @@ type tomlConfig struct {
 	Thrift     tomlThrift
 }
 
-type tomlInterfaces struct {
-	Device string
-}
-
 type tomlRunOptions struct {
 	Uid int
 	Gid int
@@ -80,7 +78,7 @@ func Bytes_Ipv4_Ntoa(bytes []byte) string {
 	return strings.Join(strarr, ".")
 }
 
-func decodePktEth(datalink int, pkt *pcap.Packet) {
+func decodePktEth(data []byte, ci *gopacket.CaptureInfo, datalink layers.LinkType) {
 	defer RECOVER("decodePktEth exception")
 
 	packet := new(Packet)
@@ -88,28 +86,30 @@ func decodePktEth(datalink int, pkt *pcap.Packet) {
 	var eth_type uint16
 
 	switch datalink {
-	case pcap.LINKTYPE_ETHERNET: // Ethernet
+	case layers.LinkTypeEthernet:
 		l2hlen = 14
 
 		// bytes 12 and 13 are the pkt type
-		eth_type = Bytes_Ntohs(pkt.Data[12:14])
+		eth_type = Bytes_Ntohs(data[12:14])
 
-	case pcap.LINKTYPE_LINUX_SLL: // linux loopback
+	case layers.LinkTypeLinuxSLL: // linux loopback
 		l2hlen = 16
 
 		// bytes 14 and 15 are the pkt type
-		eth_type = Bytes_Ntohs(pkt.Data[14:16])
+		eth_type = Bytes_Ntohs(data[14:16])
 
-	case pcap.LINKTYPE_NULL: // bsd loopback
+	case layers.LinkTypeNull: // bsd loopback
 		l2hlen = 4
 
-		if Bytes_Htohl(pkt.Data[0:4]) == 2 {
+		if Bytes_Htohl(data[0:4]) == 2 {
 			eth_type = 0x800
 		} else {
 			// unknown
 			eth_type = 0
 		}
-
+	default:
+		DEBUG("pcapread", "Unknown link type")
+		return
 	}
 
 	if eth_type != 0x800 {
@@ -117,24 +117,24 @@ func decodePktEth(datalink int, pkt *pcap.Packet) {
 		return
 	}
 
-	if len(pkt.Data) < l2hlen+20 {
+	if len(data) < l2hlen+20 {
 		DEBUG("ip", "Packet too short to be ethernet")
 		return
 	}
 
 	// IP header
-	iphl := int((uint16(pkt.Data[l2hlen]) & 0x0f) * 4)
+	iphl := int((uint16(data[l2hlen]) & 0x0f) * 4)
 	if iphl < 20 {
 		DEBUG("ip", "IP header shorter than 20 bytes, ignoring")
 		return
 	}
-	if len(pkt.Data) < l2hlen+iphl {
+	if len(data) < l2hlen+iphl {
 		DEBUG("ip", "Packet too short to be IP")
 		return
 	}
-	iphdr := pkt.Data[l2hlen : l2hlen+iphl]
+	iphdr := data[l2hlen : l2hlen+iphl]
 
-	packet.ts = pkt.Time
+	packet.ts = ci.Timestamp
 
 	packet.tuple.Src_ip = Bytes_Ntohl(iphdr[12:16])
 	packet.tuple.Dst_ip = Bytes_Ntohl(iphdr[16:20])
@@ -155,18 +155,18 @@ func decodePktEth(datalink int, pkt *pcap.Packet) {
 		return
 	}
 
-	if len(pkt.Data) < l2hlen+iphl+20 {
+	if len(data) < l2hlen+iphl+20 {
 		DEBUG("ip", "Packet too short to be TCP")
 		return
 	}
 
-	tcphl := int((uint16(pkt.Data[l2hlen+iphl+12]) >> 4) * 4)
-	if tcphl > 20 && len(pkt.Data) < l2hlen+iphl+tcphl {
+	tcphl := int((uint16(data[l2hlen+iphl+12]) >> 4) * 4)
+	if tcphl > 20 && len(data) < l2hlen+iphl+tcphl {
 		DEBUG("ip", "Packet too short to contain TCP header")
 		return
 	}
 
-	tcphdr := pkt.Data[l2hlen+iphl : l2hlen+iphl+tcphl]
+	tcphdr := data[l2hlen+iphl : l2hlen+iphl+tcphl]
 	packet.tuple.Src_port = Bytes_Ntohs(tcphdr[0:2])
 	packet.tuple.Dst_port = Bytes_Ntohs(tcphdr[2:4])
 
@@ -177,12 +177,12 @@ func decodePktEth(datalink int, pkt *pcap.Packet) {
 		return
 	}
 
-	if len(pkt.Data) < l2hlen+ip_length {
+	if len(data) < l2hlen+ip_length {
 		DEBUG("ip", "Captured packet smaller then advertised in IP layer")
 		return
 	}
 
-	packet.payload = pkt.Data[l2hlen+iphl+int(data_offset) : l2hlen+ip_length]
+	packet.payload = data[l2hlen+iphl+int(data_offset) : l2hlen+ip_length]
 
 	FollowTcp(tcphdr, packet)
 }
@@ -265,7 +265,6 @@ func main() {
 		logLevel = LOG_DEBUG
 	}
 
-	var h *pcap.Pcap
 	var err error
 
 	if _ConfigMeta, err = toml.DecodeFile(*configfile, &_Config); err != nil {
@@ -283,40 +282,17 @@ func main() {
 		log.SetOutput(ioutil.Discard)
 	}
 
-	if *file != "" {
-		h, err = pcap.Openoffline(*file)
-		if h == nil {
-			ERR("Openoffline(%s) failed: %s", *file, err)
-			return
-		}
-	} else {
-		device := _Config.Interfaces.Device
-
-		h, err = pcap.Openlive(device, 65535, true, 0)
-		if h == nil {
-			ERR("pcap.Openlive failed(%s): %s", device, err)
-			return
-		}
+	_Config.Interfaces.Bpf_filter = configToFilter(&_Config)
+	sniffer, err := CreateSniffer(&_Config.Interfaces, file)
+	if err != nil {
+		CRIT("Error creating sniffer: %s", err)
+		return
 	}
-	defer func() {
-		h.Close()
-	}()
+	datalink := sniffer.Datalink()
 
 	if err = DropPrivileges(); err != nil {
 		CRIT(err.Error())
 		return
-	}
-
-	if *file == "" {
-		filter := configToFilter(&_Config)
-		if filter != "" {
-			DEBUG("pcapfilter", "Installing filter '%s'", filter)
-			err := h.Setfilter(filter)
-			if err != nil {
-				ERR("pcap.Setfilter failed: %s", err)
-				return
-			}
-		}
 	}
 
 	if err = Publisher.Init(*publishDisabled); err != nil {
@@ -339,12 +315,6 @@ func main() {
 		return
 	}
 
-	datalink := h.Datalink()
-	if datalink != pcap.LINKTYPE_ETHERNET && datalink != pcap.LINKTYPE_LINUX_SLL &&
-		datalink != pcap.LINKTYPE_NULL {
-		ERR("Unsupported link type: %d", datalink)
-	}
-
 	loadGeoIPData()
 
 	if *cpuprofile != "" {
@@ -361,21 +331,14 @@ func main() {
 	loopCount := 1
 	var lastPktTime *time.Time = nil
 	for live {
-		var pkt *pcap.Packet
-		var res int32
-
 		if *oneAtAtime {
 			fmt.Println("Press enter to read packet")
 			fmt.Scanln()
 		}
 
-		pkt, res = h.NextEx()
-		switch res {
-		case -1:
-			ERR("pcap.NextEx() error: %s", h.Geterror())
-			live = false
-			continue
-		case -2:
+		data, ci, err := sniffer.DataSource.ReadPacketData()
+
+		if err == io.EOF {
 			DEBUG("pcapread", "End of file")
 			loopCount += 1
 			if *loop > 0 && loopCount > *loop {
@@ -387,43 +350,38 @@ func main() {
 			}
 
 			DEBUG("pcapread", "Reopening the file")
-			h.Close()
-			h, err = pcap.Openoffline(*file)
-			if h == nil {
-				ERR("Openoffline(%s) failed: %s", *file, err)
-				return
+			err = sniffer.Reopen()
+			if err != nil {
+				CRIT("Error reopening file: %s", err)
+				live = false
+				continue
 			}
 			lastPktTime = nil
 			continue
-		case 0:
-			// timeout
-			continue
-		}
-		if res != 1 {
-			panic(fmt.Sprintf("Unexpected return code from pcap.NextEx: %d", res))
 		}
 
-		if pkt == nil {
-			panic("Nil packet despite res=1")
+		if err != nil {
+			CRIT("Sniffing error: %s", err)
+			live = false
 		}
 
 		if *file != "" {
 			if lastPktTime != nil && !*topSpeed {
-				sleep := pkt.Time.Sub(*lastPktTime)
+				sleep := ci.Timestamp.Sub(*lastPktTime)
 				if sleep > 0 {
 					time.Sleep(sleep)
 				} else {
 					WARN("Time in pcap went backwards: %d", sleep)
 				}
 			}
-			_lastPktTime := pkt.Time
+			_lastPktTime := ci.Timestamp
 			lastPktTime = &_lastPktTime
-			pkt.Time = time.Now() // overwrite what we get from the pcap
+			ci.Timestamp = time.Now() // overwrite what we get from the pcap
 		}
 		counter++
 
 		DEBUG("pcapread", "Packet number: %d", counter)
-		decodePktEth(datalink, pkt)
+		decodePktEth(data, &ci, datalink)
 	}
 	INFO("Input finish. Processed %d packets. Have a nice day!", counter)
 
---
+++ sniffer.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"code.google.com/p/gopacket"
+	"code.google.com/p/gopacket/layers"
+	"code.google.com/p/gopacket/pcap"
+	"fmt"
+)
+
+type SnifferSetup struct {
+	pcapHandle *pcap.Handle
+	config     *tomlInterfaces
+
+	DataSource gopacket.PacketDataSource
+}
+
+type tomlInterfaces struct {
+	Device     string
+	Devices    []string
+	Type       string
+	File       string
+	With_vlans bool
+	Bpf_filter string
+	Snaplen    int
+}
+
+func CreateSniffer(config *tomlInterfaces, file *string) (*SnifferSetup, error) {
+	var sniffer SnifferSetup
+	var err error
+
+	sniffer.config = config
+
+	if file != nil {
+		// we read file with the pcap provider
+		sniffer.config.Type = "pcap"
+		sniffer.config.File = *file
+	}
+
+	// set defaults
+	if len(sniffer.config.Device) == 0 {
+		sniffer.config.Device = "any"
+	}
+
+	if len(sniffer.config.Devices) == 0 {
+		// 'devices' not set but 'device' is set. For backwards compatibility,
+		// use the one configured device
+		if len(sniffer.config.Device) > 0 {
+			sniffer.config.Devices = []string{sniffer.config.Device}
+		}
+	}
+	if sniffer.config.Snaplen == 0 {
+		sniffer.config.Snaplen = 1514
+	}
+
+	if sniffer.config.Type == "pcap" || sniffer.config.Type == "" {
+		if len(sniffer.config.File) > 0 {
+			sniffer.pcapHandle, err = pcap.OpenOffline(sniffer.config.File)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			if len(sniffer.config.Devices) > 1 {
+				return nil, fmt.Errorf("Pcap sniffer only supports one device. You can use 'any' if you want")
+			}
+			sniffer.pcapHandle, err = pcap.OpenLive(
+				sniffer.config.Devices[0],
+				int32(sniffer.config.Snaplen),
+				true,
+				pcap.BlockForever)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		sniffer.DataSource = gopacket.PacketDataSource(sniffer.pcapHandle)
+
+	} else {
+		return nil, fmt.Errorf("Unknown sniffer type: %s", sniffer.config.Type)
+	}
+
+	return &sniffer, nil
+}
+
+func (sniffer *SnifferSetup) Reopen() error {
+	var err error
+
+	if sniffer.config.Type != "pcap" || sniffer.config.File == "" {
+		return fmt.Errorf("Reopen is only possible for files")
+	}
+
+	sniffer.pcapHandle.Close()
+	sniffer.pcapHandle, err = pcap.OpenOffline(sniffer.config.File)
+	if err != nil {
+		return err
+	}
+
+	sniffer.DataSource = gopacket.PacketDataSource(sniffer.pcapHandle)
+
+	return nil
+}
+
+func (sniffer *SnifferSetup) Close() {
+	switch sniffer.config.Type {
+	case "pcap":
+		sniffer.pcapHandle.Close()
+	}
+}
+
+func (sniffer *SnifferSetup) Datalink() layers.LinkType {
+	if sniffer.config.Type == "pcap" {
+		return sniffer.pcapHandle.LinkType()
+	}
+	return layers.LinkTypeEthernet
+}
