0dd7926a15f13d1874acead172ecba9e75ac24c6
Nathan Franzen <nfranzen@megliozen.com>
2015-02-18 11:18:10 -0800

    add option in [passwords] to strip base64-encoded passwords from http request and headers

---
+++ tests/test_0012_http_basicauth.py
@@ -0,0 +1,19 @@
+from pbtests.packetbeat import TestCase
+import re
+
+class Test(TestCase):
+
+    def test_http_auth(self):
+        self.render_config_template(
+            http_send_all_headers=1,
+            http_strip_authorization=1
+        )
+        self.run_packetbeat(pcap="http_basicauth.pcap",
+            debug_selectors=["http", "httpdetailed"])
+        objs = self.read_output()
+
+        assert len(objs) == 1
+        assert all([o["type"] == "http" for o in objs])
+        assert all([o["http"]["request"]["headers"]["authorization"] == "*" for o in objs])
+        assert all([ re.search("Authorization:\*+", o["request_raw"]) != None  for o in objs])
+        
