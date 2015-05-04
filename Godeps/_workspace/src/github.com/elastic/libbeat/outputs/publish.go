793dab04bc4a448476dda04487d048a3c96174a4
Tudor Golubenco <tudor@packetbeat.com>
2015-05-04 12:08:33 +0200

    Updated libbeat dep.

--- Godeps/_workspace/src/github.com/elastic/libbeat/outputs/publish.go
+++ Godeps/_workspace/src/github.com/elastic/libbeat/outputs/publish.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"os"
-	"strings"
 	"time"
 
 	"github.com/elastic/libbeat/common"
@@ -14,7 +13,7 @@ import (
 
 type PublisherType struct {
 	name                string
-	tags                string
+	tags                []string
 	disabled            bool
 	Index               string
 	Output              []OutputInterface
@@ -103,10 +102,10 @@ func (publisher *PublisherType) publishFromQueue() {
 
 func (publisher *PublisherType) publishEvent(event common.MapStr) error {
 
-	// the @timestamp is mandatory
-	ts, ok := event["@timestamp"].(common.Time)
+	// the timestamp is mandatory
+	ts, ok := event["timestamp"].(common.Time)
 	if !ok {
-		return errors.New("Missing '@timestamp' field from event.")
+		return errors.New("Missing 'timestamp' field from event.")
 	}
 
 	// the count is mandatory
@@ -309,9 +308,7 @@ func (publisher *PublisherType) Init(publishDisabled bool,
 		logp.Info("No agent name configured, using hostname '%s'", publisher.name)
 	}
 
-	if len(agent.Tags) > 0 {
-		publisher.tags = strings.Join(agent.Tags, " ")
-	}
+	publisher.tags = agent.Tags
 
 	if !publisher.disabled && publisher.TopologyOutput != nil {
 		RefreshTopologyFreq := 10 * time.Second
