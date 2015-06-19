549ac2588017a397e1d2877967d85ea6da4bdf30
Monica Sarbu <monica@elastic.co>
2015-06-19 17:10:07 +0200

    Retry sending the bulk request up to max_retries

--- outputs/elasticsearch/bulkapi.go
+++ outputs/elasticsearch/bulkapi.go
@@ -38,41 +38,57 @@ func (es *Elasticsearch) Bulk(index string, doc_type string,
 		return nil, err
 	}
 
-	conn := es.connectionPool.GetConnection()
+	for attempt := 0; attempt < es.MaxRetries; attempt++ {
 
-	url := conn.Url + path
-	if len(params) > 0 {
-		url = url + "?" + UrlEncode(params)
+		conn := es.connectionPool.GetConnection()
+		logp.Debug("elasticsearch", "Use connection %s", conn.Url)
+
+		url := conn.Url + path
+		if len(params) > 0 {
+			url = url + "?" + UrlEncode(params)
+		}
+
+		req, err := http.NewRequest("POST", url, &buf)
+		if err != nil {
+			return nil, err
+		}
+
+		logp.Debug("elasticsearch", "Sending bulk request to %s", url)
+
+		req.Header.Add("Accept", "application/json")
+		if conn.Username != "" || conn.Password != "" {
+			req.SetBasicAuth(conn.Username, conn.Password)
+		}
+
+		resp, err := es.client.Do(req)
+		if err != nil {
+			// request fails
+			logp.Warn("Request fails: %s", err)
+			es.connectionPool.MarkDead(conn)
+			return nil, err
+		}
+
+		defer resp.Body.Close()
+		obj, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+		var result QueryResult
+		err = json.Unmarshal(obj, &result)
+		if err != nil {
+			return nil, err
+		}
+
+		if resp.StatusCode > 499 {
+			// request fails
+			es.connectionPool.MarkDead(conn)
+			return &result, fmt.Errorf("ES returned an error: %s", resp.Status)
+		}
+		// request with success
+		es.connectionPool.MarkLive(conn)
+
+		return &result, err
 	}
 
-	req, err := http.NewRequest("POST", url, &buf)
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Add("Accept", "application/json")
-	if conn.Username != "" || conn.Password != "" {
-		req.SetBasicAuth(conn.Username, conn.Password)
-	}
-
-	resp, err := es.client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	defer resp.Body.Close()
-	obj, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	var result QueryResult
-	err = json.Unmarshal(obj, &result)
-	if err != nil {
-		return nil, err
-	}
-
-	if resp.StatusCode > 299 {
-		return &result, fmt.Errorf("ES returned an error: %s", resp.Status)
-	}
-	return &result, err
+	return nil, fmt.Errorf("Request fails to be send after %d retries", es.MaxRetries)
 }
