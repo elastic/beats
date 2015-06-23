8f03faff1a119b395c5a1afd8a9b8244013fd5d8
Monica Sarbu <monica@elastic.co>
2015-06-23 14:08:45 +0200

    Add test with 2 Elasticsearch nodes that are both failing with error

--- outputs/elasticsearch/api_mock_test.go
+++ outputs/elasticsearch/api_mock_test.go
@@ -148,3 +148,35 @@ func TestMultipleHosts(t *testing.T) {
 	}
 
 }
+
+func TestMultipleFailingHosts(t *testing.T) {
+
+	if testing.Verbose() {
+		logp.LogInit(logp.LOG_DEBUG, "", false, true, []string{"elasticsearch"})
+	}
+
+	index := fmt.Sprintf("packetbeat-unittest-%d", os.Getpid())
+	body := map[string]interface{}{
+		"user":      "test",
+		"post_date": "2009-11-15T14:12:12",
+		"message":   "trying out",
+	}
+	server1 := ElasticsearchMock(503, []byte("Something went wrong"))
+	server2 := ElasticsearchMock(500, []byte("Something went wrong"))
+
+	logp.Debug("elasticsearch", "%s, %s", server1.URL, server2.URL)
+	es := NewElasticsearch([]string{server1.URL, server2.URL}, "", "")
+
+	params := map[string]string{
+		"refresh": "true",
+	}
+	_, err := es.Index(index, "test", "1", params, body)
+	if err == nil {
+		t.Errorf("Index() should return error.")
+	}
+
+	if !strings.Contains(err.Error(), "500 Internal Server Error") {
+		t.Errorf("Should return <500 Internal Server Error> instead of %v", err)
+	}
+
+}
