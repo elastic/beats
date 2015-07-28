590fbd16916a165380e16c4c651ee8671ff6708e
Tudor Golubenco <tudor@packetbeat.com>
2015-07-28 20:20:56 +0200

    Proper error handling code in the mysqlParseResponse function
    The problem was that the initial version of this function assumed
    that the message is already a valid MySQL message. While indeed
    some basic checks on it were made at the layer below (separating
    the message out of the TCP stream), there are still plenty of things
    that can go wrong.
    
    This adds error handling in (hopefully) all possible branches and
    a set of unit tests that exploit most of those.
    
    Closes #180.

--- Godeps/_workspace/src/github.com/elastic/libbeat/logp/log.go
+++ Godeps/_workspace/src/github.com/elastic/libbeat/logp/log.go
@@ -64,10 +64,10 @@ func IsDebug(selector string) bool {
 func msg(level Priority, prefix string, format string, v ...interface{}) {
 	if _log.level >= level {
 		if _log.toSyslog {
-			_log.syslog[level].Output(2, fmt.Sprintf(format, v...))
+			_log.syslog[level].Output(3, fmt.Sprintf(format, v...))
 		}
 		if _log.toStderr {
-			_log.logger.Output(2, fmt.Sprintf(prefix+format, v...))
+			_log.logger.Output(3, fmt.Sprintf(prefix+format, v...))
 		}
 		if _log.toFile {
 			_log.rotator.WriteLine([]byte(fmt.Sprintf(prefix+format, v...)))
@@ -76,19 +76,19 @@ func msg(level Priority, prefix string, format string, v ...interface{}) {
 }
 
 func Info(format string, v ...interface{}) {
-	msg(LOG_INFO, "INFO ", format, v)
+	msg(LOG_INFO, "INFO ", format, v...)
 }
 
 func Warn(format string, v ...interface{}) {
-	msg(LOG_WARNING, "WARN ", format, v)
+	msg(LOG_WARNING, "WARN ", format, v...)
 }
 
 func Err(format string, v ...interface{}) {
-	msg(LOG_ERR, "ERR ", format, v)
+	msg(LOG_ERR, "ERR ", format, v...)
 }
 
 func Critical(format string, v ...interface{}) {
-	msg(LOG_CRIT, "CRIT ", format, v)
+	msg(LOG_CRIT, "CRIT ", format, v...)
 }
 
 // WTF prints the message at CRIT level and panics immediately with the same
