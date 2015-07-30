22d2ae63887ddb4e7958c0354896f38960c86c61
Tudor Golubenco <tudor@packetbeat.com>
2015-07-30 12:29:38 +0200

    Moved cpuprofile and memprofile options

--- service/service.go
+++ service/service.go
@@ -1,8 +1,12 @@
 package service
 
 import (
+	"flag"
+	"log"
 	"os"
 	"os/signal"
+	"runtime"
+	"runtime/pprof"
 	"syscall"
 
 	"github.com/elastic/libbeat/logp"
@@ -26,4 +30,64 @@ func HandleSignals(stopFunction func()) {
 		logp.Debug("service", "Received svc stop/shutdown request")
 		stopFunction()
 	})
+}
+
+// cmdline flags
+var memprofile, cpuprofile *string
+
+func CmdLineFlags(flags *flag.FlagSet) {
+	memprofile = flags.String("memprofile", "", "Write memory profile to this file")
+	cpuprofile = flags.String("cpuprofile", "", "Write cpu profile to file")
+}
+
+func WithMemProfile() bool {
+	return *memprofile != ""
+}
+
+func WithCpuProfile() bool {
+	return *cpuprofile != ""
+}
+
+func BeforeRun() {
+
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+	}
+}
+
+func Cleanup() {
+	if *cpuprofile != "" {
+		pprof.StopCPUProfile()
+	}
+
+	if *memprofile != "" {
+		runtime.GC()
+
+		writeHeapProfile(*memprofile)
+
+		debugMemStats()
+	}
+}
+
+func debugMemStats() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	logp.Debug("mem", "Memory stats: In use: %d Total (even if freed): %d System: %d",
+		m.Alloc, m.TotalAlloc, m.Sys)
+}
+
+func writeHeapProfile(filename string) {
+	f, err := os.Create(filename)
+	if err != nil {
+		logp.Err("Failed creating file %s: %s", filename, err)
+		return
+	}
+	pprof.WriteHeapProfile(f)
+	f.Close()
+
+	logp.Info("Created memory profile file %s.", filename)
 }
