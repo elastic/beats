687209d6bf692e69904204a66e72b061ae2d82ac
Ruflin <spam@ruflin.com>
2015-08-20 11:04:42 +0200

    Add teest for swap and pids

--- sigar_test.go
+++ sigar_test.go
@@ -8,8 +8,9 @@ import (
 func TestGetSystemLoad(t *testing.T) {
 	load, err := GetSystemLoad()
 
-	assert.Nil(t, err)
 	assert.NotNil(t, load)
+	assert.Nil(t, err)
+
 	assert.True(t, (load.Load1 > 0))
 	assert.True(t, (load.Load5 > 0))
 	assert.True(t, (load.Load15 > 0))
@@ -18,12 +19,33 @@ func TestGetSystemLoad(t *testing.T) {
 func TestGetMemory(t *testing.T) {
 	mem, err := GetMemory()
 
-	assert.Nil(t, err)
 	assert.NotNil(t, mem)
+	assert.Nil(t, err)
 
 	assert.True(t, (mem.Total > 0))
 	assert.True(t, (mem.Used > 0))
 	assert.True(t, (mem.Free >= 0))
 	assert.True(t, (mem.ActualFree >= 0))
 	assert.True(t, (mem.ActualUsed > 0))
+}
+
+func TestGetSwap(t *testing.T) {
+	swap, err := GetSwap()
+
+	assert.NotNil(t, swap)
+	assert.Nil(t, err)
+
+	assert.True(t, (swap.Total >= 0))
+	assert.True(t, (swap.Used >= 0))
+	assert.True(t, (swap.Total >= 0))
+}
+
+func TestPids(t *testing.T) {
+	pids, err := Pids()
+
+	assert.NotNil(t, pids)
+	assert.Nil(t, err)
+
+	// Assuming at least 2 processes are running
+	assert.True(t, (len(pids) > 1))
 }
