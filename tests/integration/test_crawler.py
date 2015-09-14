1b5222101d1f628219469aa0daa769797d214bb0
Ruflin <spam@ruflin.com>
2015-09-14 16:09:17 +0200

    First step of integration tests implementation

--- tests/integration/test_crawler.py
+++ tests/integration/test_crawler.py
@@ -7,8 +7,8 @@ import time
 class Test(TestCase):
     def test_crawler(self):
         self.render_config_template(
-            path="/var/log/*"
-            #path=os.path.abspath(self.working_dir) + "/log/*"
+            #path="/var/log/*"
+            path=os.path.abspath(self.working_dir) + "/log/*"
         )
         os.mkdir(self.working_dir + "/log/")
 
@@ -16,11 +16,17 @@ class Test(TestCase):
 
         file.write("hello world")
         file.write("\n")
+        file.write("more")
+        file.write("\n")
+        file.write("to")
+        file.write("\n")
+        file.write("say")
+        file.write("\n")
         file.close()
 
         proc = self.start_filebeat()
 
-        time.sleep(40)
+        time.sleep(10)
         proc.kill_and_wait()
 
 
