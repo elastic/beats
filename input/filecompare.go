6611476264295a635cb1770d6096dbf67848a382
Ruflin <spam@ruflin.com>
2015-09-21 09:22:26 +0200

    Remove compareFileStats function as not needed anymore

--- input/filecompare.go
+++ input/filecompare.go
@@ -26,11 +26,6 @@ func (f1 *File) IsSameFile(f2 *File) bool {
 	return os.SameFile(f1.FileInfo, f2.FileInfo)
 }
 
-// Compare file stats. Inode id and device are compared
-func compareFileStats(s1 *syscall.Stat_t, s2 *syscall.Stat_t) bool {
-	return (s1.Dev == s2.Dev && s1.Ino == s2.Ino)
-}
-
 // SafeFileRotate safely rotates an existing file under path and replaces it with the tempfile
 func SafeFileRotate(path, tempfile string) error {
 	if e := os.Rename(tempfile, path); e != nil {
