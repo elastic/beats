27a932e46b114f74a0261a6587819d5487e3f3be
Tudor Golubenco <tudor@packetbeat.com>
2015-09-22 12:50:23 +0200

    Fix SafeFileRotate on Windows
    This whole thing is caused by the fact that os.Rename fails on
    Windows if the destination exists. This was somehow taken into
    account by the existing code by moving .filebeat to filebeat.old
    first, but it didn't quite work because:
    
    1. .filebeat might not exist
    2. .filebeat.old might exist

--- input/filecompare.go
+++ input/filecompare.go
@@ -3,14 +3,15 @@
 package input
 
 import (
-	"github.com/elastic/libbeat/logp"
 	"os"
+
+	"github.com/elastic/libbeat/logp"
 )
 
 // SafeFileRotate safely rotates an existing file under path and replaces it with the tempfile
 func SafeFileRotate(path, tempfile string) error {
 	if e := os.Rename(tempfile, path); e != nil {
-		logp.Info("Registry rotate error: rename of %s to %s - Error: %s", tempfile, path, e)
+		logp.Err("Rotate error: %s", e)
 		return e
 	}
 	return nil
