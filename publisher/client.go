348ed210c841edba52db1fe8d4211061948db1d0
urso <steffen.siering@elastic.co>
2015-09-22 17:12:43 +0200

    typo

--- publisher/client.go
+++ publisher/client.go
@@ -12,7 +12,7 @@ type Client interface {
 	// PublishEvents publishes multiple events with given options. If Confirm
 	// option is set, PublishEvent will block until output plugins report
 	// success or failure state being returned by this method.
-	PublishEvents(event []common.MapStr, opts ...ClientOption) bool
+	PublishEvents(events []common.MapStr, opts ...ClientOption) bool
 }
 
 // ChanClient will forward all published events one by one to the given channel
@@ -58,11 +58,13 @@ func (c *client) getClient(opts []ClientOption) eventPublisher {
 	return c.publisher.asyncPublisher.client()
 }
 
+// PublishEvent will publish the event on the channel. Options will be ignored.
 func (c ChanClient) PublishEvent(event common.MapStr, opts ...ClientOption) bool {
 	c.Channel <- event
 	return true
 }
 
+// PublishEvents publishes all event on the configured channel. Options will be ignored.
 func (c ChanClient) PublishEvents(events []common.MapStr, opts ...ClientOption) bool {
 	for _, event := range events {
 		c.Channel <- event
