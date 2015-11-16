97cf8b1703cbe408ee01e50c7dbd611dd66dfe4a
Tudor Golubenco <tudor@packetbeat.com>
2015-11-16 22:35:35 +0100

    Remove beat.version

--- beat/beat.go
+++ beat/beat.go
@@ -96,7 +96,7 @@ func (b *Beat) LoadConfig() {
 
 	logp.Debug("beat", "Initializing output plugins")
 
-	if err := publisher.Publisher.Init(b.Name, b.Version, b.Config.Output, b.Config.Shipper); err != nil {
+	if err := publisher.Publisher.Init(b.Name, b.Config.Output, b.Config.Shipper); err != nil {
 		fmt.Printf("Error Initialising publisher: %v\n", err)
 		logp.Critical(err.Error())
 		os.Exit(1)
