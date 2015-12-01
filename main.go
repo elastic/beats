5debb12edc19040d087e1d2744570d727341481b
Andrew Kroh <andrew.kroh@elastic.co>
2015-12-01 09:57:00 -0500

    Update to most recent libbeat version and shared files

--- main.go
+++ main.go
@@ -1,40 +1,16 @@
 package main
 
 import (
-	"os"
-
 	"github.com/elastic/libbeat/beat"
-	"github.com/elastic/libbeat/logp"
 	winlogbeat "github.com/elastic/winlogbeat/beat"
 )
 
-// Version of winlogbeat.
+// Version of Winlogbeat.
 var Version = "0.0.1"
 
 // Name of this beat.
 var Name = "winlogbeat"
 
 func main() {
-	// Create Beater.
-	wlb := &winlogbeat.Winlogbeat{}
-
-	// Initialize beat.
-	b := beat.NewBeat(Name, Version, wlb)
-
-	// Additional command line arguments are used to overwrite config options.
-	b.CommandLineSetup()
-
-	// Load base config.
-	b.LoadConfig()
-
-	// Configures beat.
-	err := wlb.Config(b)
-	if err != nil {
-		logp.Critical("Config error: %v", err)
-		os.Exit(1)
-	}
-
-	// Run beat. This calls first beater.Setup,
-	// then beater.Run and beater.Cleanup in the end
-	b.Run()
+	beat.Run(Name, Version, winlogbeat.New())
 }
