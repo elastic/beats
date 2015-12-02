6710399708df3266d1bcd696b289e9d3af346afd
Tudor Golubenco <tudor@packetbeat.com>
2015-12-02 09:33:35 +0100

    Dropped Godeps

--- packetbeat/Godeps/_workspace/src/github.com/elastic/libbeat/common/statuses.go
+++
@@ -1,9 +0,0 @@
-package common
-
-// standardized status values
-const (
-	OK_STATUS           = "OK"
-	ERROR_STATUS        = "Error"
-	SERVER_ERROR_STATUS = "Server Error"
-	CLIENT_ERROR_STATUS = "Client Error"
-)
