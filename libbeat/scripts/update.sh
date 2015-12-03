ff4229714bf35e5401c78e9a65cbd625bd84be6d
ruflin <spam@ruflin.com>
2015-12-03 14:12:17 +0100

    Bring update script to the most update state Remove godep dependencies Add newest version of config files

--- libbeat/scripts/update.sh
+++ libbeat/scripts/update.sh
@@ -6,7 +6,7 @@ set -e
 GOPATH=`echo $GOPATH | tr ':' '\n' | head -1`
 BEATNAME=$1
 BEATPATH=$2
-LIBBEAT=${GOPATH}/src/github.com/elastic/libbeat
+LIBBEAT=${GOPATH}/src/github.com/elastic/beats/libbeat
 
 # Setup
 if [ -z $BEATNAME ]; then
@@ -39,25 +39,3 @@ echo "Update config file"
 rm etc/$BEATNAME.yml
 cat etc/beat.yml ${LIBBEAT}/etc/libbeat.yml > etc/$BEATNAME.yml
 sed -i "" -e s/beatname/$BEATNAME/g etc/$BEATNAME.yml
-
-# Update Libbeat
-echo "Update libbeat deps"
-godep update github.com/elastic/libbeat/...
-
-echo "Update .editorconfig"
-cat ${LIBBEAT}/.editorconfig > .editorconfig
-
-echo "Update .gitattributes"
-cat ${LIBBEAT}/.gitattributes > .gitattributes
-
-echo "Update LICENSE"
-cat ${LIBBEAT}/LICENSE > LICENSE
-
-echo "Update MAKEFILE"
-cat ${LIBBEAT}/scripts/Makefile > scripts/Makefile
-sed -i "" -e s/libbeat.test/$BEATNAME.test/g scripts/Makefile
-sed -i "" -e "s/.PHONY: build$/.PHONY: $BEATNAME/" scripts/Makefile
-sed -i "" -e "s/^build:/$BEATNAME:/" scripts/Makefile
-
-echo "Update crosscompile.bash"
-cat ${LIBBEAT}/scripts/crosscompile.bash > scripts/crosscompile.bash
