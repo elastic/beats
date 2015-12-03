df7245043aabf60251131d63d2ede0ca54309199
Jasper Lievisse Adriaanse <jasper@humppa.nl>
2015-12-03 21:39:32 +0100

    Fix build with recent thrift

--- packetbeat/protos/thrift/thrift_idl.go
+++ packetbeat/protos/thrift/thrift_idl.go
@@ -27,8 +27,8 @@ func fieldsToArrayById(fields []*parser.Field) []*string {
 
 	max := 0
 	for _, field := range fields {
-		if field.Id > max {
-			max = field.Id
+		if field.ID > max {
+			max = field.ID
 		}
 	}
 
@@ -36,7 +36,7 @@ func fieldsToArrayById(fields []*parser.Field) []*string {
 
 	for _, field := range fields {
 		if len(field.Name) > 0 {
-			output[field.Id] = &field.Name
+			output[field.ID] = &field.Name
 		}
 	}
 
