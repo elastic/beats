6435194af9f42cbf778ca0a1a92276caf41a0da8
Magnus Bäck <magnus@noun.se>
2015-12-09 22:15:37 +0100

    Filebeat: Support array and dictionary fields.
    The fields parameter in the prospector configuration was
    previously of the type map[string]string and therefore
    only supported string fields. This has been changed to
    MapStr (i.e. map[string]interface{}) since that's what
    libbeat sends over the wire anyway, hence Filebeat now
    supports not only strings but any kind of arrays or
    dictionaries you can have in a YAML file.
    
    For this to work it was necessary to have MapStr implement
    yaml.Unmarshaler to provide a custom YAML unmarshaler. The
    reason is that the JSON library will unmarshal dictionaries
    as map[interface{}]interface{} whenever it can, and those
    can't be marshalled as JSON. The MapStr unmarshaller
    recursively converts all encountered maps so that their
    keys are of string type.

--- filebeat/config/config_test.go
+++ filebeat/config/config_test.go
@@ -33,7 +33,7 @@ func TestReadConfig(t *testing.T) {
 	assert.Equal(t, "/var/log/s*.log", prospectors[0].Paths[1])
 	assert.Equal(t, "log", prospectors[0].Input)
 	assert.Equal(t, 3, len(prospectors[0].Harvester.Fields))
-	assert.Equal(t, 1, len(prospectors[0].Harvester.Fields["review"]))
+	assert.Equal(t, "1", prospectors[0].Harvester.Fields["review"])
 	assert.Equal(t, "24h", prospectors[0].IgnoreOlder)
 	assert.Equal(t, "10s", prospectors[0].ScanFrequency)
 
