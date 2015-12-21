f420cf8eedbdceced77659f4e91d0a4dd981878e
Tudor Golubenco <tudor@packetbeat.com>
2015-12-22 00:33:09 +0100

    Added systemd unit files for debian and centos platforms
    * Tested with Centos7 and Ubuntu 1504
    * Does the needed systemctl daemon-reload from a post script
    * Copies the unit files in /lib/systemd/system even if systemd
      is not installed. Then the init script is automatically used.
    
    Also fixes bug #40 by putting back the version numbers.

--- platforms/debian/build.sh
+++ platforms/debian/build.sh
@@ -7,6 +7,7 @@ runid=debian-$BEAT-$RELEASE-$ARCH
 cat beats/$BEAT.yml archs/$ARCH.yml releases/$RELEASE.yml > build/settings-$runid.yml
 gotpl platforms/debian/run.sh.j2 < build/settings-$runid.yml > build/run-$runid.sh
 gotpl platforms/debian/init.j2 < build/settings-$runid.yml > build/$runid.init
+gotpl platforms/centos/systemd.j2 < build/settings-$runid.yml > build/$runid.service
 chmod +x build/run-$runid.sh
 
 docker run -v `pwd`/build:/build -e BUILDID=$BUILDID -e RUNID=$runid --name build-image tudorg/fpm /build/run-$runid.sh
