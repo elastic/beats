75d961af79e4339c74eab6861718cefd0932ff5a
urso <steffen.siering@elastic.co>
2015-12-26 03:38:09 +0100

    Add async load balancer output mode

---
+++ libbeat/outputs/mode/backoff.go
@@ -0,0 +1,46 @@
+package mode
+
+import "time"
+
+type backoff struct {
+	duration time.Duration
+	done     <-chan struct{}
+
+	init time.Duration
+	max  time.Duration
+}
+
+func newBackoff(done <-chan struct{}, init, max time.Duration) *backoff {
+	return &backoff{
+		duration: init,
+		init:     init,
+		max:      max,
+	}
+}
+
+func (b *backoff) Reset() {
+	b.duration = b.init
+}
+
+func (b *backoff) Wait() bool {
+	backoff := b.duration
+	b.duration *= 2
+	if b.duration > b.max {
+		b.duration = b.max
+	}
+
+	select {
+	case <-b.done:
+		return false
+	case <-time.After(backoff):
+		return true
+	}
+}
+
+func (b *backoff) WithError(err error) bool {
+	if err == nil {
+		b.Reset()
+		return true
+	}
+	return b.Wait()
+}
