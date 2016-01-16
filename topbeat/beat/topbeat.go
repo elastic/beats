1a7351600e1f3d4ebe85577d6adfb5eeb706d23e
Andrew Kroh <andrew.kroh@elastic.co>
2016-01-16 17:29:45 -0500

    Add Command Line to Process Data Reported by Topbeat
    Adding go-ole as a vendored dependency
    Adding StackExchange/wmi as a vendored dependency
    Updating gosigar version
    Omit proc.cmdline if empty and update process test

--- topbeat/beat/topbeat.go
+++ topbeat/beat/topbeat.go
@@ -207,20 +207,26 @@ func (t *Topbeat) exportProcStats() error {
 
 			newProcs[process.Pid] = process
 
+			proc := common.MapStr{
+				"pid":   process.Pid,
+				"ppid":  process.Ppid,
+				"name":  process.Name,
+				"state": process.State,
+				"mem":   process.Mem,
+				"cpu":   process.Cpu,
+			}
+
+			if process.CmdLine != "" {
+				proc["cmdline"] = process.CmdLine
+			}
+
 			event := common.MapStr{
 				"@timestamp": common.Time(time.Now()),
 				"type":       "process",
-				"proc": common.MapStr{
-					"pid":     process.Pid,
-					"ppid":    process.Ppid,
-					"name":    process.Name,
-					"state":   process.State,
-					"cmdline": process.CmdLine,
-					"mem":     process.Mem,
-					"cpu":     process.Cpu,
-				},
-				"count": 1,
+				"count":      1,
+				"proc":       proc,
 			}
+
 			t.events.PublishEvent(event)
 		}
 	}
