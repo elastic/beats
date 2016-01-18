13614679be69fce021c568e8ee5fe271cc7df01b
Andrew Kroh <andrew.kroh@elastic.co>
2016-01-18 17:02:08 -0500

    Update to golang 1.5.3

--- dev-tools/packer/docker/xgo-image/build.sh
+++ dev-tools/packer/docker/xgo-image/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 docker build --rm=true -t tudorg/xgo-base base/ && \
-    docker build --rm=true -t tudorg/xgo-1.5.1 go-1.5.1/ &&
+    docker build --rm=true -t tudorg/xgo-1.5.3 go-1.5.3/ &&
     docker build --rm=true -t tudorg/beats-builder beats-builder
