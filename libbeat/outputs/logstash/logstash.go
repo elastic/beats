4caa9a67ccc3ac9a9f9c84b6e76a457195c32481
urso <steffen.siering@elastic.co>
2016-02-01 19:16:59 +0100

    Implement failover mode on top of single connection mode

--- libbeat/outputs/logstash/logstash.go
+++ libbeat/outputs/logstash/logstash.go
@@ -44,7 +44,7 @@ const (
 	logstashDefaultTimeout   = 30 * time.Second
 	logstasDefaultMaxTimeout = 90 * time.Second
 	defaultSendRetries       = 3
-	defaultMaxWindowSize     = 1024
+	defaultMaxWindowSize     = 2048
 	defaultCompressionLevel  = 3
 )
 
@@ -120,7 +120,8 @@ func (lj *logstash) init(
 			m, err = mode.NewLoadBalancerMode(clients, maxAttempts,
 				waitRetry, timeout, maxWaitRetry)
 		} else {
-			m, err = mode.NewFailOverConnectionMode(clients, maxAttempts, waitRetry, timeout)
+			m, err = mode.NewFailOverConnectionMode(clients, maxAttempts,
+				waitRetry, timeout, maxWaitRetry)
 		}
 	}
 	if err != nil {
