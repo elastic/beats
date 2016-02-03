0700031822d14083ad771c150868ce72422a79c1
urso <steffen.siering@elastic.co>
2016-02-03 22:48:32 +0100

    More tests in logstash protocol

--- libbeat/outputs/logstash/transport_test.go
+++ libbeat/outputs/logstash/transport_test.go
@@ -16,7 +16,11 @@ type mockServer struct {
 	timeout   time.Duration
 	err       error
 	handshake func(net.Conn)
+	transp    func() (TransportClient, error)
 }
+
+type mockServerFactory func(*testing.T, time.Duration, string) *mockServer
+type transportFactory func(addr string) (TransportClient, error)
 
 func (m *mockServer) Addr() string {
 	return m.Listener.Addr().String()
@@ -40,6 +44,21 @@ func (m *mockServer) await() chan net.Conn {
 		c <- client
 	}()
 	return c
+}
+
+func (m *mockServer) connectPair(to time.Duration) (net.Conn, TransportClient, error) {
+	transp, err := m.transp()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	await := m.await()
+	err = transp.Connect(to)
+	if err != nil {
+		return nil, nil, err
+	}
+	client := <-await
+	return client, transp, nil
 }
 
 func newMockServerTLS(t *testing.T, to time.Duration, cert string) *mockServer {
@@ -77,6 +96,9 @@ func newMockServerTLS(t *testing.T, to time.Duration, cert string) *mockServer {
 
 		server.err = tlsConn.Handshake()
 	}
+	server.transp = func() (TransportClient, error) {
+		return connectTLS(cert)(server.Addr())
+	}
 
 	return server
 }
@@ -89,11 +111,17 @@ func newMockServerTCP(t *testing.T, to time.Duration, cert string) *mockServer {
 
 	server := &mockServer{Listener: tcpListener, timeout: to}
 	server.handshake = func(client net.Conn) {}
+	server.transp = func() (TransportClient, error) {
+		return connectTCP(server.Addr())
+	}
 	return server
 }
 
-type mockServerFactory func(*testing.T, time.Duration, string) *mockServer
-type transportFactory func(addr string) (TransportClient, error)
+func (m *mockServer) clientDeadline(client net.Conn, to time.Duration) {
+	if m.err == nil {
+		m.err = client.SetDeadline(time.Now().Add(to))
+	}
+}
 
 func connectTCP(addr string) (TransportClient, error) {
 	return newTCPClient(addr, 0)
@@ -112,20 +140,16 @@ func connectTLS(certName string) transportFactory {
 	}
 }
 
-func (m *mockServer) clientDeadline(client net.Conn, to time.Duration) {
-	if m.err == nil {
-		m.err = client.SetDeadline(time.Now().Add(to))
-	}
-}
-
 func TestTransportReconnectsOnConnect(t *testing.T) {
 	certName := "ca_test"
 	timeout := 2 * time.Second
 	genCertsForIPIfMIssing(t, net.IP{127, 0, 0, 1}, certName)
 
-	run := func(makeServer mockServerFactory, makeTransp transportFactory) {
+	run := func(makeServer mockServerFactory) {
 		server := makeServer(t, timeout, certName)
-		transp, err := makeTransp(server.Addr())
+		defer server.Close()
+
+		transp, err := server.transp()
 		if err != nil {
 			t.Fatalf("Failed to create client: %v", err)
 		}
@@ -150,8 +174,8 @@ func TestTransportReconnectsOnConnect(t *testing.T) {
 		transp.Close()
 	}
 
-	run(newMockServerTCP, connectTCP)
-	run(newMockServerTLS, connectTLS(certName))
+	run(newMockServerTCP)
+	run(newMockServerTLS)
 }
 
 func TestTransportFailConnectUnknownAddress(t *testing.T) {
@@ -180,21 +204,15 @@ func TestTransportClosedOnWriteReadError(t *testing.T) {
 	timeout := 2 * time.Second
 	genCertsForIPIfMIssing(t, net.IP{127, 0, 0, 1}, certName)
 
-	run := func(makeServer mockServerFactory, makeTransp transportFactory) {
+	run := func(makeServer mockServerFactory) {
 		server := makeServer(t, timeout, certName)
-		transp, err := makeTransp(server.Addr())
+		defer server.Close()
+
+		client, transp, err := server.connectPair(timeout)
 		if err != nil {
 			t.Fatalf("Failed to create client: %v", err)
 		}
-
-		await := server.await()
-		err = transp.Connect(timeout)
-		if err != nil {
-			t.Fatalf("Failed to connect: %v", err)
-		}
-		client := <-await
 		client.Close()
-		time.Sleep(5000 * time.Millisecond)
 
 		var buf [10]byte
 		transp.Write([]byte("test3"))
@@ -203,6 +221,6 @@ func TestTransportClosedOnWriteReadError(t *testing.T) {
 		assert.False(t, transp.IsConnected())
 	}
 
-	run(newMockServerTCP, connectTCP)
-	run(newMockServerTLS, connectTLS(certName))
+	run(newMockServerTCP)
+	run(newMockServerTLS)
 }
