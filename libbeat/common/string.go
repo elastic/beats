d0dbb5ec448c78d5902bca454dc5f62e9cdd6ccb
Kevin Kirsche <Kev.Kirsche+GitHub@gmail.com>
2016-02-18 20:03:41 -0800

    Document NetString & MarshalText interface
    This adds GoLint documentation that explains what a netstring is and why the MarshalText function exists.

--- libbeat/common/string.go
+++ libbeat/common/string.go
@@ -1,10 +1,13 @@
 package common
 
+// NetString store the byte length of the data that follows, making it easier
+// to unambiguously pass text and byte data between programs that could be
+// sensitive to values that could be interpreted as delimiters or terminators
+// (such as a null character).
 type NetString []byte
 
-// implement encoding.TextMarshaller interface to treat []byte as raw string
-// by other encoders/serializers (e.g. JSON)
-
+// MarshalText exists to implement encoding.TextMarshaller interface to
+// treat []byte as raw string by other encoders/serializers (e.g. JSON)
 func (n NetString) MarshalText() ([]byte, error) {
 	return n, nil
 }
