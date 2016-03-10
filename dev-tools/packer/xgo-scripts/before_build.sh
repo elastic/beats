0de4a115e47fdaf3d22b39311ce6c21414265091
ruflin <spam@ruflin.com>
2016-03-10 14:18:01 +0100

    Enabled template loading by default
    When the beat is started with the default config file, it tries to load template into elasticsearch.
    
    To do this, the following changes were required:
    
    * Uncomment template config in libbeat.yml. If it should also be enabled by default when commented, an enable / disable flag would be needed as otherwise it couldn't be disabled
    * *beat.template.json file was moved to top beat directory. This runs the template automatically every time the beat is started also in the development environment. Same behaviour as for the config file.
    * Makefile commands were updated for the new location of the file
    * Documentation must be updated to mention the template loading as default and show the shell command as a second option. See https://github.com/elastic/beats/pull/1135
    * Dynamically update template path in config file during packaging, so it is also loaded by default on the different environments. This is only needed for linux environments under /etc/$beatname/$beatname.template.json

--- dev-tools/packer/xgo-scripts/before_build.sh
+++ dev-tools/packer/xgo-scripts/before_build.sh
@@ -17,22 +17,32 @@ else
     BEAT_PATH=$BEATS_PATH
 fi
 
+echo $BEAT_PATH
+
 cd $BEAT_PATH
 
 PREFIX=/build
 
 echo $PREFIX
 
-cp etc/$BEATNAME.template.json $PREFIX/$BEATNAME.template.json
+
+# Copy template
+cp $BEATNAME.template.json $PREFIX/$BEATNAME.template.json
+
 # linux
 cp $BEATNAME.yml $PREFIX/$BEATNAME-linux.yml
+
+# Updates template path for linux distros
+sed -i "s@path: \"$BEATNAME.template.json\"@path: \"/etc/$BEATNAME/$BEATNAME.template.json\"@" $PREFIX/$BEATNAME-linux.yml
+
 # binary
 cp $BEATNAME.yml $PREFIX/$BEATNAME-binary.yml
+
 # darwin
 cp $BEATNAME.yml $PREFIX/$BEATNAME-darwin.yml
+
 # win
 cp $BEATNAME.yml $PREFIX/$BEATNAME-win.yml
-
 
 # Contains beat specific adjustments. As it is platform specific knowledge, it should be in packer not the beats itself
 PREFIX=$PREFIX make before-build
