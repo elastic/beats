f73c95166c602fa14b2bce8a4082b4b71f9699ff
ruflin <spam@ruflin.com>
2016-03-11 14:32:36 +0100

    Move platform build steps to beats packer
    For each platform that beats packer builds a binary, it has additional knowledge about where to store config files and data. For this each platform has its own configuration file. The generic copy / paste part of these config file adjustments was now moved to packer. There is still a `build-before` step call which is made to each beat to make adjustments to the config file which are beat specific. For this each beat still needs platform about the different platforms. Hopefully in the future with data and config directory, this special code can be removed.

--- dev-tools/packer/xgo-scripts/filebeat_before_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-
-BEAT_PATH=/go/src/github.com/elastic/beats/filebeat
-cd $BEAT_PATH
-PREFIX=/build make install-cfg
--- dev-tools/packer/xgo-scripts/winlogbeat_before_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-
-BEAT_PATH=/go/src/github.com/elastic/beats/winlogbeat
-cd $BEAT_PATH
-PREFIX=/build make install-cfg
