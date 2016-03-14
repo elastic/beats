4ff18638854c2f7b1b9e0eb7aea832dfe360a983
chrsblck <chrisbblack@gmail.com>
2016-03-14 14:38:20 -0700

    Add Redis INFO keyspace stats

--- metricbeat/module/redis/info/data.go
+++ metricbeat/module/redis/info/data.go
@@ -2,6 +2,7 @@ package info
 
 import (
 	"strconv"
+	"strings"
 
 	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/libbeat/logp"
@@ -100,9 +101,55 @@ func eventMapping(info map[string]string) common.MapStr {
 			"latest_fork_usec":       info["latest_fork_usec"],
 			"migrate_cached_sockets": info["migrate_cached_sockets"],
 		},
+		"keyspace": getKeyspaceStats(info),
 	}
 
 	return event
+}
+
+func getKeyspaceStats(info map[string]string) map[string]common.MapStr {
+	keyspaceMap := findKeyspaceStats(info)
+	return parseKeyspaceStats(keyspaceMap)
+}
+
+// findKeyspaceStats will grep for keyspace ("^db" keys) and return the resulting map
+func findKeyspaceStats(info map[string]string) map[string]string {
+	keyspace := map[string]string{}
+
+	for k, v := range info {
+		if strings.HasPrefix(k, "db") {
+			keyspace[k] = v
+		}
+	}
+	return keyspace
+}
+
+// parseKeyspaceStats resolves the overloaded value string that Redis returns for keyspace
+func parseKeyspaceStats(keyspaceMap map[string]string) map[string]common.MapStr {
+	keyspace := map[string]common.MapStr{}
+	for k, v := range keyspaceMap {
+
+		// Extract out the overloaded values for db keyspace
+		// fmt: info[db0] = keys=795341,expires=0,avg_ttl=0
+		dbInfo := parseRedisLine(v, ",")
+
+		if len(dbInfo) == 3 {
+			db := map[string]string{}
+			for _, dbEntry := range dbInfo {
+				stats := parseRedisLine(dbEntry, "=")
+
+				if len(stats) == 2 {
+					db[stats[0]] = stats[1]
+				}
+			}
+			keyspace[k] = common.MapStr{
+				"keys":    toInt(db["keys"]),
+				"expires": toInt(db["expires"]),
+				"avg_ttl": toInt(db["avg_ttl"]),
+			}
+		}
+	}
+	return keyspace
 }
 
 func toInt(param string) int {
