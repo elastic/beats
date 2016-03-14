6a422876501391dbdeccf902e9240a0e5d2e70eb
ruflin <spam@ruflin.com>
2016-03-14 09:02:23 +0100

    Prospector and Harvester Cleanup
    This PR simplifies https://github.com/elastic/beats/pull/964 by already applying the non related parts.
    
    * Remove unnecessary for loop in readline

--- filebeat/harvester/util.go
+++ filebeat/harvester/util.go
@@ -12,16 +12,14 @@ import (
 // In case of partial lines, readLine does return and error and en empty string
 // This could potentialy be improved / replaced by https://github.com/elastic/beats/libbeat/tree/master/common/streambuf
 func readLine(reader processor.LineProcessor) (time.Time, string, int, error) {
-	for {
-		l, err := reader.Next()
+	l, err := reader.Next()
 
-		// Full line read to be returned
-		if l.Bytes != 0 && err == nil {
-			return l.Ts, string(l.Content), l.Bytes, err
-		}
-
-		return time.Time{}, "", 0, err
+	// Full line read to be returned
+	if l.Bytes != 0 && err == nil {
+		return l.Ts, string(l.Content), l.Bytes, err
 	}
+
+	return time.Time{}, "", 0, err
 }
 
 // InitRegexps initializes a list of compiled regular expressions.
