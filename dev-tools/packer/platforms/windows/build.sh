e5f3a19d93c0451e950eeba3ab6391c32eeb64d5
Cyrille Verrier <cyrille.verrier@fabriscale.com>
2016-03-14 22:00:01 +0100

    Configure dev-tools to be reusable for community beats

--- dev-tools/packer/platforms/windows/build.sh
+++ dev-tools/packer/platforms/windows/build.sh
@@ -2,13 +2,15 @@
 
 set -e
 
+BASEDIR=$(dirname "$0")
+
 # executed from the top directory
 runid=windows-$BEAT-$ARCH
 
 cat beats/$BEAT.yml archs/$ARCH.yml version.yml > build/settings-$runid.yml
-gotpl platforms/windows/run.sh.j2 < build/settings-$runid.yml > build/run-$runid.sh
-gotpl platforms/windows/install-service.ps1.j2 < build/settings-$runid.yml > build/install-service-$BEAT.ps1
-gotpl platforms/windows/uninstall-service.ps1.j2 < build/settings-$runid.yml > build/uninstall-service-$BEAT.ps1
+gotpl ${BASEDIR}/run.sh.j2 < build/settings-$runid.yml > build/run-$runid.sh
+gotpl ${BASEDIR}/install-service.ps1.j2 < build/settings-$runid.yml > build/install-service-$BEAT.ps1
+gotpl ${BASEDIR}/uninstall-service.ps1.j2 < build/settings-$runid.yml > build/uninstall-service-$BEAT.ps1
 chmod +x build/run-$runid.sh
 
 docker run -v `pwd`/build:/build -e BUILDID=$BUILDID -e RUNID=$runid --name build-image tudorg/fpm /build/run-$runid.sh
