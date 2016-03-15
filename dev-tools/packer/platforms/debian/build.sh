d864f4bae7169d88a55d4eb78646cfa8c106ee53
Cyrille Verrier <cyrille.verrier@fabriscale.com>
2016-03-15 10:26:30 +0100

    fix archs path

--- dev-tools/packer/platforms/debian/build.sh
+++ dev-tools/packer/platforms/debian/build.sh
@@ -3,10 +3,12 @@
 set -e
 
 BASEDIR=$(dirname "$0")
+ARCHDIR=${BASEDIR}/../../
 
 # executed from the top directory
 runid=debian-$BEAT-$ARCH
-cat beats/$BEAT.yml archs/$ARCH.yml version.yml > build/settings-$runid.yml
+
+cat beats/$BEAT.yml ${ARCHDIR}/archs/$ARCH.yml version.yml > build/settings-$runid.yml
 gotpl ${BASEDIR}/run.sh.j2 < build/settings-$runid.yml > build/run-$runid.sh
 gotpl ${BASEDIR}/init.j2 < build/settings-$runid.yml > build/$runid.init
 gotpl ${BASEDIR}/systemd.j2 < build/settings-$runid.yml > build/$runid.service
--- dev-tools/packer/platforms/windows/build.sh
+++ dev-tools/packer/platforms/windows/build.sh
@@ -3,11 +3,12 @@
 set -e
 
 BASEDIR=$(dirname "$0")
+ARCHDIR=${BASEDIR}/../..
 
 # executed from the top directory
 runid=windows-$BEAT-$ARCH
 
-cat beats/$BEAT.yml archs/$ARCH.yml version.yml > build/settings-$runid.yml
+cat beats/$BEAT.yml ${ARCHDIR}/archs/$ARCH.yml version.yml > build/settings-$runid.yml
 gotpl ${BASEDIR}/run.sh.j2 < build/settings-$runid.yml > build/run-$runid.sh
 gotpl ${BASEDIR}/install-service.ps1.j2 < build/settings-$runid.yml > build/install-service-$BEAT.ps1
 gotpl ${BASEDIR}/uninstall-service.ps1.j2 < build/settings-$runid.yml > build/uninstall-service-$BEAT.ps1
