432406d85e811cf908b0cdeb90c6e1ece8dd9f25
ruflin <spam@ruflin.com>
2016-03-17 12:32:52 +0100

    Add Timeout config option for metricseter to exit
    The fetch on each metricseter is called based on the predefined ticker without taking delays of fetching into account. This can lead to the issue, that one metricset adds more requests before the last one was ended. The timeout variable was introduced. This allows to configure when the metricseter should exist. By default it is set to period, but it can also be selected larger or smaller then period based on the specific needs.
    
    * Add timeout config option which is by default set to period
    * Implement timeout for redis and mysql

--- metricbeat/helper/interfacer.go
+++ metricbeat/helper/interfacer.go
@@ -1,13 +1,33 @@
+/**
+
+= MetricSeter
+
+== Timeout
+Each metricseter must implement on his side to make sure the timeout is followed.
+Otherwise it can lead to the issue that multiple calls for one metricset happen in parallel and pile up.
+
+*/
+
 package helper
 
 import (
 	"github.com/elastic/beats/libbeat/common"
 )
 
+/*
+
+Implementation Background of Fetch
+
+The initial idea was to use a Cancel function, but it was not implemented as it would add complexity by requiring
+an identifier for each fetch call and it would have to be decide, which fetch call must be canceled.
+
+*/
+
 // Base configuration for each module/metricsets combination
 type ModuleConfig struct {
 	Hosts      []string `config:"hosts"`
 	Period     string   `config:"period"`
+	Timeout    string   `config:"timeout"`
 	Module     string   `config:"module"`
 	MetricSets []string `config:"metricsets"`
 	Enabled    bool     `config:"enabled"`
@@ -27,6 +47,7 @@ type MetricSeter interface {
 	// Fetch is called for each host. In case where host does not exist, it can be transferred
 	// differently in the setup to have a different meaning. An example here is for filesystem
 	// of topbeat, where each host could be a filesystem.
+	// Fetch is called on the predefined interval and does not take delays into account.
 	Fetch(ms *MetricSet, host string) (common.MapStr, error)
 }
 
