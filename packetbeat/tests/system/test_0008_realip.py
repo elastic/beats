623ff239a215840ee5acced8d8b5a278a1e78cab
Andrew Kroh <andrew.kroh@elastic.co>
2016-03-27 14:49:42 -0400

    Split real_ip_header value when it contains multiple IPs
    Fixes #1236

--- packetbeat/tests/system/test_0008_realip.py
+++ packetbeat/tests/system/test_0008_realip.py
@@ -23,3 +23,20 @@ class Test(TestCase):
 
         assert o["real_ip"] == "89.247.39.104"
         assert o["client_location"] == "52.528503, 13.410904"
+
+    def test_x_forwarded_for_multiple_ip(self):
+        self.render_config_template(
+            http_ports=[80],
+            http_real_ip_header="X-Forwarded-For",
+            http_send_all_headers=True,
+            geoip_paths=["geoip_city.dat"]
+        )
+        self.copy_files(["geoip_city.dat"])
+        self.run_packetbeat(pcap="http_x_forwarded_for.pcap", debug_selectors=["http"])
+
+        objs = self.read_output()
+        assert len(objs) == 1
+        o = objs[0]
+
+        assert o["real_ip"] == "89.247.39.104"
+        assert o["client_location"] == "52.528503, 13.410904"
