26da82958d4de0a3b848804636690d0d58a7816a
Hendrik Saly <hendrikdev22@gmail.com>
2016-03-29 23:16:19 +0200

    new pcap files and system test skeletons

--- packetbeat/tests/system/test_0073_drda_db2_server.py
+++ packetbeat/tests/system/test_0073_drda_db2_server.py
@@ -10,12 +10,7 @@ class Test(BaseTest):
             drda_ports=[1527],
         )
 
-        self.run_packetbeat(pcap="drda/drda_simple.pcap", debug_selectors=["*"])
+        self.run_packetbeat(pcap="drda/db2_server.pcap", debug_selectors=["*"])
         objs = self.read_output()
 
-        assert len(objs) == 21
-        check_fields(objs[8], {
-            'query': 'select * from derbyDb',
-            'bytes_in': 258,
-            'bytes_out': 511,
-        })
+        assert len(objs) > 0
