e9d0787149b2d5dea6fa7b09cadc31bd599f795e
urso <steffen.siering@elastic.co>
2016-04-08 14:12:44 +0200

    Update redis default config

--- libbeat/outputs/redis/config.go
+++ libbeat/outputs/redis/config.go
@@ -27,31 +27,15 @@ type redisConfig struct {
 
 var (
 	defaultConfig = redisConfig{
-		Port:        6379,
-		LoadBalance: true,
-		Timeout:     30 * time.Second,
-		MaxRetries:  3,
+		Port:             6379,
+		LoadBalance:      true,
+		Timeout:          5 * time.Second,
+		MaxRetries:       3,
+		TLS:              nil,
+		Db:               0,
+		DataType:         "list",
+		HostTopology:     "",
+		PasswordTopology: "",
+		DbTopology:       1,
 	}
 )
-
-/*
-type redisConfig struct {
-	Host              string        `config:"host"`
-	Port              int           `config:"port"`
-	Password          string        `config:"password"`
-	Db                int           `config:"db"`
-	DbTopology        int           `config:"db_topology"`
-	Timeout           time.Duration `config:"timeout"`
-	Index             string        `config:"index"`
-	ReconnectInterval int           `config:"reconnect_interval"`
-	DataType          string        `config:"datatype"`
-}
-
-var (
-	defaultConfig = redisConfig{
-		DbTopology:        1,
-		Timeout:           5 * time.Second,
-		ReconnectInterval: 1,
-	}
-)
-*/
