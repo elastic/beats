4039554a62cbddffd95c54fcde3220ce2d385dbd
Tudor Golubenco <tudor@elastic.co>
2016-04-21 16:26:30 +0200

    Added beatname.sh in /usr/bin (#1446)
    For Deb and Rpm installations. This makes it easy to run the Beat in foreground, while still being executed with the write paths.
    
    * Fix postinst script
    * Added beatname.sh in /usr/bin
    
    Part of #1371.

--- dev-tools/packer/platforms/debian/build.sh
+++ dev-tools/packer/platforms/debian/build.sh
@@ -10,9 +10,11 @@ runid=debian-$BEAT-$ARCH
 
 cat beats/$BEAT.yml ${ARCHDIR}/archs/$ARCH.yml version.yml > build/settings-$runid.yml
 gotpl ${BASEDIR}/run.sh.j2 < build/settings-$runid.yml > build/run-$runid.sh
+chmod +x build/run-$runid.sh
 gotpl ${BASEDIR}/init.j2 < build/settings-$runid.yml > build/$runid.init
 gotpl ${BASEDIR}/systemd.j2 < build/settings-$runid.yml > build/$runid.service
-chmod +x build/run-$runid.sh
+gotpl ${BASEDIR}/beatname.sh.j2 < build/settings-$runid.yml > build/beatname-$runid.sh
+chmod +x build/beatname-$runid.sh
 
 docker run --rm -v `pwd`/build:/build \
     -e BUILDID=$BUILDID -e SNAPSHOT=$SNAPSHOT -e RUNID=$runid \
