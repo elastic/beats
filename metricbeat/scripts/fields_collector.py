c5ec5a4c869ef9fb4fdcc0de6fee48077588ebd6
Nicolas Ruflin <spam@ruflin.com>
2016-04-26 20:09:46 +0200

    Collect fields for modules and metricsets (#1493)
    * Add python script to collect fields.yml files
    * Update templates and docs
    * Add fields.yml for each module

---
+++ metricbeat/scripts/fields_collector.py
@@ -0,0 +1,54 @@
+import os
+
+# Collects fields for all modules and metricset
+
+
+def collect():
+
+    base_dir = "module"
+    path = os.path.abspath("module")
+
+    # yml file
+    fields_yml = ""
+
+    # Iterate over all modules
+    for module in os.listdir(base_dir):
+
+        module_fields = path + "/" + module + "/fields.yml"
+
+        # Only check folders where fields.yml exists
+        if os.path.isfile(module_fields) == False:
+            continue
+
+        # Load module yaml
+        with file(module_fields) as f:
+            tmp = f.read()
+            fields_yml += tmp
+
+
+        # Iterate over all metricsets
+        for metricset in os.listdir(base_dir + "/" + module):
+
+            metricset_fields = path + "/" + module + "/" + metricset + "/fields.yml"
+
+            # Only check folders where fields.yml exists
+            if os.path.isfile(metricset_fields) == False:
+                continue
+
+            # Load metricset yaml
+            with file(metricset_fields) as f:
+                # Add 4 spaces for indentation in fron of each line
+                for line in f:
+                    if len(line.strip()) > 0:
+                        fields_yml += "    " + line
+                    else:
+                        fields_yml += line
+
+    # output string so it can be concatenated
+    print fields_yml
+
+if __name__ == "__main__":
+    collect()
+
+
+
