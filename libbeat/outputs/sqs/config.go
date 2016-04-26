608dac1ca0b34b152a4dd9b2d13b04f44c85c1bb
韦德望 <weidewang@funguide.com.cn>
2016-04-26 09:59:18 +0800

    fix sqs config

--- libbeat/outputs/sqs/config.go
+++ libbeat/outputs/sqs/config.go
@@ -5,7 +5,7 @@ type config struct {
 	SecretAccessKey string `config:"secret_access_key"`
 	AccessKeyID     string `config:"access_key_id"`
 	Region          string `config:"region"`
-	QueueName       string `config:"region"`
+	QueueName       string `config:"queue_name"`
 }
 
 var (
