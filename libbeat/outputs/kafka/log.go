9404abba2d19c45159e1567da3e8bc5dfa8b2b45
Steffen Siering <steffen.siering@elastic.co>
2016-04-28 18:48:15 +0200

    Fix kafka log message printing (#1516)

--- libbeat/outputs/kafka/log.go
+++ libbeat/outputs/kafka/log.go
@@ -5,13 +5,13 @@ import "github.com/elastic/beats/libbeat/logp"
 type kafkaLogger struct{}
 
 func (kafkaLogger) Print(v ...interface{}) {
-	logp.Warn("kafka message: %v", v)
+	logp.Warn("kafka message: %v", v...)
 }
 
 func (kafkaLogger) Printf(format string, v ...interface{}) {
-	logp.Warn(format, v)
+	logp.Warn(format, v...)
 }
 
 func (kafkaLogger) Println(v ...interface{}) {
-	logp.Warn("kafka message: %v", v)
+	logp.Warn("kafka message: %v", v...)
 }
