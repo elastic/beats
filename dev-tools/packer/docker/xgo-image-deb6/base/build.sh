910eb6571f48b0be31b31d37b5bfd8d24397afd0
Cyrille Verrier <cyrille.verrier@fabriscale.com>
2016-05-07 21:41:43 +0200

    packer: output more statements during the build process

--- dev-tools/packer/docker/xgo-image-deb6/base/build.sh
+++ dev-tools/packer/docker/xgo-image-deb6/base/build.sh
@@ -17,15 +17,16 @@
 
 # Download the canonical import path (may fail, don't allow failures beyond)
 
+SRC_FOLDER=$SOURCE
+DST_FOLDER=$GOPATH/src/$1
+
 if [ $1 = "github.com/elastic/beats" ]; then
-        SRC_FOLDER=$SOURCE
-        DST_FOLDER=$GOPATH/src/$1
         WORKING_DIRECTORY=$GOPATH/src/$1
 else
-        SRC_FOLDER=$SOURCE
-        DST_FOLDER=$GOPATH/src/$1
         WORKING_DIRECTORY=$GOPATH/src/`dirname $1`
 fi
+
+echo "Working directory=$WORKING_DIRECTORY"
 
 if [ "$SOURCE" != "" ]; then
         mkdir -p ${DST_FOLDER}
@@ -93,40 +94,44 @@ if [ -n $BEFORE_BUILD ]; then
 fi
 
 # Build for each platform individually
-echo "Compiling for linux/amd64..."
+echo "Compiling $PACK for linux/amd64..."
 HOST=x86_64-linux PREFIX=/usr/local $BUILD_DEPS /deps
 GOOS=linux GOARCH=amd64 CGO_ENABLED=1 go get -d ./$PACK
 sh -c "GOOS=linux GOARCH=amd64 CGO_ENABLED=1 go build $V $R $LDARGS -o $NAME-linux-amd64$R ./$PACK"
 
-echo "Compiling for linux/386..."
+echo "Compiling $PACK for linux/386..."
 HOST=i686-linux PREFIX=/usr/local $BUILD_DEPS /deps
 GOOS=linux GOARCH=386 CGO_ENABLED=1 go get -d ./$PACK
 sh -c "GOOS=linux GOARCH=386 CGO_ENABLED=1 go build $V $LDARGS -o $NAME-linux-386 ./$PACK"
 
-#echo "Compiling for linux/arm..."
+#echo "Compiling $PACK for linux/arm..."
 #CC=arm-linux-gnueabi-gcc HOST=arm-linux PREFIX=/usr/local/arm $BUILD_DEPS /deps
 #CC=arm-linux-gnueabi-gcc GOOS=linux GOARCH=arm CGO_ENABLED=1 GOARM=5 go get -d ./$PACK
 #CC=arm-linux-gnueabi-gcc GOOS=linux GOARCH=arm CGO_ENABLED=1 GOARM=5 go build $V -o $NAME-linux-arm ./$PACK
 
-#echo "Compiling for windows/amd64..."
+#echo "Compiling $PACK for windows/amd64..."
 #CC=x86_64-w64-mingw32-gcc HOST=x86_64-w64-mingw32 PREFIX=/usr/x86_64-w64-mingw32 $BUILD_DEPS /deps
 #CC=x86_64-w64-mingw32-gcc GOOS=windows GOARCH=amd64 CGO_ENABLED=1 go get -d ./$PACK
 #CC=x86_64-w64-mingw32-gcc GOOS=windows GOARCH=amd64 CGO_ENABLED=1 go build  $V $R -o $NAME-windows-amd64$R.exe ./$PACK
 
-#echo "Compiling for windows/386..."
+#echo "Compiling $PACK for windows/386..."
 #CC=i686-w64-mingw32-gcc HOST=i686-w64-mingw32 PREFIX=/usr/i686-w64-mingw32 $BUILD_DEPS /deps
 #CC=i686-w64-mingw32-gcc GOOS=windows GOARCH=386 CGO_ENABLED=1 go get -d ./$PACK
 #CC=i686-w64-mingw32-gcc GOOS=windows GOARCH=386 CGO_ENABLED=1 go build $V -o $NAME-windows-386.exe ./$PACK
 
-#echo "Compiling for darwin/amd64..."
+#echo "Compiling $PACK for darwin/amd64..."
 #CC=o64-clang HOST=x86_64-apple-darwin10 PREFIX=/usr/local $BUILD_DEPS /deps
 #CC=o64-clang GOOS=darwin GOARCH=amd64 CGO_ENABLED=1 go get -d ./$PACK
 #CC=o64-clang GOOS=darwin GOARCH=amd64 CGO_ENABLED=1 go build $V $R -o $NAME-darwin-amd64$R ./$PACK
 
-#echo "Compiling for darwin/386..."
+#echo "Compiling $PACK for darwin/386..."
 #CC=o32-clang HOST=i386-apple-darwin10 PREFIX=/usr/local $BUILD_DEPS /deps
 #CC=o32-clang GOOS=darwin GOARCH=386 CGO_ENABLED=1 go get -d ./$PACK
 #CC=o32-clang GOOS=darwin GOARCH=386 CGO_ENABLED=1 go build $V -o $NAME-darwin-386 ./$PACK
 
+# The binary files are the 2 last created files
 echo "Moving binaries to host..."
+ls -t | head -n 2
 cp `ls -t | head -n 2` /build
+
+echo "Build process completed"
