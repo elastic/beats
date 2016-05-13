ff0b6cc40a5cb547fb23c5fe6434d18b18f61c26
Nicolas Ruflin <spam@ruflin.com>
2016-05-13 15:14:54 +0200

    Update fields structure to be array (#1636)
    This makes the structure more flexible as it is easy to add new elements by just appending to the file. The sections array was removed as key and title are now part of the fields directly.
    
    * Updated all documents to the new format
    * Implemented sorting of entries for docs based on key
    * Update scripts to handle the new structure

--- libbeat/scripts/generate_template.py
+++ libbeat/scripts/generate_template.py
@@ -29,10 +29,6 @@ def fields_to_es_template(args, input, output, index):
         print "fields.yml is empty. Cannot generate template."
         return
 
-    # Remove sections as only needed for docs
-    if "sections" in docs.keys():
-        del docs["sections"]
-
     # Each template needs defaults
     if "defaults" not in docs.keys():
         print("No defaults are defined. Each template needs at" +
@@ -41,10 +37,8 @@ def fields_to_es_template(args, input, output, index):
 
     defaults = docs["defaults"]
 
-    # de-dot
-    for doc, section in docs.items():
-        if doc != "defaults":
-            docs[doc] = dedot(section)
+    for k, section in enumerate(docs["fields"]):
+        docs["fields"][k] = dedot(section)
 
     # skeleton
     template = {
@@ -71,12 +65,11 @@ def fields_to_es_template(args, input, output, index):
 
     properties = {}
     dynamic_templates = []
-    for doc, section in docs.items():
-        if doc != "defaults":
-            prop, dynamic = fill_section_properties(args, section,
-                                                    defaults, "")
-            properties.update(prop)
-            dynamic_templates.extend(dynamic)
+    for section in docs["fields"]:
+        prop, dynamic = fill_section_properties(args, section,
+                                                defaults, "")
+        properties.update(prop)
+        dynamic_templates.extend(dynamic)
 
     template["mappings"]["_default_"]["properties"] = properties
     if len(dynamic_templates) > 0:
@@ -278,9 +271,9 @@ if __name__ == "__main__":
     with open(args.path + "/etc/fields.yml", 'r') as f:
         fields = f.read()
 
-        # Appends beat fields from libbeat
+        # Prepend beat fields from libbeat
         with file(args.es_beats + "/libbeat/_beat/fields.yml") as f:
-            fields += f.read()
+            fields = f.read() + fields
 
         with open(target, 'w') as output:
             fields_to_es_template(args, fields, output, args.beatname + "-*")
