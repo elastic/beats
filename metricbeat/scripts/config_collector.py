0b382561d5559f2db45e4c38d21de310b4695a3f
Tudor Golubenco <tudor@elastic.co>
2016-05-16 21:18:31 +0200

    Added short versions for the configuration files (#1637)
    For now, the short files are named like `beatname.short.yml` and I kept the
    `beatname.yml` to be the full one.  The intention is to switch them around. The
    short ones to become the default and the long ones to be something like
    `beatname.full.yml` or similar.
    
    But before doing that I wanted the others in the team to have a look. The
    decision of what to include in the short version is fairly arbitrary, I just
    did it based on what I thought makes most sense.
    
    I kept the ES and LS outputs in the configuration file. I considered having
    only ES for all the beats except Filebeat, but that would complicate the
    generation part and I think it's nice to highlight that we support multiple
    outputs.

--- metricbeat/scripts/config_collector.py
+++ metricbeat/scripts/config_collector.py
@@ -1,8 +1,30 @@
 import os
+import argparse
 
 # Collects config for all modules
 
-header = """###################### Metricbeat Configuration Example #######################
+header = """########################## Metricbeat Configuration ###########################
+
+# This file is a full configuration example documenting all non-deprecated
+# options in comments. For a shorter configuration example, that contains only
+# the most common options, please see metricbeat.short.yml in the same directory.
+#
+# You can find the full configuration reference here:
+# https://www.elastic.co/guide/en/beats/metricbeat/index.html
+
+#==========================  Modules configuration ============================
+metricbeat.modules:
+
+"""
+
+header_short = """###################### Metricbeat Configuration Example #######################
+
+# This file is an example configuration file highlighting only the most common
+# options. The metricbeat.yml file from the same directory contains all the
+# supported options with more comments. You can use it as a reference.
+#
+# You can find the full configuration reference here:
+# https://www.elastic.co/guide/en/beats/metricbeat/index.html
 
 #==========================  Modules configuration ============================
 metricbeat.modules:
@@ -10,7 +32,7 @@ metricbeat.modules:
 """
 
 
-def collect():
+def collect(short=False):
 
     base_dir = "module"
     path = os.path.abspath("module")
@@ -21,7 +43,10 @@ def collect():
     # Iterate over all modules
     for module in os.listdir(base_dir):
 
-        module_configs = path + "/" + module + "/_beat/config.yml"
+        if short:
+            module_configs = path + "/" + module + "/_beat/config.short.yml"
+        else:
+            module_configs = path + "/" + module + "/_beat/config.yml"
 
         # Only check folders where fields.yml exists
         if not os.path.isfile(module_configs):
@@ -37,4 +62,9 @@ def collect():
     print config_yml
 
 if __name__ == "__main__":
-    collect()
+    parser = argparse.ArgumentParser(
+        description="Collects modules docs")
+    parser.add_argument("--short", action="store_true",
+                        help="Collect the short versions")
+    args = parser.parse_args()
+    collect(args.short)
