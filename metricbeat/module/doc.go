c8a7c0c3002d17a662c91e2aa86f390a03b0ef61
ruflin <spam@ruflin.com>
2016-05-17 20:31:46 +0200

    Update redis to new data structure
    Add template for all fields.

--- metricbeat/module/doc.go
+++ metricbeat/module/doc.go
@@ -11,21 +11,26 @@ For the key names, metricbeat follows the naming conventions below:
 * Prevent namespace duplication. If connections appears in the namespace, it's not needed in the sub document
 * Do not use complex abbreviations. A list of standardised abbreviations can be found below.
 * Organise the documents from the general to the details, which allows namespacing. The type should always be last, like .pct.
+* If two fields are the same but with different units, remove the less granular one
+* In case the value correlates with the name of a nested document, use value inside the document
+* Do not use . in the names
 
 The goal is to have a similar experience across all metrics.
 
 
 = Abbrevations
 
-List of standardised words across all metricsets
+List of standardised words and units across all metricsets. On the left are the ones to be used, on the right the options seen in metricsets.
 
 * avg: average
 * count:
+* day: days, d
 * max: maximumg
 * min: minimum
 * pct: precentage
 * request:
-
+* sec: seconds, second, s
+*
 
 */
 package module
