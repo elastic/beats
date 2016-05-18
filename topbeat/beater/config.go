b84c506f9da34eff0aff6b9055d31f94e3292c6b
urso <steffen.siering@elastic.co>
2016-05-18 14:23:20 +0200

    Streamline topbeat config loading

--- topbeat/beater/config.go
+++ topbeat/beater/config.go
@@ -1,17 +1,48 @@
 package beater
 
-type TopConfig struct {
-	Period *int64
-	Procs  *[]string
-	Stats  struct {
-		System     *bool `config:"system"`
-		Proc       *bool `config:"process"`
-		Filesystem *bool `config:"filesystem"`
-		CpuPerCore *bool `config:"cpu_per_core"`
-	}
-}
+import (
+	"errors"
+	"time"
+)
 
 type ConfigSettings struct {
-	Input   *TopConfig `config:"input"`
-	Topbeat *TopConfig `config:"topbeat"`
+	Topbeat TopConfig `config:"topbeat"`
+}
+
+type TopConfig struct {
+	Period time.Duration     `config:"period"  validate:"min=1ms"`
+	Procs  []string          `config:"procs"`
+	Stats  StatsEnableConfig `config:"stats"`
+}
+
+type StatsEnableConfig struct {
+	System     bool `config:"system"`
+	Proc       bool `config:"process"`
+	Filesystem bool `config:"filesystem"`
+	CPUPerCore bool `config:"cpu_per_core"`
+}
+
+var (
+	defaultConfig = TopConfig{
+		Period: 10 * time.Second,
+		Procs:  []string{".*"}, //all processes
+		Stats: StatsEnableConfig{
+			System:     true,
+			Proc:       true,
+			Filesystem: true,
+			CPUPerCore: false,
+		},
+	}
+)
+
+func (c *TopConfig) Validate() error {
+	return nil
+}
+
+func (c *StatsEnableConfig) Validate() error {
+	if !c.System && !c.Proc && !c.Filesystem {
+		return errors.New("Invalid statistics configuration (enable one of 'system', 'process' or 'filesystem')")
+	}
+
+	return nil
 }
