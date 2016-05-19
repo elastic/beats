5913e7a88cb117952358fd9de34860a0917d0bbb
Tudor Golubenco <tudor@elastic.co>
2016-05-19 15:48:09 +0200

    Swap full and short configuration files (#1674)
    * filebeat.yml is now the short version
    * filebeat.full.yml is now the complete version
    
    For Metricbeat, if only config.yml exists, it is used for both
    short and full.

--- metricbeat/scripts/config_collector.py
+++ metricbeat/scripts/config_collector.py
@@ -4,11 +4,11 @@ import yaml
 
 # Collects config for all modules
 
-header = """########################## Metricbeat Configuration ###########################
+header_full = """########################## Metricbeat Configuration ###########################
 
 # This file is a full configuration example documenting all non-deprecated
 # options in comments. For a shorter configuration example, that contains only
-# the most common options, please see metricbeat.short.yml in the same directory.
+# the most common options, please see metricbeat.yml in the same directory.
 #
 # You can find the full configuration reference here:
 # https://www.elastic.co/guide/en/beats/metricbeat/index.html
@@ -21,7 +21,7 @@ metricbeat.modules:
 header_short = """###################### Metricbeat Configuration Example #######################
 
 # This file is an example configuration file highlighting only the most common
-# options. The metricbeat.yml file from the same directory contains all the
+# options. The metricbeat.full.yml file from the same directory contains all the
 # supported options with more comments. You can use it as a reference.
 #
 # You can find the full configuration reference here:
@@ -33,13 +33,17 @@ metricbeat.modules:
 """
 
 
-def collect(beat_path, short=False):
+def collect(beat_path, full=False):
 
     base_dir = beat_path + "/module"
     path = os.path.abspath(base_dir)
 
     # yml file
-    config_yml = header
+
+    if full:
+        config_yml = header_full
+    else:
+        config_yml = header_short
 
     # Iterate over all modules
     for module in os.listdir(base_dir):
@@ -51,16 +55,15 @@ def collect(beat_path, short=False):
         # By default, short config is read if short is set
         short_config = True
 
-        # Check if short config exists
-        if short:
-            short_module_config = beat_path + "/config.short.yml"
-            if os.path.isfile(short_module_config):
-                module_configs = short_module_config
+        # Check if full config exists
+        if full:
+            full_module_config = beat_path + "/config.full.yml"
+            if os.path.isfile(full_module_config):
+                module_configs = full_module_config
 
         # Only check folders where config exists
         if not os.path.isfile(module_configs):
             continue
-
 
         # Load title from fields.yml
         with open(beat_path + "/fields.yml") as f:
@@ -68,10 +71,10 @@ def collect(beat_path, short=False):
             title = fields[0]["title"]
 
             # Check if short config was disabled in fields.yml
-            if short and "short_config" in fields[0]:
+            if not full and "short_config" in fields[0]:
                 short_config = fields[0]["short_config"]
 
-        if short and short_config == False:
+        if not full and short_config is False:
             continue
 
         config_yml += get_title_line(title)
@@ -84,6 +87,7 @@ def collect(beat_path, short=False):
         config_yml += "\n"
     # output string so it can be concatenated
     print config_yml
+
 
 # Makes sure every title line is 79 + newline chars long
 def get_title_line(title):
@@ -101,10 +105,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Collects modules config")
     parser.add_argument("path", help="Path to the beat folder")
-    parser.add_argument("--short", action="store_true",
-                        help="Collect the short versions")
+    parser.add_argument("--full", action="store_true",
+                        help="Collect the full versions")
 
     args = parser.parse_args()
     beat_path = args.path
 
-    collect(beat_path, args.short)
+    collect(beat_path, args.full)
