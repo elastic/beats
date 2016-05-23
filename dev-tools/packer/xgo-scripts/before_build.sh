6f04c8f121f53acb7974a60ceccb059c00445563
Tudor Golubenco <tudor@elastic.co>
2016-05-23 08:40:51 +0200

    Install full configs in packages (#1682)
    This adds the "full" version of the configuration files to the
    config path of all the artifacts.

--- dev-tools/packer/xgo-scripts/before_build.sh
+++ dev-tools/packer/xgo-scripts/before_build.sh
@@ -33,15 +33,15 @@ cp $BEATNAME.template-es2x.json $PREFIX/$BEATNAME.template-es2x.json
 
 # linux
 cp $BEATNAME.yml $PREFIX/$BEATNAME-linux.yml
-
-# binary
-cp $BEATNAME.yml $PREFIX/$BEATNAME-binary.yml
+cp $BEATNAME.full.yml $PREFIX/$BEATNAME-linux.full.yml
 
 # darwin
 cp $BEATNAME.yml $PREFIX/$BEATNAME-darwin.yml
+cp $BEATNAME.full.yml $PREFIX/$BEATNAME-darwin.full.yml
 
 # win
 cp $BEATNAME.yml $PREFIX/$BEATNAME-win.yml
+cp $BEATNAME.full.yml $PREFIX/$BEATNAME-win.full.yml
 
 # Contains beat specific adjustments. As it is platform specific knowledge, it should be in packer not the beats itself
 PREFIX=$PREFIX make before-build
