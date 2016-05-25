2d2240f67c5f0ce1f4c7052f3087a1b81d028663
Nicolas Ruflin <spam@ruflin.com>
2016-05-25 11:14:30 +0200

    Add alpha3 snapshots for testing (#1722)
    Add script to simplify common parts between elasticsearch docker images

---
+++ testing/environments/docker/elasticsearch/setup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+
+# grab gosu for easy step-down from root
+gpg --keyserver ha.pool.sks-keyservers.net --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4
+arch="$(dpkg --print-architecture)" \
+	&& set -x \
+	&& curl -o /usr/local/bin/gosu -fSL "https://github.com/tianon/gosu/releases/download/1.3/gosu-$arch" \
+	&& curl -o /usr/local/bin/gosu.asc -fSL "https://github.com/tianon/gosu/releases/download/1.3/gosu-$arch.asc" \
+	&& gpg --verify /usr/local/bin/gosu.asc \
+	&& rm /usr/local/bin/gosu.asc \
+	&& chmod +x /usr/local/bin/gosu
+
+apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 46095ACC8548582C1A2699A9D27D666CD88E42B4
+
+wget $1 -O elasticsearch.deb
+
+dpkg -i elasticsearch.deb
+
+set -ex \
+	&& for path in \
+		/usr/share/elasticsearch/data \
+		/usr/share/elasticsearch/logs \
+		/usr/share/elasticsearch/config \
+		/usr/share/elasticsearch/config/scripts \
+	; do \
+		mkdir -p "$path"; \
+		chown -R elasticsearch:elasticsearch "$path"; \
+	done
