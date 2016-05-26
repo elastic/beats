74dc43b953cb9519e461b29dd3a5b4a73ce05aae
Monica Sarbu <monica@elastic.co>
2016-05-26 08:02:32 +0200

    Update Metricbeat examples in docs (#1733)
    * Update examples from the system module of Metricbeat
    * Replace hostname with host.example.com
    * Replace real username

--- metricbeat/module/system/filesystem/doc.go
+++ metricbeat/module/system/filesystem/doc.go
@@ -4,27 +4,32 @@ for each of the mounted file systems.
 
 An example event looks as following:
 
-    {
-      "@timestamp": "2016-04-26T19:30:19.475Z",
-      "beat": {
-        "hostname": "host.example.com",
-        "name": "host.example.com"
-      },
-      "metricset": "filesystem",
-      "module": "system",
-      "rtt": 434,
-      "system-filesystem": {
-        "avail": 41159540736,
-        "device_name": "/dev/disk1",
-        "files": 60981246,
-        "free": 41421684736,
-        "free_files": 10048716,
-        "mount_point": "/",
-        "total": 249779191808,
-        "used": 208357507072,
-        "used_p": 0.83
-      },
-      "type": "metricsets"
+{
+  "@timestamp": "2016-05-25T20:51:36.813Z",
+  "beat": {
+    "hostname": "host.example.com",
+    "name": "host.example.com"
+  },
+  "metricset": {
+    "module": "system",
+    "name": "filesystem",
+    "rtt": 55
+  },
+  "system": {
+    "filesystem": {
+      "avail": 13838553088,
+      "device_name": "/dev/disk1",
+      "files": 60981246,
+      "free": 14100697088,
+      "free_files": 3378553,
+      "mount_point": "/",
+      "total": 249779191808,
+      "used": 235678494720,
+      "used_p": 0.9435
     }
+  },
+  "type": "metricsets"
+}
+
 */
 package filesystem
