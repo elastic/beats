d78cda76e7a2130a04435c8b16ef32381aa8c802
Nicolas Ruflin <spam@ruflin.com>
2016-05-26 23:23:54 +0200

    Add Redis Keyspace metricset (#1728)
    * Extract it from the redis implementation because we now push arrays as separate documents
    * Extract common parts to module

--- metricbeat/module/redis/info/data.go
+++ metricbeat/module/redis/info/data.go
@@ -1,8 +1,6 @@
 package info
 
 import (
-	"strings"
-
 	"github.com/elastic/beats/libbeat/common"
 	h "github.com/elastic/beats/metricbeat/helper"
 )
@@ -116,53 +114,7 @@ func eventMapping(info map[string]string) common.MapStr {
 			"latest_fork_usec":       h.ToInt("latest_fork_usec", info),
 			"migrate_cached_sockets": h.ToInt("migrate_cached_sockets", info),
 		},
-		"keyspace": getKeyspaceStats(info),
 	}
 
 	return event
-}
-
-func getKeyspaceStats(info map[string]string) map[string]common.MapStr {
-	keyspaceMap := findKeyspaceStats(info)
-	return parseKeyspaceStats(keyspaceMap)
-}
-
-// findKeyspaceStats will grep for keyspace ("^db" keys) and return the resulting map
-func findKeyspaceStats(info map[string]string) map[string]string {
-	keyspace := map[string]string{}
-
-	for k, v := range info {
-		if strings.HasPrefix(k, "db") {
-			keyspace[k] = v
-		}
-	}
-	return keyspace
-}
-
-// parseKeyspaceStats resolves the overloaded value string that Redis returns for keyspace
-func parseKeyspaceStats(keyspaceMap map[string]string) map[string]common.MapStr {
-	keyspace := map[string]common.MapStr{}
-	for k, v := range keyspaceMap {
-
-		// Extract out the overloaded values for db keyspace
-		// fmt: info[db0] = keys=795341,expires=0,avg_ttl=0
-		dbInfo := parseRedisLine(v, ",")
-
-		if len(dbInfo) == 3 {
-			db := map[string]string{}
-			for _, dbEntry := range dbInfo {
-				stats := parseRedisLine(dbEntry, "=")
-
-				if len(stats) == 2 {
-					db[stats[0]] = stats[1]
-				}
-			}
-			keyspace[k] = common.MapStr{
-				"keys":    h.ToInt("keys", db),
-				"expires": h.ToInt("expires", db),
-				"avg_ttl": h.ToInt("avg_ttl", db),
-			}
-		}
-	}
-	return keyspace
 }
