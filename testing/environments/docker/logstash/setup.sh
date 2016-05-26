204b771bf004701c05c5411b229a69fef4c4b1bb
Nicolas Ruflin <spam@ruflin.com>
2016-05-26 10:11:33 +0200

    Docker file cleanup (#1724)
    * Make sure Kibana is started for local test environment
    * Test command simplification
    * Remove old Docker files
    * Simplify logstash setup through script

---
+++ testing/environments/docker/logstash/setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# install logstash
+set -x && \
+    mkdir -p /var/tmp && \
+    wget -qO /var/tmp/logstash.deb $1 && \
+    apt-get update -y && \
+    apt-get install -y logrotate git && \
+    dpkg -i /var/tmp/logstash.deb && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+logstash-plugin install logstash-input-beats
