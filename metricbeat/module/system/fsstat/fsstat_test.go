6a30ff51754b35bb58d1048a059ba025f9e7762f
Nicolas Ruflin <spam@ruflin.com>
2016-05-30 21:25:55 +0200

    Add building example data for metricsets (#1762)
    Added:
    * filesystem
    * fsstat
    * process
    
    Further changes
    * Add Makefile command to generate data.json examples
    * Dynamically link data.json in docs. Less duplicates and less conflicts

---
+++ metricbeat/module/system/fsstat/fsstat_test.go
@@ -0,0 +1,29 @@
+package fsstat
+
+import (
+	"testing"
+
+	"time"
+
+	mbtest "github.com/elastic/beats/metricbeat/mb/testing"
+)
+
+func TestData(t *testing.T) {
+	f := mbtest.NewEventFetcher(t, getConfig())
+
+	// Do a first fetch to have precentages
+	f.Fetch()
+	time.Sleep(1 * time.Second)
+
+	err := mbtest.WriteEvent(f, t)
+	if err != nil {
+		t.Fatal("write", err)
+	}
+}
+
+func getConfig() map[string]interface{} {
+	return map[string]interface{}{
+		"module":     "system",
+		"metricsets": []string{"fsstat"},
+	}
+}
