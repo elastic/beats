dedc109373545f01aac76cb8066338829f85af84
Nicolas Ruflin <spam@ruflin.com>
2016-06-24 22:44:47 +0200

    Update dependencies (#1905)
    * Add .bat to cleanup script
    * Replace labix.org/v2 through gopkg.in/mgo.v2 as it allows setting a version number
    * A more versions numbers to dependencies and update them
    * Fix imports which broke metricbeat crosscompile

--- metricbeat/include/list.go
+++ metricbeat/include/list.go
@@ -20,7 +20,6 @@ import (
 	_ "github.com/elastic/beats/metricbeat/module/redis/info"
 	_ "github.com/elastic/beats/metricbeat/module/redis/keyspace"
 	_ "github.com/elastic/beats/metricbeat/module/system"
-	_ "github.com/elastic/beats/metricbeat/module/system/common"
 	_ "github.com/elastic/beats/metricbeat/module/system/core"
 	_ "github.com/elastic/beats/metricbeat/module/system/cpu"
 	_ "github.com/elastic/beats/metricbeat/module/system/diskio"
