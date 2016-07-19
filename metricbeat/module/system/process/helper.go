b5c9a99b7c28c1143c25f164fe09c8f56a33bbed
nickivanov <nickivanov@users.noreply.github.com>
2016-07-19 08:39:59 -0400

    Add PGID field to Process struct used in Metricbeat (#2021)
    * Added PGID field to Process struct to use the recent gosigar addition.
    * Added pgid field documentation.
    * Added pgid notice to CHANGELOG.
    * Fixed test for pgid field.

--- metricbeat/module/system/process/helper.go
+++ metricbeat/module/system/process/helper.go
@@ -20,6 +20,7 @@ type ProcsMap map[int]*Process
 type Process struct {
 	Pid      int    `json:"pid"`
 	Ppid     int    `json:"ppid"`
+	Pgid     int    `json:"pgid"`
 	Name     string `json:"name"`
 	Username string `json:"username"`
 	State    string `json:"state"`
@@ -48,6 +49,7 @@ func newProcess(pid int) (*Process, error) {
 	proc := Process{
 		Pid:      pid,
 		Ppid:     state.Ppid,
+		Pgid:     state.Pgid,
 		Name:     state.Name,
 		State:    getProcState(byte(state.State)),
 		Username: state.Username,
@@ -132,6 +134,7 @@ func (procStats *ProcStats) GetProcessEvent(process *Process, last *Process) com
 	proc := common.MapStr{
 		"pid":      process.Pid,
 		"ppid":     process.Ppid,
+		"pgid":     process.Pgid,
 		"name":     process.Name,
 		"state":    process.State,
 		"username": process.Username,
