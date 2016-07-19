7bd61bf29f217e79437c354818b86ea9ef90e624
Tudor Golubenco <tudor@elastic.co>
2016-07-19 14:52:43 +0200

    Added info about schema to the Metricbeat dev guide (#2057)

--- metricbeat/schema/doc.go
+++ metricbeat/schema/doc.go
@@ -1,8 +1,8 @@
 /*
-Package schema contains helper utilities to parse, convert, rename and reorganize data
+Package schema contains helper utilities to parse, convert, rename, and reorganize data
 from the format returned by the various monitored systems to the data model used by
 Metricbeat.
 
-See the unit tests in the mapstrstr and mapstriface for usage examples.
+See the docs in the mapstrstr and mapstriface sub-packages for usage examples.
 */
 package schema
