e49d5373f0bec8a63f873557801cf1ba3f76ee9c
Steffen Siering <steffen.siering@elastic.co>
2016-07-19 23:17:50 +0200

    Backport: Check stdout being available in console output (#2063)

--- libbeat/outputs/console/console.go
+++ libbeat/outputs/console/console.go
@@ -2,6 +2,7 @@ package console
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 
 	"github.com/elastic/beats/libbeat/common"
@@ -20,28 +21,23 @@ func (p plugin) NewOutput(
 	topologyExpire int,
 ) (outputs.Outputer, error) {
 	pretty := config.Pretty != nil && *config.Pretty
-	return newConsole(pretty), nil
+	c := newConsole(pretty)
+
+	// check stdout actually being available
+	if _, err := c.out.Stat(); err != nil {
+		return nil, fmt.Errorf("console output initialization failed with: %v", err)
+	}
+
+	return c, nil
 }
 
 type console struct {
 	pretty bool
+	out    *os.File
 }
 
 func newConsole(pretty bool) *console {
-	return &console{pretty}
-}
-
-func writeBuffer(buf []byte) error {
-	written := 0
-	for written < len(buf) {
-		n, err := os.Stdout.Write(buf[written:])
-		if err != nil {
-			return err
-		}
-
-		written += n
-	}
-	return nil
+	return &console{pretty: pretty, out: os.Stdout}
 }
 
 func (c *console) PublishEvent(
@@ -63,10 +59,10 @@ func (c *console) PublishEvent(
 		return err
 	}
 
-	if err = writeBuffer(jsonEvent); err != nil {
+	if err = c.writeBuffer(jsonEvent); err != nil {
 		goto fail
 	}
-	if err = writeBuffer([]byte{'\n'}); err != nil {
+	if err = c.writeBuffer([]byte{'\n'}); err != nil {
 		goto fail
 	}
 
@@ -78,4 +74,17 @@ fail:
 	}
 	outputs.SignalFailed(s, err)
 	return err
+}
+
+func (c *console) writeBuffer(buf []byte) error {
+	written := 0
+	for written < len(buf) {
+		n, err := c.out.Write(buf[written:])
+		if err != nil {
+			return err
+		}
+
+		written += n
+	}
+	return nil
 }
--- libbeat/outputs/console/console_test.go
+++ libbeat/outputs/console/console_test.go
@@ -44,8 +44,9 @@ func event(k, v string) common.MapStr {
 	return common.MapStr{k: v}
 }
 
-func run(c *console, events ...common.MapStr) (string, error) {
+func run(pretty bool, events ...common.MapStr) (string, error) {
 	return withStdout(func() {
+		c := newConsole(pretty)
 		for _, event := range events {
 			c.PublishEvent(nil, outputs.Options{}, event)
 		}
@@ -53,21 +54,21 @@ func run(c *console, events ...common.MapStr) (string, error) {
 }
 
 func TestConsoleOneEvent(t *testing.T) {
-	lines, err := run(newConsole(false), event("event", "myevent"))
+	lines, err := run(false, event("event", "myevent"))
 	assert.Nil(t, err)
 	expected := "{\"event\":\"myevent\"}\n"
 	assert.Equal(t, expected, lines)
 }
 
 func TestConsoleOneEventIndented(t *testing.T) {
-	lines, err := run(newConsole(true), event("event", "myevent"))
+	lines, err := run(true, event("event", "myevent"))
 	assert.Nil(t, err)
 	expected := "{\n  \"event\": \"myevent\"\n}\n"
 	assert.Equal(t, expected, lines)
 }
 
 func TestConsoleMultipleEvents(t *testing.T) {
-	lines, err := run(newConsole(false),
+	lines, err := run(false,
 		event("event", "event1"),
 		event("event", "event2"),
 		event("event", "event3"),
@@ -79,7 +80,7 @@ func TestConsoleMultipleEvents(t *testing.T) {
 }
 
 func TestConsoleMultipleEventsIndented(t *testing.T) {
-	lines, err := run(newConsole(true),
+	lines, err := run(true,
 		event("event", "event1"),
 		event("event", "event2"),
 		event("event", "event3"),
