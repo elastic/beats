621a2f34548d366c6e5edc5a7d87d4caadf299fd
Tudor Golubenco <tudor@elastic.co>
2016-07-26 04:18:37 +0200

    Unmarshal JSON numbers as ints where possible (#2091)
    The change modifies the unmarshaling logic to try parsing the numbers
    in an int64 first, and only on error parse to a float64. In practice,
    this means that e.g. 1 will become an int64 and 1.0 will become a
    float64.
    
    Fixes #2038. Includes a system test to verify that ticket.

--- filebeat/harvester/reader/json.go
+++ filebeat/harvester/reader/json.go
@@ -1,6 +1,7 @@
 package reader
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 
@@ -25,8 +26,9 @@ func NewJSON(r Reader, cfg *JSONConfig) *JSON {
 // decodeJSON unmarshals the text parameter into a MapStr and
 // returns the new text column if one was requested.
 func (r *JSON) decodeJSON(text []byte) ([]byte, common.MapStr) {
-	var jsonFields common.MapStr
-	err := json.Unmarshal(text, &jsonFields)
+	var jsonFields map[string]interface{}
+
+	err := unmarshal(text, &jsonFields)
 	if err != nil {
 		logp.Err("Error decoding JSON: %v", err)
 		if r.cfg.AddErrorKey {
@@ -56,6 +58,60 @@ func (r *JSON) decodeJSON(text []byte) ([]byte, common.MapStr) {
 	}
 
 	return []byte(textString), jsonFields
+}
+
+// unmarshal is equivalent with json.Unmarshal but it converts numbers
+// to int64 where possible, instead of using always float64.
+func unmarshal(text []byte, fields *map[string]interface{}) error {
+	dec := json.NewDecoder(bytes.NewReader(text))
+	dec.UseNumber()
+	err := dec.Decode(fields)
+	if err != nil {
+		return err
+	}
+	transformNumbersDict(*fields)
+	return nil
+}
+
+// transformNumbersDict walks a json decoded tree an replaces json.Number
+// with int64, float64, or string, in this order of preference (i.e. if it
+// parses as an int, use int. if it parses as a float, use float. etc).
+func transformNumbersDict(dict common.MapStr) {
+	for k, v := range dict {
+		switch vv := v.(type) {
+		case json.Number:
+			dict[k] = transformNumber(vv)
+		case map[string]interface{}:
+			transformNumbersDict(vv)
+		case []interface{}:
+			transformNumbersArray(vv)
+		}
+	}
+}
+
+func transformNumber(value json.Number) interface{} {
+	i64, err := value.Int64()
+	if err == nil {
+		return i64
+	}
+	f64, err := value.Float64()
+	if err == nil {
+		return f64
+	}
+	return value.String()
+}
+
+func transformNumbersArray(arr []interface{}) {
+	for i, v := range arr {
+		switch vv := v.(type) {
+		case json.Number:
+			arr[i] = transformNumber(vv)
+		case map[string]interface{}:
+			transformNumbersDict(vv)
+		case []interface{}:
+			transformNumbersArray(vv)
+		}
+	}
 }
 
 // Next decodes JSON and returns the filled Line object.
