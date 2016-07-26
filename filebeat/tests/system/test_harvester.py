elastic/beats

commit 1ce32d2c51e7f17d5f62f07ad9447e50aab3f061

Harvester Cleanup (#2090)
* Rename FileEvent to Event
* Remove double information between FileEvent and State (Source, Fileinfo)
* Remove readLine function as not needed anymore

filebeat/harvester/log.go
@@ -44,7 +44,8 @@ func (h *Harvester) Harvest() {
 
 	logp.Info("Harvester started for file: %s", h.state.Source)
 
-	r, err := h.newLineReader()
+	r, err := h.newLogFileReader()
+
 	if err != nil {
 		logp.Err("Stop Harvesting. Unexpected encoding line reader error: %s", err)
 		return
@@ -63,8 +64,7 @@ func (h *Harvester) Harvest() {
 		default:
 		}
 
-		// Partial lines return error and are only read on completion
-		ts, text, bytesRead, jsonFields, err := readLine(r)
+		message, err := r.Next()
 		if err != nil {
 			switch err {
 			case reader.ErrFileTruncate:
@@ -83,16 +83,24 @@ func (h *Harvester) Harvest() {
 			return
 		}
 
-		// Update offset if complete line has been processed
-		h.state.Offset += int64(bytesRead)
+		// Update offset
+		h.state.Offset += int64(message.Bytes)
 
-		event := h.createEvent()
+		// Create state event
+		event := input.NewEvent(h.getState())
 
+		text := string(message.Content)
+
+		// Check if data should be added to event
 		if h.shouldExportLine(text) {
-			event.ReadTime = ts
-			event.Bytes = bytesRead
+			event.ReadTime = message.Ts
+			event.Bytes = message.Bytes
 			event.Text = &text
-			event.JSONFields = jsonFields
+			event.JSONFields = message.Fields
+			event.EventMetadata = h.config.EventMetadata
+			event.InputType = h.config.InputType
+			event.DocumentType = h.config.DocumentType
+			event.JSONConfig = h.config.JSON
 		}
 
 		// Always send event to update state, also if lines was skipped
@@ -103,29 +111,9 @@ func (h *Harvester) Harvest() {
 	}
 }
 
-// createEvent creates and empty event.
-// By default the offset is set to 0, means no bytes read. This can be used to report the status
-// of a harvester
-func (h *Harvester) createEvent() *input.FileEvent {
-
-	event := &input.FileEvent{
-		EventMetadata: h.config.EventMetadata,
-		Source:        h.state.Source,
-		InputType:     h.config.InputType,
-		DocumentType:  h.config.DocumentType,
-		Offset:        h.state.Offset,
-		Bytes:         0,
-		Fileinfo:      h.state.Fileinfo,
-		JSONConfig:    h.config.JSON,
-		State:         h.getState(),
-	}
-
-	return event
-}
-
 // sendEvent sends event to the spooler channel
 // Return false if event was not sent
-func (h *Harvester) sendEvent(event *input.FileEvent) bool {
+func (h *Harvester) sendEvent(event *input.Event) bool {
 	select {
 	case <-h.done:
 		return false
@@ -297,7 +285,7 @@ func (h *Harvester) newLogFileReaderConfig() reader.LogFileConfig {
 	}
 }
 
-func (h *Harvester) newLineReader() (reader.Reader, error) {
+func (h *Harvester) newLogFileReader() (reader.Reader, error) {
 
 	readerConfig := h.newLogFileReaderConfig()
 

filebeat/beater/filebeat.go
@@ -51,7 +51,7 @@ func (fb *Filebeat) Run(b *beat.Beat) error {
 	}
 
 	// Channel from harvesters to spooler
-	publisherChan := make(chan []*input.FileEvent, 1)
+	publisherChan := make(chan []*input.Event, 1)
 
 	// Publishes event to output
 	publisher := publish.New(config.PublishAsync,

filebeat/harvester/harvester.go
@@ -27,7 +27,7 @@ import (
 type Harvester struct {
 	config          harvesterConfig
 	state           file.State
-	prospectorChan  chan *input.FileEvent
+	prospectorChan  chan *input.Event
 	file            source.FileSource /* the file being watched */
 	done            chan struct{}
 	encodingFactory encoding.EncodingFactory
@@ -37,7 +37,7 @@ type Harvester struct {
 func NewHarvester(
 	cfg *common.Config,
 	state file.State,
-	prospectorChan chan *input.FileEvent,
+	prospectorChan chan *input.Event,
 	done chan struct{},
 ) (*Harvester, error) {
 

filebeat/harvester/log_test.go
@@ -77,7 +77,7 @@ func TestReadLine(t *testing.T) {
 	h.encoding, err = h.encodingFactory(readFile)
 	assert.NoError(t, err)
 
-	r, err := h.newLineReader()
+	r, err := h.newLogFileReader()
 	assert.NoError(t, err)
 
 	// Read third line

filebeat/input/event.go
@@ -2,7 +2,6 @@ package input
 
 import (
 	"fmt"
-	"os"
 	"time"
 
 	"github.com/elastic/beats/filebeat/harvester/reader"
@@ -11,34 +10,31 @@ import (
 	"github.com/elastic/beats/libbeat/logp"
 )
 
-// FileEvent is sent to the output and must contain all relevant information
-type FileEvent struct {
+// Event is sent to the output and must contain all relevant information
+type Event struct {
 	common.EventMetadata
 	ReadTime     time.Time
-	Source       string
 	InputType    string
 	DocumentType string
-	Offset       int64
 	Bytes        int
 	Text         *string
-	Fileinfo     os.FileInfo
 	JSONFields   common.MapStr
 	JSONConfig   *reader.JSONConfig
 	State        file.State
 }
 
-func NewEvent(state file.State) *FileEvent {
-	return &FileEvent{
+func NewEvent(state file.State) *Event {
+	return &Event{
 		State: state,
 	}
 }
 
-func (f *FileEvent) ToMapStr() common.MapStr {
+func (f *Event) ToMapStr() common.MapStr {
 	event := common.MapStr{
 		common.EventMetadataKey: f.EventMetadata,
 		"@timestamp":            common.Time(f.ReadTime),
-		"source":                f.Source,
-		"offset":                f.Offset, // Offset here is the offset before the starting char.
+		"source":                f.State.Source,
+		"offset":                f.State.Offset, // Offset here is the offset before the starting char.
 		"type":                  f.DocumentType,
 		"input_type":            f.InputType,
 	}
@@ -56,7 +52,7 @@ func (f *FileEvent) ToMapStr() common.MapStr {
 // respecting the KeysUnderRoot and OverwriteKeys configuration options.
 // If MessageKey is defined, the Text value from the event always
 // takes precedence.
-func mergeJSONFields(f *FileEvent, event common.MapStr) {
+func mergeJSONFields(f *Event, event common.MapStr) {
 
 	// The message key might have been modified by multiline
 	if len(f.JSONConfig.MessageKey) > 0 && f.Text != nil {

filebeat/input/event_test.go
@@ -9,17 +9,17 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFileEventToMapStr(t *testing.T) {
+func TestEventToMapStr(t *testing.T) {
 	// Test 'fields' is not present when it is nil.
-	event := FileEvent{}
+	event := Event{}
 	mapStr := event.ToMapStr()
 	_, found := mapStr["fields"]
 	assert.False(t, found)
 }
 
-func TestFileEventToMapStrJSON(t *testing.T) {
+func TestEventToMapStrJSON(t *testing.T) {
 	type io struct {
-		Event         FileEvent
+		Event         Event
 		ExpectedItems common.MapStr
 	}
 
@@ -30,7 +30,7 @@ func TestFileEventToMapStrJSON(t *testing.T) {
 	tests := []io{
 		{
 			// by default, don't overwrite keys
-			Event: FileEvent{
+			Event: Event{
 				DocumentType: "test_type",
 				Text:         &text,
 				JSONFields:   common.MapStr{"type": "test", "text": "hello"},
@@ -43,7 +43,7 @@ func TestFileEventToMapStrJSON(t *testing.T) {
 		},
 		{
 			// overwrite keys if asked
-			Event: FileEvent{
+			Event: Event{
 				DocumentType: "test_type",
 				Text:         &text,
 				JSONFields:   common.MapStr{"type": "test", "text": "hello"},
@@ -56,7 +56,7 @@ func TestFileEventToMapStrJSON(t *testing.T) {
 		},
 		{
 			// without keys_under_root, put everything in a json key
-			Event: FileEvent{
+			Event: Event{
 				DocumentType: "test_type",
 				Text:         &text,
 				JSONFields:   common.MapStr{"type": "test", "text": "hello"},
@@ -69,7 +69,7 @@ func TestFileEventToMapStrJSON(t *testing.T) {
 		},
 		{
 			// when MessageKey is defined, the Text overwrites the value of that key
-			Event: FileEvent{
+			Event: Event{
 				DocumentType: "test_type",
 				Text:         &text,
 				JSONFields:   common.MapStr{"type": "test", "text": "hi"},
@@ -83,7 +83,7 @@ func TestFileEventToMapStrJSON(t *testing.T) {
 		{
 			// when @timestamp is in JSON and overwrite_keys is true, parse it
 			// in a common.Time
-			Event: FileEvent{
+			Event: Event{
 				ReadTime:     now,
 				DocumentType: "test_type",
 				Text:         &text,
@@ -98,7 +98,7 @@ func TestFileEventToMapStrJSON(t *testing.T) {
 		{
 			// when the parsing on @timestamp fails, leave the existing value and add an error key
 			// in a common.Time
-			Event: FileEvent{
+			Event: Event{
 				ReadTime:     now,
 				DocumentType: "test_type",
 				Text:         &text,
@@ -114,7 +114,7 @@ func TestFileEventToMapStrJSON(t *testing.T) {
 		{
 			// when the @timestamp has the wrong type, leave the existing value and add an error key
 			// in a common.Time
-			Event: FileEvent{
+			Event: Event{
 				ReadTime:     now,
 				DocumentType: "test_type",
 				Text:         &text,
@@ -129,7 +129,7 @@ func TestFileEventToMapStrJSON(t *testing.T) {
 		},
 		{
 			// if overwrite_keys is true, but the `type` key in json is not a string, ignore it
-			Event: FileEvent{
+			Event: Event{
 				DocumentType: "test_type",
 				Text:         &text,
 				JSONFields:   common.MapStr{"type": 42},
@@ -142,7 +142,7 @@ func TestFileEventToMapStrJSON(t *testing.T) {
 		},
 		{
 			// if overwrite_keys is true, but the `type` key in json is empty, ignore it
-			Event: FileEvent{
+			Event: Event{
 				DocumentType: "test_type",
 				Text:         &text,
 				JSONFields:   common.MapStr{"type": ""},
@@ -155,7 +155,7 @@ func TestFileEventToMapStrJSON(t *testing.T) {
 		},
 		{
 			// if overwrite_keys is true, but the `type` key in json starts with _, ignore it
-			Event: FileEvent{
+			Event: Event{
 				DocumentType: "test_type",
 				Text:         &text,
 				JSONFields:   common.MapStr{"type": "_type"},

filebeat/prospector/prospector.go
@@ -17,8 +17,8 @@ type Prospector struct {
 	cfg           *common.Config // Raw config
 	config        prospectorConfig
 	prospectorer  Prospectorer
-	spoolerChan   chan *input.FileEvent
-	harvesterChan chan *input.FileEvent
+	spoolerChan   chan *input.Event
+	harvesterChan chan *input.Event
 	done          chan struct{}
 	states        *file.States
 	wg            sync.WaitGroup
@@ -29,12 +29,12 @@ type Prospectorer interface {
 	Run()
 }
 
-func NewProspector(cfg *common.Config, states file.States, spoolerChan chan *input.FileEvent) (*Prospector, error) {
+func NewProspector(cfg *common.Config, states file.States, spoolerChan chan *input.Event) (*Prospector, error) {
 	prospector := &Prospector{
 		cfg:           cfg,
 		config:        defaultConfig,
 		spoolerChan:   spoolerChan,
-		harvesterChan: make(chan *input.FileEvent),
+		harvesterChan: make(chan *input.Event),
 		done:          make(chan struct{}),
 		states:        states.Copy(),
 		wg:            sync.WaitGroup{},

filebeat/publish/publish.go
@@ -20,7 +20,7 @@ type LogPublisher interface {
 type syncLogPublisher struct {
 	pub     publisher.Publisher
 	client  publisher.Client
-	in, out chan []*input.FileEvent
+	in, out chan []*input.Event
 
 	done chan struct{}
 	wg   sync.WaitGroup
@@ -29,7 +29,7 @@ type syncLogPublisher struct {
 type asyncLogPublisher struct {
 	pub     publisher.Publisher
 	client  publisher.Client
-	in, out chan []*input.FileEvent
+	in, out chan []*input.Event
 
 	// list of in-flight batches
 	active   batchList
@@ -44,7 +44,7 @@ type asyncLogPublisher struct {
 type eventsBatch struct {
 	next   *eventsBatch
 	flag   int32
-	events []*input.FileEvent
+	events []*input.Event
 }
 
 type batchList struct {
@@ -70,7 +70,7 @@ var (
 
 func New(
 	async bool,
-	in, out chan []*input.FileEvent,
+	in, out chan []*input.Event,
 	pub publisher.Publisher,
 ) LogPublisher {
 	if async {
@@ -80,7 +80,7 @@ func New(
 }
 
 func newSyncLogPublisher(
-	in, out chan []*input.FileEvent,
+	in, out chan []*input.Event,
 	pub publisher.Publisher,
 ) *syncLogPublisher {
 	return &syncLogPublisher{
@@ -101,7 +101,7 @@ func (p *syncLogPublisher) Start() {
 		logp.Info("Start sending events to output")
 
 		for {
-			var events []*input.FileEvent
+			var events []*input.Event
 			select {
 			case <-p.done:
 				return
@@ -143,7 +143,7 @@ func (p *syncLogPublisher) Stop() {
 }
 
 func newAsyncLogPublisher(
-	in, out chan []*input.FileEvent,
+	in, out chan []*input.Event,
 	pub publisher.Publisher,
 ) *asyncLogPublisher {
 	return &asyncLogPublisher{

commit 9bf9e47a1d52e8eccb13970e6ef71464c4199464

Fix empty lines sent
Empty lines should not be sent as an event. To make sure this does not happen a test was added.

This was probably broken in https://github.com/elastic/beats/pull/2090

filebeat/harvester/log.go
@@ -91,8 +91,9 @@ func (h *Harvester) Harvest() {
 
 		text := string(message.Content)
 
-		// Check if data should be added to event
-		if h.shouldExportLine(text) {
+		// Check if data should be added to event. Only export events where Bytes > 0
+		// Content length cannot be checked because of JSON
+		if message.Bytes > 0 && h.shouldExportLine(text) {
 			event.ReadTime = message.Ts
 			event.Bytes = message.Bytes
 			event.Text = &text

filebeat/tests/system/test_harvester.py
@@ -146,3 +146,53 @@ class Test(BaseTest):
 
         # Make sure the state for the file was persisted
         assert len(data) == 1
+
+
+    def test_empty_line(self):
+        """
+        Checks that no empty events are sent for an empty line but state is still updated
+        """
+        self.render_config_template(
+            path=os.path.abspath(self.working_dir) + "/log/test.log",
+        )
+        os.mkdir(self.working_dir + "/log/")
+
+        logfile = self.working_dir + "/log/test.log"
+
+        filebeat = self.start_beat()
+
+        with open(logfile, 'w') as f:
+            f.write("Hello world\n")
+
+        # Let it read the file
+        self.wait_until(
+            lambda: self.output_has(lines=1), max_timeout=10)
+
+        with open(logfile, 'a') as f:
+            f.write("\n")
+
+        expectedOffset = 13
+
+        if os.name == "nt":
+            # Two additional newline chars
+            expectedOffset += 2
+
+        # Wait until offset for new line is updated
+        self.wait_until(
+            lambda: self.log_contains(
+                "offset: " + str(expectedOffset)),
+            max_timeout=15)
+
+        with open(logfile, 'a') as f:
+            f.write("Third line\n")
+
+        # Make sure only 2 events are written
+        self.wait_until(
+            lambda: self.output_has(lines=2), max_timeout=10)
+
+        filebeat.check_kill_and_wait()
+
+        data = self.get_registry()
+
+        # Make sure the state for the file was persisted
+        assert len(data) == 1