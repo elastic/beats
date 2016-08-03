c3a03293952e70ef052762977fb3fe2e0025fa12
Douae Jeouit <douae.jeouit@gmail.com>
2016-08-03 17:28:31 +0200

    Add network metricset

---
+++ metricbeat/module/docker/network/data.go
@@ -0,0 +1,38 @@
+package network
+
+import (
+"github.com/elastic/beats/libbeat/common"
+//"fmt"
+)
+
+func eventsMapping( netsStatsList []NETstats) [] common.MapStr {
+	myEvents := [] common.MapStr{}
+	//fmt.Printf(" Taille cpuStatsList : ",len(cpuStatsList),"\n")
+	for _, netsStats := range netsStatsList {
+	myEvents = append(myEvents, eventMapping(netsStats))
+	}
+	return myEvents
+}
+func eventMapping( myNetStats NETstats) common.MapStr{
+	event := common.MapStr{
+		"@timestamp":      myNetStats.Time,
+		"type":            "net",
+		"container": common.MapStr{
+			"id":  myNetStats.MyContainer.Id,
+			"name": myNetStats.MyContainer.Name,
+			"labels": myNetStats.MyContainer.Labels,
+		},
+		//"dockerSocket": myNetStats.MyContainer.Socket,
+		myNetStats.NameInterface: common.MapStr{
+		 "rxBytes":   myNetStats.RxBytes  ,
+		 "rxDropped":  myNetStats.RxDropped  ,
+		 "rxErrors":   myNetStats.RxErrors,
+		 "rxPackets":  myNetStats.RxPackets,
+		 "txBytes":    myNetStats.TxBytes,
+		 "txDropped":   myNetStats.TxDropped,
+		 "txErrors":   myNetStats.TxErrors,
+		 "txPackets":   myNetStats.TxPackets,
+		 },
+}
+return event
+}
