acc4c9f9220b3a156e18e74830668fe0298a92ac
Douae Jeouit <douae.jeouit@gmail.com>
2016-08-04 12:09:22 +0200

    Update network metricset

--- metricbeat/module/docker/network/helper.go
+++ metricbeat/module/docker/network/helper.go
@@ -5,7 +5,7 @@ import (
 	dc "github.com/fsouza/go-dockerclient"
 	"github.com/elastic/beats/metricbeat/module/docker"
 	"github.com/elastic/beats/libbeat/logp"
-	"fmt"
+	//"fmt"
 )
 
 type NETService struct {
@@ -35,59 +35,43 @@ type NETstats struct {
 	TxErrors  float64
 	TxPackets float64
 }
-/*func (NT NETService) getNetworkStatsList ()map[string][]NETstats {
 
-}*/
 func (NT NETService) GetNetworkStatsPerContainer( rawStats []docker.DockerStat) []NETstats{
 	formatedStats := []NETstats{}
 	if len(rawStats) !=0 {
 		for _,myStats := range rawStats {
 			for nameInterface, rawnNetStats := range myStats.Stats.Networks{
-				formatedStats = append(formatedStats, NT.getNetworkStats(nameInterface,myStats.Stats.Read,&myStats.Container,&rawnNetStats))
+				formatedStats = append(formatedStats, NT.getNetworkStats(nameInterface, & rawnNetStats, &myStats))
 			}
 		}
 	}else{
-		fmt.Printf("No container is running \n")
+		logp.Info("No container is running \n")
 	}
-
-
 	return formatedStats
 }
-func (NT NETService) checkStats (containerID string, nameInterface string) bool{
-	if _, exist := NT.NetworkStatPerContainer[containerID][nameInterface]; exist{
-		return true
-	}
-	return false
-
-}
-func (NT NETService) getNetworkStats (  nameInterface string, time time.Time, container *dc.APIContainers, rawStats * dc.NetworkStats) NETstats{
+func (NT NETService) getNetworkStats (nameInterface string, rawNetStats *dc.NetworkStats, myRawstats *docker.DockerStat) NETstats{
 
 	 myNETstats := NETstats{}
-	if rawStats != nil{
-
-		newNetworkStats := getNewNet(time,rawStats)
-		oldNetworkStat, exist := NT.NetworkStatPerContainer[container.ID][nameInterface]
-		//fmt.Printf(" EXIST : ", exist)
-		//fmt.Printf(" NEW RXBYTES: ", newNetworkStats.RxBytes)
-		//fmt.Printf(" OLD RXBYTES:", oldNetworkStat.RxBytes)
-		if(exist) {
-			myNETstats = NETstats{
-				MyContainer: docker.InitCurrentContainer(container),
-				Time: time,
-				NameInterface: nameInterface,
-				RxBytes: NT.getRxBytesPerSecond(&newNetworkStats,&oldNetworkStat),
-				RxDropped: NT.getRxDroppedPerSecond(&newNetworkStats,&oldNetworkStat),
-				RxErrors: NT.getRxErrorsPerSecond(&newNetworkStats, &oldNetworkStat),
-				RxPackets: NT.getRxPacketsPerSecond(&newNetworkStats,&oldNetworkStat),
-				TxBytes: NT.getTxBytesPerSecond(&newNetworkStats, &oldNetworkStat),
-				TxDropped: NT.getTxDroppedPerSecond(&newNetworkStats,&oldNetworkStat),
-				TxErrors: NT.getTxErrorsPerSecond(&newNetworkStats, &oldNetworkStat),
-				TxPackets: NT.getTxPacketsPerSecond(&newNetworkStats,&oldNetworkStat),
+	newNetworkStats := getNewNetRAw(myRawstats.Stats.Read, rawNetStats)
+	oldNetworkStat, exist := NT.NetworkStatPerContainer[myRawstats.Container.ID][nameInterface]
+	if(exist) {
+		myNETstats = NETstats{
+			MyContainer: docker.InitCurrentContainer(&myRawstats.Container),
+			Time: myRawstats.Stats.Read,
+			NameInterface: nameInterface,
+			RxBytes: NT.getRxBytesPerSecond(&newNetworkStats,&oldNetworkStat),
+			RxDropped: NT.getRxDroppedPerSecond(&newNetworkStats,&oldNetworkStat),
+			RxErrors: NT.getRxErrorsPerSecond(&newNetworkStats, &oldNetworkStat),
+			RxPackets: NT.getRxPacketsPerSecond(&newNetworkStats,&oldNetworkStat),
+			TxBytes: NT.getTxBytesPerSecond(&newNetworkStats, &oldNetworkStat),
+			TxDropped: NT.getTxDroppedPerSecond(&newNetworkStats,&oldNetworkStat),
+			TxErrors: NT.getTxErrorsPerSecond(&newNetworkStats, &oldNetworkStat),
+			TxPackets: NT.getTxPacketsPerSecond(&newNetworkStats,&oldNetworkStat),
 			}
 		}else {
 			myNETstats = NETstats{
-				MyContainer: docker.InitCurrentContainer(container),
-				Time: time,
+				MyContainer: docker.InitCurrentContainer(&myRawstats.Container),
+				Time: myRawstats.Stats.Read,
 				NameInterface: nameInterface,
 				RxBytes: 0,
 				RxDropped:0,
@@ -99,20 +83,16 @@ func (NT NETService) getNetworkStats (  nameInterface string, time time.Time, co
 				TxPackets: 0,
 			}
 		}
-		 if _,exist := NT.NetworkStatPerContainer[container.ID]; !exist{
-			NT.NetworkStatPerContainer[container.ID]= make (map [string]NETRaw)
+		 if _,exist := NT.NetworkStatPerContainer[myRawstats.Container.ID]; !exist{
+			NT.NetworkStatPerContainer[myRawstats.Container.ID]= make (map [string]NETRaw)
 		}
-		NT.NetworkStatPerContainer[container.ID][nameInterface] = newNetworkStats
+		NT.NetworkStatPerContainer[myRawstats.Container.ID][nameInterface] = newNetworkStats
 
-
-	}else{
-		logp.Info(" Unvailable stats ")
-	}
 	return myNETstats
 
 }
 
-func getNewNet(time  time.Time, stats *dc.NetworkStats) NETRaw{
+func getNewNetRAw(time  time.Time, stats *dc.NetworkStats) NETRaw{
 	return NETRaw{
 		Time: time,
 		RxBytes: stats.RxBytes,
@@ -123,46 +103,46 @@ func getNewNet(time  time.Time, stats *dc.NetworkStats) NETRaw{
 		TxDropped: stats.TxDropped,
 		TxErrors: stats.TxErrors,
 		TxPackets: stats.TxPackets,
-
 	}
 
 
 }
+func (NT NETService) checkStats (containerID string, nameInterface string) bool{
+	if _, exist := NT.NetworkStatPerContainer[containerID][nameInterface]; exist{
+		return true
+	}
+	return false
+
+}
+
 func (NT NETService) getRxBytesPerSecond(newStats *NETRaw, oldStats *NETRaw) float64 {
 	duration := newStats.Time.Sub(oldStats.Time)
 	return NT.calculatePerSecond(duration, oldStats.RxBytes, newStats.RxBytes)
 }
-
 func (NT NETService) getRxDroppedPerSecond(newStats *NETRaw, oldStats *NETRaw) float64 {
 	duration := newStats.Time.Sub(oldStats.Time)
 	return NT.calculatePerSecond(duration, oldStats.RxDropped, newStats.RxDropped)
 }
-
 func (NT NETService) getRxErrorsPerSecond(newStats *NETRaw, oldStats *NETRaw) float64 {
 	duration := newStats.Time.Sub(oldStats.Time)
 	return NT.calculatePerSecond(duration, oldStats.RxErrors, newStats.RxErrors)
 }
-
 func (NT NETService) getRxPacketsPerSecond(newStats *NETRaw, oldStats *NETRaw) float64 {
 	duration := newStats.Time.Sub(oldStats.Time)
 	return NT.calculatePerSecond(duration,oldStats.RxPackets, newStats.RxPackets)
 }
-
 func (NT NETService) getTxBytesPerSecond(newStats *NETRaw, oldStats *NETRaw) float64 {
 	duration := newStats.Time.Sub(oldStats.Time)
 	return NT.calculatePerSecond(duration, oldStats.TxBytes, newStats.TxBytes)
 }
-
 func (NT NETService) getTxDroppedPerSecond(newStats *NETRaw, oldStats *NETRaw) float64 {
 	duration := newStats.Time.Sub(oldStats.Time)
 	return NT.calculatePerSecond(duration, oldStats.TxDropped, newStats.TxDropped)
 }
-
 func (NT NETService) getTxErrorsPerSecond(newStats *NETRaw, oldStats *NETRaw) float64 {
 	duration := newStats.Time.Sub(oldStats.Time)
 	return NT.calculatePerSecond(duration, oldStats.TxErrors, newStats.TxErrors)
 }
-
 func (NT NETService) getTxPacketsPerSecond(newStats *NETRaw, oldStats *NETRaw) float64 {
 	duration := newStats.Time.Sub(oldStats.Time)
 	return NT.calculatePerSecond(duration ,oldStats.TxPackets, newStats.TxPackets)
