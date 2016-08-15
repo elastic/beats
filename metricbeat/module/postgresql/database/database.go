8480819d3ccaa17034da56528a59ed8deb6fc70a
Tudor Golubenco <tudor@packetbeat.com>
2016-08-15 11:49:35 +0300

    Add the PostgreSQL database metricset
    * Interrogates pg_stat_database for stats about each database.
    * Adds system tests
    
    Part of #2255.

--- metricbeat/module/postgresql/database/database.go
+++ metricbeat/module/postgresql/database/database.go
@@ -1,32 +1,30 @@
-package activity
+package database
 
 import (
 	"database/sql"
 
 	_ "github.com/lib/pq"
-	"github.com/pkg/errors"
 
 	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/metricbeat/mb"
 	"github.com/elastic/beats/metricbeat/module/postgresql"
+	"github.com/pkg/errors"
 )
 
 // init registers the MetricSet with the central registry.
 // The New method will be called after the setup of the module and before starting to fetch data
 func init() {
-	if err := mb.Registry.AddMetricSet("postgresql", "activity", New); err != nil {
+	if err := mb.Registry.AddMetricSet("postgresql", "database", New); err != nil {
 		panic(err)
 	}
 }
 
-// MetricSet type defines all fields of the Postgresql MetricSet
+// MetricSet type defines all fields of the MetricSet
 type MetricSet struct {
 	mb.BaseMetricSet
 }
 
 // New create a new instance of the MetricSet
-// Part of new is also setting up the configuration by processing additional
-// configuration entries if needed.
 func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 
 	config := struct{}{}
@@ -40,17 +38,16 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 	}, nil
 }
 
-// Fetch implements the data gathering and data conversion to the right format.
+// Fetch methods implements the data gathering and data conversion to the right format
 func (m *MetricSet) Fetch() ([]common.MapStr, error) {
 
-	// TODO: Find a way to pass the timeout
 	db, err := sql.Open("postgres", m.Host())
 	if err != nil {
 		return nil, err
 	}
 	defer db.Close()
 
-	results, err := postgresql.QueryStats(db, "SELECT * FROM pg_stat_activity")
+	results, err := postgresql.QueryStats(db, "SELECT * FROM pg_stat_database")
 	if err != nil {
 		return nil, errors.Wrap(err, "QueryStats")
 	}
