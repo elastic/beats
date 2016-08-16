0739c75c9debff5b49a01b5cee14f97b3890ca0c
medcl <m@medcl.net>
2016-08-16 09:52:48 +0800

    throw error while compressor is not set but hit compress flag

--- packetbeat/protos/cassandra/internal/gocql/frame.go
+++ packetbeat/protos/cassandra/internal/gocql/frame.go
@@ -200,6 +200,11 @@ func (f *Framer) ReadFrame() (data map[string]interface{}, err error) {
 
 	if f.Header.Flags&flagCompress == flagCompress {
 		//decompress data and switch to use bytearray decoder
+		if f.compres == nil {
+			logp.Err("hit compress flag, but comprossor was not set")
+			panic(errors.New("hit compress flag, but comprossor was not set"))
+		}
+
 		decoder := &ByteArrayDecoder{}
 		buf := make([]byte, f.Header.BodyLength)
 		f.r.Read(buf)
