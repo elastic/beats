70b8fbe5e2c0a4523b73d727f1535ca0c6c721aa
Tudor Golubenco <tudor@packetbeat.com>
2016-09-13 13:16:44 +0200

    Update to Go 1.7.1
    This updates packaging, docker files for tests, travis and appveyor configs.

--- dev-tools/packer/docker/xgo-image-deb6/build.sh
+++ dev-tools/packer/docker/xgo-image-deb6/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 docker build --rm=true -t tudorg/xgo-deb6-base base/ && \
-    docker build --rm=true -t tudorg/xgo-deb6-1.7 go-1.7/ &&
+    docker build --rm=true -t tudorg/xgo-deb6-1.7.1 go-1.7.1/ &&
     docker build --rm=true -t tudorg/beats-builder-deb6 beats-builder
