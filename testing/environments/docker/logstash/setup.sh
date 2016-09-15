706d46850cde7cbaf4a90bc1006342922eda9474
Tudor Golubenco <tudor@packetbeat.com>
2016-09-15 11:22:17 +0200

    Update testing environment to beta1 IRC
    - Changed the logstash dockerfile to use the tar.gz which is much
      simpler.
    - Unfortunately it still doesn't work, I posted:
      https://github.com/logstash-plugins/logstash-input-beats/issues/129
    - For elasticsearch I did a hack for now, but we should consider moving
      to the tar.gz as well

--- testing/environments/docker/logstash/setup.sh
+++ testing/environments/docker/logstash/setup.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
 
-# setup logstash dependencies
-set -x && \
-    apt-get update && \
-    apt-get install -y --no-install-recommends logrotate && \
-    apt-get clean && \
-    rm -rf /var/lib/apt/lists/*
-
 # download & install logstash
 set -x && \
-    mkdir -p /var/tmp && \
-    wget -qO /var/tmp/logstash.deb $1 && \
-    dpkg -i /var/tmp/logstash.deb && \
+    cd /opt && \
+    wget -q $URL && \
+    tar xzf logstash-$VERSION.tar.gz
 
-
-export PATH=$PATH:/usr/share/logstash/bin
-logstash-plugin install logstash-input-beats
+#logstash-plugin install logstash-input-beats
