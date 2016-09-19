cba62f95e71cefb88d6f78d011d93a0651c11fe5
Tudor Golubenco <tudor@packetbeat.com>
2016-09-19 11:41:50 +0200

    Updated to IRC2 and addressed comments.

--- testing/environments/docker/logstash/setup.sh
+++ testing/environments/docker/logstash/setup.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
+# setup logstash dependencies
+set -x && \
+    apt-get update && \
+    apt-get install -y --no-install-recommends logrotate && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
 # download & install logstash
 set -x && \
-    cd /opt && \
-    wget -q $URL && \
-    tar xzf logstash-$VERSION.tar.gz
+    mkdir -p /var/tmp && \
+    wget -qO /var/tmp/logstash.deb $1 && \
+    dpkg -i /var/tmp/logstash.deb && \
 
-#logstash-plugin install logstash-input-beats
+
+export PATH=$PATH:/usr/share/logstash/bin
+logstash-plugin install logstash-input-beats
