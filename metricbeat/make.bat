eb88c2cfe91178a886ccc5233e886510819f9b86
Andrew Kroh <andrew.kroh@elastic.co>
2016-10-28 15:12:47 -0400

    Metricbeat system module updates for Windows
    - Added system core metricset for Windows. Per core metrics were implemented
      in elastic/gosigar.
    - Added logging of process/system details on Windows to aid in debugging (user,
      arch, cores, sid, privs).
    - Fixes #2860 (PPID is zero on Windows).
    - Fixes #1704 (Server 2003 - PID’s not recognized). The command line arguments
      for the process will not be reported on XP and 2003.
    - Fixes #1897 (OpenProcess access denied on Windows). Added code to enable the
      SeDebugPrivilege when it is available.
    - Fixes #2885 (diskio metricset fails on XP and 2003).
    - Enabled fsstats by default in Metricbeat config.

---
+++ metricbeat/make.bat
@@ -0,0 +1,34 @@
+@echo off
+
+REM
+REM Batch script to build and test on Windows. You can use this in conjunction
+REM with the Vagrant machine.
+REM
+
+go install github.com/elastic/beats/vendor/github.com/pierrre/gotestcover
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo Building
+go build
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo Testing
+mkdir build\coverage
+gotestcover -race -coverprofile=build/coverage/integration.cov github.com/elastic/beats/metricbeat/...
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo System Testing
+go test -c -covermode=atomic -coverpkg ./...
+if %errorlevel% neq 0 exit /b %errorlevel%
+nosetests -v -w tests\system --process-timeout=30
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo Aggregating Coverage Reports
+python ..\dev-tools\aggregate_coverage.py -o build\coverage\system.cov .\build\system-tests\run
+if %errorlevel% neq 0 exit /b %errorlevel%
+python ..\dev-tools\aggregate_coverage.py -o build\coverage\full.cov .\build\coverage
+if %errorlevel% neq 0 exit /b %errorlevel%
+go tool cover -html=build\coverage\full.cov -o build\coverage\full.html
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo Success
