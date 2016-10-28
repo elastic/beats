2a3d7e2fd3ed0571244e388d117932c3ed294fc9
urso <steffen.siering@elastic.co>
2016-10-28 16:04:42 +0200

    some heartbeat golint

--- heartbeat/monitors/defaults/default.go
+++ heartbeat/monitors/defaults/default.go
@@ -1,6 +1,7 @@
 package defaults
 
 import (
+	// register standard active monitors
 	_ "github.com/elastic/beats/heartbeat/monitors/active/http"
 	_ "github.com/elastic/beats/heartbeat/monitors/active/icmp"
 	_ "github.com/elastic/beats/heartbeat/monitors/active/tcp"
