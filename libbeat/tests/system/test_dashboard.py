c3d9910bed077ba9a2dd6842d7bfa0ceef0c220b
Martin Scholz <martin.scholz83@outlook.com>
2016-11-09 12:00:00 +0100

    Fix unsupported characters in Dashboards (#2946)
    Make sure the name of the exported dashboards and visualizations don't contain unsupported characters.

--- libbeat/tests/system/test_dashboard.py
+++ libbeat/tests/system/test_dashboard.py
@@ -4,6 +4,7 @@ from nose.plugins.attrib import attr
 import os
 import subprocess
 import unittest
+import re
 
 INTEGRATION_TESTS = os.environ.get('INTEGRATION_TESTS', False)
 
@@ -28,6 +29,38 @@ class Test(BaseTest):
             content, err = p.communicate()
 
             assert p.returncode == 0
+    
+    @unittest.skipUnless(INTEGRATION_TESTS, "integration test")
+    @attr('integration')
+    def test_export_dashboard(self):
+        """
+        Test export dashboards and remove unsupported characters
+        """
+        beats = ["metricbeat", "packetbeat", "filebeat", "winlogbeat"]
+
+        for beat in beats:
+            if os.name == "nt":                
+                path = "..\..\..\\"+ beat + "\etc\kibana"
+            else:
+                path = "../../../"+ beat + "/etc/kibana"
+
+            command = "python ../../../dev-tools/export_dashboards.py --url http://"+ self.get_elasticsearch_host() + " --dir " + path + " --regex " + beat + "-*"
+
+            if os.name == "nt":
+                command = "python ..\..\..\dev-tools/export_dashboards.py --url http://"+ self.get_elasticsearch_host() + " --dir " + path + " --regex " + beat + "-*"
+            
+            print command
+
+            p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            content, err = p.communicate()
+
+            assert p.returncode == 0
+
+            files = os.listdir(path)
+
+            for f in files:
+                self.assertIsNone(re.search('[:\>\<"/\\\|\?\*]', f))
+            
 
     def get_elasticsearch_host(self):
         return os.getenv('ES_HOST', 'localhost') + ':' + os.getenv('ES_PORT', '9200')
