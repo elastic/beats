f18eb3bc86e84f75f8d1d520c0034840e462a517
Andrew Kroh <andrew.kroh@elastic.co>
2016-11-21 12:27:50 -0500

    Implement HostParser for postgresql module

--- metricbeat/module/postgresql/activity/activity.go
+++ metricbeat/module/postgresql/activity/activity.go
@@ -16,7 +16,7 @@ import (
 // init registers the MetricSet with the central registry.
 // The New method will be called after the setup of the module and before starting to fetch data
 func init() {
-	if err := mb.Registry.AddMetricSet("postgresql", "activity", New); err != nil {
+	if err := mb.Registry.AddMetricSet("postgresql", "activity", New, postgresql.ParseURL); err != nil {
 		panic(err)
 	}
 }
@@ -24,43 +24,18 @@ func init() {
 // MetricSet type defines all fields of the Postgresql MetricSet
 type MetricSet struct {
 	mb.BaseMetricSet
-	connectionString string
 }
 
 // New create a new instance of the MetricSet
 // Part of new is also setting up the configuration by processing additional
 // configuration entries if needed.
 func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
-
-	config := struct {
-		Hosts    []string `config:"hosts"    validate:"nonzero,required"`
-		Username string   `config:"username"`
-		Password string   `config:"password"`
-	}{
-		Username: "",
-		Password: "",
-	}
-
-	if err := base.Module().UnpackConfig(&config); err != nil {
-		return nil, err
-	}
-
-	url, err := postgresql.ParseURL(base.Host(), config.Username, config.Password,
-		base.Module().Config().Timeout)
-	if err != nil {
-		return nil, err
-	}
-
-	return &MetricSet{
-		BaseMetricSet:    base,
-		connectionString: url,
-	}, nil
+	return &MetricSet{BaseMetricSet: base}, nil
 }
 
 // Fetch implements the data gathering and data conversion to the right format.
 func (m *MetricSet) Fetch() ([]common.MapStr, error) {
-
-	db, err := sql.Open("postgres", m.connectionString)
+	db, err := sql.Open("postgres", m.HostData().URI)
 	if err != nil {
 		return nil, err
 	}
