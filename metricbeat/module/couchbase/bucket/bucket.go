1cc6c86f9e13cfc8e2da9b16c130771197f7bade
Daniel Paschke <paschdan@gmail.com>
2016-11-28 16:17:45 +0100

    adds basic bucket stats

--- metricbeat/module/couchbase/bucket/bucket.go
+++ metricbeat/module/couchbase/bucket/bucket.go
@@ -3,12 +3,26 @@ package bucket
 import (
 	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/metricbeat/mb"
+	"net/http"
+	"fmt"
+	"github.com/elastic/beats/metricbeat/mb/parse"
+)
+
+const (
+	defaultScheme = "http"
+	defaultPath = "/pools/default/buckets"
+)
+var (
+	hostParser = parse.URLHostParserBuilder{
+		DefaultScheme: defaultScheme,
+		DefaultPath:   defaultPath,
+	}.Build()
 )
 
 // init registers the MetricSet with the central registry.
 // The New method will be called after the setup of the module and before starting to fetch data
 func init() {
-	if err := mb.Registry.AddMetricSet("couchbase", "bucket", New); err != nil {
+	if err := mb.Registry.AddMetricSet("couchbase", "bucket", New, hostParser); err != nil {
 		panic(err)
 	}
 }
@@ -19,7 +33,7 @@ func init() {
 // multiple fetch calls.
 type MetricSet struct {
 	mb.BaseMetricSet
-	counter int
+	client *http.Client
 }
 
 // New create a new instance of the MetricSet
@@ -35,19 +49,31 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 
 	return &MetricSet{
 		BaseMetricSet: base,
-		counter:       1,
+		client: &http.Client{Timeout: base.Module().Config().Timeout},
 	}, nil
 }
 
 // Fetch methods implements the data gathering and data conversion to the right format
 // It returns the event which is then forward to the output. In case of an error, a
 // descriptive error must be returned.
-func (m *MetricSet) Fetch() (common.MapStr, error) {
-
-	event := common.MapStr{
-		"counter": m.counter,
+func (m *MetricSet) Fetch() ([]common.MapStr, error) {
+	req, err := http.NewRequest("GET", m.HostData().SanitizedURI, nil)
+	if m.HostData().User != "" || m.HostData().Password != "" {
+		req.SetBasicAuth(m.HostData().User, m.HostData().Password)
 	}
-	m.counter++
 
-	return event, nil
+	resp, err := m.client.Do(req)
+
+	if err != nil {
+		return nil, fmt.Errorf("error making http request: %v", err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("Error Connecting to Couchbase %d: %s", resp.StatusCode, resp.Status)
+	}
+
+	return eventsMapping(resp.Body), nil
+
 }
---
+++ metricbeat/module/couchbase/bucket/data.go
@@ -0,0 +1,61 @@
+package bucket
+
+import (
+	"io"
+	"github.com/elastic/beats/libbeat/common"
+	"encoding/json"
+	"github.com/elastic/beats/libbeat/logp"
+)
+
+type BucketQuota struct {
+	RAM    int `json:"ram"`
+	RawRAM int `json:"rawRAM"`
+}
+
+type BucketBasicStats struct {
+	QuotaPercentUsed float64 `json:"quotaPercentUsed"`
+	OpsPerSec        int `json:"opsPerSec"`
+	DiskFetches      int `json:"diskFetches"`
+	ItemCount        int `json:"itemCount"`
+	DiskUsed         int `json:"diskUsed"`
+	DataUsed         int `json:"dataUsed"`
+	MemUsed          int `json:"memUsed"`
+}
+
+type Buckets []struct {
+	Name       string `json:"name"`
+	BucketType string `json:"bucketType"`
+	Quota      BucketQuota `json:"quota"`
+	BasicStats BucketBasicStats `json:"basicStats"`
+}
+
+func eventsMapping(body io.Reader) []common.MapStr {
+
+	var d Buckets
+	err := json.NewDecoder(body).Decode(&d)
+	if err != nil {
+		logp.Err("Error: ", err)
+	}
+
+	events := []common.MapStr{}
+
+	for _, Bucket := range d {
+		event := common.MapStr{
+			"name": Bucket.Name,
+			"bucketType": Bucket.BucketType,
+			"quota_RAM": Bucket.Quota.RAM,
+			"quota_RawRAM": Bucket.Quota.RawRAM,
+			"stats_QuotaPercUse": Bucket.BasicStats.QuotaPercentUsed,
+			"stats_OpsPerSec": Bucket.BasicStats.OpsPerSec,
+			"stats_DiskFetches": Bucket.BasicStats.DiskFetches,
+			"stats_ItemCount": Bucket.BasicStats.ItemCount,
+			"stats_DiskUsed": Bucket.BasicStats.DiskUsed,
+			"stats_DataUsed": Bucket.BasicStats.DataUsed,
+			"stats_MemUsed": Bucket.BasicStats.MemUsed,
+		}
+
+		events = append(events, event)
+	}
+
+	return events
+}
