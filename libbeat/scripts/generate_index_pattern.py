89b17b8b428ee7aa96fccc91b21b1f19eeb005a8
Tom Vaughan <thomas.david.vaughan@gmail.com>
2016-12-07 19:12:08 -0300

    Fix print statement to be python 3 compliant.

--- libbeat/scripts/generate_index_pattern.py
+++ libbeat/scripts/generate_index_pattern.py
@@ -35,7 +35,7 @@ def field_to_json(desc, path, output):
     global unique_fields
 
     if path in unique_fields:
-        print "ERROR: Field", path, "is duplicated. Please delete it and try again. Fields already are", unique_fields
+        print("ERROR: Field {} is duplicated. Please delete it and try again. Fields already are {}".format(path, ", ".join(unique_fields)))
         sys.exit(1)
     else:
         unique_fields.append(path)
