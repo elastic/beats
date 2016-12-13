18ca3b7408950a7fc8cfda085e919c1c64a318dd
Cyrille Verrier <cyrille.verrier@fabriscale.com>
2016-12-13 13:37:37 +0100

    generated main.go uses appVersion from generated version.go

--- generate/beat/{{cookiecutter.beat}}/main.go
+++ generate/beat/{{cookiecutter.beat}}/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	err := beat.Run("{{cookiecutter.beat}}", {{cookiecutter.beat}}Version, beater.New)
+	err := beat.Run("{{cookiecutter.beat}}", appVersion, beater.New)
 	if err != nil {
 		os.Exit(1)
 	}
