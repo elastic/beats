1f420d73d2b4e3a71fbb574233ab4b43322262f9
Scott Crespo <screspo@highcornergroup.com>
2016-12-29 17:53:41 -0500

    implementing multi-node direct reporting for mongodb.serverStatus

--- metricbeat/module/mongodb/status/status.go
+++ metricbeat/module/mongodb/status/status.go
@@ -1,6 +1,9 @@
 package status
 
 import (
+	"fmt"
+	"sync"
+
 	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/metricbeat/mb"
 	"github.com/elastic/beats/metricbeat/module/mongodb"
@@ -28,8 +31,8 @@ func init() {
 // multiple fetch calls.
 type MetricSet struct {
 	mb.BaseMetricSet
-	dialInfo     *mgo.DialInfo
-	mongoSession *mgo.Session
+	dialInfo      *mgo.DialInfo
+	mongoSessions []*mgo.Session
 }
 
 // New creates a new instance of the MetricSet
@@ -42,24 +45,66 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 	}
 	dialInfo.Timeout = base.Module().Config().Timeout
 
-	mongoSession := mongodb.NewSession(dialInfo)
-	mongoSession.SetMode(mgo.Monotonic, true)
+	// instantiate direct connections to each of the configured Mongo hosts
+	mongoSessions, err := mongodb.NewDirectSessions(dialInfo.Addrs, dialInfo)
+	if err != nil {
+		return nil, err
+	}
 
 	return &MetricSet{
 		BaseMetricSet: base,
 		dialInfo:      dialInfo,
-		mongoSession:  mongoSession,
+		mongoSessions: mongoSessions,
 	}, nil
 }
 
 // Fetch methods implements the data gathering and data conversion to the right format
 // It returns the event which is then forward to the output. In case of an error, a
 // descriptive error must be returned.
-func (m *MetricSet) Fetch() (common.MapStr, error) {
-	result := map[string]interface{}{}
-	if err := m.mongoSession.DB("admin").Run(bson.D{{"serverStatus", 1}}, &result); err != nil {
-		return nil, errors.Wrap(err, "mongodb fetch failed")
+func (m *MetricSet) Fetch() ([]common.MapStr, error) {
+
+	// create a wait group because we're going to spawn a goroutine for each host target
+	var wg sync.WaitGroup
+	wg.Add(len(m.mongoSessions))
+
+	// events is the value returned by this function
+	var events []common.MapStr
+
+	// created buffered channel to receive async results from each of the nodes
+	channel := make(chan interface{}, len(m.mongoSessions))
+
+	for _, mongo := range m.mongoSessions {
+		go func(mongo *mgo.Session) {
+			defer wg.Done()
+			channel <- m.fetchNodeStatus(mongo)
+		}(mongo)
 	}
 
-	return eventMapping(result), nil
+	// wait for goroutines to complete
+	wg.Wait()
+	close(channel)
+
+	// pull results off of the channel and append to events
+	for data := range channel {
+		events = append(events, data.(common.MapStr))
+	}
+
+	// if we didn't get results from any node, return an error
+	if len(events) == 0 {
+		err := errors.New("Failed to retrieve db stats from all nodes")
+		return events, err
+	}
+
+	fmt.Printf("%v", events)
+
+	return events, nil
+}
+
+func (m *MetricSet) fetchNodeStatus(session *mgo.Session) common.MapStr {
+	result := common.MapStr{}
+	if err := session.DB("admin").Run(bson.D{{"serverStatus", 1}}, &result); err != nil {
+		return nil
+	}
+
+	return eventMapping(result)
 }
