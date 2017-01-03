d22444a29d900a3c3e8eec0678b65ef9d6bb8fce
Tudor Golubenco <tudor@elastic.co>
2017-01-03 16:55:59 +0200

    Update Go to version 1.7.4 (#3277)

--- dev-tools/packer/docker/xgo-image-deb6/build.sh
+++ dev-tools/packer/docker/xgo-image-deb6/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 docker build --rm=true -t tudorg/xgo-deb6-base base/ && \
-    docker build --rm=true -t tudorg/xgo-deb6-1.7.1 go-1.7.1/ &&
+    docker build --rm=true -t tudorg/xgo-deb6-1.7.4 go-1.7.4/ &&
     docker build --rm=true -t tudorg/beats-builder-deb6 beats-builder
