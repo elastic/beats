757dbd02848b3065a3a17f5ed620255530096b50
Scott Crespo <screspo@highcornergroup.com>
2017-01-08 12:39:04 -0500

    added experimental flag to mongodb dbstats metricset

--- metricbeat/module/mongodb/dbstats/dbstats.go
+++ metricbeat/module/mongodb/dbstats/dbstats.go
@@ -33,6 +33,8 @@ type MetricSet struct {
 // Part of new is also setting up the configuration by processing additional
 // configuration entries if needed.
 func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
+	logp.Warn("EXPERIMENTAL: The %v %v metricset is experimental", base.Module().Name(), base.Name())
+
 	dialInfo, err := mgo.ParseURL(base.HostData().URI)
 	if err != nil {
 		return nil, err
