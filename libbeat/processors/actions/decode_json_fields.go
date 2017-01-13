1f6fe81f66bdc38a93e997ccb7f67cc4963d7939
Jeff Williams <jeffw@wherethebitsroam.com>
2017-01-13 09:21:58 +0100

    Fix mulitple json elements in one log entry (#3356)
    Fix mulitple json elements in one log entry

--- libbeat/processors/actions/decode_json_fields.go
+++ libbeat/processors/actions/decode_json_fields.go
@@ -165,6 +165,10 @@ func DecodeJSON(text []byte, to *interface{}) error {
 		return err
 	}
 
+	if dec.More() {
+		return errors.New("multiple json elements found")
+	}
+
 	switch O := interface{}(*to).(type) {
 	case map[string]interface{}:
 		jsontransform.TransformNumbers(O)
