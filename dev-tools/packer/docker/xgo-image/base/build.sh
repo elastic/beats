144f1b09c0ad6fad91dcdc0a74bef9dbede5270a
Nicolas Ruflin <spam@ruflin.com>
2017-01-17 15:04:34 +0100

    Cleanup before_build script (#3386)
    * Pass ES_BEATS variable to before_build.sh scripts. This removes the logic to figure out which is the correct path. ES_BEAT path should be passed relative to the BEAT_PATH.
    * Move gotpl and go-yaml installation to the Dockerfile itself. This removes the requirement to install go dependencies in the scripts and moves the special handling for debian6 to the debian6 image.
    * Improve naming of variables
    * Pass BEAT_NAME, BEAT_PATH, ES_BEATS as environment variables. This makes it possible to use the same naming/logic in all scripts.
    
    The above allows more flexibility on how community beats can use package. As before-build is now executed as the first step, a beat can use this step to fetch beat dependencies with `go get github.com/elastic/beats`. This would fetch the master branch for building every time and does not require to vendor it. A disadvantage of the above is that `before-build` is called twice, once for each debian version.

--- dev-tools/packer/docker/xgo-image/base/build.sh
+++ dev-tools/packer/docker/xgo-image/base/build.sh
@@ -21,7 +21,6 @@
 # Download the canonical import path (may fail, don't allow failures beyond)
 SRC_FOLDER=$SOURCE
 
-BEAT_PATH=$1
 DST_FOLDER=`dirname $GOPATH/src/$BEAT_PATH`
 GIT_REPO=$BEAT_PATH
 
@@ -110,8 +109,8 @@ if [ "$STATIC" == "true" ]; then LDARGS=--ldflags\ \'-extldflags\ \"-static\"\';
 
 if [ -n $BEFORE_BUILD ]; then
 	chmod +x /scripts/$BEFORE_BUILD
-	echo "Execute /scripts/$BEFORE_BUILD ${BEAT_PATH}"
-	/scripts/$BEFORE_BUILD ${BEAT_PATH}
+	echo "Execute /scripts/$BEFORE_BUILD ${BEAT_PATH} ${ES_BEATS}"
+	/scripts/$BEFORE_BUILD
 fi
 
 
