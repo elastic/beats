61273fc14e9d28982fb27f1a0a474c01b0ab7ffe
sebastienmusso <s.musso@treeptik.fr>
2017-01-20 11:36:27 +0100

    add healthcheck metricset in docker module

---
+++ metricbeat/module/docker/healthcheck/data.go
@@ -0,0 +1,50 @@
+package healthcheck
+
+import (
+	//"time"
+
+	"github.com/elastic/beats/libbeat/common"
+	"github.com/elastic/beats/metricbeat/mb"
+	"github.com/elastic/beats/metricbeat/module/docker"
+
+	dc "github.com/fsouza/go-dockerclient"
+	"reflect"
+	"strings"
+)
+
+func eventsMapping(containersList []dc.APIContainers, m *MetricSet) []common.MapStr {
+	myEvents := []common.MapStr{}
+	emptyEvent := common.MapStr{}
+	for _, container := range containersList {
+		returnevent := eventMapping(&container, m)
+		if !reflect.DeepEqual(emptyEvent, returnevent) {
+			myEvents = append(myEvents, eventMapping(&container, m))
+		}
+	}
+	return myEvents
+}
+
+func eventMapping(cont *dc.APIContainers, m *MetricSet) common.MapStr {
+	event := common.MapStr{}
+	if strings.Contains(cont.Status, "(") && strings.Contains(cont.Status, ")") {
+		container, _ := m.dockerClient.InspectContainer(cont.ID)
+		last_event := len(container.State.Health.Log) - 1
+		if last_event >= 0 {
+			event = common.MapStr{
+				mb.ModuleData: common.MapStr{
+					"container": common.MapStr{
+						"name": docker.ExtractContainerName(cont.Names),
+					},
+				},
+				"status":           container.State.Health.Status,
+				"failingstreak":    container.State.Health.FailingStreak,
+				"event_start_date": common.Time(container.State.Health.Log[last_event].Start),
+				"event_end_date":   common.Time(container.State.Health.Log[last_event].End),
+				"event_exit_code":  container.State.Health.Log[last_event].ExitCode,
+				"event_output":     container.State.Health.Log[last_event].Output,
+			}
+		}
+	}
+
+	return event
+}
