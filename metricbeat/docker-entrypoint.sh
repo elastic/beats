696f56af440c6c749e697d8db034778f681fbe4c
Amanda H. L. de Andrade <amanda.andrade@serpro.gov.br>
2017-01-24 14:05:38 -0200

    Add CEPH Module with health metrics (#3311)

--- metricbeat/docker-entrypoint.sh
+++ metricbeat/docker-entrypoint.sh
@@ -23,6 +23,7 @@ waitFor() {
 
 # Main
 waitFor ${APACHE_HOST} ${APACHE_PORT} Apache
+waitFor ${CEPH_HOST} ${CEPH_PORT} Ceph
 waitFor ${COUCHBASE_HOST} ${COUCHBASE_PORT} Couchbase
 waitFor ${HAPROXY_HOST} ${HAPROXY_PORT} HAProxy
 waitFor ${KAFKA_HOST} ${KAFKA_PORT} Kafka
