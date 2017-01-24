04de25ac8d6c3c435d0d1b0f8fe06a82d0e9fcf3
Nick Revin <whois@nicorevin.ru>
2017-01-24 18:16:43 +0300

    Add TTL option to the logstash output

--- libbeat/outputs/logstash/logstash.go
+++ libbeat/outputs/logstash/logstash.go
@@ -140,13 +140,14 @@ func makeClientFactory(
 	compressLvl := cfg.CompressionLevel
 	maxBulkSz := cfg.BulkMaxSize
 	to := cfg.Timeout
+	ttl := cfg.TTL
 
 	return func(host string) (mode.ProtocolClient, error) {
 		t, err := transport.NewClient(tcfg, "tcp", host, cfg.Port)
 		if err != nil {
 			return nil, err
 		}
-		return newLumberjackClient(t, compressLvl, maxBulkSz, to, cfg.Index)
+		return newLumberjackClient(t, compressLvl, maxBulkSz, to, ttl, cfg.Index)
 	}
 }
 
@@ -158,13 +159,14 @@ func makeAsyncClientFactory(
 	maxBulkSz := cfg.BulkMaxSize
 	queueSize := cfg.Pipelining - 1
 	to := cfg.Timeout
+	ttl := cfg.TTL
 
 	return func(host string) (mode.AsyncProtocolClient, error) {
 		t, err := transport.NewClient(tcfg, "tcp", host, cfg.Port)
 		if err != nil {
 			return nil, err
 		}
-		return newAsyncLumberjackClient(t, queueSize, compressLvl, maxBulkSz, to, cfg.Index)
+		return newAsyncLumberjackClient(t, queueSize, compressLvl, maxBulkSz, to, ttl, cfg.Index)
 	}
 }
 
