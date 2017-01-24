5433c0ee5bb5048dea1efe8750697fb3c507cd15
ruflin <spam@ruflin.com>
2017-01-24 13:15:28 +0100

    Improve PHP-PFM module
    * Add docker environment for integration and system testing
    * Add system test file to check for correct docs.
    * Brings docs in line with generated output
    * Update data.json
    * Remove hostname fields as already part of metricset
    * Apply schema instead of manual conversion
    * Rename pool.pool to pool.name
    * Remove separate http client as not needed anymore
    
    This is a follow up PR for https://github.com/elastic/beats/pull/3415

--- metricbeat/module/php_fpm/pool/data.go
+++ metricbeat/module/php_fpm/pool/data.go
@@ -1,18 +1,21 @@
 package pool
 
-type poolStats struct {
-	Pool               string `json:"pool"`
-	ProcessManager     string `json:"process manager"`
-	StartTime          int    `json:"start time"`
-	StartSince         int    `json:"start since"`
-	AcceptedConn       int    `json:"accepted conn"`
-	ListenQueue        int    `json:"listen queue"`
-	MaxListenQueue     int    `json:"max listen queue"`
-	ListenQueueLen     int    `json:"listen queue len"`
-	IdleProcesses      int    `json:"idle processes"`
-	ActiveProcesses    int    `json:"active processes"`
-	TotalProcesses     int    `json:"total processes"`
-	MaxActiveProcesses int    `json:"max active processes"`
-	MaxChildrenReached int    `json:"max children reached"`
-	SlowRequests       int    `json:"slow requests"`
-}
+import (
+	s "github.com/elastic/beats/metricbeat/schema"
+	c "github.com/elastic/beats/metricbeat/schema/mapstriface"
+)
+
+var (
+	schema = s.Schema{
+		"name": c.Str("pool"),
+		"connections": s.Object{
+			"accepted": c.Int("accepted conn"),
+			"queued":   c.Int("listen queue"),
+		},
+		"processes": s.Object{
+			"idle":   c.Int("idle processes"),
+			"active": c.Int("active processes"),
+		},
+		"slow_requests": c.Int("slow requests"),
+	}
+)
