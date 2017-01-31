544b2855a1e20f05810d2b8457db9246f38fc509
Nicolas Ruflin <spam@ruflin.com>
2017-01-31 13:01:36 +0100

    Cleanup CEPH module (#3455)
    * Add system tests to check for doc fields
    * Fix docs / fields which do not correspond
    * Use shared client

---
+++ metricbeat/tests/system/test_ceph.py
@@ -0,0 +1,31 @@
+import os
+import metricbeat
+import unittest
+
+
+class Test(metricbeat.BaseTest):
+    @unittest.skipUnless(metricbeat.INTEGRATION_TESTS, "integration test")
+    def test_health(self):
+        """
+        ceph health metricset test
+        """
+        self.render_config_template(modules=[{
+            "name": "ceph",
+            "metricsets": ["health"],
+            "hosts": self.get_hosts(),
+            "period": "1s"
+        }])
+        proc = self.start_beat()
+        self.wait_until(lambda: self.output_lines() > 0, max_timeout=20)
+        proc.check_kill_and_wait()
+
+        output = self.read_output_json()
+        self.assertTrue(len(output) >= 1)
+        evt = output[0]
+        print evt
+
+        self.assert_fields_are_documented(evt)
+
+    def get_hosts(self):
+        return [os.getenv('CEPH_HOST', 'localhost') + ':' +
+                os.getenv('CEPH_PORT', '5000')]
