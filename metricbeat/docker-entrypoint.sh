045aa4892c4854faa83869ce3cac3c2df7d06b70
ruflin <spam@ruflin.com>
2017-02-03 12:44:12 +0100

    Create all environments based on docker files in the modules
    * Move all docker image specific configs into the modules by using a Dockerfile for each module
    * Move environment variables to module and link it from the composer file
    * Remove duplicated code for `wait_for` function
    
    This change will make it possible to partially auto generate the compose file and it brings the environment config closer to the module itself.

--- metricbeat/docker-entrypoint.sh
+++ metricbeat/docker-entrypoint.sh
@@ -1,25 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-# Wait for. Params: host, port, service
-waitFor() {
-    echo -n "Waiting for ${3}(${1}:${2}) to start."
-    for ((i=1; i<=90; i++)) do
-        if nc -vz ${1} ${2} 2>/dev/null; then
-            echo
-            echo "${3} is ready!"
-            return 0
-        fi
-
-        ((i++))
-        echo -n '.'
-        sleep 1
-    done
-
-    echo
-    echo >&2 "${3} is not available"
-    echo >&2 "Address: ${1}:${2}"
-}
+source ./../libbeat/scripts/wait_for.sh
 
 # Main
 waitFor ${APACHE_HOST} ${APACHE_PORT} Apache
