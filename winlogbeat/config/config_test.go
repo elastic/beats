60e799937d48a67359b70edb2586b8c36ab6b961
Monica Sarbu <monica@elastic.co>
2017-02-07 22:10:05 +0100

    Add dashboards to the list of configuration options (#3551)

--- winlogbeat/config/config_test.go
+++ winlogbeat/config/config_test.go
@@ -44,7 +44,7 @@ func TestConfigValidate(t *testing.T) {
 				},
 				map[string]interface{}{"other": "value"},
 			},
-			"1 error: Invalid top-level key 'other' found. Valid keys are bulk_queue_size, " +
+			"1 error: Invalid top-level key 'other' found. Valid keys are bulk_queue_size, dashboards, " +
 				"fields, fields_under_root, geoip, logging, max_procs, " +
 				"name, output, path, processors, queue_size, refresh_topology_freq, tags, topology_expire, winlogbeat",
 		},
