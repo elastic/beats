ba27a3e45eef8fc8a7a114c41cca75fda2cb556a
Nicolas Ruflin <spam@ruflin.com>
2017-03-30 22:54:55 +0200

    Add Beta and Experimental logging (#3840)
    These two logging methods can be used for logging beta and experimental features. This makes sure all of the features are logged the same way.
    
    Reload Configuration was changed to beta from experimental.

--- libbeat/logp/log.go
+++ libbeat/logp/log.go
@@ -132,11 +132,21 @@ func Critical(format string, v ...interface{}) {
 	msg(LOG_CRIT, "CRIT ", format, v...)
 }
 
-// Deprecate logs a deprecation message
-// The version string contains the version when the future willb e removed
+// Deprecate logs a deprecation message.
+// The version string contains the version when the future will be removed
 func Deprecate(version string, format string, v ...interface{}) {
 	postfix := fmt.Sprintf(" Will be removed in version: %s", version)
-	msg(LOG_WARNING, "WARN DEPRECATED: ", format+postfix, v...)
+	Warn("DEPRECATED: "+format+postfix, v...)
+}
+
+// Experimental logs the usage of an experimental feature.
+func Experimental(format string, v ...interface{}) {
+	Warn("EXPERIMENTAL: "+format, v...)
+}
+
+// Beta logs the usage of an beta feature.
+func Beta(format string, v ...interface{}) {
+	Warn("BETA: "+format, v...)
 }
 
 // WTF prints the message at CRIT level and panics immediately with the same
