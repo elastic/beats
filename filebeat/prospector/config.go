791b3263757d8816e1829acc5ae7b2f6b11a0ab3
Vijay Samuel <vija@ebay.com>
2017-03-30 02:08:19 -0700

    Prospector level processors

--- filebeat/prospector/config.go
+++ filebeat/prospector/config.go
@@ -8,6 +8,7 @@ import (
 	"github.com/elastic/beats/filebeat/harvester/reader"
 	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/libbeat/common/match"
+	"github.com/elastic/beats/libbeat/processors"
 )
 
 var (
@@ -26,23 +27,24 @@ var (
 )
 
 type prospectorConfig struct {
-	common.EventMetadata `config:",inline"` // Fields and tags to add to events.
-	Enabled              bool               `config:"enabled"`
-	DocumentType         string             `config:"document_type"`
-	ExcludeFiles         []match.Matcher    `config:"exclude_files"`
-	IgnoreOlder          time.Duration      `config:"ignore_older"`
-	Paths                []string           `config:"paths"`
-	ScanFrequency        time.Duration      `config:"scan_frequency" validate:"min=0,nonzero"`
-	InputType            string             `config:"input_type"`
-	CleanInactive        time.Duration      `config:"clean_inactive" validate:"min=0"`
-	CleanRemoved         bool               `config:"clean_removed"`
-	HarvesterLimit       uint64             `config:"harvester_limit" validate:"min=0"`
-	Symlinks             bool               `config:"symlinks"`
-	TailFiles            bool               `config:"tail_files"`
-	JSON                 *reader.JSONConfig `config:"json"`
-	Pipeline             string             `config:"pipeline"`
-	Module               string             `config:"_module_name"`  // hidden option to set the module name
-	Fileset              string             `config:"_fileset_name"` // hidden option to set the fileset name
+	common.EventMetadata `config:",inline"`      // Fields and tags to add to events.
+	Enabled              bool                    `config:"enabled"`
+	DocumentType         string                  `config:"document_type"`
+	ExcludeFiles         []match.Matcher         `config:"exclude_files"`
+	IgnoreOlder          time.Duration           `config:"ignore_older"`
+	Paths                []string                `config:"paths"`
+	ScanFrequency        time.Duration           `config:"scan_frequency" validate:"min=0,nonzero"`
+	InputType            string                  `config:"input_type"`
+	CleanInactive        time.Duration           `config:"clean_inactive" validate:"min=0"`
+	CleanRemoved         bool                    `config:"clean_removed"`
+	HarvesterLimit       uint64                  `config:"harvester_limit" validate:"min=0"`
+	Symlinks             bool                    `config:"symlinks"`
+	TailFiles            bool                    `config:"tail_files"`
+	JSON                 *reader.JSONConfig      `config:"json"`
+	Pipeline             string                  `config:"pipeline"`
+	Module               string                  `config:"_module_name"`  // hidden option to set the module name
+	Fileset              string                  `config:"_fileset_name"` // hidden option to set the fileset name
+	Processors           processors.PluginConfig `config:"processors"`
 }
 
 func (config *prospectorConfig) Validate() error {
