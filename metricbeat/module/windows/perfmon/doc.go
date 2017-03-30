9fdc094d06e9043b2fb125bf7dad147616c24ea9
Martin Scholz <martin.scholz83@outlook.com>
2017-03-30 18:31:00 +0200

    Fix doc.go

--- metricbeat/module/windows/perfmon/doc.go
+++ metricbeat/module/windows/perfmon/doc.go
@@ -3,5 +3,5 @@ Package perfmon collect windows performance counters.
 */
 package perfmon
 
-// go:generate go tool cgo -godefs defs_windows.go > defs_windows_amd64.go
-// go:generate go tool cgo -godefs defs_windows.go > defs_windows_386.go
+//go:generate go tool cgo -godefs defs_windows.go > defs_windows_amd64.go
+//go:generate go tool cgo -godefs defs_windows.go > defs_windows_386.go
