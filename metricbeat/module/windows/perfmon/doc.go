962fb7ee632bd0d39fe7b545adddfccc1a4f2d52
Martin Scholz <martin.scholz83@outlook.com>
2017-03-30 11:34:04 +0200

    add missing go generate lines

--- metricbeat/module/windows/perfmon/doc.go
+++ metricbeat/module/windows/perfmon/doc.go
@@ -2,3 +2,6 @@
 Package perfmon collect windows performance counters.
 */
 package perfmon
+
+// go:generate go tool cgo -godefs defs_windows.go > defs_windows_amd64.go
+// go:generate go tool cgo -godefs defs_windows.go > defs_windows_386.go
