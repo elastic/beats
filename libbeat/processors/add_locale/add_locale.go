a45355dfc564134c8504a5941d353432b4b674b2
Martin Scholz <martin.scholz83@outlook.com>
2017-04-04 11:03:39 +0200

    Add add_locale processor

---
+++ libbeat/processors/add_locale/add_locale.go
@@ -0,0 +1,50 @@
+package actions
+
+import (
+	"time"
+
+	"github.com/elastic/beats/libbeat/common"
+	"github.com/elastic/beats/libbeat/processors"
+	"github.com/pkg/errors"
+)
+
+type addLocale struct {
+	timezone string
+}
+
+// init registers the add_locale processor.
+func init() {
+	processors.RegisterPlugin("add_locale",
+		configChecked(newAddLocale,
+			requireFields("timezone")))
+}
+
+func newAddLocale(c common.Config) (processors.Processor, error) {
+	config := struct {
+		TimeZone string `config:"timezone"`
+	}{}
+
+	err := c.Unpack(&config)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to unpack add_locale config")
+	}
+
+	l := addLocale{timezone: config.TimeZone}
+
+	return l, nil
+}
+
+func (l addLocale) Run(event common.MapStr) (common.MapStr, error) {
+	zone, err := time.LoadLocation(l.timezone)
+
+	if err != nil {
+		return event, err
+	}
+
+	event.Put("beat.timezone", zone.String())
+	return event, nil
+}
+
+func (l addLocale) String() string {
+	return "add_locale=" + l.timezone
+}
