ee07419cd283d5d5f5ebf6081adfd0915100ffcb
Andrew Kroh <andrew.kroh@elastic.co>
2017-04-07 13:33:03 -0400

    Rename Filebeat module from system.audit to auditd.log (#3941)
    * Rename Filebeat module from system.audit to auditd.log
    
    This moves the `audit` fileset from the `system` module into its own module named `auditd`. The new fileset name is `log`.
    
    * Add changelog entry for auditd module.

--- filebeat/fileset/modules_test.go
+++ filebeat/fileset/modules_test.go
@@ -29,6 +29,7 @@ func TestNewModuleRegistry(t *testing.T) {
 		{Module: "nginx"},
 		{Module: "mysql"},
 		{Module: "system"},
+		{Module: "auditd"},
 	}
 
 	reg, err := newModuleRegistry(modulesPath, configs, nil, "5.2.0")
@@ -36,9 +37,10 @@ func TestNewModuleRegistry(t *testing.T) {
 	assert.NotNil(t, reg)
 
 	expectedModules := map[string][]string{
+		"auditd": {"log"},
 		"nginx":  {"access", "error"},
 		"mysql":  {"slowlog", "error"},
-		"system": {"syslog", "audit", "auth"},
+		"system": {"syslog", "auth"},
 	}
 
 	assert.Equal(t, len(expectedModules), len(reg.registry))
