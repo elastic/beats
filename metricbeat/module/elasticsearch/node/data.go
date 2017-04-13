2321cf92889657ba42eab4f9e1495cd6a92c33b9
ruflin <spam@ruflin.com>
2017-04-13 09:59:48 +0200

    Add elasticsearch node metricset

---
+++ metricbeat/module/elasticsearch/node/data.go
@@ -0,0 +1,50 @@
+package node
+
+import (
+	"encoding/json"
+
+	"github.com/elastic/beats/libbeat/common"
+	s "github.com/elastic/beats/metricbeat/schema"
+	c "github.com/elastic/beats/metricbeat/schema/mapstriface"
+)
+
+var (
+	schema = s.Schema{
+		"name":    c.Str("name"),
+		"version": c.Str("version"),
+		"jvm": c.Dict("jvm", s.Schema{
+			"version": c.Str("version"),
+			"memory": c.Dict("mem", s.Schema{
+				"heap_init": s.Object{
+					"bytes": c.Int("heap_init_in_bytes"),
+				},
+			}),
+		}),
+	}
+)
+
+func eventsMapping(content []byte) ([]common.MapStr, error) {
+
+	nodesStruct := struct {
+		Nodes map[string]map[string]interface{} `json:"nodes"`
+	}{}
+
+	json.Unmarshal(content, &nodesStruct)
+
+	var events []common.MapStr
+	errors := s.NewErrors()
+
+	for name, node := range nodesStruct.Nodes {
+		event, errs := eventMapping(node)
+		// Write name here as full name only available as key
+		event["name"] = name
+		events = append(events, event)
+		errors.AddErrors(errs)
+	}
+
+	return events, errors
+}
+
+func eventMapping(node map[string]interface{}) (common.MapStr, *s.Errors) {
+	return schema.Apply(node)
+}
