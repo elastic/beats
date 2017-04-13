472daad130244472bcb2fbdb381ca712125928fd
Nicolas Ruflin <spam@ruflin.com>
2017-04-13 13:20:13 +0200

    Add elasticsearch node metricset (#4011)

---
+++ metricbeat/module/elasticsearch/node/node.go
@@ -0,0 +1,53 @@
+package node
+
+import (
+	"github.com/elastic/beats/libbeat/common"
+	"github.com/elastic/beats/metricbeat/helper"
+	"github.com/elastic/beats/metricbeat/mb"
+	"github.com/elastic/beats/metricbeat/mb/parse"
+)
+
+// init registers the MetricSet with the central registry.
+// The New method will be called after the setup of the module and before starting to fetch data
+func init() {
+	if err := mb.Registry.AddMetricSet("elasticsearch", "node", New, hostParser); err != nil {
+		panic(err)
+	}
+}
+
+var (
+	hostParser = parse.URLHostParserBuilder{
+		DefaultScheme: "http",
+		PathConfigKey: "path",
+		// This only fetches data for the local node.
+		DefaultPath: "_nodes/_local",
+	}.Build()
+)
+
+// MetricSet type defines all fields of the MetricSet
+type MetricSet struct {
+	mb.BaseMetricSet
+	http *helper.HTTP
+}
+
+// New create a new instance of the MetricSet
+func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
+	return &MetricSet{
+		base,
+		helper.NewHTTP(base),
+	}, nil
+}
+
+// Fetch methods implements the data gathering and data conversion to the right format
+// It returns the event which is then forward to the output. In case of an error, a
+// descriptive error must be returned.
+func (m *MetricSet) Fetch() ([]common.MapStr, error) {
+
+	content, err := m.http.FetchContent()
+	if err != nil {
+		return nil, err
+	}
+
+	events, _ := eventsMapping(content)
+	return events, nil
+}
