54ee7c77c2082bb342eba9ba71171c43db544bb0
Carlos Pérez-Aradros Herce <exekias@gmail.com>
2017-04-17 20:02:40 +0200

    Fix MongoDB dbstats fields mapping
    (cherry picked from commit 6b0b0773b1425c7fa6f57fe066c653b25cb7ed0e)

--- metricbeat/module/mongodb/dbstats/data.go
+++ metricbeat/module/mongodb/dbstats/data.go
@@ -6,19 +6,31 @@ import (
 )
 
 var schema = s.Schema{
-	"db":           c.Str("db"),
-	"collections":  c.Int("collections"),
-	"objects":      c.Int("objects"),
-	"avg_obj_size": c.Int("avgObjSize"),
-	"data_size":    c.Int("dataSize"),
-	"storage_size": c.Int("storageSize"),
-	"num_extents":  c.Int("numExtents"),
-	"indexes":      c.Int("indexes"),
-	"index_size":   c.Int("indexSize"),
+	"db":          c.Str("db"),
+	"collections": c.Int("collections"),
+	"objects":     c.Int("objects"),
+	"avg_obj_size": s.Object{
+		"bytes": c.Int("avgObjSize"),
+	},
+	"data_size": s.Object{
+		"bytes": c.Int("dataSize"),
+	},
+	"storage_size": s.Object{
+		"bytes": c.Int("storageSize"),
+	},
+	"num_extents": c.Int("numExtents"),
+	"indexes":     c.Int("indexes"),
+	"index_size": s.Object{
+		"bytes": c.Int("indexSize"),
+	},
 	// mmapv1 only
-	"ns_size_mb": c.Int("nsSizeMB", s.Optional),
+	"ns_size_mb": s.Object{
+		"mb": c.Int("nsSizeMB", s.Optional),
+	},
 	// mmapv1 only
-	"file_size": c.Int("fileSize", s.Optional),
+	"file_size": s.Object{
+		"bytes": c.Int("fileSize", s.Optional),
+	},
 	// mmapv1 only
 	"data_file_version": c.Dict("dataFileVersion", s.Schema{
 		"major": c.Int("major"),
@@ -26,8 +38,10 @@ var schema = s.Schema{
 	}, c.DictOptional),
 	// mmapv1 only
 	"extent_free_list": c.Dict("extentFreeList", s.Schema{
-		"num":  c.Int("num"),
-		"size": c.Int("size"),
+		"num": c.Int("num"),
+		"size": s.Object{
+			"bytes": c.Int("size", s.Optional),
+		},
 	}, c.DictOptional),
 }
 
