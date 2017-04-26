85f1f1e23b6741943bcf5b45d9e9797260deaf91
amandahla <amanda.andrade@serpro.gov.br>
2017-04-26 15:30:11 -0300

    NOTICE updated.

--- metricbeat/module/vsphere/datastore/datastore_test.go
+++ metricbeat/module/vsphere/datastore/datastore_test.go
@@ -1,8 +1,9 @@
 package datastore
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestFetchEventContents(t *testing.T) {
--- metricbeat/module/vsphere/virtualmachine/virtualmachine_test.go
+++ metricbeat/module/vsphere/virtualmachine/virtualmachine_test.go
@@ -1,8 +1,9 @@
 package virtualmachine
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestFetchEventContents(t *testing.T) {
