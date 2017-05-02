628615e167f141c2c785a9c8ba925a1d92ce3320
Nicolas Ruflin <spam@ruflin.com>
2017-05-02 10:51:32 +0200

    Move handling of json fields to harvester (#4159)
    * Move handling of json fields to harvester
    
    Because of historical reasons, the common.MapStr event was generated in `input.Event` which required the JSONConfig to be passed to the Event object. This is not nice because it is harvester specific knowledge in the event object which should not be the case. With the introduction of the data object is is now possible that the arranging of the json fields already happens in the harvester. The @timestamp field had to be moved to the harvester to make sure json error handling still works as before. Unused Event fields were removed.
    
    Further generalisation of the event handling will be introduce with the addition of new prospector types.
    
    * fix state updates

--- filebeat/input/event.go
+++ filebeat/input/event.go
@@ -1,33 +1,23 @@
 package input
 
 import (
-	"time"
-
-	"github.com/elastic/beats/filebeat/harvester/reader"
 	"github.com/elastic/beats/filebeat/input/file"
 	"github.com/elastic/beats/libbeat/common"
-	"github.com/elastic/beats/libbeat/common/jsontransform"
 )
 
 // Event is sent to the output and must contain all relevant information
 type Event struct {
 	EventMeta
-	Text       *string
-	JSONConfig *reader.JSONConfig
-	Data       common.MapStr // Use in readers to add data to the event
-
+	Data common.MapStr // Use in readers to add data to the event
 }
 
 type EventMeta struct {
-	common.EventMetadata
-	Pipeline     string
-	Fileset      string
-	Module       string
-	InputType    string
-	DocumentType string
-	ReadTime     time.Time
-	Bytes        int
-	State        file.State
+	Pipeline  string
+	Fileset   string
+	Module    string
+	InputType string
+	Bytes     int
+	State     file.State
 }
 
 type Data struct {
@@ -45,13 +35,10 @@ func NewEvent(state file.State) *Event {
 }
 
 func (e *Event) ToMapStr() common.MapStr {
-	event := common.MapStr{
-		common.EventMetadataKey: e.EventMetadata,
-		"@timestamp":            common.Time(e.ReadTime),
-		"source":                e.State.Source,
-		"offset":                e.State.Offset, // Offset here is the offset before the starting char.
-		"type":                  e.DocumentType,
-		"input_type":            e.InputType,
+
+	event := e.Data
+	if event == nil {
+		event = common.MapStr{}
 	}
 
 	if e.Fileset != "" && e.Module != "" {
@@ -61,23 +48,6 @@ func (e *Event) ToMapStr() common.MapStr {
 		}
 	}
 
-	// Add data fields which are added by the readers
-	for key, value := range e.Data {
-		event[key] = value
-	}
-
-	// Check if json fields exist
-	var jsonFields common.MapStr
-	if fields, ok := event["json"]; ok {
-		jsonFields = fields.(common.MapStr)
-	}
-
-	if e.JSONConfig != nil && len(jsonFields) > 0 {
-		mergeJSONFields(e, event, jsonFields)
-	} else if e.Text != nil {
-		event["message"] = *e.Text
-	}
-
 	return event
 }
 
@@ -85,13 +55,11 @@ func (e *Event) GetData() Data {
 	return Data{
 		Event: e.ToMapStr(),
 		Metadata: EventMeta{
-			Pipeline:      e.Pipeline,
-			Bytes:         e.Bytes,
-			State:         e.State,
-			Fileset:       e.Fileset,
-			Module:        e.Module,
-			ReadTime:      e.ReadTime,
-			EventMetadata: e.EventMetadata,
+			Pipeline: e.Pipeline,
+			Bytes:    e.Bytes,
+			State:    e.State,
+			Fileset:  e.Fileset,
+			Module:   e.Module,
 		},
 	}
 }
@@ -111,23 +79,4 @@ func (eh *Data) GetMetadata() common.MapStr {
 // Events without data are only state updates
 func (eh *Data) HasData() bool {
 	return eh.Metadata.Bytes > 0
-}
-
-// mergeJSONFields writes the JSON fields in the event map,
-// respecting the KeysUnderRoot and OverwriteKeys configuration options.
-// If MessageKey is defined, the Text value from the event always
-// takes precedence.
-func mergeJSONFields(e *Event, event common.MapStr, jsonFields common.MapStr) {
-
-	// The message key might have been modified by multiline
-	if len(e.JSONConfig.MessageKey) > 0 && e.Text != nil {
-		jsonFields[e.JSONConfig.MessageKey] = *e.Text
-	}
-
-	if e.JSONConfig.KeysUnderRoot {
-		// Delete existing json key
-		delete(event, "json")
-
-		jsontransform.WriteJSONKeys(event, jsonFields, e.JSONConfig.OverwriteKeys, reader.JsonErrorKey)
-	}
 }
