2d28daf536b1b5af09ac1e28f84f2b466a7a5fba
Andrew Kroh <andrew.kroh@elastic.co>
2017-05-04 17:15:17 -0400

    Miscellaneous test fixes
    - Fix and enable the python smoke test for heartbeat
    - Remove fmt.Printf from metricbeat ceph module
    - Fix Windows path issues in libbeat/paths tests
    - Fix ioutil.TempDir usage in Packetbeat tests (it broke windows)

--- metricbeat/module/ceph/monitor_health/monitor_health.go
+++ metricbeat/module/ceph/monitor_health/monitor_health.go
@@ -1,8 +1,6 @@
 package monitor_health
 
 import (
-	"fmt"
-
 	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/libbeat/logp"
 	"github.com/elastic/beats/metricbeat/helper"
@@ -46,15 +44,10 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 }
 
 func (m *MetricSet) Fetch() ([]common.MapStr, error) {
-
 	content, err := m.HTTP.FetchContent()
 	if err != nil {
 		return nil, err
 	}
 
-	fmt.Printf("%+v", string(content))
-	fmt.Printf("%+v", eventsMapping(content))
-
 	return eventsMapping(content), nil
-
 }
--- metricbeat/module/ceph/monitor_health/monitor_health_test.go
+++ metricbeat/module/ceph/monitor_health/monitor_health_test.go
@@ -32,10 +32,10 @@ func TestFetchEventContents(t *testing.T) {
 
 	f := mbtest.NewEventsFetcher(t, config)
 	events, err := f.Fetch()
-	event := events[0]
-	if !assert.NoError(t, err) {
-		t.FailNow()
+	if err != nil {
+		t.Fatal(err)
 	}
+	event := events[0]
 
 	t.Logf("%s/%s event: %+v", f.Module().Name(), f.Name(), event.StringToPrint())
 
@@ -68,5 +68,4 @@ func TestFetchEventContents(t *testing.T) {
 
 	total = store_stats["total"].(common.MapStr)
 	assert.EqualValues(t, 8488943, total["bytes"])
-
 }
