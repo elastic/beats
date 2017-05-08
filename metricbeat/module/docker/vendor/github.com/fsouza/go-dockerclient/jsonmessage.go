14396f434913013d73b570876d6724564deefed2
Nicolas Ruflin <spam@ruflin.com>
2017-05-08 10:42:50 +0200

    Update docker dependency and modify docker client (#4245)
    Previously a fork of the docker repository was used to reduced the number of dependencies. This was now changed to a fork of the go-dockerclient. The fork of the docker client is easier to maintain as the code base is much smaller and it allows to remove a large list of dependencies from the docker library.
    
    The change to the docker client can be found here: https://github.com/ruflin/go-dockerclient/commit/ba365ff5e4281feb28654e4ca599a1defd063497 In general dependencies when running the client from the terminal were removed as these are not needed for metricbeat. Removing these dependencies also makes crosscompiling for more platforms possible as these libs were only available for a subset of our platforms.
    
    Docker was updated to v17.05.0-ce to include some fixes for compiling on mipsle.

--- metricbeat/module/docker/vendor/github.com/fsouza/go-dockerclient/jsonmessage.go
+++ metricbeat/module/docker/vendor/github.com/fsouza/go-dockerclient/jsonmessage.go
@@ -1,4 +1,7 @@
-package jsonmessage
+package docker
+
+// NOTE: This is a modified copy of the file https://github.com/moby/moby/tree/master/pkg/jsonmessage
+// 	     This heavily reduces the number of packages which are a dependency.
 
 import (
 	"encoding/json"
@@ -29,6 +32,8 @@ type JSONProgress struct {
 	Current    int64 `json:"current,omitempty"`
 	Total      int64 `json:"total,omitempty"`
 	Start      int64 `json:"start,omitempty"`
+	// If true, don't show xB/yB
+	HideCounts bool `json:"hidecounts,omitempty"`
 }
 
 func (p *JSONProgress) String() string {
@@ -53,10 +58,60 @@ type JSONMessage struct {
 	Aux *json.RawMessage `json:"aux,omitempty"`
 }
 
-// Display displays the JSONMessage to `out`. `isTerminal` describes if `out`
+/* Satisfied by gotty.TermInfo as well as noTermInfo from below */
+type termInfo interface {
+	Parse(attr string, params ...interface{}) (string, error)
+}
+
+type noTermInfo struct{} // canary used when no terminfo.
+
+func (ti *noTermInfo) Parse(attr string, params ...interface{}) (string, error) {
+	return "", fmt.Errorf("noTermInfo")
+}
+
+func clearLine(out io.Writer, ti termInfo) {
+	// el2 (clear whole line) is not exposed by terminfo.
+
+	// First clear line from beginning to cursor
+	if attr, err := ti.Parse("el1"); err == nil {
+		fmt.Fprintf(out, "%s", attr)
+	} else {
+		fmt.Fprintf(out, "\x1b[1K")
+	}
+	// Then clear line from cursor to end
+	if attr, err := ti.Parse("el"); err == nil {
+		fmt.Fprintf(out, "%s", attr)
+	} else {
+		fmt.Fprintf(out, "\x1b[K")
+	}
+}
+
+func cursorUp(out io.Writer, ti termInfo, l int) {
+	if l == 0 { // Should never be the case, but be tolerant
+		return
+	}
+	if attr, err := ti.Parse("cuu", l); err == nil {
+		fmt.Fprintf(out, "%s", attr)
+	} else {
+		fmt.Fprintf(out, "\x1b[%dA", l)
+	}
+}
+
+func cursorDown(out io.Writer, ti termInfo, l int) {
+	if l == 0 { // Should never be the case, but be tolerant
+		return
+	}
+	if attr, err := ti.Parse("cud", l); err == nil {
+		fmt.Fprintf(out, "%s", attr)
+	} else {
+		fmt.Fprintf(out, "\x1b[%dB", l)
+	}
+}
+
+// Display displays the JSONMessage to `out`. `termInfo` is non-nil if `out`
 // is a terminal. If this is the case, it will erase the entire current line
 // when displaying the progressbar.
-func (jm *JSONMessage) Display(out io.Writer, isTerminal bool) error {
+func (jm *JSONMessage) Display(out io.Writer, termInfo termInfo) error {
 	if jm.Error != nil {
 		if jm.Error.Code == 401 {
 			return fmt.Errorf("Authentication is required.")
@@ -64,10 +119,10 @@ func (jm *JSONMessage) Display(out io.Writer, isTerminal bool) error {
 		return jm.Error
 	}
 	var endl string
-	if isTerminal && jm.Stream == "" && jm.Progress != nil {
-		// <ESC>[2K = erase entire current line
-		fmt.Fprintf(out, "%c[2K\r", 27)
+	if termInfo != nil && jm.Stream == "" && jm.Progress != nil {
+		clearLine(out, termInfo)
 		endl = "\r"
+		fmt.Fprintf(out, endl)
 	} else if jm.Progress != nil && jm.Progress.String() != "" { //disable progressbar in non-terminal
 		return nil
 	}
@@ -82,7 +137,7 @@ func (jm *JSONMessage) Display(out io.Writer, isTerminal bool) error {
 	if jm.From != "" {
 		fmt.Fprintf(out, "(from %s) ", jm.From)
 	}
-	if jm.Progress != nil && isTerminal {
+	if jm.Progress != nil && termInfo != nil {
 		fmt.Fprintf(out, "%s %s%s", jm.Status, jm.Progress.String(), endl)
 	} else if jm.ProgressMessage != "" { //deprecated
 		fmt.Fprintf(out, "%s %s%s", jm.Status, jm.ProgressMessage, endl)
@@ -102,6 +157,9 @@ func DisplayJSONMessagesStream(in io.Reader, out io.Writer, terminalFd uintptr, 
 		dec = json.NewDecoder(in)
 		ids = make(map[string]int)
 	)
+
+	var termInfo termInfo
+
 	for {
 		diff := 0
 		var jm JSONMessage
@@ -133,13 +191,13 @@ func DisplayJSONMessagesStream(in io.Reader, out io.Writer, terminalFd uintptr, 
 				// with no ID.
 				line = len(ids)
 				ids[jm.ID] = line
-				if isTerminal {
+				if termInfo != nil {
 					fmt.Fprintf(out, "\n")
 				}
 			}
 			diff = len(ids) - line
-			if isTerminal && diff > 0 {
-				fmt.Fprintf(out, "%c[%dA", 27, diff)
+			if termInfo != nil {
+				cursorUp(out, termInfo, diff)
 			}
 		} else {
 			// When outputting something that isn't progress
@@ -149,9 +207,9 @@ func DisplayJSONMessagesStream(in io.Reader, out io.Writer, terminalFd uintptr, 
 			// with multiple tags).
 			ids = make(map[string]int)
 		}
-		err := jm.Display(out, isTerminal)
-		if jm.ID != "" && isTerminal && diff > 0 {
-			fmt.Fprintf(out, "%c[%dB", 27, diff)
+		err := jm.Display(out, termInfo)
+		if jm.ID != "" && termInfo != nil {
+			cursorDown(out, termInfo, diff)
 		}
 		if err != nil {
 			return err
@@ -164,9 +222,4 @@ type stream interface {
 	io.Writer
 	FD() uintptr
 	IsTerminal() bool
-}
-
-// DisplayJSONMessagesToStream prints json messages to the output stream
-func DisplayJSONMessagesToStream(in io.Reader, stream stream, auxCallback func(*json.RawMessage)) error {
-	return DisplayJSONMessagesStream(in, stream, stream.FD(), stream.IsTerminal(), auxCallback)
 }
--- metricbeat/module/docker/vendor/github.com/fsouza/go-dockerclient/tar.go
+++ metricbeat/module/docker/vendor/github.com/fsouza/go-dockerclient/tar.go
@@ -6,60 +6,14 @@ package docker
 
 import (
 	"fmt"
-	"io"
 	"io/ioutil"
 	"os"
 	"path"
 	"path/filepath"
 	"strings"
 
-	"github.com/docker/docker/pkg/archive"
 	"github.com/docker/docker/pkg/fileutils"
 )
-
-func createTarStream(srcPath, dockerfilePath string) (io.ReadCloser, error) {
-	excludes, err := parseDockerignore(srcPath)
-	if err != nil {
-		return nil, err
-	}
-
-	includes := []string{"."}
-
-	// If .dockerignore mentions .dockerignore or the Dockerfile
-	// then make sure we send both files over to the daemon
-	// because Dockerfile is, obviously, needed no matter what, and
-	// .dockerignore is needed to know if either one needs to be
-	// removed.  The deamon will remove them for us, if needed, after it
-	// parses the Dockerfile.
-	//
-	// https://github.com/docker/docker/issues/8330
-	//
-	forceIncludeFiles := []string{".dockerignore", dockerfilePath}
-
-	for _, includeFile := range forceIncludeFiles {
-		if includeFile == "" {
-			continue
-		}
-		keepThem, err := fileutils.Matches(includeFile, excludes)
-		if err != nil {
-			return nil, fmt.Errorf("cannot match .dockerfile: '%s', error: %s", includeFile, err)
-		}
-		if keepThem {
-			includes = append(includes, includeFile)
-		}
-	}
-
-	if err := validateContextDirectory(srcPath, excludes); err != nil {
-		return nil, err
-	}
-	tarOpts := &archive.TarOptions{
-		ExcludePatterns: excludes,
-		IncludeFiles:    includes,
-		Compression:     archive.Uncompressed,
-		NoLchown:        true,
-	}
-	return archive.TarWithOptions(srcPath, tarOpts)
-}
 
 // validateContextDirectory checks if all the contents of the directory
 // can be read and returns an error if some files can't be read.
