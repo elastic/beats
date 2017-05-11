413917ecbbef8d366a90e85b6e490bd3494813ab
Tudor Golubenco <tudor@elastic.co>
2017-05-11 16:52:07 +0200

    Template setting in the configuration file (#4284)
    This allows adding and overwriting index/template settings via the
    configuration file.
    
    Example config:
    
        setup.template.settings:
          index.number_of_shards: 1
          index.number_of_replicas: 1
    
    Needed for #4112, part of #3654.

--- libbeat/template/load_integration_test.go
+++ libbeat/template/load_integration_test.go
@@ -3,10 +3,13 @@
 package template
 
 import (
+	"encoding/json"
+	"fmt"
 	"path/filepath"
 	"testing"
 	"time"
 
+	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/libbeat/outputs/elasticsearch"
 	"github.com/elastic/beats/libbeat/version"
 
@@ -43,7 +46,7 @@ func TestLoadTemplate(t *testing.T) {
 	fieldsPath := absPath + "/fields.yml"
 	index := "testbeat"
 
-	tmpl, err := New(version.GetDefaultVersion(), client.GetVersion(), index)
+	tmpl, err := New(version.GetDefaultVersion(), client.GetVersion(), index, common.MapStr{})
 	assert.NoError(t, err)
 	content, err := tmpl.Load(fieldsPath)
 	assert.NoError(t, err)
@@ -114,7 +117,7 @@ func TestLoadBeatsTemplate(t *testing.T) {
 		fieldsPath := absPath + "/fields.yml"
 		index := beat
 
-		tmpl, err := New(version.GetDefaultVersion(), client.GetVersion(), index)
+		tmpl, err := New(version.GetDefaultVersion(), client.GetVersion(), index, common.MapStr{})
 		assert.NoError(t, err)
 		content, err := tmpl.Load(fieldsPath)
 		assert.NoError(t, err)
@@ -136,4 +139,57 @@ func TestLoadBeatsTemplate(t *testing.T) {
 		// Make sure it was removed
 		assert.False(t, loader.CheckTemplate(tmpl.GetName()))
 	}
+}
+
+func TestTemplateSettings(t *testing.T) {
+
+	// Setup ES
+	client := elasticsearch.GetTestingElasticsearch()
+	err := client.Connect(5 * time.Second)
+	assert.Nil(t, err)
+
+	// Load template
+	absPath, err := filepath.Abs("../")
+	assert.NotNil(t, absPath)
+	assert.Nil(t, err)
+
+	fieldsPath := absPath + "/fields.yml"
+
+	settings := common.MapStr{
+		"index": common.MapStr{
+			"number_of_shards": 1,
+		},
+	}
+	tmpl, err := New(version.GetDefaultVersion(), client.GetVersion(), "testbeat", settings)
+	assert.NoError(t, err)
+	content, err := tmpl.Load(fieldsPath)
+	assert.NoError(t, err)
+
+	loader := &Loader{
+		client: client,
+	}
+
+	// Load template
+	err = loader.LoadTemplate(tmpl.GetName(), content)
+	assert.Nil(t, err)
+
+	// Check that it contains the mapping
+	status, body, err := loader.client.Request("GET", "/_template/"+tmpl.GetName(), "", nil, nil)
+	assert.NoError(t, err)
+	assert.Equal(t, status, 200)
+
+	var response common.MapStr
+	err = json.Unmarshal(body, &response)
+	assert.NoError(t, err)
+
+	templateJSON := common.MapStr(response[tmpl.GetName()].(map[string]interface{}))
+	val, err := templateJSON.GetValue(fmt.Sprintf("settings.index.number_of_shards"))
+	assert.NoError(t, err)
+	assert.Equal(t, val.(string), "1")
+
+	// Delete template again to clean up
+	client.Request("DELETE", "/_template/"+tmpl.GetName(), "", nil, nil)
+
+	// Make sure it was removed
+	assert.False(t, loader.CheckTemplate(tmpl.GetName()))
 }
