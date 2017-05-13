868e1d01a171daf3beb9e0540c97641f214af0e6
Andrew Kroh <andrew.kroh@elastic.co>
2017-05-13 10:41:35 -0400

    Clean geoip.paths before using the path (#4306)
    Use filepath.Clean on the configured paths to fix any invalid OS path separators.
    
    Skip the geoip test with symlinks on Windows (`os.symlink` isn’t supported on Windows).

--- packetbeat/tests/system/test_0011_geoip.py
+++ packetbeat/tests/system/test_0011_geoip.py
@@ -1,5 +1,8 @@
-from packetbeat import BaseTest
 import os
+import unittest
+import sys
+
+from packetbeat import BaseTest
 
 """
 Tests for reading the geoip files.
@@ -43,6 +46,7 @@ class Test(BaseTest):
         assert o["real_ip"] == "89.247.39.104"
         assert o["client_location"] == "52.528503, 13.410904"
 
+    @unittest.skipIf(sys.platform.startswith("win"), "requires unix for symlinks")
     def test_geoip_symlink(self):
         """
         Should be able to follow symlinks to GeoIP libs.
