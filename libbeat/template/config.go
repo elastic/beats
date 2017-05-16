7c237d2adf3fd23dcd74cfb57533fb6657e3d86b
Tudor Golubenco <tudor@elastic.co>
2017-05-16 09:51:18 +0200

    Add option to adjust _source options + improvements (#4317)
    This adds/fixes:
    
    * Ability to disable _source, or set other _source related options
    * Moves template index settings under `settings.index`
    * Fixes the overwrite logic (was using the wrong template name on the check)
    * Fixes error handling
    * Integration tests for overwritting
    
    Part of #3654 and #4112.

--- libbeat/template/config.go
+++ libbeat/template/config.go
@@ -1,12 +1,17 @@
 package template
 
 type TemplateConfig struct {
-	Enabled      bool                   `config:"enabled"`
-	Name         string                 `config:"name"`
-	Fields       string                 `config:"fields"`
-	Overwrite    bool                   `config:"overwrite"`
-	OutputToFile string                 `config:"output_to_file"`
-	Settings     map[string]interface{} `config:"settings"`
+	Enabled      bool             `config:"enabled"`
+	Name         string           `config:"name"`
+	Fields       string           `config:"fields"`
+	Overwrite    bool             `config:"overwrite"`
+	OutputToFile string           `config:"output_to_file"`
+	Settings     templateSettings `config:"settings"`
+}
+
+type templateSettings struct {
+	Index  map[string]interface{} `config:"index"`
+	Source map[string]interface{} `config:"_source"`
 }
 
 var (
