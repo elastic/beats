1b6425d10cb9c2bd571509be6ab595531f82a21e
Tudor Golubenco <tudor@elastic.co>
2017-05-18 09:48:04 +0200

    Add the option to generate the template into a file (#4323)
    Part of #3654.
    
    This adds two settings: `setup.template.output_to_file.path` and `setup.template.output_to_file.version`. The version refers to the ES version and is optional, we'll use the Beats version if not specified. I put it under `output_to_file` to make it clear that it only applies when outputting to a file:
    
    To generate a config, one can do:
    
    ```
    ./metricbeat -e -E setup.template.output_to_file.path=template.json
    ```
    
    In the current version, the Beat automatically stops after generating the template, but the output might be slightly confusing:
    
    ```
    2017/05/16 09:55:02.043671 load.go:116: INFO Template for Elasticsearch 6.0.0-alpha2 written to: template.json
    2017/05/16 09:55:02.043717 beat.go:538: CRIT Exiting: Stopping after successfully writing the template to the file.
    Exiting: Stopping after successfully writing the template to the file.
    ```
    
    IMO this is better than the alternative of leaving it running.
    
    To generate the template for the 2.x version, one can do:
    
    ```
    ./metricbeat -e -E setup.template.output_to_file.path=template.json -E setup.template.output_to_file.version=2.4.0
    ```
    
    Remaining TODOs:
    
    * [x] System test
    * [x] Docs
    * [x] Changelog

--- libbeat/tests/system/base.py
+++ libbeat/tests/system/base.py
@@ -9,4 +9,11 @@ class BaseTest(TestCase):
         self.beat_name = "mockbeat"
         self.beat_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
         self.test_binary = self.beat_path + "/libbeat.test"
+        self.beats = [
+            "filebeat",
+            "heartbeat",
+            "metricbeat",
+            "packetbeat",
+            "winlogbeat"
+        ]
         super(BaseTest, self).setUpClass()
