eb899db9f4d9fdfe9ae92c3cce2e5178c4a9b835
Nicolas Ruflin <spam@ruflin.com>
2017-05-22 16:05:21 +0200

    Bring back prospector.type which went missing (#4361)
    It seems in one of the recent refactorings prospector.type went missing. Test added to make sure it will not go missing again.

---
+++ filebeat/tests/system/test_base.py
@@ -0,0 +1,28 @@
+from filebeat import BaseTest
+import os
+
+"""
+Test for basic object
+"""
+
+
+class Test(BaseTest):
+
+    def test_base(self):
+        """
+        Test if the basic fields exist.
+        """
+        self.render_config_template(
+            path=os.path.abspath(self.working_dir) + "/test.log",
+        )
+
+        with open(self.working_dir + "/test.log", "w") as f:
+            f.write("test message\n")
+
+        filebeat = self.start_beat()
+        self.wait_until(lambda: self.output_has(lines=1))
+        filebeat.check_kill_and_wait()
+
+        output = self.read_output()[0]
+        assert "@timestamp" in output
+        assert "prospector.type" in output
