b1e44a55a2bef68e9621b54d9222951c77181293
Nicolas Ruflin <spam@ruflin.com>
2017-05-22 16:05:21 +0200

    Bring back prospector.type which went missing (#4361)
    It seems in one of the recent refactorings prospector.type went missing. Test added to make sure it will not go missing again.

--- filebeat/harvester/forwarder.go
+++ filebeat/harvester/forwarder.go
@@ -67,6 +67,7 @@ func (f *Forwarder) Send(data *util.Data) error {
 
 	if data.HasEvent() {
 		data.Event[common.EventMetadataKey] = f.Config.EventMetadata
+		data.Event.Put("prospector.type", f.Config.Type)
 
 		// run the filters before sending to spooler
 		data.Event = f.Processors.Run(data.Event)
