b4b3b4248cdb87afd929aa04e827f0c9d9e4574f
sudhindra.s <sudhindra.s@media.net>
2017-05-29 18:21:52 +0530

    fixed test files

--- packetbeat/tests/system/test_0064_graphite.py
+++ packetbeat/tests/system/test_0064_graphite.py
@@ -2,46 +2,40 @@ from packetbeat import BaseTest
 
 
 class Test(BaseTest):
-	"""
-	Basic Graphite Tests
-	"""
+    """
+    Basic Graphite Tests
+    """
 
-	def test_line_protocol(self):
-		"""
-		Should correctly pass for cases where the 
-		input follows line protocol
-		"""
+    def test_line_protocol(self):
+        """
+        Should correctly pass for cases where the
+        input follows line protocol
+        """
 
-		self.render_config_template(
-			graphite_ports=[2003]
-		)
+        self.render_config_template(
+            graphite_ports=[2003]
+        )
 
-		self.run_packetbeat(pcap="graphite_line_test.pcap")
+        self.run_packetbeat(pcap="graphite_line_test.pcap")
 
-		objs = self.read_output()
+        objs = self.read_output()
 
-		assert all([o["type"] == "graphite" for o in objs])
-		assert all([o["bytes_out"] == 0 for o in objs])
+        assert all([o["type"] == "graphite" for o in objs])
+        assert all([o["bytes_out"] == 0 for o in objs])
 
-	def test_pickle_protocol(self):
-		"""
-		Should correctly pass for cases where the 
-		input follows pickle protocol
-		"""
+    def test_pickle_protocol(self):
+        """
+        Should correctly pass for cases where the
+        input follows pickle protocol
+        """
 
-		self.render_config_template(
-			graphite_ports=[2003]
-		)
+        self.render_config_template(
+            graphite_ports=[2003]
+        )
 
-		self.run_packetbeat(pcap="graphite_pickle_test.pcap")
+        self.run_packetbeat(pcap="graphite_pickle_test.pcap")
 
-		objs = self.read_output()
+        objs = self.read_output()
 
-		assert all([o["type"] == "graphite" for o in objs])
-		assert all([o["bytes_out"] == 0 for o in objs])
-
-
-
-		
-
-
+        assert all([o["type"] == "graphite" for o in objs])
+        assert all([o["bytes_out"] == 0 for o in objs])
