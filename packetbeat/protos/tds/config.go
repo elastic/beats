c560748b2e5e818407f39cb359e1623c3636b046
sudhindra.s <sudhindra.s@media.net>
2017-05-30 20:02:32 +0530

    fixed errors final

--- packetbeat/protos/tds/config.go
+++ packetbeat/protos/tds/config.go
@@ -1,22 +1,22 @@
-package tds
+// package tds
 
-import (
-	"github.com/elastic/beats/packetbeat/config"
-	"github.com/elastic/beats/packetbeat/protos"
-)
+// import (
+// 	"github.com/elastic/beats/packetbeat/config"
+// 	"github.com/elastic/beats/packetbeat/protos"
+// )
 
-type tdsConfig struct {
-	config.ProtocolCommon `config: ",inline"`
-	MaxRowLength          int `config: "max_row_length"`
-	MaxRows               int `config: "max_rows"`
-}
+// type tdsConfig struct {
+// 	config.ProtocolCommon `config: ",inline"`
+// 	MaxRowLength          int `config: "max_row_length"`
+// 	MaxRows               int `config: "max_rows"`
+// }
 
-var (
-	defaultConfig = tdsConfig{
-		ProtocolCommon: config.ProtocolCommon{
-			TransactionTimeout: protos.DefaultTransactionExpiration,
-		},
-		MaxRowLength: 1024,
-		MaxRows:      10,
-	}
-)
+// var (
+// 	defaultConfig = tdsConfig{
+// 		ProtocolCommon: config.ProtocolCommon{
+// 			TransactionTimeout: protos.DefaultTransactionExpiration,
+// 		},
+// 		MaxRowLength: 1024,
+// 		MaxRows:      10,
+// 	}
+// )
