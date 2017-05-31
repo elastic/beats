fc7860ee83cf2e3050ed17956d9c8a92e0440ff5
sudhindra.s <sudhindra.s@media.net>
2017-05-31 13:58:48 +0530

    added test files

---
+++ packetbeat/tests/system/test_0065_tds.py
@@ -0,0 +1,28 @@
+from packetbeat import BaseTest
+
+
+class Test(BaseTest):
+    """
+    Basic Graphite Tests
+    """
+
+    def test_tds_protocol(self):
+        """
+        Should correctly pass for cases where the
+        input follows tds protocol
+        """
+
+        self.render_config_template(
+            tds_ports=[1433]
+        )
+
+        self.run_packetbeat(pcap="mssql.pcap")
+
+        objs = self.read_output()
+
+        assert all([o["type"] == "tds" for o in objs])
+        #assert all([o["bytes_in"] > 0 for o in objs])
+        #assert all([o["bytes_out"] > 0 for o in objs])
+        assert all([o["port"] == 1433 for o in objs])
+        # print objs[0]["request"]
+        # print "here"
