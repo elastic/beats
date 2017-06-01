ab548fbb4e889591941d41b12a57ceb58edabbda
sudhindra.s <sudhindra.s@media.net>
2017-06-01 17:12:05 +0530

    fix test files

--- packetbeat/tests/system/test_0065_tds.py
+++ packetbeat/tests/system/test_0065_tds.py
@@ -16,13 +16,13 @@ class Test(BaseTest):
             tds_ports=[1433]
         )
 
-        self.run_packetbeat(pcap="mssql.pcap")
+        self.run_packetbeat(pcap="tds.pcap")
 
         objs = self.read_output()
 
         assert all([o["type"] == "tds" for o in objs])
-        #assert all([o["bytes_in"] > 0 for o in objs])
-        #assert all([o["bytes_out"] > 0 for o in objs])
+        # assert all([o["bytes_in"] > 0 for o in objs])
+        # assert all([o["bytes_out"] > 0 for o in objs])
         assert all([o["port"] == 1433 for o in objs])
-        # print objs[0]["request"]
-        # print "here"
+
+        # assert objs[0]["request"] == ""  
