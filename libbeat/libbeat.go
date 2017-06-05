1c916263668943d8870ecfeb4f6f6a533c2077fe
Carlos Pérez-Aradros Herce <exekias@gmail.com>
2017-06-05 17:43:03 +0200

    Update beat generator to follow new subcommands structure

--- libbeat/libbeat.go
+++ libbeat/libbeat.go
@@ -3,17 +3,11 @@ package main
 import (
 	"os"
 
-	"github.com/spf13/cobra"
-
 	"github.com/elastic/beats/libbeat/cmd"
 	"github.com/elastic/beats/libbeat/mock"
 )
 
-var RootCmd *cobra.Command
-
-func init() {
-	RootCmd = cmd.GenRootCmd(mock.Name, mock.Version, mock.New)
-}
+var RootCmd = cmd.GenRootCmd(mock.Name, mock.Version, mock.New)
 
 func main() {
 	if err := RootCmd.Execute(); err != nil {
