fab03dcada0923b9d7b61ac8a33729671391ffa6
Carlos Pérez-Aradros Herce <exekias@gmail.com>
2017-06-15 04:50:01 +0200

    Rename `kubernetes` processor -> `add_kubernetes_metadata` (#4473)
    * Rename `kubernetes` processor -> `add_kubernetes_metadata`
    
    * Update CHANGELOG

--- filebeat/beater/filebeat.go
+++ filebeat/beater/filebeat.go
@@ -20,7 +20,7 @@ import (
 	"github.com/elastic/beats/filebeat/spooler"
 
 	// Add filebeat level processors
-	_ "github.com/elastic/beats/filebeat/processor/kubernetes"
+	_ "github.com/elastic/beats/filebeat/processor/add_kubernetes_metadata"
 )
 
 var (
