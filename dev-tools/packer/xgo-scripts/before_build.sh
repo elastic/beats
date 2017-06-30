3cbf94aec3ba7cd0c0cf4eb7f0f2ed76d95ebd3e
Tudor Golubenco <tudor@packetbeat.com>
2017-06-30 10:33:54 +0200

    Remove import_dashboards from packaging

--- dev-tools/packer/xgo-scripts/before_build.sh
+++ dev-tools/packer/xgo-scripts/before_build.sh
@@ -37,15 +37,6 @@ PREFIX=$PREFIX make before-build
 mkdir -p $PREFIX/homedir
 make install-home HOME_PREFIX=$PREFIX/homedir
 
-# Build dashboards
-for TARGET in $TARGETS; do
-	echo "Compiling import_dashboards for $TARGET"
-	XGOOS=`echo $TARGET | cut -d '/' -f 1`
-	XGOARCH=`echo $TARGET | cut -d '/' -f 2`
-
-	GOOS=$XGOOS GOARCH=$XGOARCH go build -ldflags "-X main.beat=${BEAT_NAME}" -o $PREFIX/import_dashboards-$XGOOS-$XGOARCH ${ES_BEATS}/dev-tools/cmd/import_dashboards/import_dashboards.go
-done
-
 if [ -n "BUILDID" ]; then
     echo "$BUILDID" > $PREFIX/homedir/.build_hash.txt
 fi
