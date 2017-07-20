25ac1eda9dcc3b1b0d7dfa7992b6bb52e70bc2c6
DeDe Morton <dede.morton@elastic.co>
2017-07-20 15:28:02 -0700

    Add auditbeat docs (#4697)
    * Add auditbeat docs
    
    * Update gs instructions and make misc style changes
    
    * Add docs for file metricset
    
    * Add fixes from prelim review
    
    * Set release-state to unreleased

--- auditbeat/module/audit/doc.go
+++ auditbeat/module/audit/doc.go
@@ -1,4 +1,4 @@
-// Package audit is a Metricbeat module that reports security-relevant
+// Package audit is an Auditbeat module that reports security-relevant
 // information based on data captured from the operating system (OS) or services
 // running on the OS.
 package audit
