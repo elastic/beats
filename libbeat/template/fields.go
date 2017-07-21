f58a439e247d8df9f466216a2444a62ec367d39e
Nicolas Ruflin <spam@ruflin.com>
2017-07-21 12:10:48 +0200

    Add HasKey method to Fields (#4730)
    This can be used for checks if a specific key exists in a Fields array. This becomes useful for checking if all fields are documented for example.

--- libbeat/template/fields.go
+++ libbeat/template/fields.go
@@ -1,6 +1,10 @@
 package template
 
-import "github.com/elastic/beats/libbeat/common"
+import (
+	"strings"
+
+	"github.com/elastic/beats/libbeat/common"
+)
 
 var (
 	defaultType = "keyword"
@@ -59,4 +63,39 @@ func (f Fields) process(path string, esVersion common.Version) common.MapStr {
 	}
 
 	return output
+}
+
+// HasKey checks if inside fields the given key exists
+// The key can be in the form of a.b.c and it will check if the nested field exist
+// In case the key is `a` and there is a value `a.b` false is return as it only
+// returns true if it's a leave node
+func (f Fields) HasKey(key string) bool {
+	keys := strings.Split(key, ".")
+	return f.hasKey(keys)
+}
+
+func (f Fields) hasKey(keys []string) bool {
+	// Nothing to compare anymore
+	if len(keys) == 0 {
+		return false
+	}
+
+	key := keys[0]
+	keys = keys[1:]
+
+	for _, field := range f {
+		if field.Name == key {
+
+			if len(field.Fields) > 0 {
+				return field.Fields.hasKey(keys)
+			}
+			// Last entry in the tree but still more keys
+			if len(keys) > 0 {
+				return false
+			}
+
+			return true
+		}
+	}
+	return false
 }
