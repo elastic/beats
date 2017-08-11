386c9e0f7da55f075eadf360b26f504a1788f684
Nicolas Ruflin <spam@ruflin.com>
2017-08-11 12:52:41 +0200

    Update build_docs.sh script to support dev-guide (#4873)
    Currently the dev-guide is not build as part of `make docs`. This adjusments to the scripts and Makefile add the dev-guide and offer more flexibility for docs build also for community beats.
    
    The script was also moved out of libbeat to the global script directory as it does not directly belong to libbeat.

---
+++ script/build_docs.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+
+name=$1
+path=$2
+build_dir=$3
+
+docs_dir=$build_dir/docs
+html_dir=$build_dir/html_docs
+
+# Checks if docs clone already exists
+if [ ! -d $docs_dir ]; then
+    # Only head is cloned
+    git clone --depth=1 https://github.com/elastic/docs.git $docs_dir
+else
+    echo "$docs_dir already exists. Not cloning."
+fi
+
+
+index="${GOPATH%%:*}/src/${path}/index.asciidoc"
+
+if [ -f "$index" ]; then
+  echo "Building docs for ${name}..."
+  dest_dir="$html_dir/${name}"
+  mkdir -p "$dest_dir"
+  params=""
+  if [ "$PREVIEW" = "1" ]; then
+    params="--chunk=1 -open chunk=1 -open"
+  fi
+  $docs_dir/build_docs.pl $params --doc "$index" -out "$dest_dir"
+fi
