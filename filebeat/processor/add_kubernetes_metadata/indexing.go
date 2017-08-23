c811edd9e7d46edcb38f09ace8b9aaf264642fd5
Sven Woltmann <sven.woltmann@gmail.com>
2017-08-23 18:51:10 +0200

    add_kubernetes_metadata processor supports '/var/log/containers/' log path
    The add_kubernetes_metadata processor's LogPathMatcher could extract
    a Docker container ID - and hence enrich a log document with Kubernetes
    metadata - only if the log path was '/var/lib/docker/containers/'.
    
    With this commit, the LogPathMatcher can extract the container ID also
    from a '/var/log/containers/' log path (Kubernetes symlinks).

--- filebeat/processor/add_kubernetes_metadata/indexing.go
+++ filebeat/processor/add_kubernetes_metadata/indexing.go
@@ -43,19 +43,31 @@ func newLogsPathMatcher(cfg common.Config) (add_kubernetes_metadata.Matcher, err
 		logPath = logPath + "/"
 	}
 
+	logp.Debug("kubernetes", "logs_path matcher log path: %s", logPath)
+
 	return &LogPathMatcher{LogsPath: logPath}, nil
 }
 
 func (f *LogPathMatcher) MetadataIndex(event common.MapStr) string {
 	if value, ok := event["source"]; ok {
 		source := value.(string)
-		logp.Debug("kubernetes", "Incoming source value: ", source)
+		logp.Debug("kubernetes", "Incoming source value: %s", source)
 		cid := ""
 		if strings.Contains(source, f.LogsPath) {
-			//Docker container is 64 chars in length
-			cid = source[len(f.LogsPath) : len(f.LogsPath)+64]
+			if f.LogsPath == "/var/log/containers/" && strings.HasSuffix(source, ".log") {
+				// In case of the Kubernetes log path "/var/log/containers/",
+				// the container ID will be located right before the ".log" ending.
+				sourceLen := len(source)
+				cid = source[sourceLen-68 : sourceLen-4]
+			} else {
+				// In any other case, we assume the container ID will follow right after the log path.
+				//Docker container is 64 chars in length
+				cid = source[len(f.LogsPath) : len(f.LogsPath)+64]
+			}
+			logp.Debug("kubernetes", "Using container id: %s", cid)
+		} else {
+			logp.Debug("kubernetes", "Error extracting container id - source value does not contain log path.")
 		}
-		logp.Debug("kubernetes", "Using container id: ", cid)
 
 		if cid != "" {
 			return cid
--- filebeat/processor/add_kubernetes_metadata/indexing_test.go
+++ filebeat/processor/add_kubernetes_metadata/indexing_test.go
@@ -30,3 +30,22 @@ func TestLogsPathMatcher(t *testing.T) {
 
 	assert.Equal(t, output, cid)
 }
+
+func TestLogsPathMatcherVarLogContainers(t *testing.T) {
+	var testConfig = common.NewConfig()
+	testConfig.SetString("logs_path", -1, "/var/log/containers/")
+
+	logMatcher, err := newLogsPathMatcher(*testConfig)
+	assert.Nil(t, err)
+
+	cid := "0069869de9adf97f574c62029aeba65d1ecd85a2a112e87fbc28afe4dec2b843"
+	logPath := fmt.Sprintf("/var/log/containers/kube-proxy-4d7nt_kube-system_kube-proxy-%s.log", cid)
+
+	input := common.MapStr{
+		"source": logPath,
+	}
+
+	output := logMatcher.MetadataIndex(input)
+
+	assert.Equal(t, output, cid)
+}
