917ef2de1c3d815ee57d75fc2577bb7aa3a33dca
Tudor Golubenco <tudor@elastic.co>
2017-08-25 15:28:05 +0200

    Delay device opening (#4939) (#5012)
    * Delay device opening
    
    Reimplement the sniffer to delay:
    - opening the sniffing device until sniffer.Run is executed.
    - do some preliminary config validation on New only
    - split file handling into separate sniffer handle type, to clean up the
      run loop a little
    
    * fix pcap file close handling
    
    * always sleep for a few ms on pcap file close
    
    * Reduce chance for races on publisher being closed on shutdown
    
    * Fix flows setup being too late
    
    * reduce flow timeout in system tests
    
    * replace sleep in file pcap reader with default setting in test config file
    
    (cherry picked from commit be011f333d269f03a0213d858b0e95a78a8e3603)

---
+++ packetbeat/sniffer/afpacket.go
@@ -0,0 +1,27 @@
+package sniffer
+
+import "fmt"
+
+// Computes the block_size and the num_blocks in such a way that the
+// allocated mmap buffer is close to but smaller than target_size_mb.
+// The restriction is that the block_size must be divisible by both the
+// frame size and page size.
+func afpacketComputeSize(targetSizeMb int, snaplen int, pageSize int) (
+	frameSize int, blockSize int, numBlocks int, err error) {
+
+	if snaplen < pageSize {
+		frameSize = pageSize / (pageSize / snaplen)
+	} else {
+		frameSize = (snaplen/pageSize + 1) * pageSize
+	}
+
+	// 128 is the default from the gopacket library so just use that
+	blockSize = frameSize * 128
+	numBlocks = (targetSizeMb * 1024 * 1024) / blockSize
+
+	if numBlocks == 0 {
+		return 0, 0, 0, fmt.Errorf("Buffer size too small")
+	}
+
+	return frameSize, blockSize, numBlocks, nil
+}
