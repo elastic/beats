8d6de7d25451d6b6bda1985f4feb5e63f678dc22
Steffen Siering <steffen.siering@elastic.co>
2017-09-12 14:45:12 +0200

    Reorder processors in publisher pipeline (#5149)
    * Reorder processors pipeline order
    
    - reorder processors, such that all client processors run before the
      global processors
    - remove support for EventsMetadataKey
    
    * Combine fields and tags if possible
    
    - combine client internal field updates and configured field updates
      into one dictionary
    - combine proessors for adding client and global fields+tags if no
      client processors are configured
    
    * Do not copy if no processors are defined
    
    Do not Clone (deep copy) fields being added in the pipeline if no
    processors are configured.
    
    As processors might add/remove fields and potentially modify shared
    field objects, these must be copied if there is a chance global shared
    structured being overwritten by processors. Especially if processors are
    guarded by conditions.
    
    * Fix processors order once again
    
    Problem is local fields+tags must be applied after the globaly
    configured fields and tests, while client processors must be run before
    the global ones.
    
    * review
    
    - use 'disabled' flag
    - simplify pipeline fields init
    
    (cherry picked from commit 60826036b13358b3189c53f4f2d99eda01489cc7)

--- libbeat/publisher/pipeline/pipeline.go
+++ libbeat/publisher/pipeline/pipeline.go
@@ -60,10 +60,13 @@ type pipelineProcessors struct {
 	// The pipeline its processor settings for
 	// constructing the clients complete processor
 	// pipeline on connect.
-	beatMetaProcessor  beat.Processor
-	eventMetaProcessor beat.Processor
-	processors         beat.Processor
-	disabled           bool // disabled is set if outputs have been disabled via CLI
+	beatsMeta common.MapStr
+	fields    common.MapStr
+	tags      []string
+
+	processors beat.Processor
+
+	disabled bool // disabled is set if outputs have been disabled via CLI
 }
 
 // Settings is used to pass additional settings to a newly created pipeline instance.
@@ -132,39 +135,17 @@ func New(
 	out outputs.Group,
 	settings Settings,
 ) (*Pipeline, error) {
-	annotations := settings.Annotations
 	var err error
 
-	var beatMeta beat.Processor
-	if meta := annotations.Beat; meta != nil {
-		beatMeta = beatAnnotateProcessor(meta)
-	}
-
-	var eventMeta beat.Processor
-	if em := annotations.Event; len(em.Fields) > 0 || len(em.Tags) > 0 {
-		eventMeta = eventAnnotateProcessor(em)
-	}
-
-	var prog beat.Processor
-	if ps := settings.Processors; ps != nil && len(ps.List) > 0 {
-		tmp := &program{title: "global"}
-		for _, p := range ps.List {
-			tmp.add(p)
-		}
-		prog = tmp
-	}
-
 	log := defaultLogger
+	annotations := settings.Annotations
+	processors := settings.Processors
+	disabledOutput := settings.Disabled
 	p := &Pipeline{
 		logger:           log,
 		waitCloseMode:    settings.WaitCloseMode,
 		waitCloseTimeout: settings.WaitClose,
-		processors: pipelineProcessors{
-			beatMetaProcessor:  beatMeta,
-			eventMetaProcessor: eventMeta,
-			processors:         prog,
-			disabled:           settings.Disabled,
-		},
+		processors:       makePipelineProcessors(annotations, processors, disabledOutput),
 	}
 	p.ackBuilder = &pipelineEmptyACK{p}
 	p.ackActive = atomic.MakeBool(true)
@@ -381,4 +362,39 @@ func (e *waitCloser) dec(n int) {
 
 func (e *waitCloser) wait() {
 	e.events.Wait()
+}
+
+func makePipelineProcessors(
+	annotations Annotations,
+	processors *processors.Processors,
+	disabled bool,
+) pipelineProcessors {
+	p := pipelineProcessors{
+		disabled: disabled,
+	}
+
+	hasProcessors := processors != nil && len(processors.List) > 0
+	if hasProcessors {
+		tmp := &program{title: "global"}
+		for _, p := range processors.List {
+			tmp.add(p)
+		}
+		p.processors = tmp
+	}
+
+	if meta := annotations.Beat; meta != nil {
+		p.beatsMeta = common.MapStr{"beat": meta}
+	}
+
+	if em := annotations.Event; len(em.Fields) > 0 {
+		fields := common.MapStr{}
+		common.MergeFields(fields, em.Fields.Clone(), em.FieldsUnderRoot)
+		p.fields = fields
+	}
+
+	if t := annotations.Event.Tags; len(t) > 0 {
+		p.tags = t
+	}
+
+	return p
 }
