73cbf065ef49cd458454328672ec217de2ea0012
berfinsari <berfinsari21@gmail.com>
2017-09-14 14:20:31 +0300

    add etcd module with self metricset

---
+++ metricbeat/module/etcd/self/self.go
@@ -0,0 +1,54 @@
+package self
+
+import (
+	"github.com/elastic/beats/libbeat/common"
+	"github.com/elastic/beats/libbeat/common/cfgwarn"
+	"github.com/elastic/beats/metricbeat/helper"
+	"github.com/elastic/beats/metricbeat/mb"
+	"github.com/elastic/beats/metricbeat/mb/parse"
+)
+
+const (
+	defaultScheme = "http"
+	defaultPath   = "/v2/stats/self"
+)
+
+var (
+	hostParser = parse.URLHostParserBuilder{
+		DefaultScheme: defaultScheme,
+		DefaultPath:   defaultPath,
+	}.Build()
+)
+
+func init() {
+	if err := mb.Registry.AddMetricSet("etcd", "self", New, hostParser); err != nil {
+		panic(err)
+	}
+}
+
+type MetricSet struct {
+	mb.BaseMetricSet
+	http *helper.HTTP
+}
+
+func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
+	cfgwarn.Experimental("The etcd self metricset is experimental")
+	config := struct{}{}
+
+	if err := base.Module().UnpackConfig(&config); err != nil {
+		return nil, err
+	}
+
+	return &MetricSet{
+		base,
+		helper.NewHTTP(base),
+	}, nil
+}
+
+func (m *MetricSet) Fetch() (common.MapStr, error) {
+	content, err := m.http.FetchContent()
+	if err != nil {
+		return nil, err
+	}
+	return eventMapping(content), nil
+}
