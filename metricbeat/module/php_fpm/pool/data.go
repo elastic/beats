20038a3725ce101ae54b7b0ccbacc5eccbcc893f
Gilad Peleg <giladp007@gmail.com>
2017-09-29 08:59:50 +0300

    Enhance php_fpm metric beat with more kpis

--- metricbeat/module/php_fpm/pool/data.go
+++ metricbeat/module/php_fpm/pool/data.go
@@ -8,14 +8,24 @@ import (
 var (
 	schema = s.Schema{
 		"name": c.Str("pool"),
+		"process_manager": c.Str("process manager"),
+		"slow_requests": c.Int("slow requests"),
 		"connections": s.Object{
 			"accepted": c.Int("accepted conn"),
-			"queued":   c.Int("listen queue"),
+			"max_queue_limit": c.Int("listen queue len"),
+			"max_queued": c.Int("max listen queue"),
+			"queued": c.Int("listen queue"),
 		},
 		"processes": s.Object{
-			"idle":   c.Int("idle processes"),
 			"active": c.Int("active processes"),
+			"idle":   c.Int("idle processes"),
+			"max_active": c.Int("max active processes"),
+			"max_children": c.Int("max children reached"),
+			"total": c.Int("total processes"),
 		},
-		"slow_requests": c.Int("slow requests"),
+		"start": s.Object{
+			"since": c.Int("start since"),
+			"time": c.Int("start time"),
+		},
 	}
 )
