b6e4fed0f092d236627dab3fbc4605faa8404952
Carlos Pérez-Aradros Herce <exekias@gmail.com>
2017-10-24 14:25:55 +0200

    Update `beta` messages for config reload (#5202) (#5428)
    (cherry picked from commit bc84970df6de1e4017cdd1718358f13c60cae92d)

--- filebeat/crawler/crawler.go
+++ filebeat/crawler/crawler.go
@@ -11,7 +11,6 @@ import (
 	"github.com/elastic/beats/filebeat/registrar"
 	"github.com/elastic/beats/libbeat/cfgfile"
 	"github.com/elastic/beats/libbeat/common"
-	"github.com/elastic/beats/libbeat/common/cfgwarn"
 	"github.com/elastic/beats/libbeat/logp"
 )
 
@@ -53,8 +52,6 @@ func (c *Crawler) Start(r *registrar.Registrar, configProspectors *common.Config
 	}
 
 	if configProspectors.Enabled() {
-		cfgwarn.Beta("Loading separate prospectors is enabled.")
-
 		c.prospectorsReloader = cfgfile.NewReloader(configProspectors)
 		prospectorsFactory := prospector.NewFactory(c.out, r, c.beatDone)
 		if err := c.prospectorsReloader.Check(prospectorsFactory); err != nil {
@@ -67,8 +64,6 @@ func (c *Crawler) Start(r *registrar.Registrar, configProspectors *common.Config
 	}
 
 	if configModules.Enabled() {
-		cfgwarn.Beta("Loading separate modules is enabled.")
-
 		c.modulesReloader = cfgfile.NewReloader(configModules)
 		modulesFactory := fileset.NewFactory(c.out, r, c.beatVersion, pipelineLoaderFactory, c.beatDone)
 		if err := c.modulesReloader.Check(modulesFactory); err != nil {
