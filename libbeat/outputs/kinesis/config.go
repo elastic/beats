d1996cfc8861200b17065953e8c8c683e7ff8ff2
Sherif Ragab <sherif@improbable.io>
2017-10-31 10:33:19 +0000

    Adding Kinesis output plugin.

---
+++ libbeat/outputs/kinesis/config.go
@@ -0,0 +1,37 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+package kinesisout
+
+import (
+	"errors"
+
+	//"github.com/elastic/beats/libbeat/outputs/codec"
+	"github.com/aws/aws-sdk-go/aws"
+)
+
+
+type config struct {
+	Region          string           `config:"region"`
+	StreamName      string           `config:"stream"`
+	PartitionKey    string           `config:"partition"`
+	MaxRetries      int              `config:"retries"`
+	Endpoint        string           `config:"endpoint"`
+	LogLevel        aws.LogLevelType `config:"log"`
+	DisableSSL      bool             `config:"nossl"`
+	AccessKeyID     string           `config:"access_key_id"`
+	SecretAccessKey string           `config:"secret_access_key"`
+}
+
+var defaultConfig = config{
+	Region: "eu-west-1",
+	PartitionKey: "static",
+}
+
+func (c *config) Validate() error {
+	keyProvided := (c.AccessKeyID != "")
+	secretProvided := (c.SecretAccessKey != "")
+	if keyProvided != secretProvided {
+		return errors.New("Must supply both a key and a secret, or neither to pass credentials via the environment")
+	}
+	return nil
+}
---
+++ libbeat/outputs/kinesis/kinesis_test.go
@@ -0,0 +1,5 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+// +build !integration
+
+package kinesisout
