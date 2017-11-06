df0b8b609db0808fa6f7db78209ca784ca473d57
liketic <ke.like@outlook.com>
2017-11-06 13:09:35 +0800

    Avoid double slash when join url and path (#5503)

--- libbeat/outputs/elasticsearch/client.go
+++ libbeat/outputs/elasticsearch/client.go
@@ -84,7 +84,7 @@ var (
 	errExpectedItemObject    = errors.New("expected item response object")
 	errExpectedStatusCode    = errors.New("expected item status code")
 	errUnexpectedEmptyObject = errors.New("empty object")
-	errExcpectedObjectEnd    = errors.New("expected end of object")
+	errExpectedObjectEnd     = errors.New("expected end of object")
 	errTempBulkFailure       = errors.New("temporary bulk send failure")
 )
 
@@ -505,7 +505,7 @@ func itemStatus(reader *jsonReader) (int, []byte, error) {
 		return 0, nil, err
 	}
 	if kind != dictEnd {
-		err = errExcpectedObjectEnd
+		err = errExpectedObjectEnd
 		logp.Err("Failed to parse bulk response item: %s", err)
 		return 0, nil, err
 	}
