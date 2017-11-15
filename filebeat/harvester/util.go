08c92c77b2ae526b73bc142e4846d2213f2e33ad
Nicolas Ruflin <spam@ruflin.com>
2017-11-16 00:17:08 +1100

    Remove prospector validation as not needed anymore (#5537)
    Since the prospector are now registered through init, the validation is not needed anymore as in case a prospector type does not exist, and error is returned anyway. The harvester types are still needed because several harvesters use the log harvester.

--- filebeat/harvester/util.go
+++ filebeat/harvester/util.go
@@ -10,14 +10,6 @@ const (
 	UdpType   = "udp"
 )
 
-// ValidType of valid input types
-var ValidType = map[string]struct{}{
-	StdinType: {},
-	LogType:   {},
-	RedisType: {},
-	UdpType:   {},
-}
-
 // MatchAny checks if the text matches any of the regular expressions
 func MatchAny(matchers []match.Matcher, text string) bool {
 	for _, m := range matchers {
