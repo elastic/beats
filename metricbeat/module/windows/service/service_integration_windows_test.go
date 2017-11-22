3d836234fbc16c86c084c8e34fbbedc9d95d0559
Martin Scholz <martin.scholz83@outlook.com>
2017-11-22 12:27:50 +0100

    Refactor integration test

--- metricbeat/module/windows/service/service_integration_windows_test.go
+++ metricbeat/module/windows/service/service_integration_windows_test.go
@@ -5,8 +5,16 @@ package service
 import (
 	"testing"
 
+	"github.com/StackExchange/wmi"
+	"github.com/stretchr/testify/assert"
+
 	mbtest "github.com/elastic/beats/metricbeat/mb/testing"
 )
+
+type Win32Service struct {
+	Name      string
+	ProcessId uint32
+}
 
 func TestData(t *testing.T) {
 	config := map[string]interface{}{
@@ -28,12 +36,37 @@ func TestReadService(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	var wmiSrc []Win32Service
+
+	// Get services per WMI
+	err = wmi.Query("SELECT * FROM Win32_Service ", &wmiSrc)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Get services per windows module
 	services, err := reader.Read()
 	if err != nil {
 		t.Fatal(err)
 	}
 
+	//Compare them
 	for _, s := range services {
-		t.Log(s)
+		// Look if the service is in the wmi src
+		var found bool
+		for _, w := range wmiSrc {
+			if w.Name == s["name"] {
+				if s["pid"] != nil {
+					assert.Equal(t, w.ProcessId, s["pid"])
+				}
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			// Service is not in the wmi query
+			t.Errorf("Service %s can not be found by wmi query", s["name"])
+		}
 	}
 }
