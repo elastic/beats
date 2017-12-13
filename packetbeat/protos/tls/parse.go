e06cee767c886dd6e7775551b70bdf6339fc6da0
Adrian Serrano <adrisr83@gmail.com>
2017-12-13 13:20:18 +0100

    TLS certificates include public key size (#5804)
    This adds a `public_key_size` entry to certificates that contains
    the number of bits in the public key. RSA, DSA and ECDSA public keys
    are supported. The value of this field is zero if the key size
    cannot be determined.

--- packetbeat/protos/tls/parse.go
+++ packetbeat/protos/tls/parse.go
@@ -1,6 +1,9 @@
 package tls
 
 import (
+	"crypto/dsa"
+	"crypto/ecdsa"
+	"crypto/rsa"
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/hex"
@@ -488,6 +491,35 @@ func (version tlsVersion) String() string {
 	return fmt.Sprintf("%d.%d", version.major, version.minor)
 }
 
+func getKeySize(key interface{}) int {
+	if key == nil {
+		return 0
+	}
+	switch pubKey := key.(type) {
+	case *rsa.PublicKey:
+		if n := pubKey.N; n != nil {
+			return n.BitLen()
+		}
+
+	case *dsa.PublicKey:
+		if p := pubKey.Parameters.P; p != nil {
+			return p.BitLen()
+		}
+		if y := pubKey.Y; y != nil {
+			return y.BitLen()
+		}
+
+	case *ecdsa.PublicKey:
+		if params := pubKey.Params(); params != nil {
+			return params.BitSize
+		}
+		if y := pubKey.Y; y != nil {
+			return y.BitLen()
+		}
+	}
+	return 0
+}
+
 func certToMap(cert *x509.Certificate, includeRaw bool) common.MapStr {
 	certMap := common.MapStr{
 		"signature_algorithm":  cert.SignatureAlgorithm.String(),
@@ -498,6 +530,9 @@ func certToMap(cert *x509.Certificate, includeRaw bool) common.MapStr {
 		"subject":              toMap(&cert.Subject),
 		"not_before":           cert.NotBefore,
 		"not_after":            cert.NotAfter,
+	}
+	if keySize := getKeySize(cert.PublicKey); keySize > 0 {
+		certMap["public_key_size"] = keySize
 	}
 	san := make([]string, 0, len(cert.DNSNames)+len(cert.IPAddresses)+len(cert.EmailAddresses))
 	san = append(append(san, cert.DNSNames...), cert.EmailAddresses...)
