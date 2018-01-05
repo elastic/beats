ec26953c58d691aa07307fe11fc334c4eb463cef
Nicolas Ruflin <spam@ruflin.com>
2018-01-05 17:04:09 +1000

    Dedot keys for jolokia metricsets (#5957)
    This should prevent name collisions and allow filtering on these fields. The feature is enabled by default and cannot be disable as otherwise ingestion could stop as Elasticsearch is returning an error. Currently not config option is provided as this should not be disabled.
    
    * Introduces DeDot function in common package
    * Adds tests to jmx for new functionality
    * Use new function also for docker labels
    
    Related to https://github.com/elastic/beats/issues/5942
    Closes https://github.com/elastic/beats/pull/5916

--- metricbeat/module/docker/helper.go
+++ metricbeat/module/docker/helper.go
@@ -54,7 +54,7 @@ func DeDotLabels(labels map[string]string) common.MapStr {
 	for k, v := range labels {
 		// This is necessary so that ES does not interpret '.' fields as new
 		// nested JSON objects, and also makes this compatible with ES 2.x.
-		label := strings.Replace(k, ".", "_", -1)
+		label := common.DeDot(k)
 		outputLabels.Put(label, v)
 	}
 
