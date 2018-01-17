786e080a6f73b16e416f6ae3bd5d6720b06f5935
Nicolas Ruflin <spam@ruflin.com>
2018-01-17 14:00:07 +1100

    Add config option for http endpoint (#6088)

--- libbeat/api/server.go
+++ libbeat/api/server.go
@@ -15,7 +15,7 @@ import (
 
 // Start starts the metrics api endpoint on the configured host and port
 func Start(cfg *common.Config, info beat.Info) {
-	cfgwarn.Beta("Metrics endpoint is enabled.")
+	cfgwarn.Experimental("Metrics endpoint is enabled.")
 	config := DefaultConfig
 	cfg.Unpack(&config)
 
