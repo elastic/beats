a1b6989c5af0f068938a71455c1a227f06dca6fe
Noémi Ványi <kvch@users.noreply.github.com>
2018-01-25 19:35:09 +0100

    New metrics: CPU ticks && CPU time (#6097) (#6178)
    * libbeat: add GetOwnResourceUsageTimeInMillis
    
    Support getting CPU usage time from `getrusage`.
    
    * libbeat: add CPU time and CPU ticks metrics
    
    (cherry picked from commit 4a4e33d3ea7ea207de96506a6935e5a860c1b791)

--- libbeat/metric/system/process/process.go
+++ libbeat/metric/system/process/process.go
@@ -59,6 +59,13 @@ type Stats struct {
 	envRegexps  []match.Matcher // List of regular expressions used to whitelist env vars.
 }
 
+// Ticks of CPU for a process
+type Ticks struct {
+	User   uint64
+	System uint64
+	Total  uint64
+}
+
 // newProcess creates a new Process object and initializes it with process
 // state information. If the process's command line and environment variables
 // are known they should be passed in to avoid re-fetching the information.
@@ -225,6 +232,20 @@ func getProcState(b byte) string {
 		return "zombie"
 	}
 	return "unknown"
+}
+
+// GetOwnResourceUsageTimeInMillis return the user and system CPU usage time in milliseconds
+func GetOwnResourceUsageTimeInMillis() (int64, int64, error) {
+	r := sigar.Rusage{}
+	err := r.Get(0)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	uTime := int64(r.Utime / time.Millisecond)
+	sTime := int64(r.Stime / time.Millisecond)
+
+	return uTime, sTime, nil
 }
 
 func (procStats *Stats) getProcessEvent(process *Process) common.MapStr {
