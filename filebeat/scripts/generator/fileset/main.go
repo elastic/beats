407bf14dfdee6f3ec6afd840bc4c8227321328fc
Evgeniy Sokolov <ewgraf@gmail.com>
2018-01-28 11:57:27 +0100

    automatically configure module.yml when add fileset

--- filebeat/scripts/generator/fileset/main.go
+++ filebeat/scripts/generator/fileset/main.go
@@ -11,19 +11,49 @@ import (
 
 func copyTemplatesToDest(templatesPath, name, filesetPath, module, fileset string) error {
 	template := path.Join(templatesPath, name)
+	dest := path.Join(filesetPath, name)
+	return copyTemplate(template, dest, module, fileset)
+}
+
+func readTemplate(template, module, fileset string) ([]byte, error) {
 	c, err := ioutil.ReadFile(template)
 	if err != nil {
-		return err
+		return []byte{}, fmt.Errorf("cannot read template: %v", err)
 	}
 
 	c = bytes.Replace(c, []byte("{module}"), []byte(module), -1)
 	c = bytes.Replace(c, []byte("{fileset}"), []byte(fileset), -1)
 
-	dest := path.Join(filesetPath, name)
+	return c, nil
+}
+
+func copyTemplate(template, dest, module, fileset string) error {
+	c, err := readTemplate(template, module, fileset)
+	if err != nil {
+		return err
+	}
+
 	err = ioutil.WriteFile(dest, c, os.ModePerm)
 	if err != nil {
 		return fmt.Errorf("cannot copy template: %v", err)
 	}
+	return nil
+}
+
+func appendTemplate(template, dest, module, fileset string) error {
+	c, err := readTemplate(template, module, fileset)
+	if err != nil {
+		return err
+	}
+
+	f, err := os.OpenFile(dest, os.O_WRONLY|os.O_APPEND, os.ModePerm)
+	if err == nil {
+		_, err = f.Write(c)
+	}
+	if err != nil {
+		return fmt.Errorf("cannot append template: %v", err)
+	}
+
 	return nil
 }
 
@@ -74,7 +104,15 @@ func generateFileset(module, fileset, modulePath, beatsPath string) error {
 			return err
 		}
 	}
-	return nil
+
+	return addFilesetDashboard(module, fileset, modulePath, beatsPath)
+}
+
+func addFilesetDashboard(module, fileset, modulePath, beatsPath string) error {
+	templatesPath := path.Join(beatsPath, "scripts", "module")
+	template := path.Join(templatesPath, "module-fileset.yml")
+	dest := path.Join(modulePath, "module", module, "module.yml")
+	return appendTemplate(template, dest, module, fileset)
 }
 
 func main() {
@@ -106,5 +144,5 @@ func main() {
 		os.Exit(3)
 	}
 
-	fmt.Println("New module was generated. After setting up Grok pattern in pipeline.json, please generate fields.yml")
+	fmt.Println("New module was generated, please check that module.yml file have proper fileset dashboard settings. After setting up Grok pattern in pipeline.json, please generate fields.yml")
 }
