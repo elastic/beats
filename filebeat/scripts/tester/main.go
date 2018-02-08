a01f5b686b5deb5070ad25eaa384aeb0fbf2bf1d
Noémi Ványi <sitbackandwait@gmail.com>
2018-02-08 19:19:56 +0100

    address review notes

--- filebeat/scripts/tester/main.go
+++ filebeat/scripts/tester/main.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/elastic/beats/filebeat/harvester/encoding"
@@ -16,7 +17,69 @@ import (
 	"github.com/elastic/beats/libbeat/common/match"
 )
 
-func getLogsFromFile(logfile, multiPattern string, multiNegate bool, matchMode string) ([]string, error) {
+func main() {
+	esURL := flag.String("elasticsearch", "http://localhost:9200", "Elasticsearch URL")
+	path := flag.String("pipeline", "", "Path to pipeline")
+	modulesPath := flag.String("modules", "./modules", "Path to modules")
+
+	log := flag.String("log", "", "Single log line to test")
+	logfile := flag.String("logfile", "", "Path to log file")
+
+	multiPattern := flag.String("multiline.pattern", "", "Multiline pattern")
+	multiNegate := flag.Bool("multiline.negate", false, "Multiline negate")
+	multiMode := flag.String("multiline.mode", "before", "Multiline mode")
+	maxBytes := flag.Int("maxbytes", 10485760, "Number of max bytes to be read")
+
+	verbose := flag.Bool("verbose", false, "Call Simulate API with verbose option")
+	simulateVerbose := flag.Bool("simulate.verbose", false, "Print full output of Simulate API with verbose option")
+	flag.Parse()
+
+	if *path == "" {
+		os.Stderr.WriteString("Error: -pipeline is required\n")
+		os.Exit(1)
+	}
+
+	if *log == "" && *logfile == "" {
+		os.Stderr.WriteString("Error: -log or -logs has to be specified\n")
+		os.Exit(1)
+	}
+
+	if *multiPattern != "" && *logfile == "" {
+		os.Stderr.WriteString("Error: -multiline-pattern is set but -logfile is not\n")
+		os.Exit(1)
+	}
+
+	var logs []string
+	var err error
+	if *logfile != "" {
+		logs, err = getLogsFromFile(*logfile, *multiPattern, *multiNegate, *multiMode, *maxBytes)
+		if err != nil {
+			os.Stderr.WriteString(fmt.Sprintf("Error while reading logs from file: %v\n", err))
+			os.Exit(2)
+		}
+	} else {
+		logs = []string{*log}
+	}
+
+	paths, err := getPipelinePath(*path, *modulesPath)
+	if err != nil {
+		os.Stderr.WriteString(err.Error())
+		os.Exit(3)
+	}
+	if len(paths) == 0 {
+		os.Stderr.WriteString("No pipeline file was found\n")
+		os.Exit(3)
+	}
+
+	for _, path := range paths {
+		err = testPipeline(*esURL, path, logs, *verbose, *simulateVerbose)
+		if err != nil {
+			os.Stderr.WriteString(err.Error())
+			os.Exit(4)
+		}
+	}
+}
+func getLogsFromFile(logfile, multiPattern string, multiNegate bool, matchMode string, maxBytes int) ([]string, error) {
 	f, err := os.Open(logfile)
 	if err != nil {
 		return nil, err
@@ -53,7 +116,7 @@ func getLogsFromFile(logfile, multiPattern string, multiNegate bool, matchMode s
 			return nil, err
 		}
 	}
-	r = reader.NewLimit(r, 10485760)
+	r = reader.NewLimit(r, maxBytes)
 
 	var logs []string
 	for {
@@ -65,6 +128,62 @@ func getLogsFromFile(logfile, multiPattern string, multiNegate bool, matchMode s
 	}
 
 	return logs, nil
+}
+
+func getPipelinePath(path, modulesPath string) ([]string, error) {
+	isPipelineFile := strings.HasSuffix(path, ".json")
+	if isPipelineFile {
+		return []string{path}, nil
+	}
+
+	var paths []string
+	stat, err := os.Stat(path)
+	if err != nil {
+		parts := strings.Split(path, "/")
+		if len(parts) != 2 {
+			return nil, fmt.Errorf("Cannot find pipeline in %s\n", path)
+		}
+		module := parts[0]
+		fileset := parts[1]
+
+		pathToPipeline := filepath.Join(modulesPath, module, fileset, "ingest", "pipeline.json")
+		return []string{pathToPipeline}, nil
+	}
+
+	if stat.IsDir() {
+		files, err := ioutil.ReadDir(path)
+		if err != nil {
+			return nil, err
+		}
+		for _, f := range files {
+			isPipelineFile := strings.HasSuffix(f.Name(), ".json")
+			if isPipelineFile {
+				fullPath := filepath.Join(path, f.Name())
+				paths = append(paths, fullPath)
+			}
+		}
+		return paths, nil
+	}
+	return paths, nil
+
+}
+
+func testPipeline(esURL, path string, logs []string, verbose, simulateVerbose bool) error {
+	pipeline, err := readPipeline(path)
+	if err != nil {
+		return fmt.Errorf("Error while reading pipeline: %v\n", err)
+	}
+
+	resp, err := runSimulate(esURL, pipeline, logs, simulateVerbose)
+	if err != nil {
+		return fmt.Errorf("Error while sending request to Elasticsearch: %v\n", err)
+	}
+
+	err = showResp(resp, verbose, simulateVerbose)
+	if err != nil {
+		return fmt.Errorf("Error while reading response from Elasticsearch: %v\n", err)
+	}
+	return nil
 }
 
 func readPipeline(path string) (map[string]interface{}, error) {
@@ -82,7 +201,7 @@ func readPipeline(path string) (map[string]interface{}, error) {
 	return p, nil
 }
 
-func runSimulate(url string, pipeline map[string]interface{}, logs []string) (*http.Response, error) {
+func runSimulate(url string, pipeline map[string]interface{}, logs []string, verbose bool) (*http.Response, error) {
 	var sources []map[string]string
 	for _, l := range logs {
 		s := map[string]string{
@@ -110,65 +229,89 @@ func runSimulate(url string, pipeline map[string]interface{}, logs []string) (*h
 	payload := p.String()
 	client := http.Client{}
 
-	return client.Post(url+"/_ingest/pipeline/_simulate", "application/json", strings.NewReader(payload))
+	simulateURL := url + "/_ingest/pipeline/_simulate"
+	if verbose {
+		simulateURL += "?verbose"
+	}
+
+	return client.Post(simulateURL, "application/json", strings.NewReader(payload))
 }
 
-func showResp(resp *http.Response) {
+func showResp(resp *http.Response, verbose, simulateVerbose bool) error {
 	b := new(bytes.Buffer)
 	b.ReadFrom(resp.Body)
 	var r common.MapStr
-	_ = json.Unmarshal(b.Bytes(), &r)
-	fmt.Println(r.StringToPrint())
+	err := json.Unmarshal(b.Bytes(), &r)
+	if err != nil {
+		return err
+	}
+
+	if verbose {
+		fmt.Println(r.StringToPrint())
+	} else {
+		docErrors, err := getDocErrors(r, simulateVerbose)
+		if err != nil {
+			return err
+		}
+
+		for _, d := range docErrors {
+			fmt.Println(d.StringToPrint())
+		}
+	}
+	return nil
 }
 
-func main() {
-	esURL := flag.String("elasticsearch", "http://localhost:9200", "Elasticsearch URL")
-	path := flag.String("pipeline", "", "Path to pipeline")
-	log := flag.String("log", "", "Single log line to test")
-	logfile := flag.String("logfile", "", "Path to log file")
-	multiPattern := flag.String("multiline.pattern", "", "Multiline pattern")
-	multiNegate := flag.Bool("multiline.negate", false, "Multiline negate")
-	multiMode := flag.String("multiline.mode", "before", "Multiline mode")
-	flag.Parse()
-
-	if *path == "" {
-		fmt.Println("Error: -path is required")
-		os.Exit(1)
+func getDocErrors(r common.MapStr, simulateVerbose bool) ([]common.MapStr, error) {
+	d, err := r.GetValue("docs")
+	if err != nil {
+		return nil, err
 	}
 
-	if *log == "" && *logfile == "" {
-		os.Stderr.WriteString("Error: -log or -logs has to be specified\n")
-		os.Exit(1)
+	docs := d.([]interface{})
+	if simulateVerbose {
+		return getErrorsSimulateVerbose(docs)
 	}
 
-	if *multiPattern != "" && *logfile == "" {
-		os.Stderr.WriteString("Error: -multiline-pattern is set but -logfile is not\n")
-		os.Exit(1)
-	}
+	return getRegularErrors(docs)
+}
 
-	var logs []string
-	var err error
-	if *logfile != "" {
-		logs, err = getLogsFromFile(*logfile, *multiPattern, *multiNegate, *multiMode)
+func getRegularErrors(docs []interface{}) ([]common.MapStr, error) {
+	var errors []common.MapStr
+	for _, d := range docs {
+		dd := d.(map[string]interface{})
+		doc := common.MapStr(dd)
+		hasError, err := doc.HasKey("doc._source.error")
 		if err != nil {
-			os.Stderr.WriteString(fmt.Sprintf("Error while reading logs from file: %v\n", err))
-			os.Exit(2)
+			return nil, err
 		}
-	} else {
-		logs = []string{*log}
-	}
 
-	pipeline, err := readPipeline(*path)
-	if err != nil {
-		os.Stderr.WriteString(fmt.Sprintf("Error while reading pipeline: %v\n", err))
-		os.Exit(2)
+		if hasError {
+			errors = append(errors, doc)
+		}
 	}
+	return errors, nil
+}
 
-	resp, err := runSimulate(*esURL, pipeline, logs)
-	if err != nil {
-		os.Stderr.WriteString(fmt.Sprintf("Error while sending request to Elasticsearch: %v\n", err))
-		os.Exit(2)
+func getErrorsSimulateVerbose(docs []interface{}) ([]common.MapStr, error) {
+	var errors []common.MapStr
+	for _, d := range docs {
+		pr := d.(map[string]interface{})
+		p := common.MapStr(pr)
+
+		rr, err := p.GetValue("processor_results")
+		if err != nil {
+			return nil, err
+		}
+		res := rr.([]interface{})
+		hasError := false
+		for _, r := range res {
+			rres := r.(map[string]interface{})
+			result := common.MapStr(rres)
+			hasError, _ = result.HasKey("error")
+			if hasError {
+				errors = append(errors, p)
+			}
+		}
 	}
-
-	showResp(resp)
+	return errors, nil
 }
