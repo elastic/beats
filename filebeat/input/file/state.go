fa7f861d60b473fd87531d2b72971be23ffefcc5
urso <steffen.siering@elastic.co>
2018-02-09 16:43:19 +0100

    Filebeat in memory states registry improvements
    - make States type easier to find by moving into separate states.go file
    - Ensure provides States constructor is actually used
    - Add ID->array index, index for faster lookups on update and find
      operations. When updating states in a big registry, the Updates
      converged to quadratic complexity. The index helps in keeping the
      complexity about linear in number of state updates.
    - Debug will print number of states subject to future cleanups (if state TTL > 0)
    - Add title to states unit tests

--- filebeat/input/file/state.go
+++ filebeat/input/file/state.go
@@ -2,11 +2,9 @@ package file
 
 import (
 	"os"
-	"sync"
 	"time"
 
 	"github.com/elastic/beats/libbeat/common/file"
-	"github.com/elastic/beats/libbeat/logp"
 )
 
 // State is used to communicate the reading state of a file
@@ -52,118 +50,4 @@ func (s *State) IsEqual(c *State) bool {
 // IsEmpty returns true if the state is empty
 func (s *State) IsEmpty() bool {
 	return *s == State{}
-}
-
-// States handles list of FileState
-type States struct {
-	states []State
-	sync.RWMutex
-}
-
-func NewStates() *States {
-	return &States{
-		states: []State{},
-	}
-}
-
-// Update updates a state. If previous state didn't exist, new one is created
-func (s *States) Update(newState State) {
-	s.Lock()
-	defer s.Unlock()
-
-	index, _ := s.findPrevious(newState)
-	newState.Timestamp = time.Now()
-
-	if index >= 0 {
-		s.states[index] = newState
-	} else {
-		// No existing state found, add new one
-		s.states = append(s.states, newState)
-		logp.Debug("input", "New state added for %s", newState.Source)
-	}
-}
-
-func (s *States) FindPrevious(newState State) State {
-	s.RLock()
-	defer s.RUnlock()
-	_, state := s.findPrevious(newState)
-	return state
-}
-
-// findPreviousState returns the previous state fo the file
-// In case no previous state exists, index -1 is returned
-func (s *States) findPrevious(newState State) (int, State) {
-	// TODO: This could be made potentially more performance by using an index (harvester id) and only use iteration as fall back
-	for index, oldState := range s.states {
-		// This is using the FileStateOS for comparison as FileInfo identifiers can only be fetched for existing files
-		if oldState.IsEqual(&newState) {
-			return index, oldState
-		}
-	}
-
-	return -1, State{}
-}
-
-// Cleanup cleans up the state array. All states which are older then `older` are removed
-// The number of states that were cleaned up is returned
-func (s *States) Cleanup() int {
-	s.Lock()
-	defer s.Unlock()
-
-	statesBefore := len(s.states)
-
-	currentTime := time.Now()
-	states := s.states[:0]
-
-	for _, state := range s.states {
-
-		expired := (state.TTL > 0 && currentTime.Sub(state.Timestamp) > state.TTL)
-
-		if state.TTL == 0 || expired {
-			if state.Finished {
-				logp.Debug("state", "State removed for %v because of older: %v", state.Source, state.TTL)
-				continue // drop state
-			} else {
-				logp.Err("State for %s should have been dropped, but couldn't as state is not finished.", state.Source)
-			}
-		}
-
-		states = append(states, state) // in-place copy old state
-	}
-	s.states = states
-
-	return statesBefore - len(s.states)
-}
-
-// Count returns number of states
-func (s *States) Count() int {
-	s.RLock()
-	defer s.RUnlock()
-
-	return len(s.states)
-}
-
-// Returns a copy of the file states
-func (s *States) GetStates() []State {
-	s.RLock()
-	defer s.RUnlock()
-
-	newStates := make([]State, len(s.states))
-	copy(newStates, s.states)
-
-	return newStates
-}
-
-// SetStates overwrites all internal states with the given states array
-func (s *States) SetStates(states []State) {
-	s.Lock()
-	defer s.Unlock()
-	s.states = states
-}
-
-// Copy create a new copy of the states object
-func (s *States) Copy() *States {
-	states := NewStates()
-	states.states = s.GetStates()
-	return states
 }
