b3323932f6ae80ef5052bd998cbe1b59f355101f
Adrian Serrano <adrisr83@gmail.com>
2018-02-15 23:52:47 +0100

    Packetbeat: print unknown MongoDB opcodes (#6255)
    When packetbeat encounters an unknown opcode it will print its
    value as a decimal integer instead of an empty string. Otherwise
    it will print the following line in the log:
    
    2018/01/26 06:30:01.311175 mongodb_parser.go:42: ERR Unknown operation
    code:
    
    which is not very helpful. It has been changed to:
    
    2018/01/26 06:30:01.311175 mongodb_parser.go:42: ERR Unknown operation
    code: (value=nnn)

--- packetbeat/protos/mongodb/mongodb_test.go
+++ packetbeat/protos/mongodb/mongodb_test.go
@@ -333,3 +333,15 @@ func TestMaxDocSize(t *testing.T) {
 
 	assert.Equal(t, "\"1234 ...\n\"123\"\n\"12\"", res["response"])
 }
+
+func TestOpCodeNames(t *testing.T) {
+	for _, testData := range []struct {
+		code     int32
+		expected string
+	}{
+		{1, "OP_REPLY"},
+		{-1, "(value=-1)"},
+	} {
+		assert.Equal(t, testData.expected, opCode(testData.code).String())
+	}
+}
