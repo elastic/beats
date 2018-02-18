0142a9bc1f56cfa2dec474e6659d40f8115fd563
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2018-02-18 22:27:14 +0530

    Log reconnect attempts (#5715)

--- libbeat/publisher/pipeline/output.go
+++ libbeat/publisher/pipeline/output.go
@@ -77,21 +77,30 @@ func (w *netClientWorker) Close() error {
 
 func (w *netClientWorker) run() {
 	for !w.closed.Load() {
+		reconnectAttempts := 0
+
 		// start initial connect loop from first batch, but return
 		// batch to pipeline for other outputs to catch up while we're trying to connect
 		for batch := range w.qu {
 			batch.Cancelled()
 
 			if w.closed.Load() {
+				logp.Info("Closed connection")
 				return
+			}
+
+			if reconnectAttempts > 0 {
+				logp.Info("Attempting to reconnect with %d reconnect attempt(s)", reconnectAttempts)
 			}
 
 			err := w.client.Connect()
 			if err != nil {
 				logp.Err("Failed to connect: %v", err)
+				reconnectAttempts++
 				continue
 			}
 
+			reconnectAttempts = 0
 			break
 		}
 
