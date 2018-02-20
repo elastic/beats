dd7a379c26eea2773c6aa662e1b63ac966305438
simitt <silvia.mitter@elastic.co>
2018-02-20 16:01:01 +0100

    Make normalization processor optional.
    In case a beat.Event is marked to be normalized already,
    do not run the normalization processor again.

--- libbeat/beat/event.go
+++ libbeat/beat/event.go
@@ -12,10 +12,11 @@ import (
 // The `Meta`-fields can be used to pass additional meta-data to the outputs.
 // Output can optionally publish a subset of Meta, or ignore Meta.
 type Event struct {
-	Timestamp time.Time
-	Meta      common.MapStr
-	Fields    common.MapStr
-	Private   interface{} // for beats private use
+	Timestamp  time.Time
+	Meta       common.MapStr
+	Fields     common.MapStr
+	Private    interface{} // for beats private use
+	Normalized bool
 }
 
 var (
