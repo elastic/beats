ff08906b85ad3b1b266157b0487de2bead8d1fe9
ph <phpellerin@gmail.com>
2018-02-21 10:29:20 -0500

    adding initial udp configuration

---
+++ filebeat/input/syslog/config.go
@@ -0,0 +1,27 @@
+package main
+
+import "github.com/elastic/beats/filebeat/harvester"
+
+type config struct {
+	harvester.ForwarderConfig `config:",inline"`
+	Protocol                  string    `config:"protocol"`
+	UDP                       udpConfig `config:"udp"`
+}
+
+type udpConfig struct {
+	Host           string `config:"host"`
+	MaxMessageSize int    `config:"max_message_size"`
+}
+
+var defaultConfig = config{
+	ForwarderConfig: harvester.ForwarderConfig{
+		Type: "syslog",
+	},
+	Protocol: "UDP",
+	Udp:      defaultUDPConfig,
+}
+
+var defaultUDPConfig = udpConfig{
+	MaxMessageSize: 10240,
+	Host:           "localhost:8080",
+}
