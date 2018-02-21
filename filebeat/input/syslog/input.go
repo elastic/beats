2a0aaa535edd5a769de84708f4abf19cd9669ef4
ph <phpellerin@gmail.com>
2018-02-21 10:39:44 -0500

    adding the plugin hook for syslog

--- filebeat/input/syslog/input.go
+++ filebeat/input/syslog/input.go
@@ -1,1 +1,73 @@
 package main
+
+import (
+	"github.com/elastic/beats/filebeat/channel"
+	"github.com/elastic/beats/filebeat/harvester"
+	"github.com/elastic/beats/filebeat/input"
+	"github.com/elastic/beats/libbeat/common"
+	"github.com/elastic/beats/libbeat/common/cfgwarn"
+	"github.com/elastic/beats/libbeat/logp"
+)
+
+//go:generate ragel -Z -G2 parser.rl -o parser.go
+
+func init() {
+	err := input.Register("syslog", NewInput)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Input define a syslog input
+type Input struct {
+	harvester *Harvester
+	started   bool
+	outlet    channel.Outleter
+}
+
+// NewInput creates a new syslog input
+func NewInput(
+	cfg *common.Config,
+	outlet channel.Factory,
+	context input.Context,
+) (input.Input, error) {
+	cfgwarn.Experimental("Syslog input type is used")
+
+	out, err := outlet(cfg, context.DynamicFields)
+	if err != nil {
+		return nil, err
+	}
+
+	forwarder := harvester.NewForwarder(out)
+	return &Input{
+		outlet:    out,
+		harvester: NewHarvester(nil, cfg),
+		started:   false,
+	}
+}
+
+// Run starts and execute the UDP server.
+func (p *Input) Run() {
+	if !p.started {
+		logp.Info("Starting Syslog input")
+		p.started = true
+		go func() {
+			defer p.outlet.Close()
+			err := p.harvester.Run()
+			if err != nil {
+				logp.Err("Error running harvester:: %v", err)
+			}
+		}()
+	}
+}
+
+// Stop stops the UDP input
+func (p *Input) Stop() {
+	logp.Info("stopping Syslog input")
+	p.harvester.Stop()
+}
+
+// Wait suspends the UDP input
+func (p *Input) Wait() {
+	p.Stop()
+}
