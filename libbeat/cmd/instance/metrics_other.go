75474c7d5247a55c8f08d51a9232e1edcdd6b48d
Noémi Ványi <sitbackandwait@gmail.com>
2018-03-06 03:40:57 +0100

    libbeat: add ephemeral id even if no internal memory metrics is reported

--- libbeat/cmd/instance/metrics_other.go
+++ libbeat/cmd/instance/metrics_other.go
@@ -2,7 +2,19 @@
 
 package instance
 
-import "github.com/elastic/beats/libbeat/logp"
+import (
+	"github.com/satori/go.uuid"
+
+	"github.com/elastic/beats/libbeat/logp"
+)
+
+var (
+	ephemeralID uuid.UUID
+)
+
+func init() {
+	ephemeralID = uuid.NewV4()
+}
 
 func setupMetrics(name string) error {
 	logp.Warn("Metrics not implemented for this OS.")
