e18db5d41971a74529b71e4b7f69908e30754289
Noémi Ványi <sitbackandwait@gmail.com>
2018-03-12 12:52:18 +0100

    fix incorrect build tags && create common file for all platforms

--- libbeat/cmd/instance/metrics_other.go
+++ libbeat/cmd/instance/metrics_other.go
@@ -1,20 +1,12 @@
-// +build !darwin,!linux,!cgo darwin,!cgo freebsd,!cgo openbsd
+// +build !darwin !cgo
+// +build !freebsd !cgo
+// +build !linux,!windows
 
 package instance
 
 import (
-	"github.com/satori/go.uuid"
-
 	"github.com/elastic/beats/libbeat/logp"
 )
-
-var (
-	ephemeralID uuid.UUID
-)
-
-func init() {
-	ephemeralID = uuid.NewV4()
-}
 
 func setupMetrics(name string) error {
 	logp.Warn("Metrics not implemented for this OS.")
