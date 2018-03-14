55382fc88d18876a5d068fef666f91e73802ad78
ruflin <spam@ruflin.com>
2018-03-14 10:19:07 +0100

    Add json.ignore_decoding_error to not log decoding errors
    Closes https://github.com/elastic/beats/issues/6045

--- libbeat/tests/system/beat/beat.py
+++ libbeat/tests/system/beat/beat.py
@@ -210,7 +210,7 @@ class TestCase(unittest.TestCase, ComposeMixin):
 
         output_path = os.path.join(self.working_dir, output)
         with open(output_path, "wb") as f:
-            os.chmod(output_path, 0600)
+            os.chmod(output_path, 0o600)
             f.write(output_str.encode('utf8'))
 
     # Returns output as JSON object with flattened fields (. notation)
