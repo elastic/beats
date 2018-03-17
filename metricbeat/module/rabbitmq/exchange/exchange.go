501b64e115bc154ba7214efd051131fd127583c7
Krassimir Valev <Krassimir.Valev@bmw.de>
2018-03-17 17:43:55 +0100

    Code documentation

--- metricbeat/module/rabbitmq/exchange/exchange.go
+++ metricbeat/module/rabbitmq/exchange/exchange.go
@@ -20,17 +20,27 @@ var (
 	}.Build()
 )
 
+// init registers the MetricSet with the central registry as soon as the program
+// starts. The New function will be called later to instantiate an instance of
+// the MetricSet for each host defined in the module's configuration. After the
+// MetricSet has been created then Fetch will begin to be called periodically.
 func init() {
 	if err := mb.Registry.AddMetricSet("rabbitmq", "exchange", New, hostParser); err != nil {
 		panic(err)
 	}
 }
 
+// MetricSet holds any configuration or state information. It must implement
+// the mb.MetricSet interface. And this is best achieved by embedding
+// mb.BaseMetricSet because it implements all of the required mb.MetricSet
+// interface methods except for Fetch.
 type MetricSet struct {
 	mb.BaseMetricSet
 	*helper.HTTP
 }
 
+// New creates a new instance of the MetricSet. New is responsible for unpacking
+// any MetricSet specific configuration options if there are any.
 func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 	cfgwarn.Beta("The rabbitmq exchange metricset is beta")
 
@@ -46,6 +56,9 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 	}, nil
 }
 
+// Fetch methods implements the data gathering and data conversion to the right
+// format. It publishes the event which is then forwarded to the output. In case
+// of an error set the Error field of mb.Event or simply call report.Error().
 func (m *MetricSet) Fetch() ([]common.MapStr, error) {
 	content, err := m.HTTP.FetchContent()
 
