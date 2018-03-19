7756f942ac04583850a96f25180a0038f10e5a3c
Jaime Soriano Pastor <jaime@elastic.co>
2018-03-19 01:10:06 -0700

    Metricbeat: Add munin module (#6473) (#6517)
    Add munin module, with node metricset that obtain metrics
    from a running munin node using the same protocol used by
    munin masters.

--- metricbeat/include/list.go
+++ metricbeat/include/list.go
@@ -83,6 +83,8 @@ import (
 	_ "github.com/elastic/beats/metricbeat/module/mongodb/collstats"
 	_ "github.com/elastic/beats/metricbeat/module/mongodb/dbstats"
 	_ "github.com/elastic/beats/metricbeat/module/mongodb/status"
+	_ "github.com/elastic/beats/metricbeat/module/munin"
+	_ "github.com/elastic/beats/metricbeat/module/munin/node"
 	_ "github.com/elastic/beats/metricbeat/module/mysql"
 	_ "github.com/elastic/beats/metricbeat/module/mysql/status"
 	_ "github.com/elastic/beats/metricbeat/module/nginx"
