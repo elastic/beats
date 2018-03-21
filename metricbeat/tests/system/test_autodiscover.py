b5f8e3aedacc3a50476b9db4c67665e629a6b5fa
Carlos Pérez-Aradros Herce <exekias@gmail.com>
2018-03-21 14:19:48 +0100

    Stop if autodiscover configuration is wrong (#6609)
    This change stops the beat if the configuration for builders or appenders is wrong.
    
    I also added a new integration test on label based autodiscover

--- metricbeat/tests/system/test_autodiscover.py
+++ metricbeat/tests/system/test_autodiscover.py
@@ -15,7 +15,7 @@ class TestAutodiscover(metricbeat.BaseTest):
                      "integration test not available on 2.x")
     def test_docker(self):
         """
-        Test docker autodiscover starts modules
+        Test docker autodiscover starts modules from templates
         """
         import docker
         docker_client = docker.from_env()
@@ -52,4 +52,43 @@ class TestAutodiscover(metricbeat.BaseTest):
         # Check metadata is added
         assert output[0]['docker']['container']['image'] == 'memcached:1.5.3'
         assert output[0]['docker']['container']['labels'] == {}
+        assert 'name' in output[0]['docker']['container']
+
+    @unittest.skipIf(not INTEGRATION_TESTS or
+                     os.getenv("TESTING_ENVIRONMENT") == "2x",
+                     "integration test not available on 2.x")
+    def test_docker_labels(self):
+        """
+        Test docker autodiscover starts modules from labels
+        """
+        import docker
+        docker_client = docker.from_env()
+
+        self.render_config_template(
+            autodiscover={
+                'docker': {
+                    'builders': '- type: metrics',
+                },
+            },
+        )
+
+        proc = self.start_beat()
+        docker_client.images.pull('memcached:1.5.3')
+        labels = {
+            'co.elastic.metrics/module': 'memcached',
+            'co.elastic.metrics/hosts': "'${data.host}:11211'",
+        }
+        container = docker_client.containers.run('memcached:1.5.3', labels=labels, detach=True)
+
+        self.wait_until(lambda: self.log_contains('Autodiscover starting runner: memcached'))
+        sleep(2)
+
+        container.stop()
+        self.wait_until(lambda: self.log_contains('Autodiscover stopping runner: memcached'))
+
+        output = self.read_output_json()
+        proc.check_kill_and_wait()
+
+        # Check metadata is added
+        assert output[0]['docker']['container']['image'] == 'memcached:1.5.3'
         assert 'name' in output[0]['docker']['container']
