4ed40e130701f61455a2f55000e5dfa4505f0693
sudhindra.s <sudhindra.s@bmmdvsudhindra.internal.directi.com>
2018-03-22 23:26:13 +0530

    fix import issue in parser.go

--- packetbeat/protos/graphite/parser.go
+++ packetbeat/protos/graphite/parser.go
@@ -1,167 +1,165 @@
 package graphite
 
 import (
-    "bytes"
-    "errors"
-    "reflect"
-    "strconv"
-    "strings"
-    "time"
+	"bytes"
+	"errors"
+	"reflect"
+	"strconv"
+	"strings"
+	"time"
 
-    "github.com/hydrogen18/stalecucumber"
+	"github.com/hydrogen18/stalecucumber"
 
-    "github.com/elastic/beats/libbeat/common"
-    "github.com/elastic/beats/libbeat/common/streambuf"
-    "github.com/elastic/beats/packetbeat/protos/applayer"
+	"github.com/elastic/beats/libbeat/common"
+	"github.com/elastic/beats/libbeat/common/streambuf"
+	"github.com/elastic/beats/packetbeat/protos/applayer"
 )
 
 type parser struct {
-    buf     streambuf.Buffer
-    config * parserConfig
-    message * message
+	buf     streambuf.Buffer
+	config  *parserConfig
+	message *message
 
-    onMessage func(m * message) error
+	onMessage func(m *message) error
 }
 
 type parserConfig struct {
-    maxBytes int
+	maxBytes int
 }
 
 type message struct {
-    applayer.Message
+	applayer.Message
 
-    // indicator for parsed message being complete or requires more messages
-    // (if false) to be merged to generate full message.
-    isComplete bool
-    data       map[string]interface{}
+	// indicator for parsed message being complete or requires more messages
+	// (if false) to be merged to generate full message.
+	isComplete bool
+	data       map[string]interface{}
 
-    // list element use by 'transactions' for correlation
-    next * message
+	// list element use by 'transactions' for correlation
+	next *message
 }
 
 // Error code if stream exceeds max allowed size on append.
-var(
-    ErrStreamTooLarge=errors.New("Stream data too large")
+var (
+	ErrStreamTooLarge = errors.New("Stream data too large")
 )
 
-func(p * parser) init(
-    cfg * parserConfig,
-    onMessage func(*message) error,
+func (p *parser) init(
+	cfg *parserConfig,
+	onMessage func(*message) error,
 ) {
-    *p = parser{
-        buf: streambuf.Buffer{},
-        config: cfg,
-        onMessage: onMessage,
-    }
+	*p = parser{
+		buf:       streambuf.Buffer{},
+		config:    cfg,
+		onMessage: onMessage,
+	}
 
 }
 
-func(p * parser) append(data[]byte) error {
-    _, err := p.buf.Write(data)
-    if err != nil {
-        return err
-    }
+func (p *parser) append(data []byte) error {
+	_, err := p.buf.Write(data)
+	if err != nil {
+		return err
+	}
 
-    if p.config.maxBytes > 0 & & p.buf.Total() > p.config.maxBytes {
-        return ErrStreamTooLarge
-    }
-    return nil
+	if p.config.maxBytes > 0 && p.buf.Total() > p.config.maxBytes {
+		return ErrStreamTooLarge
+	}
+	return nil
 }
 
-func(p * parser) feed(ts time.Time, data[]byte) error {
-    if err := p.append(data)
-    err != nil {
-        return err
-    }
+func (p *parser) feed(ts time.Time, data []byte) error {
+	if err := p.append(data); err != nil {
+		return err
+	}
 
-    for p.buf.Total() > 0 {
-        if p.message == nil {
-            // allocate new message object to be used by parser with current timestamp
-            p.message = p.newMessage(ts)
-        }
+	for p.buf.Total() > 0 {
+		if p.message == nil {
+			// allocate new message object to be used by parser with current timestamp
+			p.message = p.newMessage(ts)
+		}
 
-        msg, err := p.parse()
-        if err != nil {
-            return err
-        }
-        if msg == nil {
-            break // wait for more data
-        }
+		msg, err := p.parse()
+		if err != nil {
+			return err
+		}
+		if msg == nil {
+			break // wait for more data
+		}
 
-        // reset buffer and message -> handle next message in buffer
-        p.buf.Reset()
-        p.message = nil
+		// reset buffer and message -> handle next message in buffer
+		p.buf.Reset()
+		p.message = nil
 
-        // call message handler callback
-        if err := p.onMessage(msg)
-        err != nil {
-            return err
-        }
-    }
+		// call message handler callback
+		if err := p.onMessage(msg); err != nil {
+			return err
+		}
+	}
 
-    return nil
+	return nil
 }
 
-func(p * parser) newMessage(ts time.Time) * message {
-    return & message{
-        Message: applayer.Message{
-            Ts: ts,
-        },
-    }
+func (p *parser) newMessage(ts time.Time) *message {
+	return &message{
+		Message: applayer.Message{
+			Ts: ts,
+		},
+	}
 }
 
-func(p * parser) parse()(*message, error) {
-    // get the length of data in buffer
-    length := p.buf.Len()
-    // Read the entire buffer content
-    buf, err := p.buf.Collect(length)
-    if err == streambuf.ErrNoMoreBytes || length <= 2 {
-        return nil, nil
-    }
+func (p *parser) parse() (*message, error) {
+	// get the length of data in buffer
+	length := p.buf.Len()
+	// Read the entire buffer content
+	buf, err := p.buf.Collect(length)
+	if err == streambuf.ErrNoMoreBytes || length <= 2 {
+		return nil, nil
+	}
 
-    msg := p.message
-    msg.Size = uint64(p.buf.BufferConsumed())
+	msg := p.message
+	msg.Size = uint64(p.buf.BufferConsumed())
 
-    isRequest := true
-    dir := applayer.NetOriginalDirection
-    pickledData := common.NetString(buf)
-    pickledDataIo := bytes.Buffer(string(pickledData))
-    // Unpickle data into an interface
-    data, err := stalecucumber.Unpickle(pickledDataIo)
-    if err != nil {
-        if strings.Contains(err.Error(), "Opcode is invalid") {
-            return nil, nil
-        }
-        // Line protocol
-        dataStr := string(pickledData)
-        var data[]string
-        data = strings.Fields(dataStr)
-        for _, value := range data {
-            msg.Notes = append(msg.Notes, value) // data * /
-        }
-    } else {
-        // Extract pickle data fields
-        for _, i := range data.([]interface{}) {
-            for _, j := range i.([]interface{}) {
-                if reflect.TypeOf(j).Kind() == reflect.String {
-                    msg.Notes = append(msg.Notes, j.(string))
-                } else {
-                    for _, k := range j.([]interface{}) {
-                        if reflect.TypeOf(k).Kind() == reflect.Int64 {
-                            msg.Notes = append(msg.Notes, strconv.Itoa(int(k.(int64))))
+	isRequest := true
+	dir := applayer.NetOriginalDirection
+	pickledData := common.NetString(buf)
+	pickledDataIo := bytes.Buffer(string(pickledData))
+	// Unpickle data into an interface
+	data, err := stalecucumber.Unpickle(pickledDataIo)
+	if err != nil {
+		if strings.Contains(err.Error(), "Opcode is invalid") {
+			return nil, nil
+		}
+		// Line protocol
+		dataStr := string(pickledData)
+		var data []string
+		data = strings.Fields(dataStr)
+		for _, value := range data {
+			msg.Notes = append(msg.Notes, value) //data*/
+		}
+	} else {
+		// Extract pickle data fields
+		for _, i := range data.([]interface{}) {
+			for _, j := range i.([]interface{}) {
+				if reflect.TypeOf(j).Kind() == reflect.String {
+					msg.Notes = append(msg.Notes, j.(string))
+				} else {
+					for _, k := range j.([]interface{}) {
+						if reflect.TypeOf(k).Kind() == reflect.Int64 {
+							msg.Notes = append(msg.Notes, strconv.Itoa(int(k.(int64))))
 
-                        } else if reflect.TypeOf(k).Kind() == reflect.Float64 {
-                            msg.Notes = append(msg.Notes, strconv.FormatFloat(k.(float64), 'E', -1, 64))
+						} else if reflect.TypeOf(k).Kind() == reflect.Float64 {
+							msg.Notes = append(msg.Notes, strconv.FormatFloat(k.(float64), 'E', -1, 64))
 
-                        } else {
-                            msg.Notes = append(msg.Notes, k.(string))
-                        }
-                    }
-                }
-            }
-        }
-    }
-    msg.IsRequest = isRequest
-    msg.Direction = dir
-    return msg, nil
+						} else {
+							msg.Notes = append(msg.Notes, k.(string))
+						}
+					}
+				}
+			}
+		}
+	}
+	msg.IsRequest = isRequest
+	msg.Direction = dir
+	return msg, nil
 }
