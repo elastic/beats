04cdafda481e25ded9c510f130be036c5c10b5c3
sudhindra.s <sudhindra.s@bmmdvsudhindra.internal.directi.com>
2018-03-23 12:00:33 +0530

    fix goimports style in trans.go

--- packetbeat/protos/graphite/trans.go
+++ packetbeat/protos/graphite/trans.go
@@ -5,7 +5,6 @@ import (
 
     "github.com/elastic/beats/libbeat/common"
     "github.com/elastic/beats/libbeat/logp"
-
     "github.com/elastic/beats/packetbeat/procs"
     "github.com/elastic/beats/packetbeat/protos/applayer"
 )
@@ -65,8 +64,8 @@ func(trans * transactions) onRequest(
     dir uint8,
     msg * message,
 ) error {
-    prev:= trans.requests.last()
-    merged, err:= trans.tryMergeRequests(prev, msg)
+    prev := trans.requests.last()
+    merged, err := trans.tryMergeRequests(prev, msg)
     if err != nil {
         return err
     }
@@ -97,8 +96,8 @@ func(trans * transactions) onResponse(
     dir uint8,
     msg * message,
 ) error {
-    prev:= trans.responses.last()
-    merged, err:= trans.tryMergeResponses(prev, msg)
+    prev := trans.responses.last()
+    merged, err := trans.tryMergeResponses(prev, msg)
     if err != nil {
         return err
     }
@@ -135,8 +134,8 @@ func(trans * transactions) tryMergeResponses(prev, msg * message)(merged bool, e
 }
 
 func(trans * transactions) correlate() error {
-    requests:= &trans.requests
-    responses:= &trans.responses
+    requests := &trans.requests
+    responses := &trans.responses
 
     // drop responses with missing requests
     if requests.empty() {
@@ -150,15 +149,15 @@ func(trans * transactions) correlate() error {
     // merge requests with responses into transactions
     // merge requests with responses into transactions
     for !responses.empty() & & !requests.empty() {
-        resp:= responses.first()
+        resp := responses.first()
         if !resp.isComplete {
             break
         }
 
-        requ:= requests.pop()
+        requ := requests.pop()
         responses.pop()
 
-        if err:= trans.onTransaction(requ, resp)
+        if err := trans.onTransaction(requ, resp)
         err != nil {
             return err
         }
@@ -185,7 +184,7 @@ func(ml * messageList) pop() * message {
         return nil
     }
 
-    msg:= ml.head
+    msg := ml.head
     ml.head = ml.head.next
     if ml.head == nil {
         ml.tail = nil
