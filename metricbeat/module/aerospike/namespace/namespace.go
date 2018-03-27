8a3c73b636f3e8dceaad3d90d46b76248d08d377
ruflin <spam@ruflin.com>
2018-03-27 08:22:49 +0200

    Set namespace as default metricset in Aerospike module
    * Remove metricset config from short config
    * Document default

--- metricbeat/module/aerospike/namespace/namespace.go
+++ metricbeat/module/aerospike/namespace/namespace.go
@@ -16,9 +16,9 @@ import (
 // init registers the MetricSet with the central registry.
 // The New method will be called after the setup of the module and before starting to fetch data
 func init() {
-	if err := mb.Registry.AddMetricSet("aerospike", "namespace", New); err != nil {
-		panic(err)
-	}
+	mb.Registry.MustAddMetricSet("aerospike", "namespace", New,
+		mb.DefaultMetricSet(),
+	)
 }
 
 // MetricSet type defines all fields of the MetricSet
