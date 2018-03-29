99026959a90fb20db8da92500e4fffec3f4d9338
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2018-03-29 07:16:49 +0200

    Set default metricset in windows module to `service` (#6675)

--- metricbeat/module/windows/service/service.go
+++ metricbeat/module/windows/service/service.go
@@ -11,9 +11,9 @@ import (
 // init registers the MetricSet with the central registry.
 // The New method will be called after the setup of the module and before starting to fetch data
 func init() {
-	if err := mb.Registry.AddMetricSet("windows", "service", New); err != nil {
-		panic(err)
-	}
+	mb.Registry.MustAddMetricSet("windows", "service", New,
+		mb.DefaultMetricSet(),
+	)
 }
 
 // MetricSet type defines all fields of the MetricSet
