5c20a52d41be788b5fadd0ea5a4c29771c18de16
Mathias Olsson <hypp74@gmail.com>
2018-04-30 09:04:17 +0200

    split testcase into two

--- libbeat/processors/add_host_metadata/add_host_metadata_test.go
+++ libbeat/processors/add_host_metadata/add_host_metadata_test.go
@@ -13,7 +13,7 @@ import (
 	"github.com/elastic/go-sysinfo/types"
 )
 
-func TestRun(t *testing.T) {
+func TestConfigDefault(t *testing.T) {
 	event := &beat.Event{
 		Fields:    common.MapStr{},
 		Timestamp: time.Now(),
@@ -42,25 +42,31 @@ func TestRun(t *testing.T) {
 	v, err = newEvent.GetValue("host.mac")
 	assert.Error(t, err)
 	assert.Nil(t, v)
+}
 
-	event = &beat.Event{
+func TestConfigNetInfoEnabled(t *testing.T) {
+	event := &beat.Event{
 		Fields:    common.MapStr{},
 		Timestamp: time.Now(),
 	}
-	testConfig, err = common.NewConfigFrom(map[string]interface{}{
+	testConfig, err := common.NewConfigFrom(map[string]interface{}{
 		"netinfo.enabled": true,
 	})
 	assert.NoError(t, err)
 
-	p, err = newHostMetadataProcessor(testConfig)
+	p, err := newHostMetadataProcessor(testConfig)
 	if runtime.GOOS != "windows" && runtime.GOOS != "darwin" && runtime.GOOS != "linux" {
 		assert.IsType(t, types.ErrNotImplemented, err)
 		return
 	}
 	assert.NoError(t, err)
 
-	newEvent, err = p.Run(event)
+	newEvent, err := p.Run(event)
 	assert.NoError(t, err)
+
+	v, err := newEvent.GetValue("host.os.family")
+	assert.NoError(t, err)
+	assert.NotNil(t, v)
 
 	v, err = newEvent.GetValue("host.ip")
 	assert.NoError(t, err)
