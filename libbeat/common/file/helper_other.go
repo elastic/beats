d570f9d51af0ed044137bd306bb7825b2ea84242
urso <steffen.siering@elastic.co>
2018-05-02 13:48:06 +0200

    Registry file fsync improvements
    - Return error if Sync fails
    - Execute fsync on new parent directory
    - improve metrics:
      - registrar.writes.total: total number of registry write attempts
      - registrar.writes.fail: total number of failed write attempts
      - registrar.writes.success: total number of successfull write attempts

--- libbeat/common/file/helper_other.go
+++ libbeat/common/file/helper_other.go
@@ -4,12 +4,26 @@ package file
 
 import (
 	"os"
+	"path/filepath"
 )
 
 // SafeFileRotate safely rotates an existing file under path and replaces it with the tempfile
 func SafeFileRotate(path, tempfile string) error {
+	parent := filepath.Dir(path)
+
 	if e := os.Rename(tempfile, path); e != nil {
 		return e
 	}
+
+	// best-effort fsync on parent directory. The fsync is required by some
+	// filesystems, so to update the parents directory metadata to actually
+	// contain the new file being rotated in.
+	f, err := os.Open(parent)
+	if err != nil {
+		return nil // ignore error
+	}
+	defer f.Close()
+	f.Sync()
+
 	return nil
 }
