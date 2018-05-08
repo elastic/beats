870030baabb56b4ba5e5d27eba6483cb1165ff55
Adrian Marquis <adrian.marquis@f24.com>
2018-05-08 11:41:16 +0200

    Hello Hound

--- metricbeat/module/mysql/galera_status/status.go
+++ metricbeat/module/mysql/galera_status/status.go
@@ -1,5 +1,5 @@
 /*
-Package galerastatus fetches MySQL Galera server status metrics.
+Package galera_status fetches MySQL Galera server status metrics.
 
 For more information on the query it uses, see:
 http://dev.mysql.com/doc/refman/5.7/en/show-status.html
