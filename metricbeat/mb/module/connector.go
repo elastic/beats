7900cd267d61a99b352b0bd4e30848531997c8e7
urso <steffen.siering@elastic.co>
2018-05-11 15:42:04 +0200

    Add local event scheduling capabilities to metricbeat

--- metricbeat/mb/module/connector.go
+++ metricbeat/mb/module/connector.go
@@ -4,19 +4,23 @@ import (
 	"github.com/elastic/beats/libbeat/beat"
 	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/libbeat/processors"
+	"github.com/elastic/beats/libbeat/publisher/scheduling"
 )
 
 // Connector configures and establishes a beat.Client for publishing events
 // to the publisher pipeline.
 type Connector struct {
-	pipeline      beat.Pipeline
-	processors    *processors.Processors
-	eventMeta     common.EventMetadata
-	dynamicFields *common.MapStrPointer
+	pipeline           beat.Pipeline
+	processors         *processors.Processors
+	eventMeta          common.EventMetadata
+	dynamicFields      *common.MapStrPointer
+	schedulingGroup    string
+	schedulingPolicies []beat.SchedulingPolicy
 }
 
 type connectorConfig struct {
 	Processors           processors.PluginConfig `config:"processors"`
+	Scheduling           scheduling.LocalConfig  `config:"scheduling"`
 	common.EventMetadata `config:",inline"`      // Fields and tags to add to events.
 }
 
@@ -31,18 +35,27 @@ func NewConnector(pipeline beat.Pipeline, c *common.Config, dynFields *common.Ma
 		return nil, err
 	}
 
+	schedGroup, schedPolicies, err := scheduling.LoadLocal(config.Scheduling)
+	if err != nil {
+		return nil, err
+	}
+
 	return &Connector{
-		pipeline:      pipeline,
-		processors:    processors,
-		eventMeta:     config.EventMetadata,
-		dynamicFields: dynFields,
+		pipeline:           pipeline,
+		processors:         processors,
+		eventMeta:          config.EventMetadata,
+		dynamicFields:      dynFields,
+		schedulingGroup:    schedGroup,
+		schedulingPolicies: schedPolicies,
 	}, nil
 }
 
 func (c *Connector) Connect() (beat.Client, error) {
 	return c.pipeline.ConnectWith(beat.ClientConfig{
-		EventMetadata: c.eventMeta,
-		Processor:     c.processors,
-		DynamicFields: c.dynamicFields,
+		SchedulingGroup:    c.schedulingGroup,
+		SchedulingPolicies: c.schedulingPolicies,
+		EventMetadata:      c.eventMeta,
+		Processor:          c.processors,
+		DynamicFields:      c.dynamicFields,
 	})
 }
