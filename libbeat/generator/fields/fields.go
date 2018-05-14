9556c3a24645054ccd96feacdf0122b87d51a186
Noémi Ványi <sitbackandwait@gmail.com>
2018-05-14 17:24:12 +0200

    concat fields of libbeat and beat_name

--- libbeat/generator/fields/fields.go
+++ libbeat/generator/fields/fields.go
@@ -75,5 +75,62 @@ func Generate(beatsPath, beatName string, files []*YmlFile) error {
 		return err
 	}
 
-	return writeGeneratedFieldsYml(beatsPath, beatName, files)
+	err = writeGeneratedFieldsYml(beatsPath, beatName, files)
+	if err != nil {
+		return err
+	}
+
+	return AppendFromLibbeat(beatsPath, beatName)
+}
+
+// AppendFromLibbeat appends fields.yml of libbeat to the fields.yml
+func AppendFromLibbeat(beatsPath, beatName string) error {
+	fieldsMetaPath := path.Join(beatsPath, beatName, "_meta", "fields.yml")
+	generatedPath := path.Join(beatsPath, beatName, generatedFieldsYml)
+	err := cpIfNotExists(fieldsMetaPath, generatedPath)
+	if err != nil {
+		return err
+	}
+
+	fieldsPath := path.Join(beatsPath, beatName, "fields.yml")
+	if beatName == "libbeat" {
+		return cp(generatedPath, fieldsPath, os.O_RDWR|os.O_CREATE)
+	}
+
+	libbeatPath := path.Join(beatsPath, "libbeat", generatedFieldsYml)
+	return appendLibbeatFields(generatedFieldsYml, fieldsPath, libbeatPath)
+}
+
+func cpIfNotExists(inPath, outPath string) error {
+	_, err := os.Stat(outPath)
+	if os.IsNotExist(err) {
+		return cp(inPath, outPath, os.O_RDWR|os.O_CREATE)
+	}
+	return nil
+
+}
+
+func appendLibbeatFields(generatedPath, fieldsPath, libbeatPath string) error {
+	err := cp(libbeatPath, fieldsPath, os.O_RDWR|os.O_CREATE)
+	if err != nil {
+		return nil
+	}
+
+	return cp(generatedPath, fieldsPath, os.O_WRONLY|os.O_APPEND)
+}
+
+func cp(in, out string, mode int) error {
+	input, err := ioutil.ReadFile(in)
+	if err != nil {
+		return err
+	}
+
+	output, err := os.OpenFile(out, mode, 0644)
+	if err != nil {
+		return err
+	}
+	defer output.Close()
+
+	_, err = output.Write(input)
+	return err
 }
