fde45836e199ef56cd8f68d99f5899fab5d9ae35
Nicolas Ruflin <spam@ruflin.com>
2018-05-14 18:53:16 +0200

    Enhance testing of Metricbeat Elasticearch module (#7078)
    This PR is to have a few new ideas on how we could test across versions without running the services itself. This approach here does only work for services with an http endpoint.
    
    * Standardise on 2 methods which are used to test the mapping
    * Add http endpoint tests for `node` metricset. Could be used further.

--- metricbeat/module/elasticsearch/index_summary/data_test.go
+++ metricbeat/module/elasticsearch/index_summary/data_test.go
@@ -17,23 +17,8 @@ var info = elasticsearch.Info{
 	ClusterName: "helloworld",
 }
 
-func TestIndex(t *testing.T) {
-	versions := []string{
-		"175", "201", "240", "512", "623",
-	}
-
-	_, err := ioutil.ReadFile("../index/_meta/test/output.json")
-	assert.NoError(t, err)
-
-	for _, v := range versions {
-		input, err := ioutil.ReadFile("../index/_meta/test/stats." + v + ".json")
-		assert.NoError(t, err)
-
-		reporter := &mbtest.CapturingReporterV2{}
-		eventMapping(reporter, info, input)
-		assert.True(t, len(reporter.GetEvents()) >= 1)
-		assert.Equal(t, 0, len(reporter.GetErrors()))
-	}
+func TestMapper(t *testing.T) {
+	elasticsearch.TestMapperWithInfo(t, "../index/_meta/test/stats.*.json", eventMapping)
 }
 
 func TestEmpty(t *testing.T) {
