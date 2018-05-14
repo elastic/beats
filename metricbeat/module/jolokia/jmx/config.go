9121c5c1edf62fea79d88fa9b85ed6825f0d8b50
whale-shark <ryo1.cedar.reverse+github@gmail.com>
2018-05-15 02:34:52 +0900

    Metricbeat: Support Jolokia proxy mode (#6475)

--- metricbeat/module/jolokia/jmx/config.go
+++ metricbeat/module/jolokia/jmx/config.go
@@ -5,12 +5,20 @@ import "encoding/json"
 type JMXMapping struct {
 	MBean      string
 	Attributes []Attribute
+	Target     Target
 }
 
 type Attribute struct {
 	Attr  string
 	Field string
 	Event string
+}
+
+// Target inputs the value you want to set for jolokia target block
+type Target struct {
+	URL      string
+	User     string
+	Password string
 }
 
 // RequestBlock is used to build the request blocks of the following format:
@@ -29,7 +37,12 @@ type Attribute struct {
 //       "attribute":[
 //          "CollectionTime",
 //          "CollectionCount"
-//       ]
+//       ],
+//       "target":{
+//          "url":"service:jmx:rmi:///jndi/rmi://targethost:9999/jmxrmi",
+//          "user":"jolokia",
+//          "password":"s!cr!t"
+//       }
 //    }
 // ]
 type RequestBlock struct {
@@ -37,6 +50,20 @@ type RequestBlock struct {
 	MBean     string                 `json:"mbean"`
 	Attribute []string               `json:"attribute"`
 	Config    map[string]interface{} `json:"config"`
+	Target    *TargetBlock           `json:"target,omitempty"`
+}
+
+// TargetBlock is used to build the target blocks of the following format into RequestBlock.
+//
+// "target":{
+//    "url":"service:jmx:rmi:///jndi/rmi://targethost:9999/jmxrmi",
+//    "user":"jolokia",
+//    "password":"s!cr!t"
+// }
+type TargetBlock struct {
+	URL      string `json:"url"`
+	User     string `json:"user,omitempty"`
+	Password string `json:"password,omitempty"`
 }
 
 type attributeMappingKey struct {
@@ -66,6 +93,13 @@ func buildRequestBodyAndMapping(mappings []JMXMapping) ([]byte, AttributeMapping
 			Type:   "read",
 			MBean:  mapping.MBean,
 			Config: config,
+		}
+
+		if len(mapping.Target.URL) != 0 {
+			rb.Target = new(TargetBlock)
+			rb.Target.URL = mapping.Target.URL
+			rb.Target.User = mapping.Target.User
+			rb.Target.Password = mapping.Target.Password
 		}
 
 		for _, attribute := range mapping.Attributes {
