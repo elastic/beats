5a4a97ada9563b135cedebc5579c2430b94b07f6
Carlos Pérez-Aradros Herce <carlos@elastic.co>
2018-05-15 16:08:26 +0200

    Move testing code to its own package

--- metricbeat/module/kubernetes/apiserver/apiserver_test.go
+++ metricbeat/module/kubernetes/apiserver/apiserver_test.go
@@ -5,14 +5,14 @@ package apiserver
 import (
 	"testing"
 
-	"github.com/elastic/beats/metricbeat/helper/prometheus"
+	"github.com/elastic/beats/metricbeat/helper/prometheus/ptest"
 )
 
 const testFile = "_meta/test/metrics"
 
 func TestEventMapping(t *testing.T) {
-	prometheus.TestMetricSet(t, "kubernetes", "apiserver",
-		prometheus.TestCases{
+	ptest.TestMetricSet(t, "kubernetes", "apiserver",
+		ptest.TestCases{
 			{
 				MetricsFile:  "./_meta/test/metrics",
 				ExpectedFile: "./_meta/test/metrics.expected",
