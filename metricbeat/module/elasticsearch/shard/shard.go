95ab81edf05fab9f23f09bcc6e894cb410f9a173
Nicolas Ruflin <spam@ruflin.com>
2018-05-17 12:30:40 +0200

    Add x-pack data for Elasticsearch shard metricset (#7097)
    * Introduces GetNodeInfo method to fetch additional info about the node. This should become obsolete in the future.
    * Refactor shard metricset to use module level hostParser and metricset.
    
    The xpack feature works but will need further testing with new builds of Elasticsearch. The plan is to test all xpack metricsets together when they are all done and do further tweaks.

--- metricbeat/module/elasticsearch/shard/shard.go
+++ metricbeat/module/elasticsearch/shard/shard.go
@@ -2,55 +2,51 @@ package shard
 
 import (
 	"github.com/elastic/beats/libbeat/common/cfgwarn"
-	"github.com/elastic/beats/metricbeat/helper"
 	"github.com/elastic/beats/metricbeat/mb"
-	"github.com/elastic/beats/metricbeat/mb/parse"
+	"github.com/elastic/beats/metricbeat/module/elasticsearch"
 )
 
 func init() {
 	mb.Registry.MustAddMetricSet("elasticsearch", "shard", New,
-		mb.WithHostParser(hostParser),
+		mb.WithHostParser(elasticsearch.HostParser),
 		mb.DefaultMetricSet(),
 		mb.WithNamespace("elasticsearch.shard"),
 	)
 }
 
-var (
-	hostParser = parse.URLHostParserBuilder{
-		DefaultScheme: "http",
-		PathConfigKey: "path",
-		// Get the stats from the local node
-		DefaultPath: "_cluster/state/version,master_node,routing_table",
-	}.Build()
+const (
+	// Get the stats from the local node
+	statePath = "/_cluster/state/version,master_node,routing_table"
 )
 
 // MetricSet type defines all fields of the MetricSet
 type MetricSet struct {
-	mb.BaseMetricSet
-	http *helper.HTTP
+	*elasticsearch.MetricSet
 }
 
 // New create a new instance of the MetricSet
 func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 	cfgwarn.Beta("The elasticsearch shard metricset is beta")
 
-	http, err := helper.NewHTTP(base)
+	// Get the stats from the local node
+	ms, err := elasticsearch.NewMetricSet(base, statePath)
 	if err != nil {
 		return nil, err
 	}
-	return &MetricSet{
-		BaseMetricSet: base,
-		http:          http,
-	}, nil
+	return &MetricSet{MetricSet: ms}, nil
 }
 
 // Fetch methods implements the data gathering and data conversion to the right format
 func (m *MetricSet) Fetch(r mb.ReporterV2) {
-	content, err := m.http.FetchContent()
+	content, err := m.HTTP.FetchContent()
 	if err != nil {
 		r.Error(err)
 		return
 	}
 
-	eventsMapping(r, content)
+	if m.XPack {
+		eventsMappingXPack(r, m, content)
+	} else {
+		eventsMapping(r, content)
+	}
 }
