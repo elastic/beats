88e1fd285731c71fc396defc6ff90bb96158fff8
Carlos Pérez-Aradros Herce <carlos@elastic.co>
2018-05-27 22:40:53 +0200

    Add central management API client

---
+++ libbeat/management/api/client.go
@@ -0,0 +1,89 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/elastic/beats/libbeat/common"
+	"github.com/elastic/beats/libbeat/setup/kibana"
+	"github.com/pkg/errors"
+)
+
+const defaultTimeout = 10 * time.Second
+
+// Client to Central Management API
+type Client struct {
+	client *kibana.Client
+}
+
+// ConfigFromURL generates a full kibana client config from an URL
+func ConfigFromURL(kibanaURL string) (*kibana.Config, error) {
+	data, err := url.Parse(kibanaURL)
+	if err != nil {
+		return nil, err
+	}
+
+	var username, password string
+	if data.User != nil {
+		username = data.User.Username()
+		password, _ = data.User.Password()
+	}
+
+	return &kibana.Config{
+		Protocol: data.Scheme,
+		Host:     data.Host,
+		Path:     data.Path,
+		Username: username,
+		Password: password,
+		Timeout:  defaultTimeout,
+	}, nil
+}
+
+// NewClient creates and returns a kibana client
+func NewClient(cfg *kibana.Config) (*Client, error) {
+	client, err := kibana.NewClientWithConfig(cfg)
+	if err != nil {
+		return nil, err
+	}
+	return &Client{
+		client: client,
+	}, nil
+}
+
+// do a request to the API and unmarshall the message, error if anything fails
+func (c *Client) request(method, extraPath string,
+	params common.MapStr, headers http.Header, message interface{}) (int, error) {
+
+	paramsJSON, err := json.Marshal(params)
+	if err != nil {
+		return 400, err
+	}
+
+	statusCode, result, err := c.client.Request(method, extraPath, nil, headers, bytes.NewBuffer(paramsJSON))
+	if err != nil {
+		return statusCode, err
+	}
+
+	if statusCode >= 300 {
+		err = extractError(result)
+	} else {
+		if err = json.Unmarshal(result, message); err != nil {
+			return statusCode, errors.Wrap(err, "error unmarshaling response")
+		}
+	}
+
+	return statusCode, err
+}
+
+func extractError(result []byte) error {
+	var kibanaResult struct {
+		Message string
+	}
+	if err := json.Unmarshal(result, &kibanaResult); err != nil {
+		return errors.Wrap(err, "parsing kibana response")
+	}
+	return errors.New(kibanaResult.Message)
+}
