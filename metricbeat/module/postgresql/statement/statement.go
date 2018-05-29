8aa871d266ed0139a263bf8b813c1c751f5c209f
zinefer <zinefer@gmail.com>
2018-05-29 19:25:09 +0000

    postgresql metricset fixes find lost fetch code add changelog message catch rename stragglers

--- metricbeat/module/postgresql/statement/statement.go
+++ metricbeat/module/postgresql/statement/statement.go
@@ -1,9 +1,17 @@
 package statement
 
 import (
+	"database/sql"
+
+	"github.com/pkg/errors"
+
 	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/libbeat/common/cfgwarn"
 	"github.com/elastic/beats/metricbeat/mb"
+	"github.com/elastic/beats/metricbeat/module/postgresql"
+
+	// Register postgresql database/sql driver
+	_ "github.com/lib/pq"
 )
 
 // init registers the MetricSet with the central registry as soon as the program
@@ -11,7 +19,9 @@ import (
 // the MetricSet for each host defined in the module's configuration. After the
 // MetricSet has been created then Fetch will begin to be called periodically.
 func init() {
-	mb.Registry.MustAddMetricSet("postgresql", "statement", New)
+	mb.Registry.MustAddMetricSet("postgresql", "statement", New,
+		mb.WithHostParser(postgresql.ParseURL),
+	)
 }
 
 // MetricSet holds any configuration or state information. It must implement
@@ -20,7 +30,6 @@ func init() {
 // interface methods except for Fetch.
 type MetricSet struct {
 	mb.BaseMetricSet
-	counter int
 }
 
 // New creates a new instance of the MetricSet. New is responsible for unpacking
@@ -35,18 +44,29 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 
 	return &MetricSet{
 		BaseMetricSet: base,
-		counter:       1,
 	}, nil
 }
 
 // Fetch methods implements the data gathering and data conversion to the right
 // format. It publishes the event which is then forwarded to the output. In case
 // of an error set the Error field of mb.Event or simply call report.Error().
-func (m *MetricSet) Fetch(report mb.ReporterV2) {
-	report.Event(mb.Event{
-		MetricSetFields: common.MapStr{
-			"counter": m.counter,
-		},
-	})
-	m.counter++
+func (m *MetricSet) Fetch() ([]common.MapStr, error) {
+	db, err := sql.Open("postgres", m.HostData().URI)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	results, err := postgresql.QueryStats(db, "SELECT * FROM pg_stat_statements")
+	if err != nil {
+		return nil, errors.Wrap(err, "QueryStats")
+	}
+
+	events := []common.MapStr{}
+	for _, result := range results {
+		data, _ := schema.Apply(result)
+		events = append(events, data)
+	}
+
+	return events, nil
 }
