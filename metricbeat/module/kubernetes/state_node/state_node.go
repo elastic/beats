3b8ed950dd0d7024ffbce33ddcddea8d3c960433
Carlos Pérez-Aradros Herce <carlos@elastic.co>
2018-05-31 01:05:22 +0200

    Add waiting/termination reason field

--- metricbeat/module/kubernetes/state_node/state_node.go
+++ metricbeat/module/kubernetes/state_node/state_node.go
@@ -28,7 +28,7 @@ var (
 			"kube_node_status_capacity_cpu_cores":       p.Metric("cpu.capacity.cores"),
 			"kube_node_status_allocatable_cpu_cores":    p.Metric("cpu.allocatable.cores"),
 			"kube_node_spec_unschedulable":              p.BooleanMetric("status.unschedulable"),
-			"kube_node_status_ready":                    p.LabelMetric("status.ready", "condition"),
+			"kube_node_status_ready":                    p.LabelMetric("status.ready", "condition", false),
 		},
 
 		Labels: map[string]p.LabelMap{
