20b18d803edd3f33f23076b9d52ba9f6e25b5cac
Steve Adams <steve.adams78@gmail.com>
2018-06-03 14:26:54 +1200

    modified:   packetbeat/tests/system/test_0066_hl7v2_request_message.py 	modified:   packetbeat/tests/system/test_0067_hl7v2_response_message.py

--- packetbeat/tests/system/test_0066_hl7v2_request_message.py
+++ packetbeat/tests/system/test_0066_hl7v2_request_message.py
@@ -4,7 +4,13 @@ from packetbeat import BaseTest
 class Test(BaseTest):
 
     def test_hl7v2_accept(self):
-        self.render_config_template()
+        self.render_config_template(
+            hl7v2_ports=[2575],
+            hl7v2_segment_selection_mode="Include",
+            hl7v2_field_selection_mode="Include",
+            hl7v2_segments=[MSH,PID],
+            hl7v2_fields=[MSH-10,PID-5],
+        )
         self.run_packetbeat(pcap="hl7v2_application_accept.pcap",
                             debug_selectors=["*"])
         objs = self.read_output()
--- packetbeat/tests/system/test_0067_hl7v2_response_message.py
+++ packetbeat/tests/system/test_0067_hl7v2_response_message.py
@@ -4,7 +4,13 @@ from packetbeat import BaseTest
 class Test(BaseTest):
 
     def test_hl7v2_accept(self):
-        self.render_config_template()
+        self.render_config_template(
+            hl7v2_ports=[2575],
+            hl7v2_segment_selection_mode="Include",
+            hl7v2_field_selection_mode="Include",
+            hl7v2_segments=[MSA],
+            hl7v2_fields=[MSA-1],
+        )
         self.run_packetbeat(pcap="hl7v2_application_accept.pcap",
                             debug_selectors=["*"])
         objs = self.read_output()
