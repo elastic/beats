f77dd206b1fa09f13110d7c841c4742e2f9421c9
Andrew Kroh <andrew.kroh@elastic.co>
2018-06-07 19:27:03 -0400

    Remove dev-tools/packer and related deps

--- dev-tools/packer/docker/xgo-image-deb7/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cp -r ../../../vendor/gopkg.in/yaml.v2 beats-builder/yaml.v2
-cp -r ../../../vendor/github.com/tsg/gotpl beats-builder/gotpl
-docker build --rm=true -t tudorg/xgo-deb7-base base/ && \
-    docker build --rm=true -t tudorg/xgo-deb7-1.10.3 go-1.10.3/ &&
-    docker build --rm=true -t tudorg/beats-builder-deb7 beats-builder
