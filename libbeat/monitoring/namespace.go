4d8c5d2ab5de69cbfbfb4af53105cea2b5c4c51f
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2018-06-07 12:47:05 +0200

    libbeat: Get and initialize namespaces in mutual exclusion

--- libbeat/monitoring/namespace.go
+++ libbeat/monitoring/namespace.go
@@ -1,6 +1,15 @@
 package monitoring
 
-var namespaces = map[string]*Namespace{}
+import (
+	"sync"
+)
+
+var namespaces = struct {
+	sync.Mutex
+	m map[string]*Namespace
+}{
+	m: make(map[string]*Namespace),
+}
 
 // Namespace contains the name of the namespace and it's registry
 type Namespace struct {
@@ -8,21 +17,18 @@ type Namespace struct {
 	registry *Registry
 }
 
-func newNamespace(name string) *Namespace {
-	n := &Namespace{
-		name: name,
-	}
-	namespaces[name] = n
-	return n
-}
-
 // GetNamespace gets the namespace with the given name.
 // If the namespace does not exist yet, a new one is created.
 func GetNamespace(name string) *Namespace {
-	if n, ok := namespaces[name]; ok {
-		return n
+	namespaces.Lock()
+	defer namespaces.Unlock()
+
+	n, ok := namespaces.m[name]
+	if !ok {
+		n = &Namespace{name: name}
+		namespaces.m[name] = n
 	}
-	return newNamespace(name)
+	return n
 }
 
 // SetRegistry sets the registry of the namespace
