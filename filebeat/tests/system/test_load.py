431156aa5f3e8790ede3523515bb5bd5c8c46639
Josh Soref <jsoref@users.noreply.github.com>
2018-06-24 19:44:08 +0000

    spelling: persisted

--- filebeat/tests/system/test_load.py
+++ filebeat/tests/system/test_load.py
@@ -34,7 +34,7 @@ class Test(BaseTest):
         total_lines = 1000
         lines_per_file = 10
         # Each line should have the same length + line ending
-        # Some spare capacity is added to make sure all events are presisted
+        # Some spare capacity is added to make sure all events are persisted
         line_length = len(str(total_lines)) + 1
 
         # Setup python log handler
