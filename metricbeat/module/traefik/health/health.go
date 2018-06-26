07afa97a4934fb6fc0bd666f5875fc7889aa1d0c
Shaunak Kashyap <ycombinator@gmail.com>
2018-06-26 08:07:40 -0700

    Publishing uptime.sec

--- metricbeat/module/traefik/health/health.go
+++ metricbeat/module/traefik/health/health.go
@@ -1,9 +1,30 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package health
 
 import (
-	"github.com/elastic/beats/libbeat/common"
+	"fmt"
+
 	"github.com/elastic/beats/libbeat/common/cfgwarn"
+	"github.com/elastic/beats/metricbeat/helper"
 	"github.com/elastic/beats/metricbeat/mb"
+	"github.com/elastic/beats/metricbeat/mb/parse"
+	"github.com/pkg/errors"
 )
 
 // init registers the MetricSet with the central registry as soon as the program
@@ -11,8 +32,18 @@ import (
 // the MetricSet for each host defined in the module's configuration. After the
 // MetricSet has been created then Fetch will begin to be called periodically.
 func init() {
-	mb.Registry.MustAddMetricSet("traefik", "health", New)
+	mb.Registry.MustAddMetricSet("traefik", "health", New,
+		mb.WithHostParser(hostParser),
+		mb.DefaultMetricSet(),
+	)
 }
+
+var (
+	hostParser = parse.URLHostParserBuilder{
+		DefaultScheme: "http",
+		DefaultPath:   "health",
+	}.Build()
+)
 
 // MetricSet holds any configuration or state information. It must implement
 // the mb.MetricSet interface. And this is best achieved by embedding
@@ -20,22 +51,21 @@ func init() {
 // interface methods except for Fetch.
 type MetricSet struct {
 	mb.BaseMetricSet
-	counter int
+	http *helper.HTTP
 }
 
 // New creates a new instance of the MetricSet. New is responsible for unpacking
 // any MetricSet specific configuration options if there are any.
 func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 	cfgwarn.Experimental("The traefik health metricset is experimental.")
-
-	config := struct{}{}
-	if err := base.Module().UnpackConfig(&config); err != nil {
+	http, err := helper.NewHTTP(base)
+	if err != nil {
 		return nil, err
 	}
 
 	return &MetricSet{
-		BaseMetricSet: base,
-		counter:       1,
+		base,
+		http,
 	}, nil
 }
 
@@ -43,10 +73,16 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 // format. It publishes the event which is then forwarded to the output. In case
 // of an error set the Error field of mb.Event or simply call report.Error().
 func (m *MetricSet) Fetch(report mb.ReporterV2) {
+	data, err := m.http.FetchJSON()
+	if err != nil {
+		report.Error(errors.Wrap(err, "failed to sample health"))
+		return
+	}
+
+	fmt.Println(data)
+
+	event, _ := eventMapping(data)
 	report.Event(mb.Event{
-		MetricSetFields: common.MapStr{
-			"counter": m.counter,
-		},
+		MetricSetFields: event,
 	})
-	m.counter++
 }
