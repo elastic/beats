6abccafd4286af69126a510f3e32de5fd7cfaa82
Shaunak Kashyap <ycombinator@gmail.com>
2018-06-26 09:13:06 -0700

    Fixing imports formatting

--- metricbeat/module/traefik/health/health.go
+++ metricbeat/module/traefik/health/health.go
@@ -20,11 +20,12 @@ package health
 import (
 	"fmt"
 
+	"github.com/pkg/errors"
+
 	"github.com/elastic/beats/libbeat/common/cfgwarn"
 	"github.com/elastic/beats/metricbeat/helper"
 	"github.com/elastic/beats/metricbeat/mb"
 	"github.com/elastic/beats/metricbeat/mb/parse"
-	"github.com/pkg/errors"
 )
 
 // init registers the MetricSet with the central registry as soon as the program
