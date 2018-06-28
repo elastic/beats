8446a7440d61c598f6fc9278f33bdb5cd7bd261e
Nicolas Ruflin <spam@ruflin.com>
2018-06-28 09:55:57 +0200

    Refactor asset generation (#7441)
    * Extract asset template part into libbeat so it can be reused
    * Have a Beat and a Name as part an asset definition. This will be later used for the module name.

--- winlogbeat/include/fields.go
+++ winlogbeat/include/fields.go
@@ -24,7 +24,7 @@ import (
 )
 
 func init() {
-	if err := asset.SetFields("winlogbeat/fields.yml", Asset); err != nil {
+	if err := asset.SetFields("winlogbeat", "fields.yml", Asset); err != nil {
 		panic(err)
 	}
 }
