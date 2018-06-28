f0ab0eb265184c31335f7971d5e392565b60159f
Shaunak Kashyap <ycombinator@gmail.com>
2018-06-28 12:14:00 -0700

    Extract mock response into test fixture file

--- metricbeat/module/traefik/health/health_test.go
+++ metricbeat/module/traefik/health/health_test.go
@@ -21,6 +21,7 @@ package health
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -31,32 +32,14 @@ import (
 	mbtest "github.com/elastic/beats/metricbeat/mb/testing"
 )
 
-// raw response copied from Traefik instance's health API endpoint
-const response = `{
-	"pid": 1,
-	"uptime": "17h51m23.252891567s",
-	"uptime_sec": 64283.252891567,
-	"time": "2018-06-27 22:07:28.966768969 +0000 UTC m=+64283.314491879",
-	"unixtime": 1530137248,
-	"status_code_count": {},
-	"total_status_code_count": {
-		"200": 17,
-	  	"404": 1
-	},
-	"count": 0,
-	"total_count": 18,
-	"total_response_time": "272.119µs",
-	"total_response_time_sec": 0.000272119,
-	"average_response_time": "15.117µs",
-	"average_response_time_sec": 1.5117e-05
-}
-`
-
 func TestFetchEventContents(t *testing.T) {
+	mockResponse, err := ioutil.ReadFile("./_meta/test/simple.json")
+	assert.NoError(t, err)
+
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-		w.Write([]byte(response))
+		w.Write([]byte(mockResponse))
 	}))
 	defer server.Close()
 
