7584cf8fad1d0145e7f8aceee96594dcf42e0518
Shaunak Kashyap <ycombinator@gmail.com>
2018-06-27 18:15:41 -0700

    Fixing system test

--- metricbeat/tests/system/test_traefik.py
+++ metricbeat/tests/system/test_traefik.py
@@ -31,29 +31,6 @@ class Test(metricbeat.BaseTest):
 
         self.assert_fields_are_documented(evt)
 
-    @unittest.skipUnless(metricbeat.INTEGRATION_TESTS, "integration test")
-    def test_health_stats(self):
-        """
-        traefik health_stats metricset test
-        """
-        self.render_config_template(modules=[{
-            "name": "traefik",
-            "metricsets": ["health_stats"],
-            "hosts": self.get_hosts(),
-            "period": "1s",
-        }])
-        proc = self.start_beat()
-        self.wait_until(lambda: self.output_lines() > 0, max_timeout=20)
-        proc.check_kill_and_wait()
-        self.assert_no_logged_warnings()
-
-        output = self.read_output_json()
-        self.assertTrue(len(output) >= 1)
-        evt = output[0]
-        print(evt)
-
-        self.assert_fields_are_documented(evt)
-
     def get_hosts(self):
-        return [os.getenv('traefik_HOST', 'localhost') + ':' +
-                os.getenv('traefik_PORT', '9600')]
+        return [os.getenv('TRAEFIK_HOST', 'localhost') + ':' +
+                os.getenv('TRAEFIK_API_PORT', '8080')]
