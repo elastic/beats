ff5c8a2982b430e177d55bd9047bec1aa8993c46
Shaunak Kashyap <ycombinator@gmail.com>
2018-06-27 18:14:11 -0700

    Adding system test file (WIP)

---
+++ metricbeat/tests/system/test_traefik.py
@@ -0,0 +1,59 @@
+import os
+import metricbeat
+import unittest
+import time
+
+
+class Test(metricbeat.BaseTest):
+
+    COMPOSE_SERVICES = ['traefik']
+
+    @unittest.skipUnless(metricbeat.INTEGRATION_TESTS, "integration test")
+    def test_health(self):
+        """
+        traefik health metricset test
+        """
+        self.render_config_template(modules=[{
+            "name": "traefik",
+            "metricsets": ["health"],
+            "hosts": self.get_hosts(),
+            "period": "1s",
+        }])
+        proc = self.start_beat()
+        self.wait_until(lambda: self.output_lines() > 0, max_timeout=20)
+        proc.check_kill_and_wait()
+        self.assert_no_logged_warnings()
+
+        output = self.read_output_json()
+        self.assertTrue(len(output) >= 1)
+        evt = output[0]
+        print(evt)
+
+        self.assert_fields_are_documented(evt)
+
+    @unittest.skipUnless(metricbeat.INTEGRATION_TESTS, "integration test")
+    def test_health_stats(self):
+        """
+        traefik health_stats metricset test
+        """
+        self.render_config_template(modules=[{
+            "name": "traefik",
+            "metricsets": ["health_stats"],
+            "hosts": self.get_hosts(),
+            "period": "1s",
+        }])
+        proc = self.start_beat()
+        self.wait_until(lambda: self.output_lines() > 0, max_timeout=20)
+        proc.check_kill_and_wait()
+        self.assert_no_logged_warnings()
+
+        output = self.read_output_json()
+        self.assertTrue(len(output) >= 1)
+        evt = output[0]
+        print(evt)
+
+        self.assert_fields_are_documented(evt)
+
+    def get_hosts(self):
+        return [os.getenv('traefik_HOST', 'localhost') + ':' +
+                os.getenv('traefik_PORT', '9600')]
