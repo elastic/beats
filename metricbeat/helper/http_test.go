5b0f68fdc15bedc5f5a1f966b274effc55811fcc
Carlos Pérez-Aradros Herce <carlos@elastic.co>
2018-07-06 13:09:50 +0200

    Add `bearer_token_file` paramter to HTTP helper
    This change allows to load barer tokens from files in modules using
    the HTTP helper. For instance:
    
    ```
    - module: kubernetes
      metricsets:
        - pod
      bearer_token_file: /var/run/secrets/kubernetes.io/serviceaccount/token
      ssl.certificate_authorities:
        - /var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt
    ```

---
+++ metricbeat/helper/http_test.go
@@ -0,0 +1,54 @@
+package helper
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetAuthHeaderFromToken(t *testing.T) {
+	tests := []struct {
+		Name, Content, Expected string
+	}{
+		{
+			"Test a token is read",
+			"testtoken",
+			"Bearer testtoken",
+		},
+		{
+			"Test a token is trimmed",
+			"testtoken\n",
+			"Bearer testtoken",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			content := []byte(test.Content)
+			tmpfile, err := ioutil.TempFile("", "token")
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer os.Remove(tmpfile.Name())
+
+			if _, err := tmpfile.Write(content); err != nil {
+				t.Fatal(err)
+			}
+			if err := tmpfile.Close(); err != nil {
+				t.Fatal(err)
+			}
+
+			header, err := getAuthHeaderFromToken(tmpfile.Name())
+			assert.NoError(t, err)
+			assert.Equal(t, test.Expected, header)
+		})
+	}
+}
+
+func TestGetAuthHeaderFromTokenNoFile(t *testing.T) {
+	header, err := getAuthHeaderFromToken("nonexistingfile")
+	assert.Equal(t, "", header)
+	assert.Error(t, err)
+}
