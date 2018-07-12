58dc30b8d39bb011a097a7e29263873571c31a94
Adrian Serrano <adrisr83@gmail.com>
2018-07-12 03:53:52 +0200

    Allow to specify auditd rules in separate files (#7331)
    * Auditbeat: Allow to specify auditd rules in separate files (#7113)
    
    Added a new configuration field, `audit_rule_files` to the auditd
    module. It allows to specify a set of files that contain audit rules.
    
    - module: auditd
      audit_rule_files: [ '${path.config}/auditd.rules.d/*.conf' ]
    
    Rules will be loaded into the kernel in the following order:
    1. Rules specified in the `audit_rules` field at auditbeat.yml.
    2. Rules from files in the `audit_rule_files`, in the order they are
       listed.
    3. If a path contains a glob, the resulting files are loaded in
       lexicographical order.
    
    Closes #7113
    
    * Install sample rules in /etc/auditbeat/audit.rules.d
    
    Updated the build scripts to install sample audit rules in auditbeats
    configuration directory. Different rules are used for 32 and 64 bit
    platforms.

--- auditbeat/module/auditd/config.go
+++ auditbeat/module/auditd/config.go
@@ -20,6 +20,11 @@ package auditd
 import (
 	"bufio"
 	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"sort"
 	"strings"
 	"time"
 
@@ -31,20 +36,22 @@ import (
 )
 
 const (
-	moduleName    = "auditd"
-	metricsetName = "auditd"
+	moduleName         = "auditd"
+	metricsetName      = "auditd"
+	recursiveGlobDepth = 8
 )
 
 // Config defines the kernel metricset's possible configuration options.
 type Config struct {
-	ResolveIDs   bool   `config:"resolve_ids"`         // Resolve UID/GIDs to names.
-	FailureMode  string `config:"failure_mode"`        // Failure mode for the kernel (silent, log, panic).
-	BacklogLimit uint32 `config:"backlog_limit"`       // Max number of message to buffer in the auditd.
-	RateLimit    uint32 `config:"rate_limit"`          // Rate limit in messages/sec of messages from auditd.
-	RawMessage   bool   `config:"include_raw_message"` // Include the list of raw audit messages in the event.
-	Warnings     bool   `config:"include_warnings"`    // Include warnings in the event (for dev/debug purposes only).
-	RulesBlob    string `config:"audit_rules"`         // Audit rules. One rule per line.
-	SocketType   string `config:"socket_type"`         // Socket type to use with the kernel (unicast or multicast).
+	ResolveIDs   bool     `config:"resolve_ids"`         // Resolve UID/GIDs to names.
+	FailureMode  string   `config:"failure_mode"`        // Failure mode for the kernel (silent, log, panic).
+	BacklogLimit uint32   `config:"backlog_limit"`       // Max number of message to buffer in the auditd.
+	RateLimit    uint32   `config:"rate_limit"`          // Rate limit in messages/sec of messages from auditd.
+	RawMessage   bool     `config:"include_raw_message"` // Include the list of raw audit messages in the event.
+	Warnings     bool     `config:"include_warnings"`    // Include warnings in the event (for dev/debug purposes only).
+	RulesBlob    string   `config:"audit_rules"`         // Audit rules. One rule per line.
+	RuleFiles    []string `config:"audit_rule_files"`    // List of rule files.
+	SocketType   string   `config:"socket_type"`         // Socket type to use with the kernel (unicast or multicast).
 
 	// Tuning options (advanced, use with care)
 	ReassemblerMaxInFlight uint32        `config:"reassembler.max_in_flight"`
@@ -56,6 +63,8 @@ type Config struct {
 	// One of "user-space", "kernel", "both", "none", "auto" (default)
 	BackpressureStrategy  string `config:"backpressure_strategy"`
 	StreamBufferConsumers int    `config:"stream_buffer_consumers"`
+
+	auditRules []auditRule
 }
 
 type auditRule struct {
@@ -63,10 +72,30 @@ type auditRule struct {
 	data  []byte
 }
 
+type ruleWithSource struct {
+	rule   auditRule
+	source string
+}
+
+type ruleSet map[string]ruleWithSource
+
+var defaultConfig = Config{
+	ResolveIDs:             true,
+	FailureMode:            "silent",
+	BacklogLimit:           8192,
+	RateLimit:              0,
+	RawMessage:             false,
+	Warnings:               false,
+	ReassemblerMaxInFlight: 50,
+	ReassemblerTimeout:     2 * time.Second,
+	StreamBufferQueueSize:  8192,
+	StreamBufferConsumers:  0,
+}
+
 // Validate validates the rules specified in the config.
 func (c *Config) Validate() error {
 	var errs multierror.Errors
-	_, err := c.rules()
+	err := c.loadRules()
 	if err != nil {
 		errs = append(errs, err)
 	}
@@ -87,47 +116,46 @@ func (c *Config) Validate() error {
 }
 
 // Rules returns a list of rules specified in the config.
-func (c Config) rules() ([]auditRule, error) {
-	var errs multierror.Errors
-	var auditRules []auditRule
-	ruleSet := map[string]auditRule{}
-	s := bufio.NewScanner(bytes.NewBufferString(c.RulesBlob))
-	for s.Scan() {
-		line := strings.TrimSpace(s.Text())
-		if len(line) == 0 || line[0] == '#' {
-			continue
-		}
+func (c Config) rules() []auditRule {
+	return c.auditRules
+}
 
-		// Parse the CLI flags into an intermediate rule specification.
-		r, err := flags.Parse(line)
+func (c *Config) loadRules() error {
+	var paths []string
+	for _, pattern := range c.RuleFiles {
+		absPattern, err := filepath.Abs(pattern)
 		if err != nil {
-			errs = append(errs, errors.Wrapf(err, "failed on rule '%v'", line))
-			continue
+			return fmt.Errorf("unable to get the absolute path for %s: %v", pattern, err)
 		}
-
-		// Convert rule specification to a binary rule representation.
-		data, err := rule.Build(r)
+		files, err := filepath.Glob(absPattern)
 		if err != nil {
-			errs = append(errs, errors.Wrapf(err, "failed on rule '%v'", line))
-			continue
+			return err
 		}
-
-		// Detect duplicates based on the normalized binary rule representation.
-		existingRule, found := ruleSet[string(data)]
-		if found {
-			errs = append(errs, errors.Errorf("failed on rule '%v' because its a duplicate of '%v'", line, existingRule.flags))
-			continue
-		}
-		auditRule := auditRule{flags: line, data: []byte(data)}
-		ruleSet[string(data)] = auditRule
-
-		auditRules = append(auditRules, auditRule)
+		sort.Strings(files)
+		paths = append(paths, files...)
 	}
 
-	if len(errs) > 0 {
-		return nil, errors.Wrap(errs.Err(), "invalid audit_rules")
+	knownRules := ruleSet{}
+
+	rules, err := readRules(bytes.NewBufferString(c.RulesBlob), "(audit_rules at auditbeat.yml)", knownRules)
+	if err != nil {
+		return err
 	}
-	return auditRules, nil
+	c.auditRules = append(c.auditRules, rules...)
+
+	for _, filename := range paths {
+		fHandle, err := os.Open(filename)
+		if err != nil {
+			return fmt.Errorf("unable to open rule file '%s': %v", filename, err)
+		}
+		rules, err = readRules(fHandle, filename, knownRules)
+		if err != nil {
+			return err
+		}
+		c.auditRules = append(c.auditRules, rules...)
+	}
+
+	return nil
 }
 
 func (c Config) failureMode() (uint32, error) {
@@ -143,15 +171,45 @@ func (c Config) failureMode() (uint32, error) {
 	}
 }
 
-var defaultConfig = Config{
-	ResolveIDs:             true,
-	FailureMode:            "silent",
-	BacklogLimit:           8192,
-	RateLimit:              0,
-	RawMessage:             false,
-	Warnings:               false,
-	ReassemblerMaxInFlight: 50,
-	ReassemblerTimeout:     2 * time.Second,
-	StreamBufferQueueSize:  8192,
-	StreamBufferConsumers:  0,
+func readRules(reader io.Reader, source string, knownRules ruleSet) (rules []auditRule, err error) {
+	var errs multierror.Errors
+
+	s := bufio.NewScanner(reader)
+	for lineNum := 1; s.Scan(); lineNum++ {
+		location := fmt.Sprintf("%s:%d", source, lineNum)
+		line := strings.TrimSpace(s.Text())
+		if len(line) == 0 || line[0] == '#' {
+			continue
+		}
+
+		// Parse the CLI flags into an intermediate rule specification.
+		r, err := flags.Parse(line)
+		if err != nil {
+			errs = append(errs, errors.Wrapf(err, "at %s: failed to parse rule '%v'", location, line))
+			continue
+		}
+
+		// Convert rule specification to a binary rule representation.
+		data, err := rule.Build(r)
+		if err != nil {
+			errs = append(errs, errors.Wrapf(err, "at %s: failed to interpret rule '%v'", location, line))
+			continue
+		}
+
+		// Detect duplicates based on the normalized binary rule representation.
+		existing, found := knownRules[string(data)]
+		if found {
+			errs = append(errs, errors.Errorf("at %s: rule '%v' is a duplicate of '%v' at %s", location, line, existing.rule.flags, existing.source))
+			continue
+		}
+		rule := auditRule{flags: line, data: []byte(data)}
+		knownRules[string(data)] = ruleWithSource{rule, location}
+
+		rules = append(rules, rule)
+	}
+
+	if len(errs) > 0 {
+		return nil, errors.Wrap(errs.Err(), "failed loading rules")
+	}
+	return rules, nil
 }
