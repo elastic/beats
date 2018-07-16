29c1de660a88d05285ed3bc21b6cf8c4d2539cce
berfinsari <berfinsari21@gmail.com>
2018-07-16 16:17:42 +0300

    Fix missing license header issue

--- metricbeat/module/envoyproxy/doc.go
+++ metricbeat/module/envoyproxy/doc.go
@@ -13,7 +13,7 @@
 // "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
-// under the License
+// under the License.
 
 // Package envoyproxy is a Metricbeat module that contains MetricSets.
 package envoyproxy
