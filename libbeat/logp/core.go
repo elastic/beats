03f7f861ccdf2cb9ecd8a76267dc130a861a19d8
Andrew Kroh <andrew.kroh@elastic.co>
2018-07-27 10:51:41 -0400

    Trim space from debug selectors in -d
    Using `-d "publish, processors"` wasn't working expected because `processors` included a leading space. Each selector is now trimmed.
    
    Workarounds include using `-d publish -d processors` or `-d publish,processors`.

--- libbeat/logp/core.go
+++ libbeat/logp/core.go
@@ -23,6 +23,7 @@ import (
 	golog "log"
 	"os"
 	"path/filepath"
+	"strings"
 	"sync/atomic"
 	"unsafe"
 
@@ -89,7 +90,7 @@ func Configure(cfg Config) error {
 	selectors := make(map[string]struct{}, len(cfg.Selectors))
 	if cfg.Level.Enabled(DebugLevel) && len(cfg.Selectors) > 0 {
 		for _, sel := range cfg.Selectors {
-			selectors[sel] = struct{}{}
+			selectors[strings.TrimSpace(sel)] = struct{}{}
 		}
 
 		// Default to all enabled if no selectors are specified.
