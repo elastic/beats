6523646b86108523de4abb1bfa22917adfbdd837
Pier-Hugues Pellerin <phpellerin@gmail.com>
2018-07-30 16:01:10 -0400

    Extract the Dissect test suite into an external file (#7768)
    We currently have three implementations of dissect (LS, Ingest
    and beats), moving the common test case to an external file is the first
    step to be able to converge to a common specification.
    
    cc @guyboertje @jakelandis
    
    (cherry picked from commit 3e85c9a)

--- libbeat/processors/dissect/dissect_test.go
+++ libbeat/processors/dissect/dissect_test.go
@@ -19,15 +19,14 @@ package dissect
 
 import (
 	"encoding/json"
-	"flag"
+	"fmt"
 	"io/ioutil"
+	"os"
 	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-
-var export = flag.Bool("test.export-dissect", false, "export dissect tests to JSON.")
 
 func TestNoToken(t *testing.T) {
 	_, err := New("hello")
@@ -51,196 +50,19 @@ type dissectTest struct {
 	Fail     bool   `json:"fail"`
 }
 
-var tests = []dissectTest{
-	{
-		Name: "When all the defined fields are captured by we have remaining data",
-		Tok:  "level=%{level} ts=%{timestamp} caller=%{caller} msg=\"%{message}\"",
-		Msg:  "level=info ts=2018-06-27T17:19:13.036579993Z caller=main.go:222 msg=\"Starting OK\" version=\"(version=2.3.1, branch=HEAD, revision=188ca45bd85ce843071e768d855722a9d9dabe03)\"}",
-		Expected: Map{
-			"level":     "info",
-			"timestamp": "2018-06-27T17:19:13.036579993Z",
-			"caller":    "main.go:222",
-			"message":   "Starting OK",
-		},
-	},
-	{
-		Name: "Complex stack trace",
-		Tok:  "%{day}-%{month}-%{year} %{hour} %{severity} [%{thread_id}] %{origin} %{message}",
-		Msg: `18-Apr-2018 06:53:20.411 INFO [http-nio-8080-exec-1] org.apache.coyote.http11.Http11Processor.service Error parsing HTTP request header
- Note: further occurrences of HTTP header parsing errors will be logged at DEBUG level.
- java.lang.IllegalArgumentException: Invalid character found in method name. HTTP method names must be tokens
-    at org.apache.coyote.http11.Http11InputBuffer.parseRequestLine(Http11InputBuffer.java:426)
-    at org.apache.coyote.http11.Http11Processor.service(Http11Processor.java:687)
-    at org.apache.coyote.AbstractProcessorLight.process(AbstractProcessorLight.java:66)
-    at org.apache.coyote.AbstractProtocol$ConnectionHandler.process(AbstractProtocol.java:790)
-    at org.apache.tomcat.util.net.NioEndpoint$SocketProcessor.doRun(NioEndpoint.java:1459)
-    at org.apache.tomcat.util.net.SocketProcessorBase.run(SocketProcessorBase.java:49)
-    at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1149)
-    at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:624)
-    at org.apache.tomcat.util.threads.TaskThread$WrappingRunnable.run(TaskThread.java:61)
-    at java.lang.Thread.run(Thread.java:748)`,
-		Expected: Map{
-			"day":       "18",
-			"month":     "Apr",
-			"year":      "2018",
-			"hour":      "06:53:20.411",
-			"severity":  "INFO",
-			"thread_id": "http-nio-8080-exec-1",
-			"origin":    "org.apache.coyote.http11.Http11Processor.service",
-			"message": `Error parsing HTTP request header
- Note: further occurrences of HTTP header parsing errors will be logged at DEBUG level.
- java.lang.IllegalArgumentException: Invalid character found in method name. HTTP method names must be tokens
-    at org.apache.coyote.http11.Http11InputBuffer.parseRequestLine(Http11InputBuffer.java:426)
-    at org.apache.coyote.http11.Http11Processor.service(Http11Processor.java:687)
-    at org.apache.coyote.AbstractProcessorLight.process(AbstractProcessorLight.java:66)
-    at org.apache.coyote.AbstractProtocol$ConnectionHandler.process(AbstractProtocol.java:790)
-    at org.apache.tomcat.util.net.NioEndpoint$SocketProcessor.doRun(NioEndpoint.java:1459)
-    at org.apache.tomcat.util.net.SocketProcessorBase.run(SocketProcessorBase.java:49)
-    at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1149)
-    at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:624)
-    at org.apache.tomcat.util.threads.TaskThread$WrappingRunnable.run(TaskThread.java:61)
-    at java.lang.Thread.run(Thread.java:748)`,
-		},
-	},
-	{
-		Name: "fails when delimiter is not found at the beginning of the string",
-		Tok:  "/var/log/%{key}.log",
-		Msg:  "foobar",
-		Fail: true,
-	},
-	{
-		Name: "fails when delimiter is not found after the key",
-		Tok:  "/var/log/%{key}.log",
-		Msg:  "/var/log/foobar",
-		Fail: true,
-	},
-	{
-		Name:     "simple dissect",
-		Tok:      "%{key}",
-		Msg:      "foobar",
-		Expected: Map{"key": "foobar"},
-	},
-	{
-		Name:     "dissect two replacement",
-		Tok:      "%{key1} %{key2}",
-		Msg:      "foo bar",
-		Expected: Map{"key1": "foo", "key2": "bar"},
-	},
-	{
-		Name:     "one level dissect not end of string",
-		Tok:      "/var/%{key}/log",
-		Msg:      "/var/foobar/log",
-		Expected: Map{"key": "foobar"},
-	},
-	{
-		Name:     "one level dissect",
-		Tok:      "/var/%{key}",
-		Msg:      "/var/foobar/log",
-		Expected: Map{"key": "foobar/log"},
-	},
-	{
-		Name:     "multiple keys dissect end of string",
-		Tok:      "/var/%{key}/log/%{key1}",
-		Msg:      "/var/foobar/log/apache",
-		Expected: Map{"key": "foobar", "key1": "apache"},
-	},
-	{
-		Name:     "multiple keys not end of string",
-		Tok:      "/var/%{key}/log/%{key1}.log",
-		Msg:      "/var/foobar/log/apache.log",
-		Expected: Map{"key": "foobar", "key1": "apache"},
-	},
-	{
-		Name:     "simple ordered",
-		Tok:      "%{+key/3} %{+key/1} %{+key/2}",
-		Msg:      "1 2 3",
-		Expected: Map{"key": "2 3 1"},
-	},
-	{
-		Name:     "simple append",
-		Tok:      "%{key}-%{+key}-%{+key}",
-		Msg:      "1-2-3",
-		Expected: Map{"key": "1-2-3"},
-	},
-	{
-		Name:     "indirect field",
-		Tok:      "%{key} %{&key}",
-		Msg:      "hello world",
-		Expected: Map{"key": "hello", "hello": "world"},
-	},
-	{
-		Name:     "skip field",
-		Tok:      "%{} %{key}",
-		Msg:      "hello world",
-		Expected: Map{"key": "world"},
-	},
-	{
-		Name:     "named skiped field with indirect",
-		Tok:      "%{?key} %{&key}",
-		Msg:      "hello world",
-		Expected: Map{"hello": "world"},
-	},
-	{
-		Name: "missing fields",
-		Tok:  "%{name},%{addr1},%{addr2},%{addr3},%{city},%{zip}",
-		Msg:  "Jane Doe,4321 Fifth Avenue,,,New York,87432",
-		Expected: Map{
-			"name":  "Jane Doe",
-			"addr1": "4321 Fifth Avenue",
-			"addr2": "",
-			"addr3": "",
-			"city":  "New York",
-			"zip":   "87432",
-		},
-	},
-	{
-		Name: "ignore right padding",
-		Tok:  "%{id} %{function->} %{server}",
-		Msg:  "00000043 ViewReceive     machine-321",
-		Expected: Map{
-			"id":       "00000043",
-			"function": "ViewReceive",
-			"server":   "machine-321",
-		},
-	},
-	{
-		Name: "padding on the last key need a delimiter",
-		Tok:  "%{id} %{function} %{server->} ",
-		Msg:  "00000043 ViewReceive machine-321    ",
-		Expected: Map{
-			"id":       "00000043",
-			"function": "ViewReceive",
-			"server":   "machine-321",
-		},
-	},
-	{
-		Name: "ignore left padding",
-		Tok:  "%{id->} %{function} %{server}",
-		Msg:  "00000043    ViewReceive machine-321",
-		Expected: Map{
-			"id":       "00000043",
-			"function": "ViewReceive",
-			"server":   "machine-321",
-		},
-	},
-	{
-		Name: "when the delimiters contains `{` and `}`",
-		Tok:  "{%{a}}{%{b}} %{rest}",
-		Msg:  "{c}{d} anything",
-		Expected: Map{
-			"a":    "c",
-			"b":    "d",
-			"rest": "anything",
-		},
-	},
+var tests []dissectTest
+
+func init() {
+	content, err := ioutil.ReadFile("testdata/dissect_tests.json")
+	if err != nil {
+		fmt.Printf("could not read the content of 'dissect_tests', error: %s", err)
+		os.Exit(1)
+	}
+
+	json.Unmarshal(content, &tests)
 }
 
 func TestDissect(t *testing.T) {
-	if export != nil && *export {
-		dumpJSON()
-		return
-	}
-
 	for _, test := range tests {
 		if test.Skip {
 			continue
@@ -349,16 +171,4 @@ func BenchmarkDissect(b *testing.B) {
 			o = re.FindAllStringSubmatch(by, -1)
 		}
 	})
-}
-
-func dumpJSON() {
-	b, err := json.MarshalIndent(&tests, "", "\t")
-	if err != nil {
-		panic("could not marshal json")
-	}
-
-	err = ioutil.WriteFile("dissect_tests.json", b, 0666)
-	if err != nil {
-		panic("could not write to file")
-	}
 }
