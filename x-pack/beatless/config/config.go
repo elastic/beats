6e26e1e7d80c60470062a88f2bdb767f9f35dbc8
Pier-Hugues Pellerin <phpellerin@gmail.com>
2018-08-01 15:49:25 -0400

    Beatless initial PR
    Add a new beat inside the x-pack folder under the Elastic License,
    minimal requirement changes to have a build and a test running.

---
+++ x-pack/beatless/config/config.go
@@ -0,0 +1,14 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+// Config is put into a different package to prevent cyclic imports in case
+// it is needed in several locations
+
+package config
+
+// Config default configuration for Beatless.
+type Config struct {
+}
+
+var DefaultConfig = Config{}
