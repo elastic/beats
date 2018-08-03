ffa6efea39868a37d20d2ee3cb54c7647922e47f
Pier-Hugues Pellerin <phpellerin@gmail.com>
2018-08-03 08:54:50 -0400

    License check
    Main makefile exclude ASL2 for x-pack but check for Elastic.
    Beats can override the license in their Makefile.

--- x-pack/beatless/config/config.go
+++ x-pack/beatless/config/config.go
@@ -2,9 +2,6 @@
 // or more contributor license agreements. Licensed under the Elastic License;
 // you may not use this file except in compliance with the Elastic License.
 
-// Config is put into a different package to prevent cyclic imports in case
-// it is needed in several locations
-
 package config
 
 // Config default configuration for Beatless.
