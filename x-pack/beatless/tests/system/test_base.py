4af1ba8ca930e4e383a71aba2d6f328a658f8961
Pier-Hugues Pellerin <phpellerin@gmail.com>
2018-08-06 07:16:29 -0400

    Beatless initial PR (#7837)
    * Beatless initial PR
    
    Add a new beat inside the x-pack folder under the Elastic License,
    minimal requirement changes to have a build and a test running.
    
    Main makefile exclude ASL2 for x-pack but check for Elastic.
    Beats can override the license in their Makefile.

---
+++ x-pack/beatless/tests/system/test_base.py
@@ -0,0 +1,19 @@
+from beatless import BaseTest
+
+import os
+
+
+class Test(BaseTest):
+
+    def test_base(self):
+        """
+        Basic test with exiting Beatless normally
+        """
+        self.render_config_template(
+            path=os.path.abspath(self.working_dir) + "/log/*"
+        )
+
+        beatless_proc = self.start_beat()
+        self.wait_until(lambda: self.log_contains("beatless is running"))
+        exit_code = beatless_proc.kill_and_wait()
+        assert exit_code == 0
