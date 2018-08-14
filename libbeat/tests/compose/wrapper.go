56729d7eedb669a1dc261834b1335b3145a726d4
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2018-08-14 18:53:25 +0200

    --no-ansi for compose

--- libbeat/tests/compose/wrapper.go
+++ libbeat/tests/compose/wrapper.go
@@ -90,7 +90,7 @@ func (d *wrapperDriver) LockFile() string {
 
 func (d *wrapperDriver) cmd(ctx context.Context, command string, arg ...string) *exec.Cmd {
 	var args []string
-	args = append(args, "--project-name", d.Name)
+	args = append(args, "--no-ansi", "--project-name", d.Name)
 	for _, f := range d.Files {
 		args = append(args, "--file", f)
 	}
