b9f10034f30fc3bf00288051d6e98d68daffb339
Andrei Chasovskikh <andrey.chasovskikh@gmail.com>
2018-08-15 17:52:54 -0700

    Rename client input/output buffer metrics
    https://github.com/antirez/redis/commit/be88c0b16a53f5763d8fc1ae683f99ee39b0d68e

--- metricbeat/module/redis/info/data.go
+++ metricbeat/module/redis/info/data.go
@@ -26,10 +26,10 @@ import (
 var (
 	schema = s.Schema{
 		"clients": s.Object{
-			"connected":           c.Int("connected_clients"),
-			"longest_output_list": c.Int("client_longest_output_list"),
-			"biggest_input_buf":   c.Int("client_biggest_input_buf"),
-			"blocked":             c.Int("blocked_clients"),
+			"connected":         c.Int("connected_clients"),
+			"max_output_buffer": c.Int("client_recent_max_output_buffer"),
+			"max_input_buffer":  c.Int("client_recent_max_input_buffer"),
+			"blocked":           c.Int("blocked_clients"),
 		},
 		"cluster": s.Object{
 			"enabled": c.Bool("cluster_enabled"),
