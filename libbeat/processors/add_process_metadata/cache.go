49a7ff7df83b7555cd9f0ed061d47ffa19f5c054
Adrian Serrano <adrisr83@gmail.com>
2018-08-23 17:00:42 +0200

    Use a RWMutex in cache
    And save the error from the PID lookup so that old PIDs are not queried
    every time.

--- libbeat/processors/add_process_metadata/cache.go
+++ libbeat/processors/add_process_metadata/cache.go
@@ -24,6 +24,7 @@ import (
 
 type processCacheEntry struct {
 	metadata   *processMetadata
+	err        error
 	expiration time.Time
 }
 
@@ -31,7 +32,7 @@ type processCache struct {
 	cache      map[int]processCacheEntry
 	provider   processMetadataProvider
 	expiration time.Duration
-	mutex      sync.Mutex
+	rwMutex    sync.RWMutex
 }
 
 func newProcessCache(expiration time.Duration, provider processMetadataProvider) processCache {
@@ -42,22 +43,28 @@ func newProcessCache(expiration time.Duration, provider processMetadataProvider)
 	}
 }
 
-func (pc *processCache) GetProcessMetadata(pid int) (*processMetadata, error) {
-	if pid == 0 {
-		return nil, nil
+func (pc *processCache) getEntryUnlocked(pid int) (entry processCacheEntry, valid bool) {
+	if entry, valid = pc.cache[pid]; valid {
+		valid = entry.expiration.After(time.Now())
 	}
-	pc.mutex.Lock()
-	defer pc.mutex.Unlock()
+	return
+}
 
-	entry, found := pc.cache[pid]
-	if !found || entry.expiration.Before(time.Now()) {
-		var err error
-		entry.metadata, err = pc.provider.GetProcessMetadata(pid)
-		if err != nil {
-			return nil, err
+func (pc *processCache) GetProcessMetadata(pid int) (*processMetadata, error) {
+	pc.rwMutex.RLock()
+	entry, valid := pc.getEntryUnlocked(pid)
+	pc.rwMutex.RUnlock()
+
+	if !valid {
+		pc.rwMutex.Lock()
+		defer pc.rwMutex.Unlock()
+		// Make sure someone else didn't generate this entry while we were
+		// waiting for the write lock
+		if entry, valid = pc.getEntryUnlocked(pid); !valid {
+			entry.metadata, entry.err = pc.provider.GetProcessMetadata(pid)
+			entry.expiration = time.Now().Add(pc.expiration)
+			pc.cache[pid] = entry
 		}
-		entry.expiration = time.Now().Add(pc.expiration)
-		pc.cache[pid] = entry
 	}
-	return entry.metadata, nil
+	return entry.metadata, entry.err
 }
