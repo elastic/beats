aefc948051eb00e4bdbabbf474016e42186979c1
SeungYong Oh <flyer1217@gmail.com>
2018-08-31 22:22:49 +0900

    Fix docker json message.Bytes when partial docker logs are joined (#8177)

--- filebeat/reader/readjson/docker_json.go
+++ filebeat/reader/readjson/docker_json.go
@@ -136,6 +136,7 @@ func (p *DockerJSONReader) Next() (reader.Message, error) {
 					return message, err
 				}
 				message.Content = append(message.Content, next.Content...)
+				message.Bytes += next.Bytes
 			}
 		} else {
 			message, err = parseCRILog(message, &crioLine)
