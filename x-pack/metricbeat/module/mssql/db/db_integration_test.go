6ac24dff6a2dd5fd0d2cf59a0a74f660be790791
sayden <mariocaster@gmail.com>
2018-09-05 23:10:58 +0200

    Atomic commit of the current work in progress MS SQL metricset Experimental `db` metricset Move methods to call SQL concurrently to the mssql package so it's available to all metricsets Implement io.Close to do cleanup on the db metricset Rename function rowsToMapR to eventsMapping Promote database_id to the parent level within the schema DB connection is created and closed on each Fetch call. Minor improvements on pointer receiver namings on fetcher Updated data.json when launching go tests with -data Added comment to NewDB function. Make private newModule function as it is only used within mssql package but not from any metricset db_integration_test.go rewritten to work with ReporterV2 Removed unused variable and fix typo Remove metricset io from this PR and added license headers Moved files to X-Pack folder Delete references to mssql
    Squash into a single commit after moved files to x-pack folder

---
+++ x-pack/metricbeat/module/mssql/db/db_integration_test.go
@@ -0,0 +1,61 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+// +build integration
+
+package db
+
+import (
+	mbtest "github.com/elastic/beats/metricbeat/mb/testing"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"github.com/elastic/beats/libbeat/tests/compose"
+)
+
+func TestFetch(t *testing.T) {
+	compose.EnsureUp(t, "mssql")
+
+	f := mbtest.NewReportingMetricSetV2(t, getConfig())
+	events, errs := mbtest.ReportingFetchV2(f)
+	assert.Empty(t, errs)
+	if !assert.NotEmpty(t, events) {
+		t.FailNow()
+	}
+
+	t.Logf("Module: %s Metricset: %s", f.Module().Name(), f.Name())
+
+	for _, event := range events {
+		userPercent, err := event.MetricSetFields.GetValue("log_space_usage.used_percent")
+		assert.NoError(t, err)
+		if userPercentFloat, ok := userPercent.(float64); !ok {
+			t.Fail()
+		} else {
+			assert.True(t, userPercentFloat > 0)
+		}
+	}
+}
+
+func getConfig() map[string]interface{} {
+	return map[string]interface{}{
+		"module":     "mssql",
+		"metricsets": []string{"db"},
+		"host":       "127.0.0.1",
+		"user":       "sa",
+		"password":   "1234_asdf",
+		"port":       1433,
+	}
+}
