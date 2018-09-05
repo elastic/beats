2f57c641a577c93149690f27f65d406369a9345d
Shaunak Kashyap <ycombinator@gmail.com>
2018-09-05 10:01:37 -0700

    Allow a Beat to override monitoring-related fields (#7850)
    * Adding TODOs
    
    * Allow a beat to override monitoring-related fields
    
    * Disallowing override of system_api_version (until we need it)
    
    * Fixing datatype
    
    * Create Settings structs for monitoring-specific info
    
    * Changes from review
    
    * Make runFlags part of settings struct
    
    * Adding comments
    
    * Adding developer changelog entry
    
    * Adding default for index prefix
    
    * Fixing deprecated note markers
    
    * Reuse report.Settings
    
    * Extract beat.Info back out of Settings struct
    
    * Updating generator template
    
    * Tabs, not spaces :eyeroll:
    
    (cherry picked from commit 74cbb7949f4d85b2fc67889e4b39394fb1b088b1)

--- libbeat/cmd/instance/settings.go
+++ libbeat/cmd/instance/settings.go
@@ -15,21 +15,19 @@
 // specific language governing permissions and limitations
 // under the License.
 
-package beat
+package instance
 
-import "github.com/satori/go.uuid"
+import (
+	"github.com/spf13/pflag"
 
-// Info stores a beats instance meta data.
-type Info struct {
-	Beat        string    // The actual beat's name
-	IndexPrefix string    // The beat's index prefix in Elasticsearch.
-	Version     string    // The beat version. Defaults to the libbeat version when an implementation does not set a version
-	Name        string    // configured beat name
-	Hostname    string    // hostname
-	UUID        uuid.UUID // ID assigned to beat instance
+	"github.com/elastic/beats/libbeat/monitoring/report"
+)
 
-	// Monitoring-related fields
-	Monitoring struct {
-		DefaultUsername string // The default username to be used to connect to Elasticsearch Monitoring
-	}
+// Settings contains basic settings for any beat to pass into GenRootCmd
+type Settings struct {
+	Name        string
+	IndexPrefix string
+	Version     string
+	Monitoring  report.Settings
+	RunFlags    *pflag.FlagSet
 }
