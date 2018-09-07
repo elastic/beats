300ebcbb98662bd0847e2a24fd1c0593012ff748
Gil Raphaelli <g@raphaelli.com>
2018-09-07 11:06:29 -0400

    Abort with non zero exit code on errors

--- dev-tools/jenkins_ci.ps1
+++ dev-tools/jenkins_ci.ps1
@@ -1,14 +1,20 @@
-function Exec
-{
+function Exec {
+    [CmdletBinding()]
     param(
-        [Parameter(Position=0,Mandatory=1)][scriptblock]$cmd,
-        [Parameter(Position=1,Mandatory=0)][string]$errorMessage = ($msgs.error_bad_command -f $cmd)
+        [Parameter(Mandatory = $true)]
+        [scriptblock]$cmd,
+        [string]$errorMessage = ($msgs.error_bad_command -f $cmd)
     )
 
-    & $cmd
-    if ($LastExitCode -ne 0) {
-        Write-Error $errorMessage
-        exit $LastExitCode
+    try {
+        $global:lastexitcode = 0
+        & $cmd
+        if ($lastexitcode -ne 0) {
+            throw $errorMessage
+        }
+    }
+    catch [Exception] {
+        throw $_
     }
 }
 
