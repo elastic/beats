8f18a0475565ca7e07842074b306ba7558e07e17
Shaunak Kashyap <ycombinator@gmail.com>
2018-09-06 17:05:58 -0700

    Un-hardcode index name

--- metricbeat/module/elasticsearch/index_summary/data_xpack.go
+++ metricbeat/module/elasticsearch/index_summary/data_xpack.go
@@ -24,6 +24,7 @@ import (
 	"github.com/elastic/beats/libbeat/common"
 	s "github.com/elastic/beats/libbeat/common/schema"
 	c "github.com/elastic/beats/libbeat/common/schema/mapstriface"
+	"github.com/elastic/beats/metricbeat/helper/elastic"
 	"github.com/elastic/beats/metricbeat/mb"
 	"github.com/elastic/beats/metricbeat/module/elasticsearch"
 )
@@ -105,7 +106,7 @@ func eventMappingXPack(r mb.ReporterV2, m *MetricSet, info elasticsearch.Info, c
 	event.RootFields.Put("type", "indices_stats")
 	event.RootFields.Put("source_node", sourceNode)
 
-	event.Index = ".monitoring-es-6-mb"
+	event.Index = elastic.MakeXPackMonitoringIndexName(elastic.Elasticsearch)
 
 	r.Event(event)
 
