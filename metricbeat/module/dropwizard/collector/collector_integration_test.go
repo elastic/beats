9a21006f0487b9f428d393bdc8acb50983df3438
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2018-09-11 13:49:50 +0200

    Run dropwizard tests with testrunner

--- metricbeat/module/dropwizard/collector/collector_integration_test.go
+++ metricbeat/module/dropwizard/collector/collector_integration_test.go
@@ -20,7 +20,6 @@
 package collector
 
 import (
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -30,83 +29,68 @@ import (
 	mbtest "github.com/elastic/beats/metricbeat/mb/testing"
 )
 
-func TestFetch(t *testing.T) {
-	compose.EnsureUp(t, "dropwizard")
+func TestCollector(t *testing.T) {
+	t.Parallel()
 
-	f := mbtest.NewEventsFetcher(t, getConfig())
-	events, err := f.Fetch()
+	runner := compose.TestRunner{Service: "dropwizard"}
 
-	hasTag := false
-	doesntHaveTag := false
-	for _, event := range events {
+	runner.Run(t, compose.Suite{
+		"Fetch": func(t *testing.T, r compose.R) {
+			f := mbtest.NewEventsFetcher(t, getConfig(r.Host()))
+			events, err := f.Fetch()
 
-		ok, _ := event.HasKey("my_histogram")
-		if ok {
-			_, err := event.GetValue("tags")
-			if err == nil {
-				t.Fatal("write", "my_counter not supposed to have tags")
-			}
-			doesntHaveTag = true
-		}
+			hasTag := false
+			doesntHaveTag := false
+			for _, event := range events {
 
-		ok, _ = event.HasKey("my_counter")
-		if ok {
-			tagsRaw, err := event.GetValue("tags")
-			if err != nil {
-				t.Fatal("write", err)
-			} else {
-				tags, ok := tagsRaw.(common.MapStr)
-				if !ok {
-					t.Fatal("write", "unable to cast tags to common.MapStr")
-				} else {
-					assert.Equal(t, len(tags), 1)
-					hasTag = true
+				ok, _ := event.HasKey("my_histogram")
+				if ok {
+					_, err := event.GetValue("tags")
+					if err == nil {
+						t.Fatal("write", "my_counter not supposed to have tags")
+					}
+					doesntHaveTag = true
+				}
+
+				ok, _ = event.HasKey("my_counter")
+				if ok {
+					tagsRaw, err := event.GetValue("tags")
+					if err != nil {
+						t.Fatal("write", err)
+					} else {
+						tags, ok := tagsRaw.(common.MapStr)
+						if !ok {
+							t.Fatal("write", "unable to cast tags to common.MapStr")
+						} else {
+							assert.Equal(t, len(tags), 1)
+							hasTag = true
+						}
+					}
 				}
 			}
-		}
-	}
-	assert.Equal(t, hasTag, true)
-	assert.Equal(t, doesntHaveTag, true)
-	if !assert.NoError(t, err) {
-		t.FailNow()
-	}
+			assert.Equal(t, hasTag, true)
+			assert.Equal(t, doesntHaveTag, true)
+			if !assert.NoError(t, err) {
+				t.FailNow()
+			}
 
-	t.Logf("%s/%s event: %+v", f.Module().Name(), f.Name(), events)
+			t.Logf("%s/%s event: %+v", f.Module().Name(), f.Name(), events)
+		},
+		"Data": func(t *testing.T, r compose.R) {
+			f := mbtest.NewEventsFetcher(t, getConfig(r.Host()))
+			err := mbtest.WriteEvents(f, t)
+			if err != nil {
+				t.Fatal("write", err)
+			}
+		},
+	})
 }
 
-func TestData(t *testing.T) {
-	compose.EnsureUp(t, "dropwizard")
-
-	f := mbtest.NewEventsFetcher(t, getConfig())
-	err := mbtest.WriteEvents(f, t)
-	if err != nil {
-		t.Fatal("write", err)
-	}
-}
-
-func getEnvHost() string {
-	host := os.Getenv("DROPWIZARD_HOST")
-
-	if len(host) == 0 {
-		host = "127.0.0.1"
-	}
-	return host
-}
-
-func getEnvPort() string {
-	port := os.Getenv("DROPWIZARD_PORT")
-
-	if len(port) == 0 {
-		port = "8080"
-	}
-	return port
-}
-
-func getConfig() map[string]interface{} {
+func getConfig(host string) map[string]interface{} {
 	return map[string]interface{}{
 		"module":       "dropwizard",
 		"metricsets":   []string{"collector"},
-		"hosts":        []string{getEnvHost() + ":" + getEnvPort()},
+		"hosts":        []string{host},
 		"namespace":    "testnamespace",
 		"metrics_path": "/test/metrics",
 		"enabled":      true,
