88930732043c37dedbb491bb3d1b10aac6fac46d
Adrian Serrano <adrisr83@gmail.com>
2018-09-12 12:58:50 +0200

    Introducing the add_process_metadata processor (#6789) (#8066)
    The `add_process_metadata` processor allows enriching events based on
    process information obtained from a process ID.
    
    ```
    processors:
        - add_process_metadata:
            match_pids: [ system.process.ppid ]
            target_fields:
                system.process.parent: ~
    	    system.process.parent_cmdline: process.title
    ```
    
    Internally it generates the following document:
    
    ```
    "process": {
      "name":  "systemd",
      "title": "/usr/lib/systemd/systemd --switched-root --system --deserialize 22",
      "exe":   "/usr/lib/systemd/systemd",
      "args":  ["/usr/lib/systemd/systemd", "--switched-root", "--system", "--deserialize", "22"],
      "env": {
    	"HOME":       "/",
            "TERM":       "linux",
            "BOOT_IMAGE": "/boot/vmlinuz-4.11.8-300.fc26.x86_64",
            "LANG":       "en_US.UTF-8",
      },
      "pid:    1,
      "ppid:   0,
      "start_time": "2018-08-22T08:44:50.684Z",
    }
    ```
    
    Configuration options
    
    - match_pids
    
    List of fields to lookup for a PID. The processor will search the list
    sequentially until the field is found in the current event, and the PID
    lookup will be applied to the value of this field.
    
    - target_fields
    
    Map of destination fields to populate with process information. For example
    `a: process.args` will create the `a` field with the value of `process.args`.
    
    To save all the generated document in an object field, you can use
    `field: process` or `field: ~`.
    
    - ignore_missing
    
    When set to false, events that don't contain any of the fields in `match_pids`
    will cause an error.
    
    - overwrite_fields
    
    By default, if a target field already exists, it will not be overwritten
    and an error will be logged. If `overwrite_fields` is set to `true`, this
    condition will be ignored.
    
    - restricted_fields
    
    By default, the `process.env` field is never output, to avoid leaking
    sensitive data. If `restricted_fields` is true, the field will be available.

--- libbeat/processors/add_process_metadata/test_provider.go
+++ libbeat/processors/add_process_metadata/test_provider.go
@@ -6,7 +6,7 @@
 // not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-// http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing,
 // software distributed under the License is distributed on an
@@ -15,9 +15,15 @@
 // specific language governing permissions and limitations
 // under the License.
 
-// Package windows contains various Windows system calls.
-package windows
+package add_process_metadata
 
-// Use "GOOS=windows go generate -v -x" to generate the sources.
-// Add -trace to enable debug prints around syscalls.
-//go:generate go run $GOROOT/src/syscall/mksyscall_windows.go -systemdll=false -output zsyscall_windows.go kernel32.go version.go psapi.go ntdll.go
+type testProvider map[int]processMetadata
+
+func (p testProvider) GetProcessMetadata(pid int) (result *processMetadata, err error) {
+	meta, found := p[pid]
+	if !found {
+		return nil, nil
+	}
+	meta.fields = meta.toMap()
+	return &meta, nil
+}
