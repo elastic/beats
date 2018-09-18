4bd1205894d4d6737b8df1e585f86418765e1f25
Andrew Kroh <andrew.kroh@elastic.co>
2018-09-18 08:34:33 -0400

    Add skeleton x-pack Auditbeat module (#8252)
    This adds an skeleton x-pack module to Auditbeat. The module is only included in the Elastic licensed Auditbeat binary.
    
    The config and fields.yml data are not yet included in the packaging. Additional updates are required.

---
+++ x-pack/auditbeat/module/sysinfo/host/host.go
@@ -0,0 +1,50 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package host
+
+import (
+	"github.com/pkg/errors"
+
+	"github.com/elastic/beats/libbeat/common"
+	"github.com/elastic/beats/libbeat/common/cfgwarn"
+	"github.com/elastic/beats/metricbeat/mb"
+)
+
+const (
+	moduleName    = "sysinfo"
+	metricsetName = "host"
+)
+
+func init() {
+	mb.Registry.MustAddMetricSet(moduleName, metricsetName, New,
+		mb.DefaultMetricSet(),
+	)
+}
+
+// MetricSet collects data about the host.
+type MetricSet struct {
+	mb.BaseMetricSet
+}
+
+// New constructs a new MetricSet.
+func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
+	cfgwarn.Experimental("The %v/%v dataset is experimental", moduleName, metricsetName)
+
+	config := defaultConfig
+	if err := base.Module().UnpackConfig(&config); err != nil {
+		return nil, errors.Wrapf(err, "failed to unpack the %v/%v config", moduleName, metricsetName)
+	}
+
+	return &MetricSet{base}, nil
+}
+
+// Fetch collects data about the host. It is invoked periodically.
+func (ms *MetricSet) Fetch(report mb.ReporterV2) {
+	report.Event(mb.Event{
+		RootFields: common.MapStr{
+			"hello": "world",
+		},
+	})
+}
