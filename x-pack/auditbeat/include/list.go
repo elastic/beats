ef247ed38747b13c63d7e78d238401ae459da5fb
Andrew Kroh <andrew.kroh@elastic.co>
2018-09-18 08:34:33 -0400

    Add skeleton x-pack Auditbeat module (#8252)
    This adds an skeleton x-pack module to Auditbeat. The module is only included in the Elastic licensed Auditbeat binary.
    
    The config and fields.yml data are not yet included in the packaging. Additional updates are required.

---
+++ x-pack/auditbeat/include/list.go
@@ -0,0 +1,11 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package include
+
+import (
+	// Include all Auditbeat modules so that they register their
+	// factories with the global registry.
+	_ "github.com/elastic/beats/x-pack/auditbeat/module/sysinfo/host"
+)
