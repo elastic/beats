29385e3da0c3cb5d0a205f786314ed4d0996efe2
Carlos Pérez-Aradros Herce <carlos@elastic.co>
2018-09-25 18:19:35 +0200

    Use beat name var when registering confs
    This should make Auditbeat or any other beat based on Metricbeat have
    their own namespace for confs

--- metricbeat/beater/metricbeat.go
+++ metricbeat/beater/metricbeat.go
@@ -210,7 +210,7 @@ func (bt *Metricbeat) Run(b *beat.Beat) error {
 	// Centrally managed modules
 	factory := module.NewFactory(bt.moduleOptions...)
 	modules := cfgfile.NewRunnerList(management.DebugK, factory, b.Publisher)
-	reload.Register.MustRegisterList("metricbeat.modules", modules)
+	reload.Register.MustRegisterList(b.Info.Beat+".modules", modules)
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
