7365320e0860a3b46f8fc4b7c02d6c81870b8965
Carlos Pérez-Aradros Herce <carlos@elastic.co>
2018-09-28 14:34:57 +0200

    Allow user/passwd based enrollment
    This allows to enroll using the following workflow:
    
    ```
    $ <beat> enroll http://kibana:5601 --username elastic
    Enter password:
    
    Enrolled and ready to retrieve settings from Kibana
    ```
    
    It also allows to pass the password as an env variable:
    
    ```
    PASS=...
    $ <beat> enroll http://kibana:5601 --username elastic --password env:PASS
    
    Enrolled and ready to retrieve settings from Kibana
    ```

--- x-pack/libbeat/cmd/enroll.go
+++ x-pack/libbeat/cmd/enroll.go
@@ -13,6 +13,7 @@ import (
 	"github.com/elastic/beats/libbeat/cmd/instance"
 	"github.com/elastic/beats/libbeat/common/cli"
 	"github.com/elastic/beats/x-pack/libbeat/management"
+	"github.com/elastic/beats/x-pack/libbeat/management/api"
 )
 
 func getBeat(name, version string) (*instance.Beat, error) {
@@ -30,16 +31,52 @@ func getBeat(name, version string) (*instance.Beat, error) {
 }
 
 func genEnrollCmd(name, version string) *cobra.Command {
+	var username, password string
+
 	enrollCmd := cobra.Command{
-		Use:   "enroll <kibana_url> <enrollment_token>",
+		Use:   "enroll <kibana_url> [<enrollment_token>]",
 		Short: "Enroll in Kibana for Central Management",
-		Args:  cobra.ExactArgs(2),
+		Long: `This will enroll in  Kibana Beats Central Management. If you pass an enrollment token
+		it will be used. You can also enroll using a username and password combination.`,
+		Args: cobra.RangeArgs(1, 2),
 		Run: cli.RunWith(func(cmd *cobra.Command, args []string) error {
 			beat, err := getBeat(name, version)
 			kibanaURL := args[0]
-			enrollmentToken := args[1]
-			if err != nil {
-				return err
+
+			if username == "" && len(args) == 1 {
+				return errors.New("You should pass either an enrollment token or use --username flag")
+			}
+
+			var enrollmentToken string
+			if len(args) == 2 {
+				// use given enrollment token
+				enrollmentToken = args[1]
+				if err != nil {
+					return err
+				}
+			} else {
+				// retrieve an enrollment token using username/password
+				config, err := api.ConfigFromURL(kibanaURL)
+				if err != nil {
+					return err
+				}
+
+				// pass username/password
+				config.IgnoreVersion = true
+				config.Username = username
+				config.Password, err = cli.ReadPassword(password)
+				if err != nil {
+					return err
+				}
+
+				client, err := api.NewClient(config)
+				if err != nil {
+					return err
+				}
+				enrollmentToken, err = client.CreateEnrollmentToken()
+				if err != nil {
+					return errors.Wrap(err, "Creating a new enrollment token")
+				}
 			}
 
 			if err = management.Enroll(beat, kibanaURL, enrollmentToken); err != nil {
@@ -50,6 +87,9 @@ func genEnrollCmd(name, version string) *cobra.Command {
 			return nil
 		}),
 	}
+
+	enrollCmd.Flags().StringVar(&username, "username", "elastic", "Username to use when enrolling without token")
+	enrollCmd.Flags().StringVar(&password, "password", "stdin", "Method to read the password to use when enrolling without token (stdin or env:VAR_NAME)")
 
 	return &enrollCmd
 }
