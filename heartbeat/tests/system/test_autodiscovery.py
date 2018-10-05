2b8a71110273c5cea3e1297854cc7b0cd0e00764
Andrew Cholakian <andrew@andrewvc.com>
2018-10-05 09:35:13 -0500

    more debug

--- heartbeat/tests/system/test_autodiscovery.py
+++ heartbeat/tests/system/test_autodiscovery.py
@@ -38,7 +38,8 @@ class TestAutodiscover(BaseTest):
 
         proc = self.start_beat()
 
-        self.wait_until(lambda: self.log_contains(re.compile('autodiscover.+Got a start event:', re.I)))
+        self.wait_until(lambda: self.log_contains(
+            re.compile('autodiscover.+Got a start event:', re.I)))
 
         self.wait_until(lambda: self.output_count(lambda x: x >= 1))
 
@@ -48,16 +49,16 @@ class TestAutodiscover(BaseTest):
         matched = False
         matcher = re.compile("redis", re.I)
         for i, container in enumerate(docker_client.containers.list()):
-            print "Search for redis in: {} ".format(container.image.tags)
             for tag in container.image.tags:
                 if matcher.search(tag):
-                    print "matched tag: {}".format(tag)
                     network_settings = container.attrs['NetworkSettings']
-                    host = network_settings['IPAddress']
+                    host = network_settings['Networks'].values()[
+                        0]['IPAddress']
                     port = network_settings['Ports'].keys()[0].split("/")[0]
                     # Check metadata is added
                     expected = 'tcp-tcp@%s:%s' % (host, port)
-                    if expected == output[0]['monitor']['id']:
+                    actual = output[0]['monitor']['id']
+                    if expected == actual:
                         matched = True
 
         assert matched
