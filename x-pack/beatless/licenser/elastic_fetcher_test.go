e07106ca6fe2c5474bbf6d2eb6286797bdf90344
Pier-Hugues Pellerin <phpellerin@gmail.com>
2018-10-18 09:25:57 -0400

    Update for new UUID library

--- x-pack/beatless/licenser/elastic_fetcher_test.go
+++ x-pack/beatless/licenser/elastic_fetcher_test.go
@@ -14,7 +14,7 @@ import (
 	"testing"
 	"time"
 
-	uuid "github.com/satori/go.uuid"
+	"github.com/gofrs/uuid"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/elastic/beats/libbeat/outputs/elasticsearch"
