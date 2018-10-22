bd0978868c84f38387b0e28820d2debb5ca1fae8
Carlos Pérez-Aradros Herce <exekias@gmail.com>
2018-10-22 22:33:19 +0100

    Allow starting the beat without an output when CM is enabled  (#8567) (#8657)
    * Allow starting the beat without an output when CM is enabled
    
    Central management will take care of configuring the output, so we
    are allowed to start the beat with a nil output
    
    (cherry picked from commit 1012302e57eaf46d7dfe9244e85a719afb592d09)

--- libbeat/cmd/instance/beat.go
+++ libbeat/cmd/instance/beat.go
@@ -21,6 +21,7 @@ import (
 	"context"
 	cryptRand "crypto/rand"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"math"
@@ -309,6 +310,18 @@ func (b *Beat) createBeater(bt beat.Creator) (beat.Beater, error) {
 	}
 
 	debugf("Initializing output plugins")
+
+	outputEnabled := b.Config.Output.IsSet() && b.Config.Output.Config().Enabled()
+	if !outputEnabled {
+		if b.ConfigManager.Enabled() {
+			logp.Info("Output is configured through Central Management")
+		} else {
+			msg := "No outputs are defined. Please define one under the output section."
+			logp.Info(msg)
+			return nil, errors.New(msg)
+		}
+	}
+
 	pipeline, err := pipeline.Load(b.Info,
 		pipeline.Monitors{
 			Metrics:   reg,
