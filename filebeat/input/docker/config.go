57420abb68b7720a7dcab442b9ad216e2285dc3d
Carlos Pérez-Aradros Herce <exekias@gmail.com>
2018-10-23 22:04:55 +0100

    Add CHANGELOG and docs for CRI force flag (#8699)
    (cherry picked from commit 06ec3b9c9394bdda9592f34fe7705b48802f9123)

--- filebeat/input/docker/config.go
+++ filebeat/input/docker/config.go
@@ -34,6 +34,9 @@ type config struct {
 
 	// Enable CRI flags parsing (to be switched to default in 7.0)
 	CRIFlags bool `config:"cri.parse_flags"`
+
+	// Fore CRI format (don't perform autodetection)
+	CRIForce bool `config:"cri.force"`
 }
 
 type containers struct {
