3e1b03ecf06080b3196753951e5c75b0f5cbaa9f
Andrew Kroh <andrew.kroh@elastic.co>
2018-10-24 08:15:17 -0400

    Add Suricata module to Filebeat (#8693)
    This adds a Filebeat module for ingesting the logs created by Suricata IDS/IPS/NSM. The module collects the logs from the Suricata Eve JSON output (https://suricata.readthedocs.io/en/latest/output/eve/eve-json-format.html). #8153
    
    The module is included in the Elastic licensed Filebeat package. It is considered beta at this stage. It includes two sample dashboards.
    
    It uses Elastic Common Schema (ECS) for field naming were applicable, but it has to work-around the conflict with the existing source field in Filebeat. source_ecs holds the data that goes into the ECS source field. This will be rectified in the next major release when we can make a breaking change.
    
    The development tooling for the building/testing/packaging of x-pack modules is still a bit of a WIP. So at the moment testing and packaging continues to happen through the OSS filebeat directory.
    
    Co-authored-by: Adrian Serrano <adrisr83@gmail.com>
    Co-authored-by: Andrew Kroh <andrew.kroh@elastic.co>
    Co-authored-by: Mathieu Martin <mathieu.martin@elastic.co>
    Co-authored-by: Tudor Golubenco <tudor@elastic.co>

---
+++ dev-tools/mage/kibana.go
@@ -0,0 +1,89 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package mage
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/magefile/mage/sh"
+	"github.com/pkg/errors"
+)
+
+// KibanaDashboards collects the Kibana dashboards files and generates the
+// index patterns based on the fields.yml file. It outputs to build/kibana.
+func KibanaDashboards(moduleDirs ...string) error {
+	var kibanaBuildDir = "build/kibana"
+
+	if err := os.MkdirAll(kibanaBuildDir, 0755); err != nil {
+		return err
+	}
+
+	// Copy the OSS Beat's common dashboards if they exist. This assumes that
+	// X-Pack Beats only add dashboards with modules (this will require a
+	// change if we have X-Pack only Beats).
+	cp := &CopyTask{Source: OSSBeatDir("_meta/kibana"), Dest: kibanaBuildDir}
+	if err := cp.Execute(); err != nil && !os.IsNotExist(errors.Cause(err)) {
+		return err
+	}
+
+	// Copy dashboards from modules.
+	for _, dir := range moduleDirs {
+		kibanaDirs, err := filepath.Glob(filepath.Join(dir, "*/_meta/kibana"))
+		if err != nil {
+			return err
+		}
+
+		for _, kibanaDir := range kibanaDirs {
+			cp := &CopyTask{Source: kibanaDir, Dest: kibanaBuildDir}
+			if err = cp.Execute(); err != nil {
+				return err
+			}
+		}
+	}
+
+	esBeatsDir, err := ElasticBeatsDir()
+	if err != nil {
+		return err
+	}
+
+	// Convert 6.x dashboards to strings.
+	err = sh.Run("python",
+		filepath.Join(esBeatsDir, "libbeat/scripts/unpack_dashboards.py"),
+		"--glob="+filepath.Join(kibanaBuildDir, "6/dashboard/*.json"))
+	if err != nil {
+		return err
+	}
+
+	beatVersion, err := BeatQualifiedVersion()
+	if err != nil {
+		return err
+	}
+
+	// Generate Kibana index pattern files from fields.yml.
+	indexPatternCmd := sh.RunCmd("go", "run",
+		filepath.Join(esBeatsDir, "dev-tools/cmd/kibana_index_pattern/kibana_index_pattern.go"),
+		"-beat", BeatName,
+		"-version", beatVersion,
+		"-index", BeatIndexPrefix+"-*",
+		"-fields", "fields.yml",
+		"-out", kibanaBuildDir,
+	)
+
+	return indexPatternCmd()
+}
