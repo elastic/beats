0b053adada4224dc053a325df07315af9b4b5070
Carlos Pérez-Aradros Herce <carlos@elastic.co>
2018-10-26 14:23:29 +0200

    Add test_mangement.py

---
+++ x-pack/libbeat/tests/system/test_management.py
@@ -0,0 +1,16 @@
+import sys
+import os
+
+
+sys.path.append(os.path.join(os.path.dirname(__file__),
+                             '../../../../libbeat/tests/system'))
+
+
+from base import BaseTest
+
+
+INTEGRATION_TESTS = os.environ.get('INTEGRATION_TESTS', False)
+
+
+class TestManagement(BaseTest):
+    pass
