3149d0cb68774b4302fc67c881bd97b8ff11c04f
Carlos Pérez-Aradros Herce <carlos@elastic.co>
2018-10-30 18:35:31 +0100

    Test enroll works

--- x-pack/libbeat/tests/system/test_management.py
+++ x-pack/libbeat/tests/system/test_management.py
@@ -1,11 +1,6 @@
 import sys
 import os
 
-
-sys.path.append(os.path.join(os.path.dirname(__file__),
-                             '../../../../libbeat/tests/system'))
-
-
 from base import BaseTest
 
 
@@ -14,5 +9,36 @@ INTEGRATION_TESTS = os.environ.get('INTEGRATION_TESTS', False)
 
 class TestManagement(BaseTest):
 
-    def test_broken(self):
-        assert False
+    def test_enroll(self):
+        """
+        Enroll the beat in Kibana Central Management
+        """
+
+        config_path = os.path.join(self.working_dir, "mockbeat.yml")
+        self.render_config_template("mockbeat", config_path)
+
+        config_content = open(config_path, 'r').read()
+
+        exit_code = self.run_beat(
+            extra_args=["enroll", self.get_kibana_url(),
+                        "--password", "env:PASS", "--force"],
+            logging_args=["-v", "-d", "*"],
+            env={
+                'PASS': 'changeme',
+            })
+
+        assert exit_code == 0
+        assert self.log_contains("Enrolled and ready to retrieve settings")
+
+        # Enroll creates a keystore (to store access token)
+        assert os.path.isfile(os.path.join(
+            self.working_dir, "mockbeat.keystore"))
+
+        # Settings backup has been created
+        assert os.path.isfile(os.path.join(
+            self.working_dir, "mockbeat.yml.bak"))
+        backup_content = open(config_path + ".bak", 'r').read()
+        assert config_content == backup_content
+
+    def get_kibana_url(self):
+        return 'http://' + os.getenv('KIBANA_HOST', 'kibana') + ':' + os.getenv('KIBANA_PORT', '5601')
