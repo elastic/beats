115f75eb635247a65c6672518ba8e2295f33ed62
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2018-11-08 11:40:51 +0100

    Add possible message to test check
    TestCommands.test_modules_test_error relays on the presence of an error
    message, this error message seems to be different under some
    circustances, as seen in CI builds.
    
    Add the timeout message already seen, and print logs in case of failure
    so it is more easy to fix if happens in the future.

--- metricbeat/tests/system/test_cmd.py
+++ metricbeat/tests/system/test_cmd.py
@@ -136,10 +136,15 @@ class TestCommands(metricbeat.BaseTest):
             extra_args=["test", "modules"])
 
         assert exit_code == 0
-        assert any((
-            self.log_contains("ERROR error making http request"),
-            self.log_contains("ERROR timeout waiting for event"),
-        ))
+        try:
+            assert any((
+                self.log_contains("ERROR error making http request"),
+                self.log_contains("ERROR timeout waiting for an event"),
+            ))
+        except:
+            # Print log to help debugging this if error message changes
+            print self.get_log()
+            raise
         assert self.log_contains("cpu...OK")
         assert self.log_contains("memory...OK")
 
