146dabeb7179cce367b554a9277e0c8b49e9bb54
urso <steffen.siering@elastic.co>
2018-11-27 23:26:54 +0100

    update formatting

--- libbeat/common/event_test.go
+++ libbeat/common/event_test.go
@@ -236,7 +236,7 @@ func TestNormalizeValue(t *testing.T) {
 	type myuint64 uint64
 
 	runTests(checkEq, map[string]testCase{
-		"nil": {nil, nil},
+		"nil":                               {nil, nil},
 		"pointers are dereferenced":         {&someString, someString},
 		"drop nil string pointer":           {nilStringPtr, nil},
 		"drop nil time pointer":             {nilTimePtr, nil},
