ab1714df2e969fb7534df931c63d79fe021c6907
Noémi Ványi <sitbackandwait@gmail.com>
2018-11-28 20:26:57 +0100

    Add cursor_seek_fallback option (#9234)
    New option is introduced to configure the fallback method of seek mode `"cursor"`. The option is named `cursor_seek_fallback`. The name is taken from the community Journalbeat: https://github.com/mheese/journalbeat/blob/master/config/journalbeat.yml#L3
    
    By default it seeks to the beginning as before. But from now on it is possible to configure it to start reading from the end of the journal.
    (cherry picked from commit f3cbc0a)

--- journalbeat/input/config.go
+++ journalbeat/input/config.go
@@ -30,14 +30,14 @@ type Config struct {
 	// Paths stores the paths to the journal files to be read.
 	Paths []string `config:"paths"`
 	// MaxBackoff is the limit of the backoff time.
-	Backoff time.Duration `config:"backoff" validate:"min=0,nonzero"`
+	MaxBackoff time.Duration `config:"max_backoff" validate:"min=0,nonzero"`
 	// Backoff is the current interval to wait before
 	// attemting to read again from the journal.
-	BackoffFactor int `config:"backoff_factor" validate:"min=1"`
-	// BackoffFactor is the multiplier of Backoff.
-	MaxBackoff time.Duration `config:"max_backoff" validate:"min=0,nonzero"`
+	Backoff time.Duration `config:"backoff" validate:"min=0,nonzero"`
 	// Seek is the method to read from journals.
 	Seek config.SeekMode `config:"seek"`
+	// CursorSeekFallback sets where to seek if registry file is not available.
+	CursorSeekFallback config.SeekMode `config:"cursor_seek_fallback"`
 	// Matches store the key value pairs to match entries.
 	Matches []string `config:"include_matches"`
 
@@ -50,9 +50,9 @@ type Config struct {
 var (
 	// DefaultConfig is the defaults for an inputs
 	DefaultConfig = Config{
-		Backoff:       1 * time.Second,
-		BackoffFactor: 2,
-		MaxBackoff:    60 * time.Second,
-		Seek:          config.SeekCursor,
+		Backoff:            1 * time.Second,
+		MaxBackoff:         60 * time.Second,
+		Seek:               config.SeekCursor,
+		CursorSeekFallback: config.SeekHead,
 	}
 )
