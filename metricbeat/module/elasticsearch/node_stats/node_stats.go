1ceeb8792ddd18a27d469968bbba1be0ba81d6ba
Shaunak Kashyap <ycombinator@gmail.com>
2018-11-28 00:51:31 -0800

    Cherry-pick #9168 to 6.x: Index cluster.id, node.id, and node.name in elasticsearch/node_stats metricset (#9255)
    Cherry-pick of PR #9168 to 6.x branch. Original message: 
    
    This PR teaches the `elasticsearch/node_stats` metricset to index the Elasticsearch `cluster_uuid` as the module-level `cluster.id` field, as well as the node ID and node `name` fields as the metricset-level `node.id` and `node.name` fields, respectively.

--- metricbeat/module/elasticsearch/node_stats/node_stats.go
+++ metricbeat/module/elasticsearch/node_stats/node_stats.go
@@ -63,17 +63,27 @@ func (m *MetricSet) Fetch(r mb.ReporterV2) {
 		return
 	}
 
+	info, err := elasticsearch.GetInfo(m.HTTP, m.getServiceURI())
+	if err != nil {
+		elastic.ReportAndLogError(err, r, m.Log)
+		return
+	}
+
 	if m.XPack {
-		err = eventsMappingXPack(r, m, content)
+		err = eventsMappingXPack(r, m, *info, content)
 		if err != nil {
 			m.Log.Error(err)
 			return
 		}
 	} else {
-		err = eventsMapping(r, content)
+		err = eventsMapping(r, *info, content)
 		if err != nil {
 			elastic.ReportAndLogError(err, r, m.Log)
 			return
 		}
 	}
+}
+
+func (m *MetricSet) getServiceURI() string {
+	return m.HostData().SanitizedURI + nodeStatsPath
 }
