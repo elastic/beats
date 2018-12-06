223da9c1e3ae3efdaf624a796f5d77ea0a44e95d
Christoph Wurm <cw@elastic.co>
2018-12-06 14:13:16 +0100

    Add reportChanges().

--- x-pack/auditbeat/module/system/host/host.go
+++ x-pack/auditbeat/module/system/host/host.go
@@ -8,6 +8,7 @@ import (
 	"net"
 	"time"
 
+	"github.com/OneOfOne/xxhash"
 	"github.com/joeshaw/multierror"
 	"github.com/pkg/errors"
 
@@ -24,15 +25,87 @@ const (
 	metricsetName = "host"
 
 	eventTypeState = "state"
+	eventTypeEvent = "event"
 
-	eventActionHost = "host"
+	eventActionHost        = "host"
+	eventActionIdChanged   = "host_id_changed"
+	eventActionReboot      = "reboot"
+	eventActionHostChanged = "host_changed"
 )
 
 // Host represents information about a host.
 type Host struct {
-	info  types.HostInfo
-	addrs []net.Addr
-	macs  []net.HardwareAddr
+	info types.HostInfo
+	// Uptime() in types.HostInfo recalculates the uptime every time it is called -
+	// so storing it permanently here.
+	uptime time.Duration
+	addrs  []net.Addr
+	macs   []net.HardwareAddr
+}
+
+// changeDetectionHash creates a hash of selected parts of the host information.
+// This is used later to detect changes to a host over time.
+func (host *Host) changeDetectionHash() uint64 {
+	h := xxhash.New64()
+
+	mapstr := host.toMapStr()
+
+	// We detect changes to the ID separately, so ignore here.
+	mapstr.Delete("id")
+
+	// Changes in uptime and boottime indicate a system reboot,
+	// which we report separately.
+	mapstr.Delete("uptime")
+	mapstr.Delete("boottime")
+
+	h.WriteString(mapstr.String())
+	return h.Sum64()
+}
+
+func (host *Host) toMapStr() common.MapStr {
+	mapstr := common.MapStr{
+		// https://github.com/elastic/ecs#-host-fields
+		"uptime":              host.uptime,
+		"boottime":            host.info.BootTime,
+		"containerized":       host.info.Containerized,
+		"timezone.name":       host.info.Timezone,
+		"timezone.offset.sec": host.info.TimezoneOffsetSec,
+		"name":                host.info.Hostname,
+		"id":                  host.info.UniqueID,
+		// TODO "host.type": ?
+		"architecture": host.info.Architecture,
+
+		// https://github.com/elastic/ecs#-operating-system-fields
+		"os": common.MapStr{
+			"platform": host.info.OS.Platform,
+			"name":     host.info.OS.Name,
+			"family":   host.info.OS.Family,
+			"version":  host.info.OS.Version,
+			"kernel":   host.info.KernelVersion,
+		},
+	}
+
+	var ipStrings []string
+	for _, addr := range host.addrs {
+		switch v := addr.(type) {
+		case *net.IPNet:
+			ipStrings = append(ipStrings, v.IP.String())
+		case *net.IPAddr:
+			ipStrings = append(ipStrings, v.IP.String())
+		}
+	}
+	mapstr.Put("ip", ipStrings)
+
+	var macStrings []string
+	for _, mac := range host.macs {
+		macStr := mac.String()
+		if macStr != "" {
+			macStrings = append(macStrings, macStr)
+		}
+	}
+	mapstr.Put("mac", macStrings)
+
+	return mapstr
 }
 
 func init() {
@@ -47,6 +120,7 @@ type MetricSet struct {
 	config    config
 	log       *logp.Logger
 	lastState time.Time
+	lastHost  *Host
 }
 
 // New constructs a new MetricSet.
@@ -77,6 +151,12 @@ func (ms *MetricSet) Fetch(report mb.ReporterV2) {
 		}
 		ms.log.Debugf("Next state update by %v", ms.lastState.Add(ms.config.effectiveStatePeriod()))
 	}
+
+	err := ms.reportChanges(report)
+	if err != nil {
+		ms.log.Error(err)
+		report.Error(err)
+	}
 }
 
 // reportState reports the current state of the host.
@@ -93,6 +173,50 @@ func (ms *MetricSet) reportState(report mb.ReporterV2) error {
 	return nil
 }
 
+// reportChanges detects and reports any changes to this host since the last call.
+func (ms *MetricSet) reportChanges(report mb.ReporterV2) error {
+	currentHost, err := getHost()
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		ms.lastHost = currentHost
+	}()
+
+	if ms.lastHost == nil {
+		// First run - no changes possible
+		return nil
+	}
+
+	// Report ID changes as a separate, special event.
+	if ms.lastHost.info.UniqueID != currentHost.info.UniqueID {
+		/*
+		 Issue two events - one for the host with the old ID, one for the new
+		 - to link them (since the unique ID is what identifies a host).
+		*/
+		eventOldHost := hostEvent(ms.lastHost, eventTypeEvent, eventActionIdChanged)
+		eventOldHost.MetricSetFields.Put("new_id", currentHost.info.UniqueID)
+		report.Event(eventOldHost)
+
+		eventNewHost := hostEvent(currentHost, eventTypeEvent, eventActionIdChanged)
+		eventNewHost.MetricSetFields.Put("old_id", ms.lastHost.info.UniqueID)
+		report.Event(eventNewHost)
+	}
+
+	// Report reboots separately
+	if !currentHost.info.BootTime.Equal(ms.lastHost.info.BootTime) {
+		report.Event(hostEvent(currentHost, eventTypeEvent, eventActionReboot))
+	}
+
+	// Report any other changes.
+	if ms.lastHost.changeDetectionHash() != currentHost.changeDetectionHash() {
+		report.Event(hostEvent(currentHost, eventTypeEvent, eventActionHostChanged))
+	}
+
+	return nil
+}
+
 func getHost() (*Host, error) {
 	sysinfoHost, err := sysinfo.Host()
 	if err != nil {
@@ -105,66 +229,25 @@ func getHost() (*Host, error) {
 	}
 
 	host := &Host{
-		info:  sysinfoHost.Info(),
-		addrs: addrs,
-		macs:  macs,
+		info:   sysinfoHost.Info(),
+		uptime: sysinfoHost.Info().Uptime(),
+		addrs:  addrs,
+		macs:   macs,
 	}
 
 	return host, nil
 }
 
 func hostEvent(host *Host, eventType string, eventAction string) mb.Event {
-	event := mb.Event{
+	return mb.Event{
 		RootFields: common.MapStr{
 			"event": common.MapStr{
 				"type":   eventType,
 				"action": eventAction,
 			},
 		},
-		MetricSetFields: common.MapStr{
-			// https://github.com/elastic/ecs#-host-fields
-			"uptime":              host.info.Uptime(),
-			"boottime":            host.info.BootTime,
-			"containerized":       host.info.Containerized,
-			"timezone.name":       host.info.Timezone,
-			"timezone.offset.sec": host.info.TimezoneOffsetSec,
-			"name":                host.info.Hostname,
-			"id":                  host.info.UniqueID,
-			// TODO "host.type": ?
-			"architecture": host.info.Architecture,
-
-			// https://github.com/elastic/ecs#-operating-system-fields
-			"os": common.MapStr{
-				"platform": host.info.OS.Platform,
-				"name":     host.info.OS.Name,
-				"family":   host.info.OS.Family,
-				"version":  host.info.OS.Version,
-				"kernel":   host.info.KernelVersion,
-			},
-		},
+		MetricSetFields: host.toMapStr(),
 	}
-
-	var ipStrings []string
-	for _, addr := range host.addrs {
-		switch v := addr.(type) {
-		case *net.IPNet:
-			ipStrings = append(ipStrings, v.IP.String())
-		case *net.IPAddr:
-			ipStrings = append(ipStrings, v.IP.String())
-		}
-	}
-	event.MetricSetFields.Put("ip", ipStrings)
-
-	var macStrings []string
-	for _, mac := range host.macs {
-		macStr := mac.String()
-		if macStr != "" {
-			macStrings = append(macStrings, macStr)
-		}
-	}
-	event.MetricSetFields.Put("mac", macStrings)
-
-	return event
 }
 
 // getNetInfo is originally copied from libbeat/processors/add_host_metadata.go.
