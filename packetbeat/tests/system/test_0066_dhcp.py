6a4ce43b62d1972922007725b15d23000a4aa944
Andrew Kroh <andrew.kroh@elastic.co>
2018-12-12 06:23:42 -0800

    Restructure client/server and process fields (#9303)
    Changed
    
    - client_ip - client.ip (changed to ip type)
    - client_port - client.port
    - client_server - client.domain
    - client_proc - client.process.name
    - client_cmdline - client.process.args (changed to an array)
    - direction - network.direction
    - ip - server.ip (changed to ip type)
    - port - server.port
    - server - server.domain
    - proc - server.process.name
    - cmdline - server.process.args (change to an array)
    
    Added
    
    - client.process.executable
    - client.process.working_directory
    - client.process.pid
    - client.process.ppid
    - client.process.start
    - server.process.executable
    - server.process.working_directory
    - server.process.pid
    - server.process.ppid
    - server.process.start
    
    The top-level `process` field is populated with the `server.process`
    or `client.process` (server takes priority). Likewise for flows `process`
    is populated with the `destination.process` or the `client.process`
    (destination takes priority).

--- packetbeat/tests/system/test_0066_dhcp.py
+++ packetbeat/tests/system/test_0066_dhcp.py
@@ -14,8 +14,8 @@ class Test(BaseTest):
         objs = self.read_output(types=['dhcpv4'])
         assert len(objs) == 4
 
-        assert objs[0]["client_ip"] == "0.0.0.0"
-        assert objs[0]["client_port"] == 68
+        assert objs[0]["client.ip"] == "0.0.0.0"
+        assert objs[0]["client.port"] == 68
         assert objs[0]["dhcpv4.client_mac"] == "00:0b:82:01:fc:42"
         assert objs[0]["dhcpv4.flags"] == "unicast"
         assert objs[0]["dhcpv4.hardware_type"] == "Ethernet"
@@ -31,14 +31,14 @@ class Test(BaseTest):
         assert objs[0]["dhcpv4.option.requested_ip_address"] == "0.0.0.0"
         assert objs[0]["dhcpv4.seconds"] == 0
         assert objs[0]["dhcpv4.transaction_id"] == "0x00003d1d"
-        assert objs[0]["ip"] == "255.255.255.255"
-        assert objs[0]["port"] == 67
+        assert objs[0]["server.ip"] == "255.255.255.255"
+        assert objs[0]["server.port"] == 67
         assert objs[0]["status"] == "OK"
         assert objs[0]["transport"] == "udp"
         assert objs[0]["type"] == "dhcpv4"
 
-        assert objs[1]["client_ip"] == "192.168.0.10"
-        assert objs[1]["client_port"] == 68
+        assert objs[1]["client.ip"] == "192.168.0.10"
+        assert objs[1]["client.port"] == 68
         assert objs[1]["dhcpv4.assigned_ip"] == "192.168.0.10"
         assert objs[1]["dhcpv4.client_mac"] == "00:0b:82:01:fc:42"
         assert objs[1]["dhcpv4.flags"] == "unicast"
@@ -53,14 +53,14 @@ class Test(BaseTest):
         assert objs[1]["dhcpv4.option.subnet_mask"] == "255.255.255.0"
         assert objs[1]["dhcpv4.seconds"] == 0
         assert objs[1]["dhcpv4.transaction_id"] == "0x00003d1d"
-        assert objs[1]["ip"] == "192.168.0.1"
-        assert objs[1]["port"] == 67
+        assert objs[1]["server.ip"] == "192.168.0.1"
+        assert objs[1]["server.port"] == 67
         assert objs[1]["status"] == "OK"
         assert objs[1]["transport"] == "udp"
         assert objs[1]["type"] == "dhcpv4"
 
-        assert objs[2]["client_ip"] == "0.0.0.0"
-        assert objs[2]["client_port"] == 68
+        assert objs[2]["client.ip"] == "0.0.0.0"
+        assert objs[2]["client.port"] == 68
         assert objs[2]["dhcpv4.client_mac"] == "00:0b:82:01:fc:42"
         assert objs[2]["dhcpv4.flags"] == "unicast"
         assert objs[2]["dhcpv4.hardware_type"] == "Ethernet"
@@ -77,14 +77,14 @@ class Test(BaseTest):
         assert objs[2]["dhcpv4.option.server_identifier"] == "192.168.0.1"
         assert objs[2]["dhcpv4.seconds"] == 0
         assert objs[2]["dhcpv4.transaction_id"] == "0x00003d1e"
-        assert objs[2]["ip"] == "255.255.255.255"
-        assert objs[2]["port"] == 67
+        assert objs[2]["server.ip"] == "255.255.255.255"
+        assert objs[2]["server.port"] == 67
         assert objs[2]["status"] == "OK"
         assert objs[2]["transport"] == "udp"
         assert objs[2]["type"] == "dhcpv4"
 
-        assert objs[3]["client_ip"] == "192.168.0.10"
-        assert objs[3]["client_port"] == 68
+        assert objs[3]["client.ip"] == "192.168.0.10"
+        assert objs[3]["client.port"] == 68
         assert objs[3]["dhcpv4.assigned_ip"] == "192.168.0.10"
         assert objs[3]["dhcpv4.client_mac"] == "00:0b:82:01:fc:42"
         assert objs[3]["dhcpv4.flags"] == "unicast"
@@ -99,8 +99,8 @@ class Test(BaseTest):
         assert objs[3]["dhcpv4.option.subnet_mask"] == "255.255.255.0"
         assert objs[3]["dhcpv4.seconds"] == 0
         assert objs[3]["dhcpv4.transaction_id"] == "0x00003d1e"
-        assert objs[3]["ip"] == "192.168.0.1"
-        assert objs[3]["port"] == 67
+        assert objs[3]["server.ip"] == "192.168.0.1"
+        assert objs[3]["server.port"] == 67
         assert objs[3]["status"] == "OK"
         assert objs[3]["transport"] == "udp"
         assert objs[3]["type"] == "dhcpv4"
