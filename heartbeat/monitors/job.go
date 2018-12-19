37a5b1f6374203e42239dcd254162f9f40929224
Andrew Cholakian <andrew@andrewvc.com>
2018-12-19 09:03:33 -0600

    Unify heartbeat jobs and tasks for simplicity (#9258)
    Unify heartbeat jobs and tasks for simplicity
    
    In heartbeat we have separate notions of jobs and tasks.
    
    This can make following the code and sharing the code among monitors confusing. This patch unifies the two. There is a tiny amount of extra execution overhead.
    
    Tasks only passed a single `common.MapStr`, where we now pass a `*beat.Event`, however, the benefit is that we have a more consistent and simple codebase.
    
    Also makes events arguments to Jobs rather than return values.
    
    This lets us reason about concurrency more easily since jobs are now explicitly
    about mutation, and accidentally sharing data between tasks is made more difficult.

--- heartbeat/monitors/job.go
+++ heartbeat/monitors/job.go
@@ -21,10 +21,89 @@ import (
 	"github.com/elastic/beats/libbeat/beat"
 )
 
-// Job represents the work done by a single check by a given Monitor.
+// A Job represents a unit of execution, and may return multiple continuation jobs.
 type Job interface {
 	Name() string
-	Run() (beat.Event, []jobRunner, error)
+	Run(event *beat.Event) ([]Job, error)
 }
 
-type jobRunner func() (beat.Event, []jobRunner, error)
+// NamedJob represents a job with an explicitly specified name.
+type NamedJob struct {
+	name string
+	run  func(event *beat.Event) ([]Job, error)
+}
+
+// CreateNamedJob makes a new NamedJob.
+func CreateNamedJob(name string, run func(event *beat.Event) ([]Job, error)) *NamedJob {
+	return &NamedJob{name, run}
+}
+
+// Name returns the name of this job.
+func (f *NamedJob) Name() string {
+	return f.name
+}
+
+// Run executes the job.
+func (f *NamedJob) Run(event *beat.Event) ([]Job, error) {
+	return f.run(event)
+}
+
+// AnonJob represents a job with no assigned name, backed by just a function.
+type AnonJob func(event *beat.Event) ([]Job, error)
+
+// Name returns "" for AnonJob values.
+func (aj AnonJob) Name() string {
+	return ""
+}
+
+// Run executes the function.
+func (aj AnonJob) Run(event *beat.Event) ([]Job, error) {
+	return aj(event)
+}
+
+// AfterJob creates a wrapped version of the given Job that runs additional
+// code after the original Job, possibly altering return values.
+func AfterJob(j Job, after func(*beat.Event, []Job, error) ([]Job, error)) Job {
+
+	return CreateNamedJob(
+		j.Name(),
+		func(event *beat.Event) ([]Job, error) {
+			next, err := j.Run(event)
+
+			return after(event, next, err)
+		},
+	)
+}
+
+// AfterJobSuccess creates a wrapped version of the given Job that runs additional
+// code after the original job if the original Job succeeds, possibly altering
+// return values.
+func AfterJobSuccess(j Job, after func(*beat.Event, []Job, error) ([]Job, error)) Job {
+	return AfterJob(j, func(event *beat.Event, cont []Job, err error) ([]Job, error) {
+		if err != nil {
+			return cont, err
+		}
+
+		return after(event, cont, err)
+	})
+}
+
+// MakeSimpleJob creates a new Job from a callback function. The callback should
+// return an valid event and can not create any sub-tasks to be executed after
+// completion.
+func MakeSimpleJob(f func(*beat.Event) error) Job {
+	return AnonJob(func(event *beat.Event) ([]Job, error) {
+		err := f(event)
+		return nil, err
+	})
+}
+
+// WrapAll takes a list of jobs and wraps them all with the provided Job wrapping
+// function.
+func WrapAll(jobs []Job, fn func(Job) Job) []Job {
+	var wrapped []Job
+	for _, j := range jobs {
+		wrapped = append(wrapped, fn(j))
+	}
+	return wrapped
+}
