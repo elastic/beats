1b83f5e6334c46ae3bb459e73d548ae10f067ff4
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2018-12-21 16:23:10 +0100

    Add option to support moved filebeat modules (#9432)
    If a module needs to be moved, existing configuration will stop working,
    this change adds an option to mark an old module as moved to other name.
    
    This is being considered for the movement of apache2 module to apache to
    be coherent with metricbeat module (#9402).

--- filebeat/fileset/modules_test.go
+++ filebeat/fileset/modules_test.go
@@ -126,6 +126,24 @@ func TestNewModuleRegistryConfig(t *testing.T) {
 	assert.NotContains(t, reg.registry["nginx"], "error")
 }
 
+func TestMovedModule(t *testing.T) {
+	modulesPath, err := filepath.Abs("./test/moved_module")
+	assert.NoError(t, err)
+
+	configs := []*ModuleConfig{
+		&ModuleConfig{
+			Module: "old",
+			Filesets: map[string]*FilesetConfig{
+				"test": {},
+			},
+		},
+	}
+
+	reg, err := newModuleRegistry(modulesPath, configs, nil, "5.2.0")
+	assert.NoError(t, err)
+	assert.NotNil(t, reg)
+}
+
 func TestApplyOverrides(t *testing.T) {
 	falseVar := false
 	trueVar := true
