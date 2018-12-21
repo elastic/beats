502ddb0d9d7bef42748664a8f54ccae8d05a5fa1
beats-jenkins <spam@ruflin.com>
2018-12-21 14:10:54 +0100

    push one version

--- deploy/kubernetes/.travis/setup.sh
+++ deploy/kubernetes/.travis/setup.sh
@@ -7,10 +7,10 @@ set -e
 export CHANGE_MINIKUBE_NONE_USER=true
 
 mkdir -p $HOME/bin/
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$TRAVIS_K8S_VERSION/bin/linux/amd64/kubectl && \
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.10.0/bin/linux/amd64/kubectl && \
       chmod +x kubectl && mv kubectl $HOME/bin/
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/$TRAVIS_MINIKUBE_VERSION/minikube-linux-amd64 && chmod +x minikube && mv minikube $HOME/bin/
-$HOME/bin/minikube start --vm-driver=none --kubernetes-version=$TRAVIS_K8S_VERSION --logtostderr
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.10.0/minikube-linux-amd64 && chmod +x minikube && mv minikube $HOME/bin/
+$HOME/bin/minikube start --vm-driver=none --kubernetes-version=v1.10.0 --logtostderr
 $HOME/bin/minikube update-context
 JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; \
         until $HOME/bin/kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1; done
