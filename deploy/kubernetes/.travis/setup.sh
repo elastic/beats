5721d7a4fec16582e8686f09155f4c996b9b54ee
beats-jenkins <spam@ruflin.com>
2018-12-21 13:46:30 +0100

    add more paths

--- deploy/kubernetes/.travis/setup.sh
+++ deploy/kubernetes/.travis/setup.sh
@@ -6,11 +6,12 @@ set -e
 
 export CHANGE_MINIKUBE_NONE_USER=true
 
+mkdir -p $HOME/bin/
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$TRAVIS_K8S_VERSION/bin/linux/amd64/kubectl && \
       chmod +x kubectl && mv kubectl $HOME/bin/
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/$TRAVIS_MINIKUBE_VERSION/minikube-linux-amd64 && chmod +x minikube && mv minikube $HOME/bin/
-minikube start --vm-driver=none --kubernetes-version=$TRAVIS_K8S_VERSION --logtostderr
-minikube update-context
+$HOME/bin/minikube start --vm-driver=none --kubernetes-version=$TRAVIS_K8S_VERSION --logtostderr
+$HOME/bin/minikube update-context
 JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; \
-        until kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1; done
+        until $HOME/bin/kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1; done
 
