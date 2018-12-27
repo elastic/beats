4e8f855a09e7151808cb9f2e1da02bf453e2ccc8
Shaunak Kashyap <ycombinator@gmail.com>
2018-12-27 11:19:31 -0800

    Accept multiple ingest pipelines in Filebeat (#8914)
    Motivated by https://github.com/elastic/beats/pull/8852#issuecomment-434973388.
    
    Starting with 6.5.0, Elasticsearch Ingest Pipelines have gained the ability to:
    - run sub-pipelines via the [`pipeline` processor](https://www.elastic.co/guide/en/elasticsearch/reference/6.5/pipeline-processor.html), and
    - conditionally run processors via an [`if` field](https://www.elastic.co/guide/en/elasticsearch/reference/6.5/ingest-processors.html).
    
    These abilities combined present the opportunity for a fileset to ingest the same _logical_ information presented in different formats, e.g. plaintext vs. json versions of the same log files. Imagine an entry point ingest pipeline that detects the format of a log entry and then conditionally delegates further processing of that log entry, depending on the format, to another pipeline.
    
    This PR allows filesets to specify one or more ingest pipelines via the `ingest_pipeline` property in their `manifest.yml`. If more than one ingest pipeline is specified, the first one is taken to be the entry point ingest pipeline.
    
    ```yaml
    ingest_pipeline:
      - pipeline-ze-boss.json
      - pipeline-plain.json
      - pipeline-json.json
    ```
    _This is just to show that the existing functionality will continue to work as-is._
    ```yaml
    ingest_pipeline: pipeline.json
    ```
    
    Now, if the root pipeline wants to delegate processing to another pipeline, it must use a `pipeline` processor to do so. This processor's `name` field will need to reference the other pipeline by its name. To ensure correct referencing, the `name` field must be specified as follows:
    
    ```json
    {
      "pipeline" : {
        "name": "{< IngestPipeline "pipeline-plain" >}"
      }
    }
    ```
    
    This will ensure that the specified name gets correctly converted to the corresponding name in Elasticsearch, since Filebeat prefixes it's "raw" Ingest pipeline names with `filebeat-<version>-<module>-<fileset>-` when loading them into Elasticsearch.
    
    (cherry picked from commit 5ba1f11dc3ea582649800de1a9255c731ee61fac)

--- filebeat/fileset/fileset.go
+++ filebeat/fileset/fileset.go
@@ -34,6 +34,8 @@ import (
 	"strings"
 	"text/template"
 
+	errw "github.com/pkg/errors"
+
 	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/libbeat/logp"
 	mlimporter "github.com/elastic/beats/libbeat/ml-importer"
@@ -41,13 +43,18 @@ import (
 
 // Fileset struct is the representation of a fileset.
 type Fileset struct {
-	name       string
-	mcfg       *ModuleConfig
-	fcfg       *FilesetConfig
-	modulePath string
-	manifest   *manifest
-	vars       map[string]interface{}
-	pipelineID string
+	name        string
+	mcfg        *ModuleConfig
+	fcfg        *FilesetConfig
+	modulePath  string
+	manifest    *manifest
+	vars        map[string]interface{}
+	pipelineIDs []string
+}
+
+type pipeline struct {
+	id       string
+	contents map[string]interface{}
 }
 
 // New allocates a new Fileset object with the given configuration.
@@ -83,12 +90,12 @@ func (fs *Fileset) Read(beatVersion string) error {
 		return err
 	}
 
-	fs.vars, err = fs.evaluateVars()
+	fs.vars, err = fs.evaluateVars(beatVersion)
 	if err != nil {
 		return err
 	}
 
-	fs.pipelineID, err = fs.getPipelineID(beatVersion)
+	fs.pipelineIDs, err = fs.getPipelineIDs(beatVersion)
 	if err != nil {
 		return err
 	}
@@ -101,7 +108,7 @@ func (fs *Fileset) Read(beatVersion string) error {
 type manifest struct {
 	ModuleVersion   string                   `config:"module_version"`
 	Vars            []map[string]interface{} `config:"var"`
-	IngestPipeline  string                   `config:"ingest_pipeline"`
+	IngestPipeline  []string                 `config:"ingest_pipeline"`
 	Input           string                   `config:"input"`
 	Prospector      string                   `config:"prospector"`
 	MachineLearning []struct {
@@ -148,10 +155,10 @@ func (fs *Fileset) readManifest() (*manifest, error) {
 }
 
 // evaluateVars resolves the fileset variables.
-func (fs *Fileset) evaluateVars() (map[string]interface{}, error) {
+func (fs *Fileset) evaluateVars(beatVersion string) (map[string]interface{}, error) {
 	var err error
 	vars := map[string]interface{}{}
-	vars["builtin"], err = fs.getBuiltinVars()
+	vars["builtin"], err = fs.getBuiltinVars(beatVersion)
 	if err != nil {
 		return nil, err
 	}
@@ -262,7 +269,14 @@ func applyTemplate(vars map[string]interface{}, templateString string, specialDe
 	if specialDelims {
 		tpl = tpl.Delims("{<", ">}")
 	}
-	tpl, err := tpl.Parse(templateString)
+
+	tplFunctions, err := getTemplateFunctions(vars)
+	if err != nil {
+		return "", errw.Wrap(err, "error fetching template functions")
+	}
+	tpl = tpl.Funcs(tplFunctions)
+
+	tpl, err = tpl.Parse(templateString)
 	if err != nil {
 		return "", fmt.Errorf("Error parsing template %s: %v", templateString, err)
 	}
@@ -274,9 +288,27 @@ func applyTemplate(vars map[string]interface{}, templateString string, specialDe
 	return buf.String(), nil
 }
 
+func getTemplateFunctions(vars map[string]interface{}) (template.FuncMap, error) {
+	builtinVars, ok := vars["builtin"].(map[string]interface{})
+	if !ok {
+		return nil, fmt.Errorf("error fetching built-in vars as a dictionary")
+	}
+
+	return template.FuncMap{
+		"IngestPipeline": func(shortID string) string {
+			return formatPipelineID(
+				builtinVars["module"].(string),
+				builtinVars["fileset"].(string),
+				shortID,
+				builtinVars["beatVersion"].(string),
+			)
+		},
+	}, nil
+}
+
 // getBuiltinVars computes the supported built in variables and groups them
 // in a dictionary
-func (fs *Fileset) getBuiltinVars() (map[string]interface{}, error) {
+func (fs *Fileset) getBuiltinVars(beatVersion string) (map[string]interface{}, error) {
 	host, err := os.Hostname()
 	if err != nil || len(host) == 0 {
 		return nil, fmt.Errorf("Error getting the hostname: %v", err)
@@ -289,8 +321,11 @@ func (fs *Fileset) getBuiltinVars() (map[string]interface{}, error) {
 	}
 
 	return map[string]interface{}{
-		"hostname": hostname,
-		"domain":   domain,
+		"hostname":    hostname,
+		"domain":      domain,
+		"module":      fs.mcfg.Module,
+		"fileset":     fs.name,
+		"beatVersion": beatVersion,
 	}, nil
 }
 
@@ -327,7 +362,11 @@ func (fs *Fileset) getInputConfig() (*common.Config, error) {
 	}
 
 	// force our pipeline ID
-	err = cfg.SetString("pipeline", -1, fs.pipelineID)
+	rootPipelineID := ""
+	if len(fs.pipelineIDs) > 0 {
+		rootPipelineID = fs.pipelineIDs[0]
+	}
+	err = cfg.SetString("pipeline", -1, rootPipelineID)
 	if err != nil {
 		return nil, fmt.Errorf("Error setting the pipeline ID in the input config: %v", err)
 	}
@@ -347,43 +386,60 @@ func (fs *Fileset) getInputConfig() (*common.Config, error) {
 	return cfg, nil
 }
 
-// getPipelineID returns the Ingest Node pipeline ID
-func (fs *Fileset) getPipelineID(beatVersion string) (string, error) {
-	path, err := applyTemplate(fs.vars, fs.manifest.IngestPipeline, false)
-	if err != nil {
-		return "", fmt.Errorf("Error expanding vars on the ingest pipeline path: %v", err)
+// getPipelineIDs returns the Ingest Node pipeline IDs
+func (fs *Fileset) getPipelineIDs(beatVersion string) ([]string, error) {
+	var pipelineIDs []string
+	for _, ingestPipeline := range fs.manifest.IngestPipeline {
+		path, err := applyTemplate(fs.vars, ingestPipeline, false)
+		if err != nil {
+			return nil, fmt.Errorf("Error expanding vars on the ingest pipeline path: %v", err)
+		}
+
+		pipelineIDs = append(pipelineIDs, formatPipelineID(fs.mcfg.Module, fs.name, path, beatVersion))
 	}
 
-	return formatPipelineID(fs.mcfg.Module, fs.name, path, beatVersion), nil
+	return pipelineIDs, nil
 }
 
-// GetPipeline returns the JSON content of the Ingest Node pipeline that parses the logs.
-func (fs *Fileset) GetPipeline(esVersion common.Version) (pipelineID string, content map[string]interface{}, err error) {
-	path, err := applyTemplate(fs.vars, fs.manifest.IngestPipeline, false)
-	if err != nil {
-		return "", nil, fmt.Errorf("Error expanding vars on the ingest pipeline path: %v", err)
-	}
-
-	strContents, err := ioutil.ReadFile(filepath.Join(fs.modulePath, fs.name, path))
-	if err != nil {
-		return "", nil, fmt.Errorf("Error reading pipeline file %s: %v", path, err)
-	}
-
+// GetPipelines returns the JSON content of the Ingest Node pipeline that parses the logs.
+func (fs *Fileset) GetPipelines(esVersion common.Version) (pipelines []pipeline, err error) {
 	vars, err := fs.turnOffElasticsearchVars(fs.vars, esVersion)
 	if err != nil {
-		return "", nil, err
+		return nil, err
 	}
 
-	jsonString, err := applyTemplate(vars, string(strContents), true)
-	if err != nil {
-		return "", nil, fmt.Errorf("Error interpreting the template of the ingest pipeline: %v", err)
+	for idx, ingestPipeline := range fs.manifest.IngestPipeline {
+		path, err := applyTemplate(fs.vars, ingestPipeline, false)
+		if err != nil {
+			return nil, fmt.Errorf("Error expanding vars on the ingest pipeline path: %v", err)
+		}
+
+		strContents, err := ioutil.ReadFile(filepath.Join(fs.modulePath, fs.name, path))
+		if err != nil {
+			return nil, fmt.Errorf("Error reading pipeline file %s: %v", path, err)
+		}
+
+		jsonString, err := applyTemplate(vars, string(strContents), true)
+		if err != nil {
+			return nil, fmt.Errorf("Error interpreting the template of the ingest pipeline: %v", err)
+		}
+
+		var content map[string]interface{}
+		err = json.Unmarshal([]byte(jsonString), &content)
+		if err != nil {
+			return nil, fmt.Errorf("Error JSON decoding the pipeline file: %s: %v", path, err)
+		}
+
+		pipelineID := fs.pipelineIDs[idx]
+
+		p := pipeline{
+			pipelineID,
+			content,
+		}
+		pipelines = append(pipelines, p)
 	}
 
-	err = json.Unmarshal([]byte(jsonString), &content)
-	if err != nil {
-		return "", nil, fmt.Errorf("Error JSON decoding the pipeline file: %s: %v", path, err)
-	}
-	return fs.pipelineID, content, nil
+	return pipelines, nil
 }
 
 // formatPipelineID generates the ID to be used for the pipeline ID in Elasticsearch
