7e38917f07a44bc8cff7fdbb372c2fbb20a2f404
Shaunak Kashyap <ycombinator@gmail.com>
2018-12-28 06:25:30 -0800

    Cherry-pick #8914 to 6.x: Accept multiple ingest pipelines in Filebeat (#9811)
    Cherry-pick of PR #8914 to 6.x branch. Original message: 
    
    Motivated by https://github.com/elastic/beats/pull/8852#issuecomment-434973388.
    
    Starting with 6.5.0, Elasticsearch Ingest Pipelines have gained the ability to:
    - run sub-pipelines via the [`pipeline` processor](https://www.elastic.co/guide/en/elasticsearch/reference/6.5/pipeline-processor.html), and
    - conditionally run processors via an [`if` field](https://www.elastic.co/guide/en/elasticsearch/reference/6.5/ingest-processors.html).
    
    These abilities combined present the opportunity for a fileset to ingest the same _logical_ information presented in different formats, e.g. plaintext vs. json versions of the same log files. Imagine an entry point ingest pipeline that detects the format of a log entry and then conditionally delegates further processing of that log entry, depending on the format, to another pipeline.
    
    This PR allows filesets to specify one or more ingest pipelines via the `ingest_pipeline` property in their `manifest.yml`. If more than one ingest pipeline is specified, the first one is taken to be the entry point ingest pipeline.
    
    #### Example with multiple pipelines
    ```yaml
    ingest_pipeline:
      - pipeline-ze-boss.json 
      - pipeline-plain.json
      - pipeline-json.json
    ```
    #### Example with a single pipeline
    _This is just to show that the existing functionality will continue to work as-is._
    ```yaml
    ingest_pipeline: pipeline.json
    ```
    
    Now, if the root pipeline wants to delegate processing to another pipeline, it must use a `pipeline` processor to do so. This processor's `name` field will need to reference the other pipeline by its name. To ensure correct referencing, the `name` field must be specified as follows:
    
    ```json
    {
      "pipeline" : {
        "name": "{< IngestPipeline "pipeline-plain" >}"
      }
    }
    ```
    
    This will ensure that the specified name gets correctly converted to the corresponding name in Elasticsearch, since Filebeat prefixes it's "raw" Ingest pipeline names with `filebeat-<version>-<module>-<fileset>-` when loading them into Elasticsearch.

--- filebeat/fileset/fileset_test.go
+++ filebeat/fileset/fileset_test.go
@@ -48,7 +48,7 @@ func TestLoadManifestNginx(t *testing.T) {
 	manifest, err := fs.readManifest()
 	assert.NoError(t, err)
 	assert.Equal(t, manifest.ModuleVersion, "1.0")
-	assert.Equal(t, manifest.IngestPipeline, "ingest/default.json")
+	assert.Equal(t, manifest.IngestPipeline, []string{"ingest/default.json"})
 	assert.Equal(t, manifest.Input, "config/nginx-access.yml")
 
 	vars := manifest.Vars
@@ -60,11 +60,14 @@ func TestLoadManifestNginx(t *testing.T) {
 func TestGetBuiltinVars(t *testing.T) {
 	fs := getModuleForTesting(t, "nginx", "access")
 
-	vars, err := fs.getBuiltinVars()
+	vars, err := fs.getBuiltinVars("6.6.0")
 	assert.NoError(t, err)
 
 	assert.IsType(t, vars["hostname"], "a-mac-with-esc-key")
 	assert.IsType(t, vars["domain"], "local")
+	assert.Equal(t, "nginx", vars["module"])
+	assert.Equal(t, "access", vars["fileset"])
+	assert.Equal(t, "6.6.0", vars["beatVersion"])
 }
 
 func TestEvaluateVarsNginx(t *testing.T) {
@@ -74,7 +77,7 @@ func TestEvaluateVarsNginx(t *testing.T) {
 	fs.manifest, err = fs.readManifest()
 	assert.NoError(t, err)
 
-	vars, err := fs.evaluateVars()
+	vars, err := fs.evaluateVars("6.6.0")
 	assert.NoError(t, err)
 
 	builtin := vars["builtin"].(map[string]interface{})
@@ -97,7 +100,7 @@ func TestEvaluateVarsNginxOverride(t *testing.T) {
 	fs.manifest, err = fs.readManifest()
 	assert.NoError(t, err)
 
-	vars, err := fs.evaluateVars()
+	vars, err := fs.evaluateVars("6.6.0")
 	assert.NoError(t, err)
 
 	assert.Equal(t, "no_plugins", vars["pipeline"])
@@ -110,7 +113,7 @@ func TestEvaluateVarsMySQL(t *testing.T) {
 	fs.manifest, err = fs.readManifest()
 	assert.NoError(t, err)
 
-	vars, err := fs.evaluateVars()
+	vars, err := fs.evaluateVars("6.6.0")
 	assert.NoError(t, err)
 
 	builtin := vars["builtin"].(map[string]interface{})
@@ -144,14 +147,16 @@ func TestResolveVariable(t *testing.T) {
 		{
 			Value: "test-{{.value}}",
 			Vars: map[string]interface{}{
-				"value": 2,
+				"value":   2,
+				"builtin": map[string]interface{}{},
 			},
 			Expected: "test-2",
 		},
 		{
 			Value: []interface{}{"test-{{.value}}", "test1-{{.value}}"},
 			Vars: map[string]interface{}{
-				"value": 2,
+				"value":   2,
+				"builtin": map[string]interface{}{},
 			},
 			Expected: []interface{}{"test-2", "test1-2"},
 		},
@@ -216,11 +221,14 @@ func TestGetPipelineNginx(t *testing.T) {
 	assert.NoError(t, fs.Read("5.2.0"))
 
 	version := common.MustNewVersion("5.2.0")
-	pipelineID, content, err := fs.GetPipeline(*version)
+	pipelines, err := fs.GetPipelines(*version)
 	assert.NoError(t, err)
-	assert.Equal(t, "filebeat-5.2.0-nginx-access-default", pipelineID)
-	assert.Contains(t, content, "description")
-	assert.Contains(t, content, "processors")
+	assert.Len(t, pipelines, 1)
+
+	pipeline := pipelines[0]
+	assert.Equal(t, "filebeat-5.2.0-nginx-access-default", pipeline.id)
+	assert.Contains(t, pipeline.contents, "description")
+	assert.Contains(t, pipeline.contents, "processors")
 }
 
 func TestGetPipelineConvertTS(t *testing.T) {
@@ -251,11 +259,13 @@ func TestGetPipelineConvertTS(t *testing.T) {
 
 		t.Run(fmt.Sprintf("es=%v", esVersion), func(t *testing.T) {
 			ver := common.MustNewVersion(esVersion)
-			pipelineID, content, err := fs.GetPipeline(*ver)
+			pipelines, err := fs.GetPipelines(*ver)
 			require.NoError(t, err)
-			assert.Equal(t, pipelineName, pipelineID)
 
-			marshaled, err := json.Marshal(content)
+			pipeline := pipelines[0]
+			assert.Equal(t, pipelineName, pipeline.id)
+
+			marshaled, err := json.Marshal(pipeline.contents)
 			require.NoError(t, err)
 			if cfg.Timezone {
 				assert.Contains(t, string(marshaled), "beat.timezone")
