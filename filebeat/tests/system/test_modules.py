3ebc73b660bfacbf3f6b0df2daf02bbb130873c8
Shaunak Kashyap <ycombinator@gmail.com>
2018-12-29 06:14:17 -0800

    Cherry-pick #9827 to 6.x: Remove trailing whitespace from golden files (#9832)
    Cherry-pick of PR #9827 to 6.x branch. Original message: 
    
    When system tests are run with the `GENERATE` environment variable set, we regenerate golden files used by these tests. To do this we use the Python `json.dump` function, which adds trailing whitespaces when it is called with the `indent` argument (which we do). From the Python docs at https://docs.python.org/2/library/json.html:
    
    > Note Since the default item separator is ', ', the output might include trailing whitespace when indent is specified. You can use separators=(',', ': ') to avoid this.
    
    Per the suggestion in the docs, this change adds the `separators` argument as well, to avoid generating trailing whitespace.
    
    This PR also regenerates all golden files with the new regeneration code.

--- filebeat/tests/system/test_modules.py
+++ filebeat/tests/system/test_modules.py
@@ -159,7 +159,8 @@ class Test(BaseTest):
                 for k, obj in enumerate(objects):
                     objects[k] = self.flatten_object(obj, {}, "")
                     clean_keys(objects[k])
-                json.dump(objects, f, indent=4, sort_keys=True)
+
+                json.dump(objects, f, indent=4, separators=(',', ': '), sort_keys=True)
 
         with open(test_file + "-expected.json", "r") as f:
             expected = json.load(f)
