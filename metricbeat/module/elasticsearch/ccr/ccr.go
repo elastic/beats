2a118c24d3d1775e030f7bc8baa5f077247e282f
Shaunak Kashyap <ycombinator@gmail.com>
2019-01-09 07:00:50 -0800

    Pulling up various getServiceURI methods from individual metricsets (#9896) (#9946)
    (cherry picked from commit 0286cc70a070d369af818a3f089646dcb0283748)

--- metricbeat/module/elasticsearch/ccr/ccr.go
+++ metricbeat/module/elasticsearch/ccr/ccr.go
@@ -58,7 +58,7 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 
 // Fetch gathers stats for each follower shard from the _ccr/stats API
 func (m *MetricSet) Fetch(r mb.ReporterV2) {
-	isMaster, err := elasticsearch.IsMaster(m.HTTP, m.getServiceURI())
+	isMaster, err := elasticsearch.IsMaster(m.HTTP, m.GetServiceURI())
 	if err != nil {
 		err = errors.Wrap(err, "error determining if connected Elasticsearch node is master")
 		elastic.ReportAndLogError(err, r, m.Log)
@@ -71,7 +71,7 @@ func (m *MetricSet) Fetch(r mb.ReporterV2) {
 		return
 	}
 
-	info, err := elasticsearch.GetInfo(m.HTTP, m.getServiceURI())
+	info, err := elasticsearch.GetInfo(m.HTTP, m.GetServiceURI())
 	if err != nil {
 		elastic.ReportAndLogError(err, r, m.Log)
 		return
@@ -116,7 +116,7 @@ func (m *MetricSet) Fetch(r mb.ReporterV2) {
 }
 
 func (m *MetricSet) checkCCRAvailability(currentElasticsearchVersion string) (message string, err error) {
-	license, err := elasticsearch.GetLicense(m.HTTP, m.getServiceURI())
+	license, err := elasticsearch.GetLicense(m.HTTP, m.GetServiceURI())
 	if err != nil {
 		return "", errors.Wrap(err, "error determining Elasticsearch license")
 	}
@@ -142,9 +142,4 @@ func (m *MetricSet) checkCCRAvailability(currentElasticsearchVersion string) (me
 	}
 
 	return "", nil
-}
-
-func (m *MetricSet) getServiceURI() string {
-	return m.HostData().SanitizedURI + ccrStatsPath
-
 }
