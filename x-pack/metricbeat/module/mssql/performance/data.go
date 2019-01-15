c6b858fa32441c2f2788f26f70b772a3d67379ef
Mario Castro <mariocaster@gmail.com>
2019-01-15 14:44:43 +0100

    Rename metrics names in Performance Metricset of MSSQL Module (#10074)

--- x-pack/metricbeat/module/mssql/performance/data.go
+++ x-pack/metricbeat/module/mssql/performance/data.go
@@ -11,33 +11,17 @@ import (
 
 var (
 	schema = s.Schema{
-		"page_splits": s.Object{
-			"sec": c.Int("Page Splits/sec", s.Optional),
-		},
-		"lock_waits": s.Object{
-			"sec": c.Int("Lock Waits/sec", s.Optional),
-		},
-		"user_connections":   c.Int("User Connections", s.Optional),
-		"transactions":       c.Int("Transactions", s.Optional),
-		"active_temp_tables": c.Int("Active Temp Tables", s.Optional),
-		"connections_reset": s.Object{
-			"sec": c.Int("Connection Reset/sec", s.Optional),
-		},
-		"logouts": s.Object{
-			"sec": c.Int("Logouts/sec", s.Optional),
-		},
-		"logins": s.Object{
-			"sec": c.Int("Logins/sec", s.Optional),
-		},
-		"recompilations": s.Object{
-			"sec": c.Int("SQL Re-Compilations/sec", s.Optional),
-		},
-		"compilations": s.Object{
-			"sec": c.Int("SQL Compilations/sec", s.Optional),
-		},
-		"batch_requests": s.Object{
-			"sec": c.Int("Batch Requests/sec", s.Optional),
-		},
+		"page_splits_per_sec":       c.Int("Page Splits/sec", s.Optional),
+		"lock_waits_per_sec":        c.Int("Lock Waits/sec", s.Optional),
+		"user_connections":          c.Int("User Connections", s.Optional),
+		"transactions":              c.Int("Transactions", s.Optional),
+		"active_temp_tables":        c.Int("Active Temp Tables", s.Optional),
+		"connections_reset_per_sec": c.Int("Connection Reset/sec", s.Optional),
+		"logouts_per_sec":           c.Int("Logouts/sec", s.Optional),
+		"logins_per_sec":            c.Int("Logins/sec", s.Optional),
+		"recompilations_per_sec":    c.Int("SQL Re-Compilations/sec", s.Optional),
+		"compilations_per_sec":      c.Int("SQL Compilations/sec", s.Optional),
+		"batch_requests_per_sec":    c.Int("Batch Requests/sec", s.Optional),
 		"buffer": s.Object{
 			"cache_hit": s.Object{
 				"pct": c.Float("Buffer cache hit ratio", s.Optional),
@@ -45,11 +29,9 @@ var (
 			"page_life_expectancy": s.Object{
 				"sec": c.Int("Page life expectancy", s.Optional),
 			},
-			"checkpoint_pages": s.Object{
-				"sec": c.Int("Checkpoint pages/sec", s.Optional),
-			},
-			"database_pages": c.Int("Database pages", s.Optional),
-			"target_pages":   c.Int("Target pages", s.Optional),
+			"checkpoint_pages_per_sec": c.Int("Checkpoint pages/sec", s.Optional),
+			"database_pages":           c.Int("Database pages", s.Optional),
+			"target_pages":             c.Int("Target pages", s.Optional),
 		},
 	}
 )
