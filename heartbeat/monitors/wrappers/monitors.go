ce6e2492e9d78bac9b83d5681c6d0018d456cbce
Andrew Cholakian <andrew@andrewvc.com>
2019-01-14 22:14:08 -0600

    WIP Job Summaries
    Improve summaries

--- heartbeat/monitors/wrappers/monitors.go
+++ heartbeat/monitors/wrappers/monitors.go
@@ -18,7 +18,11 @@
 package wrappers
 
 import (
+	"fmt"
+	"sync"
 	"time"
+
+	"github.com/gofrs/uuid"
 
 	"github.com/elastic/beats/heartbeat/eventext"
 	"github.com/elastic/beats/heartbeat/look"
@@ -90,5 +94,83 @@ func addMonitorDuration(job jobs.Job) jobs.Job {
 		}
 
 		return cont, err
+	}
+}
+
+// genAddSummary summarizes the job, adding the `summary` field to the last event emitted.
+func genAddSummary(id string, js []jobs.Job) jobs.JobWrapper {
+	// This is a tricky method. The way this works is that we track the state across jobs in the
+	// state struct here.
+	state := struct {
+		mtx        sync.Mutex
+		remaining  uint16
+		lastly     sync.Once
+		up         uint16
+		down       uint16
+		checkGroup string
+		generation uint64
+	}{
+		mtx: sync.Mutex{},
+	}
+	// Note this is not threadsafe, must be called from a mutex
+	resetState := func() {
+		state.remaining = uint16(len(js))
+		state.up = 0
+		state.down = 0
+		state.generation++
+		u, err := uuid.NewV1()
+		if err != nil {
+			panic(fmt.Sprintf("cannot generate UUIDs on this system: %s", err))
+		}
+		state.checkGroup = u.String()
+	}
+	resetState()
+
+	// executed after each job to update the state
+	afterEach := func(event *beat.Event) {
+		eventStatus, _ := event.GetValue("monitor.status")
+		if eventStatus == "up" {
+			state.up++
+		} else {
+			state.down++
+		}
+		// No error check needed here
+		event.PutValue("monitor.check_group", state.checkGroup)
+		state.remaining--
+	}
+
+	// executed after the last job to create the finalizing
+	// continuation
+	afterLast := func(event *beat.Event) {
+		var statusStr string
+		if state.down == 0 {
+			statusStr = "up"
+		} else {
+			statusStr = "down"
+		}
+
+		eventext.MergeEventFields(event, common.MapStr{
+			"summary": common.MapStr{
+				"status": statusStr,
+				"up":     state.up,
+				"down":   state.down,
+			},
+		})
+	}
+
+	return func(job jobs.Job) jobs.Job {
+		return func(event *beat.Event) ([]jobs.Job, error) {
+			cont, err := job(event)
+			state.mtx.Lock()
+			defer state.mtx.Unlock()
+
+			afterEach(event)
+			if state.remaining == 0 {
+				afterLast(event)
+				resetState()
+			}
+
+			return cont, err
+		}
 	}
 }
