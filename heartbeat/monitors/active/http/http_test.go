29a4952626e22211abc7305d54cd7a6d9a8348f9
Andrew Cholakian <andrew@andrewvc.com>
2019-01-17 12:18:06 -0600

    Make timeout 5s

--- heartbeat/monitors/active/http/http_test.go
+++ heartbeat/monitors/active/http/http_test.go
@@ -50,7 +50,7 @@ func testRequest(t *testing.T, testURL string) *beat.Event {
 func testTLSRequest(t *testing.T, testURL string, extraConfig map[string]interface{}) *beat.Event {
 	configSrc := map[string]interface{}{
 		"urls":    testURL,
-		"timeout": "2s",
+		"timeout": "5s",
 	}
 
 	if extraConfig != nil {
@@ -231,7 +231,7 @@ func TestLargeResponse(t *testing.T) {
 
 	configSrc := map[string]interface{}{
 		"urls":                server.URL,
-		"timeout":             "2s",
+		"timeout":             "5s",
 		"check.response.body": "x",
 	}
 
