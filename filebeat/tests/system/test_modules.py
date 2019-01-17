6570d0f4d9be109eaf31053d12b6833cfabbc621
Nicolas Ruflin <spam@ruflin.com>
2019-01-17 22:53:34 +0100

    Rename beat.version to agent.version in the configs and docs (#10078)
    beat.version field was renamed to agent.version. This is now a follow up to also update the docs and config files.
    
    For the template name generation the field also had to be renamed. The old beat.* field was left in for backward compatiblity. In cases users migrated their config to 7 and forget to replace it in the template name, things keep working.
    
    Add support for observer in template strings.

--- filebeat/tests/system/test_modules.py
+++ filebeat/tests/system/test_modules.py
@@ -202,7 +202,7 @@ def clean_keys(obj):
     host_keys = ["host.name", "agent.hostname", "agent.type", "agent.ephemeral_id", "agent.id"]
     # The create timestamps area always new
     time_keys = ["read_timestamp", "event.created"]
-    # source path and beat.version can be different for each run
+    # source path and agent.version can be different for each run
     other_keys = ["log.file.path", "agent.version"]
 
     for key in host_keys + time_keys + other_keys:
