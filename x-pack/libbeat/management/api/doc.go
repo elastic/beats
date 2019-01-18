77db481ec1009da9e60de06e1f9ec819f0ca47b5
Pier-Hugues Pellerin <phpellerin@gmail.com>
2019-01-18 14:22:12 -0500

    header

--- x-pack/libbeat/management/api/doc.go
+++ x-pack/libbeat/management/api/doc.go
@@ -1,3 +1,7 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
 /*
 The Kibana CM Api returns a configuration format which cannot be ingested directly by our
 configuration parser, it need to be transformed from the generic format into an adapted format
