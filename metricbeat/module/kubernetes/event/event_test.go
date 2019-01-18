051e964d415dcb326163dfc55d710bbf16562102
Vijay Samuel <vjsamuel@ebay.com>
2019-01-17 20:44:35 -0800

    Add remaining memory metrics of pods in Kubernetes metricbeat module

--- metricbeat/module/kubernetes/event/event_test.go
+++ metricbeat/module/kubernetes/event/event_test.go
@@ -20,7 +20,7 @@ package event
 import (
 	"testing"
 
-	"github.com/ericchiang/k8s/apis/core/v1"
+	v1 "github.com/ericchiang/k8s/apis/core/v1"
 	k8s_io_apimachinery_pkg_apis_meta_v1 "github.com/ericchiang/k8s/apis/meta/v1"
 	"github.com/stretchr/testify/assert"
 
