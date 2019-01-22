820bc6ac3fe889966fdfd5a25fbb2a114008bb1d
beats-jenkins <spam@ruflin.com>
2019-01-22 11:19:20 +0100

    release postgresql.statement metricset as GA

--- metricbeat/module/postgresql/statement/statement.go
+++ metricbeat/module/postgresql/statement/statement.go
@@ -23,7 +23,6 @@ import (
 	"github.com/pkg/errors"
 
 	"github.com/elastic/beats/libbeat/common"
-	"github.com/elastic/beats/libbeat/common/cfgwarn"
 	"github.com/elastic/beats/metricbeat/mb"
 	"github.com/elastic/beats/metricbeat/module/postgresql"
 
@@ -52,8 +51,6 @@ type MetricSet struct {
 // New creates a new instance of the MetricSet. New is responsible for unpacking
 // any MetricSet specific configuration options if there are any.
 func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
-	cfgwarn.Beta("The statement metricset is beta")
-
 	config := struct{}{}
 	if err := base.Module().UnpackConfig(&config); err != nil {
 		return nil, err
