1907a3dcf5c2e7a7878b8b964b213897294e86db
beats-jenkins <spam@ruflin.com>
2019-01-23 20:24:40 +0100

    update paths again

--- filebeat/module/mongodb/fields.go
+++ filebeat/module/mongodb/fields.go
@@ -30,7 +30,7 @@ func init() {
 }
 
 // AssetMongodb returns asset data.
-// This is the base64 encoded gzipped contents of /Users/ruflin/Dev/gopath/src/github.com/elastic/beats/filebeat/module/mongodb.
+// This is the base64 encoded gzipped contents of module/mongodb.
 func AssetMongodb() string {
 	return "eJyUkdFO6zAMhu/7FL92vz1AL4502LS7wjOE1g3WkjhK0rHy9CjtJFoIBXxp6/f3Wd7jQmMNK05L91wBiZOhGrt7Z1cBHcU2sE8srsa/CgAa6QZD6CXAqxDZaTQ5cHqAEY2eDcVDBfRMpov1lNnDKUtLVq40eqqhgwz+3ing5jpPy9AHsUvaBMq1hC2BRvRiy1fgJjTXUVxS7OKd8L1ByeLDoxXrxZFLq/WbZOA8uDbPlEGrEmkJ/KZyA9LDUoxK0ypEN2V9fuHxqWn+P55Ww/n4C42vErqiokt0+5PgcY78qMOOk3Kd4ZjI/UKqYBfpSoHTWEgrwyqu+l6ll+n3B0NXMquZZR3UfFAKAxVQpVO2SaXEZ857AAAA//9KC+uk"
 }
