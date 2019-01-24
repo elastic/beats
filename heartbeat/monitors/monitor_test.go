129cb0c81210025eb2edfae6412b0407ab85ba95
Andrew Cholakian <andrew@andrewvc.com>
2019-01-24 06:01:32 -0800

    [Heartbeat] Incorporate factory metadata for autodiscover (#10258)
    [Heartbeat] Incorporate factory metadata for autodiscover
    
    Heartbeat factories get metadata from autodiscover and other sources.
    
    This change automatically adds that data to events keeping heartbeat behavior in-line with other beats.

--- heartbeat/monitors/monitor_test.go
+++ heartbeat/monitors/monitor_test.go
@@ -38,7 +38,7 @@ func TestMonitor(t *testing.T) {
 	require.NoError(t, err)
 	defer sched.Stop()
 
-	mon, err := newMonitor(serverMonConf, reg, pipelineConnector, sched, false)
+	mon, err := newMonitor(serverMonConf, reg, pipelineConnector, sched, false, nil)
 	require.NoError(t, err)
 
 	mon.Start()
@@ -86,7 +86,7 @@ func TestDuplicateMonitorIDs(t *testing.T) {
 	defer sched.Stop()
 
 	makeTestMon := func() (*Monitor, error) {
-		return newMonitor(serverMonConf, reg, pipelineConnector, sched, false)
+		return newMonitor(serverMonConf, reg, pipelineConnector, sched, false, nil)
 	}
 
 	m1, m1Err := makeTestMon()
