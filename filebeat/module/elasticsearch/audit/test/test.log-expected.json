elastic/beats

commit bdd8c49c194117d2b017d9f0a158686ed4f5989c

Cherry-pick #10135 to 6.x: Elasticsearch/audit fileset should be more lenient in parsing node name (#10174)
Cherry-pick of PR #10135 to 6.x branch. Original message: 

Resolves https://github.com/elastic/beats/issues/10035.

This PR:

* Uses `DATA` instead of `WORD` in the grok pattern for parsing out `elasticsearch.node.name`,
* Breaks out the grok pattern into pattern definitions to increase readability
* Removes a redundant `?` after a `*` in the grok pattern (between `elasticsearch.audit.action` and `elasticsearch.audit.uri`), and
* Properly reindents the pipeline JSON (so you might want to view the diff with `?w=1` appended to the URL)

filebeat/module/elasticsearch/audit/test/test.log-expected.json
@@ -1,12 +1,11 @@
 [
     {
         "@timestamp": "2018-06-19T05:16:15.549Z",
-        "elasticsearch.audit.event_type": "authentication_failed",
         "elasticsearch.audit.layer": "rest",
         "elasticsearch.audit.origin_address": "147.107.128.77",
         "elasticsearch.audit.principal": "i030648",
-        "elasticsearch.audit.uri": "/_xpack/security/_authenticate",
         "event.dataset": "elasticsearch.audit",
+        "event.type": "authentication_failed",
         "fileset.module": "elasticsearch",
         "fileset.name": "audit",
         "input.type": "log",
@@ -17,13 +16,12 @@
     },
     {
         "@timestamp": "2018-06-19T05:07:52.304Z",
-        "elasticsearch.audit.event_type": "authentication_failed",
         "elasticsearch.audit.layer": "rest",
         "elasticsearch.audit.origin_address": "172.22.0.3",
         "elasticsearch.audit.principal": "rado",
-        "elasticsearch.audit.uri": "/_xpack/security/_authenticate",
         "elasticsearch.node.name": "v_VJhjV",
         "event.dataset": "elasticsearch.audit",
+        "event.type": "authentication_failed",
         "fileset.module": "elasticsearch",
         "fileset.name": "audit",
         "input.type": "log",
@@ -35,13 +33,13 @@
     {
         "@timestamp": "2018-06-19T05:00:15.778Z",
         "elasticsearch.audit.action": "indices:data/read/scroll/clear",
-        "elasticsearch.audit.event_type": "access_granted",
         "elasticsearch.audit.layer": "transport",
         "elasticsearch.audit.origin_address": "192.168.1.165",
         "elasticsearch.audit.origin_type": "local_node",
         "elasticsearch.audit.principal": "_xpack_security",
         "elasticsearch.audit.request": "ClearScrollRequest",
         "event.dataset": "elasticsearch.audit",
+        "event.type": "access_granted",
         "fileset.module": "elasticsearch",
         "fileset.name": "audit",
         "input.type": "log",
@@ -52,12 +50,11 @@
     },
     {
         "@timestamp": "2018-06-19T05:07:45.544Z",
-        "elasticsearch.audit.event_type": "anonymous_access_denied",
         "elasticsearch.audit.layer": "rest",
         "elasticsearch.audit.origin_address": "172.22.0.3",
-        "elasticsearch.audit.uri": "/_xpack/security/_authenticate",
         "elasticsearch.node.name": "v_VJhjV",
         "event.dataset": "elasticsearch.audit",
+        "event.type": "anonymous_access_denied",
         "fileset.module": "elasticsearch",
         "fileset.name": "audit",
         "input.type": "log",
@@ -68,12 +65,11 @@
     },
     {
         "@timestamp": "2018-06-19T05:26:27.268Z",
-        "elasticsearch.audit.event_type": "authentication_failed",
         "elasticsearch.audit.layer": "rest",
         "elasticsearch.audit.origin_address": "147.107.128.77",
         "elasticsearch.audit.principal": "N078801",
-        "elasticsearch.audit.uri": "/_xpack/security/_authenticate",
         "event.dataset": "elasticsearch.audit",
+        "event.type": "authentication_failed",
         "fileset.module": "elasticsearch",
         "fileset.name": "audit",
         "input.type": "log",
@@ -85,13 +81,13 @@
     {
         "@timestamp": "2018-06-19T05:55:26.898Z",
         "elasticsearch.audit.action": "cluster:monitor/main",
-        "elasticsearch.audit.event_type": "access_denied",
         "elasticsearch.audit.layer": "transport",
         "elasticsearch.audit.origin_address": "147.107.128.77",
         "elasticsearch.audit.origin_type": "rest",
         "elasticsearch.audit.principal": "_anonymous",
         "elasticsearch.audit.request": "MainRequest",
         "event.dataset": "elasticsearch.audit",
+        "event.type": "access_denied",
         "fileset.module": "elasticsearch",
         "fileset.name": "audit",
         "input.type": "log",
@@ -102,19 +98,34 @@
     },
     {
         "@timestamp": "2018-06-19T05:24:15.190Z",
-        "elasticsearch.audit.event_type": "authentication_failed",
         "elasticsearch.audit.layer": "rest",
         "elasticsearch.audit.origin_address": "172.18.0.3",
         "elasticsearch.audit.principal": "elastic",
-        "elasticsearch.audit.request_body": "body",
-        "elasticsearch.audit.uri": "/_nodes?filter_path=nodes.*.version%2Cnodes.*.http.publish_address%2Cnodes.*.ip",
         "elasticsearch.node.name": "v_VJhjV",
         "event.dataset": "elasticsearch.audit",
+        "event.type": "authentication_failed",
         "fileset.module": "elasticsearch",
         "fileset.name": "audit",
         "input.type": "log",
         "message": "[2018-06-19T05:24:15,190] [v_VJhjV] [rest] [authentication_failed]\torigin_address=[172.18.0.3], principal=[elastic], uri=[/_nodes?filter_path=nodes.*.version%2Cnodes.*.http.publish_address%2Cnodes.*.ip], request_body=[body]",
         "offset": 986,
+        "prospector.type": "log",
+        "service.name": "elasticsearch"
+    },
+    {
+        "@timestamp": "2019-01-08T14:15:02.011Z",
+        "elasticsearch.audit.layer": "transport",
+        "elasticsearch.audit.origin_address": "192.168.2.1",
+        "elasticsearch.audit.origin_type": "transport",
+        "elasticsearch.audit.principal": "username",
+        "elasticsearch.node.name": "NodeName-0",
+        "event.dataset": "elasticsearch.audit",
+        "event.type": "access_granted",
+        "fileset.module": "elasticsearch",
+        "fileset.name": "audit",
+        "input.type": "log",
+        "message": "[2019-01-08T14:15:02,011] [NodeName-0] [transport] [access_granted]     origin_type=[transport], origin_address=[192.168.2.1], principal=[username], realm=[active_directory], roles=[kibana_user,my_custom_role_1,foo_reader], action=[indices:data/read/search[free_context]], indices=[foo-2019.01.04,foo-2019.01.03,foo-2019.01.06,foo-2019.01.05,foo-2019.01.08,servicelog-2019.01.07], request=[SearchFreeContextRequest]",
+        "offset": 1210,
         "prospector.type": "log",
         "service.name": "elasticsearch"
     }

filebeat/module/elasticsearch/audit/test/test.log
@@ -5,3 +5,4 @@
 [2018-06-19T05:26:27,268] [rest] [authentication_failed]	origin_address=[147.107.128.77], principal=[N078801], uri=[/_xpack/security/_authenticate]
 [2018-06-19T05:55:26,898] [transport] [access_denied]	origin_type=[rest], origin_address=[147.107.128.77], principal=[_anonymous], action=[cluster:monitor/main], request=[MainRequest]
 [2018-06-19T05:24:15,190] [v_VJhjV] [rest] [authentication_failed]	origin_address=[172.18.0.3], principal=[elastic], uri=[/_nodes?filter_path=nodes.*.version%2Cnodes.*.http.publish_address%2Cnodes.*.ip], request_body=[body]
+[2019-01-08T14:15:02,011] [NodeName-0] [transport] [access_granted]     origin_type=[transport], origin_address=[192.168.2.1], principal=[username], realm=[active_directory], roles=[kibana_user,my_custom_role_1,foo_reader], action=[indices:data/read/search[free_context]], indices=[foo-2019.01.04,foo-2019.01.03,foo-2019.01.06,foo-2019.01.05,foo-2019.01.08,servicelog-2019.01.07], request=[SearchFreeContextRequest]

commit 897c1bd06ee91c970601fcd1bc9e749ae784828a

Fixing breakage caused in #10174 (#10340)
* Fixing event type field to be non-ECS

* Fix parsing of URI field

filebeat/module/elasticsearch/audit/test/test.log-expected.json
@@ -1,11 +1,12 @@
 [
     {
         "@timestamp": "2018-06-19T05:16:15.549Z",
+        "elasticsearch.audit.event_type": "authentication_failed",
         "elasticsearch.audit.layer": "rest",
         "elasticsearch.audit.origin_address": "147.107.128.77",
         "elasticsearch.audit.principal": "i030648",
+        "elasticsearch.audit.uri": "/_xpack/security/_authenticate",
         "event.dataset": "elasticsearch.audit",
-        "event.type": "authentication_failed",
         "fileset.module": "elasticsearch",
         "fileset.name": "audit",
         "input.type": "log",
@@ -16,12 +17,13 @@
     },
     {
         "@timestamp": "2018-06-19T05:07:52.304Z",
+        "elasticsearch.audit.event_type": "authentication_failed",
         "elasticsearch.audit.layer": "rest",
         "elasticsearch.audit.origin_address": "172.22.0.3",
         "elasticsearch.audit.principal": "rado",
+        "elasticsearch.audit.uri": "/_xpack/security/_authenticate",
         "elasticsearch.node.name": "v_VJhjV",
         "event.dataset": "elasticsearch.audit",
-        "event.type": "authentication_failed",
         "fileset.module": "elasticsearch",
         "fileset.name": "audit",
         "input.type": "log",
@@ -33,13 +35,13 @@
     {
         "@timestamp": "2018-06-19T05:00:15.778Z",
         "elasticsearch.audit.action": "indices:data/read/scroll/clear",
+        "elasticsearch.audit.event_type": "access_granted",
         "elasticsearch.audit.layer": "transport",
         "elasticsearch.audit.origin_address": "192.168.1.165",
         "elasticsearch.audit.origin_type": "local_node",
         "elasticsearch.audit.principal": "_xpack_security",
         "elasticsearch.audit.request": "ClearScrollRequest",
         "event.dataset": "elasticsearch.audit",
-        "event.type": "access_granted",
         "fileset.module": "elasticsearch",
         "fileset.name": "audit",
         "input.type": "log",
@@ -50,11 +52,12 @@
     },
     {
         "@timestamp": "2018-06-19T05:07:45.544Z",
+        "elasticsearch.audit.event_type": "anonymous_access_denied",
         "elasticsearch.audit.layer": "rest",
         "elasticsearch.audit.origin_address": "172.22.0.3",
+        "elasticsearch.audit.uri": "/_xpack/security/_authenticate",
         "elasticsearch.node.name": "v_VJhjV",
         "event.dataset": "elasticsearch.audit",
-        "event.type": "anonymous_access_denied",
         "fileset.module": "elasticsearch",
         "fileset.name": "audit",
         "input.type": "log",
@@ -65,11 +68,12 @@
     },
     {
         "@timestamp": "2018-06-19T05:26:27.268Z",
+        "elasticsearch.audit.event_type": "authentication_failed",
         "elasticsearch.audit.layer": "rest",
         "elasticsearch.audit.origin_address": "147.107.128.77",
         "elasticsearch.audit.principal": "N078801",
+        "elasticsearch.audit.uri": "/_xpack/security/_authenticate",
         "event.dataset": "elasticsearch.audit",
-        "event.type": "authentication_failed",
         "fileset.module": "elasticsearch",
         "fileset.name": "audit",
         "input.type": "log",
@@ -81,13 +85,13 @@
     {
         "@timestamp": "2018-06-19T05:55:26.898Z",
         "elasticsearch.audit.action": "cluster:monitor/main",
+        "elasticsearch.audit.event_type": "access_denied",
         "elasticsearch.audit.layer": "transport",
         "elasticsearch.audit.origin_address": "147.107.128.77",
         "elasticsearch.audit.origin_type": "rest",
         "elasticsearch.audit.principal": "_anonymous",
         "elasticsearch.audit.request": "MainRequest",
         "event.dataset": "elasticsearch.audit",
-        "event.type": "access_denied",
         "fileset.module": "elasticsearch",
         "fileset.name": "audit",
         "input.type": "log",
@@ -98,12 +102,14 @@
     },
     {
         "@timestamp": "2018-06-19T05:24:15.190Z",
+        "elasticsearch.audit.event_type": "authentication_failed",
         "elasticsearch.audit.layer": "rest",
         "elasticsearch.audit.origin_address": "172.18.0.3",
         "elasticsearch.audit.principal": "elastic",
+        "elasticsearch.audit.request_body": "body",
+        "elasticsearch.audit.uri": "/_nodes?filter_path=nodes.*.version%2Cnodes.*.http.publish_address%2Cnodes.*.ip",
         "elasticsearch.node.name": "v_VJhjV",
         "event.dataset": "elasticsearch.audit",
-        "event.type": "authentication_failed",
         "fileset.module": "elasticsearch",
         "fileset.name": "audit",
         "input.type": "log",
@@ -115,6 +121,7 @@
     {
         "@timestamp": "2019-01-08T14:15:02.011Z",
         "elasticsearch.audit.action": "indices:data/read/search[free_context]",
+        "elasticsearch.audit.event_type": "access_granted",
         "elasticsearch.audit.indices": [
             "foo-2019.01.04",
             "foo-2019.01.03",
@@ -136,7 +143,6 @@
         ],
         "elasticsearch.node.name": "NodeName-0",
         "event.dataset": "elasticsearch.audit",
-        "event.type": "access_granted",
         "fileset.module": "elasticsearch",
         "fileset.name": "audit",
         "input.type": "log",