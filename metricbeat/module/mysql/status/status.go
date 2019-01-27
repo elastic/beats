ed982ebe7312b185d9a07a01cb9a40967b0c73a6
Jaime Arrocha <jarrocha27@gmail.com>
2019-01-27 14:48:31 -0700

    New functions to close a mysql connection. Issue #9415

--- metricbeat/module/mysql/status/status.go
+++ metricbeat/module/mysql/status/status.go
@@ -107,10 +107,8 @@ func (m *MetricSet) loadStatus(db *sql.DB) (map[string]string, error) {
 
 // Close closes the database connection and prevents future queries.
 func (m *MetricSet) Close() error {
-	err := m.db.Close()
-	if err != nil {
-		return errors.Wrap(err, "mysql-status close failed")
+	if m.db == nil {
+		return errors.New("invalid database handle")
 	}
-
-	return nil
+	return errors.Wrap(m.db.Close(), "failed to close mysql database client")
 }
