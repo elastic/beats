7f2ab44c3e2a9a43b338c1bd408836f4cdc5141d
Mario Castro <mariocaster@gmail.com>
2019-01-31 20:54:44 +0100

    Cherry-pick #10341 to 6.x: server Metricset for Zookeeper Metricbeat module (#10375)
    Cherry pick wasn't completed clean as we needed to adjust `service.version` field, which is documented in `fields.yml` on the module as (in master it is in ECS but ECS is not merged in 6.x)

--- metricbeat/tests/system/test_zookeeper.py
+++ metricbeat/tests/system/test_zookeeper.py
@@ -50,6 +50,35 @@ class ZooKeeperMntrTest(metricbeat.BaseTest):
 
         self.assert_fields_are_documented(evt)
 
+    @unittest.skipUnless(metricbeat.INTEGRATION_TESTS, "integration test")
+    @attr('integration')
+    def test_output(self):
+        """
+        ZooKeeper server module outputs an event.
+        """
+        self.render_config_template(modules=[{
+            "name": "zookeeper",
+            "metricsets": ["server"],
+            "hosts": self.get_hosts(),
+            "period": "5s"
+        }])
+        proc = self.start_beat()
+        self.wait_until(lambda: self.output_lines() > 0)
+        proc.check_kill_and_wait()
+        self.assert_no_logged_warnings()
+
+        output = self.read_output_json()
+        self.assertEqual(len(output), 1)
+        evt = output[0]
+
+        self.assertItemsEqual(self.de_dot(ZK_FIELDS), evt.keys())
+
+        zk_srvr = evt["zookeeper"]["server"]
+
+        assert zk_srvr["connections"] >= 0
+
+        self.assert_fields_are_documented(evt)
+
     def get_hosts(self):
         return [os.getenv('ZOOKEEPER_HOST', 'localhost') + ':' +
                 os.getenv('ZOOKEEPER_PORT', '2181')]
