dc876e77879f118077730ce3aca8e03fc44df850
Christoph Wurm <cw@elastic.co>
2019-02-05 14:05:58 +0000

    [Auditbeat] System module: Add entity_id fields (#10500)
    Implements `{entity}.entity_id` as a SHA-256 hash as proposed in https://github.com/elastic/beats/issues/10463.
    
    Closes https://github.com/elastic/beats/issues/10463.

--- x-pack/auditbeat/tests/system/test_metricsets.py
+++ x-pack/auditbeat/tests/system/test_metricsets.py
@@ -17,7 +17,8 @@ class Test(AuditbeatXPackTest):
         host metricset collects general information about a server.
         """
 
-        fields = ["system.audit.host.uptime", "system.audit.host.ip", "system.audit.host.os.name"]
+        fields = ["system.audit.host.id", "system.audit.host.uptime", "system.audit.host.ip",
+                  "system.audit.host.os.name"]
 
         # Metricset is experimental and that generates a warning, TODO: remove later
         self.check_metricset("system", "host", COMMON_FIELDS + fields, warnings_allowed=True)
@@ -48,7 +49,7 @@ class Test(AuditbeatXPackTest):
         package metricset collects information about installed packages on a system.
         """
 
-        fields = ["system.audit.package.name", "system.audit.package.version"]
+        fields = ["system.audit.package.entity_id", "system.audit.package.name", "system.audit.package.version"]
 
         # Metricset is experimental and that generates a warning, TODO: remove later
         self.check_metricset("system", "package", COMMON_FIELDS + fields, warnings_allowed=True)
@@ -58,8 +59,8 @@ class Test(AuditbeatXPackTest):
         process metricset collects information about processes running on a system.
         """
 
-        fields = ["process.pid", "process.ppid", "process.name", "process.executable", "process.args",
-                  "process.start", "process.working_directory", "user.id", "user.group.id"]
+        fields = ["process.entity_id", "process.pid", "process.ppid", "process.name", "process.executable",
+                  "process.args", "process.start", "process.working_directory", "user.id", "user.group.id"]
 
         # Windows does not have effective and saved IDs, and user.name is not always filled for system processes.
         if sys.platform != "win32":
@@ -75,7 +76,7 @@ class Test(AuditbeatXPackTest):
         socket metricset collects information about open sockets on a system.
         """
 
-        fields = ["destination.port"]
+        fields = ["socket.entity_id", "destination.port"]
 
         # errors_allowed=True - The socket metricset fills the `error` field if the process enrichment fails
         # (e.g. process has exited). This should not fail the test.
@@ -88,7 +89,7 @@ class Test(AuditbeatXPackTest):
         user metricset collects information about users on a server.
         """
 
-        fields = ["system.audit.user.name"]
+        fields = ["user.entity_id", "system.audit.user.name"]
 
         # Metricset is experimental and that generates a warning, TODO: remove later
         self.check_metricset("system", "user", COMMON_FIELDS + fields, warnings_allowed=True)
