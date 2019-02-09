1ac0f2ce64f2c3e816a44f997a7d97bded92d26f
Christoph Wurm <cw@elastic.co>
2019-02-09 11:50:04 +0000

    Use rpmsqSetInterruptSafety() when possible.

--- x-pack/auditbeat/module/system/package/rpm_linux.go
+++ x-pack/auditbeat/module/system/package/rpm_linux.go
@@ -12,6 +12,7 @@ import (
 	"unsafe"
 
 	"github.com/coreos/pkg/dlopen"
+	"github.com/joeshaw/multierror"
 )
 
 /*
@@ -103,27 +104,38 @@ my_rpmtsFree(void *f, rpmts ts) {
   rpmtsFree(ts);
 }
 
-// By default, librpm is going to trap SIGINT and SIGTERM,
+// By default, librpm is going to trap various UNIX signals including SIGINT and SIGTERM
 // which will prevent Beats from shutting down correctly.
 //
 // This disables that behavior. We should be very dilligent in
 // cleaning up in our use of librpm.
 //
 // More recent versions of librpm have a new function rpmsqSetInterruptSafety()
-// to do this.
+// to do this, see below.
 //
 // See also:
 // - librpm traps signals and calls exit(1) to terminate the whole process incl. our Go code: https://github.com/rpm-software-management/rpm/blob/rpm-4.11.3-release/lib/rpmdb.c#L640
 // - has caused problems for gdb before, calling rpmsqEnable(_, NULL) is the workaround they also use: https://bugzilla.redhat.com/show_bug.cgi?id=643031
-// - the new rpmsqSetInterruptSafety(), unfortunately only available in librpm>=4.14.2.1 (CentOS 7 has 4.11.3): https://github.com/rpm-software-management/rpm/commit/56f49d7f5af7c1c8a3eb478431356195adbfdd25
+// - the new rpmsqSetInterruptSafety(), unfortunately only available in librpm>=4.14.0 (CentOS 7 has 4.11.3): https://github.com/rpm-software-management/rpm/commit/56f49d7f5af7c1c8a3eb478431356195adbfdd25
 void
 my_disableLibrpmSignalTraps(void *f) {
 	int (*rpmsqEnable)(int, rpmsqAction_t);
 	rpmsqEnable = (int (*)(int, rpmsqAction_t))f;
 
-	// Disable traps
-	rpmsqEnable(-SIGTERM, NULL);
+	// Disable all traps
+	rpmsqEnable(-SIGHUP, NULL);
 	rpmsqEnable(-SIGINT, NULL);
+	rpmsqEnable(-SIGTERM, NULL);
+	rpmsqEnable(-SIGQUIT, NULL);
+	rpmsqEnable(-SIGPIPE, NULL);
+}
+
+void
+my_rpmsqSetInterruptSafety(void *f, int on) {
+	void (*rpmsqSetInterruptSafety)(int);
+	rpmsqSetInterruptSafety = (void (*)(int))f;
+
+	rpmsqSetInterruptSafety(on);
 }
 */
 import "C"
@@ -163,11 +175,6 @@ func dlopenCFunctions() (*cFunctions, error) {
 	var cFun cFunctions
 
 	librpm, err := dlopen.GetHandle(librpmNames)
-	if err != nil {
-		return nil, err
-	}
-
-	cFun.rpmsqEnable, err = librpm.GetSymbolPointer("rpmsqEnable")
 	if err != nil {
 		return nil, err
 	}
@@ -217,6 +224,22 @@ func dlopenCFunctions() (*cFunctions, error) {
 		return nil, err
 	}
 
+	// Only available in librpm>=4.13.0
+	rpmsqSetInterruptSafety, err := librpm.GetSymbolPointer("rpmsqSetInterruptSafety")
+	if err != nil {
+		var err2 error
+		// Only available in librpm<4.14.0
+		cFun.rpmsqEnable, err2 = librpm.GetSymbolPointer("rpmsqEnable")
+		if err2 != nil {
+			var errs multierror.Errors
+			errs = append(errs, err)
+			errs = append(errs, err2)
+			return nil, errs.Err()
+		}
+	} else {
+		C.my_rpmsqSetInterruptSafety(rpmsqSetInterruptSafety, 1)
+	}
+
 	return &cFun, nil
 }
 
@@ -243,7 +266,9 @@ func listRPMPackages() ([]*Package, error) {
 	if mi == nil {
 		return nil, fmt.Errorf("Failed to get match iterator")
 	}
-	C.my_disableLibrpmSignalTraps(cFun.rpmsqEnable)
+	if cFun.rpmsqEnable != nil {
+		C.my_disableLibrpmSignalTraps(cFun.rpmsqEnable)
+	}
 	defer C.my_rpmdbFreeIterator(cFun.rpmdbFreeIterator, mi)
 
 	var packages []*Package
