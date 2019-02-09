252ec04bf5a9f33c51cb7396269424b52e4ee1fe
Shaunak Kashyap <ycombinator@gmail.com>
2019-02-08 22:33:07 -0800

    Refactoring

--- libbeat/tests/system/test_monitoring.py
+++ libbeat/tests/system/test_monitoring.py
@@ -16,10 +16,6 @@ class Test(BaseTest):
         self.es = Elasticsearch([self.get_elasticsearch_url()])
         self.es_monitoring = Elasticsearch([self.get_elasticsearch_monitoring_url()])
 
-    def tearDown(self):
-        if self.proc:
-            self.proc.check_kill_and_wait()
-
     @unittest.skipUnless(INTEGRATION_TESTS, "integration test")
     @attr('integration')
     def test_via_output_cluster(self):
@@ -42,13 +38,15 @@ class Test(BaseTest):
         self.init_output_cluster()
         self.init_monitoring_cluster()
 
-        self.proc = self.start_beat(config="mockbeat.yml")
+        proc = self.start_beat(config="mockbeat.yml")
         self.wait_until(lambda: self.log_contains("mockbeat start running."))
         self.wait_until(lambda: self.log_contains(re.compile("\[monitoring\].*Publish event")))
         self.wait_until(lambda: self.log_contains(re.compile(
             "Connection to .*elasticsearch\("+self.get_elasticsearch_url()+"\).* established")))
         self.wait_until(lambda: self.monitoring_doc_exists('beats_stats'))
         self.wait_until(lambda: self.monitoring_doc_exists('beats_state'))
+
+        proc.check_kill_and_wait()
 
         for monitoring_doc_type in ['beats_stats', 'beats_state']:
             field_names = ['cluster_uuid', 'timestamp', 'interval_ms', 'type', 'source_node', monitoring_doc_type]
@@ -73,13 +71,15 @@ class Test(BaseTest):
 
         self.init_monitoring_cluster()
 
-        self.proc = self.start_beat(config="mockbeat.yml")
+        proc = self.start_beat(config="mockbeat.yml")
         self.wait_until(lambda: self.log_contains("mockbeat start running."))
         self.wait_until(lambda: self.log_contains(re.compile("\[monitoring\].*Publish event")))
         self.wait_until(lambda: self.log_contains(re.compile(
             "Connection to .*elasticsearch\("+self.get_elasticsearch_monitoring_url()+"\).* established")))
         self.wait_until(lambda: self.monitoring_doc_exists('beats_stats'))
         self.wait_until(lambda: self.monitoring_doc_exists('beats_state'))
+
+        proc.check_kill_and_wait()
 
         for monitoring_doc_type in ['beats_stats', 'beats_state']:
             field_names = ['cluster_uuid', 'timestamp', 'interval_ms', 'type', monitoring_doc_type]
@@ -106,7 +106,7 @@ class Test(BaseTest):
         self.init_output_cluster()
         self.init_monitoring_cluster()
 
-        self.proc = self.start_beat(config="mockbeat.yml")
+        proc = self.start_beat(config="mockbeat.yml")
         self.wait_until(lambda: self.log_contains("mockbeat start running."))
         self.wait_until(lambda: self.log_contains(re.compile("\[monitoring\].*Publish event")))
         self.wait_until(lambda: self.log_contains(re.compile(
@@ -114,10 +114,10 @@ class Test(BaseTest):
         self.wait_until(lambda: self.monitoring_doc_exists('beats_stats'))
         self.wait_until(lambda: self.monitoring_doc_exists('beats_state'))
 
+        proc.check_kill_and_wait()
+
         # TODO: Get a beats_stats doc
         # TODO: Get a beats_state doc
-
-        self.proc.check_kill_and_wait()
 
         self.render_config_template(
             "mockbeat",
@@ -130,13 +130,15 @@ class Test(BaseTest):
 
         self.init_monitoring_cluster()
 
-        self.proc = self.start_beat(config="mockbeat.yml")
+        proc = self.start_beat(config="mockbeat.yml")
         self.wait_until(lambda: self.log_contains("mockbeat start running."))
         self.wait_until(lambda: self.log_contains(re.compile("\[monitoring\].*Publish event")))
         self.wait_until(lambda: self.log_contains(re.compile(
             "Connection to .*elasticsearch\("+self.get_elasticsearch_monitoring_url()+"\).* established")))
         self.wait_until(lambda: self.monitoring_doc_exists('beats_stats'))
         self.wait_until(lambda: self.monitoring_doc_exists('beats_state'))
+
+        proc.check_kill_and_wait()
 
         # TODO: Get a beats_stats doc
         # TODO: Get a beats_state doc
