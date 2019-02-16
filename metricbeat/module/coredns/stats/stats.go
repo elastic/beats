92f5c3e56870a93bd2598835f4c72a0eaad1477e
Ioannis Androulidakis <ioannis@arrikto.com>
2019-02-16 16:18:48 +0200

    Review fixes
    * Rename metrics to stats in coredns/stats/_meta/fields.yaml
    * Use .sec naming convention instead of .seconds
    
    Signed-off-by: Ioannis Androulidakis <ioannis@arrikto.com>

--- metricbeat/module/coredns/stats/stats.go
+++ metricbeat/module/coredns/stats/stats.go
@@ -26,7 +26,7 @@ var mapping = &prometheus.MetricsMapping{
 	Metrics: map[string]prometheus.MetricMap{
 		"coredns_panic_count_total":              prometheus.Metric("panic.count.total"),
 		"coredns_dns_request_count_total":        prometheus.Metric("dns.request.count.total"),
-		"coredns_dns_request_duration_seconds":   prometheus.Metric("dns.request.duration.seconds"),
+		"coredns_dns_request_duration_seconds":   prometheus.Metric("dns.request.duration.sec"),
 		"coredns_dns_request_size_bytes":         prometheus.Metric("dns.request.size.bytes"),
 		"coredns_dns_request_do_count_total":     prometheus.Metric("dns.request.do.count.total"),
 		"coredns_dns_request_type_count_total":   prometheus.Metric("dns.request.type.count.total"),
