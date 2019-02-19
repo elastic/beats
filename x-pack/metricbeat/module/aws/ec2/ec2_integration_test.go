e42edb90c8ec86e1db0f93309cf227ef03a5a5d2
kaiyan-sheng <kaiyan.sheng@elastic.co>
2019-02-19 08:00:00 -0700

    Move aws_testing.go under mtest

--- x-pack/metricbeat/module/aws/ec2/ec2_integration_test.go
+++ x-pack/metricbeat/module/aws/ec2/ec2_integration_test.go
@@ -2,8 +2,6 @@
 // or more contributor license agreements. Licensed under the Elastic License;
 // you may not use this file except in compliance with the Elastic License.
 
-// +build integration
-
 package ec2
 
 import (
@@ -12,11 +10,11 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	mbtest "github.com/elastic/beats/metricbeat/mb/testing"
-	"github.com/elastic/beats/x-pack/metricbeat/module/aws"
+	"github.com/elastic/beats/x-pack/metricbeat/module/aws/mtest"
 )
 
 func TestFetch(t *testing.T) {
-	config, info := aws.GetConfigForTest("ec2")
+	config, info := mtest.GetConfigForTest("ec2")
 	if info != "" {
 		t.Skip("Skipping TestFetch: " + info)
 	}
@@ -36,36 +34,36 @@ func TestFetch(t *testing.T) {
 
 	for _, event := range events {
 		// RootField
-		aws.CheckEventField("service.name", "string", event, t)
-		aws.CheckEventField("cloud.availability_zone", "string", event, t)
-		aws.CheckEventField("cloud.provider", "string", event, t)
-		aws.CheckEventField("cloud.image.id", "string", event, t)
-		aws.CheckEventField("cloud.instance.id", "string", event, t)
-		aws.CheckEventField("cloud.machine.type", "string", event, t)
-		aws.CheckEventField("cloud.provider", "string", event, t)
-		aws.CheckEventField("cloud.region", "string", event, t)
+		mtest.CheckEventField("service.name", "string", event, t)
+		mtest.CheckEventField("cloud.availability_zone", "string", event, t)
+		mtest.CheckEventField("cloud.provider", "string", event, t)
+		mtest.CheckEventField("cloud.image.id", "string", event, t)
+		mtest.CheckEventField("cloud.instance.id", "string", event, t)
+		mtest.CheckEventField("cloud.machine.type", "string", event, t)
+		mtest.CheckEventField("cloud.provider", "string", event, t)
+		mtest.CheckEventField("cloud.region", "string", event, t)
 		// MetricSetField
-		aws.CheckEventField("cpu.total.pct", "float", event, t)
-		aws.CheckEventField("cpu.credit_usage", "float", event, t)
-		aws.CheckEventField("cpu.credit_balance", "float", event, t)
-		aws.CheckEventField("cpu.surplus_credit_balance", "float", event, t)
-		aws.CheckEventField("cpu.surplus_credits_charged", "float", event, t)
-		aws.CheckEventField("network.in.packets", "float", event, t)
-		aws.CheckEventField("network.out.packets", "float", event, t)
-		aws.CheckEventField("network.in.bytes", "float", event, t)
-		aws.CheckEventField("network.out.bytes", "float", event, t)
-		aws.CheckEventField("diskio.read.bytes", "float", event, t)
-		aws.CheckEventField("diskio.write.bytes", "float", event, t)
-		aws.CheckEventField("diskio.read.ops", "float", event, t)
-		aws.CheckEventField("diskio.write.ops", "float", event, t)
-		aws.CheckEventField("status.check_failed", "int", event, t)
-		aws.CheckEventField("status.check_failed_system", "int", event, t)
-		aws.CheckEventField("status.check_failed_instance", "int", event, t)
+		mtest.CheckEventField("cpu.total.pct", "float", event, t)
+		mtest.CheckEventField("cpu.credit_usage", "float", event, t)
+		mtest.CheckEventField("cpu.credit_balance", "float", event, t)
+		mtest.CheckEventField("cpu.surplus_credit_balance", "float", event, t)
+		mtest.CheckEventField("cpu.surplus_credits_charged", "float", event, t)
+		mtest.CheckEventField("network.in.packets", "float", event, t)
+		mtest.CheckEventField("network.out.packets", "float", event, t)
+		mtest.CheckEventField("network.in.bytes", "float", event, t)
+		mtest.CheckEventField("network.out.bytes", "float", event, t)
+		mtest.CheckEventField("diskio.read.bytes", "float", event, t)
+		mtest.CheckEventField("diskio.write.bytes", "float", event, t)
+		mtest.CheckEventField("diskio.read.ops", "float", event, t)
+		mtest.CheckEventField("diskio.write.ops", "float", event, t)
+		mtest.CheckEventField("status.check_failed", "int", event, t)
+		mtest.CheckEventField("status.check_failed_system", "int", event, t)
+		mtest.CheckEventField("status.check_failed_instance", "int", event, t)
 	}
 }
 
 func TestData(t *testing.T) {
-	config, info := aws.GetConfigForTest("ec2")
+	config, info := mtest.GetConfigForTest("ec2")
 	if info != "" {
 		t.Skip("Skipping TestData: " + info)
 	}
--- x-pack/metricbeat/module/aws/mtest/integration.go
+++ x-pack/metricbeat/module/aws/mtest/integration.go
@@ -2,9 +2,7 @@
 // or more contributor license agreements. Licensed under the Elastic License;
 // you may not use this file except in compliance with the Elastic License.
 
-// +build integration
-
-package aws
+package mtest
 
 import (
 	"os"
