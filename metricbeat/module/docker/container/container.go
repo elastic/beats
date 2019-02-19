49e0450a7f7bff543837db6222db12357e5bfcb4
kaiyan-sheng <kaiyan.sheng@elastic.co>
2019-02-19 08:36:05 -0700

    Change docker container field names to match ECS

--- metricbeat/module/docker/container/container.go
+++ metricbeat/module/docker/container/container.go
@@ -20,10 +20,11 @@ package container
 import (
 	"context"
 
+	"github.com/elastic/beats/libbeat/logp"
+
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 
-	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/metricbeat/mb"
 	"github.com/elastic/beats/metricbeat/module/docker"
 )
@@ -39,34 +40,42 @@ type MetricSet struct {
 	mb.BaseMetricSet
 	dockerClient *client.Client
 	dedot        bool
+	logger       *logp.Logger
 }
 
 // New creates a new instance of the docker container MetricSet.
 func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
+	logger := logp.NewLogger("docker")
 	config := docker.DefaultConfig()
 	if err := base.Module().UnpackConfig(&config); err != nil {
 		return nil, err
 	}
 
-	client, err := docker.NewDockerClient(base.HostData().URI, config)
+	dockerClient, err := docker.NewDockerClient(base.HostData().URI, config)
 	if err != nil {
 		return nil, err
 	}
 
 	return &MetricSet{
 		BaseMetricSet: base,
-		dockerClient:  client,
+		dockerClient:  dockerClient,
 		dedot:         config.DeDot,
+		logger:        logger,
 	}, nil
 }
 
 // Fetch returns a list of all containers as events.
 // This is based on https://docs.docker.com/engine/reference/api/docker_remote_api_v1.24/#/list-containers.
-func (m *MetricSet) Fetch() ([]common.MapStr, error) {
+func (m *MetricSet) Fetch(report mb.ReporterV2) {
 	// Fetch a list of all containers.
 	containers, err := m.dockerClient.ContainerList(context.Background(), types.ContainerListOptions{})
 	if err != nil {
-		return nil, err
+		m.logger.Error(err.Error())
+		report.Error(err)
 	}
-	return eventsMapping(containers, m.dedot), nil
+
+	for _, container := range containers {
+		event := eventMapping(&container, m.dedot)
+		report.Event(event)
+	}
 }
