cdb23525d19e56a3bd734331f5e8150e0c283d23
kaiyan-sheng <kaiyan.sheng@elastic.co>
2019-02-20 23:37:26 -0700

    Change unit test back

--- libbeat/processors/add_docker_metadata/add_docker_metadata.go
+++ libbeat/processors/add_docker_metadata/add_docker_metadata.go
@@ -123,6 +123,11 @@ func (d *addDockerMetadata) Run(event *beat.Event) (*beat.Event, error) {
 	var cid string
 	var err error
 
+	// Extract CID
+	if v, err := event.GetValue(dockerContainerIDKey); err == nil {
+		cid, _ = v.(string)
+	}
+
 	// Extract CID from the filepath contained in the "source" field.
 	if d.sourceProcessor != nil {
 		if event.Fields["source"] != nil {
