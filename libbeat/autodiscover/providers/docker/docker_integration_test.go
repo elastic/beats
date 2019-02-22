6d69284632ad8e957b5ffd005cdb1e69e1d06598
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2019-02-22 19:03:05 +0100

    Fix docker autodiscover integration test

--- libbeat/autodiscover/providers/docker/docker_integration_test.go
+++ libbeat/autodiscover/providers/docker/docker_integration_test.go
@@ -94,18 +94,20 @@ func checkEvent(t *testing.T, listener bus.Listener, start bool) {
 			}
 			assert.Equal(t, getValue(e, "container.image.name"), "busybox")
 			// labels.dedot=true by default
-			assert.Equal(t, common.MapStr{
-				"label":       "foo",
-				"label_child": "bar",
-			},
-				getValue(e, "container.labels"))
+			assert.Equal(t,
+				common.MapStr{
+					"label": common.MapStr{
+						"value": "foo",
+						"child": "bar",
+					},
+				},
+				getValue(e, "container.labels"),
+			)
 			assert.NotNil(t, getValue(e, "container.id"))
 			assert.NotNil(t, getValue(e, "container.name"))
 			assert.NotNil(t, getValue(e, "host"))
-			assert.Equal(t, getValue(e, "container"), getValue(e, "meta.container"))
 			assert.Equal(t, getValue(e, "docker.container.id"), getValue(e, "meta.container.id"))
 			assert.Equal(t, getValue(e, "docker.container.name"), getValue(e, "meta.container.name"))
-			assert.Equal(t, getValue(e, "docker.container.labels"), getValue(e, "meta.container.labels"))
 			assert.Equal(t, getValue(e, "docker.container.image"), getValue(e, "meta.container.image.name"))
 			return
 
