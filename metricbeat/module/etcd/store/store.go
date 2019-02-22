0bb1494bc4225ab6d8265a50d3fdf74a979be750
Mario Castro <mariocaster@gmail.com>
2019-02-22 14:09:02 +0100

    [Metricbeat] Migrate ETCD Store to use ReporterV2 interface (#10815)

--- metricbeat/module/etcd/store/store.go
+++ metricbeat/module/etcd/store/store.go
@@ -18,7 +18,7 @@
 package store
 
 import (
-	"github.com/elastic/beats/libbeat/common"
+	"github.com/elastic/beats/libbeat/logp"
 	"github.com/elastic/beats/metricbeat/helper"
 	"github.com/elastic/beats/metricbeat/mb"
 	"github.com/elastic/beats/metricbeat/mb/parse"
@@ -35,6 +35,8 @@ var (
 		DefaultPath:   defaultPath,
 	}.Build()
 )
+
+var logger = logp.NewLogger("etcd.store")
 
 func init() {
 	mb.Registry.MustAddMetricSet("etcd", "store", New,
@@ -64,10 +66,18 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 	}, nil
 }
 
-func (m *MetricSet) Fetch() (common.MapStr, error) {
+// Fetch methods implements the data gathering and data conversion to the right
+// format. It publishes the event which is then forwarded to the output. In case
+// of an error set the Error field of mb.Event or simply call report.Error().
+func (m *MetricSet) Fetch(reporter mb.ReporterV2) {
 	content, err := m.http.FetchContent()
 	if err != nil {
-		return nil, err
+		logger.Error(err)
+		reporter.Error(err)
+		return
 	}
-	return eventMapping(content), nil
+
+	reporter.Event(mb.Event{
+		MetricSetFields: eventMapping(content),
+	})
 }
