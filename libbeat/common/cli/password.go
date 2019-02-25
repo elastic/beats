c3fac13b29bec715c1ec472505038b3eb1f7d1a1
Pier-Hugues Pellerin <phpellerin@gmail.com>
2019-02-25 11:06:48 -0500

    make fmt

--- libbeat/common/cli/password.go
+++ libbeat/common/cli/password.go
@@ -2,7 +2,7 @@
 // license agreements. See the NOTICE file distributed with
 // this work for additional information regarding copyright
 // ownership. Elasticsearch B.V. licenses this file to you under
-// Apache License, Version 2.0 (the "License"); you may
+// the Apache License, Version 2.0 (the "License"); you may
 // not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
