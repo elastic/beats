162d5fe6ffb5d4ce46f833ba9d2e5d4b25bbb843
sayden <mariocaster@gmail.com>
2019-02-27 20:48:51 +0100

    Atomic commit

--- metricbeat/module/kubernetes/state_replicaset/state_replicaset.go
+++ metricbeat/module/kubernetes/state_replicaset/state_replicaset.go
@@ -18,8 +18,8 @@
 package state_replicaset
 
 import (
-	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/libbeat/common/kubernetes"
+	"github.com/elastic/beats/libbeat/logp"
 	p "github.com/elastic/beats/metricbeat/helper/prometheus"
 	"github.com/elastic/beats/metricbeat/mb"
 	"github.com/elastic/beats/metricbeat/mb/parse"
@@ -56,6 +56,8 @@ var (
 			mb.NamespaceKey: "replicaset",
 		},
 	}
+
+	logger = logp.NewLogger("kubernetes.state_replicaset")
 )
 
 // init registers the MetricSet with the central registry.
@@ -91,18 +93,25 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 	}, nil
 }
 
-// Fetch methods implements the data gathering and data conversion to the right format
-// It returns the event which is then forward to the output. In case of an error, a
-// descriptive error must be returned.
-func (m *MetricSet) Fetch() ([]common.MapStr, error) {
+// Fetch methods implements the data gathering and data conversion to the right
+// format. It publishes the event which is then forwarded to the output. In case
+// of an error set the Error field of mb.Event or simply call report.Error().
+func (m *MetricSet) Fetch(reporter mb.ReporterV2) {
 	m.enricher.Start()
 
 	events, err := m.prometheus.GetProcessedMetrics(mapping)
-	if err == nil {
-		m.enricher.Enrich(events)
+	if err != nil {
+		logger.Error(err)
+		reporter.Error(err)
+		return
 	}
 
-	return events, err
+	m.enricher.Enrich(events)
+	for _, event := range events {
+		reporter.Event(mb.Event{MetricSetFields: event})
+	}
+
+	return
 }
 
 // Close stops this metricset
