d1a734324f8f41e98d5969e2c1a7b95400caceb6
Mario Castro <mariocaster@gmail.com>
2019-02-28 10:38:00 +0100

    [Metricbeat] Migrate Kubernetes volume Metricset to use ReporterV2 interface

--- metricbeat/module/kubernetes/volume/volume.go
+++ metricbeat/module/kubernetes/volume/volume.go
@@ -18,7 +18,7 @@
 package volume
 
 import (
-	"github.com/elastic/beats/libbeat/common"
+	"github.com/elastic/beats/libbeat/logp"
 	"github.com/elastic/beats/metricbeat/helper"
 	"github.com/elastic/beats/metricbeat/mb"
 	"github.com/elastic/beats/metricbeat/mb/parse"
@@ -34,6 +34,8 @@ var (
 		DefaultScheme: defaultScheme,
 		DefaultPath:   defaultPath,
 	}.Build()
+
+	logger = logp.NewLogger("kubernetes.volume")
 )
 
 // init registers the MetricSet with the central registry.
@@ -68,19 +70,21 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 	}, nil
 }
 
-// Fetch methods implements the data gathering and data conversion to the right format
-// It returns the event which is then forward to the output. In case of an error, a
-// descriptive error must be returned.
-func (m *MetricSet) Fetch() ([]common.MapStr, error) {
+// Fetch methods implements the data gathering and data conversion to the right
+// format. It publishes the event which is then forwarded to the output. In case
+// of an error set the Error field of mb.Event or simply call report.Error().
+func (m *MetricSet) Fetch(reporter mb.ReporterV2) {
 	body, err := m.http.FetchContent()
 	if err != nil {
-		return nil, err
+		logger.Error(err)
+		reporter.Error(err)
+		return
 	}
 
 	events, err := eventMapping(body)
-	if err != nil {
-		return nil, err
+	for _, e := range events {
+		reporter.Event(mb.Event{MetricSetFields: e})
 	}
 
-	return events, nil
+	return
 }
