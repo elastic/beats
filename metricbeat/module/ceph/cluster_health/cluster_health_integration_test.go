b43d954a6f8c677f7ac8ca5658ca43e362810c80
sayden <mariocaster@gmail.com>
2019-02-28 16:07:32 +0100

    Atomic commit

--- metricbeat/module/ceph/cluster_health/cluster_health_integration_test.go
+++ metricbeat/module/ceph/cluster_health/cluster_health_integration_test.go
@@ -19,18 +19,40 @@ package cluster_health
 
 import (
 	"fmt"
+	"github.com/elastic/beats/libbeat/tests/compose"
+	mbtest "github.com/elastic/beats/metricbeat/mb/testing"
+	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
-
-	mbtest "github.com/elastic/beats/metricbeat/mb/testing"
 )
 
 func TestData(t *testing.T) {
-	f := mbtest.NewEventFetcher(t, getConfig())
-	err := mbtest.WriteEvent(f, t)
-	if err != nil {
+	compose.EnsureUpWithTimeout(t, 30, "ceph")
+
+	f := mbtest.NewReportingMetricSetV2(t, getConfig())
+	events, errs := mbtest.ReportingFetchV2(f)
+	if len(errs) > 0 {
+		t.Fatalf("Expected 0 error, had %d. %v\n", len(errs), errs)
+	}
+	assert.NotEmpty(t, events)
+
+	if err := mbtest.WriteEventsReporterV2(f, t, ""); err != nil {
 		t.Fatal("write", err)
 	}
+}
+
+func TestFetch(t *testing.T) {
+	compose.EnsureUpWithTimeout(t, 30, "ceph")
+
+	f := mbtest.NewReportingMetricSetV2(t, getConfig())
+	events, errs := mbtest.ReportingFetchV2(f)
+	if len(errs) > 0 {
+		t.Fatalf("Expected 0 error, had %d. %v\n", len(errs), errs)
+	}
+	assert.NotEmpty(t, events)
+	event := events[0].MetricSetFields
+
+	t.Logf("%s/%s event: %+v", f.Module().Name(), f.Name(), event)
 }
 
 func getConfig() map[string]interface{} {
