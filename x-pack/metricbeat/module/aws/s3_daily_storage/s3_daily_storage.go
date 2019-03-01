0acee7cf242b3eb3018206a352e555fc41f69a3e
kaiyan-sheng <kaiyan.sheng@elastic.co>
2019-03-01 08:35:51 -0700

    Add AWS s3_daily_storage metricset (#10940)
    * First patch on s3 daily storage metricset
    * Add s3 daily storage metricset and TestData
    * Change number_of_object to number_of_objects
    * Add timestamp check for s3_daily_storage
    * Change select.scanned to select_scanned

--- x-pack/metricbeat/module/aws/s3_daily_storage/s3_daily_storage.go
+++ x-pack/metricbeat/module/aws/s3_daily_storage/s3_daily_storage.go
@@ -2,7 +2,7 @@
 // or more contributor license agreements. Licensed under the Elastic License;
 // you may not use this file except in compliance with the Elastic License.
 
-package s3_request
+package s3_daily_storage
 
 import (
 	"fmt"
@@ -13,13 +13,14 @@ import (
 	"github.com/pkg/errors"
 
 	"github.com/elastic/beats/libbeat/common"
+
 	"github.com/elastic/beats/libbeat/common/cfgwarn"
 	"github.com/elastic/beats/libbeat/logp"
 	"github.com/elastic/beats/metricbeat/mb"
 	"github.com/elastic/beats/x-pack/metricbeat/module/aws"
 )
 
-var metricsetName = "s3_request"
+var metricsetName = "s3_daily_storage"
 
 // init registers the MetricSet with the central registry as soon as the program
 // starts. The New function will be called later to instantiate an instance of
@@ -43,7 +44,7 @@ type MetricSet struct {
 // New creates a new instance of the MetricSet. New is responsible for unpacking
 // any MetricSet specific configuration options if there are any.
 func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
-	cfgwarn.Beta("The aws s3_request metricset is beta.")
+	cfgwarn.Beta("The aws s3_daily_storage metricset is beta.")
 	s3Logger := logp.NewLogger(aws.ModuleName)
 
 	moduleConfig := aws.Config{}
@@ -61,10 +62,10 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 		return nil, errors.Wrap(err, "error creating aws metricset")
 	}
 
-	// Check if period is set to be multiple of 60s
-	remainder := metricSet.PeriodInSec % 60
+	// Check if period is set to be multiple of 86400s
+	remainder := metricSet.PeriodInSec % 86400
 	if remainder != 0 {
-		err := errors.New("period needs to be set to 60s (or a multiple of 60s). " +
+		err := errors.New("period needs to be set to 86400s (or a multiple of 86400s). " +
 			"To avoid data missing or extra costs, please make sure period is set correctly " +
 			"in config.yml")
 		s3Logger.Info(err)
@@ -84,7 +85,7 @@ func (m *MetricSet) Fetch(report mb.ReporterV2) {
 	// Get startTime and endTime
 	startTime, endTime, err := aws.GetStartTimeEndTime(m.DurationString)
 	if err != nil {
-		logp.Error(errors.Wrap(err, "Error ParseDuration"))
+		err = errors.Wrap(err, "Error ParseDuration")
 		m.logger.Error(err.Error())
 		report.Error(err)
 		return
@@ -96,6 +97,7 @@ func (m *MetricSet) Fetch(report mb.ReporterV2) {
 		svcCloudwatch := cloudwatch.New(*m.MetricSet.AwsConfig)
 		listMetricsOutputs, err := aws.GetListMetricsOutput(namespace, regionName, svcCloudwatch)
 		if err != nil {
+			err = errors.Wrap(err, "GetListMetricsOutput failed, skipping region "+regionName)
 			m.logger.Error(err.Error())
 			report.Error(err)
 			continue
@@ -106,25 +108,22 @@ func (m *MetricSet) Fetch(report mb.ReporterV2) {
 		}
 
 		metricDataQueries := constructMetricQueries(listMetricsOutputs, m.PeriodInSec)
-		// This happens when S3 cloudwatch request metrics are not enabled.
-		if len(metricDataQueries) == 0 {
-			continue
-		}
 		// Use metricDataQueries to make GetMetricData API calls
 		metricDataOutputs, err := aws.GetMetricDataResults(metricDataQueries, svcCloudwatch, startTime, endTime)
 		if err != nil {
 			err = errors.Wrap(err, "GetMetricDataResults failed, skipping region "+regionName)
-			m.logger.Error(err.Error())
+			m.logger.Error(err)
 			report.Error(err)
 			continue
 		}
 
-		// Create Cloudwatch Events for s3_request
+		// Create Cloudwatch Events for s3_daily_storage
 		bucketNames := getBucketNames(listMetricsOutputs)
 		for _, bucketName := range bucketNames {
-			event, err := createS3RequestEvents(metricDataOutputs, regionName, bucketName)
+			event, err := createCloudWatchEvents(metricDataOutputs, regionName, bucketName)
 			if err != nil {
-				m.logger.Error(err.Error())
+				err = errors.Wrap(err, "createCloudWatchEvents failed")
+				m.logger.Error(err)
 				event.Error = err
 				report.Event(event)
 				continue
@@ -148,40 +147,12 @@ func getBucketNames(listMetricsOutputs []cloudwatch.Metric) (bucketNames []strin
 	return
 }
 
-func createMetricDataQuery(metric cloudwatch.Metric, periodInSec int, index int) (metricDataQuery cloudwatch.MetricDataQuery) {
-	statistic := "Sum"
-	period := int64(periodInSec)
-	id := "s3r" + strconv.Itoa(index)
-	metricDims := metric.Dimensions
-	bucketName := ""
-	filterID := ""
-	for _, dim := range metricDims {
-		if *dim.Name == "BucketName" {
-			bucketName = *dim.Value
-		} else if *dim.Name == "FilterId" {
-			filterID = *dim.Value
-		}
-	}
-	metricName := *metric.MetricName
-	label := bucketName + " " + filterID + " " + metricName
-	metricDataQuery = cloudwatch.MetricDataQuery{
-		Id: &id,
-		MetricStat: &cloudwatch.MetricStat{
-			Period: &period,
-			Stat:   &statistic,
-			Metric: &metric,
-		},
-		Label: &label,
-	}
-	return
-}
-
 func constructMetricQueries(listMetricsOutputs []cloudwatch.Metric, periodInSec int) []cloudwatch.MetricDataQuery {
 	metricDataQueries := []cloudwatch.MetricDataQuery{}
 	metricDataQueryEmpty := cloudwatch.MetricDataQuery{}
-	dailyMetricNames := []string{"NumberOfObjects", "BucketSizeBytes"}
+	metricNames := []string{"NumberOfObjects", "BucketSizeBytes"}
 	for i, listMetric := range listMetricsOutputs {
-		if aws.StringInSlice(*listMetric.MetricName, dailyMetricNames) {
+		if !aws.StringInSlice(*listMetric.MetricName, metricNames) {
 			continue
 		}
 
@@ -194,8 +165,36 @@ func constructMetricQueries(listMetricsOutputs []cloudwatch.Metric, periodInSec 
 	return metricDataQueries
 }
 
-// CreateS3Events creates s3_request and s3_daily_storage events from Cloudwatch metric data.
-func createS3RequestEvents(outputs []cloudwatch.MetricDataResult, regionName string, bucketName string) (event mb.Event, err error) {
+func createMetricDataQuery(metric cloudwatch.Metric, periodInSec int, index int) (metricDataQuery cloudwatch.MetricDataQuery) {
+	statistic := "Average"
+	period := int64(periodInSec)
+	id := "s3d" + strconv.Itoa(index)
+	metricDims := metric.Dimensions
+	bucketName := ""
+	storageType := ""
+	for _, dim := range metricDims {
+		if *dim.Name == "BucketName" {
+			bucketName = *dim.Value
+		} else if *dim.Name == "StorageType" {
+			storageType = *dim.Value
+		}
+	}
+	metricName := *metric.MetricName
+	label := bucketName + " " + storageType + " " + metricName
+
+	metricDataQuery = cloudwatch.MetricDataQuery{
+		Id: &id,
+		MetricStat: &cloudwatch.MetricStat{
+			Period: &period,
+			Stat:   &statistic,
+			Metric: &metric,
+		},
+		Label: &label,
+	}
+	return
+}
+
+func createCloudWatchEvents(outputs []cloudwatch.MetricDataResult, regionName string, bucketName string) (event mb.Event, err error) {
 	event.Service = metricsetName
 	event.RootFields = common.MapStr{}
 	// Cloud fields in ECS
@@ -203,7 +202,7 @@ func createS3RequestEvents(outputs []cloudwatch.MetricDataResult, regionName str
 	event.RootFields.Put("cloud.region", regionName)
 	event.RootFields.Put("cloud.provider", "aws")
 
-	// AWS s3_request metrics
+	// AWS s3_daily_storage metrics
 	mapOfMetricSetFieldResults := make(map[string]interface{})
 	// Find a timestamp for all metrics in output
 	if len(outputs) > 0 && len(outputs[0].Timestamps) > 0 {
@@ -219,7 +218,7 @@ func createS3RequestEvents(outputs []cloudwatch.MetricDataResult, regionName str
 
 	resultMetricSetFields, err := aws.EventMapping(mapOfMetricSetFieldResults, schemaMetricSetFields)
 	if err != nil {
-		err = errors.Wrap(err, "Error trying to apply schema schemaMetricSetFields in AWS s3_request metricbeat module.")
+		err = errors.Wrap(err, "Error trying to apply schema schemaMetricSetFields in AWS s3_daily_storage metricbeat module.")
 		return
 	}
 
