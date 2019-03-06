7ed8a0a13d2cb4d26ed6a69550e38906c5a9e3c5
sayden <mariocaster@gmail.com>
2019-03-06 15:57:16 +0100

    Rebase and use new logger

--- metricbeat/module/ceph/cluster_health/cluster_health.go
+++ metricbeat/module/ceph/cluster_health/cluster_health.go
@@ -18,7 +18,6 @@
 package cluster_health
 
 import (
-	"github.com/elastic/beats/libbeat/logp"
 	"github.com/elastic/beats/metricbeat/helper"
 	"github.com/elastic/beats/metricbeat/mb"
 	"github.com/elastic/beats/metricbeat/mb/parse"
@@ -34,8 +33,6 @@ var (
 		DefaultScheme: defaultScheme,
 		DefaultPath:   defaultPath,
 	}.Build()
-
-	logger = logp.NewLogger("ceph.cluster_health")
 )
 
 func init() {
@@ -69,7 +66,7 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 func (m *MetricSet) Fetch(reporter mb.ReporterV2) {
 	content, err := m.HTTP.FetchContent()
 	if err != nil {
-		logger.Error(err)
+		m.Logger().Error(err)
 		reporter.Error(err)
 		return
 	}
