3088b3aabf9183b24e0a32bdd7bb256fa9f5215f
odacremolbap <pablo.mercado@elastic.co>
2019-03-07 19:31:34 +0100

    add recommended metrics for etcd

--- metricbeat/module/etcd/metrics/metrics.go
+++ metricbeat/module/etcd/metrics/metrics.go
@@ -14,28 +14,29 @@ func init() {
 	mapping := &prometheus.MetricsMapping{
 		Metrics: map[string]prometheus.MetricMap{
 			// Server
-			"etcd_server_has_leader":    prometheus.Metric("server.has_leader"),
-			"leader_changes_seen_total": prometheus.Metric("server.leader_changes_seen_total"),
-			"proposals_committed_total": prometheus.Metric("server.proposals_committed_total"),
-			"proposals_applied_total":   prometheus.Metric("server.proposals_applied_total"),
-			"proposals_pending":         prometheus.Metric("server.proposals_pending"),
-			"proposals_failed_total":    prometheus.Metric("server.proposals_failed_total"),
+			"etcd_server_has_leader":                prometheus.Metric("server.has_leader"),
+			"etcd_server_leader_changes_seen_total": prometheus.Metric("server.leader_changes_seen_total"),
+			"etcd_server_proposals_committed_total": prometheus.Metric("server.proposals_committed_total"),
+			"etcd_server_proposals_pending":         prometheus.Metric("server.proposals_pending"),
+			"etcd_server_proposals_failed_total":    prometheus.Metric("server.proposals_failed_total"),
+			"grpc_server_started_total":             prometheus.Metric("server.grpc_started_total"),
+			"grpc_server_handled_total":             prometheus.Metric("server.grpc_handled_total"),
 
 			// Disk
-			"wal_fsync_duration_seconds":      prometheus.Metric("disk.wal_fsync_duration_seconds"),
-			"backend_commit_duration_seconds": prometheus.Metric("disk.backend_commit_duration_seconds"),
+			"etcd_mvcc_db_total_size_in_bytes":          prometheus.Metric("disk.mvcc_db_total_size_in_bytes"),
+			"etcd_disk_wal_fsync_duration_seconds":      prometheus.Metric("disk.wal_fsync_duration_seconds"),
+			"etcd_disk_backend_commit_duration_seconds": prometheus.Metric("disk.backend_commit_duration_seconds"),
 
-			// Netowrk
-			"peer_sent_bytes_total":            prometheus.Metric("network.peer_sent_bytes_total"),
-			"peer_received_bytes_total":        prometheus.Metric("network.peer_received_bytes_total"),
-			"peer_sent_failures_total":         prometheus.Metric("network.peer_sent_failures_total"),
-			"peer_received_failures_total":     prometheus.Metric("network.peer_received_failures_total"),
-			"peer_round_trip_time_seconds":     prometheus.Metric("network.peer_round_trip_time_seconds"),
-			"client_grpc_sent_bytes_total":     prometheus.Metric("network.client_grpc_sent_bytes_total"),
-			"client_grpc_received_bytes_total": prometheus.Metric("network.client_grpc_received_bytes_total"),
+			// Memory
+			"go_memstats_alloc_bytes": prometheus.Metric("memory.go_memstats_alloc_bytes"),
 
-			// Snapshot
-			"snapshot_save_total_duration_seconds": prometheus.Metric("snapshot.save_total_duration_seconds"),
+			// Network
+			"etcd_network_client_grpc_sent_bytes_total":     prometheus.Metric("network.client_grpc_sent_bytes_total"),
+			"etcd_network_client_grpc_received_bytes_total": prometheus.Metric("network.client_grpc_received_bytes_total"),
+			"etcd_network_peer_sent_bytes_total":            prometheus.Metric("network.peer_sent_bytes_total"),
+			"etcd_network_peer_received_bytes_total":        prometheus.Metric("network.peer_received_bytes_total"),
+			"etcd_network_peer_sent_failures_total":         prometheus.Metric("network.peer_sent_failures_total"),
+			"etcd_network_peer_received_failures_total":     prometheus.Metric("network.peer_received_failures_total"),
 		},
 	}
 
