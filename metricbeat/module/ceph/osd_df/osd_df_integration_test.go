fd27e462f7551f9f9b0405d80d67acab52de5199
Mario Castro <mariocaster@gmail.com>
2019-03-07 14:01:02 +0100

    [Metricbeat] Migrate Ceph osd_df to use ReporterV2 interface (#11013)

--- metricbeat/module/ceph/osd_df/osd_df_integration_test.go
+++ metricbeat/module/ceph/osd_df/osd_df_integration_test.go
@@ -26,9 +26,9 @@ import (
 )
 
 func TestData(t *testing.T) {
-	f := mbtest.NewEventsFetcher(t, getConfig())
-	err := mbtest.WriteEvents(f, t)
-	if err != nil {
+	f := mbtest.NewReportingMetricSetV2(t, getConfig())
+
+	if err := mbtest.WriteEventsReporterV2(f, t, ""); err != nil {
 		t.Fatal("write", err)
 	}
 }
