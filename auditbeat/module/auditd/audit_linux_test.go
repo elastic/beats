91b7b553065a3c079a80a9b016f104e309018684
Noémi Ványi <sitbackandwait@gmail.com>
2019-03-12 14:26:50 +0100

    move more functions

--- auditbeat/module/auditd/audit_linux_test.go
+++ auditbeat/module/auditd/audit_linux_test.go
@@ -33,7 +33,7 @@ import (
 	"github.com/prometheus/procfs"
 
 	"github.com/elastic/beats/auditbeat/core"
-	"github.com/elastic/beats/libbeat/common"
+	"github.com/elastic/beats/libbeat/asset"
 	"github.com/elastic/beats/libbeat/logp"
 	"github.com/elastic/beats/metricbeat/mb"
 	mbtest "github.com/elastic/beats/metricbeat/mb/testing"
@@ -102,7 +102,7 @@ func TestData(t *testing.T) {
 
 // assertFieldsAreDocumented mimics assert_fields_are_documented in Python system tests.
 func assertFieldsAreDocumented(t *testing.T, events []mb.Event) {
-	fieldsYml, err := common.LoadFieldsYaml("../../fields.yml")
+	fieldsYml, err := asset.LoadFieldsYaml("../../fields.yml")
 	if err != nil {
 		t.Fatal(err)
 	}
