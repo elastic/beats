7fb549ba4aea7cc7bbbbfd20c9fc2caedf1cb552
sayden <mariocaster@gmail.com>
2019-03-13 11:51:31 +0100

    Add new testing framework and fix namespace issue

--- metricbeat/module/kubernetes/state_replicaset/state_replicaset.go
+++ metricbeat/module/kubernetes/state_replicaset/state_replicaset.go
@@ -19,7 +19,6 @@ package state_replicaset
 
 import (
 	"github.com/elastic/beats/libbeat/common/kubernetes"
-	"github.com/elastic/beats/libbeat/logp"
 	p "github.com/elastic/beats/metricbeat/helper/prometheus"
 	"github.com/elastic/beats/metricbeat/mb"
 	"github.com/elastic/beats/metricbeat/mb/parse"
@@ -51,13 +50,7 @@ var (
 			"replicaset": p.KeyLabel("name"),
 			"namespace":  p.KeyLabel(mb.ModuleDataKey + ".namespace"),
 		},
-
-		ExtraFields: map[string]string{
-			mb.NamespaceKey: "replicaset",
-		},
 	}
-
-	logger = logp.NewLogger("kubernetes.state_replicaset")
 )
 
 // init registers the MetricSet with the central registry.
@@ -101,14 +94,17 @@ func (m *MetricSet) Fetch(reporter mb.ReporterV2) {
 
 	events, err := m.prometheus.GetProcessedMetrics(mapping)
 	if err != nil {
-		logger.Error(err)
+		m.Logger().Error(err)
 		reporter.Error(err)
 		return
 	}
 
 	m.enricher.Enrich(events)
 	for _, event := range events {
-		reporter.Event(mb.Event{MetricSetFields: event})
+		reporter.Event(mb.Event{
+			MetricSetFields: event,
+			Namespace:       "kubernetes.replicaset",
+		})
 	}
 
 	return
