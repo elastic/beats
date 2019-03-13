2b5d62df9eaf2a33c7a867b4002ceb520387b4ec
Chris <chrismarkou92@gmail.com>
2019-03-13 23:08:49 +0200

    Use the fixed Hist multiplier
    Signed-off-by: Chris <chrismarkou92@gmail.com>

--- metricbeat/module/coredns/stats/stats.go
+++ metricbeat/module/coredns/stats/stats.go
@@ -24,9 +24,9 @@ import (
 
 var mapping = &prometheus.MetricsMapping{
 	Metrics: map[string]prometheus.MetricMap{
-		"coredns_panic_count_total":              prometheus.Metric("panic.count.total"),
-		"coredns_dns_request_count_total":        prometheus.Metric("dns.request.count.total"),
-		"coredns_dns_request_duration_seconds":   prometheus.Metric(
+		"coredns_panic_count_total":       prometheus.Metric("panic.count.total"),
+		"coredns_dns_request_count_total": prometheus.Metric("dns.request.count.total"),
+		"coredns_dns_request_duration_seconds": prometheus.Metric(
 			"dns.request.duration.sec",
 			prometheus.OpMultiplyBuckets(1000000000)),
 		"coredns_dns_request_size_bytes":         prometheus.Metric("dns.request.size.bytes"),
