dc39f89ad1238771885c4e43ea447f62814811c6
Alex Kristiansen <alex.kristiansen@elastic.co>
2019-03-21 15:41:32 -0500

    make fmt changes

--- metricbeat/module/golang/heap/heap.go
+++ metricbeat/module/golang/heap/heap.go
@@ -20,11 +20,12 @@ package heap
 import (
 	"encoding/json"
 
+	"github.com/pkg/errors"
+
 	"github.com/elastic/beats/libbeat/logp"
 	"github.com/elastic/beats/metricbeat/helper"
 	"github.com/elastic/beats/metricbeat/mb"
 	"github.com/elastic/beats/metricbeat/mb/parse"
-	"github.com/pkg/errors"
 )
 
 var logger = logp.NewLogger("golang.heap")
