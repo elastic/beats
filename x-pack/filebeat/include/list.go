49f15079ff1ad505d1f282baf2305f389814b026
Adrian Serrano <adrisr83@gmail.com>
2019-03-28 17:17:09 +0100

    [Filebeat] Cisco ASA module (#11171)
    This adds a cisco module to x-pack/filebeat. The only fileset currently, asa, will ingest Cisco ASA logs received over syslog.
    
    Closes #9200

--- x-pack/filebeat/include/list.go
+++ x-pack/filebeat/include/list.go
@@ -9,6 +9,7 @@ package include
 import (
 	// Import packages that need to register themselves.
 	_ "github.com/elastic/beats/x-pack/filebeat/input/netflow"
+	_ "github.com/elastic/beats/x-pack/filebeat/module/cisco"
 	_ "github.com/elastic/beats/x-pack/filebeat/module/coredns"
 	_ "github.com/elastic/beats/x-pack/filebeat/module/iptables"
 	_ "github.com/elastic/beats/x-pack/filebeat/module/netflow"
