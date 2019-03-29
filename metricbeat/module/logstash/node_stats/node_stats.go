345203053e9c830bfc7170840dc16fdf136672b5
Shaunak Kashyap <ycombinator@gmail.com>
2019-03-29 04:18:56 -0700

    Get basic node info from GET _node/stats call

--- metricbeat/module/logstash/node_stats/node_stats.go
+++ metricbeat/module/logstash/node_stats/node_stats.go
@@ -92,12 +92,7 @@ func (m *MetricSet) Fetch(r mb.ReporterV2) error {
 		return eventMapping(r, content)
 	}
 
-	info, err := logstash.GetInfo(m.http, m.HostData().SanitizedURI+nodeStatsPath)
-	if err != nil {
-		m.Logger().Error(err)
-	}
-
-	err = eventMappingXPack(r, m, content, info)
+	err = eventMappingXPack(r, m, content)
 	if err != nil {
 		m.Logger().Error(err)
 	}
