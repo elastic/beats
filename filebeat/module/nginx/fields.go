elastic/beats

commit a9f567b4a43fe9978a6d1671cb1e10357b0972ea

Adding categorization fields for the system/auth module (#11334)
* Adding categorization fields for the system/auth module

This PR adds the following fields for the SSH login events:

* `event.category: authentication`
* `event.action: ssh_login`
* `event.type` either `authentication_success` or `authentication_failure`

The `event.outcome` is currently not quite ECS compliant, but I didn't touch it to
avoid a breaking change.

The PR doesn't attempt to categorize other logs besides the SSH login attempts,
so it's a subset of #9905, but it's what we need for the UI.

* Normalized event.outcome and brought back `system.auth.ssh.event`.

* changelog

dev-tools/ecs-migration.yml
@@ -486,11 +486,6 @@
   alias: true
   beat: filebeat
 
-- from: system.auth.ssh.event
-  to: event.action
-  alias: true
-  beat: filebeat
-
 - from: system.auth.program
   to: process.name
   alias: true

filebeat/docs/fields.asciidoc
@@ -13261,9 +13261,10 @@ The client IP from SSH connections that are open and immediately dropped.
 *`system.auth.ssh.event`*::
 +
 --
-type: alias
+example: Accepted
 
-alias to: event.action
+The SSH event as found in the logs (Accepted, Invalid, Failed, etc.)
+
 
 --
 

filebeat/module/system/auth/_meta/fields.yml
@@ -43,9 +43,10 @@
           The client IP from SSH connections that are open and immediately dropped.
 
       - name: event
-        type: alias
-        path: event.action
-        migration: true
+        example: Accepted
+        description: >
+          The SSH event as found in the logs (Accepted, Invalid, Failed, etc.)
+
       - name: ip
         type: alias
         path: source.ip

filebeat/module/system/fields.go
@@ -32,5 +32,5 @@ func init() {
 // AssetSystem returns asset data.
 // This is the base64 encoded gzipped contents of module/system.
 func AssetSystem() string {
-	return "eJzsmM9u4zYQxu9+ioEve1o/gA9FgQWKFug/ILn05GXIkTRYiiOQo3XUpy8oyY5iUw6luO0lOhmWZn4fP3HIET/DN+z2ELogWG8AhMTiHrYP/R/bDYDBoD01Quz28MMGAOA3Nq1FKNhDo3wgV44JwHIJBVkMuw1AqNjLQbMrqNyD+BY3AAWhNWHf5/kMTtU4ocdLugb3UHpum/GfhIJ4/dRngsJzDVJhUkK8psQpVbVSnf9McW+wU/xfybXPfVb29LeKIVHMWUdKy1SPUI1BVN28unsSpiypcHGnUVLt4dOP58hPFw/UVHo1aB/tv8ZWHCT+WkiNYbuZ2Bxs47n0ql5IbTxrDGG3FkpmJfA6ModXYwiqXGptOiqH1wb0C2ExZImdSW4IVRJ7WU0wUwJTx6TiS6tvluFwPVYIDw8/9+WHTkgP9Tek28EX5eAJgR0CF7BtVAhH9mYL7GHbtE+W9DfstrsZVYFKp6T1lyZlCjuHR3hcKrQldAIDOK7Ac2DjuWnQHOjSxZPDiRtZkkYFv/w5rF/ROs3OoY5hAaRSAioKbtCBcgaortGQErTdSdUuPSle1ON3dDIjPDUjT3Oyj9upXsvVI7fq4AU9a9gtbuDWa9wlYvOgDfs1wx2xyeisMnxRUCLPjjxVjHPlOM2p2Qk5dHJILBNvj/FqnCXyLiPnbctfC2yd+O5AgQ+azd0kZmTNF2l5WJLuI+5mtnxRHktid8f3+lbCBS+VpLvnhLuZbrFj951qOUmX7cet4ewN+Y39YmxzQ4OaCtIgPKzPAbRHJWjgqes3ta+R+hU017Vy5nJHu73to/d82bkA4LOqGzv2NfD7H49Arh8b+mtns7a9nnNqsGI2rQIOXw+t4ZN2KBRZvBrCuVGXbh398fEvOFboE0QKgM+oW5nHNseVfZFuvY+bvSGPWth37xCR6DAn78kzX29hWRpF+RJleNHCcKxIV0mB4UiiK3LlnMLx2ZVWjaCTDfCdVC9hpteZ2qJM+uPiX625Ebyy7CpOLIhTbTM2xTgo2Br0/SlAlOLwOHxMzPXQFVq7BmawUK2VIcEN3Aw3uei/3ZTNfRfl9oLt1QdjNjURmccsVzH7+bkEmqyAPsv/UgIn8soaWDk/BtfeMUH+o5c1OWOzXG4uWQvOu76wE0UujH6+OnizPD0C/DjySmI/jrzeeeT1TwAAAP//TwGvPg=="
+	return "eJzsmM2O2zYQx+9+ioEvaYFED+BD0SJA0AD9AnYvPTlcciQRoTgCOVqv+vQF9bHRypRNad3msjoZkmd+fw05H+IH+IrtAXzrGasdAGs2eID9XXdjvwNQ6KXTNWuyB/hpBwDwO6nGIOTkoBbOa1sMDsBQAbk26LMdgC/J8VGSzXVxAHYN7gByjUb5Q+fnA1hR4YQeLm5rPEDhqKmHOxEF4frUeYLcUQVcYlRCuKbEKVU0XD7fjHEvsGP837Rtnjqv5PQ/IpgEMc86YlqmelhX6FlU9YunozBhtPCzJ7Xg8gDvfn62fDf7Q6ULJ3rtQ/jPsSV5Dr9WUoNZtmCbgq0dFU5UK6m1I4neZ1uhWm0Enlum8Cr0XhRrQxu3SuE1Ht1KWDBZE84o1/syip1nEyykwDRiXNI81BfTsL/uS4S7u1+79EPLWvb517vL4KOw8IBAFoFy2NfC+xM5tQdysK+bB6PlV2z32YIqrwsruHHzICUKezYP8FAqpNFoGXpwqMBLYOWorlEd9TyKY4QjD5IkDQo+/9XXrxA6SdaiDGYeuBQMIgiu0YKwCnRVodKC0bSjqiy+Kb6px0e0fCYAn0RVhybzi5RYM75itTsACA85NUGj7aIbii78MHp/D5/tozBavYdPQptwA1lmP14TvxjyWC6N2eSpcRKziO2l9J0UKHLnAUvGRq2TEvmbggJp8c1j6byU0FOfkixri5aPkUJz/R3P3rNAyhJ8Xg75S4GNZdcetaejJHUziQle00Ua6ovabcRd9JYuymGhyd5wXa85XLGomttbbriL7lZH7LZbLcXpuo7eKEpu6Vfq9TAo+xqlzrUEpr5we5AOBaOCh7Yr3F8C9QtIqiph1bwnXh4c0Dmazz6TXhPGHPjjz/vQIwIF3Xlkk9pOxxlHtOBNCo/990ejaNQOeddqlto6c7uNfn//N5xKdBGi9oBPKBtextanjb1WNs6FTqu0Q8nk2leIiMyok3VyROctLEkjC1cg9wvNBKdSyzIq0J80y1LbYknh8N+NoRpAYxjgUYtOwsK0NA2LUPHPk/805wbwxrQrKVIQp9oWwhTsICej0HXnCEGKxVP/ObI0hZdozBaYwlw0hnsHF3AL3GjRvz6ULX1Zpc6CzdknZzI1YpnGLDYxu/25BhrNgM7Ld0mBkbwxBzbujz5qr9gg/9NiTU7pDBW7OWvFidlHsiy09UM8XxzdGZoeIr4dmkWxb4dmrzw0+zcAAP//vfvB9g=="
 }

filebeat/module/system/auth/test/test.log-expected.json
@@ -3,7 +3,11 @@
         "ecs.version": "1.0.0",
         "event.dataset": "system.auth",
         "event.module": "system",
-        "event.outcome": "Accepted",
+        "system.auth.ssh.event": "Accepted",
+        "event.outcome": "success",
+        "event.category": "authentication",
+        "event.action": "ssh_login",
+        "event.type": "authentication_success",
         "fileset.name": "auth",
         "host.hostname": "localhost",
         "input.type": "log",
@@ -21,7 +25,11 @@
         "ecs.version": "1.0.0",
         "event.dataset": "system.auth",
         "event.module": "system",
-        "event.outcome": "Accepted",
+        "system.auth.ssh.event": "Accepted",
+        "event.outcome": "success",
+        "event.category": "authentication",
+        "event.action": "ssh_login",
+        "event.type": "authentication_success",
         "fileset.name": "auth",
         "host.hostname": "localhost",
         "input.type": "log",
@@ -38,7 +46,11 @@
         "ecs.version": "1.0.0",
         "event.dataset": "system.auth",
         "event.module": "system",
-        "event.outcome": "Invalid",
+        "system.auth.ssh.event": "Invalid",
+        "event.outcome": "failure",
+        "event.category": "authentication",
+        "event.action": "ssh_login",
+        "event.type": "authentication_failure",
         "fileset.name": "auth",
         "host.hostname": "localhost",
         "input.type": "log",
@@ -53,7 +65,11 @@
         "ecs.version": "1.0.0",
         "event.dataset": "system.auth",
         "event.module": "system",
-        "event.outcome": "Failed",
+        "system.auth.ssh.event": "Failed",
+        "event.outcome": "failure",
+        "event.category": "authentication",
+        "event.action": "ssh_login",
+        "event.type": "authentication_failure",
         "fileset.name": "auth",
         "host.hostname": "slave22",
         "input.type": "log",

commit 692ef9e8b2fa07aed78eb57fb2b1de6b39cce743

Fix field alias for nginx.access.remote_ip (#11512)
- It was missing in the breaking changes doc (generated from ecs-migration.yml)
- The actual field alias was incorrectly pointing to source.ip, this has been
  adjusted to source.address
- Re-generating the documentation file also updated the breaking changes to
  include a change introduced in #11334

This should be backported to 7.0.

Closes #11510

dev-tools/ecs-migration.yml
@@ -1151,6 +1151,11 @@
 
 ## NGINX module
 
+- from: nginx.access.remote_ip
+  to: source.address
+  alias: true
+  beat: filebeat
+
 - from: nginx.access.user_name
   to: user.name
   alias: true

filebeat/docs/fields.asciidoc
@@ -11456,7 +11456,7 @@ alias to: http.response.body.bytes
 --
 type: alias
 
-alias to: source.ip
+alias to: source.address
 
 --
 

filebeat/module/nginx/access/_meta/fields.yml
@@ -16,7 +16,7 @@
       migration: true
     - name: remote_ip
       type: alias
-      path: source.ip
+      path: source.address
       migration: true
     - name: user_name
       type: alias

filebeat/module/nginx/fields.go
@@ -32,5 +32,5 @@ func init() {
 // AssetNginx returns asset data.
 // This is the base64 encoded gzipped contents of module/nginx.
 func AssetNginx() string {
-	return "eJy0l8Fu4zgMhu95CqLn1g+QwwKLAgV62MViT3tzFYl2iMqil5Q7k7cf2EnT1JUdy+nolNjm/38UaUt6gFc8bCHUFH5uACJFj1u4+7v/f7cBcKhWqI3EYQt/bAAA/mLXeYSKBVojSqGGuEcYQsBzDRV51GIDoHuWWFoOFdVbiNLhBqAi9E63g9QDBNPgh30/4qHFLdTCXXu6kmDox9MgBJVwMwXQj0u/S09jLaqeL6eMZ8z78cghGgp6shhm5APkqN/znFFSOJdIgg1HLKktPWn89Mg7nhExh9GdGcR+/BmOUcDVyQGe/wHjnKAqagHPEUjBQG8KO7SmUwQaLlpuGg4QGShY3zm8hx0qOdQhU+sJwxgULuTvP1kda7VH41AUPL0ivPz38MTyw4hD1/96Kb6o/YvGg3IndgAnBUGNLOh6rpfjnYLai9Dk7O7YHUrFEIvdIaKmp9eTGd9pTdxvYR9jWwhqy0Gx6LWSMg3VYo6VOPX7TJkzEc6ZrvDsFKXsf2Z69nFFIm6JZ4Nxz27dPP/focYiqbAoXfG5iYovWKimYMahSwx77PINRYnDmozTocu66diTpWWXW93PTa3RxE5TOss4KhRBuaXeExpL7E399Uu0pLnLITC39NPv2DTHeGWBidXgUtLhG9lxNeZTS6Z31Em9yHNZjmkmwjNZboTglEkmAmtRdd6nPoh5KFMK+Ty3oqT7dw3LtFLW21AjT6xwa14EyyFSwBBvmfHTAlojF1f1lk685S5EOZSknPpyrkK7qrgUzrMdHrsdakZpKYxgTRy+qX7zYouLR/HwXQ01I5U5Q9/XStcFp9DekVCE5fcdkgb5rDOS5RDQxiGt9N7Sc6jzTkiPZ00ghyFSRShXThMe3zB3j+m5LlJxS/Y37US2026tcH8CLb5GLvGLq/3iXtC4YpVrg6qmzt3GpqPGfr8CAAD//56RUkQ="
+	return "eJy0l8tuo0oQhvd+ilLWCQ/gxZGOIkXK4hyNZjU70u4ucClNF1PVZMZvPwI7iUMaTONMr2yg/v+rC5e+g2c8bCHUFH5vACJFj1u4+b//f7MBcKhWqI3EYQv/bAAA/mPXeYSKBVojSqGGuEcYQsBzDRV51GIDoHuWWFoOFdVbiNLhBqAi9E63g9QdBNPgu32/4qHFLdTCXXs6kmDo18MgBJVwMwXQr3O/c09jLaq+HU4Zz5j3655DNBT0ZDFU5B3kqN/zvKGkcM6RBBuOWFJbetL44ZJXPCNiDqMzM4j9+jcco4CrkwM8fgPjnKAqagGPEUjBQG8KO7SmUwQaDlpuGg4QGShY3zm8hR0qOdQhU+sJwxgUzuRvP1gde7VH41AUPD0jPP24e2D5ZcSh6389FZ/UvqPxoNyJHcBJQVAjC7qe6+l4pqD2LDRZ3R27Q6kYYrE7RNR0eT2Z8ZnWxP0W9jG2haC2HBSLXisp01At5tiJ07zPtDkT4ZTpqZwrjDtFKfufmcZ9XJGIW+LZYNyzW1fsnx1qLJIKi9IVn5uo+IKFagpmHLrEsMcuX1CUOKzJOB26bKSOg1ladrnd/TjZGk3sNKWzjKNCEZRr+j2hscTe1J8fR0uGuxwCc1s/fY9Nc4xfLzDxSjiXdPhCdtyN+dSS6R11UjfyXJZjmonwTJYrIThlkonAWlSd96kHYh7KlEI+z7Uo6fldwzKtlHU31MgTr7k1N4LlEClgiNdU/PQWrZGLi3pLC2+5C1EOJSmnnpyr0C4qLoXzbIfLroeaUVoKI1gThy/q37zY4uZRPHzVQM1IZVbo60bpsuAU2isSirD8vZ3SIJ+1UbIcAto4pJX+tvQc6rxt0v2bJpDDEKkilAtbCo8vmPuN6bkuUnFLvm/aiWyn3VrhfhtafI5c4hdX+8W9oHHFKtcGVU2d+xmbjhr7/QkAAP///8pUUQ=="
 }