3125cc2a9cb0a32595c9755a14bd80218ae4d035
Pier-Hugues Pellerin <phpellerin@gmail.com>
2019-04-04 15:23:20 -0400

    Fixes license check issue with invalid license UUID and check on 32 bits system. (#11649)
    - Relax validation of the X-Pack license UID value.
    - Fix a parsing error with the X-Pack license check on 32-bit system. 
    
    Fixes: #11640
    Fixes: #11650

--- x-pack/libbeat/licenser/manager.go
+++ x-pack/libbeat/licenser/manager.go
@@ -29,7 +29,7 @@ func mustUUIDV4() uuid.UUID {
 // OSSLicense default license to use.
 var (
 	OSSLicense = &License{
-		UUID:   mustUUIDV4(),
+		UUID:   mustUUIDV4().String(),
 		Type:   OSS,
 		Mode:   OSS,
 		Status: Active,
