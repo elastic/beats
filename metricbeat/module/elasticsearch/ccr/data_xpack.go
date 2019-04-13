273056b112514b774317be209540cef25f407683
Shaunak Kashyap <ycombinator@gmail.com>
2019-04-13 05:32:49 -0700

    Don't report errors in x-pack code path (#11796) (#11808)
    The X-Pack code path shouldn't report errors as they will get indexed into `metricbeat-*` indices, not `.monitoring-*` indices (which don't have a field for errors anyway).
    
    (cherry picked from commit 48cf37dc8fd68ef635ac80ed4b756602ec9e2e6f)

--- metricbeat/module/elasticsearch/ccr/data_xpack.go
+++ metricbeat/module/elasticsearch/ccr/data_xpack.go
@@ -34,9 +34,7 @@ func eventsMappingXPack(r mb.ReporterV2, m *MetricSet, info elasticsearch.Info, 
 	var data response
 	err := json.Unmarshal(content, &data)
 	if err != nil {
-		err = errors.Wrap(err, "failure parsing Elasticsearch CCR Stats API response")
-		r.Error(err)
-		return err
+		return errors.Wrap(err, "failure parsing Elasticsearch CCR Stats API response")
 	}
 
 	var errors multierror.Errors
