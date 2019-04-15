88dc19f03e42f48e7d83180575a0a71ac32c0e0c
Mike Place <mike.place@elastic.co>
2019-04-15 09:46:04 +0200

    x-pack fmt update

--- x-pack/filebeat/tests/system/test_xpack_modules.py
+++ x-pack/filebeat/tests/system/test_xpack_modules.py
@@ -1,9 +1,8 @@
+import test_modules
 import os
 import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../filebeat/tests/system'))
-
-import test_modules
 
 
 class XPackTest(test_modules.Test):
