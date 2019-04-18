5f4cc092a06748bb58a7cf0c66f865c4b15d69ed
Christoph Wurm <cw@elastic.co>
2019-04-18 17:45:43 +0100

    More protection.

--- x-pack/auditbeat/module/system/package/package_homebrew_test.go
+++ x-pack/auditbeat/module/system/package/package_homebrew_test.go
@@ -50,15 +50,16 @@ func TestHomebrew(t *testing.T) {
 		if len(errs) > 0 {
 			t.Fatalf("received error: %+v", errs[0])
 		}
-		assert.Len(t, events, 1)
 
-		event := mbtest.StandardizeEvent(f, events[0], core.AddDatasetToEvent)
-		checkFieldValue(t, event, "system.audit.package.name", "test-package")
-		checkFieldValue(t, event, "system.audit.package.summary", "Test package")
-		checkFieldValue(t, event, "system.audit.package.url", "https://www.elastic.co/")
-		checkFieldValue(t, event, "system.audit.package.version", "1.0.0")
-		checkFieldValue(t, event, "system.audit.package.installtime", time.Date(2019, 4, 17, 13, 14, 57, 205133721, time.FixedZone("BST", 60*60)))
-		checkFieldValue(t, event, "system.audit.package.entity_id", "Krm421rtYM4wgq1S")
+		if assert.Len(t, events, 1) {
+			event := mbtest.StandardizeEvent(f, events[0], core.AddDatasetToEvent)
+			checkFieldValue(t, event, "system.audit.package.name", "test-package")
+			checkFieldValue(t, event, "system.audit.package.summary", "Test package")
+			checkFieldValue(t, event, "system.audit.package.url", "https://www.elastic.co/")
+			checkFieldValue(t, event, "system.audit.package.version", "1.0.0")
+			checkFieldValue(t, event, "system.audit.package.installtime", time.Date(2019, 4, 17, 13, 14, 57, 205133721, time.FixedZone("BST", 60*60)))
+			checkFieldValue(t, event, "system.audit.package.entity_id", "Krm421rtYM4wgq1S")
+		}
 	}
 }
 
