b01c41160e81e6a144951c43cda1e8c273350d71
Adrian Serrano <adrisr83@gmail.com>
2019-04-18 18:48:27 +0200

    Register processor with filebeat / journalbeat

--- journalbeat/beater/journalbeat.go
+++ journalbeat/beater/journalbeat.go
@@ -35,6 +35,8 @@ import (
 
 	// Add dedicated processors
 	_ "github.com/elastic/beats/libbeat/processors/decode_csv_fields"
+	// Custom processors for journalbeat
+	_ "github.com/elastic/beats/libbeat/processors/extract_array"
 )
 
 // Journalbeat instance
