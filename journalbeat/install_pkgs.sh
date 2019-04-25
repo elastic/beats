ec4e1ae3af371845f9eb5105fc7ee497bc176dcc
Michael Madden <michael.madden@elastic.co>
2019-04-25 16:31:39 -0500

    Remove extra echo from install command.

--- journalbeat/install_pkgs.sh
+++ journalbeat/install_pkgs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -x
 
-echo 'Yes, do as I say!' | echo apt-get install -y --force-yes --no-install-recommends $*
+echo 'Yes, do as I say!' | apt-get install -y --force-yes --no-install-recommends $*
