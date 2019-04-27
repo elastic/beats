c4200b87a97653f442f12f3b578cf0a07ac29288
Simon <simon@thenom.co.uk>
2019-04-27 13:40:44 +0100

    Initial setup of gzip message decompress processor

---
+++ libbeat/processors/decompress_gzip_fields/decompress_gzip_fields.go
@@ -0,0 +1,130 @@
+package decompress_gzip_fields
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/pkg/errors"
+
+	"github.com/elastic/beats/libbeat/beat"
+	"github.com/elastic/beats/libbeat/common"
+	"github.com/elastic/beats/libbeat/processors"
+	"github.com/elastic/beats/libbeat/processors/checks"
+
+	"bytes"
+	"compress/gzip"
+	"io"
+)
+
+type decompressGzipFields struct {
+	decompressGzipFieldsConfig
+	fields map[string]string
+}
+
+type decompressGzipFieldsConfig struct {
+	Fields        common.MapStr `config:"fields"`
+	IgnoreMissing bool          `config:"ignore_missing"`
+	OverwriteKeys bool          `config:"overwrite_keys"`
+	FailOnError   bool          `config:"fail_on_error"`
+}
+
+var (
+	defaultDecompressGzipFieldsConfig = decompressGzipFieldsConfig{
+		FailOnError: true,
+	}
+)
+
+func init() {
+	processors.RegisterPlugin("decompress_gzip_fields",
+		checks.ConfigChecked(NewDecompressGzipFields,
+			checks.RequireFields("fields"),
+			checks.AllowedFields("fields", "ignore_missing", "overwrite_keys", "overwrite_keys", "fail_on_error", "when")))
+}
+
+// NewDecompressGzipFields construct a new decompress_gzip_fields processor.
+func NewDecompressGzipFields(c *common.Config) (processors.Processor, error) {
+	config := defaultDecompressGzipFieldsConfig
+
+	err := c.Unpack(&config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unpack the decompress_gzip_fields configuration: %s", err)
+	}
+	if len(config.Fields) == 0 {
+		return nil, errors.New("no fields to decompress configured")
+	}
+	f := &decompressGzipFields{decompressGzipFieldsConfig: config}
+
+	// Set fields as string -> string
+	f.fields = make(map[string]string, len(config.Fields))
+	for src, dstIf := range config.Fields.Flatten() {
+		dst, ok := dstIf.(string)
+		if !ok {
+			return nil, errors.Errorf("bad destination mapping for %s: destination field must be string, not %T (got %v)", src, dstIf, dstIf)
+		}
+		f.fields[src] = dst
+	}
+	return f, nil
+}
+
+// Run applies the decompress_gzip_fields processor to an event.
+func (f *decompressGzipFields) Run(event *beat.Event) (*beat.Event, error) {
+	saved := *event
+	if f.FailOnError {
+		saved.Fields = event.Fields.Clone()
+		saved.Meta = event.Meta.Clone()
+	}
+	for src, dest := range f.fields {
+		if err := f.decompressGzipField(src, dest, event); err != nil && f.FailOnError {
+			return &saved, err
+		}
+	}
+	return event, nil
+}
+
+func (f *decompressGzipFields) decompressGzipField(src, dest string, event *beat.Event) error {
+	// Check source value
+	data, err := event.GetValue(src)
+	if err != nil {
+		if f.IgnoreMissing && errors.Cause(err) == common.ErrKeyNotFound {
+			return nil
+		}
+		return errors.Wrapf(err, "could not fetch value for field %s", src)
+	}
+
+	text, ok := data.(string)
+	if !ok {
+		return errors.Errorf("field %s is not of string type", src)
+	}
+
+	//Uncompress gzip message
+	b := bytes.NewBuffer([]byte(text))
+
+	var r io.Reader
+	r, err = gzip.NewReader(b)
+
+	if err != nil {
+		return errors.Wrapf(err, "error decompressing field %s", src)
+	}
+
+	var resB bytes.Buffer
+	resB.ReadFrom(r)
+
+	decompressed := string(resB.Bytes())
+
+	// Write message out
+	if src != dest && !f.OverwriteKeys {
+		if _, err = event.GetValue(dest); err == nil {
+			return errors.Errorf("target field %s already has a value. Set the overwrite_keys flag or drop/rename the field first", dest)
+		}
+	}
+	if _, err = event.PutValue(dest, decompressed); err != nil {
+		return errors.Wrapf(err, "failed setting field %s", dest)
+	}
+	return nil
+}
+
+// String returns a string representation of this processor.
+func (f decompressGzipFields) String() string {
+	json, _ := json.Marshal(f.decompressGzipFieldsConfig)
+	return "decompress_gzip_fields=" + string(json)
+}
---
+++ libbeat/processors/decompress_gzip_fields/decompress_gzip_fields_test.go
@@ -0,0 +1,260 @@
+package decompress_gzip_fields
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/elastic/beats/libbeat/beat"
+	"github.com/elastic/beats/libbeat/common"
+)
+
+func TestDecompressGzipField(t *testing.T) {
+	tests := map[string]struct {
+		config   common.MapStr
+		input    beat.Event
+		expected beat.Event
+		fail     bool
+	}{
+		"self target": {
+			config: common.MapStr{
+				"fields": common.MapStr{
+					"message": "message",
+				},
+			},
+			input: beat.Event{
+				Fields: common.MapStr{
+					"message": string([]byte{31, 139, 8, 0, 0, 0, 0, 0, 0, 255, 202, 72, 205, 201, 201, 87, 40, 207, 47, 202, 73, 1, 0, 0, 0, 255, 255, 1, 0, 0, 255, 255, 133, 17, 74, 13, 11, 0, 0, 0}),
+				},
+			},
+			expected: beat.Event{
+				Fields: common.MapStr{
+					"message": "hello world",
+				},
+			},
+		},
+
+		"alternative target": {
+			config: common.MapStr{
+				"fields": common.MapStr{
+					"my": common.MapStr{
+						"field": "message",
+					},
+				},
+			},
+			input: beat.Event{
+				Fields: common.MapStr{
+					"my": common.MapStr{
+						"field": string([]byte{31, 139, 8, 0, 0, 0, 0, 0, 0, 255, 202, 72, 205, 201, 201, 87, 40, 207, 47, 202, 73, 1, 0, 0, 0, 255, 255, 1, 0, 0, 255, 255, 133, 17, 74, 13, 11, 0, 0, 0}),
+					},
+				},
+			},
+			expected: beat.Event{
+				Fields: common.MapStr{
+					"my.field": string([]byte{31, 139, 8, 0, 0, 0, 0, 0, 0, 255, 202, 72, 205, 201, 201, 87, 40, 207, 47, 202, 73, 1, 0, 0, 0, 255, 255, 1, 0, 0, 255, 255, 133, 17, 74, 13, 11, 0, 0, 0}),
+					"message":  "hello world",
+				},
+			},
+		},
+
+		"non existing field": {
+			config: common.MapStr{
+				"fields": common.MapStr{
+					"field": "my.field",
+				},
+			},
+			fail: true,
+		},
+
+		"ignore missing": {
+			config: common.MapStr{
+				"fields": common.MapStr{
+					"my_field": "my_field",
+				},
+
+				"ignore_missing": true,
+			},
+		},
+
+		"overwrite keys failure": {
+			config: common.MapStr{
+				"fields": common.MapStr{
+					"message": "existing_field",
+				},
+			},
+			input: beat.Event{
+				Fields: common.MapStr{
+					"message":        string([]byte{31, 139, 8, 0, 0, 0, 0, 0, 0, 255, 202, 72, 205, 201, 201, 87, 40, 207, 47, 202, 73, 1, 0, 0, 0, 255, 255, 1, 0, 0, 255, 255, 133, 17, 74, 13, 11, 0, 0, 0}),
+					"existing_field": 42,
+				},
+			},
+			fail: true,
+		},
+
+		"overwrite keys": {
+			config: common.MapStr{
+				"fields": common.MapStr{
+					"message": "existing_field",
+				},
+				"overwrite_keys": true,
+			},
+			input: beat.Event{
+				Fields: common.MapStr{
+					"message":        string([]byte{31, 139, 8, 0, 0, 0, 0, 0, 0, 255, 202, 72, 205, 201, 201, 87, 40, 207, 47, 202, 73, 1, 0, 0, 0, 255, 255, 1, 0, 0, 255, 255, 133, 17, 74, 13, 11, 0, 0, 0}),
+					"existing_field": 42,
+				},
+			},
+			expected: beat.Event{
+				Fields: common.MapStr{
+					"message":        string([]byte{31, 139, 8, 0, 0, 0, 0, 0, 0, 255, 202, 72, 205, 201, 201, 87, 40, 207, 47, 202, 73, 1, 0, 0, 0, 255, 255, 1, 0, 0, 255, 255, 133, 17, 74, 13, 11, 0, 0, 0}),
+					"existing_field": "hello world",
+				},
+			},
+		},
+
+		"bad type": {
+			config: common.MapStr{
+				"fields": common.MapStr{
+					"message": "message",
+				},
+			},
+			input: beat.Event{
+				Fields: common.MapStr{
+					"message": 42,
+				},
+			},
+			fail: true,
+		},
+
+		"multiple fields": {
+			config: common.MapStr{
+				"fields": common.MapStr{
+					"a": "a_uncompress",
+					"b": "b_uncompress",
+				},
+			},
+			input: beat.Event{
+				Fields: common.MapStr{
+					"a": string([]byte{31, 139, 8, 0, 0, 0, 0, 0, 0, 255, 202, 72, 205, 201, 201, 87, 40, 207, 47, 202, 73, 1, 0, 0, 0, 255, 255, 1, 0, 0, 255, 255, 133, 17, 74, 13, 11, 0, 0, 0}),
+					"b": string([]byte{31, 139, 8, 0, 20, 84, 233, 92, 0, 3, 203, 72, 205, 201, 201, 87, 40, 207, 47, 202, 73, 49, 2, 0, 179, 131, 10, 135, 12, 0, 0, 0}),
+				},
+			},
+			expected: beat.Event{
+				Fields: common.MapStr{
+					"a":            string([]byte{31, 139, 8, 0, 0, 0, 0, 0, 0, 255, 202, 72, 205, 201, 201, 87, 40, 207, 47, 202, 73, 1, 0, 0, 0, 255, 255, 1, 0, 0, 255, 255, 133, 17, 74, 13, 11, 0, 0, 0}),
+					"b":            string([]byte{31, 139, 8, 0, 20, 84, 233, 92, 0, 3, 203, 72, 205, 201, 201, 87, 40, 207, 47, 202, 73, 49, 2, 0, 179, 131, 10, 135, 12, 0, 0, 0}),
+					"a_uncompress": "hello world",
+					"b_uncompress": "hello world2",
+				},
+			},
+		},
+
+		"multiple fields failure": {
+			config: common.MapStr{
+				"fields": common.MapStr{
+					"a": "a.decompressed",
+					"b": "b.decompressed",
+				},
+			},
+			input: beat.Event{
+				Fields: common.MapStr{
+					"a": string([]byte{139, 8, 0, 0, 0, 0, 0, 0, 255, 202, 72, 205, 201, 201, 87, 40, 207, 47, 202, 73, 1, 0, 0, 0, 255, 255, 1, 0, 0, 255, 255, 133, 17, 74, 13, 11, 0, 0, 0}),
+					"b": string([]byte{139, 8, 0, 20, 84, 233, 92, 0, 3, 203, 72, 205, 201, 201, 87, 40, 207, 47, 202, 73, 49, 2, 0, 179, 131, 10, 135, 12, 0, 0, 0}),
+				},
+			},
+			expected: beat.Event{
+				Fields: common.MapStr{
+					"a": string([]byte{139, 8, 0, 0, 0, 0, 0, 0, 255, 202, 72, 205, 201, 201, 87, 40, 207, 47, 202, 73, 1, 0, 0, 0, 255, 255, 1, 0, 0, 255, 255, 133, 17, 74, 13, 11, 0, 0, 0}),
+					"b": string([]byte{139, 8, 0, 20, 84, 233, 92, 0, 3, 203, 72, 205, 201, 201, 87, 40, 207, 47, 202, 73, 49, 2, 0, 179, 131, 10, 135, 12, 0, 0, 0}),
+				},
+			},
+			fail: true,
+		},
+
+		"ignore errors": {
+			config: common.MapStr{
+				"fields": common.MapStr{
+					"a": "a",
+					"b": "b",
+					"c": "a.b",
+				},
+				"fail_on_error": false,
+			},
+			input: beat.Event{
+				Fields: common.MapStr{
+					"a": string([]byte{31, 139, 8, 0, 0, 0, 0, 0, 0, 255, 202, 72, 205, 201, 201, 87, 40, 207, 47, 202, 73, 1, 0, 0, 0, 255, 255, 1, 0, 0, 255, 255, 133, 17, 74, 13, 11, 0, 0, 0}),
+					"b": string([]byte{31, 139, 8, 0, 20, 84, 233, 92, 0, 3, 203, 72, 205, 201, 201, 87, 40, 207, 47, 202, 73, 49, 2, 0, 179, 131, 10, 135, 12, 0, 0, 0}),
+					"c": string([]byte{139, 8, 0, 0, 0, 0, 0, 0, 255, 202, 72, 205, 201, 201, 87, 40, 207, 47, 202, 73, 1, 0, 0, 0, 255, 255, 1, 0, 0, 255, 255, 133, 17, 74, 13, 11, 0, 0, 0}),
+				},
+			},
+			expected: beat.Event{
+				Fields: common.MapStr{
+					"a": "hello world",
+					"b": "hello world2",
+					"c": string([]byte{139, 8, 0, 0, 0, 0, 0, 0, 255, 202, 72, 205, 201, 201, 87, 40, 207, 47, 202, 73, 1, 0, 0, 0, 255, 255, 1, 0, 0, 255, 255, 133, 17, 74, 13, 11, 0, 0, 0}),
+				},
+			},
+		},
+
+		"restore on errors": {
+			config: common.MapStr{
+				"fields": common.MapStr{
+					"a": "a",
+					"b": "b",
+					"c": "a.b",
+				},
+				"fail_on_error": true,
+			},
+			input: beat.Event{
+				Fields: common.MapStr{
+					"a": string([]byte{31, 139, 8, 0, 0, 0, 0, 0, 0, 255, 202, 72, 205, 201, 201, 87, 40, 207, 47, 202, 73, 1, 0, 0, 0, 255, 255, 1, 0, 0, 255, 255, 133, 17, 74, 13, 11, 0, 0, 0}),
+					"b": string([]byte{31, 139, 8, 0, 20, 84, 233, 92, 0, 3, 203, 72, 205, 201, 201, 87, 40, 207, 47, 202, 73, 49, 2, 0, 179, 131, 10, 135, 12, 0, 0, 0}),
+					"c": string([]byte{139, 8, 0, 0, 0, 0, 0, 0, 255, 202, 72, 205, 201, 201, 87, 40, 207, 47, 202, 73, 1, 0, 0, 0, 255, 255, 1, 0, 0, 255, 255, 133, 17, 74, 13, 11, 0, 0, 0}),
+				},
+			},
+			expected: beat.Event{
+				Fields: common.MapStr{
+					"a": string([]byte{31, 139, 8, 0, 0, 0, 0, 0, 0, 255, 202, 72, 205, 201, 201, 87, 40, 207, 47, 202, 73, 1, 0, 0, 0, 255, 255, 1, 0, 0, 255, 255, 133, 17, 74, 13, 11, 0, 0, 0}),
+					"b": string([]byte{31, 139, 8, 0, 20, 84, 233, 92, 0, 3, 203, 72, 205, 201, 201, 87, 40, 207, 47, 202, 73, 49, 2, 0, 179, 131, 10, 135, 12, 0, 0, 0}),
+					"c": string([]byte{139, 8, 0, 0, 0, 0, 0, 0, 255, 202, 72, 205, 201, 201, 87, 40, 207, 47, 202, 73, 1, 0, 0, 0, 255, 255, 1, 0, 0, 255, 255, 133, 17, 74, 13, 11, 0, 0, 0}),
+				},
+			},
+			fail: true,
+		},
+	}
+
+	for title, tt := range tests {
+		t.Run(title, func(t *testing.T) {
+			processor, err := NewDecompressGzipFields(common.MustNewConfigFrom(tt.config))
+			if err != nil {
+				t.Fatal(err)
+			}
+			result, err := processor.Run(&tt.input)
+			if tt.expected.Fields != nil {
+				assert.Equal(t, tt.expected.Fields.Flatten(), result.Fields.Flatten())
+				assert.Equal(t, tt.expected.Meta.Flatten(), result.Meta.Flatten())
+				assert.Equal(t, tt.expected.Timestamp, result.Timestamp)
+			}
+			if tt.fail {
+				assert.Error(t, err)
+				t.Log("got expected error", err)
+				return
+			}
+			assert.NoError(t, err)
+		})
+	}
+}
+
+func TestDecompressGzipField_String(t *testing.T) {
+	p, err := NewDecompressGzipFields(common.MustNewConfigFrom(common.MapStr{
+		"fields": common.MapStr{
+			"a": "decompress.a",
+			"b": "decompress.b",
+		},
+		"ignore_missing": true,
+	}))
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, "decompress_gzip_fields={\"Fields\":{\"a\":\"decompress.a\",\"b\":\"decompress.b\"},\"IgnoreMissing\":true,\"OverwriteKeys\":false,\"FailOnError\":true}", p.String())
+}
