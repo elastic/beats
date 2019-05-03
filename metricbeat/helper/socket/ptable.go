b50265a3d7deb4ac0cedcb69749f1595c1af2121
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2019-05-03 19:12:33 +0200

    Add lines removed by mistake

--- metricbeat/helper/socket/ptable.go
+++ metricbeat/helper/socket/ptable.go
@@ -1,3 +1,5 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
 // this work for additional information regarding copyright
 // ownership. Elasticsearch B.V. licenses this file to you under
 // the Apache License, Version 2.0 (the "License"); you may
