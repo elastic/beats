f81aa4786f694e32a02796f634a685a3a779a74f
Fae Charlton <fae.charlton@elastic.co>
2019-05-03 14:28:43 -0400

    make update

--- libbeat/outputs/elasticsearch/client_proxy_test.go
+++ libbeat/outputs/elasticsearch/client_proxy_test.go
@@ -1,3 +1,20 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 // This file contains tests to confirm that elasticsearch.Client uses proxy
 // settings following the intended precedence.
 
@@ -15,8 +32,9 @@ import (
 	"sync"
 	"testing"
 
-	"github.com/elastic/beats/libbeat/outputs/outil"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/elastic/beats/libbeat/outputs/outil"
 )
 
 // TestClientPing is a placeholder test that does nothing on a standard run,
@@ -165,7 +183,9 @@ type testListeners struct {
 	server *httptest.Server
 	proxy  *httptest.Server
 
-	// T
+	// This mutex must be held in order to modify the structure.
+	// Using a mutex for servers that are expected to receive ~1 request in
+	// their lifetime might be pedantic, but I really dislike race conditions.
 	mutex sync.Mutex
 
 	serverRequestCount int // Requests directly to the server
@@ -174,7 +194,10 @@ type testListeners struct {
 	errors []string
 }
 
+// start creates and starts the server and proxy listeners.
 func (tl *testListeners) start(t *testing.T) {
+	tl.mutex.Lock()
+	defer tl.mutex.Unlock()
 	tl.server = httptest.NewServer(
 		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			tl.mutex.Lock()
