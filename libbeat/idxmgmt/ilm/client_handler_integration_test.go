0edc3d27a36821f73a0d006e2190de119586c8ea
simitt <silvia.mitter@elastic.co>
2019-05-06 16:20:56 +0200

    fix broken test

--- libbeat/idxmgmt/ilm/client_handler_integration_test.go
+++ libbeat/idxmgmt/ilm/client_handler_integration_test.go
@@ -256,14 +256,14 @@ func TestFileClientHandler_CreateILMPolicy(t *testing.T) {
 	c := newMockClient("")
 	h := ilm.NewFileClientHandler(c)
 	name := "test-policy"
-	body := map[string]interface{}{"foo": "bar"}
+	body := common.MapStr{"foo": "bar"}
 	h.CreateILMPolicy(ilm.Policy{Name: name, Body: body})
 
 	assert.Equal(t, name, c.name)
 	assert.Equal(t, "policy", c.kind)
 	var out common.MapStr
 	json.Unmarshal([]byte(c.body), &out)
-	assert.Equal(t, common.MapStr{name: body}, out)
+	assert.Equal(t, body, out)
 }
 
 type mockClient struct {
