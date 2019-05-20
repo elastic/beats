007b0cb7e664e93ba8a7f8dcd792ee6889055927
Manuel de la Peña <manuel.delapena@elastic.co>
2019-05-20 10:18:29 +0200

    Run "make update" to autogenerate docs for fields
        This command will:
        - format and normalise code
        - include new module into the existing list of Filebeat modules

--- x-pack/filebeat/include/list.go
+++ x-pack/filebeat/include/list.go
@@ -16,6 +16,7 @@ import (
 	_ "github.com/elastic/beats/x-pack/filebeat/module/netflow"
 	_ "github.com/elastic/beats/x-pack/filebeat/module/palo_alto"
 	_ "github.com/elastic/beats/x-pack/filebeat/module/rabbitmq"
+	_ "github.com/elastic/beats/x-pack/filebeat/module/shorewall"
 	_ "github.com/elastic/beats/x-pack/filebeat/module/suricata"
 	_ "github.com/elastic/beats/x-pack/filebeat/module/zeek"
 )
