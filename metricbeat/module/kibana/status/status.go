389f16bb2a5ed373770ef615a3b55979039ddb31
Shaunak Kashyap <ycombinator@gmail.com>
2019-05-24 17:35:13 -0700

    [6.8] Validate that kibana/status metricset cannot be used with xpack.enabled: true (#12264) (#12277)
    * Validate that kibana/status metricset cannot be used with xpack.enabled: true
    
    * Adding CHANGELOG entry

--- metricbeat/module/kibana/status/status.go
+++ metricbeat/module/kibana/status/status.go
@@ -18,6 +18,8 @@
 package status
 
 import (
+	"fmt"
+
 	"github.com/elastic/beats/metricbeat/helper"
 	"github.com/elastic/beats/metricbeat/helper/elastic"
 	"github.com/elastic/beats/metricbeat/mb"
@@ -53,6 +55,10 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 	ms, err := kibana.NewMetricSet(base)
 	if err != nil {
 		return nil, err
+	}
+
+	if ms.XPackEnabled {
+		return nil, fmt.Errorf("The %s metricset cannot be used with xpack.enabled: true", ms.FullyQualifiedName())
 	}
 
 	http, err := helper.NewHTTP(base)
