72c31b7bb7a1180693b656a6746ab14173b3e52c
Andrew Kroh <andrew.kroh@elastic.co>
2019-05-28 09:13:11 -0400

    Add documentation for Winlogbeat modules
    Add documentation for the security and sysmon modules that explains
    what the modules accomplish and how to configure them.
    
    Relates #11651

--- winlogbeat/scripts/mage/update.go
+++ winlogbeat/scripts/mage/update.go
@@ -32,7 +32,7 @@ func init() {
 
 	dashboards.RegisterImportDeps(build.Build, Update.Dashboards)
 
-	docs.RegisterDeps(Update.FieldDocs)
+	docs.RegisterDeps(Update.FieldDocs, Update.ModuleDocs)
 }
 
 var (
@@ -45,7 +45,7 @@ type Update mg.Namespace
 
 // All updates all generated content.
 func (Update) All() {
-	mg.Deps(Update.Fields, Update.Dashboards, Update.Config, Update.FieldDocs)
+	mg.Deps(Update.Fields, Update.Dashboards, Update.Config, Update.FieldDocs, Update.ModuleDocs)
 }
 
 // Config updates the Beat's config files.
@@ -68,4 +68,8 @@ func (Update) Fields() {
 func (Update) FieldDocs() error {
 	mg.Deps(fb.FieldsAllYML)
 	return mage.Docs.FieldDocs(mage.FieldsAllYML)
+}
+
+func (Update) ModuleDocs() error {
+	return moduleDocs()
 }
