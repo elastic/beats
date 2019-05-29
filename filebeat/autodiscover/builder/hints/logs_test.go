elastic/beats

commit 313e6d16bec56d0acf799ab06cd2072dabde65bc

Add `container` input, deprecate `docker` in favor of it (#12162)
Add `container` input, deprecate `docker` in favor of it

This change adds a new container input for better support of CRI based
scenarios.

`docker` input was acting as a catch all input for all container related
cases, but its config options were very opinionated towards Docker, with
some issues:

 * `containers.ids` setting was good to abstract logs path, but we have
 seen many cases were logs are not under default location, or follow a
 different path pattern (ie CRI logs).
 * `containers.*` settings have shown counter intuitive for many users,
 in many cases we have seen people writing `container.*` instead, ending
 up in a config error.
 * Some existing settings (`combine_partials`, `cri.parse_flags`) were
 introduced as a way to offer a backwards compatible upgrades, but it
 doesn't really make sense to disable them, as they handle actual
 format behaviors.

This new `container` input offers the same wrapper to read log files
from containers with the following changes:

 * It exposes `paths` as the `log` input, instead of `containers.ids`
 and `containers.path`.
 * `parse_flags` and `combine_partials` are hardcoded, as there is no
 good reason to disable them.
 * `stream` selector is still available, under root settings.
 * It allows to select the log format (also atodetect it), giving room
 for future format changes. `format` can be `auto` (default), `docker`
    and `CRI`.

Example configurations:

Get Docker logs:

```
filebeat.inputs:
- type: container
  paths:
    - /var/lib/docker/containers/*/*.log
```

Get Kubernetes logs:

```
filebeat.inputs:
- type: container
  paths:
    - /var/log/pods/*/*/*.log
    # this could also be used:
    #- /var/log/containers/*.log
```

Previous `docker` input is deprecated in favor of this, to be removed in 8.0

filebeat/autodiscover/builder/hints/config.go
@@ -26,7 +26,7 @@ type config struct {
 
 func defaultConfig() config {
 	rawCfg := map[string]interface{}{
-		"type": "docker",
+		"type": "container",
 		"containers": map[string]interface{}{
 			"paths": []string{
 				// To be able to use this builder with CRI-O replace paths with:

filebeat/input/container/config.go
@@ -15,35 +15,44 @@
 // specific language governing permissions and limitations
 // under the License.
 
-package docker
+package container
+
+import (
+	"fmt"
+	"strings"
+)
 
 var defaultConfig = config{
-	Partial: true,
-	Containers: containers{
-		IDs:    []string{},
-		Path:   "/var/lib/docker/containers",
-		Stream: "all",
-	},
+	Stream: "all",
+	Format: "auto",
 }
 
 type config struct {
-	// List of containers' log files to tail
-	Containers containers `config:"containers"`
-
-	// Partial configures the input to join partial lines
-	Partial bool `config:"combine_partials"`
-
-	// Enable CRI flags parsing (to be switched to default in 7.0)
-	CRIFlags bool `config:"cri.parse_flags"`
-
-	// Fore CRI format (don't perform autodetection)
-	CRIForce bool `config:"cri.force"`
-}
-
-type containers struct {
-	IDs  []string `config:"ids"`
-	Path string   `config:"path"`
-
 	// Stream can be all, stdout or stderr
 	Stream string `config:"stream"`
+
+	// Format can be auto, cri, json-file
+	Format string `config:"format"`
+}
+
+// Validate validates the config.
+func (c *config) Validate() error {
+	if !stringInSlice(c.Stream, []string{"all", "stdout", "stderr"}) {
+		return fmt.Errorf("invalid value for stream: %s, supported values are: all, stdout, stderr", c.Stream)
+	}
+
+	if !stringInSlice(strings.ToLower(c.Format), []string{"auto", "docker", "cri"}) {
+		return fmt.Errorf("invalid value for format: %s, supported values are: auto, docker, cri", c.Format)
+	}
+
+	return nil
+}
+
+func stringInSlice(str string, list []string) bool {
+	for _, v := range list {
+		if v == str {
+			return true
+		}
+	}
+	return false
 }

filebeat/input/log/config.go
@@ -109,7 +109,7 @@ type config struct {
 	DockerJSON *struct {
 		Stream   string `config:"stream"`
 		Partial  bool   `config:"partial"`
-		ForceCRI bool   `config:"force_cri_logs"`
+		Format   string `config:"format"`
 		CRIFlags bool   `config:"cri_flags"`
 	} `config:"docker-json"`
 }

filebeat/docs/filebeat-options.asciidoc
@@ -44,6 +44,7 @@ You can configure {beatname_uc} to use the following inputs:
 
 * <<{beatname_lc}-input-log>>
 * <<{beatname_lc}-input-stdin>>
+* <<{beatname_lc}-input-container>>
 * <<{beatname_lc}-input-redis>>
 * <<{beatname_lc}-input-udp>>
 * <<{beatname_lc}-input-docker>>
@@ -56,6 +57,8 @@ You can configure {beatname_uc} to use the following inputs:
 include::inputs/input-log.asciidoc[]
 
 include::inputs/input-stdin.asciidoc[]
+
+include::inputs/input-container.asciidoc[]
 
 include::inputs/input-redis.asciidoc[]
 

filebeat/docs/inputs/input-container.asciidoc
@@ -1,0 +1,64 @@
+:type: container
+
+[id="{beatname_lc}-input-{type}"]
+=== Container input
+
+++++
+<titleabbrev>Container</titleabbrev>
+++++
+
+Use the `container` input to read containers log files.
+
+This input searches for container logs under the given path, and parse them into
+common message lines, extracting timestamps too. Everything happens before line
+filtering, multiline, and JSON decoding, so this input can be used in
+combination with those settings.
+
+Example configuration:
+
+["source","yaml",subs="attributes"]
+----
+{beatname_lc}.inputs:
+- type: container
+  paths: <1>
+    - '/var/lib/docker/containers/*/*.log'
+----
+
+<1> `paths` is required. All other settings are optional.
+
+==== Configuration options
+
+The `container` input supports the following configuration options plus the
+<<{beatname_lc}-input-{type}-common-options>> described later.
+
+===== `stream`
+
+Reads from the specified streams only: `all`, `stdout` or `stderr`. The default
+is `all`.
+
+===== `format`
+
+Use the given format when reading the log file: `auto`, `docker` or `cri`. The
+default is `auto`, it will automatically detect the format. To disable
+autodetection set any of the other options.
+
+
+The following input configures {beatname_uc} to read the `stdout` stream from
+all containers under the default Kubernetes logs path:
+
+[source,yaml]
+----
+- type: container
+  stream: stdout
+  paths:
+    - "/var/log/containers/*.log"
+----
+
+include::../inputs/input-common-harvester-options.asciidoc[]
+
+include::../inputs/input-common-file-options.asciidoc[]
+
+[id="{beatname_lc}-input-{type}-common-options"]
+include::../inputs/input-common-options.asciidoc[]
+
+:type!:

filebeat/docs/inputs/input-docker.asciidoc
@@ -7,6 +7,8 @@
 <titleabbrev>Docker</titleabbrev>
 ++++
 
+deprecated[7.2.0, Use `container` input instead.]
+
 Use the `docker` input to read logs from Docker containers.
 
 This input searches for container logs under its path, and parse them into

filebeat/harvester/util.go
@@ -21,11 +21,12 @@ import "github.com/elastic/beats/libbeat/common/match"
 
 // Contains available input types
 const (
-	LogType    = "log"
-	StdinType  = "stdin"
-	RedisType  = "redis"
-	UdpType    = "udp"
-	DockerType = "docker"
+	LogType       = "log"
+	StdinType     = "stdin"
+	RedisType     = "redis"
+	UdpType       = "udp"
+	DockerType    = "docker"
+	ContainerType = "container"
 )
 
 // MatchAny checks if the text matches any of the regular expressions

filebeat/include/list.go
@@ -21,6 +21,7 @@ package include
 
 import (
 	// Import packages that need to register themselves.
+	_ "github.com/elastic/beats/filebeat/input/container"
 	_ "github.com/elastic/beats/filebeat/input/docker"
 	_ "github.com/elastic/beats/filebeat/input/log"
 	_ "github.com/elastic/beats/filebeat/input/redis"

commit ed46f38276483997b512f2e3bbf638b88e6bc442

Fix Filebeat autodiscover hints default settings
Default settings were broken after #12162, this code updates them to the
new input and makes sure that enable/disable logic works correctly.

Tests have been added to make sure this changes still work

filebeat/autodiscover/builder/hints/config.go
@@ -27,12 +27,10 @@ type config struct {
 func defaultConfig() config {
 	defaultCfgRaw := map[string]interface{}{
 		"type": "container",
-		"containers": map[string]interface{}{
-			"paths": []string{
-				// To be able to use this builder with CRI-O replace paths with:
-				// /var/log/pods/${data.kubernetes.pod.uid}/${data.kubernetes.container.name}/*.log
-				"/var/lib/docker/containers/${data.container.id}/*-json.log",
-			},
+		"paths": []string{
+			// To be able to use this builder with CRI-O replace paths with:
+			// /var/log/pods/${data.kubernetes.pod.uid}/${data.kubernetes.container.name}/*.log
+			"/var/lib/docker/containers/${data.container.id}/*-json.log",
 		},
 	}
 	defaultCfg, _ := common.NewConfigFrom(defaultCfgRaw)
@@ -53,7 +51,16 @@ func (c *config) Unpack(from *common.Config) error {
 	}
 
 	if config, err := from.Child("default_config", -1); err == nil {
-		c.DefaultConfig = config
+		fields := config.GetFields()
+		if len(fields) == 1 && fields[0] == "enabled" {
+			// only enabling/disabling default config:
+			if err := c.DefaultConfig.Merge(config); err != nil {
+				return nil
+			}
+		} else {
+			// full config provided, discard default
+			c.DefaultConfig = config
+		}
 	}
 
 	c.Key = tmpConfig.Key

filebeat/autodiscover/builder/hints/logs.go
@@ -88,8 +88,10 @@ func (l *logHints) CreateConfig(event bus.Event) []*common.Config {
 		return []*common.Config{}
 	}
 
-	// Clone original config
+	// Clone original config, enable it if disabled
 	config, _ := common.NewConfigFrom(l.config.DefaultConfig)
+	config.Remove("enabled", -1)
+
 	host, _ := event["host"].(string)
 	if host == "" {
 		return []*common.Config{}

filebeat/autodiscover/builder/hints/logs_test.go
@@ -29,14 +29,105 @@ import (
 )
 
 func TestGenerateHints(t *testing.T) {
+	customCfg := common.MustNewConfigFrom(map[string]interface{}{
+		"default_config": map[string]interface{}{
+			"type": "docker",
+			"containers": map[string]interface{}{
+				"ids": []string{
+					"${data.container.id}",
+				},
+			},
+			"close_timeout": "true",
+		},
+	})
+
+	defaultCfg := common.NewConfig()
+
+	defaultDisabled := common.MustNewConfigFrom(map[string]interface{}{
+		"default_config": map[string]interface{}{
+			"enabled": "false",
+		},
+	})
+
 	tests := []struct {
 		msg    string
+		config *common.Config
 		event  bus.Event
 		len    int
 		result common.MapStr
 	}{
 		{
-			msg: "Hints without host should return nothing",
+			msg:    "Default config is correct",
+			config: defaultCfg,
+			event: bus.Event{
+				"host": "1.2.3.4",
+				"kubernetes": common.MapStr{
+					"container": common.MapStr{
+						"name": "foobar",
+						"id":   "abc",
+					},
+				},
+				"container": common.MapStr{
+					"name": "foobar",
+					"id":   "abc",
+				},
+			},
+			len: 1,
+			result: common.MapStr{
+				"paths": []interface{}{"/var/lib/docker/containers/abc/*-json.log"},
+				"type":  "container",
+			},
+		},
+		{
+			msg:    "Config disabling works",
+			config: defaultDisabled,
+			event: bus.Event{
+				"host": "1.2.3.4",
+				"kubernetes": common.MapStr{
+					"container": common.MapStr{
+						"name": "foobar",
+						"id":   "abc",
+					},
+				},
+				"container": common.MapStr{
+					"name": "foobar",
+					"id":   "abc",
+				},
+			},
+			len: 0,
+		},
+		{
+			msg:    "Hint to enable when disabled by default works",
+			config: defaultDisabled,
+			event: bus.Event{
+				"host": "1.2.3.4",
+				"kubernetes": common.MapStr{
+					"container": common.MapStr{
+						"name": "foobar",
+						"id":   "abc",
+					},
+				},
+				"container": common.MapStr{
+					"name": "foobar",
+					"id":   "abc",
+				},
+				"hints": common.MapStr{
+					"logs": common.MapStr{
+						"enabled":       "true",
+						"exclude_lines": "^test2, ^test3",
+					},
+				},
+			},
+			len: 1,
+			result: common.MapStr{
+				"type":          "container",
+				"paths":         []interface{}{"/var/lib/docker/containers/abc/*-json.log"},
+				"exclude_lines": []interface{}{"^test2", "^test3"},
+			},
+		},
+		{
+			msg:    "Hints without host should return nothing",
+			config: customCfg,
 			event: bus.Event{
 				"hints": common.MapStr{
 					"metrics": common.MapStr{
@@ -48,7 +139,8 @@ func TestGenerateHints(t *testing.T) {
 			result: common.MapStr{},
 		},
 		{
-			msg: "Hints with logs.disable should return nothing",
+			msg:    "Hints with logs.disable should return nothing",
+			config: customCfg,
 			event: bus.Event{
 				"hints": common.MapStr{
 					"logs": common.MapStr{
@@ -60,7 +152,8 @@ func TestGenerateHints(t *testing.T) {
 			result: common.MapStr{},
 		},
 		{
-			msg: "Empty event hints should return default config",
+			msg:    "Empty event hints should return default config",
+			config: customCfg,
 			event: bus.Event{
 				"host": "1.2.3.4",
 				"kubernetes": common.MapStr{
@@ -84,7 +177,8 @@ func TestGenerateHints(t *testing.T) {
 			},
 		},
 		{
-			msg: "Hint with include|exclude_lines must be part of the input config",
+			msg:    "Hint with include|exclude_lines must be part of the input config",
+			config: customCfg,
 			event: bus.Event{
 				"host": "1.2.3.4",
 				"kubernetes": common.MapStr{
@@ -116,7 +210,8 @@ func TestGenerateHints(t *testing.T) {
 			},
 		},
 		{
-			msg: "Hint with multiline config must have a multiline in the input config",
+			msg:    "Hint with multiline config must have a multiline in the input config",
+			config: customCfg,
 			event: bus.Event{
 				"host": "1.2.3.4",
 				"kubernetes": common.MapStr{
@@ -152,7 +247,8 @@ func TestGenerateHints(t *testing.T) {
 			},
 		},
 		{
-			msg: "Hint with inputs config as json must be accepted",
+			msg:    "Hint with inputs config as json must be accepted",
+			config: customCfg,
 			event: bus.Event{
 				"host": "1.2.3.4",
 				"kubernetes": common.MapStr{
@@ -184,7 +280,8 @@ func TestGenerateHints(t *testing.T) {
 			},
 		},
 		{
-			msg: "Hint with processors config must have a processors in the input config",
+			msg:    "Hint with processors config must have a processors in the input config",
+			config: customCfg,
 			event: bus.Event{
 				"host": "1.2.3.4",
 				"kubernetes": common.MapStr{
@@ -230,7 +327,8 @@ func TestGenerateHints(t *testing.T) {
 			},
 		},
 		{
-			msg: "Hint with module should attach input to its filesets",
+			msg:    "Hint with module should attach input to its filesets",
+			config: customCfg,
 			event: bus.Event{
 				"host": "1.2.3.4",
 				"kubernetes": common.MapStr{
@@ -277,7 +375,8 @@ func TestGenerateHints(t *testing.T) {
 			},
 		},
 		{
-			msg: "Hint with module should honor defined filesets",
+			msg:    "Hint with module should honor defined filesets",
+			config: customCfg,
 			event: bus.Event{
 				"host": "1.2.3.4",
 				"kubernetes": common.MapStr{
@@ -325,7 +424,8 @@ func TestGenerateHints(t *testing.T) {
 			},
 		},
 		{
-			msg: "Hint with module should honor defined filesets with streams",
+			msg:    "Hint with module should honor defined filesets with streams",
+			config: customCfg,
 			event: bus.Event{
 				"host": "1.2.3.4",
 				"kubernetes": common.MapStr{
@@ -376,25 +476,13 @@ func TestGenerateHints(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		cfg, _ := common.NewConfigFrom(map[string]interface{}{
-			"default_config": map[string]interface{}{
-				"type": "docker",
-				"containers": map[string]interface{}{
-					"ids": []string{
-						"${data.container.id}",
-					},
-				},
-				"close_timeout": "true",
-			},
-		})
-
 		// Configure path for modules access
 		abs, _ := filepath.Abs("../../..")
 		err := paths.InitPaths(&paths.Path{
 			Home: abs,
 		})
 
-		l, err := NewLogHints(cfg)
+		l, err := NewLogHints(test.config)
 		if err != nil {
 			t.Fatal(err)
 		}