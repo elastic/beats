3737dd7e88c1b74cfccc34016ed00f3c6097296b
Noémi Ványi <sitbackandwait@gmail.com>
2019-05-30 15:10:08 +0200

    pep8 fix

--- filebeat/tests/system/test_setup.py
+++ filebeat/tests/system/test_setup.py
@@ -17,7 +17,6 @@ class Test(BaseTest):
         print("Using elasticsearch: {}".format(self.elasticsearch_url))
         self.es = Elasticsearch([self.elasticsearch_url])
 
-
     @unittest.skipIf(not INTEGRATION_TESTS,
                      "integration tests are disabled, run with INTEGRATION_TESTS=1 to enable them.")
     @unittest.skipIf(os.getenv("TESTING_ENVIRONMENT") == "2x",
@@ -28,15 +27,15 @@ class Test(BaseTest):
         """
         self.init()
         self.render_config_template(
-            elasticsearch = {
+            elasticsearch={
                 "host": "localhost:9200",
             }
         )
-                
+
         copyfile(self.beat_path + "/tests/system/input/system.yml", self.beat_path + "/modules.d/system.yml")
 
         beat_setup_modules_pipelines = self.start_beat(
-            extra_args = [
+            extra_args=[
                 "setup",
                 "-path.home", self.beat_path,
                 "--pipelines",
@@ -48,12 +47,12 @@ class Test(BaseTest):
         version = self.get_beat_version()
         system_syslog_pipeline_name = "filebeat-" + version + "-system-syslog-pipeline"
         system_syslog_pipeline = self.es.transport.perform_request("GET",
-                "/_ingest/pipeline/" + system_syslog_pipeline_name)
+                                                                   "/_ingest/pipeline/" + system_syslog_pipeline_name)
 
         assert "timezone" in system_syslog_pipeline[system_syslog_pipeline_name]["processors"][3]["date"]
 
         system_auth_pipeline_name = "filebeat-" + version + "-system-auth-pipeline"
         system_auth_pipeline = self.es.transport.perform_request("GET",
-                "/_ingest/pipeline/" + system_auth_pipeline_name)
+                                                                 "/_ingest/pipeline/" + system_auth_pipeline_name)
 
         assert "timezone" not in system_auth_pipeline[system_auth_pipeline_name]["processors"][4]["date"]
