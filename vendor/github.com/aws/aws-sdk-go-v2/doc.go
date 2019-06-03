885eda80f4ecdf8573c6d61d807dab8139416c19
Andrew Cholakian <andrew@andrewvc.com>
2019-06-03 11:34:00 -0500

    Fix govendor

--- vendor/github.com/aws/aws-sdk-go-v2/doc.go
+++ vendor/github.com/aws/aws-sdk-go-v2/doc.go
@@ -18,7 +18,7 @@ pinned to specific version of the SDK
 
 	dep ensure --add github.com/aws/aws-sdk-go-v2
 
-### Hello AWS
+Hello AWS
 
 This example shows how you can use the v2 SDK to make an API request using the
 SDK's Amazon DynamoDB client.
