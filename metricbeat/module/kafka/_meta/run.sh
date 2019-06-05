a3eeb3bfae96b9fbef1b4ba687d08a821e7debde
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2019-06-05 12:50:53 +0200

    Fix go tests, and update docs

--- metricbeat/module/kafka/_meta/run.sh
+++ metricbeat/module/kafka/_meta/run.sh
@@ -37,8 +37,15 @@ wait_for_port 9092
 
 echo "Kafka load status code $?"
 
+# ACLS used to prepare tests
 ${KAFKA_HOME}/bin/kafka-acls.sh --authorizer-properties zookeeper.connect=localhost:2181 --add --allow-principal User:producer --operation All --cluster --topic '*' --group '*'
-${KAFKA_HOME}/bin/kafka-acls.sh --authorizer-properties zookeeper.connect=localhost:2181 --add --allow-principal User:stats --operation Describe --topic '*' --group '*'
+${KAFKA_HOME}/bin/kafka-acls.sh --authorizer-properties zookeeper.connect=localhost:2181 --add --allow-principal User:consumer --operation All --cluster --topic '*' --group '*'
+
+# Minimal ACLs required by metricbeat. If this needs to be changed, please update docs too
+${KAFKA_HOME}/bin/kafka-acls.sh --authorizer-properties zookeeper.connect=localhost:2181 --add --allow-principal User:stats --operation Describe --group '*'
+${KAFKA_HOME}/bin/kafka-acls.sh --authorizer-properties zookeeper.connect=localhost:2181 --add --allow-principal User:stats --operation Read --topic '*'
+
+touch /tmp/.acls_loaded
 
 # Make sure the container keeps running
 tail -f /dev/null
