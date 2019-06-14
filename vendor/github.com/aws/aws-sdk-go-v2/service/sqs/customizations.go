1277fcb5508b618fbb1a2a436239f217abff0839
Andrew Cholakian <andrew@andrewvc.com>
2019-06-14 11:17:10 -0500

    Update AWS vendored imports + add elasticloadbalancerv2
    Since upgrading these deps in https://github.com/elastic/beats/pull/12401 is too much for one PR, we're combining them.

--- vendor/github.com/aws/aws-sdk-go-v2/service/sqs/customizations.go
+++ vendor/github.com/aws/aws-sdk-go-v2/service/sqs/customizations.go
@@ -3,7 +3,7 @@ package sqs
 import request "github.com/aws/aws-sdk-go-v2/aws"
 
 func init() {
-	initRequest = func(c *SQS, r *request.Request) {
+	initRequest = func(c *Client, r *request.Request) {
 		if !c.DisableComputeChecksums {
 			setupChecksumValidation(r)
 		}
