4d2c3b60e3b44cfdfb5c346f41d508ffa912f341
Andrew Cholakian <andrew@andrewvc.com>
2019-06-17 12:24:42 -0500

    Un-vendor

--- vendor/github.com/elastic/go-lookslike/validator/validator.go
+++
@@ -1,6 +0,0 @@
-package validator
-
-import "github.com/elastic/go-lookslike/llresult"
-
-// Validator is the result of Compile and is run against the map you'd like to test.
-type Validator func(interface{}) *llresult.Results
