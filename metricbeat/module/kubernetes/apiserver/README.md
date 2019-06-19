5ce4d38e77c00d97942fd8b986df66ea0d3e28ac
odacremolbap <odacremolbap@gmail.com>
2019-06-19 10:37:24 +0200

    update metrics for apiserver

--- metricbeat/module/kubernetes/apiserver/README.md
+++ metricbeat/module/kubernetes/apiserver/README.md
@@ -2,45 +2,12 @@
 
 ## Version history
 
-Unknown, unknown
-June 2019, `v1.14.3`
+- Unknown, unknown
+- June 2019, `v1.14.3`: this version drops `apiserver_request_count` in favor of `apiserver_request_total`. There is a new label for `dry_run`, which is rarely used. Current dashboards should be unaffected. Metric `apiserver_request_latencies` is deprecated and should be removed at next major version.
 
 ## Resources
 
-apiserver_admission_step_admission_duration_seconds_summary +
- - operation
- - rejected
- - type
-
-apiserver_audit_event_total
-apiserver_audit_requests_rejected_total
-apiserver_current_inflight_requests
-    - requestKind
-apiserver_longrunning_gauge
-    - component
-    - group
-    - resource
-    - scope
-    - subresource
-    - verb
-    - version
-
-apiserver_registered_watchers
-    - group
-    - kind
-    - version
-
-apiserver_request_duration_seconds_bucket
-    - component
-    - dry_run
-    - group
-    - resource
-    - scope
-    - subresource
-    - verb
-    - version
-
-apiserver_request_total
+`apiserver_request_total`
     - client
     - code
     - component 
@@ -52,7 +19,7 @@ apiserver_request_total
     - verb
     - version
 
-apiserver_response_sizes_bucket
+`apiserver_longrunning_gauge`
     - component
     - group
     - resource
@@ -61,7 +28,45 @@ apiserver_response_sizes_bucket
     - verb
     - version
 
-etcd_object_counts
+`etcd_object_counts`
     - resource
 
+`apiserver_current_inflight_requests`
+    - requestKind
 
+`apiserver_audit_event_total`
+
+`apiserver_audit_requests_rejected_total`
+
+
+
+
+
+### Not included
+
+Not including these buckets one for now, it is massive because of the different labels, and it might be not provide info that's worth it at visualizations
+`apiserver_request_duration_seconds_bucket`
+    - component
+    - dry_run
+    - group
+    - resource
+    - scope
+    - subresource
+    - verb
+    - version
+
+Not including these buckets one for now, it is massive because of the different labels, and it might be not provide info that's worth it at visualizations
+`apiserver_response_sizes_bucket`
+    - component
+    - group
+    - resource
+    - scope
+    - subresource
+    - verb
+    - version
+
+Metrics from `apiserver_longrunning_gauge` seems to be more interesting than this one
+`apiserver_registered_watchers`
+    - group
+    - kind
+    - version
--- metricbeat/module/kubernetes/apiserver/apiserver.go
+++ metricbeat/module/kubernetes/apiserver/apiserver.go
@@ -24,17 +24,47 @@ import (
 
 func init() {
 	mapping := &prometheus.MetricsMapping{
+		// Metrics: map[string]prometheus.MetricMap{
+		// 	"apiserver_request_count":     prometheus.Metric("request.count"),
+		// 	"apiserver_request_latencies": prometheus.Metric("request.latency"),
+		// },
+
+		// Labels: map[string]prometheus.LabelMap{
+		// 	"client":      prometheus.KeyLabel("request.client"),
+		// 	"resource":    prometheus.KeyLabel("request.resource"),
+		// 	"scope":       prometheus.KeyLabel("request.scope"),
+		// 	"subresource": prometheus.KeyLabel("request.subresource"),
+		// 	"verb":        prometheus.KeyLabel("request.verb"),
+		// },
+
 		Metrics: map[string]prometheus.MetricMap{
-			"apiserver_request_count":     prometheus.Metric("request.count"),
+			// Deprecated, remove in future releases
 			"apiserver_request_latencies": prometheus.Metric("request.latency"),
+
+			"apiserver_request_total":                 prometheus.Metric("request.count"),
+			"apiserver_current_inflight_requests":     prometheus.Metric("request.current.count"),
+			"apiserver_longrunning_gauge":             prometheus.Metric("request.longrunning.count"),
+			"etcd_object_counts":                      prometheus.Metric("etcd.object.count"),
+			"apiserver_audit_event_total":             prometheus.Metric("audit.event.count"),
+			"apiserver_audit_requests_rejected_total": prometheus.Metric("audit.rejected.count"),
 		},
 
 		Labels: map[string]prometheus.LabelMap{
 			"client":      prometheus.KeyLabel("request.client"),
-			"resource":    prometheus.KeyLabel("request.resource"),
-			"scope":       prometheus.KeyLabel("request.scope"),
-			"subresource": prometheus.KeyLabel("request.subresource"),
+			"code":        prometheus.KeyLabel("request.code"),
+			"contentType": prometheus.KeyLabel("request.content_type"),
+			"dry_run":     prometheus.KeyLabel("request.dry_run"),
+			"requestKind": prometheus.KeyLabel("request.kind"),
+
+			// when deprecating current apiserver metrics, these
+			// labels can be swithed from `request.` to `api.`
 			"verb":        prometheus.KeyLabel("request.verb"),
+			"scope":       prometheus.KeyLabel("request.scope"),
+			"resource":    prometheus.KeyLabel("request.resource"),
+			"subresource": prometheus.KeyLabel("request.subresource"),
+			"component":   prometheus.KeyLabel("api.component"),
+			"group":       prometheus.KeyLabel("api.group"),
+			"version":     prometheus.KeyLabel("api.version"),
 		},
 	}
 
