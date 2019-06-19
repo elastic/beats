83e336e22a0ed3b5da2aa5965040e50a0754226a
Shaunak Kashyap <ycombinator@gmail.com>
2019-06-19 06:21:38 -0700

    Renaming module name from beats to beat

--- metricbeat/module/beats/_meta/healthcheck.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Check that all endpoints are available
-curl -f http://localhost:5066 || exit 1
-curl -f http://localhost:5066/stats || exit 1
-curl -f http://localhost:5066/state || exit 1
