d29c5339a5da8e224ab536ee1bd896413a7dd457
Shaunak Kashyap <ycombinator@gmail.com>
2019-06-21 08:33:46 -0700

    Adding beat module with stats metricset (#12181)
    * Adding beats module with stats metricset
    
    * Rename argument for clarity
    
    * Adding tests
    
    * Adding tests + sample data
    
    * Removing debugging statements
    
    * Regenerating data.json file
    
    * Renaming anchor for beats common fields
    
    * Removing extraneous file accidentally checked in
    
    * Result of running make update
    
    * Trying a different host name
    
    * Stubbing out xpack code path
    
    * Fixing service name in system test
    
    * Fleshing out TODOs
    
    * Fixing index name
    
    * Renaming module name from beats to beat
    
    * Regenerating data.json file
    
    * Update metricbeat/docker-compose.yml
    
    Co-Authored-By: Nicolas Ruflin <spam@ruflin.com>
    
    * Update metricbeat/module/beat/_meta/config.reference.yml
    
    Co-Authored-By: Nicolas Ruflin <spam@ruflin.com>
    
    * Update metricbeat/metricbeat.reference.yml
    
    Co-Authored-By: Nicolas Ruflin <spam@ruflin.com>
    
    * Update metricbeat/docker-compose.yml
    
    Co-Authored-By: Nicolas Ruflin <spam@ruflin.com>
    
    * Update metricbeat/module/beat/_meta/config-xpack.yml
    
    Co-Authored-By: Nicolas Ruflin <spam@ruflin.com>
    
    * Update metricbeat/module/beat/_meta/config.yml
    
    Co-Authored-By: Nicolas Ruflin <spam@ruflin.com>
    
    * Update metricbeat/docker-compose.yml
    
    * Update metricbeat/modules.d/beat-xpack.yml.disabled
    
    * Update x-pack/metricbeat/metricbeat.reference.yml
    
    * Update metricbeat/modules.d/beat.yml.disabled
    
    * Renaming system test
    
    * Regenerating files
    
    * Trying to use the new testdata framework
    
    * Renaming service to avoid naming collision
    
    * Adding back data_test.go
    
    * Fixing HTTP server hostname
    
    * Trying localhost
    
    * Fixing inconsistencies
    
    * Fixing typo
    
    * Make runtime optional as it is absent in older versions of Beats
    
    * Updates from make update

---
+++ metricbeat/module/beat/testing.go
@@ -0,0 +1,49 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package beat
+
+import "os"
+
+// GetEnvHost for Metricbeat
+func GetEnvHost() string {
+	host := os.Getenv("BEAT_HOST")
+
+	if len(host) == 0 {
+		host = "127.0.0.1"
+	}
+	return host
+}
+
+// GetEnvPort for Metricbeat
+func GetEnvPort() string {
+	port := os.Getenv("BEAT_PORT")
+
+	if len(port) == 0 {
+		port = "5066"
+	}
+	return port
+}
+
+// GetConfig for Metricbeat
+func GetConfig(metricset string) map[string]interface{} {
+	return map[string]interface{}{
+		"module":     ModuleName,
+		"metricsets": []string{metricset},
+		"hosts":      []string{GetEnvHost() + ":" + GetEnvPort()},
+	}
+}
