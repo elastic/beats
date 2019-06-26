8261800abcb2bd1dec178509aa556d9a1dc55ff7
Shaunak Kashyap <ycombinator@gmail.com>
2019-06-26 15:14:34 -0400

    Extend logstash.node_stats metricset for logstash_stats stack… (#11511)
    * WIP: Adding GetPipelinesStats function
    
    * WIP: initial code for x-pack path
    
    * Implementing a couple of TODOs
    
    * Reference issues in TODOs
    
    * Getting logstash basic node info
    
    * Get basic node info from GET _node/stats call
    
    * Inlining single-use const
    
    * Updating code per LS API changes
    
    * Resolving remaining TODOs
    
    * Fixing version serialization
    
    * Fixing logstash double nesting
    
    * Removing TODOs as they have been implemented now

--- metricbeat/module/logstash/logstash.go
+++ metricbeat/module/logstash/logstash.go
@@ -45,7 +45,19 @@ type PipelineState struct {
 	Representation map[string]interface{} `json:"representation"`
 	BatchSize      int                    `json:"batch_size"`
 	Workers        int                    `json:"workers"`
-	ClusterIDs     []string               `json:"cluster_uuids,omitempty"` // TODO: see https://github.com/elastic/logstash/issues/10602
+	ClusterIDs     []string               `json:"cluster_uuids,omitempty"`
+}
+
+// PipelineStats represents the stats of a Logstash pipeline
+type PipelineStats struct {
+	ID          string                   `json:"id"`
+	Hash        string                   `json:"hash"`
+	EphemeralID string                   `json:"ephemeral_id"`
+	Events      map[string]interface{}   `json:"events"`
+	Reloads     map[string]interface{}   `json:"reloads"`
+	Queue       map[string]interface{}   `json:"queue"`
+	Vertices    []map[string]interface{} `json:"vertices"`
+	ClusterIDs  []string                 `json:"cluster_uuids,omitempty"`
 }
 
 // NewMetricSet creates a metricset that can be used to build other metricsets
@@ -83,6 +95,31 @@ func GetPipelines(http *helper.HTTP, resetURI string) ([]PipelineState, error) {
 	}
 
 	var pipelines []PipelineState
+	for pipelineID, pipeline := range pipelinesResponse.Pipelines {
+		pipeline.ID = pipelineID
+		pipelines = append(pipelines, pipeline)
+	}
+
+	return pipelines, nil
+}
+
+// GetPipelinesStats returns the list of pipelines (and their stats) running on a Logstash node
+func GetPipelinesStats(http *helper.HTTP, resetURI string) ([]PipelineStats, error) {
+	content, err := fetchPath(http, resetURI, "_node/stats", "vertices=true")
+	if err != nil {
+		return nil, errors.Wrap(err, "could not fetch node pipeline stats")
+	}
+
+	pipelinesResponse := struct {
+		Pipelines map[string]PipelineStats `json:"pipelines"`
+	}{}
+
+	err = json.Unmarshal(content, &pipelinesResponse)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not parse node pipeline stats response")
+	}
+
+	var pipelines []PipelineStats
 	for pipelineID, pipeline := range pipelinesResponse.Pipelines {
 		pipeline.ID = pipelineID
 		pipelines = append(pipelines, pipeline)
