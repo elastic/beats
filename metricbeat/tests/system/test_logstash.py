7af8a99a92427bb3f66c8232812a558e3e7cb169
Shaunak Kashyap <ycombinator@gmail.com>
2019-06-26 22:44:12 -0400

    Add version check + skip

--- metricbeat/tests/system/test_logstash.py
+++ metricbeat/tests/system/test_logstash.py
@@ -1,6 +1,10 @@
 import os
 import metricbeat
 import unittest
+from nose.plugins.skip import SkipTest
+import urllib2
+import json
+import semver
 import time
 
 
@@ -28,6 +32,11 @@ class Test(metricbeat.BaseTest):
         """
         logstash-xpack module tests
         """
+        version = self.get_version()
+        if semver.compare(version, "7.3.0") == -1:
+            # Skip for Logstash versions < 7.3.0 as necessary APIs not available
+            raise SkipTest
+
         self.render_config_template(modules=[{
             "name": "logstash",
             "metricsets": ["node", "node_stats"],
@@ -42,3 +51,12 @@ class Test(metricbeat.BaseTest):
         self.wait_until(lambda: self.output_lines() > 0)
         proc.check_kill_and_wait()
         self.assert_no_logged_warnings()
+
+    def get_version(self):
+        host = self.get_hosts()[0]
+        res = urllib2.urlopen("http://" + host + "/").read()
+
+        body = json.loads(res)
+        version = body["version"]
+
+        return version
