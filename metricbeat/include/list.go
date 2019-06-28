f9f1aa52ec93eb617c5fb27201b8f269720da52e
Pablo Mercado <pablo.mercado@elastic.co>
2019-06-28 16:02:46 +0200

    [metricbeat]kube scheduler metricset (#12521)
    * kubernetes scheduler metricset

--- metricbeat/include/list.go
+++ metricbeat/include/list.go
@@ -106,6 +106,7 @@ import (
 	_ "github.com/elastic/beats/metricbeat/module/kubernetes/node"
 	_ "github.com/elastic/beats/metricbeat/module/kubernetes/pod"
 	_ "github.com/elastic/beats/metricbeat/module/kubernetes/proxy"
+	_ "github.com/elastic/beats/metricbeat/module/kubernetes/scheduler"
 	_ "github.com/elastic/beats/metricbeat/module/kubernetes/state_container"
 	_ "github.com/elastic/beats/metricbeat/module/kubernetes/state_deployment"
 	_ "github.com/elastic/beats/metricbeat/module/kubernetes/state_node"
