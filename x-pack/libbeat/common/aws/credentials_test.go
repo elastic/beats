7ffb5770a83b5452100a87c0a4c11a946af26066
kaiyan-sheng <kaiyan.sheng@elastic.co>
2019-07-02 17:59:43 -0600

    Add documentation for aws credentials

--- x-pack/libbeat/common/aws/credentials_test.go
+++ x-pack/libbeat/common/aws/credentials_test.go
@@ -1,1 +1,27 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
 package aws
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGetAWSCredentials(t *testing.T) {
+	inputConfig := ConfigAWS{
+		AccessKeyID: "123",
+		SecretAccessKey: "abc",
+		SessionToken: "fake-session-token",
+	}
+	awsConfig, err :=GetAWSCredentials(inputConfig)
+	assert.NoError(t, err)
+
+	retrievedAWSConfig, err := awsConfig.Credentials.Retrieve()
+	assert.NoError(t, err)
+
+	assert.Equal(t, inputConfig.AccessKeyID, retrievedAWSConfig.AccessKeyID)
+	assert.Equal(t, inputConfig.SecretAccessKey, retrievedAWSConfig.SecretAccessKey)
+	assert.Equal(t, inputConfig.SessionToken, retrievedAWSConfig.SessionToken)
+}
