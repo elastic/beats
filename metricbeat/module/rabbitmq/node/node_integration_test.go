ba8680f7ff36e7ce2d34d8b40b9fba8aeed93e40
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2019-07-26 16:51:04 +0200

    Increase timeout in rabbitmq test

--- metricbeat/module/rabbitmq/node/node_integration_test.go
+++ metricbeat/module/rabbitmq/node/node_integration_test.go
@@ -30,7 +30,7 @@ import (
 )
 
 func TestData(t *testing.T) {
-	r := compose.EnsureUp(t, "rabbitmq")
+	r := compose.EnsureUpWithTimeout(t, 120, "rabbitmq")
 
 	ms := mbtest.NewReportingMetricSetV2Error(t, getConfig(r.Host()))
 	err := mbtest.WriteEventsReporterV2Error(ms, t, "")
@@ -40,8 +40,7 @@ func TestData(t *testing.T) {
 }
 
 func TestFetch(t *testing.T) {
-	t.Skip("Skipping test as it was not stable. Probably first event is empty.")
-	r := compose.EnsureUp(t, "rabbitmq")
+	r := compose.EnsureUpWithTimeout(t, 120, "rabbitmq")
 
 	reporter := &mbtest.CapturingReporterV2{}
 
