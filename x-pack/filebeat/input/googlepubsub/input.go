c8128cb026eecbb0f3833e12ba2729fffd1a8894
Steffen Siering <steffen.siering@elastic.co>
2019-07-26 03:06:49 +0200

    Refactor filebeat connector (#12997)
    The filebeat channel package wraps the publisher pipeline,
    providing support for preparing custom user settings. The drawback is,
    that inputs can not configure custom fields, processors, and ACK
    handlers. The later is required for implementing end-to-end ACK.
    
    This refactoring modifies the channel.Factory and channel.Connector
    types, such that input authors are able to make use of all the
    capabilities of the publisher pipeline.
    
    Inputs are free to configure the PublishMode. If not set, then filebeat
    defaults to guaranteed sends.
    
    The channel.Outleter should become more similar to beat.Client in the
    future, and will be eventually removed. A side effect of moving closer
    to beat.Client is the removal of the filebeat/util package.
    The complex shutdown handling in filebeat still prevents us from
    removing channel.Outlet. The complex shutdown handling is still required
    for handling the interactions between the logs input and the registrar
    on shutdown.

--- x-pack/filebeat/input/googlepubsub/input.go
+++ x-pack/filebeat/input/googlepubsub/input.go
@@ -19,7 +19,6 @@ import (
 
 	"github.com/elastic/beats/filebeat/channel"
 	"github.com/elastic/beats/filebeat/input"
-	"github.com/elastic/beats/filebeat/util"
 	"github.com/elastic/beats/libbeat/beat"
 	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/libbeat/common/atomic"
@@ -57,7 +56,7 @@ type pubsubInput struct {
 // a topic subscription.
 func NewInput(
 	cfg *common.Config,
-	outlet channel.Connector,
+	connector channel.Connector,
 	inputContext input.Context,
 ) (input.Input, error) {
 	// Extract and validate the input's configuration.
@@ -67,7 +66,11 @@ func NewInput(
 	}
 
 	// Build outlet for events.
-	out, err := outlet(cfg, inputContext.DynamicFields)
+	out, err := connector.ConnectWith(cfg, beat.ClientConfig{
+		Processing: beat.ProcessingConfig{
+			DynamicFields: inputContext.DynamicFields,
+		},
+	})
 	if err != nil {
 		return nil, err
 	}
@@ -191,27 +194,27 @@ func makeTopicID(project, topic string) string {
 	return prefix[:10]
 }
 
-func makeEvent(topicID string, msg *pubsub.Message) *util.Data {
+func makeEvent(topicID string, msg *pubsub.Message) beat.Event {
 	id := topicID + "-" + msg.ID
 
-	event := beat.Event{
+	fields := common.MapStr{
+		"event": common.MapStr{
+			"id":      id,
+			"created": time.Now().UTC(),
+		},
+		"message": string(msg.Data),
+	}
+	if len(msg.Attributes) > 0 {
+		fields.Put("labels", msg.Attributes)
+	}
+
+	return beat.Event{
 		Timestamp: msg.PublishTime.UTC(),
 		Meta: common.MapStr{
 			"id": id,
 		},
-		Fields: common.MapStr{
-			"event": common.MapStr{
-				"id":      id,
-				"created": time.Now().UTC(),
-			},
-			"message": string(msg.Data),
-		},
+		Fields: fields,
 	}
-	if len(msg.Attributes) > 0 {
-		event.Fields.Put("labels", msg.Attributes)
-	}
-
-	return &util.Data{Event: event}
 }
 
 func (in *pubsubInput) getOrCreateSubscription(ctx context.Context, client *pubsub.Client) (*pubsub.Subscription, error) {
