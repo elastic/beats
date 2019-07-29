68519cb59e1ee0984d5633bace1369db69fa38d5
Felix <felix.roessel@elastic.co>
2019-07-29 10:37:22 +0200

    IBM MQ Filebeat module (#8782)
    * Add IBM MQ Filebeat module

--- x-pack/filebeat/include/list.go
+++ x-pack/filebeat/include/list.go
@@ -14,6 +14,7 @@ import (
 	_ "github.com/elastic/beats/x-pack/filebeat/module/coredns"
 	_ "github.com/elastic/beats/x-pack/filebeat/module/envoyproxy"
 	_ "github.com/elastic/beats/x-pack/filebeat/module/googlecloud"
+	_ "github.com/elastic/beats/x-pack/filebeat/module/ibmmq"
 	_ "github.com/elastic/beats/x-pack/filebeat/module/iptables"
 	_ "github.com/elastic/beats/x-pack/filebeat/module/mssql"
 	_ "github.com/elastic/beats/x-pack/filebeat/module/netflow"
