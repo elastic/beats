62c74c9bb32c28ec66a0e17f2de4979bae2a6095
Ron cohen <ron@elastic.co>
2019-07-31 15:32:21 +0200

    move to x-pack

--- metricbeat/include/list.go
+++ metricbeat/include/list.go
@@ -160,8 +160,6 @@ import (
 	_ "github.com/elastic/beats/metricbeat/module/redis/info"
 	_ "github.com/elastic/beats/metricbeat/module/redis/key"
 	_ "github.com/elastic/beats/metricbeat/module/redis/keyspace"
-	_ "github.com/elastic/beats/metricbeat/module/statsd"
-	_ "github.com/elastic/beats/metricbeat/module/statsd/server"
 	_ "github.com/elastic/beats/metricbeat/module/system"
 	_ "github.com/elastic/beats/metricbeat/module/system/core"
 	_ "github.com/elastic/beats/metricbeat/module/system/cpu"
