e113a6c0503116a8bceceb6045deddb10a06cb2f
Michal Pristas <michal.pristas@gmail.com>
2019-08-01 13:49:50 +0200

    removed binaries

--- vendor/github.com/elastic/fleet/build/package/agent-linux-386.rpm/scripts/systemd-daemon-reload.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-systemctl daemon-reload 2> /dev/null
-exit 0
--- vendor/github.com/elastic/fleet/build/python-env/lib/python2.7/_abcoll.py
+++
@@ -1,1 +0,0 @@
-/usr/local/Cellar/python@2/2.7.15_3/Frameworks/Python.framework/Versions/2.7/lib/python2.7/_abcoll.py
