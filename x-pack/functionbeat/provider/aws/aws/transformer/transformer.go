5802d6967726d26c7c663f751e03405e7b5c9e73
Steven Basgall <sbasgall@gogoair.com>
2019-08-20 08:35:47 -0600

    fix formatting

--- x-pack/functionbeat/provider/aws/aws/transformer/transformer.go
+++ x-pack/functionbeat/provider/aws/aws/transformer/transformer.go
@@ -24,7 +24,7 @@ func CloudwatchLogs(request events.CloudwatchLogsData) []beat.Event {
 
 	for idx, logEvent := range request.LogEvents {
 		events[idx] = beat.Event{
-			Timestamp: time.Unix(0, logEvent.Timestamp * 1000000),
+			Timestamp: time.Unix(0, logEvent.Timestamp*1000000),
 			Fields: common.MapStr{
 				"message":              logEvent.Message,
 				"id":                   logEvent.ID,
