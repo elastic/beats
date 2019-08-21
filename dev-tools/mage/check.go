09d2ec50da7647e19dec7439f4d230a98f9a8ffc
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2019-08-21 14:17:18 +0200

    Add check for index patterns in dashboards

--- dev-tools/mage/check.go
+++ dev-tools/mage/check.go
@@ -245,8 +245,20 @@ type Dashboard struct {
 type dashboardObject struct {
 	Type       string `json:"type"`
 	Attributes struct {
-		Description string `json:"description"`
-		Title       string `json:"title"`
+		Description           string `json:"description"`
+		Title                 string `json:"title"`
+		KibanaSavedObjectMeta *struct {
+			SearchSourceJSON struct {
+				Index string `json:"index"`
+			} `json:"searchSourceJSON,omitempty"`
+		} `json:"kibanaSavedObjectMeta"`
+		VisState *struct {
+			Params struct {
+				Controls []struct {
+					IndexPattern string
+				} `json:"controls"`
+			} `json:"params"`
+		} `json:"visState,omitempty"`
 	} `json:"attributes"`
 }
 
@@ -270,6 +282,11 @@ func (d *Dashboard) CheckFormat(module string) []error {
 			if err := checkTitle(visualizationTitleRegexp, o.Attributes.Title, module); err != nil {
 				return errors.Wrapf(err, "expected title with format 'Some title [%s Module]', found '%s'", strings.Title(BeatName), o.Attributes.Title)
 			}
+		}
+
+		expectedIndexPattern := strings.ToLower(BeatName) + "-*"
+		if err := checkDashboardIndexPattern(expectedIndexPattern, o); err != nil {
+			return errors.Wrapf(err, "expected index pattern reference '%s'", expectedIndexPattern)
 		}
 		return nil
 	}
@@ -298,6 +315,22 @@ func checkTitle(re *regexp.Regexp, title string, module string) error {
 	foundModule := replacer.Replace(strings.ToLower(match[2]))
 	if expectedModule != foundModule {
 		return errors.Errorf("expected module name (%s), found '%s'", module, match[2])
+	}
+	return nil
+}
+
+func checkDashboardIndexPattern(expectedIndex string, o *dashboardObject) error {
+	if objectMeta := o.Attributes.KibanaSavedObjectMeta; objectMeta != nil {
+		if index := objectMeta.SearchSourceJSON.Index; index != "" && index != expectedIndex {
+			return errors.Errorf("unexpected index pattern reference found in object meta: %s", index)
+		}
+	}
+	if visState := o.Attributes.VisState; visState != nil {
+		for _, control := range visState.Params.Controls {
+			if index := control.IndexPattern; index != "" && index != expectedIndex {
+				return errors.Errorf("unexpected index pattern referende found in visualization state: %s", index)
+			}
+		}
 	}
 	return nil
 }
