bea67b734d3a2717d52326bf701d3c379356c3d2
Andrew Kroh <andrew.kroh@elastic.co>
2019-08-28 13:29:31 -0400

    Add psudeo ECS fields to fields.yml for decode_cef

--- x-pack/filebeat/processors/decode_cef/config.go
+++ x-pack/filebeat/processors/decode_cef/config.go
@@ -5,12 +5,12 @@
 package decode_cef
 
 type config struct {
-	Field         string `config:"field"        validate:"required"` // Source field containing the CEF message.
-	TargetField   string `config:"target_field" validate:"required"` // Target field for the CEF object.
-	IgnoreMissing bool   `config:"ignore_missing"`                   // Ignore missing source field.
-	IgnoreFailure bool   `config:"ignore_failure"`                   // Ignore failures when the source field does not contain a CEF message. Parse errors do not cause failures, but are added to error.message.
-	ID            string `config:"id"`                               // Instance ID for debugging purposes.
-	ECS           bool   `config:"ecs"`                              // Generate ECS fields.
+	Field         string `config:"field"`          // Source field containing the CEF message.
+	TargetField   string `config:"target_field"`   // Target field for the CEF object.
+	IgnoreMissing bool   `config:"ignore_missing"` // Ignore missing source field.
+	IgnoreFailure bool   `config:"ignore_failure"` // Ignore failures when the source field does not contain a CEF message. Parse errors do not cause failures, but are added to error.message.
+	ID            string `config:"id"`             // Instance ID for debugging purposes.
+	ECS           bool   `config:"ecs"`            // Generate ECS fields.
 }
 
 func defaultConfig() config {
