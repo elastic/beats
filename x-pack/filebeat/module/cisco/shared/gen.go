bca2c8e0758600a903163e38f7752d02649db5d5
Adrian Serrano <adrisr83@gmail.com>
2019-08-28 21:52:38 +0200

    Missing changes for Cisco FTD (#13286) (#13379)
    * Rename docs section to Field Mappings
    
    * Add explanatory comment to the generator

--- x-pack/filebeat/module/cisco/shared/gen.go
+++ x-pack/filebeat/module/cisco/shared/gen.go
@@ -4,5 +4,14 @@
 
 package shared
 
+// These generators will output the following files for the FTD:
+//  - ecs-mapping-processor.yml, an ingest pipeline processor that maps FTD
+//    security event fields to ECS.
+//  - ecs-mapping-docs.asciidoc, asciidoc tables to document those mappings.
+//
+// This files are not picked up by the FTD module. When generated, you need to
+// manually update the pipeline in ingest/asa-ftd-pipeline.yml
+// and the asciidoc tables into ../_meta/docs.asciidoc.
+
 //go:generate go run gen-ftd-ecs-mapping.go stringset.go -output ecs-mapping-processor.yml security-mappings.csv
 //go:generate go run gen-ecs-mapping-docs.go stringset.go -output ecs-mapping-docs.asciidoc security-mappings.csv
