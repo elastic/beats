316630a5586170d7268a57e1fac1cb1e01d676c8
Chris Mark <chrismarkou92@gmail.com>
2019-08-28 11:18:54 +0300

    Add container image in Kubernetes metadata (#13356)

--- libbeat/processors/add_kubernetes_metadata/indexers_test.go
+++ libbeat/processors/add_kubernetes_metadata/indexers_test.go
@@ -143,6 +143,8 @@ func TestContainerIndexer(t *testing.T) {
 	ns := "testns"
 	uid := "005f3b90-4b9d-12f8-acf0-31020a840133"
 	container := "container"
+	containerImage := "containerimage"
+	initContainerImage := "initcontainerimage"
 	initContainer := "initcontainer"
 	nodeName := "testnode"
 
@@ -184,6 +186,7 @@ func TestContainerIndexer(t *testing.T) {
 	pod.Status.ContainerStatuses = []kubernetes.PodContainerStatus{
 		{
 			Name:        container,
+			Image:       containerImage,
 			ContainerID: container1,
 		},
 	}
@@ -191,6 +194,7 @@ func TestContainerIndexer(t *testing.T) {
 	pod.Status.InitContainerStatuses = []kubernetes.PodContainerStatus{
 		{
 			Name:        initContainer,
+			Image:       initContainerImage,
 			ContainerID: container2,
 		},
 	}
@@ -206,12 +210,14 @@ func TestContainerIndexer(t *testing.T) {
 	assert.Equal(t, indices[1], "fghij")
 
 	expected["container"] = common.MapStr{
-		"name": container,
+		"name":  container,
+		"image": containerImage,
 	}
 	assert.Equal(t, expected.String(), indexers[0].Data.String())
 
 	expected["container"] = common.MapStr{
-		"name": initContainer,
+		"name":  initContainer,
+		"image": initContainerImage,
 	}
 	assert.Equal(t, expected.String(), indexers[1].Data.String())
 }
@@ -350,6 +356,7 @@ func TestIpPortIndexer(t *testing.T) {
 	ns := "testns"
 	uid := "005f3b90-4b9d-12f8-acf0-31020a840133"
 	container := "container"
+	containerImage := "containerimage"
 	ip := "1.2.3.4"
 	port := int32(80)
 	pod := kubernetes.Pod{
@@ -398,7 +405,8 @@ func TestIpPortIndexer(t *testing.T) {
 
 	pod.Spec.Containers = []v1.Container{
 		{
-			Name: container,
+			Name:  container,
+			Image: containerImage,
 			Ports: []v1.ContainerPort{
 				{
 					Name:          container,
@@ -422,6 +430,6 @@ func TestIpPortIndexer(t *testing.T) {
 	assert.Equal(t, fmt.Sprintf("%s:%d", ip, port), indices[1])
 
 	assert.Equal(t, expected.String(), indexers[0].Data.String())
-	expected["container"] = common.MapStr{"name": container}
+	expected["container"] = common.MapStr{"name": container, "image": containerImage}
 	assert.Equal(t, expected.String(), indexers[1].Data.String())
 }
