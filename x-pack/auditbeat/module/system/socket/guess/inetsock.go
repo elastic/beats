e4ae0d51f8b4b0c698931bbf63e4967430692866
Adrian Serrano <adrisr83@gmail.com>
2019-08-29 13:16:17 +0200

    [Auditbeat] New system/socket dataset using kprobes tracing events (#13058)
    This patch replaces the original `system/socket` dataset which used
    netlink to periodically poll the system for open sockets.
    
    In the new implementation, kprobe-based tracing is used to receive
    events of interest related to function calls and their arguments
    inside the kernel tcp/ip stack. From this stream of information the
    dataset constructs a model of all the network sockets in use and the
    processes running in the system at any given time.
    
    This allows the dataset to produce flow-like events with information
    like packets and bytes counters, originator process and user, with
    little impact to system performance and without missing short-lived
    sockets or processes.
    
    The dataset is in beta.

---
+++ x-pack/auditbeat/module/system/socket/guess/inetsock.go
@@ -0,0 +1,207 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+// +build linux,386 linux,amd64
+
+package guess
+
+import (
+	"bytes"
+	"encoding/binary"
+
+	"github.com/pkg/errors"
+	"golang.org/x/sys/unix"
+
+	"github.com/elastic/beats/libbeat/common"
+	"github.com/elastic/beats/x-pack/auditbeat/module/system/socket/helper"
+	"github.com/elastic/beats/x-pack/auditbeat/tracing"
+)
+
+// Guess the offsets within a struct inet_sock where the local and remote
+// addresses and ports are found.
+//
+// This is run multiple times to avoid birthdays.
+//
+// Most values appear multiple times within the struct, this is normal.
+//
+// Output:
+// INET_SOCK_LADDR : 572
+// INET_SOCK_LPORT : 582
+// INET_SOCK_RADDR : 576
+// INET_SOCK_RPORT : 580
+
+func init() {
+	if err := Registry.AddGuess(&guessInetSockIPv4{}); err != nil {
+		panic(err)
+	}
+}
+
+type guessInetSockIPv4 struct {
+	ctx            Context
+	local, remote  unix.SockaddrInet4
+	server, client int
+}
+
+// Name of this guess.
+func (g *guessInetSockIPv4) Name() string {
+	return "guess_inet_sock"
+}
+
+// Provides returns the list of variables discovered.
+func (g *guessInetSockIPv4) Provides() []string {
+	return []string{
+		"INET_SOCK_LADDR",
+		"INET_SOCK_LPORT",
+		"INET_SOCK_RADDR",
+		"INET_SOCK_RPORT",
+	}
+}
+
+// Requires declares the variables required to run this guess.
+func (g *guessInetSockIPv4) Requires() []string {
+	return []string{
+		"RET",
+	}
+}
+
+// Probes returns a kretprobe on inet_sock_accept that dumps the return
+// value (an inet_sock*).
+func (g *guessInetSockIPv4) Probes() ([]helper.ProbeDef, error) {
+	return []helper.ProbeDef{
+		{
+			Probe: tracing.Probe{
+				Type:      tracing.TypeKRetProbe,
+				Name:      "inet_sock_guess",
+				Address:   "inet_csk_accept",
+				Fetchargs: helper.MakeMemoryDump("{{.RET}}", 0, 2048),
+			},
+			Decoder: tracing.NewDumpDecoder,
+		},
+	}, nil
+}
+
+// Prepare creates a TCP/IP client and server bound to random loopback addresses
+// (127.x.x.x).
+func (g *guessInetSockIPv4) Prepare(ctx Context) (err error) {
+	g.ctx = ctx
+	g.local = unix.SockaddrInet4{
+		Port: 0,
+		Addr: randomLocalIP(),
+	}
+	g.remote = unix.SockaddrInet4{
+		Port: 0,
+		Addr: randomLocalIP(),
+	}
+	for bytes.Equal(g.local.Addr[:], g.remote.Addr[:]) {
+		g.remote.Addr = randomLocalIP()
+	}
+	if g.server, g.local, err = createSocket(g.local); err != nil {
+		return errors.Wrap(err, "error creating server")
+	}
+	if g.client, g.remote, err = createSocket(g.remote); err != nil {
+		return errors.Wrap(err, "error creating client")
+	}
+	if err = unix.Listen(g.server, 1); err != nil {
+		return errors.Wrap(err, "error in listen")
+	}
+	return nil
+}
+
+// Terminate cleans up the client and server.
+func (g *guessInetSockIPv4) Terminate() error {
+	unix.Close(g.client)
+	unix.Close(g.server)
+	return nil
+}
+
+// Trigger makes the client connect to the server, causing a inet_csk_accept
+// event.
+func (g *guessInetSockIPv4) Trigger() error {
+	if err := unix.Connect(g.client, &g.local); err != nil {
+		return err
+	}
+	fd, _, err := unix.Accept(g.server)
+	if err != nil {
+		return err
+	}
+	unix.Close(fd)
+	return nil
+}
+
+// Extract receives the dump of a struct inet_sock* and scans it for the
+// random local and remote IPs and ports. Will return lists of all the
+// offsets were each value was found.
+func (g *guessInetSockIPv4) Extract(ev interface{}) (common.MapStr, bool) {
+	data := ev.([]byte)
+
+	laddr := g.local.Addr[:]
+	lport := make([]byte, 2)
+	binary.BigEndian.PutUint16(lport, uint16(g.local.Port))
+	raddr := g.remote.Addr[:]
+	rport := make([]byte, 2)
+	binary.BigEndian.PutUint16(rport, uint16(g.remote.Port))
+	var laddrHits []int
+	var lportHits []int
+	var raddrHits []int
+	var rportHits []int
+
+	off := indexAligned(data, laddr, 0, 4)
+	for off != -1 {
+		laddrHits = append(laddrHits, off)
+		off = indexAligned(data, laddr, off+4, 4)
+	}
+
+	off = indexAligned(data, lport, 0, 2)
+	for off != -1 {
+		lportHits = append(lportHits, off)
+		off = indexAligned(data, lport, off+2, 2)
+	}
+
+	off = indexAligned(data, raddr, 0, 4)
+	for off != -1 {
+		raddrHits = append(raddrHits, off)
+		off = indexAligned(data, raddr, off+4, 4)
+	}
+
+	off = indexAligned(data, rport, 0, 2)
+	for off != -1 {
+		rportHits = append(rportHits, off)
+		off = indexAligned(data, rport, off+2, 2)
+	}
+
+	if len(laddrHits) == 0 || len(lportHits) == 0 || len(raddrHits) == 0 || len(rportHits) == 0 {
+		return nil, false
+	}
+
+	return common.MapStr{
+		"INET_SOCK_LADDR": laddrHits,
+		"INET_SOCK_LPORT": lportHits,
+		"INET_SOCK_RADDR": raddrHits,
+		"INET_SOCK_RPORT": rportHits,
+	}, true
+}
+
+// NumRepeats makes this guess to be repeated to avoid collisions.
+func (g *guessInetSockIPv4) NumRepeats() int {
+	return 4
+}
+
+// Reduce receives the output from multiple runs (list of offsets for each field)
+// and for every field it returns the first offset that appeared all the runs.
+func (g *guessInetSockIPv4) Reduce(results []common.MapStr) (result common.MapStr, err error) {
+	if result, err = consolidate(results); err != nil {
+		return nil, err
+	}
+
+	for _, key := range []string{
+		"INET_SOCK_LADDR", "INET_SOCK_LPORT",
+		"INET_SOCK_RADDR", "INET_SOCK_RPORT"} {
+		list, err := getListField(result, key)
+		if err != nil {
+			return nil, err
+		}
+		result[key] = list[0]
+	}
+	return result, nil
+}
---
+++ x-pack/auditbeat/tests/system/test_system_socket.py
@@ -0,0 +1,242 @@
+import platform
+import random
+import socket
+import unittest
+from auditbeat_xpack import *
+
+
+def is_root():
+    if 'geteuid' not in dir(os):
+        return False
+    euid = os.geteuid()
+    return euid == 0
+
+
+def is_version_below(version, target):
+    t = map(int, target.split('.'))
+    v = map(int, version.split('.'))
+    v += [0] * (len(t) - len(v))
+    for i in range(len(t)):
+        if v[i] != t[i]:
+            return v[i] < t[i]
+    return False
+
+
+# Require Linux greater or equal than 2.6.32 and 386/amd64 platform
+def is_platform_supported():
+    p = platform.platform().split('-')
+    if p[0] != 'Linux':
+        return False
+    if is_version_below(p[1], '2.6.32'):
+        return False
+    return {'i386', 'i686', 'x86_64', 'amd64'}.intersection(p)
+
+
+def enable_ipv6_loopback():
+    f = open('/proc/sys/net/ipv6/conf/lo/disable_ipv6', 'wb')
+    f.write('0\n')
+    f.close()
+
+
+@unittest.skipUnless(is_platform_supported(), "Requires Linux 2.6.32+ and 386/amd64 arch")
+@unittest.skipUnless(is_root(), "Requires root")
+class Test(AuditbeatXPackTest):
+
+    def test_tcp_ipv4(self):
+        """
+        test TCP IPv4 flow
+        """
+        self.with_runner(TCP4TestCase())
+
+    def test_udp_ipv4(self):
+        """
+        test UDP IPv4 flow
+        """
+        self.with_runner(UDP4TestCase())
+
+    def test_connected_udp_ipv4(self):
+        """
+        test connected UDP IPv4 flow
+        """
+        self.with_runner(ConnectedUDP4TestCase())
+
+    def with_runner(self, test):
+        enable_ipv6_loopback()
+        self.render_config_template(modules=[{
+            "name": "system",
+            "datasets": ["socket"],
+            "extras": {
+                "socket.flow_inactive_timeout": "2s",
+                "socket.flow_termination_timeout": "5s",
+                "socket.development_mode": "true",
+            }
+        }])
+        proc = self.start_beat()
+        try:
+            self.wait_until(lambda: self.log_contains('system/socket dataset is running.'),
+                            max_timeout=60)
+            self.execute(test)
+        except Exception:
+            proc.check_kill_and_wait()
+            raise
+        proc.check_kill_and_wait()
+
+    def noop(self):
+        pass
+
+    def execute(self, test):
+        cleanup = self.noop
+        if hasattr(test, 'cleanup'):
+            cleanup = test.cleanup
+
+        if hasattr(test, 'setup'):
+            test.setup()
+
+        try:
+            test.run()
+        except Exception:
+            cleanup()
+            raise
+
+        cleanup()
+
+        self.wait_until(lambda: self.output_lines() > 0)
+        self.wait_until(lambda: test.completed(map(lambda x: self.flatten_object(x, {}), self.read_output_json())))
+
+
+class TCP4TestCase:
+    def __init__(self):
+        pass
+
+    def run(self):
+        client, self.client_addr = socket_ipv4(socket.SOCK_STREAM, 0)
+        server, self.server_addr = socket_ipv4(socket.SOCK_STREAM, 0)
+        server.listen(8)
+        client.connect(self.server_addr)
+        acc, _ = server.accept()
+        acc.send('Hello there\n')
+        msg = client.recv(64)
+        client.send('"{}" what\n'.format(msg))
+        msg = acc.recv(64)
+        acc.close()
+        server.close()
+        client.close()
+
+    def completed(self, output):
+        return has_event(output, {
+            "source.ip": self.client_addr[0],
+            "source.port": self.client_addr[1],
+            "source.bytes": lambda x: x > 20,
+            "source.packets": lambda x: x > 4,
+            "client.ip": self.client_addr[0],
+            "client.port": self.client_addr[1],
+            "destination.ip": self.server_addr[0],
+            "destination.port": self.server_addr[1],
+            "destination.bytes": lambda x: x > 20,
+            "destination.packets": lambda x: x > 4,
+            "server.ip": self.server_addr[0],
+            "server.port": self.server_addr[1],
+            "network.transport": "tcp",
+            "network.type": "ipv4",
+            "network.direction": "outbound",
+            "group.id": str(os.getgid()),
+            "user.id": str(os.getuid()),
+            "process.pid": os.getpid(),
+        })
+
+
+class UDP4TestCase:
+    def __init__(self):
+        pass
+
+    def run(self):
+        client, self.client_addr = socket_ipv4(socket.SOCK_DGRAM, 0)
+        server, self.server_addr = socket_ipv4(socket.SOCK_DGRAM, 0)
+        for i in range(3):
+            client.sendto('Hello there {}'.format(i), self.server_addr)
+            msg, _ = server.recvfrom(64)
+        server.sendto('howdy', self.client_addr)
+        msg, _ = client.recvfrom(64)
+        client.close()
+        server.close()
+
+    def completed(self, output):
+        return has_event(output, {
+            "client.ip": self.client_addr[0],
+            "client.port": self.client_addr[1],
+            "destination.bytes": lambda x: x > 0,
+            "destination.ip": self.server_addr[0],
+            "destination.packets": 1,
+            "destination.port": self.server_addr[1],
+            "group.id": str(os.getgid()),
+            "network.direction": "outbound",
+            "network.packets": 4,
+            "network.transport": "udp",
+            "network.type": "ipv4",
+            "process.pid": os.getpid(),
+            "server.ip": self.server_addr[0],
+            "server.port": self.server_addr[1],
+            "source.bytes": lambda x: x >= 40,
+            "source.ip": self.client_addr[0],
+            "source.packets": 3,
+            "source.port": self.client_addr[1],
+            "user.id": str(os.getuid()),
+        })
+
+
+class ConnectedUDP4TestCase:
+    def __init__(self):
+        pass
+
+    def run(self):
+        client, self.client_addr = socket_ipv4(socket.SOCK_DGRAM, 0)
+        server, self.server_addr = socket_ipv4(socket.SOCK_DGRAM, 0)
+        client.connect(self.server_addr)
+        server.connect(self.client_addr)
+        for i in range(5):
+            server.send('Hello there {}'.format(i))
+            msg = client.recv(64)
+        client.send('howdy')
+        msg = server.recv(64)
+        client.send('bye')
+        msg = server.recv(64)
+        client.close()
+        server.close()
+
+    def completed(self, output):
+        return has_event(output, {
+            "server.ip": self.client_addr[0],
+            "server.port": self.client_addr[1],
+            "source.bytes": lambda x: x > 50,
+            "source.ip": self.server_addr[0],
+            "source.packets": 5,
+            "source.port": self.server_addr[1],
+            "group.id": str(os.getgid()),
+            "network.direction": "inbound",
+            "network.packets": 7,
+            "network.transport": "udp",
+            "network.type": "ipv4",
+            "process.pid": os.getpid(),
+            "client.ip": self.server_addr[0],
+            "client.port": self.server_addr[1],
+            "destination.bytes": lambda x: x >= 0,
+            "destination.ip": self.client_addr[0],
+            "destination.packets": 2,
+            "destination.port": self.client_addr[1],
+            "user.id": str(os.getuid()),
+        })
+
+
+def socket_ipv4(type, proto):
+    sock = socket.socket(socket.AF_INET, type, proto)
+    sock.bind((random_address_ipv4(), 0))
+    return sock, sock.getsockname()
+
+
+def random_address_ipv4():
+    return '127.{}.{}.{}'.format(random.randint(0, 255), random.randint(0, 255), random.randint(1, 254))
+
+
+def has_event(list, expected):
+    # True if any of the events in list contains all the keys in expected.
+    return any(all(k in obj and (obj[k] == v or callable(v) and v(obj[k])) for k, v in expected.items()) for obj in list)
