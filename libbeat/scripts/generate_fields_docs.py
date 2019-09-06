20a8e4a5446acba56263598060188fd32f00f475
DeDe Morton <dede.morton@elastic.co>
2019-09-06 10:44:21 -0700

    [6.7][DOCS] Backport asciidoctor build fixes (#13503)
    * [DOCS] Backport asciidoctor build fixes
    
    * Fix make docs (#13063) (#13159)

--- libbeat/scripts/generate_fields_docs.py
+++ libbeat/scripts/generate_fields_docs.py
@@ -11,7 +11,8 @@ def document_fields(output, section, sections, path):
         output.write("{}\n".format(section["prefix"]))
 
     # Intermediate level titles
-    if "description" in section and "prefix" not in section and "anchor" not in section:
+    if ("description" in section and "prefix" not in section and
+            "anchor" not in section):
         output.write("[float]\n")
 
     if "description" in section:
@@ -70,10 +71,12 @@ def document_field(output, field, field_path):
         if not field["enabled"]:
             output.write("{}\n\n".format("Object is not enabled."))
 
+    output.write("--\n\n")
+
     if "multi_fields" in field:
         for subfield in field["multi_fields"]:
-            document_field(output, subfield, field_path + "." + subfield["name"])
-    output.write("--\n\n")
+            document_field(output, subfield, field_path + "." +
+                           subfield["name"])
 
 
 def fields_to_asciidoc(input, output, beat):
@@ -129,8 +132,10 @@ if __name__ == "__main__":
         description="Generates the documentation for a Beat.")
     parser.add_argument("path", help="Path to the beat folder")
     parser.add_argument("beattitle", help="The beat title")
-    parser.add_argument("es_beats", help="The path to the general beats folder")
-    parser.add_argument("--output_path", default="", dest="output_path", help="Output path, if different from path")
+    parser.add_argument("es_beats",
+                        help="The path to the general beats folder")
+    parser.add_argument("--output_path", default="", dest="output_path",
+                        help="Output path, if different from path")
 
     args = parser.parse_args()
 
