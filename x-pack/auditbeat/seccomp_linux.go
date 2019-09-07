1696e6fe7e954657ad07b0ab02ef3d3d5a030fa9
Adrian Serrano <adrisr83@gmail.com>
2019-09-07 10:36:33 +0200

    [SIEM][Auditbeat] System/socket: mount tracefs/debugfs (#13526)
    Make the dataset try to mount tracefs/debugfs in their default paths if
    not mounted already.

--- x-pack/auditbeat/seccomp_linux.go
+++ x-pack/auditbeat/seccomp_linux.go
@@ -21,7 +21,7 @@ func init() {
 		}
 
 		// The system/socket dataset uses additional syscalls
-		if err := seccomp.ModifyDefaultPolicy(seccomp.AddSyscall, "perf_event_open", "eventfd2", "ppoll"); err != nil {
+		if err := seccomp.ModifyDefaultPolicy(seccomp.AddSyscall, "perf_event_open", "eventfd2", "ppoll", "mount", "umount2"); err != nil {
 			panic(err)
 		}
 	}
