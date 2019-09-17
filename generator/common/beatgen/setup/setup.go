a2985d3ac4ede07472b05d5553f68fc4f86d6f94
Alex Kristiansen <alex.kristiansen@elastic.co>
2019-09-17 14:11:41 -0700

    add lic headers

--- generator/common/beatgen/setup/setup.go
+++ generator/common/beatgen/setup/setup.go
@@ -1,3 +1,20 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package setup
 
 import (
@@ -57,6 +74,7 @@ func CopyVendor() error {
 		//Dev mode. Use CP.
 		fmt.Printf("CopyVendor running in dev mode, elastic/beats will be copied into the vendor directory with cp\n")
 		vendorPath = filepath.Join(vendorPath, "beats")
+
 		err = sh.Run("cp", "-R", beatPath, vendorPath)
 		if err != nil {
 			return errors.Wrap(err, "error copying vendor dir")
