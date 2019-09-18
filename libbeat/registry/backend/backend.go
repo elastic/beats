7c9146f5c77965625689a58bc495b19121d4ba04
urso <steffen.siering@elastic.co>
2019-09-18 03:55:59 +0200

    remove comment

--- libbeat/registry/backend/backend.go
+++ libbeat/registry/backend/backend.go
@@ -28,7 +28,6 @@ type Registry interface {
 }
 
 // Key represents the internal key.
-// type Key []byte
 type Key string
 
 // ValueDecoder is used to decode values into go structs or maps within a transaction.
