736a73b5a10f5c4d93ca62993c308128e1aaea6d
Mariana <mariana@elastic.co>
2019-09-19 12:17:12 +0200

    First commit

--- x-pack/filebeat/include/list.go
+++ x-pack/filebeat/include/list.go
@@ -12,6 +12,7 @@ import (
 	_ "github.com/elastic/beats/x-pack/filebeat/input/netflow"
 	_ "github.com/elastic/beats/x-pack/filebeat/input/s3"
 	_ "github.com/elastic/beats/x-pack/filebeat/module/aws"
+	_ "github.com/elastic/beats/x-pack/filebeat/module/azure"
 	_ "github.com/elastic/beats/x-pack/filebeat/module/cef"
 	_ "github.com/elastic/beats/x-pack/filebeat/module/cisco"
 	_ "github.com/elastic/beats/x-pack/filebeat/module/coredns"
