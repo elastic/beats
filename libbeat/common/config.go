8e913a702654b741c2cf12f8d19303c4724f0ec4
Andrew Cholakian <andrew@andrewvc.com>
2019-09-23 18:33:15 -0500

    [libbeat] Improve autodiscovery errors to print config (#13675)
    * [libbeat] Improve autodiscovery errors to print config
    
    When debugging autodiscovery config errors it is helpful to view the
    broken config. This patch makes the internal helper for printing configs
    in libbeat public and prints the config.
    
    As an example, in heartbeat, the output for a bad config is now:
    
    ```
    2019-09-13T11:05:13.805-0500    ERROR   [autodiscover]
    autodiscover/autodiscover.go:204        Auto discover config check
    failed for config '{
      "password": "xxxxx",
      "schedule": "@every 1s",
      "timeout": "1s",
      "type": "http",
      "urls": [
        "xxxxx"
    ],
    "username": "foo"
    }', won't start runner: job err parse 172.17.0.2:6379: first path
    segment in URL cannot contain colon
    ```
    
    for the config:
    
    ```
    heartbeat.autodiscover:
      providers:
        - type: docker
          templates:
            - condition:
                contains:
                  docker.container.image: redis
              config:
                - type: http
                  urls: ["${data.host}:${data.port}"]
                  schedule: "@every 1s"
                  timeout: 1s
                  username: foo
                  password: bar
    
    ```
    
    Fixes #13669
    
    * Add comment for DebugString

--- libbeat/common/config.go
+++ libbeat/common/config.go
@@ -265,7 +265,7 @@ func (c *Config) PrintDebugf(msg string, params ...interface{}) {
 		}
 	}
 
-	debugStr := configDebugString(c, filtered)
+	debugStr := DebugString(c, filtered)
 	if debugStr != "" {
 		configDebugf(selector, "%s\n%s", fmt.Sprintf(msg, params...), debugStr)
 	}
@@ -358,7 +358,9 @@ func (ns *ConfigNamespace) IsSet() bool {
 	return ns.config != nil
 }
 
-func configDebugString(c *Config, filterPrivate bool) string {
+// DebugString prints a human readable representation of the underlying config using
+// JSON formatting.
+func DebugString(c *Config, filterPrivate bool) string {
 	var bufs []string
 
 	if c.IsDict() {
