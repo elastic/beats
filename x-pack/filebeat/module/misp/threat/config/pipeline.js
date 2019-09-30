38e06986c656ca129c59a62a0bdaba4a34e0c0be
Ray Qiu <lei.qiu@elastic.co>
2019-09-30 16:07:47 -0700

    Update threat field to misp. Add additional fields for easier query and aggregation

--- x-pack/filebeat/module/misp/threat/config/pipeline.js
+++ x-pack/filebeat/module/misp/threat/config/pipeline.js
@@ -25,13 +25,13 @@ var threat = (function () {
     });
 
     var setThreatFeedField = function (evt) {
-        evt.Put("threat.threat_indicator.feed", "misp");
+        evt.Put("misp.threat_indicator.feed", "misp");
     };
 
     var convertFields = new processor.Convert({
         fields: [
-            { from: "json.Event.info", to: "threat.threat_indicator.description" },
-            { from: "json.Event.uuid", to: "threat.threat_indicator.id" },
+            { from: "json.Event.info", to: "misp.threat_indicator.description" },
+            { from: "json.Event.uuid", to: "misp.threat_indicator.id" },
         ],
         mode: "rename",
         ignore_missing: true,
@@ -46,44 +46,61 @@ var threat = (function () {
         switch (indicator_type) {
             case "ip-dst":
                 attackPattern = '[destination.ip = ' + '"' + v + '"' + ']';
+                evt.Put("destination.ip", v);
                 break;
             case "ip-src":
                 attackPattern = '[' + 'source.ip = ' + '"' + v + '"' + ']';
+                evt.Put("source.ip", v);
                 break;
             case "filename":
                 attackPattern = '[' + 'file.path = ' + '"' + v + '"' + ']';
+                evt.Put("file.path", v);
                 break;
             case "hostname":
                 attackPattern = '[' + 'source.domain = ' + '"' + v + '"' + ' OR destination.domain = ' + '"' + v + '"' + ']';
+                evt.Put("source.domain", v);
+                evt.Put("destination.domain", v);
                 break;
             case "sha512":
-                attackPattern = '[' + 'file.sha512 = ' + '"' + v + '"' + ']';
+                attackPattern = '[' + 'file.hash.sha512 = ' + '"' + v + '"' + ']';
+                evt.Put("file.hash.sha512", v);
                 break;
             case "sha256":
-                attackPattern = '[' + 'file.sha256 = ' + '"' + v + '"' + ']';
+                attackPattern = '[' + 'file.hash.sha256 = ' + '"' + v + '"' + ']';
+                evt.Put("file.hash.sha256", v);
                 break;
             case "md5":
-                attackPattern = '[' + 'file.md5 = ' + '"' + v + '"' + ']';
+                attackPattern = '[' + 'file.hash.md5 = ' + '"' + v + '"' + ']';
+                evt.Put("file.hash.md5", v);
                 break;
             case "sha1":
-                attackPattern = '[' + 'file.sha1 = ' + '"' + v + '"' + ']';
+                attackPattern = '[' + 'file.hash.sha1 = ' + '"' + v + '"' + ']';
+                evt.Put("file.hash.sha1", v);
                 break;
             case "link":
                 attackPattern = '[' + 'url.full = ' + '"' + v + '"' + ']';
+                evt.Put("url.full", v);
                 break;
             case "url":
                 attackPattern = '[' + 'url.full = ' + '"' + v + '"' + ']';
+                evt.Put("url.full", v);
                 break;
             case "domain":
-                attackPattern = '[' + 'dns.question.name = ' + '"' + v + '"' + ' OR zeek.dns.query = ' + '"' + v + '"' + ' OR url.domain = ' + '"' + v + '"' + ']';
+                attackPattern = '[' + 'dns.question.name = ' + '"' + v + '"' + ' OR url.domain = ' + '"' + v + '"' + ']';
+                evt.Put("dns.question.name", v);
+                evt.Put("url.domain", v)
                 break;
             case "domain|ip":
                 arr = v.split("|");
                 if (arr.length == 2) {
                     var domain = arr[0];
                     var ip = arr[1];
-                    attackPattern = '[' + '(' + 'dns.question.name = ' + '"' + domain + '"' + ' OR zeek.dns.query = ' + '"' + domain + '"' + ' OR url.domain = ' + '"' + domain + '"' + ')' +
+                    attackPattern = '[' + '(' + 'dns.question.name = ' + '"' + domain + '"' + ' OR url.domain = ' + '"' + domain + '"' + ')' +
                         ' AND ' + '(' + 'source.ip = ' + '"' + ip + '"' + ' OR destination.ip = ' + '"' + ip + '"' + ')' + ']';
+                    evt.Put("dns.question.name", domain);
+                    evt.Put("url.domain", domain)
+                    evt.Put("source.ip", ip);
+                    evt.Put("destination.ip", ip);
                 }
                 break;
             case "filename|sha256":
@@ -91,7 +108,9 @@ var threat = (function () {
                 if (arr.length == 2) {
                     filename = arr[0];
                     var sha256 = arr[1];
-                    attackPattern = '[' + 'file.sha256 = ' + '"' + sha256 + '"' + ' AND file.path = ' + '"' + filename + '"' + ']';
+                    attackPattern = '[' + 'file.hash.sha256 = ' + '"' + sha256 + '"' + ' AND file.path = ' + '"' + filename + '"' + ']';
+                    evt.Put("file.hash.sha256", sha256);
+                    evt.Put("file.path", filename);
                 }
                 break;
             case "filename|sha1":
@@ -99,7 +118,9 @@ var threat = (function () {
                 if (arr.length == 2) {
                     filename = arr[0];
                     var sha1 = arr[1];
-                    attackPattern = '[' + 'file.sha1 = ' + '"' + sha1 + '"' + ' AND file.path = ' + '"' + filename + '"' + ']';
+                    attackPattern = '[' + 'file.hash.sha1 = ' + '"' + sha1 + '"' + ' AND file.path = ' + '"' + filename + '"' + ']';
+                    evt.Put("file.hash.sha1", sha1);
+                    evt.Put("file.path", filename);
                 }
                 break;
             case "filename|md5":
@@ -107,14 +128,16 @@ var threat = (function () {
                 if (arr.length == 2) {
                     filename = arr[0];
                     var md5 = arr[1];
-                    attackPattern = '[' + 'file.md5 = ' + '"' + md5 + '"' + ' AND file.path = ' + '"' + filename + '"' + ']';
+                    attackPattern = '[' + 'file.hash.md5 = ' + '"' + md5 + '"' + ' AND file.path = ' + '"' + filename + '"' + ']';
+                    evt.Put("file.hash.md5", md5);
+                    evt.Put("file.path", filename);
                 }
                 break;
         }
         if (attackPattern == undefined) {
             evt.Put("error.message", 'Unsupported type: ' + indicator_type);
         }
-        evt.Put("threat.threat_indicator.attack_pattern", attackPattern);
+        evt.Put("misp.threat_indicator.attack_pattern", attackPattern);
         
     };
 
