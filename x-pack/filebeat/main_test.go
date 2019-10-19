eead4bf6b41fafd016d8d7b8b6bfc1050f0558ec
Lei Qiu <lei.qiu@elastic.co>
2019-10-19 03:53:38 +0000

    Update MISP README.md

--- x-pack/filebeat/main_test.go
+++ x-pack/filebeat/main_test.go
@@ -16,7 +16,7 @@ var systemTest *bool
 func init() {
 	systemTest = flag.Bool("systemTest", false, "Set to true when running system tests")
 	cmd.RootCmd.PersistentFlags().AddGoFlag(flag.CommandLine.Lookup("systemTest"))
-//	cmd.RootCmd.PersistentFlags().AddGoFlag(flag.CommandLine.Lookup("test.coverprofile"))
+	cmd.RootCmd.PersistentFlags().AddGoFlag(flag.CommandLine.Lookup("test.coverprofile"))
 }
 
 // Test started when the test binary is started. Only calls main.
