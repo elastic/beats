460b83c82fc614f50c064cefa498937f4899d8cb
urso <steffen.siering@elastic.co>
2019-11-03 00:24:22 +0100

    Add missing testing.Init calls

--- x-pack/heartbeat/main_test.go
+++ x-pack/heartbeat/main_test.go
@@ -15,6 +15,7 @@ import (
 var systemTest *bool
 
 func init() {
+	testing.Init()
 	systemTest = flag.Bool("systemTest", false, "Set to true when running system tests")
 	cmd.RootCmd.PersistentFlags().AddGoFlag(flag.CommandLine.Lookup("systemTest"))
 	cmd.RootCmd.PersistentFlags().AddGoFlag(flag.CommandLine.Lookup("test.coverprofile"))
