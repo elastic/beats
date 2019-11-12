91bae6e5541bdbb956bb463bafbc4b3a09a4a98d
Shaffer John <www.pig2@qq.com>
2019-11-12 16:57:44 +0800

    [Metricbeat] Add more TCP states for socket_summary (#14347)
    * add more tcp state
    
    (cherry picked from commit d84d85df72c1648d754ca2b276b1216986eb9b1e)

--- metricbeat/module/system/socket_summary/socket_summary_test.go
+++ metricbeat/module/system/socket_summary/socket_summary_test.go
@@ -82,6 +82,66 @@ func getMockedConns() []net.ConnectionStat {
 			Type:   syscall.SOCK_STREAM,
 			Status: "LISTEN",
 		},
+		net.ConnectionStat{
+			Family: syscall.AF_INET,
+			Type:   syscall.SOCK_STREAM,
+			Status: "SYN_SENT",
+		},
+		net.ConnectionStat{
+			Family: syscall.AF_INET,
+			Type:   syscall.SOCK_STREAM,
+			Status: "SYN_RECV",
+		},
+		net.ConnectionStat{
+			Family: syscall.AF_INET,
+			Type:   syscall.SOCK_STREAM,
+			Status: "SYN_RECV",
+		},
+		net.ConnectionStat{
+			Family: syscall.AF_INET,
+			Type:   syscall.SOCK_STREAM,
+			Status: "LAST_ACK",
+		},
+		net.ConnectionStat{
+			Family: syscall.AF_INET,
+			Type:   syscall.SOCK_STREAM,
+			Status: "FIN_WAIT1",
+		},
+		net.ConnectionStat{
+			Family: syscall.AF_INET,
+			Type:   syscall.SOCK_STREAM,
+			Status: "FIN_WAIT2",
+		},
+		net.ConnectionStat{
+			Family: syscall.AF_INET,
+			Type:   syscall.SOCK_STREAM,
+			Status: "FIN_WAIT2",
+		},
+		net.ConnectionStat{
+			Family: syscall.AF_INET,
+			Type:   syscall.SOCK_STREAM,
+			Status: "LAST_ACK",
+		},
+		net.ConnectionStat{
+			Family: syscall.AF_INET,
+			Type:   syscall.SOCK_STREAM,
+			Status: "CLOSING",
+		},
+		net.ConnectionStat{
+			Family: syscall.AF_INET,
+			Type:   syscall.SOCK_STREAM,
+			Status: "CLOSING",
+		},
+		net.ConnectionStat{
+			Family: syscall.AF_INET,
+			Type:   syscall.SOCK_STREAM,
+			Status: "FIN_WAIT2",
+		},
+		net.ConnectionStat{
+			Family: syscall.AF_INET,
+			Type:   syscall.SOCK_STREAM,
+			Status: "LAST_ACK",
+		},
 	}
 }
 
@@ -137,12 +197,54 @@ func TestCalculateConnStats(t *testing.T) {
 		t.Fail()
 	}
 
-	assert.Equal(t, allConns, 11)
+	tcpSynsents, err := metrics.GetValue("tcp.all.syn_sent")
+
+	if err != nil {
+		t.Fail()
+	}
+
+	tcpSynrecvs, err := metrics.GetValue("tcp.all.syn_recv")
+
+	if err != nil {
+		t.Fail()
+	}
+
+	tcpFinwait1s, err := metrics.GetValue("tcp.all.fin_wait1")
+
+	if err != nil {
+		t.Fail()
+	}
+
+	tcpFinwait2s, err := metrics.GetValue("tcp.all.fin_wait2")
+
+	if err != nil {
+		t.Fail()
+	}
+
+	tcpLastacks, err := metrics.GetValue("tcp.all.last_ack")
+
+	if err != nil {
+		t.Fail()
+	}
+
+	tcpClosings, err := metrics.GetValue("tcp.all.closing")
+
+	if err != nil {
+		t.Fail()
+	}
+
+	assert.Equal(t, allConns, 23)
 	assert.Equal(t, allListens, 2)
 	assert.Equal(t, udpConns, 2)
-	assert.Equal(t, tcpConns, 9)
+	assert.Equal(t, tcpConns, 21)
 	assert.Equal(t, tcpListens, 2)
 	assert.Equal(t, tcpEstablisheds, 2)
 	assert.Equal(t, tcpClosewaits, 3)
 	assert.Equal(t, tcpTimewaits, 1)
+	assert.Equal(t, tcpSynsents, 1)
+	assert.Equal(t, tcpSynrecvs, 2)
+	assert.Equal(t, tcpFinwait1s, 1)
+	assert.Equal(t, tcpFinwait2s, 3)
+	assert.Equal(t, tcpLastacks, 3)
+	assert.Equal(t, tcpClosings, 2)
 }
