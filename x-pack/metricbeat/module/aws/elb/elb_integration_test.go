1a3fcc449fc23f29e3c8109b28cf8c19c2ac9279
kaiyan-sheng <kaiyan.sheng@elastic.co>
2019-11-14 13:51:09 -0700

    Generate different data.json file for different load balancers

--- x-pack/metricbeat/module/aws/elb/elb_integration_test.go
+++ x-pack/metricbeat/module/aws/elb/elb_integration_test.go
@@ -7,18 +7,55 @@
 package elb
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/elastic/beats/libbeat/common"
 	mbtest "github.com/elastic/beats/metricbeat/mb/testing"
 	"github.com/elastic/beats/x-pack/metricbeat/module/aws/mtest"
 )
 
 func TestData(t *testing.T) {
+	namespaceIs := func(namespace string) func(e common.MapStr) bool {
+		return func(e common.MapStr) bool {
+			v, err := e.GetValue("aws.cloudwatch.namespace")
+			return err == nil && v == namespace
+		}
+	}
+
+	dataFiles := []struct {
+		namespace string
+		path      string
+	}{
+		{"AWS/ELB", "./_meta/data_elb.json"},
+		{"AWS/ApplicationELB", "./_meta/data_alb.json"},
+		{"AWS/NetworkELB", "./_meta/data_nlb.json"},
+	}
+
 	config, info := mtest.GetConfigForTest("elb", "300s")
 	if info != "" {
 		t.Skip("Skipping TestData: " + info)
 	}
 
-	metricSet := mbtest.NewFetcher(t, config)
-	metricSet.WriteEvents(t, "/")
+	for _, df := range dataFiles {
+		metricSet := mbtest.NewFetcher(t, config)
+
+		event, errs := metricSet.FetchEvents()
+		if errs != nil {
+			t.Skip("Skipping TestData: ", errs[0].Error())
+		}
+
+		if len(event) == 0 {
+			t.Skip("Skipping TestData: FetchEvents returns no event")
+		}
+
+		eventRootFields := event[0].RootFields
+		if eventRootFields == nil {
+			t.Skip("Skipping TestData: event root field is empty")
+		}
+
+		t.Run(fmt.Sprintf("namespace: %s", df.namespace), func(t *testing.T) {
+			metricSet.WriteEventsCond(t, df.path, namespaceIs(df.namespace))
+		})
+	}
 }
