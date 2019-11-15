998a85ff28abb0802a24327cb2aca4beb7adb135
Shaunak Kashyap <ycombinator@gmail.com>
2019-11-14 17:31:21 -0800

    WIP: unit tests

--- libbeat/processors/elasticsearch_id/elasticsearch_id_test.go
+++ libbeat/processors/elasticsearch_id/elasticsearch_id_test.go
@@ -1,1 +1,42 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package elasticsearch_id
+
+import (
+	"testing"
+
+	"github.com/elastic/beats/libbeat/common"
+
+	"github.com/elastic/beats/libbeat/beat"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDefaultTargetField(t *testing.T) {
+	p, err := New(common.MustNewConfigFrom(nil))
+	assert.NoError(t, err)
+
+	testEvent := &beat.Event{}
+
+	newEvent, err := p.Run(testEvent)
+	assert.NoError(t, err)
+
+	v, err := newEvent.GetValue("@metadata.id")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, v)
+}
