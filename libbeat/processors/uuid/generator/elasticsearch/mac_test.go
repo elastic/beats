b60e88c29fe23be3eef4852118189810abfcfee2
Shaunak Kashyap <ycombinator@gmail.com>
2019-11-15 10:00:34 -0800

    Fleshing out remaining mac unit tests

--- libbeat/processors/uuid/generator/elasticsearch/mac_test.go
+++ libbeat/processors/uuid/generator/elasticsearch/mac_test.go
@@ -18,6 +18,7 @@
 package elasticsearch
 
 import (
+	"net"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -67,5 +68,33 @@ func TestConstructDummyMulticastAddress(t *testing.T) {
 	assert.EqualValues(t, 0x01, firstOctet&0x01)
 }
 
-// TODO: test getSecureMungedMACAddress (length)
-// TODO: getMacAddress (not loopback, length)
+func TestSecureMungedMACAddress(t *testing.T) {
+	addr, err := getSecureMungedMACAddress()
+	assert.NoError(t, err)
+	assert.Len(t, addr, addrLen)
+}
+
+func TestGetMacAddress(t *testing.T) {
+	addr, err := getMacAddress()
+	assert.NoError(t, err)
+	assert.Len(t, addr, addrLen)
+
+	getLoopbackAddrs := func() [][]byte {
+		var loAddrs [][]byte
+
+		interfaces, err := net.Interfaces()
+		assert.NoError(t, err)
+
+		for _, i := range interfaces {
+			if i.Flags == net.FlagLoopback {
+				loAddrs = append(loAddrs, i.HardwareAddr)
+			}
+		}
+
+		return loAddrs
+	}
+
+	for _, loAddr := range getLoopbackAddrs() {
+		assert.NotEqual(t, loAddr, addr)
+	}
+}
