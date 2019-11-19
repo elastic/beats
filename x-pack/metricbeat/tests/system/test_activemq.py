4b4c25ccae7f81f04f768b7afefbd4b9259bb64f
Marcin Tojek <marcin.tojek@elastic.co>
2019-11-19 11:22:50 +0100

    System tests: topic metrics collected

--- x-pack/metricbeat/tests/system/test_activemq.py
+++ x-pack/metricbeat/tests/system/test_activemq.py
@@ -29,6 +29,27 @@ class ActiveMqTest(XPackTest):
 
         for evt in output:
             assert "name" in evt["activemq"]["broker"]
+            assert "pct" in evt["activemq"]["broker"]["memory"]["broker"]
+            assert "count" in evt["activemq"]["broker"]["producers"]
+            assert "count" in evt["activemq"]["broker"]["consumers"]
+
+    @unittest.skipUnless(metricbeat.INTEGRATION_TESTS, "integration test")
+    def test_topic_metrics_collected(self):
+        self.render_config_template(modules=[self.get_activemq_module_config("topic")])
+        proc = self.start_beat(home=self.beat_path)
+        self.wait_until(lambda: self.output_lines() > 0)
+        proc.check_kill_and_wait()
+        self.assert_no_logged_warnings()
+
+        output = self.read_output_json()
+        self.assertGreater(len(output), 0)
+
+        for evt in output:
+            # Advisory topic should be created by default.
+            assert "ActiveMQ.Advisory.MasterBroker" == evt["activemq"]["topic"]["name"]
+            assert "avg" in evt["activemq"]["topic"]["messages"]["size"]
+            assert "count" in evt["activemq"]["topic"]["producers"]
+            assert "count" in evt["activemq"]["topic"]["consumers"]
 
 
 class TestRelease5130(ActiveMqTest):
