65232f56553bf27387d93218f9521c3b466d6526
Shaunak Kashyap <ycombinator@gmail.com>
2019-11-20 22:34:20 -0800

    Skipping Metricbeat couchbase system tests (#14661)
    * Skipping Metricbeat couchbase system tests
    
    * Don't try to bring up Couchbase service

--- metricbeat/tests/system/test_couchbase.py
+++ metricbeat/tests/system/test_couchbase.py
@@ -4,9 +4,13 @@ import unittest
 from parameterized import parameterized
 
 
+@unittest.skip("See https://github.com/elastic/beats/issues/14660")
 class Test(metricbeat.BaseTest):
 
-    COMPOSE_SERVICES = ['couchbase']
+    # Commented out as part of skipping test. See https://github.com/elastic/beats/issues/14660.
+    # Otherwise, the tests are skipped but Docker Compose still tries to bring up
+    # the Couchbase service container and fails.
+    # COMPOSE_SERVICES = ['couchbase']
     FIELDS = ['couchbase']
 
     @parameterized.expand([
