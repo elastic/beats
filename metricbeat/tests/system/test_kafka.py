7afd014371a19c3cba7cbb87e2b64816eff61d0b
chrismark <chrismarkou92@gmail.com>
2019-11-22 15:10:15 +0200

    Fix permissions env
    Signed-off-by: chrismark <chrismarkou92@gmail.com>

--- metricbeat/tests/system/test_kafka.py
+++ metricbeat/tests/system/test_kafka.py
@@ -65,8 +65,8 @@ class KafkaTest(metricbeat.BaseTest):
             "period": "1s"
         }]
         self.render_config_template(modules=modules)
-        proc = self.start_beat()
-        self.wait_until(lambda: self.output_lines() > 0, max_timeout=400)
+        proc = self.start_beat(home=self.beat_path)
+        self.wait_until(lambda: self.output_lines() > 0, max_timeout=60)
         proc.check_kill_and_wait()
 
         output = self.read_output_json()
