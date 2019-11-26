2dd11001754d246399baddae72355ca7024ffdac
Pablo Mercado <odacremolbap@gmail.com>
2019-11-26 21:12:31 +0100

    add ingress fields

--- metricbeat/module/kubernetes/state_service/state_service.go
+++ metricbeat/module/kubernetes/state_service/state_service.go
@@ -53,7 +53,7 @@ func NewServiceMetricSet(base mb.BaseMetricSet) (mb.MetricSet, error) {
 			Metrics: map[string]p.MetricMap{
 				"kube_service_info":                         p.InfoMetric(),
 				"kube_service_labels":                       p.ExtendedInfoMetric(p.Configuration{StoreNonMappedLabels: true, NonMappedLabelsPlacement: "labels"}),
-				"kube_service_created":                      p.Metric("service.started", p.OpUnixTimestampValue()),
+				"kube_service_created":                      p.Metric("created", p.OpUnixTimestampValue()),
 				"kube_service_spec_type":                    p.InfoMetric(),
 				"kube_service_spec_external_ip":             p.InfoMetric(),
 				"kube_service_status_load_balancer_ingress": p.InfoMetric(),
