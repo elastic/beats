448410cd5d0bb0c5e7bad2c7d8b25531f0a67896
Mike Place <mike.place@elastic.co>
2019-11-29 14:06:24 +0100

    Formatting update

--- libbeat/monitoring/monitoring.go
+++ libbeat/monitoring/monitoring.go
@@ -44,7 +44,7 @@ const (
 
 var (
 	warnXPackMonitoringDeprecatedConfig = "xpack.monitoring.* settings are deprecated. Use Metricbeat with the beat-xpack module to collect and ship monitoring data."
-	warnMonitoringDeprecatedConfig = "monitoring.* settings are deprecated. Use Metricbeat with the beat-xpack module to collect and ship monitoring data."
+	warnMonitoringDeprecatedConfig      = "monitoring.* settings are deprecated. Use Metricbeat with the beat-xpack module to collect and ship monitoring data."
 )
 
 // Default is the global default metrics registry provided by the monitoring package.
