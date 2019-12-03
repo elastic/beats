d6ce7e9c9ee35f059ab233701ea511679b37a602
chrismark <chrismarkou92@gmail.com>
2019-12-03 13:25:21 +0200

    add system tests
    Signed-off-by: chrismark <chrismarkou92@gmail.com>

---
+++ x-pack/metricbeat/tests/system/test_stan.py
@@ -0,0 +1,23 @@
+import os
+import metricbeat
+import unittest
+
+STAN_FIELDS = metricbeat.COMMON_FIELDS + ["stan"]
+
+
+class TestNats(metricbeat.BaseTest):
+
+    COMPOSE_SERVICES = ['stan']
+
+
+    @parameterized.expand([
+        "stats",
+        "channels",
+        "subscriptions",
+    ])
+    @unittest.skipUnless(metricbeat.INTEGRATION_TESTS, "integration test")
+    def test_metricset(self, metricset):
+        """
+        etcd metricset tests
+        """
+        self.check_metricset("stan", metricset, self.get_hosts(), ['stan.' + metricset])
