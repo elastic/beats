60509835b14f3ec4cdf68367a837814cecbc2c1a
chrismark <chrismarkou92@gmail.com>
2019-12-03 09:44:26 +0200

    Move STAN to x-pack
    Signed-off-by: chrismark <chrismarkou92@gmail.com>

--- x-pack/metricbeat/module/stan/subscriptions/subscriptions.go
+++ x-pack/metricbeat/module/stan/subscriptions/subscriptions.go
@@ -1,21 +1,8 @@
-// Licensed to Elasticsearch B.V. under one or more contributor
-// license agreements. See the NOTICE file distributed with
-// this work for additional information regarding copyright
-// ownership. Elasticsearch B.V. licenses this file to you under
-// the Apache License, Version 2.0 (the "License"); you may
-// not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
 
-package streaming
+package subscriptions
 
 import (
 	"github.com/pkg/errors"
@@ -24,23 +11,27 @@ import (
 	"github.com/elastic/beats/metricbeat/helper"
 	"github.com/elastic/beats/metricbeat/mb"
 	"github.com/elastic/beats/metricbeat/mb/parse"
+
+	"github.com/elastic/beats/x-pack/metricbeat/module/stan"
 )
 
 const (
 	defaultScheme = "http"
-	defaultPath   = "/streaming/serverz"
+	defaultPath   = "/streaming/channelsz"
+	queryParams   = "subs=1"
 )
 
 var (
 	hostParser = parse.URLHostParserBuilder{
 		DefaultScheme: defaultScheme,
 		DefaultPath:   defaultPath,
-		PathConfigKey: "stats.metrics_path",
+		PathConfigKey: "subscriptions.metrics_path",
+		QueryParams:   queryParams,
 	}.Build()
 )
 
 func init() {
-	mb.Registry.MustAddMetricSet("stan", "stats", New,
+	mb.Registry.MustAddMetricSet("stan", "subscriptions", New,
 		mb.WithHostParser(hostParser),
 		mb.DefaultMetricSet(),
 	)
@@ -78,13 +69,13 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 // Fetch methods implements the data gathering and data conversion to the right
 // format. It publishes the event which is then forwarded to the output. In case
 // of an error set the Error field of mb.Event or simply call report.Error().
-func (m *MetricSet) Fetch(r mb.ReporterV2) error {
+func (m *MetricSet) Fetch(r mb.ReporterV2) (err error) {
+	_ = stan.AssetStan()
 	content, err := m.http.FetchContent()
 	if err != nil {
 		return errors.Wrap(err, "error in fetch")
 	}
-	err = eventMapping(content, r)
-	if err != nil {
+	if err = eventsMapping(content, r); err != nil {
 		return errors.Wrap(err, "error in mapping")
 	}
 
