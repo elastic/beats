cf2bdf9987e74d224a45774d1dc02bd13d7bd23f
chrismark <chrismarkou92@gmail.com>
2019-12-18 18:06:18 +0200

    fix health condition
    Signed-off-by: chrismark <chrismarkou92@gmail.com>

--- x-pack/metricbeat/module/stan/_meta/healthcheck.sh
+++ x-pack/metricbeat/module/stan/_meta/healthcheck.sh
@@ -2,8 +2,8 @@
 
 res=$(wget -q -O - http://0.0.0.0:8222/streaming/channelsz | sed -n 's/"count": \([[:digit:]]\+\),/\1/p')
 
-if [[ $res = 0 ]]; then
-	exit 1
+if [[ $res -gt 0 ]]; then
+	exit 0
 fi
 
-exit 0
+exit 1
