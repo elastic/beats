a437437fc0ba123e83c27720428e7b0555e27204
chrismark <chrismarkou92@gmail.com>
2019-12-18 16:56:34 +0200

    minor improvement on healthcheck
    Signed-off-by: chrismark <chrismarkou92@gmail.com>

--- x-pack/metricbeat/module/stan/_meta/healthcheck.sh
+++ x-pack/metricbeat/module/stan/_meta/healthcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-res=$(wget -q -O - http://0.0.0.0:8222/streaming/channelsz | egrep -e '"count": (\d)+,' | cut -d":" -f2 |cut -d"," -f1 | xargs)
+res=$(wget -q -O - http://0.0.0.0:8222/streaming/channelsz | sed -n 's/"count": \([[:digit:]]\+\),/\1/p')
 
 if [[ $res = 0 ]]; then
 	exit 1
