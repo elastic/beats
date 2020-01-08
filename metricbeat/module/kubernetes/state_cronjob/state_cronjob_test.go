c06109e0a3366e3b1ae820f3454d491c7d610a58
Pablo Mercado <odacremolbap@gmail.com>
2020-01-08 18:26:48 +0100

    use ksm 1.3 and 1.8 only

--- metricbeat/module/kubernetes/state_cronjob/state_cronjob_test.go
+++ metricbeat/module/kubernetes/state_cronjob/state_cronjob_test.go
@@ -29,12 +29,12 @@ func TestEventMapping(t *testing.T) {
 	ptest.TestMetricSet(t, "kubernetes", "state_cronjob",
 		ptest.TestCases{
 			{
-				MetricsFile:  "./_meta/test/kube-state-metrics.1.7",
-				ExpectedFile: "./_meta/test/kube-state-metrics.1.7.expected",
+				MetricsFile:  "../_meta/test/ksm.v1.3.0",
+				ExpectedFile: "./_meta/test/ksm.v1.3.0.expected",
 			},
 			{
-				MetricsFile:  "../_meta/test/kube-state-metrics.v1.8.0",
-				ExpectedFile: "./_meta/test/kube-state-metrics.v1.8.0.expected",
+				MetricsFile:  "../_meta/test/ksm.v1.8.0",
+				ExpectedFile: "./_meta/test/ksm.v1.8.0.expected",
 			},
 		},
 	)
