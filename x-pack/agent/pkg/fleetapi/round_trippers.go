e1d96128d13e59709e3118a11739e686ba7d1765
Pier-Hugues Pellerin <phpellerin@gmail.com>
2020-01-10 10:34:09 -0500

    Fleet auth round tripper nil ptr
    Fix an nil ptr issue when the Kibana host is not reacheable.

--- x-pack/agent/pkg/fleetapi/round_trippers.go
+++ x-pack/agent/pkg/fleetapi/round_trippers.go
@@ -48,10 +48,15 @@ func (r *FleetAuthRoundTripper) RoundTrip(req *http.Request) (*http.Response, er
 
 	req.Header.Set(key, prefix+r.apiKey)
 	resp, err := r.rt.RoundTrip(req)
+	if err != nil {
+		return resp, err
+	}
+
 	if resp.StatusCode == http.StatusUnauthorized {
 		defer resp.Body.Close()
 		return resp, ErrInvalidAPIKey
 	}
+
 	return resp, err
 }
 
