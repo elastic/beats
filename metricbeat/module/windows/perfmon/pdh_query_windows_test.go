efdf3ef50610ae11f95b4abc63b8db8744d880d1
Mariana Dima <mariana@elastic.co>
2020-01-13 12:00:24 +0100

    Cherry-pick #15305 to 7.x: Fix ocasional panic in UTF16ToStringArray when processing unicode strings (#15460)
    * Fix ocasional panic in UTF16ToStringArray when processing unicode strings (#15305)
    
    * fix
    
    * add test
    
    * Modify tests
    
    (cherry picked from commit b366e9e6e0892f66934a8ae94d61bf98da14fca6)
    
    * mage fmt
    
    * Remove spaces

--- metricbeat/module/windows/perfmon/pdh_query_windows_test.go
+++ metricbeat/module/windows/perfmon/pdh_query_windows_test.go
@@ -18,6 +18,7 @@
 package perfmon
 
 import (
+	"syscall"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -112,5 +113,25 @@ func TestObjectNameRegexp(t *testing.T) {
 		if assert.Len(t, matches, 2, "regular expression did not return any matches") {
 			assert.Equal(t, matches[1], "Web Service Cache")
 		}
+	}
+}
+
+func TestUTF16ToStringArray(t *testing.T) {
+	var array = []string{
+		"\\\\DESKTOP-RFOOE09\\Physikalischer Datenträger(0 C:)\\Schreibvorgänge/s",
+		"\\\\DESKTOP-RFOOE09\\Physikalischer Datenträger(_Total)\\Schreibvorgänge/s",
+		"",
+	}
+	var unicode []uint16
+	for _, i := range array {
+		uni, err := syscall.UTF16FromString(i)
+		assert.NoError(t, err)
+		unicode = append(unicode, uni...)
+	}
+	response := UTF16ToStringArray(unicode)
+	assert.NotNil(t, response)
+	assert.Equal(t, len(response), 2)
+	for _, res := range response {
+		assert.Contains(t, array, res)
 	}
 }
