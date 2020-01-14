c8fc8169e0cc21851586020c2a2763c0de328355
sayden <mariocaster@gmail.com>
2020-01-14 13:36:37 +0100

    Add PubSub code to Stackdriver metricset

---
+++ x-pack/metricbeat/module/googlecloud/stackdriver/metrics_requester_test.go
@@ -0,0 +1,21 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package stackdriver
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetFilterForMetric(t *testing.T) {
+	r := stackdriverMetricsRequester{config: config{Zone: "us-central1-a"}}
+
+	s := r.getFilterForMetric("compute.googleapis.com/firewall/dropped_bytes_count")
+	assert.Equal(t, `metric.type="compute.googleapis.com/firewall/dropped_bytes_count" AND resource.labels.zone = "us-central1-a"`, s)
+
+	s = r.getFilterForMetric("pubsub.googleapis.com/subscription/ack_message_count")
+	assert.Equal(t, `metric.type="pubsub.googleapis.com/subscription/ack_message_count"`, s)
+}
