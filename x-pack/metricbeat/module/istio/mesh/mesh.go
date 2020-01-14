f912cfbf390884234b04f4d13b990f6844217209
chrismark <chrismarkou92@gmail.com>
2020-01-14 16:45:18 +0200

    update
    Signed-off-by: chrismark <chrismarkou92@gmail.com>

--- x-pack/metricbeat/module/istio/mesh/mesh.go
+++ x-pack/metricbeat/module/istio/mesh/mesh.go
@@ -19,32 +19,31 @@ var (
 	hostParser = parse.URLHostParserBuilder{
 		DefaultScheme: defaultScheme,
 		DefaultPath:   defaultPath,
-		QueryParams: mb.QueryParams{"match[]": "{__name__=~\"istio.*\"}"}.String(),
+		QueryParams:   mb.QueryParams{"match[]": "{__name__=~\"istio.*\"}"}.String(),
 	}.Build()
 )
 
 var mapping = &prometheus.MetricsMapping{
 	Metrics: map[string]prometheus.MetricMap{
-		"istio_requests_total": prometheus.Metric("requests"),
+		"istio_requests_total":                  prometheus.Metric("requests"),
 		"istio_request_duration_seconds_bucket": prometheus.Metric("request.duration.bucket.count"),
 	},
 
 	Labels: map[string]prometheus.LabelMap{
-		"instance": prometheus.KeyLabel("instance"),
-		"job":      prometheus.KeyLabel("job"),
-		"connection_security_policy":      prometheus.KeyLabel("connection_security_policy"),
-		"destination_app":      prometheus.KeyLabel("destination.app"),
-		"destination_service":      prometheus.KeyLabel("destination.service.path"),
+		"instance":                      prometheus.KeyLabel("instance"),
+		"job":                           prometheus.KeyLabel("job"),
+		"connection_security_policy":    prometheus.KeyLabel("connection_security_policy"),
+		"destination_app":               prometheus.KeyLabel("destination.app"),
+		"destination_service":           prometheus.KeyLabel("destination.service.path"),
 		"destination_service_name":      prometheus.KeyLabel("destination.service.name"),
-		"destination_service_namespace":      prometheus.KeyLabel("destination.service.namespace"),
-		"destination_version":      prometheus.KeyLabel("destination.version"),
-		"le":      prometheus.KeyLabel("le"),
+		"destination_service_namespace": prometheus.KeyLabel("destination.service.namespace"),
+		"destination_version":           prometheus.KeyLabel("destination.version"),
+		"le":                            prometheus.KeyLabel("le"),
 
-		"destination_workload_namespace":      prometheus.KeyLabel("destination.workload_namespace"),
-		"reporter":      prometheus.KeyLabel("reporter"),
-		"request_protocol":      prometheus.KeyLabel("request_protocol"),
-		"response_code":      prometheus.KeyLabel("response_code"),
-
+		"destination_workload_namespace": prometheus.KeyLabel("destination.workload_namespace"),
+		"reporter":                       prometheus.KeyLabel("reporter"),
+		"request_protocol":               prometheus.KeyLabel("request_protocol"),
+		"response_code":                  prometheus.KeyLabel("response_code"),
 	},
 }
 
