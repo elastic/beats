299a302db4c83e0a3a735c49469299f938f8473b
urso <steffen.siering@elastic.co>
2020-01-21 13:50:40 +0100

    Split global and normal logger tests
    Move logp global functions to global.go and selective.go, plus split up
    tests. The change also adds the `nologpglobal` build tag to global.go and global_test.go.
    By default all global functions are available. When compiling or
    running a package its test code with `-tags=nologpglobal`, then global
    logp functions are not available.

--- libbeat/logp/global_test.go
+++ libbeat/logp/global_test.go
@@ -15,52 +15,26 @@
 // specific language governing permissions and limitations
 // under the License.
 
+//+build !nologpglobal
+
 package logp
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap"
 )
 
-func TestLogger(t *testing.T) {
-	exerciseLogger := func() {
-		// Info("unnamed global logger")
-
-		log := NewLogger("example")
-		log.Info("some message")
-		log.Infof("some message with parameter x=%v, y=%v", 1, 2)
-		log.Infow("some message", "x", 1, "y", 2)
-		log.Infow("some message", Int("x", 1))
-		log.Infow("some message with namespaced args", Namespace("metrics"), "x", 1, "y", 1)
-		log.Infow("", "empty_message", true)
-
-		// Add context.
-		log.With("x", 1, "y", 2).Warn("logger with context")
-
-		someStruct := struct {
-			X int `json:"x"`
-			Y int `json:"y"`
-		}{1, 2}
-		log.Infow("some message with struct value", "metrics", someStruct)
-	}
-
-	TestingSetup()
-	exerciseLogger()
-	TestingSetup(AsJSON())
-	exerciseLogger()
-}
-
-func TestLoggerLevel(t *testing.T) {
+func TestGlobalLoggerLevel(t *testing.T) {
 	if err := DevelopmentSetup(ToObserverOutput()); err != nil {
 		t.Fatal(err)
 	}
 
 	const loggerName = "tester"
-	logger := NewLogger(loggerName)
 
-	logger.Debug("debug")
+	Debug(loggerName, "debug")
 	logs := ObserverLogs().TakeAll()
 	if assert.Len(t, logs, 1) {
 		assert.Equal(t, zap.DebugLevel, logs[0].Level)
@@ -68,53 +42,70 @@ func TestLoggerLevel(t *testing.T) {
 		assert.Equal(t, "debug", logs[0].Message)
 	}
 
-	logger.Info("info")
+	Info("info")
 	logs = ObserverLogs().TakeAll()
 	if assert.Len(t, logs, 1) {
 		assert.Equal(t, zap.InfoLevel, logs[0].Level)
-		assert.Equal(t, loggerName, logs[0].LoggerName)
+		assert.Equal(t, "", logs[0].LoggerName)
 		assert.Equal(t, "info", logs[0].Message)
 	}
 
-	logger.Warn("warn")
+	Warn("warning")
 	logs = ObserverLogs().TakeAll()
 	if assert.Len(t, logs, 1) {
 		assert.Equal(t, zap.WarnLevel, logs[0].Level)
-		assert.Equal(t, loggerName, logs[0].LoggerName)
-		assert.Equal(t, "warn", logs[0].Message)
+		assert.Equal(t, "", logs[0].LoggerName)
+		assert.Equal(t, "warning", logs[0].Message)
 	}
 
-	logger.Error("error")
+	Err("error")
 	logs = ObserverLogs().TakeAll()
 	if assert.Len(t, logs, 1) {
 		assert.Equal(t, zap.ErrorLevel, logs[0].Level)
-		assert.Equal(t, loggerName, logs[0].LoggerName)
+		assert.Equal(t, "", logs[0].LoggerName)
 		assert.Equal(t, "error", logs[0].Message)
+	}
+
+	Critical("critical")
+	logs = ObserverLogs().TakeAll()
+	if assert.Len(t, logs, 1) {
+		assert.Equal(t, zap.ErrorLevel, logs[0].Level)
+		assert.Equal(t, "", logs[0].LoggerName)
+		assert.Equal(t, "critical", logs[0].Message)
 	}
 }
 
-func TestL(t *testing.T) {
-	if err := DevelopmentSetup(ToObserverOutput()); err != nil {
-		t.Fatal(err)
-	}
+func TestRecover(t *testing.T) {
+	const recoveryExplanation = "Something went wrong"
+	const cause = "unexpected condition"
 
-	L().Infow("infow", "rate", 2)
-	logs := ObserverLogs().TakeAll()
-	if assert.Len(t, logs, 1) {
-		log := logs[0]
-		assert.Equal(t, zap.InfoLevel, log.Level)
-		assert.Equal(t, "", log.LoggerName)
-		assert.Equal(t, "infow", log.Message)
-		assert.Contains(t, log.ContextMap(), "rate")
-	}
+	DevelopmentSetup(ToObserverOutput())
 
-	const loggerName = "tester"
-	L().Named(loggerName).Warnf("warning %d", 1)
-	logs = ObserverLogs().TakeAll()
-	if assert.Len(t, logs, 1) {
-		log := logs[0]
-		assert.Equal(t, zap.WarnLevel, log.Level)
-		assert.Equal(t, loggerName, log.LoggerName)
-		assert.Equal(t, "warning 1", log.Message)
-	}
+	defer func() {
+		logs := ObserverLogs().TakeAll()
+		if assert.Len(t, logs, 1) {
+			log := logs[0]
+			assert.Equal(t, zap.ErrorLevel, log.Level)
+			assert.Equal(t, "logp/global_test.go",
+				strings.Split(log.Caller.TrimmedPath(), ":")[0])
+			assert.Contains(t, log.Message, recoveryExplanation+
+				". Recovering, but please report this.")
+			assert.Contains(t, log.ContextMap(), "panic")
+		}
+	}()
+
+	defer Recover(recoveryExplanation)
+	panic(cause)
+}
+
+func TestIsDebug(t *testing.T) {
+	DevelopmentSetup()
+	assert.True(t, IsDebug("all"))
+
+	DevelopmentSetup(WithSelectors("*"))
+	assert.True(t, IsDebug("all"))
+
+	DevelopmentSetup(WithSelectors("only_this"))
+	assert.False(t, IsDebug("all"))
+	assert.True(t, IsDebug("only_this"))
 }
