005ac8f9c2d2ad07dfa9d964e7dc175b587db95e
Mariana Dima <mariana@elastic.co>
2020-01-21 12:27:04 +0100

    Move pdh query to shared location in order for new modules/metricsets to reuse (#15503)
    * Move pdh query to shared location
    
    * Update changelog
    
    * Fix make update
    
    * mage fmt
    
    * fix changelog
    
    (cherry picked from commit 2657f713ed3d1f1c169e19177a4456707d1188bd)

--- metricbeat/helper/windows/pdh/doc.go
+++ metricbeat/helper/windows/pdh/doc.go
@@ -15,7 +15,10 @@
 // specific language governing permissions and limitations
 // under the License.
 
-/*
-Package windows is a Metricbeat module that contains MetricSets.
-*/
-package windows
+package pdh
+
+//go:generate go run mkpdh_defs.go
+//go:generate go run ../run.go -cmd "go tool cgo -godefs defs_pdh_windows.go" -goarch amd64 -output defs_pdh_windows_amd64.go
+//go:generate go run ../run.go -cmd "go tool cgo -godefs defs_pdh_windows.go" -goarch 386 -output defs_pdh_windows_386.go
+//go:generate go run $GOROOT/src/syscall/mksyscall_windows.go -output zpdh_windows.go pdh_windows.go
+//go:generate gofmt -w defs_pdh_windows_amd64.go defs_pdh_windows_386.go zpdh_windows.go
--- metricbeat/helper/windows/pdh/pdh_windows_test.go
+++ metricbeat/helper/windows/pdh/pdh_windows_test.go
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-package perfmon
+package pdh
 
 import (
 	"syscall"
