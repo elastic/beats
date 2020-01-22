dfc8c21c29d4b86f575ae4466ac35e81c9701a15
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2020-01-22 19:03:25 +0100

    Update .ci/apm-beats-update.groovy

--- .ci/apm-beats-update.groovy
+++ .ci/apm-beats-update.groovy
@@ -85,7 +85,7 @@ pipeline {
             sh(label: 'Update Beats script', script: """
               export BEATS_VERSION=${env.GIT_BASE_COMMIT}
               git config --global --add remote.origin.fetch "+refs/pull/*/head:refs/remotes/origin/pr/*"
-              ./ci/script/apm-server-update-beats.sh
+              script/jenkins/update-beats.sh
             """)
           }
         }
