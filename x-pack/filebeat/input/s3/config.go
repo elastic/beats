e57bfb8b70a3f7c63f33b16b7d54988556709bea
kaiyan-sheng <kaiyan.sheng@elastic.co>
2020-01-28 08:02:18 -0700

    [Filebeat] Add timeout to GetObjectRequest for s3 input (#15590)
    * Add timeout to GetObjectRequest which will cancel the request if it takes too long
    * Close resp.Body from S3 GetObject API to prevent resource leak
    * Change aws_api_timeout to api_timeout
    
    (cherry picked from commit 86c3e6372ddaf73a4a602f774f3db7b91ffcb8f3)

--- x-pack/filebeat/input/s3/config.go
+++ x-pack/filebeat/input/s3/config.go
@@ -17,6 +17,7 @@ type config struct {
 	QueueURL                  string              `config:"queue_url" validate:"nonzero,required"`
 	VisibilityTimeout         time.Duration       `config:"visibility_timeout"`
 	AwsConfig                 awscommon.ConfigAWS `config:",inline"`
+	APITimeout                time.Duration       `config:"api_timeout"`
 }
 
 func defaultConfig() config {
@@ -25,6 +26,7 @@ func defaultConfig() config {
 			Type: "s3",
 		},
 		VisibilityTimeout: 300 * time.Second,
+		APITimeout:        120 * time.Second,
 	}
 }
 
@@ -32,6 +34,10 @@ func (c *config) Validate() error {
 	if c.VisibilityTimeout < 0 || c.VisibilityTimeout.Hours() > 12 {
 		return fmt.Errorf("visibility timeout %v is not within the "+
 			"required range 0s to 12h", c.VisibilityTimeout)
+	}
+	if c.APITimeout < 0 || c.APITimeout > c.VisibilityTimeout/2 {
+		return fmt.Errorf("api timeout %v needs to be larger than"+
+			" 0s and smaller than half of the visibility timeout", c.APITimeout)
 	}
 	return nil
 }
