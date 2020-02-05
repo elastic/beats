8f522be6d60e3a467c05ee7bafe0eca30c9fe8b2
Marcin Tojek <mtojek@users.noreply.github.com>
2020-02-05 23:09:51 +0100

    [Filebeat] Refactor mqtt input (#16014)
    * Refactor mqtt input
    
    * Fix: comment
    
    * Add unit tests
    
    * Test: input run
    
    * Fix Test: run and stop
    
    * Test: backoff
    
    * Adjust code after review

--- filebeat/input/mqtt/client.go
+++ filebeat/input/mqtt/client.go
@@ -18,191 +18,37 @@
 package mqtt
 
 import (
-	"crypto/tls"
-	"crypto/x509"
-	"encoding/json"
-	"io/ioutil"
-	"strings"
-	"time"
+	libmqtt "github.com/eclipse/paho.mqtt.golang"
 
-	"gopkg.in/vmihailenco/msgpack.v2"
-
-	"github.com/elastic/beats/libbeat/beat"
-	"github.com/elastic/beats/libbeat/common"
-	"github.com/elastic/beats/libbeat/logp"
-
-	MQTT "github.com/eclipse/paho.mqtt.golang"
+	"github.com/elastic/beats/libbeat/outputs"
 )
 
-func (input *mqttInput) newTLSConfig() (*tls.Config, error) {
-	config := input.config
+func createClientOptions(config mqttInputConfig, onConnectHandler func(client libmqtt.Client)) (*libmqtt.ClientOptions, error) {
+	clientOptions := libmqtt.NewClientOptions().
+		SetClientID(config.ClientID).
+		SetUsername(config.Username).
+		SetPassword(config.Password).
+		SetConnectRetry(true).
+		SetOnConnectHandler(onConnectHandler)
 
-	// Import trusted certificates from CAfile.pem.
-	// Alternatively, manually add CA certificates to
-	// default openssl CA bundle.
-	certpool := x509.NewCertPool()
-	if config.CA != "" {
-		logp.Info("[MQTT] Set the CA")
-		pemCerts, err := ioutil.ReadFile(config.CA)
+	for _, host := range config.Hosts {
+		clientOptions.AddBroker(host)
+	}
+
+	if config.TLS != nil {
+		tlsConfig, err := outputs.LoadTLSConfig(config.TLS)
 		if err != nil {
 			return nil, err
 		}
-		certpool.AppendCertsFromPEM(pemCerts)
+		clientOptions.SetTLSConfig(tlsConfig.BuildModuleConfig(""))
 	}
-
-	tlsconfig := &tls.Config{
-		// RootCAs = certs used to verify server cert.
-		RootCAs: certpool,
-		// ClientAuth = whether to request cert from server.
-		// Since the server is set up for SSL, this happens
-		// anyways.
-		ClientAuth: tls.NoClientCert,
-		// ClientCAs = certs used to validate client cert.
-		ClientCAs: nil,
-		// InsecureSkipVerify = verify that cert contents
-		// match server. IP matches what is in cert etc.
-		InsecureSkipVerify: true,
-	}
-
-	// Import client certificate/key pair
-	if config.ClientCert != "" && config.ClientKey != "" {
-		logp.Info("[MQTT] Set the Certs")
-		cert, err := tls.LoadX509KeyPair(config.ClientCert, config.ClientKey)
-		if err != nil {
-			return nil, err
-		}
-
-		// Certificates = list of certs client sends to server.
-		tlsconfig.Certificates = []tls.Certificate{cert}
-	}
-
-	// Create tls.Config with desired tls properties
-	return tlsconfig, nil
+	return clientOptions, nil
 }
 
-// Prepare MQTT client
-func (input *mqttInput) setupMqttClient() error {
-	c := input.config
-
-	logp.Info("[MQTT] Connect to broker URL: %s", c.Host)
-
-	mqttClientOpt := MQTT.NewClientOptions()
-	mqttClientOpt.SetClientID(c.ClientID)
-	mqttClientOpt.AddBroker(c.Host)
-
-	mqttClientOpt.SetMaxReconnectInterval(1 * time.Second)
-	mqttClientOpt.SetConnectionLostHandler(input.connectionLostHandler)
-	mqttClientOpt.SetOnConnectHandler(input.subscribeOnConnect)
-	mqttClientOpt.SetAutoReconnect(true)
-
-	if c.Username != "" {
-		logp.Info("[MQTT] Broker username: %s", c.Username)
-		mqttClientOpt.SetUsername(c.Username)
-	}
-
-	if c.Password != "" {
-		mqttClientOpt.SetPassword(c.Password)
-	}
-
-	if c.SSL == true {
-		logp.Info("[MQTT] Configure session to use SSL")
-		tlsconfig, err := input.newTLSConfig()
-		if err != nil {
-			return err
-		}
-		mqttClientOpt.SetTLSConfig(tlsconfig)
-	}
-
-	input.client = MQTT.NewClient(mqttClientOpt)
-	return nil
-}
-
-func (input *mqttInput) connect() error {
-	if token := input.client.Connect(); token.WaitTimeout(input.config.WaitClose) && token.Error() != nil {
-		logp.Err("MQTT Failed to connect")
-		return token.Error()
-	}
-	logp.Info("MQTT Client connected: %t", input.client.IsConnected())
-	return nil
-}
-
-func (input *mqttInput) subscribeOnConnect(client MQTT.Client) {
-	subscriptions := prepareSubscriptionsForTopics(input.config.Topics, input.config.QoS)
-
-	// Mqtt client - Subscribe to every topic in the config file, and bind with message handler
-	if token := input.client.SubscribeMultiple(subscriptions, input.onMessage); token.WaitTimeout(input.config.WaitClose) && token.Error() != nil {
-		logp.Error(token.Error())
-	}
-	logp.Info("MQTT Subscribed to configured topics")
-}
-
-// Mqtt message handler
-func (input *mqttInput) onMessage(client MQTT.Client, msg MQTT.Message) {
-	logp.Debug("MQTT", "MQTT message received: %s", string(msg.Payload()))
-	var beatEvent beat.Event
-	eventFields := make(common.MapStr)
-
-	// default case
-	var mqtt = make(common.MapStr)
-	eventFields["message"] = string(msg.Payload())
-	if input.config.DecodePayload {
-		mqtt["fields"] = decodeBytes(msg.Payload())
-	}
-
-	eventFields["is_system_topic"] = strings.HasPrefix(msg.Topic(), "$")
-	eventFields["topic"] = msg.Topic()
-
-	mqtt["id"] = msg.MessageID()
-	mqtt["retained"] = msg.Retained()
-	eventFields["mqtt"] = mqtt
-
-	// Finally sending the message to elasticsearch
-	beatEvent.Fields = eventFields
-	isSent := input.outlet.OnEvent(beatEvent)
-
-	logp.Debug("MQTT", "Event sent: %t", isSent)
-}
-
-// connectionLostHandler will try to reconnect when connection is lost
-func (input *mqttInput) connectionLostHandler(client MQTT.Client, reason error) {
-	logp.Warn("[MQTT] Connection lost: %s", reason.Error())
-
-	//Rerun the input
-	input.Run()
-}
-
-// decodeBytes will try to decode the bytes in the following order
-// 1.) Check for msgpack format
-// 2.) Check for json format
-// 3.) If every check fails, it will
-// return the the string representation
-func decodeBytes(payload []byte) common.MapStr {
-	event := make(common.MapStr)
-
-	// A msgpack payload must be a json-like object
-	err := msgpack.Unmarshal(payload, &event)
-	if err == nil {
-		logp.Debug("MQTT", "Payload decoded - msgpack")
-		return event
-	}
-
-	err = json.Unmarshal(payload, &event)
-	if err == nil {
-		logp.Debug("MQTT", "Payload decoded - as json")
-		return event
-	}
-
-	logp.Debug("MQTT", "decoded - as text")
-	return event
-}
-
-// ParseTopics will parse the config file and return a map with topic:QoS
-func prepareSubscriptionsForTopics(topics []string, qos int) map[string]byte {
-	subscriptions := make(map[string]byte)
-	for _, value := range topics {
-		// Finally, filling the subscriptions map
-		subscriptions[value] = byte(qos)
-		logp.Info("Subscribe to %v with QoS %v", value, qos)
+func createClientSubscriptions(config mqttInputConfig) map[string]byte {
+	subscriptions := map[string]byte{}
+	for _, topic := range config.Topics {
+		subscriptions[topic] = byte(config.QoS)
 	}
 	return subscriptions
 }
---
+++ filebeat/input/mqtt/input_test.go
@@ -0,0 +1,345 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package mqtt
+
+import (
+	"errors"
+	"sync"
+	"testing"
+	"time"
+
+	libmqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/stretchr/testify/require"
+
+	finput "github.com/elastic/beats/filebeat/input"
+	"github.com/elastic/beats/libbeat/beat"
+	"github.com/elastic/beats/libbeat/common"
+	"github.com/elastic/beats/libbeat/common/backoff"
+	"github.com/elastic/beats/libbeat/logp"
+)
+
+var (
+	logger = logp.NewLogger("test")
+)
+
+func TestNewInput_MissingConfigField(t *testing.T) {
+	config := common.MustNewConfigFrom(common.MapStr{
+		"topics": "#",
+	})
+	connector := new(mockedConnector)
+	var inputContext finput.Context
+
+	input, err := NewInput(config, connector, inputContext)
+
+	require.Error(t, err)
+	require.Nil(t, input)
+}
+
+func TestNewInput_ConnectWithFailed(t *testing.T) {
+	connectWithError := errors.New("failure")
+	config := common.MustNewConfigFrom(common.MapStr{
+		"hosts":  "tcp://mocked:1234",
+		"topics": "#",
+	})
+	connector := &mockedConnector{
+		connectWithError: connectWithError,
+	}
+	var inputContext finput.Context
+
+	input, err := NewInput(config, connector, inputContext)
+
+	require.Equal(t, connectWithError, err)
+	require.Nil(t, input)
+}
+
+func TestNewInput_Run(t *testing.T) {
+	config := common.MustNewConfigFrom(common.MapStr{
+		"hosts":  "tcp://mocked:1234",
+		"topics": []string{"first", "second"},
+		"qos":    2,
+	})
+
+	eventsCh := make(chan beat.Event)
+	outlet := &mockedOutleter{
+		onEventHandler: func(event beat.Event) bool {
+			eventsCh <- event
+			return true
+		},
+	}
+	connector := &mockedConnector{
+		outlet: outlet,
+	}
+	var inputContext finput.Context
+
+	firstMessage := mockedMessage{
+		duplicate: false,
+		messageID: 1,
+		qos:       2,
+		retained:  false,
+		topic:     "first",
+		payload:   []byte("first-message"),
+	}
+	secondMessage := mockedMessage{
+		duplicate: false,
+		messageID: 2,
+		qos:       2,
+		retained:  false,
+		topic:     "second",
+		payload:   []byte("second-message"),
+	}
+
+	var client *mockedClient
+	newMqttClient = func(o *libmqtt.ClientOptions) libmqtt.Client {
+		client = &mockedClient{
+			onConnectHandler: o.OnConnect,
+			messages:         []mockedMessage{firstMessage, secondMessage},
+			tokens: []libmqtt.Token{&mockedToken{
+				timeout: true,
+			}},
+		}
+		return client
+	}
+
+	input, err := NewInput(config, connector, inputContext)
+	require.NoError(t, err)
+	require.NotNil(t, input)
+
+	input.Run()
+
+	require.Equal(t, 1, client.connectCount)
+	require.Equal(t, 1, client.subscribeMultipleCount)
+	require.ElementsMatch(t, []string{"first", "second"}, client.subscriptions)
+
+	for _, event := range []beat.Event{<-eventsCh, <-eventsCh} {
+		topic, err := event.GetValue("mqtt.topic")
+		require.NoError(t, err)
+
+		if topic == "first" {
+			assertEventMatches(t, firstMessage, event)
+		} else {
+			assertEventMatches(t, secondMessage, event)
+		}
+	}
+}
+
+func TestNewInput_Run_Stop(t *testing.T) {
+	config := common.MustNewConfigFrom(common.MapStr{
+		"hosts":  "tcp://mocked:1234",
+		"topics": []string{"first", "second"},
+		"qos":    2,
+	})
+
+	const numMessages = 5
+
+	var eventProcessing sync.WaitGroup
+	eventProcessing.Add(numMessages)
+	eventsCh := make(chan beat.Event)
+	outlet := &mockedOutleter{
+		onEventHandler: func(event beat.Event) bool {
+			eventProcessing.Done()
+			eventsCh <- event
+			return true
+		},
+	}
+	connector := &mockedConnector{
+		outlet: outlet,
+	}
+	var inputContext finput.Context
+
+	var messages []mockedMessage
+	for i := 0; i < numMessages; i++ {
+		messages = append(messages, mockedMessage{
+			duplicate: false,
+			messageID: 1,
+			qos:       2,
+			retained:  false,
+			topic:     "topic",
+			payload:   []byte("a-message"),
+		})
+	}
+
+	var client *mockedClient
+	newMqttClient = func(o *libmqtt.ClientOptions) libmqtt.Client {
+		client = &mockedClient{
+			onConnectHandler: o.OnConnect,
+			messages:         messages,
+			tokens: []libmqtt.Token{&mockedToken{
+				timeout: true,
+			}},
+		}
+		return client
+	}
+
+	input, err := NewInput(config, connector, inputContext)
+	require.NoError(t, err)
+	require.NotNil(t, input)
+
+	input.Run()
+	eventProcessing.Wait()
+
+	go func() {
+		time.Sleep(100 * time.Millisecond) // let input.Stop() be executed.
+		for range eventsCh {
+		}
+	}()
+
+	input.Stop()
+}
+
+func TestRun_Once(t *testing.T) {
+	client := new(mockedClient)
+	input := &mqttInput{
+		client: client,
+		logger: logger,
+	}
+
+	input.Run()
+
+	require.Equal(t, 1, client.connectCount)
+}
+
+func TestRun_Twice(t *testing.T) {
+	client := new(mockedClient)
+	input := &mqttInput{
+		client: client,
+		logger: logger,
+	}
+
+	input.Run()
+	input.Run()
+
+	require.Equal(t, 1, client.connectCount)
+}
+
+func TestWait(t *testing.T) {
+	inflightMessages := new(sync.WaitGroup)
+	client := new(mockedClient)
+	input := &mqttInput{
+		client:           client,
+		logger:           logger,
+		inflightMessages: inflightMessages,
+	}
+
+	input.Wait()
+
+	require.Equal(t, 1, client.disconnectCount)
+}
+
+func TestStop(t *testing.T) {
+	client := new(mockedClient)
+	input := &mqttInput{
+		client: client,
+		logger: logger,
+	}
+
+	input.Stop()
+}
+
+func TestOnCreateHandler_SubscribeMultiple_Succeeded(t *testing.T) {
+	inputContext := new(finput.Context)
+	onMessageHandler := func(client libmqtt.Client, message libmqtt.Message) {}
+	var clientSubscriptions map[string]byte
+	handler := createOnConnectHandler(logger, inputContext, onMessageHandler, clientSubscriptions)
+
+	newBackoff = func(done <-chan struct{}, init, max time.Duration) backoff.Backoff {
+		return backoff.NewEqualJitterBackoff(inputContext.Done, time.Nanosecond, 2*time.Nanosecond)
+	}
+
+	client := &mockedClient{
+		tokens: []libmqtt.Token{&mockedToken{
+			timeout: true,
+		}},
+	}
+	handler(client)
+
+	require.Equal(t, 1, client.subscribeMultipleCount)
+}
+
+func TestOnCreateHandler_SubscribeMultiple_BackoffSucceeded(t *testing.T) {
+	inputContext := new(finput.Context)
+	onMessageHandler := func(client libmqtt.Client, message libmqtt.Message) {}
+	var clientSubscriptions map[string]byte
+	handler := createOnConnectHandler(logger, inputContext, onMessageHandler, clientSubscriptions)
+
+	newBackoff = func(done <-chan struct{}, init, max time.Duration) backoff.Backoff {
+		return backoff.NewEqualJitterBackoff(inputContext.Done, time.Nanosecond, 2*time.Nanosecond)
+	}
+
+	client := &mockedClient{
+		tokens: []libmqtt.Token{&mockedToken{
+			timeout: false,
+		}, &mockedToken{
+			timeout: true,
+		}},
+	}
+	handler(client)
+
+	require.Equal(t, 2, client.subscribeMultipleCount)
+}
+
+func TestOnCreateHandler_SubscribeMultiple_BackoffSignalDone(t *testing.T) {
+	inputContext := new(finput.Context)
+	onMessageHandler := func(client libmqtt.Client, message libmqtt.Message) {}
+	var clientSubscriptions map[string]byte
+	handler := createOnConnectHandler(logger, inputContext, onMessageHandler, clientSubscriptions)
+
+	mockedBackoff := &mockedBackoff{
+		waits: []bool{true, false},
+	}
+	newBackoff = func(done <-chan struct{}, init, max time.Duration) backoff.Backoff {
+		return mockedBackoff
+	}
+
+	client := &mockedClient{
+		tokens: []libmqtt.Token{&mockedToken{
+			timeout: false,
+		}, &mockedToken{
+			timeout: false,
+		}},
+	}
+	handler(client)
+
+	require.Equal(t, 2, client.subscribeMultipleCount)
+	require.Equal(t, 1, mockedBackoff.resetCount)
+}
+
+func assertEventMatches(t *testing.T, expected mockedMessage, got beat.Event) {
+	topic, err := got.GetValue("mqtt.topic")
+	require.NoError(t, err)
+	require.Equal(t, expected.topic, topic)
+
+	duplicate, err := got.GetValue("mqtt.duplicate")
+	require.NoError(t, err)
+	require.Equal(t, expected.duplicate, duplicate)
+
+	messageID, err := got.GetValue("mqtt.message_id")
+	require.NoError(t, err)
+	require.Equal(t, expected.messageID, messageID)
+
+	qos, err := got.GetValue("mqtt.qos")
+	require.NoError(t, err)
+	require.Equal(t, expected.qos, qos)
+
+	retained, err := got.GetValue("mqtt.retained")
+	require.NoError(t, err)
+	require.Equal(t, expected.retained, retained)
+
+	message, err := got.GetValue("message")
+	require.NoError(t, err)
+	require.Equal(t, string(expected.payload), message)
+}
