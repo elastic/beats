19f1be000b96985c5633052317de4af56c86426f
Shaunak Kashyap <ycombinator@gmail.com>
2020-02-05 12:34:20 -0800

    Updates Metricbeat stack module tests to use require.* (#15993)
    * Replace assert.* with require.*
    
    * Replace assert.* with require.*
    
    * Fixing imports formatting
    
    * More uses of require.*
    
    * Fixing method call args

--- metricbeat/module/elasticsearch/node/data_test.go
+++ metricbeat/module/elasticsearch/node/data_test.go
@@ -23,9 +23,9 @@ import (
 	"io/ioutil"
 	"testing"
 
-	mbtest "github.com/elastic/beats/metricbeat/mb/testing"
+	"github.com/stretchr/testify/require"
 
-	"github.com/stretchr/testify/assert"
+	mbtest "github.com/elastic/beats/metricbeat/mb/testing"
 
 	"github.com/elastic/beats/metricbeat/module/elasticsearch"
 )
@@ -43,9 +43,9 @@ func TestInvalid(t *testing.T) {
 	file := "./_meta/test/invalid.json"
 
 	content, err := ioutil.ReadFile(file)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	reporter := &mbtest.CapturingReporterV2{}
 	err = eventsMapping(reporter, info, content)
-	assert.Error(t, err)
+	require.Error(t, err)
 }
