a286a74d16bf8d82bccb296e7f9bab127277c9ae
kaiyan-sheng <kaiyan.sheng@elastic.co>
2020-02-05 09:52:50 -0700

    Add NATGateway and TrasitGateway

--- x-pack/metricbeat/module/aws/vpc/vpc_integration_test.go
+++ x-pack/metricbeat/module/aws/vpc/vpc_integration_test.go
@@ -7,18 +7,40 @@
 package vpc
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/elastic/beats/libbeat/common"
 	mbtest "github.com/elastic/beats/metricbeat/mb/testing"
 	"github.com/elastic/beats/x-pack/metricbeat/module/aws/mtest"
 )
 
 func TestData(t *testing.T) {
+	namespaceIs := func(namespace string) func(e common.MapStr) bool {
+		return func(e common.MapStr) bool {
+			v, err := e.GetValue("aws.cloudwatch.namespace")
+			return err == nil && v == namespace
+		}
+	}
+
+	dataFiles := []struct {
+		namespace string
+		path      string
+	}{
+		{"AWS/NATGateway", "./_meta/data.json"},
+		{"AWS/VPN", "./_meta/data_vpn.json"},
+		{"AWS/TransitGateway", "./_meta/data_transit_gateway.json"},
+	}
+
 	config, info := mtest.GetConfigForTest("vpc", "300s")
 	if info != "" {
 		t.Skip("Skipping TestData: " + info)
 	}
 
-	metricSet := mbtest.NewFetcher(t, config)
-	metricSet.WriteEvents(t, "/")
+	for _, df := range dataFiles {
+		metricSet := mbtest.NewFetcher(t, config)
+		t.Run(fmt.Sprintf("namespace: %s", df.namespace), func(t *testing.T) {
+			metricSet.WriteEventsCond(t, df.path, namespaceIs(df.namespace))
+		})
+	}
 }
