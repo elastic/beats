fad1535490d596fe836c24b23d81033219ffc6ec
Lei Qiu <lei.qiu@elastic.co>
2020-02-06 18:06:28 +0000

    Remove check for minimum interval. Add a test case for repeated retrievals

--- x-pack/filebeat/input/httpjson/config.go
+++ x-pack/filebeat/input/httpjson/config.go
@@ -38,9 +38,9 @@ type Pagination struct {
 }
 
 func (c *config) Validate() error {
-	if c.Interval < 3600*time.Second && c.Interval != 0 {
-		return errors.New("httpjson input: interval must not be less than 3600 seconds - ")
-	}
+	//if c.Interval < 3600*time.Second && c.Interval != 0 {
+	//	return errors.New("httpjson input: interval must not be less than 3600 seconds - ")
+	//}
 	switch strings.ToUpper(c.HTTPMethod) {
 	case "GET":
 		break
