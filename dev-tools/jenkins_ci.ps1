2b81703cae8fb5b136b9afa9302d1b40f121dda4
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2020-02-07 16:52:26 +0100

    Fix path again

--- dev-tools/jenkins_ci.ps1
+++ dev-tools/jenkins_ci.ps1
@@ -67,7 +67,7 @@ if (Test-Path "tests\system") {
     echo "Running python tests"
     choco install python -y -r --no-progress --version 3.8.1.20200110
     refreshenv
-    $env:PATH = "C:\\Python38;C:\\Python38\\Scripts;$env:PATH"
+    $env:PATH = "C:\Python38;C:\Python38\Scripts;$env:PATH"
     python --version
     exec { mage pythonUnitTest } "System test FAILURE"
 }
