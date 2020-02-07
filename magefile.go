502b7c7b06700b391ab1850d97c1929f50f7a4a4
Noémi Ványi <sitbackandwait@gmail.com>
2020-02-07 18:48:21 +0100

    exclude generator templates from license header check

--- magefile.go
+++ magefile.go
@@ -109,8 +109,8 @@ func AddLicenseHeaders() error {
 		licenser(
 			licenser.License("ASL2"),
 			licenser.Exclude("x-pack"),
-			licenser.Exclude("generator/beat/{beat}"),
-			licenser.Exclude("generator/metricbeat/{beat}"),
+			licenser.Exclude("generator/_templates/beat/{beat}"),
+			licenser.Exclude("generator/_templates/metricbeat/{beat}"),
 		),
 		licenser(
 			licenser.License("Elastic"),
@@ -133,9 +133,9 @@ func CheckLicenseHeaders() error {
 			licenser.Check(),
 			licenser.License("ASL2"),
 			licenser.Exclude("x-pack"),
-			licenser.Exclude("generator/beat/{beat}"),
-			licenser.Exclude("generator/metricbeat/{beat}"),
-			licenser.Exclude("generator/beat/{beat}"),
+			licenser.Exclude("generator/_templates/beat/{beat}"),
+			licenser.Exclude("generator/_templates/metricbeat/{beat}"),
+			licenser.Exclude("generator/_templates/beat/{beat}"),
 		),
 		licenser(
 			licenser.Check(),
