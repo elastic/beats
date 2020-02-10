0d9f03e1d62691622a6345981d34c891b561072a
Pier-Hugues Pellerin <phpellerin@gmail.com>
2020-02-10 10:00:55 -0500

    [Agent] Allow CA cert pinning on the Elasticsearch output or any code that user tlscommon.TLSConfig builder. (#16019)
    * Add a sha256 pin for the CA Certificate
    
    When multiples CA are presents on the system we cannot ensure that a
    specific one was used to validates the chains exposer by the server.
    This PRs adds a `ca_sha256` option to the `tlscommon.TLSConfig` that is used by all
    the code that has to create a TCP client with TLS support.
    
    When the option is set, it will hook a new callback in the validation
    chains that will inspect the verified and validated chains by Go to
    ensure that a lets a certificate in the chains match the provided
    sha256.
    
    Usage example for the Elasticsearch output.
    
    ```
    output.elasticsearch:
      hosts: [127.0.0.1:9200]
      ssl.ca_sha256: <base64_encoded_sha1>
    ```
    
    You can generate the pin using the **openssl** binary with the
    following command:
    
    ```
    openssl x509 -in ca.crt -pubkey -noout | openssl pkey -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64
    ```
    
    OpenSSL's [documentation](https://www.openssl.org/docs/manmaster/man1/dgst.html)
    
    You will need to start Elasticsearch with the following options
    
    ```yaml
    xpack.security.enabled: true
    indices.id_field_data.enabled: true
    xpack.license.self_generated.type: trial
    xpack.security.http.ssl.enabled: true
    xpack.security.http.ssl.key: /etc/pki/localhost/localhost.key"
    xpack.security.http.ssl.certificate: /etc/pki/localhost/localhost.crt"
    xpack.security.http.ssl.certificate_authorities: /etc/pki/ca/ca.crt"
    ```
    
    This pull request also include a new service in the docker-compose.yml
    that will start a new Elasticsearch server with TLS and security
    configured.

---
+++ libbeat/tests/system/test_ca_pinning.py
@@ -0,0 +1,81 @@
+from base import BaseTest
+from idxmgmt import IdxMgmt
+import os
+from nose.plugins.attrib import attr
+import unittest
+import logging
+from nose.tools import raises
+from elasticsearch import RequestError
+
+INTEGRATION_TESTS = os.environ.get('INTEGRATION_TESTS', False)
+
+
+class TestCAPinning(BaseTest):
+    """
+    Test beat CA pinning for elasticsearch
+    """
+
+    @unittest.skipUnless(INTEGRATION_TESTS, "integration test")
+    @attr('integration')
+    def test_sending_events_with_a_good_sha256(self):
+        """
+        Test Sending events while using ca pinning with a good sha256
+        """
+
+        ca = os.path.join(self.beat_path,
+                          "..",
+                          "testing",
+                          "environments",
+                          "docker",
+                          "elasticsearch",
+                          "pki",
+                          "ca",
+                          "ca.crt")
+
+        self.render_config_template(
+            elasticsearch={
+                "hosts": self.get_elasticsearch_url_ssl(),
+                "username": "admin",
+                "password": "changeme",
+                "ssl.certificate_authorities": [ca],
+                "ssl.ca_sha256": "8hZS8gpciuzlu+7Xi0sdv8T7RKRRxG1TWKumUQsDam0=",
+            },
+        )
+
+        proc = self.start_beat()
+        self.wait_until(lambda: self.log_contains("mockbeat start running."))
+        self.wait_until(lambda: self.log_contains("PublishEvents: 1 events have been published"))
+        proc.check_kill_and_wait()
+
+    @unittest.skipUnless(INTEGRATION_TESTS, "integration test")
+    @attr('integration')
+    def test_sending_events_with_a_bad_sha256(self):
+        """
+        Test Sending events while using ca pinning with a bad sha256
+        """
+
+        ca = os.path.join(self.beat_path,
+                          "..",
+                          "testing",
+                          "environments",
+                          "docker",
+                          "elasticsearch",
+                          "pki",
+                          "ca",
+                          "ca.crt")
+
+        self.render_config_template(
+            elasticsearch={
+                "hosts": self.get_elasticsearch_url_ssl(),
+                "username": "admin",
+                "password": "changeme",
+                "ssl.certificate_authorities": [ca],
+                "ssl.ca_sha256": "not-good-sha",
+            },
+        )
+
+        proc = self.start_beat()
+        self.wait_until(lambda: self.log_contains("mockbeat start running."))
+        self.wait_until(lambda: self.log_contains(
+            "provided CA certificate pins doesn't match any of the certificate authorities used to validate the certificate"))
+        proc.check_kill_and_wait()
