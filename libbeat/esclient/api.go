30d874f5bd0eeb41d039d94fe5626a4de33e06f1
Shaunak Kashyap <ycombinator@gmail.com>
2020-02-11 17:37:21 -0800

    Fixing typo

--- libbeat/esclient/api.go
+++ libbeat/esclient/api.go
@@ -4,7 +4,7 @@ import (
 	"github.com/elastic/beats/libbeat/common"
 )
 
-// PI defines the version-agnostic Elasticsearch API subset needed by Beats
+// API defines the version-agnostic Elasticsearch API subset needed by Beats
 type API interface {
 	GetLicense() (*License, error)
 	GetVersion() (*common.Version, error)
