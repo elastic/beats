8446cd00ff84ceae0d3d71296d0b3273db65ad88
Shaunak Kashyap <ycombinator@gmail.com>
2020-02-14 16:40:58 -0800

    Imports formatting

--- libbeat/common/url_test.go
+++ libbeat/common/url_test.go
@@ -24,9 +24,8 @@ import (
 	"net/url"
 	"testing"
 
-	"github.com/stretchr/testify/require"
-
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGetUrl(t *testing.T) {
