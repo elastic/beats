9164c1d654553e0219664c32e0e58a01c5ecb144
Marcin Tojek <mtojek@users.noreply.github.com>
2020-02-18 17:53:35 +0100

    [Metricbeat] haproxy: Add support for stats exposed over HTTPS (#16333)
    * Prepare tests first
    
    * Add support for stats exposed over HTTPS
    
    * Fix
    
    * Update CHANGELOG
    
    * Fix: https
    
    * Try: sleep
    
    * Fix
    
    * Fix: don't verify hostname in tests
    
    * Add information to docs about SSL, HTTP
    
    * Adjust code after review

--- metricbeat/module/haproxy/test_haproxy.py
+++ metricbeat/module/haproxy/test_haproxy.py
@@ -46,7 +46,7 @@ class HaproxyTest(metricbeat.BaseTest):
         proc = self.start_beat()
         self.wait_until(lambda: self.output_lines() > 0)
         proc.check_kill_and_wait()
-        self.assert_no_logged_warnings()
+        self.assert_no_logged_warnings(replace=['SSL/TLS verifications disabled.'])
 
         output = self.read_output_json()
         self.assertGreater(len(output), 0)
@@ -79,6 +79,27 @@ class HaproxyTest(metricbeat.BaseTest):
             "metricsets": ["stat"],
             "hosts": ["http://%s/stats" % (self.compose_host(port="14568/tcp"))],
             "period": "5s"
+        }])
+        self._test_stat()
+
+    @unittest.skipUnless(metricbeat.INTEGRATION_TESTS, "integration test")
+    def test_stat_https(self):
+        """
+        haproxy stat https metricset test
+        """
+        self.render_config_template(modules=[{
+            "name": "haproxy",
+            "metricsets": ["stat"],
+            "hosts": ["https://%s/stats" % (self.compose_host(port="14570/tcp"))],
+            "period": "5s",
+            "extras": {
+                "ssl.certificate_authorities": [os.path.join(os.path.dirname(__file__), '_meta/certs/ca.pem')],
+                "ssl.certificate": os.path.join(os.path.dirname(__file__), '_meta/certs/client.pem'),
+                "ssl.key": os.path.join(os.path.dirname(__file__), '_meta/certs/client.key'),
+                # TODO: verification_mode: "certificate"
+                # compose uses dynamic IP addresses and there are no IP SAN records in the certificate
+                "ssl.verification_mode": "none"
+            }
         }])
         self._test_stat()
 
