70f60d7a2a45fdf966a2dd20a60e9238c415d133
Alex Kristiansen <alex.kristiansen@elastic.co>
2020-02-25 08:51:38 -0800

    make fmt

--- metricbeat/module/system/users/users.go
+++ metricbeat/module/system/users/users.go
@@ -1,22 +1,32 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package users
 
 import (
 	"net"
 
-	"github.com/coreos/go-systemd/login1"
+	"github.com/godbus/dbus"
+	"github.com/pkg/errors"
+
 	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/libbeat/common/cfgwarn"
 	"github.com/elastic/beats/metricbeat/mb"
-	"github.com/godbus/dbus"
-	"github.com/pkg/errors"
 )
-
-type userInfo struct {
-	UID      uint32
-	User     string
-	Path     string
-	Sessions []login1.Session
-}
 
 // init registers the MetricSet with the central registry as soon as the program
 // starts. The New function will be called later to instantiate an instance of
