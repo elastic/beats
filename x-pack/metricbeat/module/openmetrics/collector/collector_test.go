0f773712f2f8d3bfc48f1a243b0341a0968a890c
chrismark <chrismarkou92@gmail.com>
2020-02-26 12:29:08 +0200

    Add Openmetrics Metricbeat module
    Signed-off-by: chrismark <chrismarkou92@gmail.com>

---
+++ x-pack/metricbeat/module/openmetrics/collector/collector_test.go
@@ -0,0 +1,32 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+// +build !integration
+
+package collector
+
+import (
+	"os"
+	"testing"
+
+	"github.com/elastic/beats/libbeat/logp"
+	"github.com/elastic/beats/metricbeat/mb"
+	mbtest "github.com/elastic/beats/metricbeat/mb/testing"
+
+	// Register input module and metricset
+	_ "github.com/elastic/beats/metricbeat/module/prometheus"
+	_ "github.com/elastic/beats/metricbeat/module/prometheus/collector"
+)
+
+func init() {
+	// To be moved to some kind of helper
+	os.Setenv("BEAT_STRICT_PERMS", "false")
+	mb.Registry.SetSecondarySource(mb.NewLightModulesSource("../../../module"))
+}
+
+func TestEventMapping(t *testing.T) {
+	logp.TestingSetup()
+
+	mbtest.TestDataFiles(t, "openmetrics", "collector")
+}
