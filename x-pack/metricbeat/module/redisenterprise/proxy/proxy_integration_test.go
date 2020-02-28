96bebe4c05637a9ef2e07e21e24b8c6cfed3c3e6
Marcin Tojek <mtojek@users.noreply.github.com>
2020-02-28 15:19:32 +0100

    [Metricbeat] Add new module for Redis Enterprise (#16482)
    Add metricsets: node, proxy

---
+++ x-pack/metricbeat/module/redisenterprise/proxy/proxy_integration_test.go
@@ -0,0 +1,48 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+// +build integration
+
+package proxy
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/elastic/beats/libbeat/tests/compose"
+	"github.com/elastic/beats/metricbeat/mb"
+	mbtest "github.com/elastic/beats/metricbeat/mb/testing"
+	_ "github.com/elastic/beats/metricbeat/module/prometheus"
+	_ "github.com/elastic/beats/metricbeat/module/prometheus/collector"
+)
+
+func init() {
+	// To be moved to some kind of helper
+	os.Setenv("BEAT_STRICT_PERMS", "false")
+	mb.Registry.SetSecondarySource(mb.NewLightModulesSource("../../../module"))
+}
+
+func TestFetch(t *testing.T) {
+	service := compose.EnsureUp(t, "redisenterprise", compose.UpWithTimeout(5*time.Minute))
+
+	f := mbtest.NewFetcher(t, getConfig(service.Host()))
+	events, errs := f.FetchEvents()
+	if len(errs) > 0 {
+		t.Fatalf("Expected 0 error, had %d. %v\n", len(errs), errs)
+	}
+	assert.NotEmpty(t, events)
+	t.Logf("%s/%s event: %+v", f.Module().Name(), f.Name(), events[0])
+}
+
+func getConfig(host string) map[string]interface{} {
+	return map[string]interface{}{
+		"module":                "redisenterprise",
+		"metricsets":            []string{"proxy"},
+		"hosts":                 []string{host},
+		"ssl.verification_mode": "none",
+	}
+}
