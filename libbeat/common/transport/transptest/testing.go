9a0549e75ac5a843b33e8edd8250cd1f5adf1970
Shaunak Kashyap <ycombinator@gmail.com>
2020-02-28 06:30:48 -0800

    Adding missing files

--- libbeat/common/transport/transptest/testing.go
+++ libbeat/common/transport/transptest/testing.go
@@ -169,7 +169,11 @@ func connectTCP(timeout time.Duration) TransportFactory {
 			Proxy:   proxy,
 			Timeout: timeout,
 		}
+<<<<<<< HEAD
 		return transport.NewClient(cfg, "tcp", addr, 0)
+=======
+		return transport.NewClient(&cfg, "tcp", addr, 0)
+>>>>>>> Adding missing files
 	}
 }
 
@@ -187,7 +191,11 @@ func connectTLS(timeout time.Duration, certName string) TransportFactory {
 			TLS:     tlsConfig,
 			Timeout: timeout,
 		}
+<<<<<<< HEAD
 		return transport.NewClient(cfg, "tcp", addr, 0)
+=======
+		return transport.NewClient(&cfg, "tcp", addr, 0)
+>>>>>>> Adding missing files
 	}
 }
 
