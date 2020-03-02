07b03a7f58e0a8a00942003abb9316bf084cc34e
Blake Rouse <blake.rouse@elastic.co>
2020-03-02 17:40:48 -0500

    Add cloudfoundry module to metricbeat (#16671)
    * Split up parts into different metricsets.
    
    * Add value and container metricsets.
    
    * Add documentation.
    
    * Revert metricbeat.yml.
    
    * Run make update.
    
    * Fix documentation add changelog.
    
    * Fix bad change in azure module.
    
    * Run make update.
    
    * Improve reference config.
    
    * Document Kibana Space ID setting (#16622)
    
    * Document Kibana Space ID setting
    
    * Update reference YML files
    
    * Update libbeat/docs/shared-kibana-config.asciidoc
    
    Co-Authored-By: DeDe Morton <dede.morton@elastic.co>
    
    * Updating reference YML files
    
    * Adding link to doc
    
    Co-authored-by: DeDe Morton <dede.morton@elastic.co>
    
    * Remove the special pins type for `ca_sha256` (#16615)
    
    We have changed the type from a special custom type to a slice of
    string. This reduce the number of exposed types and make it easier in
    general to work with in the tests.
    
    * Remove feature.Describer and enhance Details (#16658)
    
    * Make memqueue.Broker internal (now memqueue.broker) (#16667)
    
    * [docs] Add skeleton files for 7.7 release highlights and breaking changes (#16584)
    
    * [Metricbeat] Add vpc metricset for aws module (#16111)
    
    * Add vpn into vpc metricset
    * Add NATGateway and TrasitGateway
    
    * Update cisco.asciidoc (#16434)
    
    * Update cisco.asciidoc
    
    Making Elasticsearch explicit in the instructions
    
    * Update docs source in the module dir
    
    Co-authored-by: Loek van Gool <loek.vangool@elastic.co>
    
    * Fix wording in dev guide (#16497)
    
    * Added NetBeat (#12612)
    
    * Close files earlier when generating fields files (#16681)
    
    Fields generator keeps all open files while generating the global
    fields.yml file. This is not needed and may lead to "too many open
    files" errors in some environments.
    
    * [Metricbeat] Add new module for Redis Enterprise (#16482)
    
    Add metricsets: node, proxy
    
    * Add doppler_address, uaa_address, rlp_address to config.reference.yml.
    
    Co-authored-by: Shaunak Kashyap <ycombinator@gmail.com>
    Co-authored-by: DeDe Morton <dede.morton@elastic.co>
    Co-authored-by: Pier-Hugues Pellerin <phpellerin@gmail.com>
    Co-authored-by: Steffen Siering <steffen.siering@elastic.co>
    Co-authored-by: Fae Charlton <fae.charlton@elastic.co>
    Co-authored-by: kaiyan-sheng <kaiyan.sheng@elastic.co>
    Co-authored-by: Loek van Gool <loek.vangool@elastic.co>
    Co-authored-by: Harold Schreckengost <harold@haroldmschreckengost.com>
    Co-authored-by: Jaime Soriano Pastor <jaime.soriano@elastic.co>
    Co-authored-by: Marcin Tojek <mtojek@users.noreply.github.com>

---
+++ x-pack/metricbeat/module/cloudfoundry/cloudfoundry.go
@@ -0,0 +1,145 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package cloudfoundry
+
+import (
+	"context"
+	"sync"
+
+	"github.com/elastic/beats/libbeat/logp"
+	"github.com/elastic/beats/metricbeat/mb"
+	cfcommon "github.com/elastic/beats/x-pack/libbeat/common/cloudfoundry"
+)
+
+// ModuleName is the name of this module.
+const ModuleName = "cloudfoundry"
+
+type Module struct {
+	mb.BaseModule
+
+	log *logp.Logger
+
+	hub          *cfcommon.Hub
+	listener     *cfcommon.RlpListener
+	listenerLock sync.Mutex
+
+	counterReporter   mb.PushReporterV2
+	valueReporter     mb.PushReporterV2
+	containerReporter mb.PushReporterV2
+}
+
+func init() {
+	if err := mb.Registry.AddModule(ModuleName, newModule); err != nil {
+		panic(err)
+	}
+}
+
+func newModule(base mb.BaseModule) (mb.Module, error) {
+	var cfg cfcommon.Config
+	if err := base.UnpackConfig(&cfg); err != nil {
+		return nil, err
+	}
+
+	log := logp.NewLogger("cloudfoundry")
+	hub := cfcommon.NewHub(&cfg, "metricbeat", log)
+
+	// early check that listener can be created
+	_, err := hub.RlpListener(cfcommon.RlpListenerCallbacks{})
+	if err != nil {
+		return nil, err
+
+	}
+
+	return &Module{
+		BaseModule: base,
+		log:        log,
+		hub:        hub,
+	}, nil
+}
+
+func (m *Module) RunCounterReporter(reporter mb.PushReporterV2) {
+	m.listenerLock.Lock()
+	m.runReporters(reporter, m.valueReporter, m.containerReporter)
+	m.listenerLock.Unlock()
+
+	<-reporter.Done()
+
+	m.listenerLock.Lock()
+	m.runReporters(nil, m.valueReporter, m.containerReporter)
+	m.listenerLock.Unlock()
+}
+
+func (m *Module) RunValueReporter(reporter mb.PushReporterV2) {
+	m.listenerLock.Lock()
+	m.runReporters(m.counterReporter, reporter, m.containerReporter)
+	m.listenerLock.Unlock()
+
+	<-reporter.Done()
+
+	m.listenerLock.Lock()
+	m.runReporters(m.counterReporter, nil, m.containerReporter)
+	m.listenerLock.Unlock()
+}
+
+func (m *Module) RunContainerReporter(reporter mb.PushReporterV2) {
+	m.listenerLock.Lock()
+	m.runReporters(m.counterReporter, m.valueReporter, reporter)
+	m.listenerLock.Unlock()
+
+	<-reporter.Done()
+
+	m.listenerLock.Lock()
+	m.runReporters(m.counterReporter, m.valueReporter, nil)
+	m.listenerLock.Unlock()
+}
+
+func (m *Module) runReporters(counterReporter, valueReporter, containerReporter mb.PushReporterV2) {
+	if m.listener != nil {
+		m.listener.Stop()
+		m.listener = nil
+	}
+	m.counterReporter = counterReporter
+	m.valueReporter = valueReporter
+	m.containerReporter = containerReporter
+
+	start := false
+	callbacks := cfcommon.RlpListenerCallbacks{}
+	if m.counterReporter != nil {
+		start = true
+		callbacks.Counter = func(evt *cfcommon.EventCounter) {
+			m.counterReporter.Event(mb.Event{
+				Timestamp:  evt.Timestamp(),
+				RootFields: evt.ToFields(),
+			})
+		}
+	}
+	if m.valueReporter != nil {
+		start = true
+		callbacks.ValueMetric = func(evt *cfcommon.EventValueMetric) {
+			m.valueReporter.Event(mb.Event{
+				Timestamp:  evt.Timestamp(),
+				RootFields: evt.ToFields(),
+			})
+		}
+	}
+	if m.containerReporter != nil {
+		start = true
+		callbacks.ContainerMetric = func(evt *cfcommon.EventContainerMetric) {
+			m.containerReporter.Event(mb.Event{
+				Timestamp:  evt.Timestamp(),
+				RootFields: evt.ToFields(),
+			})
+		}
+	}
+	if start {
+		l, err := m.hub.RlpListener(callbacks)
+		if err != nil {
+			m.log.Errorf("failed to create RlpListener: %v", err)
+			return
+		}
+		l.Start(context.Background())
+		m.listener = l
+	}
+}
