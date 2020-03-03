ce7fa63b65138c704327064b1bba10a35f75729e
urso <steffen.siering@elastic.co>
2020-03-04 00:27:29 +0100

    init v2/doc.go

---
+++ filebeat/input/v2/doc.go
@@ -0,0 +1,27 @@
+// Package v2 provides common interfaces and helper functions for defining an
+// input extension point.
+//
+// Extension points need to provide support for querying and verifying its
+// capabilities, as well as providing support for creating and running actual
+// inputs. For this purpose the Registry and Plugin interfaces provide a common
+// interface for describing capabilities. The Loader and Input interfaces
+// provide support for creating and running the actual plugins.
+//
+// These interfaces are meant to be implemented by separate packages,
+// potentially providing additional capabilities and services to the actual
+// inputs. Having additional capabilities in dedicated packages ensures that
+// the final binary only compiles in dependencies that are actually required
+// for the selected set of Inputs and Extension point shall be able to run.
+//
+// This package provides additional helpers for combining the loaders and
+// registries defined in different packages, to create the actual extension point.
+// For example LoaderList and LoaderTable combine multiple Loaders into one
+// single loader, and ConfigsLoader allows developers to create a loader that
+// produces configurations for other configured Loaders (similar to filesets).
+//
+// A set of registries is combined using RegistryList. Use
+// `RegistryList.Validate` to check if the combined registries do not provide
+// similar input names.
+//
+// TODO: document some 'patterns' for implementing and using extensions.
+package v2
