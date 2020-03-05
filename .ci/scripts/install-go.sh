0470c63d58819db15b42c9b0f1173662c3ee77ca
Ivan Fernandez Calvo <kuisathaverat@gmail.com>
2020-03-05 15:46:54 +0100

    fix: avoid to modify go.mod file

--- .ci/scripts/install-go.sh
+++ .ci/scripts/install-go.sh
@@ -16,4 +16,5 @@ chmod +x "${GVM_CMD}"
 gvm ${GO_VERSION}|cut -d ' ' -f 2|tr -d '\"' > ${PROPERTIES_FILE}
 
 eval $(gvm ${GO_VERSION})
-go get -u github.com/kardianos/govendor
+# see https://github.com/golang/go/issues/27643
+GO111MODULE=off go get -u github.com/kardianos/govendor
