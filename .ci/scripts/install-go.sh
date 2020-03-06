78c7c6ec22fa3c9d0620caf78af10ba4a33338c4
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2020-03-06 11:50:55 +0100

    remove govendor code

--- .ci/scripts/install-go.sh
+++ .ci/scripts/install-go.sh
@@ -16,5 +16,3 @@ chmod +x "${GVM_CMD}"
 gvm ${GO_VERSION}|cut -d ' ' -f 2|tr -d '\"' > ${PROPERTIES_FILE}
 
 eval $(gvm ${GO_VERSION})
-# see https://github.com/golang/go/issues/27643
-# GO111MODULE=off go get -u github.com/kardianos/govendor
