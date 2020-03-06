5f802819c4ea146e6cb4f7f3c7b3b891c1a6a859
Ivan Fernandez Calvo <kuisathaverat@gmail.com>
2020-03-06 09:37:08 +0100

    test: overwrite build-image target

--- .ci/scripts/install-go.sh
+++ .ci/scripts/install-go.sh
@@ -17,8 +17,3 @@ gvm ${GO_VERSION}|cut -d ' ' -f 2|tr -d '\"' > ${PROPERTIES_FILE}
 
 eval $(gvm ${GO_VERSION})
 GO111MODULE=off go get -u github.com/kardianos/govendor
-
-docker images
-cd metricbeat
-docker tag store/oracle/database-instantclient:12.2.0.1 database-instantclient:12.2.0.1
-TESTING_ENVIRONMENT=snapshot ES_BEATS=.. docker-compose -p metricbeat21e89f892431fca6dc2c9c17fa89dd8a9b03e92a  -f docker-compose.yml build --force-rm
