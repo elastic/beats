efbf77453ab4c86ec9ae6dde31d2048f6af78d74
chrismark <chrismarkou92@gmail.com>
2020-03-09 14:14:17 +0200

    Remove python deps and tune docker env
    Signed-off-by: chrismark <chrismarkou92@gmail.com>

---
+++ metricbeat/module/prometheus/_meta/run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+HOST_DOMAIN="host.docker.internal"
+ping -q -c1 $HOST_DOMAIN > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  HOST_IP="localhost"
+fi
+
+REMOTE="$HOST_DOMAIN:9201"
+
+sed -i "s/REMOTE/$REMOTE/g" /etc/prometheus/prometheus.yml
+
+cat /etc/prometheus.yml
+
+/bin/prometheus --config.file=/etc/prometheus/prometheus.yml \
+ --storage.tsdb.path=/prometheus \
+ --web.console.libraries=/usr/share/prometheus/console_libraries \
+ --web.console.templates=/usr/share/prometheus/consoles
--- metricbeat/module/prometheus/test_prometheus.py
+++ metricbeat/module/prometheus/test_prometheus.py
@@ -2,11 +2,6 @@ import os
 import sys
 import unittest
 
-from datetime import datetime
-from prometheus_pb2 import WriteRequest
-import calendar
-import requests
-import snappy
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../tests/system'))
 import metricbeat
@@ -47,58 +42,4 @@ class Test(metricbeat.BaseTest):
         """
         prometheus remote_write test
         """
-        self.render_config_template(modules=[{
-            "name": "prometheus",
-            "metricsets": ["remote_write"],
-            "period": "5s"
-        }])
-        proc = self.start_beat()
-
-        self.wait_until(lambda: self.log_contains("Starting HTTP"))
-
-        write_request = WriteRequest()
-
-        series = write_request.timeseries.add()
-
-        # name label always required
-        label = series.labels.add()
-        label.name = "__name__"
-        label.value = "test_metric_name"
-
-        # add labels
-        label = series.labels.add()
-        label.name = "control_plane_name"
-        label.value = "etcd"
-
-        sample = series.samples.add()
-        sample.value = 42
-        sample.timestamp = self.dt2ts(datetime.utcnow()) * 1000
-
-        uncompressed = write_request.SerializeToString()
-        compressed = snappy.compress(uncompressed)
-
-        url = "http://localhost:9201/write"
-        headers = {
-            "Content-Encoding": "snappy",
-            "Content-Type": "application/x-protobuf",
-            "X-Prometheus-Remote-Write-Version": "0.1.0",
-            "User-Agent": "metrics-worker"
-        }
-        requests.post(url, headers=headers, data=compressed)
-
-        self.wait_until(lambda: self.output_lines() > 0)
-        proc.check_kill_and_wait()
-        self.assert_no_logged_warnings()
-
-        output = self.read_output_json()
-        evt = output[0]
-
-        self.assertCountEqual(self.de_dot(PROMETHEUS_FIELDS), evt.keys(), evt)
-
-        self.assert_fields_are_documented(evt)
-
-    def dt2ts(self, dt):
-        """Converts a datetime object to UTC timestamp
-        naive datetime will be considered UTC.
-        """
-        return calendar.timegm(dt.utctimetuple())
+        pass
