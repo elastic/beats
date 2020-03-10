0d52ea4f0c0ade03b0902bb11aeb3090fc4012fa
Michal Pristas <michal.pristas@gmail.com>
2020-03-10 12:35:19 +0000

    working monitoring tests

--- x-pack/agent/pkg/agent/operation/operator_test.go
+++ x-pack/agent/pkg/agent/operation/operator_test.go
@@ -30,8 +30,6 @@ import (
 	"github.com/elastic/beats/v7/x-pack/agent/pkg/core/plugin/state"
 )
 
-var installPath = "tests/scripts"
-
 func TestMain(m *testing.M) {
 	// init supported with test cases
 	shortSpec := program.Spec{
@@ -374,81 +372,3 @@ func TestConfigurableByFileRun(t *testing.T) {
 		t.Fatal("Process found")
 	}
 }
-
-func getTestOperator(t *testing.T, installPath string) (*Operator, *operatorCfg.Config) {
-	operatorConfig := &operatorCfg.Config{
-		RetryConfig: &retry.Config{
-			Enabled:      true,
-			RetriesCount: 2,
-			Delay:        3 * time.Second,
-			MaxDelay:     10 * time.Second,
-		},
-		ProcessConfig: &process.Config{},
-		DownloadConfig: &artifact.Config{
-			InstallPath: installPath,
-		},
-		MonitoringConfig: &monitoring.Config{
-			MonitorMetrics: false,
-		},
-	}
-
-	cfg, err := config.NewConfigFrom(operatorConfig)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	l := getLogger()
-
-	fetcher := &DummyDownloader{}
-	installer := &DummyInstaller{}
-
-	stateResolver, err := stateresolver.NewStateResolver(l)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	operator, err := NewOperator(context.Background(), l, "p1", cfg, fetcher, installer, stateResolver, nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	operator.config.DownloadConfig.OperatingSystem = "darwin"
-	operator.config.DownloadConfig.Architecture = "32"
-
-	return operator, operatorConfig
-}
-
-func getLogger() *logger.Logger {
-	l, _ := logger.New()
-	return l
-}
-
-func getProgram(binary, version string) *app.Descriptor {
-	downloadCfg := &artifact.Config{
-		InstallPath:     installPath,
-		OperatingSystem: "darwin",
-	}
-	return app.NewDescriptor(binary, version, downloadCfg, nil)
-}
-
-type TestConfig struct {
-	TestFile string
-}
-
-type DummyDownloader struct {
-}
-
-func (*DummyDownloader) Download(_ context.Context, p, v string) (string, error) {
-	return "", nil
-}
-
-var _ download.Downloader = &DummyDownloader{}
-
-type DummyInstaller struct {
-}
-
-func (*DummyInstaller) Install(p, v, _ string) error {
-	return nil
-}
-
-var _ install.Installer = &DummyInstaller{}
