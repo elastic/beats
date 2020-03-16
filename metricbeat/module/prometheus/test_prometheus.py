d8cd2fffb46becef8065f30db5e8dae0ce86bdc1
Chris Mark <chrismarkou92@gmail.com>
2020-03-16 10:07:41 +0200

    Cherry-pick #16609 to 7.x: Prometheus remote write endpoint (#16973)

--- metricbeat/module/prometheus/test_prometheus.py
+++ metricbeat/module/prometheus/test_prometheus.py
@@ -2,6 +2,7 @@ import os
 import sys
 import unittest
 
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../tests/system'))
 import metricbeat
 
@@ -34,4 +35,35 @@ class Test(metricbeat.BaseTest):
 
         self.assertCountEqual(self.de_dot(PROMETHEUS_FIELDS), evt.keys(), evt)
 
+        self.assert_fields_are_documented(evt)
+
+
+class TestRemoteWrite(metricbeat.BaseTest):
+
+    COMPOSE_SERVICES = ['prometheus-host-network']
+
+    @unittest.skipUnless(metricbeat.INTEGRATION_TESTS, "integration test")
+    def test_remote_write(self):
+        """
+        prometheus remote_write test
+        """
+        self.render_config_template(modules=[{
+            "name": "prometheus",
+            "metricsets": ["remote_write"],
+            "period": "5s",
+            "host": "localhost",
+            "port": "9201"
+        }])
+        proc = self.start_beat()
+
+        self.wait_until(lambda: self.log_contains("Starting HTTP"))
+
+        self.wait_until(lambda: self.output_lines() > 0, 60)
+        proc.check_kill_and_wait()
+        self.assert_no_logged_warnings()
+
+        output = self.read_output_json()
+        evt = output[0]
+
+        self.assertCountEqual(self.de_dot(PROMETHEUS_FIELDS), evt.keys(), evt)
         self.assert_fields_are_documented(evt)
