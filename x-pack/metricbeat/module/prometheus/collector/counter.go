142b85931e07e8318ea08f61e1d8fe4b4aa560da
Carlos Pérez-Aradros Herce <carlos@elastic.co>
2020-03-24 21:20:09 +0100

    Use Elasticsearch histogram type to store Prometheus histograms (#17061)
    * Allow to override already defined metricsets
    
    This is useful when we want to have different behaviors between flavours
    
    * Add new Prometheus skeleton for basic types usage
    
    * Refactor Prometheus module to accommodate newer uses
    
    * Add typed schema to Prometheus module
    
    This should make room for using different Elasticsearch types depending
    on the Prometheus metric
    
    * Convert Prometheus histograms to ES histograms
    
    Co-authored-by: Chris Mark <chrismarkou92@gmail.com>
    Co-authored-by: Jaime Soriano Pastor <jaime.soriano@elastic.co>

---
+++ x-pack/metricbeat/module/prometheus/collector/counter.go
@@ -0,0 +1,92 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package collector
+
+import (
+	"time"
+
+	"github.com/elastic/beats/v7/libbeat/common"
+)
+
+// CounterCache keeps a cache of the last value of all given counters
+// and allows to calculate their rate since the last call.
+// All methods are thread-unsafe and must not be called concurrently
+type CounterCache interface {
+	// Start the cache cleanup worker. It mus be called once before start using
+	// the cache
+	Start()
+
+	// Stop the cache cleanup worker. It mus be called when the cache is disposed
+	Stop()
+
+	// RateUint64 returns, for a given counter name, the difference between the given value
+	// and the value that was given in a previous call. It will return 0 on the first call
+	RateUint64(counterName string, value uint64) uint64
+
+	// RateFloat64 returns, for a given counter name, the difference between the given value
+	// and the value that was given in a previous call. It will return 0.0 on the first call
+	RateFloat64(counterName string, value float64) float64
+}
+
+type counterCache struct {
+	ints    *common.Cache
+	floats  *common.Cache
+	timeout time.Duration
+}
+
+// NewCounterCache initializes and returns a CounterCache. The timeout parameter will be
+// used to automatically expire counters that hasn't been updated in a whole timeout period
+func NewCounterCache(timeout time.Duration) CounterCache {
+	return &counterCache{
+		ints:    common.NewCache(timeout, 0),
+		floats:  common.NewCache(timeout, 0),
+		timeout: timeout,
+	}
+}
+
+// RateUint64 returns, for a given counter name, the difference between the given value
+// and the value that was given in a previous call. It will return 0 on the first call
+func (c *counterCache) RateUint64(counterName string, value uint64) uint64 {
+	prev := c.ints.PutWithTimeout(counterName, value, c.timeout)
+	if prev != nil {
+		if prev.(uint64) > value {
+			// counter reset
+			return 0
+		}
+		return value - prev.(uint64)
+	}
+
+	// first put for this value, return rate of 0
+	return 0
+}
+
+// RateFloat64 returns, for a given counter name, the difference between the given value
+// and the value that was given in a previous call. It will return 0.0 on the first call
+func (c *counterCache) RateFloat64(counterName string, value float64) float64 {
+	prev := c.floats.PutWithTimeout(counterName, value, c.timeout)
+	if prev != nil {
+		if prev.(float64) > value {
+			// counter reset
+			return 0
+		}
+		return value - prev.(float64)
+	}
+
+	// first put for this value, return rate of 0
+	return 0
+}
+
+// Start the cache cleanup worker. It mus be called once before start using
+// the cache
+func (c *counterCache) Start() {
+	c.ints.StartJanitor(c.timeout)
+	c.floats.StartJanitor(c.timeout)
+}
+
+// Stop the cache cleanup worker. It mus be called when the cache is disposed
+func (c *counterCache) Stop() {
+	c.ints.StopJanitor()
+	c.floats.StopJanitor()
+}
