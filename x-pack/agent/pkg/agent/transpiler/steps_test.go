ca18a46c2d384014209df6c7c8a295520805dba7
Michal Pristas <michal.pristas@gmail.com>
2020-03-27 13:41:12 +0100

    run specific tests

--- x-pack/agent/pkg/agent/transpiler/steps_test.go
+++ x-pack/agent/pkg/agent/transpiler/steps_test.go
@@ -6,28 +6,52 @@ package transpiler
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestIsSubpath(t *testing.T) {
-	testCases := []struct {
+	testCases := map[string][]struct {
 		root       string
 		path       string
 		resultPath string
 		isSubpath  bool
 	}{
-		{"/", "a", "/a", true},
-		{"/a", "b", "/a/b", true},
-		{"/a", "b/c", "/a/b/c", true},
-		{"/a/b", "/a/c", "/a/c", false},
-		{"/a/b", "/a/b/../c", "/a/c", false},
-		{"/a/b", "../c", "/a/c", false},
+		"linux": {
+			{"/", "a", "/a", true},
+			{"/a", "b", "/a/b", true},
+			{"/a", "b/c", "/a/b/c", true},
+			{"/a/b", "/a/c", "/a/c", false},
+			{"/a/b", "/a/b/../c", "/a/c", false},
+			{"/a/b", "../c", "/a/c", false},
+		},
+		"darwin": {
+			{"/", "a", "/a", true},
+			{"/a", "b", "/a/b", true},
+			{"/a", "b/c", "/a/b/c", true},
+			{"/a/b", "/a/c", "/a/c", false},
+			{"/a/b", "/a/b/../c", "/a/c", false},
+			{"/a/b", "../c", "/a/c", false},
+		},
+		"windows": {
+			{"/", "a", "\\a", true},
+			{"/a", "b", "\\a\\b", true},
+			{"/a", "b/c", "\\a\\b\\c", true},
+			{"/a/b", "/a/c", "\\a\\c", false},
+			{"/a/b", "/a/b/../c", "\\a\\c", false},
+			{"/a/b", "../c", "\\a\\c", false},
+		},
 	}
 
-	for _, test := range testCases {
-		t.Run(fmt.Sprintf("'%s'-'%s'", test.root, test.path), func(t *testing.T) {
+	osSpecificTests, found := testCases[runtime.GOOS]
+	if !found {
+		return
+	}
+
+	for _, test := range osSpecificTests {
+		t.Run(fmt.Sprintf("[%s]'%s-%s'", runtime.GOOS, test.root, test.path), func(t *testing.T) {
 			newPath, result := joinPaths(test.root, test.path)
 			assert.Equal(t, test.resultPath, newPath)
 			assert.Equal(t, test.isSubpath, result)
