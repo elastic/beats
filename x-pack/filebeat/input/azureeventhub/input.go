30e2d8694c20774020b6b6a5f7333d691bdaf344
Mariana Dima <mariana@elastic.co>
2020-03-30 16:13:18 +0200

    Cherry-pick #17313 to 7.x: Move azure-eventhub input to GA (#17321)
    * Move azure-eventhub input to GA (#17313)
    
    * Move to GA
    
    * update changelog
    
    * mage fmt update
    
    (cherry picked from commit 4912c8c0fa3993a3eb3921a47e9085d057e13eec)
    
    * changelog

--- x-pack/filebeat/input/azureeventhub/input.go
+++ x-pack/filebeat/input/azureeventhub/input.go
@@ -11,8 +11,6 @@ import (
 	"sync"
 	"time"
 
-	"github.com/elastic/beats/v7/libbeat/common/cfgwarn"
-
 	"github.com/pkg/errors"
 
 	"github.com/elastic/beats/v7/filebeat/channel"
@@ -62,7 +60,6 @@ func NewInput(
 	connector channel.Connector,
 	inputContext input.Context,
 ) (input.Input, error) {
-	cfgwarn.Beta("The %s input is beta", inputName)
 	var config azureInputConfig
 	if err := cfg.Unpack(&config); err != nil {
 		return nil, errors.Wrapf(err, "reading %s input config", inputName)
