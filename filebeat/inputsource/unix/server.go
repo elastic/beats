48cd0b2d03fd2d03fdddfdb889b59eeaa2172851
Andrew Stucki <andrew.stucki@elastic.co>
2020-04-03 18:12:24 -0400

    Fix up unix and add license info

--- filebeat/inputsource/unix/server.go
+++ filebeat/inputsource/unix/server.go
@@ -20,120 +20,38 @@ package unix
 import (
 	"fmt"
 	"net"
-	"sync"
 
 	"golang.org/x/net/netutil"
 
-	"github.com/elastic/beats/filebeat/inputsource/tcp"
 	"github.com/elastic/beats/v7/filebeat/inputsource/common"
-	"github.com/elastic/beats/v7/libbeat/common/atomic"
-	"github.com/elastic/beats/v7/libbeat/logp"
 )
 
-// Server represent a Unix socket server
+// Server represent a unix server
 type Server struct {
-	config       *Config
-	Listener     net.Listener
-	wg           sync.WaitGroup
-	done         chan struct{}
-	factory      tcp.HandlerFactory
-	log          *logp.Logger
-	closer       *tcp.Closer
-	clientsCount atomic.Int
+	*common.Listener
+
+	config *Config
 }
 
 // New creates a new unix server
 func New(
 	config *Config,
-	factory tcp.HandlerFactory,
+	factory common.HandlerFactory,
 ) (*Server, error) {
 	if factory == nil {
 		return nil, fmt.Errorf("HandlerFactory can't be empty")
 	}
 
-	return &Server{
-		config:  config,
-		done:    make(chan struct{}),
-		factory: factory,
-		log:     logp.NewLogger("unix").With("path", config.Path),
-		closer:  NewCloser(nil),
-	}, nil
-}
-
-// Start listens on the Unix socket.
-func (s *Server) Start() error {
-	var err error
-	s.Listener, err = s.createServer()
-	if err != nil {
-		return err
+	server := &Server{
+		config: config,
 	}
+	server.Listener = common.NewListener(common.FamilyUnix, config.Path, factory, server.createServer, &common.ListenerConfig{
+		Timeout:        config.Timeout,
+		MaxMessageSize: config.MaxMessageSize,
+		MaxConnections: config.MaxConnections,
+	})
 
-	s.closer.callback = func() { s.Listener.Close() }
-	s.log.Info("Started listening for Unix socket connection")
-
-	s.wg.Add(1)
-	go func() {
-		defer s.wg.Done()
-		s.run()
-	}()
-	return nil
-}
-
-// Run start and run a new Unix socket listener to receive new data. When a new connection is accepted, the factory
-// is used  to create a ConnectionHandler. The ConnectionHandler takes the connection as input and handles the data
-// that is being received via tha io.Reader. Most clients use the splitHandler which can take a bufio.SplitFunc and
-// parse out each message into an appropriate event. The Close() of the ConnectionHandler can be used to clean up
-// the connection either by client or server based on need.
-func (s *Server) run() {
-	for {
-		conn, err := s.Listener.Accept()
-		if err != nil {
-			select {
-			case <-s.closer.Done():
-				return
-			default:
-				s.log.Debugw("Can not accept the connection", "error", err)
-				continue
-			}
-		}
-
-		handler := s.factory(*s.config)
-		closer := common.WithCloser(s.closer, func() { conn.Close() })
-
-		s.wg.Add(1)
-		go func() {
-			defer logp.Recover("recovering from a unix client crash")
-			defer s.wg.Done()
-			defer closer.Close()
-
-			s.registerHandler()
-			defer s.unregisterHandler()
-			s.log.Debugw("New client", "total", s.clientsCount.Load())
-
-			err := handler.Handle(closer, conn)
-			if err != nil {
-				s.log.Debugw("client error", "error", err)
-			}
-
-			defer s.log.Debugw("client disconnected", "total", s.clientsCount.Load())
-		}()
-	}
-}
-
-// Stop stops accepting new incoming Unix socket connection and Close any active clients
-func (s *Server) Stop() {
-	s.log.Info("Stopping Unix socket server")
-	s.closer.Close()
-	s.wg.Wait()
-	s.log.Info("Unix socket server stopped")
-}
-
-func (s *Server) registerHandler() {
-	s.clientsCount.Inc()
-}
-
-func (s *Server) unregisterHandler() {
-	s.clientsCount.Dec()
+	return server, nil
 }
 
 func (s *Server) createServer() (net.Listener, error) {
--- filebeat/inputsource/unix/server_test.go
+++ filebeat/inputsource/unix/server_test.go
@@ -18,87 +18,260 @@
 package unix
 
 import (
+	"bufio"
+	"fmt"
+	"math/rand"
 	"net"
-	"runtime"
+	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 	"time"
 
+	"github.com/dustin/go-humanize"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/elastic/beats/v7/filebeat/inputsource"
+	netcommon "github.com/elastic/beats/v7/filebeat/inputsource/common"
+	"github.com/elastic/beats/v7/libbeat/common"
 )
 
-const maxMessageSize = 20
-const maxSocketSize = 0
-const timeout = time.Second * 15
+var defaultConfig = Config{
+	Timeout:        time.Minute * 5,
+	MaxMessageSize: 20 * humanize.MiByte,
+}
 
 type info struct {
-	message []byte
+	message string
 	mt      inputsource.NetworkMetadata
 }
 
-func TestReceiveEventFromUDP(t *testing.T) {
+func TestErrorOnEmptyLineDelimiter(t *testing.T) {
+	c := common.NewConfig()
+	config := defaultConfig
+	err := c.Unpack(&config)
+	assert.Error(t, err)
+}
+
+func TestReceiveEventsAndMetadata(t *testing.T) {
+	expectedMessages := generateMessages(5, 100)
+	largeMessages := generateMessages(10, 4096)
+	extraLargeMessages := generateMessages(2, 65*1024)
+	randomGeneratedText := randomString(900000)
+
 	tests := []struct {
-		name     string
-		message  []byte
-		expected []byte
+		name             string
+		cfg              map[string]interface{}
+		splitFunc        bufio.SplitFunc
+		expectedMessages []string
+		messageSent      string
 	}{
 		{
-			name:     "Sending a message under the MaxMessageSize limit",
-			message:  []byte("Hello world"),
-			expected: []byte("Hello world"),
+			name:             "NewLine",
+			cfg:              map[string]interface{}{},
+			splitFunc:        netcommon.SplitFunc([]byte("\n")),
+			expectedMessages: expectedMessages,
+			messageSent:      strings.Join(expectedMessages, "\n"),
 		},
 		{
-			name:     "Sending a message over the MaxMessageSize limit will truncate the message",
-			message:  []byte("Hello world not so nice"),
-			expected: []byte("Hello world not so n"),
+			name:             "NewLineWithCR",
+			cfg:              map[string]interface{}{},
+			splitFunc:        netcommon.SplitFunc([]byte("\r\n")),
+			expectedMessages: expectedMessages,
+			messageSent:      strings.Join(expectedMessages, "\r\n"),
+		},
+		{
+			name:             "CustomDelimiter",
+			cfg:              map[string]interface{}{},
+			splitFunc:        netcommon.SplitFunc([]byte(";")),
+			expectedMessages: expectedMessages,
+			messageSent:      strings.Join(expectedMessages, ";"),
+		},
+		{
+			name:             "MultipleCharsCustomDelimiter",
+			cfg:              map[string]interface{}{},
+			splitFunc:        netcommon.SplitFunc([]byte("<END>")),
+			expectedMessages: expectedMessages,
+			messageSent:      strings.Join(expectedMessages, "<END>"),
+		},
+		{
+			name:             "SingleCharCustomDelimiterMessageWithoutBoundaries",
+			cfg:              map[string]interface{}{},
+			splitFunc:        netcommon.SplitFunc([]byte(";")),
+			expectedMessages: []string{"hello"},
+			messageSent:      "hello",
+		},
+		{
+			name:             "MultipleCharCustomDelimiterMessageWithoutBoundaries",
+			cfg:              map[string]interface{}{},
+			splitFunc:        netcommon.SplitFunc([]byte("<END>")),
+			expectedMessages: []string{"hello"},
+			messageSent:      "hello",
+		},
+		{
+			name:             "NewLineMessageWithoutBoundaries",
+			cfg:              map[string]interface{}{},
+			splitFunc:        netcommon.SplitFunc([]byte("\n")),
+			expectedMessages: []string{"hello"},
+			messageSent:      "hello",
+		},
+		{
+			name:             "NewLineLargeMessagePayload",
+			cfg:              map[string]interface{}{},
+			splitFunc:        netcommon.SplitFunc([]byte("\n")),
+			expectedMessages: largeMessages,
+			messageSent:      strings.Join(largeMessages, "\n"),
+		},
+		{
+			name:             "CustomLargeMessagePayload",
+			cfg:              map[string]interface{}{},
+			splitFunc:        netcommon.SplitFunc([]byte(";")),
+			expectedMessages: largeMessages,
+			messageSent:      strings.Join(largeMessages, ";"),
+		},
+		{
+			name:             "ReadRandomLargePayload",
+			cfg:              map[string]interface{}{},
+			splitFunc:        netcommon.SplitFunc([]byte("\n")),
+			expectedMessages: []string{randomGeneratedText},
+			messageSent:      randomGeneratedText,
+		},
+		{
+			name:      "MaxReadBufferReachedUserConfigured",
+			splitFunc: netcommon.SplitFunc([]byte("\n")),
+			cfg: map[string]interface{}{
+				"max_message_size": 50000,
+			},
+			expectedMessages: []string{},
+			messageSent:      randomGeneratedText,
+		},
+		{
+			name:      "MaxBufferSizeSet",
+			splitFunc: netcommon.SplitFunc([]byte("\n")),
+			cfg: map[string]interface{}{
+				"max_message_size": 66 * 1024,
+			},
+			expectedMessages: extraLargeMessages,
+			messageSent:      strings.Join(extraLargeMessages, "\n"),
 		},
 	}
-
-	ch := make(chan info)
-	host := "localhost:0"
-	config := &Config{
-		Host:           host,
-		MaxMessageSize: maxMessageSize,
-		Timeout:        timeout,
-		ReadBuffer:     maxSocketSize,
-	}
-	fn := func(message []byte, metadata inputsource.NetworkMetadata) {
-		ch <- info{message: message, mt: metadata}
-	}
-	s := New(config, fn)
-	err := s.Start()
-	if !assert.NoError(t, err) {
-		return
-	}
-	defer s.Stop()
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			conn, err := net.Dial("udp", s.Listener.LocalAddr().String())
+			ch := make(chan *info, len(test.expectedMessages))
+			defer close(ch)
+			to := func(message []byte, mt inputsource.NetworkMetadata) {
+				ch <- &info{message: string(message), mt: mt}
+			}
+			path := filepath.Join(os.TempDir(), "test.sock")
+			test.cfg["path"] = path
+			cfg, _ := common.NewConfigFrom(test.cfg)
+			config := defaultConfig
+			err := cfg.Unpack(&config)
 			if !assert.NoError(t, err) {
 				return
 			}
-			defer conn.Close()
 
-			_, err = conn.Write(test.message)
+			factory := SplitHandlerFactory(to, test.splitFunc)
+			server, err := New(&config, factory)
 			if !assert.NoError(t, err) {
 				return
 			}
-			info := <-ch
-			assert.Equal(t, test.expected, info.message)
-			if runtime.GOOS == "windows" {
-				if len(test.expected) < len(test.message) {
-					assert.Nil(t, info.mt.RemoteAddr)
-					assert.True(t, info.mt.Truncated)
-				} else {
-					assert.NotNil(t, info.mt.RemoteAddr)
-					assert.False(t, info.mt.Truncated)
+			err = server.Start()
+			if !assert.NoError(t, err) {
+				return
+			}
+			defer server.Stop()
+
+			conn, err := net.Dial("unix", path)
+			require.NoError(t, err)
+			fmt.Fprint(conn, test.messageSent)
+			conn.Close()
+
+			var events []*info
+
+			for len(events) < len(test.expectedMessages) {
+				select {
+				case event := <-ch:
+					events = append(events, event)
+				default:
 				}
-			} else {
-				assert.NotNil(t, info.mt.RemoteAddr)
-				assert.False(t, info.mt.Truncated)
+			}
+
+			for idx, e := range events {
+				assert.Equal(t, test.expectedMessages[idx], e.message)
 			}
 		})
 	}
+}
+
+func TestReceiveNewEventsConcurrently(t *testing.T) {
+	workers := 4
+	eventsCount := 100
+	path := filepath.Join(os.TempDir(), "test.sock")
+	ch := make(chan *info, eventsCount*workers)
+	defer close(ch)
+	to := func(message []byte, mt inputsource.NetworkMetadata) {
+		ch <- &info{message: string(message), mt: mt}
+	}
+	cfg, err := common.NewConfigFrom(map[string]interface{}{"path": path})
+	if !assert.NoError(t, err) {
+		return
+	}
+	config := defaultConfig
+	err = cfg.Unpack(&config)
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	factory := SplitHandlerFactory(to, bufio.ScanLines)
+
+	server, err := New(&config, factory)
+	if !assert.NoError(t, err) {
+		return
+	}
+	err = server.Start()
+	if !assert.NoError(t, err) {
+		return
+	}
+	defer server.Stop()
+
+	samples := generateMessages(eventsCount, 1024)
+	for w := 0; w < workers; w++ {
+		go func() {
+			conn, err := net.Dial("unix", path)
+			defer conn.Close()
+			assert.NoError(t, err)
+			for _, sample := range samples {
+				fmt.Fprintln(conn, sample)
+			}
+		}()
+	}
+
+	var events []*info
+	for len(events) < eventsCount*workers {
+		select {
+		case event := <-ch:
+			events = append(events, event)
+		default:
+		}
+	}
+}
+
+func randomString(l int) string {
+	charsets := []byte("abcdefghijklmnopqrstuvwzyzABCDEFGHIJKLMNOPQRSTUVWZYZ0123456789")
+	message := make([]byte, l)
+	for i := range message {
+		message[i] = charsets[rand.Intn(len(charsets))]
+	}
+	return string(message)
+}
+
+func generateMessages(c int, l int) []string {
+	messages := make([]string, c)
+	for i := range messages {
+		messages[i] = randomString(l)
+	}
+	return messages
 }
