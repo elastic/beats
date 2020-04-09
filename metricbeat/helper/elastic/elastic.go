3edc026d6ffef76fc64c5d0bedfe200a98385f1d
Shaunak Kashyap <ycombinator@gmail.com>
2020-04-09 05:41:10 -0700

    s/ReConfigure/Reconfigure/

--- metricbeat/helper/elastic/elastic.go
+++ metricbeat/helper/elastic/elastic.go
@@ -158,7 +158,7 @@ func ConfigureModule(base *mb.BaseModule, xpackEnabledMetricsets []string, regis
 		return errors.Wrapf(err, "could not create new configuration for module %v", moduleName)
 	}
 
-	if err := base.ReConfigure(newConfig, register); err != nil {
+	if err := base.Reconfigure(newConfig, register); err != nil {
 		return errors.Wrapf(err, "could not reconfigure module %v", moduleName)
 	}
 
--- metricbeat/mb/mb_test.go
+++ metricbeat/mb/mb_test.go
@@ -409,7 +409,7 @@ func TestModuleConfigQueryParams(t *testing.T) {
 	assert.NotEqual(t, "&", res[len(res)-1])
 }
 
-func TestModuleReConfigure(t *testing.T) {
+func TestModuleReconfigure(t *testing.T) {
 	mockRegistry := NewRegister()
 
 	const moduleName = "test_module"
@@ -464,7 +464,7 @@ func TestModuleReConfigure(t *testing.T) {
 			m, _, err := NewModule(common.MustNewConfigFrom(initConfig), mockRegistry)
 			require.NoError(t, err)
 
-			err = m.ReConfigure(common.MustNewConfigFrom(test.newConfig), mockRegistry)
+			err = m.Reconfigure(common.MustNewConfigFrom(test.newConfig), mockRegistry)
 
 			var expectedNewConfig metricSetConfig
 			if test.expectedErrMsg == "" {
