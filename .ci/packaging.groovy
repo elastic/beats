2dd22e991b35f3b6a0a7ec69187c2b5340086c88
Ivan Fernandez Calvo <kuisathaverat@gmail.com>
2020-04-15 14:56:17 +0200

    fix: make beats in parallel by architecture

--- .ci/packaging.groovy
+++ .ci/packaging.groovy
@@ -8,6 +8,8 @@ pipeline {
     BASE_DIR = 'src/github.com/elastic/beats'
     JOB_GCS_BUCKET = 'beats-ci-artifacts'
     JOB_GCS_CREDENTIALS = 'beats-ci-gcs-plugin'
+    DOCKERELASTIC_SECRET = 'secret/observability-team/ci/docker-registry/prod'
+    DOCKER_REGISTRY = 'docker.elastic.co'
     SNAPSHOT = "true"
   }
   options {
@@ -32,6 +34,7 @@ pipeline {
       steps {
         deleteDir()
         gitCheckout(basedir: "${BASE_DIR}")
+        setEnvVar("GO_VERSION", readFile("${BASE_DIR}/.go-version").trim())
         stash allowEmpty: true, name: 'source', useDefaultExcludes: false
       }
     }
@@ -51,7 +54,26 @@ pipeline {
               'windows/386',
               'windows/amd64'
             )
-          }
+            axis {
+              name 'BEATS_FOLDER'
+              values (
+                'auditbeat',
+                'filebeat',
+                'heartbeat',
+                'journalbeat',
+                'metricbeat',
+                'packetbeat',
+                'winlogbeat',
+                'x-pack/auditbeat',
+                'x-pack/filebeat',
+                'x-pack/functionbeat',
+                'x-pack/heartbeat',
+                'x-pack/journalbeat',
+                'x-pack/metricbeat',
+                'x-pack/packetbeat',
+                'x-pack/winlogbeat'
+              )
+            }
         }
         stages {
           stage('Package'){
@@ -72,19 +94,19 @@ pipeline {
 }
 
 def release(){
-  dir("${BASE_DIR}"){
+  withBeatsEnv(){
     if(env.PLATFORMS == 'darwin' && params.macos){
       withMaskEnv( vars: [
           [var: "KEYCHAIN_PASS", password: getVaultSecret(secret: "secret/jenkins-ci/macos-codesign-keychain").data.password],
           [var: "KEYCHAIN", password: "/var/lib/jenkins/Library/Keychains/Elastic.keychain-db"],
           [var: "APPLE_SIGNING_ENABLED", password: "true"],
       ]){
-        sh(label: "Release ${env.PLATFORMS}", script: './dev-tools/jenkins_release.sh')
+        sh(label: "Release ${env.BEATS_FOLDER} ${env.PLATFORMS}", script: 'mage package')
       }
     } else if (env.PLATFORMS != 'darwin' && params.linux){
-      sh(label: "Release ${env.PLATFORMS}", script: './dev-tools/jenkins_release.sh')
+      sh(label: "Release ${env.BEATS_FOLDER} ${env.PLATFORMS}", script: 'mage package')
     } else {
-      unstable("Release for ${env.PLATFORMS} Not executed")
+      unstable("Release for ${env.BEATS_FOLDER} ${env.PLATFORMS} Not executed")
     }
   }
 }
@@ -97,4 +119,29 @@ def publishPackages(){
     sharedPublicly: true,
     showInline: true
   )
+}
+
+def withBeatsEnv(Closure body) {
+  def os = goos()
+  def goRoot = "${env.WORKSPACE}/.gvm/versions/go${GO_VERSION}.${os}.amd64"
+
+  withEnv([
+    "HOME=${env.WORKSPACE}",
+    "GOPATH=${env.WORKSPACE}",
+    "GOROOT=${goRoot}",
+    "PATH=${env.WORKSPACE}/bin:${goRoot}/bin:${env.PATH}",
+    "MAGEFILE_CACHE=${WORKSPACE}/.magefile",
+    "PYTHON_ENV=${WORKSPACE}/python-env",
+  ]) {
+    deleteDir()
+    unstash 'source'
+    dir("${env.BASE_DIR}/${env.BEATS_FOLDER}") {
+      sh(label: "Install Go ${GO_VERSION}", script: ".ci/scripts/install-go.sh")
+      sh(label: "Install docker-compose ${DOCKER_COMPOSE_VERSION}", script: ".ci/scripts/install-docker-compose.sh")
+      sh(label: "Install Mage", script: "make mage")
+      dockerLogin(secret: "${DOCKERELASTIC_SECRET}", registry: "${DOCKER_REGISTRY}")
+      sh(label: 'workaround packer cache', '.ci/packer_cache.sh')
+      body()
+    }
+  }
 }
