8a03547d2a369e94956cfb84c862ca671f171129
Shaunak Kashyap <ycombinator@gmail.com>
2020-04-16 18:39:54 -0700

    Trying just 1 iteration

--- libbeat/publisher/pipeline/controller_test.go
+++ libbeat/publisher/pipeline/controller_test.go
@@ -104,7 +104,7 @@ func TestOutputReload(t *testing.T) {
 				return waitUntilTrue(timeout, func() bool {
 					return uint(numEventsToPublish) == publishedCount.Load()
 				})
-			}, &quick.Config{MaxCount: 10})
+			}, &quick.Config{MaxCount: 1})
 
 			if err != nil {
 				t.Error(err)
