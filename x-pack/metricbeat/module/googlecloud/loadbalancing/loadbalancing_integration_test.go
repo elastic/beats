2399990cf097e9391aafbb44d095392e96e3fa64
kaiyan-sheng <kaiyan.sheng@elastic.co>
2020-04-22 10:40:44 -0600

    Improve TestData integration test to generate more specific data-*.json

--- x-pack/metricbeat/module/googlecloud/loadbalancing/loadbalancing_integration_test.go
+++ x-pack/metricbeat/module/googlecloud/loadbalancing/loadbalancing_integration_test.go
@@ -8,14 +8,38 @@
 package loadbalancing
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/elastic/beats/v7/libbeat/common"
 	mbtest "github.com/elastic/beats/v7/metricbeat/mb/testing"
 	"github.com/elastic/beats/v7/x-pack/metricbeat/module/googlecloud"
 )
 
 func TestData(t *testing.T) {
+	metricPrefixIs := func(metricPrefix string) func(e common.MapStr) bool {
+		return func(e common.MapStr) bool {
+			v, err := e.GetValue(metricPrefix)
+			return err == nil && v != nil
+		}
+	}
+
+	dataFiles := []struct {
+		metricPrefix string
+		path         string
+	}{
+		{"googlecloud.loadbalancing", "./_meta/data.json"},
+		{"googlecloud.loadbalancing.https", "./_meta/data_https.json"},
+		{"googlecloud.loadbalancing.l3", "./_meta/data_l3.json"},
+		{"googlecloud.loadbalancing.tcp_ssl_proxy", "./_meta/data_tcp_ssl_proxy.json"},
+	}
+
 	config := googlecloud.GetConfigForTest(t, "loadbalancing")
-	metricSet := mbtest.NewFetcher(t, config)
-	metricSet.WriteEvents(t, "/")
+
+	for _, df := range dataFiles {
+		metricSet := mbtest.NewFetcher(t, config)
+		t.Run(fmt.Sprintf("metric prefix: %s", df.metricPrefix), func(t *testing.T) {
+			metricSet.WriteEventsCond(t, df.path, metricPrefixIs(df.metricPrefix))
+		})
+	}
 }
