2fe4b43034ff5fccc6e83daf64e96ab474dd6700
kaiyan-sheng <kaiyan.sheng@elastic.co>
2020-04-24 14:48:07 -0600

    [Metricbeat GCP] Improve integration test to generate more specific data-*.json (#17906)
    * Improve TestData integration test to generate more specific data-*.json
    
    * rerun TestData to generate data.json files

--- x-pack/metricbeat/module/googlecloud/compute/compute_integration_test.go
+++ x-pack/metricbeat/module/googlecloud/compute/compute_integration_test.go
@@ -8,14 +8,39 @@
 package compute
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/elastic/beats/v7/libbeat/common"
 	mbtest "github.com/elastic/beats/v7/metricbeat/mb/testing"
 	"github.com/elastic/beats/v7/x-pack/metricbeat/module/googlecloud/stackdriver"
 )
 
 func TestData(t *testing.T) {
+	metricPrefixIs := func(metricPrefix string) func(e common.MapStr) bool {
+		return func(e common.MapStr) bool {
+			v, err := e.GetValue(metricPrefix)
+			return err == nil && v != nil
+		}
+	}
+
+	dataFiles := []struct {
+		metricPrefix string
+		path         string
+	}{
+		{"googlecloud.compute.instance", "./_meta/data.json"},
+		{"googlecloud.compute.instance.disk", "./_meta/data_disk.json"},
+		{"googlecloud.compute.instance.network", "./_meta/data_network.json"},
+		{"googlecloud.compute.instance.cpu", "./_meta/data_cpu.json"},
+		{"googlecloud.compute.firewall", "./_meta/data_firewall.json"},
+	}
+
 	config := stackdriver.GetConfigForTest(t, "compute")
-	metricSet := mbtest.NewFetcher(t, config)
-	metricSet.WriteEvents(t, "/")
+
+	for _, df := range dataFiles {
+		metricSet := mbtest.NewFetcher(t, config)
+		t.Run(fmt.Sprintf("metric prefix: %s", df.metricPrefix), func(t *testing.T) {
+			metricSet.WriteEventsCond(t, df.path, metricPrefixIs(df.metricPrefix))
+		})
+	}
 }
