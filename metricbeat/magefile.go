685855028aed20eae56699c42d5dc1091ab55cf6
Blake Rouse <blake.rouse@elastic.co>
2020-04-28 13:46:13 -0400

    Cherry-pick #17799 to 7.x: Update metricbeat to use mage for all scenarios along with Makefile shim (#17951)
    * Update metricbeat to use mage for all scenarios along with Makefile shim (#17799)
    
    * Update metricbeat to use mage and the mage make targets.
    
    * Run update and fmt.
    
    * Remove assets.go as its no longer used.
    
    * Split apart the metricbeat tests in travis into multiple jobs to take less time and not run over the timeline.
    
    * Add back in create-metricset with mage.
    
    * Re-add test_xpack_base.py.
    
    * Add kafka to requirements.txt.
    
    * Fix kafka to work with python 3.7.
    
    * Remove the extra kafka-python from rebase on master.
    
    * Fix issues from code review.
    
    * Add back in comment from merge.
    
    * Fix comment spacing.
    
    * Fix update target in generate metricbeat.
    
    * Update magefile in metricbeat generator.
    
    * Run fmt and update.
    
    (cherry picked from commit 8786d05e3e55a46921dc2be195b7e7254ff0465c)
    
    * Fix merge issue.
    
    * Run fmt and update.

--- metricbeat/magefile.go
+++ metricbeat/magefile.go
@@ -27,6 +27,7 @@ import (
 	"time"
 
 	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
 
 	devtools "github.com/elastic/beats/v7/dev-tools/mage"
 	metricbeat "github.com/elastic/beats/v7/metricbeat/scripts/mage"
@@ -46,14 +47,12 @@ import (
 	// mage:import
 	"github.com/elastic/beats/v7/dev-tools/mage/target/unittest"
 	// mage:import
-	"github.com/elastic/beats/v7/dev-tools/mage/target/update"
-	// mage:import
 	_ "github.com/elastic/beats/v7/dev-tools/mage/target/compose"
 )
 
 func init() {
-	common.RegisterCheckDeps(update.Update)
-	test.RegisterDeps(GoIntegTest, PythonIntegTest)
+	common.RegisterCheckDeps(Update)
+	test.RegisterDeps(IntegTest)
 	unittest.RegisterGoTestDeps(Fields)
 	unittest.RegisterPythonTestDeps(Fields)
 
@@ -75,8 +74,9 @@ func Package() {
 
 	devtools.UseElasticBeatOSSPackaging()
 	metricbeat.CustomizePackaging()
+	devtools.PackageKibanaDashboardsFromBuildDir()
 
-	mg.Deps(update.Update, metricbeat.PrepareModulePackagingOSS)
+	mg.Deps(Update)
 	mg.Deps(build.CrossBuild, build.CrossBuildGoDaemon)
 	mg.SerialDeps(devtools.Package, TestPackages)
 }
@@ -100,12 +100,6 @@ func Dashboards() error {
 // Config generates both the short and reference configs.
 func Config() {
 	mg.Deps(configYML, metricbeat.GenerateDirModulesD)
-}
-
-// Imports generates an include/list_{suffix}.go file containing
-// a import statement for each module and dataset.
-func Imports() error {
-	return metricbeat.GenerateOSSMetricbeatModuleIncludeListGo()
 }
 
 func configYML() error {
@@ -133,9 +127,24 @@ func MockedTests(ctx context.Context) error {
 	return devtools.GoTest(ctx, params)
 }
 
-// Fields generates a fields.yml for the Beat.
-func Fields() error {
+// Fields generates a fields.yml and fields.go for each module.
+func Fields() {
+	mg.Deps(fieldsYML, moduleFieldsGo)
+}
+
+func fieldsYML() error {
 	return devtools.GenerateFieldsYAML("module")
+}
+
+func moduleFieldsGo() error {
+	return devtools.GenerateModuleFieldsGo("module")
+}
+
+// Update is an alias for running fields, dashboards, config.
+func Update() {
+	mg.SerialDeps(Fields, Dashboards, Config,
+		metricbeat.PrepareModulePackagingOSS,
+		metricbeat.GenerateOSSMetricbeatModuleIncludeListGo)
 }
 
 // FieldsDocs generates docs/fields.asciidoc containing all fields
@@ -168,7 +177,9 @@ func IntegTest() {
 // Use TEST_TAGS=tag1,tag2 to add additional build tags.
 // Use MODULE=module to run only tests for `module`.
 func GoIntegTest(ctx context.Context) error {
-	mg.Deps(Fields)
+	if !devtools.IsInIntegTestEnv() {
+		mg.SerialDeps(Fields, Dashboards)
+	}
 	return devtools.GoTestIntegrationForModule(ctx)
 }
 
@@ -184,5 +195,31 @@ func PythonIntegTest(ctx context.Context) error {
 	return devtools.RunIntegTest("pythonIntegTest", func() error {
 		mg.Deps(devtools.BuildSystemTestBinary)
 		return devtools.PythonNoseTest(devtools.DefaultPythonTestIntegrationArgs())
-	}, devtools.ListMatchingEnvVars("NOSE_")...)
+	})
+}
+
+// CreateMetricset creates a new metricset.
+//
+// Required ENV variables:
+// * MODULE: Name of the module
+// * METRICSET: Name of the metricset
+func CreateMetricset(ctx context.Context) error {
+	ve, err := devtools.PythonVirtualenv()
+	if err != nil {
+		return err
+	}
+	python, err := devtools.LookVirtualenvPath(ve, "python")
+	if err != nil {
+		return err
+	}
+	path, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	_, err = sh.Exec(
+		map[string]string{}, os.Stdout, os.Stderr, python, "scripts/create_metricset.py",
+		"--path", path, "--module", os.Getenv("MODULE"), "--metricset", os.Getenv("METRICSET"),
+	)
+	return err
 }
