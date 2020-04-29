ea7bbbe8c79f6fe4e9acec7472572df26bc36994
kaiyan-sheng <kaiyan.sheng@elastic.co>
2020-04-29 17:25:32 -0600

    change *.count object_type_mapping_type back to long

--- x-pack/metricbeat/module/statsd/fields.go
+++ x-pack/metricbeat/module/statsd/fields.go
@@ -19,5 +19,5 @@ func init() {
 // AssetStatsd returns asset data.
 // This is the base64 encoded gzipped contents of module/statsd.
 func AssetStatsd() string {
-	return "eJykkk1ugzAQhfc+xRObSEjJAbzoJXqAyMET5MZ4LM+gittXAVIZFRZVZvl+4BvbZzxoshB1Kt4AGjSSRfM5C40BPElXQtbAyeLDAMBiYmA/RjJAoUhOyKJ3BrgHil7snDwjuYGq7z9Hp/zMFh7zqtSVunZqLx2PSU+/zqvNty/qtJIX4bq4kVO/710Hl3NI/Rps2qbK7az6mnXlmYaK7JG2/6W8R3YH5puYA2kJnWyuILobRbm05hBwA/eg6ZuL/2sdoh1grUjL76tzqx/NTwAAAP//ScW1Rw=="
+	return "eJyUkktuwyAQhvec4pc3kSwlB2DRS/QAETETiwYziBmr8u0rP1KR1l5klv/D/gY440GThahT8QbQoJEsms9FaAzgSboSsgZOFh8GAFYTA/sxkgEKRXJCFr0zwD1Q9GKX5BnJDVR9fx6d8pwtPOZNqSt17dReOh6Tnn6dZ5tvX9RpJa/CdXUjp37fuw4u55D6LdjMyaaK7mz7nG3rBYiK7MG274LeI7sD8y9p+x7mQFpCJy+3EN2Nolxacwj4Aveg6ZuL/28doh1gbUjr76tzq9/NTwAAAP//fPG2zQ=="
 }
