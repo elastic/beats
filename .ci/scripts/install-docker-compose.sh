500e0d917b953243a54d818b1f4563579e5f18d3
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-04-29 15:04:04 +0100

    ci: revert to the previous setup

--- .ci/scripts/install-docker-compose.sh
+++ .ci/scripts/install-docker-compose.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
-set -exuo pipefail
 
-function retryCommand() {
-    if [ -e /usr/local/bin/bash_standard_lib.sh ] ; then
-        # shellcheck disable=SC1091
-        source /usr/local/bin/bash_standard_lib.sh
-        retry 3 "$@"
-    else
-        "$@"
-    fi
-}
+set -exuo pipefail
 
 MSG="parameter missing."
 DOCKER_COMPOSE_VERSION=${DOCKER_COMPOSE_VERSION:?$MSG}
@@ -18,5 +9,5 @@ DC_CMD="${HOME}/bin/docker-compose"
 
 mkdir -p "${HOME}/bin"
 
-retryCommand curl -sSLo "${DC_CMD}" "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)"
+curl -sSLo "${DC_CMD}" "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)"
 chmod +x "${DC_CMD}"
--- .ci/scripts/install-go.sh
+++ .ci/scripts/install-go.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 set -exuo pipefail
 
-function retryCommand() {
-    if [ -e /usr/local/bin/bash_standard_lib.sh ] ; then
-        # shellcheck disable=SC1091
-        source /usr/local/bin/bash_standard_lib.sh
-        retry 3 "$@"
-    else
-        "$@"
-    fi
-}
-
 MSG="parameter missing."
 GO_VERSION=${GO_VERSION:?$MSG}
 PROPERTIES_FILE=${PROPERTIES_FILE:-"go_env.properties"}
@@ -20,7 +10,7 @@ GVM_CMD="${HOME}/bin/gvm"
 
 mkdir -p "${HOME}/bin"
 
-retryCommand curl -sSLo "${GVM_CMD}" "https://github.com/andrewkroh/gvm/releases/download/v0.2.2/gvm-${ARCH}-amd64"
+curl -sSLo "${GVM_CMD}" "https://github.com/andrewkroh/gvm/releases/download/v0.2.2/gvm-${ARCH}-amd64"
 chmod +x "${GVM_CMD}"
 
 gvm ${GO_VERSION}|cut -d ' ' -f 2|tr -d '\"' > ${PROPERTIES_FILE}
