1688077337e3cdc41a776d4bc42cbf06e8bb00c9
kaiyan-sheng <kaiyan.sheng@elastic.co>
2020-04-30 08:41:44 -0600

    try remove object_type_mapping_type from labels.*

--- x-pack/metricbeat/module/statsd/fields.go
+++ x-pack/metricbeat/module/statsd/fields.go
@@ -19,5 +19,5 @@ func init() {
 // AssetStatsd returns asset data.
 // This is the base64 encoded gzipped contents of module/statsd.
 func AssetStatsd() string {
-	return "eJyUkktuwyAQhvec4pc3kSwlB2DRS/QAETETiwYziBmr8u0rP1KR1l5klv/D/gY440GThahT8QbQoJEsms9FaAzgSboSsgZOFh8GAFYTA/sxkgEKRXJCFr0zwD1Q9GKX5BnJDVR9fx6d8pwtPOZNqSt17dReOh6Tnn6dZ5tvX9RpJa/CdXUjp37fuw4u55D6LdjMyaaK7mz7nG3rBYiK7MG274LeI7sD8y9p+x7mQFpCJy+3EN2Nolxacwj4Aveg6ZuL/28doh1gbUjr76tzq9/NTwAAAP//fPG2zQ=="
+	return "eJx0UEuOhCAQ3XOKFzadkHQfgMVcYg7QQSkNIwqBMhNvP1Fkgklby/epqveemGjTyGw4WwGwY08a8vsApAAs5T65yC4sGl8CAAqJOdjVkwASeTKZNEYjgMGRt1kfyicWM1Ozfx/e4q5NYY0n0lpa20O91OMfrc7Q/VDPDVyAd2EHH8wN+Z5NjG4ZT6VUstF9yFmn5iVOrs+XYN505PNLidsHL89NtP2GVIu4OXmeK6ubTtqa/wIAAP//hgt7mw=="
 }
