b7fd5a65ea14fc091bb789b2541393e19a8a2a57
Juan Álvarez <juan.alvarez@elastic.co>
2020-05-04 15:27:00 +0200

    fix import

--- libbeat/outputs/elasticsearch/client.go
+++ libbeat/outputs/elasticsearch/client.go
@@ -27,8 +27,6 @@ import (
 
 	"go.elastic.co/apm"
 
-	"github.com/elastic/beats/v7/libbeat/testing"
-
 	"github.com/elastic/beats/v7/libbeat/beat"
 	"github.com/elastic/beats/v7/libbeat/common"
 	"github.com/elastic/beats/v7/libbeat/esleg/eslegclient"
