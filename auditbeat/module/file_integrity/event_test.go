50b424ede8cdc5eb402d272ac6b64457a28a4055
Andrew Stucki <andrew.stucki@elastic.co>
2020-05-05 15:26:48 -0400

    [Auditbeat] File Integrity ECS update (#18012)
    * Add extension, mime_type and drive_letter
    
    * run go mod vendor
    
    * Add ECS categorization fields
    
    * mage fmt
    
    * Update hash and NOTICE
    
    * Update test
    
    * Run mage vendor
    
    * Add changelog entry
    
    * Extract isASCIILetter
    
    * switch over to godoc style comment
    
    (cherry picked from commit 56ba9d089cb9441af65caf4cd534f1aec1726cbc)

--- auditbeat/module/file_integrity/event_test.go
+++ auditbeat/module/file_integrity/event_test.go
@@ -37,7 +37,7 @@ var testEventTime = time.Now().UTC()
 func testEvent() *Event {
 	return &Event{
 		Timestamp: testEventTime,
-		Path:      "/home/user",
+		Path:      "/home/user/file.txt",
 		Source:    SourceScan,
 		Action:    ConfigChange,
 		Info: &Metadata{
@@ -290,8 +290,12 @@ func TestBuildEvent(t *testing.T) {
 		assert.Equal(t, testEventTime, e.Timestamp)
 
 		assertHasKey(t, fields, "event.action")
+		assertHasKey(t, fields, "event.kind")
+		assertHasKey(t, fields, "event.category")
+		assertHasKey(t, fields, "event.type")
 
 		assertHasKey(t, fields, "file.path")
+		assertHasKey(t, fields, "file.extension")
 		assertHasKey(t, fields, "file.target_path")
 		assertHasKey(t, fields, "file.inode")
 		assertHasKey(t, fields, "file.uid")
@@ -309,8 +313,50 @@ func TestBuildEvent(t *testing.T) {
 			assertHasKey(t, fields, "file.mode")
 		}
 
+		assertHasKey(t, fields, "file.hash.sha1")
+		assertHasKey(t, fields, "file.hash.sha256")
+		// Remove in 8.x
 		assertHasKey(t, fields, "hash.sha1")
 		assertHasKey(t, fields, "hash.sha256")
+	})
+	if runtime.GOOS == "windows" {
+		t.Run("drive letter", func(t *testing.T) {
+			e := testEvent()
+			e.Path = "c:\\Documents"
+			fields := buildMetricbeatEvent(e, false).MetricSetFields
+			value, err := fields.GetValue("file.drive_letter")
+			assert.NoError(t, err)
+			assert.Equal(t, "C", value)
+		})
+		t.Run("no drive letter", func(t *testing.T) {
+			e := testEvent()
+			e.Path = "\\\\remote\\Documents"
+			fields := buildMetricbeatEvent(e, false).MetricSetFields
+			_, err := fields.GetValue("file.drive_letter")
+			assert.Error(t, err)
+		})
+	}
+	t.Run("ecs categorization", func(t *testing.T) {
+		e := testEvent()
+		e.Action = ConfigChange
+		fields := buildMetricbeatEvent(e, false).MetricSetFields
+		types, err := fields.GetValue("event.type")
+		if err != nil {
+			t.Fatal(err)
+		}
+		ecsTypes, ok := types.([]string)
+		assert.True(t, ok)
+		assert.Equal(t, []string{"change"}, ecsTypes)
+
+		e.Action = Action(Created | Updated | Deleted)
+		fields = buildMetricbeatEvent(e, false).MetricSetFields
+		types, err = fields.GetValue("event.type")
+		if err != nil {
+			t.Fatal(err)
+		}
+		ecsTypes, ok = types.([]string)
+		assert.True(t, ok)
+		assert.Equal(t, []string{"change", "creation", "deletion"}, ecsTypes)
 	})
 	t.Run("no setuid/setgid", func(t *testing.T) {
 		e := testEvent()
