72a8652711c481f641b05636200307853a48c761
P1llus <pillus@chasenet.org>
2020-05-12 14:18:38 +0200

    change packagename

--- x-pack/filebeat/input/http_endpoint/config.go
+++ x-pack/filebeat/input/http_endpoint/config.go
@@ -2,7 +2,7 @@
 // or more contributor license agreements. Licensed under the Elastic License;
 // you may not use this file except in compliance with the Elastic License.
 
-package httpendpoint
+package http_endpoint
 
 import (
 	"github.com/elastic/beats/v7/libbeat/common/transport/tlscommon"
