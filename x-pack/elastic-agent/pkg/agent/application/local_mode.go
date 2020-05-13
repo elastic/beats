5d5de168ba08474ba591b9412932a562a15fc452
Blake Rouse <blake.rouse@elastic.co>
2020-05-13 07:56:41 -0400

    [Elastic Agent] Add params `prefer_v2_templates=true` on the ES output (#18318) (#18464)
    * [Elastic Agent] Add params `prefer_v2_templates=true` on the ES output
    
    To make sure we use the v2 templates we must send on bulk request the
    params `prefer_v2_templates=true` if not it will default to v1 and will
    not try to use the v2 template at all.
    
    Reference: #17809
    
    * [Libbeat] Respect the `parameters` option defined in the ES output.
    
    This make sure that any values defined in the `parameters` option of the
    ES output is correctly added to every bulk request query string.
    
    Fixes: #18325
    
    
    * Adding tests
    
    Co-authored-by: Nicolas Ruflin <spam@ruflin.com>
    (cherry picked from commit d152d0833debbdea35ba763b5c9c1c181054dbe7)
    
    Co-authored-by: Pier-Hugues Pellerin <phpellerin@gmail.com>

--- x-pack/elastic-agent/pkg/agent/application/local_mode.go
+++ x-pack/elastic-agent/pkg/agent/application/local_mode.go
@@ -92,7 +92,15 @@ func newLocal(
 	}
 
 	discover := discoverer(pathConfigFile, c.Management.Path)
-	emit := emitter(log, router, &configModifiers{Decorators: []decoratorFunc{injectMonitoring}, Filters: []filterFunc{filters.ConstraintFilter}}, monitor)
+	emit := emitter(
+		log,
+		router,
+		&configModifiers{
+			Decorators: []decoratorFunc{injectMonitoring, injectPreferV2Template},
+			Filters:    []filterFunc{filters.ConstraintFilter},
+		},
+		monitor,
+	)
 
 	var cfgSource source
 	if !c.Management.Reload.Enabled {
