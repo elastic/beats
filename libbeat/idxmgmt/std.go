1795efd413b61e55f077ab3bdd18c2354c8efb50
Shaunak Kashyap <ycombinator@gmail.com>
2020-05-19 08:55:10 -0400

    [7.8] Followup to 12606 (#18316) (#18521)
    * Followup to 12606 (#18316)
    
    * Adding developer CHANGELOG entry
    
    * Refactoring: extracting helper method
    
    * Adding unit tests
    
    * Consolidate event metadata field constants
    
    * Use events.GetMetaStringValue
    
    * Implement op_type values as enum
    
    * Add doc strings
    
    * Deference event pointer
    
    * Renaming op type consts and breaking them out into own block
    
    * Renaming type
    
    * Using stringer
    
    * Using go idiom instead of if-else
    
    * Adding default op type
    
    * Empty string for default
    
    * Store op type enum, not string, in event metadata
    
    * Using events.GetMetaStringValue
    
    * Updating dev CHANGELOG entry
    
    * Allow for op_type metadata field to be set as either string or enum
    
    * No need for .String()
    
    * Handle missing key case gracefully
    
    * Update unit tests
    
    * Update developer CHANGELOG entry
    
    * Fixing up CHANGELOG

--- libbeat/idxmgmt/std.go
+++ libbeat/idxmgmt/std.go
@@ -22,6 +22,7 @@ import (
 	"fmt"
 
 	"github.com/elastic/beats/v7/libbeat/beat"
+	"github.com/elastic/beats/v7/libbeat/beat/events"
 	"github.com/elastic/beats/v7/libbeat/common"
 	"github.com/elastic/beats/v7/libbeat/common/atomic"
 	"github.com/elastic/beats/v7/libbeat/idxmgmt/ilm"
@@ -352,28 +353,22 @@ func getEventCustomIndex(evt *beat.Event, beatInfo beat.Info) string {
 		return ""
 	}
 
-	if tmp := evt.Meta["alias"]; tmp != nil {
-		if alias, ok := tmp.(string); ok {
-			return alias
-		}
+	if alias, err := events.GetMetaStringValue(*evt, events.FieldMetaAlias); err == nil {
+		return alias
 	}
 
-	if tmp := evt.Meta["index"]; tmp != nil {
-		if idx, ok := tmp.(string); ok {
-			ts := evt.Timestamp.UTC()
-			return fmt.Sprintf("%s-%d.%02d.%02d",
-				idx, ts.Year(), ts.Month(), ts.Day())
-		}
+	if idx, err := events.GetMetaStringValue(*evt, events.FieldMetaIndex); err == nil {
+		ts := evt.Timestamp.UTC()
+		return fmt.Sprintf("%s-%d.%02d.%02d",
+			idx, ts.Year(), ts.Month(), ts.Day())
 	}
 
 	// This is functionally identical to Meta["alias"], returning the overriding
 	// metadata as the index name if present. It is currently used by Filebeat
 	// to send the index for particular inputs to formatted string templates,
 	// which are then expanded by a processor to the "raw_index" field.
-	if tmp := evt.Meta["raw_index"]; tmp != nil {
-		if idx, ok := tmp.(string); ok {
-			return idx
-		}
+	if idx, err := events.GetMetaStringValue(*evt, events.FieldMetaRawIndex); err == nil {
+		return idx
 	}
 
 	return ""
