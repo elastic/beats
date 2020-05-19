fe1a9a93bc3984bb9ce205de7f3cd7d81b320bc8
Shaunak Kashyap <ycombinator@gmail.com>
2020-05-19 08:54:48 -0400

    [7.x] Followup to 12606 (#18316) (#18520)
    * Followup to 12606 (#18316)
    
    * Adding developer CHANGELOG entry
    
    * Refactoring: extracting helper method
    
    * Adding unit tests
    
    * Consolidate event metadata field constants
    
    * Use events.GetMetaStringValue
    
    * Implement op_type values as enum
    
    * Add doc strings
    
    * Deference event pointer
    
    * Renaming op type consts and breaking them out into own block
    
    * Renaming type
    
    * Using stringer
    
    * Using go idiom instead of if-else
    
    * Adding default op type
    
    * Empty string for default
    
    * Store op type enum, not string, in event metadata
    
    * Using events.GetMetaStringValue
    
    * Updating dev CHANGELOG entry
    
    * Allow for op_type metadata field to be set as either string or enum
    
    * No need for .String()
    
    * Handle missing key case gracefully
    
    * Update unit tests
    
    * Update developer CHANGELOG entry
    
    * Fixing up CHANGELOG

---
+++ libbeat/beat/events/util_test.go
@@ -0,0 +1,90 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package events
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/elastic/beats/v7/libbeat/beat"
+	"github.com/elastic/beats/v7/libbeat/common"
+)
+
+func TestGetMetaStringValue(t *testing.T) {
+	tests := map[string]struct {
+		event         beat.Event
+		metaFieldPath string
+		expectedValue string
+		expectedErr   error
+	}{
+		"nonexistent_field": {
+			beat.Event{
+				Meta: common.MapStr{
+					"foo": "bar",
+				},
+			},
+			"nonexistent",
+			"",
+			common.ErrKeyNotFound,
+		},
+		"root": {
+			beat.Event{
+				Meta: common.MapStr{
+					"foo": "bar",
+					"baz": "hello",
+				},
+			},
+			"baz",
+			"hello",
+			nil,
+		},
+		"nested": {
+			beat.Event{
+				Meta: common.MapStr{
+					"foo": "bar",
+					"baz": common.MapStr{
+						"qux": "hello",
+					},
+				},
+			},
+			"baz.qux",
+			"hello",
+			nil,
+		},
+		"non_string": {
+			beat.Event{
+				Meta: common.MapStr{
+					"foo": "bar",
+					"baz": 17,
+				},
+			},
+			"baz",
+			"",
+			nil,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			value, err := GetMetaStringValue(test.event, test.metaFieldPath)
+			require.Equal(t, test.expectedValue, value)
+			require.Equal(t, test.expectedErr, err)
+		})
+	}
+}
