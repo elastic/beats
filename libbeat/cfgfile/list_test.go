e4c0fed2c37ef69323de7a96639cb449b5a5c60a
Chris Mark <chrismarkou92@gmail.com>
2020-05-21 15:54:15 +0300

    Cherry-pick #18564 to 7.8: [Autodiscover] Check if runner is already running before starting again (#18689)
    * [Autodiscover] Check if runner is already running before starting again (#18564)
    
    
    (cherry picked from commit b0f7ae72161ef557f74838f5ac0aee1cdcba48f0)
    
    * Update CHANGELOG.next.asciidoc

--- libbeat/cfgfile/list_test.go
+++ libbeat/cfgfile/list_test.go
@@ -103,6 +103,28 @@ func TestReloadSameConfigs(t *testing.T) {
 	assert.Equal(t, state, list.copyRunnerList())
 }
 
+func TestReloadDuplicateConfig(t *testing.T) {
+	factory := &runnerFactory{}
+	list := NewRunnerList("", factory, nil)
+
+	list.Reload([]*reload.ConfigWithMeta{
+		createConfig(1),
+	})
+
+	state := list.copyRunnerList()
+	assert.Equal(t, len(state), 1)
+
+	// This can happen in Autodiscover when a container if getting restarted
+	// but the previous one is not cleaned yet.
+	list.Reload([]*reload.ConfigWithMeta{
+		createConfig(1),
+		createConfig(1),
+	})
+
+	// nothing changed
+	assert.Equal(t, state, list.copyRunnerList())
+}
+
 func TestReloadStopConfigs(t *testing.T) {
 	factory := &runnerFactory{}
 	list := NewRunnerList("", factory, nil)
