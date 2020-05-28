b66aa230a5a91b49266f26038fbc1f207ee01310
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-05-28 17:31:58 +0100

    more verbose, skip some folders that causes issues in windows

--- .ci/scripts/pre_archive_test.py
+++ .ci/scripts/pre_archive_test.py
@@ -10,9 +10,10 @@ if __name__ == "__main__":
         os.makedirs('build')
 
     # Top level folders to be excluded
-    EXCLUDE = set(['.ci', '.git', '.github', 'vendor', 'dev-tools'])
+    EXCLUDE = set(['.ci', '.git', '.github', 'vendor', 'dev-tools', 'system-tests'])
     for root, dirs, files in os.walk('.'):
         dirs[:] = [d for d in dirs if d not in EXCLUDE]
         if root.endswith(('build')) and not root.startswith(('./build')):
             dest = os.path.join('build', root.replace('./', ''))
+            print("Copy {} into {}".format(root, dest))
             distutils.dir_util.copy_tree(root, dest, preserve_symlinks=1)
