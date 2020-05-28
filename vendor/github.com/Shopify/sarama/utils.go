d3c49783812f6842f0b3df1984f3d6280ed7b0e0
Noémi Ványi <kvch@users.noreply.github.com>
2020-05-28 11:40:54 +0200

    Update sarama to fix Kerberos authentication for Kafka (#18711) (#18809)
    (cherry picked from commit 44c654a1e5d516ad74991d9ae261b8d2ec54a6e6)

--- vendor/github.com/Shopify/sarama/utils.go
+++ vendor/github.com/Shopify/sarama/utils.go
@@ -26,9 +26,7 @@ func (slice int32Slice) Swap(i, j int) {
 
 func dupInt32Slice(input []int32) []int32 {
 	ret := make([]int32, 0, len(input))
-	for _, val := range input {
-		ret = append(ret, val)
-	}
+	ret = append(ret, input...)
 	return ret
 }
 
@@ -161,6 +159,8 @@ var (
 	V2_1_0_0  = newKafkaVersion(2, 1, 0, 0)
 	V2_2_0_0  = newKafkaVersion(2, 2, 0, 0)
 	V2_3_0_0  = newKafkaVersion(2, 3, 0, 0)
+	V2_4_0_0  = newKafkaVersion(2, 4, 0, 0)
+	V2_5_0_0  = newKafkaVersion(2, 5, 0, 0)
 
 	SupportedVersions = []KafkaVersion{
 		V0_8_2_0,
@@ -185,9 +185,11 @@ var (
 		V2_1_0_0,
 		V2_2_0_0,
 		V2_3_0_0,
+		V2_4_0_0,
+		V2_5_0_0,
 	}
 	MinVersion = V0_8_2_0
-	MaxVersion = V2_3_0_0
+	MaxVersion = V2_5_0_0
 )
 
 //ParseKafkaVersion parses and returns kafka version or error from a string
