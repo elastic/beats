8928a7d15c328fe6761b2fcfc1002811f17e4add
kaiyan-sheng <kaiyan.sheng@elastic.co>
2020-05-29 09:26:46 -0600

    Cherry-pick #18757 to 7.x: [Metricbeat] Fix getting compute instance metadata with partial zone/region config (#18839)
    * [Metricbeat] Fix getting compute instance metadata with partial zone/region config (#18757)
    
    * use zone for getting instance metadata
    
    (cherry picked from commit f4c65f31cb360aa270fd931c62b879a3016b64a4)
    
    * update changelog

--- x-pack/metricbeat/module/googlecloud/stackdriver/response_parser_test.go
+++ x-pack/metricbeat/module/googlecloud/stackdriver/response_parser_test.go
@@ -7,67 +7,8 @@ package stackdriver
 import (
 	"testing"
 
-	"github.com/golang/protobuf/ptypes/timestamp"
 	"github.com/stretchr/testify/assert"
-	"google.golang.org/genproto/googleapis/api/metric"
-	"google.golang.org/genproto/googleapis/api/monitoredres"
-	"google.golang.org/genproto/googleapis/monitoring/v3"
 )
-
-var fake *monitoring.TimeSeries = &monitoring.TimeSeries{
-	Resource: &monitoredres.MonitoredResource{
-		Type: "gce_instance",
-		Labels: map[string]string{
-			"instance_id": "4624337448093162893",
-			"project_id":  "elastic-metricbeat",
-			"zone":        "us-central1-a",
-		},
-	},
-	Metadata: &monitoredres.MonitoredResourceMetadata{
-		UserLabels: map[string]string{
-			"user": "label",
-		},
-	},
-	Metric: &metric.Metric{
-		Labels: map[string]string{
-			"instance_name": "instance-1",
-		},
-		Type: "compute.googleapis.com/instance/cpu/usage_time",
-	},
-	MetricKind: metric.MetricDescriptor_GAUGE,
-	ValueType:  metric.MetricDescriptor_DOUBLE,
-	Points: []*monitoring.Point{{
-		Value: &monitoring.TypedValue{
-			Value: &monitoring.TypedValue_DoubleValue{DoubleValue: 0.0041224284852319215},
-		},
-		Interval: &monitoring.TimeInterval{
-			StartTime: &timestamp.Timestamp{
-				Seconds: 1569932700,
-			},
-			EndTime: &timestamp.Timestamp{
-				Seconds: 1569932700,
-			},
-		},
-	}, {
-		Value: &monitoring.TypedValue{
-			Value: &monitoring.TypedValue_DoubleValue{DoubleValue: 0.004205757571772513},
-		},
-		Interval: &monitoring.TimeInterval{
-			StartTime: &timestamp.Timestamp{
-				Seconds: 1569932640,
-			},
-			EndTime: &timestamp.Timestamp{
-				Seconds: 1569932640,
-			},
-		},
-	}},
-}
-
-var metrics = []string{
-	"compute.googleapis.com/instance/cpu/utilization",
-	"compute.googleapis.com/instance/disk/read_bytes_count",
-	"compute.googleapis.com/http/server/response_latencies",
-}
 
 func TestCleanMetricNameString(t *testing.T) {
 	cases := []struct {
