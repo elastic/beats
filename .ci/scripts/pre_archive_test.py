594eb004d0c85d0388f73cf0e747acb56ffdd2e4
Victor Martinez <victormartinezrubio@gmail.com>
2020-05-29 16:22:04 +0200

    platform agnostic

--- .ci/scripts/pre_archive_test.py
+++ .ci/scripts/pre_archive_test.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     EXCLUDE = set(['.ci', '.git', '.github', 'vendor', 'dev-tools'])
     for root, dirs, files in os.walk('.'):
         dirs[:] = [d for d in dirs if d not in EXCLUDE]
-        if root.endswith(('build')) and not root.startswith(('./build')):
+        if root.endswith(('build')) and not root.startswith((".{}build".format(os.sep))):
             dest = os.path.join('build', root.replace(".{}".format(os.sep), ''))
             print("Copy {} into {}".format(root, dest))
             distutils.dir_util.copy_tree(root, dest, preserve_symlinks=1)
