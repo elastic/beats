ad14e501aa1f36c7a7f263cb08aa02f28a6d6b2a
chrismark <chrismarkou92@gmail.com>
2020-06-03 16:44:35 +0300

    Fix state
    Signed-off-by: chrismark <chrismarkou92@gmail.com>

--- libbeat/common/errors.go
+++ libbeat/common/errors.go
@@ -23,10 +23,10 @@ import (
 
 // ErrInputNotFinished struct for reporting errors related to not finished inputs
 type ErrInputNotFinished struct {
-	state string
+	State string
 }
 
 // Error method of ErrInputNotFinished
 func (e *ErrInputNotFinished) Error() string {
-	return fmt.Sprintf("Can only start an input when all related states are finished: %+v", e.state)
+	return fmt.Sprintf("Can only start an input when all related states are finished: %+v", e.State)
 }
