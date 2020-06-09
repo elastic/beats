89e02ce7b2378cd19e5e0daeca8a4d1398d7e368
beats-jenkins <spam@ruflin.com>
2020-06-09 11:31:47 +0200

    update mappings

--- x-pack/elastic-agent/pkg/agent/transpiler/rules.go
+++ x-pack/elastic-agent/pkg/agent/transpiler/rules.go
@@ -632,7 +632,7 @@ func (r *InjectStreamProcessorRule) Apply(ast *AST) error {
 				processorMap.value = append(processorMap.value, &Key{name: "fields", value: &Dict{value: []Node{
 					&Key{name: "type", value: &StrVal{value: r.Type}},
 					&Key{name: "namespace", value: &StrVal{value: namespace}},
-					&Key{name: "dataset", value: &StrVal{value: dataset}},
+					&Key{name: "name", value: &StrVal{value: dataset}},
 				}}})
 
 				addFieldsMap := &Dict{value: []Node{&Key{"add_fields", processorMap}}}
