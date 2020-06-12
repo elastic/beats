ae6beb105797fde8957f32a2448e927135b8c6cf
Andrew Kroh <andrew.kroh@elastic.co>
2020-06-12 09:03:24 -0400

    Specify an ECS version in Auditbeat/Packetbeat/Winlogbeat
    When we update the Beat (include all of its modules) we will then bump the ECS
    version that it includes in events.
    
    I went for a less granular approach than what is being used in Filebeat because
    I think it's desirable to move a whole beat to a new ECS version "at once" and
    more realistic to do so with these Beats that have fewer updates. By "at once" I
    mean we won't release a version that is partially updated. This implies that if
    we will be making multiple commits that we should use a feature branch to
    ensure the update is atomic.
    
    Closes #17688

--- auditbeat/main.go
+++ auditbeat/main.go
@@ -21,13 +21,6 @@ import (
 	"os"
 
 	"github.com/elastic/beats/v7/auditbeat/cmd"
-
-	// Register modules.
-	_ "github.com/elastic/beats/v7/auditbeat/module/auditd"
-	_ "github.com/elastic/beats/v7/auditbeat/module/file_integrity"
-
-	// Register includes.
-	_ "github.com/elastic/beats/v7/auditbeat/include"
 )
 
 func main() {
