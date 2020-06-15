e475617479703e32273e0d5c1eea680fc87d4222
Michal Pristas <michal.pristas@gmail.com>
2020-06-15 13:21:58 +0200

    remove ecs

--- go.mod
+++ go.mod
@@ -136,10 +136,11 @@ require (
 	github.com/shirou/gopsutil v2.19.11+incompatible
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/pflag v1.0.5
+	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/stretchr/testify v1.5.1
 	github.com/tsg/go-daemon v0.0.0-20200207173439-e704b93fd89b
 	github.com/tsg/gopacket v0.0.0-20190320122513-dd3d0e41124a
-	github.com/urso/ecslog v0.0.1
+	github.com/urso/magetools v0.0.0-20200125210132-c2e338f92f3a // indirect
 	github.com/vmware/govmomi v0.0.0-20170802214208-2cad15190b41
 	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c
 	github.com/yuin/gopher-lua v0.0.0-20170403160031-b402f3114ec7 // indirect
--- x-pack/elastic-agent/pkg/core/logger/logger.go
+++ x-pack/elastic-agent/pkg/core/logger/logger.go
@@ -7,36 +7,29 @@ package logger
 import (
 	"fmt"
 
-	"github.com/urso/ecslog"
-	"github.com/urso/ecslog/backend"
-	"github.com/urso/ecslog/backend/appender"
-	"github.com/urso/ecslog/backend/layout"
-
+	"github.com/elastic/beats/v7/libbeat/common"
+	"github.com/elastic/beats/v7/libbeat/logp"
+	"github.com/elastic/beats/v7/libbeat/logp/configure"
+	"github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/agent/errors"
 	"github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/config"
 )
 
+const agentName = "elastic-agent"
+
 // Logger alias ecslog.Logger with Logger.
-type Logger = ecslog.Logger
-
-// Config is a configuration of logging.
-type Config struct {
-	Level loggingLevel `config:"level"`
-}
-
-// DefaultLoggingConfig creates a default logging configuration.
-func DefaultLoggingConfig() *Config {
-	return &Config{
-		Level: loggingLevel(backend.Trace),
-	}
-}
+type Logger = logp.Logger
 
 // New returns a configured ECS Logger
 func New() (*Logger, error) {
-	return new(backend.Trace)
+	return new(DefaultLoggingConfig())
 }
 
-func createJSONBackend(lvl backend.Level) (backend.Backend, error) {
-	return appender.Console(lvl, layout.Text(true))
+// NewWithLogpLevel returns a configured logp Logger with specified level.
+func NewWithLogpLevel(level logp.Level) (*Logger, error) {
+	dc := DefaultLoggingConfig()
+	dc.Level = loggingLevel(level)
+
+	return new(dc)
 }
 
 //NewFromConfig takes the user configuration and generate the right logger.
@@ -52,44 +45,19 @@ func NewFromConfig(cfg *config.Config) (*Logger, error) {
 		return nil, err
 	}
 
-	return new(backend.Level(wrappedConfig.Logging.Level))
+	return new(wrappedConfig.Logging)
 }
 
-func new(lvl backend.Level) (*Logger, error) {
-	backend, err := createJSONBackend(lvl)
+func new(cfg *Config) (*Logger, error) {
+	logpCfg := configToLogpConfig(cfg)
+	commonLogp, err := common.NewConfigFrom(logpCfg)
 	if err != nil {
-		return nil, err
-	}
-	return ecslog.New(backend), nil
-}
-
-type loggingLevel backend.Level
-
-var loggingLevelMap = map[string]loggingLevel{
-	"trace": loggingLevel(backend.Trace),
-	"debug": loggingLevel(backend.Debug),
-	"info":  loggingLevel(backend.Info),
-	"error": loggingLevel(backend.Error),
-}
-
-func (m *loggingLevel) Unpack(v string) error {
-	mgt, ok := loggingLevelMap[v]
-	if !ok {
-		return fmt.Errorf(
-			"unknown logging level mode, received '%s' and valid values are 'trace', 'debug', 'info' or 'error'",
-			v,
-		)
-	}
-	*m = mgt
-	return nil
-}
-
-func (m *loggingLevel) String() string {
-	for s, v := range loggingLevelMap {
-		if v == *m {
-			return s
-		}
+		return nil, errors.New(err, errors.TypeConfig)
 	}
 
-	return "unknown"
+	if err := configure.Logging("", commonLogp); err != nil {
+		return nil, fmt.Errorf("error initializing logging: %v", err)
+	}
+
+	return logp.NewLogger(""), nil
 }
