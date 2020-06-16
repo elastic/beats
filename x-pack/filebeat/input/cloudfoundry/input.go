6a7f86f849d9df17941db409bcd75e651d065265
Chris Mark <chrismarkou92@gmail.com>
2020-06-16 13:27:27 +0300

    Fix missing import in cloud foundry input (#19213)
    Signed-off-by: chrismark <chrismarkou92@gmail.com>

--- x-pack/filebeat/input/cloudfoundry/input.go
+++ x-pack/filebeat/input/cloudfoundry/input.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/elastic/beats/v7/filebeat/channel"
 	"github.com/elastic/beats/v7/filebeat/input"
-	"github.com/elastic/beats/v7/libbeat/beat"
 	"github.com/elastic/beats/v7/libbeat/common"
 	"github.com/elastic/beats/v7/libbeat/common/cfgwarn"
 	"github.com/elastic/beats/v7/libbeat/logp"
