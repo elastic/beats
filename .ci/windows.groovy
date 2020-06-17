e05cdb9203a19cc1ecdeec25c6626942018bc7d7
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-06-17 14:39:39 +0100

    enable windows-2012-r2 to validate the gcc instalation

--- .ci/windows.groovy
+++ .ci/windows.groovy
@@ -12,10 +12,9 @@ import groovy.transform.Field
 /**
  List of supported windows versions to be tested with
  NOTE:
-   - 'windows-2012-r2' is not supported until gcc got installed.
    - 'windows-10' is too slow
 */
-@Field def windowsVersions = ['windows-2019', 'windows-2016', 'windows-2008-r2', 'windows-7', 'windows-7-32-bit']
+@Field def windowsVersions = ['windows-2019', 'windows-2016', 'windows-2012-r2', 'windows-2008-r2', 'windows-7', 'windows-7-32-bit']
 
 pipeline {
   agent { label 'ubuntu && immutable' }
