f3dc1c6133451f831bce11f8352e529b9dc5ce73
Michal Pristas <michal.pristas@gmail.com>
2020-06-18 14:09:44 +0200

    1on1 with logp

--- x-pack/elastic-agent/pkg/core/logger/logger.go
+++ x-pack/elastic-agent/pkg/core/logger/logger.go
@@ -6,14 +6,15 @@ package logger
 
 import (
 	"fmt"
-
-	"gopkg.in/yaml.v2"
+	"path/filepath"
 
 	"github.com/elastic/beats/v7/libbeat/common"
 	"github.com/elastic/beats/v7/libbeat/logp"
 	"github.com/elastic/beats/v7/libbeat/logp/configure"
+	"github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/agent/application/paths"
 	"github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/agent/errors"
 	"github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/config"
+	"gopkg.in/yaml.v2"
 )
 
 const agentName = "elastic-agent"
@@ -21,27 +22,41 @@ const agentName = "elastic-agent"
 // Logger alias ecslog.Logger with Logger.
 type Logger = logp.Logger
 
+type Config = logp.Config
+
 // New returns a configured ECS Logger
 func New(name string) (*Logger, error) {
-	return new(name, DefaultLoggingConfig())
+	dc, err := defaultConfig()
+	if err != nil {
+		return nil, err
+	}
+	return new(name, dc)
 }
 
 // NewWithLogpLevel returns a configured logp Logger with specified level.
 func NewWithLogpLevel(name string, level logp.Level) (*Logger, error) {
-	dc := DefaultLoggingConfig()
-	dc.Level = loggingLevel(level)
+	cfg := struct {
+		Level string `config:"level"`
+	}{Level: level.String()}
 
-	return new(name, dc)
+	commonCfg, err := common.NewConfigFrom(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return new(name, commonCfg)
 }
 
 //NewFromConfig takes the user configuration and generate the right logger.
 // TODO: Finish implementation, need support on the library that we use.
 func NewFromConfig(name string, cfg *config.Config) (*Logger, error) {
-	wrappedConfig := &struct {
-		Logging *Config `config:"logging"`
-	}{
-		Logging: DefaultLoggingConfig(),
+	defaultCfg, err := defaultConfig()
+	if err != nil {
+		return nil, err
 	}
+	wrappedConfig := &struct {
+		Logging *common.Config `config:"logging"`
+	}{Logging: defaultCfg}
 
 	if err := cfg.Unpack(&wrappedConfig); err != nil {
 		return nil, err
@@ -50,29 +65,41 @@ func NewFromConfig(name string, cfg *config.Config) (*Logger, error) {
 	return new(name, wrappedConfig.Logging)
 }
 
-func new(name string, cfg *Config) (*Logger, error) {
-	logpCfg, err := configToLogpConfig(cfg)
-	if err != nil {
-		return nil, err
+func new(name string, cfg *common.Config) (*Logger, error) {
+	if err := configure.Logging("", cfg); err != nil {
+		return nil, fmt.Errorf("error initializing logging: %v", err)
 	}
+
+	return logp.NewLogger(name), nil
+}
+
+func defaultConfig() (*common.Config, error) {
+	cfg := DefaultLoggingConfig()
 
 	// work around custom types and common config
 	// when custom type is transformed to common.Config
 	// value is determined based on reflect value which is incorrect
 	// enum vs human readable form
-	yamlCfg, err := yaml.Marshal(logpCfg)
+	yamlCfg, err := yaml.Marshal(cfg)
 	if err != nil {
 		return nil, err
 	}
-
+	fmt.Println(string(yamlCfg))
 	commonLogp, err := common.NewConfigFrom(string(yamlCfg))
 	if err != nil {
 		return nil, errors.New(err, errors.TypeConfig)
 	}
 
-	if err := configure.Logging("", commonLogp); err != nil {
-		return nil, fmt.Errorf("error initializing logging: %v", err)
-	}
+	return commonLogp, nil
+}
 
-	return logp.NewLogger(name), nil
+func DefaultLoggingConfig() *Config {
+	cfg := logp.DefaultConfig(logp.DefaultEnvironment)
+	cfg.Beat = agentName
+	cfg.ECSEnabled = true
+	cfg.Level = logp.DebugLevel
+	cfg.Files.Path = filepath.Join(paths.Home(), "data", "logs")
+	cfg.Files.Name = agentName
+
+	return &cfg
 }
