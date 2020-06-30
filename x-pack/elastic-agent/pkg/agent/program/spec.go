25e223f61504cdfdcebb01af01eaf3042d2e4a61
Blake Rouse <blake.rouse@elastic.co>
2020-06-30 08:37:02 -0400

    [Elastic Agent] Fix artifact downloading to allow endpoint-security to be downloaded (#19503)
    * Fix downloading so its not hard coded to beats repo name.
    
    * Add artifact to spec file, use artifact for download.
    
    * Fix tar installer.
    
    * Fix downloading.
    
    * Fix tests.
    
    * Add changelog.
    
    * Revert some un-needed changes.

--- x-pack/elastic-agent/pkg/agent/program/spec.go
+++ x-pack/elastic-agent/pkg/agent/program/spec.go
@@ -31,6 +31,7 @@ type Spec struct {
 	ServicePort       int                  `yaml:"service,omitempty"`
 	Cmd               string               `yaml:"cmd"`
 	Args              []string             `yaml:"args"`
+	Artifact          string               `yaml:"artifact"`
 	Rules             *transpiler.RuleList `yaml:"rules"`
 	CheckInstallSteps *transpiler.StepList `yaml:"check_install"`
 	PostInstallSteps  *transpiler.StepList `yaml:"post_install"`
