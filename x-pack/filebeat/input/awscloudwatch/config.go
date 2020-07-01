07639fe0a0bb6f12671c5c3f9b5039dc9d092e09
kaiyan-sheng <kaiyan.sheng@elastic.co>
2020-07-01 15:15:27 -0600

    Add awscloudwatch filebeat input (#19025)
    * Add awscloudwatch filebeat input
    * Use log group ARN instead of log group name and region name
    * add api_sleep, log_group_name and region_name config

---
+++ x-pack/filebeat/input/awscloudwatch/config.go
@@ -0,0 +1,57 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package awscloudwatch
+
+import (
+	"errors"
+	"time"
+
+	"github.com/elastic/beats/v7/filebeat/harvester"
+	awscommon "github.com/elastic/beats/v7/x-pack/libbeat/common/aws"
+)
+
+type config struct {
+	harvester.ForwarderConfig `config:",inline"`
+	LogGroupARN               string              `config:"log_group_arn"`
+	LogGroupName              string              `config:"log_group_name"`
+	RegionName                string              `config:"region_name"`
+	LogStreams                []string            `config:"log_streams"`
+	LogStreamPrefix           string              `config:"log_stream_prefix"`
+	StartPosition             string              `config:"start_position" default:"beginning"`
+	ScanFrequency             time.Duration       `config:"scan_frequency" validate:"min=0,nonzero"`
+	APITimeout                time.Duration       `config:"api_timeout" validate:"min=0,nonzero"`
+	APISleep                  time.Duration       `config:"api_sleep" validate:"min=0,nonzero"`
+	AwsConfig                 awscommon.ConfigAWS `config:",inline"`
+}
+
+func defaultConfig() config {
+	return config{
+		ForwarderConfig: harvester.ForwarderConfig{
+			Type: "awscloudwatch",
+		},
+		StartPosition: "beginning",
+		ScanFrequency: 10 * time.Second,
+		APITimeout:    120 * time.Second,
+		APISleep:      200 * time.Millisecond, // FilterLogEvents has a limit of 5 transactions per second (TPS)/account/Region: 1s / 5 = 200 ms
+	}
+}
+
+func (c *config) Validate() error {
+	if c.StartPosition != "beginning" && c.StartPosition != "end" {
+		return errors.New("start_position config parameter can only be " +
+			"either 'beginning' or 'end'")
+	}
+
+	if c.LogGroupARN == "" && c.LogGroupName == "" {
+		return errors.New("log_group_arn and log_group_name config parameter" +
+			"cannot be both empty")
+	}
+
+	if c.LogGroupName != "" && c.RegionName == "" {
+		return errors.New("region_name is required when log_group_name " +
+			"config parameter is given")
+	}
+	return nil
+}
