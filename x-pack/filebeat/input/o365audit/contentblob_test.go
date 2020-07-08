334ebcff386882fde52d3b72e29565963abf9b81
Steffen Siering <steffen.siering@elastic.co>
2020-07-08 13:46:43 +0200

    Move O365audit input to v2 input API (#19719)
    (cherry picked from commit 03bedd729005d4ff1d01fbd5028cfb88c26810d7)

--- x-pack/filebeat/input/o365audit/contentblob_test.go
+++ x-pack/filebeat/input/o365audit/contentblob_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/elastic/beats/v7/libbeat/beat"
@@ -21,9 +22,15 @@ type contentStore struct {
 	stopped bool
 }
 
-func (s *contentStore) onEvent(b beat.Event) bool {
+var errStopped = errors.New("stopped")
+
+func (s *contentStore) onEvent(b beat.Event, checkpointUpdate interface{}) error {
+	b.Private = checkpointUpdate
 	s.events = append(s.events, b)
-	return !s.stopped
+	if s.stopped {
+		return errStopped
+	}
+	return nil
 }
 
 func (f *fakePoll) BlobContent(t testing.TB, b poll.Transaction, data []common.MapStr, nextUrl string) poll.Transaction {
@@ -41,7 +48,7 @@ func makeEvent(ts time.Time, id string) common.MapStr {
 	}
 }
 
-func validateBlobs(t testing.TB, store contentStore, expected []string, c cursor) cursor {
+func validateBlobs(t testing.TB, store contentStore, expected []string, c checkpoint) checkpoint {
 	assert.Len(t, store.events, len(expected))
 	for idx := range expected {
 		id, err := getString(store.events[idx].Fields, fieldsPrefix+".Id")
@@ -51,14 +58,14 @@ func validateBlobs(t testing.TB, store contentStore, expected []string, c cursor
 		assert.Equal(t, expected[idx], id)
 	}
 	prev := c
-	baseLine := c.line
+	baseLine := c.Line
 	for idx, id := range expected {
 		ev := store.events[idx]
-		cursor, ok := ev.Private.(cursor)
+		cursor, ok := ev.Private.(checkpoint)
 		if !assert.True(t, ok) {
 			t.Fatal("no cursor for event id", id)
 		}
-		assert.Equal(t, idx+1+baseLine, cursor.line)
+		assert.Equal(t, idx+1+baseLine, cursor.Line)
 		assert.True(t, prev.Before(cursor))
 		prev = cursor
 	}
@@ -72,7 +79,7 @@ func TestContentBlob(t *testing.T) {
 		Logger:   logp.L(),
 		Callback: store.onEvent,
 	}
-	baseCursor := newCursor(stream{"myTenant", "contentype"}, time.Now())
+	baseCursor := checkpoint{Timestamp: time.Now()}
 	query := ContentBlob("http://test.localhost/", baseCursor, ctx)
 	data := []common.MapStr{
 		makeEvent(now.Add(-time.Hour), "e1"),
@@ -85,7 +92,7 @@ func TestContentBlob(t *testing.T) {
 	next := f.BlobContent(t, query, data, "")
 	assert.Nil(t, next)
 	c := validateBlobs(t, store, expected, baseCursor)
-	assert.Equal(t, len(expected), c.line)
+	assert.Equal(t, len(expected), c.Line)
 }
 
 func TestContentBlobResumeToLine(t *testing.T) {
@@ -93,9 +100,9 @@ func TestContentBlobResumeToLine(t *testing.T) {
 	var store contentStore
 	ctx := testConfig()
 	ctx.Callback = store.onEvent
-	baseCursor := newCursor(stream{"myTenant", "contentype"}, time.Now())
+	baseCursor := checkpoint{Timestamp: time.Now()}
 	const skip = 3
-	baseCursor.line = skip
+	baseCursor.Line = skip
 	query := ContentBlob("http://test.localhost/", baseCursor, ctx).WithSkipLines(skip)
 	data := []common.MapStr{
 		makeEvent(now.Add(-time.Hour), "e1"),
@@ -108,7 +115,7 @@ func TestContentBlobResumeToLine(t *testing.T) {
 	next := f.BlobContent(t, query, data, "")
 	assert.Nil(t, next)
 	c := validateBlobs(t, store, expected, baseCursor)
-	assert.Equal(t, len(expected), c.line-skip)
+	assert.Equal(t, len(expected), c.Line-skip)
 }
 
 func TestContentBlobPaged(t *testing.T) {
@@ -118,7 +125,7 @@ func TestContentBlobPaged(t *testing.T) {
 		Logger:   logp.L(),
 		Callback: store.onEvent,
 	}
-	baseCursor := newCursor(stream{"myTenant", "contentype"}, time.Now())
+	baseCursor := checkpoint{Timestamp: time.Now()}
 	query := ContentBlob("http://test.localhost/", baseCursor, ctx)
 	data := []common.MapStr{
 		makeEvent(now.Add(-time.Hour), "e1"),
@@ -133,17 +140,17 @@ func TestContentBlobPaged(t *testing.T) {
 	assert.NotNil(t, next)
 	assert.IsType(t, paginator{}, next)
 	c := validateBlobs(t, store, expected, baseCursor)
-	assert.Equal(t, 3, c.line)
+	assert.Equal(t, 3, c.Line)
 	store.events = nil
 	next = f.BlobContent(t, next, data[3:5], "http://test.localhost/page/3")
 	assert.IsType(t, paginator{}, next)
 	expected = []string{"e4", "e5"}
 	c = validateBlobs(t, store, expected, c)
-	assert.Equal(t, 5, c.line)
+	assert.Equal(t, 5, c.Line)
 	store.events = nil
 	next = f.BlobContent(t, next, data[5:], "")
 	assert.Nil(t, next)
 	expected = []string{"e6"}
 	c = validateBlobs(t, store, expected, c)
-	assert.Equal(t, 6, c.line)
+	assert.Equal(t, 6, c.Line)
 }
