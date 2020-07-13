483617266098b06ef0c5ee0aa95c684f665b3d85
Mariana Dima <mariana@elastic.co>
2020-07-13 21:15:54 +0200

    Add support for appinsights in the metricbeat azure module (#18940) (#19854)
    * app
    
    * changelog
    
    * update
    
    * Revert "update"
    
    This reverts commit 06db76bfb796bc0b3948bd502f009130e545cdb1.
    
    * avoid winio
    
    * fix text
    
    (cherry picked from commit 2efea84875a03e02ac877506a74cda8c6f99353c)

---
+++ x-pack/metricbeat/module/azure/app_insights/mock_service.go
@@ -0,0 +1,37 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package app_insights
+
+import (
+	"github.com/Azure/azure-sdk-for-go/services/preview/appinsights/v1/insights"
+	"github.com/stretchr/testify/mock"
+
+	"github.com/elastic/beats/v7/libbeat/logp"
+)
+
+// Service interface for the azure monitor service and mock for testing
+type Service interface {
+	GetMetricValues(applicationId string, bodyMetrics []insights.MetricsPostBodySchema) (insights.ListMetricsResultsItem, error)
+}
+
+// MockService mock for the azure monitor services
+type MockService struct {
+	mock.Mock
+}
+
+// NewMockClient instantiates a new client with the mock billing service
+func NewMockClient() *Client {
+	return &Client{
+		new(MockService),
+		Config{},
+		logp.NewLogger("test azure appinsights"),
+	}
+}
+
+// GetMetricValues will return specified app insights metrics
+func (service *MockService) GetMetricValues(applicationId string, bodyMetrics []insights.MetricsPostBodySchema) (insights.ListMetricsResultsItem, error) {
+	args := service.Called(applicationId, bodyMetrics)
+	return args.Get(0).(insights.ListMetricsResultsItem), args.Error(1)
+}
