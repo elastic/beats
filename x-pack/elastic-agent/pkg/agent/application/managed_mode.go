21a6cf087e1f71a6887d5171420b5ccc6a46fa07
Michal Pristas <michal.pristas@gmail.com>
2020-07-13 16:33:36 +0200

    [Ingest Manager] Refuse invalid stream values in configuration (#19587)
    * added stream check
    
    * changelog
    
    * fmt
    
    * invalid names
    
    * updated config files
    
    * updated config files
    
    * changes in all config files
    
    * config typo
    
    * small changes
    
    * Update go.sum

--- x-pack/elastic-agent/pkg/agent/application/managed_mode.go
+++ x-pack/elastic-agent/pkg/agent/application/managed_mode.go
@@ -152,7 +152,7 @@ func newManaged(
 		router,
 		&configModifiers{
 			Decorators: []decoratorFunc{injectMonitoring},
-			Filters:    []filterFunc{injectFleet(config), filters.ConstraintFilter},
+			Filters:    []filterFunc{filters.StreamChecker, injectFleet(config), filters.ConstraintFilter},
 		},
 		monitor,
 	)
