22014e677136158fd1386833ee0f5f8fb86e30f7
kaiyan-sheng <kaiyan.sheng@elastic.co>
2020-07-17 07:35:32 -0600

    [Filebeat] remove delimiter \n from log line in s3 input (#19972)
    * remove delimiter \n from log line in s3 input
    
    (cherry picked from commit 9ca82b4cb92c37d82c11e9283ea596232044d22f)

--- x-pack/filebeat/input/s3/input.go
+++ x-pack/filebeat/input/s3/input.go
@@ -477,7 +477,13 @@ func (p *s3Input) createEventsFromS3Info(svc s3iface.ClientAPI, info s3Info, s3C
 	// handle s3 objects that are not json content-type
 	offset := 0
 	for {
-		log, err := reader.ReadString('\n')
+		log, err := readStringAndTrimDelimiter(reader)
+		if err != nil {
+			err = errors.Wrap(err, "readStringAndTrimDelimiter failed")
+			p.logger.Error(err)
+			return err
+		}
+
 		if log == "" {
 			break
 		}
@@ -566,7 +572,8 @@ func (p *s3Input) decodeJSONWithKey(decoder *json.Decoder, objectHash string, s3
 
 func (p *s3Input) convertJSONToEvent(jsonFields interface{}, offset int, objectHash string, s3Info s3Info, s3Ctx *s3Context) error {
 	vJSON, err := json.Marshal(jsonFields)
-	log := string(vJSON)
+	logOriginal := string(vJSON)
+	log := trimLogDelimiter(logOriginal)
 	offset += len([]byte(log))
 	event := createEvent(log, offset, s3Info, objectHash, s3Ctx)
 
@@ -607,6 +614,18 @@ func (p *s3Input) deleteMessage(queueURL string, messagesReceiptHandle string, s
 		return errors.Wrapf(err, "SQS DeleteMessageRequest failed in queue %s", queueURL)
 	}
 	return nil
+}
+
+func trimLogDelimiter(log string) string {
+	return strings.TrimSuffix(log, "\n")
+}
+
+func readStringAndTrimDelimiter(reader *bufio.Reader) (string, error) {
+	logOriginal, err := reader.ReadString('\n')
+	if err != nil {
+		return logOriginal, err
+	}
+	return trimLogDelimiter(logOriginal), nil
 }
 
 func createEvent(log string, offset int, info s3Info, objectHash string, s3Ctx *s3Context) beat.Event {
