2f768eb647bac58a4350a3693a837d29b9e0e1a1
Andrew Cholakian <andrew@andrewvc.com>
2020-07-20 09:00:05 -0500

    Add script params and global status

--- heartbeat/monitors/active/synthetic/config.go
+++ heartbeat/monitors/active/synthetic/config.go
@@ -1,7 +1,10 @@
 package synthetic
 
+import "github.com/elastic/beats/v7/libbeat/common"
+
 type Config struct {
 	Script string `config:"script"`
+	ScriptParams common.MapStr `config:"script_params"`
 	Browsers []string `config:"browsers"`
 	RunnerURL string `config:"runner_url"`
 	ApiKey string `config:"api_key"`
--- heartbeat/monitors/active/synthetic/synthetic.go
+++ heartbeat/monitors/active/synthetic/synthetic.go
@@ -8,6 +8,9 @@ import (
 	"github.com/elastic/beats/v7/heartbeat/monitors/jobs"
 	"github.com/elastic/beats/v7/libbeat/beat"
 	"github.com/elastic/beats/v7/libbeat/common"
+	"github.com/elastic/beats/v7/libbeat/logp"
+	"github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/agent/errors"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -18,6 +21,7 @@ func init() {
 type synthproxyReq struct {
 	Name string `json:"name"`
 	Browser string `json:"browser"`
+	ScriptParams map[string]interface{} `json:"script_params"`
 	Script string `json:"script"`
 	ApiKey string `json:"api_key"`
 }
@@ -29,24 +33,46 @@ func create(name string, cfg *common.Config) (js []jobs.Job, endpoints int, err 
 	}
 
 	for _, browser := range config.Browsers {
-		sr := synthproxyReq{"TODO", browser, config.Script, config.ApiKey}
+		sr := synthproxyReq{
+			Name: "TODO",
+			Browser: browser,
+			Script: config.Script,
+			ScriptParams: config.ScriptParams,
+			ApiKey: config.ApiKey,
+		}
 		srJson, err := json.Marshal(sr)
 		if err != nil {
 			return nil, 0, err
 		}
 		job := monitors.MakeSimpleCont(func(event *beat.Event) error {
+			logp.Info("START JOB")
 			resp, err := http.Post(config.RunnerURL, "application/json", bytes.NewBuffer(srJson))
 			if err != nil {
 				return err
 			}
-			var decoded map[string]interface{}
-			err = json.NewDecoder(resp.Body).Decode(&decoded)
+
+			bodyStr, err := ioutil.ReadAll(resp.Body)
 			if err != nil {
+				logp.Warn("read err", err)
+			}
+
+			var decoded map[string]interface{}
+			err = json.Unmarshal(bodyStr, &decoded)
+			if err != nil {
+				logp.Warn("DECODE ERR %v. Body is '%v'", err)
 				return err
 			}
 			eventext.MergeEventFields(event, common.MapStr{
 				"journey": decoded,
 			})
+
+			if v, _ := decoded["error"]; v != nil {
+				logp.Warn("Error is: %v", v)
+				return errors.New(v)
+			}
+
+			logp.Warn("COMPLETE")
+
 			return nil
 		})
 		js = append(js, job)
