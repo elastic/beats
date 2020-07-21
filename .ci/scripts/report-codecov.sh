f15f9e19d8fcbc5a0b8551f55d63d6f610a7a021
Victor Martinez <victormartinezrubio@gmail.com>
2020-07-21 09:21:24 +0100

    Update .ci/scripts/report-codecov.sh

--- .ci/scripts/report-codecov.sh
+++ .ci/scripts/report-codecov.sh
@@ -5,7 +5,7 @@ CODECOV_URL=https://codecov.io/bash
 if [ -e /usr/local/bin/bash_standard_lib.sh ] ; then
     # shellcheck disable=SC1091
     source /usr/local/bin/bash_standard_lib.sh
-    (retry 2 curl -sSLo codecov ${CODECOV_URL})
+    (retry 3 curl -sSLo codecov ${CODECOV_URL})
 else
     curl -sSLo codecov ${CODECOV_URL}
 fi
