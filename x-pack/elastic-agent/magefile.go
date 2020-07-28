e9f8f177780caedc96a706e1362f6cbfff57d806
Blake Rouse <blake.rouse@elastic.co>
2020-07-28 10:11:42 -0400

    [Elastic Agent] Add basic protocol to control Elastic Agent. (#20146)
    * Add protocl to control Elastic Agent.
    
    * Add more to version response.
    
    * Fix CI with protoc.
    
    * Improve protocol and try and fix CI.
    
    * Fix proto.
    
    * Remove CI changes.
    
    * Update go.mod.
    
    * Add status ROLLBACK.
    
    * Run mage fmt.

--- x-pack/elastic-agent/magefile.go
+++ x-pack/elastic-agent/magefile.go
@@ -329,7 +329,7 @@ func commitID() string {
 	return commitID
 }
 
-// Update is an alias for executing fields, dashboards, config, includes.
+// Update is an alias for executing control protocol, configs, and specs.
 func Update() {
 	mg.SerialDeps(Config, BuildSpec, BuildFleetCfg)
 }
@@ -347,6 +347,11 @@ func CrossBuildGoDaemon() error {
 // Config generates both the short/reference/docker.
 func Config() {
 	mg.Deps(configYML)
+}
+
+// ControlProto generates pkg/agent/control/proto module.
+func ControlProto() error {
+	return sh.RunV("protoc", "--go_out=plugins=grpc:.", "control.proto")
 }
 
 // BuildSpec make sure that all the suppported program spec are built into the binary.
