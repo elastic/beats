4e1d3a156bc90898a48b15d08e9f27c036deb2d8
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-07-30 14:22:19 +0100

    [CI] Script to generate the build-stage table

---
+++ .ci/scripts/generate_build_table.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import os
+import yaml
+
+if __name__ == "__main__":
+
+    print("| Beat  | Stage  | Command  | MODULE  | Platforms  |")
+    print("|-------|--------|----------|---------|------------|")
+    for root, dirs, files in os.walk("."):
+        dirs.sort()
+        for file in files:
+            if file.endswith("Jenkinsfile.yml") and root != ".":
+                with open(os.path.join(root, file), 'r') as f:
+                    doc = yaml.load(f, Loader=yaml.FullLoader)
+                module = root.replace(".{}".format(os.sep), '')
+                platforms = [ doc["platform"] ]
+                for stage in doc["stages"]:
+                    withModule = False
+                    if "make" in doc["stages"][stage]:
+                        command = doc["stages"][stage]["make"]
+                    if "mage" in doc["stages"][stage]:
+                        command = doc["stages"][stage]["mage"]
+                    if "platforms" in doc["stages"][stage]:
+                        platforms = doc["stages"][stage]["platforms"]
+                    if "withModule" in doc["stages"][stage]:
+                        withModule = doc["stages"][stage]["withModule"]
+                    print("| {} | {} | `{}` | {} | `{}` |".format(module, stage, command, withModule, platforms))
