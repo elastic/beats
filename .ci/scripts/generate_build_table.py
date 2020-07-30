4b8466bd63678a8e0eacacbe9a02e6301f965415
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-07-30 14:50:11 +0100

    [docs] Add when in the table markdown

--- .ci/scripts/generate_build_table.py
+++ .ci/scripts/generate_build_table.py
@@ -5,8 +5,8 @@ import yaml
 
 if __name__ == "__main__":
 
-    print("| Beat  | Stage  | Command  | MODULE  | Platforms  |")
-    print("|-------|--------|----------|---------|------------|")
+    print("| Beat  | Stage  | Command  | MODULE  | Platforms  | When |")
+    print("|-------|--------|----------|---------|------------|------|")
     for root, dirs, files in os.walk("."):
         dirs.sort()
         for file in files:
@@ -17,6 +17,7 @@ if __name__ == "__main__":
                 platforms = [ doc["platform"] ]
                 for stage in doc["stages"]:
                     withModule = False
+                    when = "Always"
                     if "make" in doc["stages"][stage]:
                         command = doc["stages"][stage]["make"]
                     if "mage" in doc["stages"][stage]:
@@ -25,4 +26,6 @@ if __name__ == "__main__":
                         platforms = doc["stages"][stage]["platforms"]
                     if "withModule" in doc["stages"][stage]:
                         withModule = doc["stages"][stage]["withModule"]
-                    print("| {} | {} | `{}` | {} | `{}` |".format(module, stage, command, withModule, platforms))
+                    if "when" in doc["stages"][stage]:
+                        when = "some cases"
+                    print("| {} | {} | `{}` | {} | `{}` | `{}` |".format(module, stage, command, withModule, platforms, when))
