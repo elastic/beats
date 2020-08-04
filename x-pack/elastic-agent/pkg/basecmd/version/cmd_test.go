9615f41857b2582428a269f49d9a0f4d38c26ad2
Blake Rouse <blake.rouse@elastic.co>
2020-08-04 12:02:21 -0400

    Cherry-pick #20359 to 7.x: [Elastic Agent] Improve version, restart, enroll CLI commands (#20431)
    * [Elastic Agent] Improve version, restart, enroll CLI commands (#20359)
    
    * Add improve version CLI cmd.
    
    * Add new restart cmd. Perform restart at end of enroll.
    
    * Fix yaml annotations on version struct.
    
    * Fix control.Address on Windows.
    
    * Fix control.Address on Windows.
    
    * Fix windows dialer.
    
    * Fix control.Address on Windows.
    
    * Add to CHANGELOG.
    
    * Review cleanups.
    
    * Fix go vet.
    
    * Update talking to communicating.
    
    (cherry picked from commit 77b3b075b2a9733ac7ae05ce836150b6a677c5c6)
    
    * [Elastic Agent] Fix agent control socket path to always be less than 107 characters (#20426)
    
    * Fix agent control socket path to always be less than 107 characters.
    
    * Use os.TempDir.
    
    * Don't use os.TempDir.

--- x-pack/elastic-agent/pkg/basecmd/version/cmd_test.go
+++ x-pack/elastic-agent/pkg/basecmd/version/cmd_test.go
@@ -10,17 +10,90 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v2"
 
+	"github.com/elastic/beats/v7/libbeat/logp"
+
+	"github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/agent/control/server"
 	"github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/cli"
+	"github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/core/logger"
+	"github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/release"
 )
 
-func TestCmd(t *testing.T) {
+func TestCmdBinaryOnly(t *testing.T) {
 	streams, _, out, _ := cli.NewTestingIOStreams()
-	NewCommandWithArgs(streams).Execute()
+	cmd := NewCommandWithArgs(streams)
+	cmd.Flags().Set("binary-only", "true")
+	cmd.Execute()
 	version, err := ioutil.ReadAll(out)
 
-	if !assert.NoError(t, err) {
-		return
-	}
-	assert.True(t, strings.Contains(string(version), "Agent version is"))
+	require.NoError(t, err)
+	assert.True(t, strings.Contains(string(version), "Binary: "))
+	assert.False(t, strings.Contains(string(version), "Daemon: "))
+}
+
+func TestCmdBinaryOnlyYAML(t *testing.T) {
+	streams, _, out, _ := cli.NewTestingIOStreams()
+	cmd := NewCommandWithArgs(streams)
+	cmd.Flags().Set("binary-only", "true")
+	cmd.Flags().Set("yaml", "true")
+	cmd.Execute()
+	version, err := ioutil.ReadAll(out)
+
+	require.NoError(t, err)
+
+	var output Output
+	err = yaml.Unmarshal(version, &output)
+	require.NoError(t, err)
+
+	assert.Nil(t, output.Daemon)
+	assert.Equal(t, release.Info(), *output.Binary)
+}
+
+func TestCmdDaemon(t *testing.T) {
+	srv := server.New(newErrorLogger(t), nil)
+	require.NoError(t, srv.Start())
+	defer srv.Stop()
+
+	streams, _, out, _ := cli.NewTestingIOStreams()
+	cmd := NewCommandWithArgs(streams)
+	cmd.Execute()
+	version, err := ioutil.ReadAll(out)
+
+	require.NoError(t, err)
+	assert.True(t, strings.Contains(string(version), "Binary: "))
+	assert.True(t, strings.Contains(string(version), "Daemon: "))
+}
+
+func TestCmdDaemonYAML(t *testing.T) {
+	srv := server.New(newErrorLogger(t), nil)
+	require.NoError(t, srv.Start())
+	defer srv.Stop()
+
+	streams, _, out, _ := cli.NewTestingIOStreams()
+	cmd := NewCommandWithArgs(streams)
+	cmd.Flags().Set("yaml", "true")
+	cmd.Execute()
+	version, err := ioutil.ReadAll(out)
+
+	require.NoError(t, err)
+
+	var output Output
+	err = yaml.Unmarshal(version, &output)
+	require.NoError(t, err)
+
+	assert.Equal(t, release.Info(), *output.Daemon)
+	assert.Equal(t, release.Info(), *output.Binary)
+}
+
+func newErrorLogger(t *testing.T) *logger.Logger {
+	t.Helper()
+
+	loggerCfg := logger.DefaultLoggingConfig()
+	loggerCfg.Level = logp.ErrorLevel
+
+	log, err := logger.NewFromConfig("", loggerCfg)
+	require.NoError(t, err)
+	return log
 }
