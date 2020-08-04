94e93e2642eb30b6e9e83998fa4e91cf442437ac
Blake Rouse <blake.rouse@elastic.co>
2020-08-04 09:47:38 -0400

    Use os.TempDir.

--- x-pack/elastic-agent/pkg/agent/control/addr.go
+++ x-pack/elastic-agent/pkg/agent/control/addr.go
@@ -9,6 +9,9 @@ package control
 import (
 	"crypto/sha256"
 	"fmt"
+	"os"
+	"path/filepath"
+
 	"github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/agent/application/paths"
 )
 
@@ -17,5 +20,6 @@ func Address() string {
 	data := paths.Data()
 	// entire string cannot be longer than 107 characters, this forces the
 	// length to always be 88 characters (but unique per data path)
-	return fmt.Sprintf(`unix:///tmp/elastic-agent-%x.sock`, sha256.Sum256([]byte(data)))
+	path := filepath.Join(os.TempDir(), fmt.Sprintf("elastic-agent-%x.sock", sha256.Sum256([]byte(data))))
+	return fmt.Sprintf(`unix://%s`, path)
 }
