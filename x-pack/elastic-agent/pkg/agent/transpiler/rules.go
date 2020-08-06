294bac09b6361e2bedb031aa8b36c335b8d2be98
Blake Rouse <blake.rouse@elastic.co>
2020-08-06 13:58:01 -0400

    [Elastic Agent] Add support for synthetics inputs that run heartbeat (#20387)
    * Add support for synthetics inputs that run heartbeat.
    
    * Run mage fmt.
    
    * Add changelog.
    
    * Update test configs to use host instead of hosts.
    
    * Enable support for top-level inputs from Elastic Agent.

--- x-pack/elastic-agent/pkg/agent/transpiler/rules.go
+++ x-pack/elastic-agent/pkg/agent/transpiler/rules.go
@@ -1199,7 +1199,7 @@ func (r *FilterValuesWithRegexpRule) Apply(ast *AST) error {
 
 		newAST, ok := Lookup(newRoot, r.Key)
 		if !ok {
-			newNodes = append(newNodes, item)
+			// doesn't have key so its filtered out
 			continue
 		}
 
@@ -1210,7 +1210,7 @@ func (r *FilterValuesWithRegexpRule) Apply(ast *AST) error {
 		}
 
 		if n.name != r.Key {
-			newNodes = append(newNodes, item)
+			// doesn't match so its filtered out
 			continue
 		}
 
