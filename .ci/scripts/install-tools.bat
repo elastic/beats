4bf6eb91cad29ad84a20a8ab3f51ac78b6e19dda
Victor Martinez <victormartinezrubio@gmail.com>
2020-08-06 13:16:22 +0100

    [CI][BUILD] fail if choco failed (#20343)
    # Conflicts:
    #	.ci/scripts/install-tools.bat

--- .ci/scripts/install-tools.bat
+++ .ci/scripts/install-tools.bat
@@ -4,12 +4,12 @@ set PATH=%WORKSPACE%\bin;C:\ProgramData\chocolatey\bin;%PATH%
 
 where /q curl
 IF ERRORLEVEL 1 (
- choco install curl -y --no-progress --skipdownloadcache
+    choco install curl -y --no-progress --skipdownloadcache
 )
 mkdir %WORKSPACE%\bin
 where /q gvm
 IF ERRORLEVEL 1 (
- curl -sL -o %WORKSPACE%\bin\gvm.exe https://github.com/andrewkroh/gvm/releases/download/v0.2.2/gvm-windows-amd64.exe
+    curl -sL -o %WORKSPACE%\bin\gvm.exe https://github.com/andrewkroh/gvm/releases/download/v0.2.2/gvm-windows-amd64.exe
 )
 FOR /f "tokens=*" %%i IN ('"gvm.exe" use %GO_VERSION% --format=batch') DO %%i
 
@@ -18,10 +18,12 @@ go get github.com/magefile/mage
 mage -version
 where mage
 
-if not exist C:\Python38\python.exe (
-    REM Install python 3.8.
-    choco install python -y -r --no-progress --version 3.8.2 || echo ERROR && exit /b
+IF NOT EXIST C:\Python38\python.exe (
+    REM Install python 3.8
+    choco install python -y -r --no-progress --version 3.8.5
+    IF NOT ERRORLEVEL 0 (
+        exit /b 1
+    )
 )
 python --version
 where python
-
