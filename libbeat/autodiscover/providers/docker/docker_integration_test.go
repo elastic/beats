eae2d48c635ea0ad030f53db261d5516685f914b
Chris Mark <chrismarkou92@gmail.com>
2020-08-10 14:28:39 +0300

    Add leader election for autodiscover (#20281)
    (cherry picked from commit 9ab9b9747a2b3977c0562430565519e0f32e824e)

--- libbeat/autodiscover/providers/docker/docker_integration_test.go
+++ libbeat/autodiscover/providers/docker/docker_integration_test.go
@@ -56,7 +56,7 @@ func TestDockerStart(t *testing.T) {
 	s := &template.MapperSettings{nil, nil}
 	config.Templates = *s
 	k, _ := keystore.NewFileKeystore("test")
-	provider, err := AutodiscoverBuilder(bus, UUID, common.MustNewConfigFrom(config), k)
+	provider, err := AutodiscoverBuilder("mockBeat", bus, UUID, common.MustNewConfigFrom(config), k)
 	if err != nil {
 		t.Fatal(err)
 	}
