b5b3d39cc15c0a3c62cc5e33113caf0e6b8a2671
Andrew Wilkins <axw@elastic.co>
2020-08-13 11:45:29 +0800

    make fmt

--- libbeat/kibana/client_test.go
+++ libbeat/kibana/client_test.go
@@ -24,9 +24,10 @@ import (
 	"net/url"
 	"testing"
 
-	"github.com/elastic/beats/v7/libbeat/common"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/elastic/beats/v7/libbeat/common"
 )
 
 func TestErrorJson(t *testing.T) {
