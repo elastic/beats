a0e208a106075f09c30b489c220321ba5420fdae
Marc Guasch <marc.guasch@elastic.co>
2020-08-24 10:02:36 +0200

    Create stateless and cursor inputs

---
+++ x-pack/filebeat/input/httpjson/input_cursor.go
@@ -0,0 +1,60 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package httpjson
+
+import (
+	v2 "github.com/elastic/beats/v7/filebeat/input/v2"
+	cursor "github.com/elastic/beats/v7/filebeat/input/v2/input-cursor"
+	"github.com/elastic/beats/v7/libbeat/common"
+	"github.com/elastic/beats/v7/libbeat/common/transport/tlscommon"
+	"github.com/elastic/beats/v7/x-pack/filebeat/input/httpjson/config"
+)
+
+type cursorInput struct {
+	*input
+}
+
+func cursorConfigure(cfg *common.Config) ([]cursor.Source, cursor.Input, error) {
+	conf := config.Default()
+	if err := cfg.Unpack(&conf); err != nil {
+		return nil, nil, err
+	}
+
+	return newCursorInput(conf)
+}
+
+func newCursorInput(config config.Config) ([]cursor.Source, cursor.Input, error) {
+	if err := config.Validate(); err != nil {
+		return nil, nil, err
+	}
+
+	tlsConfig, err := tlscommon.LoadTLSConfig(config.TLS)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return nil,
+		&cursorInput{
+			input: &input{
+				config:    config,
+				tlsConfig: tlsConfig,
+			},
+		}, nil
+}
+
+func (in *cursorInput) Test(cursor.Source, v2.TestContext) error {
+	return in.test()
+}
+
+// Run starts the input and blocks until it ends the execution.
+// It will return on context cancellation, any other error will be retried.
+func (in *cursorInput) Run(
+	ctx v2.Context,
+	_ cursor.Source,
+	cursor cursor.Cursor,
+	publisher cursor.Publisher,
+) error {
+	return in.run(ctx, publisher, &cursor)
+}
---
+++ x-pack/filebeat/input/httpjson/input_stateless.go
@@ -0,0 +1,65 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package httpjson
+
+import (
+	v2 "github.com/elastic/beats/v7/filebeat/input/v2"
+	stateless "github.com/elastic/beats/v7/filebeat/input/v2/input-stateless"
+	"github.com/elastic/beats/v7/libbeat/beat"
+	"github.com/elastic/beats/v7/libbeat/common"
+	"github.com/elastic/beats/v7/libbeat/common/transport/tlscommon"
+	"github.com/elastic/beats/v7/x-pack/filebeat/input/httpjson/config"
+)
+
+type statelessInput struct {
+	*input
+}
+
+func statelessConfigure(cfg *common.Config) (stateless.Input, error) {
+	conf := config.Default()
+	if err := cfg.Unpack(&conf); err != nil {
+		return nil, err
+	}
+
+	return newStatelessInput(conf)
+}
+
+func newStatelessInput(config config.Config) (*statelessInput, error) {
+	if err := config.Validate(); err != nil {
+		return nil, err
+	}
+
+	tlsConfig, err := tlscommon.LoadTLSConfig(config.TLS)
+	if err != nil {
+		return nil, err
+	}
+
+	return &statelessInput{
+		input: &input{
+			config:    config,
+			tlsConfig: tlsConfig,
+		},
+	}, nil
+}
+
+func (in *statelessInput) Test(v2.TestContext) error {
+	return in.test()
+}
+
+type statlessPublisher struct {
+	wrapped stateless.Publisher
+}
+
+func (pub statlessPublisher) Publish(event beat.Event, _ interface{}) error {
+	pub.wrapped.Publish(event)
+	return nil
+}
+
+// Run starts the input and blocks until it ends the execution.
+// It will return on context cancellation, any other error will be retried.
+func (in *statelessInput) Run(ctx v2.Context, publisher stateless.Publisher) error {
+	pub := statlessPublisher{wrapped: publisher}
+	return in.run(ctx, pub, nil)
+}
