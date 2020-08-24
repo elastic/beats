5ff7ed8737b90e79af89dd4d31709a5ec82d914a
Marc Guasch <marc.guasch@elastic.co>
2020-08-24 11:19:15 +0200

    Add changelog entry and format files

--- x-pack/filebeat/input/httpjson/input_manager.go
+++ x-pack/filebeat/input/httpjson/input_manager.go
@@ -1,8 +1,13 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
 package httpjson
 
 import (
-	"github.com/elastic/go-concert/unison"
 	"go.uber.org/multierr"
+
+	"github.com/elastic/go-concert/unison"
 
 	v2 "github.com/elastic/beats/v7/filebeat/input/v2"
 	cursor "github.com/elastic/beats/v7/filebeat/input/v2/input-cursor"
