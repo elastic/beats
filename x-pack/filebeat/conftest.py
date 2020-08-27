0d39c24558a37b97516647cdce8221cda30e6b53
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2020-08-27 21:57:59 +0200

    Add oss paths

--- x-pack/filebeat/conftest.py
+++ x-pack/filebeat/conftest.py
@@ -2,4 +2,5 @@ import os
 import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../libbeat/tests/system'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../filebeat/tests/system'))
 sys.path.append(os.path.join(os.path.dirname(__file__), './tests/system'))
