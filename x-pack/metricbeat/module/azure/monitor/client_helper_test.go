2e5ac916240ac9b6158640d41d9f89c8cfd79876
Mariana <mariana@elastic.co>
2020-09-02 17:42:36 +0200

    tests

--- x-pack/metricbeat/module/azure/monitor/client_helper_test.go
+++ x-pack/metricbeat/module/azure/monitor/client_helper_test.go
@@ -7,8 +7,6 @@ package monitor
 import (
 	"testing"
 
-	az "github.com/elastic/beats/v7/x-pack/libbeat/common/azure"
-
 	"github.com/stretchr/testify/mock"
 
 	"github.com/Azure/azure-sdk-for-go/services/preview/monitor/mgmt/2019-06-01/insights"
@@ -61,11 +59,11 @@ func TestMapMetric(t *testing.T) {
 	metricDefinitions := insights.MetricDefinitionCollection{
 		Value: MockMetricDefinitions(),
 	}
-	metricConfig := az.MetricConfig{Namespace: "namespace", Dimensions: []az.DimensionConfig{{Name: "location", Value: "West Europe"}}}
-	resourceConfig := az.ResourceConfig{Metrics: []az.MetricConfig{metricConfig}}
+	metricConfig := azure.MetricConfig{Namespace: "namespace", Dimensions: []azure.DimensionConfig{{Name: "location", Value: "West Europe"}}}
+	resourceConfig := azure.ResourceConfig{Metrics: []azure.MetricConfig{metricConfig}}
 	client := azure.NewMockClient()
 	t.Run("return error when no metric definitions were found", func(t *testing.T) {
-		m := &az.MockService{}
+		m := &azure.MockService{}
 		m.On("GetMetricDefinitions", mock.Anything, mock.Anything).Return(insights.MetricDefinitionCollection{}, errors.New("invalid resource ID"))
 		client.AzureMonitorService = m
 		metric, err := mapMetrics(client, []resources.GenericResource{resource}, resourceConfig)
@@ -74,17 +72,14 @@ func TestMapMetric(t *testing.T) {
 		m.AssertExpectations(t)
 	})
 	t.Run("return all metrics when all metric names and aggregations were configured", func(t *testing.T) {
-		m := &az.MockService{}
+		m := &azure.MockService{}
 		m.On("GetMetricDefinitions", mock.Anything, mock.Anything).Return(metricDefinitions, nil)
 		client.AzureMonitorService = m
 		metricConfig.Name = []string{"*"}
-		resourceConfig.Metrics = []az.MetricConfig{metricConfig}
+		resourceConfig.Metrics = []azure.MetricConfig{metricConfig}
 		metrics, err := mapMetrics(client, []resources.GenericResource{resource}, resourceConfig)
 		assert.NoError(t, err)
-		assert.Equal(t, metrics[0].Resource.Id, "123")
-		assert.Equal(t, metrics[0].Resource.Name, "resourceName")
-		assert.Equal(t, metrics[0].Resource.Type, "resourceType")
-		assert.Equal(t, metrics[0].Resource.Location, "resourceLocation")
+		assert.Equal(t, metrics[0].ResourceId, "123")
 		assert.Equal(t, metrics[0].Namespace, "namespace")
 		assert.Equal(t, metrics[0].Names, []string{"TotalRequests", "Capacity", "BytesRead"})
 		assert.Equal(t, metrics[0].Aggregations, "Average")
@@ -92,20 +87,17 @@ func TestMapMetric(t *testing.T) {
 		m.AssertExpectations(t)
 	})
 	t.Run("return all metrics when specific metric names and aggregations were configured", func(t *testing.T) {
-		m := &az.MockService{}
+		m := &azure.MockService{}
 		m.On("GetMetricDefinitions", mock.Anything, mock.Anything).Return(metricDefinitions, nil)
 		client.AzureMonitorService = m
 		metricConfig.Name = []string{"TotalRequests", "Capacity"}
 		metricConfig.Aggregations = []string{"Average"}
-		resourceConfig.Metrics = []az.MetricConfig{metricConfig}
+		resourceConfig.Metrics = []azure.MetricConfig{metricConfig}
 		metrics, err := mapMetrics(client, []resources.GenericResource{resource}, resourceConfig)
 		assert.NoError(t, err)
 
 		assert.True(t, len(metrics) > 0)
-		assert.Equal(t, metrics[0].Resource.Id, "123")
-		assert.Equal(t, metrics[0].Resource.Name, "resourceName")
-		assert.Equal(t, metrics[0].Resource.Type, "resourceType")
-		assert.Equal(t, metrics[0].Resource.Location, "resourceLocation")
+		assert.Equal(t, metrics[0].ResourceId, "123")
 		assert.Equal(t, metrics[0].Namespace, "namespace")
 		assert.Equal(t, metrics[0].Names, []string{"TotalRequests", "Capacity"})
 		assert.Equal(t, metrics[0].Aggregations, "Average")
