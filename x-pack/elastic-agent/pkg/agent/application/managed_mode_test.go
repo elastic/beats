72e4396f460565504e30cff8e74c231e8e12afbe
Michal Pristas <michal.pristas@gmail.com>
2020-09-02 15:53:59 +0200

    [Ingest Manager] Remove Success from fleet contract (#20449)

--- x-pack/elastic-agent/pkg/agent/application/managed_mode_test.go
+++ x-pack/elastic-agent/pkg/agent/application/managed_mode_test.go
@@ -92,7 +92,6 @@ func (m *mockStreamStore) Shutdown() {}
 const fleetResponse = `
 {
 	"action": "checkin",
-	"success": true,
 	"actions": [{
 		"agent_id": "17e93530-7f42-11ea-9330-71e968b29fa4",
 		"type": "CONFIG_CHANGE",
