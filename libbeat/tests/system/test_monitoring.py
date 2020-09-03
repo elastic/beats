374b0a7fc60de84ce6ff10c776fd225faf3fcaff
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2020-09-03 17:52:02 +0200

    More escape sequences

--- libbeat/tests/system/test_monitoring.py
+++ libbeat/tests/system/test_monitoring.py
@@ -28,7 +28,6 @@ class Test(BaseTest):
         Test shipping monitoring data directly to the monitoring cluster.
         Make sure expected documents are indexed in monitoring cluster.
         """
-
         self.render_config_template(
             "mockbeat",
             monitoring={
@@ -45,7 +44,7 @@ class Test(BaseTest):
         self.wait_until(lambda: self.log_contains("mockbeat start running."))
         self.wait_until(lambda: self.log_contains(re.compile(r"\[monitoring\].*Publish event")))
         self.wait_until(lambda: self.log_contains(re.compile(
-            "Connection to .*elasticsearch\(" + self.get_elasticsearch_monitoring_url() + "\).* established")))
+            r"Connection to .*elasticsearch\({}\).* established".format(self.get_elasticsearch_monitoring_url()))))
         self.wait_until(lambda: self.monitoring_doc_exists('beats_stats'))
         self.wait_until(lambda: self.monitoring_doc_exists('beats_state'))
 
