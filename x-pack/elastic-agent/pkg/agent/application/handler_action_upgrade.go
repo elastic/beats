015f51f239b1c831f894b6d9a778fdf973d055bb
Michal Pristas <michal.pristas@gmail.com>
2020-09-07 11:00:49 +0200

    fetch

--- x-pack/elastic-agent/pkg/agent/application/handler_action_upgrade.go
+++ x-pack/elastic-agent/pkg/agent/application/handler_action_upgrade.go
@@ -8,9 +8,16 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/agent/errors"
 	"github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/artifact"
+	downloader "github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/artifact/download/localremote"
 	"github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/core/logger"
 	"github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/fleetapi"
+)
+
+const (
+	agentName         = "elastic-agent"
+	agentArtifactName = "beats/" + agentName
 )
 
 // After running Upgrade agent should download its own version specified by action
@@ -26,15 +33,48 @@ type handlerUpgrade struct {
 
 func (h *handlerUpgrade) Handle(ctx context.Context, a action, acker fleetAcker) error {
 	h.log.Debugf("handlerUpgrade: action '%+v' received", a)
-	_, ok := a.(*fleetapi.ActionUpgrade)
+	action, ok := a.(*fleetapi.ActionUpgrade)
 	if !ok {
 		return fmt.Errorf("invalid type, expected ActionUpgrade and received %T", a)
 	}
 
-	// TODO: download artifact
+	// download artifact
+	_, err := h.downloadArtifact(ctx, action)
+	if err != nil {
+		return err
+	}
+
 	// TODO: unpack correctly, skip root (symlink, config...) unpack data/*
 	// TODO: change symlink
 	// TODO: mark update happened so we can handle grace period
 	// TODO: reexec
 	return nil
+}
+
+func (h *handlerUpgrade) downloadArtifact(ctx context.Context, action *fleetapi.ActionUpgrade) (string, error) {
+	settings := *h.settings
+	if action.SourceURI != "" {
+		settings.SourceURI = action.SourceURI
+	}
+
+	fetcher := downloader.NewDownloader(h.log, &settings)
+	verifier, err := downloader.NewVerifier(h.log, &settings)
+	if err != nil {
+		return "", errors.New(err, "initiating verifier")
+	}
+
+	path, err := fetcher.Download(ctx, agentName, agentArtifactName, action.Version)
+	if err != nil {
+		return "", errors.New(err, "failed upgrade of agent binary")
+	}
+
+	matches, err := verifier.Verify(agentName, action.Version)
+	if err != nil {
+		return "", errors.New(err, "failed verification of agent binary")
+	}
+	if !matches {
+		return "", errors.New("failed verification of agent binary, hash does not match", errors.TypeSecurity)
+	}
+
+	return path, nil
 }
