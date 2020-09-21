c7eeb778f4c105e927b9a3630619679739f8b532
Blake Rouse <blake.rouse@elastic.co>
2020-09-21 18:03:02 -0400

    Fix sleep to timeout in uninstall subcommand.

--- x-pack/elastic-agent/pkg/agent/cmd/uninstall.go
+++ x-pack/elastic-agent/pkg/agent/cmd/uninstall.go
@@ -84,9 +84,9 @@ func uninstallCmd(streams *cli.IOStreams, cmd *cobra.Command, flags *globalFlags
 	if runtime.GOOS == "windows" {
 		// The installation path will still exists because we are executing from that
 		// directory. So cmd.exe is spawned to remove the directory after we exit.
-		command := fmt.Sprintf("sleep 3 && rmdir /s /q %s", install.InstallPath)
-		cmd := exec.Command(filepath.Join(os.Getenv("windir"), "system32", "cmd.exe"), "/C", command)
-		_ = cmd.Start()
+		command := fmt.Sprintf("timeout 3 && rmdir /s /q %s", install.InstallPath)
+		rmdir := exec.Command(filepath.Join(os.Getenv("windir"), "system32", "cmd.exe"), "/C", command)
+		_ = rmdir.Start()
 	}
 
 	return nil
