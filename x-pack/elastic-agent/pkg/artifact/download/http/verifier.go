aebddba1b0f3015f20368606235f12b7592326be
Michal Pristas <michal.pristas@gmail.com>
2020-09-22 16:38:07 +0200

    [Ingest Manager] Agent includes pgp file (#19480)
    [Ingest Manager] Agent includes pgp file (#19480)

--- x-pack/elastic-agent/pkg/artifact/download/http/verifier.go
+++ x-pack/elastic-agent/pkg/artifact/download/http/verifier.go
@@ -16,7 +16,6 @@ import (
 	"os"
 	"path"
 	"strings"
-	"sync"
 
 	"golang.org/x/crypto/openpgp"
 
@@ -32,20 +31,27 @@ const (
 // Verifier verifies a downloaded package by comparing with public ASC
 // file from elastic.co website.
 type Verifier struct {
-	config   *artifact.Config
-	client   http.Client
-	pgpBytes []byte
+	config        *artifact.Config
+	client        http.Client
+	pgpBytes      []byte
+	allowEmptyPgp bool
 }
 
 // NewVerifier create a verifier checking downloaded package on preconfigured
 // location agains a key stored on elastic.co website.
-func NewVerifier(config *artifact.Config) (*Verifier, error) {
+func NewVerifier(config *artifact.Config, allowEmptyPgp bool, pgp []byte) (*Verifier, error) {
+	if len(pgp) == 0 && !allowEmptyPgp {
+		return nil, errors.New("expecting PGP but retrieved none", errors.TypeSecurity)
+	}
+
 	client := http.Client{Timeout: config.Timeout}
 	rtt := withHeaders(client.Transport, headers)
 	client.Transport = rtt
 	v := &Verifier{
-		config: config,
-		client: client,
+		config:        config,
+		client:        client,
+		allowEmptyPgp: allowEmptyPgp,
+		pgpBytes:      pgp,
 	}
 
 	return v, nil
@@ -71,9 +77,11 @@ func (v *Verifier) Verify(programName, version string) (bool, error) {
 		// remove bits so they can be redownloaded
 		os.Remove(fullPath)
 		os.Remove(fullPath + ".sha512")
+		os.Remove(fullPath + ".asc")
+		return isMatch, err
 	}
 
-	return isMatch, err
+	return v.verifyAsc(programName, version)
 }
 
 func (v *Verifier) verifyHash(filename, fullPath string) (bool, error) {
@@ -120,15 +128,9 @@ func (v *Verifier) verifyHash(filename, fullPath string) (bool, error) {
 }
 
 func (v *Verifier) verifyAsc(programName, version string) (bool, error) {
-	var err error
-	var pgpBytesLoader sync.Once
-
-	pgpBytesLoader.Do(func() {
-		err = v.loadPGP(v.config.PgpFile)
-	})
-
-	if err != nil {
-		return false, errors.New(err, "loading PGP")
+	if len(v.pgpBytes) == 0 {
+		// no pgp available skip verification process
+		return true, nil
 	}
 
 	filename, err := artifact.GetArtifactName(programName, version, v.config.OS(), v.config.Arch())
@@ -147,7 +149,10 @@ func (v *Verifier) verifyAsc(programName, version string) (bool, error) {
 	}
 
 	ascBytes, err := v.getPublicAsc(ascURI)
-	if err != nil {
+	if err != nil && v.allowEmptyPgp {
+		// asc not available but we allow empty for dev use-case
+		return true, nil
+	} else if err != nil {
 		return false, errors.New(err, fmt.Sprintf("fetching asc file from %s", ascURI), errors.TypeNetwork, errors.M(errors.MetaKeyURI, ascURI))
 	}
 
@@ -198,36 +203,6 @@ func (v *Verifier) getPublicAsc(sourceURI string) ([]byte, error) {
 
 	if resp.StatusCode != 200 {
 		return nil, errors.New(fmt.Sprintf("call to '%s' returned unsuccessful status code: %d", sourceURI, resp.StatusCode), errors.TypeNetwork, errors.M(errors.MetaKeyURI, sourceURI))
-	}
-
-	return ioutil.ReadAll(resp.Body)
-}
-
-func (v *Verifier) loadPGP(file string) error {
-	var err error
-
-	if file == "" {
-		v.pgpBytes, err = v.loadPGPFromWeb()
-		return err
-	}
-
-	v.pgpBytes, err = ioutil.ReadFile(file)
-	if err != nil {
-		return errors.New(err, errors.TypeFilesystem, errors.M(errors.MetaKeyPath, file))
-	}
-
-	return nil
-}
-
-func (v *Verifier) loadPGPFromWeb() ([]byte, error) {
-	resp, err := v.client.Get(publicKeyURI)
-	if err != nil {
-		return nil, errors.New(err, "failed loading public key", errors.TypeNetwork, errors.M(errors.MetaKeyURI, publicKeyURI))
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		return nil, errors.New(fmt.Sprintf("call to '%s' returned unsuccessful status code: %d", publicKeyURI, resp.StatusCode), errors.TypeNetwork, errors.M(errors.MetaKeyURI, publicKeyURI))
 	}
 
 	return ioutil.ReadAll(resp.Body)
