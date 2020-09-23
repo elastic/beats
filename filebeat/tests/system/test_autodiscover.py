437e649bcbff6cd0bf16f1c9b94697b4df062798
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2020-09-23 13:05:03 +0200

    Make it more deterministic

--- filebeat/tests/system/test_autodiscover.py
+++ filebeat/tests/system/test_autodiscover.py
@@ -17,24 +17,25 @@ class TestAutodiscover(filebeat.BaseTest):
         """
         Test docker autodiscover starts input
         """
-        self.render_config_template(
-            inputs=False,
-            autodiscover={
-                'docker': {
-                    'cleanup_timeout': '0s',
-                    'templates': '''
-                      - condition:
-                          contains.docker.container.image: busybox
-                        config:
-                          - type: log
-                            paths:
-                              - %s/${data.docker.container.image}.log
-                    ''' % self.working_dir,
+        with self.container_running() as container:
+            self.render_config_template(
+                inputs=False,
+                autodiscover={
+                    'docker': {
+                        'cleanup_timeout': '0s',
+                        'templates': f'''
+                          - condition:
+                                equals.docker.container.name: {container.name}
+                            config:
+                              - type: log
+                                paths:
+                                  - %s/${{data.docker.container.name}}.log
+                        ''' % self.working_dir,
+                    },
                 },
-            },
-        )
+            )
 
-        self._test()
+            self._test(container)
 
     @unittest.skipIf(not INTEGRATION_TESTS or
                      os.getenv("TESTING_ENVIRONMENT") == "2x",
@@ -43,48 +44,47 @@ class TestAutodiscover(filebeat.BaseTest):
         """
         Test docker autodiscover default config settings
         """
-        self.render_config_template(
-            inputs=False,
-            autodiscover={
-                'docker': {
-                    'cleanup_timeout': '0s',
-                    'hints.enabled': 'true',
-                    'hints.default_config': '''
-                      type: log
-                      paths:
-                        - %s/${data.container.image}.log
-                    ''' % self.working_dir,
+        with self.container_running() as container:
+            self.render_config_template(
+                inputs=False,
+                autodiscover={
+                    'docker': {
+                        'cleanup_timeout': '0s',
+                        'hints.enabled': 'true',
+                        'hints.default_config': '''
+                          type: log
+                          paths:
+                            - %s/${data.container.name}.log
+                        ''' % self.working_dir,
+                    },
                 },
-            },
-        )
+            )
 
-        self._test()
+            self._test(container)
 
-    def _test(self):
-        image_name = 'busybox:latest'
-        with open(os.path.join(self.working_dir, f'{image_name}.log'), 'wb') as f:
+    def _test(self, container):
+        with open(os.path.join(self.working_dir, f'{container.name}.log'), 'wb') as f:
             f.write(b'Busybox output 1\n')
 
         proc = self.start_beat()
-        with self.container_running(image_name):
-            self.wait_until(lambda: self.log_contains('Starting runner: input'))
-            self.wait_until(lambda: self.output_has(lines=1))
+        self.wait_until(lambda: self.log_contains('Starting runner: input'))
+        self.wait_until(lambda: self.output_has(lines=1))
 
         output = self.read_output_json()
         proc.check_kill_and_wait()
 
         # Check metadata is added
         assert output[0]['message'] == 'Busybox output 1'
-        assert output[0]['container']['image']['name'] == image_name
-        assert output[0]['docker']['container']['labels'] == {}
+        assert output[0]['container']['name'] == container.name
+        assert output[0]['docker']['container']['labels'] == container.labels
         assert 'name' in output[0]['container']
 
         self.assert_fields_are_documented(output[0])
 
     @contextmanager
-    def container_running(self, image_name):
+    def container_running(self, image_name='busybox:latest'):
         import docker
         docker_client = docker.from_env()
         container = docker_client.containers.run(image_name, 'sleep 60', detach=True, remove=True)
-        yield
+        yield container
         container.remove(force=True)
