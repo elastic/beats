b5245c3e5c33d1be1903ae402f33e0df73e14132
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2020-09-23 12:19:33 +0200

    Fix autodiscover flaky tests
    Use a tagged image instead of untagged pull, this was pulling all
    busybox tags.
    Better control when the containers are started and stopped, run the
    container on background.
    Move common code to helper function.

--- filebeat/tests/system/test_autodiscover.py
+++ filebeat/tests/system/test_autodiscover.py
@@ -3,6 +3,7 @@ import filebeat
 import unittest
 
 from beat.beat import INTEGRATION_TESTS
+from contextlib import contextmanager
 
 
 class TestAutodiscover(filebeat.BaseTest):
@@ -16,9 +17,6 @@ class TestAutodiscover(filebeat.BaseTest):
         """
         Test docker autodiscover starts input
         """
-        import docker
-        docker_client = docker.from_env()
-
         self.render_config_template(
             inputs=False,
             autodiscover={
@@ -26,7 +24,7 @@ class TestAutodiscover(filebeat.BaseTest):
                     'cleanup_timeout': '0s',
                     'templates': '''
                       - condition:
-                          equals.docker.container.image: busybox
+                          contains.docker.container.image: busybox
                         config:
                           - type: log
                             paths:
@@ -36,26 +34,7 @@ class TestAutodiscover(filebeat.BaseTest):
             },
         )
 
-        with open(os.path.join(self.working_dir, 'busybox.log'), 'wb') as f:
-            f.write(b'Busybox output 1\n')
-
-        proc = self.start_beat()
-        docker_client.images.pull('busybox')
-        docker_client.containers.run('busybox', 'sleep 1')
-
-        self.wait_until(lambda: self.log_contains('Starting runner: input'))
-        self.wait_until(lambda: self.log_contains('Stopping runner: input'))
-
-        output = self.read_output_json()
-        proc.check_kill_and_wait()
-
-        # Check metadata is added
-        assert output[0]['message'] == 'Busybox output 1'
-        assert output[0]['container']['image']['name'] == 'busybox'
-        assert output[0]['docker']['container']['labels'] == {}
-        assert 'name' in output[0]['container']
-
-        self.assert_fields_are_documented(output[0])
+        self._test()
 
     @unittest.skipIf(not INTEGRATION_TESTS or
                      os.getenv("TESTING_ENVIRONMENT") == "2x",
@@ -64,9 +43,6 @@ class TestAutodiscover(filebeat.BaseTest):
         """
         Test docker autodiscover default config settings
         """
-        import docker
-        docker_client = docker.from_env()
-
         self.render_config_template(
             inputs=False,
             autodiscover={
@@ -82,23 +58,33 @@ class TestAutodiscover(filebeat.BaseTest):
             },
         )
 
-        with open(os.path.join(self.working_dir, 'busybox.log'), 'wb') as f:
+        self._test()
+
+    def _test(self):
+        image_name = 'busybox:latest'
+        with open(os.path.join(self.working_dir, f'{image_name}.log'), 'wb') as f:
             f.write(b'Busybox output 1\n')
 
         proc = self.start_beat()
-        docker_client.images.pull('busybox')
-        docker_client.containers.run('busybox', 'sleep 1')
-
-        self.wait_until(lambda: self.log_contains('Starting runner: input'))
-        self.wait_until(lambda: self.log_contains('Stopping runner: input'))
+        with self.container_running(image_name):
+            self.wait_until(lambda: self.log_contains('Starting runner: input'))
+            self.wait_until(lambda: self.output_has(lines=1))
 
         output = self.read_output_json()
         proc.check_kill_and_wait()
 
         # Check metadata is added
         assert output[0]['message'] == 'Busybox output 1'
-        assert output[0]['container']['image']['name'] == 'busybox'
+        assert output[0]['container']['image']['name'] == image_name
         assert output[0]['docker']['container']['labels'] == {}
         assert 'name' in output[0]['container']
 
         self.assert_fields_are_documented(output[0])
+
+    @contextmanager
+    def container_running(self, image_name):
+        import docker
+        docker_client = docker.from_env()
+        container = docker_client.containers.run(image_name, 'sleep 60', detach=True, remove=True)
+        yield
+        container.remove(force=True)
