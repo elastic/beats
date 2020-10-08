8771a3febe6b81979131f928cb7b6755d0ba0c64
Victor Martinez <victormartinezrubio@gmail.com>
2020-10-08 14:06:23 +0100

    Docker build resiliance with a retry (#21587) (#21680)

--- dev-tools/mage/integtest_docker.go
+++ dev-tools/mage/integtest_docker.go
@@ -26,6 +26,7 @@ import (
 	"runtime"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/pkg/errors"
 
@@ -240,5 +241,17 @@ func dockerComposeBuildImages() error {
 		os.Stderr,
 		"docker-compose", args...,
 	)
+
+	// This sleep is to avoid hitting the docker build issues when resources are not available.
+	if err != nil {
+		fmt.Println(">> Building docker images again")
+		time.Sleep(10)
+		_, err = sh.Exec(
+			composeEnv,
+			out,
+			os.Stderr,
+			"docker-compose", args...,
+		)
+	}
 	return err
 }
