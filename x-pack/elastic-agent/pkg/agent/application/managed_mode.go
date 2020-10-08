c528af80beb1307b132c2122f93f79f7603eaa51
Blake Rouse <blake.rouse@elastic.co>
2020-10-08 08:08:43 -0500

    [Elastic Agent] Reload fleet.kibana.hosts from policy change (#21599) (#21665)
    * Update the connected client for kibana from policy change.
    
    * Fix vet.
    
    * Add changelog.
    
    * Add protocol compare.
    
    * Rollback protocol and hosts on failure.
    
    (cherry picked from commit 189171723ce69e3a235ef16a80c870db74fba682)

--- x-pack/elastic-agent/pkg/agent/application/managed_mode.go
+++ x-pack/elastic-agent/pkg/agent/application/managed_mode.go
@@ -213,12 +213,17 @@ func newManaged(
 		acker,
 		combinedReporter)
 
+	policyChanger := &handlerPolicyChange{
+		log:       log,
+		emitter:   emit,
+		agentInfo: agentInfo,
+		config:    cfg,
+		store:     store,
+		setters:   []clientSetter{acker},
+	}
 	actionDispatcher.MustRegister(
 		&fleetapi.ActionPolicyChange{},
-		&handlerPolicyChange{
-			log:     log,
-			emitter: emit,
-		},
+		policyChanger,
 	)
 
 	actionDispatcher.MustRegister(
@@ -268,6 +273,9 @@ func newManaged(
 	if err != nil {
 		return nil, err
 	}
+	// add the gateway to setters, so the gateway can be updated
+	// when the hosts for Kibana are updated by the policy.
+	policyChanger.setters = append(policyChanger.setters, gateway)
 
 	managedApplication.gateway = gateway
 	return managedApplication, nil
