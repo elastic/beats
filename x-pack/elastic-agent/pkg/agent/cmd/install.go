8b34795cd90ad207f04a47c466953998848816ef
Blake Rouse <blake.rouse@elastic.co>
2020-10-13 14:28:16 -0400

    Update order.

--- x-pack/elastic-agent/pkg/agent/cmd/install.go
+++ x-pack/elastic-agent/pkg/agent/cmd/install.go
@@ -9,6 +9,9 @@ import (
 	"os"
 	"os/exec"
 
+	"github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/agent/application"
+	"github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/agent/application/paths"
+
 	"github.com/spf13/cobra"
 
 	c "github.com/elastic/beats/v7/libbeat/common/cli"
@@ -28,7 +31,7 @@ would like the Agent to operate.
 `,
 		Run: func(c *cobra.Command, args []string) {
 			if err := installCmd(streams, c, flags, args); err != nil {
-				fmt.Fprintf(streams.Err, "%v\n", err)
+				fmt.Fprintf(streams.Err, "Error: %v\n", err)
 				os.Exit(1)
 			}
 		},
@@ -52,6 +55,16 @@ func installCmd(streams *cli.IOStreams, cmd *cobra.Command, flags *globalFlags, 
 		return fmt.Errorf("already installed at: %s", install.InstallPath)
 	}
 
+	// check the lock to ensure that elastic-agent is not already running in this directory
+	locker := application.NewAppLocker(paths.Data())
+	if err := locker.TryLock(); err != nil {
+		if err == application.ErrAppAlreadyRunning {
+			return fmt.Errorf("cannot perform installation as Elastic Agent is already running from this directory")
+		}
+		return err
+	}
+	locker.Unlock()
+
 	warn.PrintNotGA(streams.Out)
 	force, _ := cmd.Flags().GetBool("force")
 	if status == install.Broken {
@@ -59,7 +72,7 @@ func installCmd(streams *cli.IOStreams, cmd *cobra.Command, flags *globalFlags, 
 			fmt.Fprintf(streams.Out, "Elastic Agent is installed but currently broken: %s\n", reason)
 			confirm, err := c.Confirm(fmt.Sprintf("Continuing will re-install Elastic Agent over the current installation at %s. Do you want to continue?", install.InstallPath), true)
 			if err != nil {
-				return fmt.Errorf("Error: problem reading prompt response")
+				return fmt.Errorf("problem reading prompt response")
 			}
 			if !confirm {
 				return fmt.Errorf("installation was cancelled by the user")
@@ -69,7 +82,7 @@ func installCmd(streams *cli.IOStreams, cmd *cobra.Command, flags *globalFlags, 
 		if !force {
 			confirm, err := c.Confirm(fmt.Sprintf("Elastic Agent will be installed at %s and will run as a service. Do you want to continue?", install.InstallPath), true)
 			if err != nil {
-				return fmt.Errorf("Error: problem reading prompt response")
+				return fmt.Errorf("problem reading prompt response")
 			}
 			if !confirm {
 				return fmt.Errorf("installation was cancelled by the user")
@@ -77,6 +90,7 @@ func installCmd(streams *cli.IOStreams, cmd *cobra.Command, flags *globalFlags, 
 		}
 	}
 
+	enroll := true
 	askEnroll := true
 	kibana, _ := cmd.Flags().GetString("kibana-url")
 	token, _ := cmd.Flags().GetString("enrollment-token")
@@ -98,7 +112,7 @@ func installCmd(streams *cli.IOStreams, cmd *cobra.Command, flags *globalFlags, 
 	}
 	if !askEnroll && (kibana == "" || token == "") {
 		// force was performed without required enrollment arguments, all done (standalone mode)
-		return nil
+		enroll = false
 	}
 
 	if kibana == "" {
@@ -122,29 +136,34 @@ func installCmd(streams *cli.IOStreams, cmd *cobra.Command, flags *globalFlags, 
 		}
 	}
 
-	enrollArgs := []string{"enroll", kibana, token, "--from-install"}
-	enrollArgs = append(enrollArgs, buildEnrollmentFlags(cmd)...)
-	enrollCmd := exec.Command(os.Args[0], enrollArgs...)
-	enrollCmd.Stdin = os.Stdin
-	enrollCmd.Stdout = os.Stdout
-	enrollCmd.Stderr = os.Stderr
-	err = enrollCmd.Start()
-	if err != nil {
-		return fmt.Errorf("failed to execute enroll command: %s", err)
-	}
-	err = enrollCmd.Wait()
-	if err != nil {
-		exitErr, ok := err.(*exec.ExitError)
-		if ok {
-			return fmt.Errorf("enroll command failed with exit code: %d", exitErr.ExitCode())
-		}
-		return fmt.Errorf("enroll command failed for unknown reason: %s", err)
-	}
-
 	err = install.Install()
 	if err != nil {
-		return fmt.Errorf("Error: %s", err)
+		return err
 	}
+
+	if enroll {
+		enrollArgs := []string{"enroll", kibana, token, "--from-install"}
+		enrollArgs = append(enrollArgs, buildEnrollmentFlags(cmd)...)
+		enrollCmd := exec.Command(install.ExecutablePath(), enrollArgs...)
+		enrollCmd.Stdin = os.Stdin
+		enrollCmd.Stdout = os.Stdout
+		enrollCmd.Stderr = os.Stderr
+		err = enrollCmd.Start()
+		if err != nil {
+			install.Uninstall()
+			return fmt.Errorf("failed to execute enroll command: %s", err)
+		}
+		err = enrollCmd.Wait()
+		if err != nil {
+			install.Uninstall()
+			exitErr, ok := err.(*exec.ExitError)
+			if ok {
+				return fmt.Errorf("enroll command failed with exit code: %d", exitErr.ExitCode())
+			}
+			return fmt.Errorf("enroll command failed for unknown reason: %s", err)
+		}
+	}
+
 	err = install.StartService()
 	if err != nil {
 		fmt.Fprintf(streams.Out, "Installation of required system files was successful, but starting of the service failed.\n")
