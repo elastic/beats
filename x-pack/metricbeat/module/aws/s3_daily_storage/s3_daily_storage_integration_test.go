7e13f83cd8d154793b18a3c4d34502efda20ea55
kaiyan-sheng <kaiyan.sheng@elastic.co>
2020-10-13 14:29:41 -0600

    Move TestFetch back

--- x-pack/metricbeat/module/aws/s3_daily_storage/s3_daily_storage_integration_test.go
+++ x-pack/metricbeat/module/aws/s3_daily_storage/s3_daily_storage_integration_test.go
@@ -10,9 +10,32 @@ package s3_daily_storage
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	mbtest "github.com/elastic/beats/v7/metricbeat/mb/testing"
 	"github.com/elastic/beats/v7/x-pack/metricbeat/module/aws/mtest"
 )
+
+func TestFetch(t *testing.T) {
+	config := mtest.GetConfigForTest(t, "s3_daily_storage", "86400s")
+
+	metricSet := mbtest.NewReportingMetricSetV2Error(t, config)
+	events, errs := mbtest.ReportingFetchV2Error(metricSet)
+	if len(errs) > 0 {
+		t.Fatalf("Expected 0 error, had %d. %v\n", len(errs), errs)
+	}
+
+	assert.NotEmpty(t, events)
+
+	for _, event := range events {
+		mtest.CheckEventField("cloud.region", "string", event, t)
+		mtest.CheckEventField("aws.dimensions.BucketName", "string", event, t)
+		mtest.CheckEventField("aws.dimensions.StorageType", "string", event, t)
+		mtest.CheckEventField("aws.s3.metrics.BucketSizeBytes.avg", "float", event, t)
+		mtest.CheckEventField("aws.s3.metrics.NumberOfObjects.avg", "float", event, t)
+		break
+	}
+}
 
 func TestData(t *testing.T) {
 	config := mtest.GetConfigForTest(t, "s3_daily_storage", "86400s")
--- x-pack/metricbeat/module/aws/s3_request/s3_request_integration_test.go
+++ x-pack/metricbeat/module/aws/s3_request/s3_request_integration_test.go
@@ -10,9 +10,45 @@ package s3_request
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	mbtest "github.com/elastic/beats/v7/metricbeat/mb/testing"
 	"github.com/elastic/beats/v7/x-pack/metricbeat/module/aws/mtest"
 )
+
+func TestFetch(t *testing.T) {
+	config := mtest.GetConfigForTest(t, "s3_request", "60s")
+
+	metricSet := mbtest.NewReportingMetricSetV2Error(t, config)
+	events, errs := mbtest.ReportingFetchV2Error(metricSet)
+	if len(errs) > 0 {
+		t.Fatalf("Expected 0 error, had %d. %v\n", len(errs), errs)
+	}
+
+	assert.NotEmpty(t, events)
+
+	for _, event := range events {
+		mtest.CheckEventField("cloud.region", "string", event, t)
+		mtest.CheckEventField("aws.dimensions.BucketName", "string", event, t)
+		mtest.CheckEventField("aws.dimensions.StorageType", "string", event, t)
+		mtest.CheckEventField("s3.metrics.AllRequests.avg", "int", event, t)
+		mtest.CheckEventField("s3.metrics.GetRequests.avg", "int", event, t)
+		mtest.CheckEventField("s3.metrics.PutRequests.avg", "int", event, t)
+		mtest.CheckEventField("s3.metrics.DeleteRequests.avg", "int", event, t)
+		mtest.CheckEventField("s3.metrics.HeadRequests.avg", "int", event, t)
+		mtest.CheckEventField("s3.metrics.PostRequests.avg", "int", event, t)
+		mtest.CheckEventField("s3.metrics.SelectRequests.avg", "int", event, t)
+		mtest.CheckEventField("s3.metrics.SelectScannedBytes.avg", "float", event, t)
+		mtest.CheckEventField("s3.metrics.SelectReturnedBytes.avg", "float", event, t)
+		mtest.CheckEventField("s3.metrics.ListRequests.avg", "int", event, t)
+		mtest.CheckEventField("s3.metrics.BytesDownloaded.avg", "float", event, t)
+		mtest.CheckEventField("s3.metrics.BytesUploaded.avg", "float", event, t)
+		mtest.CheckEventField("s3.metrics.4xxErrors.avg", "int", event, t)
+		mtest.CheckEventField("s3.metrics.5xxErrors.avg", "int", event, t)
+		mtest.CheckEventField("s3.metrics.FirstByteLatency.avg", "float", event, t)
+		mtest.CheckEventField("s3.metrics.TotalRequestLatency.avg", "float", event, t)
+	}
+}
 
 func TestData(t *testing.T) {
 	config := mtest.GetConfigForTest(t, "s3_request", "60s")
