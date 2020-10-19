89895f75cb1cb73db49251b2a5bd943b0d8971cc
Andrew Cholakian <andrew@andrewvc.com>
2020-10-19 14:25:34 -0500

    Add back missing oss code

---
+++ heartbeat/seccomp_linux.go
@@ -0,0 +1,35 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package main
+
+import (
+	"runtime"
+
+	"github.com/elastic/beats/v7/libbeat/common/seccomp"
+)
+
+func init() {
+	switch runtime.GOARCH {
+	case "amd64", "386":
+		if err := seccomp.ModifyDefaultPolicy(seccomp.AddSyscall, "execve"); err != nil {
+			panic(err)
+		}
+	case "arm":
+		// TODO: Figure out how to allow execve here
+	}
+}
