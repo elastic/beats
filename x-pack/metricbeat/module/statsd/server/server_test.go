b5a60a348109f694797603391f000e27e1523d01
urso <steffen.siering@elastic.co>
2020-10-19 16:59:35 +0200

    make fmt

--- x-pack/metricbeat/module/statsd/server/server_test.go
+++ x-pack/metricbeat/module/statsd/server/server_test.go
@@ -1,3 +1,7 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
 package server
 
 import (
@@ -7,8 +11,9 @@ import (
 	"testing"
 	"time"
 
-	mbtest "github.com/elastic/beats/v7/metricbeat/mb/testing"
 	"github.com/stretchr/testify/require"
+
+	mbtest "github.com/elastic/beats/v7/metricbeat/mb/testing"
 )
 
 func TestServerStart(t *testing.T) {
