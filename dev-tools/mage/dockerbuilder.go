81e447ad0c9a5947713c51335e4c63790280e237
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-10-21 15:40:47 +0100

    fix linting

--- dev-tools/mage/dockerbuilder.go
+++ dev-tools/mage/dockerbuilder.go
@@ -75,7 +75,8 @@ func (b *dockerBuilder) Build() error {
 		fmt.Println(">> Building docker images again (after 10 seconds)")
 		// This sleep is to avoid hitting the docker build issues when resources are not available.
 		time.Sleep(10)
-		tag, err = b.dockerBuild()
+		tagSecondTry, err := b.dockerBuild()
+		tag = tagSecondTry
 		if err != nil {
 			return errors.Wrap(err, "failed to build docker")
 		}
