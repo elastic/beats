2d1e310aa99112def671e9653e0a06c8acbdf212
kaiyan-sheng <kaiyan.sheng@elastic.co>
2020-10-21 19:17:36 -0600

    create events from ${ECS_CONTAINER_METADATA_URI_V4}/task/stats output

--- x-pack/metricbeat/module/awsfargate/task_stats/task_stats.go
+++ x-pack/metricbeat/module/awsfargate/task_stats/task_stats.go
@@ -10,6 +10,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/pkg/errors"
 
@@ -19,39 +20,7 @@ import (
 	"github.com/elastic/beats/v7/x-pack/metricbeat/module/awsfargate"
 )
 
-var (
-	metricsetName     = "task_stats"
-	taskStatsEndpoint = "${ECS_CONTAINER_METADATA_URI_V4}/task/stats"
-)
-
-type CPUUsage struct {
-	TotalUsage        float64       `json:"total_usage"`
-	PerCPUUsage       common.MapStr `json:"percpu_usage"`
-	UsageInKernelmode uint64        `json:"usage_in_kernelmode"`
-	UsageInUsermode   uint64        `json:"usage_in_usermode"`
-}
-
-type CPUStats struct {
-	CPUUsage CPUUsage `json:"cpu_usage"`
-	//PerCPUUsage                           common.MapStr
-	//TotalUsage                            float64
-	//TotalUsageNormalized                  float64
-	//UsageInKernelmode                     uint64
-	//UsageInKernelmodePercentage           float64
-	//UsageInKernelmodePercentageNormalized float64
-	//UsageInUsermode                       uint64
-	//UsageInUsermodePercentage             float64
-	//UsageInUsermodePercentageNormalized   float64
-	//SystemUsage                           uint64
-	//SystemUsagePercentage                 float64
-	//SystemUsagePercentageNormalized       float64
-}
-
-type TaskMeta struct {
-	ReadTimestamp    string   `json:"read"`
-	PreReadTimestamp string   `json:"preread"`
-	CPUStats         CPUStats `json:"cpu_stats"`
-}
+var metricsetName = "task_stats"
 
 // init registers the MetricSet with the central registry as soon as the program
 // starts. The New function will be called later to instantiate an instance of
@@ -91,50 +60,133 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 // format. It publishes the event which is then forwarded to the output. In case
 // of an error set the Error field of mb.Event or simply call report.Error().
 func (m *MetricSet) Fetch(report mb.ReporterV2) error {
-	var taskStatsOutputs map[string]interface{}
-
 	// Get response from ${ECS_CONTAINER_METADATA_URI_V4}/task/stats
 	ecsURI, ok := os.LookupEnv("ECS_CONTAINER_METADATA_URI_V4")
 	if !ok {
 		err := fmt.Errorf("lookup $ECS_CONTAINER_METADATA_URI_V4 failed")
-		m.logger.Errorf(err.Error())
+		m.logger.Error(err)
 		return err
 	}
 
-	m.logger.Debugf("ECS_CONTAINER_METADATA_URI_V4 = %s", ecsURI)
-	resp, err := http.Get(fmt.Sprintf("http://%s/task/stats", ecsURI))
+	resp, err := http.Get(fmt.Sprintf("%s/task/stats", ecsURI))
 	if err != nil {
-		m.logger.Error(fmt.Errorf("http.Get ${ECS_CONTAINER_METADATA_URI_V4}/task/stats failed: %w", err))
+		err := fmt.Errorf("http.Get failed: %w", err)
+		m.logger.Error(err)
 		return err
 	}
-
-	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		m.logger.Error(err.Error())
+		err := fmt.Errorf("ioutil.ReadAll failed: %w", err)
+		m.logger.Error(err)
 		return err
 	}
 
-	err = json.Unmarshal(body, &taskStatsOutputs)
+	var output map[string]interface{}
+	err = json.Unmarshal(body, &output)
 	if err != nil {
-		m.logger.Error(fmt.Errorf("json.Unmarshal failed: %w", err))
+		err := fmt.Errorf("json.Unmarshal failed: %w", err)
+		m.logger.Error(err)
 		return err
 	}
 
 	var events []mb.Event
-	for queryID, taskStats := range taskStatsOutputs {
-		taskMeta := taskStats.(TaskMeta)
-		m.logger.Info("queryID = ", queryID)
-		m.logger.Info("taskMeta.CPUStats = ", taskMeta.CPUStats)
-		m.logger.Info("taskMeta.ReadTimestamp = ", taskMeta.ReadTimestamp)
-		fmt.Println("=========================")
-		fmt.Println("queryID = ", queryID)
-		fmt.Println("ReadTimestamp = ", taskMeta.ReadTimestamp)
-		fmt.Println("CPUStats = ", taskMeta.CPUStats)
+	for queryID, taskStats := range output {
+		event := mb.Event{
+			ID:              queryID,
+			MetricSetFields: common.MapStr{},
+		}
 
-		event := mb.Event{}
-		event.MetricSetFields.Put("cpu_usage", taskMeta.CPUStats.CPUUsage)
+		taskMeta := taskStats.(map[string]interface{})
+		readTimestamp := taskMeta["read"].(string)
+		timestamp, err := time.Parse(time.RFC3339, readTimestamp)
+		if err != nil {
+			m.logger.Warn(fmt.Errorf("parsing timestamp %s failed, use current timestamp in event instead", readTimestamp))
+		} else {
+			event.Timestamp = timestamp
+		}
+
+		cpuStats := taskMeta["cpu_stats"].(map[string]interface{})
+		memoryStats := taskMeta["memory_stats"].(map[string]interface{})
+		networks := taskMeta["networks"].(map[string]interface{})
+
+		cpuUsage := cpuStats["cpu_usage"].(map[string]interface{})
+		throttlingData := cpuStats["throttling_data"].(map[string]interface{})
+		memStats := memoryStats["stats"].(map[string]interface{})
+
+		cpuStatsData := common.MapStr{
+			"cpu_usage": common.MapStr{
+				"total_usage":         cpuUsage["total_usage"],
+				"usage_in_kernelmode": cpuUsage["usage_in_kernelmode"],
+				"usage_in_usermode":   cpuUsage["usage_in_usermode"],
+			},
+			"throttling_data": common.MapStr{
+				"periods":           throttlingData["periods"],
+				"throttled_periods": throttlingData["throttled_periods"],
+				"throttled_time":    throttlingData["throttled_time"],
+			},
+		}
+
+		memoryStatsData := common.MapStr{
+			"usage":     memoryStats["usage"],
+			"max_usage": memoryStats["max_usage"],
+			"stats": common.MapStr{
+				"active_anon":               memStats["active_anon"],
+				"active_file":               memStats["active_file"],
+				"cache":                     memStats["cache"],
+				"dirty":                     memStats["dirty"],
+				"hierarchical_memory_limit": memStats["hierarchical_memory_limit"],
+				"hierarchical_memsw_limit":  memStats["hierarchical_memsw_limit"],
+				"inactive_anon":             memStats["inactive_anon"],
+				"inactive_file":             memStats["inactive_file"],
+				"mapped_file":               memStats["mapped_file"],
+				"pgfault":                   memStats["pgfault"],
+				"pgmajfault":                memStats["pgmajfault"],
+				"pgpgin":                    memStats["pgpgin"],
+				"pgpgout":                   memStats["pgpgout"],
+				"rss":                       memStats["rss"],
+				"rss_huge":                  memStats["rss_huge"],
+				"total_active_anon":         memStats["total_active_anon"],
+				"total_active_file":         memStats["total_active_file"],
+				"total_cache":               memStats["total_cache"],
+				"total_dirty":               memStats["total_dirty"],
+				"total_inactive_anon":       memStats["total_inactive_anon"],
+				"total_inactive_file":       memStats["total_inactive_file"],
+				"total_mapped_file":         memStats["total_mapped_file"],
+				"total_pgfault":             memStats["total_pgfault"],
+				"total_pgmajfault":          memStats["total_pgmajfault"],
+				"total_pgpgin":              memStats["total_pgpgin"],
+				"total_pgpgout":             memStats["total_pgpgout"],
+				"total_rss":                 memStats["total_rss"],
+				"total_rss_huge":            memStats["total_rss_huge"],
+				"total_unevictable":         memStats["total_unevictable"],
+				"total_writeback":           memStats["total_writeback"],
+				"unevictable":               memStats["unevictable"],
+				"writeback":                 memStats["writeback"],
+			},
+			"limit": memoryStats["limit"],
+		}
+
+		networksData := common.MapStr{}
+		for networkName, network := range networks {
+			networkData := network.(map[string]interface{})
+			dataPerInterface := common.MapStr{
+				"rx_bytes": networkData["rx_bytes"],
+				"rx_packets": networkData["rx_packets"],
+				"rx_errors": networkData["rx_errors"],
+				"rx_dropped": networkData["rx_dropped"],
+				"tx_bytes": networkData["tx_bytes"],
+				"tx_packets": networkData["tx_packets"],
+				"tx_errors": networkData["tx_errors"],
+				"tx_dropped": networkData["tx_dropped"],
+			}
+			networksData.Put(networkName, dataPerInterface)
+		}
+
+		event.MetricSetFields.Put("name", taskMeta["name"])
+		event.MetricSetFields.Put("cpu_stats", cpuStatsData)
+		event.MetricSetFields.Put("memory_stats", memoryStatsData)
+		event.MetricSetFields.Put("networks", networksData)
 		events = append(events, event)
 	}
 
