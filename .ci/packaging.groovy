32bc9019dce2a1be7148d6d6091f3b50248962ad
Andrew Stucki <andrew.stucki@elastic.co>
2020-10-27 10:49:57 -0400

    [Packetbeat] Create x-pack magefile (#21979)
    * Create packetbeat x-pack magefile
    
    * add changelog entry
    
    * Fix up packetbeat magefile
    
    * Add makefile for x-pack directory
    
    * Uncomment packaging
    
    * Add desired platforms to Jenkinsfile
    
    * Add back in fields yml and kibana dashboards on package
    
    * Add generated configs in x-pack/packetbeat
    
    * Suggested changes
    
    (cherry picked from commit 3e73c99391799badcb5b5e8e63eba779928d2426)

--- .ci/packaging.groovy
+++ .ci/packaging.groovy
@@ -102,7 +102,7 @@ pipeline {
                    'x-pack/heartbeat',
                   // 'x-pack/journalbeat',
                   'x-pack/metricbeat',
-                  // 'x-pack/packetbeat',
+                  'x-pack/packetbeat',
                   'x-pack/winlogbeat'
                 )
               }
