0317832c3078518ae472194c1a11629faf75595d
Victor Martinez <victormartinezrubio@gmail.com>
2020-11-02 21:51:40 +0000

    Cherry-pick to 7.10: [CI] support windows-7-32 bits (#19797) (#22336)

--- .ci/scripts/install-tools.bat
+++ .ci/scripts/install-tools.bat
@@ -1,6 +1,12 @@
 set GOPATH=%WORKSPACE%
 set MAGEFILE_CACHE=%WORKSPACE%\.magefile
-set PATH=%WORKSPACE%\bin;C:\ProgramData\chocolatey\bin;C:\tools\mingw64\bin;%PATH%
+
+REM Configure GCC for either 32 or 64 bits
+set MINGW_ARCH=64
+IF NOT EXIST "%PROGRAMFILES(X86)%" (
+    set MINGW_ARCH=32
+)
+set PATH=%WORKSPACE%\bin;C:\ProgramData\chocolatey\bin;C:\tools\mingw%MINGW_ARCH%\bin;%PATH%
 
 where /q curl
 IF ERRORLEVEL 1 (
@@ -35,9 +41,13 @@ if not exist C:\Python38\python.exe (
 python --version
 where python
 
-if not exist C:\tools\mingw64\bin\gcc.exe (
+where /q gcc
+IF ERRORLEVEL 1 (
     REM Install mingw 5.3.0
-    choco install mingw -y -r --no-progress --version 5.3.0 || echo ERROR && exit /b
+    choco install mingw -y -r --no-progress --version 5.3.0
+    IF NOT ERRORLEVEL 0 (
+        exit /b 1
+    )
 )
 gcc --version
 where gcc
