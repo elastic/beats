7987d44e4f6876a1a677ed68ec88af80381fa7a9
urso <steffen.siering@elastic.co>
2020-11-11 20:25:57 +0100

    deleteDir?

--- .ci/packaging.groovy
+++ .ci/packaging.groovy
@@ -55,6 +55,7 @@ pipeline {
         stage('Checkout') {
           options { skipDefaultCheckout() }
           steps {
+	    deleteDir()
             script {
               if(isUpstreamTrigger()) {
                 try {
