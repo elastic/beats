df3f19eedf1aaa887f925d87c55c728b423d36b8
sayden <mariocaster@gmail.com>
2020-11-11 18:51:54 +0100

    Fix linting

--- metricbeat/module/elasticsearch/elasticsearch_integration_test.go
+++ metricbeat/module/elasticsearch/elasticsearch_integration_test.go
@@ -128,7 +128,7 @@ func TestXPackEnabled(t *testing.T) {
 
 	metricSetToTypesMap := map[string][]string{
 		"ccr":            []string{"ccr_stats", "ccr_auto_follow_stats"},
-		"cluster_stats":  []string{},// no longer indexed into .monitoring-es-*
+		"cluster_stats":  []string{}, // no longer indexed into .monitoring-es-*
 		"enrich":         []string{"enrich_coordinator_stats"},
 		"index_recovery": []string{"index_recovery"},
 		"index_summary":  []string{}, // no longer indexed into .monitoring-es-*
