3089e56bc9fd04b811b36cff05fea8a2e497cb09
sayden <mariocaster@gmail.com>
2020-11-18 14:29:33 +0100

    Add a TestData function

--- metricbeat/module/elasticsearch/cluster_stats/data_test.go
+++ metricbeat/module/elasticsearch/cluster_stats/data_test.go
@@ -20,13 +20,82 @@
 package cluster_stats
 
 import (
+	mbtest "github.com/elastic/beats/v7/metricbeat/mb/testing"
+	"github.com/elastic/beats/v7/metricbeat/module/elasticsearch"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-func TestMapper(t *testing.T) {
-	// TODO This test must be skipped until we find a reasonable way to test the event mapping when it has to do extra
-	// HTTP calls
+func createEsMuxer(esVersion, license string, ccrEnabled bool) *http.ServeMux {
+	nodesLocalHandler := func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`{"nodes": { "foobar": {}}}`))
+	}
+	clusterStateMasterHandler := func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`{"master_node": "foobar"}`))
+	}
+	rootHandler := func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+		}
+		w.Write([]byte(`{"version": { "number": "` + esVersion + `" } }`))
+	}
+	licenseHandler := func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`{ "license": { "type": "` + license + `" } }`))
+	}
 
-	t.SkipNow()
+	mux := http.NewServeMux()
+	mux.Handle("/_nodes/_local/nodes", http.HandlerFunc(nodesLocalHandler))
+	mux.Handle("/_cluster/state/master_node", http.HandlerFunc(clusterStateMasterHandler))
+	mux.Handle("/", http.HandlerFunc(rootHandler))
+	mux.Handle("/_license", http.HandlerFunc(licenseHandler))       // for 7.0 and above
+	mux.Handle("/_xpack/license", http.HandlerFunc(licenseHandler)) // for before 7.0
+
+	mux.Handle("/_xpack/usage", http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			input, _ := ioutil.ReadFile("./_meta/test/xpack-usage.710.json")
+			w.Write(input)
+		}))
+
+	mux.Handle("/_cluster/settings", http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			input, _ := ioutil.ReadFile("./_meta/test/cluster-settings.710.json")
+			w.Write(input)
+		}))
+
+	mux.Handle("/_cluster/stats", http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			input, _ := ioutil.ReadFile("./_meta/test/cluster_stats.710.json")
+			w.Write(input)
+		}))
+
+	mux.Handle("/_cluster/state/version,master_node,nodes,routing_table", http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			input, _ := ioutil.ReadFile("./_meta/test/cluster_state.710.json")
+			w.Write(input)
+		}))
+
+	return mux
+}
+
+func TestData(t *testing.T) {
 	//elasticsearch.TestMapperWithInfo(t, "./_meta/test/cluster_stats.*.json", eventMapping)
+
+	mux := createEsMuxer("7.6.0", "platinum", false)
+
+	server := httptest.NewServer(mux)
+	defer server.Close()
+
+	ms := mbtest.NewReportingMetricSetV2Error(t, getConfig(server.URL))
+	if err := mbtest.WriteEventsReporterV2Error(ms, t, ""); err != nil {
+		t.Fatal("write", err)
+	}
+}
+func getConfig(host string) map[string]interface{} {
+	return map[string]interface{}{
+		"module":     elasticsearch.ModuleName,
+		"metricsets": []string{"cluster_stats"},
+		"hosts":      []string{host},
+	}
 }
