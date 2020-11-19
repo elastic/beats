9ce33916ec5faceefd49726933c986a30e9ca984
P1llus <pillus@chasenet.org>
2020-11-19 22:46:27 +0100

    filebeat Snyk module MVP initial commit

--- x-pack/filebeat/include/list.go
+++ x-pack/filebeat/include/list.go
@@ -49,6 +49,7 @@ import (
 	_ "github.com/elastic/beats/v7/x-pack/filebeat/module/rabbitmq"
 	_ "github.com/elastic/beats/v7/x-pack/filebeat/module/radware"
 	_ "github.com/elastic/beats/v7/x-pack/filebeat/module/snort"
+	_ "github.com/elastic/beats/v7/x-pack/filebeat/module/snyk"
 	_ "github.com/elastic/beats/v7/x-pack/filebeat/module/sonicwall"
 	_ "github.com/elastic/beats/v7/x-pack/filebeat/module/sophos"
 	_ "github.com/elastic/beats/v7/x-pack/filebeat/module/squid"
