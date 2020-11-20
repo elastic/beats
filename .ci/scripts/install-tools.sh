0b98e70d4c25815bf9c185bb30450ba7915020b8
urso <steffen.siering@elastic.co>
2020-11-20 14:13:04 +0100

    try to switch to another directory before when installing gotestsum

--- .ci/scripts/install-tools.sh
+++ .ci/scripts/install-tools.sh
@@ -4,6 +4,5 @@ set -exuo pipefail
 .ci/scripts/install-go.sh
 .ci/scripts/install-docker-compose.sh
 .ci/scripts/install-terraform.sh
-go get gotest.tools/gotestsum
-go mod tidy
+(cd /tmp && go get gotest.tools/gotestsum)
 make mage
