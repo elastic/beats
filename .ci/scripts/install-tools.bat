a744625850f02825752c5e1e75009d950cab1129
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-11-20 16:46:29 +0000

    Fix w7 32 bits gvm installation

--- .ci/scripts/install-tools.bat
+++ .ci/scripts/install-tools.bat
@@ -14,17 +14,21 @@ IF ERRORLEVEL 1 (
 )
 mkdir %WORKSPACE%\bin
 
+set GVM_BIN=gvm
+SET GVM_URL=https://github.com/andrewkroh/gvm/releases/download/v0.2.2
 IF EXIST "%PROGRAMFILES(X86)%" (
     SET GVM_FILE=gvm-windows-amd64.exe
 ) ELSE (
+    REM Windows 7 workers got a broken gvm installation.
     SET GVM_FILE=gvm-windows-386.exe
+    set GVM_BIN=gvm.exe
+    curl -L -o %WORKSPACE%\bin\%GVM_BIN% %GVM_URL%/%GVM_FILE%
 )
 
-set GVM_BIN=gvm
 where /q %GVM_BIN%
 IF ERRORLEVEL 1 (
     set GVM_BIN=gvm.exe
-    curl -L -o %WORKSPACE%\bin\%GVM_BIN% https://github.com/andrewkroh/gvm/releases/download/v0.2.2/%GVM_FILE%
+    curl -L -o %WORKSPACE%\bin\%GVM_BIN% %GVM_URL%/%GVM_FILE%
     IF ERRORLEVEL 1 (
         REM The download of gvm has failed.
         exit /b 1
