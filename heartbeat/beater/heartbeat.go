9ef78ff7937eebf11b2ac8555dd1fcd40528a906
Andrew Cholakian <andrew@andrewvc.com>
2020-12-01 12:48:11 -0600

    [Heartbeat] Fix exit on disabled monitor (#22829)
    Fixes a bug where when `enabled: false` was set on a monitor heartbeat
    would refuse to start.
    
    Fixes https://github.com/elastic/beats/issues/22665
    
    (cherry picked from commit 15f351427f003e9466db9bd003a590b2f34df713)

--- heartbeat/beater/heartbeat.go
+++ heartbeat/beater/heartbeat.go
@@ -21,12 +21,12 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/elastic/beats/v7/heartbeat/hbregistry"
-
 	"github.com/pkg/errors"
 
 	"github.com/elastic/beats/v7/heartbeat/config"
+	"github.com/elastic/beats/v7/heartbeat/hbregistry"
 	"github.com/elastic/beats/v7/heartbeat/monitors"
+	"github.com/elastic/beats/v7/heartbeat/monitors/stdfields"
 	"github.com/elastic/beats/v7/heartbeat/scheduler"
 	"github.com/elastic/beats/v7/libbeat/autodiscover"
 	"github.com/elastic/beats/v7/libbeat/beat"
@@ -127,8 +127,13 @@ func (bt *Heartbeat) RunStaticMonitors(b *beat.Beat) error {
 	for _, cfg := range bt.config.Monitors {
 		created, err := factory.Create(b.Publisher, cfg)
 		if err != nil {
+			if err == stdfields.ErrPluginDisabled {
+				continue // don't stop loading monitors just because they're disabled
+			}
+
 			return errors.Wrap(err, "could not create monitor")
 		}
+
 		created.Start()
 	}
 	return nil
