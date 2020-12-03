b4c068cb60793ffe52d71400e0aea537ac7d9acc
Shaunak Kashyap <ycombinator@gmail.com>
2020-12-03 13:42:00 +0530

    Implement basic scaffolding for rate_limit processor

---
+++ libbeat/processors/rate_limit/limit.go
@@ -0,0 +1,80 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package rate_limit
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type unit string
+
+const (
+	unitPerSecond unit = "s"
+	unitPerMinute unit = "m"
+	unitPerHour   unit = "h"
+)
+
+type Limit struct {
+	value float64
+	unit  unit
+}
+
+// Unpack creates a limit from the given string
+func (l *Limit) Unpack(str string) error {
+	parts := strings.Split(str, "/")
+	if len(parts) != 2 {
+		// TODO: make custom error?
+		return fmt.Errorf(`rate limit in invalid format: %v. Must be specified as "number/unit"`, str)
+	}
+
+	valueStr := strings.TrimSpace(parts[0])
+	unitStr := strings.TrimSpace(parts[1])
+
+	v, err := strconv.ParseFloat(valueStr, 8)
+	if err != nil {
+		return fmt.Errorf(`rate limit's limit component is not numeric: %v`, valueStr)
+	}
+
+	if allowed := []unit{unitPerSecond, unitPerMinute, unitPerHour}; !contains(allowed, unitStr) {
+		allowedStrs := make([]string, len(allowed))
+		for _, a := range allowed {
+			allowedStrs = append(allowedStrs, "/"+string(a))
+		}
+
+		return fmt.Errorf(`rate limit's unit component must be specified as one of: %v`, strings.Join(allowedStrs, ","))
+	}
+
+	u := unit(unitStr)
+
+	l.value = v
+	l.unit = u
+
+	return nil
+}
+
+func contains(allowed []unit, candidate string) bool {
+	for _, a := range allowed {
+		if candidate == string(a) {
+			return true
+		}
+	}
+
+	return false
+}
---
+++ libbeat/processors/rate_limit/rate_limit.go
@@ -0,0 +1,89 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package rate_limit
+
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+
+	"github.com/elastic/beats/v7/libbeat/beat"
+	"github.com/elastic/beats/v7/libbeat/common"
+	"github.com/elastic/beats/v7/libbeat/processors"
+	"github.com/elastic/beats/v7/libbeat/processors/rate_limit/algorithm"
+	jsprocessor "github.com/elastic/beats/v7/libbeat/processors/script/javascript/module/processor"
+)
+
+func init() {
+	processors.RegisterPlugin("rate_limit", New)
+	jsprocessor.RegisterPlugin("Fingerprint", New)
+}
+
+const processorName = "rate_limit"
+
+type rateLimit struct {
+	config    Config
+	algorithm algorithm.Algorithm
+}
+
+// New constructs a new rate limit processor.
+func New(cfg *common.Config) (processors.Processor, error) {
+	config := defaultConfig()
+	if err := cfg.Unpack(&config); err != nil {
+		// TODO: make custom error: errConfigUnpack?
+		return nil, errors.Wrap(err, "could not unpack processor configuration")
+	}
+
+	algo, err := algorithm.Factory(config.Algorithm.Name())
+	if err != nil {
+		return nil, errors.Wrap(err, "could not instantiate rate limiting algorithm")
+	}
+
+	algo.Configure(algorithm.Config{
+		Limit:  config.Limit,
+		Config: *config.Algorithm.Config(),
+	})
+
+	// TODO: flesh out fields
+	p := &rateLimit{
+		config:    config,
+		algorithm: algo,
+	}
+
+	return p, nil
+}
+
+// Run applies the configured rate limit to the given event. If the event is within the
+// configured rate limit, it is returned as-is. If not, nil is returned.
+func (p *rateLimit) Run(event *beat.Event) (*beat.Event, error) {
+	key := "" // TODO: construct key from event fields + config
+
+	if p.algorithm.IsAllowed(key) {
+		return event, nil
+	}
+
+	// TODO: log that event is being dropped
+	return nil, nil
+}
+
+func (p *rateLimit) String() string {
+	return fmt.Sprintf(
+		"%v=[limit=[%v],fields=[%v],algorithm=[%v]]",
+		processorName, p.config.Limit, p.config.Fields, p.config.Algorithm.Name(),
+	)
+}
