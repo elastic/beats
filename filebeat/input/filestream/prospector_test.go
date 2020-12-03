863c297d857b5915238e6865a7c13c4e82256a86
Noémi Ványi <kvch@users.noreply.github.com>
2020-12-03 11:30:44 +0100

    Add more Golang tests to filestream input (#22789)

--- filebeat/input/filestream/prospector_test.go
+++ filebeat/input/filestream/prospector_test.go
@@ -19,6 +19,8 @@ package filestream
 
 import (
 	"context"
+	"fmt"
+	"io/ioutil"
 	"os"
 	"testing"
 	"time"
@@ -27,30 +29,164 @@ import (
 
 	loginp "github.com/elastic/beats/v7/filebeat/input/filestream/internal/input-logfile"
 	input "github.com/elastic/beats/v7/filebeat/input/v2"
+	"github.com/elastic/beats/v7/libbeat/common/transform/typeconv"
 	"github.com/elastic/beats/v7/libbeat/logp"
 	"github.com/elastic/go-concert/unison"
 )
+
+func TestProspector_InitCleanIfRemoved(t *testing.T) {
+	testCases := map[string]struct {
+		entries             map[string]loginp.Value
+		filesOnDisk         map[string]os.FileInfo
+		cleanRemoved        bool
+		expectedCleanedKeys []string
+	}{
+		"prospector init with clean_removed enabled with no entries": {
+			entries:             nil,
+			filesOnDisk:         nil,
+			cleanRemoved:        true,
+			expectedCleanedKeys: []string{},
+		},
+		"prospector init with clean_removed disabled with entries": {
+			entries: map[string]loginp.Value{
+				"key1": &mockUnpackValue{
+					fileMeta{
+						Source:         "/no/such/path",
+						IdentifierName: "path",
+					},
+				},
+			},
+			filesOnDisk:         nil,
+			cleanRemoved:        false,
+			expectedCleanedKeys: []string{},
+		},
+		"prospector init with clean_removed enabled with entries": {
+			entries: map[string]loginp.Value{
+				"key1": &mockUnpackValue{
+					fileMeta{
+						Source:         "/no/such/path",
+						IdentifierName: "path",
+					},
+				},
+			},
+			filesOnDisk:         nil,
+			cleanRemoved:        true,
+			expectedCleanedKeys: []string{"key1"},
+		},
+	}
+
+	for name, testCase := range testCases {
+		testCase := testCase
+
+		t.Run(name, func(t *testing.T) {
+			testStore := newMockProspectorCleaner(testCase.entries)
+			p := fileProspector{
+				identifier:   mustPathIdentifier(false),
+				cleanRemoved: testCase.cleanRemoved,
+				filewatcher:  &mockFileWatcher{filesOnDisk: testCase.filesOnDisk},
+			}
+			p.Init(testStore)
+
+			assert.ElementsMatch(t, testCase.expectedCleanedKeys, testStore.cleanedKeys)
+		})
+	}
+
+}
+
+func TestProspector_InitUpdateIdentifiers(t *testing.T) {
+	f, err := ioutil.TempFile("", "existing_file")
+	if err != nil {
+		t.Fatalf("cannot create temp file")
+	}
+	defer f.Close()
+	tmpFileName := f.Name()
+	fi, err := f.Stat()
+	if err != nil {
+		t.Fatalf("cannot stat test file: %v", err)
+	}
+
+	testCases := map[string]struct {
+		entries             map[string]loginp.Value
+		filesOnDisk         map[string]os.FileInfo
+		expectedUpdatedKeys map[string]string
+	}{
+		"prospector init does not update keys if there are no entries": {
+			entries:             nil,
+			filesOnDisk:         nil,
+			expectedUpdatedKeys: map[string]string{},
+		},
+		"prospector init does not update keys of not existing files": {
+			entries: map[string]loginp.Value{
+				"not_path::key1": &mockUnpackValue{
+					fileMeta{
+						Source:         "/no/such/path",
+						IdentifierName: "not_path",
+					},
+				},
+			},
+			filesOnDisk:         nil,
+			expectedUpdatedKeys: map[string]string{},
+		},
+		"prospector init updates keys of existing files": {
+			entries: map[string]loginp.Value{
+				"not_path::key1": &mockUnpackValue{
+					fileMeta{
+						Source:         tmpFileName,
+						IdentifierName: "not_path",
+					},
+				},
+			},
+			filesOnDisk: map[string]os.FileInfo{
+				tmpFileName: fi,
+			},
+			expectedUpdatedKeys: map[string]string{"not_path::key1": "path::" + tmpFileName},
+		},
+	}
+
+	for name, testCase := range testCases {
+		testCase := testCase
+
+		t.Run(name, func(t *testing.T) {
+			testStore := newMockProspectorCleaner(testCase.entries)
+			p := fileProspector{
+				identifier:  mustPathIdentifier(false),
+				filewatcher: &mockFileWatcher{filesOnDisk: testCase.filesOnDisk},
+			}
+			p.Init(testStore)
+
+			assert.EqualValues(t, testCase.expectedUpdatedKeys, testStore.updatedKeys)
+		})
+	}
+
+}
 
 func TestProspectorNewAndUpdatedFiles(t *testing.T) {
 	minuteAgo := time.Now().Add(-1 * time.Minute)
 
 	testCases := map[string]struct {
-		events          []loginp.FSEvent
-		ignoreOlder     time.Duration
-		expectedSources []string
+		events         []loginp.FSEvent
+		ignoreOlder    time.Duration
+		expectedEvents []harvesterEvent
 	}{
 		"two new files": {
 			events: []loginp.FSEvent{
 				loginp.FSEvent{Op: loginp.OpCreate, NewPath: "/path/to/file"},
 				loginp.FSEvent{Op: loginp.OpCreate, NewPath: "/path/to/other/file"},
 			},
-			expectedSources: []string{"path::/path/to/file", "path::/path/to/other/file"},
+			expectedEvents: []harvesterEvent{
+				harvesterStart("path::/path/to/file"),
+				harvesterStart("path::/path/to/other/file"),
+				harvesterGroupStop{},
+			},
 		},
 		"one updated file": {
 			events: []loginp.FSEvent{
 				loginp.FSEvent{Op: loginp.OpWrite, NewPath: "/path/to/file"},
 			},
-			expectedSources: []string{"path::/path/to/file"},
+			expectedEvents: []harvesterEvent{
+				harvesterStart("path::/path/to/file"),
+				harvesterGroupStop{},
+			},
 		},
 		"old files with ignore older configured": {
 			events: []loginp.FSEvent{
@@ -65,8 +201,10 @@ func TestProspectorNewAndUpdatedFiles(t *testing.T) {
 					Info:    testFileInfo{"/path/to/other/file", 5, minuteAgo},
 				},
 			},
-			ignoreOlder:     10 * time.Second,
-			expectedSources: []string{},
+			ignoreOlder: 10 * time.Second,
+			expectedEvents: []harvesterEvent{
+				harvesterGroupStop{},
+			},
 		},
 		"newer files with ignore older": {
 			events: []loginp.FSEvent{
@@ -81,8 +219,12 @@ func TestProspectorNewAndUpdatedFiles(t *testing.T) {
 					Info:    testFileInfo{"/path/to/other/file", 5, minuteAgo},
 				},
 			},
-			ignoreOlder:     5 * time.Minute,
-			expectedSources: []string{"path::/path/to/file", "path::/path/to/other/file"},
+			ignoreOlder: 5 * time.Minute,
+			expectedEvents: []harvesterEvent{
+				harvesterStart("path::/path/to/file"),
+				harvesterStart("path::/path/to/other/file"),
+				harvesterGroupStop{},
+			},
 		},
 	}
 
@@ -92,15 +234,15 @@ func TestProspectorNewAndUpdatedFiles(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			p := fileProspector{
 				filewatcher: &mockFileWatcher{events: test.events},
-				identifier:  mustPathIdentifier(),
+				identifier:  mustPathIdentifier(false),
 				ignoreOlder: test.ignoreOlder,
 			}
 			ctx := input.Context{Logger: logp.L(), Cancelation: context.Background()}
-			hg := getTestHarvesterGroup()
+			hg := newTestHarvesterGroup()
 
 			p.Run(ctx, newMockMetadataUpdater(), hg)
 
-			assert.ElementsMatch(t, hg.encounteredNames, test.expectedSources)
+			assert.ElementsMatch(t, test.expectedEvents, hg.events)
 		})
 	}
 }
@@ -130,7 +272,7 @@ func TestProspectorDeletedFile(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			p := fileProspector{
 				filewatcher:  &mockFileWatcher{events: test.events},
-				identifier:   mustPathIdentifier(),
+				identifier:   mustPathIdentifier(false),
 				cleanRemoved: test.cleanRemoved,
 			}
 			ctx := input.Context{Logger: logp.L(), Cancelation: context.Background()}
@@ -138,7 +280,7 @@ func TestProspectorDeletedFile(t *testing.T) {
 			testStore := newMockMetadataUpdater()
 			testStore.set("path::/path/to/file")
 
-			p.Run(ctx, testStore, getTestHarvesterGroup())
+			p.Run(ctx, testStore, newTestHarvesterGroup())
 
 			has := testStore.has("path::/path/to/file")
 
@@ -152,27 +294,126 @@ func TestProspectorDeletedFile(t *testing.T) {
 	}
 }
 
-type testHarvesterGroup struct {
-	encounteredNames []string
+func TestProspectorRenamedFile(t *testing.T) {
+	testCases := map[string]struct {
+		events         []loginp.FSEvent
+		trackRename    bool
+		closeRenamed   bool
+		expectedEvents []harvesterEvent
+	}{
+		"one renamed file without rename tracker": {
+			events: []loginp.FSEvent{
+				loginp.FSEvent{
+					Op:      loginp.OpRename,
+					OldPath: "/old/path/to/file",
+					NewPath: "/new/path/to/file",
+				},
+			},
+			expectedEvents: []harvesterEvent{
+				harvesterStop("path::/old/path/to/file"),
+				harvesterStart("path::/new/path/to/file"),
+				harvesterGroupStop{},
+			},
+		},
+		"one renamed file with rename tracker": {
+			events: []loginp.FSEvent{
+				loginp.FSEvent{
+					Op:      loginp.OpRename,
+					OldPath: "/old/path/to/file",
+					NewPath: "/new/path/to/file",
+				},
+			},
+			trackRename: true,
+			expectedEvents: []harvesterEvent{
+				harvesterGroupStop{},
+			},
+		},
+		"one renamed file with rename tracker with close renamed": {
+			events: []loginp.FSEvent{
+				loginp.FSEvent{
+					Op:      loginp.OpRename,
+					OldPath: "/old/path/to/file",
+					NewPath: "/new/path/to/file",
+				},
+			},
+			trackRename:  true,
+			closeRenamed: true,
+			expectedEvents: []harvesterEvent{
+				harvesterStop("path::/old/path/to/file"),
+				harvesterGroupStop{},
+			},
+		},
+	}
+
+	for name, test := range testCases {
+		test := test
+
+		t.Run(name, func(t *testing.T) {
+			p := fileProspector{
+				filewatcher:       &mockFileWatcher{events: test.events},
+				identifier:        mustPathIdentifier(test.trackRename),
+				stateChangeCloser: stateChangeCloserConfig{Renamed: test.closeRenamed},
+			}
+			ctx := input.Context{Logger: logp.L(), Cancelation: context.Background()}
+
+			testStore := newMockMetadataUpdater()
+			testStore.set("path::/old/path/to/file")
+
+			hg := newTestHarvesterGroup()
+			p.Run(ctx, testStore, hg)
+
+			has := testStore.has("path::/old/path/to/file")
+			if test.trackRename {
+				assert.True(t, has)
+			} else {
+				assert.False(t, has)
+			}
+
+			assert.Equal(t, test.expectedEvents, hg.events)
+
+		})
+	}
 }
 
-func getTestHarvesterGroup() *testHarvesterGroup { return &testHarvesterGroup{make([]string, 0)} }
+type harvesterEvent interface{ String() string }
+
+type harvesterStart string
+
+func (h harvesterStart) String() string { return string(h) }
+
+type harvesterStop string
+
+func (h harvesterStop) String() string { return string(h) }
+
+type harvesterGroupStop struct{}
+
+func (h harvesterGroupStop) String() string { return "stop" }
+
+type testHarvesterGroup struct {
+	events []harvesterEvent
+}
+
+func newTestHarvesterGroup() *testHarvesterGroup {
+	return &testHarvesterGroup{make([]harvesterEvent, 0)}
+}
 
 func (t *testHarvesterGroup) Start(_ input.Context, s loginp.Source) {
-	t.encounteredNames = append(t.encounteredNames, s.Name())
+	t.events = append(t.events, harvesterStart(s.Name()))
 }
 
-func (t *testHarvesterGroup) Stop(_ loginp.Source) {
-	return
+func (t *testHarvesterGroup) Stop(s loginp.Source) {
+	t.events = append(t.events, harvesterStop(s.Name()))
 }
 
 func (t *testHarvesterGroup) StopGroup() error {
+	t.events = append(t.events, harvesterGroupStop{})
 	return nil
 }
 
 type mockFileWatcher struct {
-	nextIdx int
-	events  []loginp.FSEvent
+	nextIdx     int
+	events      []loginp.FSEvent
+	filesOnDisk map[string]os.FileInfo
 }
 
 func (m *mockFileWatcher) Event() loginp.FSEvent {
@@ -186,7 +427,7 @@ func (m *mockFileWatcher) Event() loginp.FSEvent {
 
 func (m *mockFileWatcher) Run(_ unison.Canceler) { return }
 
-func (m *mockFileWatcher) GetFiles() map[string]os.FileInfo { return nil }
+func (m *mockFileWatcher) GetFiles() map[string]os.FileInfo { return m.filesOnDisk }
 
 type mockMetadataUpdater struct {
 	table map[string]interface{}
@@ -206,6 +447,10 @@ func (mu *mockMetadataUpdater) has(id string) bool {
 }
 
 func (mu *mockMetadataUpdater) FindCursorMeta(s loginp.Source, v interface{}) error {
+	v, ok := mu.table[s.Name()]
+	if !ok {
+		return fmt.Errorf("no such id")
+	}
 	return nil
 }
 
@@ -219,11 +464,59 @@ func (mu *mockMetadataUpdater) Remove(s loginp.Source) error {
 	return nil
 }
 
-func mustPathIdentifier() fileIdentifier {
+type mockUnpackValue struct {
+	fileMeta
+}
+
+func (u *mockUnpackValue) UnpackCursorMeta(to interface{}) error {
+	return typeconv.Convert(to, u.fileMeta)
+}
+
+type mockProspectorCleaner struct {
+	available   map[string]loginp.Value
+	cleanedKeys []string
+	newEntries  map[string]fileMeta
+	updatedKeys map[string]string
+}
+
+func newMockProspectorCleaner(available map[string]loginp.Value) *mockProspectorCleaner {
+	return &mockProspectorCleaner{
+		available:   available,
+		cleanedKeys: make([]string, 0),
+		updatedKeys: make(map[string]string, 0),
+	}
+}
+
+func (c *mockProspectorCleaner) CleanIf(pred func(v loginp.Value) bool) {
+	for key, meta := range c.available {
+		if pred(meta) {
+			c.cleanedKeys = append(c.cleanedKeys, key)
+		}
+	}
+}
+
+func (c *mockProspectorCleaner) UpdateIdentifiers(updater func(v loginp.Value) (string, interface{})) {
+	for key, meta := range c.available {
+		k, _ := updater(meta)
+		if k != "" {
+			c.updatedKeys[key] = k
+		}
+	}
+}
+
+type renamedPathIdentifier struct {
+	fileIdentifier
+}
+
+func (p *renamedPathIdentifier) Supports(_ identifierFeature) bool { return true }
+
+func mustPathIdentifier(renamed bool) fileIdentifier {
 	pathIdentifier, err := newPathIdentifier(nil)
 	if err != nil {
 		panic(err)
 	}
+	if renamed {
+		return &renamedPathIdentifier{pathIdentifier}
+	}
 	return pathIdentifier
-
 }
