4540460c86911071762df77936ca6cbeb8ee1578
Marc Guasch <marc-gr@users.noreply.github.com>
2020-12-14 17:04:56 +0100

    [Filebeat] Deprecate Gsuite module and create Google Workspace one (#22950)
    * Deprecate Gsuite module and create Google Workspace one
    
    * Update with latest gsuite changes
    
    * Use deprecation tag and fix doc typo

--- x-pack/filebeat/include/list.go
+++ x-pack/filebeat/include/list.go
@@ -30,6 +30,7 @@ import (
 	_ "github.com/elastic/beats/v7/x-pack/filebeat/module/f5"
 	_ "github.com/elastic/beats/v7/x-pack/filebeat/module/fortinet"
 	_ "github.com/elastic/beats/v7/x-pack/filebeat/module/gcp"
+	_ "github.com/elastic/beats/v7/x-pack/filebeat/module/google_workspace"
 	_ "github.com/elastic/beats/v7/x-pack/filebeat/module/gsuite"
 	_ "github.com/elastic/beats/v7/x-pack/filebeat/module/ibmmq"
 	_ "github.com/elastic/beats/v7/x-pack/filebeat/module/imperva"
