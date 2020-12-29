6d5179a073a4c36a8f8c52c26ee51401626d6385
sayden <mariocaster@gmail.com>
2020-12-29 13:40:07 +0100

    Fix potentially last broken test

--- metricbeat/module/couchdb/server/server.go
+++ metricbeat/module/couchdb/server/server.go
@@ -1,4 +1,4 @@
-	// Licensed to Elasticsearch B.V. under one or more contributor
+// Licensed to Elasticsearch B.V. under one or more contributor
 // license agreements. See the NOTICE file distributed with
 // this work for additional information regarding copyright
 // ownership. Elasticsearch B.V. licenses this file to you under
