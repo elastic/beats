b9746f13c4352f583f4fbf36191516a8849d6ea4
Chris Mark <chrismarkou92@gmail.com>
2021-01-13 10:19:23 +0200

    Cherry-pick #23419 to 7.11: Handle DeletedFinalStateUnknown in k8s OnDelete (#23442)

--- libbeat/autodiscover/providers/kubernetes/service.go
+++ libbeat/autodiscover/providers/kubernetes/service.go
@@ -21,8 +21,6 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/elastic/beats/v7/libbeat/common/kubernetes/metadata"
-
 	"github.com/gofrs/uuid"
 	k8s "k8s.io/client-go/kubernetes"
 
@@ -30,6 +28,7 @@ import (
 	"github.com/elastic/beats/v7/libbeat/common"
 	"github.com/elastic/beats/v7/libbeat/common/bus"
 	"github.com/elastic/beats/v7/libbeat/common/kubernetes"
+	"github.com/elastic/beats/v7/libbeat/common/kubernetes/metadata"
 	"github.com/elastic/beats/v7/libbeat/common/safemapstr"
 	"github.com/elastic/beats/v7/libbeat/logp"
 )
