90aa1fab2a91b53a6206338929894e9185c0ebb7
Michal Pristas <michal.pristas@gmail.com>
2021-01-21 17:13:44 +0100

    fix refresh of monitoring beats

--- x-pack/elastic-agent/pkg/agent/operation/monitoring.go
+++ x-pack/elastic-agent/pkg/agent/operation/monitoring.go
@@ -130,7 +130,7 @@ func (o *Operator) generateMonitoringSteps(version string, output interface{}) [
 	watchMetrics := o.monitor.WatchMetrics()
 
 	// generate only on change
-	if watchLogs != o.isMonitoringLogs() {
+	if watchLogs != o.isMonitoringLogs() || watchLogs {
 		fbConfig, any := o.getMonitoringFilebeatConfig(output)
 		stepID := configrequest.StepRun
 		if !watchLogs || !any {
@@ -151,7 +151,7 @@ func (o *Operator) generateMonitoringSteps(version string, output interface{}) [
 
 		steps = append(steps, filebeatStep)
 	}
-	if watchMetrics != o.isMonitoringMetrics() {
+	if watchMetrics != o.isMonitoringMetrics() || watchMetrics {
 		mbConfig, any := o.getMonitoringMetricbeatConfig(output)
 		stepID := configrequest.StepRun
 		if !watchMetrics || !any {
@@ -552,7 +552,19 @@ func (o *Operator) getMetricbeatEndpoints() map[string][]string {
 	for _, a := range o.apps {
 		metricEndpoint := a.Monitor().MetricsPathPrefixed(a.Spec(), o.pipelineID)
 		if metricEndpoint != "" {
-			endpoints[strings.ReplaceAll(a.Name(), "-", "_")] = append(endpoints[a.Name()], metricEndpoint)
+			safeName := strings.ReplaceAll(a.Name(), "-", "_")
+			// prevent duplicates
+			var found bool
+			for _, ep := range endpoints[safeName] {
+				if ep == metricEndpoint {
+					found = true
+					break
+				}
+			}
+
+			if !found {
+				endpoints[safeName] = append(endpoints[safeName], metricEndpoint)
+			}
 		}
 	}
 
