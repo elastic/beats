717cb229664092950ceea9c94cd2093bdc89c88d
Michal Pristas <michal.pristas@gmail.com>
2021-01-21 22:01:49 +0100

    [Ingest Manager] Fix refresh of monitoring configuration (#23619)
    * fix refresh of monitoring beats
    
    * more descriptive comment
    
    * changelog
    
    * newline

--- x-pack/elastic-agent/pkg/agent/stateresolver/stateresolver.go
+++ x-pack/elastic-agent/pkg/agent/stateresolver/stateresolver.go
@@ -47,6 +47,10 @@ func (s *StateResolver) Resolve(
 
 	s.l.Infof("New State ID is %s", newStateID)
 	s.l.Infof("Converging state requires execution of %d step(s)", len(steps))
+	for i, step := range steps {
+		// more detailed debug log
+		s.l.Debugf("step %d: %s", i, step.String())
+	}
 
 	// Allow the operator to ack the should state when applying the steps is done correctly.
 	ack := func() {
