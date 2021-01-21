83138c9b689232da8dd5aa560d01f6c07a601c70
chrismark <chrismarkou92@gmail.com>
2021-01-21 14:16:32 +0200

    lint metricbeat
    Signed-off-by: chrismark <chrismarkou92@gmail.com>

--- metricbeat/module/kubernetes/util/kubernetes_test.go
+++ metricbeat/module/kubernetes/util/kubernetes_test.go
@@ -20,8 +20,8 @@ package util
 import (
 	"testing"
 
-	"k8s.io/client-go/tools/cache"
 	k8s "k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
 
 	"github.com/stretchr/testify/assert"
 	v1 "k8s.io/api/core/v1"
