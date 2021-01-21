628929bcca442a8d42509a217b001694de16a33a
Michal Pristas <michal.pristas@gmail.com>
2021-01-21 19:30:48 +0100

    [Ingest Manager] Fixed nil pointer during unenroll (#23609)
    [Ingest Manager] Fixed nil pointer during unenroll (#23609)

--- x-pack/elastic-agent/pkg/agent/application/lazy_acker.go
+++ x-pack/elastic-agent/pkg/agent/application/lazy_acker.go
@@ -7,6 +7,7 @@ package application
 import (
 	"context"
 
+	"github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/core/logger"
 	"github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/fleetapi"
 )
 
@@ -19,19 +20,22 @@ type ackForcer interface {
 }
 
 type lazyAcker struct {
+	log   *logger.Logger
 	acker batchAcker
 	queue []fleetapi.Action
 }
 
-func newLazyAcker(baseAcker batchAcker) *lazyAcker {
+func newLazyAcker(baseAcker batchAcker, log *logger.Logger) *lazyAcker {
 	return &lazyAcker{
 		acker: baseAcker,
 		queue: make([]fleetapi.Action, 0),
+		log:   log,
 	}
 }
 
 func (f *lazyAcker) Ack(ctx context.Context, action fleetapi.Action) error {
 	f.queue = append(f.queue, action)
+	f.log.Debugf("appending action with id '%s' to the queue", action.ID())
 
 	if _, isAckForced := action.(ackForcer); isAckForced {
 		return f.Commit(ctx)
