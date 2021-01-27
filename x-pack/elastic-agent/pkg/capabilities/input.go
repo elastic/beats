c21a7c7f8c99f4fd6e08975c451699e06ce4f248
Michal Pristas <michal.pristas@gmail.com>
2021-01-27 14:25:40 +0100

    expr

--- x-pack/elastic-agent/pkg/capabilities/input.go
+++ x-pack/elastic-agent/pkg/capabilities/input.go
@@ -4,11 +4,39 @@
 
 package capabilities
 
+import (
+	"github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/agent/transpiler"
+)
+
 type inputCapability struct {
-	Type string `json:"rule" yaml:"rule"`
+	Type  string `json:"rule" yaml:"rule"`
+	Input string `json:"input" yaml:"input"`
 }
 
 func (c *inputCapability) Apply(in interface{}) (bool, interface{}) {
+	ast, err := inputObject(in)
+	if err != nil {
+		// TODO: log error
+		return false, in
+	}
+
+	inputs, ok := transpiler.Lookup(ast, "inputs")
+	if ok {
+		renderedInputs, err := c.renderInputs(inputs)
+		if err != nil {
+			// TODO: log error
+			return false, in
+		}
+
+		err = transpiler.Insert(ast, renderedInputs, "inputs")
+		if err != nil {
+			// TODO: log error
+			return false, in
+		}
+
+		return false, ast
+	}
+
 	return false, in
 }
 
@@ -24,4 +52,46 @@ func NewInputCapability(r ruler) (Capability, error) {
 	}
 
 	return cap, nil
+}
+
+func (c *inputCapability) renderInputs(inputs transpiler.Node) (transpiler.Node, error) {
+	return inputs, nil
+
+	// l, ok := inputs.Value().(*transpiler.List)
+	// if !ok {
+	// 	return nil, fmt.Errorf("inputs must be an array")
+	// }
+
+	// nodes := []*transpiler.Dict{}
+
+	// for _, inputNode := range l.Value().([]transpiler.Node) {
+	// 	// if input does not match definition continue
+	// 	// TODO: get from input
+	// var inputType = "system/metrics"
+
+	// 	// if condition empty
+	// 	// check if condition key already exists and if it has true/false value continue
+	// 	// mark condition rule==allow
+	// 	// endif condition empty
+
+	// }
+
+	// nInputs := []transpiler.Node{}
+	// for _, node := range nodes {
+	// 	nInputs = append(nInputs, node)
+	// }
+	// return transpiler.NewList(nInputs), nil
+
+}
+
+func inputObject(a interface{}) (*transpiler.AST, error) {
+	if ast, ok := a.(*transpiler.AST); ok {
+		return ast, nil
+	}
+
+	if mm, ok := a.(map[string]interface{}); ok {
+		return transpiler.NewAST(mm)
+	}
+
+	return nil, nil
 }
