bc6f1038778911553ef5be0137ea7b0e3e0bcbde
Michal Pristas <michal.pristas@gmail.com>
2021-02-04 12:57:33 +0100

    Fixed monitoring filebeat and metricbeat not connecting to Agent over GRPC (#23843)
    Fixed monitoring filebeat and metricbeat not connecting to Agent over GRPC (#23843)

--- x-pack/elastic-agent/pkg/agent/operation/monitoring.go
+++ x-pack/elastic-agent/pkg/agent/operation/monitoring.go
@@ -138,13 +138,9 @@ func (o *Operator) generateMonitoringSteps(version string, output interface{}) [
 			stepID = configrequest.StepRemove
 		}
 		filebeatStep := configrequest.Step{
-			ID:      stepID,
-			Version: version,
-			ProgramSpec: program.Spec{
-				Name:     logsProcessName,
-				Cmd:      logsProcessName,
-				Artifact: fmt.Sprintf("%s/%s", artifactPrefix, logsProcessName),
-			},
+			ID:          stepID,
+			Version:     version,
+			ProgramSpec: loadSpecFromSupported(logsProcessName),
 			Meta: map[string]interface{}{
 				configrequest.MetaConfigKey: fbConfig,
 			},
@@ -160,13 +156,9 @@ func (o *Operator) generateMonitoringSteps(version string, output interface{}) [
 		}
 
 		metricbeatStep := configrequest.Step{
-			ID:      stepID,
-			Version: version,
-			ProgramSpec: program.Spec{
-				Name:     metricsProcessName,
-				Cmd:      metricsProcessName,
-				Artifact: fmt.Sprintf("%s/%s", artifactPrefix, metricsProcessName),
-			},
+			ID:          stepID,
+			Version:     version,
+			ProgramSpec: loadSpecFromSupported(metricsProcessName),
 			Meta: map[string]interface{}{
 				configrequest.MetaConfigKey: mbConfig,
 			},
@@ -176,6 +168,18 @@ func (o *Operator) generateMonitoringSteps(version string, output interface{}) [
 	}
 
 	return steps
+}
+
+func loadSpecFromSupported(processName string) program.Spec {
+	if loadedSpec, found := program.SupportedMap[strings.ToLower(processName)]; found {
+		return loadedSpec
+	}
+
+	return program.Spec{
+		Name:     processName,
+		Cmd:      processName,
+		Artifact: fmt.Sprintf("%s/%s", artifactPrefix, processName),
+	}
 }
 
 func (o *Operator) getMonitoringFilebeatConfig(output interface{}) (map[string]interface{}, bool) {
