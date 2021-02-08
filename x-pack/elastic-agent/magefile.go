e5508dcbe9136002aa256d02de76ae8e180f7006
Shahzad <shahzad.muhammad@elastic.co>
2021-02-08 21:55:49 +0100

    Revert "[Agent] Remove heartbeat from bundled beats in agent." (#22695)
    Co-authored-by: Andrew Cholakian <andrew@andrewvc.com>
    (cherry picked from commit f87eac2b46330ca0cbbf471ddbc197f5f33d2a56)

--- x-pack/elastic-agent/magefile.go
+++ x-pack/elastic-agent/magefile.go
@@ -564,7 +564,7 @@ func packageAgent(requiredPackages []string, packagingFn func()) {
 		defer os.RemoveAll(dropPath)
 		defer os.Unsetenv(agentDropPath)
 
-		packedBeats := []string{"filebeat", "metricbeat"}
+		packedBeats := []string{"filebeat", "heartbeat", "metricbeat"}
 
 		for _, b := range packedBeats {
 			pwd, err := filepath.Abs(filepath.Join("..", b))
