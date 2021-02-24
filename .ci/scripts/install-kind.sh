6c0470c69abce32bd5894a1b6e152bf984055f0b
Victor Martinez <victormartinezrubio@gmail.com>
2021-02-24 13:48:59 +0000

    [CI] reuse configuration from the workers if possible (#24184)

--- .ci/scripts/install-kind.sh
+++ .ci/scripts/install-kind.sh
@@ -11,7 +11,7 @@ if command -v kind
 then
     echo "Found Kind. Checking version.."
     FOUND_KIND_VERSION=$(kind --version 2>&1 >/dev/null | awk '{print $3}')
-    if [ $FOUND_KIND_VERSION == $KIND_VERSION ]
+    if [ "$FOUND_KIND_VERSION" == "$KIND_VERSION" ]
     then
         echo "Versions match. No need to install Kind. Exiting."
         exit 0
@@ -20,7 +20,22 @@ fi
 
 echo "UNMET DEP: Installing Kind"
 
+OS=$(uname -s| tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m| tr '[:upper:]' '[:lower:]')
+if [ "${ARCH}" == "aarch64" ] ; then
+    ARCH_SUFFIX=arm64
+else
+    ARCH_SUFFIX=amd64
+fi
+
 mkdir -p "${HOME}/bin"
 
-curl -sSLo "${KIND_CMD}" "https://github.com/kubernetes-sigs/kind/releases/download/${KIND_VERSION}/kind-linux-amd64"
-chmod +x "${KIND_CMD}"
+if curl -sSLo "${KIND_CMD}" "https://github.com/kubernetes-sigs/kind/releases/download/${KIND_VERSION}/kind-${OS}-${ARCH_SUFFIX}" ; then
+    chmod +x "${KIND_CMD}"
+else
+    echo "Something bad with the download, let's delete the corrupted binary"
+    if [ -e "${KIND_CMD}" ] ; then
+        rm "${KIND_CMD}"
+    fi
+    exit 1
+fi
