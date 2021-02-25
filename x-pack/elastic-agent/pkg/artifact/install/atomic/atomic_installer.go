2684d9cf207283c64c7a194fe9923d78d5cb5e76
Michal Pristas <michal.pristas@gmail.com>
2021-02-25 19:32:13 +0100

    tests

--- x-pack/elastic-agent/pkg/artifact/install/atomic/atomic_installer.go
+++ x-pack/elastic-agent/pkg/artifact/install/atomic/atomic_installer.go
@@ -10,10 +10,10 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
-	"sync"
 
 	"github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/agent/application/paths"
 	"github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/agent/program"
+	"go.uber.org/multierr"
 )
 
 type embeddedInstaller interface {
@@ -24,7 +24,6 @@ type embeddedInstaller interface {
 // successful finish.
 type Installer struct {
 	installer embeddedInstaller
-	wg        sync.WaitGroup
 }
 
 // NewInstaller creates a new AtomicInstaller
@@ -34,16 +33,8 @@ func NewInstaller(i embeddedInstaller) (*Installer, error) {
 	}, nil
 }
 
-// Allows caller to wait for install to be finished
-func (i *Installer) Wait() {
-	i.wg.Wait()
-}
-
 // Install performs installation of program in a specific version.
 func (i *Installer) Install(ctx context.Context, spec program.Spec, version, installDir string) error {
-	i.wg.Add(1)
-	defer i.wg.Done()
-
 	// tar installer uses Dir of installDir to determine location of unpack
 	tempDir, err := ioutil.TempDir(paths.TempDir(), "elastic-agent-install")
 	if err != nil {
@@ -62,22 +53,42 @@ func (i *Installer) Install(ctx context.Context, spec program.Spec, version, ins
 
 	if err := i.installer.Install(ctx, spec, version, tempInstallDir); err != nil {
 		// cleanup unfinished install
-		os.RemoveAll(tempInstallDir)
+		if rerr := os.RemoveAll(tempInstallDir); rerr != nil {
+			err = multierr.Append(err, rerr)
+		}
 		return err
 	}
 
 	if err := os.Rename(tempInstallDir, installDir); err != nil {
-		os.RemoveAll(installDir)
-		os.RemoveAll(tempInstallDir)
+		if rerr := os.RemoveAll(installDir); rerr != nil {
+			err = multierr.Append(err, rerr)
+		}
+		if rerr := os.RemoveAll(tempInstallDir); rerr != nil {
+			err = multierr.Append(err, rerr)
+		}
 		return err
 	}
 
 	// on windows rename is not atomic so if we were in cancellation process let's start over
 	// after restart
-	if err := ctx.Err(); runtime.GOOS == "windows" && err != nil {
-		os.RemoveAll(installDir)
-		os.RemoveAll(tempInstallDir)
-		return err
+	if runtime.GOOS == "windows" {
+		// sync
+		f, err := os.OpenFile(installDir, os.O_SYNC|os.O_RDWR, 0755)
+		if err == nil {
+			f.Sync()
+		}
+
+		// remove
+		if err := ctx.Err(); err != nil {
+			if rerr := os.RemoveAll(installDir); rerr != nil {
+				err = multierr.Append(err, rerr)
+			}
+			if rerr := os.RemoveAll(tempInstallDir); rerr != nil {
+				err = multierr.Append(err, rerr)
+			}
+			return err
+		}
+
 	}
 
 	return nil
