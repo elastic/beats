6e9c75c134fdb302ff06982e117e85471f881084
Andrew Cholakian <andrew@andrewvc.com>
2021-03-05 15:36:14 -0600

    [Heartbeat] Produce error rather than panic on missing source
    Fixes #24403.
    
    With the changes to the heartbeat config syntax in 7.12 the `source`
    field is now required. Our config validation code didn't actually check
    for this field's presence, which caused an NPE.
    
    This PR adds a validation checking for that config's presence. It also
    adds tests for the validation code for config sub-fields. There were no
    defects found in the validations for source.inline, or source.browser,
    but a few tests were missing.
    
    Instead of the panic seen in #24403 users will now get the error seen
    below.
    
    ```
    2021-03-05T15:41:40.146-0600	ERROR	instance/beat.go:952	Exiting: could not create monitor: job err could not parse suite config: config 'source' must be specified for this monitor, if upgrading from a previous experimental version please see our new config docs accessing 'heartbeat.monitors.0' (source:'sample-synthetics-config/heartbeat.yml')
    Exiting: could not create monitor: job err could not parse suite config: config 'source' must be specified for this monitor, if upgrading from a previous experimental version please see our new config docs accessing 'heartbeat.monitors.0' (source:'sample-synthetics-config/heartbeat.yml')
    ```

---
+++ x-pack/heartbeat/monitors/browser/config_test.go
@@ -0,0 +1,55 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package browser
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/elastic/beats/v7/x-pack/heartbeat/monitors/browser/source"
+)
+
+func TestConfig_Validate(t *testing.T) {
+	testSource := source.Source{Inline: &source.InlineSource{Script: "//something"}}
+
+	tests := []struct {
+		name    string
+		cfg     *Config
+		wantErr error
+	}{
+		{
+			"no error",
+			&Config{Id: "myid", Name: "myname", Source: &testSource},
+			nil,
+		},
+		{
+			"no id",
+			&Config{Name: "myname", Source: &testSource},
+			ErrIdRequired,
+		},
+		{
+			"no name",
+			&Config{Id: "myid", Source: &testSource},
+			ErrNameRequired,
+		},
+		{
+			"no source",
+			&Config{Id: "myid", Name: "myname"},
+			ErrSourceRequired,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.cfg.Validate()
+
+			if tt.wantErr != nil {
+				require.Equal(t, tt.wantErr, err)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
