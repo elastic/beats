0d1564b899743926dcc77eb578d8488f7f5cd668
Mario Castro <mariocaster@gmail.com>
2021-03-08 19:17:22 +0100

    [SM] Kibana mapping to remove extra fields (#23976)

--- metricbeat/module/kibana/stats/data.go
+++ metricbeat/module/kibana/stats/data.go
@@ -19,13 +19,12 @@ package stats
 
 import (
 	"encoding/json"
-
+	"github.com/elastic/beats/v7/metricbeat/helper/elastic"
 	"github.com/pkg/errors"
 
 	"github.com/elastic/beats/v7/libbeat/common"
 	s "github.com/elastic/beats/v7/libbeat/common/schema"
 	c "github.com/elastic/beats/v7/libbeat/common/schema/mapstriface"
-	"github.com/elastic/beats/v7/metricbeat/helper/elastic"
 	"github.com/elastic/beats/v7/metricbeat/mb"
 )
 
@@ -42,12 +41,12 @@ var (
 				"free_in_bytes":  c.Int("free_bytes"),
 				"used_in_bytes":  c.Int("used_bytes"),
 			}),
-			"uptime_in_millis": c.Int("uptime_ms"),
-			"distro":           c.Str("distro", s.Optional),
-			"distroRelease":    c.Str("distro_release", s.Optional),
-			"platform":         c.Str("platform", s.Optional),
-			"platformRelease":  c.Str("platform_release", s.Optional),
+			"distro":          c.Str("distro", s.Optional),
+			"distroRelease":   c.Str("distro_release", s.Optional),
+			"platform":        c.Str("platform", s.Optional),
+			"platformRelease": c.Str("platform_release", s.Optional),
 		}),
+		"kibana": c.Ifc("kibana"),
 
 		"uuid":  c.Str("kibana.uuid"),
 		"name":  c.Str("kibana.name"),
@@ -55,7 +54,6 @@ var (
 		"host": s.Object{
 			"name": c.Str("kibana.host"),
 		},
-		"usage":                  c.Ifc("usage.kibana", s.Optional),
 		"transport_address":      c.Str("kibana.transport_address"),
 		"version":                c.Str("kibana.version"),
 		"snapshot":               c.Bool("kibana.snapshot"),
@@ -117,35 +115,19 @@ func eventMapping(r mb.ReporterV2, content []byte) error {
 
 	event := mb.Event{ModuleFields: common.MapStr{}, RootFields: common.MapStr{}}
 
+
 	// Set elasticsearch cluster id
 	elasticsearchClusterID, ok := data["cluster_uuid"]
 	if !ok {
 		event.Error = elastic.MakeErrorForMissingField("cluster_uuid", elastic.Kibana)
-		r.Event(event)
 		return event.Error
 	}
 	event.ModuleFields.Put("elasticsearch.cluster.id", elasticsearchClusterID)
-
-	// Set process PID
-	process, ok := data["process"].(map[string]interface{})
-	if !ok {
-		event.Error = elastic.MakeErrorForMissingField("process", elastic.Kibana)
-		r.Event(event)
-		return event.Error
-	}
-	pid, ok := process["pid"].(float64)
-	if !ok {
-		event.Error = elastic.MakeErrorForMissingField("process.pid", elastic.Kibana)
-		r.Event(event)
-		return event.Error
-	}
-	event.RootFields.Put("process.pid", int(pid))
 
 	// Set service ID
 	uuid, err := dataFields.GetValue("uuid")
 	if err != nil {
 		event.Error = elastic.MakeErrorForMissingField("kibana.uuid", elastic.Kibana)
-		r.Event(event)
 		return event.Error
 	}
 	event.RootFields.Put("service.id", uuid)
@@ -155,24 +137,37 @@ func eventMapping(r mb.ReporterV2, content []byte) error {
 	version, err := dataFields.GetValue("version")
 	if err != nil {
 		event.Error = elastic.MakeErrorForMissingField("kibana.version", elastic.Kibana)
-		r.Event(event)
 		return event.Error
 	}
 	event.RootFields.Put("service.version", version)
 	dataFields.Delete("version")
 
 	// Set service address
-	serviceAddress, err := dataFields.GetValue("transport_address")
+	serviceAddress, err := dataFields.GetValue("kibana.transport_address")
 	if err != nil {
 		event.Error = elastic.MakeErrorForMissingField("kibana.transport_address", elastic.Kibana)
-		r.Event(event)
 		return event.Error
 	}
 	event.RootFields.Put("service.address", serviceAddress)
-	dataFields.Delete("transport_address")
+
+	// Set process PID
+	process, ok := data["process"].(map[string]interface{})
+	if !ok {
+		event.Error = elastic.MakeErrorForMissingField("process", elastic.Kibana)
+		return event.Error
+	}
+	pid, ok := process["pid"].(float64)
+	if !ok {
+		event.Error = elastic.MakeErrorForMissingField("process.pid", elastic.Kibana)
+		return event.Error
+	}
+	event.RootFields.Put("process.pid", int(pid))
+
+	dataFields.Delete("kibana")
 
 	event.MetricSetFields = dataFields
 
 	r.Event(event)
+
 	return nil
 }
