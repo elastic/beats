b92e1d6faf4b1ae57cbfdfd22a85347a8ee077e4
Michal Pristas <michal.pristas@gmail.com>
2021-03-15 14:44:54 +0100

    fmt

--- libbeat/common/jsontransform/jsonhelper.go
+++ libbeat/common/jsontransform/jsonhelper.go
@@ -21,10 +21,11 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/hashicorp/go-multierror"
+
 	"github.com/elastic/beats/v7/libbeat/beat"
 	"github.com/elastic/beats/v7/libbeat/common"
 	"github.com/elastic/beats/v7/libbeat/logp"
-	"github.com/hashicorp/go-multierror"
 )
 
 const (
