7d1aa137b197e3f6f22814780509bca173c1bdca
narph <mariana@elastic.co>
2021-03-15 18:21:45 +0100

    delete

--- x-pack/azure-vm-extension/handler/windows/scripts/test.ps1
+++
@@ -1,6 +0,0 @@
-$ScriptDirectory = Split-Path $MyInvocation.MyCommand.Path
-. (Join-Path $ScriptDirectory log.ps1)
-$ScriptDirectory = Split-Path $MyInvocation.MyCommand.Path
-. (Join-Path $ScriptDirectory helper.ps1)
-
-
