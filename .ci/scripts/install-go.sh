5c438733c2c9f3172bbddb8bf36fef19645c8079
Victor Martinez <victormartinezrubio@gmail.com>
2021-03-17 09:53:55 +0000

    [CI] bump gvm version and use the binary (#24571)
    (cherry picked from commit 34e5c09bd3fd4b1fbb59b4adf1de567aae7af0c8)
    
    # Conflicts:
    #	.ci/scripts/install-go.sh
    #	.ci/scripts/install-tools.bat
    #	Vagrantfile

--- .ci/scripts/install-go.sh
+++ .ci/scripts/install-go.sh
@@ -24,9 +24,13 @@ fi
 echo "UNMET DEP: Installing Go"
 mkdir -p "${HOME}/bin"
 
+<<<<<<< HEAD
 curl -sSLo "${GVM_CMD}" "https://github.com/andrewkroh/gvm/releases/download/v0.2.1/gvm-${ARCH}-amd64"
+=======
+curl -sSLo "${GVM_CMD}" "https://github.com/andrewkroh/gvm/releases/download/v0.3.0/gvm-${OS}-${GVM_ARCH_SUFFIX}"
+>>>>>>> 34e5c09bd ([CI] bump gvm version and use the binary (#24571))
 chmod +x "${GVM_CMD}"
 
-gvm ${GO_VERSION}|cut -d ' ' -f 2|tr -d '\"' > ${PROPERTIES_FILE}
+${GVM_CMD} "${GO_VERSION}" |cut -d ' ' -f 2|tr -d '\"' > ${PROPERTIES_FILE}
 
-eval $(gvm ${GO_VERSION})
+eval "$("${GVM_CMD}" "${GO_VERSION}")"
