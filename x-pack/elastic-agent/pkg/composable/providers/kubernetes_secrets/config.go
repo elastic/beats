a2d9051facb8813e55be0d3dc955a7d5f8735d43
chrismark <chrismarkou92@gmail.com>
2021-03-26 17:08:13 +0200

    Add code to fetch secret from k8s api
    Signed-off-by: chrismark <chrismarkou92@gmail.com>

--- x-pack/elastic-agent/pkg/composable/providers/kubernetes_secrets/config.go
+++ x-pack/elastic-agent/pkg/composable/providers/kubernetes_secrets/config.go
@@ -7,26 +7,7 @@
 
 package kubernetes_secrets
 
-import (
-	"time"
-)
-
 // Config for kubernetes provider
 type Config struct {
 	KubeConfig     string        `config:"kube_config"`
-	SyncPeriod     time.Duration `config:"sync_period"`
-	CleanupTimeout time.Duration `config:"cleanup_timeout" validate:"positive"`
-
-	// Needed when resource is a pod
-	Node string `config:"node"`
-
-	// Scope of the provider (cluster or node)
-	Scope string `config:"scope"`
-}
-
-// InitDefaults initializes the default values for the config.
-func (c *Config) InitDefaults() {
-	c.SyncPeriod = 10 * time.Minute
-	c.CleanupTimeout = 60 * time.Second
-	c.Scope = "node"
 }
--- x-pack/elastic-agent/pkg/composable/providers/kubernetes_secrets/kubernetes_secrets.go
+++ x-pack/elastic-agent/pkg/composable/providers/kubernetes_secrets/kubernetes_secrets.go
@@ -5,18 +5,18 @@
 package kubernetes_secrets
 
 import (
+	"strings"
+	"context"
+
+	k8sclient "k8s.io/client-go/kubernetes"
+
+	"github.com/elastic/beats/v7/libbeat/keystore"
+	"github.com/elastic/beats/v7/libbeat/common/kubernetes"
 	"github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/agent/errors"
 	"github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/composable"
 	"github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/config"
 	corecomp "github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/core/composable"
 	"github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/core/logger"
-)
-
-const (
-	// PodPriority is the priority that pod mappings are added to the provider.
-	PodPriority = 0
-	// ContainerPriority is the priority that container mappings are added to the provider.
-	ContainerPriority = 1
 )
 
 func init() {
@@ -27,6 +27,7 @@ func init() {
 type contextProviderK8sSecrets struct {
 	logger *logger.Logger
 	config *Config
+	client k8sclient.Interface
 }
 
 // DynamicProviderBuilder builds the dynamic provider.
@@ -39,15 +40,53 @@ func ContextProviderBuilder(logger *logger.Logger, c *config.Config) (corecomp.C
 	if err != nil {
 		return nil, errors.New(err, "failed to unpack configuration")
 	}
-	return &contextProviderK8sSecrets{logger, &cfg}, nil
+	return &contextProviderK8sSecrets{logger, &cfg, nil}, nil
 }
 
-func (p *contextProviderK8sSecrets) Fetch(value string) (string, error) {
+func (p *contextProviderK8sSecrets) Fetch(key string) (string, error) {
 	// TODO: add actual call to k8s api here to get the secret
 	return "someSecret42", nil
+	// actual code
+	// key = "kubernetes_secrets.somenamespace.somesecret.value"
+	tokens := strings.Split(key, ".")
+	if len(tokens) > 0 && tokens[0] != "kubernetes" {
+		return "", keystore.ErrKeyDoesntExists
+	}
+	if len(tokens) != 4 {
+		p.logger.Debugf(
+			"not valid secret key: %v. Secrets should be of the following format %v",
+			key,
+			"kubernetes_secrets.somenamespace.somesecret.value",
+		)
+		return "", keystore.ErrKeyDoesntExists
+	}
+	ns := tokens[1]
+	secretName := tokens[2]
+	secretVar := tokens[3]
+
+	secretIntefrace := p.client.CoreV1().Secrets(ns)
+	ctx := context.TODO()
+	secret, err := secretIntefrace.Get(ctx, secretName, metav1.GetOptions{})
+	if err != nil {
+		p.logger.Errorf("Could not retrieve secret from k8s API: %v", err)
+		return "", keystore.ErrKeyDoesntExists
+	}
+	if _, ok := secret.Data[secretVar]; !ok {
+		p.logger.Errorf("Could not retrieve value %v for secret %v", secretVar, secretName)
+		return "", keystore.ErrKeyDoesntExists
+	}
+	secretString := secret.Data[secretVar]
+	return string(secretString), nil
 }
 
 // Run runs the k8s secrets context provider.
 func (p *contextProviderK8sSecrets) Run(comm corecomp.ContextProviderComm) error {
+	client, err := kubernetes.GetKubernetesClient(p.config.KubeConfig)
+	if err != nil {
+		// info only; return nil (do nothing)
+		p.logger.Debugf("Kubernetes_secrets provider skipped, unable to connect: %s", err)
+		return nil
+	}
+	p.client = client
 	return nil
 }
