7551ef54c4b4d6020d6bb1a4c95e430bb9597d11
chrismark <chrismarkou92@gmail.com>
2021-04-01 14:06:13 +0300

    Add extra test
    Signed-off-by: chrismark <chrismarkou92@gmail.com>

--- x-pack/elastic-agent/pkg/composable/providers/kubernetessecrets/kubernetes_secrets_test.go
+++ x-pack/elastic-agent/pkg/composable/providers/kubernetessecrets/kubernetes_secrets_test.go
@@ -59,3 +59,42 @@ func Test_K8sSecretsProvider_Fetch(t *testing.T) {
 	assert.True(t, found)
 	assert.Equal(t, val, pass)
 }
+
+func Test_K8sSecretsProvider_FetchWrongSecret(t *testing.T) {
+	client := k8sfake.NewSimpleClientset()
+	ns := "test_namespace"
+	pass := "testing_passpass"
+	secret := &v1.Secret{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Secret",
+			APIVersion: "apps/v1beta1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "testing_secret",
+			Namespace: ns,
+		},
+		Data: map[string][]byte{
+			"secret_value": []byte(pass),
+		},
+	}
+	_, err := client.CoreV1().Secrets(ns).Create(context.Background(), secret, metav1.CreateOptions{})
+	require.NoError(t, err)
+
+	logger := logp.NewLogger("test_k8s_secrets")
+	cfg, err := config.NewConfigFrom(map[string]string{"a": "b"})
+	require.NoError(t, err)
+
+	p, err := ContextProviderBuilder(logger, cfg)
+	require.NoError(t, err)
+
+	fp := p.(corecomp.FetchContextProvider)
+
+	getK8sClientFunc = func(kubeconfig string) (k8sclient.Interface, error) {
+		return client, nil
+	}
+	require.NoError(t, err)
+	fp.Run(nil)
+	val, found := fp.Fetch("kubernetes_secrets.test_namespace.testing_secretHACK.secret_value")
+	assert.False(t, found)
+	assert.EqualValues(t, val, "")
+}
