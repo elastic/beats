f123a3e338efa24a78adcd81d90bd1cda78cc682
kaiyan-sheng <kaiyan.sheng@elastic.co>
2021-04-15 23:19:00 +0800

    add fields.yml for log fileset

--- x-pack/filebeat/module/awsfargate/fields.go
+++ x-pack/filebeat/module/awsfargate/fields.go
@@ -19,5 +19,5 @@ func init() {
 // AssetAwsfargate returns asset data.
 // This is the base64 encoded gzipped contents of module/awsfargate.
 func AssetAwsfargate() string {
-	return "eJx0jjGqwzAQRHudYi5gH0DFB/OJu1QpUm/klRCRtUZaE5zTBxvHRUimHIZ5r8GdFwt6VE8lkLIBNGpii+56QX+UhRNTZYsbKxlg4OpKnDRKtvgzAHCWYU4MLwVOUmKnMQc4yUoxc0GSUOGLjOhGekrG6f8gtAbwkdNQ7fbVINPIH2JrdJnYIhSZp735YrKm395+8TaZdh+/wa8AAAD//6JpVBs="
+	return "eJx0kDFuwzAMRXef4l8gPoCGAkHRbJ06dCxYmxKE0qJB0SjS0xc2YsMJnD9S0n9PPOGHrwH0WyNZIucG8OzCAefPD1y2obEwVQ74ZqcG6Ll2lkfPWgJeGgB4134SRlRDpyLceS4JnRanXNggmiqi6YDzQH9a8Pa6EdoGiJmlr2HpOqHQwA9ic/w6ckAyncbb5MBkzmVpe8ZbZNrb5T14DxdN2+yIPOdxL2t6jjSJfy3dAZGk8t3xofReXG31Xp3vV9nuXq0/+A8AAP//VOqIaQ=="
 }
