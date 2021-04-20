bc082564173a4b591f7769fbea2755ed8f6611be
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-04-20 12:00:22 +0100

    Add git add/commit commands as required by the automation

--- .ci/bump-stack-version.sh
+++ .ci/bump-stack-version.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+#
+# Given the stack version this script will bump the version.
+#
+# This script is executed by the automation we are putting in place
+# and it requires the git add/commit commands.
+#
 set -euo pipefail
 MSG="parameter missing."
 VERSION=${1:?$MSG}
@@ -14,3 +20,12 @@ fi
 echo "Update stack with version ${VERSION}"
 ${SED} -E -e "s#(image: docker\.elastic\.co/.*):[0-9]+\.[0-9]+\.[0-9]+(-[a-f0-9]{8})?#\1:${VERSION}#g" testing/environments/snapshot-oss.yml
 ${SED} -E -e "s#(image: docker\.elastic\.co/.*):[0-9]+\.[0-9]+\.[0-9]+(-[a-f0-9]{8})?#\1:${VERSION}#g" testing/environments/snapshot.yml
+
+echo "Commit changes"
+git config user.email
+git checkout -b "update-stack-version-$(date "+%Y%m%d%H%M%S")"
+git add testing/environments/snapshot-oss.yml testing/environments/snapshot.yml
+git commit -m "bump stack version ${VERSION}"
+git --no-pager log -1
+
+echo "You can now push and create a Pull Request"
