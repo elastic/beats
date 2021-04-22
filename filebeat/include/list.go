3e7e46eb7b2cf2664b4385c48a8c1149cde20a70
Alex Resnick <adr8292@gmail.com>
2021-04-22 17:14:56 +0000

    update zookeeper module based on comments.

--- filebeat/include/list.go
+++ filebeat/include/list.go
@@ -51,5 +51,4 @@ import (
 	_ "github.com/elastic/beats/v7/filebeat/module/santa"
 	_ "github.com/elastic/beats/v7/filebeat/module/system"
 	_ "github.com/elastic/beats/v7/filebeat/module/traefik"
-	_ "github.com/elastic/beats/v7/filebeat/module/zookeeper"
 )
