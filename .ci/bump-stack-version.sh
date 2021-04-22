ef9fa23b8cc3bca472676f7f7aab90d441b12cb9
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-04-22 12:22:52 +0100

    Refactor and enable the create branch parameter

--- .ci/bump-stack-version.sh
+++ .ci/bump-stack-version.sh
@@ -5,9 +5,16 @@
 # This script is executed by the automation we are putting in place
 # and it requires the git add/commit commands.
 #
+# Parameters:
+#	$1 -> the version to be bumped. Mandatory.
+#	$2 -> whether to create a branch where to commit the changes to.
+#		  this is required when reusing an existing Pull Request.
+#		  Optional. Default true.
+#
 set -euo pipefail
 MSG="parameter missing."
 VERSION=${1:?$MSG}
+CREATE_BRANCH=${2:-true}
 
 OS=$(uname -s| tr '[:upper:]' '[:lower:]')
 
@@ -17,14 +24,25 @@ else
 	SED="sed -i"
 fi
 
+FILES="testing/environments/snapshot-oss.yml
+testing/environments/snapshot.yml
+"
+
 echo "Update stack with version ${VERSION}"
-${SED} -E -e "s#(image: docker\.elastic\.co/.*):[0-9]+\.[0-9]+\.[0-9]+(-[a-f0-9]{8})?#\1:${VERSION}#g" testing/environments/snapshot-oss.yml
-${SED} -E -e "s#(image: docker\.elastic\.co/.*):[0-9]+\.[0-9]+\.[0-9]+(-[a-f0-9]{8})?#\1:${VERSION}#g" testing/environments/snapshot.yml
+for FILE in ${FILES} ; do
+	${SED} -E -e "s#(image: docker\.elastic\.co/.*):[0-9]+\.[0-9]+\.[0-9]+(-[a-f0-9]{8})?#\1:${VERSION}#g" $FILE
+done
 
 echo "Commit changes"
 git config user.email
-git checkout -b "update-stack-version-$(date "+%Y%m%d%H%M%S")"
-git add testing/environments/snapshot-oss.yml testing/environments/snapshot.yml
+if [ "$CREATE_BRANCH" = "true" ]; then
+	git checkout -b "update-stack-version-$(date "+%Y%m%d%H%M%S")"
+else
+	echo "Branch creation disabled."
+fi
+for FILE in ${FILES} ; do
+	echo "git add $FILE"
+done
 git commit -m "bump stack version ${VERSION}"
 git --no-pager log -1
 
