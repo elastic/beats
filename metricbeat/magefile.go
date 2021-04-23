b01b030773bf5e78f56ebeb393d25d3cda32567a
Manuel de la Peña <mdelapenya@gmail.com>
2021-04-23 20:20:40 +0200

    chore: import new mage namespace into filebeat and metricbeat

--- metricbeat/magefile.go
+++ metricbeat/magefile.go
@@ -51,6 +51,8 @@ import (
 	// mage:import
 	_ "github.com/elastic/beats/v7/dev-tools/mage/target/compose"
 	// mage:import
+	_ "github.com/elastic/beats/v7/dev-tools/mage/target/manifesttool"
+	// mage:import
 	_ "github.com/elastic/beats/v7/metricbeat/scripts/mage/target/metricset"
 )
 
