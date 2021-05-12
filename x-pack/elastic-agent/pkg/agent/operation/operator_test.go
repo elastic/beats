5b73732e4f13bdf8b9b3eccc271b6a3038328f81
Michal Pristas <michal.pristas@gmail.com>
2021-05-12 18:55:49 +0200

    [Elastic Agent] Disable flaky test (#25656) (#25688)
    [Elastic Agent] Disable flaky test (#25656)

--- x-pack/elastic-agent/pkg/agent/operation/operator_test.go
+++ x-pack/elastic-agent/pkg/agent/operation/operator_test.go
@@ -139,6 +139,9 @@ func TestConfigurableRun(t *testing.T) {
 }
 
 func TestConfigurableFailed(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("this test is sometimes flaky on the last part, investigating @michal")
+	}
 	p := getProgram("configurable", "1.0")
 
 	operator := getTestOperator(t, downloadPath, installPath, p)
