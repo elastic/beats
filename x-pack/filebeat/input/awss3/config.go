07879daf400aa70a2a4ef3068c6950a13499c58a
Lee Hinman <57081003+leehinman@users.noreply.github.com>
2021-05-17 12:11:11 -0500

    Add multiline support to awss3 input (#25710)
    * Add multiline support to awss3 input
    
    - only applies to non JSON logs
    
    Closes #25249
    
    Co-authored-by: Andrew Kroh <andrew.kroh@elastic.co>

--- x-pack/filebeat/input/awss3/config.go
+++ x-pack/filebeat/input/awss3/config.go
@@ -9,25 +9,39 @@ import (
 	"regexp"
 	"time"
 
+	"github.com/dustin/go-humanize"
+
+	"github.com/elastic/beats/v7/libbeat/reader/multiline"
+	"github.com/elastic/beats/v7/libbeat/reader/readfile"
 	awscommon "github.com/elastic/beats/v7/x-pack/libbeat/common/aws"
 )
 
 type config struct {
-	APITimeout               time.Duration       `config:"api_timeout"`
-	ExpandEventListFromField string              `config:"expand_event_list_from_field"`
-	FileSelectors            []FileSelectorCfg   `config:"file_selectors"`
-	FipsEnabled              bool                `config:"fips_enabled"`
-	MaxNumberOfMessages      int                 `config:"max_number_of_messages"`
-	QueueURL                 string              `config:"queue_url" validate:"nonzero,required"`
-	VisibilityTimeout        time.Duration       `config:"visibility_timeout"`
-	AwsConfig                awscommon.ConfigAWS `config:",inline"`
+	APITimeout               time.Duration           `config:"api_timeout"`
+	ExpandEventListFromField string                  `config:"expand_event_list_from_field"`
+	FileSelectors            []FileSelectorCfg       `config:"file_selectors"`
+	FipsEnabled              bool                    `config:"fips_enabled"`
+	MaxNumberOfMessages      int                     `config:"max_number_of_messages"`
+	QueueURL                 string                  `config:"queue_url" validate:"nonzero,required"`
+	VisibilityTimeout        time.Duration           `config:"visibility_timeout"`
+	AwsConfig                awscommon.ConfigAWS     `config:",inline"`
+	MaxBytes                 int                     `config:"max_bytes" validate:"min=0,nonzero"`
+	Multiline                *multiline.Config       `config:"multiline"`
+	LineTerminator           readfile.LineTerminator `config:"line_terminator"`
+	Encoding                 string                  `config:"encoding"`
+	BufferSize               int                     `config:"buffer_size"`
 }
 
 // FileSelectorCfg defines type and configuration of FileSelectors
 type FileSelectorCfg struct {
-	RegexString              string         `config:"regex"`
-	Regex                    *regexp.Regexp `config:",ignore"`
-	ExpandEventListFromField string         `config:"expand_event_list_from_field"`
+	RegexString              string                  `config:"regex"`
+	Regex                    *regexp.Regexp          `config:",ignore"`
+	ExpandEventListFromField string                  `config:"expand_event_list_from_field"`
+	MaxBytes                 int                     `config:"max_bytes" validate:"min=0,nonzero"`
+	Multiline                *multiline.Config       `config:"multiline"`
+	LineTerminator           readfile.LineTerminator `config:"line_terminator"`
+	Encoding                 string                  `config:"encoding"`
+	BufferSize               int                     `config:"buffer_size"`
 }
 
 func defaultConfig() config {
@@ -36,6 +50,9 @@ func defaultConfig() config {
 		FipsEnabled:         false,
 		MaxNumberOfMessages: 5,
 		VisibilityTimeout:   300 * time.Second,
+		LineTerminator:      readfile.AutoLineTerminator,
+		MaxBytes:            10 * humanize.MiByte,
+		BufferSize:          16 * humanize.KiByte,
 	}
 }
 
