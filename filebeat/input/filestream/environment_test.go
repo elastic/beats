b194e1b6a8a8482eb3b28fe83319fb34f337d3de
Noémi Ványi <sitbackandwait@gmail.com>
2021-05-18 11:26:29 +0200

    more timestamp test

--- filebeat/input/filestream/environment_test.go
+++ filebeat/input/filestream/environment_test.go
@@ -367,7 +367,7 @@ func (e *inputTestingEnvironment) requireEventContents(nr int, key, value string
 }
 
 func (e *inputTestingEnvironment) requireEventTimestamp(nr int, ts string) {
-	tm, err := time.Parse("2006-01-02 15:04:05.999 +0000", ts)
+	tm, err := time.Parse("2006-01-02T15:04:05.999", ts)
 	if err != nil {
 		e.t.Fatal(err)
 	}
@@ -379,7 +379,7 @@ func (e *inputTestingEnvironment) requireEventTimestamp(nr int, ts string) {
 	}
 
 	selectedEvent := events[nr]
-	require.True(e.t, selectedEvent.Timestamp.Equal(tm))
+	require.True(e.t, selectedEvent.Timestamp.Equal(tm), "got: %s, expected: %s", selectedEvent.Timestamp.String(), tm.String())
 }
 
 type testInputStore struct {
