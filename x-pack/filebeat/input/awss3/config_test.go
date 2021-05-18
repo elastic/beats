87f4f269190fb9c7fdaa869b00c9328a757757a5
Lee E. Hinman <lee.e.hinman@elastic.co>
2021-05-18 12:30:35 -0500

    aws-s3 input default content-type
    - new option `content_type`
    - can be set at input or file selector level
    - overrides Content-Type that was given to the S3 object when it was
      uploaded.
    
    Closes #25697

--- x-pack/filebeat/input/awss3/config_test.go
+++ x-pack/filebeat/input/awss3/config_test.go
@@ -144,6 +144,16 @@ func TestConfig(t *testing.T) {
 			"max_bytes <0> must be greater than 0",
 			nil,
 		},
+		{
+			"error on expand_event_list_from_field and content_type != application/json ",
+			common.MapStr{
+				"queue_url":                    queueURL,
+				"expand_event_list_from_field": "Records",
+				"content_type":                 "text/plain",
+			},
+			"content_type must be `application/json` to be used with expand_event_list_from_field",
+			nil,
+		},
 	}
 
 	for _, tc := range testCases {
