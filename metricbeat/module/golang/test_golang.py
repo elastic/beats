4382d43e7ca76f991c0a208576fe6ad0d7a0a602
michel-laterman <michel.laterman@elastic.co>
2021-05-25 15:50:11 -0700

    add import and issue text

--- metricbeat/module/golang/test_golang.py
+++ metricbeat/module/golang/test_golang.py
@@ -1,5 +1,6 @@
 import metricbeat
 import os
+import platform
 import sys
 import unittest
 
@@ -10,6 +11,7 @@ GOLANG_FIELDS = metricbeat.COMMON_FIELDS + ["golang"]
 class Test(metricbeat.BaseTest):
 
     @unittest.skipIf(platform.platform().startswith("Windows-10"),
+                     "flakey test: https://github.com/elastic/beats/issues/25858")
     def test_stats(self):
         """
         golang heap test
@@ -20,15 +22,15 @@ class Test(metricbeat.BaseTest):
             "hosts": ["http://localhost:6060"],
             "period": "1s"
         }])
-        proc=self.start_beat(
+        proc = self.start_beat(
             extra_args=["-httpprof", "localhost:6060"])
 
         self.wait_until(lambda: self.output_lines() > 0)
         proc.check_kill_and_wait()
         self.assert_no_logged_warnings()
 
-        output=self.read_output_json()
-        evt=output[0]
+        output = self.read_output_json()
+        evt = output[0]
 
         self.assertCountEqual(self.de_dot(GOLANG_FIELDS), evt.keys(), evt)
         assert evt["golang"]["heap"]["allocations"]["total"] > 0
