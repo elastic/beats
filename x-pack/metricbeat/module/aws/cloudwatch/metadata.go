bdba079053bdfaa65be5ec890c6c6f112ff4befc
kaiyan-sheng <kaiyan.sheng@elastic.co>
2021-06-01 21:48:50 +0800

    Migrate ec2 metricset to use cloudwatch input as light weight module (#25924)

---
+++ x-pack/metricbeat/module/aws/cloudwatch/metadata.go
@@ -0,0 +1,27 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package cloudwatch
+
+import (
+	awssdk "github.com/aws/aws-sdk-go-v2/aws"
+
+	"github.com/elastic/beats/v7/metricbeat/mb"
+	"github.com/elastic/beats/v7/x-pack/metricbeat/module/aws/cloudwatch/ec2"
+)
+
+// AWS namespaces
+const (
+	namespaceEC2 = "AWS/EC2"
+)
+
+// addMetadata adds metadata to the given events map based on namespace
+func addMetadata(namespace string, endpoint string, regionName string, awsConfig awssdk.Config, events map[string]mb.Event) map[string]mb.Event {
+	switch namespace {
+	case namespaceEC2:
+		return ec2.AddMetadata(endpoint, regionName, awsConfig, events)
+	default:
+		return events
+	}
+}
