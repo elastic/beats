b7cd0b279254004952e0c6cff4008b91935cb724
Marius Iversen <marius.iversen@elastic.co>
2021-06-17 10:36:13 +0200

    stashing changes

--- x-pack/filebeat/input/http_endpoint/handler.go
+++ x-pack/filebeat/input/http_endpoint/handler.go
@@ -5,8 +5,8 @@
 package http_endpoint
 
 import (
+	"bytes"
 	"encoding/json"
-	"fmt"
 	"io"
 	"net/http"
 	"time"
@@ -38,7 +38,7 @@ var (
 // Triggers if middleware validation returns successful
 func (h *httpHandler) apiResponse(w http.ResponseWriter, r *http.Request) {
 	var headers map[string]interface{}
-	objs, status, err := httpReadJSON(r.Body)
+	objs, _, status, err := httpReadJSON(r.Body)
 	if err != nil {
 		sendErrorResponse(w, status, err)
 		return
@@ -93,36 +93,77 @@ func sendErrorResponse(w http.ResponseWriter, status int, err error) {
 	e.Encode(common.MapStr{"message": err.Error()})
 }
 
-func httpReadJSON(body io.Reader) (objs []common.MapStr, status int, err error) {
+func httpReadJSON(body io.Reader) (objs []common.MapStr, rawMessages []json.RawMessage, status int, err error) {
 	if body == http.NoBody {
-		return nil, http.StatusNotAcceptable, errBodyEmpty
+		return nil, nil, http.StatusNotAcceptable, errBodyEmpty
 	}
+	obj, rawMessage, err := decodeJSON(body)
+	if err != nil {
+		return nil, nil, http.StatusBadRequest, err
+	}
+	return obj, rawMessage, 0, err
 
+}
+
+func decodeJSON(body io.Reader) (objs []common.MapStr, rawMessages []json.RawMessage, err error) {
 	decoder := json.NewDecoder(body)
-	for idx := 0; ; idx++ {
-		var obj interface{}
-		if err := decoder.Decode(&obj); err != nil {
+	for decoder.More() {
+		var raw json.RawMessage
+		if err := decoder.Decode(&raw); err != nil {
 			if err == io.EOF {
 				break
 			}
-			return nil, http.StatusBadRequest, errors.Wrapf(err, "malformed JSON object at stream position %d", idx)
+			return nil, nil, errors.Wrapf(err, "malformed JSON object at stream position %d", decoder.InputOffset())
+		}
+
+		var obj interface{}
+		if err := newJSONDecoder(bytes.NewReader(raw)).Decode(&obj); err != nil {
+			return nil, nil, errors.Wrapf(err, "malformed JSON object at stream position %d", decoder.InputOffset())
 		}
 		switch v := obj.(type) {
 		case map[string]interface{}:
 			objs = append(objs, v)
+			rawMessages = append(rawMessages, raw)
 		case []interface{}:
-			for listIdx, listObj := range v {
-				asMap, ok := listObj.(map[string]interface{})
-				if !ok {
-					return nil, http.StatusBadRequest, fmt.Errorf("%v at stream %d index %d", errUnsupportedType, idx, listIdx)
-				}
-				objs = append(objs, asMap)
+			nobjs, nrawMessages, err := decodeJSONArray(bytes.NewReader(raw))
+			if err != nil {
+				return nil, nil, errors.Wrapf(err, "recursive error %d", decoder.InputOffset())
 			}
+			objs = append(objs, nobjs...)
+			rawMessages = append(rawMessages, nrawMessages...)
 		default:
-			return nil, http.StatusBadRequest, errUnsupportedType
+			return nil, nil, errUnsupportedType
 		}
 	}
-	return objs, 0, nil
+	return objs, rawMessages, nil
+}
+
+func decodeJSONArray(body *bytes.Reader) (objs []common.MapStr, rawMessages []json.RawMessage, err error) {
+	dec := newJSONDecoder(body)
+	token, err := dec.Token()
+	if token != json.Delim('[') || err != nil {
+		panic("wrong!")
+	}
+	for dec.More() {
+		var raw json.RawMessage
+
+		if err := dec.Decode(&raw); err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, nil, errors.Wrapf(err, "malformed JSON object at stream position %d", dec.InputOffset())
+		}
+		var obj interface{}
+		if err := newJSONDecoder(bytes.NewReader(raw)).Decode(&obj); err != nil {
+			return nil, nil, errors.Wrapf(err, "malformed JSON object at stream position %d", dec.InputOffset())
+		}
+		rawMessages = append(rawMessages, raw)
+		m, ok := obj.(map[string]interface{})
+		if ok {
+			objs = append(objs, m)
+		}
+	}
+	return
 }
 
 func getIncludedHeaders(r *http.Request, headerConf []string) (includedHeaders common.MapStr) {
@@ -134,4 +175,10 @@ func getIncludedHeaders(r *http.Request, headerConf []string) (includedHeaders c
 		}
 	}
 	return includedHeaders
+}
+
+func newJSONDecoder(r io.Reader) *json.Decoder {
+	dec := json.NewDecoder(r)
+	dec.UseNumber()
+	return dec
 }
--- x-pack/filebeat/input/http_endpoint/handler_test.go
+++ x-pack/filebeat/input/http_endpoint/handler_test.go
@@ -5,26 +5,28 @@
 package http_endpoint
 
 import (
+	"encoding/json"
 	"net/http"
-	"reflect"
 	"strings"
 	"testing"
 
 	"github.com/elastic/beats/v7/libbeat/common"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_httpReadJSON(t *testing.T) {
 	tests := []struct {
-		name       string
-		body       string
-		wantObjs   []common.MapStr
-		wantStatus int
-		wantErr    bool
+		name           string
+		body           string
+		wantObjs       []common.MapStr
+		wantStatus     int
+		wantErr        bool
+		wantRawMessage []json.RawMessage
 	}{
 		{
 			name:     "single object",
-			body:     `{"a": 42, "b": "c"}`,
-			wantObjs: []common.MapStr{{"a": float64(42), "b": "c"}},
+			body:     `{"a": "42", "b": "c"}`,
+			wantObjs: []common.MapStr{{"a": "42", "b": "c"}},
 		},
 		{
 			name:     "array accepted",
@@ -38,56 +40,75 @@ func Test_httpReadJSON(t *testing.T) {
 			wantErr:    true,
 		},
 		{
-			name:       "not an object mixed",
-			body:       "[{\"a\":1},\n42,\n{\"a\":2}]",
+			name: "not an object mixed",
+			body: `[{a:1},
+								42,
+							{a:2}]`,
 			wantStatus: http.StatusBadRequest,
 			wantErr:    true,
 		},
 		{
 			name:     "sequence of objects accepted (CRLF)",
-			body:     "{\"a\":1}\r\n{\"a\":2}",
-			wantObjs: []common.MapStr{{"a": float64(1)}, {"a": float64(2)}},
+			body:     `{"a":"1"}` + "\r" + `{"a":"2"}`,
+			wantObjs: []common.MapStr{{"a": "1"}, {"a": "2"}},
 		},
 		{
-			name:     "sequence of objects accepted (LF)",
-			body:     "{\"a\":1}\n{\"a\":2}",
-			wantObjs: []common.MapStr{{"a": float64(1)}, {"a": float64(2)}},
+			name: "sequence of objects accepted (LF)",
+			body: `{"a":"1"}
+									{"a":"2"}`,
+			wantRawMessage: []json.RawMessage{
+				[]byte(`{"a":"1"}`),
+				[]byte(`{"a":"2"}`),
+			},
+			wantObjs: []common.MapStr{{"a": "1"}, {"a": "2"}},
 		},
 		{
 			name:     "sequence of objects accepted (SP)",
-			body:     "{\"a\":1} {\"a\":2}",
-			wantObjs: []common.MapStr{{"a": float64(1)}, {"a": float64(2)}},
+			body:     `{"a":"2"} {"a":"2"}`,
+			wantObjs: []common.MapStr{{"a": "2"}, {"a": "2"}},
 		},
 		{
 			name:     "sequence of objects accepted (no separator)",
-			body:     "{\"a\":1}{\"a\":2}",
-			wantObjs: []common.MapStr{{"a": float64(1)}, {"a": float64(2)}},
+			body:     `{"a":"2"}{"a":"2"}`,
+			wantObjs: []common.MapStr{{"a": "2"}, {"a": "2"}},
 		},
 		{
-			name:       "not an object in sequence",
-			body:       "{\"a\":1}\n42\n{\"a\":2}",
+			name: "not an object in sequence",
+			body: `{"a":"2"}
+									42
+						 {"a":"2"}`,
 			wantStatus: http.StatusBadRequest,
 			wantErr:    true,
 		},
 		{
-			name:     "array of objects in stream",
-			body:     `{"a":1} [{"a":2},{"a":3}] {"a":4}`,
-			wantObjs: []common.MapStr{{"a": float64(1)}, {"a": float64(2)}, {"a": float64(3)}, {"a": float64(4)}},
+			name: "array of objects in stream",
+			body: `{"a":"1"} [{"a":"2"},{"a":"3"}] {"a":"4"}`,
+			wantRawMessage: []json.RawMessage{
+				[]byte(`{"a":"1"}`),
+				[]byte(`{"a":"2"}`),
+				[]byte(`{"a":"3"}`),
+				[]byte(`{"a":"4"}`),
+			},
+			wantObjs: []common.MapStr{{"a": "1"}, {"a": "2"}, {"a": "3"}, {"a": "4"}},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotObjs, gotStatus, err := httpReadJSON(strings.NewReader(tt.body))
+			gotObjs, rawMessages, gotStatus, err := httpReadJSON(strings.NewReader(tt.body))
 			if (err != nil) != tt.wantErr {
 				t.Errorf("httpReadJSON() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if !reflect.DeepEqual(gotObjs, tt.wantObjs) {
+			if !assert.EqualValues(t, tt.wantObjs, gotObjs) {
 				t.Errorf("httpReadJSON() gotObjs = %v, want %v", gotObjs, tt.wantObjs)
 			}
 			if gotStatus != tt.wantStatus {
 				t.Errorf("httpReadJSON() gotStatus = %v, want %v", gotStatus, tt.wantStatus)
 			}
+			if tt.wantRawMessage != nil {
+				assert.Equal(t, tt.wantRawMessage, rawMessages)
+			}
+			assert.Equal(t, len(gotObjs), len(rawMessages))
 		})
 	}
 }
