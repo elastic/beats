87aaeaa9703d14be438bbf686266ad85214270c6
Alex Kristiansen <alex.kristiansen@elastic.co>
2021-06-21 08:59:29 -0700

    fix up linux/memory

--- metricbeat/module/linux/memory/doc.go
+++ metricbeat/module/linux/memory/doc.go
@@ -15,41 +15,4 @@
 // specific language governing permissions and limitations
 // under the License.
 
-// +build linux
-
 package memory
-
-import (
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-
-	mbtest "github.com/elastic/beats/v7/metricbeat/mb/testing"
-)
-
-func TestFetch(t *testing.T) {
-	f := mbtest.NewReportingMetricSetV2Error(t, getConfig())
-	events, errs := mbtest.ReportingFetchV2Error(f)
-
-	assert.Empty(t, errs)
-	if !assert.NotEmpty(t, events) {
-		t.FailNow()
-	}
-	t.Logf("%s/%s event: %+v", f.Module().Name(), f.Name(),
-		events[0].BeatEvent("linux", "memory").Fields.StringToPrint())
-}
-
-func TestData(t *testing.T) {
-	f := mbtest.NewReportingMetricSetV2Error(t, getConfig())
-	err := mbtest.WriteEventsReporterV2Error(f, t, ".")
-	if err != nil {
-		t.Fatal("write", err)
-	}
-}
-
-func getConfig() map[string]interface{} {
-	return map[string]interface{}{
-		"module":     "linux",
-		"metricsets": []string{"memory"},
-	}
-}
