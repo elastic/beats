b87e2a15a39d6d12825066e5578f8dda9f1bf5d3
Pinarzaimoglu <zaimoglupinar@gmail.com>
2021-06-27 03:55:47 +0300

    add integration test

---
+++ metricbeat/module/pulsar/broker/broker_integration_test.go
@@ -0,0 +1,36 @@
+package broker
+
+import (
+        "testing"
+
+        "github.com/stretchr/testify/assert"
+        "github.com/elastic/beats/v7/libbeat/tests/compose"
+
+        mbtest "github.com/elastic/beats/v7/metricbeat/mb/testing"
+)
+
+func TestFetch(t *testing.T) {
+        compose.EnsureUp(t, "pulsar")
+
+        metricSet := mbtest.NewFetcher(t, getConfig())
+        events, errs := metricSet.FetchEvents()
+        if len(errs) > 0 {
+                t.Fatalf("Expected 0 error, had %d. %v\n", len(errs), errs)
+        }
+        assert.NotEmpty(t, events)
+}
+
+func TestData(t *testing.T) {
+        compose.EnsureUp(t, "pulsar")
+        metricSet := mbtest.NewFetcher(t, getConfig())
+        metricSet.WriteEvents(t, "")
+}
+
+// getConfig returns config for pulsar module
+func getConfig() map[string]interface{} {
+    return map[string]interface{}{
+    "module":     "pulsar",
+    "metricsets": []string{"broker"},
+    "hosts":      []string{"http://localhost:8080"},
+  }
+}
