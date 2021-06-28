3023c56e92f85156717cc0ee237e3dd0c94f7f02
Andrew Kroh <andrew.kroh@elastic.co>
2021-06-28 16:39:51 -0400

    Add Fleet agent.id to Agent monitoring data
    This is a follow up to #26394 which set the agent.id field to the Fleet agent ID for
    for integrations that are run by Agent, but that didn't cover the Filebeat and Metricbeat
    processes that are executed by Agent for monitoring itself. This covers those processes.
    
    Relates #26394

--- x-pack/elastic-agent/pkg/agent/operation/monitoring.go
+++ x-pack/elastic-agent/pkg/agent/operation/monitoring.go
@@ -338,6 +338,16 @@ func (o *Operator) getMonitoringFilebeatConfig(outputType string, output interfa
 		"output": map[string]interface{}{
 			outputType: output,
 		},
+		"processors": []map[string]interface{}{
+			{
+				"add_fields": map[string]interface{}{
+					"target": "agent",
+					"fields": map[string]interface{}{
+						"id": o.agentInfo.AgentID(),
+					},
+				},
+			},
+		},
 	}
 
 	o.logger.Debugf("monitoring configuration generated for filebeat: %v", result)
@@ -561,6 +571,16 @@ func (o *Operator) getMonitoringMetricbeatConfig(outputType string, output inter
 		},
 		"output": map[string]interface{}{
 			outputType: output,
+		},
+		"processors": []map[string]interface{}{
+			{
+				"add_fields": map[string]interface{}{
+					"target": "agent",
+					"fields": map[string]interface{}{
+						"id": o.agentInfo.AgentID(),
+					},
+				},
+			},
 		},
 	}
 
