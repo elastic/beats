199382197881f882cdcf3e48329c1b5410eca582
Victor Martinez <victormartinezrubio@gmail.com>
2021-07-12 15:57:23 +0100

    CI: use withMageEnv (#26723)
    (cherry picked from commit 6cf5639dcdad0c08f2e2d9177fdcb96ef2af983e)
    
    # Conflicts:
    #	.ci/scripts/install-tools.bat

--- .ci/scripts/install-go.bat
+++ .ci/scripts/install-go.bat
@@ -1,15 +1,14 @@
-REM Configure GCC for either 32 or 64 bits
-set MINGW_ARCH=64
-IF NOT EXIST "%PROGRAMFILES(X86)%" (
-    set MINGW_ARCH=32
-)
-set PATH=%WORKSPACE%\bin;C:\ProgramData\chocolatey\bin;C:\tools\mingw%MINGW_ARCH%\bin;%PATH%
+set GOPATH=%WORKSPACE%
+set MAGEFILE_CACHE=%WORKSPACE%\.magefile
 
-where /q curl
-IF ERRORLEVEL 1 (
- choco install curl -y --no-progress --skipdownloadcache
+set PATH=%WORKSPACE%\bin;C:\ProgramData\chocolatey\bin;%PATH%
+
+curl --version >nul 2>&1 && (
+    echo found curl
+) || (
+    choco install curl -y --no-progress --skipdownloadcache
 )
-<<<<<<< HEAD
+
 mkdir %WORKSPACE%\bin
 
 IF EXIST "%PROGRAMFILES(X86)%" (
@@ -48,38 +47,11 @@ IF ERRORLEVEL 1 (
     exit /b 1
 )
 
-go get github.com/magefile/mage
 where mage
 mage -version
 IF ERRORLEVEL 1 (
-    exit /b 1
-)
-=======
->>>>>>> 6cf5639dc (CI: use withMageEnv (#26723))
-
-REM Set the USERPROFILE to the previous location to fix issues with chocolatey in windows 2019
-SET PREVIOUS_USERPROFILE=%USERPROFILE%
-SET USERPROFILE=%OLD_USERPROFILE%
-IF NOT EXIST C:\Python38\python.exe (
-    REM Install python 3.8
-    choco install python -y -r --no-progress --version 3.8.5
-    IF NOT ERRORLEVEL 0 (
+    go get github.com/magefile/mage
+    IF ERRORLEVEL 1 (
         exit /b 1
     )
 )
-python --version
-where python
-
-where /q gcc
-IF ERRORLEVEL 1 (
-    REM Install mingw 5.3.0
-    choco install mingw -y -r --no-progress --version 5.3.0
-    IF NOT ERRORLEVEL 0 (
-        exit /b 1
-    )
-)
-gcc --version
-where gcc
-
-REM Reset the USERPROFILE
-SET USERPROFILE=%PREVIOUS_USERPROFILE%
