835c7713587f1b6123ac6d4fbfeeae4899f9fe19
Edoardo Tenani <edoardo.tenani@elastic.co>
2021-07-15 18:21:37 +0200

    gcp: add metric prefix as explicit configuration parameter
    All metrics ingested up to this moment with the `gcp` module have been
    related to Google Cloud metrics.
    List of metrics: https://cloud.google.com/monitoring/api/metrics_gcp
    
    Other metrics, like GKE require a different prefix, for which the
    current logic was not correct (the prefix is not in the form
    `<servicename>.googleapis.com`).
    
    This commit updates the logic to respect the `metric_prefix` when
    configured and fall back to the previous logic when not.

--- x-pack/metricbeat/module/gcp/metrics/metricset.go
+++ x-pack/metricbeat/module/gcp/metrics/metricset.go
@@ -53,9 +53,21 @@ type MetricSet struct {
 type metricsConfig struct {
 	ServiceName string `config:"service"  validate:"required"`
 	// MetricPrefix allows to specify the prefix string for MetricTypes
-	MetricPrefix string   `config:"metric_prefix"`
-	MetricTypes  []string `config:"metric_types" validate:"required"`
-	Aligner      string   `config:"aligner"`
+	// Stackdriver requires metrics to be prefixed with a common prefix.
+	// This prefix changes based on the services the metrics belongs to.
+	ServiceMetricPrefix string   `config:"metric_prefix"`
+	MetricTypes         []string `config:"metric_types" validate:"required"`
+	Aligner             string   `config:"aligner"`
+}
+
+func (mc metricsConfig) MetricPrefix() string {
+	// NOTE: fallback to Google Cloud prefix for backward compatibility
+	// Prefix <service>.googleapis.com/ works only for Google Cloud metrics
+	// List: https://cloud.google.com/monitoring/api/metrics_gcp
+	if mc.ServiceMetricPrefix == "" {
+		return mc.ServiceName + ".googleapis.com/"
+	}
+	return mc.ServiceMetricPrefix
 }
 
 type metricMeta struct {
@@ -230,20 +242,11 @@ func (m *MetricSet) metricDescriptor(ctx context.Context, client *monitoring.Met
 
 	m.Logger().Debugf("metrics config %+v", m.MetricsConfig)
 	for _, sdc := range m.MetricsConfig {
-		// Stackdriver requires metrics to be prefixed with a common prefix.
-		// This prefix changes based on the services the metrics belongs to.
-		metricPrefix := sdc.MetricPrefix
-		// NOTE: fallback to Google Cloud prefix for backward compatibility
-		// Prefix <service>.googleapis.com/ works only for Google Cloud metrics
-		// List: https://cloud.google.com/monitoring/api/metrics_gcp
-		if metricPrefix == "" {
-			metricPrefix = sdc.ServiceName + ".googleapis.com/"
-		}
 
 		for _, mt := range sdc.MetricTypes {
 			m.Logger().Debugf("list metric descriptors: %s", mt)
 
-			metricFilter := fmt.Sprintf("metric.type = starts_with(\"%s%s\")", metricPrefix, mt)
+			metricFilter := fmt.Sprintf("metric.type = starts_with(\"%s%s\")", sdc.MetricPrefix(), mt)
 			req.Filter = metricFilter
 			m.Logger().Debugf("ListMetricDescriptors req: %+v", req)
 
