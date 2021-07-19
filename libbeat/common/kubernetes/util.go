4e612f69fb3315eb7e41943239566c6a4a640d4a
MichaelKatsoulis <michaelkatsoulis88@gmail.com>
2021-07-19 13:07:32 +0300

    Use errors.Wrap instead of new function

--- libbeat/common/kubernetes/util.go
+++ libbeat/common/kubernetes/util.go
@@ -19,7 +19,6 @@ package kubernetes
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -30,6 +29,8 @@ import (
 	restclient "k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+
+	"github.com/pkg/errors"
 
 	"github.com/elastic/beats/v7/libbeat/logp"
 )
@@ -93,7 +94,7 @@ func IsInCluster(kubeconfig string) bool {
 // If host is provided in the config use it directly.
 // If beat is deployed in k8s cluster, use hostname of pod which is pod name to query pod meta for node name.
 // If beat is deployed outside k8s cluster, use machine-id to match against k8s nodes for node name.
-// If node can not be discovered return NODE_NAME env var as default value. In case it is not set return error.
+// If node cannot be discovered, return NODE_NAME env var as default value. In case it is not set return error.
 func DiscoverKubernetesNode(log *logp.Logger, host string, inCluster bool, client kubernetes.Interface) (string, error) {
 	if host != "" {
 		log.Infof("kubernetes: Using node %s provided in the config", host)
@@ -110,20 +111,20 @@ func DiscoverKubernetesNode(log *logp.Logger, host string, inCluster bool, clien
 		if err != nil {
 			logerror = fmt.Errorf("kubernetes: Couldn't get namespace when beat is in cluster with error: %+v", err.Error())
 			log.Error(logerror)
-			return nodeNameEnv, enrichedError([]error{envError, logerror})
+			return nodeNameEnv, errors.Wrap(envError, logerror.Error())
 		}
 		podName, err := os.Hostname()
 		if err != nil {
 			logerror = fmt.Errorf("kubernetes: Couldn't get hostname as beat pod name in cluster with error: %+v", err.Error())
 			log.Error(logerror)
-			return nodeNameEnv, enrichedError([]error{envError, logerror})
+			return nodeNameEnv, errors.Wrap(envError, logerror.Error())
 		}
 		log.Infof("kubernetes: Using pod name %s and namespace %s to discover kubernetes node", podName, ns)
 		pod, err := client.CoreV1().Pods(ns).Get(ctx, podName, metav1.GetOptions{})
 		if err != nil {
 			logerror = fmt.Errorf("kubernetes: Querying for pod failed with error: %+v", err)
 			log.Error(logerror)
-			return nodeNameEnv, enrichedError([]error{envError, logerror})
+			return nodeNameEnv, errors.Wrap(envError, logerror.Error())
 		}
 		log.Infof("kubernetes: Using node %s discovered by in cluster pod node query", pod.Spec.NodeName)
 		return pod.Spec.NodeName, nil
@@ -133,14 +134,14 @@ func DiscoverKubernetesNode(log *logp.Logger, host string, inCluster bool, clien
 	if mid == "" {
 		logerror = errors.New("kubernetes: Couldn't collect info from any of the files in /etc/machine-id /var/lib/dbus/machine-id")
 		log.Error(logerror)
-		return nodeNameEnv, enrichedError([]error{envError, logerror})
+		return nodeNameEnv, errors.Wrap(envError, logerror.Error())
 	}
 
 	nodes, err := client.CoreV1().Nodes().List(ctx, metav1.ListOptions{})
 	if err != nil {
 		logerror = fmt.Errorf("kubernetes: Querying for nodes failed with error: %+v", err)
 		log.Error(logerror)
-		return nodeNameEnv, enrichedError([]error{envError, logerror})
+		return nodeNameEnv, errors.Wrap(envError, logerror.Error())
 	}
 	for _, n := range nodes.Items {
 		if n.Status.NodeInfo.MachineID == mid {
@@ -177,24 +178,4 @@ func InClusterNamespace() (string, error) {
 		return "", err
 	}
 	return strings.TrimSpace(string(data)), nil
-}
-
-// enrichedError gets a slice of errors as input and combines them together with : as delimiter.
-// In case one of the errors received is nil then nil is returned.
-// Example input:[first error, second error]
-// Example output: first error: second error
-func enrichedError(flatErrors []error) (enrichedError error) {
-
-	for _, err := range flatErrors {
-		if err == nil {
-			return nil
-		}
-		if enrichedError == nil {
-			enrichedError = fmt.Errorf("%+v", err)
-		} else {
-			enrichedError = fmt.Errorf("%+v: %+v", enrichedError, err)
-		}
-
-	}
-	return enrichedError
 }
