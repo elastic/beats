753a801791032dbc3884662b9d82488bc97a222a
kaiyan-sheng <kaiyan.sheng@elastic.co>
2021-07-22 08:25:03 -0600

    Fix aws dashboards and reenable test (#27003)
    (cherry picked from commit f361c9da297ea6ae226f8a0ce4d545785664c5c3)

--- x-pack/metricbeat/tests/system/test_xpack_base.py
+++ x-pack/metricbeat/tests/system/test_xpack_base.py
@@ -1,11 +1,7 @@
-import os
-import unittest
-
 import xpack_metricbeat
 import test_base
 from beat import common_tests
 
 
-@unittest.skip("https://github.com/elastic/beats/issues/26536")
 class Test(xpack_metricbeat.XPackTest, test_base.Test, common_tests.TestExportsMixin):
     pass
