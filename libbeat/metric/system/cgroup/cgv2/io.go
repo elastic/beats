5c2c37616b0e288126de92f34af56b9125ffdf92
Alex Kristiansen <alex.kristiansen@elastic.co>
2021-08-05 12:25:44 -0700

    fix crossbuild issues

--- libbeat/metric/system/cgroup/cgv2/io.go
+++ libbeat/metric/system/cgroup/cgv2/io.go
@@ -20,10 +20,8 @@ package cgv2
 import (
 	"bufio"
 	"fmt"
-	"io/fs"
 	"os"
 	"path/filepath"
-	"syscall"
 
 	"github.com/pkg/errors"
 
@@ -108,46 +106,4 @@ func getIOStats(path string, resolveDevIDs bool) (map[string]IOStat, error) {
 	}
 
 	return stats, nil
-}
-
-// fetchDeviceName will attempt to find a device name associated with a major/minor pair
-// the bool indicates if a device was found.
-func fetchDeviceName(major, minor uint64) (bool, string, error) {
-	// iterate over /dev/ and pull major and minor values
-	found := false
-	var devName string
-	walkFunc := func(path string, d fs.DirEntry, err error) error {
-		if d.IsDir() && path != "/dev/" {
-			return fs.SkipDir
-		}
-		if d.Type() != fs.ModeDevice {
-			return nil
-		}
-		fInfo, err := d.Info()
-		if err != nil {
-			return nil
-		}
-		infoT, ok := fInfo.Sys().(*syscall.Stat_t)
-		if !ok {
-			return nil
-		}
-		devID := infoT.Rdev
-		// do some bitmapping to extract the major and minor device values
-		// The odd duplicated logic here is to deal with 32 and 64 bit values.
-		// see bits/sysmacros.h
-		curMajor := ((devID & 0xfffff00000000000) >> 32) | ((devID & 0x00000000000fff00) >> 8)
-		curMinor := ((devID & 0x00000000000000ff) >> 0) | ((devID & 0x00000ffffff00000) >> 12)
-		if curMajor == major && curMinor == minor {
-			found = true
-			devName = d.Name()
-		}
-		return nil
-	}
-
-	err := filepath.WalkDir("/dev/", walkFunc)
-	if err != nil {
-		return false, "", errors.Wrap(err, "error walking /dev/")
-	}
-
-	return found, devName, nil
 }
