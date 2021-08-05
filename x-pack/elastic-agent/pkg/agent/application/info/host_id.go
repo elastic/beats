e6af0d0d934cbc43e50aa7d4123e959c21974fc6
michel-laterman <michel.laterman@elastic.co>
2021-08-05 09:26:56 -0700

    Update build tags

--- x-pack/elastic-agent/pkg/agent/application/info/host_id.go
+++ x-pack/elastic-agent/pkg/agent/application/info/host_id.go
@@ -2,10 +2,18 @@
 // or more contributor license agreements. Licensed under the Elastic License;
 // you may not use this file except in compliance with the Elastic License.
 
+// +build !linux
+// +build !windows
+// +build !darwin
+// +build !freebsd
+// +build !netbsd
+// +build !openbsd
+
 package info
 
 import "errors"
 
+// loadHostID will return an error on systems that do not have a specific implementation.
 func loadHostID() (string, error) {
 	return "", errors.New("loadHostID unimplemented")
 }
