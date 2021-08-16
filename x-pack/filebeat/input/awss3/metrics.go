5e8594e8bcd9f07e54cfc4ae8ab8662c1ecd3bb4
Andrea Spacca <andrea.spacca@elastic.co>
2021-08-16 18:40:16 +0200

    cr fixes

--- x-pack/filebeat/input/awss3/metrics.go
+++ x-pack/filebeat/input/awss3/metrics.go
@@ -25,8 +25,8 @@ type inputMetrics struct {
 	sqsMessageProcessingTime            metrics.Sample   // Histogram of the elapsed SQS processing times in nanoseconds (time of receipt to time of delete/return).
 
 	s3ObjectsRequestedTotal *monitoring.Uint // Number of S3 objects downloaded.
-	// s3ObjectsListedTotal is the number of S3 objects processed that were fully ACKed.
-	s3ObjectsAckedTotal *monitoring.Uint // Number of S3 objects fully ACKed.
+	// s3ObjectsAckedTotal is the number of S3 objects processed that were fully ACKed.
+	s3ObjectsAckedTotal *monitoring.Uint
 	// s3ObjectsListedTotal is the number of S3 objects returned by list operations.
 	s3ObjectsListedTotal *monitoring.Uint
 	// s3ObjectsProcessedTotal is the number of S3 objects that matched file_selectors rules.
