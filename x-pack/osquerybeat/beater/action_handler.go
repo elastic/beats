635fec795021c62528af732b077327880c62b9f0
Aleksandr Maus <aleksandr.maus@elastic.co>
2021-08-19 08:57:09 -0400

    Osquerybeat: Add ECS Mapping support for ad-hoc/live queries (#27265)
    * Osquerybeat: Add ECS Mapping support for ad-hoc/live queries
    
    * Fix unit test file header

--- x-pack/osquerybeat/beater/action_handler.go
+++ x-pack/osquerybeat/beater/action_handler.go
@@ -7,10 +7,13 @@ package beater
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
+	"github.com/elastic/beats/v7/libbeat/common"
 	"github.com/elastic/beats/v7/libbeat/logp"
 	"github.com/elastic/beats/v7/x-pack/osquerybeat/internal/config"
+	"github.com/elastic/beats/v7/x-pack/osquerybeat/internal/ecs"
 	"github.com/elastic/beats/v7/x-pack/osquerybeat/internal/osqdcli"
 )
 
@@ -26,8 +29,9 @@ func (a *actionHandler) Name() string {
 }
 
 type actionData struct {
-	Query string
-	ID    string
+	Query      string
+	ID         string
+	ECSMapping ecs.Mapping
 }
 
 func actionDataFromRequest(req map[string]interface{}) (ad actionData, err error) {
@@ -46,9 +50,63 @@ func actionDataFromRequest(req map[string]interface{}) (ad actionData, err error
 					ad.Query = query
 				}
 			}
+			if v, ok := m["ecs_mapping"]; ok {
+				m, ok := v.(map[string]interface{})
+				if !ok {
+					return ad, ErrActionRequest
+				}
+				ecsm, err := convertActionDataECSMapping(m)
+				if err != nil {
+					return ad, err
+				}
+				ad.ECSMapping = ecsm
+			}
 		}
 	}
 	return ad, nil
+}
+
+func convertActionDataECSMapping(m map[string]interface{}) (ecsm ecs.Mapping, err error) {
+	ecsm = make(ecs.Mapping)
+	for k, v := range m {
+		k = strings.TrimSpace(k)
+		if k == "" {
+			return ecsm, ErrActionRequest
+		}
+		valmap, ok := v.(map[string]interface{})
+		if !ok {
+			return ecsm, ErrActionRequest
+		}
+
+		var (
+			val   interface{}
+			field string
+		)
+
+		if val, ok = valmap["field"]; ok {
+			// "field" can only be string
+			field, ok = val.(string)
+			if !ok {
+				return ecsm, ErrActionRequest
+			}
+			field = strings.TrimSpace(field)
+		}
+
+		value := valmap["value"]
+		if field != "" && value != nil {
+			return ecsm, ErrActionRequest
+		}
+
+		// Should have at field or value defined in the mapping object
+		if field == "" && value == nil {
+			return ecsm, ErrActionRequest
+		}
+		ecsm[k] = ecs.MappingInfo{
+			Field: field,
+			Value: value,
+		}
+	}
+	return
 }
 
 // Execute handles the action request.
@@ -74,16 +132,16 @@ func (a *actionHandler) execute(ctx context.Context, req map[string]interface{})
 	if err != nil {
 		return fmt.Errorf("%v: %w", err, ErrQueryExecution)
 	}
-	return a.executeQuery(ctx, config.DefaultStreamIndex, ad.ID, ad.Query, "", req)
+	return a.executeQuery(ctx, config.DefaultStreamIndex, ad, "", req)
 }
 
-func (a *actionHandler) executeQuery(ctx context.Context, index, id, query, responseID string, req map[string]interface{}) error {
+func (a *actionHandler) executeQuery(ctx context.Context, index string, ad actionData, responseID string, req map[string]interface{}) error {
 
-	a.log.Debugf("Execute query: %s", query)
+	a.log.Debugf("Execute query: %s", ad.Query)
 
 	start := time.Now()
 
-	hits, err := a.cli.Query(ctx, query)
+	hits, err := a.cli.Query(ctx, ad.Query)
 
 	if err != nil {
 		a.log.Errorf("Failed to execute query, err: %v", err)
@@ -92,9 +150,17 @@ func (a *actionHandler) executeQuery(ctx context.Context, index, id, query, resp
 
 	a.log.Debugf("Completed query in: %v", time.Since(start))
 
+	var ecsFields []common.MapStr
+	// If non-empty result and the ECSMapping is present
+	if len(hits) > 0 && len(ad.ECSMapping) > 0 {
+		ecsFields = make([]common.MapStr, len(hits))
+		for i, hit := range hits {
+			ecsFields[i] = common.MapStr(ad.ECSMapping.Map(ecs.Doc(hit)))
+		}
+	}
 	if err != nil {
 		return err
 	}
-	a.bt.publishEvents(index, id, responseID, hits, nil, req["data"])
+	a.bt.publishEvents(index, ad.ID, responseID, hits, ecsFields, req["data"])
 	return nil
 }
