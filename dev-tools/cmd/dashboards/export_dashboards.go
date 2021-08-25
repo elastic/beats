1b00b8010d2c78534e5c965c4d3289ef6de6d531
Noémi Ványi <sitbackandwait@gmail.com>
2021-08-25 14:57:42 +0200

    add tests and follow up in module files

--- dev-tools/cmd/dashboards/export_dashboards.go
+++ dev-tools/cmd/dashboards/export_dashboards.go
@@ -47,7 +47,8 @@ func main() {
 	kibanaURL := flag.String("kibana", "http://localhost:5601", "Kibana URL")
 	spaceID := flag.String("space-id", "", "Space ID")
 	dashboard := flag.String("dashboard", "", "Dashboard ID")
-	fileOutput := flag.String("output", "output.ndjson", "Output file")
+	fileOutput := flag.String("output", "output.ndjson", "Output NDJSON file")
+	folderOutput := flag.String("folder", "", "Output folder to save all assets to more human friendly JSON format")
 	ymlFile := flag.String("yml", "", "Path to the module.yml file containing the dashboards")
 	flag.BoolVar(&indexPattern, "indexPattern", false, "include index-pattern in output")
 	flag.BoolVar(&quiet, "quiet", false, "be quiet")
@@ -97,12 +98,16 @@ func main() {
 	}
 
 	if len(*dashboard) > 0 {
-		err = exportSingleDashboard(client, *dashboard, *fileOutput)
+		err = exportSingleDashboard(client, *dashboard, *folderOutput, *fileOutput)
 		if err != nil {
 			log.Fatalf("Failed to export the dashboard: %v", err)
 		}
 		if !quiet {
-			log.Printf("The dashboard %s was exported under '%s'\n", *dashboard, *fileOutput)
+			if *folderOutput != "" {
+				log.Printf("The dashboard %s was exported to '%s'\n", *dashboard, *folderOutput)
+			} else {
+				log.Printf("The dashboard %s was exported under '%s'\n", *dashboard, *fileOutput)
+			}
 		}
 		return
 	}
@@ -124,19 +129,33 @@ func exportDashboardsFromYML(client *kibana.Client, ymlFile string) error {
 	return nil
 }
 
-func exportSingleDashboard(client *kibana.Client, dashboard, output string) error {
+func exportSingleDashboard(client *kibana.Client, dashboard, folder, output string) error {
 	result, err := dashboards.Export(client, dashboard)
 	if err != nil {
 		return fmt.Errorf("failed to export the dashboard: %+v", err)
 	}
 
-	if err = os.MkdirAll(filepath.Dir(output), 0755); err != nil {
+	if folder != "" {
+		fmt.Println("FOLDER", folder)
+		return writeAssetsToFolder(client, result, folder)
+	} else if output != "" {
+		return writeDashboardToFile(result, output)
+
+	}
+	return nil
+}
+
+func writeDashboardToFile(dashboard []byte, output string) error {
+	if err := os.MkdirAll(filepath.Dir(output), 0755); err != nil {
 		return errors.Wrap(err, "failed to create directory for dashboard")
 	}
 
-	err = ioutil.WriteFile(output, result, dashboards.OutputPermission)
-	if err != nil {
+	if err := ioutil.WriteFile(output, dashboard, dashboards.OutputPermission); err != nil {
 		return fmt.Errorf("failed to save the dashboard: %+v", err)
 	}
 	return nil
+}
+
+func writeAssetsToFolder(client *kibana.Client, dashboard []byte, folder string) error {
+	return dashboards.SaveToFolder(dashboard, folder, client.GetVersion())
 }
