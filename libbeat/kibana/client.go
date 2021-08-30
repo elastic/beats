eed54b8f84b6d2b5d49258d862a0f3293b0265a5
Noémi Ványi <sitbackandwait@gmail.com>
2021-08-30 18:03:47 +0200

    handle messages when HTTP 2xx  responses

--- libbeat/kibana/client.go
+++ libbeat/kibana/client.go
@@ -92,6 +92,36 @@ func extractError(result []byte) error {
 	return nil
 }
 
+func extractMessage(result []byte) error {
+	var kibanaResult struct {
+		Success bool
+		Errors  []struct {
+			Id    string
+			Type  string
+			Error struct {
+				Type       string
+				References []struct {
+					Type string
+					Id   string
+				}
+			}
+		}
+	}
+	if err := json.Unmarshal(result, &kibanaResult); err != nil {
+		return nil
+	}
+
+	if !kibanaResult.Success {
+		var errs multierror.Errors
+		for _, err := range kibanaResult.Errors {
+			errs = append(errs, fmt.Errorf("error: %s, asset ID=%s; asset type=%s; references=%+v", err.Error.Type, err.Id, err.Type, err.Error.References))
+		}
+		return errs.Err()
+	}
+
+	return nil
+}
+
 // NewKibanaClient builds and returns a new Kibana client
 func NewKibanaClient(cfg *common.Config) (*Client, error) {
 	config := DefaultClientConfig()
@@ -187,8 +217,12 @@ func (conn *Connection) Request(method, extraPath string,
 		return 0, nil, fmt.Errorf("fail to read response %s", err)
 	}
 
+	fmt.Println(resp.StatusCode)
 	if resp.StatusCode >= 300 {
 		retError = extractError(result)
+	} else {
+		fmt.Println("msg")
+		retError = extractMessage(result)
 	}
 	return resp.StatusCode, result, retError
 }
