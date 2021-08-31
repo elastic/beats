fffab996deffabda920b33e2d76849abf559bb2d
Noémi Ványi <kvch@users.noreply.github.com>
2021-08-31 12:10:44 +0200

    Import the references of dashboard assets using the Saved Objects API (#27647)
    This PR changes the dashboard loading by first loading its references and then loading the dashboards.
    
    If the assets are not loaded in the proper order, the import can fail with unknown references.
    
    (cherry picked from commit b90370d14e5829184813b3c42f351b57aa282325)

--- dev-tools/mage/kibana.go
+++ dev-tools/mage/kibana.go
@@ -69,7 +69,7 @@ func KibanaDashboards(moduleDirs ...string) error {
 	// Convert 7.x dashboards to strings.
 	err = sh.Run(pythonExe,
 		filepath.Join(esBeatsDir, "libbeat/scripts/unpack_dashboards.py"),
-		"--glob="+filepath.Join(kibanaBuildDir, "7/*/*.json"))
+		"--glob="+filepath.Join(kibanaBuildDir, "7/dashboard/*.json"))
 	if err != nil {
 		return err
 	}
