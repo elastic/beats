50bfe3a54c9a438b47b85e671b094f72646d689f
Marius Iversen <marius.iversen@elastic.co>
2021-09-01 09:24:25 +0200

    adding base64encode func to value template

--- x-pack/filebeat/input/httpjson/internal/v2/value_tpl.go
+++ x-pack/filebeat/input/httpjson/internal/v2/value_tpl.go
@@ -9,6 +9,7 @@ import (
 	"crypto/hmac"
 	"crypto/sha1"
 	"crypto/sha256"
+	"encoding/base64"
 	"encoding/hex"
 	"errors"
 	"hash"
@@ -54,6 +55,7 @@ func (t *valueTpl) Unpack(in string) error {
 			"mul":                 mul,
 			"div":                 div,
 			"hmac":                hmacString,
+			"base64Encode":        base64Encode,
 		}).
 		Delims(leftDelim, rightDelim).
 		Parse(in)
@@ -241,6 +243,15 @@ func mul(a, b int64) int64 {
 
 func div(a, b int64) int64 {
 	return a / b
+}
+
+func base64Encode(values ...string) string {
+	data := strings.Join(values[:], "")
+	if data == "" {
+		return ""
+	}
+
+	return base64.StdEncoding.EncodeToString([]byte(data))
 }
 
 func hmacString(hmacType string, hmacKey string, values ...string) string {
