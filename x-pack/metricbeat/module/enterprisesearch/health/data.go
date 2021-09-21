63e0f90c1ee4bac994260b3ed434f48ae11b5e9c
Oleksiy Kovyrin <oleksiy@kovyrin.net>
2021-09-21 12:48:36 -0400

    Final schema for the stats metricset

--- x-pack/metricbeat/module/enterprisesearch/health/data.go
+++ x-pack/metricbeat/module/enterprisesearch/health/data.go
@@ -13,13 +13,20 @@ import (
 )
 
 var (
-	timerSchema = s.Schema{
-		"max.ms": c.Float("max"),
-		"avg.ms": c.Float("mean"),
-	}
 	schema = s.Schema{
 		"cluster_uuid": c.Str("cluster_uuid"), // This is going to be included in 7.16+
+		"name": c.Str("name"),
+		"version": c.Str("version"),
+
 		"jvm": c.Dict("jvm", s.Schema{
+			"version": c.Str("version"),
+
+			"gc": c.Dict("gc", s.Schema{
+				"collection_count": c.Int("collection_count"),
+				"collection_time":  c.Int("collection_time"),
+				// TODO: Add separate metrics for old and young generation collectors
+			}),
+
 			"memory_usage": c.Dict("memory_usage", s.Schema{
 				"heap_init.bytes":                   c.Int("heap_init"),
 				"heap_used.bytes":                   c.Int("heap_used"),
@@ -29,6 +36,7 @@ var (
 				"non_heap_committed.bytes":          c.Int("non_heap_committed"),
 				"object_pending_finalization_count": c.Int("object_pending_finalization_count"),
 			}),
+
 			"threads": c.Dict("threads", s.Schema{
 				"thread_count":               c.Int("thread_count"),
 				"peak_thread_count":          c.Int("peak_thread_count"),
@@ -36,6 +44,18 @@ var (
 				"daemon_thread_count":        c.Int("daemon_thread_count"),
 			}),
 		}),
+
+		"process": c.Dict("process", s.Schema{
+			"pid": c.Int("pid"),
+			"uptime": c.Int("uptime"),
+
+			"filebeat": c.Dict("filebeat", s.Schema{
+				"pid": c.Int("pid"),
+				"restart_count": c.Int("restart_count"),
+				"seconds_since_last_restart": c.Int("seconds_since_last_restart"),
+			}),
+		}),
+
 		"crawler": c.Dict("crawler", s.Schema{
 			"workers": c.Dict("workers", s.Schema{
 				"pool_size": c.Int("pool_size"),
