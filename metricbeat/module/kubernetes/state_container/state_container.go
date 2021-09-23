523e4dd942f0d4fa359956a0a933877055917d0f
Chris Mark <chrismarkou92@gmail.com>
2021-09-23 10:12:09 +0300

    Remove deprecated fields from k8s module (#28046)

--- metricbeat/module/kubernetes/state_container/state_container.go
+++ metricbeat/module/kubernetes/state_container/state_container.go
@@ -143,20 +143,7 @@ func (m *MetricSet) Fetch(reporter mb.ReporterV2) error {
 
 	m.enricher.Enrich(events)
 
-	// Calculate deprecated nanocores values
 	for _, event := range events {
-		if request, err := event.GetValue("cpu.request.cores"); err == nil {
-			if requestCores, ok := request.(float64); ok {
-				event.Put("cpu.request.nanocores", requestCores*nanocores)
-			}
-		}
-
-		if limit, err := event.GetValue("cpu.limit.cores"); err == nil {
-			if limitCores, ok := limit.(float64); ok {
-				event.Put("cpu.limit.nanocores", limitCores*nanocores)
-			}
-		}
-
 		// applying ECS to kubernetes.container.id in the form <container.runtime>://<container.id>
 		// copy to ECS fields the kubernetes.container.image, kubernetes.container.name
 		containerFields := common.MapStr{}
