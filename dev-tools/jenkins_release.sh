80728b11525182159c81f02504ad504a0f72fab7
Austin Songer <a.songer@protonmail.com>
2021-09-22 21:56:15 -0500

    Update jenkins_release.sh

--- dev-tools/jenkins_release.sh
+++ dev-tools/jenkins_release.sh
@@ -45,7 +45,7 @@ cleanup() {
 trap cleanup EXIT
 
 # This controls the defaults used the Jenkins package job. They can be
-# overridden by setting them in the environement prior to running this script.
+# overridden by setting them in the environment prior to running this script.
 export SNAPSHOT="${SNAPSHOT:-true}"
 export PLATFORMS="${PLATFORMS:-+linux/armv7 +linux/ppc64le +linux/s390x +linux/mips64}"
 
