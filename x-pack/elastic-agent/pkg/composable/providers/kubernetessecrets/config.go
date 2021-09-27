2c62f6f0c0af11c47c142d884bfbfbb3d26750db
Alex Kristiansen <alex.kristiansen@elastic.co>
2021-09-27 12:57:38 -0500

    mage fmt

--- x-pack/elastic-agent/pkg/composable/providers/kubernetessecrets/config.go
+++ x-pack/elastic-agent/pkg/composable/providers/kubernetessecrets/config.go
@@ -2,9 +2,6 @@
 // or more contributor license agreements. Licensed under the Elastic License;
 // you may not use this file except in compliance with the Elastic License.
 
-// TODO review the need for this
-// +build linux darwin windows
-
 package kubernetessecrets
 
 // Config for kubernetes provider
