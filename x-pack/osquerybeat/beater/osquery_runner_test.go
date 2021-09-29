63a55b04cf305a064c69a31d3ddd3b94389f718b
Aleksandr Maus <aleksandr.maus@elastic.co>
2021-09-29 16:17:04 -0400

    Runner and Fetcher unit tests

--- x-pack/osquerybeat/beater/osquery_runner_test.go
+++ x-pack/osquerybeat/beater/osquery_runner_test.go
@@ -15,6 +15,7 @@ import (
 	"github.com/elastic/beats/v7/libbeat/logp"
 	"github.com/elastic/beats/v7/x-pack/osquerybeat/internal/config"
 	"github.com/elastic/beats/v7/x-pack/osquerybeat/internal/osqd"
+	"github.com/google/go-cmp/cmp"
 )
 
 func waitGroupWithTimeout(ctx context.Context, g *errgroup.Group, to time.Duration) error {
@@ -84,13 +85,92 @@ func TestOsqueryRunnerCancellable(t *testing.T) {
 	}
 
 	// Cancel
-	go func() {
-		cn()
-	}()
+	cn()
 
 	// Wait for runner stop
 	er := waitGroupWithTimeout(parentCtx, g, to)
 	if er != nil && !errors.Is(er, context.Canceled) {
 		t.Fatal("failed running:", er)
+	}
+}
+
+func TestOsqueryRunnerRestart(t *testing.T) {
+	to := 10 * time.Second
+
+	parentCtx := context.Background()
+	logger := logp.NewLogger("osquery_runner")
+
+	runCh := make(chan struct{}, 1)
+
+	var runs int
+
+	runfn := func(ctx context.Context, flags osqd.Flags, inputCh <-chan []config.InputConfig) error {
+		runs++
+		runCh <- struct{}{}
+		<-ctx.Done()
+		return nil
+	}
+
+	ctx, cn := context.WithCancel(parentCtx)
+	defer cn()
+
+	g, ctx := errgroup.WithContext(ctx)
+
+	// Start runner
+	runner := newOsqueryRunner(logger)
+	g.Go(func() error {
+		return runner.Run(ctx, runfn)
+	})
+
+	// Sent input that will start the runner function
+	runner.Update(ctx, nil)
+
+	// Wait for runner start
+	err := waitForStart(ctx, runCh, to)
+	if err != nil {
+		t.Fatal("failed starting:", err)
+	}
+
+	inputConfigs := []config.InputConfig{
+		{
+			Osquery: &config.OsqueryConfig{
+				Options: map[string]interface{}{
+					"foo": "bar",
+				},
+			},
+		},
+	}
+
+	// Update flags, this should restart the run function
+	runner.Update(ctx, inputConfigs)
+
+	// Should get another run
+	err = waitForStart(ctx, runCh, to)
+	if err != nil {
+		t.Fatal("failed starting after flags update:", err)
+	}
+
+	// Update with the same flags, should not restart the runner function
+	runner.Update(ctx, inputConfigs)
+
+	// Should timeout on waiting for another run
+	err = waitForStart(ctx, runCh, 300*time.Millisecond)
+	if err != context.DeadlineExceeded {
+		t.Fatal("unexpected error type after update with the same flags:", err)
+	}
+
+	// Cancel
+	cn()
+
+	// Wait for runner stop
+	er := waitGroupWithTimeout(parentCtx, g, to)
+	if er != nil && !errors.Is(er, context.Canceled) {
+		t.Fatal("failed running:", er)
+	}
+
+	// Check that there were total of 2 executions of run function
+	diff := cmp.Diff(2, runs)
+	if diff != "" {
+		t.Error(diff)
 	}
 }
