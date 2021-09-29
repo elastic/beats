21348f4bfa69d740b909a89e4a19be177b6cd5c5
Oleksiy Kovyrin <oleksiy@kovyrin.net>
2021-09-29 15:18:03 -0400

    Properly format health metrics

--- x-pack/metricbeat/module/enterprisesearch/health/data.go
+++ x-pack/metricbeat/module/enterprisesearch/health/data.go
@@ -27,18 +27,18 @@ var (
 			"version": c.Str("version"),
 
 			"gc": c.Dict("gc", s.Schema{
-				"collection_count":     c.Int("collection_count"),
-				"collection_time.msec": c.Int("collection_time"),
+				"collection_count": c.Int("collection_count"),
+				"collection_time":  s.Object{"ms": c.Int("collection_time")},
 				// TODO: Add separate metrics for old and young generation collectors
 			}),
 
 			"memory_usage": c.Dict("memory_usage", s.Schema{
-				"heap_init.bytes":                   c.Int("heap_init"),
-				"heap_used.bytes":                   c.Int("heap_used"),
-				"heap_committed.bytes":              c.Int("heap_committed"),
-				"heap_max.bytes":                    c.Int("heap_max"),
-				"non_heap_init.bytes":               c.Int("non_heap_init"),
-				"non_heap_committed.bytes":          c.Int("non_heap_committed"),
+				"heap_init":                         s.Object{"bytes": c.Int("heap_init")},
+				"heap_used":                         s.Object{"bytes": c.Int("heap_used")},
+				"heap_committed":                    s.Object{"bytes": c.Int("heap_committed")},
+				"heap_max":                          s.Object{"bytes": c.Int("heap_max")},
+				"non_heap_init":                     s.Object{"bytes": c.Int("non_heap_init")},
+				"non_heap_committed":                s.Object{"bytes": c.Int("non_heap_committed")},
 				"object_pending_finalization_count": c.Int("object_pending_finalization_count"),
 			}),
 
@@ -51,8 +51,8 @@ var (
 		}),
 
 		"process": c.Dict("process", s.Schema{
-			"pid":        c.Int("pid"),
-			"uptime.sec": c.Int("uptime"),
+			"pid":    c.Int("pid"),
+			"uptime": s.Object{"sec": c.Int("uptime")},
 
 			"filebeat": c.Dict("filebeat", s.Schema{
 				"pid":                         c.Int("pid"),
