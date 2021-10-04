199572ee50567f019906181a2f1f075fe84ea3a7
Taylor Swanson <taylor.swanson@elastic.co>
2021-10-04 08:18:21 -0500

    mage fmt/update

--- filebeat/input/winlog/input.go
+++ filebeat/input/winlog/input.go
@@ -106,7 +106,7 @@ runLoop:
 		openErr := api.Open(evtCheckpoint)
 		if eventlog.IsRecoverable(openErr) {
 			log.Errorf("Encountered recoverable error when opening Windows Event Log: %v", openErr)
-			timed.Wait(cancelCtx, 5 * time.Second)
+			timed.Wait(cancelCtx, 5*time.Second)
 			continue
 		} else if openErr != nil {
 			return fmt.Errorf("failed to open windows event log: %v", openErr)
