86847ded4e06e0956ed6c7a4201ec61946b86654
Noémi Ványi <sitbackandwait@gmail.com>
2021-10-20 15:25:45 +0200

    rm legacy ref

--- libbeat/tests/system/test_ilm.py
+++ libbeat/tests/system/test_ilm.py
@@ -238,7 +238,7 @@ class TestCommandSetupILMPolicy(BaseTest):
                                               "-E", "setup.ilm.enabled=false"])
 
         assert exit_code == 0
-        self.idxmgmt.assert_legacy_index_template_loaded(self.index_name)
+        self.idxmgmt.assert_index_template_loaded(self.index_name)
         self.idxmgmt.assert_alias_not_created(self.alias_name)
         self.idxmgmt.assert_policy_not_created(self.policy_name)
 
