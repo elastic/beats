f4c33a4aaf7f6f4218cdd0ad8f92bb9776517119
Noémi Ványi <sitbackandwait@gmail.com>
2021-10-20 15:25:22 +0200

    rm legacy ref

--- libbeat/tests/system/test_ilm.py
+++ libbeat/tests/system/test_ilm.py
@@ -72,7 +72,7 @@ class TestRunILM(BaseTest):
         self.wait_until(lambda: self.log_contains("PublishEvents: 1 events have been published"))
         proc.check_kill_and_wait()
 
-        self.idxmgmt.assert_legacy_index_template_loaded(self.index_name)
+        self.idxmgmt.assert_index_template_loaded(self.index_name)
         self.idxmgmt.assert_alias_not_created(self.alias_name)
         self.idxmgmt.assert_policy_not_created(self.policy_name)
 
