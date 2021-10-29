2564a96b0d769327741d2d56182269f79671a205
kaiyan-sheng <kaiyan.sheng@elastic.co>
2021-10-29 12:30:36 -0600

    Change proxy_url from url.URL to string

--- x-pack/libbeat/common/aws/credentials.go
+++ x-pack/libbeat/common/aws/credentials.go
@@ -20,23 +20,28 @@ import (
 
 // ConfigAWS is a structure defined for AWS credentials
 type ConfigAWS struct {
-	AccessKeyID          string   `config:"access_key_id"`
-	SecretAccessKey      string   `config:"secret_access_key"`
-	SessionToken         string   `config:"session_token"`
-	ProfileName          string   `config:"credential_profile_name"`
-	SharedCredentialFile string   `config:"shared_credential_file"`
-	Endpoint             string   `config:"endpoint"`
-	RoleArn              string   `config:"role_arn"`
-	ProxyUrl             *url.URL `config:"proxy_url"`
+	AccessKeyID          string `config:"access_key_id"`
+	SecretAccessKey      string `config:"secret_access_key"`
+	SessionToken         string `config:"session_token"`
+	ProfileName          string `config:"credential_profile_name"`
+	SharedCredentialFile string `config:"shared_credential_file"`
+	Endpoint             string `config:"endpoint"`
+	RoleArn              string `config:"role_arn"`
+	ProxyUrl             string `config:"proxy_url"`
 }
 
 // InitializeAWSConfig function creates the awssdk.Config object from the provided config
 func InitializeAWSConfig(config ConfigAWS) (awssdk.Config, error) {
 	AWSConfig, _ := GetAWSCredentials(config)
-	if config.ProxyUrl != nil {
+	if config.ProxyUrl != "" {
+		parsedUrl, err := url.Parse(config.ProxyUrl)
+		if err != nil {
+			return AWSConfig, err
+		}
+
 		httpClient := &http.Client{
 			Transport: &http.Transport{
-				Proxy: http.ProxyURL(config.ProxyUrl),
+				Proxy: http.ProxyURL(parsedUrl),
 			},
 		}
 		AWSConfig.HTTPClient = httpClient
