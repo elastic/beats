b9989bf2b13755b5c1555a4eb3d4850e69b54184
Peter Deng <yundeng@ebay.com>
2021-11-02 20:15:21 +0800

    add options to configure k8s client qps/burst (#28151)

--- metricbeat/module/kubernetes/util/kubernetes.go
+++ metricbeat/module/kubernetes/util/kubernetes.go
@@ -49,10 +49,11 @@ type Enricher interface {
 
 type kubernetesConfig struct {
 	// AddMetadata enables enriching metricset events with metadata from the API server
-	AddMetadata bool          `config:"add_metadata"`
-	KubeConfig  string        `config:"kube_config"`
-	Host        string        `config:"host"`
-	SyncPeriod  time.Duration `config:"sync_period"`
+	AddMetadata       bool                         `config:"add_metadata"`
+	KubeConfig        string                       `config:"kube_config"`
+	Host              string                       `config:"host"`
+	SyncPeriod        time.Duration                `config:"sync_period"`
+	KubeClientOptions kubernetes.KubeClientOptions `config:"kube_client_options"`
 }
 
 type enricher struct {
@@ -83,7 +84,7 @@ func GetWatcher(base mb.BaseMetricSet, resource kubernetes.Resource, nodeScope b
 		return nil, nil
 	}
 
-	client, err := kubernetes.GetKubernetesClient(config.KubeConfig)
+	client, err := kubernetes.GetKubernetesClient(config.KubeConfig, config.KubeClientOptions)
 	if err != nil {
 		return nil, err
 	}
