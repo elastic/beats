27d820730cdcb224b781628fcfc964a2a1f850f1
kaiyan-sheng <kaiyan.sheng@elastic.co>
2021-11-03 13:32:34 -0600

    Update credentials.go

--- x-pack/libbeat/common/aws/credentials.go
+++ x-pack/libbeat/common/aws/credentials.go
@@ -15,27 +15,13 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/sts"
 	"github.com/pkg/errors"
 
-<<<<<<< HEAD
 	"github.com/elastic/beats/v7/libbeat/common/cfgwarn"
-=======
 	"github.com/elastic/beats/v7/libbeat/common/transport/httpcommon"
->>>>>>> f025c96219 (Change proxy_url from url.URL to string (#28725))
 	"github.com/elastic/beats/v7/libbeat/logp"
 )
 
 // ConfigAWS is a structure defined for AWS credentials
 type ConfigAWS struct {
-<<<<<<< HEAD
-	AccessKeyID          string   `config:"access_key_id"`
-	SecretAccessKey      string   `config:"secret_access_key"`
-	SessionToken         string   `config:"session_token"`
-	ProfileName          string   `config:"credential_profile_name"`
-	SharedCredentialFile string   `config:"shared_credential_file"`
-	Endpoint             string   `config:"endpoint"`
-	RoleArn              string   `config:"role_arn"`
-	AWSPartition         string   `config:"aws_partition"` // Deprecated.
-	ProxyUrl             *url.URL `config:"proxy_url"`
-=======
 	AccessKeyID          string `config:"access_key_id"`
 	SecretAccessKey      string `config:"secret_access_key"`
 	SessionToken         string `config:"session_token"`
@@ -43,8 +29,8 @@ type ConfigAWS struct {
 	SharedCredentialFile string `config:"shared_credential_file"`
 	Endpoint             string `config:"endpoint"`
 	RoleArn              string `config:"role_arn"`
+	AWSPartition         string `config:"aws_partition"` // Deprecated.
 	ProxyUrl             string `config:"proxy_url"`
->>>>>>> f025c96219 (Change proxy_url from url.URL to string (#28725))
 }
 
 // InitializeAWSConfig function creates the awssdk.Config object from the provided config
