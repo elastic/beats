0ea7a4e7776df2e11aeed0153db6bddbc96025b2
Mat Schaffer <mat@schaffer.me>
2021-11-08 14:28:50 +0900

    chore(metricbeat): testing for 7 & 8 style kibana metrics
    initial exploration, WIP

--- metricbeat/module/kibana/stats/stats_test.go
+++ metricbeat/module/kibana/stats/stats_test.go
@@ -21,12 +21,18 @@
 package stats
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"sort"
 	"testing"
 
+	"github.com/mitchellh/hashstructure"
 	"github.com/stretchr/testify/require"
 
+	"github.com/elastic/beats/v7/libbeat/common"
+	"github.com/elastic/beats/v7/metricbeat/mb"
 	mbtest "github.com/elastic/beats/v7/metricbeat/mb/testing"
 	"github.com/elastic/beats/v7/metricbeat/module/kibana/mtest"
 )
@@ -51,6 +57,7 @@ func TestFetchExcludeUsage(t *testing.T) {
 			case 1: // second call
 				require.Equal(t, "true", excludeUsage) // exclude_usage is always true
 				w.WriteHeader(200)
+				w.Write([]byte("{ \"version\": { \"number\": \"7.5.0\" }}"))
 
 			case 2: // third call
 				require.Equal(t, "true", excludeUsage) // exclude_usage is always true
@@ -97,4 +104,69 @@ func TestFetchNoExcludeUsage(t *testing.T) {
 
 	// First fetch
 	mbtest.ReportingFetchV2Error(f)
+}
+
+func kibanaServer(t *testing.T, path string, version string) *httptest.Server {
+	body, err := ioutil.ReadFile(path)
+	if err != nil {
+		t.Fatalf("could not read file: %s", err)
+	}
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		query := ""
+		v := r.URL.Query()
+		if len(v) > 0 {
+			query += "?" + v.Encode()
+		}
+
+		switch r.URL.Path+query {
+		case "/api/status":
+			response := fmt.Sprintf("{ \"version\": { \"number\": \"%s\" }}", version)
+			w.Write([]byte(response))
+		case "/api/stats?exclude_usage=true&extended=true":
+			w.WriteHeader(200)
+			w.Write(body)
+		default:
+			w.WriteHeader(404)
+		}
+	}))
+	return server
+}
+
+func TestMultiProcess(t *testing.T) {
+	kib := kibanaServer(t, "_meta/testdata/multi-process.json", "8.1.0")
+	defer kib.Close()
+
+	config := mtest.GetConfig("stats", kib.URL, true)
+
+	f := mbtest.NewReportingMetricSetV2Error(t, config)
+
+	events, errors := mbtest.ReportingFetchV2Error(f)
+	fmt.Printf("%+v\n", events)
+	fmt.Printf("%+v\n", errors)
+	if (len(errors) > 0) {
+		t.Errorf("unexpected error: %v", errors[0])
+	}
+
+	var data []common.MapStr
+
+	for _, e := range events {
+		beatEvent := mbtest.StandardizeEvent(f, e, mb.AddMetricSetInfo)
+		// Overwrite service.address as the port changes every time
+		beatEvent.Fields.Put("service.address", "127.0.0.1:55555")
+		data = append(data, beatEvent.Fields)
+	}
+
+	// Sorting the events is necessary as events are not necessarily sent in the same order
+	sort.SliceStable(data, func(i, j int) bool {
+		h1, _ := hashstructure.Hash(data[i], nil)
+		h2, _ := hashstructure.Hash(data[j], nil)
+		return h1 < h2
+	})
+
+	fmt.Printf("data: %+v\n", data)
+}
+
+func TestData(t *testing.T) {
+	mbtest.TestDataFiles(t, "kibana", "stats")
 }
