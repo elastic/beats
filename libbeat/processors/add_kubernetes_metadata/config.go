879e26904f9d3e4702dbf48b380941abe961e7e0
Tetiana Kravchenko <tetiana.kravchenko@elastic.co>
2021-11-10 11:11:45 +0100

    Enhance add_kubernetes_metadata matcher (#28868)
    * add documentation for add_kubernetes_metadata matcher; support 'var/log/pods' for resource_type: pod
    
    Signed-off-by: Tetiana Kravchenko <tetiana.kravchenko@elastic.co>
    
    * Apply suggestions from code review
    
    Co-authored-by: Chris Mark <chrismarkou92@gmail.com>
    
    * add record to CHANGELOG.next.asciidoc
    
    Signed-off-by: Tetiana Kravchenko <tetiana.kravchenko@elastic.co>
    
    * address comments: log pod id instead of array; log event
    
    Signed-off-by: Tetiana Kravchenko <tetiana.kravchenko@elastic.co>
    
    * add validation for logs_path matchers config
    
    Signed-off-by: Tetiana Kravchenko <tetiana.kravchenko@elastic.co>
    
    * add comment for the config validation check
    
    Signed-off-by: Tetiana Kravchenko <tetiana.kravchenko@elastic.co>
    
    * set different sourcePath for windows in tests
    
    Signed-off-by: Tetiana Kravchenko <tetiana.kravchenko@elastic.co>
    
    Co-authored-by: Chris Mark <chrismarkou92@gmail.com>
    (cherry picked from commit ac8275f72e55b699fb5ae41c68774280d7188bd3)

--- libbeat/processors/add_kubernetes_metadata/config.go
+++ libbeat/processors/add_kubernetes_metadata/config.go
@@ -67,5 +67,37 @@ func (k *kubeAnnotatorConfig) Validate() error {
 		k.Host = ""
 	}
 
+	// Checks below were added to warn the users early on and avoid initialising the processor in case the `logs_path`
+	// matcher config is not valid: supported paths defined as a `logs_path` configuration setting are strictly defined
+	// if `resource_type` is set
+	for _, matcher := range k.Matchers {
+		if matcherCfg, ok := matcher["logs_path"]; ok {
+			if matcherCfg.HasField("resource_type") {
+				logsPathMatcher := struct {
+					LogsPath     string `config:"logs_path"`
+					ResourceType string `config:"resource_type"`
+				}{}
+
+				err := matcherCfg.Unpack(&logsPathMatcher)
+				if err != nil {
+					return fmt.Errorf("fail to unpack the `logs_path` matcher configuration: %s", err)
+				}
+				if logsPathMatcher.LogsPath == "" {
+					return fmt.Errorf("invalid logs_path matcher configuration: when resource_type is defined, logs_path must be set as well")
+				}
+				if logsPathMatcher.ResourceType != "pod" && logsPathMatcher.ResourceType != "container" {
+					return fmt.Errorf("invalid resource_type %s, valid values include `pod`, `container`", logsPathMatcher.ResourceType)
+				}
+				if logsPathMatcher.ResourceType == "pod" && !(logsPathMatcher.LogsPath == "/var/lib/kubelet/pods/" || logsPathMatcher.LogsPath == "/var/log/pods/") {
+					return fmt.Errorf("invalid logs_path defined for resource_type: %s, valid values include `/var/lib/kubelet/pods/`, `/var/log/pods/`", logsPathMatcher.ResourceType)
+				}
+				if logsPathMatcher.ResourceType == "container" && logsPathMatcher.LogsPath != "/var/log/containers/" {
+					return fmt.Errorf("invalid logs_path defined for resource_type: %s, valid value is `/var/log/containers/`", logsPathMatcher.ResourceType)
+				}
+			}
+
+		}
+	}
+
 	return nil
 }
