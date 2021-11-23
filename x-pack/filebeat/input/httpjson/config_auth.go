e2aab838bdf1601105efa634df4228b9130019fc
kush-elastic <kush.rana@elastic.co>
2021-11-23 12:30:00 +0530

    Add grant-type: passoword in httpjson oauth2

--- x-pack/filebeat/input/httpjson/config_auth.go
+++ x-pack/filebeat/input/httpjson/config_auth.go
@@ -82,6 +82,8 @@ type oAuth2Config struct {
 	// common oauth fields
 	ClientID       string              `config:"client.id"`
 	ClientSecret   string              `config:"client.secret"`
+	User           string              `config:"user"`
+	Password       string              `config:"password"`
 	EndpointParams map[string][]string `config:"endpoint_params"`
 	Provider       oAuth2Provider      `config:"provider"`
 	Scopes         []string            `config:"scopes"`
@@ -109,14 +111,30 @@ func (o *oAuth2Config) client(ctx context.Context, client *http.Client) (*http.C
 
 	switch o.getProvider() {
 	case oAuth2ProviderAzure, oAuth2ProviderDefault:
-		creds := clientcredentials.Config{
-			ClientID:       o.ClientID,
-			ClientSecret:   o.ClientSecret,
-			TokenURL:       o.getTokenURL(),
-			Scopes:         o.Scopes,
-			EndpointParams: o.getEndpointParams(),
+		if o.User != "" || o.Password != "" {
+			conf := &oauth2.Config{
+				ClientID:     o.ClientID,
+				ClientSecret: o.ClientSecret,
+				Endpoint: oauth2.Endpoint{
+					TokenURL:  o.TokenURL,
+					AuthStyle: 1,
+				},
+			}
+			token, err := conf.PasswordCredentialsToken(ctx, o.User, o.Password)
+			if err != nil {
+				return nil, fmt.Errorf("oauth2 client: error loading credentials: %w", err)
+			}
+			return conf.Client(ctx, token), nil
+		} else {
+			creds := clientcredentials.Config{
+				ClientID:       o.ClientID,
+				ClientSecret:   o.ClientSecret,
+				TokenURL:       o.getTokenURL(),
+				Scopes:         o.Scopes,
+				EndpointParams: o.getEndpointParams(),
+			}
+			return creds.Client(ctx), nil
 		}
-		return creds.Client(ctx), nil
 	case oAuth2ProviderGoogle:
 		if o.GoogleJWTFile != "" {
 			cfg, err := google.JWTConfigFromJSON(o.GoogleCredentialsJSON, o.Scopes...)
