4a62f823474edc69398bf7525fadd48e7acae28c
Andrew Kroh <andrew.kroh@elastic.co>
2021-11-30 19:06:52 -0500

    Fix add_labels flattening of arrays values
    The processor was not working as described in the docs. For example:
    
    ```yaml
    processors:
      - add_labels:
          labels:
            number: 1
            with.dots: test
            nested:
              with.dots: nested
            array:
              - do
              - re
              - with.field: mi
    ```
    
    ```json
    {
      "labels": {
        "array": [
          "do",
          "re",
          {
            "with": {
              "field": "mi"
            }
          }
        ],
        "nested.with.dots": "nested",
        "number": 1,
        "with.dots": "test"
      }
    ```
    
    ```json
    {
      "labels": {
        "number": 1,
        "with.dots": "test",
        "nested.with.dots": "nested",
        "array.0": "do",
        "array.1": "re",
        "array.2.with.field": "mi"
      }
    }
    ```

--- libbeat/processors/actions/add_labels.go
+++ libbeat/processors/actions/add_labels.go
@@ -20,6 +20,8 @@ package actions
 import (
 	"fmt"
 
+	"github.com/elastic/go-ucfg"
+
 	"github.com/elastic/beats/v7/libbeat/common"
 	"github.com/elastic/beats/v7/libbeat/processors"
 	"github.com/elastic/beats/v7/libbeat/processors/checks"
@@ -41,10 +43,15 @@ func createAddLabels(c *common.Config) (processors.Processor, error) {
 	}{}
 	err := c.Unpack(&config)
 	if err != nil {
-		return nil, fmt.Errorf("fail to unpack the add_fields configuration: %s", err)
+		return nil, fmt.Errorf("fail to unpack the add_fields configuration: %w", err)
 	}
 
-	return makeFieldsProcessor(LabelsKey, config.Labels.Flatten(), true), nil
+	flatLabels, err := flattenLabels(config.Labels)
+	if err != nil {
+		return nil, fmt.Errorf("failed to flatten labels: %w", err)
+	}
+
+	return makeFieldsProcessor(LabelsKey, flatLabels, true), nil
 }
 
 // NewAddLabels creates a new processor adding the given object to events. Set
@@ -53,8 +60,33 @@ func createAddLabels(c *common.Config) (processors.Processor, error) {
 // If labels contains nested objects, NewAddLabels will flatten keys into labels by
 // by joining names with a dot ('.') .
 // The labels will be inserted into the 'labels' field.
-func NewAddLabels(labels common.MapStr, shared bool) processors.Processor {
+func NewAddLabels(labels common.MapStr, shared bool) (processors.Processor, error) {
+	flatLabels, err := flattenLabels(labels)
+	if err != nil {
+		return nil, fmt.Errorf("failed to flatten labels: %w", err)
+	}
+
 	return NewAddFields(common.MapStr{
-		LabelsKey: labels.Flatten(),
-	}, shared, true)
+		LabelsKey: flatLabels,
+	}, shared, true), nil
+}
+
+func flattenLabels(labels common.MapStr) (common.MapStr, error) {
+	labelConfig, err := common.NewConfigFrom(labels)
+	if err != nil {
+		return nil, err
+	}
+
+	flatKeys := (*ucfg.Config)(labelConfig).FlattenedKeys()
+
+	flatMap := make(common.MapStr, len(flatKeys))
+	for _, k := range flatKeys {
+		v, err := labelConfig.String(k, -1)
+		if err != nil {
+			return nil, err
+		}
+		flatMap[k] = v
+	}
+
+	return flatMap, nil
 }
