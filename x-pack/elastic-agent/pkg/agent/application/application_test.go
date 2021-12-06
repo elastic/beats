34611974d867820886ab2bd7c9ff4ce5925f4c3e
narph <mariana@elastic.co>
2021-12-06 18:46:36 +0100

    add test on merge

--- x-pack/elastic-agent/pkg/agent/application/application_test.go
+++ x-pack/elastic-agent/pkg/agent/application/application_test.go
@@ -3,3 +3,34 @@
 // you may not use this file except in compliance with the Elastic License.
 
 package application
+
+import (
+	"github.com/elastic/beats/v7/x-pack/elastic-agent/pkg/config"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestMergeFleetConfig(t *testing.T) {
+	cfg := map[string]interface{}{
+		"fleet": map[string]interface{}{
+			"enabled":        true,
+			"kibana":         map[string]interface{}{"host": "demo"},
+			"access_api_key": "123",
+		},
+		"agent": map[string]interface{}{
+			"grpc": map[string]interface{}{
+				"port": uint16(6790),
+			},
+		},
+	}
+
+	rawConfig := config.MustNewConfigFrom(cfg)
+	storage, conf, err := mergeFleetConfig(rawConfig)
+	require.NoError(t, err)
+	assert.NotNil(t, storage)
+	assert.NotNil(t, conf)
+	assert.Equal(t, conf.Fleet.Enabled, cfg["fleet"].(map[string]interface{})["enabled"])
+	assert.Equal(t, conf.Fleet.AccessAPIKey, cfg["fleet"].(map[string]interface{})["access_api_key"])
+	assert.Equal(t, conf.Settings.GRPC.Port, cfg["agent"].(map[string]interface{})["grpc"].(map[string]interface{})["port"].(uint16))
+}
