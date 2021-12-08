8088558bfa7776dbbf1a354cebf7559636d98a7b
Tiago Queiroz <tiago.queiroz@elastic.co>
2021-12-08 15:08:22 +0100

    tlscommon: add tests for `ssl.ca_tursted_fingerprint`
    This commit adds tests to `trustRootCA` and `makeVerifyConnection`,
    the tests aim to cover the usage of `ssl.ca_tursted_fingerprint`.

--- libbeat/common/transport/tlscommon/tls_config_test.go
+++ libbeat/common/transport/tlscommon/tls_config_test.go
@@ -26,6 +26,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestMakeVerifyServerConnection(t *testing.T) {
@@ -194,6 +195,8 @@ func openTestCerts() (map[string]*x509.Certificate, error) {
 		"unknown authority": "unsigned_tls.crt",
 		"correct":           "client1.crt",
 		"wildcard":          "server.crt",
+		"es-leaf":           "es-leaf.crt",
+		"es-root-ca":        "es-root-ca-cert.crt",
 	} {
 
 		certBytes, err := ioutil.ReadFile(filepath.Join("testdata", certname))
@@ -209,4 +212,179 @@ func openTestCerts() (map[string]*x509.Certificate, error) {
 	}
 
 	return certs, nil
+}
+
+func TestTrustRootCA(t *testing.T) {
+	certs, err := openTestCerts()
+	if err != nil {
+		t.Fatalf("reading test certificates: %v", err)
+	}
+
+	nonEmptyCertPool := x509.NewCertPool()
+	nonEmptyCertPool.AddCert(certs["wildcard"])
+	nonEmptyCertPool.AddCert(certs["unknown authority"])
+
+	testCases := []struct {
+		name                 string
+		rootCAs              *x509.CertPool
+		caTrustedFingerprint string
+		peerCerts            []*x509.Certificate
+		expectingError       bool
+		expectedRootCAsLen   int
+	}{
+		{
+			name:                 "RootCA cert matches the fingerprint and is added to cfg.RootCAs",
+			caTrustedFingerprint: "e83171aa133b2b507e057fe091e296a7e58e9653c2b88d203b64a47eef6ec62b",
+			peerCerts:            []*x509.Certificate{certs["es-leaf"], certs["es-root-ca"]},
+			expectedRootCAsLen:   1,
+		},
+		{
+			name:                 "RootCA cert doesn not matche the fingerprint and is not added to cfg.RootCAs",
+			caTrustedFingerprint: "e83171aa133b2b507e057fe091e296a7e58e9653c2b88d203b64a47eef6ec62b",
+			peerCerts:            []*x509.Certificate{certs["es-leaf"], certs["es-root-ca"]},
+			expectedRootCAsLen:   0,
+		},
+		{
+			name:                 "non empty CertPool has the RootCA added",
+			rootCAs:              nonEmptyCertPool,
+			caTrustedFingerprint: "e83171aa133b2b507e057fe091e296a7e58e9653c2b88d203b64a47eef6ec62b",
+			peerCerts:            []*x509.Certificate{certs["es-leaf"], certs["es-root-ca"]},
+			expectedRootCAsLen:   3,
+		},
+		{
+			name:                 "invalis HEX encoding",
+			caTrustedFingerprint: "INVALID ENCODING",
+			expectedRootCAsLen:   0,
+			expectingError:       true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			cfg := TLSConfig{
+				RootCAs:              tc.rootCAs,
+				CATrustedFingerprint: tc.caTrustedFingerprint,
+			}
+			err := trustRootCA(&cfg, tc.peerCerts)
+			if tc.expectingError && err == nil {
+				t.Fatal("expecting an error when calling trustRootCA")
+			}
+
+			if !tc.expectingError && err != nil {
+				t.Fatalf("did not expect an error calling trustRootCA: %v", err)
+			}
+
+			if tc.expectedRootCAsLen != 0 {
+				if cfg.RootCAs == nil {
+					t.Fatal("cfg.RootCAs cannot be nil")
+				}
+
+				// we want to know the number of certificates in the CertPool (RootCAs), as it is not
+				// directly available, we use this workaround of reading the number of subjects in the pool.
+				if got, expected := len(cfg.RootCAs.Subjects()), tc.expectedRootCAsLen; got != expected {
+					t.Fatalf("expecting cfg.RootCAs to have %d element, got %d instead", expected, got)
+				}
+			}
+		})
+	}
+}
+
+func TestMakeVerifyConnectionUsesCATrustedFingerprint(t *testing.T) {
+	testCerts, err := openTestCerts()
+	if err != nil {
+		t.Fatalf("failed to open test certs: %+v", err)
+	}
+
+	testcases := map[string]struct {
+		verificationMode     TLSVerificationMode
+		peerCerts            []*x509.Certificate
+		serverName           string
+		expectedCallback     bool
+		expectingError       bool
+		CATrustedFingerprint string
+		CASHA256             []string
+	}{
+		"CATrustedFingerprint and verification mode:VerifyFull": {
+			verificationMode:     VerifyFull,
+			peerCerts:            []*x509.Certificate{testCerts["es-leaf"], testCerts["es-root-ca"]},
+			serverName:           "localhost",
+			expectedCallback:     true,
+			CATrustedFingerprint: "e83171aa133b2b507e057fe091e296a7e58e9653c2b88d203b64a47eef6ec62b",
+		},
+		"CATrustedFingerprint and verification mode:VerifyCertificate": {
+			verificationMode:     VerifyCertificate,
+			peerCerts:            []*x509.Certificate{testCerts["es-leaf"], testCerts["es-root-ca"]},
+			serverName:           "localhost",
+			expectedCallback:     true,
+			CATrustedFingerprint: "e83171aa133b2b507e057fe091e296a7e58e9653c2b88d203b64a47eef6ec62b",
+		},
+		"CATrustedFingerprint and verification mode:VerifyStrict": {
+			verificationMode:     VerifyStrict,
+			peerCerts:            []*x509.Certificate{testCerts["es-leaf"], testCerts["es-root-ca"]},
+			serverName:           "localhost",
+			expectedCallback:     true,
+			CATrustedFingerprint: "e83171aa133b2b507e057fe091e296a7e58e9653c2b88d203b64a47eef6ec62b",
+			CASHA256:             []string{Fingerprint(testCerts["es-leaf"])},
+		},
+		"CATrustedFingerprint and verification mode:VerifyNone": {
+			verificationMode: VerifyNone,
+			peerCerts:        []*x509.Certificate{testCerts["es-leaf"], testCerts["es-root-ca"]},
+			serverName:       "localhost",
+			expectedCallback: false,
+		},
+		"invalid CATrustedFingerprint and verification mode:VerifyFull returns error": {
+			verificationMode:     VerifyFull,
+			peerCerts:            []*x509.Certificate{testCerts["es-leaf"], testCerts["es-root-ca"]},
+			serverName:           "localhost",
+			expectedCallback:     true,
+			CATrustedFingerprint: "INVALID HEX ENCODING",
+			expectingError:       true,
+		},
+		"invalid CATrustedFingerprint and verification mode:VerifyCertificate returns error": {
+			verificationMode:     VerifyCertificate,
+			peerCerts:            []*x509.Certificate{testCerts["es-leaf"], testCerts["es-root-ca"]},
+			serverName:           "localhost",
+			expectedCallback:     true,
+			CATrustedFingerprint: "INVALID HEX ENCODING",
+			expectingError:       true,
+		},
+		"invalid CATrustedFingerprint and verification mode:VerifyStrict returns error": {
+			verificationMode:     VerifyStrict,
+			peerCerts:            []*x509.Certificate{testCerts["es-leaf"], testCerts["es-root-ca"]},
+			serverName:           "localhost",
+			expectedCallback:     true,
+			CATrustedFingerprint: "INVALID HEX ENCODING",
+			expectingError:       true,
+			CASHA256:             []string{Fingerprint(testCerts["es-leaf"])},
+		},
+	}
+
+	for name, test := range testcases {
+		t.Run(name, func(t *testing.T) {
+			cfg := &TLSConfig{
+				Verification:         test.verificationMode,
+				CATrustedFingerprint: test.CATrustedFingerprint,
+				CASha256:             test.CASHA256,
+			}
+
+			verifier := makeVerifyConnection(cfg)
+			if test.expectedCallback {
+				require.NotNil(t, verifier, "makeVerifyConnection returned a nil verifier")
+			} else {
+				require.Nil(t, verifier)
+				return
+			}
+
+			err := verifier(tls.ConnectionState{
+				PeerCertificates: test.peerCerts,
+				ServerName:       test.serverName,
+				VerifiedChains:   [][]*x509.Certificate{test.peerCerts},
+			})
+			if test.expectingError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
 }
