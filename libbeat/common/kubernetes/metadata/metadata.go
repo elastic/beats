95759e89fa5e72bff2e4eed2742266d01f05eb34
chrismark <chrismarkou92@gmail.com>
2021-12-08 07:37:47 +0000

    Fix
    Signed-off-by: chrismark <chrismarkou92@gmail.com>

--- libbeat/common/kubernetes/metadata/metadata.go
+++ libbeat/common/kubernetes/metadata/metadata.go
@@ -122,7 +122,7 @@ func getClusterInfoFromKubeadmConfigMap(client k8sclient.Interface) (ClusterInfo
 	if client == nil {
 		return clusterInfo, fmt.Errorf("unable to get cluster identifiers from kubeadm-config")
 	}
-	cm, err := client.CoreV1().ConfigMaps("kube-system42").Get(context.TODO(), "kubeadm-config", metav1.GetOptions{})
+	cm, err := client.CoreV1().ConfigMaps("kube-system").Get(context.TODO(), "kubeadm-config", metav1.GetOptions{})
 	if err != nil {
 		return clusterInfo, fmt.Errorf("unable to get cluster identifiers from kubeadm-config: %+v", err)
 	}
