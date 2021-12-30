d49121c87de27f830df492a2004af9e85560db40
Adrian Serrano <adrisr83@gmail.com>
2021-12-30 03:59:45 +0100

    Ensure that match_only_text and wildcard fields can be added to default_field list (#29634)
    Fixes the construction of the index template's default_field list so that fields of type match_only_text
    and wildcard can be added to it.
    
    (cherry picked from commit 84924e9f32d018e360303714c26f754dbab34f93)

--- libbeat/template/processor.go
+++ libbeat/template/processor.go
@@ -133,7 +133,7 @@ func (p *Processor) Process(fields mapping.Fields, state *fieldState, output com
 
 		if *field.DefaultField {
 			switch field.Type {
-			case "", "keyword", "text":
+			case "", "keyword", "text", "match_only_text", "wildcard":
 				addToDefaultFields(&field)
 			}
 		}
