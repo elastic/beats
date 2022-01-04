3fba2df267a08cbd6a995aea27acfbf8ca9614ea
Noémi Ványi <sitbackandwait@gmail.com>
2022-01-04 17:36:23 +0100

    adjust docs

--- libbeat/outputs/elasticsearch/client.go
+++ libbeat/outputs/elasticsearch/client.go
@@ -41,7 +41,7 @@ import (
 var (
 	errPayloadTooLarge = errors.New("the bulk payload is too large for the server. Consider to adjust `http.max_content_length` parameter in Elasticsearch or `bulk_max_size` in the beat. The batch has been dropped")
 
-	ErrTooOld = errors.New("Elasticsearch is too old. If you would like to connect to older instances enable sdfsf.dsfsd.")
+	ErrTooOld = errors.New("Elasticsearch is too old. If you would like to connect to older instances set output.elasticsearch.allow_older_versions to true.")
 )
 
 // Client is an elasticsearch client.
