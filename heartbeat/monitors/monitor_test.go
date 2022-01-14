1907a1ec849fc936572d211e931bc793ddc04cb9
Andrew Cholakian <andrewvc@elastic.co>
2022-01-14 15:38:16 -0600

    Fix testS

--- heartbeat/monitors/monitor_test.go
+++ heartbeat/monitors/monitor_test.go
@@ -25,19 +25,49 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/elastic/beats/v7/heartbeat/scheduler"
+	"github.com/elastic/beats/v7/libbeat/common"
 	"github.com/elastic/beats/v7/libbeat/monitoring"
+	"github.com/elastic/go-lookslike"
+	"github.com/elastic/go-lookslike/isdef"
 	"github.com/elastic/go-lookslike/testslike"
+	"github.com/elastic/go-lookslike/validator"
 )
 
-func TestMonitor(t *testing.T) {
-	serverMonConf := mockPluginConf(t, "", "@every 1ms", "http://example.net")
+// TestMonitorBasic tests a basic config
+func TestMonitorBasic(t *testing.T) {
+	testMonitorConfig(
+		t,
+		mockPluginConf(t, "myId", "myName", "@every 1ms", "http://example.net"),
+		mockEventMonitorValidator("myId", "myName"),
+	)
+}
+
+// TestMonitorBasic tests a config that errors out at plugin creation, but still has stdfields defined.
+// This should cause the monitor to run, but only produce error documents
+func TestMonitorCfgError(t *testing.T) {
+	testMonitorConfig(
+		t,
+		mockInvalidPluginConfigWithStdFields(t, "invalidTestId", "invalidTestName", "@every 10s"),
+		lookslike.Compose(
+			baseMockEventMonitorValidator("invalidTestId", "invalidTestName", "down"),
+			lookslike.MustCompile(common.MapStr{
+				"error": common.MapStr{
+					"message": isdef.IsStringContaining("missing required field"),
+					"type":    "io",
+				},
+			}),
+		),
+	)
+}
+
+func testMonitorConfig(t *testing.T, conf *common.Config, eventValidator validator.Validator) {
 	reg, built, closed := mockPluginsReg()
 	pipelineConnector := &MockPipelineConnector{}
 
 	sched := scheduler.Create(1, monitoring.NewRegistry(), time.Local, nil, false)
 	defer sched.Stop()
 
-	mon, err := newMonitor(serverMonConf, reg, pipelineConnector, sched.Add, nil, false)
+	mon, err := newMonitor(conf, reg, pipelineConnector, sched.Add, nil, false)
 	require.NoError(t, err)
 
 	mon.Start()
@@ -56,7 +86,7 @@ func TestMonitor(t *testing.T) {
 			pcClient.Close()
 
 			for _, event := range pcClient.Publishes() {
-				testslike.Test(t, mockEventMonitorValidator(""), event.Fields)
+				testslike.Test(t, eventValidator, event.Fields)
 			}
 		} else {
 			// Let's yield this goroutine so we don't spin
@@ -74,46 +104,4 @@ func TestMonitor(t *testing.T) {
 
 	assert.Equal(t, 1, closed.Load())
 	assert.Equal(t, true, pcClient.closed)
-}
-
-func TestCheckInvalidConfig(t *testing.T) {
-	serverMonConf := mockInvalidPluginConf(t)
-	reg, built, closed := mockPluginsReg()
-	pipelineConnector := &MockPipelineConnector{}
-
-	sched := scheduler.Create(1, monitoring.NewRegistry(), time.Local, nil, false)
-	defer sched.Stop()
-
-	m, err := newMonitor(serverMonConf, reg, pipelineConnector, sched.Add, nil, false)
-	require.Error(t, err)
-	// This could change if we decide the contract for newMonitor should always return a monitor
-	require.Nil(t, m, "For this test to work we need a nil value for the monitor.")
-
-	// These counters are both zero since this fails at config parse time
-	require.Equal(t, 0, built.Load())
-	require.Equal(t, 0, closed.Load())
-
-	require.Error(t, checkMonitorConfig(serverMonConf, reg))
-}
-
-// So long as the standard fields are present the monitor will 'run', but report an error each time,
-// even if the plugin factory fails to create a job.
-func TestInvalidConfigWithStdFields(t *testing.T) {
-	serverMonConf := mockInvalidPluginConf(t)
-	reg, built, closed := mockPluginsReg()
-	pipelineConnector := &MockPipelineConnector{}
-
-	sched := scheduler.Create(1, monitoring.NewRegistry(), time.Local, nil, false)
-	defer sched.Stop()
-
-	m, err := newMonitor(serverMonConf, reg, pipelineConnector, sched.Add, nil, false)
-	require.NoError(t, err)
-	// This could change if we decide the contract for newMonitor should always return a monitor
-	require.Nil(t, m, "For this test to work we need a nil value for the monitor.")
-
-	// These counters are both zero since this fails at config parse time
-	require.Equal(t, 0, built.Load())
-	require.Equal(t, 0, closed.Load())
-
-	require.Error(t, checkMonitorConfig(serverMonConf, reg))
 }
