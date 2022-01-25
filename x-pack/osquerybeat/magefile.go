3e7b40bb61758e6b2e4fdbae122291b1e4344254
Anderson Queiroz <anderson.queiroz@elastic.co>
2022-01-25 16:37:05 +0100

    fix osquerybuild AssembleDarwinUniversal

--- x-pack/osquerybeat/magefile.go
+++ x-pack/osquerybeat/magefile.go
@@ -18,9 +18,9 @@ import (
 	"time"
 
 	"github.com/magefile/mage/mg"
-	"github.com/magefile/mage/sh"
 
 	devtools "github.com/elastic/beats/v7/dev-tools/mage"
+	"github.com/elastic/beats/v7/dev-tools/mage/target/build"
 	"github.com/elastic/beats/v7/x-pack/osquerybeat/internal/command"
 	"github.com/elastic/beats/v7/x-pack/osquerybeat/internal/distro"
 	osquerybeat "github.com/elastic/beats/v7/x-pack/osquerybeat/scripts/mage"
@@ -211,29 +211,7 @@ func CrossBuildGoDaemon() error {
 // universal binary using `lipo`. It assumes the darwin/amd64 and darwin/arm64
 // were built and only performs the merge.
 func AssembleDarwinUniversal() error {
-	beatsArgs := map[string]string{
-		devtools.BeatName:   "",
-		"osquery-extension": "ext/osquery-extension/",
-	}
-	lipo := sh.RunCmd("lipo", "-create", "-output")
-
-	for name, path := range beatsArgs {
-		lipoTmplArgs := []string{
-			"%sbuild/golang-crossbuild/%s-darwin-universal",
-			"%sbuild/golang-crossbuild/%s-darwin-arm64",
-			"%sbuild/golang-crossbuild/%s-darwin-amd64"}
-
-		var lipoArgs []string
-		for _, arg := range lipoTmplArgs {
-			lipoArgs = append(lipoArgs, fmt.Sprintf(arg, path, name))
-		}
-
-		if err := lipo(lipoArgs...); err != nil {
-			return fmt.Errorf("could not merge %s: %w", name, err)
-		}
-	}
-
-	return nil
+	return build.AssembleDarwinUniversal()
 }
 
 // Package packages the Beat for distribution.
