cffa97e6b443b05db92167af43290be31068314a
mtojek <marcin.tojek@elastic.co>
2022-01-27 09:36:22 +0100

    Fix: sys

--- x-pack/filebeat/tests/system/test_http_endpoint.py
+++ x-pack/filebeat/tests/system/test_http_endpoint.py
@@ -179,7 +179,7 @@ class Test(BaseTest):
         assert r.json()[
             'message'] == 'wrong Content-Type header, expecting application/json'
 
-    @unittest.skipIf(os.getenv("CI") is not None and platform.system() == 'Darwin', 'Flaky test: https://github.com/elastic/beats/issues/30028')
+    @unittest.skipIf(os.getenv("CI") is not None and sys.platform.system() == 'Darwin', 'Flaky test: https://github.com/elastic/beats/issues/30028')
     def test_http_endpoint_missing_auth_value(self):
         """
         Test http_endpoint input with missing basic auth values.
