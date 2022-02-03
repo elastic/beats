7b7fa34e44d4fe2b6e67b1382ad821d804af2d9d
Victor Martinez <victormartinezrubio@gmail.com>
2022-02-03 10:05:33 +0000

    use main default branch (#29710)
    (cherry picked from commit 237937085a5a7337ba06f1268cfc55cd4b869e31)
    
    # Conflicts:
    #	.ci/jobs/apm-beats-update.yml
    #	.ci/jobs/beats-tester.yml
    #	.ci/jobs/beats.yml
    #	.ci/jobs/packaging.yml
    #	.ci/schedule-daily.groovy
    #	.ci/schedule-weekly.groovy
    #	.mergify.yml
    #	README.md
    #	filebeat/_meta/test/docs/testing.md
    #	x-pack/elastic-agent/README.md

--- .ci/schedule-daily.groovy
+++ .ci/schedule-daily.groovy
@@ -20,9 +20,13 @@ pipeline {
   stages {
     stage('Nighly beats builds') {
       steps {
+<<<<<<< HEAD
         runBuild(quietPeriod: 0, job: 'Beats/beats/master')
         runBuild(quietPeriod: 2000, job: 'Beats/beats/7.16')
         runBuild(quietPeriod: 4000, job: 'Beats/beats/7.15')
+=======
+        runBuilds(quietPeriodFactor: 2000, branches: ['main', '8.<minor>', '7.<minor>', '7.<next-minor>'])
+>>>>>>> 237937085 (use main default branch (#29710))
       }
     }
   }
