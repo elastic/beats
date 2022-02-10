5a878401bafeafbd5ec3608d16d27cfaacb5f48e
Craig MacKenzie <craig.mackenzie@elastic.co>
2022-02-09 19:27:43 -0500

    Revert "libbeat/common/transport: fix log message about TLS (#30063)"
    This reverts commit e208c22507890eb3633d55f1ba7d388f3076b2d6.
    
    This commit caused the agent to crash on startup on Windows with a nil
    pointer dereference.

--- libbeat/common/transport/tls.go
+++ libbeat/common/transport/tls.go
@@ -154,14 +154,7 @@ func tlsDialWith(
 		}
 	}
 
-	// We only check the status of config.Verification (`ssl.verification_mode`
-	// in the configuration file) because we have a custom verification logic
-	// implemented by setting tlsConfig.VerifyConnection that runs regardless of
-	// the status of tlsConfig.InsecureSkipVerify.
-	// For verification modes VerifyFull and VerifyCeritifcate we set
-	// tlsConfig.InsecureSkipVerify to true, hence it's not an indicator of
-	// whether TLS verification is enabled or not.
-	if config.Verification == tlscommon.VerifyNone {
+	if tlsConfig.InsecureSkipVerify {
 		d.Warn("security", "server's certificate chain verification is disabled")
 	} else {
 		d.Info("security", "server's certificate chain verification is enabled")
