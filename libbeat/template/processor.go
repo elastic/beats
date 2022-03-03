2d540a007404f7f456931fef6d7b6e72c8d086eb
Adrian Serrano <adrisr83@gmail.com>
2022-03-03 10:22:52 +0100

    libbeat: Don't force an ignore_above limit in wildcard fields
    Modifies libbeat's template processor to stop hardcoding a default
    `ignore_above` limit of 1024 on wildcard fields. This behavior was
    inherited from keyword fields.
    
    Closes #30096

--- libbeat/template/processor.go
+++ libbeat/template/processor.go
@@ -317,11 +317,10 @@ func (p *Processor) wildcard(f *mapping.Field, analyzers common.MapStr) common.M
 
 	property["type"] = "wildcard"
 
-	switch f.IgnoreAbove {
-	case 0: // Use libbeat default
-		property["ignore_above"] = defaultIgnoreAbove
-	case -1: // Use ES default
-	default: // Use user value
+	/* For wildcard fields, unlike keyword, don't force a default ignore_above limit.
+	   The default in ES will be used unless an explicit limit is set.
+	*/
+	if f.IgnoreAbove > 0 {
 		property["ignore_above"] = f.IgnoreAbove
 	}
 
