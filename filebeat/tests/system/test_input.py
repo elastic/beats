20c8f87718a4a6348accacaa4f0a069887734591
Tiago Queiroz <tiago.queiroz@elastic.co>
2022-03-16 12:26:37 +0100

    fix up tests, again

--- filebeat/tests/system/test_input.py
+++ filebeat/tests/system/test_input.py
@@ -62,10 +62,8 @@ class Test(BaseTest):
         fixed_entry = entries[1]
 
         # Compare the key excluding the inode and device ID bits
-        assert global_entry['_key'].startswith('filestream::.global::native::'),
-        "old key must contain '.global' ID"
-        assert fixed_entry['_key'].startswith('filestream::test-fix-global-id::native::'),
-        "key in registry does not have the expected ID"
+        assert global_entry['_key'].startswith('filestream::.global::native::'), "old key must contain '.global' ID"
+        assert fixed_entry['_key'].startswith('filestream::test-fix-global-id::native::'), "key in registry does not have the expected ID"
 
         # Compare the TTL because it indicates if the entry has been 'removed' or not
         assert global_entry['ttl'] == 0, "ttl must be 0 because that's the effect of 'removing' the entry from the registry"
