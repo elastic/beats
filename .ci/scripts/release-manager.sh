96b08d98f2352d15d8e5e6f4322c72f71c61eb25
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-03-21 14:18:54 +0000

    ci: add retry pulling docker images
    to help with flakiness while fetching docker images

--- .ci/scripts/release-manager.sh
+++ .ci/scripts/release-manager.sh
@@ -9,6 +9,8 @@
 #
 set -uexo pipefail
 
+source /usr/local/bin/bash_standard_lib.sh
+
 # set required permissions on artifacts and directory
 chmod -R a+r build/distributions/*
 chmod -R a+w build/distributions
@@ -32,7 +34,8 @@ find build/distributions -name '*linux-amd64.docker.tar.gz*' -print0 |
 
 # ensure the latest image has been pulled
 IMAGE=docker.elastic.co/infra/release-manager:latest
-docker pull --quiet $IMAGE
+(retry 3 docker pull --quiet "${IMAGE}") || echo "Error pulling ${IMAGE} Docker image, we continue"
+docker images --filter=reference=$IMAGE
 
 # Generate checksum files and upload to GCS
 docker run --rm \
