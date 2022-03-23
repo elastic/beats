0be900fbdc55fca3bf4e140c3093b36037db7423
emilioalvap <emilio.alvarezpineiro@elastic.co>
2022-03-23 19:43:21 +0100

    Fix linter issues

--- libbeat/paths/paths.go
+++ libbeat/paths/paths.go
@@ -87,7 +87,7 @@ func (paths *Path) InitPaths(cfg *Path) error {
 	// make sure the data path exists
 	err = os.MkdirAll(paths.Data, 0770)
 	if err != nil {
-		return fmt.Errorf("Failed to create data path %s: %v", paths.Data, err)
+		return fmt.Errorf("failed to create data path %s: %w", paths.Data, err)
 	}
 
 	return nil
