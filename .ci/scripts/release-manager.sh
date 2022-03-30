a20f207e8302b6558cff2c391a99613aa4f1dd9c
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-03-30 15:58:04 +0100

    ci: simplfy the preapre-releasemanager

--- .ci/scripts/release-manager.sh
+++ .ci/scripts/release-manager.sh
@@ -1,24 +1,23 @@
 #!/usr/bin/env bash
 #
-# This script is executed by the release snapshot stage.
-# It requires the below environment variables:
-# - BRANCH_NAME
-# - VAULT_ADDR
-# - VAULT_ROLE_ID
-# - VAULT_SECRET_ID
+# This script is executed by the DRA stage.
+# It prepares the required files to be consumed by the release-manager
+# It can be published as snapshot or staging, for such you use
+# the paramater $0 "snapshot" or $0 "staging"
 #
 set -ueo pipefail
 
-source /usr/local/bin/bash_standard_lib.sh
-
-# set required permissions on artifacts and directory
-chmod -R a+r build/distributions/*
-chmod -R a+w build/distributions
+readonly TYPE=${1:-snapshot}
 
 # rename dependencies.csv to the name expected by release-manager.
 VERSION=$(make get-version)
+FINAL_VERSION=$VERSION-SNAPSHOT
+if [ "$TYPE" != "snapshot" ] ; then
+  FINAL_VERSION=$VERSION
+fi
+VERSION=$(make get-version)
 mv build/distributions/dependencies.csv \
-   build/distributions/dependencies-$VERSION-SNAPSHOT.csv
+   build/distributions/dependencies-"$FINAL_VERSION".csv
 
 # rename docker files to support the unified release format.
 # TODO: this could be supported by the package system itself
@@ -37,26 +36,3 @@ find build/distributions -name '*linux-amd64.docker.tar.gz*' -print0 |
     echo "Rename file $file"
     mv "$file" "${file/linux-amd64.docker.tar.gz/docker-image-linux-amd64.tar.gz}"
   done
-
-# ensure the latest image has been pulled
-IMAGE=docker.elastic.co/infra/release-manager:latest
-(retry 3 docker pull --quiet "${IMAGE}") || echo "Error pulling ${IMAGE} Docker image, we continue"
-docker images --filter=reference=$IMAGE
-
-set -x
-
-# Generate checksum files and upload to GCS
-docker run --rm \
-  --name beats \
-  -e VAULT_ADDR \
-  -e VAULT_ROLE_ID \
-  -e VAULT_SECRET_ID \
-  --mount type=bind,readonly=false,src="$PWD",target=/artifacts \
-  "$IMAGE" \
-    cli collect \
-      --project beats \
-      --branch "$BRANCH_NAME" \
-      --commit "$(git rev-parse HEAD)" \
-      --workflow "snapshot" \
-      --artifact-set main \
-      --version "${VERSION}"
