4cb141b74332b403953816f0626ad9cd4eea2772
Noémi Ványi <sitbackandwait@gmail.com>
2022-04-11 14:38:46 +0200

    [elastic-agent-system-metrics] Add check target to magefile

--- magefile.go
+++ magefile.go
@@ -21,13 +21,21 @@
 package main
 
 import (
-	//mage:import
+	// mage:import
 	"github.com/elastic/elastic-agent-libs/dev-tools/mage"
+	"github.com/magefile/mage/mg"
 )
 
 // Aliases are shortcuts to long target names.
-//nolint: deadcode // it's used by `mage`.
+// nolint: deadcode // it's used by `mage`.
 var Aliases = map[string]interface{}{
 	"llc":  mage.Linter.LastChange,
 	"lint": mage.Linter.All,
+}
+
+// Check runs all the checks
+// nolint: deadcode,unparam // it's used as a `mage` target and requires returning an error
+func Check() error {
+	mg.Deps(mage.Deps.CheckModuleTidy)
+	return nil
 }
