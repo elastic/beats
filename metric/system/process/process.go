1903c3ae1571e62f32aab42b1752fb2fe6862ee2
Alex Kristiansen <alex.kristiansen@elastic.co>
2022-04-11 11:53:10 -0700

    [elastic-agent-system-metrics] fix bug in process cgroups disabling

--- metric/system/process/process.go
+++ metric/system/process/process.go
@@ -183,6 +183,7 @@ func (procStats *Stats) Init() error {
 		cgReader, err := cgroup.NewReaderOptions(procStats.CgroupOpts)
 		if errors.Is(err, cgroup.ErrCgroupsMissing) {
 			logp.Warn("cgroup data collection will be disabled: %v", err)
+			procStats.EnableCgroups = false
 		} else if err != nil {
 			return fmt.Errorf("error initializing cgroup reader: %w", err)
 		}
