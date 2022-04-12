75fbb6d9a4d218c07da618acd2a5bb0b35518ef7
Tiago Queiroz <tiago.queiroz@elastic.co>
2022-04-12 12:43:16 +0200

    libbeat: log debug for `proxy_url` and fixed docs (#31130)
    Fixes the documentation regarding Elasticsearch output `proxy_url`
    configuration format.
    
    Debug logs are added with the breakdown of `proxy_url`.
    
    Closes #29249

--- libbeat/esleg/eslegclient/connection.go
+++ libbeat/esleg/eslegclient/connection.go
@@ -177,6 +177,7 @@ func NewClients(cfg *common.Config, beatname string) ([]Connection, error) {
 	}
 
 	if proxyURL := config.Transport.Proxy.URL; proxyURL != nil {
+		logp.Debug("breaking down proxy URL. Scheme: '%s', host[:port]: '%s', path: '%s'", proxyURL.Scheme, proxyURL.Host, proxyURL.Path)
 		logp.Info("using proxy URL: %s", proxyURL.URI().String())
 	}
 
