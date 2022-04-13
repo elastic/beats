73aecb09ee87fea5fa592a951e3d20f63f4af2d0
Denis Rechkunov <denis.rechkunov@elastic.co>
2022-04-13 12:05:51 +0200

    Add new `shipper` output type
    This will allow us to start experimenting with the new shipper architecture.

---
+++ libbeat/outputs/shipper/config.go
@@ -0,0 +1,78 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package shipper
+
+import (
+	"time"
+
+	"github.com/elastic/beats/v7/libbeat/common/transport/tlscommon"
+
+	"google.golang.org/grpc/backoff"
+)
+
+type Backoff struct {
+	// BaseDelay is the amount of time to backoff after the first failure.
+	BaseDelay time.Duration `config:"base_delay"`
+	// Multiplier is the factor with which to multiply backoffs after a
+	// failed retry. Should ideally be greater than 1.
+	Multiplier float64 `config:"multiplier"`
+	// Jitter is the factor with which backoffs are randomized.
+	Jitter float64 `config:"jitter"`
+	// MaxDelay is the upper bound of backoff delay.
+	MaxDelay time.Duration `config:"max_delay"`
+}
+
+func (b Backoff) ToGRPCBackOff() backoff.Config {
+	return backoff.Config{
+		BaseDelay:  b.BaseDelay,
+		Multiplier: b.Multiplier,
+		Jitter:     b.Jitter,
+		MaxDelay:   b.MaxDelay,
+	}
+}
+func FromGRPCBackOff(b backoff.Config) Backoff {
+	return Backoff{
+		BaseDelay:  b.BaseDelay,
+		Multiplier: b.Multiplier,
+		Jitter:     b.Jitter,
+		MaxDelay:   b.MaxDelay,
+	}
+}
+
+type Config struct {
+	// Server address in the format of host:port, e.g. `localhost:50051`
+	Server string `config:"server"`
+	// TLS/SSL configurationf or secure connection
+	TLS *tlscommon.Config `config:"ssl"`
+	// Timeout of a single batch publishing request
+	Timeout time.Duration `config:"timeout"             validate:"min=1"`
+	// MaxRetries is how many times the same batch is attempted to be sent
+	MaxRetries int `config:"max_retries"         validate:"min=-1,nonzero"`
+	// BulkMaxSize max amount of events in a single batch
+	BulkMaxSize int `config:"bulk_max_size"`
+	// Backoff strategy configuration
+	Backoff Backoff `config:"backoff"`
+}
+
+var defaultConfig = Config{
+	TLS:         nil,
+	Timeout:     30 * time.Second,
+	MaxRetries:  3,
+	BulkMaxSize: 50,
+	Backoff:     FromGRPCBackOff(backoff.DefaultConfig),
+}
---
+++ libbeat/outputs/shipper/shipper.go
@@ -0,0 +1,211 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package shipper
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/elastic/beats/v7/libbeat/beat"
+	"github.com/elastic/beats/v7/libbeat/common"
+	"github.com/elastic/beats/v7/libbeat/common/transport/tlscommon"
+	"github.com/elastic/beats/v7/libbeat/logp"
+	"github.com/elastic/beats/v7/libbeat/outputs"
+	"github.com/elastic/beats/v7/libbeat/publisher"
+	sc "github.com/elastic/elastic-agent-shipper/api"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/backoff"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/structpb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+type shipper struct {
+	log      *logp.Logger
+	observer outputs.Observer
+	conn     *grpc.ClientConn
+	client   sc.ProducerClient
+	timeout  time.Duration
+}
+
+func init() {
+	outputs.RegisterType("shipper", makeShipper)
+}
+
+func makeShipper(
+	_ outputs.IndexManager,
+	beat beat.Info,
+	observer outputs.Observer,
+	cfg *common.Config,
+) (outputs.Group, error) {
+
+	config := defaultConfig
+	err := cfg.Unpack(&config)
+	if err != nil {
+		return outputs.Fail(err)
+	}
+
+	tls, err := tlscommon.LoadTLSConfig(config.TLS)
+	if err != nil {
+		return outputs.Fail(fmt.Errorf("invalid shipper TLS configuration: %w", err))
+	}
+
+	var creds credentials.TransportCredentials
+	if config.TLS != nil && config.TLS.Enabled != nil && *config.TLS.Enabled {
+		creds = credentials.NewTLS(tls.ToConfig())
+	} else {
+		creds = insecure.NewCredentials()
+	}
+
+	opts := []grpc.DialOption{
+		grpc.WithConnectParams(grpc.ConnectParams{
+			Backoff: backoff.Config(config.Backoff),
+		}),
+		grpc.WithTransportCredentials(creds),
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), config.Timeout)
+	defer cancel()
+	conn, err := grpc.DialContext(ctx, config.Server, opts...)
+	if err != nil {
+		return outputs.Fail(fmt.Errorf("shipper connection failed with: %w", err))
+	}
+
+	s := &shipper{
+		log:      logp.NewLogger("shipper"),
+		observer: observer,
+		conn:     conn,
+		client:   sc.NewProducerClient(conn),
+		timeout:  config.Timeout,
+	}
+
+	return outputs.Success(config.BulkMaxSize, config.MaxRetries, s)
+}
+
+//nolint: godox // this implementation is not finished
+func (c *shipper) Publish(ctx context.Context, batch publisher.Batch) error {
+	st := c.observer
+	events := batch.Events()
+	st.NewBatch(len(events))
+
+	dropped := 0
+
+	grpcEvents := make([]*sc.Event, 0, len(events))
+
+	for i, e := range events {
+
+		meta, err := convertMapStr(e.Content.Meta)
+		if err != nil {
+			// conversion errors are not recoverable, so we have to drop the event completely
+			c.log.Errorf("%d/%d failed to convert event metadata to protobuf: %w", i+1, len(events), err)
+			dropped++
+			continue
+		}
+
+		fields, err := convertMapStr(e.Content.Fields)
+		if err != nil {
+			c.log.Errorf("%d/%d failed to convert event fields to protobuf: %w", i+1, len(events), err)
+			dropped++
+			continue
+		}
+
+		grpcEvents = append(grpcEvents, &sc.Event{
+			Timestamp: timestamppb.New(e.Content.Timestamp),
+			Metadata:  meta.GetStructValue(),
+			Fields:    fields.GetStructValue(),
+			// TODO this contains temporary values, since they are required and not available from the event at the moment
+			Input: &sc.Input{
+				Id:   "beats",
+				Name: "beats",
+				Type: "beats",
+			},
+			// TODO this contains temporary values, since they are required and not propagated at the moment
+			DataStream: &sc.DataStream{
+				// Id:        "none", // not generated at the moment
+				Type:      "shipper.output",
+				Dataset:   "generic",
+				Namespace: "default",
+			},
+		})
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, c.timeout)
+	defer cancel()
+	_, err := c.client.PublishEvents(ctx, &sc.PublishRequest{
+		Events: grpcEvents,
+	})
+
+	if err != nil {
+		if status.Code(err) == codes.ResourceExhausted {
+			batch.Cancelled()
+		} else {
+			batch.Retry()
+		}
+		return fmt.Errorf("failed to publish the batch to the shipper: %w", err)
+	}
+
+	batch.ACK()
+
+	st.Dropped(dropped)
+	st.Acked(len(events) - dropped)
+
+	return nil
+}
+
+func (c *shipper) Close() error {
+	return c.conn.Close()
+}
+
+func (c *shipper) String() string {
+	return "shipper"
+}
+
+func convertMapStr(m common.MapStr) (*structpb.Value, error) {
+	if m == nil {
+		return structpb.NewNullValue(), nil
+	}
+
+	fields := make(map[string]*structpb.Value, len(m))
+
+	for key, value := range m {
+		var (
+			protoValue *structpb.Value
+			err        error
+		)
+		switch v := value.(type) {
+		case common.MapStr:
+			protoValue, err = convertMapStr(v)
+		default:
+			protoValue, err = structpb.NewValue(v)
+		}
+		if err != nil {
+			return nil, err
+		}
+		fields[key] = protoValue
+	}
+
+	s := &structpb.Struct{
+		Fields: fields,
+	}
+
+	return structpb.NewStructValue(s), nil
+}
