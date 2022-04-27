d5fe415c845b5b3c89f1674aa2e2dd0d1ce6a6ae
Lucas F. da Costa <lucas.costa@elastic.co>
2022-04-27 17:28:57 +0100

    Add heartbeat telemetry logs (step count and duration) (#31405)
    * Uptime: add logging for monitor duration and step counts
    
    * fix changed files linting errors
    
    * add monitor ID to heartbeat logging
    
    * use general logger for run events
    
    * fix Vignesh suggested changes

--- heartbeat/monitors/mocks_test.go
+++ heartbeat/monitors/mocks_test.go
@@ -53,9 +53,7 @@ func (c *MockBeatClient) PublishAll(events []beat.Event) {
 	c.mtx.Lock()
 	defer c.mtx.Unlock()
 
-	for _, e := range events {
-		c.publishes = append(c.publishes, e)
-	}
+	c.publishes = append(c.publishes, events...)
 }
 
 func (c *MockBeatClient) Close() error {
@@ -111,7 +109,7 @@ func baseMockEventMonitorValidator(id string, name string, status string) valida
 			"id":          idMatcher,
 			"name":        name,
 			"type":        "test",
-			"duration.us": isdef.IsDuration,
+			"duration.us": hbtestllext.IsInt64,
 			"status":      status,
 			"check_group": isdef.IsString,
 		},
@@ -131,6 +129,8 @@ func mockEventCustomFields() map[string]interface{} {
 	return common.MapStr{"foo": "bar"}
 }
 
+//nolint:unparam // There are no new changes to this line but
+// linter has been activated in the meantime. We'll cleanup separately.
 func createMockJob() ([]jobs.Job, error) {
 	j := jobs.MakeSimpleJob(func(event *beat.Event) error {
 		eventext.MergeEventFields(event, mockEventCustomFields())
@@ -178,6 +178,8 @@ func mockPluginBuilder() (plugin.PluginFactory, *atomic.Int, *atomic.Int) {
 func mockPluginsReg() (p *plugin.PluginsReg, built *atomic.Int, closed *atomic.Int) {
 	reg := plugin.NewPluginsReg()
 	builder, built, closed := mockPluginBuilder()
+	//nolint:errcheck // There are no new changes to this line but
+	// linter has been activated in the meantime. We'll cleanup separately.
 	reg.Add(builder)
 	return reg, built, closed
 }
@@ -203,6 +205,8 @@ func mockPluginConf(t *testing.T, id string, name string, schedule string, url s
 
 // mockBadPluginConf returns a conf with an invalid plugin config.
 // This should fail after the generic plugin checks fail since the HTTP plugin requires 'urls' to be set.
+//nolint:unparam // There are no new changes to this line but
+// linter has been activated in the meantime. We'll cleanup separately.
 func mockBadPluginConf(t *testing.T, id string, schedule string) *common.Config {
 	confMap := map[string]interface{}{
 		"type":        "test",
