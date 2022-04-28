87949288faf8450b35a12ae946050444d4272c18
Taylor Swanson <90622908+taylor-swanson@users.noreply.github.com>
2022-04-28 07:55:12 -0500

    [libbeat] Store syslog version as string  (#31446)
    - ECS expects the log.syslog.version to be a string (keyword), not a number
    - Fix linter issues with mustParseTime test functions

--- libbeat/reader/syslog/message_test.go
+++ libbeat/reader/syslog/message_test.go
@@ -26,22 +26,28 @@ import (
 	"github.com/elastic/elastic-agent-libs/mapstr"
 )
 
-func mustParseTime(layout string, value string) time.Time {
-	t, err := time.Parse(layout, value)
+// mustParseTime will parse value into a time.Time using the provided layout. If value
+// cannot be parsed, this function will panic. If layout does not specify a time zone,
+// then a time.Location should be provided by loc. If layout does specify a time zone,
+// then loc should be nil. Layouts that do not specify a year will be enriched with
+// the current year relative to the location specified for the parsed timestamp.
+func mustParseTime(layout, value string, loc *time.Location) time.Time {
+	var t time.Time
+	var err error
+
+	if loc != nil {
+		t, err = time.ParseInLocation(layout, value, loc)
+	} else {
+		t, err = time.Parse(layout, value)
+	}
 	if err != nil {
 		panic(err)
 	}
 
-	return t
-}
-
-func mustParseTimeLoc(layout string, value string, loc *time.Location) time.Time {
-	t, err := time.ParseInLocation(layout, value, loc)
-	if err != nil {
-		panic(err)
-	}
-	if layout == time.Stamp {
-		t = t.AddDate(time.Now().Year(), 0, 0)
+	// Timestamps that do not include a year will be enriched using the
+	// current year relative to the location specified for the timestamp.
+	if t.Year() == 0 {
+		t = t.AddDate(time.Now().In(t.Location()).Year(), 0, 0)
 	}
 
 	return t
@@ -56,12 +62,12 @@ func TestMessage_SetTimestampBSD(t *testing.T) {
 		"bsd-timestamp": {
 			In:    "Oct 1 22:04:15",
 			InLoc: time.Local,
-			Want:  mustParseTimeLoc(time.Stamp, "Oct 1 22:04:15", time.Local),
+			Want:  mustParseTime(time.Stamp, "Oct 1 22:04:15", time.Local),
 		},
 		"loc-nil": {
 			In:    "Oct 1 22:04:15",
 			InLoc: nil,
-			Want:  mustParseTimeLoc(time.Stamp, "Oct 1 22:04:15", time.Local),
+			Want:  mustParseTime(time.Stamp, "Oct 1 22:04:15", time.Local),
 		},
 		"invalid-timestamp-1": {
 			In:    "1985-04-12T23:20:50.52Z",
@@ -96,23 +102,23 @@ func TestMessage_SetTimestampRFC3339(t *testing.T) {
 	}{
 		"rfc3339-timestamp": {
 			In:   "1985-04-12T23:20:50.52Z",
-			Want: mustParseTime(time.RFC3339Nano, "1985-04-12T23:20:50.52Z"),
+			Want: mustParseTime(time.RFC3339Nano, "1985-04-12T23:20:50.52Z", nil),
 		},
 		"rfc3339-timestamp-with-tz": {
 			In:   "1985-04-12T19:20:50.52-04:00",
-			Want: mustParseTime(time.RFC3339Nano, "1985-04-12T19:20:50.52-04:00"),
+			Want: mustParseTime(time.RFC3339Nano, "1985-04-12T19:20:50.52-04:00", nil),
 		},
 		"rfc3339-timestamp-with-milliseconds": {
 			In:   "2003-10-11T22:14:15.123Z",
-			Want: mustParseTime(time.RFC3339Nano, "2003-10-11T22:14:15.123Z"),
+			Want: mustParseTime(time.RFC3339Nano, "2003-10-11T22:14:15.123Z", nil),
 		},
 		"rfc3339-timestamp-with-microseconds": {
 			In:   "2003-10-11T22:14:15.123456Z",
-			Want: mustParseTime(time.RFC3339Nano, "2003-10-11T22:14:15.123456Z"),
+			Want: mustParseTime(time.RFC3339Nano, "2003-10-11T22:14:15.123456Z", nil),
 		},
 		"rfc3339-timestamp-with-microseconds-with-tz": {
 			In:   "2003-10-11T22:14:15.123456-06:00",
-			Want: mustParseTime(time.RFC3339Nano, "2003-10-11T22:14:15.123456-06:00"),
+			Want: mustParseTime(time.RFC3339Nano, "2003-10-11T22:14:15.123456-06:00", nil),
 		},
 		"invalid-timestamp-1": {
 			In: "Oct 1 22:04:15",
@@ -467,7 +473,7 @@ func TestMessage_Fields(t *testing.T) {
 	}{
 		"valid": {
 			In: &message{
-				timestamp: mustParseTime(time.RFC3339Nano, "2003-10-11T22:14:15.123456-06:00"),
+				timestamp: mustParseTime(time.RFC3339Nano, "2003-10-11T22:14:15.123456-06:00", nil),
 				facility:  1,
 				severity:  5,
 				priority:  13,
@@ -499,7 +505,7 @@ func TestMessage_Fields(t *testing.T) {
 						"appname":  "su",
 						"procid":   "1024",
 						"msgid":    "msg123",
-						"version":  1,
+						"version":  "1",
 						"structured_data": map[string]map[string]string{
 							"a": {
 								"b": "c",
