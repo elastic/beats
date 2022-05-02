7ad570368bd84c250782501e895a837ef56d2542
yug-elastic <96041884+yug-elastic@users.noreply.github.com>
2022-05-02 23:36:42 +0530

    [Filebeat][CometD] Add CometD input for Salesforce connector (#30089)
    * Add authentication mechanism for cometd input for Salesforce connector
    * Add data collection mechanism for Salesforce cometd input
    * Add asciidoc for cometd input
    
    Co-authored-by: Kush Rana <kush.rana@elastic.co>
    Co-authored-by: yug-crest <yug.rajani@crestdatasys.com>
    Co-authored-by: Craig MacKenzie <craig.mackenzie@elastic.co>
    Co-authored-by: Kush Rana <89848966+kush-elastic@users.noreply.github.com>

---
+++ x-pack/filebeat/input/cometd/input.go
@@ -0,0 +1,200 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package cometd
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/elastic/beats/v7/filebeat/channel"
+	"github.com/elastic/beats/v7/filebeat/input"
+	"github.com/elastic/beats/v7/libbeat/beat"
+	"github.com/elastic/beats/v7/libbeat/logp"
+	"github.com/elastic/elastic-agent-libs/mapstr"
+
+	bay "github.com/elastic/bayeux"
+	conf "github.com/elastic/elastic-agent-libs/config"
+)
+
+const (
+	inputName = "cometd"
+)
+
+// Run starts the input worker then returns. Only the first invocation
+// will ever start the worker.
+func (in *cometdInput) Run() {
+	var err error
+	in.workerOnce.Do(func() {
+		in.workerWg.Add(1)
+		go func() {
+			in.log.Info("Input worker has started.")
+			defer in.log.Info("Input worker has stopped.")
+			defer in.workerWg.Done()
+			defer in.workerCancel()
+			in.b = bay.Bayeux{}
+			in.creds, err = bay.GetSalesforceCredentials(in.authParams)
+			if err != nil {
+				in.log.Errorw("not able to get access token", "error", err)
+				return
+			}
+			if err := in.run(); err != nil {
+				in.log.Error(err)
+				return
+			}
+		}()
+	})
+}
+
+func (in *cometdInput) run() error {
+	in.msgCh = in.b.Channel(in.workerCtx, in.msgCh, "-1", *in.creds, in.config.ChannelName)
+	for e := range in.msgCh {
+		if e.Failed() {
+			return fmt.Errorf("error collecting events: %w", e.Err)
+		}
+		if !e.Msg.Successful {
+			var event event
+			// To handle the last response where the object received was empty
+			if e.Msg.Data.Payload == nil {
+				return nil
+			}
+
+			// Convert json.RawMessage response to []byte
+			msg, err := e.Msg.Data.Payload.MarshalJSON()
+			if err != nil {
+				return fmt.Errorf("JSON error: %w", err)
+			}
+
+			// Extract event IDs from json.RawMessage
+			err = json.Unmarshal(e.Msg.Data.Payload, &event)
+			if err != nil {
+				return fmt.Errorf("error while parsing JSON: %w", err)
+			}
+			if ok := in.outlet.OnEvent(makeEvent(event.EventId, e.Msg.Channel, string(msg))); !ok {
+				in.log.Debug("OnEvent returned false. Stopping input worker.")
+				return fmt.Errorf("error ingesting data to elasticsearch")
+			}
+		}
+	}
+	return nil
+}
+
+func init() {
+	err := input.Register(inputName, NewInput)
+	if err != nil {
+		panic(fmt.Errorf("failed to register %v input: %w", inputName, err))
+	}
+}
+
+// NewInput creates a new CometD input that consumes events from
+// a topic subscription.
+func NewInput(
+	cfg *conf.C,
+	connector channel.Connector,
+	inputContext input.Context,
+) (inp input.Input, err error) {
+	// Extract and validate the input's configuration.
+	conf := defaultConfig()
+	if err = cfg.Unpack(&conf); err != nil {
+		return nil, err
+	}
+
+	var authParams bay.AuthenticationParameters
+	authParams.ClientID = conf.Auth.OAuth2.ClientID
+	authParams.ClientSecret = conf.Auth.OAuth2.ClientSecret
+	authParams.Username = conf.Auth.OAuth2.User
+	authParams.Password = conf.Auth.OAuth2.Password
+	authParams.TokenURL = conf.Auth.OAuth2.TokenURL
+
+	logger := logp.NewLogger(inputName).With(
+		"pubsub_channel", conf.ChannelName)
+
+	// Wrap input.Context's Done channel with a context.Context. This goroutine
+	// stops with the parent closes the Done channel.
+	inputCtx, cancelInputCtx := context.WithCancel(context.Background())
+	go func() {
+		<-inputContext.Done
+		cancelInputCtx()
+	}()
+
+	// If the input ever needs to be made restartable, then context would need
+	// to be recreated with each restart.
+	workerCtx, workerCancel := context.WithCancel(inputCtx)
+
+	in := &cometdInput{
+		config:       conf,
+		log:          logger,
+		inputCtx:     inputCtx,
+		workerCtx:    workerCtx,
+		workerCancel: workerCancel,
+		authParams:   authParams,
+	}
+
+	// Creating a new channel for cometd input.
+	in.msgCh = make(chan bay.MaybeMsg, 1)
+
+	// Build outlet for events.
+	in.outlet, err = connector.Connect(cfg)
+	if err != nil {
+		return nil, err
+	}
+	in.log.Infof("Initialized %s input.", inputName)
+	return in, nil
+}
+
+// Stop stops the input and waits for it to fully stop.
+func (in *cometdInput) Stop() {
+	in.workerCancel()
+	in.workerWg.Wait()
+}
+
+// Wait is an alias for Stop.
+func (in *cometdInput) Wait() {
+	in.Stop()
+}
+
+type cometdInput struct {
+	config
+
+	log      *logp.Logger
+	outlet   channel.Outleter // Output of received messages.
+	inputCtx context.Context  // Wraps the Done channel from parent input.Context.
+
+	workerCtx    context.Context    // Worker goroutine context. It's cancelled when the input stops or the worker exits.
+	workerCancel context.CancelFunc // Used to signal that the worker should stop.
+	workerOnce   sync.Once          // Guarantees that the worker goroutine is only started once.
+	workerWg     sync.WaitGroup     // Waits on worker goroutine.
+
+	msgCh      chan bay.MaybeMsg
+	b          bay.Bayeux
+	creds      *bay.Credentials
+	authParams bay.AuthenticationParameters
+}
+
+type event struct {
+	EventId string `json:"EventIdentifier"`
+}
+
+func makeEvent(id string, channel string, body string) beat.Event {
+	e := beat.Event{
+		Timestamp: time.Now().UTC(),
+		Fields: mapstr.M{
+			"event": mapstr.M{
+				"id":      id,
+				"created": time.Now().UTC(),
+			},
+			"message": body,
+			"cometd": mapstr.M{
+				"channel_name": channel,
+			},
+		},
+		Private: body,
+	}
+	e.SetID(id)
+
+	return e
+}
---
+++ x-pack/filebeat/input/cometd/input_test.go
@@ -0,0 +1,472 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package cometd
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	bay "github.com/elastic/bayeux"
+	finput "github.com/elastic/beats/v7/filebeat/input"
+	"github.com/elastic/beats/v7/filebeat/input/inputtest"
+	"github.com/elastic/beats/v7/libbeat/beat"
+	"github.com/elastic/beats/v7/libbeat/logp"
+	conf "github.com/elastic/elastic-agent-libs/config"
+	"github.com/elastic/elastic-agent-libs/mapstr"
+)
+
+const (
+	firstChannel  = "channel_name1"
+	secondChannel = "channel_name2"
+)
+
+var (
+	serverURL string
+	called1   uint64
+	called2   uint64
+	clientId  uint64
+)
+
+func TestInputDone(t *testing.T) {
+	config := mapstr.M{
+		"channel_name":              firstChannel,
+		"auth.oauth2.client.id":     "DEMOCLIENTID",
+		"auth.oauth2.client.secret": "DEMOCLIENTSECRET",
+		"auth.oauth2.user":          "salesforce_user",
+		"auth.oauth2.password":      "pwd",
+		"auth.oauth2.token_url":     "https://example.com/token",
+	}
+	inputtest.AssertNotStartedInputCanBeDone(t, NewInput, &config)
+}
+
+func TestMakeEventFailure(t *testing.T) {
+	event := beat.Event{
+		Timestamp: time.Now().UTC(),
+		Fields: mapstr.M{
+			"event": mapstr.M{
+				"id":      "DEMOID",
+				"created": time.Now().UTC(),
+			},
+			"message": "DEMOBODYFAIL",
+			"cometd": mapstr.M{
+				"channel_name": "DEMOCHANNEL",
+			},
+		},
+		Private: "DEMOBODYFAIL",
+	}
+	assert.NotEqual(t, event, makeEvent("DEMOCHANNEL", "DEMOID", "DEMOBODY"))
+}
+
+func TestSingleInput(t *testing.T) {
+	defer atomic.StoreUint64(&called1, 0)
+	defer atomic.StoreUint64(&called2, 0)
+	defer atomic.StoreUint64(&clientId, 0)
+	eventsCh := make(chan beat.Event)
+	defer close(eventsCh)
+
+	outlet := &mockedOutleter{
+		onEventHandler: func(event beat.Event) bool {
+			eventsCh <- event
+			return true
+		},
+	}
+	connector := &mockedConnector{
+		outlet: outlet,
+	}
+	var inputContext finput.Context
+
+	var expected bay.MaybeMsg
+	expected.Msg.Data.Event.ReplayID = 1234
+	expected.Msg.Data.Payload = []byte(`{"CountryIso": "IN"}`)
+	expected.Msg.Channel = firstChannel
+
+	config := map[string]interface{}{
+		"channel_name":              firstChannel,
+		"auth.oauth2.client.id":     "client.id",
+		"auth.oauth2.client.secret": "client.secret",
+		"auth.oauth2.user":          "user",
+		"auth.oauth2.password":      "password",
+	}
+
+	r := http.HandlerFunc(oauth2Handler)
+	server := httptest.NewServer(r)
+	defer server.Close()
+
+	serverURL = server.URL
+	config["auth.oauth2.token_url"] = server.URL + "/token"
+
+	cfg := conf.MustNewConfigFrom(config)
+
+	input, err := NewInput(cfg, connector, inputContext)
+	require.NoError(t, err)
+	require.NotNil(t, input)
+
+	input.Run()
+
+	event := <-eventsCh
+	assertEventMatches(t, expected, event)
+	input.Stop()
+}
+
+func TestInputStop_Wait(t *testing.T) {
+	defer atomic.StoreUint64(&called1, 0)
+	defer atomic.StoreUint64(&called2, 0)
+	defer atomic.StoreUint64(&clientId, 0)
+	eventsCh := make(chan beat.Event)
+	defer close(eventsCh)
+
+	const numMessages = 1
+
+	var eventProcessing sync.WaitGroup
+	eventProcessing.Add(numMessages)
+
+	outlet := &mockedOutleter{
+		onEventHandler: func(event beat.Event) bool {
+			eventProcessing.Done()
+			eventsCh <- event
+			return true
+		},
+	}
+	connector := &mockedConnector{
+		outlet: outlet,
+	}
+	var inputContext finput.Context
+
+	var expected bay.MaybeMsg
+	expected.Msg.Data.Event.ReplayID = 1234
+	expected.Msg.Data.Payload = []byte(`{"CountryIso": "IN"}`)
+	expected.Msg.Channel = firstChannel
+
+	config := map[string]interface{}{
+		"channel_name":              firstChannel,
+		"auth.oauth2.client.id":     "client.id",
+		"auth.oauth2.client.secret": "client.secret",
+		"auth.oauth2.user":          "user",
+		"auth.oauth2.password":      "password",
+	}
+
+	r := http.HandlerFunc(oauth2Handler)
+	server := httptest.NewServer(r)
+	defer server.Close()
+
+	serverURL = server.URL
+	config["auth.oauth2.token_url"] = server.URL + "/token"
+
+	cfg := conf.MustNewConfigFrom(config)
+
+	input, err := NewInput(cfg, connector, inputContext)
+	require.NoError(t, err)
+	require.NotNil(t, input)
+
+	require.Equal(t, 0, bay.GetConnectedCount())
+	input.Run()
+	eventProcessing.Wait()
+	require.Equal(t, 1, bay.GetConnectedCount())
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		require.Equal(t, 1, bay.GetConnectedCount()) // current open channels count should be 1
+		event := <-eventsCh
+		assertEventMatches(t, expected, event) // wait for single event
+		wg.Done()
+		time.Sleep(100 * time.Millisecond)           // let input.Stop() be executed.
+		require.Equal(t, 0, bay.GetConnectedCount()) // current open channels count should be 0
+	}()
+
+	time.Sleep(100 * time.Millisecond) // let input.Stop() be executed.
+
+	wg.Wait()
+	input.Wait()
+}
+
+func TestMultiInput(t *testing.T) {
+	defer atomic.StoreUint64(&called1, 0)
+	defer atomic.StoreUint64(&called2, 0)
+	defer atomic.StoreUint64(&clientId, 0)
+	eventsCh := make(chan beat.Event)
+	defer close(eventsCh)
+
+	const numMessages = 2
+
+	var eventProcessing sync.WaitGroup
+	eventProcessing.Add(numMessages)
+
+	outlet := &mockedOutleter{
+		onEventHandler: func(event beat.Event) bool {
+			eventProcessing.Done()
+			eventsCh <- event
+			return true
+		},
+	}
+	connector := &mockedConnector{
+		outlet: outlet,
+	}
+
+	var expected1 bay.MaybeMsg
+	expected1.Msg.Data.Event.ReplayID = 1234
+	expected1.Msg.Data.Payload = []byte(`{"CountryIso": "IN"}`)
+	expected1.Msg.Channel = firstChannel
+
+	var expected2 bay.MaybeMsg
+	expected2.Msg.Data.Event.ReplayID = 1234
+	expected2.Msg.Data.Payload = []byte(`{"CountryIso": "US"}`)
+	expected2.Msg.Channel = secondChannel
+
+	config1 := map[string]interface{}{
+		"channel_name":              firstChannel,
+		"auth.oauth2.client.id":     "client.id",
+		"auth.oauth2.client.secret": "client.secret",
+		"auth.oauth2.user":          "user",
+		"auth.oauth2.password":      "password",
+	}
+	config2 := map[string]interface{}{
+		"channel_name":              secondChannel,
+		"auth.oauth2.client.id":     "client.id",
+		"auth.oauth2.client.secret": "client.secret",
+		"auth.oauth2.user":          "user",
+		"auth.oauth2.password":      "password",
+	}
+
+	// create Server
+	r := http.HandlerFunc(oauth2Handler)
+	server := httptest.NewServer(r)
+	defer server.Close()
+	serverURL = server.URL
+	config1["auth.oauth2.token_url"] = serverURL + "/token"
+	config2["auth.oauth2.token_url"] = serverURL + "/token"
+
+	// get common config
+	cfg1 := conf.MustNewConfigFrom(config1)
+	cfg2 := conf.MustNewConfigFrom(config2)
+
+	var inputContext finput.Context
+
+	// initialize inputs
+	input1, err := NewInput(cfg1, connector, inputContext)
+	require.NoError(t, err)
+	require.NotNil(t, input1)
+
+	input2, err := NewInput(cfg2, connector, inputContext)
+	require.NoError(t, err)
+	require.NotNil(t, input2)
+
+	require.Equal(t, 0, bay.GetConnectedCount())
+	// run input
+	input1.Run()
+	defer input1.Stop()
+
+	// run input
+	input2.Run()
+	defer input2.Stop()
+
+	eventProcessing.Wait()
+
+	for _, event := range []beat.Event{<-eventsCh, <-eventsCh} {
+		channel, err := event.GetValue("cometd.channel_name")
+		require.NoError(t, err)
+
+		if channel == "channel_name1" {
+			assertEventMatches(t, expected1, event)
+		} else {
+			assertEventMatches(t, expected2, event)
+		}
+	}
+}
+
+func TestStop(t *testing.T) {
+	conf := defaultConfig()
+	logger := logp.NewLogger("test")
+	authParams := bay.AuthenticationParameters{}
+	inputCtx, cancelInputCtx := context.WithCancel(context.Background())
+	workerCtx, workerCancel := context.WithCancel(inputCtx)
+	defer cancelInputCtx()
+	input := &cometdInput{
+		config:       conf,
+		log:          logger,
+		inputCtx:     inputCtx,
+		workerCtx:    workerCtx,
+		workerCancel: workerCancel,
+		authParams:   authParams,
+	}
+	input.msgCh = make(chan bay.MaybeMsg)
+
+	input.Stop()
+	select {
+	case <-workerCtx.Done():
+	case <-time.After(time.Second): // let input.Stop() be executed.
+		require.NoError(t, fmt.Errorf("input is not stopped."))
+	}
+}
+
+func TestWait(t *testing.T) {
+	conf := defaultConfig()
+	logger := logp.NewLogger("test")
+	authParams := bay.AuthenticationParameters{}
+	inputCtx, cancelInputCtx := context.WithCancel(context.Background())
+	workerCtx, workerCancel := context.WithCancel(inputCtx)
+	defer cancelInputCtx()
+
+	input := &cometdInput{
+		config:       conf,
+		log:          logger,
+		inputCtx:     inputCtx,
+		workerCtx:    workerCtx,
+		workerCancel: workerCancel,
+		authParams:   authParams,
+	}
+	input.msgCh = make(chan bay.MaybeMsg)
+
+	go func() {
+		input.Wait()
+	}()
+
+	select {
+	case <-workerCtx.Done():
+	case <-time.After(time.Second): // let input.Stop() be executed.
+		require.NoError(t, fmt.Errorf("input is not stopped."))
+	}
+}
+
+func oauth2TokenHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	_ = r.ParseForm()
+	switch {
+	case r.Method != http.MethodPost:
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte(`{"error":"wrong method"}`))
+	default:
+		response := `{"instance_url": "` + serverURL + `", "expires_in": "60", "access_token": "abcd"}`
+		_, _ = w.Write([]byte(response))
+	}
+}
+
+func oauth2ClientIdHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	_ = r.ParseForm()
+	switch {
+	case r.Method != http.MethodPost:
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte(`{"error":"wrong method"}`))
+	default:
+		switch clientId {
+		case 0:
+			atomic.StoreUint64(&clientId, 1)
+			_, _ = w.Write([]byte(`[{"ext":{"replay":true,"payload.format":true},"minimumVersion":"1.0","clientId":"client_id1","supportedConnectionTypes":["long-polling"],"channel":"/meta/handshake","version":"1.0","successful":true}]`))
+		case 1:
+			atomic.StoreUint64(&clientId, 0)
+			_, _ = w.Write([]byte(`[{"ext":{"replay":true,"payload.format":true},"minimumVersion":"1.0","clientId":"client_id2","supportedConnectionTypes":["long-polling"],"channel":"/meta/handshake","version":"1.0","successful":true}]`))
+		default:
+		}
+	}
+}
+
+func oauth2SubscribeHandlerChannel1(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	_ = r.ParseForm()
+	switch {
+	case r.Method != http.MethodPost:
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte(`{"error":"wrong method"}`))
+	default:
+		_, _ = w.Write([]byte(`[{"clientId": "client_id1", "channel": "/meta/subscribe", "subscription": "channel_name1", "successful":true}]`))
+	}
+}
+
+func oauth2SubscribeHandlerChannel2(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	_ = r.ParseForm()
+	switch {
+	case r.Method != http.MethodPost:
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte(`{"error":"wrong method"}`))
+	default:
+		_, _ = w.Write([]byte(`[{"clientId": "client_id2", "channel": "/meta/subscribe", "subscription": "channel_name2", "successful":true}]`))
+	}
+}
+
+func oauth2EventHandlerChannel1(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	_ = r.ParseForm()
+	switch {
+	case r.Method != http.MethodPost:
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte(`{"error":"wrong method"}`))
+	default:
+		if called1 < 1 {
+			atomic.AddUint64(&called1, 1)
+			_, _ = w.Write([]byte(`[{"data": {"payload": {"CountryIso": "IN"}, "event": {"replayId":1234}}, "channel": "channel_name1"}]`))
+		}
+	}
+}
+
+func oauth2EventHandlerChannel2(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	_ = r.ParseForm()
+	switch {
+	case r.Method != http.MethodPost:
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte(`{"error":"wrong method"}`))
+	default:
+		if called2 < 1 {
+			atomic.AddUint64(&called2, 1)
+			_, _ = w.Write([]byte(`[{"data": {"payload": {"CountryIso": "US"}, "event": {"replayId":1234}}, "channel": "channel_name2"}]`))
+		}
+	}
+}
+
+func oauth2Handler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/token" {
+		oauth2TokenHandler(w, r)
+		return
+	}
+	body, _ := ioutil.ReadAll(r.Body)
+	if string(body) == `{"channel": "/meta/handshake", "supportedConnectionTypes": ["long-polling"], "version": "1.0"}` {
+		oauth2ClientIdHandler(w, r)
+		return
+	} else if string(body) == `{"channel": "/meta/connect", "connectionType": "long-polling", "clientId": "client_id1"} ` {
+		oauth2EventHandlerChannel1(w, r)
+		return
+	} else if string(body) == `{"channel": "/meta/connect", "connectionType": "long-polling", "clientId": "client_id2"} ` {
+		oauth2EventHandlerChannel2(w, r)
+		return
+	} else if string(body) == `{
+								"channel": "/meta/subscribe",
+								"subscription": "channel_name1",
+								"clientId": "client_id1",
+								"ext": {
+									"replay": {"channel_name1": "-1"}
+									}
+								}` {
+		oauth2SubscribeHandlerChannel1(w, r)
+		return
+	} else if string(body) == `{
+								"channel": "/meta/subscribe",
+								"subscription": "channel_name2",
+								"clientId": "client_id2",
+								"ext": {
+									"replay": {"channel_name2": "-1"}
+									}
+								}` {
+		oauth2SubscribeHandlerChannel2(w, r)
+		return
+	}
+}
+
+func assertEventMatches(t *testing.T, expected bay.MaybeMsg, got beat.Event) {
+	message, err := got.GetValue("message")
+	require.NoError(t, err)
+	require.Equal(t, string(expected.Msg.Data.Payload), message)
+}
