b35de06133581c448d88418137d33a2088c61f3f
kaiyan-sheng <kaiyan.sheng@elastic.co>
2022-05-02 15:59:46 -0600

    Rename awscloudwatch.* fields to aws.cloudwatch.*

--- x-pack/filebeat/input/awscloudwatch/processor.go
+++ x-pack/filebeat/input/awscloudwatch/processor.go
@@ -66,6 +66,11 @@ func createEvent(logEvent cloudwatchlogs.FilteredLogEvent, logGroup string, regi
 				"log_stream":     *logEvent.LogStreamName,
 				"ingestion_time": time.Unix(*logEvent.IngestionTime/1000, 0),
 			},
+			"aws.cloudwatch": mapstr.M{
+				"log_group":      logGroup,
+				"log_stream":     *logEvent.LogStreamName,
+				"ingestion_time": time.Unix(*logEvent.IngestionTime/1000, 0),
+			},
 			"cloud": mapstr.M{
 				"provider": "aws",
 				"region":   regionName,
