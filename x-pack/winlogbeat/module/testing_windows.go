4096abb632043a2ff454940432f0a316e0f1790d
Dan Kortschak <90160302+efd6@users.noreply.github.com>
2022-05-03 10:26:30 +0930

    {,x-pack/}winlogbeat: resurrect tests for event processing (#31464)
    This is a moderately involved change. The process is described in the steps
    below, but the reader should refer to the commits in the PR to see exactly
    what was done and when.
    
    * port forward semi-processed events for tests
    
    This brings partially processed event data from a modified version of the testing
    code at 8896fd319a257f3e0783119a7dd8d0978ef62197 (the commit immediately prior to
    the removal of the javascript processing pipeline in #29435: commit at
    2f3b0c5cbe9cfdd10e11fd52e2a259e564001100).
    
    The evtx.golden.json files were generated by removing the js processing call at
    https://github.com/elastic/beats/blob/8896fd319a257f3e0783119a7dd8d0978ef62197/x-pack/winlogbeat/module/testing_windows.go#L132-L135
    to match the code in that file as it appears here and then run go test -update
    in x-pack/winlogbeat/module/{powershell,security,sysmon}/test on a windows 2019
    host.
    
    The test package for each of the modules is also resurrected with modifications
    reflecting the loss of the javascript processor.
    
    Tests in x-pack/winlogbeat/module/{security,sysmon}/test fail in this
    commit.
    
    * make sure metadata is available for enrichment of raw values
    
    This fixes failing tests in x-pack/winlogbeat/module/security/test, but tests in
    sysmon continue to fail because sysmon-11-filedeletedetected.evtx was added in
    33acb3c392471596daf773608b250b188ac38480 (2022-01-25) after the origin of the
    forward port origin (2021-12-02).
    
    * update golden file for sysmon-11-filedeletedetected.evtx
    
    * ignore opcode field on Windows 2022
    
    Also prohibit generating golden files for PowerShell on Windows 2022 to
    prevent unnecessary work in discovering that this will fail on other
    versions.
    
    * defer event field filtering until value comparison
    
    This will result in additional diff noise if golden values are generated on multiple
    versions of windows so it may be worth keeping the version used reasonably constant.
    The version used here was 2019.
    
    (cherry picked from commit f22abe3b6067091fa389ef521f00aa3721465604)
    
    # Conflicts:
    #	x-pack/winlogbeat/module/security/test/testdata/security-windows2019_4688_Process_Created.evtx.golden.json
    #	x-pack/winlogbeat/module/testing_windows.go

--- x-pack/winlogbeat/module/testing_windows.go
+++ x-pack/winlogbeat/module/testing_windows.go
@@ -22,12 +22,17 @@ import (
 
 	"github.com/elastic/beats/v7/libbeat/common"
 	"github.com/elastic/beats/v7/libbeat/mapping"
-	"github.com/elastic/beats/v7/libbeat/processors/script/javascript"
 	"github.com/elastic/beats/v7/winlogbeat/checkpoint"
 	"github.com/elastic/beats/v7/winlogbeat/eventlog"
+<<<<<<< HEAD
 
 	// Register javascript modules.
 	_ "github.com/elastic/beats/v7/libbeat/processors/script/javascript/module"
+=======
+	"github.com/elastic/elastic-agent-libs/config"
+	"github.com/elastic/elastic-agent-libs/mapstr"
+	"github.com/elastic/go-sysinfo/providers/windows"
+>>>>>>> f22abe3b60 ({,x-pack/}winlogbeat: resurrect tests for event processing (#31464))
 )
 
 var update = flag.Bool("update", false, "update golden files")
@@ -40,17 +45,28 @@ type params struct {
 }
 
 // WithFieldFilter filters the specified fields from the event prior to
-// creating the golden file.
+// comparison of values, but retains them in the written golden files.
 func WithFieldFilter(filter []string) Option {
 	return func(p *params) {
 		p.ignoreFields = filter
 	}
 }
 
-// TestPipeline tests the given pipeline by reading events from the .evtx files
-// and processing them with the pipeline. Then it compares the results against
+// TestPipeline tests the partial pipeline by reading events from the .evtx files
+// and processing them with a basic enrichment. Then it compares the results against
 // a saved golden file. Use -update to regenerate the golden files.
-func TestPipeline(t *testing.T, evtx string, pipeline string, opts ...Option) {
+func TestPipeline(t *testing.T, evtx string, opts ...Option) {
+	// FIXME: We cannot generate golden files on Windows 2022.
+	if *update {
+		os, err := windows.OperatingSystem()
+		if err != nil {
+			t.Fatalf("failed to get operating system info: %v", err)
+		}
+		if strings.Contains(os.Name, "2022") {
+			t.Fatal("cannot generate golden files on Windows 2022: see note in powershell/test/powershell_windows_test.go")
+		}
+	}
+
 	files, err := filepath.Glob(evtx)
 	if err != nil {
 		t.Fatal(err)
@@ -66,12 +82,12 @@ func TestPipeline(t *testing.T, evtx string, pipeline string, opts ...Option) {
 
 	for _, f := range files {
 		t.Run(filepath.Base(f), func(t *testing.T) {
-			testPipeline(t, f, pipeline, &p)
+			testPipeline(t, f, &p)
 		})
 	}
 }
 
-func testPipeline(t testing.TB, evtx string, pipeline string, p *params) {
+func testPipeline(t testing.TB, evtx string, p *params) {
 	t.Helper()
 
 	path, err := filepath.Abs(evtx)
@@ -94,6 +110,7 @@ func testPipeline(t testing.TB, evtx string, pipeline string, p *params) {
 		t.Fatal(err)
 	}
 
+<<<<<<< HEAD
 	// Load javascript processor.
 	processor, err := javascript.New(common.MustNewConfigFrom(common.MapStr{
 		"file": pipeline,
@@ -102,16 +119,19 @@ func testPipeline(t testing.TB, evtx string, pipeline string, p *params) {
 		t.Fatal(err)
 	}
 
+=======
+>>>>>>> f22abe3b60 ({,x-pack/}winlogbeat: resurrect tests for event processing (#31464))
 	// Read and process events.
 	var events []common.MapStr
 	for stop := false; !stop; {
 		records, err := log.Read()
-		if err == io.EOF {
+		if err == io.EOF { //nolint:errorlint // io.EOF should never be wrapped.
 			stop = true
 		} else if err != nil {
 			t.Fatal(err)
 		}
 
+		//nolint:errcheck // All the errors returned here are from beat.Event queries and may be ignored.
 		for _, r := range records {
 			record := r.ToEvent()
 
@@ -129,19 +149,15 @@ func testPipeline(t testing.TB, evtx string, pipeline string, p *params) {
 				record.Delete("winlog.user.domain")
 			}
 
-			evt, err := processor.Run(&record)
-			if err != nil {
-				t.Fatalf("%v while processing event:\n%v", err, record.Fields.StringToPrint())
-			}
-
 			// Copy the timestamp to the beat.Event.Fields because this is what
 			// we write to the golden data for testing purposes. In the normal
 			// Beats output this the handled by the encoder (go-structform).
+			evt := &record
 			if !evt.Timestamp.IsZero() {
 				evt.Fields["@timestamp"] = evt.Timestamp.UTC()
 			}
 
-			events = append(events, filterEvent(evt.Fields, p.ignoreFields))
+			events = append(events, evt.Fields)
 		}
 	}
 
@@ -155,7 +171,7 @@ func testPipeline(t testing.TB, evtx string, pipeline string, p *params) {
 		return
 	}
 	for i, e := range events {
-		assertEqual(t, expected[i], normalize(t, e))
+		assertEqual(t, filterEvent(expected[i], p.ignoreFields), normalize(t, filterEvent(e, p.ignoreFields)))
 	}
 }
 
@@ -188,12 +204,12 @@ func writeGolden(t testing.TB, source string, events []common.MapStr) {
 		t.Fatal(err)
 	}
 
-	if err := os.MkdirAll("testdata", 0o755); err != nil {
+	if err := os.MkdirAll("testdata", 0755); err != nil {
 		t.Fatal(err)
 	}
 
 	outPath := filepath.Join("testdata", filepath.Base(source)+".golden.json")
-	if err := ioutil.WriteFile(outPath, data, 0o644); err != nil {
+	if err := ioutil.WriteFile(outPath, data, 0o644); err != nil { //nolint:gosec // Bad linter!
 		t.Fatal(err)
 	}
 }
@@ -234,7 +250,7 @@ func normalize(t testing.TB, m common.MapStr) common.MapStr {
 
 func filterEvent(m common.MapStr, ignores []string) common.MapStr {
 	for _, f := range ignores {
-		m.Delete(f)
+		m.Delete(f) //nolint:errcheck // Deleting a thing that doesn't exist is ok.
 	}
 	return m
 }
@@ -251,7 +267,7 @@ func lowercaseGUIDs(m common.MapStr) common.MapStr {
 			continue
 		}
 		if uppercaseGUIDRegex.MatchString(str) {
-			m.Put(k, strings.ToLower(str))
+			m.Put(k, strings.ToLower(str)) //nolint:errcheck // Can't fail because k has been obtained from m.
 		}
 	}
 	return m
