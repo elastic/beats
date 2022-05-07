906f17cd5b83b29d64a06d9432c298490a29dc76
Craig MacKenzie <craig.mackenzie@elastic.co>
2022-05-06 22:22:11 -0400

    Port libbeat python system tests to new model.

--- libbeat/magefile.go
+++ libbeat/magefile.go
@@ -21,15 +21,15 @@
 package main
 
 import (
+	"context"
 	devtools "github.com/elastic/beats/v7/dev-tools/mage"
 	"github.com/elastic/beats/v7/dev-tools/mage/target/build"
+	"github.com/magefile/mage/mg"
 
 	// mage:import
 	_ "github.com/elastic/beats/v7/dev-tools/mage/target/common"
 	// mage:import
 	"github.com/elastic/beats/v7/dev-tools/mage/target/unittest"
-	// mage:import
-	"github.com/elastic/beats/v7/dev-tools/mage/target/integtest"
 	// mage:import
 	_ "github.com/elastic/beats/v7/dev-tools/mage/target/integtest/containers"
 	// mage:import
@@ -38,13 +38,16 @@ import (
 
 func init() {
 	unittest.RegisterPythonTestDeps(Fields)
-	integtest.RegisterGoTestDeps(Fields)
-	integtest.RegisterPythonTestDeps(Fields)
 }
 
 // Build builds the Beat binary.
 func Build() error {
 	return devtools.Build(devtools.DefaultBuildArgs())
+}
+
+// Builds the system test binary.
+func BuildSystemTestBinary() {
+	mg.Deps(devtools.BuildSystemTestBinary)
 }
 
 // Fields generates a fields.yml for the Beat.
@@ -62,4 +65,52 @@ func Config() error {
 // were built and only performs the merge.
 func AssembleDarwinUniversal() error {
 	return build.AssembleDarwinUniversal()
+}
+
+// IntegTest executes integration tests (it uses Docker to run the tests).
+func IntegTest() {
+	mg.SerialDeps(GoIntegTest, PythonIntegTest)
+}
+
+// GoIntegTest starts the docker containers and executes the Go integration tests.
+func GoIntegTest(ctx context.Context) error {
+	mg.Deps(Fields)
+	args := devtools.DefaultGoTestIntegrationArgs()
+	// TODO: Tidy these up, put them in the default args?
+	args.Env["ES_HOST"] = "localhost"
+	args.Env["ES_USER"] = "admin"
+	args.Env["ES_PASS"] = "testing"
+	args.Env["ES_SUPERUSER_USER"] = "admin"
+	args.Env["ES_SUPERUSER_PASS"] = "testing"
+	args.Env["KIBANA_HOST"] = "localhost"
+	args.Env["KIBANA_USER"] = "beats"
+	args.Env["KIBANA_PASS"] = "testing"
+	args.Env["REDIS_HOST"] = "localhost"
+	args.Env["SREDIS_HOST"] = "localhost"
+	args.Env["LS_HOST"] = "localhost"
+	return devtools.GoIntegTest(ctx, args)
+}
+
+// PythonIntegTest starts the docker containers and executes the Python integration tests.
+// Use GENERATE=true to generate expected log files.
+// Use TESTING_FILEBEAT_MODULES=module[,module] to limit what modules to test.
+// Use TESTING_FILEBEAT_FILESETS=fileset[,fileset] to limit what fileset to test.
+func PythonIntegTest(ctx context.Context) error {
+	mg.Deps(Fields, BuildSystemTestBinary)
+	args := devtools.DefaultPythonTestIntegrationArgs()
+	// TODO: Tidy these up?
+	args.Env["INTEGRATION_TESTS"] = "1"
+	args.Env["MODULES_PATH"] = devtools.CWD("module")
+	args.Env["ES_HOST"] = "localhost"
+	args.Env["ES_USER"] = "admin"
+	args.Env["ES_PASS"] = "testing"
+	args.Env["ES_SUPERUSER_USER"] = "admin"
+	args.Env["ES_SUPERUSER_PASS"] = "testing"
+	args.Env["KIBANA_HOST"] = "localhost"
+	args.Env["KIBANA_USER"] = "beats"
+	args.Env["KIBANA_PASS"] = "testing"
+	args.Env["REDIS_HOST"] = "localhost"
+	args.Env["SREDIS_HOST"] = "localhost"
+	args.Env["LS_HOST"] = "localhost"
+	return devtools.PythonIntegTest(args)
 }
