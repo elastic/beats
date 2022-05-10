1527be08cfd768d20a178ac7522932f2a29f46c6
Lalit Satapathy <lalit.satapathy@elastic.co>
2022-05-10 18:14:46 +0530

    mage update fix to fields.go

--- x-pack/metricbeat/module/sql/fields.go
+++ x-pack/metricbeat/module/sql/fields.go
@@ -19,5 +19,5 @@ func init() {
 // AssetSql returns asset data.
 // This is the base64 encoded zlib format compressed contents of module/sql.
 func AssetSql() string {
-	return "eJy0k0Fu8jAQhfc5xRPLX4IDZPEvqi4REuIAlWM/wMXJgD2mze0rEoJSpa1AVb18npnvy0Se48C2RDqFAlCvgSVmm/VyVgCRgSaxREU1BeCYbPRH9dKU+F8AwGa9RC0uB2JLtXsm1NTobcI2Sg3TVTijpjKJBbD1DC6VXfMcjak5wC9H2yNL7KLk4zUZ1497XPRnxls8tB7Yvkl0o/wL6eE8dzOQEx1UwHfarITuiVNmbBcTahf/Drq+jBhYHddKCLQ6LG5KHS6aXDN6u/g3MZDqlVZHcR+89LdOchV4n96qZ9z+4lWO7nutpNE3u4etHtraSpr59fNxNiHzLrNKJNA0f6v21EN+1Pr8kj4CAAD//07X+ko="
+	return "eJy0k81u4jAUhfd5iiOWI8EDZDGL0SwREmL2I8c+ARcnDvY1JW9fkR+aKqUCVfXy+N77fTc/SxzZ5ognlwFixTHHYrddLzIg0FFF5igoKgMMow62EevrHL8zANht16i8SY4oKfrAiIoSrI4og6+gugqjRBUqMgNKS2di3jUvUauKI/x6pG2YYx98aoZkWj/tMcGeGW7x2Hpk++qDmeSfSI/nbzcDKdJAPHihTkLIgTglhnY1owbG5CTOsMKLPMb8x4vAJ2mSwJcda+Cae9Au/t6m2+uId5B4aO8ctYxva04dL+pUMVi9+jUz8MUL9XTtPvjf3xqfCsfH9DY94/bpDHI097WiBFvvn7Z66qltfL0c1sdZucSHzArvHVX9s2p/esiXWh9/37cAAAD//9vCGr8="
 }
