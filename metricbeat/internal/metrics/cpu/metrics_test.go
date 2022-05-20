467067b3ebc785bd7d7a39e0e28b4e563e8912aa
Tiago Queiroz <tiago.queiroz@elastic.co>
2022-05-20 15:44:05 +0200

    adding test data and finishing tests

--- metricbeat/internal/metrics/cpu/metrics_test.go
+++ metricbeat/internal/metrics/cpu/metrics_test.go
@@ -19,8 +19,10 @@ package cpu
 
 import (
 	"bufio"
+	"encoding/json"
 	"os"
 	"path/filepath"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -163,17 +165,70 @@ func TestMetricsPercentages(t *testing.T) {
 	assert.EqualValues(t, 1., total.(float64))
 }
 
-func TestInfoScanner(t *testing.T) {
-	fd, err := os.Open(filepath.Join("testdata", "cpuinfo"))
+// TestCpuinfoScanner tests the parsing of `/proc/cpuinfo` for different
+// system/CPU configurations. The lscpu GitHub contains a nice set of
+// test files: https://github.com/util-linux/util-linux/tree/master/tests/ts/lscpu/dumps
+func TestCpuinfoScanner(t *testing.T) {
+	testCases := []string{
+		"cpuinfo",
+		"cpuinfo-quad-socket",
+		"armv7", // Source: https://github.com/util-linux/util-linux/blob/master/tests/ts/lscpu/dumps/armv7.tar.gz
+	}
+	for _, tc := range testCases {
+		t.Run(tc, func(t *testing.T) {
+			sourceFd, err := os.Open(filepath.Join("testdata", tc))
+			if err != nil {
+				t.Fatalf("cannot open test file: %s", err)
+			}
+			defer sourceFd.Close()
+
+			scanner := bufio.NewScanner(sourceFd)
+			cpuInfo, err := cpuinfoScanner(scanner)
+			if err != nil {
+				t.Fatalf("cpuinfoScanner error: %s", err)
+			}
+
+			// Ignoring the error, because if there is any parsing error, generateGoldenFile
+			// will be false, making the test to run as expected
+			if generateGoldenFile, _ := strconv.ParseBool(os.Getenv("GENERATE")); generateGoldenFile {
+				t.Logf("generating golden files for test: %s", t.Name())
+				cpuinfoScannerGoldenFile(t, cpuInfo, tc)
+				return
+			}
+
+			expectedFd, err := os.Open(filepath.Join("testdata", tc+".expected.json"))
+			if err != nil {
+				t.Fatalf("cannot open test expectation file: %s", err)
+			}
+			defer expectedFd.Close()
+
+			expected := []CPUInfo{}
+			if err := json.NewDecoder(expectedFd).Decode(&expected); err != nil {
+				t.Fatalf("cannot decode goldenfile data: %s", err)
+			}
+
+			assert.Equal(t, expected, cpuInfo)
+		})
+
+	}
+}
+
+func cpuinfoScannerGoldenFile(t *testing.T, data []CPUInfo, name string) {
+	jsonData, err := json.MarshalIndent(data, "", "  ")
 	if err != nil {
-		t.Fatalf("cannot open test file: %s", err)
+		t.Fatalf("cannot marshal data into JSON: %s", err)
 	}
 
-	scanner := bufio.NewScanner(fd)
-	info, err := cpuinfoScanner(scanner)
-	if err != nil {
-		t.Fatalf("infoScnner error: %s", err)
-	}
+	// Add a line break at the end
+	jsonData = append(jsonData, '\n')
 
-	t.Log(info)
+	expectedFd, err := os.Create(filepath.Join("testdata", name+".expected.json"))
+	if err != nil {
+		t.Fatalf("cannot open/create test expectation file: %s", err)
+	}
+	defer expectedFd.Close()
+
+	if _, err := expectedFd.Write(jsonData); err != nil {
+		t.Fatalf("cannot write data to goldenfile: %s", err)
+	}
 }
