fc24693753aaaac78df74c5d3357a1c3cf5c8be1
Andrew Cholakian <andrewvc@elastic.co>
2022-05-23 16:35:09 -0500

    Fix unmerged ICMP loop code

--- heartbeat/monitors/active/icmp/stdloop.go
+++ heartbeat/monitors/active/icmp/stdloop.go
@@ -28,6 +28,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/elastic/beats/v7/libbeat/logp"
 	"golang.org/x/net/icmp"
 	"golang.org/x/net/ipv4"
 	"golang.org/x/net/ipv6"
@@ -161,15 +162,11 @@ func newICMPLoop() (*stdICMPLoop, error) {
 func (l *stdICMPLoop) runICMPRecv(conn *icmp.PacketConn, proto int) {
 	for {
 		bytes := make([]byte, 512)
-<<<<<<< HEAD
-		conn.SetReadDeadline(time.Now().Add(1 * time.Second))
-=======
 		err := conn.SetReadDeadline(time.Now().Add(time.Second))
 		if err != nil {
 			logp.L().Error("could not set read deadline for ICMP: %w", err)
 			return
 		}
->>>>>>> d3cc24a460 ([Heartbeat] Fix unintentional use of no-op logger (#31543))
 		_, addr, err := conn.ReadFrom(bytes)
 		if err != nil {
 			if neterr, ok := err.(*net.OpError); ok {
