3c8c5a0537343bfb46b0ba03a40c7998d7cf1bfd
Andrew Cholakian <andrewvc@elastic.co>
2022-05-23 16:36:19 -0500

    Fix unmerged ICMP loop code

--- heartbeat/scheduler/schedjob.go
+++ heartbeat/scheduler/schedjob.go
@@ -25,6 +25,7 @@ import (
 	"golang.org/x/sync/semaphore"
 
 	"github.com/elastic/beats/v7/libbeat/common/atomic"
+	"github.com/elastic/beats/v7/libbeat/logp"
 )
 
 type schedJob struct {
@@ -60,14 +61,10 @@ func (sj *schedJob) run() (startedAt time.Time) {
 	sj.wg.Add(1)
 	sj.activeTasks.Inc()
 	if sj.jobLimitSem != nil {
-<<<<<<< HEAD
-		sj.jobLimitSem.Acquire(sj.ctx, 1)
-=======
 		err := sj.jobLimitSem.Acquire(sj.ctx, 1)
 		if err != nil {
 			logp.L().Errorf("could not acquire semaphore: %w", err)
 		}
->>>>>>> d3cc24a460 ([Heartbeat] Fix unintentional use of no-op logger (#31543))
 	}
 
 	startedAt = sj.runTask(sj.entrypoint)
