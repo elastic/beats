491bd97d0e30a7bd5ab1f1b25fa1f6ee2e289ae4
Andrew Kroh <andrew.kroh@elastic.co>
2022-06-03 10:59:05 -0400

    Modify npipe to get directly get the user SID
    Remove the dependency on user.Current(). It fetches more information than
    is required to configure the named pipe. This only needs to know the SID
    in order to build the SDDL.

--- libbeat/api/npipe/listener_windows.go
+++ libbeat/api/npipe/listener_windows.go
@@ -22,13 +22,16 @@ package npipe
 
 import (
 	"context"
+	"fmt"
 	"net"
-	"os/user"
 	"strings"
+	"syscall"
 
 	winio "github.com/Microsoft/go-winio"
-	"github.com/pkg/errors"
 )
+
+// ntAuthoritySystemSID is a well-known SID used by the NT AUTHORITY\SYSTEM account.
+const ntAuthoritySystemSID = "S-1-5-18"
 
 // NewListener creates a new Listener receiving events over a named pipe.
 func NewListener(name, sd string) (net.Listener, error) {
@@ -38,7 +41,7 @@ func NewListener(name, sd string) (net.Listener, error) {
 
 	l, err := winio.ListenPipe(name, c)
 	if err != nil {
-		return nil, errors.Wrapf(err, "failed to listen on the named pipe %s", name)
+		return nil, fmt.Errorf("failed to listen on the named pipe %s: %w", name, err)
 	}
 
 	return l, nil
@@ -78,19 +81,9 @@ func Dial(npipe string) func(string, string) (net.Conn, error) {
 //
 // Docs: https://docs.microsoft.com/en-us/windows/win32/secauthz/security-descriptor-string-format
 func DefaultSD(forUser string) (string, error) {
-	var u *user.User
-	var err error
-	// No user configured we fallback to the current running user.
-	if len(forUser) == 0 {
-		u, err = user.Current()
-		if err != nil {
-			return "", errors.Wrap(err, "failed to retrieve the current user")
-		}
-	} else {
-		u, err = user.Lookup(forUser)
-		if err != nil {
-			return "", errors.Wrapf(err, "failed to retrieve the user %s", forUser)
-		}
+	sid, err := lookupSID(forUser)
+	if err != nil {
+		return "", err
 	}
 
 	// Named pipe security and access rights.
@@ -98,12 +91,54 @@ func DefaultSD(forUser string) (string, error) {
 	// See docs: https://docs.microsoft.com/en-us/windows/win32/ipc/named-pipe-security-and-access-rights
 	// String definition: https://docs.microsoft.com/en-us/windows/win32/secauthz/ace-strings
 	// Give generic read/write access to the specified user.
-	descriptor := "D:P(A;;GA;;;" + u.Uid + ")"
-	if u.Username == "NT AUTHORITY\\SYSTEM" {
+	descriptor := "D:P(A;;GA;;;" + sid + ")"
+	if sid == ntAuthoritySystemSID {
 		// running as SYSTEM, include Administrators group so Administrators can talk over
 		// the named pipe to the running Elastic Agent system process
 		// https://support.microsoft.com/en-us/help/243330/well-known-security-identifiers-in-windows-operating-systems
 		descriptor += "(A;;GA;;;S-1-5-32-544)" // Administrators group
 	}
 	return descriptor, nil
+}
+
+// lookupSID returns the SID of the specified username. If username is empty the
+// SID of the current user is returned.
+func lookupSID(username string) (string, error) {
+	if username == "" {
+		sid, err := currentUserSID()
+		if err != nil {
+			return "", fmt.Errorf("failed to lookup the SID of current user: %w", err)
+		}
+		return sid, nil
+	}
+
+	sid, _, _, err := syscall.LookupSID("", username)
+	if err != nil {
+		return "", fmt.Errorf("failed to lookup the SID for user %q: %w", username, err)
+	}
+	sidString, err := sid.String()
+	if err != nil {
+		return "", fmt.Errorf("failed to convert the SID for user %q to string: %w", username, err)
+	}
+	return sidString, nil
+}
+
+func currentUserSID() (string, error) {
+	t, err := syscall.OpenCurrentProcessToken()
+	if err != nil {
+		return "", err
+	}
+	defer t.Close()
+
+	u, err := t.GetTokenUser()
+	if err != nil {
+		return "", err
+	}
+
+	sid, err := u.User.Sid.String()
+	if err != nil {
+		return "", err
+	}
+
+	return sid, nil
 }
