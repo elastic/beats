5a27bb5794bbff5c5426bb66ded53af7d401dbe8
Noémi Ványi <sitbackandwait@gmail.com>
2022-06-21 15:07:20 +0200

    fix more linter issues

--- filebeat/input/filestream/copytruncate_prospector_test.go
+++ filebeat/input/filestream/copytruncate_prospector_test.go
@@ -19,7 +19,6 @@ package filestream
 
 import (
 	"context"
-	"fmt"
 	"regexp"
 	"testing"
 
@@ -145,14 +144,12 @@ func TestCopyTruncateProspector_Create(t *testing.T) {
 			for originalFile, rotatedFiles := range test.expectedRotatedFiles {
 				rFile, ok := p.rotatedFiles.table[originalFile]
 				if !ok {
-					fmt.Printf("cannot find %s in original files\n", originalFile)
-					t.FailNow()
+					t.Fatalf("cannot find %s in original files\n", originalFile)
 				}
 				require.Equal(t, len(rotatedFiles), len(rFile.rotated))
 				for i, rotatedFile := range rotatedFiles {
 					if rFile.rotated[i].path != rotatedFile {
-						fmt.Printf("%s is not a rotated file, instead %s is\n", rFile.rotated[i].path, rotatedFile)
-						t.FailNow()
+						t.Fatalf("%s is not a rotated file, instead %s is\n", rFile.rotated[i].path, rotatedFile)
 					}
 				}
 			}
