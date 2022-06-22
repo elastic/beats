abb77cd8896540293edd59e9443abd2466fa4277
Marc Guasch <marc.guasch@elastic.co>
2022-06-22 13:16:10 +0200

    Take into account RuneErrors when decoding XML codes

---
+++ libbeat/common/encoding/xml/safe_reader_test.go
@@ -0,0 +1,47 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+//go:build !integration
+// +build !integration
+
+package xml
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestInvalidXMLIsSanitized(t *testing.T) {
+	// add a byte bigger than 127 to create an invalid xml
+	xml := append(append([]byte(`<person><Name ID="123">John`), 128), []byte(`</Name></person>`)...)
+
+	expected := map[string]interface{}{
+		"person": map[string]interface{}{
+			"Name": map[string]interface{}{
+				"#text": "John\\ufffd",
+				"ID":    "123",
+			},
+		},
+	}
+
+	d := NewDecoder(NewSafeReader(xml))
+	out, err := d.Decode()
+	require.Nil(t, err)
+	assert.Equal(t, expected, out)
+}
