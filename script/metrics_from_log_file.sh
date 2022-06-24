b3c491f121ceffd4de199d06a529f1929fb821b6
Craig MacKenzie <craig.mackenzie@elastic.co>
2022-06-24 14:25:26 -0400

    Add help text and example usage.

--- script/metrics_from_log_file.sh
+++ script/metrics_from_log_file.sh
@@ -1,6 +1,34 @@
 #!/bin/sh
 set -e
 
+usage="$(basename "$0") [-h] path_to_beat_log_file
+
+Extracts the monitoring metrics from a Beat log files as JSON, regardless
+of whether the Beat log file is in structured JSON format already.
+
+where:
+    -h  show this help text
+
+Examples using reference log files:
+  $(basename "$0") ./testdata/log_with_metrics.log
+  $(basename "$0") ./testdata/log_with_metrics.json
+"
+
+while getopts ':h' option; do
+  case "$option" in
+    h) echo "$usage"
+       exit 0;;
+    *) echo "Unknown option"
+       exit 1;;
+  esac
+done
+shift $((OPTIND - 1))
+
+if [ $# -eq 0 ]; then
+    echo "No log file to parse."
+    exit 0
+fi
+
 cat "$@" | \
 grep 'Non-zero' | \
 sed -e 's/^\([0-9T:\.+\-]*\)[^{]*\(.*\)/{"timestamp": "\1", "data": \2}/' | \
