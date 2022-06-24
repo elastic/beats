8775f36f19726fce336c507bfab25cab49ea04d8
Craig MacKenzie <craig.mackenzie@elastic.co>
2022-06-24 14:52:44 -0400

    Explain how the sed command works.

--- script/metrics_from_log_file.sh
+++ script/metrics_from_log_file.sh
@@ -3,18 +3,16 @@ set -e
 
 usage="$(basename "$0") [-h] path_to_beat_log_file
 
-Extracts the monitoring metrics from a Beat log files as JSON, regardless
-of whether the Beat log file is in structured JSON format already.
+Extracts monitoring metrics from beat log files that are not in structured JSON format.
+For log files using structured JSON format, extract metrics with 'grep 'Non-zero' | jq'
 
 where:
     -h  show this help text
 
-Examples using reference log files:
-  $(basename "$0") ./testdata/log_with_metrics.json
-
-  # Extract all metrics, using jq to show changes in the active event count over time.
-  # The active event count is the number events in the pipeline, including the the queue.
-  $(basename "$0") ./testdata/log_with_metrics.log | jq \"{timestamp: .timestamp, active: .monitoring.metrics.libbeat.pipeline.events.active}\"
+Example:
+# Extract all metrics, using jq to show changes in the active event count over time.
+# The active event count is the number events in the pipeline, including the the queue.
+$(basename "$0") ./sample_logs/log_with_metrics.log | jq \"{timestamp: .timestamp, active: .monitoring.metrics.libbeat.pipeline.events.active}\"
 "
 
 while getopts ':h' option; do
@@ -32,6 +30,12 @@ if [ $# -eq 0 ]; then
     exit 0
 fi
 
+# The command below uses a sed expression to convert the start of a log line like:
+#  2022-06-15T14:20:46.928+0200	INFO	[monitoring]	log/log.go:184	Non-zero metrics in the last 30s	{"monitoring":
+# to a JSON line like:
+#  {"timestamp": "2022-06-15T14:20:46.928+0200", "data": {"monitoring":
+# and then uses jq to restore the desired JSON structure of:
+#  {"timestamp": "2022-06-15T14:20:46.928+0200", "monitoring": {...}
 cat "$@" | \
 grep 'Non-zero' | \
 sed -e 's/^\([0-9T:\.+\-]*\)[^{]*\(.*\)/{"timestamp": "\1", "data": \2}/' | \
