5917b8fbde2e53d2c22a858da802f665beda1beb
gab <gabriel.pop@elastic.co>
2022-07-12 14:42:50 +0300

    refactor distribution histogram to ES

--- x-pack/metricbeat/module/gcp/distribution.go
+++ x-pack/metricbeat/module/gcp/distribution.go
@@ -12,7 +12,7 @@ import (
 	"github.com/elastic/elastic-agent-libs/mapstr"
 )
 
-func DistributionMetricHasHistogram(d *distribution.Distribution) bool {
+func containsHistogram(d *distribution.Distribution) bool {
 	if d.Count == 0 || d.BucketOptions == nil || len(d.BucketCounts) == 0 {
 		return false
 	}
@@ -27,47 +27,57 @@ func DistributionMetricHasHistogram(d *distribution.Distribution) bool {
 	return d.Count == sum
 }
 
-func DistributionHistogramToES(d *distribution.Distribution) mapstr.M {
-	values := []float64{}
-	counts := []uint64{}
+func calcExplicitUpperBound(bucket *distribution.Distribution_BucketOptions_Explicit, i int) float64 {
+	return bucket.Bounds[i]
+}
 
-	if DistributionMetricHasHistogram(d) {
-		switch {
-		case d.BucketOptions.GetExplicitBuckets() != nil:
-			bucket := d.BucketOptions.GetExplicitBuckets()
+func calcExponentialUpperBound(bucket *distribution.Distribution_BucketOptions_Exponential, i int) float64 {
+	return bucket.Scale * (math.Pow(bucket.GrowthFactor, float64(i)))
+}
 
-			for i := 0; i < len(d.BucketCounts); i++ {
-				upperBound := bucket.Bounds[i]
+func calcLinearUpperBound(bucket *distribution.Distribution_BucketOptions_Linear, i int) float64 {
+	return bucket.Offset + (bucket.Width * float64(i))
+}
 
-				values = append(values, upperBound)
-			}
-		case d.BucketOptions.GetExponentialBuckets() != nil:
-			bucket := d.BucketOptions.GetExponentialBuckets()
-
-			for i := 1; i < len(d.BucketCounts)+1; i++ {
-				upperBound := bucket.Scale * (math.Pow(bucket.GrowthFactor, float64(i)))
-				values = append(values, upperBound)
-			}
-
-		case d.BucketOptions.GetLinearBuckets() != nil:
-			bucket := d.BucketOptions.GetLinearBuckets()
-
-			for i := 1; i < len(d.BucketCounts)+1; i++ {
-				upperBound := bucket.Offset + (bucket.Width * float64(i))
-
-				values = append(values, upperBound)
-			}
-		}
-
-		for i := range d.BucketCounts {
-			counts = append(counts, uint64(d.BucketCounts[i]))
-		}
-	}
-
-	res := mapstr.M{
+func createHistogram(values []float64, counts []uint64) mapstr.M {
+	return mapstr.M{
 		"values": values,
 		"counts": counts,
 	}
+}
 
-	return res
+func DistributionHistogramToES(d *distribution.Distribution) mapstr.M {
+	if !containsHistogram(d) {
+		return createHistogram([]float64{}, []uint64{})
+	}
+
+	values := make([]float64, 0, len(d.BucketCounts))
+	counts := make([]uint64, 0, len(d.BucketCounts))
+
+	switch {
+	case d.BucketOptions.GetExplicitBuckets() != nil:
+		bucket := d.BucketOptions.GetExplicitBuckets()
+
+		for i := range d.BucketCounts {
+			values = append(values, calcExplicitUpperBound(bucket, i))
+		}
+	case d.BucketOptions.GetExponentialBuckets() != nil:
+		bucket := d.BucketOptions.GetExponentialBuckets()
+
+		for i := range d.BucketCounts {
+			values = append(values, calcExponentialUpperBound(bucket, i+1))
+		}
+	case d.BucketOptions.GetLinearBuckets() != nil:
+		bucket := d.BucketOptions.GetLinearBuckets()
+
+		for i := range d.BucketCounts {
+			values = append(values, calcLinearUpperBound(bucket, i+1))
+		}
+	}
+
+	for i := range d.BucketCounts {
+		counts = append(counts, uint64(d.BucketCounts[i]))
+	}
+
+	return createHistogram(values, counts)
 }
