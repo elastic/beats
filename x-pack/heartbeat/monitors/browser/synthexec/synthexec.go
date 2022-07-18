605a9ec39ff67e8bb78a757cd1f459cec29e0c7e
Andrew Cholakian <andrewvc@elastic.co>
2022-07-18 15:48:46 -0500

    Improve comments

--- x-pack/heartbeat/monitors/browser/synthexec/synthexec.go
+++ x-pack/heartbeat/monitors/browser/synthexec/synthexec.go
@@ -18,7 +18,6 @@ import (
 	"runtime"
 	"strings"
 	"sync"
-	"syscall"
 	"time"
 
 	"github.com/elastic/beats/v7/heartbeat/ecserr"
@@ -115,7 +114,12 @@ func runCmd(
 	params mapstr.M,
 	filterJourneys FilterJourneyConfig,
 ) (mpx *ExecMultiplexer, err error) {
-	cmd.SysProcAttr = &syscall.SysProcAttr{
+	// Note that while cmd.SysProcAtr takes a syscall.SysProcAttr object
+	// we are passing in a unix.SysProcAttr object
+	// this is equivalent, but the unix package is not considered deprecated
+	// as the syscall package is
+	cmd.SysProcAttr = &unix.SysProcAttr{
+		// Ensure node subprocesses are killed if this process dies (linux only)
 		Pdeathsig: unix.SIGKILL,
 	}
 
