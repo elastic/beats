7ad98f4d0303a30c52566a90f18ba98b2ad7d312
Victor Martinez <victormartinezrubio@gmail.com>
2022-07-20 16:59:13 +0100

    automate the ironbank docker context generation  (#32251)
    (cherry picked from commit e41dc18a54381fb6567c49ea7219c4e7d8de7c63)
    
    # Conflicts:
    #	x-pack/winlogbeat/magefile.go

---
+++ dev-tools/packaging/templates/ironbank/metricbeat/README.md
@@ -0,0 +1,37 @@
+# metricbeat
+
+**metricbeat** Lightweight shipper for metrics
+
+Collect metrics from your systems and services. From CPU to memory, Redis to NGINX, and much more, Metricbeat is a lightweight way to send system and service statistics.
+
+For more information about metricbeat, please visit
+https://www.elastic.co/products/beats/metricbeat.
+
+### Installation instructions
+
+Please follow the documentation on [running metricbeat on Docker](https://www.elastic.co/guide/en/beats/metricbeat/current/running-on-docker.html).
+
+### Where to file issues and PRs
+
+- [Issues](https://github.com/elastic/beats/issues)
+- [PRs](https://github.com/elastic/beats/pulls)
+
+### DoD Restrictions
+
+### Where to get help
+
+- [metricbeat Discuss Forums](https://discuss.elastic.co/tags/c/elastic-stack/beats/28/metricbeat)
+- [metricbeat Documentation](https://www.elastic.co/guide/en/beats/metricbeat/current/index.html)
+
+### Still need help?
+
+You can learn more about the Elastic Community and also understand how to get more help
+visiting [Elastic Community](https://www.elastic.co/community).
+
+This software is governed by the [Elastic
+License](https://github.com/elastic/beats/blob/{{ .MajorMinor }}/licenses/ELASTIC-LICENSE.txt),
+and includes the full set of [free
+features](https://www.elastic.co/subscriptions).
+
+View the detailed release notes
+[here](https://www.elastic.co/guide/en/beats/libbeat/current/release-notes-{{ beat_version }}.html).
--- x-pack/winlogbeat/magefile.go
+++ x-pack/winlogbeat/magefile.go
@@ -38,3 +38,46 @@ func init() {
 // Update is an alias for update:all. This is a workaround for
 // https://github.com/magefile/mage/issues/217.
 func Update() { mg.Deps(winlogbeat.Update.All) }
+<<<<<<< HEAD
+=======
+
+// GoUnitTest executes the Go unit tests.
+// Use TEST_COVERAGE=true to enable code coverage profiling.
+// Use RACE_DETECTOR=true to enable the race detector.
+func GoUnitTest(ctx context.Context) error {
+	mg.SerialCtxDeps(ctx, goTestDeps...)
+	args := devtools.DefaultGoTestUnitArgs()
+	// The module unit tests depend on a running docker container to provide
+	// the ES instance to run the processor pipeline. In the absence of a
+	// test supervisor or a single test executable to ensure that only a
+	// single container is running, or additional logic to ensure no network
+	// collisions, we ensure that only one test package is running at a time.
+	args.ExtraFlags = append(args.ExtraFlags, "-p", "1")
+	return devtools.GoTest(ctx, args)
+}
+
+// PythonUnitTest executes the python system tests.
+func PythonUnitTest() error {
+	mg.SerialDeps(pythonTestDeps...)
+	mg.Deps(devtools.BuildSystemTestBinary)
+	return devtools.PythonTest(devtools.DefaultPythonTestUnitArgs())
+}
+
+// PythonVirtualEnv creates the testing virtual environment and prints its location.
+func PythonVirtualEnv() error {
+	venv, err := devtools.PythonVirtualenv(true)
+	if err != nil {
+		return err
+	}
+	fmt.Println(venv)
+	return nil
+}
+
+// Package packages the Beat for IronBank distribution.
+//
+// Use SNAPSHOT=true to build snapshots.
+func Ironbank() error {
+	fmt.Println(">> Ironbank: this module is not subscribed to the IronBank releases.")
+	return nil
+}
+>>>>>>> e41dc18a54 (automate the ironbank docker context generation  (#32251))
