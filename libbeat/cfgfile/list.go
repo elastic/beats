elastic/beats

commit 3001dd4c8fdfa3b95a6afc158536a80db7f2f04f

fix race condition when stopping inputs filestream ID bookkeeper (#32309)
This commit fixes three race conditions:
1. When reloading inputs the Reload method would not wait for the
runner to stop before starting a new input.
2. The runner's Start method was not incrementing the WaitGroup, hence
Stop would finish before the runner had stopped.
3. The input manager that keeps track of filestream input IDs did not
synchronise access to the IDs map, allowing for some race conditions
when issuing the log error about possible data duplication.

Some log messages have been modified to contain more details like the
filestream ID.

libbeat/cfgfile/list.go
@@ -81,13 +81,22 @@ func (r *RunnerList) Reload(configs []*reload.ConfigWithMeta) error {
 
 	r.logger.Debugf("Start list: %d, Stop list: %d", len(startList), len(stopList))
 
+	wg := sync.WaitGroup{}
 	// Stop removed runners
 	for hash, runner := range stopList {
+		wg.Add(1)
 		r.logger.Debugf("Stopping runner: %s", runner)
 		delete(r.runners, hash)
-		go runner.Stop()
+		go func() {
+			defer wg.Done()
+			runner.Stop()
+			r.logger.Debugf("Runner: '%s' has stopped", runner)
+		}()
 		moduleStops.Add(1)
 	}
+
+	// Wait for all runners to stop before starting new ones
+	wg.Wait()
 
 	// Start new runners
 	for hash, config := range startList {

filebeat/input/filestream/internal/input-logfile/input.go
@@ -81,6 +81,10 @@ func (inp *managedInput) Run(
 
 	inp.prospector.Run(ctx, sourceStore, hg)
 
+	// Notify the manager the input  has stopped, currently that is used to
+	// keep track of duplicated IDs
+	inp.manager.StopInput(inp.userID)
+
 	return nil
 }
 

filebeat/input/filestream/internal/input-logfile/manager.go
@@ -71,6 +71,7 @@ type InputManager struct {
 	store      *store
 	ackUpdater *updateWriter
 	ackCH      *updateChan
+	idsMux     sync.Mutex
 	ids        map[string]struct{}
 }
 
@@ -176,12 +177,14 @@ func (cim *InputManager) Create(config *conf.C) (v2.Input, error) {
 			" duplication, please add an ID and restart Filebeat")
 	}
 
+	cim.idsMux.Lock()
 	if _, exists := cim.ids[settings.ID]; exists {
 		cim.Logger.Errorf("filestream input with ID '%s' already exists, this "+
 			"will lead to data duplication, please use a different ID", settings.ID)
 	}
 
 	cim.ids[settings.ID] = struct{}{}
+	cim.idsMux.Unlock()
 
 	prospector, harvester, err := cim.Configure(config)
 	if err != nil {
@@ -224,6 +227,13 @@ func (cim *InputManager) Create(config *conf.C) (v2.Input, error) {
 		cleanTimeout:     settings.CleanTimeout,
 		harvesterLimit:   settings.HarvesterLimit,
 	}, nil
+}
+
+// StopInput peforms all necessary clean up when an input finishes.
+func (cim *InputManager) StopInput(id string) {
+	cim.idsMux.Lock()
+	delete(cim.ids, id)
+	cim.idsMux.Unlock()
 }
 
 func (cim *InputManager) getRetainedStore() *store {

filebeat/input/v2/compat/compat.go
@@ -104,11 +104,13 @@ func (f *factory) Create(
 func (r *runner) String() string { return r.input.Name() }
 
 func (r *runner) Start() {
+	r.wg.Add(1)
 	log := r.log
 	name := r.input.Name()
 
 	go func() {
-		log.Infof("Input %v starting", name)
+		defer r.wg.Done()
+		log.Infof("Input '%s' starting", name)
 		err := r.input.Run(
 			v2.Context{
 				ID:          r.id,
@@ -119,9 +121,9 @@ func (r *runner) Start() {
 			r.connector,
 		)
 		if err != nil {
-			log.Errorf("Input '%v' failed with: %+v", name, err)
+			log.Errorf("Input '%s' failed with: %+v", name, err)
 		} else {
-			log.Infof("Input '%v' stopped", name)
+			log.Infof("Input '%s' stopped (goroutine)", name)
 		}
 	}()
 }
@@ -129,7 +131,7 @@ func (r *runner) Start() {
 func (r *runner) Stop() {
 	r.sig.Cancel()
 	r.wg.Wait()
-	r.log.Infof("Input '%v' stopped", r.input.Name())
+	r.log.Infof("Input '%s' stopped (runner)", r.input.Name())
 }
 
 func configID(config *conf.C) (string, error) {

filebeat/tests/system/test_reload_inputs.py
@@ -281,11 +281,6 @@ class Test(BaseTest):
         with open(self.working_dir + "/configs/input.yml", 'w') as f:
             f.write(inputConfigTemplate.format(self.working_dir + "/logs/test.log"))
 
-        # Make sure error shows up in log file
-        self.wait_until(
-            lambda: self.log_contains("Can only start an input when all related states are finished"),
-            max_timeout=15)
-
         # Wait until old runner is stopped
         self.wait_until(
             lambda: self.log_contains("Stopping runner:"),

commit 9306946381e47ee2f6942d9f242cf9406df2a3dc

Fix race condition when reloading configuration (#32433)
This commit fixes the race condition introduced by
3001dd4c8fdfa3b95a6afc158536a80db7f2f04f that aimed to fix some race
conditions.

A test has its logging updated to use t.Log, so it only logs when
verbosity is on and test line issuing the log is also shown.

Some t.Helper() calls are also added.

libbeat/cfgfile/list.go
@@ -87,11 +87,11 @@ func (r *RunnerList) Reload(configs []*reload.ConfigWithMeta) error {
 		wg.Add(1)
 		r.logger.Debugf("Stopping runner: %s", runner)
 		delete(r.runners, hash)
-		go func() {
+		go func(runner Runner) {
 			defer wg.Done()
 			runner.Stop()
 			r.logger.Debugf("Runner: '%s' has stopped", runner)
-		}()
+		}(runner)
 		moduleStops.Add(1)
 	}
 

libbeat/autodiscover/autodiscover_test.go
@@ -533,7 +533,7 @@ func TestAutodiscoverWithMutlipleEntries(t *testing.T) {
 	wait(t, func() bool { return len(adapter.Runners()) == 3 })
 	runners = adapter.Runners()
 	// Ensure the first config is the same as before
-	fmt.Println(runners)
+	t.Log(runners)
 	assert.Equal(t, len(runners), 3)
 	assert.Equal(t, len(autodiscover.configs["mock:foo"]), 2)
 	check(t, runners, conf.MustNewConfigFrom(map[string]interface{}{"a": "b"}), true, false)
@@ -571,6 +571,7 @@ func TestAutodiscoverWithMutlipleEntries(t *testing.T) {
 }
 
 func wait(t *testing.T, test func() bool) {
+	t.Helper()
 	sleep := 20 * time.Millisecond
 	ready := test()
 	for !ready && sleep < 10*time.Second {
@@ -585,6 +586,7 @@ func wait(t *testing.T, test func() bool) {
 }
 
 func check(t *testing.T, runners []*mockRunner, expected *conf.C, started, stopped bool) {
+	t.Helper()
 	for _, r := range runners {
 		if reflect.DeepEqual(expected, r.config) {
 			ok1 := assert.Equal(t, started, r.started)