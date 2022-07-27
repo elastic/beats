d9a9003333b26823daa4e751a9fa748350bad730
Giuseppe Santoro <giuseppe.santoro@elastic.co>
2022-07-27 12:22:45 +0100

    metricsstorage with some tests

--- metricbeat/module/kubernetes/util/metrics_storage_test.go
+++ metricbeat/module/kubernetes/util/metrics_storage_test.go
@@ -1,17 +1,81 @@
 package util
 
-import "testing"
+import (
+	"testing"
 
-func TestSet(t *testing.T) {
+	"github.com/stretchr/testify/suite"
+)
+
+type ExampleTestSuite struct {
+	suite.Suite
+	Cuid string
+	MetricName string
+	MetricValue float64
+	Storage *MetricsStorage
+}
+
+func (s *ExampleTestSuite) SetupTest() {
 	ns := "namespace"
 	pod := "pod"
 	container := "container"
-	cuid := ContainerUID(ns, pod, container)
+	s.Cuid = ContainerUID(ns, pod, container)
+	s.MetricName = "s.MetricName"
+	s.MetricValue = 0.2
+	s.Storage = NewMetricsStorage()
+}
 
-	metrics := MetricsStorage{
-	}
-	metrics[cuid] := MetricsEntry{
-		NodeMemAllocatable: &Float64Metric{},
-	}
-	
+func (s *ExampleTestSuite) TestNotFoundSet() {
+	err := s.Storage.Set(s.Cuid, s.MetricName, s.MetricValue)
+	s.Nil(err)
+
+	metric, err := s.Storage.Get(s.Cuid, s.MetricName)
+	s.Nil(err)
+	s.NotNil(metric)
+
+	value := metric.Get()
+	s.Equal(s.MetricValue, value)
+}
+
+func (s *ExampleTestSuite) TestSetChange() {
+	err := s.Storage.Set(s.Cuid, s.MetricName, s.MetricValue)
+	s.Nil(err)
+
+	s.assertGetMetric(s.Storage, s.Cuid, s.MetricName, s.MetricValue)
+
+	changedMetricValue := 0.4
+
+	err = s.Storage.Set(s.Cuid, s.MetricName, changedMetricValue)
+	s.Nil(err)
+
+	s.assertGetMetric(s.Storage, s.Cuid, s.MetricName, changedMetricValue)
+}
+
+func (s *ExampleTestSuite) TestIdNotFoundGet() {
+	value, err := s.Storage.Get(s.Cuid, s.MetricName)
+	s.NotNil(err)
+	s.Nil(value)
+
+	s.Equal("Metrics not found for id: namespace/pod/container", err.Error())
+}
+
+func (s *ExampleTestSuite) TestMetricNotFoundGet() {
+	err := s.Storage.Set(s.Cuid, "anotherMetric", s.MetricValue)
+	s.Nil(err)
+
+	value, err := s.Storage.Get(s.Cuid, s.MetricName)
+	s.NotNil(err)
+	s.Nil(value)
+}
+
+func (s *ExampleTestSuite) assertGetMetric(storage *MetricsStorage, id string, name string, expectedValue float64) {
+	metric, err := storage.Get(id, name)
+	s.Nil(err)
+	s.NotNil(metric)
+
+	value := metric.Get()
+	s.Equal(expectedValue, value)
+}
+
+func TestExampleTestSuite(t *testing.T) {
+	suite.Run(t, new(ExampleTestSuite))
 }
