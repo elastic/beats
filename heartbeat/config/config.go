25cc27c20317c075cb14ed6938457c46da9ead3f
Andrew Cholakian <andrew@andrewvc.com>
2022-08-01 11:11:32 -0500

    [Heartbeat] Limit browser jobs to 2 by default
    Fixes #32082 by limiting browser jobs to 2

--- heartbeat/config/config.go
+++ heartbeat/config/config.go
@@ -46,4 +46,10 @@ type Scheduler struct {
 }
 
 // DefaultConfig is the canonical instantiation of Config.
-var DefaultConfig = Config{}
+var DefaultConfig = Config{
+	Jobs: map[string]JobLimit{
+		"browser": {
+			Limit: 2,
+		},
+	},
+}
