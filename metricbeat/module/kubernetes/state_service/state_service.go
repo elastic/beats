c7a4d525d119a3a8d0c42f36a9817ec577a68ab4
Giuseppe Santoro <giuseppe.santoro@elastic.co>
2022-08-12 12:10:35 +0100

    Feature/remove k8s cache (#32539)
    * replaced internal expiring cache with non expiring dictionary in memory
    * fixed a bug that prevented to export pod/container metrics when node/state_node metricsets were disabled

--- metricbeat/module/kubernetes/state_service/state_service.go
+++ metricbeat/module/kubernetes/state_service/state_service.go
@@ -86,7 +86,7 @@ func NewServiceMetricSet(base mb.BaseMetricSet) (mb.MetricSet, error) {
 				"hostname":         p.Label("ingress_hostname"),
 			},
 		},
-		enricher: util.NewResourceMetadataEnricher(base, &kubernetes.Service{}, mod.GetPerfMetricsCache(), false),
+		enricher: util.NewResourceMetadataEnricher(base, &kubernetes.Service{}, mod.GetMetricsRepo(), false),
 	}, nil
 }
 
