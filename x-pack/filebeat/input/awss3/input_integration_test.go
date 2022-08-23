75825d3ab94376f7b417b6ac17273e7d183b8b81
Andrea Spacca <andrea.spacca@elastic.co>
2022-08-23 11:22:51 +0900

    add '; charset=UTF-8' in integration tests

--- x-pack/filebeat/input/awss3/input_integration_test.go
+++ x-pack/filebeat/input/awss3/input_integration_test.go
@@ -327,9 +327,9 @@ func uploadS3TestFiles(t *testing.T, region, bucket string, filenames ...string)
 
 		contentType := ""
 		if strings.HasSuffix(filename, "ndjson") || strings.HasSuffix(filename, "ndjson.gz") {
-			contentType = contentTypeNDJSON
+			contentType = contentTypeNDJSON + "; charset=UTF-8"
 		} else if strings.HasSuffix(filename, "json") || strings.HasSuffix(filename, "json.gz") {
-			contentType = contentTypeJSON
+			contentType = contentTypeJSON + "; charset=UTF-8"
 		}
 
 		// Upload the file to S3.
