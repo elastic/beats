b312c959b7691ab86c47de35708e81115c62dd24
Alex Kristiansen <alex.kristiansen@elastic.co>
2022-08-23 14:33:47 -0700

    make linter happy

--- metricbeat/module/logstash/node_stats/data.go
+++ metricbeat/module/logstash/node_stats/data.go
@@ -30,8 +30,6 @@ import (
 
 	"github.com/elastic/beats/v7/metricbeat/module/logstash"
 
-	"github.com/pkg/errors"
-
 	"github.com/elastic/beats/v7/libbeat/common"
 	"github.com/elastic/beats/v7/metricbeat/mb"
 )
@@ -189,7 +187,7 @@ func eventMapping(r mb.ReporterV2, content []byte, isXpack bool, logger *logp.Lo
 	var nodeStats NodeStats
 	err := json.Unmarshal(content, &nodeStats)
 	if err != nil {
-		return errors.Wrap(err, "could not parse node stats response")
+		return fmt.Errorf("could not parse node stats response: %s", err)
 	}
 
 	timestamp := common.Time(time.Now())
@@ -250,14 +248,14 @@ func eventMapping(r mb.ReporterV2, content []byte, isXpack bool, logger *logp.Lo
 			ModuleFields: mapstr.M{},
 		}
 
-		event.ModuleFields.Put("node.stats", logstashStats)
-		event.RootFields.Put("service.id", nodeStats.ID)
-		event.RootFields.Put("service.hostname", nodeStats.Host)
-		event.RootFields.Put("service.version", nodeStats.Version)
+		_, _ = event.ModuleFields.Put("node.stats", logstashStats)
+		_, _ = event.RootFields.Put("service.id", nodeStats.ID)
+		_, _ = event.RootFields.Put("service.hostname", nodeStats.Host)
+		_, _ = event.RootFields.Put("service.version", nodeStats.Version)
 
 		if clusterUUID != "" {
-			event.ModuleFields.Put("cluster.id", clusterUUID)
-			event.ModuleFields.Put("elasticsearch.cluster.id", clusterUUID)
+			_, _ = event.ModuleFields.Put("cluster.id", clusterUUID)
+			_, _ = event.ModuleFields.Put("elasticsearch.cluster.id", clusterUUID)
 		}
 
 		// xpack.enabled in config using standalone metricbeat writes to `.monitoring` instead of `metricbeat-*`
@@ -274,8 +272,7 @@ func eventMapping(r mb.ReporterV2, content []byte, isXpack bool, logger *logp.Lo
 }
 
 func makeClusterToPipelinesMap(pipelines []PipelineStats, overrideClusterUUID string) map[string][]PipelineStats {
-	var clusterToPipelinesMap map[string][]PipelineStats
-	clusterToPipelinesMap = make(map[string][]PipelineStats)
+	clusterToPipelinesMap := make(map[string][]PipelineStats)
 
 	if overrideClusterUUID != "" {
 		clusterToPipelinesMap[overrideClusterUUID] = pipelines
