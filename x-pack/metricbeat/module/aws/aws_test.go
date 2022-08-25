04bb77d4cec0f0e363b19ed8cf76fa664a1cb535
Mario Castro <mariocaster@gmail.com>
2022-08-25 15:34:27 +0200

    root file

--- x-pack/metricbeat/module/aws/aws_test.go
+++ x-pack/metricbeat/module/aws/aws_test.go
@@ -9,39 +9,82 @@ package aws
 
 import (
 	"context"
+	"errors"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+	awscommon "github.com/elastic/beats/v7/x-pack/libbeat/common/aws"
 	"testing"
-
-	ec2types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
-	resourcegroupstaggingapitypes "github.com/aws/aws-sdk-go-v2/service/resourcegroupstaggingapi/types"
+	"time"
 
 	awssdk "github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	ec2types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
+	"github.com/aws/aws-sdk-go-v2/service/iam"
+	rdstypes "github.com/aws/aws-sdk-go-v2/service/rds/types"
+	resourcegroupstaggingapitypes "github.com/aws/aws-sdk-go-v2/service/resourcegroupstaggingapi/types"
+
+	"github.com/elastic/beats/v7/metricbeat/mb"
+	"github.com/elastic/elastic-agent-libs/logp"
+
 	"github.com/stretchr/testify/assert"
 )
 
-// MockEC2Client struct is used for unit tests.
-type MockEC2Client struct {
+type mockProvider struct {
+	listAccountAliasesTests  func(context.Context, *iam.ListAccountAliasesInput, ...func(*iam.Options)) (*iam.ListAccountAliasesOutput, error)
+	describeRegionsTests     func(context.Context, *ec2.DescribeRegionsInput, ...func(*ec2.Options)) (*ec2.DescribeRegionsOutput, error)
+	newStsFromConfigTests    func(awssdk.Config, ...func(*sts.Options)) *sts.Client
+	getCallerIdentityTests   func(*sts.Client, context.Context, *sts.GetCallerIdentityInput, ...func(*sts.Options)) (*sts.GetCallerIdentityOutput, error)
+	newIamFromConfigTests    func(awssdk.Config, ...func(*iam.Options)) iam.ListAccountAliasesAPIClient
+	retrieveCredentialsTests func(context.Context) (awssdk.Credentials, error)
 	*ec2.Client
 }
 
-func (m *MockEC2Client) DescribeRegions(ctx context.Context, params *ec2.DescribeRegionsInput, optFns ...func(*ec2.Options)) (*ec2.DescribeRegionsOutput, error) {
-	return &ec2.DescribeRegionsOutput{
-		Regions: []ec2types.Region{
-			{
-				RegionName: awssdk.String("us-west-1"),
-			},
-		},
-	}, nil
+func (m *mockProvider) Retrieve(ctx context.Context) (awssdk.Credentials, error) {
+	return m.retrieveCredentialsTests(ctx)
+}
+
+func (m *mockProvider) newIamFromConfig(config awssdk.Config, f ...func(*iam.Options)) iam.ListAccountAliasesAPIClient {
+	return m.newIamFromConfigTests(config, f...)
+}
+
+func (m *mockProvider) newStsFromConfig(config awssdk.Config, f ...func(*sts.Options)) *sts.Client {
+	return m.newStsFromConfigTests(config, f...)
+}
+
+func (m *mockProvider) getCallerIdentity(client *sts.Client, ctx context.Context, params *sts.GetCallerIdentityInput, optFns ...func(*sts.Options)) (*sts.GetCallerIdentityOutput, error) {
+	return m.getCallerIdentityTests(client, ctx, params, optFns...)
+}
+
+func (m *mockProvider) ListAccountAliases(ctx context.Context, input *iam.ListAccountAliasesInput, f ...func(*iam.Options)) (*iam.ListAccountAliasesOutput, error) {
+	return m.listAccountAliasesTests(ctx, input, f...)
+}
+
+func (m *mockProvider) DescribeRegions(ctx context.Context, params *ec2.DescribeRegionsInput, optFns ...func(*ec2.Options)) (*ec2.DescribeRegionsOutput, error) {
+	return m.describeRegionsTests(ctx, params, optFns...)
 }
 
 func TestGetRegions(t *testing.T) {
-	mockSvc := &MockEC2Client{}
+	mockSvc := &mockProvider{
+		describeRegionsTests: func(ctx context.Context, params *ec2.DescribeRegionsInput, optFns ...func(*ec2.Options)) (*ec2.DescribeRegionsOutput, error) {
+			return &ec2.DescribeRegionsOutput{
+				Regions: []ec2types.Region{{RegionName: awssdk.String("us-west-1")}}}, nil
+		},
+	}
+
 	regionsList, err := getRegions(mockSvc)
 	if err != nil {
 		t.Fatalf("failed getRegions: %s", err)
 	}
 	assert.Equal(t, 1, len(regionsList))
 	assert.Equal(t, "us-west-1", regionsList[0])
+
+	mockSvc.describeRegionsTests = func(ctx context.Context, params *ec2.DescribeRegionsInput, optFns ...func(*ec2.Options)) (*ec2.DescribeRegionsOutput, error) {
+		return nil, errors.New("error getting regions")
+	}
+	regionsList, err = getRegions(mockSvc)
+	assert.Error(t, err)
+	assert.Equal(t, []string{}, regionsList)
+	assert.ErrorContains(t, err, "failed DescribeRegions")
+
 }
 
 func TestStringInSlice(t *testing.T) {
@@ -87,6 +130,34 @@ func TestCheckTagFiltersExist(t *testing.T) {
 		tags           interface{}
 		expectedExists bool
 	}{
+		{
+			"tagFilters are included in rds tags",
+			[]Tag{
+				{
+					Key:   "Name",
+					Value: "RDS",
+				},
+				{
+					Key:   "Organization",
+					Value: "Engineering",
+				},
+			},
+			[]rdstypes.Tag{
+				{
+					Key:   awssdk.String(tagKey1),
+					Value: awssdk.String("RDS"),
+				},
+				{
+					Key:   awssdk.String(tagKey2),
+					Value: awssdk.String(tagValue2),
+				},
+				{
+					Key:   awssdk.String(tagKey3),
+					Value: awssdk.String(tagValue3),
+				},
+			},
+			true,
+		},
 		{
 			"tagFilters are included in ec2 tags",
 			[]Tag{
@@ -174,4 +245,103 @@ func TestCheckTagFiltersExist(t *testing.T) {
 			assert.Equal(t, c.expectedExists, exists)
 		})
 	}
+}
+
+func TestInitEvent(t *testing.T) {
+	regionName := "myRegion"
+	accountName := "myAccountName"
+	accountID := "myAccountId"
+	timestamp := time.Now()
+
+	//Check hardcoded expected data
+	assertEqual := func(mbEvent mb.Event, expected interface{}, key string) {
+		val, err := mbEvent.RootFields.GetValue(key)
+		if err != nil {
+			t.Fail()
+		}
+		assert.Equal(t, expected, val)
+	}
+	mbEvent1 := InitEvent(regionName, accountName, accountID, timestamp)
+	assertEqual(mbEvent1, "aws", "cloud.provider")
+	assertEqual(mbEvent1, regionName, "cloud.region")
+	assertEqual(mbEvent1, accountName, "cloud.account.name")
+	assertEqual(mbEvent1, accountID, "cloud.account.id")
+
+	// Leave every field empty
+	mbEvent2 := InitEvent("", "", "", time.Now())
+	assertNil := func(mbEvent mb.Event, key string) {
+		val, err := mbEvent.RootFields.GetValue(key)
+		if err == nil {
+			t.Fail()
+		}
+		assert.Nil(t, val)
+	}
+	assertEqual(mbEvent1, "aws", "cloud.provider")
+	assertNil(mbEvent2, "cloud.region")
+	assertNil(mbEvent2, "cloud.account.id")
+	assertNil(mbEvent2, "cloud.account.name")
+}
+
+func TestGetAccountName(t *testing.T) {
+	mock := mockProvider{
+		listAccountAliasesTests: func(ctx context.Context, input *iam.ListAccountAliasesInput, f ...func(*iam.Options)) (*iam.ListAccountAliasesOutput, error) {
+			return &iam.ListAccountAliasesOutput{AccountAliases: []string{"alias1", "alias2"}}, nil
+		},
+	}
+	logger := logp.NewLogger("aws-testing")
+	name := getAccountName(&mock, logger, "myAccountID")
+	assert.Equal(t, "alias1", name)
+
+	mock.listAccountAliasesTests = func(ctx context.Context, input *iam.ListAccountAliasesInput, f ...func(*iam.Options)) (*iam.ListAccountAliasesOutput, error) {
+		return &iam.ListAccountAliasesOutput{AccountAliases: []string{}}, nil
+	}
+	name = getAccountName(&mock, logger, "myAccountID")
+	assert.Equal(t, "myAccountID", name)
+
+	mock.listAccountAliasesTests = func(ctx context.Context, input *iam.ListAccountAliasesInput, f ...func(*iam.Options)) (*iam.ListAccountAliasesOutput, error) {
+		return nil, errors.New("an error")
+	}
+	name = getAccountName(&mock, logger, "myAccountID")
+	assert.Equal(t, "myAccountID", name)
+}
+
+func TestSetupMetricset(t *testing.T) {
+	log := logp.NewLogger("aws-testing")
+	mock := &mockProvider{
+		listAccountAliasesTests: func(ctx context.Context, input *iam.ListAccountAliasesInput, f ...func(*iam.Options)) (*iam.ListAccountAliasesOutput, error) {
+			return &iam.ListAccountAliasesOutput{AccountAliases: []string{"alias1", "alias2"}}, nil
+		},
+		newStsFromConfigTests: func(config awssdk.Config, f ...func(*sts.Options)) *sts.Client {
+			return nil
+		},
+		getCallerIdentityTests: func(client *sts.Client, ctx context.Context, params *sts.GetCallerIdentityInput, optFns ...func(*sts.Options)) (*sts.GetCallerIdentityOutput, error) {
+			return nil, nil
+		},
+	}
+	mock.newIamFromConfigTests = func(config awssdk.Config, f ...func(*iam.Options)) iam.ListAccountAliasesAPIClient {
+		return mock
+	}
+
+	config := Config{
+		Period:     0,
+		Regions:    nil,
+		Latency:    0,
+		AWSConfig:  awscommon.ConfigAWS{},
+		TagsFilter: nil,
+	}
+	awsConfig := awssdk.Config{}
+
+	t.Run("no region specified", func(t *testing.T) {
+		m := newMetricset(mb.BaseMetricSet{}, config, awsConfig)
+		err := m.setupMetricset(log, config, mock)
+		assert.Error(t, err, "failed DescribeRegions: operation error EC2: DescribeRegions, failed to resolve service endpoint, an AWS region is required, but was not found")
+	})
+
+	t.Run("region specified", func(t *testing.T) {
+		awsConfig.Region = "a_region"
+		config.Regions = []string{awsConfig.Region, "another_region"}
+		m := newMetricset(mb.BaseMetricSet{}, config, awsConfig)
+		err := m.setupMetricset(log, config, mock)
+		assert.NoError(t, err)
+	})
 }
