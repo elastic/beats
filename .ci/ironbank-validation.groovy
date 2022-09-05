d0acdedbe3ce276697085400d624e336bdf13a63
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-09-05 13:34:43 +0100

    ci: ironbank validation pipeline

---
+++ .ci/ironbank-validation.groovy
@@ -0,0 +1,71 @@
+#!/usr/bin/env groovy
+
+@Library('apm@current') _
+
+pipeline {
+  agent { label 'ubuntu-20 && immutable' }
+  environment {
+    REPO = 'beats'
+    BASE_DIR = "src/github.com/elastic/${env.REPO}"
+    PIPELINE_LOG_LEVEL = "INFO"
+    BEATS_FOLDER = "x-pack/heartbeat"
+    SLACK_CHANNEL = '#beats'
+    NOTIFY_TO = 'ironbank-beats-validation+observability-robots-internal@elastic.co'
+  }
+  options {
+    timeout(time: 31, unit: 'HOURS')
+    buildDiscarder(logRotator(numToKeepStr: '20', artifactNumToKeepStr: '20', daysToKeepStr: '30'))
+    timestamps()
+    ansiColor('xterm')
+    disableResume()
+    durabilityHint('PERFORMANCE_OPTIMIZED')
+    disableConcurrentBuilds()
+  }
+  stages {
+    stage('Checkout') {
+      options { skipDefaultCheckout() }
+      steps {
+        deleteDir()
+        gitCheckout(basedir: "${BASE_DIR}")
+        setEnvVar("GO_VERSION", readFile("${BASE_DIR}/.go-version").trim())
+      }
+    }
+    stage('Ironbank'){
+      options { skipDefaultCheckout() }
+      steps {
+        withMageEnv(){
+          dir("${env.BASE_DIR}/${env.BEATS_FOLDER}") {
+            sh(label: 'mage ironbank', script: 'mage ironbank')
+          }
+        }
+      }
+    }
+    stage('Validate'){
+      options { skipDefaultCheckout() }
+      steps {
+        withMageEnv(){
+          dir("${env.BASE_DIR}/${env.BEATS_FOLDER}") {
+            sh(label: 'mage validate-ironbank', script: 'mage validate-ironbank')
+          }
+        }
+      }
+    }
+  }
+  post {
+    failure {
+      notifyStatus(slackStatus: 'danger', subject: "[${env.REPO}] Ironbank validation failed", body: "(<${env.RUN_DISPLAY_URL}|Open>)")
+    }
+    cleanup {
+      notifyBuildResult(prComment: true)
+    }
+  }
+}
+
+def notifyStatus(def args = [:]) {
+  releaseNotification(slackChannel: "${env.SLACK_CHANNEL}",
+                      slackColor: args.slackStatus,
+                      slackCredentialsId: 'jenkins-slack-integration-token',
+                      to: "${env.NOTIFY_TO}",
+                      subject: args.subject,
+                      body: args.body)
+}
