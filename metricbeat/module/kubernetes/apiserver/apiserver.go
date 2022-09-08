df1fde7911d68aa7715097891c5944c2205c015f
Tetiana Kravchenko <tetiana.kravchenko@elastic.co>
2022-09-08 11:22:36 +0200

    Add missing cluster metadata to multiple k8s metricsests (#32979)
    * add missing field
    
    Signed-off-by: Tetiana Kravchenko <tetiana.kravchenko@elastic.co>
    
    * update log message
    
    Signed-off-by: Tetiana Kravchenko <tetiana.kravchenko@elastic.co>
    
    * fix tests
    
    Signed-off-by: Tetiana Kravchenko <tetiana.kravchenko@elastic.co>
    
    * add pr number
    
    Signed-off-by: Tetiana Kravchenko <tetiana.kravchenko@elastic.co>
    
    * volume metricset: replace adding ECS orchestrator fields with util.AddClusterECSMeta
    
    Signed-off-by: Tetiana Kravchenko <tetiana.kravchenko@elastic.co>
    
    Signed-off-by: Tetiana Kravchenko <tetiana.kravchenko@elastic.co>
    (cherry picked from commit 1f5b863424929750d9c07564470cab096cf5c6ba)
    
    # Conflicts:
    #	metricbeat/module/kubernetes/apiserver/_meta/test/metrics.1.14.expected
    #	metricbeat/module/kubernetes/apiserver/apiserver.go
    #	metricbeat/module/kubernetes/apiserver/metricset.go
    #	metricbeat/module/kubernetes/controllermanager/_meta/test/metrics.1.20.expected
    #	metricbeat/module/kubernetes/controllermanager/controllermanager.go

--- metricbeat/module/kubernetes/apiserver/apiserver.go
+++ metricbeat/module/kubernetes/apiserver/apiserver.go
@@ -22,6 +22,7 @@ import (
 	"github.com/elastic/beats/v7/metricbeat/mb"
 )
 
+<<<<<<< HEAD
 func init() {
 	//mapping := &prometheus.MetricsMapping{
 	mapping := &prometheus.MetricsMapping{
@@ -65,8 +66,48 @@ func init() {
 			"host":        prometheus.KeyLabel("request.host"),
 		},
 	}
+=======
+var mapping = &prometheus.MetricsMapping{
+	Metrics: map[string]prometheus.MetricMap{
+		"process_cpu_seconds_total":               prometheus.Metric("process.cpu.sec"),
+		"process_resident_memory_bytes":           prometheus.Metric("process.memory.resident.bytes"),
+		"process_virtual_memory_bytes":            prometheus.Metric("process.memory.virtual.bytes"),
+		"process_open_fds":                        prometheus.Metric("process.fds.open.count"),
+		"process_start_time_seconds":              prometheus.Metric("process.started.sec"),
+		"apiserver_watch_events_sizes":            prometheus.Metric("watch.events.size.bytes"),
+		"apiserver_response_sizes":                prometheus.Metric("response.size.bytes"),
+		"apiserver_request_duration_seconds":      prometheus.Metric("request.duration.us", prometheus.OpMultiplyBuckets(1000000)),
+		"apiserver_request_total":                 prometheus.Metric("request.count"),
+		"apiserver_current_inflight_requests":     prometheus.Metric("request.current.count"),
+		"apiserver_longrunning_gauge":             prometheus.Metric("request.longrunning.count"),
+		"apiserver_storage_objects":               prometheus.Metric("etcd.object.count"),
+		"apiserver_audit_event_total":             prometheus.Metric("audit.event.count"),
+		"apiserver_audit_requests_rejected_total": prometheus.Metric("audit.rejected.count"),
+		"rest_client_requests_total":              prometheus.Metric("client.request.count"),
+	},
 
-	mb.Registry.MustAddMetricSet("kubernetes", "apiserver",
-		getMetricsetFactory(mapping),
+	Labels: map[string]prometheus.LabelMap{
+		"client":      prometheus.KeyLabel("request.client"),
+		"resource":    prometheus.KeyLabel("request.resource"),
+		"scope":       prometheus.KeyLabel("request.scope"),
+		"subresource": prometheus.KeyLabel("request.subresource"),
+		"verb":        prometheus.KeyLabel("request.verb"),
+		"code":        prometheus.KeyLabel("request.code"),
+		"contentType": prometheus.KeyLabel("request.content_type"),
+		"dry_run":     prometheus.KeyLabel("request.dry_run"),
+		"requestKind": prometheus.KeyLabel("request.kind"),
+		"component":   prometheus.KeyLabel("request.component"),
+		"group":       prometheus.KeyLabel("request.group"),
+		"version":     prometheus.KeyLabel("request.version"),
+		"handler":     prometheus.KeyLabel("request.handler"),
+		"method":      prometheus.KeyLabel("request.method"),
+		"host":        prometheus.KeyLabel("request.host"),
+		"kind":        prometheus.KeyLabel("watch.events.kind"),
+	},
+}
+>>>>>>> 1f5b863424 (Add missing cluster metadata to multiple k8s metricsests (#32979))
+
+func init() {
+	mb.Registry.MustAddMetricSet("kubernetes", "apiserver", New,
 		mb.WithHostParser(prometheus.HostParser))
 }
