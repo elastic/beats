7058e4b9507f94239c655e5b0e3ff422b59fa15c
Tiago Queiroz <tiago.queiroz@elastic.co>
2022-10-21 12:09:54 +0200

    [WIP] Adding metricbeat

--- dev-tools/performance/filebeat/run.sh
+++ dev-tools/performance/filebeat/run.sh
@@ -1,9 +1,12 @@
 #! /bin/bash -x
 
-FILEBEAT_VERSION="${FILEBEAT_VERSION:=8.4.3}"
-FB_TAR_NAME=filebeat-$FILEBEAT_VERSION-linux-x86_64.tar.gz
-FB_FOLDER_NAME=filebeat-$FILEBEAT_VERSION-linux-x86_64
+BEATS_VERSION="${BEATS_VERSION:=8.4.3}"
+FB_TAR_NAME=filebeat-$BEATS_VERSION-linux-x86_64.tar.gz
+FB_FOLDER_NAME=filebeat-$BEATS_VERSION-linux-x86_64
 LOG_FILE="${LOG_FILE:=/tmp/flog.log}"
+
+MB_TAR_NAME=metricbeat-$BEATS_VERSION-linux-x86_64.tar.gz
+MB_FOLDER_NAME=metricbeat-$BEATS_VERSION-linux-x86_64
 
 # Install flog
 if [[ ! -e $FB_TAR_NAME ]]
@@ -31,6 +34,23 @@ else
     tar -xf $FB_TAR_NAME
 fi
 
+# Install Metricbeat
+if [ -e $MB_TAR_NAME ]
+then
+    echo "Metricbeat already downloaded, skipping"
+else
+    wget https://artifacts.elastic.co/downloads/beats/metricbeat/$MB_TAR_NAME
+fi
+
+if [ -e $MB_FOLDER_NAME ]
+then
+   echo "Metricbeat already extracted"
+else
+    tar -xf $MB_TAR_NAME
+fi
+
+# Deploy Metricbeat to the host
+
 # Generate a 1Gb file
 if [[ ! -e $LOG_FILE ]]
 then
@@ -41,10 +61,19 @@ fi
 
 # Copy filebeat config file
 cp ./filebeat.yml $FB_FOLDER_NAME
-cd $FB_FOLDER_NAME
+
+# Copy Metricbeat configuration files
+cp -vr ./modules.d $MB_FOLDER_NAME/
+cp -v ./metricbeat.yml $MB_FOLDER_NAME/
+
+# Start Metricbeat
+cd $MB_FOLDER_NAME
+./metricbeat &
+cd ../
 
 # Remove the registry
+cd $FB_FOLDER_NAME
 rm -rvf data
 
 # Start filebeat
-./filebeat -e -v 
+./filebeat -e -v |tee filebeat.log
