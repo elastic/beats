8b3315c265b82225fa65eadeaab058a7841df0fe
Andrew Kroh <andrew.kroh@elastic.co>
2022-11-07 15:38:09 -0500

    Ignore errcheck warnings
    Reconstructing a nested map from the flattened scalars should never result in a conflict.

--- libbeat/monitoring/report/log/log.go
+++ libbeat/monitoring/report/log/log.go
@@ -249,16 +249,16 @@ func toKeyValuePairs(snaps map[string]monitoring.FlatSnapshot) []interface{} {
 	for name, snap := range snaps {
 		data := make(mapstr.M, snapshotLen(snap))
 		for k, v := range snap.Bools {
-			data.Put(k, v)
+			data.Put(k, v) //nolint:errcheck // All keys within the flat snapshot are unique and are for scalar values.
 		}
 		for k, v := range snap.Floats {
-			data.Put(k, v)
+			data.Put(k, v) //nolint:errcheck // All keys within the flat snapshot are unique and are for scalar values.
 		}
 		for k, v := range snap.Ints {
-			data.Put(k, v)
+			data.Put(k, v) //nolint:errcheck // All keys within the flat snapshot are unique and are for scalar values.
 		}
 		for k, v := range snap.Strings {
-			data.Put(k, v)
+			data.Put(k, v) //nolint:errcheck // All keys within the flat snapshot are unique and are for scalar values.
 		}
 		if len(data) > 0 {
 			args = append(args, logp.Reflect(name, data))
