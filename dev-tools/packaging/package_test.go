b4658739bf43efcd74d733847d986d812467fcc8
Denis Rechkunov <denis.rechkunov@elastic.co>
2022-11-30 20:00:00 +0100

    Add support for excluded paths in packaging
    After an update in heartbeat we have have a path like
    `usr/share/heartbeat/.node/node/lib/node_modules/@elastic/synthetics/templates/lightweight/heartbeat.yml`
    which matches the `configFilePattern` which causes a permission check
    which the file does not satisfy. It has nothing to do with the actual
    heartbeat configuration file.

--- dev-tools/packaging/package_test.go
+++ dev-tools/packaging/package_test.go
@@ -49,6 +49,7 @@ const (
 )
 
 var (
+	excludedPathsPattern   = regexp.MustCompile(`node_modules`)
 	configFilePattern      = regexp.MustCompile(`/(\w+beat\.yml|apm-server\.yml|elastic-agent\.yml)$`)
 	manifestFilePattern    = regexp.MustCompile(`manifest.yml`)
 	modulesDirPattern      = regexp.MustCompile(`module/.+`)
@@ -572,6 +573,9 @@ func readRPM(rpmFile string) (*packageFile, *rpm.PackageFile, error) {
 	pf := &packageFile{Name: filepath.Base(rpmFile), Contents: map[string]packageEntry{}}
 
 	for _, file := range contents {
+		if excludedPathsPattern.MatchString(file.Name()) {
+			continue
+		}
 		pe := packageEntry{
 			File: file.Name(),
 			Mode: file.Mode(),
@@ -656,6 +660,10 @@ func readTarContents(tarName string, data io.Reader) (*packageFile, error) {
 			return nil, err
 		}
 
+		if excludedPathsPattern.MatchString(header.Name) {
+			continue
+		}
+
 		p.Contents[header.Name] = packageEntry{
 			File: header.Name,
 			UID:  header.Uid,
@@ -680,6 +688,9 @@ func readZip(t *testing.T, zipFile string, inspectors ...inspector) (*packageFil
 
 	p := &packageFile{Name: filepath.Base(zipFile), Contents: map[string]packageEntry{}}
 	for _, f := range r.File {
+		if excludedPathsPattern.MatchString(f.Name) {
+			continue
+		}
 		p.Contents[f.Name] = packageEntry{
 			File: f.Name,
 			Mode: f.Mode(),
@@ -767,6 +778,9 @@ func readDocker(dockerFile string) (*packageFile, *dockerInfo, error) {
 			// Check only files in working dir and entrypoint
 			if strings.HasPrefix("/"+name, workingDir) || "/"+name == entrypoint {
 				p.Contents[name] = entry
+			}
+			if excludedPathsPattern.MatchString(name) {
+				continue
 			}
 			// Add also licenses
 			for _, licenseFile := range licenseFiles {
