f208938d8146b29f0bf8f782924c5b4eb7df7501
emilioalvap <emilio.alvarezpineiro@elastic.co>
2022-12-05 16:14:26 +0100

    Fix linter for darwin

--- x-pack/heartbeat/monitors/browser/synthexec/synthexec_linux.go
+++ x-pack/heartbeat/monitors/browser/synthexec/synthexec_linux.go
@@ -1,8 +1,8 @@
 // Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
 // or more contributor license agreements. Licensed under the Elastic License;
 // you may not use this file except in compliance with the Elastic License.
-//go:build linux || darwin
-// +build linux darwin
+//go:build linux
+// +build linux
 
 package synthexec
 
