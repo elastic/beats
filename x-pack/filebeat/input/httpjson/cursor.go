ce96a506c76a94efd9bfe8b652b5026d6036a9da
Marc Guasch <marc-gr@users.noreply.github.com>
2022-12-23 10:20:06 +0100

    [filebeat] Change how we debug tpl values in httpjson (#33254)
    * Change how we debug tpl values
    
    * Change to use a switch statement
    
    * Add changelog entry
    
    * Update CHANGELOG.next.asciidoc
    
    Co-authored-by: ShourieG <105607378+ShourieG@users.noreply.github.com>
    (cherry picked from commit 32ab1168c2f5ce7442aec6576ccd53978cb3abc5)
    
    # Conflicts:
    #	x-pack/filebeat/input/httpjson/cursor.go
    #	x-pack/filebeat/input/httpjson/rate_limiter.go
    #	x-pack/filebeat/input/httpjson/request_chain_helper.go
    #	x-pack/filebeat/input/httpjson/transform_append.go
    #	x-pack/filebeat/input/httpjson/transform_set.go
    #	x-pack/filebeat/input/httpjson/value_tpl.go
    #	x-pack/filebeat/input/httpjson/value_tpl_test.go

---
+++ x-pack/filebeat/input/httpjson/cursor.go
@@ -0,0 +1,66 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package httpjson
+
+import (
+	inputcursor "github.com/elastic/beats/v7/filebeat/input/v2/input-cursor"
+	"github.com/elastic/elastic-agent-libs/logp"
+	"github.com/elastic/elastic-agent-libs/mapstr"
+)
+
+type cursor struct {
+	log *logp.Logger
+
+	cfg cursorConfig
+
+	state mapstr.M
+}
+
+func newCursor(cfg cursorConfig, log *logp.Logger) *cursor {
+	return &cursor{cfg: cfg, log: log}
+}
+
+func (c *cursor) load(cursor *inputcursor.Cursor) {
+	if c == nil || cursor == nil || cursor.IsNew() {
+		c.log.Debug("new cursor: nothing loaded")
+		return
+	}
+
+	if c.state == nil {
+		c.state = mapstr.M{}
+	}
+
+	if err := cursor.Unpack(&c.state); err != nil {
+		c.log.Errorf("Reset cursor state. Failed to read from registry: %v", err)
+		return
+	}
+
+	c.log.Debugf("cursor loaded: %v", c.state)
+}
+
+func (c *cursor) update(trCtx *transformContext) {
+	if c.cfg == nil {
+		return
+	}
+
+	if c.state == nil {
+		c.state = mapstr.M{}
+	}
+
+	for k, cfg := range c.cfg {
+		v, _ := cfg.Value.Execute(trCtx, transformable{}, "", cfg.Default, c.log)
+		if v != "" || !cfg.mustIgnoreEmptyValue() {
+			_, _ = c.state.Put(k, v)
+			c.log.Debugf("cursor.%s stored with %s", k, v)
+		}
+	}
+}
+
+func (c *cursor) clone() mapstr.M {
+	if c == nil || c.state == nil {
+		return mapstr.M{}
+	}
+	return c.state.Clone()
+}
