9a432edb339da38cb03dd981940835d8a14bdb2a
Fae Charlton <fae.charlton@elastic.co>
2023-01-25 16:30:42 -0500

    lint

--- libbeat/publisher/queue/proxy/queue_test.go
+++ libbeat/publisher/queue/proxy/queue_test.go
@@ -2,11 +2,10 @@ package proxyqueue
 
 import (
 	"testing"
-
-	"github.com/elastic/elastic-agent-libs/logp"
 )
 
 func TestQueueStuff(t *testing.T) {
-	logger := logp.NewLogger("proxy-queue-tests")
-	testQueue := NewQueue(logger, Settings{BatchSize: 2})
+	// TODO: document and test the proxy loop's data invariants
+	/*logger := logp.NewLogger("proxy-queue-tests")
+	testQueue := NewQueue(logger, Settings{BatchSize: 2})*/
 }
