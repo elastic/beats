27df2c54a177bbd7b487b7a0202d2bb40efd8348
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-02-02 14:22:37 +0100

    simplify condition

--- .ci/scripts/test-golang-version.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-if grep -q "$1" .go-version ; then
-  echo "found $1. No need to do nothing"
-  # return false
-  exit 1
-fi
-echo "not found $1. Need to update the file"
-# return true
-exit 0
