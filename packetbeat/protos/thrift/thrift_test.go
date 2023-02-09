582cbc04ebf83f49cb406e9581581673e8aa9d2f
ShourieG <105607378+ShourieG@users.noreply.github.com>
2023-02-09 14:26:47 +0530

    [Packetbeat] Fix for race condition in ProcessWatcher (#34514)
    * initial commit to fix packetbeat race condition
    
    * updated changelog
    
    * fixed issues with packetbeat flow & thrift tests, updated changelog
    
    (cherry picked from commit 2f9fa7199e0f1693fe2f131de2dd2f3354498365)
    
    # Conflicts:
    #	packetbeat/sniffer/decoders.go

--- packetbeat/protos/thrift/thrift_test.go
+++ packetbeat/protos/thrift/thrift_test.go
@@ -34,7 +34,7 @@ import (
 func thriftForTests() *thriftPlugin {
 	t := &thriftPlugin{}
 	config := defaultConfig
-	t.init(true, nil, procs.ProcessesWatcher{}, &config)
+	t.init(true, nil, &procs.ProcessesWatcher{}, &config)
 	return t
 }
 
