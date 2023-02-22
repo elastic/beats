1e23b10132643002780dba9314a28bc960865731
Denis Rechkunov <denis.rechkunov@elastic.co>
2023-02-22 15:41:09 +0100

    Add `SafeProcessor` wrapper
    Each processor might end up in multiple processor groups.
    Every group has its own `Close` that calls `Close` on each
    processor of that group which leads to multiple `Close` calls
    on the same processor.
    
    If the `SafeProcessor` wrapper was not used,
    the processor would have to handle multiple `Close` calls with
    adding `sync.Once` in its `Close` function.
    
    We make it easer for processor developers and take care of it
    in the processor registry instead.

---
+++ libbeat/processors/safe_processor_test.go
@@ -0,0 +1,84 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package processors
+
+import (
+	"testing"
+
+	"github.com/elastic/beats/v7/libbeat/beat"
+	"github.com/elastic/elastic-agent-libs/config"
+	"github.com/stretchr/testify/require"
+)
+
+type mockProcessor struct {
+	runCount   int
+	closeCount int
+}
+
+func newMockConstructor() (Constructor, *mockProcessor) {
+	p := mockProcessor{}
+	constructor := func(config *config.C) (Processor, error) {
+		return &p, nil
+	}
+	return constructor, &p
+}
+
+func (p *mockProcessor) Run(event *beat.Event) (*beat.Event, error) {
+	p.runCount++
+	return nil, nil
+}
+
+func (p *mockProcessor) Close() error {
+	p.closeCount++
+	return nil
+}
+func (p *mockProcessor) String() string {
+	return "mock-processor"
+}
+
+func TestSafeProcessor(t *testing.T) {
+	cons, p := newMockConstructor()
+	var (
+		sp  Processor
+		err error
+	)
+	t.Run("creates a wrapped processor", func(t *testing.T) {
+		sw := SafeWrap(cons)
+		sp, err = sw(nil)
+		require.NoError(t, err)
+	})
+
+	t.Run("propagates Run to a processor", func(t *testing.T) {
+		sp.Run(nil)
+		sp.Run(nil)
+		require.Equal(t, 2, p.runCount)
+	})
+
+	t.Run("propagates Close to a processor only once", func(t *testing.T) {
+		Close(sp)
+		Close(sp)
+		require.Equal(t, 1, p.closeCount)
+	})
+
+	t.Run("does not propagate Run when closed", func(t *testing.T) {
+		e, err := sp.Run(nil)
+		require.Nil(t, e)
+		require.ErrorIs(t, err, ErrClosed)
+		require.Equal(t, 2, p.runCount) // still 2 from the previous test case
+	})
+}
