cc345098670c23f6802c634e6d4c91196b9a3094
ritalwar <richa.talwar@elastic.co>
2023-02-23 10:11:44 +0530

    Update status_integration_test.go

--- metricbeat/module/mongodb/status/status_integration_test.go
+++ metricbeat/module/mongodb/status/status_integration_test.go
@@ -21,8 +21,9 @@
 package status
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 
 	"github.com/elastic/beats/v7/libbeat/tests/compose"
 	mbtest "github.com/elastic/beats/v7/metricbeat/mb/testing"
