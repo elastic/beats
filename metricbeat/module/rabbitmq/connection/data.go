33e0a27a50573d5e64f285d136a07149c397e93a
兆鹏 黄 <hzp84@qq.com>
2023-03-09 00:40:09 +0800

    chore(): rabbitmq add metrics

--- metricbeat/module/rabbitmq/connection/data.go
+++ metricbeat/module/rabbitmq/connection/data.go
@@ -18,86 +18,92 @@
 package connection
 
 import (
-	"encoding/json"
+    "encoding/json"
 
-	"github.com/pkg/errors"
+    "github.com/pkg/errors"
 
-	s "github.com/elastic/beats/v7/libbeat/common/schema"
-	c "github.com/elastic/beats/v7/libbeat/common/schema/mapstriface"
-	"github.com/elastic/beats/v7/metricbeat/mb"
-	"github.com/elastic/elastic-agent-libs/mapstr"
+    s "github.com/elastic/beats/v7/libbeat/common/schema"
+    c "github.com/elastic/beats/v7/libbeat/common/schema/mapstriface"
+    "github.com/elastic/beats/v7/metricbeat/mb"
+    "github.com/elastic/elastic-agent-libs/mapstr"
 )
 
 var (
-	schema = s.Schema{
-		"name": c.Str("name"),
-		"client_provided": s.Object{
-			"name": c.Str("client_properties.connection_name"),
-		},
-		"vhost":       c.Str("vhost", s.Required),
-		"user":        c.Str("user", s.Required),
-		"node":        c.Str("node", s.Required),
-		"state":       c.Str("state"),
-		"channels":    c.Int("channels"),
-		"channel_max": c.Int("channel_max"),
-		"frame_max":   c.Int("frame_max"),
-		"type":        c.Str("type"),
-		"packet_count": s.Object{
-			"sent":     c.Int("send_cnt"),
-			"received": c.Int("recv_cnt"),
-			"pending":  c.Int("send_pend"),
-		},
-		"octet_count": s.Object{
-			"sent":     c.Int("send_oct"),
-			"received": c.Int("recv_oct"),
-		},
-		"host": c.Str("host"),
-		"port": c.Int("port"),
-		"peer": s.Object{
-			"host": c.Str("peer_host"),
-			"port": c.Int("peer_port"),
-		},
-	}
+    schema = s.Schema{
+        "name": c.Str("name"),
+        "client_provided": s.Object{
+            "name": c.Str("client_properties.connection_name"),
+        },
+        "vhost":       c.Str("vhost", s.Required),
+        "user":        c.Str("user", s.Required),
+        "node":        c.Str("node", s.Required),
+        "state":       c.Str("state"),
+        "channels":    c.Int("channels"),
+        "channel_max": c.Int("channel_max"),
+        "frame_max":   c.Int("frame_max"),
+        "type":        c.Str("type"),
+        "packet_count": s.Object{
+            "sent":     c.Int("send_cnt"),
+            "received": c.Int("recv_cnt"),
+            "pending":  c.Int("send_pend"),
+        },
+        "octet_count": s.Object{
+            "sent":     c.Int("send_oct"),
+            "received": c.Int("recv_oct"),
+        },
+        "recv_octet_details": c.Dict("recv_octet_details", s.Schema{
+            "rate": c.Float("rate"),
+        }),
+        "send_octet_details": c.Dict("send_octet_details", s.Schema{
+            "rate": c.Float("rate"),
+        }),
+        "host": c.Str("host"),
+        "port": c.Int("port"),
+        "peer": s.Object{
+            "host": c.Str("peer_host"),
+            "port": c.Int("peer_port"),
+        },
+    }
 )
 
 func eventsMapping(content []byte, r mb.ReporterV2) error {
-	var connections []map[string]interface{}
-	err := json.Unmarshal(content, &connections)
-	if err != nil {
-		return errors.Wrap(err, "error in unmarshal")
-	}
+    var connections []map[string]interface{}
+    err := json.Unmarshal(content, &connections)
+    if err != nil {
+        return errors.Wrap(err, "error in unmarshal")
+    }
 
-	for _, node := range connections {
-		evt := eventMapping(node)
-		r.Event(evt)
-	}
-	return nil
+    for _, node := range connections {
+        evt := eventMapping(node)
+        r.Event(evt)
+    }
+    return nil
 }
 
 func eventMapping(connection map[string]interface{}) mb.Event {
-	fields, _ := schema.Apply(connection, s.FailOnRequired)
+    fields, _ := schema.Apply(connection, s.FailOnRequired)
 
-	rootFields := mapstr.M{}
-	if v, err := fields.GetValue("user"); err == nil {
-		rootFields.Put("user.name", v)
-		fields.Delete("user")
-	}
+    rootFields := mapstr.M{}
+    if v, err := fields.GetValue("user"); err == nil {
+        rootFields.Put("user.name", v)
+        fields.Delete("user")
+    }
 
-	moduleFields := mapstr.M{}
-	if v, err := fields.GetValue("vhost"); err == nil {
-		moduleFields.Put("vhost", v)
-		fields.Delete("vhost")
-	}
+    moduleFields := mapstr.M{}
+    if v, err := fields.GetValue("vhost"); err == nil {
+        moduleFields.Put("vhost", v)
+        fields.Delete("vhost")
+    }
 
-	if v, err := fields.GetValue("node"); err == nil {
-		moduleFields.Put("node.name", v)
-		fields.Delete("node")
-	}
+    if v, err := fields.GetValue("node"); err == nil {
+        moduleFields.Put("node.name", v)
+        fields.Delete("node")
+    }
 
-	event := mb.Event{
-		MetricSetFields: fields,
-		RootFields:      rootFields,
-		ModuleFields:    moduleFields,
-	}
-	return event
+    event := mb.Event{
+        MetricSetFields: fields,
+        RootFields:      rootFields,
+        ModuleFields:    moduleFields,
+    }
+    return event
 }
