7e9920575e02e4a621d27384fc874de7d7bff2ab
Andrew Kroh <andrew.kroh@elastic.co>
2023-03-22 09:10:17 -0400

    Update libbeat/monitoring/inputmon/httphandler.go
    Co-authored-by: Dan Kortschak <90160302+efd6@users.noreply.github.com>

--- libbeat/monitoring/inputmon/httphandler.go
+++ libbeat/monitoring/inputmon/httphandler.go
@@ -74,7 +74,7 @@ func (h *handler) allInputs(w http.ResponseWriter, req *http.Request) {
 		}
 
 		// Require all entries to have an 'input' and 'id' to be accessed through this API.
-		if _, ok = m["id"].(string); !ok {
+		if id, ok = m["id"].(string); !ok || id == "" {
 			continue
 		}
 
