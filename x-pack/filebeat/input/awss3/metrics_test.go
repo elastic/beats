a26f2750bb4984e637e356ba8081fad09d49f666
Kylie Meli <kylie.geller@gmail.com>
2023-03-28 10:04:30 -0400

    Update x-pack/filebeat/input/awss3/metrics_test.go
    Co-authored-by: Andrew Kroh <andrew.kroh@elastic.co>

--- x-pack/filebeat/input/awss3/metrics_test.go
+++ x-pack/filebeat/input/awss3/metrics_test.go
@@ -20,7 +20,7 @@ import (
 func TestInputMetricsClose(t *testing.T) {
 	reg := monitoring.NewRegistry()
 
-	metrics := newInputMetrics("aws-s3-aws.cloudfront_logs-8b312b5f-9f99-492c-b035-3dff354a1f01", reg, defaultConfig().MaxNumberOfMessages)
+	metrics := newInputMetrics("aws-s3-aws.cloudfront_logs-8b312b5f-9f99-492c-b035-3dff354a1f01", reg, 1)
 	metrics.Close()
 
 	reg.Do(monitoring.Full, func(s string, _ interface{}) {
