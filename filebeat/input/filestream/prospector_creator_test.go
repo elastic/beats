664fd58567c793912ddb1d2438aaca99220856c6
Denis Rechkunov <denis@rdner.de>
2023-03-30 09:28:43 +0200

    Fix linter issues

--- filebeat/input/filestream/prospector_creator_test.go
+++ filebeat/input/filestream/prospector_creator_test.go
@@ -6,7 +6,7 @@
 // not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing,
 // software distributed under the License is distributed on an
@@ -14,8 +14,6 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-
-// nolint: errcheck // It's a test file
 package filestream
 
 import (
