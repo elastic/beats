969b96e9b5462dc937b6c2e02b0a3e1afac0891e
Mika Ayenson <Mika.ayenson@elastic.co>
2023-04-10 16:30:13 -0400

    Add file analysis extension

---
+++ x-pack/osquerybeat/ext/osquery-extension/pkg/tables/host_file_analysis.go
@@ -0,0 +1,95 @@
+package tables
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"syscall"
+
+	"github.com/osquery/osquery-go/plugin/table"
+)
+
+func FileAnalysisColumns() []table.ColumnDefinition {
+	return []table.ColumnDefinition{
+		table.TextColumn("path"),
+		table.TextColumn("mode"),
+		table.TextColumn("uid"),
+		table.TextColumn("gid"),
+		table.TextColumn("size"),
+		table.TextColumn("mtime"),
+		table.TextColumn("file_type"),
+		table.TextColumn("code_sign"),
+		table.TextColumn("dependencies"),
+		table.TextColumn("symbols"),
+		table.TextColumn("strings"),
+	}
+}
+
+func GetFileAnalysisGenerateFunc() table.GenerateFunc {
+	return func(ctx context.Context, queryContext table.QueryContext) ([]map[string]string, error) {
+		var results []map[string]string
+
+		pathConstraint, exists := queryContext.Constraints["path"]
+		if !exists {
+			return results, nil
+		}
+
+		var path *string
+		for _, constraint := range pathConstraint.Constraints {
+			path = &constraint.Expression
+			break
+		}
+		if path == nil {
+			return results, nil
+		}
+
+		stat, err := os.Stat(*path)
+		if err != nil {
+			return results, nil
+		}
+
+		sys, ok := stat.Sys().(*syscall.Stat_t)
+		if !ok {
+			return results, fmt.Errorf("unable to convert stat.Sys() to *syscall.Stat_t")
+		}
+
+		mode := fmt.Sprintf("%o", stat.Mode().Perm())
+		uid := strconv.Itoa(int(sys.Uid))
+		gid := strconv.Itoa(int(sys.Gid))
+		size := strconv.Itoa(int(stat.Size()))
+		mtime := strconv.FormatInt(stat.ModTime().Unix(), 10)
+
+		// Execute macOS commands
+		fileType, _ := exec.Command("file", *path).Output()
+		codeSign, _ := exec.Command("codesign", "-dvvv", *path).Output()
+		dependencies, _ := exec.Command("otool", "-L", *path).Output()
+		symbols, _ := exec.Command("nm", *path).Output()
+		stringsOutput, _ := exec.Command("strings", "-a", *path).Output()
+
+		// Convert outputs to strings
+		fileTypeStr := strings.TrimSpace(string(fileType))
+		codeSignStr := strings.TrimSpace(string(codeSign))
+		dependenciesStr := strings.TrimSpace(string(dependencies))
+		symbolsStr := strings.TrimSpace(string(symbols))
+		stringsStr := strings.TrimSpace(string(stringsOutput))
+
+		results = append(results, map[string]string{
+			"path":         *path,
+			"mode":         mode,
+			"uid":          uid,
+			"gid":          gid,
+			"size":         size,
+			"mtime":        mtime,
+			"file_type":    fileTypeStr,
+			"code_sign":    codeSignStr,
+			"dependencies": dependenciesStr,
+			"symbols":      symbolsStr,
+			"strings":      stringsStr,
+		})
+
+		return results, nil
+	}
+}
