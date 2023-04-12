14f86013cbba4f8bb63451768cb384aa0f32543f
Fae Charlton <fae.charlton@elastic.co>
2023-04-12 16:27:20 -0400

    fix client tests

--- libbeat/publisher/pipeline/client_test.go
+++ libbeat/publisher/pipeline/client_test.go
@@ -49,6 +49,8 @@ func TestClient(t *testing.T) {
 		if err != nil {
 			panic(err)
 		}
+		// Close the built-in queue and replace with the given one.
+		p.outputController.queue.Close()
 		p.outputController.queue = qu
 
 		return p
@@ -135,6 +137,8 @@ func TestClientWaitClose(t *testing.T) {
 		if err != nil {
 			panic(err)
 		}
+		// Close the built-in queue and replace with the given one.
+		p.outputController.queue.Close()
 		p.outputController.queue = qu
 
 		return p
