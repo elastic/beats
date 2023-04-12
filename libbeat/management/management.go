3dcbbac5f3504292c51b2ad2363f7bd1f6abf8ca
Alex K <8418476+fearful-symmetry@users.noreply.github.com>
2023-04-12 07:11:04 -0700

    Fix performance issues with processors scaling under agent (#35031)
    * fix performance issues with processors scaling under agent
    
    * make linter happy
    
    * fix test
    
    * add comment
    
    * move around defaultProcessors
    
    * fix tests, add diagnostics
    
    * fix default processor on filebeat
    
    * change log line
    
    * Update CHANGELOG.next.asciidoc
    
    Adding changelog entry
    
    ---------
    
    Co-authored-by: Pierre HILBERT <pierre.hilbert@elastic.co>
    (cherry picked from commit ea1293fdb0864655554a767a96f22aeb8b052200)
    
    # Conflicts:
    #	libbeat/management/management.go

--- libbeat/management/management.go
+++ libbeat/management/management.go
@@ -109,6 +109,9 @@ type Manager interface {
 
 	// SetPayload Allows to add additional metadata to future requests made by the manager.
 	SetPayload(map[string]interface{})
+
+	// RegisterDiagnosticHook registers a callback for elastic-agent diagnostics
+	RegisterDiagnosticHook(name string, description string, filename string, contentType string, hook client.DiagnosticHook)
 }
 
 // PluginFunc for creating FactoryFunc if it matches a config
@@ -190,4 +193,19 @@ func (n *nilManager) RegisterAction(action client.Action) {}
 
 func (n *nilManager) UnregisterAction(action client.Action) {}
 
+<<<<<<< HEAD
 func (n *nilManager) SetPayload(map[string]interface{}) {}
+=======
+// Enabled returns false because management is disabled.
+// the nilManager is still used for shutdown on some cases,
+// but that does not mean the Beat is being managed externally,
+// hence it will always return false.
+func (n *fallbackManager) Enabled() bool                         { return false }
+func (n *fallbackManager) Start() error                          { return nil }
+func (n *fallbackManager) CheckRawConfig(cfg *config.C) error    { return nil }
+func (n *fallbackManager) RegisterAction(action client.Action)   {}
+func (n *fallbackManager) UnregisterAction(action client.Action) {}
+func (n *fallbackManager) SetPayload(map[string]interface{})     {}
+func (n *fallbackManager) RegisterDiagnosticHook(_ string, _ string, _ string, _ string, _ client.DiagnosticHook) {
+}
+>>>>>>> ea1293fdb0 (Fix performance issues with processors scaling under agent (#35031))
