9ad71002188e24277012c83cfebb2d9f4031ffed
Fae Charlton <fae.charlton@elastic.co>
2023-04-17 16:13:53 -0400

    disable flaky test for real

--- x-pack/metricbeat/module/sql/query/test_sql_oracle.py
+++ x-pack/metricbeat/module/sql/query/test_sql_oracle.py
@@ -5,12 +5,12 @@ import time
 from xpack_metricbeat import XPackTest, metricbeat
 
 
+@unittest.skip("Flaky test: https://github.com/elastic/beats/issues/34993")
 class Test(XPackTest):
 
     COMPOSE_SERVICES = ['oracle']
 
-    # @unittest.skipUnless(metricbeat.INTEGRATION_TESTS, "integration test")
-    @unittest.skip("Flaky test: https://github.com/elastic/beats/issues/34993")
+    @unittest.skipUnless(metricbeat.INTEGRATION_TESTS, "integration test")
     def test_query(self):
         """
         sql oracle custom query test
