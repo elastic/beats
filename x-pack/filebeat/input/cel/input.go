057cca1474d57bef2d3b4489af589fa5069ce7c8
Bharat Pasupula <123897612+bhapas@users.noreply.github.com>
2023-04-25 08:28:55 +0200

    Revert "x-pack/filebeat : Sanitize cel input resource trace filename (#35154) (#35201)"
    This reverts commit 0371f915b505bc8b501359b87842d0eb9b6ae467.

--- x-pack/filebeat/input/cel/input.go
+++ x-pack/filebeat/input/cel/input.go
@@ -18,7 +18,6 @@ import (
 	"net"
 	"net/http"
 	"net/url"
-	"path/filepath"
 	"reflect"
 	"regexp"
 	"strconv"
@@ -102,15 +101,6 @@ func (input) Run(env v2.Context, src inputcursor.Source, crsr inputcursor.Cursor
 	return input{}.run(env, src.(*source), cursor, pub)
 }
 
-// sanitizeFileName returns name with ":" and "/" replaced with "_", removing repeated instances.
-// The request.tracer.filename may have ":" when a httpjson input has cursor config and
-// the macOS Finder will treat this as path-separator and causes to show up strange filepaths.
-func sanitizeFileName(name string) string {
-	name = strings.ReplaceAll(name, ":", string(filepath.Separator))
-	name = filepath.Clean(name)
-	return strings.ReplaceAll(name, string(filepath.Separator), "_")
-}
-
 func (input) run(env v2.Context, src *source, cursor map[string]interface{}, pub inputcursor.Publisher) error {
 	cfg := src.cfg
 	log := env.Logger.With("input_url", cfg.Resource.URL)
@@ -121,8 +111,7 @@ func (input) run(env v2.Context, src *source, cursor map[string]interface{}, pub
 	ctx := ctxtool.FromCanceller(env.Cancelation)
 
 	if cfg.Resource.Tracer != nil {
-		id := sanitizeFileName(env.ID)
-		cfg.Resource.Tracer.Filename = strings.ReplaceAll(cfg.Resource.Tracer.Filename, "*", id)
+		cfg.Resource.Tracer.Filename = strings.ReplaceAll(cfg.Resource.Tracer.Filename, "*", env.ID)
 	}
 
 	client, err := newClient(ctx, cfg, log)
@@ -674,11 +663,6 @@ func newClient(ctx context.Context, cfg config, log *logp.Logger) (*http.Client,
 
 	if cfg.Resource.Tracer != nil {
 		w := zapcore.AddSync(cfg.Resource.Tracer)
-		go func() {
-			// Close the logger when we are done.
-			<-ctx.Done()
-			cfg.Resource.Tracer.Close()
-		}()
 		core := ecszap.NewCore(
 			ecszap.NewDefaultEncoderConfig(),
 			w,
--- x-pack/filebeat/input/httpjson/input_test.go
+++ x-pack/filebeat/input/httpjson/input_test.go
@@ -12,7 +12,6 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
-	"path/filepath"
 	"testing"
 	"time"
 
@@ -26,14 +25,12 @@ import (
 )
 
 func TestInput(t *testing.T) {
-	tempDirectory := t.TempDir()
 	testCases := []struct {
-		name         string
-		setupServer  func(*testing.T, http.HandlerFunc, map[string]interface{})
-		baseConfig   map[string]interface{}
-		handler      http.HandlerFunc
-		expected     []string
-		expectedFile string
+		name        string
+		setupServer func(*testing.T, http.HandlerFunc, map[string]interface{})
+		baseConfig  map[string]interface{}
+		handler     http.HandlerFunc
+		expected    []string
 	}{
 		{
 			name:        "Test simple GET request",
@@ -297,7 +294,7 @@ func TestInput(t *testing.T) {
 			},
 		},
 		{
-			name: "Test tracer filename sanitization",
+			name: "Test filename truncation",
 			setupServer: func(t *testing.T, h http.HandlerFunc, config map[string]interface{}) {
 				registerRequestTransforms()
 				t.Cleanup(func() { registeredTransforms = newRegistry() })
@@ -329,7 +326,8 @@ func TestInput(t *testing.T) {
 						"value": `[[index .last_response.body "@timestamp"]]`,
 					},
 				},
-				"request.tracer.filename": filepath.Join(tempDirectory, "logs", "http-request-trace-*.ndjson"),
+				"request.tracer.filename": "../../logs/httpjson/http-request-trace-*.ndjson",
+				"verifyfilepath":          true,
 			},
 			handler: dateCursorHandler(),
 			expected: []string{
@@ -337,7 +335,6 @@ func TestInput(t *testing.T) {
 				`{"@timestamp":"2002-10-02T15:00:01Z","foo":"bar"}`,
 				`{"@timestamp":"2002-10-02T15:00:02Z","foo":"bar"}`,
 			},
-			expectedFile: filepath.Join("logs", "http-request-trace-httpjson-foo-eb837d4c-5ced-45ed-b05c-de658135e248_https_somesource_someapi.ndjson"),
 		},
 		{
 			name: "Test pagination",
@@ -1238,8 +1235,8 @@ func TestInput(t *testing.T) {
 					}
 				}
 			}
-			if len(tc.expectedFile) != 0 {
-				if _, err := os.Stat(filepath.Join(tempDirectory, tc.expectedFile)); err == nil {
+			if tc.baseConfig["verifyfilepath"] != nil {
+				if _, err := os.Stat("../../logs/httpjson/http-request-trace-httpjson-foo-eb837d4c-5ced-45ed-b05c-de658135e248_https_somesource_someapi.ndjson"); err == nil {
 					assert.NoError(t, g.Wait())
 				} else {
 					t.Errorf("Expected log filename not found")
