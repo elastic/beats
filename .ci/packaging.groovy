0c0deb7281c366e2fc0cbf4c06af9945ac9e9fec
Shaunak Kashyap <ycombinator@gmail.com>
2023-05-03 16:24:14 -0700

    Revert "ci: disable upstream integration (#35179)" (#35321)
    This reverts commit a8bb5b5e98b0ee65c4b9a7f179b188b61171dab8.

--- .ci/packaging.groovy
+++ .ci/packaging.groovy
@@ -38,8 +38,7 @@ pipeline {
   triggers {
     issueCommentTrigger('(?i)^\\/packag[ing|e]$')
     // disable upstream trigger on a PR basis
-    // NOTE(v1v): disable the upstream integration for 8.7.1 release
-    //upstream("Beats/beats/${ env.JOB_BASE_NAME.startsWith('PR-') ? 'none' : env.JOB_BASE_NAME }")
+    upstream("Beats/beats/${ env.JOB_BASE_NAME.startsWith('PR-') ? 'none' : env.JOB_BASE_NAME }")
   }
   parameters {
     booleanParam(name: 'run_e2e', defaultValue: true, description: 'Allow to disable the e2e tets. This workaround will generate broken/buggy binaries.')
