d597f943dfbe4505dcea0b3dd2e4df2b750c62c9
emilioalvap <emilio.alvarezpineiro@elastic.co>
2023-05-08 12:12:32 +0200

    test

--- heartbeat/beater/heartbeat.go
+++ heartbeat/beater/heartbeat.go
@@ -18,6 +18,7 @@
 package beater
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"sync"
@@ -39,6 +40,7 @@ import (
 	"github.com/elastic/beats/v7/libbeat/autodiscover"
 	"github.com/elastic/beats/v7/libbeat/beat"
 	"github.com/elastic/beats/v7/libbeat/cfgfile"
+	"github.com/elastic/beats/v7/libbeat/common/backoff"
 	"github.com/elastic/beats/v7/libbeat/common/reload"
 	"github.com/elastic/beats/v7/libbeat/esleg/eslegclient"
 	"github.com/elastic/beats/v7/libbeat/management"
@@ -66,11 +68,20 @@ func New(b *beat.Beat, rawConfig *conf.C) (beat.Beater, error) {
 		return nil, fmt.Errorf("error reading config file: %w", err)
 	}
 
+	// Check if any of these can prevent using states client
 	stateLoader, replaceStateLoader := monitorstate.AtomicStateLoader(monitorstate.NilStateLoader)
 	if b.Config.Output.Name() == "elasticsearch" && !b.Manager.Enabled() {
 		// Connect to ES and setup the State loader if the output is not managed by agent
-		if err := makeStatesClient(b.Config.Output.Config(), replaceStateLoader, parsedConfig.RunFrom); err != nil {
-			logp.L().Warnf("could not connect to ES for state management during initial load: %s", err)
+		// Note this, intentionally, blocks until connected or max attempts reached
+		esClient, err := makeESClient(b.Config.Output.Config(), 3, 2*time.Second)
+		if err != nil {
+			if parsedConfig.RunOnce {
+				return nil, fmt.Errorf("run_once mode fatal error: %w", err)
+			} else {
+				logp.L().Warn(err)
+			}
+		} else {
+			replaceStateLoader(monitorstate.MakeESLoader(esClient, "synthetics-*,heartbeat-*", parsedConfig.RunFrom))
 		}
 	} else if b.Manager.Enabled() {
 		stateLoader, replaceStateLoader = monitorstate.DeferredStateLoader(monitorstate.NilStateLoader, 15*time.Second)
@@ -248,8 +259,11 @@ func (bt *Heartbeat) RunCentralMgmtMonitors(b *beat.Beat) {
 			return nil
 		}
 
-		if err := makeStatesClient(outCfg.Config(), bt.replaceStateLoader, bt.config.RunFrom); err != nil {
+		esClient, err := makeESClient(outCfg.Config(), 1, 0)
+		if err != nil {
 			logp.L().Warnf("could not connect to ES for state management during managed reload: %s", err)
+		} else {
+			bt.replaceStateLoader(monitorstate.MakeESLoader(esClient, "synthetics-*,heartbeat-*", bt.config.RunFrom))
 		}
 
 		return nil
@@ -263,7 +277,7 @@ func (bt *Heartbeat) RunCentralMgmtMonitors(b *beat.Beat) {
 func (bt *Heartbeat) RunReloadableMonitors() (err error) {
 	// Check monitor configs
 	if err := bt.monitorReloader.Check(bt.monitorFactory); err != nil {
-		logp.Error(fmt.Errorf("error loading reloadable monitors: %w", err))
+		logp.L().Error(fmt.Errorf("error loading reloadable monitors: %w", err))
 	}
 
 	// Execute the monitor
@@ -293,16 +307,27 @@ func (bt *Heartbeat) Stop() {
 	bt.stopOnce.Do(func() { close(bt.done) })
 }
 
-func makeStatesClient(cfg *conf.C, replace func(monitorstate.StateLoader), runFrom *config.LocationWithID) error {
-	esClient, err := eslegclient.NewConnectedClient(cfg, "Heartbeat")
-	if err != nil {
-		return err
+func makeESClient(cfg *conf.C, attempts int, wait time.Duration) (*eslegclient.Connection, error) {
+	var (
+		esClient *eslegclient.Connection
+		err      error
+	)
+
+	// ES client backoff
+	connectDelay := backoff.NewEqualJitterBackoff(
+		context.Background().Done(),
+		wait,
+		wait,
+	)
+
+	for i := 0; i < attempts; i++ {
+		esClient, err = eslegclient.NewConnectedClient(cfg, "Heartbeat")
+		if err == nil {
+			return esClient, nil
+		} else {
+			connectDelay.Wait()
+		}
 	}
 
-	if esClient != nil {
-		logp.L().Info("replacing states loader")
-		replace(monitorstate.MakeESLoader(esClient, "synthetics-*,heartbeat-*", runFrom))
-	}
-
-	return nil
+	return nil, fmt.Errorf("could not establish connection on %d attempts. Last error: %w", attempts, err)
 }
