676b3bc1d8edcedb4db862eba6dddde32d71e3c1
Lee E. Hinman <lee.e.hinman@elastic.co>
2023-05-19 13:52:58 -0500

    use recommended method for upgrading pip
    also revert vagrant setup changes

--- dev-tools/vagrant_scripts/winProvision.ps1
+++ dev-tools/vagrant_scripts/winProvision.ps1
@@ -58,19 +58,7 @@ if (-Not (Get-Command "python" -ErrorAction SilentlyContinue)) {
 }
 
 echo "Updating pip"
-For ($i = 0; $i -lt 5) {
-	try {
-		python -m pip install --upgrade pip 2>&1 | %{ "$_" }
-		Break
-	} catch {
-		Echo "updating pip failed"
-		$i++
-		if ($i -eq 5) {
-			Throw $_
-		}
-		Start-Sleep 5
-	}
-}
+python -m pip install --upgrade pip 2>&1 | %{ "$_" }
 
 if (-Not (Get-Command "git" -ErrorAction SilentlyContinue)) {
     echo "Installing git"
