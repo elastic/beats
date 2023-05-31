2ec629b728527c2335fa108a25790c1d0f3faa9c
Alex Kristiansen <alex.kristiansen@elastic.co>
2023-05-31 14:31:52 -0700

    fix core

--- metricbeat/module/system/core/core.go
+++ metricbeat/module/system/core/core.go
@@ -20,9 +20,8 @@
 package core
 
 import (
+	"fmt"
 	"runtime"
-
-	"github.com/pkg/errors"
 
 	"github.com/elastic/beats/v7/libbeat/common/diagnostics"
 	"github.com/elastic/beats/v7/metricbeat/mb"
@@ -53,7 +52,7 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 
 	opts, err := config.Validate()
 	if err != nil {
-		return nil, errors.Wrap(err, "error validating config")
+		return nil, fmt.Errorf("error validating config: %w", err)
 	}
 
 	if config.CPUTicks != nil && *config.CPUTicks {
@@ -71,14 +70,14 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 func (m *MetricSet) Fetch(report mb.ReporterV2) error {
 	samples, err := m.cores.FetchCores()
 	if err != nil {
-		return errors.Wrap(err, "failed to sample CPU core times")
+		return fmt.Errorf("failed to sample CPU core times: %w", err)
 
 	}
 
 	for id, sample := range samples {
 		event, err := sample.Format(m.opts)
 		if err != nil {
-			return errors.Wrap(err, "error formatting core data")
+			return fmt.Errorf("error formatting core data: %w", err)
 		}
 		event.Put("id", id)
 
