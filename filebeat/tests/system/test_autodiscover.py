ed78bb7b22e7db4d50142863dc8e5a0ef174de9b
Tiago Queiroz <tiago.queiroz@elastic.co>
2023-06-14 18:34:44 +0200

    improve test debug

--- filebeat/tests/system/test_autodiscover.py
+++ filebeat/tests/system/test_autodiscover.py
@@ -82,18 +82,18 @@ class TestAutodiscover(filebeat.BaseTest):
 
         def wait_container_start():
             for i, c in enumerate(docker_client.containers.list()):
-                print("[%d].name='%s', looking for '%s'" % (i, c.name, container.name))
                 if c.name == container.name:
                     return True
 
         self.wait_until(
             wait_container_start,
-            max_timeout=120,
             poll_interval=0.1,
-            name="wait busybox container running",
-            err_msg="busybox container is not running")
+            name="wait for test container",
+            err_msg="the test container is not running yet")
 
-        self.wait_until(lambda: self.log_contains('Starting runner: input'))
+        self.wait_until(lambda: self.log_contains('Starting runner: input'),
+                        name="wait for input to start",
+                        err_msg="did not find 'Starting runner: input' in the logs")
         self.wait_until(lambda: self.output_has(lines=1))
 
         output = self.read_output_json()
