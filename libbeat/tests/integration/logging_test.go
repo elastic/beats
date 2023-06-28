a63ac429342fa806d4ba384fdee380aa0b1f6f75
Amit Kanfer <amit.kanfer@elastic.co>
2023-06-28 13:30:56 +0200

    6

--- libbeat/tests/integration/logging_test.go
+++ libbeat/tests/integration/logging_test.go
@@ -4,33 +4,12 @@ package integration
 
 import (
 	"encoding/json"
-	"fmt"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-
-var cfg = `
-mockbeat:
-name:
-queue.mem:
-  events: 4096
-  flush.min_events: 8
-  flush.timeout: 0.1s
-output.console:
-  code.json:
-    pretty: false
-`
-
-func startMockBeat(t *testing.T, msg string, args ...string) BeatProc {
-	mockbeat := NewBeat(t, "mockbeat", "../../libbeat.test", append([]string{"-E", "http.enabled=true"}, args...)...)
-	mockbeat.WriteConfigFile(cfg)
-	mockbeat.Start()
-	mockbeat.WaitForLogs(msg, 60*time.Second, fmt.Sprintf("error waiting for log: %s", msg))
-	return mockbeat
-}
 
 func TestLoggingConsoleECS(t *testing.T) {
 	mockbeat := NewBeat(t, "mockbeat", "../../libbeat.test", "-E", "http.enabled=true", "-e")
@@ -51,6 +30,6 @@ func TestLoggingConsoleECS(t *testing.T) {
 	assert.True(t, ok)
 }
 
-// func TestLoggingFileDefault(t *testing.T) {
-// 	startMockBeat(t, "Mockbeat is alive!")
-// }
+func TestLoggingFileDefault(t *testing.T) {
+	startMockBeat(t, "Mockbeat is alive!")
+}
