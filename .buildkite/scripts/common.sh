55ef3c5046e308f8d3c61a05e731cc46ac4e87d7
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2023-07-03 17:48:02 +0300

    [elastic-agent-system-metrics] add retry

--- .buildkite/scripts/common.sh
+++ .buildkite/scripts/common.sh
@@ -32,3 +32,22 @@ install_go_dependencies() {
         go install "${pkg}@latest"
     done
 }
+
+retry() {
+    local retries=$1
+    shift
+    local count=0
+    until "$@"; do
+        exit=$?
+        wait=$((2 ** count))
+        count=$((count + 1))
+        if [ $count -lt "$retries" ]; then
+            >&2 echo "Retry $count/$retries exited $exit, retrying in $wait seconds..."
+            sleep $wait
+        else
+            >&2 echo "Retry $count/$retries exited $exit, no more retries left."
+            return $exit
+        fi
+    done
+    return 0
+}
