16b6398e324e4bd97267f2f309ce0fe3cbfccb9d
lucian-ioan <lucian.pyc@gmail.com>
2023-07-06 15:40:09 +0300

    Fix logger import

--- x-pack/filebeat/input/azureeventhub/sanitization_test.go
+++ x-pack/filebeat/input/azureeventhub/sanitization_test.go
@@ -13,7 +13,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/elastic/elastic-agent-libs/logp"
+	"github.com/elastic/beats/v7/libbeat/logp"
 )
 
 func TestParseMultipleMessagesSanitization(t *testing.T) {
