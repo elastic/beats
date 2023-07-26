e3102f14c1de84bbc4a712f11732b18d0f0259bc
“muthu-mps” <muthukumar.paramasivam@elastic.co>
2023-07-26 11:26:20 +0530

    address review comment

--- x-pack/metricbeat/module/azure/billing/billing.go
+++ x-pack/metricbeat/module/azure/billing/billing.go
@@ -129,7 +129,7 @@ func usageIntervalFrom(reference time.Time) (time.Time, time.Time) {
 // For example, if the reference time is 2007-01-09 09:41:00Z, the forecast period is:
 // The forecast data is fetched from current day - 2 and for next 30 days.
 //
-//	2007-01-08T00:00:00Z -> 2007-02-06:59:59Z
+//	2007-01-07T00:00:00Z -> 2007-02-05:59:59Z
 func forecastIntervalFrom(reference time.Time) (time.Time, time.Time) {
 	referenceUTC := reference.UTC().Truncate(24 * time.Hour).Add((-48) * time.Hour)
 	forecastStartDate := time.Date(referenceUTC.Year(), referenceUTC.Month(), referenceUTC.Day(), 0, 0, 0, 0, time.UTC)
