db08ea28a050fc22ebdfcd9474f0ce05a4bea060
Andrew Cholakian <andrew@andrewvc.com>
2023-08-15 13:59:16 -0500

    Check whether browsers can be tested

---
+++ x-pack/heartbeat/scenarios/browsercheck_test.go
@@ -0,0 +1,19 @@
+package scenarios
+
+import (
+	"os/exec"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestElasticSyntheticsRunnable(t *testing.T) {
+	// this test should fail if synthetics isn't correctly setup in the current environment
+	cmd := exec.Command("sh", "-c", "echo 'step(\"t\", () => { })' | elastic-synthetics --inline")
+	var out strings.Builder
+	cmd.Stdout = &out
+	cmd.Stderr = &out
+	cmd.Run()
+	require.Equal(t, 0, cmd.ProcessState.ExitCode(), "command exited with bad code: %s", out.String())
+}
