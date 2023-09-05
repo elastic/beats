41c05344a3615a5c2564fffa668d8a707b3bab0e
Andreas Gkizas <andreas.gkizas@elastic.co>
2023-09-05 17:07:17 +0300

    Adding comments for the change introduced

--- x-pack/metricbeat/cmd/root.go
+++ x-pack/metricbeat/cmd/root.go
@@ -73,7 +73,9 @@ func defaultProcessors() []mapstr.M {
 	//   - add_docker_metadata: ~
 	//   - add_kubernetes_metadata: ~
 
-	// We check for the existance of environmental variable NETINFO. Related to https://github.com/elastic/integrations/issues/6674
+	// Setting environmental variable NETINFO:false of Elastic Agent will disable the netinfo.enabled option of add_host_metadata processor.
+	// This will result to events not being enhanced with host.ip and host.mac
+	//Related to https://github.com/elastic/integrations/issues/6674
 	valueNETINFO, status := os.LookupEnv("NETINFO")
 
 	if valueNETINFO == "false" && status == true {
