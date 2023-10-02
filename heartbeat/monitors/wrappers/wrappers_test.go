4b9ecacc5ac6273165a29669693ab281ca26d715
Andrew Cholakian <andrewvc@elastic.co>
2023-10-02 12:04:30 -0500

    [Heartbeat] Only retry down monitors on first down check (#36719)
    * [Heartbeat] Only retry down monitors on first down check
    
    When monitors go down we don't want to keep using retries when
    it was previously down, we only want to retry when it goes down
    the first time per the retries spec.
    
    * Update failing test

--- heartbeat/monitors/wrappers/wrappers_test.go
+++ heartbeat/monitors/wrappers/wrappers_test.go
@@ -351,6 +351,10 @@ func TestMultiJobConts(t *testing.T) {
 	})
 }
 
+// TestRetryMultiCont is of somewhat dubious utility at the moment,
+// it mostly tests that we __don't__ retry on an initial down.
+// retry logic is better and more completely tested in the summarizer
+// and scenario tests.
 func TestRetryMultiCont(t *testing.T) {
 	uniqScope := isdef.ScopedIsUnique()
 
@@ -460,12 +464,6 @@ func TestRetryMultiCont(t *testing.T) {
 			retryMonFields,
 			[]jobs.Job{makeContJob(t, "http://foo.com")},
 			[]validator.Validator{
-				contJobValidator("http://foo.com", "1st"),
-				lookslike.Compose(
-					contJobValidator("http://foo.com", "2nd"),
-					summarizertesthelper.SummaryValidator(expected.js.Up, expected.js.Down),
-					hbtestllext.MaybeHasDuration,
-				),
 				contJobValidator("http://foo.com", "1st"),
 				lookslike.Compose(
 					contJobValidator("http://foo.com", "2nd"),
