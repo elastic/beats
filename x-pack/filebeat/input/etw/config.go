c76d49450f198565408fc97dbeb792fc48fa23e1
chemamartinez <chema.martinez@elastic.co>
2023-10-19 17:24:31 +0200

    First version of ETW input

---
+++ x-pack/filebeat/input/etw/config.go
@@ -0,0 +1,97 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package etw_input
+
+import (
+	"fmt"
+
+	"github.com/elastic/beats/v7/x-pack/libbeat/reader/etw"
+)
+
+var validTraceLevel = map[string]bool{
+	"critical":    true,
+	"error":       true,
+	"warning":     true,
+	"information": true,
+	"verbose":     true,
+}
+
+type config struct {
+	Logfile         string `config:"file"`
+	ProviderGUID    string `config:"provider.guid"`
+	ProviderName    string `config:"provider.name"`
+	SessionName     string `config:"session_name"` // Tag for the new session
+	TraceLevel      string `config:"trace_level"`
+	MatchAnyKeyword uint64 `config:"match_any_keyword"`
+	MatchAllKeyword uint64 `config:"match_all_keyword"`
+	Session         string `config:"session"`
+}
+
+// Create a conversion function to convert config to etw.Config
+func convertConfig(cfg config) etw.Config {
+	return etw.Config{
+		Logfile:         cfg.Logfile,
+		ProviderGUID:    cfg.ProviderGUID,
+		ProviderName:    cfg.ProviderName,
+		SessionName:     cfg.SessionName,
+		TraceLevel:      cfg.TraceLevel,
+		MatchAnyKeyword: cfg.MatchAnyKeyword,
+		MatchAllKeyword: cfg.MatchAllKeyword,
+		Session:         cfg.Session,
+	}
+}
+
+func defaultConfig() config {
+	return config{
+		Logfile:         "",
+		ProviderName:    "",
+		ProviderGUID:    "",
+		SessionName:     "",
+		TraceLevel:      "verbose",
+		MatchAnyKeyword: 0xffffffffffffffff,
+		MatchAllKeyword: 0,
+		Session:         "",
+	}
+}
+
+// Config validation
+func (c *config) validate() error {
+	if c.ProviderName == "" && c.ProviderGUID == "" && c.Logfile == "" && c.Session == "" {
+		return fmt.Errorf("provider, existing logfile or running session must be set")
+	}
+
+	if !validTraceLevel[c.TraceLevel] {
+		return fmt.Errorf("invalid Trace Level value '%s'", c.TraceLevel)
+	}
+
+	if c.ProviderGUID != "" {
+		if c.ProviderName != "" {
+			return fmt.Errorf("configuration constraint error: provider GUID and provider name cannot be defined together")
+		}
+		if c.Logfile != "" {
+			return fmt.Errorf("configuration constraint error: provider GUID and file cannot be defined together")
+		}
+		if c.Session != "" {
+			return fmt.Errorf("configuration constraint error: provider GUID and existing session cannot be defined together")
+		}
+	}
+
+	if c.ProviderName != "" {
+		if c.Logfile != "" {
+			return fmt.Errorf("configuration constraint error: provider name and file cannot be defined together")
+		}
+		if c.Session != "" {
+			return fmt.Errorf("configuration constraint error: provider name and existing session cannot be defined together")
+		}
+	}
+
+	if c.Logfile != "" {
+		if c.Session != "" {
+			return fmt.Errorf("configuration constraint error: file and existing session cannot be defined together")
+		}
+	}
+
+	return nil
+}
---
+++ x-pack/filebeat/input/etw/input.go
@@ -0,0 +1,146 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package etw_input
+
+import (
+	"fmt"
+	"sync"
+	"syscall"
+	"time"
+
+	input "github.com/elastic/beats/v7/filebeat/input/v2"
+	stateless "github.com/elastic/beats/v7/filebeat/input/v2/input-stateless"
+	"github.com/elastic/beats/v7/libbeat/beat"
+	"github.com/elastic/beats/v7/libbeat/feature"
+	"github.com/elastic/beats/v7/x-pack/libbeat/reader/etw"
+	conf "github.com/elastic/elastic-agent-libs/config"
+	"github.com/elastic/elastic-agent-libs/logp"
+	"github.com/elastic/elastic-agent-libs/mapstr"
+)
+
+const (
+	inputName = "etw"
+)
+
+type etw_input struct {
+	log        *logp.Logger
+	config     config
+	etwSession etw.Session
+}
+
+func Plugin() input.Plugin {
+	return input.Plugin{
+		Name:      inputName,
+		Stability: feature.Beta,
+		Info:      "Collect ETW logs.",
+		Manager:   stateless.NewInputManager(configure),
+	}
+}
+
+func configure(cfg *conf.C) (stateless.Input, error) {
+	conf := defaultConfig()
+	if err := cfg.Unpack(&conf); err != nil {
+		return nil, err
+	}
+
+	return newETW(conf)
+}
+
+func newETW(config config) (*etw_input, error) {
+	if err := config.validate(); err != nil {
+		return nil, err
+	}
+
+	return &etw_input{
+		config: config,
+	}, nil
+}
+
+func (e *etw_input) Name() string { return inputName }
+
+func (e *etw_input) Test(_ input.TestContext) error {
+	// ToDo
+	return nil
+}
+
+func (e *etw_input) Run(ctx input.Context, publisher stateless.Publisher) error {
+	var err error
+	e.etwSession, err = etw.NewSession(convertConfig(e.config))
+	if err != nil {
+		return fmt.Errorf("error when inicializing '%s' session: %v", e.etwSession.Name, err)
+	}
+
+	e.log := ctx.Logger.With("session", e.etwSession.Name)
+	e.log.Info("Starting " + inputName + " input")
+
+	var wg sync.WaitGroup
+
+	if e.etwSession.Realtime {
+		err = e.etwSession.CreateRealtimeSession()
+		if err != nil {
+			return fmt.Errorf("realtime session could not be created: %v", e.etwSession.Name, err)
+		}
+		e.log.Debug("created session")
+	}
+	defer func() {
+		wg.Wait() // Wait for the goroutine to finish
+		e.close()
+		e.log.Info(inputName + " input stopped")
+	}()
+
+	// Define callback that will process ETW events
+	// Callback which receives every ETW event from the reading source
+	eventReceivedCallback := func(er *etw.EventRecord) {
+		if er == nil {
+			e.log.Error("received null event record")
+			return
+		}
+
+		e.log.Debugf("received event %d with length %d", er.EventHeader.EventDescriptor.Id, er.UserDataLength)
+
+		event := make(map[string]interface{})
+		event["Header"] = er.EventHeader
+
+		if data, err := etw.GetEventProperties(er); err == nil {
+			event["EventProperties"] = data
+		} else {
+			e.log.Errorf("failed to read event properties: %s", err)
+			return
+		}
+
+		evt := beat.Event{
+			Timestamp: time.Now(),
+			Fields: mapstr.M{
+				"header": event["Header"],
+				"winlog": event["EventProperties"],
+			},
+		}
+		publisher.Publish(evt)
+
+		return
+	}
+
+	e.etwSession.Callback = syscall.NewCallback(eventReceivedCallback)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		e.log.Debug("starting to listen ETW events")
+		if err = e.etwSession.StartConsumer(); err != nil {
+			e.log.Warnf("events could not be read: %v", err)
+		}
+		e.log.Debug("stopped to read ETW events")
+	}()
+
+	return nil
+}
+
+// Closes all the opened handlers and resources
+func (e *etw_input) close() {
+	if err := e.etwSession.StopSession(); err != nil {
+		e.log.Error("failed to shutdown ETW session")
+	}
+	e.log.Info("successfully shutdown")
+}
