0b0154634570e11fd4667d0d6945b624f5dfb4fa
fearful-symmetry <alexpkristiansen@icloud.com>
2023-12-18 14:35:56 -0800

    move logstash latency window

--- libbeat/outputs/logstash/sync.go
+++ libbeat/outputs/logstash/sync.go
@@ -114,7 +114,7 @@ func (c *syncClient) Publish(_ context.Context, batch publisher.Batch) error {
 	}
 
 	for len(events) > 0 {
-		begin := time.Now()
+
 		// check if we need to reconnect
 		if c.ticker != nil {
 			select {
@@ -137,12 +137,14 @@ func (c *syncClient) Publish(_ context.Context, batch publisher.Batch) error {
 			err error
 		)
 
+		begin := time.Now()
 		if c.win == nil {
 			n, err = c.sendEvents(events)
 		} else {
 			n, err = c.publishWindowed(events)
 		}
-
+		took := time.Since(begin)
+		st.ReportLatency(took)
 		c.log.Debugf("%v events out of %v events sent to logstash host %s. Continue sending",
 			n, len(events), c.Host())
 
@@ -164,8 +166,7 @@ func (c *syncClient) Publish(_ context.Context, batch publisher.Batch) error {
 
 			return err
 		}
-		took := time.Since(begin)
-		st.ReportLatency(took)
+
 	}
 
 	batch.ACK()
