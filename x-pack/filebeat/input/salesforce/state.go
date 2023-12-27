8ce4fa4bec4d1cb8353c0d4c928be8c3b9856242
Kush Rana <kush.rana@elastic.co>
2023-12-27 11:47:46 +0530

    comments and changelog entry

--- x-pack/filebeat/input/salesforce/state.go
+++ x-pack/filebeat/input/salesforce/state.go
@@ -17,6 +17,10 @@ type state struct {
 	EventLogFile dateTimeCursor `json:"event_log_file,omitempty"`
 }
 
+// dateTimeCursor maintains two distinct states for the event collection iteration.
+// The initial state represents the time of the first event, while the subsequent state denotes the time of the last event.
+// In certain SOQL queries for specific objects, sorting by all fields may not be feasible, and there may be no specific order.
+// This design allows users to exert maximum control over the iteration process.
 type dateTimeCursor struct {
 	FirstEventTime string `struct:"first_event_time,omitempty"`
 	LastEventTime  string `struct:"last_event_time,omitempty"`
