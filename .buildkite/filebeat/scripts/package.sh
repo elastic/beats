f804bd3a58ced7692ec93a43cad37e2d6ce83d6a
Olga Naidjonoka <olga_naidjonoka@epam.com>
2024-01-04 10:14:00 +0200

    updated packaging execution conditions

--- .buildkite/filebeat/scripts/package.sh
+++ .buildkite/filebeat/scripts/package.sh
@@ -2,4 +2,33 @@
 
 set -euo pipefail
 
-echo ":: Test me ::"
+source .buildkite/filebeat/scripts/common.sh
+
+echo ":: Evaluate Filebeat Changes ::"
+
+changeset="^filebeat/
+    ^go.mod
+    ^pytest.ini
+    ^dev-tools/
+    ^libbeat/
+    ^testing/
+    ^\.buildkite/filebeat/"
+
+if ! are_files_changed "$changeset" ; then
+  message="No files changed within Filebeat changeset"
+  echo "$message"
+  buildkite-agent annotate "$message" --style 'warning' --context 'ctx-warn'
+  # This should return any error but skip the release.
+  exit 0
+fi
+
+# ToDo - remove after Beats agent is created"
+echo ":: Setup Env ::"
+add_bin_path
+with_go
+with_mage
+# ToDo - end
+
+echo ":: Start Packaging ::"
+cd filebeat
+mage package
