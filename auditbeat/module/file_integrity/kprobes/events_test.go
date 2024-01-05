30b36eb452647fb447f8611d1959b891f4706ea3
Panos Koutsovasilis <panos.koutsovasilis@elastic.co>
2024-01-06 00:44:02 +0200

    feat: add event allocations unit-tests

--- auditbeat/module/file_integrity/kprobes/events_test.go
+++ auditbeat/module/file_integrity/kprobes/events_test.go
@@ -2,7 +2,55 @@ package kprobes
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
+
+func Test_allocProbeEvents(t *testing.T) {
+	p := allocProbeEvent()
+	require.IsType(t, &ProbeEvent{}, p)
+
+	releaseProbeEvent(nil)
+
+	pE := p.(*ProbeEvent)
+	require.Zero(t, pE.MaskMonitor)
+	require.Zero(t, pE.MaskCreate)
+	require.Zero(t, pE.MaskDelete)
+	require.Zero(t, pE.MaskAttrib)
+	require.Zero(t, pE.MaskModify)
+	require.Zero(t, pE.MaskDir)
+	require.Zero(t, pE.MaskMoveTo)
+	require.Zero(t, pE.MaskMoveFrom)
+	releaseProbeEvent(pE)
+
+	p = allocDeleteProbeEvent()
+	require.IsType(t, &ProbeEvent{}, p)
+
+	pE = p.(*ProbeEvent)
+	require.Zero(t, pE.MaskMonitor)
+	require.Zero(t, pE.MaskCreate)
+	require.Equal(t, pE.MaskDelete, uint32(1))
+	require.Zero(t, pE.MaskAttrib)
+	require.Zero(t, pE.MaskModify)
+	require.Zero(t, pE.MaskDir)
+	require.Zero(t, pE.MaskMoveTo)
+	require.Zero(t, pE.MaskMoveFrom)
+	releaseProbeEvent(pE)
+
+	p = allocMonitorProbeEvent()
+	require.IsType(t, &ProbeEvent{}, p)
+
+	pE = p.(*ProbeEvent)
+	require.Equal(t, pE.MaskMonitor, uint32(1))
+	require.Zero(t, pE.MaskCreate)
+	require.Zero(t, pE.MaskDelete)
+	require.Zero(t, pE.MaskAttrib)
+	require.Zero(t, pE.MaskModify)
+	require.Zero(t, pE.MaskDir)
+	require.Zero(t, pE.MaskMoveTo)
+	require.Zero(t, pE.MaskMoveFrom)
+	releaseProbeEvent(pE)
+}
 
 func BenchmarkEventAllocation(b *testing.B) {
 	var p *ProbeEvent
