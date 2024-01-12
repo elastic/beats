884f61e27f804897a0e8573848e4304d8db92259
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2024-01-12 15:55:05 +0200

    debug windows tests

--- .buildkite/metricbeat/scripts/win_unit_tests.ps1
+++ .buildkite/metricbeat/scripts/win_unit_tests.ps1
@@ -2,14 +2,14 @@ $ErrorActionPreference = "Stop" # set -e
 $WorkFolder = "metricbeat"
 # Forcing to checkout again all the files with a correct autocrlf.
 # Doing this here because we cannot set git clone options before.
-# function fixCRLF {
-#     Write-Host "-- Fixing CRLF in git checkout --"
-#     git config core.autocrlf input
-#     git rm --quiet --cached -r .
-#     git reset --quiet --hard
-# }
+function fixCRLF {
+    Write-Host "-- Fixing CRLF in git checkout --"
+    git config core.autocrlf input
+    git rm --quiet --cached -r .
+    git reset --quiet --hard
+}
 function withGolang($version) {
-    Write-Host "-- Install golang --"
+    Write-Host "-- Install golang $version --"
     choco install -y golang --version=$version
     $env:ChocolateyInstall = Convert-Path "$((Get-Command choco).Path)\..\.."
     Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
@@ -30,7 +30,7 @@ function installGoDependencies {
     }
 }
 
-# fixCRLF
+fixCRLF
 withGolang $env:GO_VERSION
 installGoDependencies
 
@@ -43,7 +43,7 @@ mage build unitTest
 
 Pop-Location
 
-$EXITCODE=$LASTEXITCODE
+# $EXITCODE=$LASTEXITCODE
 $ErrorActionPreference = "Stop"
 
-Exit $EXITCODE
+# Exit $EXITCODE
