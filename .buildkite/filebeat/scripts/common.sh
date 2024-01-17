e2bab43c8857e9283a86e5b7860840d17891758a
Olga Naidjonoka <olga_naidjonoka@epam.com>
2024-01-17 12:34:39 +0200

    moved env setup to separate script

--- .buildkite/filebeat/scripts/common.sh
+++ .buildkite/filebeat/scripts/common.sh
@@ -2,10 +2,10 @@
 
 set -euo pipefail
 
-WORKSPACE="$(pwd)"
-BIN="${WORKSPACE}/bin"
-HW_TYPE="$(uname -m)"
-PLATFORM_TYPE="$(uname)"
+#WORKSPACE="$(pwd)"
+#BIN="${WORKSPACE}/bin"
+#HW_TYPE="$(uname -m)"
+#PLATFORM_TYPE="$(uname)"
 
 add_bin_path() {
     echo "Adding PATH to the environment variables..."
@@ -85,7 +85,7 @@ retry() {
 }
 
 are_files_changed() {
-  changeset=$1
+  local changeset=$1
 
   if git diff --name-only HEAD@{1} HEAD | grep -qE "$changeset"; then
     return 0;
@@ -95,11 +95,11 @@ are_files_changed() {
   fi
 }
 
-prepare_win() {
-  local os
-  os="$(uname)"
-  if [[ $os = MINGW* ]]; then
-    choco install mingw -y
-    choco install python --version=3.11.0 -y
-  fi
-}
+#prepare_win() {
+##  local os
+##  os="$(uname)"
+#  if [[ ${PLATFORM_TYPE} = MINGW* ]]; then
+#    choco install mingw -y
+#    choco install python --version=3.11.0 -y
+#  fi
+#}
