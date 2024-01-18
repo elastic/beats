376991aeee3447539066844498ec6cc997b4c689
kaiyan-sheng <kaiyan.sheng@elastic.co>
2024-01-17 21:11:34 -0700

    disable test_enterprisesearch.py test_health and test_stats

--- x-pack/metricbeat/module/enterprisesearch/test_enterprisesearch.py
+++ x-pack/metricbeat/module/enterprisesearch/test_enterprisesearch.py
@@ -10,7 +10,7 @@ class Test(XPackTest):
     COMPOSE_TIMEOUT = 600
 
     # -------------------------------------------------------------------------
-    @unittest.skipUnless(metricbeat.INTEGRATION_TESTS, 'integration test')
+    @unittest.skip
     def test_health(self):
         """Tests the Health API and the associated metricset"""
 
@@ -27,7 +27,7 @@ class Test(XPackTest):
         self.assertIn("jvm", health)
 
     # -------------------------------------------------------------------------
-    @unittest.skipUnless(metricbeat.INTEGRATION_TESTS, 'integration test')
+    @unittest.skip
     def test_stats(self):
         """Tests the Stats API and the associated metricset"""
 
