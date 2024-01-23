ec3f080c9b0b2586b22fc8722f5d7d73358293c8
Olga Naidjonoka <olga_naidjonoka@epam.com>
2024-01-23 19:57:54 +0200

    debugging arm packaing

--- auditbeat/magefile.go
+++ auditbeat/magefile.go
@@ -21,6 +21,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"time"
 
 	"github.com/magefile/mage/mg"
@@ -94,6 +96,8 @@ func AssembleDarwinUniversal() error {
 func Package() {
 	start := time.Now()
 	defer func() { fmt.Println("package ran for", time.Since(start)) }()
+
+	log.SetOutput(os.Stdout)
 
 	devtools.UseElasticBeatOSSPackaging()
 	devtools.PackageKibanaDashboardsFromBuildDir()
