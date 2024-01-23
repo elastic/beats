f575912c27c9047f7db61ee8ae0506d9553faa8e
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2024-01-23 14:51:11 +0200

    add DEBIAN_FRONTEND option to pre-command

--- .buildkite/metricbeat/scripts/common.sh
+++ .buildkite/metricbeat/scripts/common.sh
@@ -7,6 +7,9 @@ platform_type="$(uname)"
 platform_type_lowercase=$(echo "$platform_type" | tr '[:upper:]' '[:lower:]')
 arch_type="$(uname -m)"
 pipeline_name="metricbeat"
+DEBIAN_FRONTEND="noninteractive"
+sudo mkdir -p /etc/needrestart
+echo "\$nrconf{restart} = 'a';" | sudo tee -a /etc/needrestart/needrestart.conf > /dev/null
 
 with_docker_compose() {
   local version=$1
