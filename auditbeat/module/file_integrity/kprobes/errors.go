625da6b66fc765113e5da2bc81978b7ed7d1b742
Panos Koutsovasilis <panos.koutsovasilis@elastic.co>
2024-01-24 09:42:08 +0200

    feat: implement path traverser to produce monitor events by walking a path

--- auditbeat/module/file_integrity/kprobes/errors.go
+++ auditbeat/module/file_integrity/kprobes/errors.go
@@ -24,4 +24,5 @@ var (
 	ErrVerifyMissingEvents     = errors.New("probe missing events")
 	ErrVerifyUnexpectedEvent   = errors.New("received an event that was not expected")
 	ErrSymbolNotFound          = errors.New("symbol not found")
+	ErrAckTimeout              = errors.New("timeout")
 )
