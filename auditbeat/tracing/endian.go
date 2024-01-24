cd3c3b1357ed8d32da07a544710b4e089f05355c
Panos Koutsovasilis <panos.koutsovasilis@elastic.co>
2024-01-24 04:46:09 +0200

    feat: modernise tracing endian.go to use binary.NativeEndian

--- auditbeat/tracing/endian.go
+++ auditbeat/tracing/endian.go
@@ -21,22 +21,8 @@ package tracing
 
 import (
 	"encoding/binary"
-	"unsafe"
 )
 
 // MachineEndian is either binary.BigEndian or binary.LittleEndian, depending
 // on the current architecture.
-var MachineEndian = getCPUEndianness()
-
-func getCPUEndianness() binary.ByteOrder {
-	myInt32 := new(uint32)
-	copy((*[4]byte)(unsafe.Pointer(myInt32))[:], []byte{0x12, 0x34, 0x56, 0x78})
-	switch *myInt32 {
-	case 0x12345678:
-		return binary.BigEndian
-	case 0x78563412:
-		return binary.LittleEndian
-	default:
-		panic("cannot determine endianness")
-	}
-}
+var MachineEndian = binary.NativeEndian
