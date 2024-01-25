1f30beb87f58d5451fd09bcf8fd73098498f5b27
Panos Koutsovasilis <panos.koutsovasilis@elastic.co>
2024-01-25 02:10:21 +0200

    fix: revert pollAll in perfevent.go to named returns as these can be properly documented

--- auditbeat/tracing/perfevent.go
+++ auditbeat/tracing/perfevent.go
@@ -520,12 +520,7 @@ func (m *recordMerger) readSampleNonBlock(ev *perf.Event, ctx context.Context) (
 	return nil, true
 }
 
-func pollAll(evs []*perf.Event, timeout time.Duration) (int, int, error) {
-	var (
-		active int
-		closed int
-		err    error
-	)
+func pollAll(evs []*perf.Event, timeout time.Duration) (active int, closed int, err error) {
 	pollfds := make([]unix.PollFd, len(evs))
 	for idx, ev := range evs {
 		fd, err := ev.FD()
@@ -551,5 +546,5 @@ func pollAll(evs []*perf.Event, timeout time.Duration) (int, int, error) {
 			closed++
 		}
 	}
-	return active, closed, err
+	return
 }
