8282de05250b1fa649bf4a6147b5cb5e67c95dc4
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2024-01-25 17:49:00 +0200

    debug Go Int Tests

--- .buildkite/scripts/go_int_tests.sh
+++ .buildkite/scripts/go_int_tests.sh
@@ -7,8 +7,9 @@ set -euo pipefail
 beats_subfilder=$1
 
 echo "--- Run Go Intergration Tests for $beats_subfilder"
-sudo chmod -R go-w "${beats_subfilder}/"
+sudo chmod -R go-w ${beats_subfilder}/
 pushd "${beats_subfilder}" > /dev/null
+umask 0022
 mage goIntegTest
 
 popd > /dev/null
