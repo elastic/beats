d684936439b54982f605040a0b5b2de7c3086d10
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2024-01-26 15:30:54 +0200

    full test

--- .buildkite/scripts/stress_tests.sh
+++ .buildkite/scripts/stress_tests.sh
@@ -12,6 +12,6 @@ echo "--- Run Stress Tests for $beats_subfolder"
 pushd "${beats_subfolder}" > /dev/null
 
 umask 0022
-make STRESS_TEST_OPTIONS='-timeout=20m -race -v -parallel 1' stress-tests
+make STRESS_TEST_OPTIONS='-timeout=20m -race -v -parallel 1' GOTEST_OUTPUT_OPTIONS='| go-junit-report > libbeat-stress-test.xml' stress-tests
 
 popd > /dev/null
