40810b99fbc61d269244178e21b1b2cf63902b2b
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2024-01-29 18:45:40 +0200

    debug

---
+++ .buildkite/scripts/install_tools.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+source .buildkite/scripts/common.sh
+
+set -euo pipefail
+
+echo "--- Env preparation"
+
+DEBIAN_FRONTEND="noninteractive"
+sudo mkdir -p /etc/needrestart
+echo "\$nrconf{restart} = 'a';" | sudo tee -a /etc/needrestart/needrestart.conf > /dev/null
+
+add_bin_path
+with_go "${GO_VERSION}"
+with_mage
+with_python
+
+if command -v docker-compose &> /dev/null
+then
+  echo "Found docker-compose. Checking version.."
+  FOUND_DOCKER_COMPOSE_VERSION=$(docker-compose --version | awk '{print $3}'|sed s/\,//)
+  if [ $FOUND_DOCKER_COMPOSE_VERSION == $DOCKER_COMPOSE_VERSION ]; then
+    echo "Versions match. No need to install docker-compose. Exiting."
+  else
+    echo "Versions don't match. Need to install the correct version of docker-compose."
+    with_docker_compose "${DOCKER_COMPOSE_VERSION}"
+  fi
+else
+  with_docker_compose "${DOCKER_COMPOSE_VERSION}"
+fi
