781e1638de5e0951b513f7ac38a23669d3d6d38f
Pavel Zorin <pavel.zorin@elastic.co>
2024-01-31 15:27:20 +0000

    Corrected coverage path

--- .buildkite/xpack/elastic-agent/scripts/steps/unit-tests.sh
+++ .buildkite/xpack/elastic-agent/scripts/steps/unit-tests.sh
@@ -7,5 +7,5 @@ echo "--- Unit tests"
 RACE_DETECTOR=true TEST_COVERAGE=true build unitTest
 TESTS_EXIT_STATUS=$?
 # Copy coverage file to build directory so it can be downloaded as an artifact
-cp build/TEST-go-unit.cov coverage.out
+# cp build/TEST-go-unit.cov coverage.out
 exit $TESTS_EXIT_STATUS
