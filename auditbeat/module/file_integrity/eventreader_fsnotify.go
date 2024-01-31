1509a1a64064889de41a94d456cabc39b2aef5f9
Panos Koutsovasilis <panos.koutsovasilis@elastic.co>
2024-01-31 06:29:32 +0200

    fix: address compilation issues for non-linux oses

--- auditbeat/module/file_integrity/eventreader_fsnotify.go
+++ auditbeat/module/file_integrity/eventreader_fsnotify.go
@@ -23,7 +23,6 @@ import (
 	"errors"
 	"fmt"
 	"path/filepath"
-	"runtime"
 	"syscall"
 	"time"
 
@@ -33,7 +32,7 @@ import (
 	"github.com/elastic/elastic-agent-libs/logp"
 )
 
-type reader struct {
+type fsNotifyReader struct {
 	watcher monitor.Watcher
 	config  Config
 	eventC  chan Event
@@ -42,28 +41,7 @@ type reader struct {
 	parsers []FileParser
 }
 
-// NewEventReader creates a new EventProducer backed by fsnotify.
-func NewEventReader(c Config) (EventProducer, error) {
-
-	if runtime.GOOS == "linux" {
-		switch c.ForceBackend {
-		case BackendKProbes:
-			return &kProbesReader{
-				config:  c,
-				log:     logp.NewLogger(moduleName),
-				parsers: FileParsers(c),
-			}, nil
-		}
-	}
-
-	return &reader{
-		config:  c,
-		log:     logp.NewLogger(moduleName),
-		parsers: FileParsers(c),
-	}, nil
-}
-
-func (r *reader) Start(done <-chan struct{}) (<-chan Event, error) {
+func (r *fsNotifyReader) Start(done <-chan struct{}) (<-chan Event, error) {
 	watcher, err := monitor.New(r.config.Recursive, r.config.IsExcludedPath)
 	if err != nil {
 		return nil, err
@@ -118,7 +96,7 @@ func (r *reader) Start(done <-chan struct{}) (<-chan Event, error) {
 	return r.eventC, nil
 }
 
-func (r *reader) enqueueEvents(done <-chan struct{}) (events []*Event) {
+func (r *fsNotifyReader) enqueueEvents(done <-chan struct{}) (events []*Event) {
 	for {
 		ev := r.nextEvent(done)
 		if ev == nil {
@@ -128,7 +106,7 @@ func (r *reader) enqueueEvents(done <-chan struct{}) (events []*Event) {
 	}
 }
 
-func (r *reader) consumeEvents(done <-chan struct{}) {
+func (r *fsNotifyReader) consumeEvents(done <-chan struct{}) {
 	defer close(r.eventC)
 	defer r.watcher.Close()
 
@@ -142,7 +120,7 @@ func (r *reader) consumeEvents(done <-chan struct{}) {
 	}
 }
 
-func (r *reader) nextEvent(done <-chan struct{}) *Event {
+func (r *fsNotifyReader) nextEvent(done <-chan struct{}) *Event {
 	for {
 		select {
 		case <-done:
---
+++ auditbeat/module/file_integrity/eventreader_linux.go
@@ -0,0 +1,44 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+//go:build linux
+
+package file_integrity
+
+import (
+	"github.com/elastic/elastic-agent-libs/logp"
+)
+
+// NewEventReader creates a new EventProducer backed by fsnotify.
+func NewEventReader(c Config) (EventProducer, error) {
+	switch c.ForceBackend {
+	case BackendKProbes:
+		return &kProbesReader{
+			config:  c,
+			log:     logp.NewLogger(moduleName),
+			parsers: FileParsers(c),
+		}, nil
+	case BackendFSNotify:
+		fallthrough
+	default:
+		return &fsNotifyReader{
+			config:  c,
+			log:     logp.NewLogger(moduleName),
+			parsers: FileParsers(c),
+		}, nil
+	}
+}
