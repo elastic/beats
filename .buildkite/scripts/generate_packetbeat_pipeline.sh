e54f671d9ba29b400d8db15408eef08eb5fddd8f
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2024-02-01 18:12:31 +0200

    migrate packetbeat pipeline

---
+++ .buildkite/scripts/generate_packetbeat_pipeline.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+source .buildkite/scripts/common.sh
+
+set -euo pipefail
+
+pipelineName="pipeline.metricbeat-dynamic.yml"
+
+cat > $pipelineName <<- YAML
+
+steps:
+
+# YAML
+
+# if are_conditions_met_mandatory_tests; then
+#   cat >> $pipelineName <<- YAML
+
+  - group: "Mandatory Tests"
+    key: "mandatory-tests"
+    steps:
+      - label: ":linux: Ubuntu Unit Tests"
+        key: "mandatory-linux-unit-test"
+        command: ".buildkite/scripts/unit_tests.sh"
+        agents:
+          provider: "gcp"
+          image: "${IMAGE_UBUNTU_X86_64}"
+          machineType: "c2-standard-16"
+        artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
+
+      - label: ":windows: Windows Unit Tests - {{matrix.image}}"
+        command: ".buildkite/scripts/win_unit_tests.ps1"
+        key: "mandatory-win-unit-tests"
+        agents:
+          provider: "gcp"
+          image: "{{matrix.image}}"
+          machine_type: "n2-standard-8"
+          disk_size: 100
+          disk_type: "pd-ssd"
+        matrix:
+          setup:
+            image:
+              - "${IMAGE_WIN_2016}"
+              - "${IMAGE_WIN_2022}"
+        artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
+
+# YAML
+# fi
+
+# if are_conditions_met_extended_tests && are_conditions_met_macos_tests; then
+#   cat >> $pipelineName <<- YAML
+
+  - group: "Extended Tests"
+    key: "extended-tests"
+    steps:
+      - label: ":mac: MacOS Unit Tests"
+        key: "extended-macos-unit-tests"
+        command: ".buildkite/scripts/unit_tests.sh"
+        agents:
+          provider: "orka"
+          imagePrefix: "${IMAGE_MACOS_X86_64}"
+        artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
+
+      - label: ":linux: ARM Ubuntu Unit Tests"
+        key: "extended-arm64-unit-test"
+        command: ".buildkite/scripts/unit_tests.sh"
+        agents:
+          provider: "gcp"
+          image: "${IMAGE_UBUNTU_ARM_64}"
+          machineType: "c2-standard-16"
+        artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
+
+# YAML
+# fi
+
+# if are_conditions_met_extended_windows_tests; then
+#   cat >> $pipelineName <<- YAML
+
+  - group: "Extended Windowds Tests"
+    key: "extended-win-tests"
+    steps:
+      - label: ":windows: Win 2019 Unit Tests"
+        key: "extended-win-2019-unit-tests"
+        command: ".buildkite/scripts/win_unit_tests.ps1"
+        agents:
+          provider: "gcp"
+          image: "${IMAGE_WIN_2019}"
+          machine_type: "n2-standard-8"
+          disk_size: 100
+          disk_type: "pd-ssd"
+        artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
+
+# YAML
+# fi
+
+# if are_conditions_met_extended_windows_tests; then
+#   cat >> $pipelineName <<- YAML
+
+  - group: "Packaging"    # TODO: check conditions for future the main pipeline migration: https://github.com/elastic/beats/pull/28589
+    key: "packaging"
+    steps:
+      - label: ":linux: Packaging Linux"
+        key: "packaging-linux"
+        command: ".buildkite/scripts/packaging.sh"
+        agents:
+          provider: "gcp"
+          image: "${IMAGE_UBUNTU_X86_64}"
+          machineType: "c2-standard-16"
+        env:
+          PLATFORMS: "+all linux/amd64 linux/arm64 windows/amd64 darwin/amd64 darwin/arm64"
+
+      - label: ":linux: Packaging ARM"
+        key: "packaging-arm"
+        command: ".buildkite/scripts/packaging.sh"
+        agents:
+          provider: "aws"
+          imagePrefix: "${IMAGE_UBUNTU_ARM_64}"
+          instanceType: "t4g.xlarge"
+        env:
+          PLATFORMS: "linux/arm64"
+          PACKAGES: "docker"
+
+    depends_on:
+    - step: "mandatory-tests"
+      allow_failure: false
+    - step: "extended-tests"
+      allow_failure: true
+    - step: "extended-win-tests"
+      allow_failure: true
+
+YAML
+# fi
+
+cat $pipelineName       #remove after tests
+
+buildkite-agent pipeline upload $pipelineName
---
+++ .buildkite/scripts/install_tools.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+source .buildkite/scripts/common.sh
+
+set -euo pipefail
+
+echo "--- Env preparation"
+
+# Temporary solution to fix the issues with "sudo apt get...." https://elastic.slack.com/archives/C0522G6FBNE/p1706003603442859?thread_ts=1706003209.424539&cid=C0522G6FBNE
+# It could be removed when we use our own image for the BK agent.
+if [ "${platform_type}" == "Linux" ]; then
+  DEBIAN_FRONTEND="noninteractive"
+  #sudo command doesn't work at the "pre-command" hook because of another user environment (root with strange permissions)
+  sudo mkdir -p /etc/needrestart
+  echo "\$nrconf{restart} = 'a';" | sudo tee -a /etc/needrestart/needrestart.conf > /dev/null
+fi
+
+add_bin_path
+
+if command -v docker-compose &> /dev/null
+then
+  echo "Found docker-compose. Checking version.."
+  FOUND_DOCKER_COMPOSE_VERSION=$(docker-compose --version | awk '{print $4}'|sed s/\,//)
+  if [ $FOUND_DOCKER_COMPOSE_VERSION == $DOCKER_COMPOSE_VERSION ]; then
+    echo "Versions match. No need to install docker-compose. Exiting."
+  elif [ "${platform_type}" == "Linux" && "${arch_type}" == "aarch64" ]; then
+    with_docker_compose "${DOCKER_COMPOSE_VERSION_AARCH64}"
+  elif [ "${platform_type}" == "Linux" && "${arch_type}" == "x86_64" ]; then
+    with_docker_compose "${DOCKER_COMPOSE_VERSION}"
+  fi
+else
+  with_docker_compose "${DOCKER_COMPOSE_VERSION}"
+fi
+
+with_go "${GO_VERSION}"
+with_mage
+with_python
+
+#sudo command doesn't work at the "pre-command" hook because of another user environment (root with strange permissions)
+#sudo chmod -R go-w "${BEATS_PROJECT_NAME}/"     #fix the fulesystem permissions issue like this:https://buildkite.com/elastic/beats-metricbeat/builds/1154#018d12db-dc0c-4bcd-b9b4-d5dece0b42c6/272-1267
+
+sudo chmod -R go-w "${BEATS_PROJECT_NAME}/"     #fix the fulesystem permissions issue like this:https://buildkite.com/elastic/beats-metricbeat/builds/1154#018d12db-dc0c-4bcd-b9b4-d5dece0b42c6/272-1267
+
+pushd "${BEATS_PROJECT_NAME}" > /dev/null
+
+umask 0022    # fix the filesystem permissions issue like this: https://buildkite.com/elastic/beats-metricbeat/builds/1329#018d3179-25a9-475b-a2c8-64329dfe092b/320-1696
+
+popd > /dev/null
