aec3839bee8b06be58fb6ef7e95012934555835d
Pavel Zorin <pavel.zorin@elastic.co>
2024-02-01 12:52:39 +0000

    Fix hooks

--- .buildkite/xpack/elastic-agent/scripts/steps/unit-tests.sh
+++ .buildkite/xpack/elastic-agent/scripts/steps/unit-tests.sh
@@ -4,7 +4,7 @@ set -uo pipefail
 source .buildkite/scripts/common.sh
 
 echo "--- Unit tests"
-RACE_DETECTOR=true TEST_COVERAGE=true build unitTest
+RACE_DETECTOR=true TEST_COVERAGE=true mage build unitTest
 TESTS_EXIT_STATUS=$?
 
 exit $TESTS_EXIT_STATUS
