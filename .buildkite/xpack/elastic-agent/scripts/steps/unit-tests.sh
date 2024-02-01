fbd57f2c08160b29f50c36f5c82da06f633a306f
Pavel Zorin <pavel.zorin@elastic.co>
2024-02-01 13:03:46 +0000

    Fix hooks

--- .buildkite/xpack/elastic-agent/scripts/steps/unit-tests.sh
+++ .buildkite/xpack/elastic-agent/scripts/steps/unit-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -uo pipefail
 
-source .buildkite/scripts/common.sh
+source .buildkite/xpack/elastic-agent/scripts/steps/common.sh
 
 echo "--- Unit tests"
 RACE_DETECTOR=true TEST_COVERAGE=true mage build unitTest
