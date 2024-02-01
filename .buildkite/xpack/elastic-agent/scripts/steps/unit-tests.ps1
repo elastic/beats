88d21cb113456f9eff774005741e2959d8cae0e0
Pavel Zorin <pavel.zorin@elastic.co>
2024-02-01 17:16:47 +0000

    fixed paths

--- .buildkite/xpack/elastic-agent/scripts/steps/unit-tests.ps1
+++ .buildkite/xpack/elastic-agent/scripts/steps/unit-tests.ps1
@@ -6,7 +6,6 @@ git rm --quiet --cached -r .
 git reset --quiet --hard
 
 $env:GOTMPDIR = "$env:BUILDKITE_BUILD_CHECKOUT_PATH"
-$env:SOURCE_DIR="$env:BUILDKITE_BUILD_CHECKOUT_PATH\\xpack\\elastic-agent"
 
 Write-Host "--- Build"
 mage -d "$env:SOURCE_DIR" build
@@ -18,7 +17,7 @@ if ($LASTEXITCODE -ne 0) {
 Write-Host "--- Unit tests"
 $env:TEST_COVERAGE = $true
 $env:RACE_DETECTOR = $true
-mage -d "$env:SOURCE_DIR" unitTest
+mage -d ".\\x-pack\\elastic-agent" unitTest
 # Copy coverage file to build directory so it can be downloaded as an artifact
 # cp .\build\TEST-go-unit.cov coverage.out
 
--- .buildkite/xpack/elastic-agent/scripts/steps/unit-tests.sh
+++ .buildkite/xpack/elastic-agent/scripts/steps/unit-tests.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 set -uo pipefail
 
-source ".buildkite/xpack/elastic-agent/scripts/steps/common.sh"
-
-SOURCE_DIR="$WORKSPACE/xpack/elastic-agent"
+source ".buildkite/x-pack/elastic-agent/scripts/steps/common.sh"
 
 echo "--- Unit tests"
-RACE_DETECTOR=true TEST_COVERAGE=true mage -d "$SOURCE_DIR" build unitTest
+RACE_DETECTOR=true TEST_COVERAGE=true mage -d "x-pack/elastic-agent" build unitTest
 TESTS_EXIT_STATUS=$?
 
 exit $TESTS_EXIT_STATUS
