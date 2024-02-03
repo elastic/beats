9aa7c6c6a941a7a8b7d7dbe119dbf4547ca6bda1
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2024-02-03 10:41:51 +0200

    conflict solving

--- .buildkite/scripts/stress_tests.sh
+++ .buildkite/scripts/stress_tests.sh
@@ -1,17 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-source .buildkite/scripts/common.sh
+source .buildkite/scripts/install_tools.sh
 
 set -euo pipefail
 
 beats_subfolder=$1
 
-sudo chmod -R go-w ${beats_subfolder}/
-
 echo "--- Run Stress Tests for $beats_subfolder"
 pushd "${beats_subfolder}" > /dev/null
 
-umask 0022
 make STRESS_TEST_OPTIONS='-timeout=20m -race -v -parallel 1' GOTEST_OUTPUT_OPTIONS='| go-junit-report > libbeat-stress-test.xml' stress-tests
 
 popd > /dev/null
