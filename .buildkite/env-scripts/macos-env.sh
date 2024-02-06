7490f478c50ea4cea9af68a66ffdba2f8504866d
Olga Naidjonoka <olga_naidjonoka@epam.com>
2024-02-06 21:07:57 +0200

    added tag definition

--- .buildkite/env-scripts/macos-env.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ $PLATFORM_TYPE == Darwin* ]]; then
-  echo ":: Setting larger ulimit on MacOS ::"
-  # To bypass file descriptor errors like "Too many open files error" on MacOS
-  ulimit -Sn 50000
-  echo ":: ULIMIT :: $(ulimit -n)"
-fi
