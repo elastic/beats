6f4b03536fd00f067ec39f7d1f791d1ec4671573
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2024-02-06 18:10:04 +0200

    fix RHEL9 dependencies

--- .buildkite/scripts/common.sh
+++ .buildkite/scripts/common.sh
@@ -148,7 +148,7 @@ with_dependencies() {
       sudo apt-get install -y libsystemd-dev libpcap-dev
     elif [ "${linuxType}" = "rhel" ]; then
       sudo yum update -y
-      sudo yum install -y systemd-devel
+      sudo yum install -y systemd-devel libpcap-devel
     fi
   elif [ "${platform_type}" == "Darwin" ]; then
     pip3 install libpcap
