0397dbefdbdc4f2aa1b6525ddfb032caee7ad725
Olga Naidjonoka <olga_naidjonoka@epam.com>
2024-02-06 12:21:19 +0200

    changed ubuntu agent

---
+++ .buildkite/filebeat/scripts/packaging/package.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo ":: Start Packaging ::"
+mage -d filebeat package
+
+calculate_tags() {
+  if [ "$snapshot" = true ]; then
+    aliasVersion="${VERSION%.*}${IMG_POSTFIX}"
+    sourceTag+=${IMG_POSTFIX}
+  fi
+
+  local tags="${BUILDKITE_COMMIT}"
+
+#  if isPR; then
+#    tags+=("pr-${CHANGE_ID}")
+#  else
+#    tags+=("${sourceTag}")
+#  fi
+#
+#  if ! isPR && [ -n "$(aliasVersion)" ]; then
+#    tags+=("${aliasVersion}")
+#  fi
+#
+#  echo "${tags[@]}"
+}
+
+#buildkite-agent annotate "Tag '$TAG' has been created." --style 'success' --context 'ctx-success'
