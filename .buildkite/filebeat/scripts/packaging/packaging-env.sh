c48f933c89849379e9fc8cc498c101411df97fc6
Olga Naidjonoka <olga_naidjonoka@epam.com>
2024-02-06 12:21:19 +0200

    changed ubuntu agent

--- .buildkite/filebeat/scripts/packaging/packaging-env.sh
+++ .buildkite/filebeat/scripts/packaging/packaging-env.sh
@@ -7,13 +7,14 @@ deploy/kubernetes/.*-kubernetes.yaml"
 packaging_changeset="^dev-tools/packaging/
 ^.go-version"
 
-# Env vars for Packaging stage
 DOCKER_REGISTRY="docker.elastic.co"
 SNAPSHOT=true
 VERSION=$(make get-version | tr -d '\n')
 ONLY_DOCS=$(changeset_applies "$docs_changeset")
 PACKAGING_CHANGES=$(changeset_applies "$packaging_changeset")
 GO_MOD_CHANGES=$(changeset_applies "^go.mod")
+# Change the postfix to -SNAPSHOT, once Jenkins is disabled
+IMG_POSTFIX="-BK-SNAPSHOT"
 
 export PACKAGING_CHANGES
 export ONLY_DOCS
@@ -22,3 +23,11 @@ export DOCKER_REGISTRY
 export SNAPSHOT
 export VERSION
 export REPO
+export IMG_POSTFIX
+
+set_git_config() {
+  git config user.name "${GITHUB_USERNAME_SECRET}"
+  git config user.email "${GITHUB_EMAIL_SECRET}"
+}
+
+set_git_config
