0facfabf067918799169578122ecf7bab37997ae
Olga Naidjonoka <olga_naidjonoka@epam.com>
2024-02-06 21:07:57 +0200

    added tag definition

--- .buildkite/filebeat/scripts/packaging/package.sh
+++ .buildkite/filebeat/scripts/packaging/package.sh
@@ -2,35 +2,62 @@
 
 set -euo pipefail
 
-echo ":: Start Packaging ::"
-mage -d filebeat package
+#source .buildkite/env-scripts/linux-env.sh
+source .buildkite/env-scripts/util.sh
 
-calculate_tags() {
-  if [ "$snapshot" = true ]; then
+#echo "--- Creating package"
+#mage -d filebeat package
+
+
+TAGS=("${BUILDKITE_COMMIT}")
+
+VARIANTS=("" "-ubi" "-oss")
+
+# IMAGES
+SOURCE_NAMESPACE="beats"
+BEAT_NAME="filebeat"
+SOURCE="beats/${BEAT_NAME}${variant}"
+TARGET="observability-ci/${BEAT_NAME}"
+
+# ARGS
+REGISTRY="docker.elastic.co"
+SNAPSHOT=true
+VERSION="$(make get-version)",
+images: images
+
+#VARIANTS -> foreach = IMAGES: image map with SOURCE, TARGET, ARCH
+
+#IMAGES -> foreach = tag and push
+# registry: ${REGISTRY},
+# sourceTag: calculate_tags->sourceTag,
+# targetTag: "${tag}" (non arm) // ${tag}-${image.arch} (arm) --> foreach $TAGS
+# source: ${SOURCE},
+# target: ${TARGET}
+
+
+define_tags() {
+echo "--- Defined tags"
+  if [ "$SNAPSHOT" = true ]; then
     aliasVersion="${VERSION%.*}${IMG_POSTFIX}"
-    sourceTag+=${IMG_POSTFIX}
+    sourceTag+="${VERSION}${IMG_POSTFIX}"
   fi
 
-  local tags="${BUILDKITE_COMMIT}"
+  if is_pr; then
+    TAGS+=("pr-${GITHUB_PR_NUMBER}")
+  else
+    TAGS+=("${sourceTag}" "${aliasVersion}")
+  fi
 
-#  if isPR; then
-#    tags+=("pr-${CHANGE_ID}")
-#  else
-#    tags+=("${sourceTag}")
-#  fi
-#
-#  if ! isPR && [ -n "$(aliasVersion)" ]; then
-#    tags+=("${aliasVersion}")
-#  fi
-#
-#  echo "${tags[@]}"
+  local tag=""
+  for tag in "${TAGS[@]}"; do
+    echo "$tag"
+  done
 }
 
 #buildkite-agent annotate "Tag '$TAG' has been created." --style 'success' --context 'ctx-success'
 
-#set_git_config() {
-#  git config user.name "${GITHUB_USERNAME_SECRET}"
-#  git config user.email "${GITHUB_EMAIL_SECRET}"
-#}
-#
+echo "--- Calculating tags"
+daefine_tags
+
+#echo "--- Setting git config"
 #set_git_config
