538027317b4cad4c849fa2e4ddf0ce32ab106ad0
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2024-02-08 15:20:09 +0200

    fix VAR name

--- .buildkite/scripts/generate_winlogbeat_pipeline.sh
+++ .buildkite/scripts/generate_winlogbeat_pipeline.sh
@@ -94,7 +94,7 @@ if are_conditions_met_packaging; then
         agents:
           provider: "gcp"
           image: "${IMAGE_UBUNTU_X86_64}"
-          machineType: "${GCP_HI_PERF_MASHINE_TYPE}"
+          machineType: "${GCP_HI_PERF_MACHINE_TYPE}"
         env:
           PLATFORMS: "+all linux/amd64 linux/arm64 windows/amd64 darwin/amd64 darwin/arm64"
 
