a18ea535b1a52096bb0d6a7858d90e984f5c280b
Olga Naidjonoka <olga_naidjonoka@epam.com>
2024-02-08 19:37:13 +0200

    added comments

--- .buildkite/heartbeat/scripts/unit-tests.sh
+++ .buildkite/heartbeat/scripts/unit-tests.sh
@@ -2,11 +2,14 @@
 
 set -euo pipefail
 
+# Remove when custom image is set up
 source .buildkite/env-scripts/linux-env.sh
 
 echo "--- Running Unit Tests"
+# Remove when custom image is set up
 sudo chmod -R go-w heartbeat/
 
 cd heartbeat
+# Remove when custom image is set up
 umask 0022
 mage build unitTest
