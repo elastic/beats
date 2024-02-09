1a46e70065207bf249b85d8060ae282aba2168e6
Olga Naidjonoka <olga_naidjonoka@epam.com>
2024-02-09 12:06:18 +0200

    added docker img params

--- .buildkite/filebeat/scripts/packaging/packaging-env.sh
+++ .buildkite/filebeat/scripts/packaging/packaging-env.sh
@@ -7,20 +7,27 @@ deploy/kubernetes/.*-kubernetes.yaml"
 packaging_changeset="^dev-tools/packaging/
 ^.go-version"
 
-DOCKER_REGISTRY="docker.elastic.co"
-SNAPSHOT=true
 VERSION=$(make get-version | tr -d '\n')
 ONLY_DOCS=$(changeset_applies "$docs_changeset")
 PACKAGING_CHANGES=$(changeset_applies "$packaging_changeset")
 GO_MOD_CHANGES=$(changeset_applies "^go.mod")
 # Change the postfix to -SNAPSHOT, once Jenkins is disabled
-IMG_POSTFIX="-BK-SNAPSHOT"
+
 
 export PACKAGING_CHANGES
 export ONLY_DOCS
 export GO_MOD_CHANGES
 export DOCKER_REGISTRY
-export SNAPSHOT
 export VERSION
 export REPO
-export IMG_POSTFIX
+#export IMG_POSTFIX
+
+
+#VARIANTS -> foreach = IMAGES: image map with SOURCE, TARGET, ARCH
+
+#IMAGES -> foreach = tag and push
+# registry: ${REGISTRY},
+# sourceTag: calculate_tags->sourceTag,
+# targetTag: "${tag}" (non arm) // ${tag}-${image.arch} (arm) --> foreach $TAGS
+# source: ${SOURCE},
+# target: ${TARGET}
