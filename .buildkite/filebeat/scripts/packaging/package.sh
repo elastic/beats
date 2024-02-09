e27f07a79b687d65c0c62fc7b90f08eec51f1772
Olga Naidjonoka <olga_naidjonoka@epam.com>
2024-02-09 12:06:18 +0200

    added docker img params

--- .buildkite/filebeat/scripts/packaging/package.sh
+++ .buildkite/filebeat/scripts/packaging/package.sh
@@ -5,59 +5,58 @@ set -euo pipefail
 #source .buildkite/env-scripts/linux-env.sh
 source .buildkite/env-scripts/util.sh
 
+IMG_POSTFIX="-BK-SNAPSHOT"
+VARIANTS=("" "-ubi" "-oss")
+VERSION="$(make get-version)"
+SOURCE_TAG+="${VERSION}${IMG_POSTFIX}"
+BEAT_NAME="filebeat"
+TARGET="observability-ci/${BEAT_NAME}"
+
+define_tags() {
+  aliasVersion="${VERSION%.*}${IMG_POSTFIX}"
+  tags=("${BUILDKITE_COMMIT}")
+
+  if is_pr; then
+    tags+=("pr-${GITHUB_PR_NUMBER}")
+  else
+    tags+=("${SOURCE_TAG}" "${aliasVersion}")
+  fi
+}
+
+check_is_arm() {
+  if [[ ${HW_TYPE} == "aarch64" || ${HW_TYPE} == "arm64" ]]; then
+    is_arm="-arm"
+  else
+    is_arm=""
+  fi
+}
+
+define_tags
+
+for variant in "${VARIANTS[@]}"; do
+  echo "--- PARAMS for variant: $variant"
+
+  check_is_arm
+  registry=${DOCKER_REGISTRY}
+  sourceTag=$SOURCE_TAG
+  source="beats/${BEAT_NAME}${variant}"
+  target=$TARGET
+
+  echo "Registry: $registry"
+  echo "Source: $source"
+  echo "Source tag: $sourceTag"
+  echo "Target: $target"
+
+  for tag in "${tags[@]}"; do
+    targetTag=$tag${is_arm}
+    echo "Target tag: $targetTag"
+  done
+done
+
 #echo "--- Creating package"
 #mage -d filebeat package
 
-
-TAGS=("${BUILDKITE_COMMIT}")
-
-VARIANTS=("" "-ubi" "-oss")
-
-# IMAGES
-SOURCE_NAMESPACE="beats"
-BEAT_NAME="filebeat"
-SOURCE="beats/${BEAT_NAME}${variant}"
-TARGET="observability-ci/${BEAT_NAME}"
-
-# ARGS
-REGISTRY="docker.elastic.co"
-SNAPSHOT=true
-VERSION="$(make get-version)",
-images: images
-
-#VARIANTS -> foreach = IMAGES: image map with SOURCE, TARGET, ARCH
-
-#IMAGES -> foreach = tag and push
-# registry: ${REGISTRY},
-# sourceTag: calculate_tags->sourceTag,
-# targetTag: "${tag}" (non arm) // ${tag}-${image.arch} (arm) --> foreach $TAGS
-# source: ${SOURCE},
-# target: ${TARGET}
-
-
-define_tags() {
-echo "--- Defined tags"
-  if [ "$SNAPSHOT" = true ]; then
-    aliasVersion="${VERSION%.*}${IMG_POSTFIX}"
-    sourceTag+="${VERSION}${IMG_POSTFIX}"
-  fi
-
-  if is_pr; then
-    TAGS+=("pr-${GITHUB_PR_NUMBER}")
-  else
-    TAGS+=("${sourceTag}" "${aliasVersion}")
-  fi
-
-  local tag=""
-  for tag in "${TAGS[@]}"; do
-    echo "$tag"
-  done
-}
-
-#buildkite-agent annotate "Tag '$TAG' has been created." --style 'success' --context 'ctx-success'
-
-echo "--- Calculating tags"
-daefine_tags
-
 #echo "--- Setting git config"
 #set_git_config
+
+#buildkite-agent annotate "Tag '$TAG' has been created." --style 'success' --context 'ctx-success'
