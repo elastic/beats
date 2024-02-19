39303609e8a78e58601c6a95cc9f1e0b834e038b
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2024-02-19 21:09:15 +0200

    full test

--- .buildkite/scripts/setenv.sh
+++ .buildkite/scripts/setenv.sh
@@ -6,9 +6,8 @@ SETUP_GVM_VERSION="v0.5.1"
 DOCKER_COMPOSE_VERSION="1.21.0"
 DOCKER_COMPOSE_VERSION_AARCH64="v2.21.0"
 SETUP_WIN_PYTHON_VERSION="3.11.0"
-NMAP_WIN_VERSION="7.12"
+NMAP_WIN_VERSION="7.12"           # Earlier versions of NMap provide WinPcap (the winpcap packages don't install nicely because they pop-up a UI)
 GO_VERSION=$(cat .go-version)
-
 
 export SETUP_GVM_VERSION
 export DOCKER_COMPOSE_VERSION
