dec06a64d1d3bf2f44a068d0226d9985ba1a6134
Pavel Zorin <pavel.zorin@elastic.co>
2024-02-23 15:40:01 +0100

    Added dependencies report

--- .buildkite/x-pack/elastic-agent/scripts/steps/package.sh
+++ .buildkite/x-pack/elastic-agent/scripts/steps/package.sh
@@ -3,6 +3,8 @@ set -uo pipefail
 source ".buildkite/x-pack/elastic-agent/scripts/steps/setenv.sh"
 source ".buildkite/x-pack/elastic-agent/scripts/steps/common.sh"
 
+DIST_PATH="x-pack/elastic-agent/build/distributions"
+
 set -x
 if test -z "${ManifestURL=:""}"; then
   echo "Missing variable ManifestURL, export it before use."
@@ -18,3 +20,9 @@ export AGENT_DROP_PATH=build/elastic-agent-drop
 mkdir -p $AGENT_DROP_PATH
 
 mage -v -d x-pack/elastic-agent clean downloadManifest package ironbank fixDRADockerArtifacts
+
+echo  "+++ Generate dependencies report"
+BEAT_VERSION_FULL=$(curl -s -XGET "${ManifestURL}" |jq '.version' -r )
+bash dev-tools/dependencies-report
+mkdir -p $DIST_PATH/reports
+mv dependencies.csv "$DIST_PATH/reports/dependencies-${BEAT_VERSION_FULL}.csv"
