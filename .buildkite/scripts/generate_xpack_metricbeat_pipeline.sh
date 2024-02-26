8ae572e2e2a9160ad704a9d6f8b53738a1a18aaf
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2024-02-26 10:23:35 +0200

    increase the maximum timeout for x-pack/metricbeat pipeline

--- .buildkite/scripts/generate_xpack_metricbeat_pipeline.sh
+++ .buildkite/scripts/generate_xpack_metricbeat_pipeline.sh
@@ -46,58 +46,58 @@ steps:
           machineType: "${GCP_HI_PERF_MACHINE_TYPE}"
         artifact_paths: "${BEATS_PROJECT_NAME}/build/*.xml"
 
-      # - label: ":windows: Windows Unit Tests - {{matrix.image}}"
-      #   command: ".buildkite/scripts/win_unit_tests.ps1"
-      #   key: "mandatory-win-unit-tests"
-      #   agents:
-      #     provider: "gcp"
-      #     image: "{{matrix.image}}"
-      #     machineType: "${GCP_WIN_MACHINE_TYPE}"
-      #     disk_size: 100
-      #     disk_type: "pd-ssd"
-      #   matrix:
-      #     setup:
-      #       image:
-      #         - "${IMAGE_WIN_2016}"
-      #         - "${IMAGE_WIN_2022}"
-      #   artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
+      - label: ":windows: Windows Unit Tests - {{matrix.image}}"
+        command: ".buildkite/scripts/win_unit_tests.ps1"
+        key: "mandatory-win-unit-tests"
+        agents:
+          provider: "gcp"
+          image: "{{matrix.image}}"
+          machineType: "${GCP_WIN_MACHINE_TYPE}"
+          disk_size: 100
+          disk_type: "pd-ssd"
+        matrix:
+          setup:
+            image:
+              - "${IMAGE_WIN_2016}"
+              - "${IMAGE_WIN_2022}"
+        artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
 
 ### TODO: this condition will be changed in the Phase 3 of the Migration Plan https://docs.google.com/document/d/1IPNprVtcnHlem-uyGZM0zGzhfUuFAh4LeSl9JFHMSZQ/edit#heading=h.sltz78yy249h
-  # - group: "Extended Windows Tests"
-  #   key: "extended-win-tests"
-  #   steps:
-  #     - label: ":windows: Windows 10 Unit Tests"
-  #       key: "extended-win-10-unit-tests"
-  #       command: ".buildkite/scripts/win_unit_tests.ps1"
-  #       agents:
-  #         provider: "gcp"
-  #         image: "${IMAGE_WIN_10}"
-  #         machineType: "${GCP_WIN_MACHINE_TYPE}"
-  #         disk_size: 100
-  #         disk_type: "pd-ssd"
-  #       artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
+  - group: "Extended Windows Tests"
+    key: "extended-win-tests"
+    steps:
+      - label: ":windows: Windows 10 Unit Tests"
+        key: "extended-win-10-unit-tests"
+        command: ".buildkite/scripts/win_unit_tests.ps1"
+        agents:
+          provider: "gcp"
+          image: "${IMAGE_WIN_10}"
+          machineType: "${GCP_WIN_MACHINE_TYPE}"
+          disk_size: 100
+          disk_type: "pd-ssd"
+        artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
 
-  #     - label: ":windows: Windows 11 Unit Tests"
-  #       key: "extended-win-11-unit-tests"
-  #       command: ".buildkite/scripts/win_unit_tests.ps1"
-  #       agents:
-  #         provider: "gcp"
-  #         image: "${IMAGE_WIN_11}"
-  #         machineType: "${GCP_WIN_MACHINE_TYPE}"
-  #         disk_size: 100
-  #         disk_type: "pd-ssd"
-  #       artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
+      - label: ":windows: Windows 11 Unit Tests"
+        key: "extended-win-11-unit-tests"
+        command: ".buildkite/scripts/win_unit_tests.ps1"
+        agents:
+          provider: "gcp"
+          image: "${IMAGE_WIN_11}"
+          machineType: "${GCP_WIN_MACHINE_TYPE}"
+          disk_size: 100
+          disk_type: "pd-ssd"
+        artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
 
-  #     - label: ":windows: Win 2019 Unit Tests"
-  #       key: "extended-win-2019-unit-tests"
-  #       command: ".buildkite/scripts/win_unit_tests.ps1"
-  #       agents:
-  #         provider: "gcp"
-  #         image: "${IMAGE_WIN_2019}"
-  #         machineType: "${GCP_WIN_MACHINE_TYPE}"
-  #         disk_size: 100
-  #         disk_type: "pd-ssd"
-  #       artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
+      - label: ":windows: Win 2019 Unit Tests"
+        key: "extended-win-2019-unit-tests"
+        command: ".buildkite/scripts/win_unit_tests.ps1"
+        agents:
+          provider: "gcp"
+          image: "${IMAGE_WIN_2019}"
+          machineType: "${GCP_WIN_MACHINE_TYPE}"
+          disk_size: 100
+          disk_type: "pd-ssd"
+        artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
 
 YAML
 else
@@ -105,7 +105,10 @@ else
   exit 0
 fi
 
-if [[ are_conditions_met_aws_tests || are_conditions_met_macos_tests ]]; then
+#TODO: replace by commented-out below condition when issues mentioned in the PR https://github.com/elastic/beats/pull/38081 are resolved
+
+# if [[ are_conditions_met_aws_tests || are_conditions_met_macos_tests ]]; then
+if [[ are_conditions_met_macos_tests ]]; then
   cat >> $pipelineName <<- YAML
 
   - group: "Extended Tests"
@@ -129,54 +132,56 @@ if  are_conditions_met_macos_tests; then
 YAML
 fi
 
-if  are_conditions_met_aws_tests; then
-  cat >> $pipelineName <<- YAML
-      - label: ":linux: ${MODULE^^} Cloud Tests"
-        key: "extended-cloud-test"
-        command: "cd $BEATS_PROJECT_NAME && mage build test"
-        env:
-          MODULE: "$MODULE"
-        agents:
-          provider: "gcp"
-          image: "${IMAGE_UBUNTU_X86_64}"
-          machineType: "${GCP_HI_PERF_MACHINE_TYPE}"
-        artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
+#TODO: uncomment the commented-out below step when issues mentioned in the PR https://github.com/elastic/beats/pull/38081 are resolved
 
-YAML
-fi
+# if  are_conditions_met_aws_tests; then
+#   cat >> $pipelineName <<- YAML
+#       - label: ":linux: ${MODULE^^} Cloud Tests"
+#         key: "extended-cloud-test"
+#         command: "cd $BEATS_PROJECT_NAME && mage build test"
+#         env:
+#           MODULE: "$MODULE"
+#         agents:
+#           provider: "gcp"
+#           image: "${IMAGE_UBUNTU_X86_64}"
+#           machineType: "${GCP_HI_PERF_MACHINE_TYPE}"
+#         artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
+
+# YAML
+# fi
 
 echo "Check and add the Packaging into the pipeline"
 if are_conditions_met_packaging; then
   cat >> $pipelineName <<- YAML
 
-  # - wait: ~
-  #   depends_on:
-  #     - step: "mandatory-tests"
-  #       allow_failure: false
+  - wait: ~
+    depends_on:
+      - step: "mandatory-tests"
+        allow_failure: false
 
-  # - group: "Packaging"    # TODO: check conditions for future the main pipeline migration: https://github.com/elastic/beats/pull/28589
-  #   key: "packaging"
-  #   steps:
-  #     - label: ":linux: Packaging Linux"
-  #       key: "packaging-linux"
-  #       command: "cd $BEATS_PROJECT_NAME && mage package"
-  #       agents:
-  #         provider: "gcp"
-  #         image: "${IMAGE_UBUNTU_X86_64}"
-  #         machineType: "${GCP_HI_PERF_MACHINE_TYPE}"
-  #       env:
-  #         PLATFORMS: "${PACKAGING_PLATFORMS}"
+  - group: "Packaging"    # TODO: check conditions for future the main pipeline migration: https://github.com/elastic/beats/pull/28589
+    key: "packaging"
+    steps:
+      - label: ":linux: Packaging Linux"
+        key: "packaging-linux"
+        command: "cd $BEATS_PROJECT_NAME && mage package"
+        agents:
+          provider: "gcp"
+          image: "${IMAGE_UBUNTU_X86_64}"
+          machineType: "${GCP_HI_PERF_MACHINE_TYPE}"
+        env:
+          PLATFORMS: "${PACKAGING_PLATFORMS}"
 
-  #     - label: ":linux: Packaging ARM"
-  #       key: "packaging-arm"
-  #       command: "cd $BEATS_PROJECT_NAME && mage package"
-  #       agents:
-  #         provider: "aws"
-  #         imagePrefix: "${IMAGE_UBUNTU_ARM_64}"
-  #         instanceType: "${AWS_ARM_INSTANCE_TYPE}"
-  #       env:
-  #         PLATFORMS: "${PACKAGING_ARM_PLATFORMS}"
-  #         PACKAGES: "docker"
+      - label: ":linux: Packaging ARM"
+        key: "packaging-arm"
+        command: "cd $BEATS_PROJECT_NAME && mage package"
+        agents:
+          provider: "aws"
+          imagePrefix: "${IMAGE_UBUNTU_ARM_64}"
+          instanceType: "${AWS_ARM_INSTANCE_TYPE}"
+        env:
+          PLATFORMS: "${PACKAGING_ARM_PLATFORMS}"
+          PACKAGES: "docker"
 
 YAML
 fi
