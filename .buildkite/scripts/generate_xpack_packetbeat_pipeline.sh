7c9fcd8337b0ece31cce5b8dec30852c99371ecf
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2024-02-27 18:19:38 +0200

    test

--- .buildkite/scripts/generate_xpack_packetbeat_pipeline.sh
+++ .buildkite/scripts/generate_xpack_packetbeat_pipeline.sh
@@ -15,49 +15,49 @@ steps:
   - group: "Mandatory Tests"
     key: "mandatory-tests"
     steps:
-      - label: ":linux: Ubuntu Unit Tests"
-        key: "mandatory-linux-unit-test"
-        command: "cd $BEATS_PROJECT_NAME && mage build unitTest"
-        agents:
-          provider: "gcp"
-          image: "${IMAGE_UBUNTU_X86_64}"
-          machineType: "${GCP_DEFAULT_MACHINE_TYPE}"
-        artifact_paths: "${BEATS_PROJECT_NAME}/build/*.xml"
+      # - label: ":linux: Ubuntu Unit Tests"
+      #   key: "mandatory-linux-unit-test"
+      #   command: "cd $BEATS_PROJECT_NAME && mage build unitTest"
+      #   agents:
+      #     provider: "gcp"
+      #     image: "${IMAGE_UBUNTU_X86_64}"
+      #     machineType: "${GCP_DEFAULT_MACHINE_TYPE}"
+      #   artifact_paths: "${BEATS_PROJECT_NAME}/build/*.xml"
 
-      - label: ":linux: Ubuntu System Tests"
-        key: "mandatory-linux-system-test"
-        command: "cd $BEATS_PROJECT_NAME && mage systemTest"
-        agents:
-          provider: "gcp"
-          image: "${IMAGE_UBUNTU_X86_64}"
-          machineType: "${GCP_DEFAULT_MACHINE_TYPE}"
-        artifact_paths: "${BEATS_PROJECT_NAME}/build/*.xml"
+      # - label: ":linux: Ubuntu System Tests"
+      #   key: "mandatory-linux-system-test"
+      #   command: "cd $BEATS_PROJECT_NAME && mage systemTest"
+      #   agents:
+      #     provider: "gcp"
+      #     image: "${IMAGE_UBUNTU_X86_64}"
+      #     machineType: "${GCP_DEFAULT_MACHINE_TYPE}"
+      #   artifact_paths: "${BEATS_PROJECT_NAME}/build/*.xml"
 
-      - label: ":rhel: RHEL-9 Unit Tests"
-        key: "mandatory-rhel9-unit-test"
-        command: ".buildkite/scripts/unit_tests.sh"
-        agents:
-          provider: "gcp"
-          image: "${IMAGE_RHEL9_X86_64}"
-          machineType: "${GCP_DEFAULT_MACHINE_TYPE}"
-        artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
+      # - label: ":rhel: RHEL-9 Unit Tests"
+      #   key: "mandatory-rhel9-unit-test"
+      #   command: ".buildkite/scripts/unit_tests.sh"
+      #   agents:
+      #     provider: "gcp"
+      #     image: "${IMAGE_RHEL9_X86_64}"
+      #     machineType: "${GCP_DEFAULT_MACHINE_TYPE}"
+      #   artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
 
 
-      - label: ":windows: Windows Unit Tests - {{matrix.image}}"
-        command: ".buildkite/scripts/win_unit_tests.ps1 unittests"
-        key: "mandatory-win-unit-tests"
-        agents:
-          provider: "gcp"
-          image: "{{matrix.image}}"
-          machineType: "${GCP_WIN_MACHINE_TYPE}"
-          disk_size: 100
-          disk_type: "pd-ssd"
-        matrix:
-          setup:
-            image:
-              - "${IMAGE_WIN_2016}"
-              - "${IMAGE_WIN_2022}"
-        artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
+      # - label: ":windows: Windows Unit Tests - {{matrix.image}}"
+      #   command: ".buildkite/scripts/win_unit_tests.ps1 unittests"
+      #   key: "mandatory-win-unit-tests"
+      #   agents:
+      #     provider: "gcp"
+      #     image: "{{matrix.image}}"
+      #     machineType: "${GCP_WIN_MACHINE_TYPE}"
+      #     disk_size: 100
+      #     disk_type: "pd-ssd"
+      #   matrix:
+      #     setup:
+      #       image:
+      #         - "${IMAGE_WIN_2016}"
+      #         - "${IMAGE_WIN_2022}"
+      #   artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
 
       - label: ":windows: Windows 2022 System Tests"
         key: "mandatory-win-2022-system-tests"
@@ -76,33 +76,33 @@ steps:
     key: "extended-win-tests"
     steps:
 
-      - label: ":windows: Windows Unit Tests - {{matrix.image}}"
-        command: ".buildkite/scripts/win_unit_tests.ps1 unittests"
-        key: "extended-win-unit-tests"
-        agents:
-          provider: "gcp"
-          image: "{{matrix.image}}"
-          machineType: "${GCP_WIN_MACHINE_TYPE}"
-          disk_size: 100
-          disk_type: "pd-ssd"
-        matrix:
-          setup:
-            image:
-              - "${IMAGE_WIN_10}"
-              - "${IMAGE_WIN_11}"
-              - "${IMAGE_WIN_2019}"
-        artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
+      # - label: ":windows: Windows Unit Tests - {{matrix.image}}"
+      #   command: ".buildkite/scripts/win_unit_tests.ps1 unittests"
+      #   key: "extended-win-unit-tests"
+      #   agents:
+      #     provider: "gcp"
+      #     image: "{{matrix.image}}"
+      #     machineType: "${GCP_WIN_MACHINE_TYPE}"
+      #     disk_size: 100
+      #     disk_type: "pd-ssd"
+      #   matrix:
+      #     setup:
+      #       image:
+      #         - "${IMAGE_WIN_10}"
+      #         - "${IMAGE_WIN_11}"
+      #         - "${IMAGE_WIN_2019}"
+      #   artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
 
-      - label: ":windows: Windows 10 System Tests"
-        key: "extended-win-10-system-tests"
-        command: ".buildkite/scripts/win_unit_tests.ps1 systemtests"
-        agents:
-          provider: "gcp"
-          image: "${IMAGE_WIN_10}"
-          machineType: "${GCP_WIN_MACHINE_TYPE}"
-          disk_size: 100
-          disk_type: "pd-ssd"
-        artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
+      # - label: ":windows: Windows 10 System Tests"
+      #   key: "extended-win-10-system-tests"
+      #   command: ".buildkite/scripts/win_unit_tests.ps1 systemtests"
+      #   agents:
+      #     provider: "gcp"
+      #     image: "${IMAGE_WIN_10}"
+      #     machineType: "${GCP_WIN_MACHINE_TYPE}"
+      #     disk_size: 100
+      #     disk_type: "pd-ssd"
+      #   artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
 
 YAML
 else
@@ -110,79 +110,79 @@ else
   exit 0
 fi
 
-if [[ are_conditions_met_arm_tests || are_conditions_met_macos_tests ]]; then
-  cat >> $pipelineName <<- YAML
+# if [[ are_conditions_met_arm_tests || are_conditions_met_macos_tests ]]; then
+#   cat >> $pipelineName <<- YAML
 
-  - group: "Extended Tests"
-    key: "extended-tests"
-    steps:
+#   - group: "Extended Tests"
+#     key: "extended-tests"
+#     steps:
 
-YAML
-fi
+# YAML
+# fi
 
-if  are_conditions_met_macos_tests; then
-  cat >> $pipelineName <<- YAML
+# if  are_conditions_met_macos_tests; then
+#   cat >> $pipelineName <<- YAML
 
-      - label: ":mac: MacOS Unit Tests"
-        key: "extended-macos-unit-tests"
-        command: ".buildkite/scripts/unit_tests.sh"
-        agents:
-          provider: "orka"
-          imagePrefix: "${IMAGE_MACOS_X86_64}"
-        artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
+#       - label: ":mac: MacOS Unit Tests"
+#         key: "extended-macos-unit-tests"
+#         command: ".buildkite/scripts/unit_tests.sh"
+#         agents:
+#           provider: "orka"
+#           imagePrefix: "${IMAGE_MACOS_X86_64}"
+#         artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
 
-YAML
-fi
+# YAML
+# fi
 
-if  are_conditions_met_arm_tests; then
-  cat >> $pipelineName <<- YAML
-      - label: ":linux: ARM Ubuntu Unit Tests"
-        key: "extended-arm64-unit-test"
-        command: "cd $BEATS_PROJECT_NAME && mage build unitTest"
-        agents:
-          provider: "aws"
-          imagePrefix: "${IMAGE_UBUNTU_ARM_64}"
-          instanceType: "${AWS_ARM_INSTANCE_TYPE}"
-        artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
+# if  are_conditions_met_arm_tests; then
+#   cat >> $pipelineName <<- YAML
+#       - label: ":linux: ARM Ubuntu Unit Tests"
+#         key: "extended-arm64-unit-test"
+#         command: "cd $BEATS_PROJECT_NAME && mage build unitTest"
+#         agents:
+#           provider: "aws"
+#           imagePrefix: "${IMAGE_UBUNTU_ARM_64}"
+#           instanceType: "${AWS_ARM_INSTANCE_TYPE}"
+#         artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
 
-YAML
-fi
+# YAML
+# fi
 
-echo "Check and add the Packaging into the pipeline"
-if are_conditions_met_packaging; then
-  cat >> $pipelineName <<- YAML
+# echo "Check and add the Packaging into the pipeline"
+# if are_conditions_met_packaging; then
+#   cat >> $pipelineName <<- YAML
 
-  - wait: ~
-    depends_on:
-      - step: "mandatory-tests"
-        allow_failure: false
+#   - wait: ~
+#     depends_on:
+#       - step: "mandatory-tests"
+#         allow_failure: false
 
-  - group: "Packaging"    # TODO: check conditions for future the main pipeline migration: https://github.com/elastic/beats/pull/28589
-    key: "packaging"
-    steps:
-      - label: ":linux: Packaging Linux"
-        key: "packaging-linux"
-        command: "cd $BEATS_PROJECT_NAME && mage package"
-        agents:
-          provider: "gcp"
-          image: "${IMAGE_UBUNTU_X86_64}"
-          machineType: "${GCP_HI_PERF_MACHINE_TYPE}"
-        env:
-          PLATFORMS: "${PACKAGING_PLATFORMS}"
+#   - group: "Packaging"    # TODO: check conditions for future the main pipeline migration: https://github.com/elastic/beats/pull/28589
+#     key: "packaging"
+#     steps:
+#       - label: ":linux: Packaging Linux"
+#         key: "packaging-linux"
+#         command: "cd $BEATS_PROJECT_NAME && mage package"
+#         agents:
+#           provider: "gcp"
+#           image: "${IMAGE_UBUNTU_X86_64}"
+#           machineType: "${GCP_HI_PERF_MACHINE_TYPE}"
+#         env:
+#           PLATFORMS: "${PACKAGING_PLATFORMS}"
 
-      - label: ":linux: Packaging ARM"
-        key: "packaging-arm"
-        command: "cd $BEATS_PROJECT_NAME && mage package"
-        agents:
-          provider: "aws"
-          imagePrefix: "${IMAGE_UBUNTU_ARM_64}"
-          instanceType: "${AWS_ARM_INSTANCE_TYPE}"
-        env:
-          PLATFORMS: "${PACKAGING_ARM_PLATFORMS}"
-          PACKAGES: "docker"
+#       - label: ":linux: Packaging ARM"
+#         key: "packaging-arm"
+#         command: "cd $BEATS_PROJECT_NAME && mage package"
+#         agents:
+#           provider: "aws"
+#           imagePrefix: "${IMAGE_UBUNTU_ARM_64}"
+#           instanceType: "${AWS_ARM_INSTANCE_TYPE}"
+#         env:
+#           PLATFORMS: "${PACKAGING_ARM_PLATFORMS}"
+#           PACKAGES: "docker"
 
-YAML
-fi
+# YAML
+# fi
 
 echo "--- Printing dynamic steps"     #TODO: remove if the pipeline is public
 cat $pipelineName
