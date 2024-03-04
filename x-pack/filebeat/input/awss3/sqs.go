7120e9fd26906f94e7d7ed0f118a2dadd66cabc6
Andrea Spacca <andrea.spacca@elastic.co>
2024-03-04 16:33:02 +0900

    Improve sqsReader.Receive

--- x-pack/filebeat/input/awss3/sqs.go
+++ x-pack/filebeat/input/awss3/sqs.go
@@ -60,54 +60,82 @@ func newSQSReader(log *logp.Logger, metrics *inputMetrics, sqs sqsAPI, maxMessag
 }
 
 func (r *sqsReader) Receive(ctx context.Context, pipeline beat.Pipeline) error {
-	// This loop tries to keep the workers busy as much as possible while
+	// The loop tries to keep the ProcessSQS workers busy as much as possible while
 	// honoring the max message cap as opposed to a simpler loop that receives
-	// N messages, waits for them all to finish, then requests N more messages.
-	var workerWg sync.WaitGroup
-	endingChan := make(chan error, 1)
+	// N messages, waits for them all to finish sending events to the queue, then requests N more messages.
+	var processingWg sync.WaitGroup
+
+	// At the end of the loop, after a shutdown or anything else, we still need to wait for the DeleteSQS goroutines to
+	// complete, otherwise the messages will be sent back to the queue even if they should be deleted.
+	var deletionWg sync.WaitGroup
+
+	// We send to processingChan the outcome of each ProcessSQS call.
+	// We don't want to buffer the processingChan, since it will prevent workers ProcessSQS goroutines to return.
 	processingChan := make(chan processingOutcome)
 
-	go func(ctx context.Context) {
-		// Wait for all workers to finish.
+	// We use deletionChan to throttle the number of DeleteSQS goroutines.
+	// deletionChan := make(chan struct{}, 3200)
+
+	go func() {
 		for {
 			select {
-			case processOutcome, ok := <-processingChan:
+			case outcome, ok := <-processingChan:
+				// processingChang is closed, no more outcomes to process, we can exit.
 				if !ok {
-					if errors.Is(ctx.Err(), context.Canceled) {
-						// A canceled context is a normal shutdown.
-						close(endingChan)
-						return
-					}
-
-					endingChan <- ctx.Err()
 					return
 				}
 
-				go func(processOutcome processingOutcome) {
+				// A ProcessSQS goroutine has sent an outcome, let's process it asynchronously in order to handle SQS message deletion.
+				go func(outcome processingOutcome) {
+					// Mark deletion wait group as done when the goroutine is done.
+					defer deletionWg.Done()
+
+					r.log.Debugw("Waiting worker when deleting SQS message.",
+						"message_id", *outcome.msg.MessageId,
+						"elapsed_time_ns", time.Since(outcome.start))
+
+					// We don't want to cap processingChan, since it will prevent workers ProcessSQS goroutines to return
+					// and in flight message would be capped as well.
+					// We want to cap number of goroutines for DeleteSQS
+					// deletionChan <- struct{}{}
+
+					r.log.Debugw("Waited worker when deleting SQS message.",
+						"message_id", *outcome.msg.MessageId,
+						"elapsed_time_ns", time.Since(outcome.start))
+
+					r.log.Debugw("Waiting acker when deleting SQS message.",
+						"message_id", *outcome.msg.MessageId,
+						"elapsed_time_ns", time.Since(outcome.start))
+
 					// Wait for all events to be ACKed before proceeding.
-					processOutcome.acker.Wait()
+					outcome.acker.Wait()
 
-					// Stop keepalive routine before deleting visibility.
-					processOutcome.keepaliveCancel()
-					processOutcome.keepaliveWg.Wait()
+					r.log.Debugw("Waited acker when deleting SQS message.",
+						"message_id", *outcome.msg.MessageId,
+						"elapsed_time_ns", time.Since(outcome.start))
 
-					err := r.msgHandler.DeleteSQS(processOutcome.msg, processOutcome.receiveCount, processOutcome.processingErr, processOutcome.handles)
+					// Stop keepalive visibility routine before deleting.
+					outcome.keepaliveCancel()
+					outcome.keepaliveWg.Wait()
+
+					err := r.msgHandler.DeleteSQS(outcome.msg, outcome.receiveCount, outcome.processingErr, outcome.handles)
 					if err != nil {
 						r.log.Warnw("Failed deleting SQS message.",
 							"error", err,
-							"message_id", *processOutcome.msg.MessageId,
-							"elapsed_time_ns", time.Since(processOutcome.start))
+							"message_id", *outcome.msg.MessageId,
+							"elapsed_time_ns", time.Since(outcome.start))
 					} else {
-						r.log.Warnw("Success deleting SQS message.",
-							"message_id", *processOutcome.msg.MessageId,
-							"elapsed_time_ns", time.Since(processOutcome.start))
+						r.log.Debugw("Success deleting SQS message.",
+							"message_id", *outcome.msg.MessageId,
+							"elapsed_time_ns", time.Since(outcome.start))
 					}
-				}(processOutcome)
-			default:
-				time.Sleep(500 * time.Microsecond)
+
+					// <-deletionChan
+				}(outcome)
 			}
+
 		}
-	}(ctx)
+	}()
 
 	for ctx.Err() == nil {
 		// Determine how many SQS workers are available.
@@ -133,17 +161,20 @@ func (r *sqsReader) Receive(ctx context.Context, pipeline beat.Pipeline) error {
 		// Release unused workers.
 		r.workerSem.Release(workers - len(msgs))
 
-		// Process each SQS message asynchronously with a goroutine.
 		r.log.Debugf("Received %v SQS messages.", len(msgs))
 		r.metrics.sqsMessagesReceivedTotal.Add(uint64(len(msgs)))
-		workerWg.Add(len(msgs))
+
+		// Add to processing wait group to wait for all messages to be processed.
+		processingWg.Add(len(msgs))
 
 		for _, msg := range msgs {
+			// Process each SQS message asynchronously with a goroutine.
 			go func(msg types.Message, start time.Time) {
 				id := r.metrics.beginSQSWorker()
 				defer func() {
+					// Mark processing wait group as done.
 					r.metrics.endSQSWorker(id)
-					workerWg.Done()
+					processingWg.Done()
 					r.workerSem.Release(1)
 				}()
 
@@ -168,10 +199,22 @@ func (r *sqsReader) Receive(ctx context.Context, pipeline beat.Pipeline) error {
 
 				defer client.Close()
 
+				r.log.Debugw("Going to process SQS message.",
+					"message_id", *msg.MessageId,
+					"elapsed_time_ns", time.Since(start))
+
 				acker := awscommon.NewEventACKTracker(ctx)
 
 				receiveCount, handles, keepaliveCancel, keepaliveWg, processingErr := r.msgHandler.ProcessSQS(ctx, &msg, client, acker)
 
+				r.log.Debugw("Success processing SQS message.",
+					"message_id", *msg.MessageId,
+					"elapsed_time_ns", time.Since(start))
+
+				// Add to deletion waiting group before sending to processingChan.
+				deletionWg.Add(1)
+
+				// Send the outcome to the processingChan so the deletion goroutine can delete the message.
 				processingChan <- processingOutcome{
 					start:           start,
 					keepaliveWg:     keepaliveWg,
@@ -186,10 +229,21 @@ func (r *sqsReader) Receive(ctx context.Context, pipeline beat.Pipeline) error {
 		}
 	}
 
-	workerWg.Wait()
+	// Wait for all processing goroutines to finish.
+	processingWg.Wait()
+
+	// We need to close the processingChan to signal to the deletion goroutines that they should stop.
 	close(processingChan)
 
-	return <-endingChan
+	// Wait for all deletion goroutines to finish.
+	deletionWg.Wait()
+
+	if errors.Is(ctx.Err(), context.Canceled) {
+		// A canceled context is a normal shutdown.
+		return nil
+	}
+
+	return ctx.Err()
 }
 
 func (r *sqsReader) GetApproximateMessageCount(ctx context.Context) (int, error) {
