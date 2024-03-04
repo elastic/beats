306b2ff3ebf17e5a001ae6a1bc4f67d843eb0475
Victor Martinez <VictorMartinezRubio@gmail.com>
2024-03-04 10:06:25 +0100

    debug traces

--- .buildkite/scripts/generate_pipeline.sh
+++ .buildkite/scripts/generate_pipeline.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Install dependencies
+echo "--- Install dependencies"
 pip3 install --quiet jinja2
 pip3 install --quiet PyYAML
 
+echo "--- Run pipeline generator in dry-run mode"
+python3 .buildkite/pipeline.py || true
+
 # Run the python generator - likely this should be called in the
 # catalog-info.yaml
+echo "--- Upload pipeline"
 python3 .buildkite/pipeline.py | buildkite-agent pipeline upload
