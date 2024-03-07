5823883848013875d4e31e1fd91381e320354fb3
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2024-03-07 12:59:10 +0200

    test with hardcoded MODULE=''

--- .buildkite/scripts/cloud_tests.sh
+++ .buildkite/scripts/cloud_tests.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 echo "--- Run Cloud Tests for $BEATS_PROJECT_NAME"
 pushd "${BEATS_PROJECT_NAME}" > /dev/null
 
-MODULE=aws mage build test
+MODULE='' mage build test
 
 popd > /dev/null
 
