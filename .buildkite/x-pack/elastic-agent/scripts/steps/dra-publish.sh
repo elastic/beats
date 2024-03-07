6e0efc257a7719ef4b3d19a704731012ded5f925
Pavel Zorin <pavel.zorin@elastic.co>
2024-03-07 15:53:28 +0100

    Added branch exclusion

--- .buildkite/x-pack/elastic-agent/scripts/steps/dra-publish.sh
+++ .buildkite/x-pack/elastic-agent/scripts/steps/dra-publish.sh
@@ -11,7 +11,7 @@ CI_DRA_ROLE_PATH="kv/ci-shared/release/dra-role"
 
 # force main branch on PR's or it won't execute
 # because the PR branch does not have a project folder in release-manager
-if [[ "${BUILDKITE_PULL_REQUEST:="false"}" != "false" ] || "${BUILDKITE_BRANCH}" == "packaging_elastic_agent" ]; then
+if [[ "${BUILDKITE_PULL_REQUEST:="false"}" != "false" || "${BUILDKITE_BRANCH}" == "packaging_elastic_agent" ]]; then
     BRANCH=7.17
     DRY_RUN="--dry-run"
     echo "+++ Running in PR or test branch and setting branch 7.17 and --dry-run"
