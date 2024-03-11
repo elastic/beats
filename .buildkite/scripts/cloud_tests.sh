25547b4ef8c75e9c356573b39e0bc25c0bf5e022
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2024-03-11 11:56:39 +0200

    move conditions to the pre-command hook

--- .buildkite/scripts/cloud_tests.sh
+++ .buildkite/scripts/cloud_tests.sh
@@ -9,18 +9,6 @@ trap 'cleanup; unset_secrets' EXIT
 echo "--- Run Cloud Tests for $BEATS_PROJECT_NAME"
 pushd "${BEATS_PROJECT_NAME}" > /dev/null
 
-# Cloud stage for x-pack/metricbeat
-if [[ "$BUILDKITE_PIPELINE_SLUG" == "beats-xpack-metricbeat" ]]; then
-  if [[ "${BUILDKITE_STEP_KEY}" == "mandatory-int-test" || "${BUILDKITE_STEP_KEY}" == "mandatory-python-int-test" || "${BUILDKITE_STEP_KEY}" == "extended-cloud-test" ]]; then
-    # Configure MODULE env variable.
-    defineModuleFromTheChangeSet "${MODULE_DIR}"
-  fi
-  if [[ "${BUILDKITE_STEP_KEY}" == "extended-cloud-test" ]]; then
-    # Start the required services for the required
-    startCloudTestEnv "${MODULE_DIR}"
-  fi
-fi
-
 mage build test
 
 popd > /dev/null
