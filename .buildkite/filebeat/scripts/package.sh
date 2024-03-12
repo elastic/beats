1cab75c23b561e5f7d43859a21784a5b1464a496
Victor Martinez <VictorMartinezRubio@gmail.com>
2024-03-12 11:51:55 +0100

    test: add traces and remove unrequired steps

--- .buildkite/filebeat/scripts/package.sh
+++ .buildkite/filebeat/scripts/package.sh
@@ -4,6 +4,11 @@ set -euo pipefail
 
 source .buildkite/env-scripts/linux-env.sh
 
+echo "--- Git status"
+git --no-pager status || true
+git --no-pager log -n2 || true
+git --no-pager diff || true
+
 echo "--- Start Packaging"
 cd filebeat
 umask 0022
