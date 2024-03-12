74a4d1f4d91c571429a51fb458c82983d0249eeb
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2024-03-12 18:32:46 +0200

    update setenv.sh

--- .buildkite/scripts/setenv.sh
+++ .buildkite/scripts/setenv.sh
@@ -11,15 +11,12 @@ SETUP_WIN_PYTHON_VERSION="3.11.0"
 NMAP_WIN_VERSION="7.12"           # Earlier versions of NMap provide WinPcap (the winpcap packages don't install nicely because they pop-up a UI)
 GO_VERSION=$(cat .go-version)
 ASDF_MAGE_VERSION="1.15.0"
-<<<<<<< HEAD
 PACKAGING_PLATFORMS="+all linux/amd64 linux/arm64 windows/amd64 darwin/amd64 darwin/arm64"
 PACKAGING_ARM_PLATFORMS="linux/arm64"
-=======
 ASDF_TERRAFORM_VERSION="1.0.2"
 PACKAGING_PLATFORMS="+all linux/amd64 linux/arm64 windows/amd64 darwin/amd64 darwin/arm64"
 PACKAGING_ARM_PLATFORMS="linux/arm64"
 AWS_REGION="eu-central-1"
->>>>>>> 843010c1cbddd201bb4c08a79fd649a0af3c04ce
 
 export SETUP_GVM_VERSION
 export DOCKER_COMPOSE_VERSION
@@ -30,14 +27,11 @@ export GO_VERSION
 export ASDF_MAGE_VERSION
 export PACKAGING_PLATFORMS
 export PACKAGING_ARM_PLATFORMS
-<<<<<<< HEAD
-=======
 export REPO
 export TMP_FOLDER
 export DOCKER_REGISTRY
 export ASDF_TERRAFORM_VERSION
 export AWS_REGION
->>>>>>> 843010c1cbddd201bb4c08a79fd649a0af3c04ce
 
 exportVars() {
   local platform_type="$(uname)"
@@ -65,16 +59,7 @@ exportVars() {
 }
 
 if [[ "$BUILDKITE_PIPELINE_SLUG" == "beats-metricbeat" || "$BUILDKITE_PIPELINE_SLUG" == "beats-xpack-metricbeat" ]]; then
-<<<<<<< HEAD
-  exportVars
-  export RACE_DETECTOR="true"
-  export TEST_COVERAGE="true"
-  export DOCKER_PULL="0"
-fi
 
-if [[ "$BUILDKITE_PIPELINE_SLUG" == "beats-xpack-metricbeat" ]]; then
-=======
->>>>>>> 843010c1cbddd201bb4c08a79fd649a0af3c04ce
   exportVars
   export RACE_DETECTOR="true"
   export TEST_COVERAGE="true"
