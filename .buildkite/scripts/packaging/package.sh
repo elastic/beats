a50d354828330265574e0104e3845b6211dffb4f
Olga Naidjonoka <olga_naidjonoka@epam.com>
2024-03-14 14:37:14 +0200

    added package-util script

--- .buildkite/scripts/packaging/package.sh
+++ .buildkite/scripts/packaging/package.sh
@@ -2,8 +2,7 @@
 
 set -euo pipefail
 
-#source .buildkite/scripts/packaging/package-util.sh
-#source .buildkite/env-scripts/util.sh
+source .buildkite/scripts/packaging/package-util.sh
 
 IMG_POSTFIX="-SNAPSHOT"
 VARIANTS=("" "-ubi" "-oss")
@@ -13,7 +12,7 @@ TARGET="observability-ci/${BEATS_PROJECT_NAME}"
 
 echo "--- Creating package"
 mage -d "${BEATS_PROJECT_NAME}" package
-#
+
 #echo "--- Distribution list"
 #dir="${BEATS_PROJECT_NAME}/build/distributions"
 #buildkite-agent artifact upload "$dir/*.tar.gz;$dir/*.tar.gz.sha512"
