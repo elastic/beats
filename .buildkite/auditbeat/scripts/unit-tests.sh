b92ffd764ebbfa403b7db70949ae4ddcd9978ef1
Pavel Zorin <pavel.zorin@elastic.co>
2024-03-14 23:57:00 +0100

    Use beats images

--- .buildkite/auditbeat/scripts/unit-tests.sh
+++ .buildkite/auditbeat/scripts/unit-tests.sh
@@ -7,6 +7,5 @@ source .buildkite/env-scripts/linux-env.sh
 echo "--- Running Unit Tests"
 sudo chmod -R go-w auditbeat/
 
-cd auditbeat
 umask 0022
-mage build unitTest
+mage -d auditbeat build unitTest
