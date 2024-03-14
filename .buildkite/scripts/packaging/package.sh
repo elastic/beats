26546a00926b1e5fcab497e6ae275d9eb8b44745
Olga Naidjonoka <olga_naidjonoka@epam.com>
2024-03-14 14:51:53 +0200

    added artifact upload

--- .buildkite/scripts/packaging/package.sh
+++ .buildkite/scripts/packaging/package.sh
@@ -13,9 +13,9 @@ TARGET="observability-ci/${BEATS_PROJECT_NAME}"
 echo "--- Creating package"
 mage -d "${BEATS_PROJECT_NAME}" package
 
-#echo "--- Distribution list"
-#dir="${BEATS_PROJECT_NAME}/build/distributions"
-#buildkite-agent artifact upload "$dir/*.tar.gz;$dir/*.tar.gz.sha512"
+echo "--- Distribution list"
+dir="${BEATS_PROJECT_NAME}/build/distributions"
+buildkite-agent artifact upload "$dir/*.tar.gz;$dir/*.tar.gz.sha512"
 #
 #echo "--- Docker image list"
 #docker images
