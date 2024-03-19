1dcce414d3526029475190789d6901b7d77f5225
Alexandros Sapranidis <alexandros@elastic.co>
2024-03-19 17:58:16 +0200

    Show pipeline in expanded mode
    The pipeline that this script is running is only creating a dynamic
    pipeline and thats why its ok to have it by default show expanded output
    
    Signed-off-by: Alexandros Sapranidis <alexandros@elastic.co>

--- .buildkite/scripts/generate_pipeline.sh
+++ .buildkite/scripts/generate_pipeline.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 echo "~~~ Install dependencies"
 pip3 install --quiet "ruamel.yaml<0.18.0"
 
-echo "~~~ Run pipeline generator in dry-run mode"
+echo "+++ Run pipeline generator in dry-run mode"
 python3 .buildkite/pipeline.py | yq .
 
 echo "~~~ Upload pipeline"
