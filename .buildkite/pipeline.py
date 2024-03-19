a74a5b684b6fd8a8f94f64d9ce04e87b641723dc
Alexandros Sapranidis <alexandros@elastic.co>
2024-03-19 15:54:30 +0200

    Cleanup
    - Replace yaml with Ruamel
    - Respect order from the blueprint of the pipeline
    - Remove dataclasses
    
    Signed-off-by: Alexandros Sapranidis <alexandros@elastic.co>

--- .buildkite/pipeline.py
+++ .buildkite/pipeline.py
@@ -1,77 +1,78 @@
 #!/usr/bin/env python3
 from typing import Any
-import yaml
+from ruamel.yaml import YAML
 import os
-from dataclasses import dataclass, field
 import subprocess
 import fnmatch
+import sys
 
 
-@dataclass(unsafe_hash=True)
 class Agent:
     """Buildkite Agent object"""
-    image: str
+
+    def __init__(self, image: str, provider: str):
+        self.image: str = image
+        self.provider: str = provider
 
     def create_entity(self):
         raise NotImplementedError("Not implemented yet")
 
 
-@dataclass(unsafe_hash=True)
 class AWSAgent(Agent):
     """AWS Agent object"""
-    image: str
 
-    def create_entity(self) -> dict[str, dict[str, str]]:
+    def __init__(self, image: str, instance_type: str = None):
+        super().__init__(image, "aws")
+        if instance_type is None:
+            self.instance_type: str = "t4g.large"
+        else:
+            self.instance_type = instance_type
+
+    def create_entity(self) -> dict[str, str]:
         return {
-            "agents": {
-                "provider": "aws",
-                "imagePrefix": self.image,
-                "instanceType": "t4g.large",
-            }
+            "provider": self.provider,
+            "imagePrefix": self.image,
+            "instanceType": self.instance_type,
         }
 
 
-@dataclass(unsafe_hash=True)
 class GCPAgent(Agent):
     """GCP Agent object"""
-    image: str
 
-    def create_entity(self) -> dict[str, dict[str, str]]:
+    def __init__(self, image: str):
+        super().__init__(image, "gcp")
+
+    def create_entity(self) -> dict[str, str]:
         return {
-            "agents": {
-                "provider": "gcp",
-                "image": self.image,
-            }
+            "provider": self.provider,
+            "image": self.image,
         }
 
 
-@dataclass(unsafe_hash=True)
 class OrkaAgent(Agent):
     """Orka Agent object"""
-    image: str
 
-    def create_entity(self) -> dict[str, dict[str, str]]:
+    def __init__(self, image: str):
+        super().__init__(image, "orka")
+
+    def create_entity(self) -> dict[str, str]:
         return {
-            "agents": {
-                "provider": "orka",
-                "imagePrefix": self.image,
-            }
+            "provider": self.provider,
+            "imagePrefix": self.image,
         }
 
 
-@dataclass(unsafe_hash=True)
 class Step:
     """Buildkite Step object"""
 
-    command: str
-    agent: Agent
-    name: str
-    project: str
-    category: str
-    label: str = field(init=False)
-    comment: str = field(init=False)
-
-    def __post_init__(self):
+    def __init__(
+        self, name: str, command: str, project: str, category: str, agent: Agent
+    ):
+        self.command: str = command
+        self.agent: Agent = agent
+        self.name: str = name
+        self.project: str = project
+        self.category: str = category
         self.comment = "/test " + self.project + " " + self.name
         self.label = self.name
 
@@ -82,44 +83,65 @@ class Step:
         data = {
             "label": f"{self.project} {self.name}",
             "command": [self.command],
-            "notify": [{
-                "github_commit_status": {
-                    "context": f"{self.project}: {self.name}",
+            "notify": [
+                {
+                    "github_commit_status": {
+                        "context": f"{self.project}: {self.name}",
+                    }
                 }
-            }],
+            ],
+            "agents": self.agent.create_entity(),
         }
-        data.update(self.agent.create_entity())
         return data
 
 
-@dataclass(unsafe_hash=True)
 class Group:
     """Buildkite Group object"""
 
-    project: str
-    category: str
-    steps: list[Step]
+    def __init__(self, project: str, category: str, steps: list[Step]):
+        self.project: str = project
+        self.category: str = category
+        self.steps: list[Step] = steps
 
     def __lt__(self, other):
         return self.project < other.project
 
-    def create_entity(self) -> dict[str, Any] | None:
+    def create_entity(self) -> dict[str, Any]:
         if len(self.steps) == 0:
-            return
+            return {}
 
         data = {
             "group": f"{self.project} {self.category}",
             "key": f"{self.project}-{self.category}",
-            "steps": [step.create_entity() for step in sorted(self.steps)],
+            "steps": [step.create_entity() for step in self.steps],
         }
 
         return data
 
 
-@dataclass()
-class Pipeline:
+class GitHelper:
+    def __init__(self):
+        self.files: list[str] = []
+
+    def get_pr_changeset(self) -> list[str]:
+        base_branch = os.getenv("BUILDKITE_PULL_REQUEST_BASE_BRANCH", "main")
+        diff_command = ["git", "diff", "--name-only", "{}...HEAD".format(base_branch)]
+        result = subprocess.run(diff_command, stdout=subprocess.PIPE)
+        if result.returncode == 0:
+            self.files = result.stdout.decode().splitlines()
+        else:
+            print(f"Detecting changed files failed, exiting [{result.returncode}]")
+            exit(result.returncode)
+        return self.files
+
+
+class BuildkitePipeline:
     """Buildkite Pipeline object"""
-    groups: list[Group]
+
+    def __init__(self, groups: list[Group] = None):
+        if groups is None:
+            groups = []
+        self.groups: list[Group] = groups
 
     def create_entity(self):
         data = {"steps": [group.create_entity() for group in self.groups]}
@@ -127,11 +149,11 @@ class Pipeline:
 
 
 def is_pr() -> bool:
-    return os.getenv('BUILDKITE_PULL_REQUEST') != "false"
+    return os.getenv("BUILDKITE_PULL_REQUEST") != "false"
 
 
 def group_comment(group: Group) -> bool:
-    comment = os.getenv('GITHUB_PR_TRIGGER_COMMENT')
+    comment = os.getenv("GITHUB_PR_TRIGGER_COMMENT")
     if comment:
         # the comment should be a subset of the values
         # in .buildkite/pull-requests.json
@@ -143,25 +165,8 @@ def group_comment(group: Group) -> bool:
         else:
             # i.e: test filebeat extended
             return (
-                comment_prefix + " " + group.project + " " + group.category
-                in comment
+                comment_prefix + " " + group.project + " " + group.category in comment
             )
-
-
-changed_files = None
-
-
-def get_pr_changeset():
-    global changed_files
-    if not changed_files:
-        base_branch = os.getenv('BUILDKITE_PULL_REQUEST_BASE_BRANCH')
-        diff_command = [
-            "git", "diff", "--name-only", "{}...HEAD".format(base_branch)
-        ]
-        result = subprocess.run(diff_command, stdout=subprocess.PIPE)
-        changed_files = result.stdout.decode().splitlines()
-        print("Changed files: {}".format(changed_files))
-    return changed_files
 
 
 def filter_files_by_glob(files, patterns: list[str]):
@@ -173,17 +178,23 @@ def filter_files_by_glob(files, patterns: list[str]):
     return False
 
 
-def is_in_pr_changeset(project_changeset_filters: list[str]) -> bool:
-    changeset = get_pr_changeset()
+def is_in_pr_changeset(
+    project_changeset_filters: list[str], changeset: list[str]
+) -> bool:
     return filter_files_by_glob(changeset, project_changeset_filters)
 
 
-def is_group_enabled(group: Group, changeset_filters: list[str]) -> bool:
+def is_group_enabled(
+    group: Group, changeset_filters: list[str], changeset: list[str]
+) -> bool:
     if not is_pr():
         return True
 
-    if is_pr() and is_in_pr_changeset(changeset_filters) and \
-            group.category.startswith("mandatory"):
+    if (
+        is_pr()
+        and is_in_pr_changeset(changeset_filters, changeset)
+        and group.category.startswith("mandatory")
+    ):
         return True
 
     return group_comment(group)
@@ -196,16 +207,19 @@ def fetch_stage(name: str, stage, project: str, category: str) -> Step:
     if ("provider" not in stage) or stage["provider"] == "gcp":
         agent = GCPAgent(image=stage["platform"])
     elif stage["provider"] == "aws":
-        agent = AWSAgent(image=stage["platform"])
+        agent = AWSAgent(
+            image=stage["platform"],
+        )
     elif stage["provider"] == "orka":
         agent = OrkaAgent(image=stage["platform"])
 
     return Step(
-            category=category,
-            command=stage["command"],
-            name=name,
-            agent=agent,
-            project=project)
+        category=category,
+        command=stage["command"],
+        name=name,
+        agent=agent,
+        project=project,
+    )
 
 
 def fetch_group(stages, project: str, category: str) -> Group:
@@ -214,50 +228,51 @@ def fetch_group(stages, project: str, category: str) -> Group:
     steps = []
 
     for stage in stages:
-        steps.append(fetch_stage(
-                category=category,
-                name=stage,
-                project=project,
-                stage=stages[stage]))
+        steps.append(
+            fetch_stage(
+                category=category, name=stage, project=project, stage=stages[stage]
+            )
+        )
 
-    return Group(
-                project=project,
-                category=category,
-                steps=steps)
+    return Group(project=project, category=category, steps=steps)
 
 
 def fetch_pr_pipeline() -> list[Group]:
-    groups = []
-    extended_groups = []
-    with open(".buildkite/buildkite.yml", "r", encoding="utf8") as file:
-        doc = yaml.load(file, yaml.FullLoader)
+    groups: list[Group] = []
 
+    git_helper = GitHelper()
+    changeset = git_helper.get_pr_changeset()
+    yaml = YAML(typ="safe")
+    with open(".buildkite/buildkite.yml", "r", encoding="utf8") as file:
+        doc = yaml.load(file)
         for project in doc["projects"]:
             project_file = os.path.join(project, "buildkite.yml")
             if not os.path.isfile(project_file):
                 continue
-            with open(project_file, "r", encoding="utf8") as file:
-                project_obj = yaml.load(file, yaml.FullLoader)
+            with open(project_file, "r", encoding="utf8") as project_fp:
+                project_obj = yaml.load(project_fp)
 
-                group = fetch_group(stages=project_obj["stages"]["mandatory"],
-                                    project=project,
-                                    category="mandatory")
+                group = fetch_group(
+                    stages=project_obj["stages"]["mandatory"],
+                    project=project,
+                    category="mandatory",
+                )
 
-                if is_group_enabled(group, project_obj["when"]["changeset"]):
+                if is_group_enabled(group, project_obj["when"]["changeset"], changeset):
                     groups.append(group)
 
-                group = fetch_group(stages=project_obj["stages"]["extended"],
-                                    project=project,
-                                    category="extended")
+                group = fetch_group(
+                    stages=project_obj["stages"]["extended"],
+                    project=project,
+                    category="extended",
+                )
 
-                if is_group_enabled(group, project_obj["when"]["changeset"]):
-                    extended_groups.append(group)
+                if is_group_enabled(group, project_obj["when"]["changeset"], changeset):
+                    groups.append(group)
 
     # TODO: improve this merging lists
     all_groups = []
-    for group in sorted(groups):
-        all_groups.append(group)
-    for group in sorted(extended_groups):
+    for group in groups:
         all_groups.append(group)
 
     return all_groups
@@ -281,25 +296,30 @@ class PRComment:
 # Returns a group of steps corresponding to the comment
 def fetch_pr_comment_group_pipeline(comment: PRComment) -> list[Group]:
     groups = []
+    yaml = YAML(typ="safe")
     with open(".buildkite/buildkite.yml", "r", encoding="utf8") as file:
-        doc = yaml.load(file, yaml.FullLoader)
+        doc = yaml.load(file)
         if comment.project in doc["projects"]:
             project_file = os.path.join(comment.project, "buildkite.yml")
 
             if not os.path.isfile(project_file):
-                raise FileNotFoundError("buildkite.yml not found in: " +
-                                        "{}".format(comment.project))
-            with open(project_file, "r", encoding="utf8") as file:
-                project_obj = yaml.load(file, yaml.FullLoader)
+                raise FileNotFoundError(
+                    "buildkite.yml not found in: " + "{}".format(comment.project)
+                )
+            with open(project_file, "r", encoding="utf8") as project_fp:
+                project_obj = yaml.load(project_fp)
 
                 if not project_obj["stages"][comment.group]:
-                    raise ValueError("Group not found in {} buildkike.yml: {}"
-                                     .format(comment.project, comment.group))
+                    raise ValueError(
+                        "Group not found in {} buildkike.yml: {}".format(
+                            comment.project, comment.group
+                        )
+                    )
 
                 group = fetch_group(
                     stages=project_obj["stages"][comment.group],
                     project=comment.project,
-                    category="mandatory"
+                    category="mandatory",
                 )
                 groups.append(group)
 
@@ -309,35 +329,42 @@ def fetch_pr_comment_group_pipeline(comment: PRComment) -> list[Group]:
 # A comment like "/test filebeat extended unitTest-macos"
 def fetch_pr_comment_step_pipeline(comment: PRComment) -> list[Group]:
     groups = []
+    yaml = YAML(typ="safe")
     with open(".buildkite/buildkite.yml", "r", encoding="utf8") as file:
-        doc = yaml.load(file, yaml.FullLoader)
+        doc = yaml.load(file)
         if comment.project in doc["projects"]:
             project_file = os.path.join(comment.project, "buildkite.yml")
 
             if not os.path.isfile(project_file):
-                raise FileNotFoundError("buildkite.yml not found in: " +
-                                        "{}".format(comment.project))
-            with open(project_file, "r", encoding="utf8") as file:
-                project_obj = yaml.load(file, yaml.FullLoader)
+                raise FileNotFoundError(
+                    "buildkite.yml not found in: " + "{}".format(comment.project)
+                )
+            with open(project_file, "r", encoding="utf8") as project_fp:
+                project_obj = yaml.load(project_fp)
 
                 if not project_obj["stages"][comment.group]:
-                    raise ValueError("Group not found in {} buildkike.yml: {}"
-                                     .format(comment.project, comment.group))
+                    raise ValueError(
+                        "Group not found in {} buildkike.yml: {}".format(
+                            comment.project, comment.group
+                        )
+                    )
 
                 group = fetch_group(
                     stages=project_obj["stages"][comment.group],
                     project=comment.project,
-                    category="mandatory"
+                    category="mandatory",
                 )
 
-                filtered_steps = list(filter(
-                    lambda step: step.name == comment.step,
-                    group.steps
-                ))
+                filtered_steps = list(
+                    filter(lambda step: step.name == comment.step, group.steps)
+                )
 
                 if not filtered_steps:
-                    raise ValueError("Step {} not found in {} buildkike.yml"
-                                     .format(comment.step, comment.project))
+                    raise ValueError(
+                        "Step {} not found in {} buildkike.yml".format(
+                            comment.step, comment.project
+                        )
+                    )
                 group.steps = filtered_steps
                 groups.append(group)
 
@@ -373,17 +400,24 @@ def pr_comment_pipeline(pr_comment: PRComment) -> list[Group]:
 # TODO: validate unique stages!
 def main() -> None:
     all_groups = []
-    if is_pr() and not os.getenv('GITHUB_PR_TRIGGER_COMMENT'):
+    if is_pr() and not os.getenv("GITHUB_PR_TRIGGER_COMMENT"):
         all_groups = fetch_pr_pipeline()
 
-    if is_pr() and os.getenv('GITHUB_PR_TRIGGER_COMMENT'):
-        print("GITHUB_PR_TRIGGER_COMMENT: {}".format(
-            os.getenv('GITHUB_PR_TRIGGER_COMMENT')))
-        comment = PRComment(os.getenv('GITHUB_PR_TRIGGER_COMMENT'))
+    if is_pr() and os.getenv("GITHUB_PR_TRIGGER_COMMENT"):
+        print(
+            "GITHUB_PR_TRIGGER_COMMENT: {}".format(
+                os.getenv("GITHUB_PR_TRIGGER_COMMENT")
+            )
+        )
+        comment = PRComment(os.getenv("GITHUB_PR_TRIGGER_COMMENT"))
         all_groups = pr_comment_pipeline(comment)
 
     # Produce now the pipeline
-    print(yaml.dump(Pipeline(all_groups).create_entity()))
+    print(
+        "# yaml-language-server: $schema=https://raw.githubusercontent.com/buildkite/pipeline-schema/main/schema.json"
+    )
+    yaml = YAML(typ="safe")
+    yaml.dump(BuildkitePipeline(all_groups).create_entity(), sys.stdout)
 
 
 if __name__ == "__main__":
--- .buildkite/scripts/generate_pipeline.sh
+++ .buildkite/scripts/generate_pipeline.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "--- Install dependencies"
-pip3 install --quiet PyYAML
+echo "~~~ Install dependencies"
+pip3 install --quiet "ruamel.yaml<0.18.0"
 
-echo "--- Run pipeline generator in dry-run mode"
+echo "~~~ Run pipeline generator in dry-run mode"
 python3 .buildkite/pipeline.py | yq .
 
 # Run the python generator - likely this should be called in the
 # catalog-info.yaml
-# echo "--- Upload pipeline"
+echo "~~~ Upload pipeline"
 ## Remove when is ready
-# python3 .buildkite/pipeline.py | buildkite-agent pipeline upload
+python3 .buildkite/pipeline.py | buildkite-agent pipeline upload
