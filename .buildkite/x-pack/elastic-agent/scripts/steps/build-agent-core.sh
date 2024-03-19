4a7dfbf22a1b7cd591f9019d411f2d14b37bdc34
Pavel Zorin <pavel.zorin@elastic.co>
2024-03-20 00:31:11 +0100

    Elastic agent core publishing

--- .buildkite/x-pack/elastic-agent/scripts/steps/build-agent-core.sh
+++ .buildkite/x-pack/elastic-agent/scripts/steps/build-agent-core.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 echo "+++ Build Agent artifacts"
 
 SNAPSHOT=""
+
+BEAT_VERSION=$(grep -oE '[0-9]+\.[0-9]+\.[0-9]+(\-[a-zA-Z]+[0-9]+)?' "libbeat/version/version.go")
 BEAT_VERSION_FULL=$BEAT_VERSION
 if [ "$DRA_WORKFLOW" == "snapshot" ]; then
     SNAPSHOT="true"
