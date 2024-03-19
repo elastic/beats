996d322b2439670db554709c046c14eb0b92055c
Alexandros Sapranidis <alexandros@elastic.co>
2024-03-19 17:52:22 +0200

    Remove comments
    Signed-off-by: Alexandros Sapranidis <alexandros@elastic.co>

--- .buildkite/scripts/generate_pipeline.sh
+++ .buildkite/scripts/generate_pipeline.sh
@@ -7,8 +7,5 @@ pip3 install --quiet "ruamel.yaml<0.18.0"
 echo "~~~ Run pipeline generator in dry-run mode"
 python3 .buildkite/pipeline.py | yq .
 
-# Run the python generator - likely this should be called in the
-# catalog-info.yaml
 echo "~~~ Upload pipeline"
-## Remove when is ready
 python3 .buildkite/pipeline.py | buildkite-agent pipeline upload
