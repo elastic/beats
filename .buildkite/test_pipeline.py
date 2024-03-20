357e7326200949a0c06e784518692a9e9d786c0e
Alexandros Sapranidis <alexandros@elastic.co>
2024-03-20 21:02:30 +0200

    Added some more tests
    Signed-off-by: Alexandros Sapranidis <alexandros@elastic.co>

--- .buildkite/test_pipeline.py
+++ .buildkite/test_pipeline.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 import pipeline
 
@@ -5,16 +7,33 @@ import pipeline
 @pytest.fixture
 def ubuntu2204_aws_agent():
     return {
+        "command": "fake-cmd",
+        "platform": "platform-ingest-beats-ubuntu-2204-aarch64",
+        "provider": "aws"
+    }
+
+
+@pytest.fixture()
+def fake_simple_group():
+    return {
+        "unitTest": {
             "command": "fake-cmd",
-            "platform": "platform-ingest-beats-ubuntu-2204-aarch64",
-            "provider": "aws"
+            "platform": "family/platform-ingest-beats-ubuntu-2204",
+        },
+        "integrationTest": {
+            "command": "fake-integration",
+            "platform": "family/platform-ingest-beats-ubuntu-2204",
+            "env": {
+                "FOO": "BAR",
+            },
+        },
     }
 
 
 def test_fetch_stage(ubuntu2204_aws_agent):
-    step = pipeline.fetch_stage("test", ubuntu2204_aws_agent, "fake", "fake")
+    step = pipeline.fetch_stage("test", ubuntu2204_aws_agent, "fake", "fake-category")
     assert step.create_entity() == {
-        "label": "fake fake",
+        "label": "fake test",
         "command": ["cd fake", "fake-cmd"],
         "notify": [
             {
@@ -34,3 +53,19 @@ def test_fetch_stage(ubuntu2204_aws_agent):
         ],
     }
 
+
+def test_fetch_group(fake_simple_group):
+    group = pipeline.fetch_group(fake_simple_group, "fake-project", "testing")
+    assert len(group.steps) == 2
+    for step in group.steps:
+        assert "testing" == step.category
+        assert "gcp" == step.agent.provider
+
+    assert group.steps[1].env.get("FOO") == "BAR"
+
+
+def test_is_pr():
+    os.environ["BUILDKITE_PULL_REQUEST___"] = ""
+    assert pipeline.is_pr() is False
+    os.environ["BUILDKITE_PULL_REQUEST"] = "true"
+    assert pipeline.is_pr() is True
