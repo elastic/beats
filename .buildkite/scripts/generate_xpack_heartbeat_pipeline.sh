b9e864deec3636129eb0089124d73353e76ebab1
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2024-03-21 15:39:17 +0200

    relove conflicts

--- .buildkite/scripts/generate_xpack_heartbeat_pipeline.sh
+++ .buildkite/scripts/generate_xpack_heartbeat_pipeline.sh
@@ -33,44 +33,48 @@ steps:
           machineType: "${GCP_HI_PERF_MACHINE_TYPE}"
         artifact_paths: "${BEATS_PROJECT_NAME}/build/*.xml"
 
-      - label: ":windows: Windows Unit Tests - {{matrix.image}}"
-        command: ".buildkite/scripts/win_unit_tests.ps1"
-        key: "mandatory-win-unit-tests"
-        agents:
-          provider: "gcp"
-          image: "{{matrix.image}}"
-          machineType: "${GCP_WIN_MACHINE_TYPE}"
-          disk_size: 100
-          disk_type: "pd-ssd"
-        matrix:
-          setup:
-            image:
-              - "${IMAGE_WIN_2016}"
-              - "${IMAGE_WIN_2022}"
-        artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
+# TODO: there are windows test failures already reported
+# https://github.com/elastic/beats/issues/23957 and https://github.com/elastic/beats/issues/23958
+# waiting for being fixed.
 
-## TODO: this condition will be changed in the Phase 3 of the Migration Plan https://docs.google.com/document/d/1IPNprVtcnHlem-uyGZM0zGzhfUuFAh4LeSl9JFHMSZQ/edit#heading=h.sltz78yy249h
+#       - label: ":windows: Windows Unit Tests - {{matrix.image}}"
+#         command: ".buildkite/scripts/win_unit_tests.ps1"
+#         key: "mandatory-win-unit-tests"
+#         agents:
+#           provider: "gcp"
+#           image: "{{matrix.image}}"
+#           machineType: "${GCP_WIN_MACHINE_TYPE}"
+#           disk_size: 100
+#           disk_type: "pd-ssd"
+#         matrix:
+#           setup:
+#             image:
+#               - "${IMAGE_WIN_2016}"
+#               - "${IMAGE_WIN_2022}"
+#         artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
 
-  - group: "Extended Windows Tests"
-    key: "extended-win-tests"
-    steps:
+# ## TODO: this condition will be changed in the Phase 3 of the Migration Plan https://docs.google.com/document/d/1IPNprVtcnHlem-uyGZM0zGzhfUuFAh4LeSl9JFHMSZQ/edit#heading=h.sltz78yy249h
 
-      - label: ":windows: Windows Unit Tests - {{matrix.image}}"
-        command: ".buildkite/scripts/win_unit_tests.ps1"
-        key: "extended-win-unit-tests"
-        agents:
-          provider: "gcp"
-          image: "{{matrix.image}}"
-          machineType: "${GCP_WIN_MACHINE_TYPE}"
-          disk_size: 100
-          disk_type: "pd-ssd"
-        matrix:
-          setup:
-            image:
-              - "${IMAGE_WIN_10}"
-              - "${IMAGE_WIN_11}"
-              - "${IMAGE_WIN_2019}"
-        artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
+#   - group: "Extended Windows Tests"
+#     key: "extended-win-tests"
+#     steps:
+
+#       - label: ":windows: Windows Unit Tests - {{matrix.image}}"
+#         command: ".buildkite/scripts/win_unit_tests.ps1"
+#         key: "extended-win-unit-tests"
+#         agents:
+#           provider: "gcp"
+#           image: "{{matrix.image}}"
+#           machineType: "${GCP_WIN_MACHINE_TYPE}"
+#           disk_size: 100
+#           disk_type: "pd-ssd"
+#         matrix:
+#           setup:
+#             image:
+#               - "${IMAGE_WIN_10}"
+#               - "${IMAGE_WIN_11}"
+#               - "${IMAGE_WIN_2019}"
+#         artifact_paths: "${BEATS_PROJECT_NAME}/build/*.*"
 
 YAML
 else
