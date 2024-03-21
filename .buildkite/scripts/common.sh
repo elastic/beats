4b9a428e4d82f8dd0307241ebf0e25c53d869e3c
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2024-03-21 21:29:43 +0200

    debug withNodeSJ

--- .buildkite/scripts/common.sh
+++ .buildkite/scripts/common.sh
@@ -530,13 +530,12 @@ startCloudTestEnv() {
 }
 
 withNodeJSEnv() {
-  local version="$1"
+  local version="v$1"
   echo "Installing the NodeJs version $version"
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-  nvm install "$version"
-  nvm use "$version"
+  source ~/.bashrc
+  nvm install $version
+  nvm use $version
   node --version
   nvm version | head -n1 > "${NVM_DIR}/.nvm-node-version"
 }
