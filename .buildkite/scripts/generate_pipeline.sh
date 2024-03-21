aa188db8d1f8a72e08f85bd040a541e7b6721e60
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2024-03-21 17:30:53 +0200

    Add tests for pipeline generator

--- .buildkite/scripts/generate_pipeline.sh
+++ .buildkite/scripts/generate_pipeline.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 echo "~~~ Install dependencies"
 pip3 install --quiet "ruamel.yaml<0.18.0"
 
+.buildkite/scripts/run_dynamic_pipeline_tests.sh
+
 echo "+++ Run pipeline generator in dry-run mode"
 python3 .buildkite/pipeline.py | yq .
 
