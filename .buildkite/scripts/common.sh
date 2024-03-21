ea1b8ff85edd3b5102af5e8c44c90fe3a5229666
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2024-03-21 22:58:31 +0200

    add dependencies

--- .buildkite/scripts/common.sh
+++ .buildkite/scripts/common.sh
@@ -542,8 +542,23 @@ withNodeJSEnv() {
   export PATH="${nvmPath}:${PATH}"
   nvm use "$version"
   node --version
+}
+
+installNodeJsDependencies() {
   # install dependencies to run browsers
-  sudo npx playwright install-deps
+  sudo apt-get install libatk1.0-0\
+    libatk-bridge2.0-0\
+    libcups2\
+    libxkbcommon0\
+    libatspi2.0-0\
+    libxcomposite1\
+    libxdamage1\
+    libxfixes3\
+    libxrandr2\
+    libgbm1\
+    libpango-1.0-0\
+    libcairo2\
+    libasound2
 }
 
 teardown() {
--- .buildkite/scripts/install_tools.sh
+++ .buildkite/scripts/install_tools.sh
@@ -45,6 +45,7 @@ config_git
 if [[ "$BUILDKITE_PIPELINE_SLUG" == "beats-xpack-heartbeat" ]]; then
   # Install NodeJS
   withNodeJSEnv "${NODEJS_VERSION}"
+  installNodeJsDependencies
   export ELASTIC_SYNTHETICS_CAPABLE=true
   echo "Install @elastic/synthetics"
   npm i -g @elastic/synthetics
