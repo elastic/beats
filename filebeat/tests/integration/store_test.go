36fa4e7f5bf2bf479eb59023f6113bb2ea0c112a
Tiago Queiroz <tiago.queiroz@elastic.co>
2024-03-22 12:12:36 +0100

    Add tests for store GC

--- filebeat/tests/integration/store_test.go
+++ filebeat/tests/integration/store_test.go
@@ -1,3 +1,5 @@
+//go:build integration
+
 // Licensed to Elasticsearch B.V. under one or more contributor
 // license agreements. See the NOTICE file distributed with
 // this work for additional information regarding copyright
@@ -14,8 +16,6 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-
-//go:build integration
 
 package integration
 
