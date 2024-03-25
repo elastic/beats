73b03ac035b7d813a35e5fec67beaf762ba526c1
Pavel Zorin <pavel.zorin@elastic.co>
2024-03-25 10:12:51 +0100

    cleanup

--- x-pack/elastic-agent/magefile.go
+++ x-pack/elastic-agent/magefile.go
@@ -315,7 +315,6 @@ func Package() {
 		packages string
 	}{
 		{"darwin/amd64", "darwin-x86_64.tar.gz"},
-		{"darwin/arm64", "darwin-aarch64.tar.gz"},
 		{"linux/amd64", "linux-x86_64.tar.gz"},
 		{"linux/arm64", "linux-arm64.tar.gz"},
 		{"windows/amd64", "windows-x86_64.zip"},
