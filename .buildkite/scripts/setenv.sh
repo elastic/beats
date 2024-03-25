65558fdf8b19cf64763f44bda02f15ecb91cd232
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2024-03-25 11:19:54 +0200

    export variables

--- .buildkite/scripts/setenv.sh
+++ .buildkite/scripts/setenv.sh
@@ -1,52 +1,38 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-REPO="beats"
-TMP_FOLDER="tmp.${REPO}"
-DOCKER_REGISTRY="docker.elastic.co"
-SETUP_GVM_VERSION="v0.5.1"
-DOCKER_COMPOSE_VERSION="1.21.0"
-DOCKER_COMPOSE_VERSION_AARCH64="v2.21.0"
-SETUP_WIN_PYTHON_VERSION="3.11.0"
-NMAP_WIN_VERSION="7.12"           # Earlier versions of NMap provide WinPcap (the winpcap packages don't install nicely because they pop-up a UI)
-GO_VERSION=$(cat .go-version)
-ASDF_MAGE_VERSION="1.15.0"
-PACKAGING_PLATFORMS="+all linux/amd64 linux/arm64 windows/amd64 darwin/amd64 darwin/arm64"
-PACKAGING_ARM_PLATFORMS="linux/arm64"
-ASDF_TERRAFORM_VERSION="1.0.2"
-AWS_REGION="eu-central-1"
-NODEJS_VERSION="18.17.1"
-IMAGE_UBUNTU_X86_64="family/platform-ingest-beats-ubuntu-2204"
-IMAGE_UBUNTU_ARM_64="platform-ingest-beats-ubuntu-2204-aarch64"
-DEFAULT_UBUNTU_X86_64_IMAGE="family/core-ubuntu-2204"
-IMAGE_RHEL9_X86_64="family/platform-ingest-beats-rhel-9"
-IMAGE_WIN_10="family/general-windows-10"
-IMAGE_WIN_11="family/general-windows-11"
-IMAGE_WIN_2016="family/core-windows-2016"
-IMAGE_WIN_2019="family/core-windows-2019"
-IMAGE_WIN_2022="family/core-windows-2022"
-IMAGE_MACOS_X86_64="generic-13-ventura-x64"
-GCP_DEFAULT_MACHINE_TYPE="c2d-highcpu-8"
-GCP_HI_PERF_MACHINE_TYPE="c2d-highcpu-16"
-GCP_WIN_MACHINE_TYPE="n2-standard-8"
-AWS_ARM_INSTANCE_TYPE="t4g.xlarge"
-BEATS_PROJECT_NAME="x-pack/packetbeat"
+export REPO="beats"
+export TMP_FOLDER="tmp.${REPO}"
+export DOCKER_REGISTRY="docker.elastic.co"
+export SETUP_GVM_VERSION="v0.5.1"
+export DOCKER_COMPOSE_VERSION="1.21.0"
+export DOCKER_COMPOSE_VERSION_AARCH64="v2.21.0"
+export SETUP_WIN_PYTHON_VERSION="3.11.0"
+export NMAP_WIN_VERSION="7.12"           # Earlier versions of NMap provide WinPcap (the winpcap packages don't install nicely because they pop-up a UI)
+export ASDF_MAGE_VERSION="1.15.0"
+export PACKAGING_PLATFORMS="+all linux/amd64 linux/arm64 windows/amd64 darwin/amd64 darwin/arm64"
+export PACKAGING_ARM_PLATFORMS="linux/arm64"
+export ASDF_TERRAFORM_VERSION="1.0.2"
+export AWS_REGION="eu-central-1"
+export NODEJS_VERSION="18.17.1"
+export IMAGE_UBUNTU_X86_64="family/platform-ingest-beats-ubuntu-2204"
+export IMAGE_UBUNTU_ARM_64="platform-ingest-beats-ubuntu-2204-aarch64"
+export DEFAULT_UBUNTU_X86_64_IMAGE="family/core-ubuntu-2204"
+export IMAGE_RHEL9_X86_64="family/platform-ingest-beats-rhel-9"
+export IMAGE_WIN_10="family/general-windows-10"
+export IMAGE_WIN_11="family/general-windows-11"
+export IMAGE_WIN_2016="family/core-windows-2016"
+export IMAGE_WIN_2019="family/core-windows-2019"
+export IMAGE_WIN_2022="family/core-windows-2022"
+export IMAGE_MACOS_X86_64="generic-13-ventura-x64"
+export GCP_DEFAULT_MACHINE_TYPE="c2d-highcpu-8"
+export GCP_HI_PERF_MACHINE_TYPE="c2d-highcpu-16"
+export GCP_WIN_MACHINE_TYPE="n2-standard-8"
+export AWS_ARM_INSTANCE_TYPE="t4g.xlarge"
 
-export SETUP_GVM_VERSION
-export DOCKER_COMPOSE_VERSION
-export DOCKER_COMPOSE_VERSION_AARCH64
-export SETUP_WIN_PYTHON_VERSION
-export NMAP_WIN_VERSION
+GO_VERSION=$(cat .go-version)
 export GO_VERSION
-export ASDF_MAGE_VERSION
-export PACKAGING_PLATFORMS
-export PACKAGING_ARM_PLATFORMS
-export REPO
-export TMP_FOLDER
-export DOCKER_REGISTRY
-export ASDF_TERRAFORM_VERSION
-export AWS_REGION
-export NODEJS_VERSION
+
 
 exportVars() {
   local platform_type="$(uname)"
