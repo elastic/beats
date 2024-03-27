b0ab6ce7ec82c45278a2aa9a8b7cc3ebd953da49
harnish-elastic <harnish.chavda@elastic.co>
2024-03-27 15:56:12 +0530

    change in python version to fix CI issue

--- .ci/scripts/install-tools.bat
+++ .ci/scripts/install-tools.bat
@@ -15,8 +15,8 @@ REM Set the USERPROFILE to the previous location to fix issues with chocolatey i
 SET PREVIOUS_USERPROFILE=%USERPROFILE%
 SET USERPROFILE=%OLD_USERPROFILE%
 IF NOT EXIST C:\Python38\python.exe (
-    REM Install python 3.8
-    choco install python -y -r --no-progress --version 3.8.5 || exit /b 1
+    REM Install python 3.11.3
+    choco install python -y -r --no-progress --version 3.11.3 || exit /b 1
 )
 python --version
 where python
