08026216eb76a4a35831b1c35d76c7d8efe6ef10
Andreas Gkizas <andreas.gkizas@elastic.co>
2024-03-28 14:55:39 +0200

    Adding default value to config period of remote_write

--- x-pack/metricbeat/module/prometheus/remote_write/data.go
+++ x-pack/metricbeat/module/prometheus/remote_write/data.go
@@ -45,14 +45,15 @@ func remoteWriteEventsGeneratorFactory(base mb.BaseMetricSet) (remote_write.Remo
 
 	if config.UseTypes {
 		// By default prometheus in remote_write pushes data with the interval of 60s. In order to calculate counter rate we are setting Period to 60secs accordingly
-		if time.Duration(config.Period)*time.Second < 60*time.Second {
-			config.Period = time.Second * 60
+		duration, err := time.ParseDuration(config.Period.String())
+		if duration < 60*time.Second || err != nil {
+			duration = time.Second * 60
 		}
-		logp.Debug("Period for counter cache for remote_write", config.Period.String())
+		logp.Debug("Period for counter cache for remote_write", duration.String())
 
 		// use a counter cache with a timeout of 5x the period, as a safe value
 		// to make sure that all counters are available between fetches
-		counters := collector.NewCounterCache(config.Period * 5)
+		counters := collector.NewCounterCache(duration * 5)
 
 		g := remoteWriteTypedGenerator{
 			counterCache: counters,
