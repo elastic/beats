84286b1d4e066c47a6eae3db6851dade76534f61
Alexandros Sapranidis <alexandros@elastic.co>
2024-03-29 17:30:07 +0200

    Remove scripts dependencies from cloud_tests step
    Signed-off-by: Alexandros Sapranidis <alexandros@elastic.co>

--- .buildkite/scripts/cloud_tests.sh
+++ .buildkite/scripts/cloud_tests.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 # What Terraform Module will run
 if [[ "$BUILDKITE_PIPELINE_SLUG" == "beats-xpack-metricbeat" ]]; then
@@ -7,19 +8,40 @@ elif [[ "$BUILDKITE_PIPELINE_SLUG" == "beats-xpack-filebeat" ]]; then
   export MODULE_DIR="x-pack/filebeat/input/awss3/_meta/terraform"
 fi
 
-source .buildkite/scripts/install_tools.sh
+teardown() {
+  # Teardown resources after using them
+  echo "~~~ Terraform Cleanup"
+  .ci/scripts/terraform-cleanup.sh "${MODULE_DIR}"              #TODO: move all docker-compose files from the .ci to .buildkite folder before switching to BK
 
-set -euo pipefail
+  echo "~~~ Docker Compose Cleanup"
+  docker-compose -f .ci/jobs/docker-compose.yml down -v         #TODO: move all docker-compose files from the .ci to .buildkite folder before switching to BK
+}
 
 trap 'teardown || true; unset_secrets' EXIT
 
 # Prepare the cloud resources using Terraform
-startCloudTestEnv "${MODULE_DIR}"
+#startCloudTestEnv "${MODULE_DIR}"
+echo "~~~ Loading creds"
+set +o xtrace
+export AWS_ACCESS_KEY_ID=$BEATS_AWS_ACCESS_KEY
+export AWS_SECRET_ACCESS_KEY=$BEATS_AWS_SECRET_KEY
+export TEST_TAGS="${TEST_TAGS:+$TEST_TAGS,}aws"
+set -o xtrace
+
+
+echo "~~~ Run docker-compose services for emulated cloud env"
+docker-compose -f .ci/jobs/docker-compose.yml up -d        #TODO: move all docker-compose files from the .ci to .buildkite folder before switching to BK
+echo "~~~ Initialize TF cloud resources"
+pushd "$dir"
+export TF_VAR_BRANCH=$(echo "${BUILDKITE_BRANCH}" | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9-]/-/g')
+export TF_VAR_BUILD_ID="${BUILDKITE_BUILD_ID}"
+export TF_VAR_CREATED_DATE=$(date +%s)
+export TF_VAR_ENVIRONMENT="ci"
+export TF_VAR_REPO="${REPO}"
+terraform init && terraform apply -auto-approve
+popd
 
 # Run tests
-echo "--- Run Cloud Tests for $BEATS_PROJECT_NAME"
-pushd "${BEATS_PROJECT_NAME}" > /dev/null
-
+echo "~~~ Run Cloud Tests for $BEATS_PROJECT_NAME"
+pushd "${BEATS_PROJECT_NAME}"
 mage build test
-
-popd > /dev/null
