b34fba759178a3bb4dc1193035a93b005c67728c
Siarhei Harbuz <87968844+sharbuz@users.noreply.github.com>
2024-04-05 14:18:00 +0300

    Update setenv.sh

--- .buildkite/scripts/setenv.sh
+++ .buildkite/scripts/setenv.sh
@@ -1,23 +1,6 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-<<<<<<< HEAD
-REPO="beats"
-TMP_FOLDER="tmp.${REPO}"
-DOCKER_REGISTRY="docker.elastic.co"
-SETUP_GVM_VERSION="v0.5.1"
-DOCKER_COMPOSE_VERSION="1.21.0"
-DOCKER_COMPOSE_VERSION_AARCH64="v2.21.0"
-SETUP_WIN_PYTHON_VERSION="3.11.0"
-NMAP_WIN_VERSION="7.12"           # Earlier versions of NMap provide WinPcap (the winpcap packages don't install nicely because they pop-up a UI)
-GO_VERSION=$(cat .go-version)
-ASDF_MAGE_VERSION="1.15.0"
-ASDF_TERRAFORM_VERSION="1.0.2"
-PACKAGING_PLATFORMS="+all linux/amd64 linux/arm64 windows/amd64 darwin/amd64"
-PACKAGING_ARM_PLATFORMS="linux/arm64"
-AWS_REGION="eu-central-1"
-NODEJS_VERSION="18.17.1"
-=======
 
 export REPO="beats"
 export DOCKER_REGISTRY="docker.elastic.co"
@@ -26,13 +9,10 @@ export DOCKER_COMPOSE_VERSION="1.21.0"
 export DOCKER_COMPOSE_VERSION_AARCH64="v2.21.0"
 export ASDF_NODEJS_VERSION="18.17.1"
 export AWS_REGION="eu-central-1"
->>>>>>> 80dab50f0c (replace default images (#38583))
-
 WORKSPACE=${WORKSPACE:-"$(pwd)"}
 export WORKSPACE
 GO_VERSION=$(cat .go-version)
 export GO_VERSION
-
 
 exportVars() {
   local platform_type="$(uname)"
