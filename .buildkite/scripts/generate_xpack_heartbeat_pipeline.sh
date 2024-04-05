fe0c210ab8bc322360b76d2fc7e327c5573d52e4
Alexandros Sapranidis <alexandros@elastic.co>
2024-04-05 15:30:04 +0300

    Fix x-pack heartbeat pipeline yaml
    Signed-off-by: Alexandros Sapranidis <alexandros@elastic.co>

--- .buildkite/scripts/generate_xpack_heartbeat_pipeline.sh
+++ .buildkite/scripts/generate_xpack_heartbeat_pipeline.sh
@@ -9,9 +9,7 @@ pipelineName="pipeline.xpack-heartbeat-dynamic.yml"
 echo "Add the mandatory and extended tests without additional conditions into the pipeline"
 if are_conditions_met_mandatory_tests; then
   cat > $pipelineName <<- YAML
-
 steps:
-
   - group: "Mandatory Tests"
     key: "mandatory-tests"
     steps:
@@ -22,6 +20,7 @@ steps:
           provider: "gcp"
           image: "${IMAGE_UBUNTU_X86_64}"
           machineType: "${GCP_DEFAULT_MACHINE_TYPE}"
+        artifact_paths:
           - "$BEATS_PROJECT_NAME/build/*.xml"
           - "$BEATS_PROJECT_NAME/build/*.json"
         notify:
@@ -42,53 +41,51 @@ steps:
           - github_commit_status:
               context: "$BEATS_PROJECT_NAME: Go Integration Tests"
 
-
       - label: ":windows: Windows Unit Tests - {{matrix.image}}"
         skip: "see elastic/beats#23957 and elastic/beats#23958"
-       command: |
+        command: |
          Set-Location -Path $BEATS_PROJECT_NAME
          mage build unitTest
-       key: "mandatory-win-unit-tests"
-       agents:
+        key: "mandatory-win-unit-tests"
+        agents:
          provider: "gcp"
          image: "{{matrix.image}}"
          machineType: "${GCP_WIN_MACHINE_TYPE}"
          disk_size: 100
          disk_type: "pd-ssd"
-       matrix:
+        matrix:
          setup:
            image:
              - "${IMAGE_WIN_2016}"
              - "${IMAGE_WIN_2022}"
-       artifact_paths:
+        artifact_paths:
          - "$BEATS_PROJECT_NAME/build/*.xml"
          - "$BEATS_PROJECT_NAME/build/*.json"
 
-# ## TODO: this condition will be changed in the Phase 3 of the Migration Plan https://docs.google.com/document/d/1IPNprVtcnHlem-uyGZM0zGzhfUuFAh4LeSl9JFHMSZQ/edit#heading=h.sltz78yy249h
-   - group: "Extended Windows Tests"
-     key: "extended-win-tests"
-     steps:
-      - label: ":windows: Windows Unit Tests - {{matrix.image}}"
-        skip: "see elastic/beats#23957 and elastic/beats#23958"
-        command: |
-          Set-Location -Path $BEATS_PROJECT_NAME
-          mage build unitTest
-        key: "extended-win-unit-tests"
-        agents:
-          provider: "gcp"
-          image: "{{matrix.image}}"
-          machineType: "${GCP_WIN_MACHINE_TYPE}"
-          disk_size: 100
-          disk_type: "pd-ssd"
-        matrix:
-          setup:
-            image:
-              - "${IMAGE_WIN_10}"
-              - "${IMAGE_WIN_11}"
-              - "${IMAGE_WIN_2019}"
-        artifact_paths:
-          - "$BEATS_PROJECT_NAME/build/*.xml"
-          - "$BEATS_PROJECT_NAME/build/*.json"
+  - group: "Extended Windows Tests" ## TODO: this condition will be changed in the Phase 3
+    key: "extended-win-tests"
+    steps:
+    - label: ":windows: Windows Unit Tests - {{matrix.image}}"
+      skip: "see elastic/beats#23957 and elastic/beats#23958"
+      command: |
+        Set-Location -Path $BEATS_PROJECT_NAME
+        mage build unitTest
+      key: "extended-win-unit-tests"
+      agents:
+        provider: "gcp"
+        image: "{{matrix.image}}"
+        machineType: "${GCP_WIN_MACHINE_TYPE}"
+        disk_size: 100
+        disk_type: "pd-ssd"
+      matrix:
+        setup:
+          image:
+            - "${IMAGE_WIN_10}"
+            - "${IMAGE_WIN_11}"
+            - "${IMAGE_WIN_2019}"
+      artifact_paths:
+        - "$BEATS_PROJECT_NAME/build/*.xml"
+        - "$BEATS_PROJECT_NAME/build/*.json"
 YAML
 else
   echo "The conditions don't match to requirements for generating pipeline steps."
@@ -97,11 +94,9 @@ fi
 
 if are_conditions_met_macos_tests; then
   cat >> $pipelineName <<- YAML
-
   - group: "Extended Tests"
     key: "extended-tests"
     steps:
-
       - label: ":mac: MacOS Unit Tests"
         key: "extended-macos-unit-tests"
         command: ".buildkite/scripts/unit_tests.sh"
@@ -121,7 +116,6 @@ fi
 echo "Check and add the Packaging into the pipeline"
 if are_conditions_met_packaging; then
   cat >> $pipelineName <<- YAML
-
   - wait: ~
     depends_on:
       - step: "mandatory-tests"
@@ -163,7 +157,7 @@ YAML
 fi
 
 echo "+++ Printing dynamic steps"
-cat $pipelineName | yq . -P
+yq . -P -e $pipelineName || (echo "Yaml formatting error, bellow is the YAML"; cat $pipelineName; exit 1)
 
 echo "--- Loading dynamic steps"
 buildkite-agent pipeline upload $pipelineName
