15f07e260263f12fd16b906b76ba385e86f26c38
Tiago Queiroz <tiago.queiroz@elastic.co>
2024-04-09 08:18:33 +0200

    Fix lint errors

--- filebeat/input/v2/input-cursor/clean.go
+++ filebeat/input/v2/input-cursor/clean.go
@@ -44,6 +44,7 @@ type cleaner struct {
 // once the last event has been ACKed.
 func (c *cleaner) run(canceler unison.Canceler, store *store, interval time.Duration) {
 	started := time.Now()
+	//nolint: errcheck // gcStore does not return an error
 	timed.Periodic(canceler, interval, func() error {
 		gcStore(c.log, started, store)
 		return nil
