446e631c034e87677be65beaab32e45cc3dcaff0
Alexandros Sapranidis <alexandros@elastic.co>
2024-04-10 15:51:02 +0300

    fixup! Add script for diffing files

--- .buildkite/scripts/diff-files.sh
+++ .buildkite/scripts/diff-files.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -ueo pipefail
-
+# This script is used to detect what diff command we are going to use to detect the changed files for triggering pipelines
 if [[ -n "${BUILDKITE_PULL_REQUEST:-}" ]]; then
   git diff --name-only ${BUILDKITE_PULL_REQUEST_BASE_BRANCH:-}...HEAD
 else
