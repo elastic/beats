ca00da7755f1032d3a89e4a54d83e97299c565df
Siarhei Harbuz <siarhei.harbuz@elastic.co>
2024-04-11 09:51:50 +0300

    cleanup

--- .buildkite/scripts/macos_unit_tests.sh
+++ .buildkite/scripts/macos_unit_tests.sh
@@ -4,6 +4,8 @@ source .buildkite/scripts/common.sh
 
 set -euo pipefail
 
+
+
 if [[ -z "${GO_VERSION-""}" ]]; then
   GO_VERSION=$(cat "${WORKSPACE}/.go-version")
   export GO_VERSION
@@ -19,7 +21,7 @@ with_macos_docker_compose
 
 if [[ "$BUILDKITE_PIPELINE_SLUG" == "beats-xpack-heartbeat" ]]; then
   # Install NodeJS
-  withNodeJSEnv "${NODEJS_VERSION}"
+  withNodeJSEnv "${ASDF_NODEJS_VERSION}"
   installNodeJsDependencies
 
   echo "Install @elastic/synthetics"
@@ -28,14 +30,11 @@ fi
 
 mage dumpVariables
 
-#sudo command doesn't work at the "pre-command" hook because of another user environment (root with strange permissions)
 sudo chmod -R go-w "${BEATS_PROJECT_NAME}/"     #TODO: Remove when the issue is solved https://github.com/elastic/beats/issues/37838
 
 pushd "${BEATS_PROJECT_NAME}" > /dev/null
 
-#TODO "umask 0022" has to be removed after our own image is ready (it has to be moved to the image)
 umask 0022    # fix the filesystem permissions issue like this: https://buildkite.com/elastic/beats-metricbeat/builds/1329#018d3179-25a9-475b-a2c8-64329dfe092b/320-1696
-
 echo "--- Run Unit Tests"
 mage build unitTest
 
