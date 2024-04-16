f28a1e385399a2ec2cc60266e39f627e3fae2e09
Pavel Zorin <pavel.zorin@elastic.co>
2024-04-16 15:16:08 +0200

    Staging

--- .buildkite/x-pack/agentbeat/scripts/dra.sh
+++ .buildkite/x-pack/agentbeat/scripts/dra.sh
@@ -2,9 +2,9 @@ set -uo pipefail
 
 DRY_RUN=""
 
-if [[ "${BUILDKITE_PULL_REQUEST:="false"}" != "false" || "${BUILDKITE_BRANCH}" == "ci_agentbeat_pack" ]]; then
+if [[ "${BUILDKITE_PULL_REQUEST:="false"}" != "false" ]]; then
     BRANCH=main
-    # DRY_RUN="--dry-run"
+    DRY_RUN="--dry-run"
     echo "+++ Running in PR and setting branch main and --dry-run"
 fi
 
