c9e07d2bcd1dc15f6f4adce9a07a98483ee600af
Olga Naidjonoka <olga_naidjonoka@epam.com>
2024-04-17 11:21:11 +0300

    added changeset ps1 script and updated xpack-winlogbeat pipeline

---
+++ .buildkite/scripts/changesets.psm1
@@ -0,0 +1,71 @@
+function ArePathsChanged($patterns) {
+    $changedlist = @()
+    foreach ($pattern in $patterns) {
+        $changedFiles = & git diff --name-only "HEAD@{1}" HEAD | Select-String -Pattern $pattern
+        if ($changedFiles) {
+            $changedlist += $changedFiles
+        }
+    }
+    if ($changedlist) {
+        Write-Output "--- Files changed:"
+        Write-Output $changedlist
+        return $true
+    }
+    else {
+        Write-Output "--- No files changed within specified changeset:"
+        Write-Output $patterns
+        return $false
+    }
+}
+
+function AreChangedOnlyPaths($patterns) {
+    $changedFiles = & git diff --name-only "HEAD@{1}" HEAD
+    $matchedFiles = @()
+    foreach ($pattern in $patterns) {
+        $matched = $changedFiles | Select-String -Pattern $pattern
+        if ($matched) {
+            $matchedFiles += $matched
+        }
+    }
+    if (($matchedFiles.Count -eq $changedFiles.Count) -or ($changedFiles.Count -eq 0)) {
+        return $true
+    }
+    return $false
+}
+
+# This function sets a `MODULE` env var, required by IT tests, containing a comma separated list of modules for a given beats project (specified via the first argument).
+# The list is built depending on directories that have changed under `modules/` excluding anything else such as asciidoc and png files.
+# `MODULE` will empty if no changes apply.
+function DefineModuleFromTheChangeSet($projectPath) {
+    $projectPathTransformed = $projectPath -replace '/', '\\'
+    $projectPathExclusion = "((?!^$projectPathTransformed\\\/).)*\$"
+    $exclude = @("^($projectPathExclusion|((?!\\/module\\/).)*\$|.*\\.asciidoc|.*\\.png)")
+
+    $changedModules = ''
+
+    $moduleDirs = Get-ChildItem -Directory "$projectPath\module"
+    foreach($moduleDir in $moduleDirs) {
+        if((ArePathsChanged($moduleDir)) -and !(AreChangedOnlyPaths($exclude))) {
+            if(!$changedModules) {
+                $changedModules = $moduleDir.Name
+            }
+            else {
+                $changedModules += ',' + $moduleDir.Name
+            }
+        }
+    }
+
+    # TODO: remove this conditional when issue https://github.com/elastic/ingest-dev/issues/2993 gets resolved
+    if(!$changedModules) {
+        if($Env:BUILDKITE_PIPELINE_SLUG -eq 'beats-xpack-metricbeat') {
+            $Env:MODULE = "aws"
+        }
+        else {
+            $Env:MODULE = "kubernetes"
+        }
+    }
+    else {
+        # TODO: once https://github.com/elastic/ingest-dev/issues/2993 gets resolved, this should be the only thing we export
+        $Env:MODULE = $changedModules
+    }
+}
--- .buildkite/scripts/common.sh
+++ .buildkite/scripts/common.sh
@@ -80,9 +80,6 @@ case "${BUILDKITE_PIPELINE_SLUG}" in
   "beats-packetbeat")
     BEAT_CHANGESET_REFERENCE=${packetbeat_changeset[@]}
     ;;
-  "beats-winlogbeat")
-    BEAT_CHANGESET_REFERENCE=${winlogbeat_changeset[@]}
-    ;;
   "beats-xpack-metricbeat")
     BEAT_CHANGESET_REFERENCE=${xpack_metricbeat_changeset[@]}
     ;;
@@ -515,7 +512,7 @@ if are_paths_changed "${packaging_changeset[@]}" ; then
   export PACKAGING_CHANGES="true"
 fi
 
-if [[ "$BUILDKITE_STEP_KEY" == "xpack-winlogbeat-pipeline" || "$BUILDKITE_STEP_KEY" == "xpack-metricbeat-pipeline" || "$BUILDKITE_STEP_KEY" == "xpack-dockerlogbeat-pipeline" || "$BUILDKITE_STEP_KEY" == "metricbeat-pipeline" ]]; then
+if [[ "$BUILDKITE_STEP_KEY" == "xpack-metricbeat-pipeline" || "$BUILDKITE_STEP_KEY" == "xpack-dockerlogbeat-pipeline" || "$BUILDKITE_STEP_KEY" == "metricbeat-pipeline" ]]; then
   # Set the MODULE env variable if possible, it should be defined before generating pipeline's steps. It is used in multiple pipelines.
   defineModuleFromTheChangeSet "${BEATS_PROJECT_NAME}"
 fi
