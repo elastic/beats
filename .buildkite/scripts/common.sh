198a80d06d08487511aec0f1a50068b69e888dc3
Siarhei Harbuz <87968844+sharbuz@users.noreply.github.com>
2024-04-22 12:42:53 +0300

    Update common.sh

--- .buildkite/scripts/common.sh
+++ .buildkite/scripts/common.sh
@@ -27,16 +27,12 @@ XPACK_MODULE_PATTERN="^x-pack\\/[a-z0-9]+beat\\/module\\/([^\\/]+)\\/.*"
 # define if needed run cloud-specific tests for the particular beat
 [ -z "${run_xpack_metricbeat_aws_tests+x}" ] && run_xpack_metricbeat_aws_tests="$(buildkite-agent meta-data get run_xpack_metricbeat_aws_tests --default "false")"
 
-packetbeat_changeset=(
-  "^packetbeat/.*"
+winlogbeat_changeset=(
+  "^winlogbeat/.*"
   )
 
 xpack_dockerlogbeat_changeset=(
   "^x-pack/dockerlogbeat/.*"
-  )
-
-winlogbeat_changeset=(
-  "^winlogbeat/.*"
   )
 
 ci_changeset=(
