7d6369ff35098f1c9b38bbc48e4f07f676efd282
Fae Charlton <fae.charlton@elastic.co>
2024-04-23 15:27:55 -0400

    lint

--- x-pack/filebeat/input/awss3/input_benchmark_test.go
+++ x-pack/filebeat/input/awss3/input_benchmark_test.go
@@ -8,7 +8,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -134,7 +133,7 @@ type constantS3 struct {
 var _ s3API = (*constantS3)(nil)
 
 func newConstantS3(t testing.TB) *constantS3 {
-	data, err := ioutil.ReadFile(cloudtrailTestFile)
+	data, err := os.ReadFile(cloudtrailTestFile)
 	if err != nil {
 		t.Fatal(err)
 	}
