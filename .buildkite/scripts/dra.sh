e0927c2aeba1610cd284d3e423d018899242eaee
Alexandros Sapranidis <alexandros@elastic.co>
2024-04-25 18:30:19 +0300

    Remove chown workaround
    Signed-off-by: Alexandros Sapranidis <alexandros@elastic.co>

--- .buildkite/scripts/dra.sh
+++ .buildkite/scripts/dra.sh
@@ -25,8 +25,6 @@ function release_manager_login {
 
 release_manager_login
 
-echo "+++ Changing permissions for the BK API commands"
-sudo chown -R :1000 build/distributions/
 
 echo "+++ :hammer_and_pick: Listing $BRANCH $DRA_WORKFLOW DRA artifacts..."
 set +x
