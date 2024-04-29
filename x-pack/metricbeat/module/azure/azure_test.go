4f91e56f0d2af8c243d4481eb4d822f60f3dbfe2
Maurizio Branca <maurizio.branca@gmail.com>
2024-04-29 05:37:04 +0200

    Add missing heaer

--- x-pack/metricbeat/module/azure/azure_test.go
+++ x-pack/metricbeat/module/azure/azure_test.go
@@ -1,3 +1,7 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
 package azure
 
 import (
@@ -10,25 +14,25 @@ func TestGroupMetricsDefinitionsByResourceId(t *testing.T) {
 	t.Run("Group metrics definitions by resource ID", func(t *testing.T) {
 		metrics := []Metric{
 			{
-				ResourceId: "test",
-				Namespace:  "test",
-				Names:      []string{"name1"},
+				ResourceId: "resource-1",
+				Namespace:  "namespace-1",
+				Names:      []string{"metric-1"},
 			},
 			{
-				ResourceId: "test",
-				Namespace:  "test",
-				Names:      []string{"name2"},
+				ResourceId: "resource-1",
+				Namespace:  "namespace-1",
+				Names:      []string{"metric-2"},
 			},
 			{
-				ResourceId: "test",
-				Namespace:  "test",
-				Names:      []string{"name3"},
+				ResourceId: "resource-1",
+				Namespace:  "namespace-1",
+				Names:      []string{"metric-3"},
 			},
 		}
 
 		metricsByResourceId := groupMetricsDefinitionsByResourceId(metrics)
 
 		assert.Equal(t, 1, len(metricsByResourceId))
-		assert.Equal(t, 3, len(metricsByResourceId["test"]))
+		assert.Equal(t, 3, len(metricsByResourceId["resource-1"]))
 	})
 }
