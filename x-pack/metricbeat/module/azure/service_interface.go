fae8b76f2462b52229fee3f4ae81be96b5d13af5
Maurizio Branca <maurizio.branca@gmail.com>
2024-04-29 16:55:10 +0200

    Update docs formatting

--- x-pack/metricbeat/module/azure/service_interface.go
+++ x-pack/metricbeat/module/azure/service_interface.go
@@ -19,12 +19,12 @@ type Service interface {
 	//
 	// If the timegrain is empty, the default timegrain for the metric is used and returned.
 	GetMetricValues(
-		resourceId string,    // resourceId is the ID of the resource to query (e.g. "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/{resourceProviderNamespace}/{resourceType}/{resourceName}")
-		namespace string,     // namespace is the metric namespace to query (e.g. "Microsoft.Compute/virtualMachines")
-		timegrain string,     // timegrain is the timegrain to use for the metric query (e.g. "PT1M"); if empty, returns the default timegrain for the metric.
-		timespan string,      // timespan is the time interval to query (e.g. 2024-04-29T14:03:00Z/2024-04-29T14:04:00Z)
+		resourceId string, // resourceId is the ID of the resource to query (e.g. "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/{resourceProviderNamespace}/{resourceType}/{resourceName}")
+		namespace string, // namespace is the metric namespace to query (e.g. "Microsoft.Compute/virtualMachines")
+		timegrain string, // timegrain is the timegrain to use for the metric query (e.g. "PT1M"); if empty, returns the default timegrain for the metric.
+		timespan string, // timespan is the time interval to query (e.g. 2024-04-29T14:03:00Z/2024-04-29T14:04:00Z)
 		metricNames []string, // metricNames is the list of metric names to query (e.g. ["ServiceApiLatency", "Availability"])
-		aggregations string,  // aggregations is the comma-separated list of aggregations to use for the metric query (e.g. "Average,Maximum,Minimum")
-		filter string,        // filter is the filter to query for dimensions (e.g. "ActivityType eq '*' AND ActivityName eq '*' AND StatusCode eq '*' AND StatusCodeClass eq '*'")
+		aggregations string, // aggregations is the comma-separated list of aggregations to use for the metric query (e.g. "Average,Maximum,Minimum")
+		filter string, // filter is the filter to query for dimensions (e.g. "ActivityType eq '*' AND ActivityName eq '*' AND StatusCode eq '*' AND StatusCodeClass eq '*'")
 	) ([]armmonitor.Metric, string, error)
 }
