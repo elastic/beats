61aa9221b3094281c1bc29c599e65b2a18e16200
fearful-symmetry <alexpkristiansen@icloud.com>
2024-05-03 14:36:15 -0700

    Revert "linter..."
    This reverts commit 03505d8fcc13dc4bbb219c899232fa88bf22fdef.

--- metricbeat/module/windows/perfmon/data.go
+++ metricbeat/module/windows/perfmon/data.go
@@ -20,7 +20,6 @@
 package perfmon
 
 import (
-	"errors"
 	"fmt"
 	"regexp"
 	"strconv"
@@ -49,7 +48,7 @@ func (re *Reader) groupToEvents(counters map[string][]pdh.CounterValue) []mb.Eve
 				// The counter has a negative value or the counter was successfully found, but the data returned is not valid.
 				// This error can occur if the counter value is less than the previous value. (Because counter values always increment, the counter value rolls over to zero when it reaches its maximum value.)
 				// This is not an error that stops the application from running successfully and a positive counter value should be retrieved in the later calls.
-				if errors.Is(val.Err.Error, pdh.PDH_CALC_NEGATIVE_VALUE) || errors.Is(val.Err.Error, pdh.PDH_INVALID_DATA) {
+				if val.Err.Error == pdh.PDH_CALC_NEGATIVE_VALUE || val.Err.Error == pdh.PDH_INVALID_DATA {
 					re.log.Debugw("Counter value retrieval returned",
 						"error", val.Err.Error, "cstatus", pdh.PdhErrno(val.Err.CStatus), logp.Namespace("perfmon"), "query", counterPath)
 					continue
@@ -94,11 +93,9 @@ func (re *Reader) groupToEvents(counters map[string][]pdh.CounterValue) []mb.Eve
 		}
 	}
 	// Write the values into the map.
-	events := make([]mb.Event, len(eventMap))
-	iter := 0
+	var events []mb.Event
 	for _, val := range eventMap {
-		events[iter] = *val
-		iter++
+		events = append(events, *val)
 	}
 	return events
 }
@@ -114,7 +111,7 @@ func (re *Reader) groupToSingleEvent(counters map[string][]pdh.CounterValue) mb.
 				// Some counters, such as rate counters, require two counter values in order to compute a displayable value. In this case we must call PdhCollectQueryData twice before calling PdhGetFormattedCounterValue.
 				// For more information, see Collecting Performance Data (https://docs.microsoft.com/en-us/windows/desktop/PerfCtrs/collecting-performance-data).
 				if val.Err.Error != nil {
-					if errors.Is(val.Err.Error, pdh.PDH_CALC_NEGATIVE_VALUE) || errors.Is(val.Err.Error, pdh.PDH_INVALID_DATA) {
+					if val.Err.Error == pdh.PDH_CALC_NEGATIVE_VALUE || val.Err.Error == pdh.PDH_INVALID_DATA {
 						re.log.Debugw("Counter value retrieval returned",
 							"error", val.Err.Error, "cstatus", pdh.PdhErrno(val.Err.CStatus), logp.Namespace("perfmon"), "query", counterPath)
 						continue
