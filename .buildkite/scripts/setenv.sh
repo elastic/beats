2a94bf837374881e2e20940a748e42f71e593b2d
Olga Naidjonoka <olga_naidjonoka@epam.com>
2024-05-09 16:49:50 +0300

    fixed conflicts

--- .buildkite/scripts/setenv.sh
+++ .buildkite/scripts/setenv.sh
@@ -2,40 +2,6 @@
 
 set -euo pipefail
 
-<<<<<<< HEAD
-REPO="beats"
-TMP_FOLDER="tmp.${REPO}"
-DOCKER_REGISTRY="docker.elastic.co"
-SETUP_GVM_VERSION="v0.5.1"
-DOCKER_COMPOSE_VERSION="1.21.0"
-DOCKER_COMPOSE_VERSION_AARCH64="v2.21.0"
-SETUP_WIN_PYTHON_VERSION="3.11.0"
-NMAP_WIN_VERSION="7.12"           # Earlier versions of NMap provide WinPcap (the winpcap packages don't install nicely because they pop-up a UI)
-GO_VERSION=$(cat .go-version)
-ASDF_MAGE_VERSION="1.15.0"
-ASDF_TERRAFORM_VERSION="1.0.2"
-PACKAGING_PLATFORMS="+all linux/amd64 linux/arm64 windows/amd64 darwin/amd64"
-PACKAGING_ARM_PLATFORMS="linux/arm64"
-AWS_REGION="eu-central-1"
-NODEJS_VERSION="18.17.1"
-
-export SETUP_GVM_VERSION
-export DOCKER_COMPOSE_VERSION
-export DOCKER_COMPOSE_VERSION_AARCH64
-export SETUP_WIN_PYTHON_VERSION
-export NMAP_WIN_VERSION
-export GO_VERSION
-export ASDF_MAGE_VERSION
-export PACKAGING_PLATFORMS
-export PACKAGING_ARM_PLATFORMS
-export REPO
-export TMP_FOLDER
-export DOCKER_REGISTRY
-export ASDF_TERRAFORM_VERSION
-export AWS_REGION
-export NODEJS_VERSION
-
-=======
 WORKSPACE=${WORKSPACE:-"$(pwd)"}
 GO_VERSION=$(cat .go-version)
 
@@ -51,7 +17,6 @@ export AWS_REGION="eu-central-1"
 export WORKSPACE
 export GO_VERSION
 
->>>>>>> 8664cae202 (Deploy/K8S pipeline post-migration fixes (#39295))
 exportVars() {
   local platform_type="$(uname)"
   local arch_type="$(uname -m)"
