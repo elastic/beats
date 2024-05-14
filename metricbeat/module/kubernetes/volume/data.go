835d9911e379f1bdeb5b168abb9b8037a1c03eff
Mirko Bez <mirko.bez@elastic.co>
2024-05-14 17:32:09 +0200

    Remove empty lines added by mistake

--- metricbeat/module/kubernetes/volume/data.go
+++ metricbeat/module/kubernetes/volume/data.go
@@ -40,7 +40,6 @@ func eventMapping(content []byte, logger *logp.Logger) ([]mapstr.M, error) {
 	node := summary.Node
 	for _, pod := range summary.Pods {
 		for _, volume := range pod.Volume {
-
 			volumeEvent := mapstr.M{
 				mb.ModuleDataKey: mapstr.M{
 					"namespace": pod.PodRef.Namespace,
@@ -76,7 +75,6 @@ func eventMapping(content []byte, logger *logp.Logger) ([]mapstr.M, error) {
 			if volume.Inodes > 0 {
 				kubernetes2.ShouldPut(volumeEvent, "fs.inodes.pct", float64(volume.InodesUsed)/float64(volume.Inodes), logger)
 			}
-
 			if volume.PvcRef.Name != "" {
 				kubernetes2.ShouldPut(volumeEvent, mb.ModuleDataKey+".persistentvolumeclaim.name", volume.PvcRef.Name, logger)
 			}
