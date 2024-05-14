bf809fa9b0689d42927b07350f4be4f819744d3c
emilioalvap <emilio.alvarezpineiro@elastic.co>
2024-05-14 11:53:45 +0200

    Adjust comment size

--- x-pack/heartbeat/monitors/browser/synthexec/synthcmd.go
+++ x-pack/heartbeat/monitors/browser/synthexec/synthcmd.go
@@ -11,7 +11,12 @@ import (
 	"strings"
 )
 
+<<<<<<< Updated upstream
 // Variant of exec.command with redacted params, which could contain sensitive stuff
+=======
+// Variant of exec.command with redacted params and playwright options, which
+// might contain sensitive information.
+>>>>>>> Stashed changes
 type SynthCmd struct {
 	*exec.Cmd
 }
