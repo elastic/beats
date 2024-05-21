2c16d592f7a44154ff97e43f74b5310552473ae1
emmanueltouzery <etouzery@gmail.com>
2024-05-21 21:32:38 +0200

    Apply suggestions from code review
    Co-authored-by: Tiago Queiroz <me@tiago.life>

--- libbeat/statestore/backend/memlog/store_test.go
+++ libbeat/statestore/backend/memlog/store_test.go
@@ -19,21 +19,18 @@ package memlog
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/elastic/elastic-agent-libs/logp"
 	"github.com/elastic/elastic-agent-libs/mapstr"
 )
 
-func init() {
-	logp.DevelopmentSetup()
-}
-
 func TestRecoverFromCorruption(t *testing.T) {
 	path := t.TempDir()
-	defer os.RemoveAll(path)
+	logp.DevelopmentSetup()
 
 	if err := copyPath(path, "testdata/1/logfile_incomplete/"); err != nil {
 		t.Fatalf("Failed to copy test file to the temporary directory: %v", err)
@@ -42,13 +39,17 @@ func TestRecoverFromCorruption(t *testing.T) {
 	store, err := openStore(logp.NewLogger("test"), path, 0660, 4096, false, func(_ uint64) bool {
 		return false
 	})
-	assert.NoError(t, err)
-
-	assert.Equal(t, true, store.disk.logInvalid)
+	require.NoError(t, err, "openStore must succeed")
+	require.True(t, store.disk.logInvalid, "expecting the log file to be invalid")
 
 	err = store.logOperation(&opSet{K: "key", V: mapstr.M{
 		"field": 42,
 	}})
-	assert.NoError(t, err)
-	assert.Equal(t, false, store.disk.logInvalid)
+	require.NoError(t, err, "logOperation must succeed")
+	require.False(t, store.disk.logInvalid, "log file must be valid")
+	require.FileExistsf(t, filepath.Join(path, "7.json"), "expecting the checkpoint file to have been created")
+
+	file, err := os.Stat(filepath.Join(path, "log.json"))
+	require.NoError(t, err, "Stat on the log file must succeed")
+	require.Equal(t, int64(0), file.Size(), "expecting the log file to be truncated")
 }
