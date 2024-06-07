8c100a69798866630989f8fa2545409f423b0098
emilioalvap <emilio.alvarezpineiro@elastic.co>
2024-06-07 18:41:25 +0200

    [Heartbeat] Unit test plugin imports inside cmd module

---
+++ x-pack/heartbeat/cmd/root_test.go
@@ -0,0 +1,27 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+//go:build linux || synthetics
+
+package cmd
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/elastic/beats/v7/heartbeat/monitors/plugin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRootCmdPlugins(t *testing.T) {
+	// Test all required plugins are exported by this module, since it's the one imported by agentbeat
+	// https://github.com/elastic/beats/pull/39818
+	t.Parallel()
+	plugins := []string{"http", "tcp", "icmp", "browser"}
+	for _, p := range plugins {
+		t.Run(fmt.Sprintf("%s plugin", p), func(t *testing.T) {
+			_, found := plugin.GlobalPluginsReg.Get(p)
+			assert.True(t, found)
+		})
+	}
+}
