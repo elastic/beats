cd305c0396b15030215cdcc29755aa38530faf90
Olga Naidjonoka <olga_naidjonoka@epam.com>
2024-06-13 14:29:11 +0300

    updated initCloudEnv script

--- .buildkite/scripts/initCloudEnv.sh
+++ .buildkite/scripts/initCloudEnv.sh
@@ -9,13 +9,13 @@ exportAwsSecrets() {
   export AWS_SECRET_ACCESS_KEY=$BEATS_AWS_SECRET_KEY
   export TEST_TAGS="${TEST_TAGS:+$TEST_TAGS,}aws"
 
-  # AWS_REGION is not set here, since AWS region is taken from *.tf file:
+  # AWS_REGION is not set here, since AWS region is taken from beat corresponding *.tf file:
   # - x-pack/metricbeat/module/aws/terraform.tf
   # - x-pack/filebeat/input/awscloudwatch/_meta/terraform/variables.tf
 }
 
 terraformApply() {
-  echo "--- Exporting Terraform Env Vars"
+  echo "Exporting Terraform Env Vars"
   TF_VAR_BRANCH=$(echo "${BUILDKITE_BRANCH}" | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9-]/-/g')
   TF_VAR_CREATED_DATE=$(date +%s)
   export TF_VAR_BUILD_ID="${BUILDKITE_BUILD_ID}"
@@ -37,6 +37,8 @@ terraformDestroy() {
   find "$MODULE_DIR" -name terraform.tfstate -print0 | while IFS= read -r -d '' tfstate; do
     cd "$(dirname "$tfstate")"
     buildkite-agent artifact upload "**/terraform.tfstate"
+    buildkite-agent artifact upload "**/.terraform/**"
+    buildkite-agent artifact upload "outputs*.yml"
     if ! terraform destroy -auto-approve; then
       return 1
     fi
@@ -45,9 +47,48 @@ terraformDestroy() {
   return 0
 }
 
+dockerUp() {
+  echo "--- COMPOSE UP: Current folder: $(pwd)"
+  echo "~~~ Run docker-compose services for emulated cloud env"
+  docker-compose -f .buildkite/deploy/docker/docker-compose.yml up -d
+}
+
 dockerTeardown() {
+  echo "--- COMPOSE DOWN: Current folder: $(pwd)"
   echo "~~~ Docker Compose Teardown"
   docker-compose -f .buildkite/deploy/docker/docker-compose.yml down -v
+}
+
+terraformSetup() {
+  max_retries=2
+  timeout=5
+  retries=0
+
+  while true; do
+    echo "~~~ Setting up Terraform"
+    out=$(terraformApply 2>&1)
+    exit_code=$?
+
+    echo "$out"
+
+    if [ $exit_code -eq 0 ]; then
+      break
+    else
+      retries=$((retries + 1))
+
+      if [ $retries -gt $max_retries ]; then
+        teardown
+        echo "+++ Terraform init & apply failed: $out"
+        exit 1
+      fi
+
+      teardown
+
+      sleep_time=$((timeout * retries))
+      echo "~~~~ Retry #$retries failed. Retrying after ${sleep_time}s..."
+      sleep $sleep_time
+    fi
+  done
 }
 
 teardown() {
@@ -58,36 +99,5 @@ teardown() {
 trap 'teardown' EXIT
 
 exportAwsSecrets
-
-echo "~~~ Run docker-compose services for emulated cloud env"
-docker-compose -f .buildkite/deploy/docker/docker-compose.yml up -d
-
-max_retries=2
-timeout=5
-retries=0
-
-while true; do
-  echo "~~~ Setting up Terraform"
-  out=$(terraformApply 2>&1)
-  exit_code=$?
-
-  echo "$out"
-
-  if [ $exit_code -eq 0 ]; then
-    break
-  else
-    retries=$((retries + 1))
-
-    if [ $retries -gt $max_retries ]; then
-      terraformDestroy
-      echo "+++ Terraform init & apply failed: $out"
-      exit 1
-    fi
-
-    terraformDestroy
-
-    sleep_time=$((timeout * retries))
-    echo "~~~~ Retry #$retries failed. Retrying after ${sleep_time}s..."
-    sleep $sleep_time
-  fi
-done
+dockerUp
+terraformSetup
