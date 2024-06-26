f224875f05e92cea93ae2c0856043830b25a1ac8
Tiago Queiroz <tiago.queiroz@elastic.co>
2024-06-26 15:36:51 -0400

    Add test to ensure events are in the same format

--- filebeat/input/journald/input_test.go
+++ filebeat/input/journald/input_test.go
@@ -21,11 +21,16 @@ package journald
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"os"
 	"path"
+	"path/filepath"
 	"testing"
 
+	"github.com/elastic/beats/v7/libbeat/beat"
 	"github.com/elastic/elastic-agent-libs/mapstr"
+	"github.com/stretchr/testify/require"
 )
 
 func TestInputFieldsTranslation(t *testing.T) {
@@ -89,28 +94,64 @@ func TestCompare(t *testing.T) {
 	env := newInputTestingEnvironment(t)
 
 	inp := env.mustCreateInput(mapstr.M{
-		"paths":                 []string{path.Join("testdata", "input-multiline-parser.journal")},
-		"include_matches.match": []string{"_SYSTEMD_USER_UNIT=log-service.service"},
+		"paths": []string{path.Join("testdata", "input-multiline-parser.journal")},
+		// "include_matches.match": []string{"_SYSTEMD_USER_UNIT=log-service.service"},
 	})
 
 	ctx, cancelInput := context.WithCancel(context.Background())
 	defer cancelInput()
 
 	env.startInput(ctx, inp)
-	env.waitUntilEventCount(6)
-
+	env.waitUntilEventCount(8)
 	t.Log("Legacy journald input ok, starting journalctl")
 
 	env2 := newInputTestingEnvironment(t)
 	inp2 := env2.mustCreateInput(mapstr.M{
-		"paths":                 []string{path.Join("testdata", "input-multiline-parser.journal")},
-		"include_matches.match": []string{"_SYSTEMD_USER_UNIT=log-service.service"},
-		"journalctl":            true,
+		"paths": []string{path.Join("testdata", "input-multiline-parser.journal")},
+		// "include_matches.match": []string{"_SYSTEMD_USER_UNIT=log-service.service"},
+		"journalctl": true,
 	})
 
 	ctx2, cancelInput2 := context.WithCancel(context.Background())
 	defer cancelInput2()
 
 	env2.startInput(ctx2, inp2)
-	env2.waitUntilEventCount(6)
+	env2.waitUntilEventCount(8)
+}
+
+// TestCompareGoSystemdWithJournalctl ensures the new implementation produces
+// events in the same format as the original one. We use the events from the
+// already existing journal file 'input-multiline-parser.journal'
+func TestCompareGoSystemdWithJournalctl(t *testing.T) {
+	env := newInputTestingEnvironment(t)
+	inp := env.mustCreateInput(mapstr.M{
+		"paths":      []string{path.Join("testdata", "input-multiline-parser.journal")},
+		"journalctl": true,
+	})
+
+	ctx2, cancelInput2 := context.WithCancel(context.Background())
+	defer cancelInput2()
+
+	env.startInput(ctx2, inp)
+	env.waitUntilEventCount(8)
+
+	rawEvents := env.pipeline.GetAllEvents()
+	events := []beat.Event{}
+	for _, evt := range rawEvents {
+		evt.Delete("event.created")
+		events = append(events, evt)
+	}
+
+	// Read JSON events
+	goldenEvents := []beat.Event{}
+	data, err := os.ReadFile(filepath.Join("testdata", "input-multiline-parser-events.json"))
+	if err != nil {
+		t.Fatalf("cannot read golden file: %s", err)
+	}
+
+	if err := json.Unmarshal(data, &goldenEvents); err != nil {
+		t.Fatalf("cannot unmarshal golden events: %s", err)
+	}
+
+	require.Equal(t, goldenEvents, events, "events do not match reference")
 }
