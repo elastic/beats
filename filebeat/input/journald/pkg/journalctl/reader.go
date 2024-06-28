319771714aa0a5df48c28589d58f337e6449b001
Tiago Queiroz <tiago.queiroz@elastic.co>
2024-06-28 16:06:27 -0400

    Add units, identifiers and small refactoring

--- filebeat/input/journald/pkg/journalctl/reader.go
+++ filebeat/input/journald/pkg/journalctl/reader.go
@@ -1,3 +1,20 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package journalctl
 
 import (
@@ -13,6 +30,7 @@ import (
 	"time"
 
 	"github.com/coreos/go-systemd/v22/sdjournal"
+
 	"github.com/elastic/beats/v7/filebeat/input/journald/pkg/journalfield"
 	"github.com/elastic/beats/v7/filebeat/input/journald/pkg/journalread"
 	input "github.com/elastic/beats/v7/filebeat/input/v2"
@@ -38,6 +56,8 @@ type Reader struct {
 func New(
 	logger *logp.Logger,
 	canceler input.Canceler,
+	units []string,
+	syslogIdentifiers []string,
 	matchers journalfield.IncludeMatches,
 	mode journalread.SeekMode,
 	cursor string,
@@ -61,6 +81,14 @@ func New(
 		// Do not append anything
 	default:
 		return nil, fmt.Errorf("unknown seek mode %v", mode)
+	}
+
+	for _, u := range units {
+		args = append(args, "--unit", u)
+	}
+
+	for _, i := range syslogIdentifiers {
+		args = append(args, "--identifier", i)
 	}
 
 	for _, m := range matchers.Matches {
