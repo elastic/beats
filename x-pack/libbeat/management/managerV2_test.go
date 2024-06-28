0bb4e1018cef32d6cb9c05960a39db7058605f6c
Blake Rouse <blake.rouse@elastic.co>
2024-06-28 18:20:06 -0400

    Add APM trace configuration to V2AgentManager (#40030)

--- x-pack/libbeat/management/managerV2_test.go
+++ x-pack/libbeat/management/managerV2_test.go
@@ -27,6 +27,7 @@ import (
 	"github.com/elastic/beats/v7/libbeat/common/reload"
 	"github.com/elastic/beats/v7/libbeat/features"
 	"github.com/elastic/beats/v7/libbeat/tests/integration"
+	"github.com/elastic/beats/v7/libbeat/version"
 )
 
 func TestManagerV2(t *testing.T) {
@@ -36,6 +37,8 @@ func TestManagerV2(t *testing.T) {
 	r.MustRegisterOutput(output)
 	inputs := &reloadableList{}
 	r.MustRegisterInput(inputs)
+	apm := &reloadable{}
+	r.MustRegisterAPM(apm)
 
 	configsSet := atomic.Bool{}
 	configsCleared := atomic.Bool{}
@@ -46,22 +49,25 @@ func TestManagerV2(t *testing.T) {
 		if currentIdx == 1 {
 			oCfg := output.Config()
 			iCfgs := inputs.Configs()
-			if oCfg != nil && len(iCfgs) == 3 {
+			apmCfg := apm.Config()
+			if oCfg != nil && len(iCfgs) == 3 && apmCfg != nil {
 				configsSet.Store(true)
-				t.Log("output and inputs configuration set")
+				t.Log("output, inputs, and APM configuration set")
 			}
 		} else if currentIdx == 2 {
 			oCfg := output.Config()
 			iCfgs := inputs.Configs()
-			if oCfg == nil || len(iCfgs) != 3 {
+			apmCfg := apm.Config()
+			if oCfg == nil || len(iCfgs) != 3 || apmCfg == nil {
 				// should not happen (config no longer set)
 				configsSet.Store(false)
-				t.Log("output and inputs configuration cleared (should not happen)")
+				t.Log("output, inputs, and APM configuration cleared (should not happen)")
 			}
 		} else {
 			oCfg := output.Config()
 			iCfgs := inputs.Configs()
-			if oCfg == nil && len(iCfgs) == 0 {
+			apmCfg := apm.Config()
+			if oCfg == nil && len(iCfgs) == 0 && apmCfg == nil {
 				configsCleared.Store(true)
 			}
 			if len(observed.Units) == 0 {
@@ -78,125 +84,161 @@ func TestManagerV2(t *testing.T) {
 		t.Logf("FQDN feature flag set to %v", fqdnEnabled.Load())
 	}
 
-	srv := integration.NewMockServer([][]*proto.UnitExpected{
+	agentInfo := &proto.AgentInfo{
+		Id:       "elastic-agent-id",
+		Version:  version.GetDefaultVersion(),
+		Snapshot: true,
+	}
+	apmConfig := &proto.ElasticAPM{
+		Tls: &proto.ElasticAPMTLS{
+			ServerCa:   "serverca",
+			ServerCert: "servercert",
+			SkipVerify: true, // set to true as false is default when empty
+		},
+		Environment:  "unit-testing",
+		ApiKey:       "apikey123",
+		SecretToken:  "secrettoken123",
+		Hosts:        []string{"localhost:8200", "otherhost:8200"},
+		GlobalLabels: "label1,label2",
+	}
+	srv := integration.NewMockServer([]*proto.CheckinExpected{
 		{
-			{
-				Id:             "output-unit",
-				Type:           proto.UnitType_OUTPUT,
-				ConfigStateIdx: 1,
-				Config: &proto.UnitExpectedConfig{
-					Id:   "default",
-					Type: "elasticsearch",
-					Name: "elasticsearch",
+			AgentInfo: agentInfo,
+			Units: []*proto.UnitExpected{
+				{
+					Id:             "output-unit",
+					Type:           proto.UnitType_OUTPUT,
+					ConfigStateIdx: 1,
+					Config: &proto.UnitExpectedConfig{
+						Id:   "default",
+						Type: "elasticsearch",
+						Name: "elasticsearch",
+					},
+					State:    proto.State_HEALTHY,
+					LogLevel: proto.UnitLogLevel_INFO,
 				},
-				State:    proto.State_HEALTHY,
-				LogLevel: proto.UnitLogLevel_INFO,
-			},
-			{
-				Id:             "input-unit-1",
-				Type:           proto.UnitType_INPUT,
-				ConfigStateIdx: 1,
-				Config: &proto.UnitExpectedConfig{
-					Id:   "system/metrics-system-default-system-1",
-					Type: "system/metrics",
-					Name: "system-1",
-					Streams: []*proto.Stream{
-						{
-							Id: "system/metrics-system.filesystem-default-system-1",
-							Source: integration.RequireNewStruct(t, map[string]interface{}{
-								"metricsets": []interface{}{"filesystem"},
-								"period":     "1m",
-							}),
+				{
+					Id:             "input-unit-1",
+					Type:           proto.UnitType_INPUT,
+					ConfigStateIdx: 1,
+					Config: &proto.UnitExpectedConfig{
+						Id:   "system/metrics-system-default-system-1",
+						Type: "system/metrics",
+						Name: "system-1",
+						Streams: []*proto.Stream{
+							{
+								Id: "system/metrics-system.filesystem-default-system-1",
+								Source: integration.RequireNewStruct(t, map[string]interface{}{
+									"metricsets": []interface{}{"filesystem"},
+									"period":     "1m",
+								}),
+							},
 						},
 					},
+					State:    proto.State_HEALTHY,
+					LogLevel: proto.UnitLogLevel_INFO,
 				},
-				State:    proto.State_HEALTHY,
-				LogLevel: proto.UnitLogLevel_INFO,
-			},
-			{
-				Id:             "input-unit-2",
-				Type:           proto.UnitType_INPUT,
-				ConfigStateIdx: 1,
-				Config: &proto.UnitExpectedConfig{
-					Id:   "system/metrics-system-default-system-2",
-					Type: "system/metrics",
-					Name: "system-2",
-					Streams: []*proto.Stream{
-						{
-							Id: "system/metrics-system.filesystem-default-system-2",
-							Source: integration.RequireNewStruct(t, map[string]interface{}{
-								"metricsets": []interface{}{"filesystem"},
-								"period":     "1m",
-							}),
-						},
-						{
-							Id: "system/metrics-system.filesystem-default-system-3",
-							Source: integration.RequireNewStruct(t, map[string]interface{}{
-								"metricsets": []interface{}{"filesystem"},
-								"period":     "1m",
-							}),
+				{
+					Id:             "input-unit-2",
+					Type:           proto.UnitType_INPUT,
+					ConfigStateIdx: 1,
+					Config: &proto.UnitExpectedConfig{
+						Id:   "system/metrics-system-default-system-2",
+						Type: "system/metrics",
+						Name: "system-2",
+						Streams: []*proto.Stream{
+							{
+								Id: "system/metrics-system.filesystem-default-system-2",
+								Source: integration.RequireNewStruct(t, map[string]interface{}{
+									"metricsets": []interface{}{"filesystem"},
+									"period":     "1m",
+								}),
+							},
+							{
+								Id: "system/metrics-system.filesystem-default-system-3",
+								Source: integration.RequireNewStruct(t, map[string]interface{}{
+									"metricsets": []interface{}{"filesystem"},
+									"period":     "1m",
+								}),
+							},
 						},
 					},
+					State:    proto.State_HEALTHY,
+					LogLevel: proto.UnitLogLevel_INFO,
 				},
-				State:    proto.State_HEALTHY,
-				LogLevel: proto.UnitLogLevel_INFO,
+			},
+			Features:    nil,
+			FeaturesIdx: 1,
+		},
+		{
+			AgentInfo: agentInfo,
+			Units: []*proto.UnitExpected{
+				{
+					Id:             "output-unit",
+					Type:           proto.UnitType_OUTPUT,
+					ConfigStateIdx: 1,
+					State:          proto.State_HEALTHY,
+					LogLevel:       proto.UnitLogLevel_INFO,
+				},
+				{
+					Id:             "input-unit-1",
+					Type:           proto.UnitType_INPUT,
+					ConfigStateIdx: 1,
+					State:          proto.State_HEALTHY,
+					LogLevel:       proto.UnitLogLevel_DEBUG,
+				},
+				{
+					Id:             "input-unit-2",
+					Type:           proto.UnitType_INPUT,
+					ConfigStateIdx: 1,
+					State:          proto.State_HEALTHY,
+					LogLevel:       proto.UnitLogLevel_INFO,
+				},
+			},
+			Features:    &proto.Features{Fqdn: &proto.FQDNFeature{Enabled: true}},
+			FeaturesIdx: 2,
+			Component: &proto.Component{
+				ApmConfig: &proto.APMConfig{Elastic: apmConfig},
 			},
 		},
 		{
-			{
-				Id:             "output-unit",
-				Type:           proto.UnitType_OUTPUT,
-				ConfigStateIdx: 1,
-				State:          proto.State_HEALTHY,
-				LogLevel:       proto.UnitLogLevel_INFO,
+			AgentInfo: agentInfo,
+			Units: []*proto.UnitExpected{
+				{
+					Id:             "output-unit",
+					Type:           proto.UnitType_OUTPUT,
+					ConfigStateIdx: 1,
+					State:          proto.State_STOPPED,
+					LogLevel:       proto.UnitLogLevel_INFO,
+				},
+				{
+					Id:             "input-unit-1",
+					Type:           proto.UnitType_INPUT,
+					ConfigStateIdx: 1,
+					State:          proto.State_STOPPED,
+					LogLevel:       proto.UnitLogLevel_DEBUG,
+				},
+				{
+					Id:             "input-unit-2",
+					Type:           proto.UnitType_INPUT,
+					ConfigStateIdx: 1,
+					State:          proto.State_STOPPED,
+					LogLevel:       proto.UnitLogLevel_INFO,
+				},
 			},
-			{
-				Id:             "input-unit-1",
-				Type:           proto.UnitType_INPUT,
-				ConfigStateIdx: 1,
-				State:          proto.State_HEALTHY,
-				LogLevel:       proto.UnitLogLevel_DEBUG,
-			},
-			{
-				Id:             "input-unit-2",
-				Type:           proto.UnitType_INPUT,
-				ConfigStateIdx: 1,
-				State:          proto.State_HEALTHY,
-				LogLevel:       proto.UnitLogLevel_INFO,
+			Features:    nil,
+			FeaturesIdx: 2,
+			Component: &proto.Component{
+				ApmConfig: &proto.APMConfig{Elastic: apmConfig},
 			},
 		},
 		{
-			{
-				Id:             "output-unit",
-				Type:           proto.UnitType_OUTPUT,
-				ConfigStateIdx: 1,
-				State:          proto.State_STOPPED,
-				LogLevel:       proto.UnitLogLevel_INFO,
-			},
-			{
-				Id:             "input-unit-1",
-				Type:           proto.UnitType_INPUT,
-				ConfigStateIdx: 1,
-				State:          proto.State_STOPPED,
-				LogLevel:       proto.UnitLogLevel_DEBUG,
-			},
-			{
-				Id:             "input-unit-2",
-				Type:           proto.UnitType_INPUT,
-				ConfigStateIdx: 1,
-				State:          proto.State_STOPPED,
-				LogLevel:       proto.UnitLogLevel_INFO,
-			},
+			AgentInfo:   agentInfo,
+			Units:       []*proto.UnitExpected{},
+			Features:    nil,
+			FeaturesIdx: 2,
 		},
-		{},
 	},
-		[]uint64{1, 2, 2, 2},
-		[]*proto.Features{
-			nil,
-			{Fqdn: &proto.FQDNFeature{Enabled: true}},
-			nil,
-			nil,
-		},
 		onObserved,
 		500*time.Millisecond,
 	)
