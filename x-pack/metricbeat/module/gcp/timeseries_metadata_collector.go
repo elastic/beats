669928095df551e5bb7ce7b00be4df664ea54641
Linu Elias <linu.elias@elastic.co>
2024-07-01 11:58:38 +0530

    gcp ommitted label fix

--- x-pack/metricbeat/module/gcp/timeseries_metadata_collector.go
+++ x-pack/metricbeat/module/gcp/timeseries_metadata_collector.go
@@ -100,6 +100,10 @@ func (s *StackdriverTimeSeriesMetadataCollector) Metadata(ctx context.Context, i
 		// common.Mapstr seems to not work as expected when deleting keys so I have to iterate over all results to add
 		// the ones I want
 		for k, v := range s.timeSeries.Resource.Labels {
+
+			if k == TimeSeriesResponsePathForECSInstanceID {
+				_, _ = ecs.Put(ECSCloudInstanceIDKey, v)
+			}
 			if k == TimeSeriesResponsePathForECSAvailabilityZone || k == TimeSeriesResponsePathForECSInstanceID || k == TimeSeriesResponsePathForECSAccountID {
 				continue
 			}
