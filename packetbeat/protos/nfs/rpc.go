eca92de6a6e820edca1ea6edcb85e6337a64615b
Nicholas Berlin <nicholas.berlin@elastic.co>
2024-07-11 15:58:54 -0400

    Add multifragment rpc support
    If nfs is being transported over TCP and an RPC message is split into multiple
    record fragments (very rare), Packetbeat will stop issuing callbacks until
    the rawData stream overflows and resets. Overflow occurs at 10MB of stream data.

--- packetbeat/protos/nfs/rpc.go
+++ packetbeat/protos/nfs/rpc.go
@@ -187,6 +187,47 @@ func getRPCConnection(private protos.ProtocolData) *rpcConnectionData {
 }
 
 // Parse function is used to process TCP payloads.
+//
+// From RFC 5531:
+//
+// 11. Record Marking Standard
+// When RPC messages are passed on top of a byte stream transport
+// protocol (like TCP), it is necessary to delimit one message from
+// another in order to detect and possibly recover from protocol errors.
+// This is called record marking (RM).  One RPC message fits into one RM
+// record.
+//
+// A record is composed of one or more record fragments.  A record
+// fragment is a four-byte header followed by 0 to (2**31) - 1 bytes of
+// fragment data.  The bytes encode an unsigned binary number; as with
+// XDR integers, the byte order is from highest to lowest.  The number
+// encodes two values -- a boolean that indicates whether the fragment
+// is the last fragment of the record (bit value 1 implies the fragment
+// is the last fragment) and a 31-bit unsigned binary value that is the
+// length in bytes of the fragment's data.  The boolean value is the
+// highest-order bit of the header; the length is the 31 low-order bits.
+// (Note that this record specification is NOT in XDR standard form!)
+//
+
+func parseFragment(rawData []byte) (partialMessage []byte, fragmentSize int, isLast bool) {
+	if len(rawData) < 4 {
+		debugf("Waiting for more data")
+		return nil, 0, false
+	}
+
+	header := uint32(binary.BigEndian.Uint32(rawData[0:4]))
+	fragmentSize = 4 + int(header&rpcSizeMask)
+	isLast = (header & rpcLastFrag) != 0
+
+	if len(rawData) < fragmentSize {
+		debugf("Waiting for more data")
+		return nil, 0, false
+	}
+
+	partialMessage = rawData[4:fragmentSize]
+	return partialMessage, fragmentSize, isLast
+}
+
 func (r *rpc) handleRPCFragment(
 	conn *rpcConnectionData,
 	pkt *protos.Packet,
@@ -209,28 +250,29 @@ func (r *rpc) handleRPCFragment(
 
 	for len(st.rawData) > 0 {
 
-		if len(st.rawData) < 4 {
-			debugf("Waiting for more data")
+		rpcMessage, totalSize, isLast := parseFragment(st.rawData)
+		if rpcMessage == nil {
 			break
 		}
 
-		marker := uint32(binary.BigEndian.Uint32(st.rawData[0:4]))
-		size := int(marker & rpcSizeMask)
-		islast := (marker & rpcLastFrag) != 0
+		for !isLast {
+			fragment, size, nextIsLast := parseFragment(st.rawData[totalSize:])
+			if fragment == nil {
+				break
+			}
 
-		if len(st.rawData)-4 < size {
-			debugf("Waiting for more data")
+			totalSize += size
+			isLast = nextIsLast
+			rpcMessage = append(rpcMessage, fragment...)
+		}
+
+		if !isLast {
 			break
 		}
 
-		if !islast {
-			logp.Warn("multifragment rpc message")
-			break
-		}
-
-		xdr := newXDR(st.rawData[4 : 4+size])
+		xdr := newXDR(rpcMessage)
 		// keep the rest of the next fragment
-		st.rawData = st.rawData[4+size:]
+		st.rawData = st.rawData[totalSize:]
 
 		r.handleRPCPacket(xdr, pkt.Ts, tcptuple, dir)
 	}
