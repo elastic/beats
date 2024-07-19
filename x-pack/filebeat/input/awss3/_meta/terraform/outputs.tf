7cb424d6b711e1a1006f2c728cfa646f0ebc12df
mjmbischoff <mjmbischoff@controplex.com>
2024-07-20 00:40:08 +0200

    reverting commenting out localstack variables, tbh localstack part should be separate terraform dir/setup but is out of scope of this PR.

--- x-pack/filebeat/input/awss3/_meta/terraform/outputs.tf
+++ x-pack/filebeat/input/awss3/_meta/terraform/outputs.tf
@@ -12,12 +12,12 @@ resource "local_file" "secrets" {
   file_permission = "0644"
 }
 
-# resource "local_file" "secrets-localstack" {
-#   content = yamlencode({
-#     "queue_url" : aws_sqs_queue.filebeat-integtest-localstack.url
-#     "aws_region" : aws_s3_bucket.filebeat-integtest-localstack.region
-#     "bucket_name" : aws_s3_bucket.filebeat-integtest-localstack.id
-#   })
-#   filename        = "${path.module}/outputs-localstack.yml"
-#   file_permission = "0644"
-# }
+resource "local_file" "secrets-localstack" {
+  content = yamlencode({
+    "queue_url" : aws_sqs_queue.filebeat-integtest-localstack.url
+    "aws_region" : aws_s3_bucket.filebeat-integtest-localstack.region
+    "bucket_name" : aws_s3_bucket.filebeat-integtest-localstack.id
+  })
+  filename        = "${path.module}/outputs-localstack.yml"
+  file_permission = "0644"
+}
