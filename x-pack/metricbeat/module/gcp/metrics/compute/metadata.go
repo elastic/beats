2701e3d59cd98fc2bcfc22e2184d506ca1bee241
Gabriel Pop <94497545+gpop63@users.noreply.github.com>
2024-09-03 13:00:14 +0300

    Update x-pack/metricbeat/module/gcp/metrics/compute/metadata.go
    Co-authored-by: subham sarkar <sarkar.subhams2@gmail.com>

--- x-pack/metricbeat/module/gcp/metrics/compute/metadata.go
+++ x-pack/metricbeat/module/gcp/metrics/compute/metadata.go
@@ -192,7 +192,7 @@ func (s *metadataCollector) getComputeInstances(ctx context.Context) {
 	start := time.Now()
 	defer func() {
 		totalTime := time.Since(start)
-		s.logger.Debugf("total time taken for compute AggregatedList request: %s", totalTime)
+		s.logger.Debugf("Total time taken for compute AggregatedList request: %s", totalTime)
 	}()
 
 	it := instancesClient.AggregatedList(ctx, &computepb.AggregatedListInstancesRequest{
