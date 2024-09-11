de0d2b29e107a12923c350306bdee611575a3a5a
Kush Rana <89848966+kush-elastic@users.noreply.github.com>
2024-09-11 09:19:50 +0530

    [Metricbeat][vSphere] Support for configurable IntervalId for performance API (#40678)
    * initial commit for intervalId supports for performance metrics
    
    * update docs and fix CI
    
    * Add changelog entry
    
    * fix CI
    
    * resolve review comments
    
    * fix loggers
    
    * resolved review comments
    
    * update versions
    
    * update UTs
    
    * update integration tests
    
    * 10s -> 20s
    
    * Update CHANGELOG.next.asciidoc
    
    Co-authored-by: Aman <38116245+devamanv@users.noreply.github.com>
    
    * Update metricbeat/docs/modules/vsphere.asciidoc
    
    Co-authored-by: Aman <38116245+devamanv@users.noreply.github.com>
    
    * make update
    
    * add recover for ToMetricSeries panic
    
    * return error instead just logging it.
    
    * remove restriction of interval IDs
    
    * remove unnecessary validations
    
    * remove recover and add empty condition
    
    * update changelog entry
    
    * Fix wrapping of errors in loggers
    
    * update data.json
    
    * update data.json
    
    * fix CI and loggers
    
    * update changelog entries
    
    * make update
    
    * fix changelog entries
    
    * update changelog entry
    
    ---------
    
    Co-authored-by: Aman <38116245+devamanv@users.noreply.github.com>
    (cherry picked from commit c75a7a4b3dab942e78fe4c8c48225811ad216b4b)

--- metricbeat/module/vsphere/datastore/datastore_test.go
+++ metricbeat/module/vsphere/datastore/datastore_test.go
@@ -19,6 +19,7 @@ package datastore
 
 import (
 	"testing"
+	"time"
 
 	mbtest "github.com/elastic/beats/v7/metricbeat/mb/testing"
 
@@ -105,5 +106,6 @@ func getConfig(ts *simulator.Server) map[string]interface{} {
 		"username":   "user",
 		"password":   "pass",
 		"insecure":   true,
+		"period":     time.Second * 20,
 	}
 }
