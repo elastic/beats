8050139b164c3e75a3a02c23a29895a368e94981
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2024-09-12 16:46:06 -0300

    add log if race is enabled for an unsupported platform

--- dev-tools/mage/gotest.go
+++ dev-tools/mage/gotest.go
@@ -276,14 +276,18 @@ func GoTest(ctx context.Context, params GoTestArgs) error {
 
 	if params.Race {
 		// Enable the race detector for supported platforms.
-		// This is an intersection of the supported platforms for beats and Go.
+		// This is an intersection of the supported platforms for Beats and Go.
 		//
 		// See https://go.dev/doc/articles/race_detector#Requirements.
-		raceAmd64 := os.Getenv("DEV_ARCH") == "amd64"
-		raceArm64 := os.Getenv("DEV_ARCH") == "arm64" &&
-			slices.Contains([]string{"linux", "darwin"}, os.Getenv("DEV_OS"))
+		devOS := os.Getenv("DEV_OS")
+		devArch := os.Getenv("DEV_ARCH")
+		raceAmd64 := devArch == "amd64"
+		raceArm64 := devArch == "arm64" &&
+			slices.Contains([]string{"linux", "darwin"}, devOS)
 		if raceAmd64 || raceArm64 {
 			testArgs = append(testArgs, "-race")
+		} else {
+			log.Printf("Warning: skipping -race flag for unsupported platform %s/%s\n", devOS, devArch)
 		}
 	}
 	if len(params.Tags) > 0 {
