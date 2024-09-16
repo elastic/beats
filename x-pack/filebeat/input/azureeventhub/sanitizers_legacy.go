15e813aab397acc151a96b0b84569f208d4d2c99
Maurizio Branca <maurizio.branca@gmail.com>
2024-09-17 01:53:00 +0200

    Address linter complaints

--- x-pack/filebeat/input/azureeventhub/sanitizers_legacy.go
+++ x-pack/filebeat/input/azureeventhub/sanitizers_legacy.go
@@ -16,7 +16,7 @@ import "errors"
 // - NEW_LINES: replaces new lines with spaces
 // - SINGLE_QUOTES: replaces single quotes with double quotes
 //
-// The legacy `sanitize_options` is deprecated and will be removed in the 
+// The legacy `sanitize_options` is deprecated and will be removed in the
 // 9.0 release.
 // Users should use the `sanitizers` configuration option instead.
 //
