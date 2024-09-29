2a707b4bebe9a6f49915f15014b650c2b7d80437
kruskal <99559985+kruskall@users.noreply.github.com>
2024-09-29 13:36:40 +0200

    test: update ThriftTest file

--- packetbeat/tests/files/ThriftTest.thrift
+++ packetbeat/tests/files/ThriftTest.thrift
@@ -22,24 +22,27 @@
  */
 
 namespace c_glib TTest
-namespace java thrift.test
 namespace cpp thrift.test
-namespace rb Thrift.Test
-namespace perl ThriftTest
-namespace csharp Thrift.Test
+namespace delphi Thrift.Test
+namespace go thrifttest
+namespace java thrift.test
 namespace js ThriftTest
-namespace st ThriftTest
+namespace lua ThriftTest
+namespace netstd ThriftTest
+namespace perl ThriftTest
+namespace php ThriftTest
 namespace py ThriftTest
 namespace py.twisted ThriftTest
-namespace go ThriftTest
-namespace php ThriftTest
-namespace delphi Thrift.Test
-namespace cocoa ThriftTest
+namespace rb Thrift.Test
+namespace st ThriftTest
+namespace xsd test (uri = 'http://thrift.apache.org/ns/ThriftTest')
 
 // Presence of namespaces and sub-namespaces for which there is
 // no generator should compile with warnings only
 namespace noexist ThriftTest
 namespace cpp.noexist ThriftTest
+
+namespace * thrift.test
 
 /**
  * Docstring!
@@ -54,7 +57,7 @@ enum Numberz
   EIGHT = 8
 }
 
-// const Numberz myNumberz = Numberz.ONE;
+const Numberz myNumberz = Numberz.ONE;
 // the following is expected to fail:
 // const Numberz urNumberz = ONE;
 
@@ -76,14 +79,14 @@ struct Bools {
 struct Xtruct
 {
   1:  string string_thing,
-  4:  byte   byte_thing,
+  4:  i8     byte_thing,
   9:  i32    i32_thing,
   11: i64    i64_thing
 }
 
 struct Xtruct2
 {
-  1: byte   byte_thing,
+  1: i8     byte_thing,  // used to be byte, hence the name
   2: Xtruct struct_thing,
   3: i32    i32_thing
 }
@@ -101,13 +104,23 @@ struct Insanity
 {
   1: map<Numberz, UserId> userMap,
   2: list<Xtruct> xtructs
-}
+} (python.immutable= "")
 
 struct CrazyNesting {
   1: string string_field,
   2: optional set<Insanity> set_field,
-  3: required list< map<set<i32>,map<i32,set<list<map<Insanity,string>>>>>> list_field,
+  // Do not insert line break as test/go/Makefile.am is removing this line with pattern match
+  3: required list<map<set<i32> (python.immutable = ""), map<i32,set<list<map<Insanity,string>(python.immutable = "")> (python.immutable = "")>>>> list_field,
   4: binary binary_field
+  5: uuid uuid_field
+}
+
+union SomeUnion {
+  1: map<Numberz, UserId> map_thing,
+  2: string string_thing,
+  3: i32 i32_thing,
+  4: Xtruct3 xtruct_thing,
+  5: Insanity insanity_thing
 }
 
 exception Xception {
@@ -132,90 +145,112 @@ service ThriftTest
    * Prints "testVoid()" and returns nothing.
    */
   void         testVoid(),
-  
+
   /**
    * Prints 'testString("%s")' with thing as '%s'
    * @param string thing - the string to print
    * @return string - returns the string 'thing'
    */
   string       testString(1: string thing),
-  
+
+  /**
+   * Prints 'testBool("%s")' where '%s' with thing as 'true' or 'false'
+   * @param bool  thing - the bool data to print
+   * @return bool  - returns the bool 'thing'
+   */
+  bool         testBool(1: bool thing),
+
   /**
    * Prints 'testByte("%d")' with thing as '%d'
-   * @param byte thing - the byte to print
-   * @return byte - returns the byte 'thing'
+   * The types i8 and byte are synonyms, use of i8 is encouraged, byte still exists for the sake of compatibility.
+   * @param byte thing - the i8/byte to print
+   * @return i8 - returns the i8/byte 'thing'
    */
-  byte         testByte(1: byte thing),
-  
+  i8           testByte(1: i8 thing),
+
   /**
    * Prints 'testI32("%d")' with thing as '%d'
    * @param i32 thing - the i32 to print
    * @return i32 - returns the i32 'thing'
    */
   i32          testI32(1: i32 thing),
- 
+
   /**
    * Prints 'testI64("%d")' with thing as '%d'
    * @param i64 thing - the i64 to print
    * @return i64 - returns the i64 'thing'
    */
   i64          testI64(1: i64 thing),
-  
+
   /**
    * Prints 'testDouble("%f")' with thing as '%f'
    * @param double thing - the double to print
    * @return double - returns the double 'thing'
    */
   double       testDouble(1: double thing),
-  
+
+  /**
+   * Prints 'testBinary("%s")' where '%s' is a hex-formatted string of thing's data
+   * @param binary  thing - the binary data to print
+   * @return binary  - returns the binary 'thing'
+   */
+  binary       testBinary(1: binary thing),
+
+  /**
+   * Prints 'testUuid("%s")' where '%s' is the uuid given. Note that the uuid byte order should be correct.
+   * @param uuid  thing - the uuid to print
+   * @return uuid  - returns the uuid 'thing'
+   */
+  uuid       testUuid(1: uuid thing),
+
   /**
    * Prints 'testStruct("{%s}")' where thing has been formatted into a string of comma separated values
    * @param Xtruct thing - the Xtruct to print
    * @return Xtruct - returns the Xtruct 'thing'
    */
   Xtruct       testStruct(1: Xtruct thing),
-  
+
   /**
    * Prints 'testNest("{%s}")' where thing has been formatted into a string of the nested struct
    * @param Xtruct2 thing - the Xtruct2 to print
    * @return Xtruct2 - returns the Xtruct2 'thing'
    */
   Xtruct2      testNest(1: Xtruct2 thing),
- 
+
   /**
-   * Prints 'testMap("{%s")' where thing has been formatted into a string of  'key => value' pairs
+   * Prints 'testMap("{%s")' where thing has been formatted into a string of 'key => value' pairs
    *  separated by commas and new lines
    * @param map<i32,i32> thing - the map<i32,i32> to print
    * @return map<i32,i32> - returns the map<i32,i32> 'thing'
    */
   map<i32,i32> testMap(1: map<i32,i32> thing),
-  
+
   /**
-   * Prints 'testStringMap("{%s}")' where thing has been formatted into a string of  'key => value' pairs
+   * Prints 'testStringMap("{%s}")' where thing has been formatted into a string of 'key => value' pairs
    *  separated by commas and new lines
    * @param map<string,string> thing - the map<string,string> to print
    * @return map<string,string> - returns the map<string,string> 'thing'
    */
   map<string,string> testStringMap(1: map<string,string> thing),
-  
+
   /**
-   * Prints 'testSet("{%s}")' where thing has been formatted into a string of  values
+   * Prints 'testSet("{%s}")' where thing has been formatted into a string of values
    *  separated by commas and new lines
    * @param set<i32> thing - the set<i32> to print
    * @return set<i32> - returns the set<i32> 'thing'
    */
   set<i32>     testSet(1: set<i32> thing),
-  
+
   /**
-   * Prints 'testList("{%s}")' where thing has been formatted into a string of  values
+   * Prints 'testList("{%s}")' where thing has been formatted into a string of values
    *  separated by commas and new lines
    * @param list<i32> thing - the list<i32> to print
    * @return list<i32> - returns the list<i32> 'thing'
    */
   list<i32>    testList(1: list<i32> thing),
-  
+
   /**
-   * Prints 'testEnum("%d")' where thing has been formatted into it's numeric value
+   * Prints 'testEnum("%d")' where thing has been formatted into its numeric value
    * @param Numberz thing - the Numberz to print
    * @return Numberz - returns the Numberz 'thing'
    */
@@ -237,45 +272,45 @@ service ThriftTest
   map<i32,map<i32,i32>> testMapMap(1: i32 hello),
 
   /**
-   * So you think you've got this all worked, out eh?
+   * So you think you've got this all worked out, eh?
    *
-   * Creates a the returned map with these values and prints it out:
-   *   { 1 => { 2 => argument, 
-   *            3 => argument, 
+   * Creates a map with these values and prints it out:
+   *   { 1 => { 2 => argument,
+   *            3 => argument,
    *          },
    *     2 => { 6 => <empty Insanity struct>, },
    *   }
-   * @return map<UserId, map<Numberz,Insanity>> - a map with the above values 
+   * @return map<UserId, map<Numberz,Insanity>> - a map with the above values
    */
   map<UserId, map<Numberz,Insanity>> testInsanity(1: Insanity argument),
 
   /**
    * Prints 'testMulti()'
-   * @param byte arg0 - 
-   * @param i32 arg1 - 
-   * @param i64 arg2 - 
-   * @param map<i16, string> arg3 - 
-   * @param Numberz arg4 - 
-   * @param UserId arg5 - 
+   * @param i8 arg0 -
+   * @param i32 arg1 -
+   * @param i64 arg2 -
+   * @param map<i16, string> arg3 -
+   * @param Numberz arg4 -
+   * @param UserId arg5 -
    * @return Xtruct - returns an Xtruct with string_thing = "Hello2, byte_thing = arg0, i32_thing = arg1
    *    and i64_thing = arg2
    */
-  Xtruct testMulti(1: byte arg0, 2: i32 arg1, 3: i64 arg2, 4: map<i16, string> arg3, 5: Numberz arg4, 6: UserId arg5),
+  Xtruct testMulti(1: i8 arg0, 2: i32 arg1, 3: i64 arg2, 4: map<i16, string> arg3, 5: Numberz arg4, 6: UserId arg5),
 
   /**
    * Print 'testException(%s)' with arg as '%s'
    * @param string arg - a string indication what type of exception to throw
    * if arg == "Xception" throw Xception with errorCode = 1001 and message = arg
-   * elsen if arg == "TException" throw TException
+   * else if arg == "TException" throw TException
    * else do not throw anything
    */
   void testException(1: string arg) throws(1: Xception err1),
 
   /**
    * Print 'testMultiException(%s, %s)' with arg0 as '%s' and arg1 as '%s'
-   * @param string arg - a string indication what type of exception to throw
+   * @param string arg - a string indicating what type of exception to throw
    * if arg0 == "Xception" throw Xception with errorCode = 1001 and message = "This is an Xception"
-   * elsen if arg0 == "Xception2" throw Xception2 with errorCode = 2002 and message = "This is an Xception2"
+   * else if arg0 == "Xception2" throw Xception2 with errorCode = 2002 and struct_thing.string_thing = "This is an Xception2"
    * else do not throw anything
    * @return Xtruct - an Xtruct with string_thing = arg1
    */
@@ -292,7 +327,12 @@ service ThriftTest
 
 service SecondService
 {
-  void blahBlah()
+  /**
+   * Prints 'testString("%s")' with thing as '%s'
+   * @param string thing - the string to print
+   * @return string - returns the string 'thing'
+   */
+  string secondtestString(1: string thing)
 }
 
 struct VersioningTestV1 {
@@ -305,7 +345,7 @@ struct VersioningTestV2 {
        1: i32 begin_in_both,
 
        2: i32 newint,
-       3: byte newbyte,
+       3: i8 newbyte,
        4: i16 newshort,
        5: i64 newlong,
        6: double newdouble
@@ -374,4 +414,13 @@ struct StructA {
 struct StructB {
   1: optional StructA aa;
   2: required StructA ab;
+}
+
+struct OptionalSetDefaultTest {
+  1: optional set<string> with_default = [ "test" ]
+}
+
+struct OptionalBinary {
+  1: optional set<binary> bin_set = {}
+  2: optional map<binary,i32> bin_map = {}
 }
