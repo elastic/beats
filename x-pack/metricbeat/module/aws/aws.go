e2376ea5e29b64dffaba0fce0dc1f40a6aa84f52
Andreas Gkizas <andreas.gkizas@elastic.co>
2024-10-03 09:07:15 +0300

    setting to max limit to 500

--- x-pack/metricbeat/module/aws/aws.go
+++ x-pack/metricbeat/module/aws/aws.go
@@ -212,7 +212,7 @@ func NewMetricSet(base mb.BaseMetricSet) (*MetricSet, error) {
 	//The Limit variable defines maximum number of returned results per page. The default value is 25 and the maximum value is 500.
 	if config.LimitRestAPI != nil {
 		if *config.LimitRestAPI > 500 {
-			base.Logger().Debug("apigateway_max_results config value can not exceed value 500. Setting apigateway_max_results=25")
+			base.Logger().Debug("apigateway_max_results config value can not exceed value 500. Setting apigateway_max_results=500")
 			*config.LimitRestAPI = 500
 		} else if *config.LimitRestAPI <= 0 {
 			base.Logger().Debug("apigateway_max_results config value can not be <=0. Setting apigateway_max_results=25")
