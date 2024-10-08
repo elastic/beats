3d787777458ced638f1619323fddadaa2d3907da
Tiago Queiroz <tiago.queiroz@elastic.co>
2024-10-08 15:42:46 -0400

    Choose between journald and log input based on flag, update module
    The module configuration template has been updated to use the new
    input, the new input can select journald or traditional log files
    based on the flags `use_journald` or `use_files`.

--- filebeat/input/systemlogs/input.go
+++ filebeat/input/systemlogs/input.go
@@ -28,7 +28,7 @@ import (
 	v2 "github.com/elastic/beats/v7/filebeat/input/v2"
 	cursor "github.com/elastic/beats/v7/filebeat/input/v2/input-cursor"
 	"github.com/elastic/beats/v7/libbeat/feature"
-	"github.com/elastic/elastic-agent-libs/config"
+	conf "github.com/elastic/elastic-agent-libs/config"
 	"github.com/elastic/elastic-agent-libs/logp"
 )
 
@@ -41,21 +41,38 @@ func init() {
 	}
 }
 
+type config struct {
+	UseJournald bool    `config:"use_journald"`
+	UseFiles    bool    `config:"use_files"`
+	Files       *conf.C `config:"files" yaml:"files"`
+	Journald    *conf.C `config:"journald" yaml:"journald"`
+}
+
+func (c *config) Validate() error {
+	if c.UseFiles && c.UseJournald {
+		return errors.New("'use_journald' and 'use_files' cannot both be true")
+	}
+
+	if c.Files == nil && c.Journald == nil {
+		return errors.New("one of 'journald' or 'files' must be set")
+	}
+
+	return nil
+}
+
 // newV1Input creates a new log input
 func newV1Input(
-	cfg *config.C,
+	cfg *conf.C,
 	outlet channel.Connector,
 	context v1.Context,
 ) (v1.Input, error) {
-	useLogs, err := cfg.Bool("use_logs", -1)
+
+	useJournald, cfg, err := decide(cfg)
 	if err != nil {
-		return nil, fmt.Errorf("cannot parse 'use_logs': %w", err)
+		return nil, fmt.Errorf("cannot decide between journald and files: %w", err)
 	}
 
-	if useLogs {
-		if err := cfg.SetString("type", -1, "log"); err != nil {
-			return nil, fmt.Errorf("cannot set 'type': %w", err)
-		}
+	if !useJournald {
 		inp, err := loginput.NewInput(cfg, outlet, context)
 		if err != nil {
 			return nil, fmt.Errorf("cannot create log input: %w", err)
@@ -88,18 +105,66 @@ func PluginV2(logger *logp.Logger, store cursor.StateStore) v2.Plugin {
 
 // configure checks whether the journald input must be created and
 // delegates to journald.Configure if needed.
-func configure(cfg *config.C) ([]cursor.Source, cursor.Input, error) {
-	useJournald, err := cfg.Bool("use_journald", -1)
+func configure(cfg *conf.C) ([]cursor.Source, cursor.Input, error) {
+	useJournald, cfg, err := decide(cfg)
 	if err != nil {
-		return nil, nil, fmt.Errorf("cannot parse 'use_journald': %w", err)
+		return nil, nil, fmt.Errorf("cannot decide between journald and files: %w", err)
 	}
 
 	if useJournald {
-		if err := cfg.SetString("type", -1, "journald"); err != nil {
-			return nil, nil, fmt.Errorf("cannot set 'type': %w", err)
-		}
 		return journald.Configure(cfg)
 	}
 
-	return nil, nil, errors.New("cannot initialise system logs")
+	return nil, nil, errors.New("cannot initialise system-logs with journald input")
+}
+
+// decide returns:
+//   - use Jounrald (input V2)
+//   - the new config
+//   - error, if any
+func decide(c *conf.C) (bool, *conf.C, error) {
+	cfg := config{}
+	if err := c.Unpack(&cfg); err != nil {
+		return false, nil, err
+	}
+
+	if cfg.UseJournald {
+		cfg, err := toJournaldConfig(c)
+		return true, cfg, err
+	}
+
+	if cfg.UseFiles {
+		cfg, err := toFilesConfig(c)
+		return false, cfg, err
+	}
+
+	// TODO: implement checking the files
+
+	return false, nil, errors.New("[WIP] either set use_journald or use_files")
+}
+
+func toJournaldConfig(cfg *conf.C) (*conf.C, error) {
+	newCfg, err := cfg.Child("journald", -1)
+	if err != nil {
+		return nil, fmt.Errorf("cannot extract 'journald' block: %w", err)
+	}
+
+	if err := newCfg.SetString("type", -1, "journald"); err != nil {
+		return nil, fmt.Errorf("cannot set 'type': %w", err)
+	}
+
+	return newCfg, nil
+}
+
+func toFilesConfig(cfg *conf.C) (*conf.C, error) {
+	newCfg, err := cfg.Child("files", -1)
+	if err != nil {
+		return nil, fmt.Errorf("cannot extract 'journald' block: %w", err)
+	}
+
+	if err := newCfg.SetString("type", -1, "log"); err != nil {
+		return nil, fmt.Errorf("cannot set 'type': %w", err)
+	}
+
+	return newCfg, nil
 }
