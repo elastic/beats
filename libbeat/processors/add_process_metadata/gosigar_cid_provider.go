7830a44eac288162dc55be068b4084d9cf44bdd5
Alex K. <8418476+fearful-symmetry@users.noreply.github.com>
2024-10-10 10:39:41 -0700

    Add tests, fallback properly if we can't init the cgroups (#41189)
    * add tests, fallback properly if we can't init the cgroups
    
    * linter
    
    * fix test

--- libbeat/processors/add_process_metadata/gosigar_cid_provider.go
+++ libbeat/processors/add_process_metadata/gosigar_cid_provider.go
@@ -18,9 +18,7 @@
 package add_process_metadata
 
 import (
-	"errors"
 	"fmt"
-	"io/fs"
 	"path/filepath"
 	"regexp"
 	"strings"
@@ -82,15 +80,12 @@ func newCidProvider(cgroupPrefixes []string, cgroupRegex *regexp.Regexp, process
 // getProcessCgroups returns a mapping of cgroup subsystem name to path. It
 // returns an error if it failed to retrieve the cgroup info.
 func (p gosigarCidProvider) getProcessCgroups(pid int) (cgroup.PathList, error) {
+	//return nil if we aren't supporting cgroups
+	if p.processCgroupPaths == nil {
+		return cgroup.PathList{}, nil
+	}
 	pathList, err := p.processCgroupPaths.ProcessCgroupPaths(pid)
 	if err != nil {
-		var pathError *fs.PathError
-		if errors.As(err, &pathError) {
-			// do no thing when err is nil or when os.PathError happens because the process don't exist,
-			// or not running in linux system
-			return cgroup.PathList{}, nil
-		}
-		// should never happen
 		return cgroup.PathList{}, fmt.Errorf("failed to read cgroups for pid=%v: %w", pid, err)
 	}
 
