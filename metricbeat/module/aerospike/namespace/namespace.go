b46dd41678ba7582fb447e0a5d62f66ecb5af94c
Mirko Bez <mirko.bez@elastic.co>
2024-10-14 20:24:01 +0200

    Add property InfoPolicy to the Metricset

--- metricbeat/module/aerospike/namespace/namespace.go
+++ metricbeat/module/aerospike/namespace/namespace.go
@@ -45,6 +45,7 @@ type MetricSet struct {
 	host         *as.Host
 	clientPolicy *as.ClientPolicy
 	client       *as.Client
+	infoPolicy   *as.InfoPolicy
 }
 
 // New create a new instance of the MetricSet
@@ -70,6 +71,7 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 		BaseMetricSet: base,
 		host:          host,
 		clientPolicy:  clientPolicy,
+		infoPolicy:    as.NewInfoPolicy(),
 	}, nil
 }
 
@@ -82,14 +84,14 @@ func (m *MetricSet) Fetch(reporter mb.ReporterV2) error {
 	}
 
 	for _, node := range m.client.GetNodes() {
-		info, err := node.RequestInfo(as.NewInfoPolicy(), "namespaces")
+		info, err := node.RequestInfo(m.infoPolicy, "namespaces")
 		if err != nil {
 			m.Logger().Error("Failed to retrieve namespaces from node %s", node.GetName())
 			continue
 		}
 
 		for _, namespace := range strings.Split(info["namespaces"], ";") {
-			info, err := node.RequestInfo(as.NewInfoPolicy(), "namespace/"+namespace)
+			info, err := node.RequestInfo(m.infoPolicy, "namespace/"+namespace)
 			if err != nil {
 				m.Logger().Error("Failed to retrieve metrics for namespace %s from node %s", namespace, node.GetName())
 				continue
