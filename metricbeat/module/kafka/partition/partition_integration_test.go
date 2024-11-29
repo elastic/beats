22766faabc9be454397f8fac59b4af774d294778
Khushi Jain <khushi.jain@elastic.co>
2024-11-29 15:17:15 +0530

    Update Beats to the current Sarama fork (#41655)
    - Update Beats to the current sarama fork
    
    (cherry picked from commit 4942592a8a272b5355c0be531e5f052b2c7a1d56)
    
    # Conflicts:
    #	go.sum
    #	metricbeat/module/kafka/consumergroup/consumergroup_integration_test.go
    #	metricbeat/module/kafka/partition/partition_integration_test.go

--- metricbeat/module/kafka/partition/partition_integration_test.go
+++ metricbeat/module/kafka/partition/partition_integration_test.go
@@ -26,8 +26,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/Shopify/sarama"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/elastic/sarama"
 
 	"github.com/elastic/beats/v7/libbeat/tests/compose"
 	mbtest "github.com/elastic/beats/v7/metricbeat/mb/testing"
@@ -105,13 +106,21 @@ func TestTopic(t *testing.T) {
 
 	// Its possible that other topics exists -> select the right data
 	for _, data := range dataBefore {
+<<<<<<< HEAD
 		if data.ModuleFields["topic"].(mapstr.M)["name"] == testTopic { //nolint:errcheck // it's fine for a test
+=======
+		if data.ModuleFields["topic"].(mapstr.M)["name"] == testTopic {
+>>>>>>> 4942592a8 (Update Beats to the current Sarama fork (#41655))
 			offsetBefore, _ = data.MetricSetFields["offset"].(mapstr.M)["newest"].(int64)
 		}
 	}
 
 	for _, data := range dataAfter {
+<<<<<<< HEAD
 		if data.ModuleFields["topic"].(mapstr.M)["name"] == testTopic { //nolint:errcheck // it's fine for a test
+=======
+		if data.ModuleFields["topic"].(mapstr.M)["name"] == testTopic {
+>>>>>>> 4942592a8 (Update Beats to the current Sarama fork (#41655))
 			offsetAfter, _ = data.MetricSetFields["offset"].(mapstr.M)["newest"].(int64)
 		}
 	}
