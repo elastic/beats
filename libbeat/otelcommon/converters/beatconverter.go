9583762130755b577ae9172bb030618a02fc9385
Khushi Jain <khushi.jain@elastic.co>
2024-12-03 12:54:59 +0530

    chore

--- libbeat/otelcommon/converters/beatconverter.go
+++ libbeat/otelcommon/converters/beatconverter.go
@@ -1,3 +1,20 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package converters
 
 import (
@@ -26,10 +43,8 @@ func newConverter(set confmap.ConverterSettings) confmap.Converter {
 	}
 }
 
-// converts beats output config to OTel config ß
+// converts beats output config to OTel config
 func (c converter) Convert(_ context.Context, conf *confmap.Conf) error {
-	// var err error
-	fmt.Println(conf.ToStringMap())
 	var out map[string]any
 	receiverCfg, _ := conf.Sub("receivers::filebeatreceiver")
 	outputs, _ := receiverCfg.Sub("output")
@@ -50,8 +65,9 @@ func (c converter) Convert(_ context.Context, conf *confmap.Conf) error {
 			}
 			conf.Merge(confmap.NewFromStringMap(out))
 		case "kafka":
-			return fmt.Errorf("unsupported output")
+			return fmt.Errorf("%s is currently unsupported in otel mode", key)
 		case "default":
+			return fmt.Errorf("%s is unsupported output", key)
 		}
 	}
 
