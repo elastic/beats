cc9b79a7a01242d9c9a80dfed90e14ba3ffa8866
Marc Guasch <marc.guasch@elastic.co>
2024-12-27 04:42:51 -0800

    Add retry if needed in pre command hook

--- .buildkite/scripts/install_macos_tools.sh
+++ .buildkite/scripts/install_macos_tools.sh
@@ -8,24 +8,7 @@ PLATFORM_TYPE_LOWERCASE=$(uname | tr '[:upper:]' '[:lower:]')
 
 export BIN=${WORKSPACE:-$PWD}/bin
 
-retry() {
-  local retries=$1
-  shift
-  local count=0
-  until "$@"; do
-    exit=$?
-    wait=$((2 ** count))
-    count=$((count + 1))
-    if [ $count -lt "$retries" ]; then
-      >&2 echo "Retry $count/$retries exited $exit, retrying in $wait seconds..."
-      sleep $wait
-    else
-      >&2 echo "Retry $count/$retries exited $exit, no more retries left."
-      return $exit
-    fi
-  done
-  return 0
-}
+source ./retry.sh
 
 create_workspace() {
   if [[ ! -d "${BIN}" ]]; then
