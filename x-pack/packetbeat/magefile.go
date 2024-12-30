dca136d8d80900649202f2e94b10c69f2b962b4a
Marc Guasch <marc.guasch@elastic.co>
2024-12-30 02:14:20 -0800

    Revert "Do not skip npcap installation for system tests"
    This reverts commit 90a8265adf0430f13529a82b38a85a782920b93c.

--- x-pack/packetbeat/magefile.go
+++ x-pack/packetbeat/magefile.go
@@ -137,7 +137,12 @@ func TestPackages() error {
 }
 
 func SystemTest(ctx context.Context) error {
-	mg.SerialDeps(xpacketbeat.GetNpcapInstaller, devtools.BuildSystemTestBinary)
+	// Buildkite (CI) images have preinstalled npcap
+	if os.Getenv("CI") == "true" {
+		mg.SerialDeps(devtools.BuildSystemTestBinary)
+	} else {
+		mg.SerialDeps(xpacketbeat.GetNpcapInstaller, devtools.BuildSystemTestBinary)
+	}
 
 	args := devtools.DefaultGoTestIntegrationArgs()
 	args.Packages = []string{"./tests/system/..."}
