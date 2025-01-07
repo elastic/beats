44c25ce8773b6cc06ba186d041f55e79db9fb8cf
Tiago Queiroz <tiago.queiroz@elastic.co>
2025-01-07 17:55:45 -0500

    Add a test for an "emtpy" registry
    This commit adds a test for when Filebat's registry is empty, that's
    done by using the benchmark input.
    
    The benchmark input is fixed so it can run under Elastic-Agent.

--- x-pack/filebeat/tests/integration/registrydiagnostics_test.go
+++ x-pack/filebeat/tests/integration/registrydiagnostics_test.go
@@ -27,7 +27,7 @@ import (
 	"github.com/elastic/elastic-agent-client/v7/pkg/proto"
 )
 
-func TestRegistryIsInDiagnostics(t *testing.T) {
+func TestFilestreamRegistryIsInDiagnostics(t *testing.T) {
 	filebeat := NewFilebeat(t)
 	logfile := filepath.Join(filebeat.TempDir(), "log.log")
 	integration.GenerateLogFile(t, logfile, 2, false)
@@ -93,63 +93,98 @@ func TestRegistryIsInDiagnostics(t *testing.T) {
 		SentActions: map[string]*mock.PerformAction{},
 	}
 
-	// requirerequestDiagnostics runs on a different goroutine, we cannot call t.Fatal
-	requestDiagnostics := func() {
-		assert.Eventuallyf(t, func() bool {
-			return filebeat.CountFileLines(outputGlob) == 2
-		},
-			1*time.Minute,
-			100*time.Millisecond,
-			"output file '%s' does not contain two events", outputGlob)
-
-		// Once we're done, set it back to false
-		defer waitingForDiagnostics.Store(false)
-		server.ActionsChan <- &mock.PerformAction{
-			Type:  proto.ActionRequest_DIAGNOSTICS,
-			Name:  "diagnostics",
-			Level: proto.ActionRequest_COMPONENT, // aka diagnostics for the whole Beat
-			DiagCallback: func(diagResults []*proto.ActionDiagnosticUnitResult, diagErr error) {
-				if diagErr != nil {
-					t.Errorf("diagnostics failed: %s", diagErr)
-				}
-
-				for _, dr := range diagResults {
-					if dr.Name != "registry" {
-						continue
-					}
-
-					if len(dr.Content) == 0 {
-						t.Errorf("registry cannot be an empty file")
-						return
-					}
-
-					gzipReader, err := gzip.NewReader(bytes.NewReader(dr.Content))
-					if err != nil {
-						t.Errorf("cannot create gzip reader: '%s'", err)
-						return
-					}
-					defer gzipReader.Close()
-
-					tarReader := tar.NewReader(gzipReader)
-					for {
-						header, err := tarReader.Next()
-						if errors.Is(err, io.EOF) {
-							t.Error("registry log file not found in tar archive")
-							return
-						}
-
-						if header.Name != "registry/filebeat/log.json" {
-							continue
-						}
-
-						validateLastRegistryEntry(t, tarReader, 100, logfile)
-						testDone <- struct{}{}
-						return
-					}
-				}
-				t.Error("diagnostics do not contain a valid registry")
-			},
+	server.CheckinV2Impl = func(observed *proto.CheckinObserved) *proto.CheckinExpected {
+		// No matter the current state, we always return the same units
+		checkinExpected := proto.CheckinExpected{
+			Units: units,
 		}
+
+		// If any unit is not healthy, just return the expected state
+		for _, unit := range observed.Units {
+			if unit.GetState() != proto.State_HEALTHY {
+				return &checkinExpected
+			}
+		}
+
+		// All units are healthy, we can request the diagnostics.
+		// Ensure we don't have any diagnostics being requested already.
+		if waitingForDiagnostics.CompareAndSwap(false, true) {
+			// Request the diagnostics asynchronously
+			go requestDiagnosticsAndVerifyRegistry(t, filebeat, outputGlob, logfile, 100, &waitingForDiagnostics, server, testDone, false)
+		}
+		return &checkinExpected
+	}
+
+	if err := server.Start(); err != nil {
+		t.Fatalf("cannot start gRPC server: %s", err)
+	}
+
+	filebeat.Start(
+		"-E", fmt.Sprintf(`management.insecure_grpc_url_for_testing="localhost:%d"`, server.Port),
+		"-E", "management.enabled=true",
+		"-E", "queue.mem.flush.timeout=0",
+	)
+
+	<-testDone
+}
+
+func TestEmptyegistryIsInDiagnostics(t *testing.T) {
+	filebeat := NewFilebeat(t)
+	input := proto.UnitExpected{
+		Id:             "input-" + t.Name(),
+		Type:           proto.UnitType_INPUT,
+		ConfigStateIdx: 1,
+		State:          proto.State_HEALTHY,
+		LogLevel:       proto.UnitLogLevel_DEBUG,
+		Config: &proto.UnitExpectedConfig{
+			Id:   "unit-filestream-" + t.Name(),
+			Type: "benchmark",
+			Name: "Benchmark-" + t.Name(),
+			Streams: []*proto.Stream{
+				{
+					Id: "stream-benchmark-" + t.Name(),
+					Source: integration.RequireNewStruct(t, map[string]interface{}{
+						"id":      "stream-benchmark-" + t.Name(),
+						"enabled": true,
+						"count":   2,
+					}),
+				},
+			},
+		},
+	}
+
+	output := proto.UnitExpected{
+		Id:             "unit-output-" + t.Name(),
+		Type:           proto.UnitType_OUTPUT,
+		ConfigStateIdx: 1,
+		State:          proto.State_HEALTHY,
+		LogLevel:       proto.UnitLogLevel_DEBUG,
+		Config: &proto.UnitExpectedConfig{
+			Id:   "output-" + t.Name(),
+			Type: "file",
+			Name: "file",
+			Source: integration.RequireNewStruct(t,
+				map[string]interface{}{
+					"type":     "file",
+					"path":     filebeat.TempDir(),
+					"filename": "output",
+				}),
+		},
+	}
+	outputGlob := filepath.Join(filebeat.TempDir(), "output*")
+
+	var units = []*proto.UnitExpected{
+		&output,
+		&input,
+	}
+
+	waitingForDiagnostics := atomic.Bool{}
+	testDone := make(chan struct{})
+
+	server := &mock.StubServerV2{
+		ActionImpl:  func(response *proto.ActionResponse) error { return nil },
+		ActionsChan: make(chan *mock.PerformAction),
+		SentActions: map[string]*mock.PerformAction{},
 	}
 
 	server.CheckinV2Impl = func(observed *proto.CheckinObserved) *proto.CheckinExpected {
@@ -169,7 +204,7 @@ func TestRegistryIsInDiagnostics(t *testing.T) {
 		// Ensure we don't have any diagnostics being requested already.
 		if waitingForDiagnostics.CompareAndSwap(false, true) {
 			// Request the diagnostics asynchronously
-			go requestDiagnostics()
+			go requestDiagnosticsAndVerifyRegistry(t, filebeat, outputGlob, "", 0, &waitingForDiagnostics, server, testDone, true)
 		}
 		return &checkinExpected
 	}
@@ -224,5 +259,86 @@ func validateLastRegistryEntry(t *testing.T, reader io.Reader, expectedSize int,
 			"expecting offset to be %d, got %d instead",
 			expectedSize,
 			entry.Data.Cursor.Offset)
+	}
+}
+
+// requestDiagnosticsAndVerifyRegistry runs on a different goroutine, we cannot call t.Fatal
+func requestDiagnosticsAndVerifyRegistry(
+	t *testing.T,
+	filebeat *integration.BeatProc,
+	outputGlob,
+	logfile string,
+	logfileOffset int,
+	waitingForDiagnostics *atomic.Bool,
+	server *mock.StubServerV2,
+	testDone chan<- struct{},
+	emtpyRegistyLogFile bool) {
+
+	assert.Eventuallyf(t, func() bool {
+		return filebeat.CountFileLines(outputGlob) == 2
+	},
+		1*time.Minute,
+		100*time.Millisecond,
+		"output file '%s' does not contain two events", outputGlob)
+
+	// Once we're done, set it back to false
+	defer waitingForDiagnostics.Store(false)
+	server.ActionsChan <- &mock.PerformAction{
+		Type:  proto.ActionRequest_DIAGNOSTICS,
+		Name:  "diagnostics",
+		Level: proto.ActionRequest_COMPONENT, // aka diagnostics for the whole Beat
+		DiagCallback: func(diagResults []*proto.ActionDiagnosticUnitResult, diagErr error) {
+			// Let the test finish when this callback finishes
+			defer func() {
+				testDone <- struct{}{}
+			}()
+
+			if diagErr != nil {
+				t.Errorf("diagnostics failed: %s", diagErr)
+				return
+			}
+
+			for _, dr := range diagResults {
+				if dr.Name != "registry" {
+					continue
+				}
+
+				if len(dr.Content) == 0 {
+					t.Errorf("registry cannot be an empty file")
+					return
+				}
+
+				gzipReader, err := gzip.NewReader(bytes.NewReader(dr.Content))
+				if err != nil {
+					t.Errorf("cannot create gzip reader: '%s'", err)
+					return
+				}
+				defer gzipReader.Close()
+
+				tarReader := tar.NewReader(gzipReader)
+				for {
+					header, err := tarReader.Next()
+					if errors.Is(err, io.EOF) {
+						t.Error("registry log file not found in tar archive")
+						return
+					}
+
+					if header.Name != "registry/filebeat/log.json" {
+						continue
+					}
+
+					if emtpyRegistyLogFile {
+						if header.Size != 0 {
+							t.Errorf("expecting registry log file to be empty, got %d bytes instead", header.Size)
+						}
+						return
+					}
+
+					validateLastRegistryEntry(t, tarReader, logfileOffset, logfile)
+					return
+				}
+			}
+			t.Error("diagnostics do not contain a valid registry")
+		},
 	}
 }
