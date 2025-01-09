42410cf48d186ac6073ab00baa22fe08df2731cc
Christiano Haesbaert <haesbaert@elastic.co>
2025-01-09 16:17:21 +0100

    Add a test for the actual metric set, more to come

--- x-pack/auditbeat/module/system/process/quark_provider_linux_test.go
+++ x-pack/auditbeat/module/system/process/quark_provider_linux_test.go
@@ -12,6 +12,12 @@ import (
 	"testing"
 	"time"
 
+	"github.com/elastic/beats/v7/auditbeat/ab"
+	"github.com/elastic/beats/v7/auditbeat/helper/tty"
+	"github.com/elastic/beats/v7/libbeat/common/capabilities"
+	"github.com/elastic/beats/v7/metricbeat/mb"
+	mbtest "github.com/elastic/beats/v7/metricbeat/mb/testing"
+	"github.com/elastic/elastic-agent-libs/mapstr"
 	quark "github.com/elastic/go-quark"
 	"github.com/stretchr/testify/require"
 )
@@ -66,6 +72,30 @@ func TestForkExecExit(t *testing.T) {
 	require.True(t, qev.Process.Exit.Valid)
 	require.Zero(t, qev.Process.Exit.ExitCode)
 	// Don't care about ExitTime, it's also not precise
+}
+
+func TestQuarkMetricSet(t *testing.T) {
+	config := getConfig()
+	config["process.backend"] = "kernel_tracing"
+
+	f := mbtest.NewPushMetricSetV2WithRegistry(t, config, ab.Registry)
+	ms, ok := f.(*QuarkMetricSet)
+	require.True(t, ok)
+
+	events := mbtest.RunPushMetricSetV2(1*time.Second, 0, ms)
+	require.NotEmpty(t, events)
+
+	// target is an mb.Event that we build externally
+	target := makeSelfEvent(t)
+
+	// What we actually got from beats
+	actual := firstEventOfPid(t, events, os.Getpid())
+
+	for tk, tv := range target.RootFields {
+		av, err := actual.RootFields.GetValue(tk)
+		require.NoError(t, err)
+		require.Equal(t, tv, av)
+	}
 }
 
 func openQueue(t *testing.T) *quark.Queue {
@@ -146,14 +176,77 @@ func drainFirstOfPid(t *testing.T, qq *quark.Queue, pid int) quark.Event {
 	return quark.Event{} // NOTREACHED
 }
 
-// func filterPid(qevs []quark.Event, pid int) []quark.Event {
-// 	var pidQevs []quark.Event
+// firstEventOfPid looks up the first event of `pid` in `events`
+func firstEventOfPid(t *testing.T, events []mb.Event, pid int) mb.Event {
+	for _, event := range events {
+		pid, err := event.RootFields.GetValue("process.pid")
+		require.NoError(t, err)
+		if pid.(uint32) == uint32(os.Getpid()) {
+			return event
+		}
+	}
 
-// 	for _, qev := range qevs {
-// 		if qev.Process.Pid == uint32(pid) {
-// 			pidQevs = append(pidQevs, qev)
-// 		}
-// 	}
+	t.Fatalf("Can't find event of pid %d", pid)
 
-// 	return pidQevs
-// }
+	return mb.Event{} // NOTREACHED
+}
+
+// makeSelfEvent builds what should be the event that quark will
+// generate as a initial snapshot of the current process
+func makeSelfEvent(t *testing.T) mb.Event {
+	qq := openQueue(t)
+	defer qq.Close()
+
+	qp, ok := qq.Lookup(os.Getpid())
+	require.True(t, ok)
+
+	cwd, err := os.Getwd()
+	require.NoError(t, err)
+
+	exe, err := os.Executable()
+	require.NoError(t, err)
+
+	interactive := tty.InteractiveFromTTY(tty.TTYDev{
+		Major: qp.Proc.TtyMajor,
+		Minor: qp.Proc.TtyMinor,
+	})
+
+	capEff, err := capabilities.FromPid(capabilities.Effective, os.Getpid())
+	require.NoError(t, err)
+	capPer, err := capabilities.FromPid(capabilities.Permitted, os.Getpid())
+	require.NoError(t, err)
+
+	self := mb.Event{
+		RootFields: mapstr.M{
+			"event.type":                            []string{"info"},
+			"event.action":                          "existing_process",
+			"event.category":                        []string{"process"},
+			"event.kind":                            "event",
+			"process.name":                          qp.Comm,
+			"process.args":                          qp.Cmdline,
+			"process.args_count":                    len(qp.Cmdline),
+			"process.pid":                           uint32(os.Getpid()),
+			"process.working_directory":             cwd,
+			"process.executable":                    exe,
+			"process.parent.pid":                    uint32(os.Getppid()),
+			"process.start":                         time.Unix(0, int64(qp.Proc.TimeBoot)),
+			"user.id":                               uint32(0),
+			"user.group.id":                         uint32(0),
+			"user.effective.id":                     uint32(0),
+			"user.saved.id":                         qp.Proc.Suid,
+			"user.saved.group.id":                   qp.Proc.Sgid,
+			"user.name":                             "root",
+			"user.group.name":                       "root",
+			"process.interactive":                   interactive,
+			"process.thread.capabilities.effective": capEff,
+			"process.thread.capabilities.permitted": capPer,
+		},
+	}
+
+	if qp.Proc.TtyMajor != 0 {
+		self.RootFields["process.tty.char_device.major"] = qp.Proc.TtyMajor
+		self.RootFields["process.tty.char_device.minor"] = qp.Proc.TtyMinor
+	}
+
+	return self
+}
