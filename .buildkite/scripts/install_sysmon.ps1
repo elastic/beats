27de21085b0b6845288aba00df5e2cc8cdf68a58
Marc Guasch <marc.guasch@elastic.co>
2025-01-20 13:40:26 +0100

    Remove unused function

--- .buildkite/scripts/install_sysmon.ps1
+++ .buildkite/scripts/install_sysmon.ps1
@@ -2,43 +2,6 @@ $downloadUrl = "https://live.sysinternals.com/Sysmon64.exe"
 $tempFolder = "$env:TEMP\SysmonDownload"
 $sysmonPath = "$tempFolder\Sysmon64.exe"
 
-function Retry()
-{
-    param(
-        [Parameter(Mandatory=$true)][Action]$action,
-        [Parameter(Mandatory=$false)][int]$maxAttempts = 3
-    )
-
-    $attempts=1    
-    $ErrorActionPreferenceToRestore = $ErrorActionPreference
-    $ErrorActionPreference = "Stop"
-
-    do
-    {
-        try
-        {
-            $action.Invoke();
-            break;
-        }
-        catch [Exception]
-        {
-            Write-Host $_.Exception.Message
-        }
-
-        # exponential backoff delay
-        $attempts++
-        if ($attempts -le $maxAttempts) {
-            Write-Host("Action failed. Waiting " + $retryDelaySeconds + " seconds before attempt " + $attempts + " of " + $maxAttempts + ".")
-            Start-Sleep 5
-        }
-        else {
-            $ErrorActionPreference = $ErrorActionPreferenceToRestore
-            Write-Error $_.Exception.Message
-        }
-    } while ($attempts -le $maxAttempts)
-    $ErrorActionPreference = $ErrorActionPreferenceToRestore
-}
-
 if (!(Test-Path $tempFolder)) {
     New-Item -ItemType Directory -Path $tempFolder
 }
