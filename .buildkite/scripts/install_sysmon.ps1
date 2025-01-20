56d88e955f5f31fb2ded3ebf855531d1328743c3
Marc Guasch <marc.guasch@elastic.co>
2025-01-20 13:20:27 +0100

    Add retries and simplify call

--- .buildkite/scripts/install_sysmon.ps1
+++ .buildkite/scripts/install_sysmon.ps1
@@ -2,6 +2,43 @@ $downloadUrl = "https://live.sysinternals.com/Sysmon64.exe"
 $tempFolder = "$env:TEMP\SysmonDownload"
 $sysmonPath = "$tempFolder\Sysmon64.exe"
 
+function Retry()
+{
+    param(
+        [Parameter(Mandatory=$true)][Action]$action,
+        [Parameter(Mandatory=$false)][int]$maxAttempts = 3
+    )
+
+    $attempts=1    
+    $ErrorActionPreferenceToRestore = $ErrorActionPreference
+    $ErrorActionPreference = "Stop"
+
+    do
+    {
+        try
+        {
+            $action.Invoke();
+            break;
+        }
+        catch [Exception]
+        {
+            Write-Host $_.Exception.Message
+        }
+
+        # exponential backoff delay
+        $attempts++
+        if ($attempts -le $maxAttempts) {
+            Write-Host("Action failed. Waiting " + $retryDelaySeconds + " seconds before attempt " + $attempts + " of " + $maxAttempts + ".")
+            Start-Sleep 5
+        }
+        else {
+            $ErrorActionPreference = $ErrorActionPreferenceToRestore
+            Write-Error $_.Exception.Message
+        }
+    } while ($attempts -le $maxAttempts)
+    $ErrorActionPreference = $ErrorActionPreferenceToRestore
+}
+
 if (!(Test-Path $tempFolder)) {
     New-Item -ItemType Directory -Path $tempFolder
 }
@@ -25,15 +62,23 @@ function ParseErrorForResponseBody($Error) {
     }
 }
 
-try {
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    $result = Invoke-WebRequest -Uri $downloadUrl -OutFile $sysmonPath -UseBasicParsing
-}
-catch {
-    $resp = ParseErrorForResponseBody($_)
-    Write-Host "$resp"
-    exit 1
-}
+$attempts=1
+do
+{
+    $attempts++
+    try {
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+        $result = Invoke-WebRequest -Uri $downloadUrl -OutFile $sysmonPath -UseBasicParsing
+        break
+    }
+    catch {
+        $resp = ParseErrorForResponseBody($_)
+        Write-Host "$resp"
+        if ($attempts -gt 5) {
+            exit 1
+        }
+    }
+} while ($attempts -le 5)
 
 Write-Host "Sysmon64.exe downloaded successfully."
 
