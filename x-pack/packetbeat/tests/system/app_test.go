7881f3dbd3d72cb0cc9f7455f810936e3489a2fd
Marc Guasch <marc.guasch@elastic.co>
2025-01-20 23:21:06 -0800

    Enable npcap installation on CI

--- x-pack/packetbeat/tests/system/app_test.go
+++ x-pack/packetbeat/tests/system/app_test.go
@@ -24,7 +24,7 @@ import (
 )
 
 // Keep in sync with NpcapVersion in magefile.go.
-const NpcapVersion = "1.79"
+const NpcapVersion = "1.80"
 
 func TestWindowsNpcapInstaller(t *testing.T) {
 	if runtime.GOOS != "windows" {
