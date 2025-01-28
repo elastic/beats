5b27a39a8f687cf16d4fe888e7a4160539aa6d03
Orestis Floros <orestis.floros@elastic.co>
2025-01-28 12:30:32 +0100

    Simplify test

--- libbeat/statestore/backend/es/notifier_test.go
+++ libbeat/statestore/backend/es/notifier_test.go
@@ -22,30 +22,33 @@ import (
 	"testing"
 	"time"
 
-	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	conf "github.com/elastic/elastic-agent-libs/config"
 )
 
-func createTestConfigs(n int) ([]*conf.C, error) {
+func createTestConfigs(t *testing.T, n int) []*conf.C {
 	var res []*conf.C
 	for i := 0; i < n; i++ {
 		c, err := conf.NewConfigFrom(map[string]any{
 			"id": i,
 		})
-		if err != nil {
-			return nil, err
-		}
+		require.NoError(t, err)
+		require.NotNil(t, c)
+		id, err := c.Int("id", -1)
+		require.NoError(t, err, "sanity check: id is stored")
+		require.Equal(t, int64(i), id, "sanity check: id is correct")
 		res = append(res, c)
 	}
-	return res, nil
+	return res
 }
 
-func waitWithTimeout(wg *sync.WaitGroup, timeout time.Duration) bool {
-	done := make(chan struct{})
+func wgWait(t *testing.T, wg *sync.WaitGroup) {
+	const timeout = 1 * time.Second
+	t.Helper()
 
+	done := make(chan struct{})
 	go func() {
 		wg.Wait()
 		close(done)
@@ -53,36 +56,10 @@ func waitWithTimeout(wg *sync.WaitGroup, timeout time.Duration) bool {
 
 	select {
 	case <-done:
-		return true
+		return
 	case <-time.After(timeout):
-		return false
+		require.Fail(t, "timeout waiting for WaitGroup")
 	}
-}
-
-func confDiff(t *testing.T, c1, c2 *conf.C) string {
-	var m1, m2 map[string]any
-	err := c1.Unpack(&m1)
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = c2.Unpack(&m2)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	return cmp.Diff(m1, m2)
-}
-
-func getMatchOrdered(t *testing.T, conf1, conf2 []*conf.C) []*conf.C {
-	var matchingOrdered []*conf.C
-	for _, c1 := range conf1 {
-		for _, c2 := range conf2 {
-			if confDiff(t, c1, c2) == "" {
-				matchingOrdered = append(matchingOrdered, c2)
-			}
-		}
-	}
-	return matchingOrdered
 }
 
 func TestSubscribeAndNotify(t *testing.T) {
@@ -96,47 +73,42 @@ func TestSubscribeAndNotify(t *testing.T) {
 	)
 
 	unsubFirst := notifier.Subscribe(func(c *conf.C) {
+		defer wg.Done()
 		mx.Lock()
 		defer mx.Unlock()
 		receivedFirst = append(receivedFirst, c)
-		wg.Done()
 	})
 	defer unsubFirst()
 
 	unsubSecond := notifier.Subscribe(func(c *conf.C) {
+		defer wg.Done()
 		mx.Lock()
 		defer mx.Unlock()
 		receivedSecond = append(receivedSecond, c)
-		wg.Done()
 	})
 	defer unsubSecond()
 
 	const totalNotifications = 3
 
-	configs, err := createTestConfigs(totalNotifications)
-	require.NoError(t, err)
+	configs := createTestConfigs(t, totalNotifications)
 
 	wg.Add(totalNotifications * 2)
-	for i := 0; i < totalNotifications; i++ {
-		notifier.Notify(configs[i])
+	for _, config := range configs {
+		notifier.Notify(config)
 	}
 
-	require.True(t, waitWithTimeout(&wg, time.Second))
-
-	receivedFirst = getMatchOrdered(t, configs, receivedFirst)
-	assert.Len(t, receivedFirst, totalNotifications)
-
-	receivedSecond = getMatchOrdered(t, configs, receivedSecond)
-	assert.Len(t, receivedSecond, totalNotifications)
+	wgWait(t, &wg)
+	assert.ElementsMatch(t, configs, receivedFirst)
+	assert.ElementsMatch(t, configs, receivedSecond)
 
 	// Receive old config
 	wg.Add(1)
 	notifier.Subscribe(func(c *conf.C) {
+		defer wg.Done()
 		mx.Lock()
 		defer mx.Unlock()
-		defer wg.Done()
 	})
-	require.True(t, waitWithTimeout(&wg, time.Second))
+	wgWait(t, &wg)
 }
 
 func TestUnsubscribe(t *testing.T) {
@@ -149,40 +121,37 @@ func TestUnsubscribe(t *testing.T) {
 	notifier := NewNotifier()
 
 	unsubFirst := notifier.Subscribe(func(c *conf.C) {
+		defer wg.Done()
 		mx.Lock()
 		defer mx.Unlock()
 		receivedFirst = append(receivedFirst, c)
-		wg.Done()
 	})
 	defer unsubFirst()
 
 	unsubSecond := notifier.Subscribe(func(c *conf.C) {
+		defer wg.Done()
 		mx.Lock()
 		defer mx.Unlock()
 		receivedSecond = append(receivedSecond, c)
-		wg.Done()
 	})
 	defer unsubSecond()
 
 	const totalNotifications = 3
 
-	configs, err := createTestConfigs(totalNotifications)
-	require.NoError(t, err)
+	configs := createTestConfigs(t, totalNotifications)
 
 	// Unsubscribe first
 	unsubFirst()
 
 	// Notify
 	wg.Add(totalNotifications)
-	for i := 0; i < totalNotifications; i++ {
-		notifier.Notify(configs[i])
+	for _, config := range configs {
+		notifier.Notify(config)
 	}
 
-	require.True(t, waitWithTimeout(&wg, time.Second))
+	wgWait(t, &wg)
 	assert.Empty(t, receivedFirst)
-
-	receivedSecond = getMatchOrdered(t, configs, receivedSecond)
-	assert.Len(t, receivedSecond, totalNotifications)
+	assert.ElementsMatch(t, configs, receivedSecond)
 }
 
 func TestConcurrentSubscribeAndNotify(t *testing.T) {
@@ -201,16 +170,16 @@ func TestConcurrentSubscribeAndNotify(t *testing.T) {
 	wg.Add(count)
 	for i := 0; i < count; i++ {
 		go func() {
+			defer wgSub.Done()
 			mxSub.Lock()
 			defer mxSub.Unlock()
 			unsub := notifier.Subscribe(func(c *conf.C) {
+				defer wg.Done()
 				mx.Lock()
 				defer mx.Unlock()
 				received = append(received, c)
-				wg.Done()
 			})
 			unsubFns = append(unsubFns, unsub)
-			wgSub.Done()
 		}()
 	}
 	defer func() {
@@ -220,7 +189,7 @@ func TestConcurrentSubscribeAndNotify(t *testing.T) {
 	}()
 
 	// Wait for all subscribers to be added
-	wgSub.Wait()
+	wgWait(t, &wgSub)
 
 	// Notify
 	c, err := conf.NewConfigFrom(map[string]any{
@@ -230,6 +199,10 @@ func TestConcurrentSubscribeAndNotify(t *testing.T) {
 	notifier.Notify(c)
 
 	// Wait for all
-	require.True(t, waitWithTimeout(&wg, time.Second))
-	assert.Len(t, received, count)
+	wgWait(t, &wg)
+	expected := make([]*conf.C, count)
+	for i := 0; i < count; i++ {
+		expected[i] = c
+	}
+	assert.Equal(t, expected, received)
 }
