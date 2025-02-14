ae5a34f17bb1d2c879fdb9b72a00c41e35d04ae3
Christiano Haesbaert <haesbaert@elastic.co>
2025-02-14 10:35:33 +0100

    tedu XXX

--- x-pack/auditbeat/module/system/process/quark_provider_linux.go
+++ x-pack/auditbeat/module/system/process/quark_provider_linux.go
@@ -250,7 +250,6 @@ func (ms *QuarkMetricSet) toEvent(quarkEvent quark.Event) (mb.Event, bool) {
 	} else {
 		ms.log.Debugf("skipping hash %s (inonum %d vs %d)", process.Filename, process.Proc.MntInonum, ms.selfMntNsIno)
 	}
-	event.RootFields.Put("quark", true) // XXX REMOVE ME
 
 	return event, true
 }
