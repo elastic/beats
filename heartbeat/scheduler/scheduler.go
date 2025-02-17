7b13c968e4cc2b2fd13a4d47ab67e1fa0a23cc1e
Shahzad <shahzad31comp@gmail.com>
2025-02-17 09:40:32 +0100

    add docs

--- heartbeat/scheduler/scheduler.go
+++ heartbeat/scheduler/scheduler.go
@@ -193,7 +193,8 @@ func (s *Scheduler) Add(sched Schedule, pmws []maintwin.ParsedMaintWin, id strin
 		var activeMainWin *maintwin.ParsedMaintWin
 		for _, pmw := range pmws {
 			if pmw.IsActive(now) {
-				activeMainWin = &pmw
+				pmwCopy := pmw 
+				activeMainWin = &pmwCopy
 				break
 			}
 		}
