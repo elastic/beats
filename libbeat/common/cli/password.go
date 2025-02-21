6cea3b3823227940b9a7129980be536acf81960b
kruskall <99559985+kruskall@users.noreply.github.com>
2025-02-21 02:37:26 +0100

    Update password.go

--- libbeat/common/cli/password.go
+++ libbeat/common/cli/password.go
@@ -61,11 +61,13 @@ func ReadPassword(def string) (string, error) {
 }
 
 func stdin(p string) (string, error) {
+	//nolint:forbidigo // ignore
 	fmt.Print("Enter password: ")
 	bytePassword, err := term.ReadPassword(int(syscall.Stdin))
 	if err != nil {
 		return "", fmt.Errorf("reading password input: %w", err)
 	}
+	//nolint:forbidigo // ignore
 	fmt.Println()
 	return string(bytePassword), nil
 }
