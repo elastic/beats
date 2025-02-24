16ac1c2716e7d930660b1d1462f5adf03feafd9f
Olga Naydyonock <olga.naidjonoka@elastic.co>
2025-02-24 10:11:54 +0200

    Update .buildkite/scripts/packaging/packaging.sh
    Co-authored-by: Victor Martinez <victormartinezrubio@gmail.com>

--- .buildkite/scripts/packaging/packaging.sh
+++ .buildkite/scripts/packaging/packaging.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+#
+# Centralise the mage package for a given beat in Buildkite.
+# It enables multi-arch builds to avoid the exec format errors when 
+# attempting to build arm64 inside arm64 workers.
+# For further details, see https://github.com/elastic/elastic-agent/pull/6948
+# and https://github.com/elastic/golang-crossbuild/pull/507
+#
 
 set -ueo pipefail
 
