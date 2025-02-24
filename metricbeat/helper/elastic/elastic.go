b5316b037e3e43ed8e89eb1969ff513632221fa6
Valentin Crettaz <valentin.crettaz@elastic.co>
2025-02-24 15:04:44 +0100

    Fix logged message
    Co-authored-by: Anderson Queiroz <anderson.queiroz@elastic.co>

--- metricbeat/helper/elastic/elastic.go
+++ metricbeat/helper/elastic/elastic.go
@@ -166,7 +166,7 @@ func NewModule(base *mb.BaseModule, xpackEnabledMetricsets []string, optionalXpa
 		// Type cast the metricsets to a slice of strings
 		cfgdMetricsetsSlice, ok := cfgdMetricsets.([]interface{})
 		if !ok {
-			return nil, fmt.Errorf("configured metricsets are not an array for module %v: %v", moduleName, cfgdMetricsets)
+			return nil, fmt.Errorf("configured metricsets are not an slice for module %s: %v", moduleName, cfgdMetricsets)
 		}
 
 		cfgdMetricsetsStrings := make([]string, 0, len(cfgdMetricsetsSlice))
