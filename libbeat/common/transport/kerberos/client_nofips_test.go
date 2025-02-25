aece42b84d22514c7c326ff4468a14510fec4bab
kruskall <99559985+kruskall@users.noreply.github.com>
2025-02-25 12:51:10 +0100

    feat(fips): return an error when creating a kerberos client (#42597)
    * feat(fips): return an error when creating a kerberos client
    
    kerberos lib is implementing a lot of crypto :(
    
    * test: add kerberos client tests
    
    ---------
    
    Co-authored-by: Pierre HILBERT <pierre.hilbert@elastic.co>
    (cherry picked from commit bbfa29021664ae4d5f4a67465593ec441dad6c13)

--- libbeat/common/transport/kerberos/client_nofips_test.go
+++ libbeat/common/transport/kerberos/client_nofips_test.go
@@ -15,14 +15,25 @@
 // specific language governing permissions and limitations
 // under the License.
 
+//go:build !requirefips
+
 package kerberos
 
 import (
 	"net/http"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
-type Client interface {
-	Do(req *http.Request) (*http.Response, error)
-
-	CloseIdleConnections()
+func TestNewClient(t *testing.T) {
+	cfg, err := os.CreateTemp(t.TempDir(), "config")
+	require.NoError(t, err)
+	c, err := NewClient(&Config{
+		AuthType:   authPassword,
+		ConfigPath: cfg.Name(),
+	}, http.DefaultClient, "")
+	require.Nil(t, err)
+	require.NotNil(t, c)
 }
