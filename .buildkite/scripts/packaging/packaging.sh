c7cf22af5d35116577224970c2656ab6fec38d55
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2025-02-26 11:45:05 +0200

    Revert "use more recent pinned version of qemu"
    This reverts commit 4014a14cad4b68c4e615c8797a5acbe7607ab6d4.

--- .buildkite/scripts/packaging/packaging.sh
+++ .buildkite/scripts/packaging/packaging.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # Centralise the mage package for a given beat in Buildkite.
-# It enables multi-arch builds to avoid the exec format errors when
+# It enables multi-arch builds to avoid the exec format errors when 
 # attempting to build arm64 inside arm64 workers.
 # For further details, see https://github.com/elastic/elastic-agent/pull/6948
 # and https://github.com/elastic/golang-crossbuild/pull/507
@@ -14,7 +14,7 @@ BEAT_DIR=${1:?-"Error: Beat directory must be specified."}
 
 # NOTE: Pinned to QEMU v8.x since v9.x breaks compilation of amd64 binaries in arm64.
 # See: https://gitlab.com/qemu-project/qemu/-/issues/2560
-BINFMT_IMAGE="tonistiigi/binfmt:qemu-v9.2.0-51"
+BINFMT_IMAGE="tonistiigi/binfmt:qemu-v8.1.5"
 # Make sure to uninstall first to avoid conflicts
 docker run --privileged --rm "$BINFMT_IMAGE" --uninstall qemu-*
 docker run --privileged --rm "$BINFMT_IMAGE" --install all
