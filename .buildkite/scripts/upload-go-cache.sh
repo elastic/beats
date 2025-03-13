0f94b8d5cb03159c1efd696f7a1d24b55c9eb9bf
kruskal <99559985+kruskall@users.noreply.github.com>
2025-03-13 02:14:17 +0100

    ci: try again

---
+++ .buildkite/scripts/upload-go-cache.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+name=$(sha256sum go.mod | head -c 40)
+
+if ! buildkite-agent artifact search "$name.tar.gz"; then
+	echo "cache already up to date."
+	exit 0
+fi
+
+go mod download
+
+tar -czvf "$name.tar.gz" $(go env GOMODCACHE)
+
+buildkite-agent artifact upload "$name.tar.gz"
+
+
