bc3d7bf667e94683abdbf316f4c9ee0058526af5
kruskal <99559985+kruskall@users.noreply.github.com>
2025-03-13 23:17:46 +0100

    ci: ensure go mod cache exists before extracting cache

--- .buildkite/scripts/download-go-cache.sh
+++ .buildkite/scripts/download-go-cache.sh
@@ -4,4 +4,6 @@ name=$(sha256sum go.mod | head -c 40)
 
 buildkite-agent artifact download "$name.tar.gz" . --build "$BUILDKITE_TRIGGERED_FROM_BUILD_ID"
 
+mkdir -p $(go env GOMODCACHE)
+
 tar -xf "$name.tar.gz" -C $(go env GOMODCACHE)
