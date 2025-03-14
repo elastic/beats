5edac208e803914e4f91d239e49e2affc2329699
kruskall <99559985+kruskall@users.noreply.github.com>
2025-03-14 01:51:01 +0100

    Update mage-install.mk

--- dev-tools/make/mage-install.mk
+++ dev-tools/make/mage-install.mk
@@ -1,6 +1,6 @@
-MAGE_VERSION     ?= $(shell grep 'magefile' go.mod | cut -d' ' -f2)
-MAGE_PRESENT     := $(shell mage --version 2> /dev/null | grep $(MAGE_VERSION))
 MAGE_IMPORT_PATH ?= github.com/magefile/mage
+MAGE_VERSION     ?= $(shell go list -m $(MAGE_IMPORT_PATH) | cut -d' ' -f2)
+MAGE_PRESENT     := $(shell mage --version 2> /dev/null | grep $(MAGE_VERSION))
 export MAGE_IMPORT_PATH
 
 .PHONY: mage
