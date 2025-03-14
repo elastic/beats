54c3c24b6b192c37925fa4261a70c81774358f88
kruskal <99559985+kruskall@users.noreply.github.com>
2025-03-14 20:56:08 +0100

    ci: use a variable to store folder name

--- .buildkite/scripts/download-go-cache.sh
+++ .buildkite/scripts/download-go-cache.sh
@@ -4,13 +4,15 @@ name=$(sha256sum go.mod | head -c 40)
 
 buildkite-agent artifact download "$name.tar.gz" . --build "$BUILDKITE_TRIGGERED_FROM_BUILD_ID"
 
-mkdir -p "$(go env GOMODCACHE)/cache"
+folder="$(go env GOMODCACHE)/cache"
+
+mkdir -p "$folder"
 
 id
 
 ls -alh $(go env GOMODCACHE)
 
-echo "extracting cache archive to $(go env GOMODCACHE)/cache"
-tar -xf "$name.tar.gz" --skip-old-files -C "$(go env GOMODCACHE)/cache"
+echo "extracting cache archive to $folder"
+tar -xf "$name.tar.gz" --skip-old-files -C "$folder"
 
 ls -alh $(go env GOMODCACHE)
