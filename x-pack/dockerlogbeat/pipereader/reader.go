886635cd518693f5d06b66505399fded1fdacc48
kruskall <99559985+kruskall@users.noreply.github.com>
2025-03-18 02:50:49 +0100

    Update reader.go

--- x-pack/dockerlogbeat/pipereader/reader.go
+++ x-pack/dockerlogbeat/pipereader/reader.go
@@ -9,7 +9,6 @@ import (
 	"encoding/binary"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"syscall"
 
 	"github.com/docker/docker/api/types/plugins/logdriver"
@@ -60,7 +59,7 @@ func (reader *PipeReader) ReadMessage(log *logdriver.LogEntry) error {
 		}
 
 		// 2) we have a too-large message. Disregard length bytes
-		_, err = io.CopyBuffer(ioutil.Discard, io.LimitReader(reader.fifoPipe, int64(lenFrame)), reader.bodyBuf)
+		_, err = io.CopyBuffer(io.Discard, io.LimitReader(reader.fifoPipe, int64(lenFrame)), reader.bodyBuf)
 		if err != nil {
 			return fmt.Errorf("error emptying buffer: %w", err)
 		}
