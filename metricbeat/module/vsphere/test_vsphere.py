818104938394bc717c23f295db1a933a1deb5366
stefans-elastic <stefan.stas@elastic.co>
2025-03-24 10:43:30 +0200

    fix vsphere test

--- metricbeat/module/vsphere/test_vsphere.py
+++ metricbeat/module/vsphere/test_vsphere.py
@@ -65,7 +65,7 @@ class TestVsphere(metricbeat.BaseTest):
         proc = self.start_beat()
         self.wait_until(lambda: self.output_lines() > 0)
         proc.check_kill_and_wait()
-        self.assert_no_logged_warnings()
+        self.assert_no_logged_warnings(replace=['Your vSphere connection is configured as insecure. This can lead to man-in-the-middle attack.'])
 
         output = self.read_output_json()
         self.assertEqual(len(output), 4)
