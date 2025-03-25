075d3e2e19c2821dbd6e5dc2b4980d590433fe21
Khushi Jain <khushi.jain@elastic.co>
2025-03-25 18:25:52 +0530

    fix tests and lint

--- libbeat/outputs/elasticsearch/client_integration_test.go
+++ libbeat/outputs/elasticsearch/client_integration_test.go
@@ -83,7 +83,7 @@ func testPublishEvent(t *testing.T, index string, cfg map[string]interface{}) {
 	// drop old index preparing test
 	_, _, _ = client.conn.Delete(index, "", "", nil)
 
-	batch := encodeBatch[outest.Batch](client, outest.NewBatch(beat.Event{
+	batch := encodeBatch[*outest.Batch](client, outest.NewBatch(beat.Event{
 		Timestamp: time.Now(),
 		Fields: mapstr.M{
 			"type":    "libbeat",
@@ -135,7 +135,7 @@ func TestClientPublishEventWithPipeline(t *testing.T) {
 	}
 
 	publish := func(event beat.Event) {
-		batch := encodeBatch(client, outest.NewBatch(event))
+		batch := encodeBatch[*outest.Batch](client, outest.NewBatch(event))
 		err := output.Publish(context.Background(), batch)
 		if err != nil {
 			t.Fatal(err)
@@ -217,7 +217,7 @@ func TestClientBulkPublishEventsWithDeadletterIndex(t *testing.T) {
 	_, _, _ = client.conn.Delete(index, "", "", nil)
 	_, _, _ = client.conn.Delete(deadletterIndex, "", "", nil)
 
-	batch := encodeBatch(client, outest.NewBatch(beat.Event{
+	batch := encodeBatch[*outest.Batch](client, outest.NewBatch(beat.Event{
 		Timestamp: time.Now(),
 		Fields: mapstr.M{
 			"type":      "libbeat",
@@ -230,7 +230,7 @@ func TestClientBulkPublishEventsWithDeadletterIndex(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	batch = encodeBatch(client, outest.NewBatch(beat.Event{
+	batch = encodeBatch[*outest.Batch](client, outest.NewBatch(beat.Event{
 		Timestamp: time.Now(),
 		Fields: mapstr.M{
 			"type":      "libbeat",
@@ -279,7 +279,7 @@ func TestClientBulkPublishEventsWithPipeline(t *testing.T) {
 	}
 
 	publish := func(events ...beat.Event) {
-		batch := encodeBatch(client, outest.NewBatch(events...))
+		batch := encodeBatch[*outest.Batch](client, outest.NewBatch(events...))
 		err := output.Publish(context.Background(), batch)
 		if err != nil {
 			t.Fatal(err)
@@ -352,7 +352,7 @@ func TestClientPublishTracer(t *testing.T) {
 
 	_, _, _ = client.conn.Delete(index, "", "", nil)
 
-	batch := encodeBatch(client, outest.NewBatch(beat.Event{
+	batch := encodeBatch[*outest.Batch](client, outest.NewBatch(beat.Event{
 		Timestamp: time.Now(),
 		Fields: mapstr.M{
 			"message": "Hello world",
