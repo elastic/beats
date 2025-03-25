0d9e43847c01be7094e28889dab5a8ec02b0f26d
Olga Naydyonock <olga.naidjonoka@elastic.co>
2025-03-25 15:56:55 +0200

    updated matrix generation to run on gh action related changes

--- .github/workflows/macos.d/generate-matrix.sh
+++ .github/workflows/macos.d/generate-matrix.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-readarray -t whitelist < ".github/workflows/macos.d/values.d/whitelisted-dirs"
+readarray -t whitelist <".github/workflows/macos.d/values.d/beats-macos"
 changeList=$(git diff --name-only HEAD~1)
 changedDirs=($(echo "$changeList" | sed 's/^[ 0-9.]\+% //g' | awk -F/ '{print $1}' | sort -u))
 
@@ -11,23 +11,25 @@ for dir in "${changedDirs[@]}"; do
   fi
 done
 
+osArray=("${MACOS_13_X64}" "${MACOS_14_ARM}")
 # If the workflow changed - run all tests, no matter of what was the other changes
-if [[ $changeList == *"macos-unit-tests-pr.yml"* ]]; then
-  osArray=("${MACOS_13_X64}" "${MACOS_14_ARM}")
-  matrix=$(jq -n --argjson dirs "$(printf '%s\n' "${beats[@]}" | jq -R . | jq -s .)" \
-    --argjson osArray "$(printf '%s\n' "${osArray[@]}" | jq -R . | jq -s .)" \
-    '[ $dirs[] as $dir | $osArray[] as $os | {beat: $dir, os: $os} ]')
-fi
-
-# Check if the changes were done in beats
-# if yes - generate a matrix to run tests only for changed beats
-# if not - skip the following jobs
-if [[ "${#beats[@]}" != 0 ]]; then
-  matrix=$(jq -n --argjson dirs "$(printf '%s\n' "${beats[@]}" | jq -R . | jq -s .)" \
+if [[ $changeList == *"macos-unit-tests-pr.yml"* || $changeList == *"macos.d/generate-matrix.sh"* ]]; then
+  matrix=$(jq -n --argjson dirs "$(printf '%s\n' "${whitelist[@]}" | jq -R . | jq -s .)" \
     --argjson osArray "$(printf '%s\n' "${osArray[@]}" | jq -R . | jq -s .)" \
     '[ $dirs[] as $dir | $osArray[] as $os | {beat: $dir, os: $os} ]')
 
   echo "matrix={\"include\":$(echo $matrix)}" >>$GITHUB_OUTPUT
 else
-  echo "No changes detected, tests will be skipped"
+  # Check if the changes affected beats
+  # if yes - generate a matrix to run tests only for changed beats
+  # if not - skip the following jobs
+  if [[ "${#beats[@]}" != 0 ]]; then
+    matrix=$(jq -n --argjson dirs "$(printf '%s\n' "${beats[@]}" | jq -R . | jq -s .)" \
+      --argjson osArray "$(printf '%s\n' "${osArray[@]}" | jq -R . | jq -s .)" \
+      '[ $dirs[] as $dir | $osArray[] as $os | {beat: $dir, os: $os} ]')
+
+    echo "matrix={\"include\":$(echo $matrix)}" >>$GITHUB_OUTPUT
+  else
+    echo "No changes detected, tests will be skipped"
+  fi
 fi
