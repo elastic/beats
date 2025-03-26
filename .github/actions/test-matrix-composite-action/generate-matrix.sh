1d5b588e60bb213ad7f5c02a0cc85742c0fcab5c
Olga Naydyonock <olga.naidjonoka@elastic.co>
2025-03-26 16:23:45 +0200

    updated composite action files

--- .github/actions/test-matrix-composite-action/generate-matrix.sh
+++ .github/actions/test-matrix-composite-action/generate-matrix.sh
@@ -8,6 +8,7 @@ OS_VERSIONS=${2:?"OS_VERSIONS parameter not set"}
 # Name of a workflow file to track if changed
 WORKFLOW_FILE=${3:?"WORKFLOW_FILE parameter not set"}
 
+os=("${OS_VERSIONS}")
 changeList=$(git diff --name-only HEAD~1)
 changedDirs=($(echo "$changeList" | sed 's/^[ 0-9.]\+% //g' | awk -F/ '{print $1}' | sort -u))
 
@@ -16,13 +17,13 @@ if [[ $changeList == *"$WORKFLOW_FILE"* || $changeList == *"test-matrix-composit
   # Since GH actions do not support accessing the `paths` section in a runtime, get required beats manually
   beats=($(awk '/paths:/ {flag=1; next} /^[^ ]/ {flag=0} flag && $2 !~ /^\.github/ {print $2}'))
   matrix=$(jq -n --argjson dirs "$(printf '%s\n' "${beats[@]}" | jq -R . | jq -s .)" \
-    --argjson osArray "$(printf '%s\n' "${OS_VERSIONS[@]}" | jq -R . | jq -s .)" \
+    --argjson osArray "$(printf '%s\n' "${os[@]}" | jq -R . | jq -s .)" \
     '[ $dirs[] as $dir | $osArray[] as $os | {beat: $dir, os: $os} ]')
 
   echo "matrix={\"include\":$(echo $matrix)}" >>$GITHUB_OUTPUT
 else
     matrix=$(jq -n --argjson dirs "$(printf '%s\n' "${changedDirs[@]}" | jq -R . | jq -s .)" \
-      --argjson osArray "$(printf '%s\n' "${OS_VERSIONS[@]}" | jq -R . | jq -s .)" \
+      --argjson osArray "$(printf '%s\n' "${os[@]}" | jq -R . | jq -s .)" \
       '[ $dirs[] as $dir | $osArray[] as $os | {beat: $dir, os: $os} ]')
 
     echo "matrix={\"include\":$(echo $matrix)}" >>$GITHUB_OUTPUT
