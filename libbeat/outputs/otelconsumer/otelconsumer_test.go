f29ee9eec43059d6f95c3475e1515e8f7170ac3d
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2025-03-27 16:22:03 -0300

    otelconsumer: set log record observed timestamp
    The observed timestamp for log records defaults to the Unix epoch.
    Per [logs.proto](https://github.com/open-telemetry/opentelemetry-proto/blob/2bd940b2b77c1ab57c27166af21384906da7bb2b/opentelemetry/proto/logs/v1/logs.proto#L139-L159), this should be the time when the event was seen
    by the collection system.
    
    Since otelconsumer is used by receivers, set this to time.Now().
    This is distinct from the log record timestamp, which reflects
    when the event occurred and is already set correctly.

--- libbeat/outputs/otelconsumer/otelconsumer_test.go
+++ libbeat/outputs/otelconsumer/otelconsumer_test.go
@@ -183,6 +183,7 @@ func TestPublish(t *testing.T) {
 	})
 
 	t.Run("sets the @timestamp field with the correct format", func(t *testing.T) {
+		testStartedAt := time.Now()
 		batch := outest.NewBatch(event3)
 		batch.Events()[0].Content.Timestamp = time.Date(2025, time.January, 29, 9, 2, 39, 0, time.UTC)
 
@@ -194,6 +195,10 @@ func TestPublish(t *testing.T) {
 			recordTimestamp = record.Timestamp().AsTime().UTC().Format("2006-01-02T15:04:05.000Z")
 			assert.True(t, ok, "timestamp field not found")
 			bodyTimestamp = field.AsString()
+			observedTime := record.ObservedTimestamp().AsTime()
+			assert.Conditionf(t, func() bool {
+				return observedTime.After(testStartedAt) && observedTime.Before(time.Now())
+			}, "observed timestamp should be between %s and %s", testStartedAt, time.Now())
 			return nil
 		})
 
