a269b10658dc1f34f25cda94a59f08531bbba42d
Anderson Queiroz <anderson.queiroz@elastic.co>
2025-04-11 12:47:04 +0200

    [Flaky Test] oteltest.CheckReceivers increase timeout (#43834)
    * fbreceiver/TestMultipleReceivers assert.Greater instead of require.Conditionf and improve logs
    
    (cherry picked from commit c9d6ad4c8cefd517223412fd1c43ecd9426ed7d1)
    
    # Conflicts:
    #	libbeat/otelbeat/oteltest/oteltest.go
    #	x-pack/filebeat/fbreceiver/receiver_test.go

--- x-pack/filebeat/fbreceiver/receiver_test.go
+++ x-pack/filebeat/fbreceiver/receiver_test.go
@@ -7,6 +7,7 @@ package fbreceiver
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"testing"
 	"time"
 
@@ -53,10 +54,86 @@ func TestNewReceiver(t *testing.T) {
 	receiverSettings := receiver.Settings{}
 	receiverSettings.Logger = zap.New(core)
 
+<<<<<<< HEAD
 	var countLogs int
 	logConsumer, err := consumer.NewLogs(func(ctx context.Context, ld plog.Logs) error {
 		countLogs = countLogs + ld.LogRecordCount()
 		return nil
+=======
+	factory := NewFactory()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, err := factory.CreateLogs(context.Background(), receiverSettings, cfg, nil)
+		require.NoError(b, err)
+	}
+}
+
+func TestMultipleReceivers(t *testing.T) {
+	// This test verifies that multiple receivers can be instantiated
+	// in isolation, started, and can ingest logs without interfering
+	// with each other.
+	config := Config{
+		Beatconfig: map[string]interface{}{
+			"filebeat": map[string]interface{}{
+				"inputs": []map[string]interface{}{
+					{
+						"type":    "benchmark",
+						"enabled": true,
+						"message": "test",
+						"count":   1,
+					},
+				},
+			},
+			"output": map[string]interface{}{
+				"otelconsumer": map[string]interface{}{},
+			},
+			"logging": map[string]interface{}{
+				"level": "debug",
+				"selectors": []string{
+					"*",
+				},
+			},
+			"path.home": t.TempDir(),
+		},
+	}
+
+	factory := NewFactory()
+	oteltest.CheckReceivers(oteltest.CheckReceiversParams{
+		T: t,
+		Receivers: []oteltest.ReceiverConfig{
+			{
+				Name:    "r1",
+				Config:  &config,
+				Factory: factory,
+			},
+			{
+				Name:    "r2",
+				Config:  &config,
+				Factory: factory,
+			},
+		},
+		AssertFunc: func(t *assert.CollectT, logs map[string][]mapstr.M, zapLogs *observer.ObservedLogs) {
+			r1ok := assert.Greater(t, len(logs["r1"]), 0, "receive r1 does not have any logs")
+			r2ok := assert.Greater(t, len(logs["r2"]), 0, "receive r2 does not have any logs")
+			// logs for debug if it fails again
+			fmt.Printf("len(logs[\"r1\"]): %d\n", len(logs["r1"]))
+			fmt.Printf("len(logs[\"r2\"]): %d\n", len(logs["r2"]))
+			if !r1ok || !r2ok {
+				fmt.Printf("logs[\"r1\"]: %v\n", logs["r1"])
+				fmt.Printf("logs[\"r2\"]: %v\n", logs["r2"])
+				fmt.Printf("all logs: %v\n", logs)
+			}
+
+			// Make sure that each receiver has a separate logger
+			// instance and does not interfere with others. Previously, the
+			// logger in Beats was global, causing logger fields to be
+			// overwritten when multiple receivers started in the same process.
+			r1StartLogs := zapLogs.FilterMessageSnippet("Beat ID").FilterField(zap.String("name", "r1"))
+			assert.Equal(t, 1, r1StartLogs.Len(), "r1 should have a single start log")
+			r2StartLogs := zapLogs.FilterMessageSnippet("Beat ID").FilterField(zap.String("name", "r2"))
+			require.Equal(t, 1, r2StartLogs.Len(), "r2 should have a single start log")
+		},
+>>>>>>> c9d6ad4c8 ([Flaky Test] oteltest.CheckReceivers increase timeout (#43834))
 	})
 	assert.NoError(t, err, "Error creating log consumer")
 
