b09d656140758a5d43bf62ec5cd891224c4a6745
Anderson Queiroz <anderson.queiroz@elastic.co>
2025-04-15 15:53:10 +0200

    skip for all OSs

--- x-pack/filebeat/input/netflow/netflow_test.go
+++ x-pack/filebeat/input/netflow/netflow_test.go
@@ -97,9 +97,7 @@ func TestNetFlow(t *testing.T) {
 			pluginCfg, err := conf.NewConfigFrom(mapstr.M{})
 			require.NoError(t, err)
 			if isReversed {
-				if runtime.GOOS == "darwin" {
-					t.Skip("Flaky on macOS: https://github.com/elastic/beats/issues/43670")
-				}
+				t.Skip("Flaky on macOS: https://github.com/elastic/beats/issues/43670")
 
 				// if pcap is reversed packet order we need to have multiple workers
 				// and thus enable the input packets lru
