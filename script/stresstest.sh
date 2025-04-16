1c042531b8c0ad59c9a6123ed06289de012b0736
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2025-04-16 11:57:07 -0300

    add validation and help

--- script/stresstest.sh
+++ script/stresstest.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 #
 # stresstest.sh is a helper script to run specific Go tests with x/tools/cmd/stress.
-#
-# Usage: ./script/stresstest.sh [package_path] ^TestName$ [stress options...]
-#
-# Example: ./script/stresstest.sh ./libbeat/common/backoff ^TestBackoff$ -p 32
 
 set -e
 
-test_package_path=${1:-$(pwd)}
+function usage() {
+  echo "Usage: $0 [package_path] TestName [stress options...]"
+  echo ""
+  echo "package_path: Path to the Go package containing the tests."
+  echo "TestName: Regular expression to match the test to run, equivalent to -test.run."
+  echo "[stress options]: Options to pass to the stress command."
+  echo ""
+  echo "Example: $0 ./libbeat/common/backoff ^TestBackoff$ -p 32"
+}
+
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+  usage
+  exit 0
+fi
+
+if [[ $# -lt 2 ]]; then
+  echo "Error: Missing arguments."
+  usage
+  exit 1
+fi
+
+if [ ! -d "$1" ]; then
+  echo "Error: Package path '$1' does not exist."
+  usage
+  exit 1
+fi
+
+test_package_path=${1}
 test_exec_file="$(basename $test_package_path).test"
 test_regex=${2}
 stress_options=("${@:3}")
