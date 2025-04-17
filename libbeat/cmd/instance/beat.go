5935c2d51812f639e50478155fbe3f3c22fe3c35
Vihas Makwana <vihas.makwana@elastic.co>
2025-04-17 13:15:30 +0530

    merge conflict

--- libbeat/cmd/instance/beat.go
+++ libbeat/cmd/instance/beat.go
@@ -466,12 +466,8 @@ func NewBeatReceiver(settings Settings, receiverConfig map[string]interface{}, u
 		}
 	}
 
-<<<<<<< HEAD
 	uniq_reg := b.Beat.Info.Monitoring.Namespace.GetRegistry()
 
-=======
-	namespaceReg := b.Info.Monitoring.Namespace.GetRegistry()
->>>>>>> 9ecf8c484 ([mbreceiver][fbreceiver] - use logp.DefaultConfig (#43800))
 	reg := b.Info.Monitoring.StatsRegistry.GetRegistry("libbeat")
 	if reg == nil {
 		reg = b.Info.Monitoring.StatsRegistry.NewRegistry("libbeat")
