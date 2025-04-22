a5743f09cae201286c3c2d9fd935b97f7bb4354e
kruskal <99559985+kruskall@users.noreply.github.com>
2025-04-22 13:04:10 +0200

    feat(auditbeat): enforce fips140=only when running in fips mode

---
+++ x-pack/auditbeat/fips.go
@@ -0,0 +1,10 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+//go:build requirefips
+
+// enforce fips140=only when running in fips mode
+//go:debug fips140=only
+
+package main
