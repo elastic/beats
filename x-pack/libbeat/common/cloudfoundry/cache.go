afd06f7754d3de9f9ad3dc66774819e386b0f529
subham sarkar <subham.sarkar@elastic.co>
2025-05-02 02:00:02 +0530

    Address review comments

--- x-pack/libbeat/common/cloudfoundry/cache.go
+++ x-pack/libbeat/common/cloudfoundry/cache.go
@@ -5,9 +5,12 @@
 package cloudfoundry
 
 import (
+	"encoding/base64"
 	"fmt"
+	"strconv"
 	"time"
 
+	"github.com/cespare/xxhash/v2"
 	"github.com/cloudfoundry-community/go-cfclient"
 	"github.com/pkg/errors" //nolint:gomodguard // go-cfclient uses pkg/errors internally, must be upgraded to at least https://github.com/cloudfoundry-community/go-cfclient/commit/0ada4e9452
 
@@ -136,4 +139,10 @@ func (c *clientCacheWrap) Close() error {
 		return fmt.Errorf("closing cache: %w", err)
 	}
 	return nil
+}
+
+// sanitizeCacheName returns a unique string that can be used safely as part of a file name
+func sanitizeCacheName(name string) string {
+	h := xxhash.Sum64([]byte(name))
+	return base64.RawURLEncoding.EncodeToString([]byte(strconv.FormatUint(h, 10)))
 }
