330909c7a7ad966c7a52c57d7cdc74d8185a0bd5
Pierre HILBERT <pierre.hilbert@elastic.co>
2025-05-21 15:18:14 +0200

    Revert "Fix mapstr"
    This reverts commit c4d34ed02f92873a9dfe1ea438b2363ce60508f4.

--- libbeat/dashboards/kibana_loader.go
+++ libbeat/dashboards/kibana_loader.go
@@ -104,7 +104,7 @@ func (loader KibanaLoader) ImportIndexFile(file string) error {
 		return fmt.Errorf("fail to read index-pattern from file %s: %v", file, err)
 	}
 
-	var indexContent mapstr.M
+	var indexContent common.MapStr
 	err = json.Unmarshal(reader, &indexContent)
 	if err != nil {
 		return fmt.Errorf("fail to unmarshal the index content from file %s: %v", file, err)
