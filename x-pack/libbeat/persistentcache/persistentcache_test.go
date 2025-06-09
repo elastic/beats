7bb749e749ebe40e935d1f0d9d14ce0c6a0a1b32
Khushi Jain <khushi.jain@elastic.co>
2025-06-09 09:43:59 +0530

    make check update

--- x-pack/libbeat/persistentcache/persistentcache_test.go
+++ x-pack/libbeat/persistentcache/persistentcache_test.go
@@ -16,6 +16,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	"github.com/elastic/elastic-agent-libs/logp"
 	"github.com/elastic/elastic-agent-libs/logp/logptest"
 )
 
