fab4fe07c9220c9768668d187f5c16c45662cfda
mjmbischoff <mjmbischoff@controplex.com>
2025-06-12 19:11:25 +0200

    Adding the now processor to avoid having to resort to scripts to add the current timestamp

---
+++ libbeat/processors/now/now.go
@@ -0,0 +1,71 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package now
+
+import (
+	"fmt"
+	"github.com/elastic/beats/v7/libbeat/beat"
+	"github.com/elastic/beats/v7/libbeat/processors"
+	"github.com/elastic/beats/v7/libbeat/processors/checks"
+	jsprocessor "github.com/elastic/beats/v7/libbeat/processors/script/javascript/module/processor/registry"
+	"github.com/elastic/elastic-agent-libs/config"
+	"github.com/elastic/elastic-agent-libs/logp"
+	"time"
+)
+
+var currentTime = time.Now
+
+type now struct {
+	config nowConfig
+	log    *logp.Logger
+}
+
+type nowConfig struct {
+	Field string `config:"field" validate:"required"`
+}
+
+func init() {
+	processors.RegisterPlugin("now",
+		checks.ConfigChecked(New,
+			checks.RequireFields("field"),
+			checks.AllowedFields("field")))
+	jsprocessor.RegisterPlugin("Now", New)
+}
+
+func New(c *config.C, log *logp.Logger) (beat.Processor, error) {
+	nowConfig := nowConfig{}
+
+	if err := c.Unpack(&nowConfig); err != nil {
+		return nil, fmt.Errorf("failed to unpack the configuration of now processor: %w", err)
+	}
+
+	return &now{
+		config: nowConfig,
+		log:    log.Named("now"),
+	}, nil
+
+}
+
+func (n *now) Run(event *beat.Event) (*beat.Event, error) {
+	_, err := event.PutValue(n.config.Field, currentTime())
+	return event, err
+}
+
+func (n *now) String() string {
+	return "now=" + fmt.Sprintf("%+v", n.config.Field)
+}
---
+++ libbeat/processors/now/now_test.go
@@ -0,0 +1,107 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package now
+
+import (
+	"github.com/elastic/beats/v7/libbeat/beat"
+	"github.com/elastic/elastic-agent-libs/logp/logptest"
+	"github.com/elastic/elastic-agent-libs/mapstr"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestNow(t *testing.T) {
+	es9ReleaseDate := time.Date(2025, 04, 8, 12, 0, 0, 42, time.UTC)
+	currentTime = func() time.Time { return es9ReleaseDate }
+
+	var testCases = []struct {
+		description string
+		config      nowConfig
+		Input       mapstr.M
+		Output      mapstr.M
+		error       bool
+	}{
+		{
+			description: "Single target field now",
+			config: nowConfig{
+				Field: "field1",
+			},
+			Output: mapstr.M{
+				"field1": es9ReleaseDate,
+			},
+			error: false,
+		},
+		{
+			description: "Target field with now plus existing field",
+			config: nowConfig{
+				Field: "field1",
+			},
+			Input: mapstr.M{
+				"field2": "some data",
+			},
+			Output: mapstr.M{
+				"field1": es9ReleaseDate,
+				"field2": "some data",
+			},
+			error: false,
+		},
+		{
+			description: "Target with with existing value",
+			config: nowConfig{
+				Field: "field1",
+			},
+			Input: mapstr.M{
+				"field1": "existing data",
+				"field2": "some data",
+			},
+			Output: mapstr.M{
+				"field1": es9ReleaseDate,
+				"field2": "some data",
+			},
+			error: false,
+		},
+	}
+
+	for _, test := range testCases {
+		test := test
+		t.Run(test.description, func(t *testing.T) {
+			t.Parallel()
+
+			processor := &now{
+				log:    logptest.NewTestingLogger(t, "now"),
+				config: test.config,
+			}
+
+			inputEvent := &beat.Event{
+				Fields: test.Input,
+			}
+
+			outputEvent, err := processor.Run(inputEvent)
+			if !test.error {
+				assert.NoError(t, err)
+			} else {
+				assert.Error(t, err)
+			}
+
+			assert.Equal(t, test.Output, outputEvent.Fields)
+		})
+
+	}
+
+}
