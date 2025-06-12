bde4549bbfbc1a88f2371bc6f590cc51958c5f9f
Anderson Queiroz <anderson.queiroz@elastic.co>
2025-06-12 12:12:40 +0200

    simulate 2 inputs on the output test

--- libbeat/outputs/outputs_test.go
+++ libbeat/outputs/outputs_test.go
@@ -121,8 +121,10 @@ func testOutputMetrics(t *testing.T,
 	configuration any,
 	evFields []map[string]any) {
 
-	counter := &beat.CountOutputListener{}
-	observer := publisher.OutputListener{Listener: counter}
+	counter1 := &beat.CountOutputListener{}
+	observer1 := publisher.OutputListener{Listener: counter1}
+	counter2 := &beat.CountOutputListener{}
+	observer2 := publisher.OutputListener{Listener: counter2}
 
 	log, logBuff := logp.NewInMemoryLocal(output, logp.ConsoleEncoderConfig())
 	defer func() {
@@ -131,10 +133,11 @@ func testOutputMetrics(t *testing.T,
 		}
 	}()
 
-	lconsumer, err := consumer.NewLogs(func(ctx context.Context, ld plog.Logs) error {
-		return nil
-	})
-	require.NoError(t, err)
+	lconsumer, err := consumer.NewLogs(
+		func(ctx context.Context, ld plog.Logs) error {
+			return nil
+		})
+	require.NoError(t, err, "could not create log consumer")
 	beatInfo := beat.Info{
 		Logger:      log,
 		LogConsumer: lconsumer,
@@ -151,26 +154,32 @@ func testOutputMetrics(t *testing.T,
 		cfg)
 	require.NoError(t, err, "could not create output group")
 
-	var evs []publisher.Event
-	for _, fields := range evFields {
-		ev := publisher.Event{
-			Content: beat.Event{
-				Timestamp:  time.Time{},
-				Meta:       nil,
-				Fields:     fields,
-				Private:    nil,
-				TimeSeries: false,
-			},
-			OutputListener: observer,
+	eventsForInput := func(observer publisher.OutputListener) []publisher.Event {
+		var evs []publisher.Event
+		for _, fields := range evFields {
+			ev := publisher.Event{
+				Content: beat.Event{
+					Timestamp:  time.Time{},
+					Meta:       nil,
+					Fields:     fields,
+					Private:    nil,
+					TimeSeries: false,
+				},
+				OutputListener: observer,
+			}
+
+			if og.EncoderFactory != nil {
+				encoderFactory := og.EncoderFactory()
+				e, _ := encoderFactory.EncodeEntry(ev)
+				ev = e.(publisher.Event)
+			}
+			evs = append(evs, ev)
 		}
 
-		if og.EncoderFactory != nil {
-			encoderFactory := og.EncoderFactory()
-			e, _ := encoderFactory.EncodeEntry(ev)
-			ev = e.(publisher.Event)
-		}
-		evs = append(evs, ev)
+		return evs
 	}
+	evs := eventsForInput(observer1)
+	evs = append(evs, eventsForInput(observer2)...)
 
 	client := og.Clients[0]
 	if connectable, ok := client.(outputs.Connectable); ok {
@@ -178,7 +187,6 @@ func testOutputMetrics(t *testing.T,
 			"could not connect %s", client.String())
 	}
 
-	// Try publishing a batch that can be split
 	err = client.Publish(context.Background(), &mockBatch{evs: evs})
 	require.NoError(t, err, "could not publish events")
 	require.NoError(t, og.Clients[0].Close(), "failed to close output client")
@@ -186,24 +194,41 @@ func testOutputMetrics(t *testing.T,
 	snapshot := monitoring.CollectStructSnapshot(reg, monitoring.Full, false)
 	events := snapshot["events"].(map[string]any)
 
-	evAcked := events["acked"].(int64)
-	evNew := events["total"].(int64)
-	evDropped := events["dropped"].(int64)
-	evDeadLetter := events["dead_letter"].(int64)
-	evDuplicated := events["duplicates"].(int64)
-	evTooMany := events["toomany"].(int64)
-	evRetrieable := events["failed"].(int64)
+	globalAcked := events["acked"].(int64)
+	globalNew := events["total"].(int64)
+	globalDropped := events["dropped"].(int64)
+	globalDeadLetter := events["dead_letter"].(int64)
+	globalDuplicated := events["duplicates"].(int64)
+	globalTooMany := events["toomany"].(int64)
+	globalRetrieable := events["failed"].(int64)
 
-	assert.Equal(t, evNew, counter.NewLoad())
-	assert.Equal(t, evAcked, counter.AckedLoad())
-	assert.Equal(t, evDropped, counter.DroppedLoad())
-	assert.Equal(t, evDeadLetter, counter.DeadLetterLoad())
-	assert.Equal(t, evDuplicated, counter.DuplicateEventsLoad())
-	assert.Equal(t, evTooMany, counter.ErrTooManyLoad())
-	assert.Equal(t, evRetrieable, counter.RetryableErrorsLoad())
+	wantEventsPerInput := int64(len(evFields))
+
+	// Check that each input saw the correct number of initial events.
+	assert.Equal(t,
+		wantEventsPerInput, counter1.NewLoad(), "Input 1 total events mismatch")
+	assert.Equal(t,
+		wantEventsPerInput, counter2.NewLoad(), "Input 2 total events mismatch")
+
+	// Check that global metrics are the sum of individual observer metrics.
+	assert.Equal(t, globalNew, counter1.NewLoad()+counter2.NewLoad(),
+		"Global NewTotal mismatch with sum of counters")
+	assert.Equal(t, globalAcked, counter1.AckedLoad()+counter2.AckedLoad(),
+		"Global Acked mismatch with sum of counters")
+	assert.Equal(t, globalDropped, counter1.DroppedLoad()+counter2.DroppedLoad(),
+		"Global Dropped mismatch with sum of counters")
+	assert.Equal(t, globalDeadLetter, counter1.DeadLetterLoad()+counter2.DeadLetterLoad(),
+		"Global DeadLetter mismatch with sum of counters")
+	assert.Equal(t, globalDuplicated, counter1.DuplicateEventsLoad()+counter2.DuplicateEventsLoad(),
+		"Global Duplicates mismatch with sum of counters")
+	assert.Equal(t, globalTooMany, counter1.ErrTooManyLoad()+counter2.ErrTooManyLoad(),
+		"Global TooMany mismatch with sum of counters")
+	assert.Equal(t, globalRetrieable, counter1.RetryableErrorsLoad()+counter2.RetryableErrorsLoad(),
+		"Global Retriable/Failed mismatch with sum of counters")
 
 	if t.Failed() {
-		t.Log("OutputListener metrics: ", counter)
+		t.Log("Input1 metrics: ", counter1)
+		t.Log("Input2 metrics: ", counter2)
 
 		snapshotJson, err := json.Marshal(snapshot)
 		require.NoErrorf(t, err,
