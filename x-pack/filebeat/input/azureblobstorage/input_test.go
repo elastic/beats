5820836a365f4251926c1b594d6803a6cd66afc3
Shourie Ganguly <shourie.ganguly@elastic.co>
2025-06-16 16:36:16 +0530

    [filebeat][ABS] - Added missing "text/csv" content-type filter support (#44824)
    Currently content-type "text/csv" did not have official filtering
    support even though we support  csv decoding. This made blobs having this
    content type defined were not picked up for processing. This PR as a "bugfix",
    adds support for explicit "text/csv" content-type filtering in the
    scheduler.

--- x-pack/filebeat/input/azureblobstorage/input_test.go
+++ x-pack/filebeat/input/azureblobstorage/input_test.go
@@ -520,6 +520,30 @@ func Test_StorageClient(t *testing.T) {
 				mock.BeatsFilesContainer_csv[1]: true,
 			},
 		},
+		{
+			name: "ReadCSV",
+			baseConfig: map[string]interface{}{
+				"account_name":                        "beatsblobnew",
+				"auth.shared_credentials.account_key": "7pfLm1betGiRyyABEM/RFrLYlafLZHbLtGhB52LkWVeBxE7la9mIvk6YYAbQKYE/f0GdhiaOZeV8+AStsAdr/Q==",
+				"max_workers":                         1,
+				"poll":                                true,
+				"poll_interval":                       "10s",
+				"decoding.codec.csv.enabled":          true,
+				"decoding.codec.csv.comma":            " ",
+				"containers": []map[string]interface{}{
+					{
+						"name":                       beatsCSVContainer,
+						"decoding.codec.csv.enabled": true,
+						"decoding.codec.csv.comma":   " ",
+					},
+				},
+			},
+			mockHandler: mock.AzureStorageFileServer,
+			expected: map[string]bool{
+				mock.BeatsFilesContainer_csv[0]: true,
+				mock.BeatsFilesContainer_csv[1]: true,
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
