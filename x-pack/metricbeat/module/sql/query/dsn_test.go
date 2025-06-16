5479c2999ab8a6a59088d2a4e8fcd0c44d155710
Mykola Kmet <mykola.kmet@elastic.co>
2025-06-16 17:08:49 +0300

    [metricbeat] [sql] dsn_test.go: address go-lint error (goimport)

--- x-pack/metricbeat/module/sql/query/dsn_test.go
+++ x-pack/metricbeat/module/sql/query/dsn_test.go
@@ -10,9 +10,10 @@ import (
 	"os"
 	"testing"
 
-	"github.com/elastic/elastic-agent-libs/transport/tlscommon"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/elastic/elastic-agent-libs/transport/tlscommon"
 )
 
 const (
