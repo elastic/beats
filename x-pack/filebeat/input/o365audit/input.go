75fc147f35b4b6f5d7bfc66c8c44a2b404d6e586
Shaunak Kashyap <ycombinator@gmail.com>
2025-06-23 23:22:50 -0700

    Remove ExcludeForFIPS implementation

--- x-pack/filebeat/input/o365audit/input.go
+++ x-pack/filebeat/input/o365audit/input.go
@@ -53,6 +53,7 @@ type apiEnvironment struct {
 }
 
 func Plugin(log *logp.Logger, store statestore.States) v2.Plugin {
+	fmt.Println("in o365audit plugin")
 	return v2.Plugin{
 		Name:       pluginName,
 		Stability:  feature.Experimental,
