d9c9651671f4a2f5d13472c72f9204a18b8c42ef
Michael Bischoff <mjmbischoff@controplex.com>
2025-06-24 14:33:27 +0200

    Adding the now processor (#44795)
    This commit adds the now processor to allow setting the current timestamp to fields like event.created without having to resort to using the script processor.
    ---------
    
    Co-authored-by: Fabrizio Ferri-Benedetti <algernon@fastmail.com>
    (cherry picked from commit 6e0bab6580b87a8dcb49c69bb4daa3d5b2cf8540)

---
+++ libbeat/processors/now/now_test.go
@@ -0,0 +1,175 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package now
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/elastic/beats/v7/libbeat/beat"
+	"github.com/elastic/elastic-agent-libs/config"
+	"github.com/elastic/elastic-agent-libs/logp/logptest"
+	"github.com/elastic/elastic-agent-libs/mapstr"
+)
+
+func TestNow(t *testing.T) {
+	es9ReleaseDate := time.Date(2025, 04, 8, 12, 0, 0, 42, time.UTC)
+	currentTime = func() time.Time { return es9ReleaseDate }
+
+	var testCases = []struct {
+		description string
+		config      nowConfig
+		Input       mapstr.M
+		Output      mapstr.M
+		error       bool
+		badConfig   bool
+	}{
+		{
+			description: "Single target field now",
+			config: nowConfig{
+				Field: "field1",
+			},
+			Output: mapstr.M{
+				"field1": es9ReleaseDate,
+			},
+			error:     false,
+			badConfig: false,
+		},
+		{
+			description: "Target field with now plus existing field",
+			config: nowConfig{
+				Field: "field1",
+			},
+			Input: mapstr.M{
+				"field2": "some data",
+			},
+			Output: mapstr.M{
+				"field1": es9ReleaseDate,
+				"field2": "some data",
+			},
+			error:     false,
+			badConfig: false,
+		},
+		{
+			description: "Target with existing value",
+			config: nowConfig{
+				Field: "field1",
+			},
+			Input: mapstr.M{
+				"field1": "existing data",
+				"field2": "some data",
+			},
+			Output: mapstr.M{
+				"field1": es9ReleaseDate,
+				"field2": "some data",
+			},
+			error:     false,
+			badConfig: false,
+		},
+		{
+			description: "Target with dot's (nested field)",
+			config: nowConfig{
+				Field: "nested.field1",
+			},
+			Input: mapstr.M{
+				"input": "should equal output",
+			},
+			Output: mapstr.M{
+				"nested": mapstr.M{
+					"field1": es9ReleaseDate,
+				},
+				"input": "should equal output",
+			},
+			error:     false,
+			badConfig: false,
+		},
+		{
+			description: "Target with dot's and leaf value along the path, causing error",
+			config: nowConfig{
+				Field: "nested.field1",
+			},
+			Input: mapstr.M{
+				"nested": "existing 'leaf' data",
+				"input":  "should equal output",
+			},
+			Output: mapstr.M{
+				"nested": "existing 'leaf' data",
+				"input":  "should equal output",
+			},
+			error:     true,
+			badConfig: false,
+		},
+		{
+			description: "Bad config, no field set",
+			config:      nowConfig{},
+			error:       true,
+			badConfig:   true,
+		},
+	}
+
+	for _, test := range testCases {
+		test := test
+		t.Run(test.description, func(t *testing.T) {
+			t.Parallel()
+
+			configInput, err := config.NewConfigFrom(test.config)
+			require.NoError(t, err, "Failed to create config from test case")
+
+			processor, err := New(configInput, logptest.NewTestingLogger(t, "now"))
+			if test.badConfig {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+
+			inputEvent := &beat.Event{
+				Fields: test.Input,
+			}
+
+			outputEvent, err := processor.Run(inputEvent)
+			if !test.error {
+				assert.NoError(t, err)
+			} else {
+				assert.Error(t, err)
+			}
+
+			assert.Equal(t, test.Output, outputEvent.Fields, "Output event does not match expected")
+		})
+	}
+}
+
+func TestEffectiveEventRendering(t *testing.T) {
+	es9ReleaseDate := time.Date(2025, 04, 8, 12, 0, 0, 42, time.UTC)
+	currentTime = func() time.Time { return es9ReleaseDate }
+
+	configInput, err := config.NewConfigFrom(nowConfig{
+		Field: "target",
+	})
+	require.NoError(t, err)
+
+	processor, err := New(configInput, logptest.NewTestingLogger(t, "now"))
+	require.NoError(t, err)
+
+	outputEvent, err := processor.Run(&beat.Event{})
+	require.NoError(t, err)
+
+	assert.Equal(t, "{\"target\":\"2025-04-08T12:00:00.000000042Z\"}", outputEvent.Fields.String(), "Date is not rendered as expected")
+}
