f8871fbae24b90d327afe46afd20649d011421b3
Fae Charlton <fae.charlton@elastic.co>
2025-06-26 17:41:13 -0400

    lint

--- libbeat/outputs/elasticsearch/client.go
+++ libbeat/outputs/elasticsearch/client.go
@@ -43,9 +43,9 @@ import (
 var (
 	errPayloadTooLarge = errors.New("the bulk payload is too large for the server. Consider to adjust `http.max_content_length` parameter in Elasticsearch or `bulk_max_size` in the beat. The batch has been dropped")
 
-	ErrTooOld = errors.New("Elasticsearch is too old. Please upgrade the instance. If you would like to connect to older instances set output.elasticsearch.allow_older_versions to true") //nolint:staticcheck //false positive
+	ErrTooOld = errors.New("Elasticsearch is too old. Please upgrade the instance. If you would like to connect to older instances set output.elasticsearch.allow_older_versions to true") //nolint:staticcheck //false positive (Elasticsearch should be capitalized)
 
-	errTooMany = errors.New("Elasticsearch returned error 429 Too Many Requests, throttling connection")
+	errTooMany = errors.New("Elasticsearch returned error 429 Too Many Requests, throttling connection") //nolint:staticcheck //false positive (Elasticsearch should be capitalized)
 )
 
 // Client is an elasticsearch client.
