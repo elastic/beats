3b0113832b37d2173a60132027c788fd4a0af6f2
Shourie Ganguly <shourie.ganguly@elastic.co>
2025-06-27 17:11:45 +0530

    refactored testdata folder

--- x-pack/filebeat/input/gcs/mock/mock.go
+++ x-pack/filebeat/input/gcs/mock/mock.go
@@ -67,7 +67,7 @@ func GCSFileServer() http.Handler {
 						return
 					}
 				} else if fileBuckets[path[0]] && availableFileObjects[path[0]][path[1]] {
-					absPath, _ := filepath.Abs("mock/testdata/" + path[1])
+					absPath, _ := filepath.Abs("testdata/" + path[1])
 					data, _ := os.ReadFile(absPath)
 					switch path[1] {
 					case "multiline.json":
