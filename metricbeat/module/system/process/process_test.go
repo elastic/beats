c7b04e14c3a112b0ab0080ae778afbf68c0aba75
Khushi Jain <khushi.jain@elastic.co>
2025-07-07 15:24:10 +0530

    make check

--- metricbeat/module/system/process/process_test.go
+++ metricbeat/module/system/process/process_test.go
@@ -30,7 +30,6 @@ import (
 	"github.com/elastic/beats/v7/metricbeat/mb"
 	mbtest "github.com/elastic/beats/v7/metricbeat/mb/testing"
 	_ "github.com/elastic/beats/v7/metricbeat/module/system"
-	"github.com/elastic/elastic-agent-libs/logp"
 	"github.com/elastic/elastic-agent-system-metrics/metric/system/process"
 )
 
