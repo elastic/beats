bb9a4e31dc1f5ffc955da9f409cfbcd4105c8247
Tiago Queiroz <tiago.queiroz@elastic.co>
2025-07-09 13:01:27 -0400

    Add tests for the UDP input

---
+++ filebeat/input/inputtest/netInput.go
@@ -0,0 +1,80 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package inputtest
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/elastic/beats/v7/libbeat/monitoring/inputmon"
+)
+
+type NetInputMetrics struct {
+	Published int `json:"events_published"`
+	Read      int `json:"events_read"`
+	Packets   int `json:"received_events_total"`
+}
+
+func GetNetInputMetrics(t *testing.T) NetInputMetrics {
+	data, err := inputmon.MetricSnapshotJSON(nil)
+	if err != nil {
+		t.Fatalf("cannot get metrics snapshot: %s", err)
+	}
+
+	metrics := []NetInputMetrics{}
+
+	if err := json.Unmarshal(data, &metrics); err != nil {
+		t.Fatalf("cannot read metrics: %s", err)
+	}
+
+	if len(metrics) == 0 {
+		return NetInputMetrics{}
+	}
+
+	return metrics[0]
+}
+
+func RequireNetInputMetrics(t *testing.T, timeout time.Duration, want NetInputMetrics) {
+	msg := &strings.Builder{}
+	require.Eventuallyf(
+		t,
+		func() bool {
+			msg.Reset()
+			got := GetNetInputMetrics(t)
+			fmt.Fprintf(
+				msg,
+				"%d events read, %d events published, packets %d",
+				got.Read,
+				got.Published,
+				got.Packets,
+			)
+			return got.Read == want.Read && got.Published == want.Published && got.Packets == want.Packets
+		},
+		timeout,
+		100*time.Millisecond,
+		"expecting %d evens read, %d published, %d packets. Got %s",
+		want.Read,
+		want.Published,
+		want.Packets,
+		msg)
+}
---
+++ filebeat/input/udp/input_test.go
@@ -0,0 +1,97 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package udp
+
+import (
+	"context"
+	"errors"
+	"net"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/elastic/beats/v7/filebeat/input/inputtest"
+	v2 "github.com/elastic/beats/v7/filebeat/input/v2"
+	"github.com/elastic/beats/v7/filebeat/input/v2/testpipeline"
+	conf "github.com/elastic/elastic-agent-libs/config"
+	"github.com/elastic/elastic-agent-libs/logp"
+)
+
+func TestInput(t *testing.T) {
+	serverAddr := "127.0.0.1:9042"
+	wg := sync.WaitGroup{}
+	inp, err := configure(conf.MustNewConfigFrom(map[string]any{
+		"host": serverAddr,
+	}))
+	if err != nil {
+		t.Fatalf("cannot create input: %s", err)
+	}
+
+	pipeline := testpipeline.NewPipelineConnector()
+	client, _ := pipeline.Connect()
+
+	ctx, cancel := context.WithCancel(t.Context())
+	v2Ctx := v2.Context{
+		ID:          t.Name(),
+		Cancelation: ctx,
+		Logger:      logp.NewNopLogger(),
+	}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := inp.Run(v2Ctx, client); err != nil {
+			if !errors.Is(err, context.Canceled) {
+				t.Errorf("input exited with error: %s", err)
+			}
+		}
+	}()
+
+	// Give the input to start running. Because it's UDP we cannot know from
+	// the client side if the server is up and running.
+	time.Sleep(time.Second)
+
+	runUDPClient(t, serverAddr, []string{"foo", "bar"})
+	inputtest.RequireNetInputMetrics(
+		t,
+		3*time.Second,
+		inputtest.NetInputMetrics{Packets: 2})
+
+	// Stop the input, this removes all metrics
+	cancel()
+
+	// Ensure the input Run method returns
+	wg.Wait()
+}
+
+func runUDPClient(t *testing.T, address string, dataToSend []string) {
+	conn, err := net.Dial("udp", address)
+	if err != nil {
+		t.Fatalf("cannot create connection: %s", err)
+	}
+	defer conn.Close()
+
+	// Send data to the server
+	for _, data := range dataToSend {
+		_, err = conn.Write([]byte(data + "\n"))
+		if err != nil {
+			t.Logf("Error sending data: %s, retrying in 100ms", err)
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+}
