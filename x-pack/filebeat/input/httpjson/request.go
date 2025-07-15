81d304e929c7c5a1802cb1be65c63e30cb9a2314
Dan Kortschak <dan.kortschak@elastic.co>
2025-07-16 09:28:46 +0930

    x-pack/filebeat/input/httpjson: share oauth2 clients where possible (#44976)
    The httpjson input makes new http.Client values for every chain step.
    This is inefficient for a variety of reasons, but the configuration API
    that we expose makes the general fix for this non-trivial. It is,
    however, possible to fix the OAuth2 behaviour that arises from this
    where root parent steps in a chain make use of OAuth2 and so do its
    children, but the children needlessly request tokens because they do
    not have access to the oauth2.Transports token cache. The fix here is
    to retain the prepared OAuth2 client in the relevant config and use
    that if it is non-nil. This does not fix the case that a non-root
    parent shares OAuth2 authentication with its children, but this is
    likely to be uncommon.

--- x-pack/filebeat/input/httpjson/request.go
+++ x-pack/filebeat/input/httpjson/request.go
@@ -325,7 +325,7 @@ func newRequestFactory(ctx context.Context, config config, stat status.StatusRep
 		if ch.Step != nil {
 			ts, _ := newBasicTransformsFromConfig(registeredTransforms, ch.Step.Request.Transforms, requestNamespace, stat, log)
 			ch.Step.Auth = tryAssignAuth(config.Auth, ch.Step.Auth)
-			client, err := newChainHTTPClient(ctx, ch.Step.Auth, ch.Step.Request, stat, log, reg)
+			client, err := newHTTPClient(ctx, ch.Step.Auth, ch.Step.Request, stat, log, reg, nil)
 			if err != nil {
 				return nil, fmt.Errorf("failed in creating chain http client with error: %w", err)
 			}
@@ -352,7 +352,7 @@ func newRequestFactory(ctx context.Context, config config, stat status.StatusRep
 			ts, _ := newBasicTransformsFromConfig(registeredTransforms, ch.While.Request.Transforms, requestNamespace, stat, log)
 			policy := newHTTPPolicy(evaluateResponse, ch.While.Until, stat, log)
 			ch.While.Auth = tryAssignAuth(config.Auth, ch.While.Auth)
-			client, err := newChainHTTPClient(ctx, ch.While.Auth, ch.While.Request, stat, log, reg, policy)
+			client, err := newHTTPClient(ctx, ch.While.Auth, ch.While.Request, stat, log, reg, policy)
 			if err != nil {
 				return nil, fmt.Errorf("failed in creating chain http client with error: %w", err)
 			}
