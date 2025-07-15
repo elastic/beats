d82d3236b103111942970cb177145ac6f7bb1fc6
Khushi Jain <khushi.jain@elastic.co>
2025-07-15 18:58:09 +0530

    correct test

--- libbeat/outputs/fileout/pathformatstring_test.go
+++ libbeat/outputs/fileout/pathformatstring_test.go
@@ -25,11 +25,12 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/elastic/elastic-agent-libs/config"
+	"github.com/elastic/elastic-agent-libs/logp/logptest"
 )
 
 func TestCheckNilDoesntPanic(t *testing.T) {
 	newCfg := config.NewConfig()
-	handler, err := readConfig(newCfg)
+	handler, err := readConfig(newCfg, logptest.NewTestingLogger(t, ""))
 	require.NoError(t, err)
 	_, err = handler.Path.Run(time.Now())
 	require.Error(t, err)
