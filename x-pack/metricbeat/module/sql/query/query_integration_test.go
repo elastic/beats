c0e61adc38eaa8f7f71082b85af6709bfaf97e5f
subham sarkar <subham.sarkar@elastic.co>
2025-07-17 17:53:41 +0530

    Fix CI

--- x-pack/metricbeat/module/sql/query/query_integration_test.go
+++ x-pack/metricbeat/module/sql/query/query_integration_test.go
@@ -371,7 +371,7 @@ func waitForOracleConnection(t *testing.T, host, port string) {
 		if err == nil {
 			conn.Close()
 			// Give Oracle a bit more time to fully initialize
-			time.Sleep(60 * time.Second)
+			time.Sleep(120 * time.Second)
 			return
 		}
 
