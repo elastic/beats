39c08d6eae526c570c2a406e459901091f91bda8
Arjun <pkillarjun@protonmail.com>
2025-07-24 20:14:27 +0530

    [filebeat] revert support for fuzz-testing
    Signed-off-by: Arjun <pkillarjun@protonmail.com>

--- script/test_fuzz.sh
+++ script/test_fuzz.sh
@@ -6,10 +6,6 @@ set -e
 # It's not recomended to run only for 10 minutes, we can add this project to oss-fuzz
 # infrastructure and then run time would be 24/7.
 
-go test -fuzz=FuzzIsRFC5424Format -fuzztime=600s -run=^$ ./filebeat/input/syslog
-go test -fuzz=FuzzParserRFC3164 -fuzztime=600s -run=^$ ./filebeat/input/syslog
-go test -fuzz=FuzzParserRFC5424 -fuzztime=600s -run=^$ ./filebeat/input/syslog
-
 go test -fuzz=FuzzFormat -fuzztime=600s -run=^$ ./libbeat/common/dtfmt
 go test -fuzz=FuzzNew -fuzztime=600s -run=^$ ./libbeat/processors/dissect
 go test -fuzz=FuzzParseRFC3164 -fuzztime=600s -run=^$ ./libbeat/reader/syslog
