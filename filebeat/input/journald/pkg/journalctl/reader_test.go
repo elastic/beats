3e93eb817a2660182799db744075906f65c42e48
kruskal <99559985+kruskall@users.noreply.github.com>
2025-08-04 23:21:03 +0200

    refactor: replace lopg.ObserverLogs with logptest
    ObserverLogs is relying on a testing package in a non-test package
    
    move to logptest

--- filebeat/input/journald/pkg/journalctl/reader_test.go
+++ filebeat/input/journald/pkg/journalctl/reader_test.go
@@ -31,6 +31,7 @@ import (
 	"github.com/elastic/beats/v7/filebeat/input/journald/pkg/journalfield"
 	input "github.com/elastic/beats/v7/filebeat/input/v2"
 	"github.com/elastic/elastic-agent-libs/logp"
+	"github.com/elastic/elastic-agent-libs/logp/logptest"
 )
 
 //go:embed testdata/corner-cases.json
@@ -71,7 +72,7 @@ func TestEventWithNonStringData(t *testing.T) {
 var jdEvent []byte
 
 func TestRestartsJournalctlOnError(t *testing.T) {
-	logp.DevelopmentSetup(logp.ToObserverOutput())
+	logger, observedLogs := logptest.NewTestingLoggerWithObserver(t, "")
 	ctx := context.Background()
 
 	mock := JctlMock{
@@ -100,7 +101,7 @@ func TestRestartsJournalctlOnError(t *testing.T) {
 		return &mock, nil
 	}
 
-	reader, err := New(logp.L(), ctx, nil, nil, nil, journalfield.IncludeMatches{}, []int{}, SeekHead, "", 0, "", factory)
+	reader, err := New(logger, ctx, nil, nil, nil, journalfield.IncludeMatches{}, []int{}, SeekHead, "", 0, "", factory)
 	if err != nil {
 		t.Fatalf("cannot instantiate journalctl reader: %s", err)
 	}
@@ -129,7 +130,7 @@ func TestRestartsJournalctlOnError(t *testing.T) {
 	//  - reader error: 'journalctl exited with code 42', restarting...
 	//  - starting new mock journalclt ID: 2
 
-	logs := logp.ObserverLogs().TakeAll()
+	logs := observedLogs.TakeAll()
 	if len(logs) != 3 {
 		t.Fatalf("expecting 3 log lines from 'input.journald.reader.journalctl-runner', got %d", len(logs))
 	}
