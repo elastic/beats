d354f2b0d9c1d1e5c729b79fac026c57d20433d9
Edmo Vamerlatti Costa <11836452+edmocosta@users.noreply.github.com>
2025-08-08 17:20:59 +0200

    [OTel] Add beat name and version into the OTel consumer context (#45744)
    This commit added the beats name and version into the Collector context.Context using the standard client.Info metadata, so components can access it downstream.
    
    (cherry picked from commit 5b9e5a96edf0ad2422d03b642cb11fe6fe5a591d)
    
    # Conflicts:
    #	NOTICE.txt
    #	go.mod

--- x-pack/libbeat/outputs/otelconsumer/otelconsumer.go
+++ x-pack/libbeat/outputs/otelconsumer/otelconsumer.go
@@ -10,6 +10,8 @@ import (
 	"runtime"
 	"time"
 
+	"go.opentelemetry.io/collector/client"
+
 	"github.com/elastic/beats/v7/libbeat/beat"
 	"github.com/elastic/beats/v7/libbeat/common"
 	"github.com/elastic/beats/v7/libbeat/otelbeat/otelmap"
@@ -28,6 +30,8 @@ import (
 const (
 	// esDocumentIDAttribute is the attribute key used to store the document ID in the log record.
 	esDocumentIDAttribute = "elasticsearch.document_id"
+	beatNameCtxKey        = "beat_name"
+	beatVersionCtxtKey    = "beat_version"
 )
 
 func init() {
@@ -150,7 +154,7 @@ func (out *otelConsumer) logsPublish(ctx context.Context, batch publisher.Batch)
 		}
 	}
 
-	err := out.logsConsumer.ConsumeLogs(ctx, pLogs)
+	err := out.logsConsumer.ConsumeLogs(out.newConsumerContext(ctx), pLogs)
 	if err != nil {
 		// Permanent errors shouldn't be retried. This tipically means
 		// the data cannot be serialized by the exporter that is attached
@@ -173,6 +177,19 @@ func (out *otelConsumer) logsPublish(ctx context.Context, batch publisher.Batch)
 	st.NewBatch(len(events))
 	st.AckedEvents(len(events))
 	return nil
+}
+
+// newConsumerContext creates a new context.Context adding the beats metadata
+// to the client.Info. This is used to pass the beat name and version to the
+// Collector, so it can be used by the components to access that data.
+func (out *otelConsumer) newConsumerContext(ctx context.Context) context.Context {
+	clientInfo := client.Info{
+		Metadata: client.NewMetadata(map[string][]string{
+			beatNameCtxKey:     {out.beatInfo.Beat},
+			beatVersionCtxtKey: {out.beatInfo.Version},
+		}),
+	}
+	return client.NewContext(ctx, clientInfo)
 }
 
 func (out *otelConsumer) String() string {
