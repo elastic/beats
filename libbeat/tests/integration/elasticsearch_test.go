1ca56eb6e02278bbab12dd3d1562ad77be08b06b
Anderson Queiroz <anderson.queiroz@elastic.co>
2025-09-09 08:00:59 +0200

    fix merge conflicts

--- libbeat/tests/integration/elasticsearch_test.go
+++ libbeat/tests/integration/elasticsearch_test.go
@@ -20,32 +20,21 @@
 package integration
 
 import (
-<<<<<<< HEAD
 	"errors"
+	"fmt"
 	"io"
 	"net/http"
-=======
-	"context"
-	"fmt"
-	"net/url"
 	"os"
 	"path/filepath"
->>>>>>> 716277334 (libbeat: add 'eventfd2' to default seccomp policy (#46372))
 	"testing"
 	"time"
 
 	"github.com/gofrs/uuid/v5"
 	"github.com/rcrowley/go-metrics"
 	"github.com/stretchr/testify/require"
-<<<<<<< HEAD
-
-	"github.com/elastic/mock-es/pkg/api"
-=======
-	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
-	"go.opentelemetry.io/otel/sdk/metric/metricdata"
 
 	"github.com/elastic/elastic-agent-libs/testing/certutil"
->>>>>>> 716277334 (libbeat: add 'eventfd2' to default seccomp policy (#46372))
+	"github.com/elastic/mock-es/pkg/api"
 )
 
 var esCfg = `
@@ -109,37 +98,11 @@ func TestESOutputRecoversFromNetworkError(t *testing.T) {
 	s.Close()
 }
 
-<<<<<<< HEAD
-func startMockES(t *testing.T, addr string) (*http.Server, metrics.Registry) {
-	uid := uuid.Must(uuid.NewV4())
-	mr := metrics.NewRegistry()
-	es := api.NewAPIHandler(uid, "foo2", mr, time.Now().Add(24*time.Hour), 0, 0, 0, 0, 0)
-
-	s := http.Server{Addr: addr, Handler: es, ReadHeaderTimeout: time.Second}
-	go func() {
-		if err := s.ListenAndServe(); !errors.Is(http.ErrServerClosed, err) {
-			t.Errorf("could not start mock-es server: %s", err)
-		}
-	}()
-
-	require.Eventually(t, func() bool {
-		resp, err := http.Get("http://" + addr) //nolint: noctx // It's just a test
-		if err != nil {
-			//nolint: errcheck // We're just draining the body, we can ignore the error
-			io.Copy(io.Discard, resp.Body)
-			resp.Body.Close()
-			return false
-		}
-		return true
-	},
-		time.Second, time.Millisecond, "mock-es server did not start on '%s'", addr)
-
-	return &s, mr
-=======
 func TestReloadCA(t *testing.T) {
 	mockbeat := NewBeat(t, "mockbeat", "../../libbeat.test")
 
-	s, esAddr, _, _ := StartMockES(t, ":4242", 0, 0, 0, 0, 0)
+	esAddr := "localhost:4242"
+	s, _ := startMockES(t, esAddr)
 	defer s.Close()
 
 	_, _, pair, err := certutil.NewRootCA()
@@ -162,7 +125,7 @@ logging.level: debug
 	mockbeat.Start()
 
 	// 1. wait mockbeat to start
-	mockbeat.WaitLogsContains(
+	mockbeat.WaitForLogs(
 		fmt.Sprint("mockbeat start running"),
 		10*time.Second,
 		"did not find 'mockbeat start running' log")
@@ -172,23 +135,49 @@ logging.level: debug
 	require.NoError(t, err, "could not rotate CA")
 
 	// 3. Wait for cert change detection logs
-	mockbeat.WaitLogsContains(
+	mockbeat.WaitForLogs(
 		fmt.Sprintf("some of the following files have been modified: [%s]", caPath),
 		10*time.Second,
 		"did not detect CA rotation")
 
 	// 4. Wait for CA load log
-	mockbeat.WaitLogsContains(
+	mockbeat.WaitForLogs(
 		fmt.Sprintf("Successfully loaded CA certificate: %s", caPath),
 		10*time.Second,
 		"did not find 'Successfully loaded CA' log")
 
 	// 5. wait mockbeat to start again
-	mockbeat.WaitLogsContains(
+	mockbeat.WaitForLogs(
 		fmt.Sprint("mockbeat start running"),
 		10*time.Second,
 		"did not find 'mockbeat start running' log again")
->>>>>>> 716277334 (libbeat: add 'eventfd2' to default seccomp policy (#46372))
+}
+
+func startMockES(t *testing.T, addr string) (*http.Server, metrics.Registry) {
+	uid := uuid.Must(uuid.NewV4())
+	mr := metrics.NewRegistry()
+	es := api.NewAPIHandler(uid, "foo2", mr, time.Now().Add(24*time.Hour), 0, 0, 0, 0, 0)
+
+	s := http.Server{Addr: addr, Handler: es, ReadHeaderTimeout: time.Second}
+	go func() {
+		if err := s.ListenAndServe(); !errors.Is(http.ErrServerClosed, err) {
+			t.Errorf("could not start mock-es server: %s", err)
+		}
+	}()
+
+	require.Eventually(t, func() bool {
+		resp, err := http.Get("http://" + addr) // nolint: noctx // It's just a test
+		if err != nil {
+			// nolint: errcheck // We're just draining the body, we can ignore the error
+			io.Copy(io.Discard, resp.Body)
+			resp.Body.Close()
+			return false
+		}
+		return true
+	},
+		time.Second, time.Millisecond, "mock-es server did not start on '%s'", addr)
+
+	return &s, mr
 }
 
 // waitForEventToBePublished waits for at least one event published
@@ -204,25 +193,11 @@ func waitForEventToBePublished(t *testing.T, mr metrics.Registry) {
 			return false
 		}
 
-<<<<<<< HEAD
 		sc, ok := total.(*metrics.StandardCounter)
 		if !ok {
 			t.Fatalf("expecting 'bulk.create.total' to be *metrics.StandardCounter, but got '%T' instead",
 				total,
 			)
-=======
-		for _, sm := range rm.ScopeMetrics {
-			for _, m := range sm.Metrics {
-				if m.Name == "bulk.create.total" {
-					total := int64(0)
-					//nolint:errcheck // It's a test
-					for _, dp := range m.Data.(metricdata.Sum[int64]).DataPoints {
-						total += dp.Value
-					}
-					return total >= 1
-				}
-			}
->>>>>>> 716277334 (libbeat: add 'eventfd2' to default seccomp policy (#46372))
 		}
 
 		return sc.Count() > 1
