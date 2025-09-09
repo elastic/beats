39f9c4737107020572cb6a8545ad7261306825c2
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2025-09-09 09:07:11 -0300

    fix imports

--- x-pack/metricbeat/mbreceiver/receiver_leak_test.go
+++ x-pack/metricbeat/mbreceiver/receiver_leak_test.go
@@ -12,6 +12,7 @@ import (
 	"time"
 
 	"github.com/elastic/beats/v7/libbeat/otelbeat/oteltest"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component"
