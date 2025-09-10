76cacea41a08eb83979b519fd84e3579a3a553d4
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2025-09-10 18:56:33 -0300

    tentative fix for TestReceiverDegraded, TestConsumeContract and TestFilestreamTruncateBlockedOutput
    These tests are currently marked as flaky.
    
    TestReceiverDegraded and TestConsumeContract are failing on linux/arm64
    due to goroutine leaks from TCP clients.
    
    TestFilestreamTruncateBlockedOutput is likely failing due to a small
    assertion time in env.requireOffsetInRegistry.
    
    For the goroutine leaks, allow the TCP client goroutines, since they are
    hard to track and could be from other tests. The stacktrace only goes as
    far as the standard library os package.
    
    For the filestream test, increase the timeout for the assertion.

--- libbeat/otelbeat/oteltest/oteltest.go
+++ libbeat/otelbeat/oteltest/oteltest.go
@@ -20,6 +20,7 @@ package oteltest
 
 import (
 	"context"
+	"runtime"
 	"strings"
 	"sync"
 	"testing"
@@ -208,7 +209,7 @@ func CheckReceivers(params CheckReceiversParams) {
 
 // VerifyNoLeaks fails the test if any goroutines are leaked during the test.
 func VerifyNoLeaks(t *testing.T) {
-	goleak.VerifyNone(t,
+	skipped := []goleak.Option{
 		// Skip known leaks:
 		// Possibly fixed by https://github.com/elastic/beats/pull/46417
 		goleak.IgnoreAnyFunction("github.com/elastic/beats/v7/libbeat/publisher/queue/memqueue.(*ackLoop).run"),
@@ -218,5 +219,13 @@ func VerifyNoLeaks(t *testing.T) {
 		// False positive, from init in cloud.google.com/go/pubsub and filebeat/input/gcppubsub.
 		// See https://github.com/googleapis/google-cloud-go/issues/10948
 		// and https://github.com/census-instrumentation/opencensus-go/issues/1191
-		goleak.IgnoreAnyFunction("go.opencensus.io/stats/view.(*worker).start"))
+		goleak.IgnoreAnyFunction("go.opencensus.io/stats/view.(*worker).start"),
+	}
+
+	if runtime.GOOS == "linux" && runtime.GOARCH == "arm64" {
+		// On arm64, some HTTP transport goroutines are leaked while still dialing.
+		skipped = append(skipped, goleak.IgnoreAnyFunction("net/http.(*Transport).startDialConnForLocked"))
+	}
+
+	goleak.VerifyNone(t, skipped...)
 }
