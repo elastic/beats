6f8616ce9e869706910dedc86695dc6742dc7fa2
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2025-09-10 11:40:20 -0300

    remove duplicated client import

--- x-pack/libbeat/outputs/otelconsumer/otelconsumer_test.go
+++ x-pack/libbeat/outputs/otelconsumer/otelconsumer_test.go
@@ -15,7 +15,6 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"go.opentelemetry.io/collector/client"
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/consumer/consumererror"
 	"go.opentelemetry.io/collector/pdata/pcommon"
