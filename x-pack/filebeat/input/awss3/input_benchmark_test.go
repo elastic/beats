18ce1a04171874580882ba9d2bedba1f80c60b03
kruskal <99559985+kruskall@users.noreply.github.com>
2025-09-14 15:18:46 +0200

    test: use test output to write table

--- x-pack/filebeat/input/awss3/input_benchmark_test.go
+++ x-pack/filebeat/input/awss3/input_benchmark_test.go
@@ -446,7 +446,7 @@ func TestBenchmarkInputS3(t *testing.T) {
 		})
 	}
 
-	w := tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', tabwriter.Debug)
+	w := tabwriter.NewWriter(t.Output(), 1, 1, 1, ' ', tabwriter.Debug)
 	fmt.Fprintln(w, strings.Join(headers, "\t"))
 	for _, d := range data {
 		fmt.Fprintln(w, strings.Join(d, "\t"))
