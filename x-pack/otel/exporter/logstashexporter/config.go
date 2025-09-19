4ce6678ce1f152efcae3f926d4cd54644cdda62f
edmocosta <11836452+edmocosta@users.noreply.github.com>
2025-09-19 14:32:31 +0200

    Add logstash exporter logic

--- x-pack/otel/exporter/logstashexporter/config.go
+++ x-pack/otel/exporter/logstashexporter/config.go
@@ -4,4 +4,36 @@
 
 package logstashexporter
 
+import (
+	"go.opentelemetry.io/collector/component"
+
+	"github.com/elastic/beats/v7/libbeat/outputs"
+	"github.com/elastic/beats/v7/libbeat/outputs/logstash"
+	"github.com/elastic/elastic-agent-libs/config"
+)
+
 type Config map[string]any
+
+func createDefaultConfig() component.Config {
+	return &Config{}
+}
+
+type logstashOutputConfig struct {
+	outputs.HostWorkerCfg `config:",inline"`
+	logstash.Config       `config:",inline"`
+}
+
+func parseLogstashConfig(cfg *component.Config) (*config.C, *logstashOutputConfig, error) {
+	rawConfig, err := config.NewConfigFrom(&cfg)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	parsedConfig := logstashOutputConfig{}
+	err = rawConfig.Unpack(&parsedConfig)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return rawConfig, &parsedConfig, nil
+}
