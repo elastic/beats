dfb2e51aaceba90db5935d284f214cab708b7823
Vihas <vihas.makwana@elastic.co>
2025-09-25 16:27:31 +0530

    merge conflicts

--- filebeat/beater/filebeat.go
+++ filebeat/beater/filebeat.go
@@ -129,28 +129,6 @@ func newBeater(b *beat.Beat, plugins PluginFactory, rawConfig *conf.C) (beat.Bea
 		}
 	}
 
-<<<<<<< HEAD
-	if b.Manager != nil {
-		b.Manager.RegisterDiagnosticHook("input_metrics", "Metrics from active inputs.",
-			"input_metrics.json", "application/json", func() []byte {
-				data, err := inputmon.MetricSnapshotJSON(b.Monitoring.InputsRegistry())
-				if err != nil {
-					b.Info.Logger.Warnw("Failed to collect input metric snapshot for Agent diagnostics.", "error", err)
-					return []byte(err.Error())
-				}
-				return data
-			})
-
-		b.Manager.RegisterDiagnosticHook(
-			"registry",
-			"Filebeat's registry",
-			"registry.tar.gz",
-			"application/octet-stream",
-			gzipRegistry)
-	}
-
-=======
->>>>>>> 32b677700 ([otel] - implement RegisterDiagnosticHook for otel manager (#46753))
 	// Add inputs created by the modules
 	config.Inputs = append(config.Inputs, moduleInputs...)
 
