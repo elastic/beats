08d816e47b2bd8c35e6229d3e87b848385d80994
Fae Charlton <fae.charlton@elastic.co>
2025-10-21 15:29:56 -0400

    Remove unused tests

--- libbeat/publisher/queue/memqueue/queue_test.go
+++ libbeat/publisher/queue/memqueue/queue_test.go
@@ -21,7 +21,6 @@ import (
 	"encoding/binary"
 	"flag"
 	"fmt"
-	"math"
 	"math/rand/v2"
 	"sync"
 	"sync/atomic"
@@ -276,32 +275,6 @@ func makeTestQueue(sz, minEvents int, flushTimeout time.Duration) queuetest.Queu
 			FlushTimeout:  flushTimeout,
 		}, 0, nil)
 	}
-}
-
-func TestAdjustInputQueueSize(t *testing.T) {
-	t.Run("zero yields default value (main queue size=0)", func(t *testing.T) {
-		assert.Equal(t, minInputQueueSize, AdjustInputQueueSize(0, 0))
-	})
-	t.Run("zero yields default value (main queue size=10)", func(t *testing.T) {
-		assert.Equal(t, minInputQueueSize, AdjustInputQueueSize(0, 10))
-	})
-	t.Run("can't go below min", func(t *testing.T) {
-		assert.Equal(t, minInputQueueSize, AdjustInputQueueSize(1, 0))
-	})
-	t.Run("can set any value within bounds", func(t *testing.T) {
-		for q, mainQueue := minInputQueueSize+1, 4096; q < int(float64(mainQueue)*maxInputQueueSizeRatio); q += 10 {
-			assert.Equal(t, q, AdjustInputQueueSize(q, mainQueue))
-		}
-	})
-	t.Run("can set any value if no upper bound", func(t *testing.T) {
-		for q := minInputQueueSize + 1; q < math.MaxInt32; q *= 2 {
-			assert.Equal(t, q, AdjustInputQueueSize(q, 0))
-		}
-	})
-	t.Run("can't go above upper bound", func(t *testing.T) {
-		mainQueue := 4096
-		assert.Equal(t, int(float64(mainQueue)*maxInputQueueSizeRatio), AdjustInputQueueSize(mainQueue, mainQueue))
-	})
 }
 
 func TestBatchFreeEntries(t *testing.T) {
