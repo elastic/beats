4c75531b5d0da3b891a12be9d84adcc2201eb272
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2025-10-23 16:05:48 -0300

    Update TestHTTPJSONInputOTel

--- x-pack/filebeat/tests/integration/otel_test.go
+++ x-pack/filebeat/tests/integration/otel_test.go
@@ -196,13 +196,68 @@ processors:
    - add_kubernetes_metadata: ~
 `
 
-	// start filebeat in otel mode
-	filebeatOTel := integration.NewBeat(
-		t,
-		"filebeat-otel",
-		"../../filebeat.test",
-		"otel",
-	)
+	otelConfigFile := `receivers:
+  filebeatreceiver:
+    filebeat:
+      inputs:
+        - type: httpjson
+          id: httpjson-e2e-otel
+          request.url: http://localhost:8090/test
+    output:
+      otelconsumer:
+    processors:
+      - add_host_metadata: ~
+      - add_cloud_metadata: ~
+      - add_docker_metadata: ~
+      - add_kubernetes_metadata: ~
+    queue.mem.flush.timeout: 0s
+    setup.template.enabled: false
+exporters:
+  elasticsearch:
+    auth:
+      authenticator: beatsauth
+    compression: gzip
+    compression_params:
+      level: 1
+    endpoints:
+      - {{ .ESURL }}
+    logs_index: logs-integration-{{ .Namespace }}
+    mapping:
+      mode: bodymap
+    max_conns_per_host: 1
+    password: {{ .Password }}
+    retry:
+      enabled: true
+      initial_interval: 1s
+      max_interval: 1m0s
+      max_retries: 3
+    sending_queue:
+      batch:
+        flush_timeout: 10s
+        max_size: 1600
+        min_size: 0
+        sizer: items
+      block_on_overflow: true
+      enabled: true
+      num_consumers: 1
+      queue_size: 3200
+      wait_for_result: true
+    user: {{ .Username }}
+extensions:
+  beatsauth:
+    idle_connection_timeout: 3s
+    proxy_disable: false
+    timeout: 1m30s
+service:
+  extensions:
+    - beatsauth
+  pipelines:
+    logs:
+      receivers:
+        - filebeatreceiver
+      exporters:
+        - elasticsearch
+`
 
 	optionsValue := options{
 		ESURL:    fmt.Sprintf("%s://%s", host.Scheme, host.Host),
@@ -212,10 +267,8 @@ processors:
 
 	var configBuffer bytes.Buffer
 	optionsValue.Namespace = otelNamespace
-	require.NoError(t, template.Must(template.New("config").Parse(configFile)).Execute(&configBuffer, optionsValue))
-
-	filebeatOTel.WriteConfigFile(configBuffer.String())
-	filebeatOTel.Start()
+	require.NoError(t, template.Must(template.New("config").Parse(otelConfigFile)).Execute(&configBuffer, optionsValue))
+	oteltest.NewCollector(t, configBuffer.String())
 
 	// reset buffer
 	configBuffer.Reset()
