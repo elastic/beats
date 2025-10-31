c18dc5c71b8a3e41f8400e343c4017b0fd3edbc0
Vihas Makwana <121151420+VihasMakwana@users.noreply.github.com>
2025-10-31 11:11:02 +0530

    [beatreceiver] only add default processors if running under agent (#47299)
    * chore: only add default processors if running unde ragent
    
    * fix test
    
    * fix test
    
    * tab
    
    * fix race condition
    
    * iterate over keys instead of map
    
    * fix test case
    
    (cherry picked from commit 78f115e4f37aec07ae38bc50e8984dce992a7596)

--- x-pack/filebeat/tests/integration/otel_test.go
+++ x-pack/filebeat/tests/integration/otel_test.go
@@ -368,6 +368,7 @@ func TestFilebeatOTelReceiverE2E(t *testing.T) {
     http.enabled: true
     http.host: localhost
     http.port: {{.MonitoringPort}}
+    management.otel.enabled: true
 exporters:
   debug:
     use_internal_logger: false
