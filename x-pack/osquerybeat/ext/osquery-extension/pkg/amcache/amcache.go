1ac2b43fdeeb10704d4d6b2c5172c4f214300830
Brian McKinney <brian.mckinney@elastic.co>
2025-11-05 14:10:17 -0600

    restrict amcache.go to windows only

--- x-pack/osquerybeat/ext/osquery-extension/pkg/amcache/amcache.go
+++ x-pack/osquerybeat/ext/osquery-extension/pkg/amcache/amcache.go
@@ -2,11 +2,14 @@
 // or more contributor license agreements. Licensed under the Elastic License;
 // you may not use this file except in compliance with the Elastic License.
 
+//go:build windows
+
 package amcache
 
 import (
-	"github.com/elastic/beats/v7/x-pack/osquerybeat/ext/osquery-extension/pkg/logger"
 	"fmt"
+
+	"github.com/elastic/beats/v7/x-pack/osquerybeat/ext/osquery-extension/pkg/logger"
 	"github.com/elastic/beats/v7/x-pack/osquerybeat/ext/osquery-extension/pkg/hooks"
 )
 
