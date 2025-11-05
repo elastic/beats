a4262f1ae8c342c7868b2aa774c70bf9303a5341
kruskall <99559985+kruskall@users.noreply.github.com>
2025-11-05 08:04:52 +0100

    refactor: replace multierr with stdlib errors (#45275)
    * refactor: replce multierr with stdlib errors
    
    errors wrapping and joining is part of the stdlib
    
    * lint: regenerate notice
    
    * lint: forbid multierr import
    
    * lint: cef parsers errors
    
    * lint: cef parsers errors
    
    * lint: cef parsers errors
    
    * lint: fix linter issues
    
    * lint: cef parsers errors
    
    * lint: cef parsers errors
    
    * Update cef_test.go

--- winlogbeat/sys/wineventlog/renderer.go
+++ winlogbeat/sys/wineventlog/renderer.go
@@ -33,7 +33,6 @@ import (
 	"unsafe"
 
 	"github.com/cespare/xxhash/v2"
-	"go.uber.org/multierr"
 	"golang.org/x/sys/windows"
 
 	"github.com/elastic/beats/v7/winlogbeat/sys"
@@ -81,7 +80,7 @@ func (r *Renderer) Close() error {
 	if r == nil {
 		return errors.New("closing nil renderer")
 	}
-	return multierr.Combine(
+	return errors.Join(
 		r.metadataCache.close(),
 		r.systemContext.Close(),
 		r.userContext.Close(),
@@ -124,7 +123,7 @@ func (r *Renderer) Render(handle EvtHandle) (*winevent.Event, string, error) {
 		errs = append(errs, fmt.Errorf("failed to get the event message string: %w", err))
 	}
 
-	return event, "", multierr.Combine(errs...)
+	return event, "", errors.Join(errs...)
 }
 
 // renderSystem writes all the system context properties into the event.
@@ -446,7 +445,7 @@ func (r *XMLRenderer) Render(handle EvtHandle) (*winevent.Event, string, error) 
 	// and version mismatches. Use static enrichment only.
 	if r.isForwarded {
 		enrichRawValuesWithNames(nil, event)
-		return event, string(outBytes), multierr.Combine(errs...)
+		return event, string(outBytes), errors.Join(errs...)
 	}
 
 	// This always returns a non-nil value (even on error).
@@ -477,7 +476,7 @@ func (r *XMLRenderer) Render(handle EvtHandle) (*winevent.Event, string, error) 
 		}
 	}
 
-	return event, string(outBytes), multierr.Combine(errs...)
+	return event, string(outBytes), errors.Join(errs...)
 }
 
 func (r *XMLRenderer) buildEventFromXML(x []byte, recoveredErr error) *winevent.Event {
