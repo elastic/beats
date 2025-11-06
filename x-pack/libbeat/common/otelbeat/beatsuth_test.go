918a9fe5672c30659b6f2693b452e9eb7b9f2928
Khushi Jain <khushi.jain@elastic.co>
2025-11-06 15:05:10 +0530

    move beatsauth test

--- x-pack/libbeat/common/otelbeat/beatsuth_test.go
+++ x-pack/libbeat/common/otelbeat/beatsuth_test.go
@@ -1,23 +1,8 @@
-// Licensed to Elasticsearch B.V. under one or more contributor
-// license agreements. See the NOTICE file distributed with
-// this work for additional information regarding copyright
-// ownership. Elasticsearch B.V. licenses this file to you under
-// the Apache License, Version 2.0 (the "License"); you may
-// not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
 
-//go:build !requirefips
-
-package oteltest
+package otelbeat
 
 import (
 	"bytes"
@@ -56,6 +41,7 @@ import (
 	"gopkg.in/yaml.v2"
 
 	"github.com/elastic/beats/v7/libbeat/otelbeat/beatconverter"
+	"github.com/elastic/beats/v7/libbeat/otelbeat/oteltest"
 	"github.com/elastic/beats/v7/x-pack/otel/extension/beatsauthextension"
 	"github.com/elastic/elastic-agent-libs/testing/proxytest"
 	"github.com/elastic/elastic-agent-libs/transport/tlscommontest"
@@ -99,7 +85,7 @@ func TestMTLS(t *testing.T) {
 	}
 
 	// get client certificates paths
-	clientCertificate, clientKey := GetClientCerts(t, caCert, "")
+	clientCertificate, clientKey := oteltest.GetClientCerts(t, caCert, "")
 
 	// start test server with given server and root certs
 	certPool := x509.NewCertPool()
@@ -173,7 +159,7 @@ func TestKeyPassPhrase(t *testing.T) {
 	}
 
 	// get client certificates paths with key file encrypted in PKCS#8 format
-	clientCertificate, clientKey := GetClientCerts(t, caCert, "your-password")
+	clientCertificate, clientKey := oteltest.GetClientCerts(t, caCert, "your-password")
 
 	// start test server with given server and root certs
 	certPool := x509.NewCertPool()
