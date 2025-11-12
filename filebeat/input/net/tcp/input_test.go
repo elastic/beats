5a72156b51aefe556e42120c77d0fa278dccaa44
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2025-11-12 09:15:26 -0300

    fix(filebeat): prevent data corruption in TCP/Syslog input
    Mutating a buffer received from a TCP callback could corrupt data in the
    underlying bufio.Scanner. Copy the data to a separate buffer before
    passing it to the callback.

--- filebeat/input/net/tcp/input_test.go
+++ filebeat/input/net/tcp/input_test.go
@@ -20,8 +20,12 @@
 package tcp
 
 import (
+	"bytes"
 	"context"
 	"errors"
+	"fmt"
+	"net"
+	"runtime"
 	"sync"
 	"testing"
 	"time"
@@ -29,9 +33,12 @@ import (
 	netinput "github.com/elastic/beats/v7/filebeat/input/net"
 	"github.com/elastic/beats/v7/filebeat/input/net/nettest"
 	v2 "github.com/elastic/beats/v7/filebeat/input/v2"
+	libbeattesting "github.com/elastic/beats/v7/libbeat/testing"
 	conf "github.com/elastic/elastic-agent-libs/config"
 	"github.com/elastic/elastic-agent-libs/logp"
 	"github.com/elastic/elastic-agent-libs/monitoring"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestInput(t *testing.T) {
@@ -94,4 +101,78 @@ func TestInput(t *testing.T) {
 	default:
 		// No more events on the channel, test passed
 	}
+}
+
+func BenchmarkInput(b *testing.B) {
+	port, err := libbeattesting.AvailableTCP4Port()
+	if err != nil {
+		b.Fatalf("cannot find available port: %s", err)
+	}
+	serverAddr := net.JoinHostPort("localhost", fmt.Sprintf("%d", port))
+
+	inp, err := configure(conf.MustNewConfigFrom(map[string]any{
+		"host":              serverAddr,
+		"number_of_workers": 2,
+	}))
+	if err != nil {
+		b.Fatalf("cannot create input: %s", err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	v2Ctx := v2.Context{
+		ID:              b.Name(),
+		Cancelation:     ctx,
+		Logger:          logp.NewNopLogger(),
+		MetricsRegistry: monitoring.NewRegistry(),
+	}
+
+	metrics := inp.InitMetrics("tcp", v2Ctx.MetricsRegistry, v2Ctx.Logger)
+	c := make(chan netinput.DataMetadata, 5*runtime.NumCPU())
+
+	go func() {
+		if err := inp.Run(v2Ctx, c, metrics); err != nil {
+			if !errors.Is(err, context.Canceled) {
+				b.Errorf("input exited with error: %s", err)
+			}
+		}
+	}()
+
+	require.EventuallyWithTf(b, func(ct *assert.CollectT) {
+		conn, err := net.Dial("tcp", serverAddr)
+		require.NoError(ct, err)
+		conn.Close()
+	}, 30*time.Second, 100*time.Millisecond, "waiting for TCP server to start")
+
+	testMessage := bytes.Repeat([]byte("A"), 1001)
+	testMessage[len(testMessage)-1] = '\n'
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			conn, err := net.Dial("tcp", serverAddr)
+			if err != nil {
+				b.Errorf("cannot create connection: %s", err)
+				continue
+			}
+
+			for range 100 {
+				_, err = conn.Write(testMessage)
+				if err != nil {
+					b.Errorf("failed to send data: %s", err)
+					break
+				}
+			}
+			conn.Close()
+
+			// Read the events from the channel to prevent blocking
+			for range 100 {
+				select {
+				case <-c:
+				case <-time.After(time.Second):
+					b.Error("timeout waiting for event")
+				}
+			}
+		}
+	})
 }
