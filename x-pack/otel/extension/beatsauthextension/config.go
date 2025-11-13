a8a8e9e3c06a8b83defdc29435e1b2fa2c4e2be2
Khushi Jain <khushi.jain@elastic.co>
2025-11-13 18:36:51 +0530

    Add Kerberos support to beatsauth (#47534)
    * Add Kerberos support on beatsauth
    
    (cherry picked from commit 44078c0bdec86f14da9fc4d10f0f858cb7ae828a)

--- x-pack/otel/extension/beatsauthextension/config.go
+++ x-pack/otel/extension/beatsauthextension/config.go
@@ -6,11 +6,19 @@ package beatsauthextension
 
 import (
 	"go.opentelemetry.io/collector/component"
+
+	"github.com/elastic/beats/v7/libbeat/common/transport/kerberos"
+	"github.com/elastic/elastic-agent-libs/transport/httpcommon"
 )
 
 type Config struct {
 	BeatAuthConfig  map[string]interface{} `mapstructure:",remain"`
 	ContinueOnError bool                   `mapstructure:"continue_on_error"`
+}
+
+type esAuthConfig struct {
+	Kerberos  *kerberos.Config                 `config:"kerberos"`
+	Transport httpcommon.HTTPTransportSettings `config:",inline"`
 }
 
 func createDefaultConfig() component.Config {
