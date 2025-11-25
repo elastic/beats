55a1137280cec553a3a5f470c6f2ceb65d4d69a8
Lee E Hinman <57081003+leehinman@users.noreply.github.com>
2025-11-25 14:39:56 -0600

    remove otel.component.id and otel.component.kind fields from beat receivers (#47729)
    * remove component id and component kind fields from beat receivers
    
    * add real PR link
    
    * update integration tests
    
    * fix merge mistake
    
    (cherry picked from commit ca1c17bca6ad79874ff8ebf0c1177bf516980337)
    
    # Conflicts:
    #	x-pack/filebeat/input/gcppubsub/otel_test.go
    #	x-pack/filebeat/tests/integration/otel_lsexporter_test.go
    #	x-pack/filebeat/tests/integration/otel_test.go
    #	x-pack/libbeat/outputs/otelconsumer/otelconsumer.go
    #	x-pack/libbeat/outputs/otelconsumer/otelconsumer_test.go
    #	x-pack/metricbeat/mbreceiver/receiver_test.go

--- x-pack/libbeat/outputs/otelconsumer/otelconsumer.go
+++ x-pack/libbeat/outputs/otelconsumer/otelconsumer.go
@@ -32,12 +32,15 @@ import (
 const (
 	// esDocumentIDAttribute is the attribute key used to store the document ID in the log record.
 	esDocumentIDAttribute = "elasticsearch.document_id"
+<<<<<<< HEAD
 	beatNameCtxKey        = "beat_name"
 	beatVersionCtxtKey    = "beat_version"
 	// otelComponentIDKey is the key used to store the Beat receiver's component id in the beat event.
 	otelComponentIDKey = "otelcol.component.id"
 	// otelComponentKindKey is the key used to store the Beat receiver's component kind in the beat event. This is always "receiver".
 	otelComponentKindKey = "otelcol.component.kind"
+=======
+>>>>>>> ca1c17bca (remove otel.component.id and otel.component.kind fields from beat receivers (#47729))
 )
 
 func init() {
@@ -148,15 +151,6 @@ func (out *otelConsumer) logsPublish(ctx context.Context, batch publisher.Batch)
 			}
 		}
 		logRecord.SetObservedTimestamp(observedTimestamp)
-
-		if agent, _ := beatEvent.GetValue("agent"); agent != nil {
-			switch agent := agent.(type) {
-			case mapstr.M:
-				agent[otelComponentIDKey] = out.beatInfo.ComponentID
-				agent[otelComponentKindKey] = "receiver"
-				beatEvent["agent"] = agent
-			}
-		}
 
 		otelmap.ConvertNonPrimitive(beatEvent)
 
