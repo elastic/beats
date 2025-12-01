e58f5d079b896892b9d5740bd4e5c37dc3e33a40
Marc Guasch <marc.guasch@elastic.co>
2025-12-01 16:33:23 +0100

    fix: update build constraint for non-Windows platforms in ldap_default.go

--- libbeat/processors/translate_ldap_attribute/ldap_default.go
+++ libbeat/processors/translate_ldap_attribute/ldap_default.go
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-//go:build !windows || requirefips
+//go:build !windows && !requirefips
 
 package translate_ldap_attribute
 
