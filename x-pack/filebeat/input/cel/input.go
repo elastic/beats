031b04a04be05b8b620c3334f2d41b4702aaf9f3
Dan Kortschak <dan.kortschak@elastic.co>
2025-12-04 13:51:06 +1030

    x-pack/filebeat/input/cel: log unpublished event count and exit publish loop on context cancellation (#47730)
    (cherry picked from commit ab3d2c3fb53d8d9d233e169ae00cf5799b29d584)

--- x-pack/filebeat/input/cel/input.go
+++ x-pack/filebeat/input/cel/input.go
@@ -514,6 +514,7 @@ func (i input) run(env v2.Context, src *source, cursor map[string]interface{}, p
 
 			start = time.Now()
 			var hadPublicationError bool
+		loop:
 			for i, e := range events {
 				event, ok := e.(map[string]interface{})
 				if !ok {
@@ -540,6 +541,25 @@ func (i input) run(env v2.Context, src *source, cursor map[string]interface{}, p
 						}
 						pubCursor = cursor
 					}
+				}
+				// This is checked prior to the publish attempt since the
+				// cursor.Publisher interface does not document the behaviour
+				// related to context cancellation and the context is not
+				// explicitly passed in, so favour this explicit clarity.
+				switch err := ctx.Err(); {
+				case err == nil:
+				case errors.Is(err, context.Canceled), errors.Is(err, context.DeadlineExceeded):
+					log.Infow("context cancelled with unpublished events", "unpublished", len(events)-i)
+					// Don't update status, since we are about to pass
+					// through the Running state and then fall through
+					// to the input exit with a change to Stopped.
+					break loop
+				default:
+					// This should never happen.
+					log.Warnw("failed with unpublished events", "error", err, "unpublished", len(events)-i)
+					health.UpdateStatus(status.Degraded, "error publishing events: "+err.Error())
+					isDegraded = true
+					break loop
 				}
 				err = pub.Publish(beat.Event{
 					Timestamp: time.Now(),
