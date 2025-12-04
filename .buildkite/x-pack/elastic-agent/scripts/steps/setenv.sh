9f20c88d13c3775369450e72f951cc2de1c11c20
Pierre HILBERT <pierre.hilbert@elastic.co>
2025-12-04 09:20:43 +0100

    Bump GVM version in remaining places

--- .buildkite/x-pack/elastic-agent/scripts/steps/setenv.sh
+++ .buildkite/x-pack/elastic-agent/scripts/steps/setenv.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-SETUP_GVM_VERSION="v0.5.1"
+SETUP_GVM_VERSION="v0.6.0" # https://github.com/andrewkroh/gvm/issues/44#issuecomment-1013231151
 DOCKER_COMPOSE_VERSION="1.21.0"
 DOCKER_COMPOSE_VERSION_AARCH64="v2.21.0"
 SETUP_WIN_PYTHON_VERSION="3.11.0"
