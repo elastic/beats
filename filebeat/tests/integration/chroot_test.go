295f7b863875a9c3d73b49f54109971a774e85cf
Tiago Queiroz <tiago.queiroz@elastic.co>
2025-12-10 16:14:22 -0500

    Test cleanup

--- filebeat/tests/integration/chroot_test.go
+++ filebeat/tests/integration/chroot_test.go
@@ -5,7 +5,6 @@ package integration
 import (
 	"archive/tar"
 	"bytes"
-	"context"
 	"fmt"
 	"io"
 	"os"
@@ -14,157 +13,100 @@ import (
 	"testing"
 	"time"
 
-	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/build"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/mount"
 	"github.com/docker/docker/client"
 	"github.com/elastic/elastic-agent-libs/testing/fs"
+	"github.com/gofrs/uuid/v5"
 )
 
 func TestJournaldChroot(t *testing.T) {
-	ctx := context.Background()
-
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
 		t.Fatalf("Failed to create Docker client: %s", err)
 	}
 	defer cli.Close()
 
+	imageName := "journald-chroot"
+	syslogID := uuid.Must(uuid.NewV4()).String()
+
+	generateJournaldLogs(t, syslogID, 5, 100)
+
 	tempDir := fs.TempDir(t, filepath.Join("..", "..", "build"))
-	logFile := fs.NewLogFile(t, tempDir, "container-logs-*.log")
+	containerLogFile := fs.NewLogFile(t, tempDir, "container-logs-*.log")
 
-	dockerfileDir := "testdata/journald_chroot"
-	dockerfileName := "Dockerfile"
-	imageName := "filebeat-chroot-test"
-	filebeatBinaryPath := "filebeat"
-
-	buildFilebeatBinary(t)
-
-	buildDockerImage(ctx, t, cli, dockerfileDir, dockerfileName, imageName, filebeatBinaryPath)
-
-	containerName := "filebeat-chroot-container"
-	startDockerContainer(ctx, t, cli, imageName, containerName, logFile)
-
-	assertJournalctlStarted(t, logFile)
+	filebeatPath := buildFilebeatBinary(t, tempDir)
+	buildDockerImage(t, cli, imageName, tempDir, filebeatPath)
+	startDockerContainer(t, cli, imageName, syslogID, containerLogFile)
+	assertJournalctlStarted(t, containerLogFile, syslogID)
 }
 
-func buildDockerImage(ctx context.Context, t *testing.T, cli *client.Client, dockerfileDir, dockerfileName, imageName, filebeatBinaryPath string) {
-	buildContext, err := archiveTarWithFilebeat(dockerfileDir, filebeatBinaryPath)
-	if err != nil {
-		t.Fatalf("Failed to create build context: %s", err)
-	}
+func buildDockerImage(t *testing.T, cli *client.Client, imageName, tempDir, filebeatPath string) {
+	buildContextDir := "testdata/journald_chroot"
 
-	buildOptions := types.ImageBuildOptions{
+	buildOptions := build.ImageBuildOptions{
 		Tags:       []string{imageName},
-		Dockerfile: dockerfileName,
+		Dockerfile: "Dockerfile", // Dockerfile path relative to the build context
 	}
-
-	resp, err := cli.ImageBuild(ctx, buildContext, buildOptions)
+	buildContext := createDockerContext(t, buildContextDir, filebeatPath)
+	resp, err := cli.ImageBuild(t.Context(), buildContext, buildOptions)
 	if err != nil {
 		t.Fatalf("Failed to build Docker image: %s", err)
 	}
 	defer resp.Body.Close()
 
-	io.Copy(os.Stdout, resp.Body)
+	// Keep the logs in case something goes wrong
+	f := fs.NewLogFile(t, tempDir, "docker-build-log-*.log")
+	if _, err := io.Copy(f, resp.Body); err != nil {
+		t.Logf("cannot read Docker build logs: %s", err)
+	}
 }
 
-func archiveTarWithFilebeat(dir, filebeatBinaryPath string) (io.Reader, error) {
-	buf := new(bytes.Buffer)
-	tw := tar.NewWriter(buf)
-	defer tw.Close()
-
-	// Add the directory contents to the tar archive
-	err := filepath.Walk(dir, func(file string, fi os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		header, err := tar.FileInfoHeader(fi, file)
-		if err != nil {
-			return err
-		}
-
-		header.Name, _ = filepath.Rel(dir, file)
-		if fi.IsDir() {
-			header.Name += "/"
-		}
-
-		if err := tw.WriteHeader(header); err != nil {
-			return err
-		}
-
-		if !fi.IsDir() {
-			f, err := os.Open(file)
-			if err != nil {
-				return err
-			}
-			defer f.Close()
-
-			if _, err := io.Copy(tw, f); err != nil {
-				return err
-			}
-		}
-		return nil
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	// Add the Filebeat binary to the tar archive
-	filebeatFile, err := os.Open(filebeatBinaryPath)
-	if err != nil {
-		return nil, fmt.Errorf("failed to open Filebeat binary: %w", err)
-	}
-	defer filebeatFile.Close()
-
-	fileInfo, err := filebeatFile.Stat()
-	if err != nil {
-		return nil, fmt.Errorf("failed to stat Filebeat binary: %w", err)
-	}
-
-	header, err := tar.FileInfoHeader(fileInfo, filebeatBinaryPath)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create tar header for Filebeat binary: %w", err)
-	}
-	header.Name = "filebeat" // Place the binary in the root of the build context
-
-	if err := tw.WriteHeader(header); err != nil {
-		return nil, fmt.Errorf("failed to write tar header for Filebeat binary: %w", err)
-	}
-
-	if _, err := io.Copy(tw, filebeatFile); err != nil {
-		return nil, fmt.Errorf("failed to copy Filebeat binary to tar archive: %w", err)
-	}
-
-	return buf, nil
-}
-
-func buildFilebeatBinary(t *testing.T) {
-	cmd := exec.Command("go", "build", "-ldflags", "-extldflags \"-static\" -s", "-tags", "timetzdata", "../../")
+func buildFilebeatBinary(t *testing.T, tempDir string) string {
+	filebeatPath := filepath.Join(tempDir, "filebeat_static")
+	cmd := exec.Command(
+		"go",
+		"build",
+		"-ldflags",
+		"-extldflags \"-static\" -s",
+		"-tags",
+		"timetzdata",
+		"-o",
+		filebeatPath,
+		"../../")
 	cmd.Env = append(os.Environ(), "CGO_ENABLED=0")
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		t.Fatalf("Failed to build Filebeat binary: %s\nOutput: %s", err, output)
 	}
+
+	return filebeatPath
 }
 
-func startDockerContainer(ctx context.Context, t *testing.T, cli *client.Client, imageName, containerName string, logFile *fs.LogFile) string {
-	resp, err := cli.ContainerCreate(
-		ctx, &container.Config{
-			Image: imageName,
+func startDockerContainer(t *testing.T, cli *client.Client, imageName, syslogID string, logFile *fs.LogFile) string {
+	ctx := t.Context()
+
+	containerConfig := &container.Config{
+		Image: imageName,
+		Tty:   true,
+		Env: []string{
+			"SYSLOG_ID=" + syslogID,
 		},
-		&container.HostConfig{
-			Mounts: []mount.Mount{
-				{
-					Type:   mount.TypeBind,
-					Source: "/",
-					Target: "/host",
-				},
+	}
+	hostConfig := &container.HostConfig{
+		Mounts: []mount.Mount{
+			{
+				Type:   mount.TypeBind,
+				Source: "/",
+				Target: "/hostfs",
 			},
-			CapAdd:     []string{"CAP_SYS_CHROOT"}, // Required for chroot
-			AutoRemove: true,
 		},
-		nil, nil, "")
+		CapAdd:     []string{"CAP_SYS_CHROOT"}, // Required for chroot
+		AutoRemove: true,
+	}
+
+	resp, err := cli.ContainerCreate(ctx, containerConfig, hostConfig, nil, nil, "")
 	if err != nil {
 		t.Fatalf("Failed to create Docker container: %s", err)
 	}
@@ -183,20 +125,94 @@ func startDockerContainer(ctx context.Context, t *testing.T, cli *client.Client,
 	go func() {
 		defer attachResp.Close()
 		if _, err := io.Copy(logFile, attachResp.Reader); err != nil {
-			t.Errorf("Error streaming container logs: %s", err)
+			t.Logf("Error streaming container logs: %s", err)
 		}
 	}()
 
-	// Start the container
 	if err := cli.ContainerStart(ctx, resp.ID, container.StartOptions{}); err != nil {
 		t.Fatalf("Failed to start Docker container: %s", err)
 	}
 
-	// Allow some time for the container to start
 	return resp.ID
 }
 
-func assertJournalctlStarted(t *testing.T, logFile *fs.LogFile) {
+func assertJournalctlStarted(t *testing.T, logFile *fs.LogFile, syslogID string) {
+	t.Helper()
 	// Wait for the log message "journalctl started with PID XX"
 	logFile.WaitLogsContains(t, "journalctl started with PID", 30*time.Second, "journalctl did not start")
+	for range 5 {
+		logFile.WaitLogsContains(t, syslogID, 5*time.Second, "did not find event")
+	}
+}
+
+func createDockerContext(t *testing.T, dir, filebeatPath string) io.Reader {
+	buf := new(bytes.Buffer)
+	tw := tar.NewWriter(buf)
+	defer tw.Close()
+
+	// Add the directory contents to the tar archive
+	err := filepath.Walk(dir, func(file string, fi os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		header, err := tar.FileInfoHeader(fi, file)
+		if err != nil {
+			return fmt.Errorf("cannot get FileInfoHeader for %q: %s", file, err)
+		}
+
+		header.Name, _ = filepath.Rel(dir, file)
+		if fi.IsDir() {
+			header.Name += "/"
+		}
+
+		if err := tw.WriteHeader(header); err != nil {
+			return err
+		}
+
+		if !fi.IsDir() {
+			f, err := os.Open(file)
+			if err != nil {
+				return fmt.Errorf("cannot open file: %s", err)
+			}
+			defer f.Close()
+
+			if _, err := io.Copy(tw, f); err != nil {
+				return fmt.Errorf("cannot read %q: %s", file, err)
+			}
+		}
+
+		return nil
+	})
+	if err != nil {
+		t.Fatalf("cannot generate tar archive: %s", err)
+	}
+
+	// Add the Filebeat binary to the tar archive
+	filebeatFile, err := os.Open(filebeatPath)
+	if err != nil {
+		t.Fatalf("failed to open Filebeat binary: %s", err)
+	}
+	defer filebeatFile.Close()
+
+	fileInfo, err := filebeatFile.Stat()
+	if err != nil {
+		t.Fatalf("failed to stat Filebeat binary: %s", err)
+	}
+
+	header, err := tar.FileInfoHeader(fileInfo, filebeatPath)
+	if err != nil {
+		t.Fatalf("failed to create tar header for Filebeat binary: %s", err)
+	}
+	header.Name = "filebeat" // Place the binary in the root of the build context
+
+	if err := tw.WriteHeader(header); err != nil {
+		t.Fatalf("failed to write tar header for Filebeat binary: %s", err)
+	}
+
+	if _, err := io.Copy(tw, filebeatFile); err != nil {
+		t.Fatalf("failed to copy Filebeat binary to tar archive: %s", err)
+	}
+
+	return buf
 }
