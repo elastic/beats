1d6b8eb2ff73b3b7211aa90ed91fb6fbafa775c7
Hiruma31 <romain.grenet@gmail.com>
2025-12-12 15:51:44 -0500

    feat: Adds file owner and group to the events' meta fields (#47331)
    * filebeat: Adds file owner and group to the events' meta fields
    
    ---------
    
    Co-authored-by: Anderson Queiroz <anderson.queiroz@elastic.co>

--- filebeat/input/filestream/config.go
+++ filebeat/input/filestream/config.go
@@ -49,6 +49,11 @@ type config struct {
 	// This feature is experimental and subject to change.
 	GZIPExperimental bool `config:"gzip_experimental"`
 
+	// Whether to add the file owner name and group to the event metadata.
+	// Disabled by default.
+	IncludeFileOwnerName      bool `config:"include_file_owner_name"`
+	IncludeFileOwnerGroupName bool `config:"include_file_owner_group_name"`
+
 	// -1 means that registry will never be cleaned, disabling clean_inactive.
 	// Setting it to 0 also disables clean_inactive
 	// "clean_inactive" is parsed, again, and used by internal/input-logfile/manager.go
@@ -131,15 +136,17 @@ type copyTruncateConfig commonRotationConfig
 
 func defaultConfig() config {
 	return config{
-		Reader:         defaultReaderConfig(),
-		Paths:          []string{},
-		Close:          defaultCloserConfig(),
-		CleanInactive:  -1,
-		CleanRemoved:   true,
-		HarvesterLimit: 0,
-		IgnoreOlder:    0,
-		Delete:         defaultDeleterConfig(),
-		FileWatcher:    defaultFileWatcherConfig(), // Config key: prospector.scanner
+		Reader:                    defaultReaderConfig(),
+		Paths:                     []string{},
+		Close:                     defaultCloserConfig(),
+		IncludeFileOwnerName:      false,
+		IncludeFileOwnerGroupName: false,
+		CleanInactive:             -1,
+		CleanRemoved:              true,
+		HarvesterLimit:            0,
+		IgnoreOlder:               0,
+		Delete:                    defaultDeleterConfig(),
+		FileWatcher:               defaultFileWatcherConfig(), // Config key: prospector.scanner
 	}
 }
 
@@ -222,7 +229,7 @@ func (c *config) Validate() error {
 	return nil
 }
 
-// checkUnsupportedParams checks if unsupported/deprecated/discouraged paramaters are set and logs a warning
+// checkUnsupportedParams checks if unsupported/deprecated/discouraged parameters are set and logs a warning
 func (c config) checkUnsupportedParams(logger *logp.Logger) {
 	if c.AllowIDDuplication {
 		logger.Named("filestream").Warn(
