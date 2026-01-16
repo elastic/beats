14c9180ec64135bf4240637378fe376adbaafeb9
Marc Guasch <marc.guasch@elastic.co>
2026-01-16 12:08:08 +0100

    Not block on SSPI negotiation

--- libbeat/processors/translate_ldap_attribute/ldap_windows.go
+++ libbeat/processors/translate_ldap_attribute/ldap_windows.go
@@ -20,26 +20,54 @@
 package translate_ldap_attribute
 
 import (
+	"errors"
 	"fmt"
+	"time"
 
 	"github.com/go-ldap/ldap/v3"
 	"github.com/go-ldap/ldap/v3/gssapi"
 )
 
+// sspiBindTimeout is the maximum time to wait for SSPI bind operations.
+// SSPI can hang indefinitely when credentials are unavailable (e.g., local user accounts).
+const sspiBindTimeout = 10 * time.Second
+
+var errSSPITimeout = errors.New("SSPI bind timed out - this may indicate the process is running as a local user without Kerberos credentials")
+
 func (client *ldapClient) bindPlatformSpecific(conn *ldap.Conn, spn string) error {
-	client.log.Info("Attempting Windows SSPI Bind")
+	client.log.Infow("Attempting Windows SSPI Bind", "spn", spn)
 
-	sspiClient, err := gssapi.NewSSPIClient()
-	if err != nil {
-		return fmt.Errorf("failed to create SSPI client: %w", err)
+	resultCh := make(chan error, 1)
+
+	go func() {
+		client.log.Debug("Creating SSPI client")
+		sspiClient, err := gssapi.NewSSPIClient()
+		if err != nil {
+			resultCh <- fmt.Errorf("failed to create SSPI client: %w", err)
+			return
+		}
+		defer sspiClient.DeleteSecContext()
+
+		client.log.Debug("SSPI client created, performing GSSAPIBind")
+		err = conn.GSSAPIBind(sspiClient, spn, "")
+		if err != nil {
+			resultCh <- fmt.Errorf("SSPI bind failed: %w", err)
+			return
+		}
+
+		resultCh <- nil
+	}()
+
+	select {
+	case err := <-resultCh:
+		if err != nil {
+			client.log.Errorw("SSPI bind failed", "error", err)
+		} else {
+			client.log.Info("Windows SSPI Bind Successful")
+		}
+		return err
+	case <-time.After(sspiBindTimeout):
+		client.log.Warnw("SSPI bind timed out", "timeout", sspiBindTimeout, "spn", spn)
+		return errSSPITimeout
 	}
-	defer sspiClient.DeleteSecContext()
-
-	err = conn.GSSAPIBind(sspiClient, spn, "")
-	if err != nil {
-		return fmt.Errorf("SSPI bind failed: %w", err)
-	}
-
-	client.log.Info("Windows SSPI Bind Successful")
-	return nil
 }
