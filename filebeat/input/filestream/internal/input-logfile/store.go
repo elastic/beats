73bccf4088a5e64a9d6b08032f7d0a868e201e85
Tiago Queiroz <tiago.queiroz@elastic.co>
2026-01-26 17:53:42 -0500

    Organise code

--- filebeat/input/filestream/internal/input-logfile/store.go
+++ filebeat/input/filestream/internal/input-logfile/store.go
@@ -27,6 +27,7 @@ import (
 	"time"
 
 	"github.com/elastic/beats/v7/libbeat/common/cleanup"
+	"github.com/elastic/beats/v7/libbeat/common/file"
 	"github.com/elastic/beats/v7/libbeat/common/transform/typeconv"
 	"github.com/elastic/beats/v7/libbeat/statestore"
 
@@ -503,6 +504,54 @@ func (s *sourceStore) TakeOver(fn func(TakeOverState) (string, any)) {
 		res.Release()
 		s.store.log.Infof("migrated entry in registry from '%s' to '%s'. Cursor: %v", k, newKey, res.cursor)
 	}
+}
+
+// TakeOverState is the state of a file whose state is being taken over.
+// [newTakeOverState] correctly populates the fields according to the source
+// state (Log or Filestream). Only the required outside of this package is
+// exported.
+type TakeOverState struct {
+	FileStateOS    file.StateOS
+	IdentifierName string
+	Key            string
+	Meta           map[string]string
+	Source         string
+
+	logInpOffset int64
+}
+
+// newTakeOverState creates a TakeOverState populated by the logSt or filestreamSt.
+// If filestreamSt is not nil, it is used, otherwise logSt is used.
+// An error is only returned if filestreamSt.UnpackCursorMeta fails, the only
+// reason for it to fail is if the data format in the store has changed.
+func newTakeOverState(logSt inpFile.State, filestreamSt *resource) (TakeOverState, error) {
+	st := TakeOverState{}
+
+	if filestreamSt != nil {
+		meta := struct {
+			IdentifierName string `json:"identifier_name" struct:"identifier_name"`
+			Source         string `json:"source" struct:"source"`
+		}{}
+
+		if err := filestreamSt.UnpackCursorMeta(&meta); err != nil {
+			return st, err
+		}
+
+		st.Source = meta.Source
+		st.IdentifierName = meta.IdentifierName
+		st.Key = filestreamSt.key
+
+		return st, nil
+	}
+
+	st.Source = logSt.Source
+	st.Meta = logSt.Meta
+	st.IdentifierName = logSt.IdentifierName
+	st.FileStateOS = logSt.FileStateOS
+	st.logInpOffset = logSt.Offset
+	st.Key = logSt.Id
+
+	return st, nil
 }
 
 func (s *store) Retain() { s.refCount.Retain() }
