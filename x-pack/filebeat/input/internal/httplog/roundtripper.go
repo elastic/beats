11b26804c6489d3458a4702d609191446ba2ceb4
Chris Berkhout <chrisberkhout@gmail.com>
2026-02-03 10:33:04 +0100

    Updated request trace logs godoc to cover additional fields.

--- x-pack/filebeat/input/internal/httplog/roundtripper.go
+++ x-pack/filebeat/input/internal/httplog/roundtripper.go
@@ -112,6 +112,9 @@ type LoggingRoundTripper struct {
 //
 // Fields logged in requests:
 //
+//	transaction.id
+//	trace.id
+//	span.id
 //	url.original
 //	url.scheme
 //	url.path
@@ -128,12 +131,18 @@ type LoggingRoundTripper struct {
 //
 // Fields logged in responses:
 //
+//	transaction.id
+//	trace.id
+//	span.id
 //	http.response.status_code
 //	http.response.body.content
 //	http.response.body.truncated
 //	http.response.body.bytes
 //	http.response.mime_type
 //	http.response.header
+//
+// The trace.id and span.id fields are populated with IDs from the OTel span in
+// context.
 func (rt *LoggingRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
 	// Create a child logger for this request.
 	txID := rt.nextTxID()
