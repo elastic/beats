74b98da0ccb5bc95d91b8d6ed55f5b434afbf5cd
Chris Berkhout <chrisberkhout@gmail.com>
2026-02-04 15:50:34 +0100

    Rename extraOpts to otelhttpOptions, use nil rather than []otelhttp.Option{}.

--- x-pack/filebeat/input/cel/cel_metrics_test.go
+++ x-pack/filebeat/input/cel/cel_metrics_test.go
@@ -19,7 +19,6 @@ import (
 	"github.com/elastic/elastic-agent-libs/logp"
 	"github.com/elastic/elastic-agent-libs/monitoring"
 
-	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
 	"go.opentelemetry.io/otel/sdk/resource"
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
@@ -64,7 +63,7 @@ func TestOTELCELMetrics(t *testing.T) {
 		t.Fatalf("failed to create exporter: %v", err)
 	}
 
-	otelCELMetrics, transport, err := newOTELCELMetrics(log, *resource, client.Transport, metricExporter, []otelhttp.Option{})
+	otelCELMetrics, transport, err := newOTELCELMetrics(log, *resource, client.Transport, metricExporter, nil)
 	if err != nil {
 		t.Fatalf("failed to create otelCELMetrics: %v", err)
 	}
