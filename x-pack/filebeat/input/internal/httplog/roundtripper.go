c74d6ac910ed76aacea05958c8a98538335d8684
Chris Berkhout <chrisberkhout@gmail.com>
2026-02-04 15:53:23 +0100

    Comment clarification - OTel span only in context for CEL so far.

--- x-pack/filebeat/input/internal/httplog/roundtripper.go
+++ x-pack/filebeat/input/internal/httplog/roundtripper.go
@@ -142,7 +142,7 @@ type LoggingRoundTripper struct {
 //	http.response.header
 //
 // The trace.id and span.id fields are populated with IDs from the OTel span in
-// context.
+// context if it exists.
 func (rt *LoggingRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
 	// Create a child logger for this request.
 	txID := rt.nextTxID()
