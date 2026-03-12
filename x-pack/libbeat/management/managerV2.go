b057003b77efa06650d5a640e94ba4024435a495
Fae Charlton <fae.charlton@elastic.co>
2026-03-12 16:55:40 -0400

    Fix race that can block managerV2 shutdown (#49414)
    As described in https://github.com/elastic/beats/issues/49388, `BeatV2Manager` can miss the shutdown signal because its `Stop` method notifies the manager by sending to its signal channel `stopChan` rather than closing it, but there are two goroutines that both listen on that channel.
    
    This PR changes `Stop` to close the channel rather than just sending. It also removes the second `stopChan` listener in `watchErrChan`, since the main goroutine already calls the context canceler for that helper when `stopChan` unblocks (this isn't strictly necessary but it will keep error states visible for a little longer during shutdown, and is what was previously happening in the "good" path where the main worker received the stop signal first).
    
    (cherry picked from commit d39cb49e5926af67cc9bea4dbc6dda1ae2076a22)

--- x-pack/libbeat/management/managerV2.go
+++ x-pack/libbeat/management/managerV2.go
@@ -216,7 +216,7 @@ func NewV2AgentManagerWithClient(config *Config, registry *reload.Registry, agen
 		units:              make(map[unitKey]*agentUnit),
 		status:             status.Running,
 		message:            "Healthy",
-		stopChan:           make(chan struct{}, 1),
+		stopChan:           make(chan struct{}),
 		changeDebounce:     time.Second,
 		// forceReloadDebounce is greater than changeDebounce because it is only
 		// used when an input has not reached its finished state, this means some events
@@ -349,7 +349,9 @@ func (cm *BeatV2Manager) Start() error {
 
 // Stop stops the current Manager and close the connection to Elastic Agent.
 func (cm *BeatV2Manager) Stop() {
-	cm.stopChan <- struct{}{}
+	cm.stopOnce.Do(func() {
+		close(cm.stopChan)
+	})
 }
 
 // WaitForStop blocks until the manager has fully stopped, or timeout elapses.
@@ -542,8 +544,6 @@ func (cm *BeatV2Manager) watchErrChan(ctx context.Context) {
 			if !errors.Is(err, context.Canceled) {
 				cm.logger.Errorf("elastic-agent-client error: %s", err)
 			}
-		case <-cm.stopChan:
-			return
 		}
 	}
 }
