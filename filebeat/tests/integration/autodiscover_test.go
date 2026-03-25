cbf3fd6ca9c3d63921c2f8ed7f5c157eeeb54276
Tiago Queiroz <tiago.queiroz@elastic.co>
2026-03-25 12:26:59 -0400

    Use ES as output and read the container log files directly

--- filebeat/tests/integration/autodiscover_test.go
+++ filebeat/tests/integration/autodiscover_test.go
@@ -28,7 +28,6 @@ import (
 	"io"
 	"os"
 	"path/filepath"
-	"runtime"
 	"strings"
 	"testing"
 	"time"
@@ -106,54 +105,61 @@ func TestHintsKubernetes(t *testing.T) {
 }
 
 func TestAutodiscoverFilestreamTakeOverDoesNotReingest(t *testing.T) {
-	if runtime.GOOS != "linux" {
-		t.Skip("This test needs access to log files from Docker, it can only run on Linux")
-	}
-	canReadContainerLogs(t)
-	containerID := startFlogDocker(t)
+	integration.EnsureESIsRunning(t)
+
 	filebeat := integration.NewBeat(
 		t,
 		"filebeat",
 		"../../filebeat.test",
 	)
 	workDir := filebeat.TempDir()
-	outputFile := filepath.Join(workDir, "output*")
+	kubeConfigPath, nodeName, podName := startFlogKubernetesForTakeOver(t, workDir)
+	esURL := integration.GetESAdminURL(t, "http")
+	esUser := esURL.User.Username()
+	esPass, _ := esURL.User.Password()
+	index := fmt.Sprintf("test-autodiscover-take-over-%s", uuid.Must(uuid.NewV4()).String())
 
 	tmplVars := map[string]any{
-		"HomeFolder":  workDir,
-		"ContainerID": containerID,
+		"homeFolder": workDir,
+		"kubeConfig": kubeConfigPath,
+		"nodeName":   nodeName,
+		"podName":    podName,
+		"esHost":     fmt.Sprintf("%s://%s", esURL.Scheme, esURL.Host),
+		"esUser":     esUser,
+		"esPass":     esPass,
+		"index":      index,
 	}
 
 	filebeat.WriteConfigFile(
-		getConfig(t, tmplVars, "autodiscover", "take-over-log-input.yml"),
+		getConfig(t, tmplVars, "autodiscover", "take-over-log-input-k8s.yml"),
 	)
 	filebeat.Start()
 
 	// Wait until at least 5 events are ingested
 	require.Eventually(
 		t,
-		func() bool { return filebeat.CountFileLines(outputFile) >= 5 },
+		func() bool { return countEventsInES(t, index, 1000) >= 5 },
 		30*time.Second,
 		200*time.Millisecond,
 		"did not ingest the initial events")
 
 	filebeat.Stop()
-	logInputIngested := filebeat.CountFileLines(outputFile)
+	logInputIngested := countEventsInES(t, index, 1000)
 
 	// Re-Start Filebeat with Filestream and take_over enabled
 	filebeat.WriteConfigFile(
-		getConfig(t, tmplVars, "autodiscover", "take-over-filestream-input.yml"),
+		getConfig(t, tmplVars, "autodiscover", "take-over-filestream-input-k8s.yml"),
 	)
 	filebeat.Start()
 
-	// Wait for at least two extra event to be ingested
+	// Wait for at least two extra events to be ingested
 	require.EventuallyWithT(
 		t,
 		func(collect *assert.CollectT) {
-			lines := filebeat.CountFileLines(outputFile)
-			if lines <= logInputIngested+2 {
+			totalEvents := countEventsInES(t, index, 1000)
+			if totalEvents <= logInputIngested+2 {
 				collect.Errorf(
-					"Expecting more lines in the output than the %d from the first run",
+					"expecting more events in Elasticsearch than the %d from the first run",
 					logInputIngested,
 				)
 			}
@@ -162,9 +168,8 @@ func TestAutodiscoverFilestreamTakeOverDoesNotReingest(t *testing.T) {
 		time.Second,
 		"No new events ingested")
 
-	// Stop the container and get the total number of events generated
-	stopContainer(t, containerID)
-	generatedEvents := countContainerLogLines(t, containerID)
+	// Stop the pod and get the total number of events generated
+	generatedEvents := stopPodK8sAndCountLogs(t, kubeConfigPath, podName)
 
 	// Wait for Filebeat to fully ingest the file, we do it by waiting the file
 	// to be closed due to inactivity.
@@ -173,7 +178,18 @@ func TestAutodiscoverFilestreamTakeOverDoesNotReingest(t *testing.T) {
 		20*time.Second,
 		"Filebeat did not close the log file due to inactivity")
 
-	totalEventsIngested := filebeat.CountFileLines(outputFile)
+	// Wait until at least all published events can be found in Elasticsearch
+	require.Eventuallyf(
+		t,
+		func() bool { return countEventsInES(t, index, generatedEvents+100) >= generatedEvents },
+		30*time.Second,
+		200*time.Millisecond,
+		"did not ingest all generated events from pod %s", podName,
+	)
+
+	filebeat.Stop()
+
+	totalEventsIngested := countEventsInES(t, index, generatedEvents+100)
 
 	require.Equalf(
 		t,
@@ -351,53 +367,159 @@ func startFlogDocker(t *testing.T) string {
 	return resp.ID
 }
 
-func countContainerLogLines(t *testing.T, containerID string) int {
-	cli, err := docker.NewClient(client.DefaultDockerHost, nil, nil, logp.NewNopLogger())
+// startFlogKubernetesForTakeOver creates a Kind cluster and starts a flog pod
+// that writes one log line per second to stdout.
+func startFlogKubernetesForTakeOver(t *testing.T, workDir string) (kubeConfigPath, nodeName, podName string) {
+	uid := uuid.Must(uuid.NewV4()).String()
+
+	defer func() {
+		if t.Failed() {
+			t.Log("To see the Kind logs search for 'cluster.ProviderWithLogger' and uncomment it.")
+		}
+	}()
+
+	provider := cluster.NewProvider(
+	// Uncomment the next line to have Kind logs written to stderr.
+	// You will also have to import "sigs.k8s.io/kind/pkg/cmd"
+	// cluster.ProviderWithLogger(cmd.NewLogger()),
+	)
+
+	clusterName := fmt.Sprintf("test-cluster-%s", uid)
+	err := provider.Create(clusterName, cluster.CreateWithV1Alpha4Config(&v1alpha4.Cluster{}))
 	if err != nil {
-		t.Fatalf("cannot create Docker client: %s", err)
+		t.Fatalf("could not create cluster: %s", err)
 	}
 
-	logsReader, err := cli.ContainerLogs(t.Context(), containerID, container.LogsOptions{
-		ShowStdout: true,
-		ShowStderr: false,
-		Tail:       "all",
+	t.Cleanup(func() {
+		if err := provider.Delete(clusterName, ""); err != nil {
+			t.Logf("could not delete K8s cluster: %s", err)
+		}
 	})
+
+	time.Sleep(30 * time.Second)
+
+	var kubeConfig string
+	require.Eventually(t, func() bool {
+		kubeConfig, err = provider.KubeConfig(clusterName, false)
+		return err == nil
+	}, 30*time.Second, 100*time.Millisecond, "could not get kube config")
+
+	kubeConfigPath = filepath.Join(workDir, "kube-config")
+	if err = os.WriteFile(kubeConfigPath, []byte(kubeConfig), 0666); err != nil {
+		t.Fatalf("cannot write kube config file: %s", err)
+	}
+
+	config, err := clientcmd.RESTConfigFromKubeConfig([]byte(kubeConfig))
 	if err != nil {
-		t.Fatalf("cannot get stdout logs for container %q: %s", containerID, err)
-	}
-	defer logsReader.Close()
-
-	lineCount := 0
-	scanner := bufio.NewScanner(logsReader)
-	scanner.Buffer(make([]byte, 0, 64*1024), 1024*1024)
-	for scanner.Scan() {
-		lineCount++
+		t.Fatal(err)
 	}
 
-	if err := scanner.Err(); err != nil {
-		t.Fatalf("cannot scan stdout logs for container %q: %s", containerID, err)
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	return lineCount
+	podName = "flog-pod-" + uid
+	pod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      podName,
+			Namespace: "default",
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:  "flog",
+					Image: "mingrammer/flog",
+					Args:  []string{"-s", "1", "-d", "1", "-l"},
+				},
+			},
+		},
+	}
+
+	pod, err = clientset.CoreV1().Pods("default").Create(t.Context(), pod, metav1.CreateOptions{})
+	if err != nil {
+		t.Fatalf("could not create pod: %s", err)
+	}
+
+	t.Cleanup(func() {
+		ctx := context.Background()
+		if err := clientset.CoreV1().Pods("default").Delete(ctx, pod.Name, metav1.DeleteOptions{}); err != nil {
+			t.Logf("could not remove pod: %s", err)
+		}
+	})
+
+	require.Eventually(
+		t,
+		func() bool {
+			pod, err = clientset.CoreV1().Pods("default").Get(t.Context(), pod.Name, metav1.GetOptions{})
+			if err != nil {
+				return false
+			}
+			if pod.Status.Phase == corev1.PodRunning &&
+				len(pod.Status.ContainerStatuses) > 0 &&
+				pod.Spec.NodeName != "" {
+				nodeName = pod.Spec.NodeName
+				return true
+			}
+			return false
+		},
+		60*time.Second,
+		100*time.Millisecond,
+		"pod did not start within timeout",
+	)
+
+	return
 }
 
-func stopContainer(t *testing.T, containerID string) {
+// stopPodK8sAndCountLogs returns the number of log lines generated by the pod
+// and then deletes it.
+func stopPodK8sAndCountLogs(t *testing.T, kubeConfigPath, podName string) int {
+	data, err := os.ReadFile(kubeConfigPath)
+	if err != nil {
+		t.Fatalf("cannot read kube config: %s", err)
+	}
+	config, err := clientcmd.RESTConfigFromKubeConfig(data)
+	if err != nil {
+		t.Fatalf("cannot build REST config: %s", err)
+	}
+	cs, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		t.Fatalf("cannot create clientset: %s", err)
+	}
+
+	logsReader, err := cs.CoreV1().Pods("default").GetLogs(podName, &corev1.PodLogOptions{
+		Container: "flog",
+		Follow:    true,
+	}).Stream(t.Context())
+	if err != nil {
+		t.Fatalf("cannot get logs for pod %q: %s", podName, err)
+	}
+
+	t.Cleanup(func() { logsReader.Close() })
+
+	if err := cs.CoreV1().Pods("default").Delete(t.Context(), podName, metav1.DeleteOptions{}); err != nil {
+		t.Fatalf("cannot delete pod %q: %s", podName, err)
+	}
+
+	logLines := countReaderLines(t, logsReader)
+
+	return logLines
+}
+
+func countEventsInES(t *testing.T, index string, size int) int {
+	return len(integration.GetEventsMsgFromES(t, index, size))
+}
+
+func countReaderLines(t *testing.T, r io.Reader) int {
 	t.Helper()
-
-	cli, err := docker.NewClient(client.DefaultDockerHost, nil, nil, logp.NewNopLogger())
-	if err != nil {
-		t.Fatalf("cannot create Docker client: %s", err)
+	s := bufio.NewScanner(r)
+	s.Buffer(make([]byte, 0, 64*1024), 1024*1024)
+	n := 0
+	for s.Scan() {
+		n++
 	}
-
-	if err := cli.ContainerStop(t.Context(), containerID, container.StopOptions{}); err != nil {
-		t.Fatalf("cannot stop container %q: %s", containerID, err)
+	if err := s.Err(); err != nil {
+		t.Fatalf("cannot scan logs: %s", err)
 	}
-}
-
-func canReadContainerLogs(t *testing.T) {
-	_, err := os.Stat("/var/lib/docker/containers")
-	if err != nil {
-		t.Fatalf("Cannot read '/var/lib/docker/containers', this test usually"+
-			" needs to be run as root. Error: %s", err)
-	}
+	return n
 }
