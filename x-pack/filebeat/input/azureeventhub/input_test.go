395d54e80d7d1b4acdf74eda77ce5fad9900bd1e
Maurizio Branca <maurizio.branca@elastic.co>
2026-03-31 00:35:59 +0200

    Cleanup

--- x-pack/filebeat/input/azureeventhub/input_test.go
+++ x-pack/filebeat/input/azureeventhub/input_test.go
@@ -8,38 +8,9 @@ package azureeventhub
 
 import (
 	"sync"
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-
-	conf "github.com/elastic/elastic-agent-libs/config"
-	"github.com/elastic/elastic-agent-libs/logp"
 
 	"github.com/elastic/beats/v7/libbeat/beat"
 )
-
-func TestCreateWithProcessorV1FallsBackToV2(t *testing.T) {
-	logp.TestingSetup(logp.WithSelectors("azureeventhub"))
-	log := logp.NewLogger("azureeventhub")
-
-	manager := &eventHubInputManager{log: log}
-
-	config := conf.MustNewConfigFrom(map[string]interface{}{
-		"eventhub":                         "test-hub",
-		"connection_string":                "Endpoint=sb://test.servicebus.windows.net/;SharedAccessKeyName=test;SharedAccessKey=test",
-		"storage_account":                  "teststorage",
-		"storage_account_connection_string": "DefaultEndpointsProtocol=https;AccountName=teststorage;AccountKey=secret;EndpointSuffix=core.windows.net",
-		"processor_version":                "v1",
-	})
-
-	input, err := manager.Create(config)
-	require.NoError(t, err)
-	require.NotNil(t, input)
-
-	_, ok := input.(*eventHubInputV2)
-	assert.True(t, ok, "expected eventHubInputV2 when processor_version is v1")
-}
 
 // ackClient is a fake beat.Client that ACKs the published messages.
 type fakeClient struct {
