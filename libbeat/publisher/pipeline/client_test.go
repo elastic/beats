df60c845ec42dcca491af8aee70182dcc424807c
Fae Charlton <fae.charlton@elastic.co>
2026-04-09 12:56:33 -0400

    [libbeat] Make queue interfaces a generic with an explicit entry type (#49954)
    Remove the type definition of `queue.Entry` as an alias for `interface{}` and make the top-level queue interfaces take a generic type parameter for their entry type. Explicitly specify the queue in the Beats pipeline as a `queue.Queue[publisher.Event]` (the actual event type used everywhere except some test code). Generalize the memory queue's creation helpers to accept any type parameter for their entries.
    
    This is a functional no-op that makes the queue API expect and return explicit types, thereby removing a number of type conversions / checks and preventing accidental use of related types (`beat.Event`, `*publisher.Event`...) when writing new code. There are other helpers adjacent to the queue that would be straightforward to generalize as well, but for the most part I preferred explicitly specifying `publisher.Event` for everything except the memory queue itself (which was intentionally written to be generic even though Go did not support generics at that time), at least until there is a use case to do the same with other components.

--- libbeat/publisher/pipeline/client_test.go
+++ libbeat/publisher/pipeline/client_test.go
@@ -45,7 +45,7 @@ import (
 	"github.com/elastic/elastic-agent-libs/monitoring"
 )
 
-func makePipeline(t *testing.T, settings Settings, qu queue.Queue) *Pipeline {
+func makePipeline(t *testing.T, settings Settings, qu queue.Queue[publisher.Event]) *Pipeline {
 	t.Helper()
 	logger := logptest.NewTestingLogger(t, "")
 	p, err := New(beat.Info{Logger: logger},
@@ -94,7 +94,7 @@ func TestClient(t *testing.T) {
 		l := logptest.NewTestingLogger(t, "")
 
 		// a small in-memory queue with a very short flush interval
-		q := memqueue.NewQueue(l, nil, memqueue.Settings{
+		q := memqueue.NewQueue[publisher.Event](l, nil, memqueue.Settings{
 			Events:        5,
 			MaxGetRequest: 1,
 			FlushTimeout:  time.Millisecond,
@@ -136,8 +136,7 @@ func TestClient(t *testing.T) {
 					continue
 				}
 				for i := 0; i < batch.Count(); i++ {
-					//nolint:errcheck // it always succeeds
-					e := batch.Entry(i).(publisher.Event)
+					e := batch.Entry(i)
 					received = append(received, e.Content)
 				}
 				batch.Done()
@@ -199,8 +198,7 @@ func TestClient(t *testing.T) {
 
 func TestClientWaitClose(t *testing.T) {
 	logger := logptest.NewTestingLogger(t, "")
-
-	q := memqueue.NewQueue(logger, nil, memqueue.Settings{Events: 1}, 0, nil)
+	q := memqueue.NewQueue[publisher.Event](logger, nil, memqueue.Settings{Events: 1}, 0, nil)
 	pipeline := makePipeline(t, Settings{}, q)
 	defer pipeline.Close()
 
@@ -404,7 +402,7 @@ func testInputMetrics(t *testing.T, beatInfo beat.Info, clientCfg beat.ClientCon
 
 	cc, ok := c.(*client)
 	require.True(t, ok, "pipeline.ConnectWith return value cannot be cast to client")
-	cc.producer = &testProducer{publish: func(try bool, event queue.Entry) (queue.EntryID, bool) {
+	cc.producer = &testProducer{publish: func(try bool, event publisher.Event) (queue.EntryID, bool) {
 		return queue.EntryID(1), true
 	}}
 
