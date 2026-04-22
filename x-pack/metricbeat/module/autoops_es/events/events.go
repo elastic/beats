df5c89750348dda2a67461e0bf6932eccb3df8ab
Chris Earle <pickypg@users.noreply.github.com>
2026-04-22 08:50:18 -0600

    [AutoOps] Use UUIDv7 in place of UUIDv4 and drop dashes (#50078)
    This changes over from using random and unpredictable UUIDv4 to
    UUIDv7. While changing this, it also drops the dashes to avoid
    unneeded repetition (dashes can be reconstituted if needed given
    their predictable location).
    
    This also drops usage of "random" Transaction IDs and simply stops
    sending them. Random Transaction IDs provide no value as it
    implies the data is not grouped and thus this even further reduces
    sending unneeded data.
    
    (cherry picked from commit 26db6c4dc5afd4643519019f253516d2f2e8271c)
    
    # Conflicts:
    #	x-pack/metricbeat/module/autoops_es/events/events.go

--- x-pack/metricbeat/module/autoops_es/events/events.go
+++ x-pack/metricbeat/module/autoops_es/events/events.go
@@ -10,9 +10,28 @@ import (
 	"github.com/elastic/elastic-agent-libs/mapstr"
 )
 
-// Create a new Metricbeat Event object with a random Transaction ID (so it has no predictable relationship to other events outside of @timestamp).
-func CreateEventWithRandomTransactionId(info *utils.ClusterInfo, metricSetFields mapstr.M) mb.Event {
-	return CreateEvent(info, metricSetFields, utils.NewUUIDV4())
+// Create a new Metricbeat Event object without a Transaction ID (so it has no predictable relationship to other events outside of @timestamp).
+func CreateEventWithoutTransactionId(info *utils.ClusterInfo, metricSetFields mapstr.M) mb.Event {
+	return mb.Event{
+		MetricSetFields: metricSetFields,
+		ModuleFields: mapstr.M{
+			"cluster": mapstr.M{
+				"id":      info.ClusterID,
+				"name":    info.ClusterName,
+				"version": info.Version.Number.String(),
+			},
+		},
+		RootFields: mapstr.M{
+			"event": mapstr.M{
+				"kind": "metric",
+			},
+			"orchestrator": mapstr.M{
+				"resource": mapstr.M{
+					"id": utils.GetResourceID(),
+				},
+			},
+		},
+	}
 }
 
 // Create a new Metricbeat Event object containing expected fields and the dynamic portion.
@@ -48,20 +67,34 @@ func ReportEvent(r mb.ReporterV2, event mb.Event, index int, total int) {
 	r.Event(event)
 }
 
+<<<<<<< HEAD
 // Report Metricbeat Events marked with the same transaction
 func ReportEvents(r mb.ReporterV2, events []mb.Event) {
 	var total = len(events)
 
 	for index, event := range events {
+=======
+// Create a new Metricbeat Events without a Transaction ID (so it has no predictable relationship to other events outside of @timestamp) and report them.
+// This should only be used when it happens to be an array of events that have no relationship to each other.
+func CreateAndReportEventsWithoutTransactionId(r mb.ReporterV2, info *utils.ClusterInfo, metricSets []mapstr.M) {
+	var total = len(metricSets)
+
+	for index, metricSetFields := range metricSets {
+		event := CreateEventWithoutTransactionId(info, metricSetFields)
+
+>>>>>>> 26db6c4dc ([AutoOps] Use UUIDv7 in place of UUIDv4 and drop dashes (#50078))
 		ReportEvent(r, event, index, total)
 	}
 }
 
+<<<<<<< HEAD
 // Create a new Metricbeat Events with a shared, random Transaction ID
 func CreateAndReportEventsWithRandomTransactionId(r mb.ReporterV2, info *utils.ClusterInfo, metricSets []mapstr.M) {
 	CreateAndReportEvents(r, info, metricSets, utils.NewUUIDV4())
 }
 
+=======
+>>>>>>> 26db6c4dc ([AutoOps] Use UUIDv7 in place of UUIDv4 and drop dashes (#50078))
 // Create a new Metricbeat Events
 func CreateAndReportEvents(r mb.ReporterV2, info *utils.ClusterInfo, metricSets []mapstr.M, transactionId string) {
 	var total = len(metricSets)
