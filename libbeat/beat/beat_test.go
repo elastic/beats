563d1f13022df8deb53e790dae9ad471b5a90149
michal <michal.pristas@elastic.co>
2026-04-24 11:37:42 +0200

    no need for info change

--- libbeat/beat/beat_test.go
+++ libbeat/beat/beat_test.go
@@ -104,12 +104,6 @@ func TestUserAgentString(t *testing.T) {
 			beat:             &Beat{Info: Info{Beat: "testbeat", FIPSDistribution: false}, Manager: nil},
 			expectedComments: []string{},
 		},
-		{
-			name: "agentless",
-			beat: &Beat{Info: Info{Beat: "testbeat", IsAgentless: true},
-				Manager: testManager{isEnabled: false}},
-			expectedComments: []string{"Standalone", "agentless"},
-		},
 	}
 
 	// User-Agent will take the form of
