9037ea8896fd49b2ddc2b6f7e39186f9b95058b1
wucm667 <109257021+wucm667@users.noreply.github.com>
2026-05-21 06:07:05 +0800

    x-pack/filebeat/input/{cel,httpjson,entityanalytics/provider/okta}: fix Okta OAuth2 token refresh for jwk_pem and jwk_file auth (#50433)
    The oktaTokenSource struct always stored the JWK JSON value for token
    refresh, which is nil when using jwk_file or jwk_pem authentication.
    When the token expired, Token() passed empty data to generateOktaJWT,
    causing authentication failures.
    
    Add an oktaJWKPEM field to oktaTokenSource and populate the correct
    field based on the configured authentication mode. Token() now
    dispatches to generateOktaJWTPEM when PEM data is present.
    
    (cherry picked from commit fc31b6e5a5fd7710c6d0114c1fbe45dd71d1ec72)
    
    # Conflicts:
    #	x-pack/filebeat/input/entityanalytics/provider/okta/oauth2.go
    #	x-pack/filebeat/input/entityanalytics/provider/okta/oauth2_test.go

--- x-pack/filebeat/input/cel/config_okta_auth.go
+++ x-pack/filebeat/input/cel/config_okta_auth.go
@@ -32,11 +32,12 @@ import (
 // oktaTokenSource is a custom implementation of the oauth2.TokenSource interface.
 // For more information, see https://pkg.go.dev/golang.org/x/oauth2#TokenSource
 type oktaTokenSource struct {
-	mu      sync.Mutex
-	ctx     context.Context
-	conf    *oauth2.Config
-	token   *oauth2.Token
-	oktaJWK []byte
+	mu         sync.Mutex
+	ctx        context.Context
+	conf       *oauth2.Config
+	token      *oauth2.Token
+	oktaJWK    []byte
+	oktaJWKPEM string
 }
 
 // fetchOktaOauthClient fetches an OAuth2 client using the Okta JWK credentials.
@@ -101,10 +102,11 @@ func (o *oAuth2Config) fetchOktaOauthClient(ctx context.Context) (*http.Client, 
 	}
 
 	tokenSource := &oktaTokenSource{
-		conf:    conf,
-		ctx:     ctx,
-		oktaJWK: o.OktaJWKJSON,
-		token:   token,
+		conf:       conf,
+		ctx:        ctx,
+		oktaJWK:    o.OktaJWKJSON,
+		oktaJWKPEM: o.OktaJWKPEM,
+		token:      token,
 	}
 	// reuse the tokenSource to refresh the token (automatically calls
 	// the custom Token() method when token is no longer valid).
@@ -122,7 +124,13 @@ func (ts *oktaTokenSource) Token() (*oauth2.Token, error) {
 	ts.mu.Lock()
 	defer ts.mu.Unlock()
 
-	oktaJWT, err := generateOktaJWT(ts.oktaJWK, ts.conf)
+	var oktaJWT string
+	var err error
+	if ts.oktaJWKPEM != "" {
+		oktaJWT, err = generateOktaJWTPEM(ts.oktaJWKPEM, ts.conf)
+	} else {
+		oktaJWT, err = generateOktaJWT(ts.oktaJWK, ts.conf)
+	}
 	if err != nil {
 		return nil, fmt.Errorf("error generating Okta JWT: %w", err)
 	}
