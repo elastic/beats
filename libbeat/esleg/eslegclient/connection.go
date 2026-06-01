8a21a6e5f6f6bf162f378063834bb06da3098d16
michal <michal.pristas@elastic.co>
2026-06-01 14:46:12 +0200

    mage check

--- libbeat/esleg/eslegclient/connection.go
+++ libbeat/esleg/eslegclient/connection.go
@@ -31,9 +31,9 @@ import (
 	"go.elastic.co/apm/module/apmelasticsearch/v2"
 
 	"github.com/elastic/beats/v7/libbeat/common"
-	"github.com/elastic/beats/v7/libbeat/version"
 	"github.com/elastic/beats/v7/libbeat/common/productorigin"
 	"github.com/elastic/beats/v7/libbeat/common/transport/kerberos"
+	"github.com/elastic/beats/v7/libbeat/version"
 	cfg "github.com/elastic/elastic-agent-libs/config"
 	"github.com/elastic/elastic-agent-libs/logp"
 	"github.com/elastic/elastic-agent-libs/testing"
