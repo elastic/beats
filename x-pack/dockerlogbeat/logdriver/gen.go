6ab9381c421816311fdef7833e6fa33ef569aab5
Lee E. Hinman <lee.e.hinman@elastic.co>
2026-06-05 15:19:55 -0500

    fix comment

--- x-pack/dockerlogbeat/logdriver/gen.go
+++ x-pack/dockerlogbeat/logdriver/gen.go
@@ -2,7 +2,7 @@
 // or more contributor license agreements. Licensed under the Elastic License;
 // you may not use this file except in compliance with the Elastic License.
 
-// To regenerate entry.pb.go, run: mage update
+// To regenerate entry.pb.go, run: mage generate
 // (requires protoc and protoc-gen-gogofaster: go install github.com/gogo/protobuf/protoc-gen-gogofaster@latest)
 
 //go:generate protoc --gogofaster_out=import_path=logdriver:. entry.proto
