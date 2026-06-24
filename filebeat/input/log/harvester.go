96d28d30aadaeb73b0759ab68624caaf7bc5bf84
Orestis Floros <orestis.floros@elastic.co>
2026-06-24 12:12:48 +0200

    Fix redundant "later again" in harvester runtime error
    Remove the redundant "again" in the log harvester runtime error "Aborting
    harvesting and retrying file later again" -> "Aborting harvesting and retrying
    file later".
    
    Only files owned by @elastic/elastic-agent-data-plane were changed; the PANW
    panos repeatcnt field description from the same issue belongs to another team.
    
    Relates to #50159

--- filebeat/input/log/harvester.go
+++ filebeat/input/log/harvester.go
@@ -560,7 +560,7 @@ func (h *Harvester) validateFile(f *os.File) error {
 
 	// Compares the stat of the opened file to the state given by the input. Abort if not match.
 	if !os.SameFile(h.state.Fileinfo, info) {
-		return errors.New("file info is not identical with opened file. Aborting harvesting and retrying file later again")
+		return errors.New("file info is not identical with opened file. Aborting harvesting and retrying file later")
 	}
 
 	h.encoding, err = h.encodingFactory(f)
