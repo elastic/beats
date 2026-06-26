e127dfb071537a93d0fd36c9915f444ba084120c
Orestis Floros <orestis.floros@elastic.co>
2026-06-26 11:46:09 +0200

    osquerybeat: fix data race on osquery runner cancel function (#51520)
    osquerybeat: fix data race on osquery runner cancel function
    
    The `process` closure in `osqueryRunner.Run` reads the `cn` (context cancel
    function) variable without holding the mutex, while the goroutine running
    osqueryd can concurrently set `cn = nil` through `cancel()`. This is a data
    race that can cause a nil-pointer crash when osquery restarts after a
    configuration change.
    
    Fix by guarding every read and write of `cn` in `process` with the existing
    mutex
    
    (cherry picked from commit ecb945502da720d999f18f092fa4683c537a7666)
    
    # Conflicts:
    #	x-pack/osquerybeat/beater/osquery_runner.go

--- x-pack/osquerybeat/beater/osquery_runner.go
+++ x-pack/osquerybeat/beater/osquery_runner.go
@@ -44,11 +44,11 @@ func (r *osqueryRunner) Run(parentCtx context.Context, runfn osqueryRunFunc) err
 	var mx sync.Mutex
 	cancel := func() {
 		mx.Lock()
+		defer mx.Unlock()
 		if cn != nil {
 			cn()
 			cn = nil
 		}
-		defer mx.Unlock()
 	}
 
 	// Cleanup on exit
@@ -63,29 +63,44 @@ func (r *osqueryRunner) Run(parentCtx context.Context, runfn osqueryRunFunc) err
 		lastKnownInputs = inputs
 		newFlags := config.GetOsqueryOptions(inputs)
 
+<<<<<<< HEAD
 		// If Osqueryd is running and flags are different: stop osquery
 		if cn != nil && !osqd.FlagsAreSame(flags, newFlags) {
+=======
+		// cn is cleared by the spawned goroutine's cancel() on exit, so guard it with mx.
+		mx.Lock()
+		running := cn != nil
+		mx.Unlock()
+
+		// If Osqueryd is running and flags are different or log level changed: stop osquery
+		if running && (!osqd.FlagsAreSame(flags, newFlags) || logLevel != newLogLevel) {
+>>>>>>> ecb945502 (osquerybeat: fix data race on osquery runner cancel function (#51520))
 			r.log.Info("Osquery is running and options changed, stop osqueryd")
 
 			// Cancel context
 			cancel()
 
-			// Wait until osquery runner exists
+			// Wait until osquery runner exits
 			wg.Wait()
 		}
 
+<<<<<<< HEAD
 		// Set the flags to use
 		flags = newFlags
 
+=======
+		mx.Lock()
+>>>>>>> ecb945502 (osquerybeat: fix data race on osquery runner cancel function (#51520))
 		// Start osqueryd if not running
 		if cn == nil {
 			r.log.Info("Start osqueryd")
+
+			flags = newFlags
+			logLevel = newLogLevel
 			inputCh = make(chan []config.InputConfig, 1)
 			ctx, cn = context.WithCancel(parentCtx)
 
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
+			wg.Go(func() {
 				err := runfn(ctx, flags, inputCh)
 
 				// Reset cancellable
@@ -94,8 +109,9 @@ func (r *osqueryRunner) Run(parentCtx context.Context, runfn osqueryRunFunc) err
 				// Forward error to main loop
 				r.log.Debugf("Forward osquery run error to the main runner loop: %v", err)
 				errCh <- err
-			}()
+			})
 		}
+		mx.Unlock()
 
 		select {
 		case inputCh <- inputs:
