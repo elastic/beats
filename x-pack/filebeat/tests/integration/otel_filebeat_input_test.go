a9eb0de080962997da3b1ab65d98118da2aed1d0
Khushi Jain <khushi.jain@elastic.co>
2026-06-29 15:15:53 +0530

    add mqtt and integ test

--- x-pack/filebeat/tests/integration/otel_filebeat_input_test.go
+++ x-pack/filebeat/tests/integration/otel_filebeat_input_test.go
@@ -21,6 +21,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	mqtttestutil "github.com/elastic/beats/v7/filebeat/input/mqtt/testutil"
 	redistestutil "github.com/elastic/beats/v7/filebeat/input/redis/testutil"
 	"github.com/elastic/beats/v7/libbeat/tests/integration"
 	"github.com/elastic/beats/v7/x-pack/otel/oteltest"
@@ -44,8 +45,8 @@ func TestCELInputOTelE2E(t *testing.T) {
 	user := host.User.Username()
 	password, _ := host.User.Password()
 
-	otelNamespace := fmt.Sprintf("%x", uuid.Must(uuid.NewV4()))
-	fbNamespace := fmt.Sprintf("%x", uuid.Must(uuid.NewV4()))
+	otelNamespace := strings.ReplaceAll(uuid.Must(uuid.NewV4()).String(), "-", "")
+	fbNamespace := strings.ReplaceAll(uuid.Must(uuid.NewV4()).String(), "-", "")
 
 	otelIndex := "logs-integration-" + otelNamespace
 	fbIndex := "logs-integration-" + fbNamespace
@@ -410,8 +411,8 @@ func TestRedisInputOTelE2E(t *testing.T) {
 	user := host.User.Username()
 	password, _ := host.User.Password()
 
-	otelNamespace := fmt.Sprintf("%x", uuid.Must(uuid.NewV4()))
-	fbNamespace := fmt.Sprintf("%x", uuid.Must(uuid.NewV4()))
+	otelNamespace := strings.ReplaceAll(uuid.Must(uuid.NewV4()).String(), "-", "")
+	fbNamespace := strings.ReplaceAll(uuid.Must(uuid.NewV4()).String(), "-", "")
 
 	otelIndex := "logs-integration-" + otelNamespace
 	fbIndex := "logs-filebeat-" + fbNamespace
@@ -626,6 +627,228 @@ service:
 	}
 
 	// compare docs
+	oteltest.AssertMapsEqual(t, filebeatDoc, otelDoc, ignoredFields, "expected documents to be equal")
+}
+
+func TestMQTTInputOTelE2E(t *testing.T) {
+	integration.EnsureESIsRunning(t)
+
+	otelHome := t.TempDir()
+
+	host := integration.GetESURL(t, "http")
+	user := host.User.Username()
+	password, _ := host.User.Password()
+
+	otelNamespace := fmt.Sprintf("%x", uuid.Must(uuid.NewV4()))
+	fbNamespace := fmt.Sprintf("%x", uuid.Must(uuid.NewV4()))
+
+	otelIndex := "logs-integration-" + otelNamespace
+	fbIndex := "logs-integration-" + fbNamespace
+	mqttInputTestMsg := "mqtt-input-otel-e2e-test-event"
+
+	topic := fmt.Sprintf("test-mqtt-input-%s", uuid.Must(uuid.NewV4()).String())
+
+	emitCtx, emitCancel := context.WithCancel(t.Context())
+	t.Cleanup(emitCancel)
+
+	publisher := mqtttestutil.CreatePublisher(t, "mqtt-test-publisher")
+	mqtttestutil.EmitMessages(t, emitCtx, publisher, topic, mqttInputTestMsg)
+
+	type options struct {
+		Index    string
+		ESURL    string
+		Username string
+		Password string
+		Broker   string
+		Topic    string
+		ClientID string
+		PathHome string
+	}
+
+	mqttFilebeatConfig := `filebeat.inputs:
+- type: mqtt
+  id: mqtt-input-e2e
+  hosts:
+    - {{ .Broker }}
+  topics:
+    - {{ .Topic }}
+  client_id: {{ .ClientID }}
+
+output:
+  elasticsearch:
+    hosts:
+      - {{ .ESURL }}
+    username: {{ .Username }}
+    password: {{ .Password }}
+    index: {{ .Index }}
+
+queue.mem.flush.timeout: 0s
+setup.template.enabled: false
+processors:
+    - add_host_metadata: ~
+    - add_cloud_metadata: ~
+    - add_docker_metadata: ~
+    - add_kubernetes_metadata: ~
+`
+
+	mqttOTelConfig := `exporters:
+    elasticsearch:
+        auth:
+            authenticator: beatsauth
+        compression: gzip
+        compression_params:
+            level: 1
+        endpoints:
+            - {{ .ESURL }}
+        logs_index: {{ .Index }}
+        max_conns_per_host: 1
+        password: {{ .Password }}
+        retry:
+            enabled: true
+            initial_interval: 1s
+            max_interval: 1m0s
+            max_retries: 3
+        sending_queue:
+            batch:
+                flush_timeout: 10s
+                max_size: 1600
+                min_size: 0
+                sizer: items
+            block_on_overflow: true
+            enabled: true
+            num_consumers: 1
+            queue_size: 3200
+            wait_for_result: true
+        user: {{ .Username }}
+extensions:
+    beatsauth:
+        idle_connection_timeout: 3s
+        proxy_disable: false
+        timeout: 1m30s
+receivers:
+    filebeatreceiver:
+        filebeat:
+            inputs:
+                - type: mqtt
+                  id: mqtt-input-e2e
+                  hosts:
+                    - {{ .Broker }}
+                  topics:
+                    - {{ .Topic }}
+                  client_id: {{ .ClientID }}
+        processors:
+            - add_host_metadata: ~
+            - add_cloud_metadata: ~
+            - add_docker_metadata: ~
+            - add_kubernetes_metadata: ~
+        queue.mem.flush.timeout: 0s
+        setup.template.enabled: false
+        path.home: {{ .PathHome }}
+        management.otel.enabled: true
+service:
+    extensions:
+        - beatsauth
+    pipelines:
+        logs:
+            exporters:
+                - elasticsearch
+            receivers:
+                - filebeatreceiver
+    telemetry:
+        metrics:
+            level: none
+`
+
+	optionsValue := options{
+		ESURL:    fmt.Sprintf("%s://%s", host.Scheme, host.Host),
+		Username: user,
+		Password: password,
+		Broker:   mqtttestutil.HostPort(),
+		Topic:    topic,
+		PathHome: otelHome,
+	}
+
+	var configBuffer bytes.Buffer
+	optionsValue.ClientID = "otel-mqtt-input-e2e"
+	optionsValue.Index = otelIndex
+	require.NoError(t, template.Must(template.New("config").Parse(mqttOTelConfig)).Execute(&configBuffer, optionsValue))
+
+	oteltestcol.New(t, configBuffer.String())
+
+	configBuffer.Reset()
+
+	optionsValue.ClientID = "fb-mqtt-input-e2e"
+	optionsValue.Index = fbIndex
+	require.NoError(t, template.Must(template.New("config").Parse(mqttFilebeatConfig)).Execute(&configBuffer, optionsValue))
+
+	filebeat := integration.NewBeat(
+		t,
+		"filebeat",
+		"../../filebeat.test",
+	)
+	filebeat.WriteConfigFile(configBuffer.String())
+	filebeat.Start()
+	defer filebeat.Stop()
+
+	es := integration.GetESClient(t, "http")
+
+	t.Cleanup(func() {
+		deleteMQTTDataStreamsFromES(t, es, []string{
+			otelIndex,
+			fbIndex,
+		})
+	})
+
+	rawQuery := map[string]any{
+		"query": map[string]any{
+			"bool": map[string]any{
+				"must": []map[string]any{
+					{
+						"match_phrase": map[string]any{
+							"input.type": "mqtt",
+						},
+					},
+					{
+						"match_phrase": map[string]any{
+							"message": mqttInputTestMsg,
+						},
+					},
+				},
+			},
+		},
+		"sort": []map[string]any{
+			{"@timestamp": map[string]any{"order": "asc"}},
+		},
+	}
+
+	var filebeatDocs estools.Documents
+	var otelDocs estools.Documents
+	var err error
+
+	require.EventuallyWithTf(t,
+		func(ct *assert.CollectT) {
+			findCtx, findCancel := context.WithTimeout(t.Context(), 900*time.Millisecond)
+			defer findCancel()
+
+			otelDocs, err = estools.PerformQueryForRawQuery(findCtx, rawQuery, ".ds-"+otelIndex+"*", es)
+			assert.NoError(ct, err)
+			assert.GreaterOrEqual(ct, otelDocs.Hits.Total.Value, 1, "expected at least 1 otel document, got %d", otelDocs.Hits.Total.Value)
+
+			filebeatDocs, err = estools.PerformQueryForRawQuery(findCtx, rawQuery, ".ds-"+fbIndex+"*", es)
+			assert.NoError(ct, err)
+			assert.GreaterOrEqual(ct, filebeatDocs.Hits.Total.Value, 1, "expected at least 1 filebeat document, got %d", filebeatDocs.Hits.Total.Value)
+		},
+		3*time.Minute, 1*time.Second, "expected at least 1 document for both filebeat and otel modes")
+
+	filebeatDoc := filebeatDocs.Hits.Hits[0].Source
+	otelDoc := otelDocs.Hits.Hits[0].Source
+	ignoredFields := []string{
+		"@timestamp",
+		"agent.ephemeral_id",
+		"agent.id",
+		"mqtt.message_id",
+	}
+
 	oteltest.AssertMapsEqual(t, filebeatDoc, otelDoc, ignoredFields, "expected documents to be equal")
 }
 
