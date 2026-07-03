0f52153d434c2bb1d9555de22daed5c4b48ae427
Khushi Jain <khushi.jain@elastic.co>
2026-07-03 11:43:16 +0530

    remove it

--- x-pack/otel/oteltestcol/collector.go
+++ x-pack/otel/oteltestcol/collector.go
@@ -92,7 +92,7 @@ func New(tb testing.TB, configYAML string) *Collector {
 
 	require.Eventually(tb, func() bool {
 		return col.GetState() == otelcol.StateRunning
-	}, 15*time.Second, 10*time.Millisecond, "Collector did not start in time")
+	}, 10*time.Second, 10*time.Millisecond, "Collector did not start in time")
 
 	return &Collector{collector: col, observer: observer}
 }
