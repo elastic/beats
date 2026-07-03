9f1ba58beed1bd55d45961924f29e707a89d78e5
Orestis Floros <orestis.floros@elastic.co>
2026-07-03 07:01:16 +0200

    fix(filestream): honor offset-0 harvester close in SizeOrBytesIngested (#51675)
    When a harvester was closed before ingesting anything (for example while
    still in its initial backoff) it reported an offset of 0. The watcher
    could not tell this genuine 0 apart from "no offset reported", so
    SizeOrBytesIngested returned the file size instead of 0. The write
    comparison prevDesc.SizeOrBytesIngested() < fd.Info.Size() was then
    false, no write event was emitted, no new harvester was started, and the
    file's contents were never ingested, causing silent data loss.
    
    Track whether an offset was set explicitly with a bytesIngestedSet flag
    so an ingested offset of 0 is honored. The watcher now emits a write
    event and restarts the harvester in this case.
    
    (cherry picked from commit 8fc2ff170abddbe1eedf62e9f7607b00a5873191)
    
    # Conflicts:
    #	filebeat/input/filestream/fswatch_integration_test.go
    #	filebeat/input/filestream/internal/input-logfile/fswatch.go

--- filebeat/input/filestream/internal/input-logfile/fswatch.go
+++ filebeat/input/filestream/internal/input-logfile/fswatch.go
@@ -64,21 +64,32 @@ type FileDescriptor struct {
 	Info file.ExtendedFileInfo
 	// Fingerprint is a computed hash of the file header
 	Fingerprint string
+<<<<<<< HEAD
 	// bytesIngested is the number of bytes already ingested by the harvester
 	// for this file
+=======
+	// GZIP indicates if the file is compressed with GZIP.
+	GZIP bool
+
+	// bytesIngested is the number of bytes already ingested by the harvester for this file.
+>>>>>>> 8fc2ff170 (fix(filestream): honor offset-0 harvester close in SizeOrBytesIngested (#51675))
 	bytesIngested int64
+	// bytesIngestedSet distinguishes an explicit ingested offset of 0 (a harvester closed before
+	// ingesting anything) from bytesIngested never having been set.
+	bytesIngestedSet bool
 }
 
 // SetBytesIngested allows for setting a size that is different than the one in Info
 func (fd *FileDescriptor) SetBytesIngested(s int64) {
 	fd.bytesIngested = s
+	fd.bytesIngestedSet = true
 }
 
 // SizeOrBytesIngested returns the bytes ingested for the file or its size.
-// If [SetBytesIngested] has been called with a value other
-// than zero, the bytes ingested is returned, otherwise Info.Size() is returned.
+// If [SetBytesIngested] has been called, the bytes ingested is returned
+// (including a value of zero), otherwise Info.Size() is returned.
 func (fd FileDescriptor) SizeOrBytesIngested() int64 {
-	if fd.bytesIngested != 0 {
+	if fd.bytesIngestedSet {
 		return fd.bytesIngested
 	}
 
