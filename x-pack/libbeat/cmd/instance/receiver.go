f98853a2da2513861054b52ee77fd775f7ff58e9
Lee E Hinman <57081003+leehinman@users.noreply.github.com>
2026-07-21 14:07:19 -0500

    Fix beat receiver Shutdown hanging when Start fails before the beater is launched (#52106)
    * fix nil runDone channel
    
    * update changelog fragment
    
    * fix linter issue
    
    * replace trySyncPath
    
    (cherry picked from commit 16aa64e0e366847b14117608aecf5531d3d07e5a)
    
    # Conflicts:
    #	x-pack/libbeat/cmd/instance/receiver.go
    #	x-pack/libbeat/cmd/instance/receiver_test.go

--- x-pack/libbeat/cmd/instance/receiver.go
+++ x-pack/libbeat/cmd/instance/receiver.go
@@ -97,14 +97,30 @@ func NewBeatReceiver(ctx context.Context, b *instance.Beat, creator beat.Creator
 		return BeatReceiver{}, fmt.Errorf("error getting %s creator:%w", b.Info.Beat, err)
 	}
 	return BeatReceiver{
+<<<<<<< HEAD
 		beat:   b,
 		beater: beater,
 		Logger: b.Info.Logger,
+=======
+		beat:                b,
+		beater:              beater,
+		Logger:              b.Info.Logger,
+		bridge:              bridge,
+		releaseSystemBridge: releaseSystem,
+		runDone:             make(chan error, 1),
+>>>>>>> 16aa64e0e (Fix beat receiver Shutdown hanging when Start fails before the beater is launched (#52106))
 	}, nil
 }
 
 // BeatReceiver.Start() starts the beat receiver.
-func (br *BeatReceiver) Start(host component.Host) error {
+func (br *BeatReceiver) Start(host component.Host) (retErr error) {
+	// If Start returns an error before the beater.Run goroutine is launched,
+	// signal runDone so that a concurrent Shutdown does not block forever on it.
+	defer func() {
+		if retErr != nil {
+			br.runDone <- retErr
+		}
+	}()
 	var groupReporter otelstatus.RunnerReporter
 	if w, ok := br.beater.(cfgfile.WithOtelFactoryWrapper); ok {
 		groupReporter = otelstatus.NewGroupStatusReporter(host)
@@ -149,6 +165,7 @@ func (br *BeatReceiver) Start(host component.Host) error {
 		br.reporter = rep
 	}
 
+<<<<<<< HEAD
 	br.beat.Manager.SetStopCallback(func() {
 		if c, ok := br.beat.Publisher.(io.Closer); ok {
 			if err := c.Close(); err != nil {
@@ -158,6 +175,8 @@ func (br *BeatReceiver) Start(host component.Host) error {
 	})
 
 	br.runDone = make(chan error, 1)
+=======
+>>>>>>> 16aa64e0e (Fix beat receiver Shutdown hanging when Start fails before the beater is launched (#52106))
 	go func() {
 		err := br.beater.Run(&br.beat.Beat)
 		if err != nil {
