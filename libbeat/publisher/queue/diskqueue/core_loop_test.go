8d3dccfaa266c85735ea0254f3ab16e56c5b1bd7
Tiago Queiroz <tiago.queiroz@elastic.co>
2026-08-17 17:07:52 -0400

    make check

--- libbeat/publisher/queue/diskqueue/core_loop_test.go
+++ libbeat/publisher/queue/diskqueue/core_loop_test.go
@@ -24,6 +24,9 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
 	"github.com/elastic/beats/v7/libbeat/publisher"
 	"github.com/elastic/beats/v7/libbeat/publisher/queue"
 	"github.com/elastic/beats/v7/libbeat/publisher/queue/queuetest"
@@ -32,8 +35,6 @@ import (
 	"github.com/elastic/elastic-agent-libs/monitoring"
 	"github.com/elastic/elastic-agent-libs/paths"
 	"github.com/elastic/elastic-agent-libs/testing/fs"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestHandleProducerWriteRequest(t *testing.T) {
